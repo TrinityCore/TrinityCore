1e1ba89c4960a76a61ceeeb90d0194b46872ec52
XTZGZoReX <none@none>
2008-10-10 15:20:27 -0500

    [svn] * Fixing some typos in SQL files. * Applying proper structure to SQL updates. * Fixing ImpConfig compile problems. * Moving INSTALL to INSTALL.linux to avoid autoconf collisions.
    --HG--
    branch : trunk
    rename : INSTALL => INSTALL.linux
    rename : sql/updates/10_instantiated_battlegrounds.sql => sql/updates/10_world.sql
    rename : sql/updates/11_arena_points_characters.sql => sql/updates/11_characters.sql

--- sql/drop_mysql.sql
+++ sql/drop_mysql.sql
@@ -1,20 +1,20 @@
-REVOKE ALL PRIVILEGES ON * . * FROM 'mangos'@'localhost';
+REVOKE ALL PRIVILEGES ON * . * FROM 'trinity'@'localhost';
 
-REVOKE ALL PRIVILEGES ON `mangos` . * FROM 'mangos'@'localhost';
+REVOKE ALL PRIVILEGES ON `mangos` . * FROM 'trinity'@'localhost';
 
-REVOKE GRANT OPTION ON `mangos` . * FROM 'mangos'@'localhost';
+REVOKE GRANT OPTION ON `mangos` . * FROM 'trinity'@'localhost';
 
-REVOKE ALL PRIVILEGES ON `characters` . * FROM 'mangos'@'localhost';
+REVOKE ALL PRIVILEGES ON `characters` . * FROM 'trinity'@'localhost';
 
-REVOKE GRANT OPTION ON `characters` . * FROM 'mangos'@'localhost';
+REVOKE GRANT OPTION ON `characters` . * FROM 'trinity'@'localhost';
 
-REVOKE ALL PRIVILEGES ON `realmd` . * FROM 'mangos'@'localhost';
+REVOKE ALL PRIVILEGES ON `realmd` . * FROM 'trinity'@'localhost';
 
-REVOKE GRANT OPTION ON `realmd` . * FROM 'mangos'@'localhost';
+REVOKE GRANT OPTION ON `realmd` . * FROM 'trinity'@'localhost';
 
-DELETE FROM `user` WHERE CONVERT( User USING utf8 ) = CONVERT( 'mangos' USING utf8 ) AND CONVERT( Host USING utf8 ) = CONVERT( 'localhost' USING utf8 ) ;
+DELETE FROM `user` WHERE CONVERT( User USING utf8 ) = CONVERT( 'trinity' USING utf8 ) AND CONVERT( Host USING utf8 ) = CONVERT( 'localhost' USING utf8 ) ;
 
-DROP DATABASE IF EXISTS `mangos` ;
+DROP DATABASE IF EXISTS `world` ;
 
 DROP DATABASE IF EXISTS `characters` ;
 
