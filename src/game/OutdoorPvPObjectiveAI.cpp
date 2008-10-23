b8362cd7f13bf51e5aa1b3e9755d39e39682676f
w12x <none@none>
2008-10-23 03:29:52 -0500

    [svn] Fixed copyright notices to comply with GPL.
    --HG--
    branch : trunk

--- src/game/OutdoorPvPObjectiveAI.cpp
+++ src/game/OutdoorPvPObjectiveAI.cpp
@@ -1,8 +1,6 @@
 /* 
  * Copyright (C) 2008 Trinity <http://www.trinitycore.org/>
  *
- * Thanks to the original authors: MaNGOS <http://www.mangosproject.org/>
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
