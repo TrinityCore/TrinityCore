4dc5978adf4695839757826cc7716b35c77a1156
w12x <none@none>
2008-10-23 03:29:52 -0500

    [svn] Fixed copyright notices to comply with GPL.
    --HG--
    branch : trunk

--- src/game/InstanceSaveMgr.h
+++ src/game/InstanceSaveMgr.h
@@ -1,7 +1,7 @@
 /*
-/* Copyright (C) 2008 Trinity <http://www.trinitycore.org/>
+ * Copyright (C) 2005-2008 MaNGOS <http://www.mangosproject.org/>
  *
- * Thanks to the original authors: ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * Copyright (C) 2008 Trinity <http://www.trinitycore.org/>
  *
  *
  * This program is free software; you can redistribute it and/or modify
