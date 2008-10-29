c0724f53e53ce6bea88d28ff8743edf8f0fd5307
KingPin <none@none>
2008-10-29 17:11:32 -0500

    [svn] * fixed my typo
    --HG--
    branch : trunk

--- sql/updates/140_world.sql
+++ sql/updates/140_world.sql
@@ -6,9 +6,9 @@ INSERT INTO command VALUES
 ('npc unfollow',2,'Syntax: .npc unfollow\r\n\r\nSelected creature (non pet) stop follow you.'),
 ('waterwalk',2,'Syntax: .waterwalk on/off\r\n\r\nSet on/off waterwalk state for selected player or self if no player selected.');
 
-DELETE FROM mangos_string WHERE entry IN (171,172,336,337,338,339,340,341,342,5007,5008);
+DELETE FROM trinity_string WHERE entry IN (171,172,336,337,338,339,340,341,342,5007,5008);
 
-INSERT INTO mangos_string VALUES
+INSERT INTO trinity_string VALUES
 (172,'server console command',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (336,'You repair all %s''s items.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (337,'All your items repaired by %s.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
