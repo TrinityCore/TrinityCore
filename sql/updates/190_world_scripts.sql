e89aa871c61dd74283231fd28de8604d7c973d01
megamage <none@none>
2008-11-06 16:17:36 -0600

    [svn] Update SerpentShrine Cavern script. Patch provided by streetrat.
    --HG--
    branch : trunk

---
+++ sql/updates/190_world_scripts.sql
@@ -0,0 +1,7 @@
+UPDATE creature_template SET ScriptName = 'boss_leotheras_the_blind' WHERE entry = '21215';
+UPDATE creature_template SET ScriptName = 'boss_leotheras_the_blind_demonform' WHERE entry = '21875';
+UPDATE creature_template SET ScriptName = 'mob_greyheart_spellbinder' WHERE entry = '21806';
+UPDATE creature_template SET ScriptName = 'mob_inner_demon' WHERE entry = '21857';
+
+UPDATE creature_template SET ScriptName = 'mob_toxic_sporebat', speed = '1' WHERE entry = '22140';
+UPDATE creature_template SET modelid_A = '11686', modelid_H = '11686', flags_extra = '128' WHERE entry = '22207';
