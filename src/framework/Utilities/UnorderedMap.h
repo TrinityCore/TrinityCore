ad217ed2f4301ce984b4e0f4cf11e0006815b591
KingPin <none@none>
2008-11-12 06:41:30 -0600

    [svn] * more fixes for hash/unordered map. source mangos.
    --HG--
    branch : trunk

--- src/framework/Utilities/UnorderedMap.h
+++ src/framework/Utilities/UnorderedMap.h
@@ -26,7 +26,7 @@
 
 #if COMPILER == COMPILER_INTEL
 #include <ext/hash_map>
-#elif COMPILER == COMPILER_GNU && __GNUC__ >= 4
+#elif COMPILER == COMPILER_GNU && (__GNUC__ > 4 || __GNUC__ == 4 && __GNUC_MINOR__ >= 3)
 #include <tr1/unordered_map>
 #elif COMPILER == COMPILER_GNU && __GNUC__ >= 3
 #include <ext/hash_map>
@@ -47,7 +47,7 @@ using stdext::hash_map;
 #elif COMPILER == COMPILER_INTEL
 #define UNORDERED_MAP std::hash_map
 using std::hash_map;
-#elif COMPILER == COMPILER_GNU && __GNUC__ >= 4
+#elif COMPILER == COMPILER_GNU && (__GNUC__ > 4 || __GNUC__ == 4 && __GNUC_MINOR__ >= 3)
 #define UNORDERED_MAP std::tr1::unordered_map
 #elif COMPILER == COMPILER_GNU && __GNUC__ >= 3
 #define UNORDERED_MAP __gnu_cxx::hash_map
