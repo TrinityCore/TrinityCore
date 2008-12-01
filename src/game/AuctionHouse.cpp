0e18c2bd1a4e3fe33e42afa8c9cf777ffd716476
megamage <none@none>
2008-12-01 10:49:54 -0600

    *Use buyout to buy an auction when the last bid is close to the buyout price. By Paradox.
    --HG--
    branch : trunk

--- src/game/AuctionHouse.cpp
+++ src/game/AuctionHouse.cpp
@@ -367,7 +367,7 @@ void WorldSession::HandleAuctionPlaceBid( WorldPacket & recv_data )
         return;
     }
 
-    if (price < (auction->bid + objmgr.GetAuctionOutBid(auction->bid)))
+    if ((price < (auction->bid + objmgr.GetAuctionOutBid(auction->bid))) && ((price < auction->buyout) || (auction->buyout == 0)))
     {
         //auction has already higher bid, client tests it!
         //SendAuctionCommandResult(auction->auctionId, AUCTION_PLACE_BID, ???);
