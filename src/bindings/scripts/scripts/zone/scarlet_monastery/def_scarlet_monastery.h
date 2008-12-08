88bfcd49ed186a0e3b08d7cac4314804da5e5eab
megamage <none@none>
2008-12-08 17:30:52 -0600

    *Headless horseman script. By Silver1ce.
    --HG--
    branch : trunk

---
+++ src/bindings/scripts/scripts/zone/scarlet_monastery/def_scarlet_monastery.h
@@ -0,0 +1,10 @@
+/* Copyright (C) 2006 - 2008 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * This program is free software licensed under GPL version 2
+ * Please see the included DOCS/LICENSE.TXT for more information */
+
+#ifndef DEF_SCARLET_M
+#define DEF_SCARLET_M
+
+#define DATA_HORSEMAN_EVENT			1
+#define GAMEOBJECT_PUMPKIN_SHRINE	2
+#endif
