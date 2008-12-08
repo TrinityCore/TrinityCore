6d93941685a8239c8d298bbcf07ae3ba0cef4a69
megamage <none@none>
2008-12-08 09:56:32 -0600

    *Fix Improved Blizzard. By Larva.
    --HG--
    branch : trunk

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -697,8 +697,8 @@ void Spell::prepareDataForTriggerSystem()
         m_canTrigger = false;         // Triggered spells can`t trigger another
         switch (m_spellInfo->SpellFamilyName)
         {
-            case SPELLFAMILY_MAGE:    // Arcane Missles triggers need do it
-                if (m_spellInfo->SpellFamilyFlags & 0x0000000000200000LL) m_canTrigger = true;
+            case SPELLFAMILY_MAGE:    // Arcane Missles / Blizzard triggers need do it
+                if (m_spellInfo->SpellFamilyFlags & 0x0000000000200080LL) m_canTrigger = true;
             break;
             case SPELLFAMILY_WARLOCK: // For Hellfire Effect / Rain of Fire / Seed of Corruption triggers need do it
                 if (m_spellInfo->SpellFamilyFlags & 0x0000800000000060LL) m_canTrigger = true;
