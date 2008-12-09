069ca8746ec4a4cb1fcc7d4c050ac956f3bea382
megamage <none@none>
2008-12-08 18:56:01 -0600

    *Add spell target type check in Unit::CastSpell. Allow CastSpell(Gameobject*) and CastSpell(NULL).
    --HG--
    branch : trunk

--- src/game/Unit.h
+++ src/game/Unit.h
@@ -1001,7 +1001,7 @@ class TRINITY_DLL_SPEC Unit : public WorldObject
         void CastCustomSpell(Unit* Victim, uint32 spellId, int32 const* bp0, int32 const* bp1, int32 const* bp2, bool triggered, Item *castItem= NULL, Aura* triggeredByAura = NULL, uint64 originalCaster = 0);
         void CastCustomSpell(Unit* Victim,SpellEntry const *spellInfo, int32 const* bp0, int32 const* bp1, int32 const* bp2, bool triggered, Item *castItem= NULL, Aura* triggeredByAura = NULL, uint64 originalCaster = 0);
         void CastSpell(float x, float y, float z, uint32 spellId, bool triggered, Item *castItem = NULL, Aura* triggeredByAura = NULL, uint64 originalCaster = 0);
-        void CastSpell(float x, float y, float z, SpellEntry const *spellInfo, bool triggered, Item *castItem = NULL, Aura* triggeredByAura = NULL, uint64 originalCaster = 0);
+        void CastSpell(GameObject *go, uint32 spellId, bool triggered, Item *castItem = NULL, Aura* triggeredByAura = NULL, uint64 originalCaster = 0);
 
         bool IsDamageToThreatSpell(SpellEntry const * spellInfo) const;
 
