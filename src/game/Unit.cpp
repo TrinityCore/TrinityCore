b5525b0c96a9f3466b3605c45a8222b37cf642ce
megamage <none@none>
2008-12-17 23:44:11 -0600

    *Fix calculation of level-based spell damage.
    --HG--
    branch : trunk

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -10471,7 +10471,9 @@ int32 Unit::CalculateSpellDamage(SpellEntry const* spellProto, uint8 effect_inde
     if(spellProto->Attributes & SPELL_ATTR_LEVEL_DAMAGE_CALCULATION && spellProto->spellLevel &&
             spellProto->Effect[effect_index] != SPELL_EFFECT_WEAPON_PERCENT_DAMAGE &&
             spellProto->Effect[effect_index] != SPELL_EFFECT_KNOCK_BACK)
-        value = int32(value*0.25f*exp(getLevel()*(70-spellProto->spellLevel)/1000.0f));
+            //there are many more: slow speed, -healing pct
+        //value = int32(value*0.25f*exp(getLevel()*(70-spellProto->spellLevel)/1000.0f));
+        value = int32(value * getLevel() / (spellProto->spellLevel ? spellProto->spellLevel : 1));
 
     return value;
 }
