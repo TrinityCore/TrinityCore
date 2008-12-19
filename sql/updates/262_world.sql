beb2a0cb7ee53819976013363b2c15d459c96ead
runningnak3d <none@none>
2008-12-19 15:46:23 +0000

    * Some much needed SQL clean up * more to come :)
    --HG--
    branch : trunk

--- sql/updates/262_world.sql
+++ sql/updates/262_world.sql
@@ -55,7 +55,7 @@ DELETE FROM `command` WHERE `name` IN (
 );
 
 INSERT INTO `command` (`name`, `security`, `help`) VALUES
-('server exit',4,'Syntax: .server exit\r\n\r\nTerminate mangosd NOW. Exit code 0.'),
+('server exit',4,'Syntax: .server exit\r\n\r\nTerminate trinity-core NOW. Exit code 0.'),
 ('server idleshutdown',3,'Syntax: .server idleshutdown #delay [#exist_code]\r\n\r\nShut the server down after #delay seconds if no active connections are present (no players). Use #exist_code or 0 as program exist code.'),
 ('server idleshutdown cancel',3,'Syntax: .server idleshutdown cancel\r\n\r\nCancel the restart/shutdown timer if any.'),
 ('server idlerestart',3,'Syntax: .server idlerestart #delay\r\n\r\nRestart the server after #delay seconds if no active connections are present (no players). Use #exist_code or 2 as program exist code.'),
@@ -68,3 +68,34 @@ INSERT INTO `command` (`name`, `security`, `help`) VALUES
 DELETE FROM trinity_string WHERE entry IN (251);
 INSERT INTO trinity_string VALUES
 (251,'Text%d (ID: %i): %s',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+ALTER TABLE event_scripts
+  DROP datatext,
+  ADD COLUMN dataint int(11) NOT NULL default '0' AFTER datalong2; 
+
+ALTER TABLE gameobject_scripts
+  DROP datatext,
+  ADD COLUMN dataint int(11) NOT NULL default '0' AFTER datalong2; 
+
+ALTER TABLE quest_end_scripts
+  DROP datatext,
+  ADD COLUMN dataint int(11) NOT NULL default '0' AFTER datalong2; 
+
+ALTER TABLE quest_start_scripts
+  DROP datatext,
+  ADD COLUMN dataint int(11) NOT NULL default '0' AFTER datalong2; 
+
+ALTER TABLE spell_scripts
+  DROP datatext,
+  ADD COLUMN dataint int(11) NOT NULL default '0' AFTER datalong2; 
+
+ALTER TABLE creature_movement
+  DROP `text1`,
+  DROP `text2`,
+  DROP `text3`,
+  DROP `text4`,
+  DROP `text5`,
+  ADD COLUMN textid1 int(11) NOT NULL default '0' AFTER waittime,
+  ADD COLUMN textid2 int(11) NOT NULL default '0' AFTER textid1,
+  ADD COLUMN textid3 int(11) NOT NULL default '0' AFTER textid2,
+  ADD COLUMN textid4 int(11) NOT NULL default '0' AFTER textid3,
+  ADD COLUMN textid5 int(11) NOT NULL default '0' AFTER textid4;
---
+++ sql/updates/537_world.sql
@@ -0,0 +1,2 @@
+-- Removing the now unused creature_movement table
+DROP TABLE IF EXISTS `creature_movement`;
