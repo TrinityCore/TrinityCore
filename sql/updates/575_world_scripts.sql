8bbe1f2647bb41e840fc643f972551b7c266f334
runningnak3d <none@none>
2008-12-22 20:59:45 +0000

    * And yet more SQL clean up
    --HG--
    branch : trunk
    rename : sql/updates/488_world_script.sql => sql/updates/488_world_scripts.sql
    rename : sql/updates/502_trinity_scripts.sql => sql/updates/502_world_scripts.sql
    rename : sql/updates/566_world_spell_script_target.sql => sql/updates/566_world.sql

--- sql/updates/575_world_scripts.sql
+++ sql/updates/575_world_scripts.sql
@@ -1,1 +1,1 @@
-UPDATE `item_template` SET `scriptname` = "item_only_for_flight" WHERE `entry` IN (34475, 34489, 24538);
+UPDATE `item_template` SET `ScriptName` = "item_only_for_flight" WHERE `entry` IN (34475, 34489, 24538);
