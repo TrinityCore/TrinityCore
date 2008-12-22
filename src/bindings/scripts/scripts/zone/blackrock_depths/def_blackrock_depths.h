65a182de18c76f8625d6f1b6b8d8f25c74345dde
Blaymoira <none@none>
2008-12-22 21:10:15 +0100

    *Adding parts for quest 4322 for future development.
    --HG--
    branch : trunk

---
+++ src/bindings/scripts/scripts/zone/blackrock_depths/def_blackrock_depths.h
@@ -0,0 +1,35 @@
+/* Copyright (C) 2006 - 2008 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * This program is free software licensed under GPL version 2
+ * Please see the included DOCS/LICENSE.TXT for more information */
+
+#ifndef DEF_BLACKROCK_DEPTHS_H
+#define DEF_BLACKROCK_DEPTHS_H
+
+#define DATA_DUGHAL						0
+#define DATA_SUPPLY_ROOM				1
+#define DATA_JAZ						2
+#define DATA_SHILL						3
+#define DATA_CREST						4
+#define DATA_TOBIAS						5
+#define DATA_QUEST_JAIL_BREAK			6
+
+#define ENCOUNTER_STATE_NOT_STARTED			100
+#define ENCOUNTER_STATE_BEFORE_START		101
+#define ENCOUNTER_STATE_IN_PROGRESS			102
+#define ENCOUNTER_STATE_OBJECTIVE_COMPLETED	103
+#define ENCOUNTER_STATE_ENDED				104
+#define ENCOUNTER_STATE_FAILED				105
+
+#define DATA_GATE_D							111
+#define DATA_GATE_SR						112
+#define DATA_GATE_J							113
+#define DATA_GATE_S							114
+#define DATA_GATE_C							115
+#define DATA_GATE_T							116
+#define DATA_GATE_SC						117
+#define DATA_CREATURE_JAZ					118
+#define DATA_CREATURE_OGRABISI				119
+#define DATA_CREATURE_CREST					120
+#define DATA_CREATURE_SHILL					121
+
+#endif
