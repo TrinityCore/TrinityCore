c30c87a69067bcd6e0650337bacec5f82b7a5191
runningnak3d <none@none>
2008-12-23 10:14:58 +0000

    * Last of the SQL clean up (for now -- until someone commits some crap again * :P )
    --HG--
    branch : trunk

--- sql/updates/262_characters.sql
+++ sql/updates/262_characters.sql
@@ -2,4 +2,4 @@ ALTER TABLE `guild_bank_tab`
   CHANGE COLUMN `TabText` `TabText` text;
 
 ALTER TABLE `character_aura` ADD `stackcount` INT NOT NULL DEFAULT '1' AFTER `effect_index` ;
-ALTER TABLE `pet_aura` ADD `stackcount` INT NOT NULL DEFAULT '1' AFTER `effect_index` ;
+ALTER TABLE `pet_aura` ADD `stackcount` INT NOT NULL DEFAULT '1' AFTER `effect_index`;
