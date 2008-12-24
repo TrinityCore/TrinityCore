26db87fbe018afbbe155db529471bc64debbf230
Blaymoira <none@none>
2008-12-24 23:18:28 +0100

    *Add support for quest 9645 - original patch by BroodWyrm
    --HG--
    branch : trunk

---
+++ sql/updates/636_world_scripts.sql
@@ -0,0 +1,9 @@
+-- event script for Medivh's Journal
+delete from event_scripts where id = 10951;
+insert into event_scripts values
+(10951,0,10,17651, 300000, 0,-11159,-1907.22,91.48,0);
+
+-- remove every Image from Medivh (should not be spawned by db)
+delete from creature where id = 17651;
+-- add script
+update creature_template set scriptname = 'npc_image_of_medivh' where entry = 17651;
