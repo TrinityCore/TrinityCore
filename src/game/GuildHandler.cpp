28656d040aa0dd6a6cfd255c68d4bcdc91215089
Blaymoira <none@none>
2008-12-29 01:16:46 +0100

    *Correctly show Guild Promoting / Demoting - by Machiavelli
    --HG--
    branch : trunk

--- src/game/GuildHandler.cpp
+++ src/game/GuildHandler.cpp
@@ -375,14 +375,14 @@ void WorldSession::HandleGuildDemoteOpcode(WorldPacket& recvPacket)
 
     guild->ChangeRank(plGuid, (slot->RankId+1));
     // Put record into guildlog
-    guild->LogGuildEvent(GUILD_EVENT_LOG_DEMOTE_PLAYER, GetPlayer()->GetGUIDLow(), GUID_LOPART(plGuid), (slot->RankId+1));
+    guild->LogGuildEvent(GUILD_EVENT_LOG_DEMOTE_PLAYER, GetPlayer()->GetGUIDLow(), GUID_LOPART(plGuid), (slot->RankId));
 
     WorldPacket data(SMSG_GUILD_EVENT, (2+30));             // guess size
     data << (uint8)GE_DEMOTION;
     data << (uint8)3;
     data << GetPlayer()->GetName();
     data << plName;
-    data << guild->GetRankName(slot->RankId+1);
+    data << guild->GetRankName(slot->RankId);
     guild->BroadcastPacket(&data);
 }
 
