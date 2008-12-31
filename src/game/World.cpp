6f0b9fe9444653455f336554aa1edb262a1e91b6
Blaymoira <none@none>
2008-12-31 15:07:29 +0100

    *Added rewrited ticket system - by Machiavelli
    --HG--
    branch : trunk

--- src/game/World.cpp
+++ src/game/World.cpp
@@ -59,7 +59,7 @@
 #include "CellImpl.h"
 #include "InstanceSaveMgr.h"
 #include "WaypointManager.h"
-#include "GMTicketMgr.h"
+#include "TicketMgr.h"
 #include "Util.h"
 #include "Language.h"
 #include "CreatureGroups.h"
@@ -737,14 +737,11 @@ void World::LoadConfigSettings(bool reload)
     }
 
     m_configs[CONFIG_GM_LOGIN_STATE]       = sConfig.GetIntDefault("GM.LoginState",2);
-    m_configs[CONFIG_GM_ACCEPT_TICKETS]    = sConfig.GetIntDefault("GM.AcceptTickets",2);
     m_configs[CONFIG_GM_CHAT]              = sConfig.GetIntDefault("GM.Chat",2);
     m_configs[CONFIG_GM_WISPERING_TO]      = sConfig.GetIntDefault("GM.WhisperingTo",2);
-
     m_configs[CONFIG_GM_IN_GM_LIST]        = sConfig.GetBoolDefault("GM.InGMList",false);
     m_configs[CONFIG_GM_IN_WHO_LIST]       = sConfig.GetBoolDefault("GM.InWhoList",false);
     m_configs[CONFIG_GM_LOG_TRADE]         = sConfig.GetBoolDefault("GM.LogTrade", false);
-
     m_configs[CONFIG_START_GM_LEVEL] = sConfig.GetIntDefault("GM.StartLevel", 1);
     if(m_configs[CONFIG_START_GM_LEVEL] < m_configs[CONFIG_START_PLAYER_LEVEL])
     {
