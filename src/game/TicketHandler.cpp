ff44f76f4b270611d616f01b5fb4f5933e9b634b
biglad <none@none>
2009-01-01 16:08:15 +0000

    * Fix crash bug in new ticket system - Thanks Machiavelli
    --HG--
    branch : trunk

--- src/game/TicketHandler.cpp
+++ src/game/TicketHandler.cpp
@@ -60,7 +60,7 @@ void WorldSession::HandleGMTicketCreateOpcode( WorldPacket & recv_data )
 	ticket->playerGuid = GetPlayer()->GetGUID();
 	ticket->message = ticketText;
 	ticket->timestamp = time(NULL);
-	ticket->closed = false;
+	ticket->closed = 0;
 	ticket->assignedToGM = 0;
 	ticket->comment = "";
 
