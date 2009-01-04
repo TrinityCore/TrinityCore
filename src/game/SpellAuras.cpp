3797d15426846d258075742448858f834259e4c5
megamage <none@none>
2009-01-04 09:54:21 -0600

    *Mangos [7019] Fix AP/RAP bonus from stat at aura apply/remove. Not use CLASSMASK_WAND_USERS mask for AP bonus. By DiSlord. *Mangos [7020] Initilize glyph order at loading/reset. This must fix problems with glyph use for old(converted) characters. By VladimirMangos.
    --HG--
    branch : trunk

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -4855,11 +4855,9 @@ void Aura::HandleAuraModRangedAttackPowerOfStatPercent(bool apply, bool Real)
     if(!Real)
         return;
 
-    if(m_target->GetTypeId() == TYPEID_PLAYER && (m_target->getClassMask() & CLASSMASK_WAND_USERS)!=0)
-        return;
-
     // Recalculate bonus
-    ((Player*)m_target)->UpdateAttackPowerAndDamage(true);
+    if(m_target->GetTypeId() == TYPEID_PLAYER && !(m_target->getClassMask() & CLASSMASK_WAND_USERS))
+        ((Player*)m_target)->UpdateAttackPowerAndDamage(true);
 }
 
 void Aura::HandleAuraModAttackPowerOfStatPercent(bool apply, bool Real)
@@ -4868,11 +4866,9 @@ void Aura::HandleAuraModAttackPowerOfStatPercent(bool apply, bool Real)
     if(!Real)
         return;
 
-    if(m_target->GetTypeId() == TYPEID_PLAYER && (m_target->getClassMask() & CLASSMASK_WAND_USERS)!=0)
-        return;
-
     // Recalculate bonus
-    ((Player*)m_target)->UpdateAttackPowerAndDamage(false);
+    if(m_target->GetTypeId() == TYPEID_PLAYER)
+        ((Player*)m_target)->UpdateAttackPowerAndDamage(false);
 }
 
 /********************************/
