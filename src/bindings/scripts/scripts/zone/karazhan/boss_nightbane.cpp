4c2a8e6c204806a9e1c6f19b32d281b08e96a724
megamage <none@none>
2009-01-04 10:02:31 -0600

    *Fix build.
    --HG--
    branch : trunk

--- src/bindings/scripts/scripts/zone/karazhan/boss_nightbane.cpp
+++ src/bindings/scripts/scripts/zone/karazhan/boss_nightbane.cpp
@@ -143,9 +143,9 @@ struct TRINITY_DLL_DECL boss_nightbaneAI : public ScriptedAI
 	void HandleTerraceDoors(bool open)
     {
 		if(GameObject *Door = GameObject::GetGameObject((*m_creature),pInstance->GetData64(DATA_MASTERS_TERRACE_DOOR_1)))
-			Door->SetUInt32Value(GAMEOBJECT_STATE, open ? 0 : 1);
+			Door->SetGoState(open ? 0 : 1);
 		if(GameObject *Door = GameObject::GetGameObject((*m_creature),pInstance->GetData64(DATA_MASTERS_TERRACE_DOOR_2)))
-			Door->SetUInt32Value(GAMEOBJECT_STATE, open ? 0 : 1);
+			Door->SetGoState(open ? 0 : 1);
     }
 
     void Aggro(Unit *who)
