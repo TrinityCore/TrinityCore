c86c0e7e6b19b43843a5947a2c5f2106c377bf52
megamage <none@none>
2009-01-05 14:53:33 -0600

    *Fix build.
    --HG--
    branch : trunk

---
+++ src/game/BattleGroundDS.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+#ifndef __BATTLEGROUNDDS_H
+#define __BATTLEGROUNDDS_H
+
+class BattleGround;
+
+class BattleGroundDSScore : public BattleGroundScore
+{
+    public:
+        BattleGroundDSScore() {};
+        virtual ~BattleGroundDSScore() {};
+        //TODO fix me
+};
+
+class BattleGroundDS : public BattleGround
+{
+    friend class BattleGroundMgr;
+
+    public:
+        BattleGroundDS();
+        ~BattleGroundDS();
+        void Update(time_t diff);
+
+        /* inherited from BattlegroundClass */
+        virtual void AddPlayer(Player *plr);
+        void RemovePlayer(Player *plr, uint64 guid);
+        void HandleAreaTrigger(Player *Source, uint32 Trigger);
+        bool SetupBattleGround();
+        void HandleKillPlayer(Player* player, Player *killer);
+};
+#endif
