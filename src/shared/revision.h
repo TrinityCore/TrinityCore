b1527f699a806a7c124fd67dd7e56216513ad14c
Blaymoira <none@none>
2009-01-18 20:01:45 +0100

    *Missed part in prev commit
    --HG--
    branch : trunk

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,7 +1,7 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define _REVISION "896"
- #define _HASH "2fc9fa91df4b"
+ #define _REVISION "905"
+ #define _HASH "9c8352b53bb6"
  #define _REVISION_DATE "*"
  #define _REVISION_TIME "*"
 #endif // __REVISION_H__
