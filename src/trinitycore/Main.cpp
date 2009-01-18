0e23c797c363e2f9800b692613d982c22a10a45e
Blaymoira <none@none>
2009-01-18 13:08:42 +0100

    Some changes for genrevision
    --HG--
    branch : trunk

--- src/trinitycore/Main.cpp
+++ src/trinitycore/Main.cpp
@@ -64,7 +64,7 @@ uint32 realmID;                                             ///< Id of the realm
 void usage(const char *prog)
 {
     sLog.outString("Usage: \n %s [<options>]\n"
-        "    --version                print version and exist\n\r"
+        "    --version                print version and exit\n\r"
         "    -c config_file           use config_file as configuration file\n\r"
         #ifdef WIN32
         "    Running as service functions:\n\r"
