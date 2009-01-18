f2bfc577384632a7d6fa34c3ebb0c12951e0bab0
Blaymoira <none@none>
2009-01-18 19:48:01 +0100

    *Updated blackrock_depths - thanks for nofantasy
    --HG--
    branch : trunk

--- src/bindings/scripts/scripts/zone/blackrock_depths/instance_blackrock_depths.cpp
+++ src/bindings/scripts/scripts/zone/blackrock_depths/instance_blackrock_depths.cpp
@@ -32,209 +32,276 @@ update `instance_template` set `script`='instance_blackrock_depths' where `map`=
 #include "precompiled.h"
 #include "def_blackrock_depths.h"
 
-#define ENCOUNTERS 7
-
+#define ENCOUNTERS              6
+    
+#define C_EMPEROR               9019
+#define C_PHALANX               9502
+    
+#define GO_ARENA1               161525
+#define GO_ARENA2               161522
+#define GO_ARENA3               161524
+#define GO_ARENA4               161523
+#define GO_SHADOW_LOCK          161460
+#define GO_SHADOW_MECHANISM     161461
+#define GO_SHADOW_GIANT_DOOR    157923
+#define GO_SHADOW_DUMMY         161516
+#define GO_BAR_KEG_SHOT         170607
+#define GO_BAR_KEG_TRAP         171941
+#define GO_BAR_DOOR             170571
+#define GO_TOMB_ENTER           170576
+#define GO_TOMB_EXIT            170577
+#define GO_LYCEUM               170558
+#define GO_GOLEM_ROOM_N         170573
+#define GO_GOLEM_ROOM_S         170574
+#define GO_THONE_ROOM           170575
+    
 struct TRINITY_DLL_DECL instance_blackrock_depths : public ScriptedInstance
 {
 	instance_blackrock_depths(Map *map) : ScriptedInstance(map) {Initialize();};
+    
+	uint32 Encounter[ENCOUNTERS];
+	std::string str_data;
+    
+	uint64 EmperorGUID;
+	uint64 PhalanxGUID;
+    
+	uint64 GoArena1GUID;
+	uint64 GoArena2GUID;
+	uint64 GoArena3GUID;
+	uint64 GoArena4GUID;
+	uint64 GoShadowLockGUID;
+	uint64 GoShadowMechGUID;
+	uint64 GoShadowGiantGUID;
+	uint64 GoShadowDummyGUID;
+	uint64 GoBarKegGUID;
+	uint64 GoBarKegTrapGUID;
+	uint64 GoBarDoorGUID;
+	uint64 GoTombEnterGUID;
+	uint64 GoTombExitGUID;
+	uint64 GoLyceumGUID;
+	uint64 GoGolemNGUID;
+	uint64 GoGolemSGUID;
+	uint64 GoThoneGUID;
 
-	uint64 GateDughal;
-	uint64 SupplyRoomGate;
-	uint64 GateJaz;
-	uint64 GateShill;
-	uint64 GateCrest;
-	uint64 GateTobias;
-	uint64 SupplyCrate;
-
-	uint64 Jaz;
-	uint64 Ograbisi;
-	uint64 ShillDinger;
-	uint64 CrestKiller;
-
-	uint32 state;
-
-	bool Encounters[ENCOUNTERS];
-
+	uint32 BarAleCount;
+   
 	void Initialize()
-    {   
-	
-		GateDughal = 0;
-		SupplyRoomGate = 0;
-		GateJaz = 0;
-		GateShill = 0;
-		GateCrest = 0;
-		GateTobias = 0;
-		SupplyCrate = 0;
-
-		Jaz = 0;
-		Ograbisi = 0;
-		ShillDinger = 0;
-		CrestKiller = 0;
-
-		state = 0;
-
-		for(uint8 i = 0; i < ENCOUNTERS; i++)
-			Encounters[i] = false;
-    }
-	
-	bool IsEncounterInProgress() const
 	{
+		EmperorGUID = 0;
+		PhalanxGUID = 0;
+
+		GoArena1GUID = 0;
+		GoArena2GUID = 0;
+		GoArena3GUID = 0;
+		GoArena4GUID = 0;
+		GoShadowLockGUID = 0;
+		GoShadowMechGUID = 0;
+		GoShadowGiantGUID = 0;
+		GoShadowDummyGUID = 0;
+		GoBarKegGUID = 0;
+		GoBarKegTrapGUID = 0;
+		GoBarDoorGUID = 0;
+		GoTombEnterGUID = 0;
+		GoTombExitGUID = 0;
+		GoLyceumGUID = 0;
+		GoGolemNGUID = 0;
+		GoGolemSGUID = 0;
+		GoThoneGUID = 0;
+
+		BarAleCount = 0;
+   
 		for(uint8 i = 0; i < ENCOUNTERS; i++)
-            if(Encounters[i]) return true;
+			Encounter[i] = NOT_STARTED;
+	}
+   
+	Player* GetPlayerInMap()
+	{
+		Map::PlayerList const& players = instance->GetPlayers();
 
-        return false;
-    }
+		if (!players.isEmpty())
+		{
+			for(Map::PlayerList::const_iterator itr = players.begin(); itr != players.end(); ++itr)
+			{
+				if (Player* plr = itr->getSource())
+					return plr;
+			}
+		}
+   
+		debug_log("SD2: Instance Blackrock Depths: GetPlayerInMap, but PlayerList is empty!");
+		return NULL;
+	}
+   
+	void OnCreatureCreate(Creature *creature, uint32 creature_entry)
+	{
+		switch(creature->GetEntry())
+		{
+		case C_EMPEROR: EmperorGUID = creature->GetGUID(); break;
+		case C_PHALANX: PhalanxGUID = creature->GetGUID(); break;
+		}
+	}
 
-	void OpenGO(uint64 DoorGUID, bool open)
-    {
-        if(GameObject *Door = instance->GetGameObjectInMap(DoorGUID))
-            Door->SetUInt32Value(GAMEOBJECT_STATE, open ? 0 : 1);
-    }
+   	void OnObjectCreate(GameObject* go)
+	{
+		switch(go->GetEntry())
+		{
+		case GO_ARENA1: GoArena1GUID = go->GetGUID(); break;
+		case GO_ARENA2: GoArena2GUID = go->GetGUID(); break;
+		case GO_ARENA3: GoArena3GUID = go->GetGUID(); break;
+		case GO_ARENA4: GoArena4GUID = go->GetGUID(); break;
+		case GO_SHADOW_LOCK: GoShadowLockGUID = go->GetGUID(); break;
+		case GO_SHADOW_MECHANISM: GoShadowMechGUID = go->GetGUID(); break;
+		case GO_SHADOW_GIANT_DOOR: GoShadowGiantGUID = go->GetGUID(); break;
+		case GO_SHADOW_DUMMY: GoShadowDummyGUID = go->GetGUID(); break;
+		case GO_BAR_KEG_SHOT: GoBarKegGUID = go->GetGUID(); break;
+		case GO_BAR_KEG_TRAP: GoBarKegTrapGUID = go->GetGUID(); break;
+		case GO_BAR_DOOR: GoBarDoorGUID = go->GetGUID(); break;
+		case GO_TOMB_ENTER: GoTombEnterGUID = go->GetGUID(); break;
+		case GO_TOMB_EXIT: GoTombExitGUID = go->GetGUID(); break;
+		case GO_LYCEUM: GoLyceumGUID = go->GetGUID(); break;
+		case GO_GOLEM_ROOM_N: GoGolemNGUID = go->GetGUID(); break;
+		case GO_GOLEM_ROOM_S: GoGolemSGUID = go->GetGUID(); break;
+		case GO_THONE_ROOM: GoThoneGUID = go->GetGUID(); break;
+		}
+	}
+   
+	void SetData(uint32 type, uint32 data)
+	{
+		Player *player = GetPlayerInMap();
 
-    void CloseGO(uint64 DoorGUID, bool close)
-    {
-        if(GameObject *Door = instance->GetGameObjectInMap(DoorGUID))
-            Door->SetUInt32Value(GAMEOBJECT_STATE, close ? 1 : 0);
-    }
+		if (!player)
+		{
+			debug_log("SD2: Instance Blackrock Depths: SetData (Type: %u Data %u) cannot find any player.", type, data);
+			return;
+		}
+   
+		debug_log("SD2: Instance Blackrock Depths: SetData update (Type: %u Data %u)", type, data);
+   
+		switch(type)
+		{
+		case TYPE_RING_OF_LAW:
+			Encounter[0] = data;
+			break;
+		case TYPE_VAULT:
+			Encounter[1] = data;
+			break;
+		case TYPE_BAR:
+			if (data == SPECIAL)
+				++BarAleCount;
+			else
+				Encounter[2] = data;
+			break;
+		case TYPE_TOMB_OF_SEVEN:
+			Encounter[3] = data;
+			break;
+		case TYPE_LYCEUM:
+			Encounter[4] = data;
+			break;
+		case TYPE_IRON_HALL:
+			Encounter[5] = data;
+			break;
+		}
 
+		if (data == DONE)
+		{
+			OUT_SAVE_INST_DATA;
+
+			std::ostringstream saveStream;
+			saveStream << Encounter[0] << " " << Encounter[1] << " " << Encounter[2] << " "
+				<< Encounter[3] << " " << Encounter[4] << " " << Encounter[5];
+
+			str_data = saveStream.str();
+   
+			SaveToDB();
+			OUT_SAVE_INST_DATA_COMPLETE;
+		}
+	}
+   
 	uint32 GetData(uint32 type)
 	{
 		switch(type)
 		{
-		case DATA_DUGHAL:			return Encounters[0];break;
-		case DATA_SUPPLY_ROOM:		return Encounters[1];break;
-		case DATA_JAZ:				return Encounters[2];break;
-		case DATA_SHILL:			return Encounters[3];break;
-		case DATA_CREST:			return Encounters[4];break;
-		case DATA_TOBIAS:			return Encounters[5];break;
-		case DATA_QUEST_JAIL_BREAK: return Encounters[6];break;
-		case DATA_GATE_D:			return GateDughal;	break;
-		case DATA_GATE_T:			return GateTobias;	break;
-		case DATA_GATE_C:			return GateCrest;	break;
-		case DATA_GATE_J:			return GateJaz;		break;
-		case DATA_GATE_S:			return GateShill;	break;
-		case DATA_CREATURE_JAZ:
-			if((((Creature*)Jaz)->isDead()))
-				state = 1;
+		case TYPE_RING_OF_LAW:
+			return Encounter[0];
+		case TYPE_VAULT:
+			return Encounter[1];
+		case TYPE_BAR:
+			if (Encounter[2] == IN_PROGRESS && BarAleCount == 3)
+				return SPECIAL;
 			else
-				state = 0;
-			break;
-		case DATA_CREATURE_OGRABISI:
-			if((((Creature*)Ograbisi)->isDead()))
-				state = 1;
-			else
-				state = 0;
-			break;
-		case DATA_CREATURE_CREST:
-			if((((Creature*)CrestKiller)->isDead()))
-				state = 1;
-			else
-				state = 0;
-			break;
-		case DATA_CREATURE_SHILL:
-			if((((Creature*)ShillDinger)->isDead()))
-				state = 1;
-			else
-				state = 0;
-			break;
+				return Encounter[2];
+		case TYPE_TOMB_OF_SEVEN:
+			return Encounter[3];
+		case TYPE_LYCEUM:
+			return Encounter[4];
+		case TYPE_IRON_HALL:
+			return Encounter[5];
 		}
 		return 0;
 	}
-
-	void OnObjectCreate(GameObject *go)
+   
+	uint64 GetData64(uint32 data)
 	{
-		switch(go->GetEntry())
+		switch(data)
 		{
-		case 170561: SupplyRoomGate = go->GetGUID(); state = go->GetUInt32Value(GAMEOBJECT_STATE); break; 
-		case 170562: GateDughal = go->GetGUID(); state = go->GetUInt32Value(GAMEOBJECT_STATE);	break;
-		case 170566: GateTobias = go->GetGUID(); state = go->GetUInt32Value(GAMEOBJECT_STATE);	break;
-		case 170567: GateCrest = go->GetGUID(); state = go->GetUInt32Value(GAMEOBJECT_STATE); break;
-		case 170568: GateJaz = go->GetGUID(); state = go->GetUInt32Value(GAMEOBJECT_STATE); break;
-		case 170569: GateShill = go->GetGUID(); state = go->GetUInt32Value(GAMEOBJECT_STATE); break;
-		case 166872: SupplyCrate = go->GetGUID(); state = go->GetUInt32Value(GAMEOBJECT_STATE); break;
+		case DATA_EMPEROR:
+			return EmperorGUID;
+		case DATA_PHALANX:
+			return PhalanxGUID;
+		case DATA_ARENA1:
+			return GoArena1GUID;
+		case DATA_ARENA2:
+			return GoArena2GUID;
+		case DATA_ARENA3:
+			return GoArena3GUID;
+		case DATA_ARENA4:
+			return GoArena4GUID;
+		case DATA_GO_BAR_KEG:
+			return GoBarKegGUID;
+		case DATA_GO_BAR_KEG_TRAP:
+			return GoBarKegTrapGUID;
+		case DATA_GO_BAR_DOOR:
+			return GoBarDoorGUID;
 		}
+		return 0;
 	}
-
-    void OnCreatureCreate(Creature *creature, uint32 creature_entry) 
+   
+	const char* Save()
 	{
-		switch(creature_entry)
-        {
-         case 9680: CrestKiller = creature->GetGUID(); break; 
-		 case 9677: Ograbisi = creature->GetGUID(); break;
-         case 9681: Jaz = creature->GetGUID(); break;
-         case 9678: ShillDinger = creature->GetGUID(); break;
-        }		
+		return str_data.c_str();
 	}
+   
+	void Load(const char* in)
+	{
+		if (!in)
+		{
+			OUT_LOAD_INST_DATA_FAIL;
+			return;
+		}
+   
+		OUT_LOAD_INST_DATA(in);
 
-    void SetData(uint32 type, uint32 data)
-    {
-        switch(type)
-        {
-        case DATA_DUGHAL: Encounters[0] = data; break;
-        case DATA_SUPPLY_ROOM: Encounters[1] = data; break;
-        case DATA_JAZ: Encounters[2] = data; break;
-        case DATA_SHILL: Encounters[3] = data; break;
-        case DATA_CREST: Encounters[4] = data; break;
-        case DATA_TOBIAS: Encounters[5] = data; break;
-        case DATA_QUEST_JAIL_BREAK: Encounters[6] = data; break;
-		case DATA_GATE_SR:
-            if(SupplyRoomGate)
-            {
-               if(data == NOT_STARTED)
-					OpenGO(SupplyRoomGate, true);
-			   else
-					CloseGO(SupplyRoomGate, true);
-            }
-	        break;
-         case DATA_GATE_SC:
-            if(SupplyCrate)
-            {
-               if(data == NOT_STARTED)
-					OpenGO(SupplyCrate, true);
-			   else
-					CloseGO(SupplyCrate, true);
-            }
-	        break;
-		 case DATA_CREATURE_JAZ:
-			 if(data)
-			 {
-				 (((Creature*)Jaz)->setFaction(54));
-				 (((Creature*)Ograbisi)->setFaction(54));
-				 (((Creature*)Ograbisi)->Say("Ograbisi needs new hat", LANG_UNIVERSAL, NULL));
-			 }
-			 else
-			 {
-				 (((Creature*)Jaz)->setFaction(35));
-				 (((Creature*)Ograbisi)->setFaction(35));
-			 }
-			 break;
- 		 case DATA_CREATURE_SHILL:
-			 if(data)
-				(((Creature*)ShillDinger)->setFaction(54));
-			 else
-				(((Creature*)ShillDinger)->setFaction(35));
-			 break;
- 		 case DATA_CREATURE_CREST:
-			 if(data)
-				(((Creature*)CrestKiller)->setFaction(54));
-			 else
-				(((Creature*)CrestKiller)->setFaction(35));
-			 break;
-        }
-    }
+		std::istringstream loadStream(in);
+		loadStream >> Encounter[0] >> Encounter[1] >> Encounter[2] >> Encounter[3]
+		>> Encounter[4] >> Encounter[5];
 
+		for(uint8 i = 0; i < ENCOUNTERS; ++i)
+			if (Encounter[i] == IN_PROGRESS)
+				Encounter[i] = NOT_STARTED;
+   
+		OUT_LOAD_INST_DATA_COMPLETE;
+	}
 };
-
+   
 InstanceData* GetInstanceData_instance_blackrock_depths(Map* map)
 {
-    return new instance_blackrock_depths(map);
+	return new instance_blackrock_depths(map);
 }
-
-void AddSC_instance_blackrock_depths()
-{
-    Script *newscript;
-    newscript = new Script;
-    newscript->Name = "instance_blackrock_depths";
-    newscript->GetInstanceData = &GetInstanceData_instance_blackrock_depths;
-    newscript->RegisterSelf();
-}
+   
+   void AddSC_instance_blackrock_depths()
+   {
+       Script *newscript;
+       newscript = new Script;
+       newscript->Name = "instance_blackrock_depths";
+       newscript->GetInstanceData = &GetInstanceData_instance_blackrock_depths;
+       newscript->RegisterSelf();
+   }
