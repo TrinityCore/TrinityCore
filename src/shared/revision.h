219599b59cfbe98961f1288af3dceda4d2a3c745
Blaymoira <none@none>
2009-01-18 20:13:44 +0100

    *Fixed CMakeLists *Remove not needed file
    --HG--
    branch : trunk

--- src/shared/revision.h
+++
@@ -1,7 +0,0 @@
-#ifndef __REVISION_H__
-#define __REVISION_H__
- #define _REVISION "905"
- #define _HASH "9c8352b53bb6"
- #define _REVISION_DATE "*"
- #define _REVISION_TIME "*"
-#endif // __REVISION_H__
