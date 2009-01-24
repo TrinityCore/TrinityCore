c8bbb4a9735db6ddd559308f315b6fadc4b9c560
megamage <none@none>
2009-01-23 22:10:59 -0600

    *Fix the bug that player cannot see other units after changing map (the last rev does not work).
    --HG--
    branch : trunk

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -1735,7 +1735,7 @@ bool Player::TeleportTo(uint32 mapid, float x, float y, float z, float orientati
 
             ResetContestedPvP();
 
-            DestroyForNearbyPlayers();
+            /*DestroyForNearbyPlayers();
             {
                 UpdateData data;
                 for(ClientGUIDs::iterator i = m_clientGUIDs.begin(); i != m_clientGUIDs.end(); ++i)
@@ -1743,7 +1743,7 @@ bool Player::TeleportTo(uint32 mapid, float x, float y, float z, float orientati
                 WorldPacket packet;
                 data.BuildPacket(&packet);
                 GetSession()->SendPacket(&packet);
-            }
+            }*/
             m_clientGUIDs.clear();
 
             // remove player from battleground on far teleport (when changing maps)
