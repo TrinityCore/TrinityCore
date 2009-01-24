071f81d85a435dde4753725b71179be61bc263ec
Blaymoira <none@none>
2009-01-24 12:11:45 +0100

    *Changes in instance_black_temple *Fix a typo
    --HG--
    branch : trunk

--- sql/world_scripts_full.sql
+++ sql/world_scripts_full.sql
@@ -715,7 +715,7 @@ UPDATE `creature_template` SET `ScriptName`='npc_neltharaku' WHERE `entry`=21657
 UPDATE `creature_template` SET `ScriptName`='npc_oronok_tornheart' WHERE `entry`=21183;
 UPDATE `creature_template` SET `ScriptName`='mob_mature_netherwing_drake' WHERE `entry`=21648;
 UPDATE `creature_template` SET `ScriptName`='mob_enslaved_netherwing_drake' WHERE `entry`=21722;
-UPDATE `creature_template` SET `ScriptName`='npc_overlord_morghor' WHERE entry=		;
+UPDATE `creature_template` SET `ScriptName`='npc_overlord_morghor' WHERE entry=	23139;
 UPDATE `creature_template` SET `ScriptName`='' WHERE entry=22083;
 UPDATE `creature_template` SET `ScriptName`='' WHERE entry=23141;
 UPDATE `creature_template` SET `ScriptName`='npc_earthmender_wilda' where entry=21027;
