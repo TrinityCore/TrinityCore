82a10f03a29fcdf33abab0cc813f827502c11cdc
megamage <none@none>
2009-01-26 17:57:02 -0600

    *Fix build.
    --HG--
    branch : trunk

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -1,7 +1,7 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define _REVISION "1087"
- #define _HASH "89fbd8b03c1a"
+ #define _REVISION "1091"
+ #define _HASH "88c4b2b6b9a2"
  #define _REVISION_DATE "*"
  #define _REVISION_TIME "*"
 #endif // __REVISION_H__
