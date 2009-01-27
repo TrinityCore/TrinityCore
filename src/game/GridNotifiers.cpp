a8c836f8d953fc442a2d07f2d1fd0c9a4d177617
megamage <none@none>
2009-01-27 14:47:46 -0600

    *Update to Mangos 7194.
    --HG--
    branch : trunk

--- src/game/GridNotifiers.cpp
+++ src/game/GridNotifiers.cpp
@@ -249,9 +249,6 @@ ObjectUpdater::Visit(GridRefManager<T> &m)
     }
 }
 
-template void ObjectUpdater::Visit<GameObject>(GameObjectMapType &);
-template void ObjectUpdater::Visit<DynamicObject>(DynamicObjectMapType &);
-
 bool CannibalizeObjectCheck::operator()(Corpse* u)
 {
     // ignore bones
@@ -268,3 +265,6 @@ bool CannibalizeObjectCheck::operator()(Corpse* u)
 
     return false;
 }
+
+template void ObjectUpdater::Visit<GameObject>(GameObjectMapType &);
+template void ObjectUpdater::Visit<DynamicObject>(DynamicObjectMapType &);
