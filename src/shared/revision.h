1681aa70a3bf6023eae0c5139e61a0753f370836
panaut0lordv <panaut0lordv@gmail.com>
2009-01-30 19:12:30 +0100

    Removed revision.h as it restores on both linux&win when building.
    --HG--
    branch : trunk

--- src/shared/revision.h
+++
@@ -1,7 +0,0 @@
-#ifndef __REVISION_H__
-#define __REVISION_H__
- #define _REVISION "1116"
- #define _HASH "81edc7393bc6"
- #define _REVISION_DATE "*"
- #define _REVISION_TIME "*"
-#endif // __REVISION_H__
