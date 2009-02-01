c63bafad96492b87c1156a1190b907e18e5c480f
megamage <none@none>
2009-02-01 16:29:01 -0600

    [7211] Re-calculate "gameobject for quests" data at `quest_template` reload. By VladimirMangos.
    --HG--
    branch : trunk

--- src/game/Level3.cpp
+++ src/game/Level3.cpp
@@ -616,6 +616,11 @@ bool ChatHandler::HandleReloadQuestTemplateCommand(const char*)
     sLog.outString( "Re-Loading Quest Templates..." );
     objmgr.LoadQuests();
     SendGlobalGMSysMessage("DB table `quest_template` (quest definitions) reloaded.");
+
+    /// dependent also from `gameobject` but this table not reloaded anyway
+    sLog.outString( "Re-Loading GameObjects for quests..." );
+    objmgr.LoadGameObjectForQuests();
+    SendGlobalGMSysMessage("Data GameObjects for quests reloaded.");
     return true;
 }
 
