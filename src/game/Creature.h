69cf5a61c9fde787d1a1241a6b2337252689fd25
megamage <none@none>
2009-02-02 09:53:01 -0600

    [7220] Check creature spells data at server startup. Author: VladimirMangos
    --HG--
    branch : trunk

--- src/game/Creature.h
+++ src/game/Creature.h
@@ -195,10 +195,7 @@ struct CreatureInfo
     int32   resistance4;
     int32   resistance5;
     int32   resistance6;
-    uint32  spell1;
-    uint32  spell2;
-    uint32  spell3;
-    uint32  spell4;
+    uint32  spells[CREATURE_MAX_SPELLS];
     uint32  PetSpellDataId;
     uint32  mingold;
     uint32  maxgold;
