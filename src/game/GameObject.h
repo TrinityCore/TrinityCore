e9ad8376ade57baa995e2029d8ed105758f367a3
QAston <none@none>
2009-02-04 19:43:00 +0100

    *Implement stealth detection for traps.
    --HG--
    branch : trunk

--- src/game/GameObject.h
+++ src/game/GameObject.h
@@ -571,6 +571,7 @@ class TRINITY_DLL_SPEC GameObject : public WorldObject
         void TriggeringLinkedGameObject( uint32 trapEntry, Unit* target);
 
         bool isVisibleForInState(Player const* u, bool inVisibleList) const;
+        bool canDetectTrap(Player const* u, float distance) const;
 
         GameObject* LookupFishingHoleAround(float range);
 
