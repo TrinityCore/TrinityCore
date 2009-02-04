e38043f44032fd16d2585f2bed09fef1eadbb3c8
Drahy <none@none>
2009-02-04 12:42:26 +0100

    * Updated copyright notice for new year (2009) part 2 * Changed mangos website
    --HG--
    branch : trunk

--- src/bindings/interface/ScriptMgr.h
+++ src/bindings/interface/ScriptMgr.h
@@ -1,7 +1,7 @@
 /* 
  * Copyright (C) 2008-2009 Trinity <http://www.trinitycore.org/>
  *
- * Thanks to the original authors: MaNGOS <http://www.mangosproject.org/>
+ * Thanks to the original authors: MaNGOS <http://getmangos.com/>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
