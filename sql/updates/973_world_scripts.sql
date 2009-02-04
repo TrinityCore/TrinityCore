2552fe2dc19ad34a25147b528ecf4436d8eb1904
Drahy <none@none>
2009-02-04 17:11:58 +0100

    * Updated script structure according to newest ACID 0.0.6 for trinity * Updated some of world sql files - not all of them yet * Cleaned up full script sql
    --HG--
    branch : trunk

--- sql/updates/973_world_scripts.sql
+++ sql/updates/973_world_scripts.sql
@@ -1,1 +1,1 @@
-update creature_template set scriptname = 'npc_enraged_spirit' where entry in (21050, 21061, 21060, 21059);
+UPDATE `creature_template` SET `ScriptName`='npc_enraged_spirit' WHERE `entry` IN (21050, 21061, 21060, 21059);
