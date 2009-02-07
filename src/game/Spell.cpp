e22e98a625e466c3b336e9ecc7b7fb4fd715804a
QAston <none@none>
2009-02-07 23:07:53 +0100

    *Do not check behind for Mutilate. By Blaymoira.
    --HG--
    branch : trunk

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -3738,7 +3738,9 @@ uint8 Spell::CanCast(bool strict)
         //Must be behind the target.
         if( m_spellInfo->AttributesEx2 == 0x100000 && (m_spellInfo->AttributesEx & 0x200) == 0x200 && target->HasInArc(M_PI, m_caster)
             //Exclusion for Pounce: Facing Limitation was removed in 2.0.1, but it still uses the same, old Ex-Flags
-            && (!(m_spellInfo->SpellFamilyName == SPELLFAMILY_DRUID && m_spellInfo->SpellFamilyFlags.IsEqual(0x20000,0,0))))
+            && (!(m_spellInfo->SpellFamilyName == SPELLFAMILY_DRUID && m_spellInfo->SpellFamilyFlags.IsEqual(0x20000,0,0)))
+            //Multilate - same reason
+            && (!(m_spellInfo->SpellFamilyName == SPELLFAMILY_ROGUE && m_spellInfo->SpellFamilyFlags[1] & 200000)))
         {
             SendInterrupted(2);
             return SPELL_FAILED_NOT_BEHIND;
