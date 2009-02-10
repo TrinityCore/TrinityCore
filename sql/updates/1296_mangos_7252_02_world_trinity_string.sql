144665415dcbd5c57c4f176b6b488df53e4c36e7
Drahy <none@none>
2009-02-11 00:43:32 +0100

    * Fixed typo in latest mangos update file
    --HG--
    branch : trunk

--- sql/updates/1296_mangos_7252_02_world_trinity_string.sql
+++ sql/updates/1296_mangos_7252_02_world_trinity_string.sql
@@ -1,7 +1,7 @@
 /*ALTER TABLE db_version CHANGE COLUMN required_7252_01_mangos_command required_7252_02_mangos_mangos_string bit;*/
 
-DELETE FROM `mangos_string` WHERE entry IN (737,738,739,740,741,742,743,744,745,746);
-INSERT INTO `mangos_string` VALUES
+DELETE FROM `trinity_string` WHERE entry IN (737,738,739,740,741,742,743,744,745,746);
+INSERT INTO `trinity_string` VALUES
 (737,'Arenas are set to 1v1 for debugging. So, don\'t join as group.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (738,'Arenas are set to normal playercount.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (739,'Battlegrounds are set to 1v0 for debugging.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
