d0df416e0546ab7e77110602f7db323521e5cae4
Drahy <none@none>
2009-02-10 23:01:26 +0100

    * Renamed and fixed latest mangos update sqls * Fixed script name according to script code stormwind_harbor * Updated CMakeLists and world_scripts_full
    --HG--
    branch : trunk

--- sql/updates/1288_mangos_7242_01_world_spell_bonus_data.sql
+++ sql/updates/1288_mangos_7242_01_world_spell_bonus_data.sql
@@ -1,4 +1,4 @@
-ALTER TABLE db_version CHANGE COLUMN required_7235_01_mangos_command required_7242_01_mangos_spell_bonus_data bit;
+/*ALTER TABLE db_version CHANGE COLUMN required_7235_01_mangos_command required_7242_01_mangos_spell_bonus_data bit;*/
 
 DELETE FROM spell_bonus_data WHERE entry IN (34913, 43043, 43044);
 INSERT INTO spell_bonus_data VALUES
