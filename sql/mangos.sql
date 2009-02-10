a18071878f2cccb8771ba64d471943c29dd4d962
megamage <none@none>
2009-02-09 19:55:58 -0600

    [7242] Fixed spell 30482 and ranks spell bonus for triggered spell.
        Signed-off-by: ApoC <apoc@nymfe.net>
    
    --HG--
    branch : trunk

--- sql/mangos.sql
+++ sql/mangos.sql
@@ -22,7 +22,7 @@
 DROP TABLE IF EXISTS `db_version`;
 CREATE TABLE `db_version` (
   `version` varchar(120) default NULL,
-  `required_7235_01_mangos_command` bit(1) default NULL
+  `required_7242_01_mangos_spell_bonus_data` bit(1) default NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Used DB version notes';
 
 --
---
+++ sql/updates/7242_01_mangos_spell_bonus_data.sql
@@ -0,0 +1,7 @@
+ALTER TABLE db_version CHANGE COLUMN required_7235_01_mangos_command required_7242_01_mangos_spell_bonus_data bit;
+
+DELETE FROM spell_bonus_data WHERE entry IN (34913, 43043, 43044);
+INSERT INTO spell_bonus_data VALUES
+ (34913, 0, 0, 0, 'Mage - Molten Armor Triggered Rank 1'),
+ (43043, 0, 0, 0, 'Mage - Molten Armor Triggered Rank 2'),
+ (43044, 0, 0, 0, 'Mage - Molten Armor Triggered Rank 3');
