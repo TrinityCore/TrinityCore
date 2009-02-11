1a7660ca54b56274d32ab5e8f1d5c7b0bca3c03c
megamage <none@none>
2009-02-11 11:51:17 -0600

    [7263] Fixed AP calculation in some cases
        Fixed use AP dynamic mods (bonuses from stats)
        Apply ITEM_MOD_ATTACK_POWER for ranged AP
        Implement feral AP from weapon dps for druids
    
        Signed-off-by: DiSlord <dislord@nomail.com>
    
    --HG--
    branch : trunk

--- src/game/ItemPrototype.h
+++ src/game/ItemPrototype.h
@@ -110,6 +110,7 @@ enum ITEM_FLAGS
     ITEM_FLAGS_WRAPPER                        = 0x00000200, // used or not used wrapper
     ITEM_FLAGS_PARTY_LOOT                     = 0x00000800, // determines if item is party loot or not
     ITEM_FLAGS_CHARTER                        = 0x00002000, // arena/guild charter
+    ITEM_FLAGS_PROSPECTABLE                   = 0x00040000,
     ITEM_FLAGS_UNIQUE_EQUIPPED                = 0x00080000,
     ITEM_FLAGS_USEABLE_IN_ARENA               = 0x00200000,
     ITEM_FLAGS_THROWABLE                      = 0x00400000, // not used in game for check trow possibility, only for item in game tooltip
