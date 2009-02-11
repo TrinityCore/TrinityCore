3720befa0caf30f39f4eea0e8d834b58bf4831a7
QAston <none@none>
2009-02-11 20:38:02 +0100

    *Add one more RewChoiceItemId field in quest template (SQL and feature request by Brian).
    --HG--
    branch : trunk

---
+++ sql/updates/1325_world.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `quest_template` ADD COLUMN `RewChoiceItemId7` mediumint(8) unsigned NOT NULL default '0' AFTER `RewChoiceItemId6`;
+ALTER TABLE `quest_template` ADD COLUMN `RewChoiceItemCount7` smallint(5) unsigned NOT NULL default '0' AFTER `RewChoiceItemCount6`;
