70c534cf8ffc1567f81773c3dc41317ec5767f43
megamage <none@none>
2009-02-13 20:02:00 -0600

    [7271] Implement mangosd stop and pause if it work as Windows service. Author: rilex
    --HG--
    branch : trunk

--- src/mangosd/WorldRunnable.cpp
+++ src/mangosd/WorldRunnable.cpp
@@ -39,6 +39,11 @@
 #define WORLD_SLEEP_CONST 100                               //Is this still needed?? [On linux some time ago not working 50ms]
 #endif
 
+#ifdef WIN32
+#include "ServiceWin32.h"
+extern int m_ServiceStatus;
+#endif
+
 /// Heartbeat for the World
 void WorldRunnable::run()
 {
@@ -73,6 +78,11 @@ void WorldRunnable::run()
         }
         else
             prevSleepTime = 0;
+
+        #ifdef WIN32
+            if (m_ServiceStatus == 0) World::StopNow(SHUTDOWN_EXIT_CODE);
+            while (m_ServiceStatus == 2) Sleep(1000);
+        #endif
     }
 
     sWorld.KickAll();                                       // save and kick all players
