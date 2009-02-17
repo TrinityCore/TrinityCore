d71225775adcf03709944364c4628c16e78029a6
QAston <none@none>
2009-02-17 16:41:45 +0100

    Backed out changeset e85c3b6d551b
    --HG--
    branch : trunk

--- sql/updates/1325_world.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `quest_template` ADD COLUMN `RewChoiceItemId7` mediumint(8) unsigned NOT NULL default '0' AFTER `RewChoiceItemId6`;
-ALTER TABLE `quest_template` ADD COLUMN `RewChoiceItemCount7` smallint(5) unsigned NOT NULL default '0' AFTER `RewChoiceItemCount6`;
