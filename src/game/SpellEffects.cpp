2156fec0d2b0958535495c9edc7d30e2b8e95da5
megamage <none@none>
2009-02-16 20:10:40 -0600

    [7284] Avoid 2 times skill value request. Author: VladimirMangos
    --HG--
    branch : trunk

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -3137,8 +3137,7 @@ void Spell::EffectOpenLock(uint32 /*i*/)
         }
 
         // update skill if really known
-        uint32 SkillValue = player->GetPureSkillValue(SkillId);
-        if(SkillValue)                                      // non only item base skill
+        if(uint32 SkillValue = player->GetPureSkillValue(SkillId))
         {
             if(gameObjTarget)
             {
@@ -3150,7 +3149,6 @@ void Spell::EffectOpenLock(uint32 /*i*/)
             else if(itemTarget)
             {
                 // Do one skill-up
-                uint32 SkillValue = player->GetPureSkillValue(SkillId);
                 player->UpdateGatherSkill(SkillId, SkillValue, reqSkillValue);
             }
         }
