83adfb5692e3daa4d09d233f193f23b746e5e4c4
megamage <none@none>
2009-02-19 18:46:19 -0600

    Added support for pools in pools
        Pools inside another pool cannot have a number of spawned objects <> 1 (maxlimit value)
    
        Note: pools in a pool in a pool in a pool... is possible and working,
        but circulare inclusion is not detected and will cause a core infinit loop
    
    Author: Neo2003
    
    --HG--
    branch : trunk

--- sql/updates/7299_01_mangos_pools.sql
+++ sql/updates/7299_01_mangos_pools.sql
@@ -12,6 +12,13 @@ CREATE TABLE `pool_gameobject` (
   PRIMARY KEY  (`guid`,`pool_entry`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+CREATE TABLE `pool_pool` (
+  `pool_id` mediumint(8) unsigned NOT NULL default '0',
+  `mother_pool` mediumint(8) unsigned NOT NULL default '0',
+  `chance` float NOT NULL default '0',
+  PRIMARY KEY  (`pool_id`,`mother_pool`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
 CREATE TABLE `pool_template` (
   `entry` mediumint(8) unsigned NOT NULL default '0' COMMENT 'Pool entry',
   `max_limit` int(10) unsigned NOT NULL default '0' COMMENT 'Max number of objects (0) is no limit',
