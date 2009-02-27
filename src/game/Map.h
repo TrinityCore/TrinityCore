00b7908c51d400a49aa64d99abe776eca01c42dc
megamage <none@none>
2009-02-27 12:15:06 -0600

    [7337] Make some methods const. Author: ApoC
    --HG--
    branch : trunk

--- src/game/Map.h
+++ src/game/Map.h
@@ -219,8 +219,8 @@ class TRINITY_DLL_SPEC Map : public GridRefManager<NGridType>, public Trinity::O
         // assert print helper
         bool CheckGridIntegrity(Creature* c, bool moved) const;
 
-        uint32 GetInstanceId() { return i_InstanceId; }
-        uint8 GetSpawnMode() { return (i_spawnMode); }
+        uint32 GetInstanceId() const { return i_InstanceId; }
+        uint8 GetSpawnMode() const { return (i_spawnMode); }
         virtual bool CanEnter(Player* /*player*/) { return true; }
         const char* GetMapName() const;
 
