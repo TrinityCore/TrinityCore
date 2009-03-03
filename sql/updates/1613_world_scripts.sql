46f15b44a2baf7310f0fdf17ea027ac35a59ca4c
Drahy <none@none>
2009-03-03 18:41:13 +0100

    * Fixed typo * Added missing scripts
    --HG--
    branch : trunk

--- sql/updates/1613_world_scripts.sql
+++ sql/updates/1613_world_scripts.sql
@@ -1,3 +1,3 @@
 -- Script for Skarvald and Dalronn
-update creature_template set scriptname = 'boss_skarvald_the_constructor' where entry in (24200,27390);
-update creature_template set scriptname = 'boss_dalronn_the_controller' where entry in (24201,27389);
+UPDATE `creature_template` SET `ScriptName`= 'boss_skarvald_the_constructor' WHERE `entry` IN (24200,27390);
+UPDATE `creature_template` SET `ScriptName`= 'boss_dalronn_the_controller' WHERE `entry` IN (24201,27389);
