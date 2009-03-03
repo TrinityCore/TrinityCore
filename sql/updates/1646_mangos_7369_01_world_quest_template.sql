fbc610a81e5f00b2c7ca252d87b02e6853fe2af5
Drahy <none@none>
2009-03-03 16:45:40 +0100

    * Updated sql trinity code for proper built * Revert some changes for 3.0.x core
    --HG--
    branch : trunk

--- sql/updates/1646_mangos_7369_01_world_quest_template.sql
+++ sql/updates/1646_mangos_7369_01_world_quest_template.sql
@@ -1,4 +1,4 @@
-ALTER TABLE db_version CHANGE COLUMN required_7349_01_mangos_spell_area required_7369_01_mangos_quest_template bit;
+/*ALTER TABLE db_version CHANGE COLUMN required_7349_01_mangos_spell_area required_7369_01_mangos_quest_template bit;*/
 
 ALTER TABLE quest_template
   DROP COLUMN ReqSourceRef1,
