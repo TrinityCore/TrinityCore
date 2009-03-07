6281fa8ef1c9c5bee6b7da75fb3c2e0ac5571249
Drahy <none@none>
2009-03-07 22:17:45 +0100

    * Trinity sql coding style * Added 1703_world.sql * Small fixies in other files
    --HG--
    branch : trunk

--- sql/updates/7399_01_mangos_mangos_string.sql
+++
@@ -1,6 +0,0 @@
--- ALTER TABLE db_version CHANGE COLUMN required_7393_01_mangos_game_event required_7399_01_mangos_mangos_string bit;
-
-DELETE FROM mangos_string WHERE entry in (753, 754, 755);
-INSERT INTO mangos_string VALUES (753,'The battle for Warsong Gulch begins in 2 minutes.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
-INSERT INTO mangos_string VALUES (754,'The battle for Arathi Basin begins in 2 minutes.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
-INSERT INTO mangos_string VALUES (755,'The battle for Eye of the Storm begins in 2 minutes.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
