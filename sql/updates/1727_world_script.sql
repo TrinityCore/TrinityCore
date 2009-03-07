3b2396fe41c8b7f1f089b5a9166373f5da3f85b2
BroodWyrm <none@none>
2009-03-08 00:13:03 +0100

    *Add Scripts for Ingvar the Plunderer Event
    --HG--
    branch : trunk

---
+++ sql/updates/1727_world_script.sql
@@ -0,0 +1,3 @@
+update creature_template set scriptname = 'boss_ingvar_the_plunderer' where entry = 23954;
+update creature_template set scriptname = 'mob_annhylde_the_caller' where entry = 24068;
+update creature_template set scriptname = 'mob_ingvar_throw_dummy' where entry = 23997;
