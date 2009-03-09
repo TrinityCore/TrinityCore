9074b19fea65a0d11074d6d10777e6a4c09c6882
megamage <none@none>
2009-03-09 17:46:07 -0600

    [7420] Not set cdecl calling convention for x64 platform for Unic/Linux. Author: VladimirMangos
        This will prevent generation "warning: 'cdecl' attribute ignored"
    
    --HG--
    branch : trunk

--- src/framework/Platform/Define.h
+++ src/framework/Platform/Define.h
@@ -57,6 +57,8 @@
 #  define TRINITY_GET_PROC_ADDR dlsym
 #  if defined(__APPLE_CC__) && defined(BIG_ENDIAN)
 #    define TRINITY_IMPORT __attribute__ ((longcall))
+#  elif defined(__x86_64__)
+#    define TRINITY_IMPORT
 #  else
 #    define TRINITY_IMPORT __attribute__ ((cdecl))
 #  endif //__APPLE_CC__ && BIG_ENDIAN
