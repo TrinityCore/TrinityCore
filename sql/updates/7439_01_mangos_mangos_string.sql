05c7639e7b6d87ef15267c990bc089d181d55d84
Drahy <none@none>
2009-03-14 22:42:20 +0100

    * Added trinity sql coding style
    --HG--
    branch : trunk

--- sql/updates/7439_01_mangos_mangos_string.sql
+++
@@ -1,5 +0,0 @@
-/*ALTER TABLE db_version CHANGE COLUMN required_7422_01_mangos_mangos_string required_7439_01_mangos_mangos_string bit;*/
-
-DELETE FROM mangos_string WHERE entry in (175);
-INSERT INTO mangos_string VALUES
- (175,'Liquid level: %f, ground: %f, type: %d, status: %d',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
