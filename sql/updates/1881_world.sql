7abb0342b9ca209e621624c4d62adead6453e474
Blaymoira <none@none>
2009-03-14 09:42:00 +0100

    *Correct FamilyMask for spell 31124 and ranks - by Anubisss
    --HG--
    branch : trunk

---
+++ sql/updates/1881_world.sql
@@ -0,0 +1,4 @@
+DELETE FROM spell_proc_event where entry in (31124, 31126);
+insert into spell_proc_event () VALUES
+(31124, 0, 8, 536870926, 0, 0, 0, 0, 0, 0, 0),
+(31126, 0, 8, 536870926, 0, 0, 0, 0, 0, 0, 0);
