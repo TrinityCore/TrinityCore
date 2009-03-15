8b96182b1cb8d79c63fc0a30377cea9a480b418c
QAston <none@none>
2009-03-15 02:06:59 +0100

    *Fix Shattered Barrier
    --HG--
    branch : trunk

--- src/game/SpellMgr.h
+++ src/game/SpellMgr.h
@@ -603,9 +603,8 @@ enum ProcFlagsEx
    PROC_EX_ABSORB              = 0x0000400,
    PROC_EX_REFLECT             = 0x0000800,
    PROC_EX_INTERRUPT           = 0x0001000,                 // Melee hit result can be Interrupt (not used)
-   PROC_EX_AURA_REMOVE         = 0x0002000,
-   PROC_EX_RESERVED2           = 0x0004000,
-   PROC_EX_RESERVED3           = 0x0008000,
+   PROC_EX_AURA_REMOVE_DISPEL  = 0x0002000,
+   PROC_EX_AURA_REMOVE_EXPIRE  = 0x0004000,
    PROC_EX_EX_TRIGGER_ALWAYS   = 0x0010000,                 // If set trigger always ( no matter another flags) used for drop charges
    PROC_EX_EX_ONE_TIME_TRIGGER = 0x0020000                  // If set trigger always but only one time (not used)
 };
