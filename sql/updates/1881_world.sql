b264aafb53e12655798bdfee4d138d1ada24e46d
Anubisss <none@none>
2009-03-15 13:54:00 +0100

    *Remove incorrect sqls. Add the correct one.
    --HG--
    branch : trunk

--- sql/updates/1881_world.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `spell_proc_event` WHERE `entry` IN (31124, 31126);
-INSERT INTO `spell_proc_event` VALUES
-(31124, 0, 8, 16777222, 0, 0, 0, 0, 0, 0, 0),
-(31126, 0, 8, 16777222, 0, 0, 0, 0, 0, 0, 0);
--- sql/updates/1906_world.sql
+++
@@ -1,6 +0,0 @@
--- Nature's Grace --
-DELETE FROM `spell_proc_event` WHERE `entry` IN (16880, 61345, 61346);
-INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
-(16880, 0x00, 0, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x0000002, 0.000000, 0.000000, 0),
-(61345, 0x00, 0, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x0000002, 0.000000, 0.000000, 0),
-(61346, 0x00, 0, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x0000002, 0.000000, 0.000000, 0);
