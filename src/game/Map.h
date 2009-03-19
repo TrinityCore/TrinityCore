ef47783882e609406f879856d2d3ce5ee865ec80
megamage <none@none>
2009-03-18 20:41:49 -0600

    [7487] Avoid attempt use InstanceSave data for non-dungeons. Author: VladimirMangos
        Also check map existance and correctness at instance data loading.
        Removed unused and totally bugged InstanceMap::GetResetTime.
    
    --HG--
    branch : trunk

--- src/game/Map.h
+++ src/game/Map.h
@@ -544,7 +544,6 @@ class TRINITY_DLL_SPEC InstanceMap : public Map
         uint32 GetScriptId() { return i_script_id; }
         InstanceData* GetInstanceData() { return i_data; }
         void PermBindAllPlayers(Player *player);
-        time_t GetResetTime();
         void UnloadAll();
         bool CanEnter(Player* player);
         void SendResetWarnings(uint32 timeLeft) const;
