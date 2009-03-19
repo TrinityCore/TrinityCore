319d540c2cb695f5325842b32784323d3e041a9b
megamage <none@none>
2009-03-19 15:16:02 -0600

    *Update gameobject visual and command. By Vladimir.
    --HG--
    branch : trunk

---
+++ sql/updates/7495_01_mangos_mangos_string.sql
@@ -0,0 +1,7 @@
+ALTER TABLE db_version CHANGE COLUMN required_7493_01_mangos_command required_7495_01_mangos_mangos_string bit;
+
+DELETE FROM mangos_string WHERE entry IN (276,277,524);
+INSERT INTO mangos_string VALUES
+(276,'Game Object |cffffffff|Hgameobject:%d|h[%s]|h|r (GUID: %u) turned',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(277,'Game Object |cffffffff|Hgameobject:%d|h[%s]|h|r (GUID: %u) moved',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(524,'Selected object:\n|cffffffff|Hgameobject:%d|h[%s]|h|r GUID: %u ID: %u\nX: %f Y: %f Z: %f MapId: %u\nOrientation: %f',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
