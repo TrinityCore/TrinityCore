f3a543bef3690dae643653ff97120db17ba13d34
megamage <none@none>
2009-03-20 14:38:53 -0600

    *Do not delete charminfo for guardian.
    --HG--
    branch : trunk

--- src/game/Creature.h
+++ src/game/Creature.h
@@ -446,7 +446,7 @@ class TRINITY_DLL_SPEC Creature : public Unit
         void GetRespawnCoord(float &x, float &y, float &z, float* ori = NULL, float* dist =NULL) const;
         uint32 GetEquipmentId() const { return m_equipmentId; }
 
-        uint32 GetSummonMask() const { return m_summonMask; }
+        uint32 HasSummonMask(uint32 mask) const { return mask & m_summonMask; }
         bool isSummon() const   { return m_summonMask & SUMMON_MASK_SUMMON; }
         bool isPet() const      { return m_summonMask & SUMMON_MASK_PET; }
         bool isVehicle() const  { return m_summonMask & SUMMON_MASK_VEHICLE; }
