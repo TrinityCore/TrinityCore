bb5bca0dd120bb97e6493490f7d06cb2ad5953a9
megamage <none@none>
2009-03-21 22:37:48 -0600

    *Remove 243 sql.
    --HG--
    branch : trunk

--- sql/updates/1236_world.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `spell_proc_event` WHERE `entry` = 36070;
-INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
-(36070, 0x00, 0, 0x00000000, 0x00000008, 0x00000000, 0.000000, 0.000000, 0);
-
