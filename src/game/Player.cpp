a6d6b214560f75723000d448717b9d7ccfee9be7
megamage <none@none>
2009-03-26 17:51:40 -0600

    *Fix build.
    --HG--
    branch : trunk

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -636,40 +636,40 @@ bool Player::Create( uint32 guidlow, const std::string& name, uint8 race, uint8 
     //Reputations if "StartAllReputation" is enabled, -- TODO: Fix this in a better way
     if(sWorld.getConfig(CONFIG_START_ALL_REP))
     {
-        SetFactionReputation(sFactionStore.LookupEntry(942),42999);
-        SetFactionReputation(sFactionStore.LookupEntry(935),42999);
-        SetFactionReputation(sFactionStore.LookupEntry(936),42999);
-        SetFactionReputation(sFactionStore.LookupEntry(1011),42999);
-        SetFactionReputation(sFactionStore.LookupEntry(970),42999);
-        SetFactionReputation(sFactionStore.LookupEntry(967),42999);
-        SetFactionReputation(sFactionStore.LookupEntry(989),42999);
-        SetFactionReputation(sFactionStore.LookupEntry(932),42999);
-        SetFactionReputation(sFactionStore.LookupEntry(934),42999);
-        SetFactionReputation(sFactionStore.LookupEntry(1038),42999);
-        SetFactionReputation(sFactionStore.LookupEntry(1077),42999);
+        GetReputationMgr().SetReputation(sFactionStore.LookupEntry(942),42999);
+        GetReputationMgr().SetReputation(sFactionStore.LookupEntry(935),42999);
+        GetReputationMgr().SetReputation(sFactionStore.LookupEntry(936),42999);
+        GetReputationMgr().SetReputation(sFactionStore.LookupEntry(1011),42999);
+        GetReputationMgr().SetReputation(sFactionStore.LookupEntry(970),42999);
+        GetReputationMgr().SetReputation(sFactionStore.LookupEntry(967),42999);
+        GetReputationMgr().SetReputation(sFactionStore.LookupEntry(989),42999);
+        GetReputationMgr().SetReputation(sFactionStore.LookupEntry(932),42999);
+        GetReputationMgr().SetReputation(sFactionStore.LookupEntry(934),42999);
+        GetReputationMgr().SetReputation(sFactionStore.LookupEntry(1038),42999);
+        GetReputationMgr().SetReputation(sFactionStore.LookupEntry(1077),42999);
 
         // Factions depending on team, like cities and some more stuff
         switch(GetTeam())
         {
         case ALLIANCE:
-            SetFactionReputation(sFactionStore.LookupEntry(72),42999);
-            SetFactionReputation(sFactionStore.LookupEntry(47),42999);
-            SetFactionReputation(sFactionStore.LookupEntry(69),42999);
-            SetFactionReputation(sFactionStore.LookupEntry(930),42999);
-            SetFactionReputation(sFactionStore.LookupEntry(730),42999);
-            SetFactionReputation(sFactionStore.LookupEntry(978),42999);
-            SetFactionReputation(sFactionStore.LookupEntry(54),42999);
-            SetFactionReputation(sFactionStore.LookupEntry(946),42999);
+            GetReputationMgr().SetReputation(sFactionStore.LookupEntry(72),42999);
+            GetReputationMgr().SetReputation(sFactionStore.LookupEntry(47),42999);
+            GetReputationMgr().SetReputation(sFactionStore.LookupEntry(69),42999);
+            GetReputationMgr().SetReputation(sFactionStore.LookupEntry(930),42999);
+            GetReputationMgr().SetReputation(sFactionStore.LookupEntry(730),42999);
+            GetReputationMgr().SetReputation(sFactionStore.LookupEntry(978),42999);
+            GetReputationMgr().SetReputation(sFactionStore.LookupEntry(54),42999);
+            GetReputationMgr().SetReputation(sFactionStore.LookupEntry(946),42999);
             break;
         case HORDE:
-            SetFactionReputation(sFactionStore.LookupEntry(76),42999);
-            SetFactionReputation(sFactionStore.LookupEntry(68),42999);
-            SetFactionReputation(sFactionStore.LookupEntry(81),42999);
-            SetFactionReputation(sFactionStore.LookupEntry(911),42999);
-            SetFactionReputation(sFactionStore.LookupEntry(729),42999);
-            SetFactionReputation(sFactionStore.LookupEntry(941),42999);
-            SetFactionReputation(sFactionStore.LookupEntry(530),42999);
-            SetFactionReputation(sFactionStore.LookupEntry(947),42999);
+            GetReputationMgr().SetReputation(sFactionStore.LookupEntry(76),42999);
+            GetReputationMgr().SetReputation(sFactionStore.LookupEntry(68),42999);
+            GetReputationMgr().SetReputation(sFactionStore.LookupEntry(81),42999);
+            GetReputationMgr().SetReputation(sFactionStore.LookupEntry(911),42999);
+            GetReputationMgr().SetReputation(sFactionStore.LookupEntry(729),42999);
+            GetReputationMgr().SetReputation(sFactionStore.LookupEntry(941),42999);
+            GetReputationMgr().SetReputation(sFactionStore.LookupEntry(530),42999);
+            GetReputationMgr().SetReputation(sFactionStore.LookupEntry(947),42999);
             break;
         default:
             break;
