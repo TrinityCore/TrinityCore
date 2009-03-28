8b29e701e26dc8d9fc05a806efcc570b506a77b9
megamage <none@none>
2009-03-28 16:34:34 -0600

    [7562] Drop code for now not existed spell support. (Touch of Weakness triggered spell) Author: cccyril
    --HG--
    branch : trunk

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -1474,31 +1474,6 @@ void Spell::EffectDummy(uint32 i)
                     m_caster->CastSpell(unitTarget, hurt, true, 0);
                 return;
             }
-            switch(m_spellInfo->Id )
-            {
-                case 28598:                                 // Touch of Weakness triggered spell
-                {
-                    if(!unitTarget || !m_triggeredByAuraSpell)
-                        return;
-
-                    uint32 spellid = 0;
-                    switch(m_triggeredByAuraSpell->Id)
-                    {
-                        case 2652:  spellid =  2943; break; // Rank 1
-                        case 19261: spellid = 19249; break; // Rank 2
-                        case 19262: spellid = 19251; break; // Rank 3
-                        case 19264: spellid = 19252; break; // Rank 4
-                        case 19265: spellid = 19253; break; // Rank 5
-                        case 19266: spellid = 19254; break; // Rank 6
-                        case 25461: spellid = 25460; break; // Rank 7
-                        default:
-                            sLog.outError("Spell::EffectDummy: Spell 28598 triggered by unhandled spell %u",m_triggeredByAuraSpell->Id);
-                            return;
-                    }
-                    m_caster->CastSpell(unitTarget, spellid, true, NULL);
-                    return;
-                }
-            }
             break;
         case SPELLFAMILY_DRUID:
             // Starfall
@@ -4106,7 +4081,6 @@ void Spell::SpellDamageWeaponDmg(uint32 i)
     }
 
     // some spell specific modifiers
-    //float weaponDamagePercentMod = 1.0f;                    // applied to weapon damage (and to fixed effect damage bonus if customBonusDamagePercentMod not set
     float totalDamagePercentMod  = 1.0f;                    // applied to final bonus+weapon damage
     int32 fixed_bonus = 0;
     int32 spell_bonus = 0;                                  // bonus specific for spell
@@ -4267,6 +4241,7 @@ void Spell::SpellDamageWeaponDmg(uint32 i)
                 break;
             case SPELL_EFFECT_WEAPON_PERCENT_DAMAGE:
                 weaponDamagePercentMod *= float(CalculateDamage(j,unitTarget)) / 100.0f;
+                break;
             default:
                 break;                                      // not weapon damage effect, just skip
         }
