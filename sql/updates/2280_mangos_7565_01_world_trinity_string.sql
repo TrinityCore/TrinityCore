06d09707ba3d5be5509b7784428c4bf9c039d677
Drahy <none@none>
2009-03-29 11:25:40 +0200

    * Used trinity sql coding style for proper import
    --HG--
    branch : trunk

--- sql/updates/2280_mangos_7565_01_world_trinity_string.sql
+++ sql/updates/2280_mangos_7565_01_world_trinity_string.sql
@@ -1,7 +1,7 @@
-ALTER TABLE db_version CHANGE COLUMN required_7560_01_mangos_gameobject_template required_7565_01_mangos_mangos_string bit;
+/*ALTER TABLE db_version CHANGE COLUMN required_7560_01_mangos_gameobject_template required_7565_01_mangos_mangos_string bit;*/
 
-DELETE FROM mangos_string WHERE entry IN(1010,1011,1012,1013,1014);
-INSERT INTO mangos_string VALUES
+DELETE FROM `trinity_string` WHERE `entry` IN (1010,1011,1012,1013,1014);
+INSERT INTO `trinity_string` VALUES
 (1010,'|    Account    |       Character      |       IP        | GM | Expansion |',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (1011,'|<Error>        | %20s |<Error>          |<Er>| <Error>   |',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
 (1012,'===========================================================================',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
