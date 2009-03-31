6f5efa3c7aa32c463fe0dae78a86a78f80cf1d1b
XTZGZoReX <none@none>
2009-03-31 09:18:03 +0200

    * Do not print chat logs to console. It will simply screw up console output. * Fix a possible crash in if/else checks in channel chat logging.
    --HG--
    branch : trunk

--- src/game/ChatHandler.cpp
+++ src/game/ChatHandler.cpp
@@ -503,12 +503,17 @@ void WorldSession::HandleMessagechatOpcode( WorldPacket & recv_data )
                         chn->HasFlag(CHANNEL_FLAG_GENERAL) ||
                         chn->HasFlag(CHANNEL_FLAG_CITY) ||
                         chn->HasFlag(CHANNEL_FLAG_LFG))
+                    {
                         if(sWorld.getConfig(CONFIG_CHATLOG_SYSCHAN))
                             sLog.outChat("[SYSCHAN] Player %s tells channel %s: %s",
-                            GetPlayer()->GetName(), chn->GetName().c_str(), msg.c_str());
-                        else if(sWorld.getConfig(CONFIG_CHATLOG_CHANNEL))
+                                GetPlayer()->GetName(), chn->GetName().c_str(), msg.c_str());
+                    }
+                    else
+                    {
+                        if(sWorld.getConfig(CONFIG_CHATLOG_CHANNEL))
                             sLog.outChat("[CHANNEL] Player %s tells channel %s: %s",
-                            GetPlayer()->GetName(), chn->GetName().c_str(), msg.c_str());
+                                GetPlayer()->GetName(), chn->GetName().c_str(), msg.c_str());
+                    }
                 }
             }
         } break;
