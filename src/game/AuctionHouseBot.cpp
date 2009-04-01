169604a24fe528d1302e1b572999c3c46f977f32
Chaz Brown <iamparadox@netscape.net>
2009-03-31 21:09:38 -0400

    Fix typo in AHBot Thanks for noticing it, Medwise.
    --HG--
    branch : trunk

--- src/game/AuctionHouseBot.cpp
+++ src/game/AuctionHouseBot.cpp
@@ -816,8 +816,8 @@ void AuctionHouseBot()
 ///////////////////////////////////////////////////////////////////////////////
 void AuctionHouseBotInit()
 {
-    sConfig.GetBoolDefault("AuctionHouseBot.EnableSeller", false);
-    sConfig.GetBoolDefault("AuctionHouseBot.EnableBuyer", false);
+    AHBSeller = sConfig.GetBoolDefault("AuctionHouseBot.EnableSeller", false);
+    AHBBuyer = sConfig.GetBoolDefault("AuctionHouseBot.EnableBuyer", false);
     No_Bind = sConfig.GetBoolDefault("AuctionHouseBot.No_Bind", true);
     Bind_When_Picked_Up = sConfig.GetBoolDefault("AuctionHouseBot.Bind_When_Picked_Up", false);
     Bind_When_Equipped = sConfig.GetBoolDefault("AuctionHouseBot.Bind_When_Equipped", true);
