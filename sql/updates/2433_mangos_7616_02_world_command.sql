78f3c37723ed7f53fa703cd9844acc237a50ace2
Drahy <none@none>
2009-04-06 06:28:07 +0200

    * Moved some 2.4.3 sql to proper folder * Added 2450_world which contains all updated from moved files * Updated world_script_full * Used trinity sql coding style
    --HG--
    branch : trunk

--- sql/updates/2433_mangos_7616_02_world_command.sql
+++ sql/updates/2433_mangos_7616_02_world_command.sql
@@ -1,7 +1,6 @@
-ALTER TABLE db_version CHANGE COLUMN required_7616_01_mangos_mangos_string required_7616_02_mangos_command bit;
+/*ALTER TABLE db_version CHANGE COLUMN required_7616_01_mangos_mangos_string required_7616_02_mangos_command bit;*/
 
 DELETE FROM `command` WHERE `name` IN ('debug playsound','debug play sound','debug play cinematic','debug play movie');
-
 INSERT INTO `command` VALUES
 ('debug play cinematic',1,'Syntax: .debug play cinematic #cinematicid\r\n\r\nPlay cinematic #cinematicid for you. You stay at place while your mind fly.\r\n'),
 ('debug play movie',1,'Syntax: .debug play movie #movieid\r\n\r\nPlay movie #movieid for you.'),
