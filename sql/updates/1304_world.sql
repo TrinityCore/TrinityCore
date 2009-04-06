781e597807044c733069727701c49c2558aa3d29
Drahy <none@none>
2009-04-06 06:28:07 +0200

    * Moved some 2.4.3 sql to proper folder * Added 2450_world which contains all updated from moved files * Updated world_script_full * Used trinity sql coding style
    --HG--
    branch : trunk

--- sql/updates/1304_world.sql
+++
@@ -1,3 +0,0 @@
-delete from command where name in ('reload spell_linked_spell');
-insert into command (name, security, help) values
-('reload spell_linked_spell','3','Usage: .reload spell_linked_spell\r\nReloads the spell_linked_spell DB table.'), 
