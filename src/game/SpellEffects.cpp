5292a6e98c6e72ce7ee3291b6e61abb5b1b41f1a
QAston <none@none>
2009-04-07 23:53:13 +0200

    *Again a typofix-sorry for that
    --HG--
    branch : trunk

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -2050,7 +2050,7 @@ void Spell::EffectTriggerSpell(uint32 i)
         {
             uint32 dispelMask = GetDispellMask(DISPEL_ALL);
             Unit::AuraMap& Auras = m_caster->GetAuras();
-            for(Unit::AuraMap::iterator iter = Auras.begin(); iter != Auras.end(); ++iter)
+            for(Unit::AuraMap::iterator iter = Auras.begin(); iter != Auras.end();)
             {
                 // remove all harmful spells on you...
                 SpellEntry const* spell = iter->second->GetSpellProto();
