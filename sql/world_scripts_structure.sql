d0fe1056340bc93d93b5e7e7f714586f03f9aec7
Drahy <none@none>
2009-04-10 09:35:05 +0200

    * Added table script_waypoint and deleted non used files also from previous commits and make it in one file for better orientation, so please import after 2559 only 2565
    --HG--
    branch : trunk

--- sql/world_scripts_structure.sql
+++ sql/world_scripts_structure.sql
@@ -98,9 +98,21 @@ CREATE TABLE IF NOT EXISTS `script_texts` (
   PRIMARY KEY  (`entry`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Script Texts';
 
+DROP TABLE IF EXISTS script_waypoint;
+CREATE TABLE script_waypoint (
+  entry mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT 'creature_template entry',
+  pointid mediumint(8) unsigned NOT NULL DEFAULT '0',
+  location_x float NOT NULL DEFAULT '0',
+  location_y float NOT NULL DEFAULT '0',
+  location_z float NOT NULL DEFAULT '0',
+  waittime int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'waittime in millisecs',
+  point_comment text,
+  PRIMARY KEY (entry, pointid)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Script Creature waypoints';
+
+
 DROP TABLE IF EXISTS `sd2_db_version`;
 DROP TABLE IF EXISTS `script_db_version`;
 CREATE TABLE `script_db_version` (
 `version` varchar(255) NOT NULL default '' COMMENT 'Database version string'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
