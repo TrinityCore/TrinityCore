52de70ec540f8a571a01ff5817a441cc63a4b264
Anubisss <none@none>
2009-04-12 00:05:56 +0200

    *Delete unused scripts in instance Ahn'kahet.
    --HG--
    branch : trunk

--- src/bindings/scripts/scripts/zone/Azjol-Nerub/ahnkahet/boss_aminitar.cpp
+++ src/bindings/scripts/scripts/zone/Azjol-Nerub/ahnkahet/boss_aminitar.cpp
@@ -1,15 +1,35 @@
+/*
+ * Copyright (C) 2009 Trinity <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
 /* Script Data Start
-SDName: Boss Amanitar
+SDName: boss_aminitar
 SDAuthor: LordVanMartin
-SD%Complete: 
+SD%Complete: 0
 SDComment:  Only appears in heroic mode
-SDCategory: 
+SDCategory: Ahn'kahet
 Script Data End */
 
 /*** SQL START *** 
 update creature_template set scriptname = 'boss_amanitar' where entry = '';
 *** SQL END ***/
+
 #include "precompiled.h"
+#include "def_ahnkahet.h"
 
 #define SPELL_BASH                                   57094
 #define SPELL_ENTANGLING_ROOTS                       57095
--- src/bindings/scripts/scripts/zone/Azjol-Nerub/ahnkahet/boss_herald_volazj.cpp
+++ src/bindings/scripts/scripts/zone/Azjol-Nerub/ahnkahet/boss_herald_volazj.cpp
@@ -1,15 +1,35 @@
-/* Script Data Start
-SDName: Boss volazj
+/*
+ * Copyright (C) 2009 Trinity <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/* ScriptData
+SDName: boss_herald_volazj
 SDAuthor: LordVanMartin
-SD%Complete: 
-SDComment: 
-SDCategory: 
-Script Data End */
+SD%Complete: 0
+SDComment:
+SDCategory: Ahn'kahet
+EndScriptData */
 
 /*** SQL START *** 
 update creature_template set scriptname = 'boss_volazj' where entry = '';
 *** SQL END ***/
+
 #include "precompiled.h"
+#include "def_ahnkahet.h"
 
 //Spells
 #define SPELL_INSANITY                         57496 //Dummy
