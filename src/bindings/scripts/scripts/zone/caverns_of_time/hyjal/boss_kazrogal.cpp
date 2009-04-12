a8a0a03b1b5eb17a841baba14f58206685d375c9
Rat <none@none>
2009-04-12 19:49:23 +0200

    *hyjal update -lowered raid damage to 700k -added horde base cleaning wave (watch out for infernal rain)
    --HG--
    branch : trunk

--- src/bindings/scripts/scripts/zone/caverns_of_time/hyjal/boss_kazrogal.cpp
+++ src/bindings/scripts/scripts/zone/caverns_of_time/hyjal/boss_kazrogal.cpp
@@ -31,7 +31,6 @@ struct TRINITY_DLL_DECL boss_kazrogalAI : public hyjal_trashAI
         pInstance = ((ScriptedInstance*)c->GetInstanceData());
         go = false;    
         pos = 0;
-        Reset();
         SpellEntry *TempSpell = (SpellEntry*)GetSpellStore()->LookupEntry(SPELL_MARK);
         if(TempSpell && TempSpell->EffectImplicitTargetA[0] != 1)
         {
