7f08ddca8f79e560d772832aefa39bcff47af5e2
Drahy <none@none>
2009-04-15 18:09:55 +0200

    * Used trinity sql coding style * Please Use Delete + Insert instead Ignore it will be better for this kind of data thx
    --HG--
    branch : trunk

--- sql/updates/2686_world_command.sql
+++ sql/updates/2686_world_command.sql
@@ -1,1 +1,3 @@
-insert into `command` (`name`, `security`, `help`) values('go ticket','1','Syntax: .go ticket #ticketid\r\nTeleports the user to the location where $ticketid was created.');
+DELETE FROM `command` WHERE `name` IN ('go ticket');
+INSERT INTO `command` VALUES 
+('go ticket','1','Syntax: .go ticket #ticketid\r\nTeleports the user to the location where $ticketid was created.');
--- sql/updates/2689_world_spell_linked_spell.sql
+++ sql/updates/2689_world_spell_linked_spell.sql
@@ -1,1 +1,2 @@
-INSERT IGNORE INTO `spell_linked_spell` VALUES (51723, 52874, 0, 'Fan Of Knives');
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger` IN (51723);
+INSERT INTO `spell_linked_spell` VALUES (51723, 52874, 0, 'Fan Of Knives');
