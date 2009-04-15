4fd5933e75251e2532c9a760e968d4f7f8e7399c
Drahy <none@none>
2009-04-15 18:09:55 +0200

    * Used trinity sql coding style * Please Use Delete + Insert instead Ignore it will be better for this kind of data thx
    --HG--
    branch : trunk

--- sql/updates/7662_02_mangos_spell_bonus_data.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE db_version CHANGE COLUMN required_7662_01_mangos_spell_chain required_7662_02_mangos_spell_bonus_data bit;
-
-DELETE FROM `spell_bonus_data` WHERE entry IN (50288, 50294);
-INSERT INTO `spell_bonus_data` VALUES
-(50288, 0.05, 0, 0, "Druid - Starfall"),
-(50294, 0.012, 0, 0, "Druid - Starfall AOE");
