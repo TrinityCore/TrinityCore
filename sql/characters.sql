68781fb8b3bf071af0e42e397a064b2c75526429
Chaz Brown <iamparadox@netscape.net>
2009-04-21 02:09:52 -0400

    Fix characters.sql bad merge leftovers
    --HG--
    branch : trunk

--- sql/characters.sql
+++ sql/characters.sql
@@ -16,29 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
-<<<<<<< HEAD:sql/characters.sql
-=======
--- Table structure for table `character_db_version`
---
-
-DROP TABLE IF EXISTS `character_db_version`;
-CREATE TABLE `character_db_version` (
-  `required_7644_01_characters_character_pet` bit(1) default NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Last applied sql update to DB';
-
---
--- Dumping data for table `character_db_version`
---
-
-LOCK TABLES `character_db_version` WRITE;
-/*!40000 ALTER TABLE `character_db_version` DISABLE KEYS */;
-INSERT INTO `character_db_version` VALUES
-(NULL);
-/*!40000 ALTER TABLE `character_db_version` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
->>>>>>> ddfa7199207e499dd233854a75ac3a6216938873:sql/characters.sql
 -- Table structure for table `account_data`
 --
 
