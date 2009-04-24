2f400c74341b3f453706957379b783bc01821d82
Chaz Brown <iamparadox@netscape.net>
2009-04-24 03:09:41 -0400

    Backed out changeset c1aef8af04ef (rev 2895) until I (or someone else) can figure out how to do it correctly.
    --HG--
    branch : trunk

--- src/bindings/scripts/ScriptMgr.cpp
+++ src/bindings/scripts/ScriptMgr.cpp
@@ -948,12 +948,12 @@ void ScriptsInit()
     outstring_log("");
 
     //Get configuration file
-    if (!TScriptConfig.SetSource(sConfig.GetFilename()))
+    if (!TScriptConfig.SetSource(_TRINITY_SCRIPT_CONFIG))
     {
         CanLoadDB = false;
         error_log("TSCR: Unable to open configuration file. Database will be unaccessible. Configuration values will use default.");
     }
-    else outstring_log("TSCR: Using configuration file %s",TScriptConfig.GetFilename());
+    else outstring_log("TSCR: Using configuration file %s",_TRINITY_SCRIPT_CONFIG);
 
     outstring_log("");
 
