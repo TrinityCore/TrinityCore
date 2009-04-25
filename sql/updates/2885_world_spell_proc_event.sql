6909b20f2cc05d4584861f89460083f1334c6be1
Drahy <none@none>
2009-04-26 01:51:58 +0200

    * Used trinity sql coding style
    --HG--
    branch : trunk

--- sql/updates/2885_world_spell_proc_event.sql
+++ sql/updates/2885_world_spell_proc_event.sql
@@ -6,4 +6,3 @@ INSERT INTO `spell_proc_event` VALUES
 (19615, 0x00, 0, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x0000002, 0.000000, 0.000000, 0);
 
 INSERT INTO trinity_string (entry, content_default)VALUES (6616 , "Pet spells will reset for all players at login. Strongly recommend re-login!");
-
