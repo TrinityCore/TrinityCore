9240a9df73060bb4d21085057d57a72983d11556
megamage <none@none>
2009-04-27 18:43:39 -0500

    [7717] Use more safe code in EventAI. Author: VladimirMangos
    --HG--
    branch : trunk

--- src/game/CreatureEventAIMgr.h
+++ src/game/CreatureEventAIMgr.h
@@ -32,9 +32,9 @@ class CreatureEventAIMgr
         void LoadCreatureEventAI_Summons();
         void LoadCreatureEventAI_Scripts();
 
-        CreatureEventAI_Event_Map&  GetCreatureEventAIMap()       { return m_CreatureEventAI_Event_Map; }
-        CreatureEventAI_Summon_Map& GetCreatureEventAISummonMap() { return m_CreatureEventAI_Summon_Map; }
-        CreatureEventAI_TextMap&    GetCreatureEventAITextMap()   { return m_CreatureEventAI_TextMap; }
+        CreatureEventAI_Event_Map  const& GetCreatureEventAIMap()       const { return m_CreatureEventAI_Event_Map; }
+        CreatureEventAI_Summon_Map const& GetCreatureEventAISummonMap() const { return m_CreatureEventAI_Summon_Map; }
+        CreatureEventAI_TextMap    const& GetCreatureEventAITextMap()   const { return m_CreatureEventAI_TextMap; }
 
     private:
         CreatureEventAI_Event_Map  m_CreatureEventAI_Event_Map;
