9c1e32038ae5adce81e6d236d61fb92dc6c9bc23
Drahy <none@none>
2009-04-29 18:35:08 +0200

    * Used trinity sql coding style
    --HG--
    branch : trunk

--- sql/updates/7720_01_mangos_mangos_string.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE db_version CHANGE COLUMN required_7714_01_mangos_command required_7720_01_mangos_mangos_string bit;
-
-DELETE FROM mangos_string WHERE entry IN(557,558,559);
-INSERT INTO mangos_string VALUES
-(557,'%s level up you to (%i)',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(558,'%s level down you to (%i)',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-(559,'%s reset your level progress.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
