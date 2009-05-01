460c4e44aa559a102fc46cc6cdfa406e2bf5181b
Anubisss <none@none>
2009-05-01 21:40:49 +0200

    *Make spell Protection of Elune immune from spell Denouement Wisp.
    --HG--
    branch : trunk

---
+++ sql/updates/3039_world_spell_linked_spell.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = 38528 AND `spell_effect` = -32124;
+
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES
+(38528, -32124, 2, 'Protection of Elune - Denouement Wisp');
