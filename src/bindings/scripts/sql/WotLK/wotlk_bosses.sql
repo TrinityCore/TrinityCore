9fbaeb27e7acba262bcf2c639b193150c24ee251
Anubisss <none@none>
2009-05-01 19:36:49 +0200

    *Delete 2 not needed scripts. *Add the correct Utgarde Keep's SQL updates to wotlk_bosses.sql
    --HG--
    branch : trunk

--- src/bindings/scripts/sql/WotLK/wotlk_bosses.sql
+++ src/bindings/scripts/sql/WotLK/wotlk_bosses.sql
@@ -95,11 +95,15 @@ UPDATE `creature_template` SET `ScriptName`='boss_sjonnir' WHERE `entry`='27978'
 /*  */
 
 /* UTGARDE KEEP */
-UPDATE `instance_template` SET `script`='instance_keep' WHERE `map`=206;
-UPDATE `creature_template` SET `ScriptName`='boss_dalronn' WHERE `entry`='24201';
-UPDATE `creature_template` SET `ScriptName`='boss_ingvar' WHERE `entry`='23954';
+UPDATE `instance_template` SET `script`='instance_utgarde_keep' WHERE `map`=206;
+UPDATE `creature_template` SET `ScriptName`='boss_dalronn_the_controller' WHERE `entry`='24201';
+UPDATE `creature_template` SET `ScriptName`='boss_ingvar_the_plunderer' WHERE `entry`='23954';
+UPDATE `creature_template` SET `ScriptName`='mob_annhylde_the_caller' WHERE `entry`=24068;
+UPDATE `creature_template` SET `ScriptName`='mob_ingvar_throw_dummy' WHERE `entry`=23997;
 UPDATE `creature_template` SET `ScriptName`='boss_keleseth' WHERE `entry`='23953';
-UPDATE `creature_template` SET `ScriptName`='boss_skarvald' WHERE `entry`='24200';
+UPDATE `creature_template` SET `scriptname` = 'mob_frost_tomb' WHERE `entry` = '23965';
+UPDATE `creature_template` SET `ScriptName`='mob_vrykul_skeleton' WHERE `entry`=23970;
+UPDATE `creature_template` SET `ScriptName`='boss_skarvald_the_constructor' WHERE `entry`='24200';
 
 /* UTGARDE PINNACLE */
 UPDATE `instance_template` SET `script`='instance_pinnacle' WHERE `map`=1196;
