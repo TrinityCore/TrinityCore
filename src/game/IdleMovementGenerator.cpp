5140aad19467e3cf34f0182c0c46448beee59ff8
megamage <none@none>
2009-05-01 21:04:02 -0500

    *Send stop packet iff necessary. Hope this can fix the bug that cannot immediatly remove confuse movement and face wrong direction after charging.
    --HG--
    branch : trunk

--- src/game/IdleMovementGenerator.cpp
+++ src/game/IdleMovementGenerator.cpp
@@ -23,14 +23,19 @@
 
 IdleMovementGenerator si_idleMovement;
 
+// StopMoving is needed to make unit stop if its last movement generator expires
+// But it should not be sent otherwise there are many redundent packets
 void IdleMovementGenerator::Initialize(Unit &owner)
 {
-    owner.StopMoving();
+    if(owner.hasUnitState(UNIT_STAT_MOVE))
+        owner.StopMoving();
 }
 
 void
-IdleMovementGenerator::Reset(Unit& /*owner*/)
+IdleMovementGenerator::Reset(Unit& owner)
 {
+    if(owner.hasUnitState(UNIT_STAT_MOVE))
+        owner.StopMoving();
 }
 
 void
