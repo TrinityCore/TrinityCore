260a864031c6c8b6ce1bfda2db4c2060c9437a90
Drahy <none@none>
2009-05-03 19:21:52 +0200

    * Big cleanup in sql folder * Redone Full sql with scripts * Recommend you only reimport world_scripts_full + 3089_world_trinity_string of course
    --HG--
    branch : trunk

--- sql/updates/3005_world_scripts.sql
+++
@@ -1,3 +0,0 @@
-update creature_template set scriptname = 'npc_unworthy_initiate' where entry in (29519,29520,29565,29566,29567);
-update creature_template set scriptname = 'npc_unworthy_initiate_anchor' where entry = 29521;
-update gameobject_template set scriptname = 'go_acherus_soul_prison' where entry in (191577,191580,191581,191582,191583,191584,191585,191586,191587,191588,191589,191590);
