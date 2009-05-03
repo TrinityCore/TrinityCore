e08969b1434561f48fb62d0f952de03225822791
QAston <none@none>
2009-05-03 20:00:29 +0200

    *Update some procflags for 309. *Do not trigger aoe damage for explosive shot.
    --HG--
    branch : trunk

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -6572,7 +6572,7 @@ void AuraEffect::PeriodicDummyTick()
                 if (!caster)
                     return;
                 int32 damage = m_amount;
-                caster->CastCustomSpell(m_target, 56298, &damage, 0, 0, true, 0, this);
+                caster->CastCustomSpell(m_target, 53352, &damage, 0, 0, true, 0, this);
                 return;
             }
             switch (spell->Id)
