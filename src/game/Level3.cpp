68244e96aa8f767b71d7ca6124b6888a5d3d42c0
megamage <none@none>
2009-05-03 22:23:39 -0500

    [7748] Better use LANG_PLAYER_NOT_FOUND message in Modify Gender. Author: AlexDereka
    --HG--
    branch : trunk

--- src/game/Level3.cpp
+++ src/game/Level3.cpp
@@ -7457,7 +7457,7 @@ bool ChatHandler::HandleModifyGenderCommand(const char *args)
 
     if(!player)
     {
-        PSendSysMessage(LANG_NO_PLAYER, args);
+        PSendSysMessage(LANG_PLAYER_NOT_FOUND);
         SetSentErrorMessage(true);
         return false;
     }
