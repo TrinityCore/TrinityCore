7d1521ff0c866d76c573a6e907daa66ef4fab09f
megamage <none@none>
2009-05-03 22:33:41 -0500

    *Fix build.
    --HG--
    branch : trunk

---
+++ src/game/pchdef.h
@@ -0,0 +1,16 @@
+//add here most rarely modified headers to speed up debug build compilation
+#include "WorldSocket.h"                                    // must be first to make ACE happy with ACE includes in it
+#include "Common.h"
+
+#include "MapManager.h"
+#include "Log.h"
+#include "ObjectAccessor.h"
+#include "ObjectDefines.h"
+#include "Database/SQLStorage.h"
+#include "Opcodes.h"
+#include "SharedDefines.h"
+
+#ifdef FASTBUILD
+//add additional headers here to speed up compilation in release builds even more
+#include "ObjectMgr.h"
+#endif
