af59c5d53677ede5b826fb55bfd1bb0d6ad4a307
Anubisss <none@none>
2009-05-04 20:21:14 +0200

    *Fix Healing Way stack counting in Healing Wave's script.
    --HG--
    branch : trunk

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -9033,8 +9033,13 @@ uint32 Unit::SpellHealingBonus(SpellEntry const *spellProto, uint32 healamount, 
         int32 pctMod = 0;
         Unit::AuraList const& auraDummy = pVictim->GetAurasByType(SPELL_AURA_DUMMY);
         for(Unit::AuraList::const_iterator itr = auraDummy.begin(); itr!=auraDummy.end(); ++itr)
+        {
             if((*itr)->GetId() == 29203)
-                pctMod += (*itr)->GetModifier()->m_amount;
+            {
+                pctMod = (*itr)->GetModifier()->m_amount * (*itr)->GetStackAmount();
+                break;
+            }
+        }
         // Apply bonus
         if (pctMod)
             heal = heal * (100 + pctMod) / 100;
