0e4c1167df6493e841f7957d639a961ed559b020
megamage <none@none>
2009-05-06 00:07:28 -0500

    *add SPELL_ATTR_CU_EXCLUDE_SELF
    --HG--
    branch : trunk

--- src/game/SpellMgr.h
+++ src/game/SpellMgr.h
@@ -679,6 +679,7 @@ inline bool IsProfessionSkill(uint32 skill)
 #define SPELL_ATTR_CU_LINK_AURA         0x00001000
 #define SPELL_ATTR_CU_LINK_REMOVE       0x00002000
 #define SPELL_ATTR_CU_MOVEMENT_IMPAIR   0x00004000
+#define SPELL_ATTR_CU_EXCLUDE_SELF      0x00008000
 
 
 typedef std::vector<uint32> SpellCustomAttribute;
