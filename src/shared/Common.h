a68e6e12c72f8f96ff8485cd2e5a14e8df5fd5f6
megamage <none@none>
2009-05-08 18:45:26 -0500

    [7795] Added comments. Author: AlexDereka
    --HG--
    branch : trunk

--- src/shared/Common.h
+++ src/shared/Common.h
@@ -62,26 +62,17 @@
 #include "Platform/Define.h"
 
 #if COMPILER == COMPILER_MICROSOFT
-
-#pragma warning(disable:4996)
-
+#   pragma warning(disable:4996)                            // 'function': was declared deprecated
 #ifndef __SHOW_STUPID_WARNINGS__
-
-#pragma warning(disable:4244)
-
-#pragma warning(disable:4267)
-
-#pragma warning(disable:4800)
-
-#pragma warning(disable:4018)
-
-#pragma warning(disable:4311)
-
-#pragma warning(disable:4305)
-
-#pragma warning(disable:4005)
-
-#pragma warning(disable:4522)//warning when class has 2 constructosr
+#   pragma warning(disable:4005)                            // 'identifier' : macro redefinition
+#   pragma warning(disable:4018)                            // 'expression' : signed/unsigned mismatch
+#   pragma warning(disable:4244)                            // 'argument' : conversion from 'type1' to 'type2', possible loss of data
+#   pragma warning(disable:4267)                            // 'var' : conversion from 'size_t' to 'type', possible loss of data
+#   pragma warning(disable:4305)                            // 'identifier' : truncation from 'type1' to 'type2'
+#   pragma warning(disable:4311)                            // 'variable' : pointer truncation from 'type' to 'type'
+#   pragma warning(disable:4355)                            // 'this' : used in base member initializer list
+#   pragma warning(disable:4800)                            // 'type' : forcing value to bool 'true' or 'false' (performance warning)
+#   pragma warning(disable:4522)                            //warning when class has 2 constructosr
 #endif                                                      // __SHOW_STUPID_WARNINGS__
 #endif                                                      // __GNUC__
 
