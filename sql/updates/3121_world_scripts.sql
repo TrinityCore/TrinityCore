1c23f9fc202b9079a0d051b561692882e3ee2d70
Drahy <none@none>
2009-05-08 09:57:45 +0200

    * Lost some sql files for sql coding style
    --HG--
    branch : trunk

--- sql/updates/3121_world_scripts.sql
+++ sql/updates/3121_world_scripts.sql
@@ -1,1 +1,1 @@
-UPDATE `creature_template` SET `ScriptName`='npc_dragonflayer_forge_master' WHERE `entry` = 24079;
+UPDATE `creature_template` SET `ScriptName`='npc_dragonflayer_forge_master' WHERE `entry` =24079;
