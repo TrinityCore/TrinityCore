3ca5d7668a1a393fabc5191e6334964f5a512cf6
megamage <none@none>
2009-05-09 10:48:55 -0500

    *Init player motionmaster after field value are initialized.
    --HG--
    branch : trunk

--- src/game/CharacterHandler.cpp
+++ src/game/CharacterHandler.cpp
@@ -571,7 +571,6 @@ void WorldSession::HandlePlayerLogin(LoginQueryHolder * holder)
     uint64 playerGuid = holder->GetGuid();
 
     Player* pCurrChar = new Player(this);
-    pCurrChar->GetMotionMaster()->Initialize();
      // for send server info and strings (config)
     ChatHandler chH = ChatHandler(pCurrChar);
 
@@ -584,6 +583,8 @@ void WorldSession::HandlePlayerLogin(LoginQueryHolder * holder)
         m_playerLoading = false;
         return;
     }
+
+    pCurrChar->GetMotionMaster()->Initialize();
 
     SetPlayer(pCurrChar);
 
