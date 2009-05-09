85d2057cdff38801dbe5cb77c80c00bf45362e65
megamage <none@none>
2009-05-08 19:22:17 -0500

    [7802] Use more appropriate field types in `character_achievement*` tables. Better check data at loading. Author: VladimirMangos
    --HG--
    branch : trunk

---
+++ sql/updates/7802_02_characters_character_achievement_progress.sql
@@ -0,0 +1,7 @@
+ALTER TABLE character_db_version CHANGE COLUMN required_7802_01_characters_character_achievement required_7802_02_characters_character_achievement_progress bit;
+
+ALTER TABLE character_achievement_progress
+  CHANGE COLUMN guid guid int(11) unsigned NOT NULL,
+  CHANGE COLUMN criteria criteria int(11) unsigned NOT NULL,
+  CHANGE COLUMN counter counter int(11) unsigned NOT NULL,
+  CHANGE COLUMN date date bigint(11) unsigned NOT NULL default '0';
