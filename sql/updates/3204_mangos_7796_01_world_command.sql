14213727c4ddcc5975ca102e3f7d099a8e8462fe
Drahy <none@none>
2009-05-09 12:27:41 +0200

    * Updated world_script_waypoints * Updated world_scripts_structure * Moved older sql files to 3.0.9_old folder and in file 0000_3.0.9_old (sql/updates) are stored all these sqls * Also created folder Obsolete_updates where you can find unverified sqls * This should help with sql maintaining
    --HG--
    branch : trunk

--- sql/updates/3204_mangos_7796_01_world_command.sql
+++ sql/updates/3204_mangos_7796_01_world_command.sql
@@ -1,7 +1,6 @@
-ALTER TABLE db_version CHANGE COLUMN required_7782_01_mangos_spell_proc_event required_7796_01_mangos_command bit;
+/*ALTER TABLE db_version CHANGE COLUMN required_7782_01_mangos_spell_proc_event required_7796_01_mangos_command bit;*/
 
 DELETE FROM `command` WHERE `name` IN ('go taxinode','lookup taxinode');
-
 INSERT INTO `command` VALUES
 ('go taxinode',1,'Syntax: .go taxinode #taxinode\r\n\r\nTeleport player to taxinode coordinates. You can look up zone using .lookup taxinode $namepart'),
 ('lookup taxinode',3,'Syntax: .lookup taxinode $substring\r\n\r\nSearch and output all taxinodes with provide $substring in name.');
--- sql/updates/7802_01_characters_character_achievement.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE character_db_version CHANGE COLUMN required_7644_01_characters_character_pet required_7802_01_characters_character_achievement bit;
-
-ALTER TABLE character_achievement
-  CHANGE COLUMN guid guid int(11) unsigned NOT NULL,
-  CHANGE COLUMN achievement achievement int(11) unsigned NOT NULL,
-  CHANGE COLUMN date date bigint(11) unsigned NOT NULL default '0';
