e35d07ad81f5acedcbe436bf429db5581edc6b81
megamage <none@none>
2009-05-09 10:50:41 -0500

    *Update Sapphiron script.
    --HG--
    branch : trunk

--- src/bindings/scripts/scripts/creature/mob_generic_creature.cpp
+++ src/bindings/scripts/scripts/creature/mob_generic_creature.cpp
@@ -156,11 +156,58 @@ struct TRINITY_DLL_DECL generic_creatureAI : public ScriptedAI
         }
     }
 };
+
 CreatureAI* GetAI_generic_creature(Creature *_Creature)
 {
     return new generic_creatureAI (_Creature);
 }
 
+struct TRINITY_DLL_DECL trigger_periodicAI : public NullCreatureAI
+{
+    trigger_periodicAI(Creature* c) : NullCreatureAI(c)
+    {
+        if(me->m_spells[0])
+        {
+            if(me->m_spells[1])
+                spell = GetSpellStore()->LookupEntry(HEROIC(me->m_spells[0], me->m_spells[1]));
+            else
+                spell = GetSpellStore()->LookupEntry(me->m_spells[0]);
+        }
+        else
+            spell = NULL;
+
+        if(me->m_spells[2])
+        {
+            if(me->m_spells[3])
+                interval = HEROIC(me->m_spells[2], me->m_spells[3]);
+            else
+                interval = me->m_spells[2];
+        }
+        else
+            interval = 1000;
+        timer = interval;
+    }
+
+    uint32 timer, interval;
+    const SpellEntry * spell;
+
+    void UpdateAI(const uint32 diff)
+    {
+        if(timer < diff)
+        {
+            if(spell)
+                me->CastSpell(me, spell, true);
+            timer = interval;
+        }
+        else
+            timer -= diff;
+    }
+};
+
+CreatureAI* GetAI_trigger_periodic(Creature *_Creature)
+{
+    return new trigger_periodicAI (_Creature);
+}
 
 void AddSC_generic_creature()
 {
@@ -168,6 +215,11 @@ void AddSC_generic_creature()
     newscript = new Script;
     newscript->Name="generic_creature";
     newscript->GetAI = &GetAI_generic_creature;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name="trigger_periodic";
+    newscript->GetAI = &GetAI_trigger_periodic;
     newscript->RegisterSelf();
 }
 
