39ec94082b773871d2956e2b349d389948f0f6f2
QAston <none@none>
2009-05-15 18:09:44 +0200

    *Add sqls for latest commit.
    --HG--
    branch : trunk

--- sql/world_spell_full.sql
+++ sql/world_spell_full.sql
@@ -1494,3 +1494,25 @@ INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `Spell
 (9799, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0),
 (56372, 0, 3, 0, 128, 0, 16384, 0, 0, 0, 0);
 -- (42370, 0x00, 11, 0x00000080, 0x00000000, 0x00000000, 0.000000, 0.000000, 0);
+
+-- --------
+-- ENCHANT PROC
+-- --------
+
+CREATE TABLE `spell_enchant_proc_data` (
+  `entry` INT(10) UNSIGNED NOT NULL,
+  `customChance` INT(10) UNSIGNED NOT NULL DEFAULT '0',
+  `PPMChance` FLOAT UNSIGNED NOT NULL DEFAULT '0', 
+  `procEx` FLOAT UNSIGNED NOT NULL DEFAULT '0'
+) ENGINE=MYISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Spell enchant proc data';
+
+-- Frostbrand Weapon
+INSERT INTO spell_enchant_proc_data (`entry`, `customChance`, `PPMChance`,`procEx`) VALUES (2, 0, 8.8,0);
+INSERT INTO spell_enchant_proc_data (`entry`, `customChance`, `PPMChance`,`procEx`) VALUES (12, 0, 8.8,0);
+INSERT INTO spell_enchant_proc_data (`entry`, `customChance`, `PPMChance`,`procEx`) VALUES (524, 0, 8.8,0);
+INSERT INTO spell_enchant_proc_data (`entry`, `customChance`, `PPMChance`,`procEx`) VALUES (1667, 0, 8.8,0);
+INSERT INTO spell_enchant_proc_data (`entry`, `customChance`, `PPMChance`,`procEx`) VALUES (1668, 0, 8.8,0);
+INSERT INTO spell_enchant_proc_data (`entry`, `customChance`, `PPMChance`,`procEx`) VALUES (2635, 0, 8.8,0);
+INSERT INTO spell_enchant_proc_data (`entry`, `customChance`, `PPMChance`,`procEx`) VALUES (3782, 0, 8.8,0);
+INSERT INTO spell_enchant_proc_data (`entry`, `customChance`, `PPMChance`,`procEx`) VALUES (3783, 0, 8.8,0);
+INSERT INTO spell_enchant_proc_data (`entry`, `customChance`, `PPMChance`,`procEx`) VALUES (3784, 0, 8.8,0);
