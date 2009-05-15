15c78a728e08ce95999b980ce7572092bc0a75cb
megamage <none@none>
2009-05-15 16:36:59 -0500

    *Skip Mangos ([7833] Implement levelup spells for non-hunter pets. Author: VladimirMangos)
    --HG--
    branch : trunk

--- src/game/SharedDefines.h
+++ src/game/SharedDefines.h
@@ -1865,6 +1865,8 @@ enum CreatureFamily
     CREATURE_FAMILY_SPIRIT_BEAST   = 46
 };
 
+#define MAX_CREATURE_FAMILY          47
+
 enum CreatureTypeFlags
 {
     CREATURE_TYPEFLAGS_TAMEABLE        = 0x0001,
