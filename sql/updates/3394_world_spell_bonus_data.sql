609f7bd6b97875cf8fcbe38b8615908d896f3684
megamage <none@none>
2009-05-18 14:50:29 -0500

    *Add a missing part of sql of 3391. By nesocip
    --HG--
    branch : trunk

--- sql/updates/3394_world_spell_bonus_data.sql
+++ sql/updates/3394_world_spell_bonus_data.sql
@@ -1,4 +1,4 @@
-DELETE FROM `spell_bonus_data` WHERE `entry` IN (50288,50294,53191,53188,53194,53189,53195,53190);
+DELETE FROM `spell_bonus_data` WHERE `entry` IN (48505,50288,50294,53191,53188,53194,53189,53195,53190);
 INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `comments`) VALUES
 (50288, 0.05, 0, 0, 'Druid - Starfall rank 1'),
 (50294, 0.012, 0, 0, 'Druid - Starfall AOE rank 1'),
