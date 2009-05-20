7df0e4e1fd3701f88eb9dafc516f394798d6db1c
Drahy <none@none>
2009-05-20 04:25:27 +0200

    * Used trinity sql coding style
    --HG--
    branch : trunk

--- sql/updates/3409_world_spell_proc_event.sql
+++ sql/updates/3409_world_spell_proc_event.sql
@@ -1,5 +1,4 @@
 DELETE FROM `spell_proc_event` WHERE `entry` IN (31878, 31877, 31876, 46913, 46914, 46915);
-
 INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
 (31876, 0, 10, 8388608, 0, 0, 0, 0, 0, 0, 0),
 (31877, 0, 10, 8388608, 0, 0, 0, 0, 0, 0, 0),
--- sql/updates/3416_mangos_7855_01_world_pools.sql
+++ sql/updates/3416_mangos_7855_01_world_pools.sql
@@ -1,4 +1,4 @@
-ALTER TABLE db_version CHANGE COLUMN required_7850_01_mangos_command required_7855_01_mangos_pools bit;
+/*ALTER TABLE db_version CHANGE COLUMN required_7850_01_mangos_command required_7855_01_mangos_pools bit;*/
 
 ALTER TABLE pool_creature
   ADD COLUMN description varchar(255) NOT NULL AFTER chance;
