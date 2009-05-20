ab513337385ea9b49c272f7b0cb3aae5306b6fc2
Kudlaty <none@none>
2009-05-20 14:57:29 +0200

    [SD2] 1075 Added 6 new taxi paths (wyrmrest temple)
    --HG--
    branch : trunk

---
+++ sql/updates/3423_world_scripts.sql
@@ -0,0 +1,1 @@
+UPDATE `creature_template` SET `ScriptName`='npc_taxi' WHERE `entry` IN (27575, 26443, 26949);
--- sql/world_scripts_full.sql
+++ sql/world_scripts_full.sql
@@ -99,7 +99,7 @@ UPDATE `creature_template` SET `ScriptName`='npc_snake_trap_serpents' WHERE `ent
 UPDATE `creature_template` SET `ScriptName`='npc_unworthy_initiate' WHERE `entry` IN (29519,29520,29565,29566,29567);
 UPDATE `creature_template` SET `ScriptName`='npc_unworthy_initiate_anchor' WHERE `entry`=29521;
 UPDATE `creature_template` SET `ScriptName`='npc_kingdom_of_dalaran_quests' WHERE `entry` IN (29169,23729,26673,27158,29158,29161,26471,29155,29159,29160,29162);
-UPDATE `creature_template` SET `ScriptName`='npc_taxi' WHERE `entry` IN (17435, 23413, 18725, 19401, 19409, 20235, 25059, 25236, 20903, 20162, 29154, 23415);
+UPDATE `creature_template` SET `ScriptName`='npc_taxi' WHERE `entry` IN (17435, 23413, 18725, 19401, 19409, 20235, 25059, 25236, 20903, 20162, 29154, 23415, 27575, 26443, 26949);
 
 /*  */
 /* ZONE */
