ceaf8c13bbf9dbe407d48bc489c0fc02e7e6aef4
megamage <none@none>
2009-05-22 16:36:54 -0500

    [7866] Added VS 2010 Beta 1 project files. Requires libeay32.dll update. This is experimental changes. Use it on own risk. Author: tomrus88 *Note: this is not supported by TC
    --HG--
    branch : trunk

--- dep/ACE_wrappers/ace/pre.h
+++ dep/ACE_wrappers/ace/pre.h
@@ -16,6 +16,7 @@
 // No header guard
 #if defined (_MSC_VER)
 # pragma warning (disable:4103)
+# pragma warning (disable:4005)
 # pragma pack (push, 8)
 #elif defined (__BORLANDC__)
 # pragma option push -a8 -b -Ve- -Vx- -w-rvl -w-rch -w-ccc -w-obs -w-aus -w-pia -w-inl -w-sig
