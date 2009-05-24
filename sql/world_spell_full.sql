3f8ad150d9cb9833b7f44a525ea3061da8e8c028
megamage <none@none>
2009-05-24 11:07:09 -0500

    *Add vehicle id for three-seat mammoth. *Fix spell impale of archavon.
    --HG--
    branch : trunk

--- sql/world_spell_full.sql
+++ sql/world_spell_full.sql
@@ -135,7 +135,10 @@ INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comm
 (-28169, 28206, 0, 'Mutating Injection - Mutagen Explosion'),
 ( 28732,-28798, 1, 'Widow\'s Embrace - Frenzy'),
 ( 54097,-54100, 1, 'Widow\'s Embrace - Frenzy (H)'),
-(-28169, 28240, 0, 'Mutating Injection - Poison Cloud');
+(-28169, 28240, 0, 'Mutating Injection - Poison Cloud'),
+-- Archavon
+( 58666, 58672, 1, 'Impale (Archavon)'),
+( 60882, 58672, 1, 'Impale (Archavon)');
 
 
 -- --------
@@ -533,6 +536,11 @@ INSERT INTO `spell_script_target` (`entry`, `type`, `targetEntry`) VALUES
 (45839, 1, 25653), -- Vengeance of the Blue Flight - Power of the Blue Flight
 (45976, 1, 25770), -- Open Portal
 (46177, 1, 25770); -- Open All Portals
+
+DELETE FROM `spell_script_target` WHERE entry IN
+(58672);
+INSERT INTO `spell_script_target` (`entry`, `type`, `targetEntry`) VALUES
+(58672, 1, 31125); -- Impale (Archavon)
 
 -- --------
 -- POSITION
@@ -1547,6 +1555,11 @@ spell2 = VALUES(spell2),
 spell3 = VALUES(spell3),
 spell4 = VALUES(spell4);
 
+-- Three-seat mammoth
+UPDATE creature_template SET VehicleId = 312 WHERE entry IN (31857,31858,31861,31862,32212,32213,32633,32640);
+
+-- frostbrood vanquisher
+update creature_template set maxhealth = 133525, minhealth = 133525, maxmana = 51360, minmana = 51360, spell1 = 53114, spell2 = 53112, spell3=53110 where entry = 28670;
 
 -- --------
 -- NAXXARAMAS
@@ -1674,10 +1687,11 @@ INSERT INTO `spell_script_target` (`entry`, `type`, `targetEntry`) VALUES
 (56669, 1, 27852),
 (61408, 1, 27852);
 
-update creature_template set maxhealth = 133525, minhealth = 133525, maxmana = 51360, minmana = 51360, spell1 = 53114, spell2 = 53112, spell3=53110 where entry = 28670;
 update creature_template set maxhealth = 30000, minhealth = 30000, speed = 2, spell1=50025, spell2=50989, VehicleId = 36 where entry = 27881;
 update creature_template set maxhealth = 50000, minhealth = 50000, speed = 1.6, spell1=50896, spell2=50652, VehicleId = 106 where entry = 28094;
 update creature_template set maxhealth = 75000, minhealth = 75000, speed = 1, spell1=51678, VehicleId = 117 where entry IN (28312,32627);
 update creature_template set maxhealth = 50000, minhealth = 50000, spell1=51362, VehicleId = 116 where entry IN (28319,32629);
 
 update creature_template set scriptname = "npc_demolisher_engineerer" where entry in (30400,30499);
+
+UPDATE creature_template SET VehicleId = 174 WHERE entry IN (31125,31722);
