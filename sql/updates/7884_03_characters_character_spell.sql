70c294069b3d0c1a799e19bdb5d8dc4d76cc129c
Drahy <none@none>
2009-05-25 02:29:41 +0200

    * Added revision info to full tables up to revision are updated * Removed some sqls from mangos already present in rev 3235 * Used trinity sql coding style
    --HG--
    branch : trunk

--- sql/updates/7884_03_characters_character_spell.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE character_db_version CHANGE COLUMN required_7802_02_characters_character_achievement_progress required_7884_03_characters_character_spell bit;
-
-DELETE FROM character_spell WHERE spell = '28734';
