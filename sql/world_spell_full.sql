bc9bf1d635f3631292f4ff4b13a513225252ece4
Kudlaty <none@none>
2009-05-26 21:24:30 +0200

    Uptade spell script target with spells (54699,54725,54744,54746)
    --HG--
    branch : trunk

--- sql/world_spell_full.sql
+++ sql/world_spell_full.sql
@@ -498,6 +498,15 @@ DELETE FROM `spell_script_target` WHERE entry IN
 INSERT INTO `spell_script_target` (`entry`, `type`, `targetEntry`) VALUES
 (58672, 1, 31125); -- Impale (Archavon)
 
+-- Teleports in dk starting area
+DELETE FROM `spell_script_target` WHERE entry IN
+(54699,54725,54744,54746);
+INSERT INTO `spell_script_target` (`entry`, `type`, `targetEntry`) VALUES
+(54699,1,0),
+(54725,1,0),
+(54744,1,0),
+(54746,1,0);
+
 -- --------
 -- POSITION
 -- --------
