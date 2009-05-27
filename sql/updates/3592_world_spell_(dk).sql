199cac6ddab1389bf5b31290344c9f3f6720b536
Drahy <none@none>
2009-05-27 20:19:23 +0200

    * Used trinity sql coding style
    --HG--
    branch : trunk

--- sql/updates/3592_world_spell_(dk).sql
+++ sql/updates/3592_world_spell_(dk).sql
@@ -1,11 +1,8 @@
-update creature_template set spell5=51890 where entry = 28511; -- Eye of Acherus flight
-
-DELETE FROM `spell_script_target` WHERE entry IN
-(51859);
+DELETE FROM `spell_script_target` WHERE entry IN (51859);
 INSERT INTO `spell_script_target` (`entry`, `type`, `targetEntry`) VALUES
 (51859, 1, 28525), -- siphon of archerus
 (51859, 1, 28542),
 (51859, 1, 28543),
 (51859, 1, 28544);
-
-update creature_template set minlevel=50,maxlevel=52,minhealth=2215,maxhealth=2317,faction_A=2084,faction_H=2084,mindmg=50,maxdmg=50 where entry=28528; -- ghoul
+UPDATE `creature_template` SET `spell5`=51890 WHERE `entry`=28511; -- Eye of Acherus flight
+UPDATE `creature_template` SET `minlevel`=50,`maxlevel`=52,`minhealth`=2215,`maxhealth`=2317,`faction_A`=2084,`faction_H`=2084,`mindmg`=50,`maxdmg`=50 WHERE `entry`=28528; -- ghoul
