82cc456d280cdc8c70ff5e88e0a4cf78d5525522
Kudlaty <none@none>
2009-05-27 18:15:29 +0200

    Add script for quest 28406
    --HG--
    branch : trunk

--- sql/world_scripts_full.sql
+++ sql/world_scripts_full.sql
@@ -102,6 +102,7 @@ UPDATE `creature_template` SET `ScriptName`='npc_unworthy_initiate' WHERE `entry
 UPDATE `creature_template` SET `ScriptName`='npc_unworthy_initiate_anchor' WHERE `entry`=29521;
 UPDATE `creature_template` SET `ScriptName`='npc_kingdom_of_dalaran_quests' WHERE `entry` IN (29169,23729,26673,27158,29158,29161,26471,29155,29159,29160,29162);
 UPDATE `creature_template` SET `ScriptName`='npc_taxi' WHERE `entry` IN (17435, 23413, 18725, 19401, 19409, 20235, 25059, 25236, 20903, 20162, 29154, 23415, 27575, 26443, 26949, 23816);
+UPDATE `creature_template` SET `ScriptName`='npc_death_knight_initiate' WHERE `entry`=28406;
 
 /*  */
 /* ZONE */
