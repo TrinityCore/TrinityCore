05011544dff19e5be0bcc06858ac0f74c1f44f75
Drahy <none@none>
2009-05-28 21:29:53 +0200

    * Used trinity sql coding style
    --HG--
    branch : trunk

--- sql/updates/3601_world.sql
+++ sql/updates/3601_world.sql
@@ -1,8 +1,8 @@
-UPDATE creature_template SET `VehicleId`=200 WHERE `entry` IN (28605,28606,28607);
-UPDATE creature_template SET `ScriptName`='npc_salanar_the_horseman' WHERE `entry`=28653;
+UPDATE `creature_template` SET `VehicleId`=200 WHERE `entry` IN (28605,28606,28607);
+UPDATE `creature_template` SET `ScriptName`='npc_salanar_the_horseman' WHERE `entry`=28653;
 
-DELETE FROM npc_spellclick_spells WHERE `npc_entry` IN (28605,28606,28607);
-INSERT INTO npc_spellclick_spells (`npc_entry`, `spell_id`, `quest_id`, `quest_status`, `cast_flags`) VALUES
+DELETE FROM `npc_spellclick_spells` WHERE `npc_entry` IN (28605,28606,28607);
+INSERT INTO `npc_spellclick_spells` (`npc_entry`, `spell_id`, `quest_id`, `quest_status`, `cast_flags`) VALUES
 (28605, 52263, 12680, 3, 1),
 (28606, 52263, 12680, 3, 1),
 (28607, 52263, 12680, 3, 1);
