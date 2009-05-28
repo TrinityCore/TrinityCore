8bf933c31dd48657c549a82b00beb6aa74cf4959
Drahy <none@none>
2009-05-28 21:29:53 +0200

    * Used trinity sql coding style
    --HG--
    branch : trunk

--- sql/updates/7902_02_mangos_pool_gameobject.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE db_version CHANGE COLUMN required_7902_01_mangos_pool_creature required_7902_02_mangos_pool_gameobject bit;
-
-ALTER TABLE `pool_gameobject` 
-   ADD INDEX `idx_guid`(`guid`);
