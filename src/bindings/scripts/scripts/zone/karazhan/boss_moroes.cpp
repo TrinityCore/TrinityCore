7a0aff4734dd4d306374aa2385e60778880c5f52
BroodWyrm <none@none>
2009-05-30 11:35:54 +0200

    *Fix some Script Bugs ... in TC2 virtual Aggo is EnterCombat now ... this fix Kiljaden, Muru, Moroes Script
    --HG--
    branch : trunk

--- src/bindings/scripts/scripts/zone/karazhan/boss_moroes.cpp
+++ src/bindings/scripts/scripts/zone/karazhan/boss_moroes.cpp
@@ -110,7 +110,7 @@ struct TRINITY_DLL_DECL boss_moroesAI : public ScriptedAI
         DoZoneInCombat();
     }
 
-    void Aggro(Unit* who)
+    void EnterCombat(Unit* who)
     {
         StartEvent();
 
@@ -348,8 +348,6 @@ struct TRINITY_DLL_DECL boss_moroes_guestAI : public ScriptedAI
         if(pInstance)
             pInstance->SetData(DATA_MOROES_EVENT, NOT_STARTED);
     }
-
-    void Aggro(Unit* who) {}
 
     void AcquireGUID()
     {
