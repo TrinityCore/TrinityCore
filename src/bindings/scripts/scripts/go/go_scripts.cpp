df4db669d56337eba0e640e13d6cca26cbd4900f
Anubisss <none@none>
2009-05-31 22:49:30 +0200

    *Merge SD2. *r988: Remove obsolete code for first boss in BT. - Just use enums. *r989: Added summons for misc ethereum gameobjects *r990: Added gossip options to learn engineering "teleport trinket" schematics (misc npc's) *r991: Correcting a few waypoints, added more texts and visuals to shadowfang prisoners - Skip: Its already merged. *r992: Added GO summons related to quest 10990, 10991 and 10992
    *Register 2 missing scripts.
    
    --HG--
    branch : trunk

--- src/bindings/scripts/scripts/go/go_scripts.cpp
+++ src/bindings/scripts/scripts/go/go_scripts.cpp
@@ -17,7 +17,7 @@
 /* ScriptData
 SDName: GO_Scripts
 SD%Complete: 100
-SDComment: Quest support: 4285,4287,4288(crystal pylons), 4296. Field_Repair_Bot->Teaches spell 22704. Barov_journal->Teaches spell 26089
+SDComment: Quest support: 4285,4287,4288(crystal pylons), 4296, 10990, 10991, 10992, Field_Repair_Bot->Teaches spell 22704. Barov_journal->Teaches spell 26089
 SDCategory: Game Objects
 EndScriptData */
 
@@ -27,10 +27,16 @@ go_northern_crystal_pylon
 go_eastern_crystal_pylon
 go_western_crystal_pylon
 go_barov_journal
+go_ethereum_prison
+go_ethereum_stasis
+go_sacred_fire_of_life
+go_shrine_of_the_birds
 go_field_repair_bot_74A
 go_orb_of_command
 go_tablet_of_madness
 go_tablet_of_the_seven
+go_tele_to_dalaran_crystal
+go_tele_to_violet_stand
 EndContentData */
 
 #include "precompiled.h"
@@ -199,24 +205,73 @@ bool GOHello_go_jump_a_tron(Player *player, GameObject* _GO)
 ## go_ethereum_prison
 ######*/
 
-float ethereum_NPC[2][7] =
+enum
 {
- {20785,20790,20789,20784,20786,20783,20788}, // hostile npc
- {22810,22811,22812,22813,22814,22815,0}      // fiendly npc (need script in acid ? only to cast spell reputation reward)
+    SPELL_REP_LC        = 39456,
+    SPELL_REP_SHAT      = 39457,
+    SPELL_REP_CE        = 39460,
+    SPELL_REP_CON       = 39474,
+    SPELL_REP_KT        = 39475,
+    SPELL_REP_SPOR      = 39476
 };
 
-bool GOHello_go_ethereum_prison(Player *player, GameObject* _GO)
+const uint32 NpcPrisonEntry[] =
 {
- _GO->SetGoState(GO_STATE_ACTIVE);
- switch(rand()%2){
-    case 0:
-        _GO->SummonCreature(ethereum_NPC[0][rand()%6],_GO->GetPositionX(),_GO->GetPositionY(),_GO->GetPositionZ()+0.3, 0,TEMPSUMMON_CORPSE_TIMED_DESPAWN,10000);
-    break;
-    case 1:
-        _GO->SummonCreature(ethereum_NPC[1][rand()%5],_GO->GetPositionX(),_GO->GetPositionY(),_GO->GetPositionZ()+0.3, 0,TEMPSUMMON_TIMED_DESPAWN,10000);
-    break;
+    22810, 22811, 22812, 22813, 22814, 22815,               //good guys
+    20783, 20784, 20785, 20786, 20788, 20789, 20790         //bad guys
+};
+
+bool GOHello_go_ethereum_prison(Player* pPlayer, GameObject* pGo)
+{
+    int Random = rand() % (sizeof(NpcPrisonEntry) / sizeof(uint32));
+
+    if(Creature* pCreature = pPlayer->SummonCreature(NpcPrisonEntry[Random],
+        pGo->GetPositionX(), pGo->GetPositionY(), pGo->GetPositionZ(), pGo->GetAngle(pPlayer),
+        TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000))
+    {
+        if(!pCreature->IsHostileTo(pPlayer))
+        {
+            uint32 Spell = 0;
+            FactionTemplateEntry const* pFaction = pCreature->getFactionTemplateEntry();
+
+            switch(pFaction->faction)
+            {
+                case 1011: Spell = SPELL_REP_LC; break;
+                case 935: Spell = SPELL_REP_SHAT; break;
+                case 942: Spell = SPELL_REP_CE; break;
+                case 933: Spell = SPELL_REP_CON; break;
+                case 989: Spell = SPELL_REP_KT; break;
+                case 970: Spell = SPELL_REP_SPOR; break;
+            }
+
+            if(Spell)
+                pCreature->CastSpell(pPlayer, Spell, false);
+            else
+                error_log("TSCR: go_ethereum_prison summoned creature (entry %u) but faction (%u) are not expected by script.", pCreature->GetEntry(), pCreature->getFaction());
+        }
+    }
+
+    return false;
 }
-return true;
+
+/*######
+## go_ethereum_stasis
+######*/
+
+const uint32 NpcStasisEntry[] =
+{
+    22825, 20888, 22827, 22826, 22828
+};
+
+bool GOHello_go_ethereum_stasis(Player* pPlayer, GameObject* pGo)
+{
+    int Random = rand() % (sizeof(NpcStasisEntry) / sizeof(uint32));
+
+    pPlayer->SummonCreature(NpcStasisEntry[Random],
+        pGo->GetPositionX(), pGo->GetPositionY(), pGo->GetPositionZ(), pGo->GetAngle(pPlayer),
+        TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000);
+
+    return false;
 }
 
 /*######
@@ -231,6 +286,46 @@ bool GOHello_go_sacred_fire_of_life(Player* pPlayer, GameObject* pGO)
         pPlayer->SummonCreature(NPC_ARIKARA, -5008.338, -2118.894, 83.657, 0.874, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 30000);
 
     return true;
+}
+
+/*######
+## go_shrine_of_the_birds
+######*/
+
+enum
+{
+    NPC_HAWK_GUARD      = 22992,
+    NPC_EAGLE_GUARD     = 22993,
+    NPC_FALCON_GUARD    = 22994,
+    GO_SHRINE_HAWK      = 185551,
+    GO_SHRINE_EAGLE     = 185547,
+    GO_SHRINE_FALCON    = 185553
+};
+
+bool GOHello_go_shrine_of_the_birds(Player* pPlayer, GameObject* pGo)
+{
+    uint32 BirdEntry = 0;
+
+    float fX, fY, fZ;
+    pGo->GetClosePoint(fX, fY, fZ, pGo->GetObjectSize(), INTERACTION_DISTANCE);
+
+    switch(pGo->GetEntry())
+    {
+        case GO_SHRINE_HAWK:
+            BirdEntry = NPC_HAWK_GUARD;
+            break;
+        case GO_SHRINE_EAGLE:
+            BirdEntry = NPC_EAGLE_GUARD;
+            break;
+        case GO_SHRINE_FALCON:
+            BirdEntry = NPC_FALCON_GUARD;
+            break;
+    }
+
+    if(BirdEntry)
+        pPlayer->SummonCreature(BirdEntry, fX, fY, fZ, pGo->GetOrientation(), TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 60000);
+
+    return false;
 }
 
 /*######
@@ -309,6 +404,11 @@ void AddSC_go_scripts()
     newscript->RegisterSelf();
 
     newscript = new Script;
+    newscript->Name = "go_shrine_of_the_birds";
+    newscript->pGOHello =           &GOHello_go_shrine_of_the_birds;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
     newscript->Name="go_tablet_of_madness";
     newscript->pGOHello =           &GOHello_go_tablet_of_madness;
     newscript->RegisterSelf();
@@ -329,9 +429,23 @@ void AddSC_go_scripts()
     newscript->RegisterSelf();
 
     newscript = new Script;
+    newscript->Name = "go_ethereum_stasis";
+    newscript->pGOHello =           &GOHello_go_ethereum_stasis;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
     newscript->Name = "go_sacred_fire_of_life";
     newscript->pGOHello =           &GOHello_go_sacred_fire_of_life;
     newscript->RegisterSelf();
 
+    newscript = new Script;
+    newscript->Name = "go_tele_to_dalaran_crystal";
+    newscript->pGOHello =           &GOHello_go_tele_to_dalaran_crystal;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name = "go_tele_to_violet_stand";
+    newscript->pGOHello =           &GOHello_go_tele_to_violet_stand;
+    newscript->RegisterSelf();
 }
 
