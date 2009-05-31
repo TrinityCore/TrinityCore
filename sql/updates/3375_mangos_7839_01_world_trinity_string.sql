10b37175f2a25635a756a0afd446806fa37d7808
Drahy <none@none>
2009-05-31 22:19:17 +0200

    * Added new sql file 3648_world_trinity_string_full - please import it to your world DB - this should fix many crash because of wrong or missing previous data * Used trinity sql coding style
    --HG--
    branch : trunk

--- sql/updates/3375_mangos_7839_01_world_trinity_string.sql
+++ sql/updates/3375_mangos_7839_01_world_trinity_string.sql
@@ -1,4 +1,4 @@
-DELETE FROM trinity_string WHERE entry IN(283);
-UPDATE trinity_string SET entry = 5011 WHERE entry = 171;
-INSERT INTO trinity_string VALUES
-(171,'You can\'t teleport self to self!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+DELETE FROM `trinity_string` WHERE `entry` IN (283,171,5011);
+INSERT INTO `trinity_string` VALUES
+(171,'You can\'t teleport self to self!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(5011,'You can\'t teleport self to self!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
