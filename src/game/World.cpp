6a58febd51935cc5808678417c751d429777ac8d
Machiavelli <none@none>
2009-06-08 16:25:07 +0200

    * Fix World::SendGlobalGMMessage, only send packet to connections with appropriate security level
    --HG--
    branch : trunk

--- src/game/World.cpp
+++ src/game/World.cpp
@@ -2663,7 +2663,7 @@ void World::SendGlobalGMMessage(WorldPacket *packet, WorldSession *self, uint32 
             itr->second->GetPlayer() &&
             itr->second->GetPlayer()->IsInWorld() &&
             itr->second != self &&
-            itr->second->GetSecurity() &&
+            itr->second->GetSecurity() > SEC_PLAYER &&
             (team == 0 || itr->second->GetPlayer()->GetTeam() == team) )
         {
             itr->second->SendPacket(packet);
