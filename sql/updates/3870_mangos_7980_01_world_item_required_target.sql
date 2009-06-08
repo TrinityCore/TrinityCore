30adb32d53ec268f71819edbc17fa09a7de5884c
megamage <none@none>
2009-06-08 18:15:25 -0500

    *Fix a typo in last rev. By Drahy.
    --HG--
    branch : trunk

--- sql/updates/3870_mangos_7980_01_world_item_required_target.sql
+++ sql/updates/3870_mangos_7980_01_world_item_required_target.sql
@@ -6,4 +6,4 @@ CREATE TABLE `item_required_target` (
   `type` tinyint(3) unsigned NOT NULL default '0',
   `targetEntry` mediumint(8) unsigned NOT NULL default '0',
   UNIQUE KEY `entry_type_target` (`entry`,`type`,`targetEntry`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED;
