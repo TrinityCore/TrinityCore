c5232138f870a8a4e5a59ecb3e0a3228373f58e2
QAston <none@none>
2009-06-09 21:48:02 +0200

    *Spell_script_target entry for Death pact.
    --HG--
    branch : trunk

--- sql/world_spell_full.sql
+++ sql/world_spell_full.sql
@@ -1590,7 +1590,8 @@ INSERT INTO `spell_script_target` (`entry`, `type`, `targetEntry`) VALUES
 (51859, 1, 28525), -- siphon of archerus
 (51859, 1, 28542),
 (51859, 1, 28543),
-(51859, 1, 28544);
+(51859, 1, 28544),
+(48743, 1, 26125); -- Death pact
 
 update creature_template set minlevel=50,maxlevel=52,minhealth=2215,maxhealth=2317,faction_A=2084,faction_H=2084,mindmg=50,maxdmg=50 where entry=28528; -- ghoul
 
