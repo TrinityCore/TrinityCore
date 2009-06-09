4a662071aa6902c9a6790c8d485b66040ae979a2
megamage <none@none>
2009-06-08 23:44:53 -0500

    *Fix dk quest until 12727. Some scripts are needed for quest 12727.
    --HG--
    branch : trunk

--- sql/world_spell_full.sql
+++ sql/world_spell_full.sql
@@ -1623,6 +1623,33 @@ INSERT INTO `spell_script_target` (`entry`, `type`, `targetEntry`) VALUES
 (52479,1,28819),
 (52479,1,28822);
 
+
+replace into creature_questrelation (id,quest) VALUES (28377,12701);
+replace into creature_involvedrelation (id,quest) VALUES (28377,12701);
+replace into creature_involvedrelation (id,quest) VALUES (28914,12723);
+replace into creature_questrelation (id,quest) VALUES (28914,12724);
+replace into creature_involvedrelation (id,quest) VALUES (28914,12724);
+replace into creature_questrelation (id,quest) VALUES (28913,12725);
+replace into creature_involvedrelation (id,quest) VALUES (28912,12725);
+replace into creature_questrelation (id,quest) VALUES (28912,12727);
+replace into creature_involvedrelation (id,quest) VALUES (28913,12727);
+
+-- ship cannon
+DELETE FROM `npc_spellclick_spells` WHERE `npc_entry` IN (28833,28887);
+INSERT INTO `npc_spellclick_spells` (`npc_entry`, `spell_id`, `quest_id`, `quest_status`, `cast_flags`) VALUES
+(28833, 52447, 12701, 3, 3),
+(28887, 52447, 12701, 3, 3);
+
+UPDATE creature_template SET spell1=52435,spell2=52576,spell5=52588,VehicleId=68,speed=0 WHERE entry IN (28833,28887);
+UPDATE creature_template SET spell1=52211 WHERE entry=28864;
+
+DELETE FROM `spell_script_target` WHERE entry IN
+(52576);
+INSERT INTO `spell_script_target` (`entry`, `type`, `targetEntry`) VALUES
+(52576,1,28834),
+(52576,1,28886);
+
+
 -- frostbrood vanquisher
 update creature_template set maxhealth = 133525, minhealth = 133525, maxmana = 51360, minmana = 51360, spell1 = 53114, spell2 = 53112, spell3=53110, VehicleId = 156 where entry = 28670;
 
