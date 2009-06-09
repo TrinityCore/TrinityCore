f23d94fb968fd5bd9000af01ccc056cb893c2caf
megamage <none@none>
2009-06-09 16:37:26 -0500

    *Update script of The Nexus 5er. By PSZ *Thanks to onkelz28 for preparing the patch
    --HG--
    branch : trunk

--- src/bindings/scripts/scripts/zone/nexus/nexus/instance_nexus.cpp
+++ src/bindings/scripts/scripts/zone/nexus/nexus/instance_nexus.cpp
@@ -1,145 +1,176 @@
+/* Copyright (C) 2006 - 2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/* ScriptData
+SDName: Instance_Nexus
+SD%Complete:
+SDComment:
+SDCategory: The Nexus, The Nexus
+EndScriptData */
+
 #include "precompiled.h"
 #include "def_nexus.h"
+
+#define NUMBER_OF_ENCOUNTERS      4
 
 struct TRINITY_DLL_DECL instance_nexus : public ScriptedInstance
 {
     instance_nexus(Map *Map) : ScriptedInstance(Map) {Initialize();};
 
-    uint64 Anomalus,
-           Ormorok,
-           Magus_telestra,
-           Magus_frost,
-           Magus_fire,
-           Magus_arcane,
-           keristrasza,
-           Commander_kolurg,
-           Commander_stoutbeard;
-    bool   IsBossDied[9];
+    std::string strInstData;
+	uint64 Anomalus;
+	uint32 Encounters[NUMBER_OF_ENCOUNTERS];
 
     void Initialize()
     {
-        Anomalus = 0;
-        Ormorok = 0;
-        Magus_telestra =0;
-        Magus_frost =0;
-        Magus_fire =0;
-        Magus_arcane =0;
-        keristrasza =0;
-        Commander_kolurg = 0;
-        Commander_stoutbeard = 0;
-        IsBossDied[0] = false;
-        IsBossDied[1] = false;
-        IsBossDied[2] = false;
-        IsBossDied[3] = false;
-        IsBossDied[4] = false;
-        IsBossDied[5] = false;
-        IsBossDied[6] = false;
-        IsBossDied[7] = false;
-        IsBossDied[8] = false;
+		Anomalus = 0;
+		for(uint8 i = 0; i < NUMBER_OF_ENCOUNTERS; i++)
+            Encounters[i] = NOT_STARTED;
     }
 
-    bool IsEncounterInProgress() const
+    void OnCreatureCreate(Creature* pCreature)
     {
-        //not active
-        return false;
-    }
-    void OnCreatureCreate(Creature *creature, uint32 creature_entry)
-    {
-        switch(creature->GetEntry())
-        {
-            case 26763:    Anomalus = creature->GetGUID();             break;
-            case 26794:    Ormorok = creature->GetGUID();              break;
-            case 26731:    Magus_telestra = creature->GetGUID();       break;
-            case 26930:    Magus_frost = creature->GetGUID();          break;
-            case 26928:    Magus_fire = creature->GetGUID();           break;
-            case 26926:    Magus_arcane = creature->GetGUID();         break;
-            case 26723:    keristrasza = creature->GetGUID();          break;
-            case 26798:    Commander_kolurg = creature->GetGUID();     break;
-            case 26796:    Commander_stoutbeard = creature->GetGUID(); break;
-        }
-    }
-    uint32 GetData(uint32 type)
-    {
-        switch(type)
-        {
-            case DATA_ANOMALUSDEAD:
-                if(IsBossDied[0])
-                    return 1;
-                break;
-            case DATA_ORMOROKDDEAD:
-                if(IsBossDied[1])
-                    return 1;
-                break;
-            case DATA_MAGUSTELESTRADEAD:
-                if(IsBossDied[2])
-                    return 1;
-                break;
-            case DATA_MAGUSTELESTRA_A_DEAD:
-                if(IsBossDied[3])
-                    return 1;
-                break;
-            case DATA_MAGUSTELESTRA_F_DEAD:
-                if(IsBossDied[4])
-                    return 1;
-                break;
-            case DATA_MAGUSTELESTRA_FI_DEAD:
-                if(IsBossDied[5])
-                    return 1;
-                break;
-            case DATA_KERISTRASZADEAD:
-                if(IsBossDied[6])
-                    return 1;
-                break;
-            case DATA_COMMANDER_KOLURGDEAD:
-                if(IsBossDied[7])
-                    return 1;
-                break;
-            case DATA_COMMANDER_STOUTBEARDDEAD:
-                if(IsBossDied[8])
-                    return 1;
-                break;
-        }
+        Map::PlayerList const& players = instance->GetPlayers();
+		uint32 TeamInInstance;
 
+        if (!players.isEmpty())
+        {
+            if (Player* pPlayer = players.begin()->getSource())
+            {
+				TeamInInstance = pPlayer->GetTeam();
+			}
+        }
+		switch(pCreature->GetEntry())
+        {
+            case 26763: Anomalus = pCreature->GetGUID(); break;
+			case 26800:
+				{
+					//26799
+					pCreature->setFaction(16);
+					if (TeamInInstance == ALLIANCE)
+						pCreature->SetDisplayId(24358);
+					break;
+				}
+			case 26802:
+				{
+					//26801
+					pCreature->setFaction(16);
+					if (TeamInInstance == ALLIANCE)
+						pCreature->SetDisplayId(24354);
+					break;
+				}
+			case 26805:
+				{
+					//26803
+					pCreature->setFaction(16);
+					if (TeamInInstance == ALLIANCE)
+						pCreature->SetDisplayId(24357);
+					break;
+				}
+			case 27949:
+				{
+					//27947
+					pCreature->setFaction(16);
+					if (TeamInInstance == ALLIANCE)
+						pCreature->SetDisplayId(24352);
+					break;
+				}
+			case 26796: 
+				{
+					//26798
+					pCreature->setFaction(16);
+					if (TeamInInstance == ALLIANCE)
+						pCreature->SetDisplayId(24352);
+					break;
+				}
+        }
+    }
+
+    uint64 GetData64(uint32 uiIdentifier)
+    {
+        switch(uiIdentifier)
+        {
+            case DATA_ANOMALUS:
+                return Anomalus;
+        }
         return 0;
     }
 
-    uint64 GetData64 (uint32 identifier)
+	uint32 GetData(uint32 identifier)
     {
+        switch(identifier)
+        {
+            case DATA_MAGUS_TELESTRA_EVENT: return Encounters[0];
+            case DATA_ANOMALUS_EVENT:       return Encounters[1];
+            case DATA_ORMOROK_EVENT:        return Encounters[2];
+			case DATA_KERISTRASZA_FREED:    return Encounters[3];
+        }
         return 0;
     }
 
-    void SetData(uint32 type, uint32 data)
+    void SetData(uint32 identifier, uint32 data)
     {
-        switch(type)
+        switch(identifier)
         {
-            case DATA_ANOMALUS_DEATH:
-                IsBossDied[0] = true;
-                break;
-            case DATA_ORMOROK_DEATH:
-                IsBossDied[1] = true;
-                break;
-            case DATA_MAGUSTELESTRA_DEATH:
-                IsBossDied[2] = true;
-                break;
-            case DATA_MAGUSTELESTRA_A_DEATH:
-                IsBossDied[3] = true;
-                break;
-            case DATA_MAGUSTELESTRA_F_DEATH:
-                IsBossDied[4] = true;
-                break;
-            case DATA_MAGUSTELESTRA_FI_DEAD:
-                IsBossDied[5] = true;
-                break;
-            case DATA_KERISTRASZA_DEATH:
-                IsBossDied[6] = true;
-                break;
-            case DATA_COMMANDER_KOLURG_DEATH:
-                IsBossDied[7] = true;
-                break;
-            case DATA_COMMANDER_STOUTBEARD_DEATH:
-                IsBossDied[8] = true;
-                break;
+            case DATA_MAGUS_TELESTRA_EVENT: Encounters[0] = data;  break;
+            case DATA_ANOMALUS_EVENT:       Encounters[1] = data;  break;
+            case DATA_ORMOROK_EVENT:        Encounters[2] = data;  break;
+			case DATA_KERISTRASZA_FREED:    Encounters[3] = data;  break;
         }
+
+		if (data == DONE)
+        {
+            OUT_SAVE_INST_DATA;
+
+            std::ostringstream saveStream;
+            saveStream << Encounters[0] << " " << Encounters[1] << " " << Encounters[2] << " "
+                << Encounters[3];
+
+            strInstData = saveStream.str();
+
+            SaveToDB();
+            OUT_SAVE_INST_DATA_COMPLETE;
+		}
+    }
+
+    const char* Save()
+    {
+        return strInstData.c_str();
+    }
+
+    void Load(const char* chrIn)
+    {
+        if (!chrIn)
+        {
+            OUT_LOAD_INST_DATA_FAIL;
+            return;
+        }
+
+        OUT_LOAD_INST_DATA(chrIn);
+
+        std::istringstream loadStream(chrIn);
+        loadStream >> Encounters[0] >> Encounters[1] >> Encounters[2] >> Encounters[3];
+
+        for(uint8 i = 1; i < NUMBER_OF_ENCOUNTERS; ++i)
+        {
+            if (Encounters[i] == IN_PROGRESS)
+                Encounters[i] = NOT_STARTED;
+        }
+
+        OUT_LOAD_INST_DATA_COMPLETE;
     }
 };
 
