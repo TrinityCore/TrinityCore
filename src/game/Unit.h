507b9fb0516dcde613af0f9d0276af8acbdcb585
QAston <none@none>
2009-06-09 19:33:38 +0200

    *Fix Heart of the Pheonix *Fix autoshoot rotation with steady shot. *Make Incanter's Absorption not interrupt other casts.
    --HG--
    branch : trunk

--- src/game/Unit.h
+++ src/game/Unit.h
@@ -1436,7 +1436,7 @@ class TRINITY_DLL_SPEC Unit : public WorldObject
         // set withDelayed to true to account delayed spells as casted
         // delayed+channeled spells are always accounted as casted
         // we can skip channeled or delayed checks using flags
-        bool IsNonMeleeSpellCasted(bool withDelayed, bool skipChanneled = false, bool skipAutorepeat = false) const;
+        bool IsNonMeleeSpellCasted(bool withDelayed, bool skipChanneled = false, bool skipAutorepeat = false, bool isAutoshoot = false) const;
 
         // set withDelayed to true to interrupt delayed spells too
         // delayed+channeled spells are always interrupted
