deb9dd1543895ce35390a3d43be7a0ee6818bf74
megamage <none@none>
2009-06-09 16:37:26 -0500

    *Update script of The Nexus 5er. By PSZ *Thanks to onkelz28 for preparing the patch
    --HG--
    branch : trunk

--- src/bindings/scripts/scripts/zone/nexus/nexus/boss_anomalus.cpp
+++ src/bindings/scripts/scripts/zone/nexus/nexus/boss_anomalus.cpp
@@ -1,129 +1,259 @@
-/* Script Data Start
-SDName: Boss anomalus
-SDAuthor: LordVanMartin
-SD%Complete:
-SDComment:
-SDCategory:
-Script Data End */
+/* Copyright (C) 2006 - 2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
-/*** SQL START ***
-update creature_template set scriptname = '' where entry = '';
-*** SQL END ***/
+/* ScriptData
+SDName: Boss_Anomalus
+SD%Complete: 
+SDComment: 
+SDCategory: The Nexus, The Nexus
+EndScriptData */
+
 #include "precompiled.h"
+#include "def_nexus.h"
 
 //Spells
-#define SPELL_SPARK_1              47751
-#define SPELL_SPARK_2              57062
+#define SPELL_SPARK_N              47751
+#define SPELL_SPARK_H              57062
 #define SPELL_RIFT_SHIELD          47748
-#define SPELL_CHARGE_RIFT          47747
-#define SPELL_ARCANE_ATTRACTION    57063
-#define SPELL_CREATE_RIFT          47743 //Dummy ---> summons (npc 26918) "Chaotic Rift", which spawns "Crazed Mana Wraith"s  (npc 26746)
+#define SPELL_CHARGE_RIFT          47747 //Works wrong (affect players, not rifts)
+#define SPELL_CREATE_RIFT          47743 //Don't work, using WA
+#define SPELL_ARCANE_ATTRACTION    57063 //No idea, when it's used
 
-#define MOB_CRAZED_MANA_WRAITH     26746
-#define NPC_CHAOTIC_RIFT           26918
+#define MOB_CRAZED_MANA_WRAITH              26746
+#define MOB_CHAOTIC_RIFT                    26918
+#define SPELL_CHAOTIC_ENERGY_BURST          47688
+#define SPELL_CHARGED_CHAOTIC_ENERGY_BURST  47737
+#define SPELL_ARCANEFORM                    48019 //Chaotic Rift visual
 
 //Yell
-#define SAY_AGGRO               -1576000
-#define SAY_KILL                -1576001
-#define SAY_DEATH               -1576002
-#define SAY_RIFT                -1576003
-#define SAY_SHIELD              -1576004
+#define SAY_AGGRO               -1576010
+#define SAY_DEATH               -1576011
+#define SAY_RIFT                -1576012
+#define SAY_SHIELD              -1576013
+
+float RiftLocation[6][3]=
+{
+    {652.64, -273.70, -8.75},
+    {634.45, -265.94, -8.44},
+    {620.73, -281.17, -9.02},
+    {626.10, -304.67, -9.44},
+    {639.87, -314.11, -9.49},
+    {651.72, -297.44, -9.37}
+};
 
 struct TRINITY_DLL_DECL boss_anomalusAI : public ScriptedAI
 {
-    boss_anomalusAI(Creature *c) : ScriptedAI(c) {}
+    boss_anomalusAI(Creature *c) : ScriptedAI(c)
+	{
+        pInstance = ((ScriptedInstance*)c->GetInstanceData());
+        Reset();
+        HeroicMode = c->GetMap()->IsHeroic();
+	}
 
-    void Reset() {}
-    void EnterCombat(Unit* who)
+	ScriptedInstance* pInstance;
+    bool HeroicMode;
+
+	uint8 Phase;
+    uint32 SPELL_SPARK_Timer;                    
+    uint32 SPELL_CREATE_RIFT_Timer;
+	uint64 ChaoticRiftGUID;
+
+    void Reset() 
+	{
+		Phase = 0;
+		SPELL_SPARK_Timer = 5000;
+		SPELL_CREATE_RIFT_Timer = 25000;
+		ChaoticRiftGUID = 0;
+
+		if(pInstance)
+            pInstance->SetData(DATA_ANOMALUS_EVENT, NOT_STARTED);
+	}
+
+	void Aggro(Unit* who) 
     {
         DoScriptText(SAY_AGGRO, m_creature);
     }
-    void AttackStart(Unit* who) {}
-    void MoveInLineOfSight(Unit* who) {}
-    void UpdateAI(const uint32 diff)
+
+    void UpdateAI(const uint32 diff) 
     {
-        //Return since we have no target
-        if(!UpdateVictim())
-            return;
-
-            //Source DBM
-        if((((m_creature->GetHealth()*100 / m_creature->GetMaxHealth()) < 80) && ((m_creature->GetHealth()*100 / m_creature->GetMaxHealth()) > 77))
-            || (((m_creature->GetHealth()*100 / m_creature->GetMaxHealth()) < 55) && ((m_creature->GetHealth()*100 / m_creature->GetMaxHealth()) > 52))
-            || (((m_creature->GetHealth()*100 / m_creature->GetMaxHealth()) < 30) && ((m_creature->GetHealth()*100 / m_creature->GetMaxHealth()) > 27)))
+        if (!UpdateVictim())
         {
-                //Summon rift at a random location
+            return;
         }
+        
+		if (m_creature->HasAura(SPELL_RIFT_SHIELD))
+		{
+			Unit* Rift;
+			if (ChaoticRiftGUID)
+				Rift = Unit::GetUnit((*m_creature), ChaoticRiftGUID);
+			if (Rift && Rift->isDead())
+			{
+				m_creature->RemoveAurasDueToSpell(SPELL_RIFT_SHIELD);
+				ChaoticRiftGUID = 0;
+			}
+			return;
+		} else
+			ChaoticRiftGUID = 0;
 
-        DoMeleeAttackIfReady();
+		if ((Phase == 0) && (m_creature->GetHealth() < m_creature->GetMaxHealth() * 0.75))
+		{
+			Phase = 1;
+			DoScriptText(SAY_SHIELD, m_creature);
+			DoCast(m_creature, SPELL_RIFT_SHIELD);
+
+            int tmp = rand()%(2);
+			Creature* Rift = m_creature->SummonCreature(MOB_CHAOTIC_RIFT, RiftLocation[tmp][0], RiftLocation[tmp][1], RiftLocation[tmp][2], 0, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 1000);
+	        if (Rift)
+			{
+				//DoCast(Rift, SPELL_CHARGE_RIFT);
+			    if (Unit* target = SelectUnit(SELECT_TARGET_RANDOM, 0))
+				    Rift->AI()->AttackStart(target);
+				ChaoticRiftGUID = Rift->GetGUID();
+				DoScriptText(SAY_RIFT , m_creature);
+			}
+		}
+
+		if ((Phase == 1) && (m_creature->GetHealth() < m_creature->GetMaxHealth() * 0.50))
+		{
+			Phase = 2;
+			DoScriptText(SAY_SHIELD , m_creature);
+			DoCast(m_creature,SPELL_RIFT_SHIELD);
+
+            int tmp = rand()%(2);
+			Creature* Rift = m_creature->SummonCreature(MOB_CHAOTIC_RIFT, RiftLocation[tmp][0], RiftLocation[tmp][1], RiftLocation[tmp][2], 0, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 1000);
+	        if (Rift)
+			{
+				//DoCast(Rift, SPELL_CHARGE_RIFT);
+			    if (Unit* target = SelectUnit(SELECT_TARGET_RANDOM, 0))
+				    Rift->AI()->AttackStart(target);
+				ChaoticRiftGUID = Rift->GetGUID();
+				DoScriptText(SAY_RIFT , m_creature);
+			}
+		}
+
+		if ((Phase == 2) && (m_creature->GetHealth() < m_creature->GetMaxHealth() * 0.25))
+		{
+			Phase = 3;
+			DoScriptText(SAY_SHIELD , m_creature);
+			DoCast(m_creature,SPELL_RIFT_SHIELD);
+
+            int tmp = rand()%(2);
+			Creature* Rift = m_creature->SummonCreature(MOB_CHAOTIC_RIFT, RiftLocation[tmp][0], RiftLocation[tmp][1], RiftLocation[tmp][2], 0, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 1000);
+	        if (Rift)
+			{
+				//DoCast(Rift, SPELL_CHARGE_RIFT);
+			    if (Unit* target = SelectUnit(SELECT_TARGET_RANDOM, 0))
+				    Rift->AI()->AttackStart(target);
+				ChaoticRiftGUID = Rift->GetGUID();
+				DoScriptText(SAY_RIFT , m_creature);
+			}
+		}
+
+		if (SPELL_SPARK_Timer < diff)
+        {
+            if (Unit* target = SelectUnit(SELECT_TARGET_RANDOM, 0))
+                DoCast(target, HeroicMode ? SPELL_SPARK_H : SPELL_SPARK_N);
+            SPELL_SPARK_Timer = 5000;
+        }else SPELL_SPARK_Timer -=diff;
+
+		if (SPELL_CREATE_RIFT_Timer < diff)
+        {
+			DoScriptText(SAY_RIFT , m_creature);
+
+			int tmp = rand()%(2);
+			Creature* Rift = m_creature->SummonCreature(MOB_CHAOTIC_RIFT, RiftLocation[tmp][0], RiftLocation[tmp][1], RiftLocation[tmp][2], 0, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 1000);
+	        if (Rift)
+			    if (Unit* target = SelectUnit(SELECT_TARGET_RANDOM, 0))
+				    Rift->AI()->AttackStart(target);
+            SPELL_CREATE_RIFT_Timer = 25000;
+        }else SPELL_CREATE_RIFT_Timer -=diff;
+
+        DoMeleeAttackIfReady();    
     }
-    void JustDied(Unit* killer)
+
+    void JustDied(Unit* killer)  
     {
         DoScriptText(SAY_DEATH, m_creature);
-    }
-    void KilledUnit(Unit *victim)
-    {
-        if(victim == m_creature)
-            return;
-        DoScriptText(SAY_KILL, m_creature);
+		if (pInstance)
+	        pInstance->SetData(DATA_ANOMALUS_EVENT, DONE);
     }
 };
 
-/*######
-## Crazed Mana Wraiths
-######*/
-#define SPELL_ARCANE_MISSILES                                 33833
-
-struct TRINITY_DLL_DECL mob_crazed_mana_wraithAI : public ScriptedAI
+struct TRINITY_DLL_DECL mob_chaotic_riftAI : public Scripted_NoMovementAI
 {
-    mob_crazed_mana_wraithAI(Creature *c) : ScriptedAI(c) {}
+    mob_chaotic_riftAI(Creature *c) : Scripted_NoMovementAI(c)
+	{
+        pInstance = ((ScriptedInstance*)c->GetInstanceData());
+        Reset();
+	}
 
-    void Reset() {}
-    void EnterCombat(Unit* who) {}
-    void UpdateAI(const uint32 diff)
+	ScriptedInstance* pInstance;
+
+	uint32 SPELL_CHAOTIC_ENERGY_BURST_Timer;
+	uint32 SUMMON_CRAZED_MANA_WRAITH_Timer;
+
+    void Reset()
+	{
+		SPELL_CHAOTIC_ENERGY_BURST_Timer = 1000;
+		SUMMON_CRAZED_MANA_WRAITH_Timer = 5000;
+		m_creature->SetDisplayId(25206); //For some reason in DB models for ally and horde are different.
+		                                 //Model for ally (1126) does not show auras. Horde model works perfect.
+										 //Set model to horde number
+		DoCast(m_creature, SPELL_ARCANEFORM, false);
+	}
+
+    void UpdateAI(const uint32 diff) 
     {
-        //Return since we have no target
-        if(!UpdateVictim())
+        if (!UpdateVictim())
+        {
             return;
+        }
 
-        DoMeleeAttackIfReady();
-    }
-    void JustDied(Unit* killer)  {}
+		if (SPELL_CHAOTIC_ENERGY_BURST_Timer < diff)
+		{
+            Unit* Anomalus = Unit::GetUnit((*m_creature), pInstance->GetData64(DATA_ANOMALUS));
+			if (Unit* target = SelectUnit(SELECT_TARGET_RANDOM, 0))
+                if (Anomalus && Anomalus->HasAura(SPELL_RIFT_SHIELD))
+					DoCast(target, SPELL_CHARGED_CHAOTIC_ENERGY_BURST);
+				else
+					DoCast(target, SPELL_CHAOTIC_ENERGY_BURST);
+            SPELL_CHAOTIC_ENERGY_BURST_Timer = 1000;
+		}else SPELL_CHAOTIC_ENERGY_BURST_Timer -=diff;
+
+		if (SUMMON_CRAZED_MANA_WRAITH_Timer < diff)
+		{
+			Creature* Wraith = m_creature->SummonCreature(MOB_CRAZED_MANA_WRAITH, m_creature->GetPositionX()+1, m_creature->GetPositionY()+1, m_creature->GetPositionZ(), 0, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 1000);
+	        if (Wraith)
+			    if (Unit* target = SelectUnit(SELECT_TARGET_RANDOM, 0))
+				    Wraith->AI()->AttackStart(target);
+            Unit* Anomalus = Unit::GetUnit((*m_creature), pInstance->GetData64(DATA_ANOMALUS));
+            if (Anomalus && Anomalus->HasAura(SPELL_RIFT_SHIELD))
+				SUMMON_CRAZED_MANA_WRAITH_Timer = 5000;
+			else
+				SUMMON_CRAZED_MANA_WRAITH_Timer = 10000;
+		}else SUMMON_CRAZED_MANA_WRAITH_Timer -=diff;
+	}
 };
 
-/*######
-## Chaotic Rift
-######*/
-#define SPELL_CHAOTIC_ENERGY_BURST                      47688
-#define SPELL_CHARGED_CHAOTIC_ENERGY_BURST              47737
-
-struct TRINITY_DLL_DECL npc_chaotic_riftAI : public ScriptedAI
+CreatureAI* GetAI_mob_chaotic_rift(Creature *_Creature)
 {
-    npc_chaotic_riftAI(Creature *c) : ScriptedAI(c) {}
-
-    void Reset() {}
-    void EnterCombat(Unit* who) {}
-    void UpdateAI(const uint32 diff)
-    {
-        //Return since we have no target
-        if(!UpdateVictim())
-            return;
-
-        DoMeleeAttackIfReady();
-    }
-    void JustDied(Unit* killer)  {}
-};
-
-CreatureAI* GetAI_npc_chaotic_rift(Creature *_Creature)
-{
-    return new npc_chaotic_riftAI (_Creature);
+    return new mob_chaotic_riftAI (_Creature);
 }
 
 CreatureAI* GetAI_boss_anomalus(Creature *_Creature)
-{
-    return new boss_anomalusAI (_Creature);
-}
-
-CreatureAI* GetAI_mob_crazed_mana_wraith(Creature *_Creature)
 {
     return new boss_anomalusAI (_Creature);
 }
@@ -136,14 +266,9 @@ void AddSC_boss_anomalus()
     newscript->Name="boss_anomalus";
     newscript->GetAI = &GetAI_boss_anomalus;
     newscript->RegisterSelf();
-
+    
     newscript = new Script;
-    newscript->Name="mob_crazed_mana_wraith";
-    newscript->GetAI = &GetAI_mob_crazed_mana_wraith;
-    newscript->RegisterSelf();
-
-    newscript = new Script;
-    newscript->Name="npc_chaotic_rift";
-    newscript->GetAI = &GetAI_npc_chaotic_rift;
+    newscript->Name="mob_chaotic_rift";
+    newscript->GetAI = &GetAI_mob_chaotic_rift;
     newscript->RegisterSelf();
 }
--- src/bindings/scripts/scripts/zone/nexus/nexus/boss_magus_telestra.cpp
+++ src/bindings/scripts/scripts/zone/nexus/nexus/boss_magus_telestra.cpp
@@ -1,103 +1,278 @@
-/* Script Data Start
-SDName: Boss magus_telestra
-SDAuthor: LordVanMartin
-SD%Complete:
-SDComment:
-SDCategory:
-Script Data End */
+/* Copyright (C) 2006 - 2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
-/*** SQL START ***
-update creature_template set scriptname = '' where entry = '';
-*** SQL END ***/
+/* ScriptData
+SDName: Boss_Magus_Telestra
+SD%Complete: 
+SDComment: 
+SDCategory: The Nexus, The Nexus
+EndScriptData */
+
 #include "precompiled.h"
 #include "def_nexus.h"
 
-
 //Spells
 
-//phase 1
-#define SPELL_ICE_NOVA          47772
-#define SPELL_ICE_NOVA          56935
-#define SPELL_FIREBOMB          47773
-#define SPELL_FIREBOMB          56934
-#define SPELL_GAVITY_WELL       47756
+#define SPELL_ICE_NOVA_N          47772
+#define SPELL_ICE_NOVA_H          56935
+#define SPELL_FIREBOMB_N          47773
+#define SPELL_FIREBOMB_H          56934
+#define SPELL_GRAVITY_WELL        47756
+#define SPELL_TELESTRA_BACK       47714
 
-//Phase 2      -->50% HP (3 clones, Frost, Fire, Arcane)
-//Frost Magus (npc 26930)
-#define FROST_MAGUS_VISUAL      47706 //Dummy
-#define SPELL_BLIZZARD_N        47727
-#define SPELL_ICE_BARB_N        47729
-//heroic
-#define SPELL_BLIZZARD_H        56936
-#define SPELL_ICE_BARB_H        56937
+float CenterOfRoom[1][4] =
+{
+    {504.80, 89.07, -16.12, 6.27}
+};
 
-//Fire Magus (npc 26928)
-#define FIRE_MAGUS_VISUAL       47705 //Dummy
-#define SPELL_FIRE_BLAST        47721
-#define SPELL_SCORCH            47723
-//heroic
-#define SPELL_FIRE_BLAST_H      56939
-#define SPELL_SCORCH_H          56938
+//At 50% HP - 3 clones, Frost, Fire, Arcane (and in 10% HP in Heroic)
+#define MOB_FIRE_MAGUS            26928
+#define MOB_FROST_MAGUS           26930
+#define MOB_ARCANE_MAGUS          26929
 
-//Arcane Magus (npc 26929)
-#define ARCANE_MAGUS_VISUAL     47704 //Dummy
-#define SPELL_CRITTER           47731
-#define SPELL_TIME_STOP         47736
+#define SPELL_FIRE_MAGUS_VISUAL   47705
+#define SPELL_FROST_MAGUS_VISUAL  47706
+#define SPELL_ARCANE_MAGUS_VISUAL 47704
 
 //Yell
-#define SAY_AGGRO             -1576010
-#define SAY_KILL              -1576011
-#define SAY_DEATH             -1576012
-#define SAY_MERGE             -1576013
-#define SAY_SPLIT_1           -1576014
-#define SAY_SPLIT_2           -1576015
+#define SAY_AGGRO                 -1576000
+#define SAY_KILL                  -1576001
+#define SAY_DEATH                 -1576002
+#define SAY_MERGE                 -1576003
+#define SAY_SPLIT_1               -1576004
+#define SAY_SPLIT_2               -1576005
 
 
 struct TRINITY_DLL_DECL boss_magus_telestraAI : public ScriptedAI
 {
-    boss_magus_telestraAI(Creature *c) : ScriptedAI(c) {}
+    boss_magus_telestraAI(Creature* c) : ScriptedAI(c) 
+    {
+        pInstance = ((ScriptedInstance*)c->GetInstanceData());
+        Reset();
+        HeroicMode = c->GetMap()->IsHeroic();
+	}
 
-    bool splited;
-    uint64 Magus_frost,
-           Magus_fire,
-           Magus_arcane;
+    ScriptedInstance* pInstance;
+    bool HeroicMode;
+
+	uint64 FireMagusGUID;
+	uint64 FrostMagusGUID;
+	uint64 ArcaneMagusGUID;
+	bool FireMagusDead;
+	bool FrostMagusDead;
+	bool ArcaneMagusDead;
+
+	uint32 AppearDelay_Timer;
+    bool AppearDelay;
+
+    uint8 Phase;
+
+    uint32 SPELL_ICE_NOVA_Timer;                    
+    uint32 SPELL_FIREBOMB_Timer;                    
+    uint32 SPELL_GRAVITY_WELL_Timer;
 
     void Reset()
     {
-        splited = false;
+        Phase = 0;
+        //These times are probably wrong
+        SPELL_ICE_NOVA_Timer =  7000;                  
+        SPELL_FIREBOMB_Timer =  0;                
+        SPELL_GRAVITY_WELL_Timer = 15000;
+
+        FireMagusGUID = 0;
+        FrostMagusGUID = 0;
+        ArcaneMagusGUID = 0;
+
+		AppearDelay = false;
+
+		m_creature->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE);
+		m_creature->SetVisibility(VISIBILITY_ON);
+
+        if(pInstance)
+            pInstance->SetData(DATA_MAGUS_TELESTRA_EVENT, NOT_STARTED);
     }
-    void EnterCombat(Unit* who)
+
+    void Aggro(Unit* who) 
     {
         DoScriptText(SAY_AGGRO, m_creature);
     }
-    void AttackStart(Unit* who) {}
-    void MoveInLineOfSight(Unit* who) {}
-    void UpdateAI(const uint32 diff)
+
+    uint64 SplitPersonality(uint32 entry)
+    {
+        Creature* Summoned = m_creature->SummonCreature(entry, m_creature->GetPositionX(), m_creature->GetPositionY(), m_creature->GetPositionZ(), m_creature->GetOrientation(), TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 1000);
+        if (Summoned)
+        {
+            switch (entry)
+			{
+				case MOB_FIRE_MAGUS:
+				{
+					Summoned->CastSpell(Summoned, SPELL_FIRE_MAGUS_VISUAL, false);
+					break;
+				}
+				case MOB_FROST_MAGUS:
+				{
+					Summoned->CastSpell(Summoned, SPELL_FROST_MAGUS_VISUAL, false);
+					break;
+				}
+				case MOB_ARCANE_MAGUS:
+				{
+					Summoned->CastSpell(Summoned, SPELL_ARCANE_MAGUS_VISUAL, false);
+					break;
+				}
+			}
+			if (Unit* target = SelectUnit(SELECT_TARGET_RANDOM, 0))
+                Summoned->AI()->AttackStart(target);
+			return Summoned->GetGUID();
+        }
+		return 0;
+    }
+	
+    void UpdateAI(const uint32 diff) 
     {
         //Return since we have no target
-        if(!UpdateVictim())
-            return;
-
-        if(!splited)
+        if (!UpdateVictim())
         {
-            if ((m_creature->GetHealth()*100 / m_creature->GetMaxHealth()) <= 50)
-            {
-                DoScriptText(SAY_SPLIT_1, m_creature);
-                //HandleSplit
-                splited = true;
-            }
+            return;
         }
 
-        DoMeleeAttackIfReady();
+        if (AppearDelay)
+        {
+            m_creature->StopMoving();
+            m_creature->AttackStop();
+            if (AppearDelay_Timer < diff)
+            {
+                m_creature->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE);
+                AppearDelay = false;
+            }else AppearDelay_Timer -= diff;
+			return;
+        }
+
+
+		if ((Phase == 1)||(Phase == 3))
+		{
+	        Unit* FireMagus;
+			Unit* FrostMagus;
+			Unit* ArcaneMagus;
+			if (FireMagusGUID)
+				FireMagus = Unit::GetUnit((*m_creature), FireMagusGUID);
+	        if (FrostMagusGUID)
+				FrostMagus = Unit::GetUnit((*m_creature), FrostMagusGUID);
+	        if (ArcaneMagusGUID)
+				ArcaneMagus = Unit::GetUnit((*m_creature), ArcaneMagusGUID);
+			if (FireMagus && FireMagus->isDead())
+				FireMagusDead = true;
+			if (FrostMagus && FrostMagus->isDead())
+				FrostMagusDead = true;
+			if (ArcaneMagus && ArcaneMagus->isDead())
+				ArcaneMagusDead = true;
+			if (FireMagusDead && FrostMagusDead && ArcaneMagusDead)
+			{
+				m_creature->GetMotionMaster()->Clear();
+				m_creature->Relocate(CenterOfRoom[0][0], CenterOfRoom[0][1], CenterOfRoom[0][2], CenterOfRoom[0][3]);
+				DoCast(m_creature, SPELL_TELESTRA_BACK);
+				m_creature->SetVisibility(VISIBILITY_ON);
+				if (Phase == 1)
+					Phase = 2;
+				if (Phase == 3)
+					Phase = 4;
+                FireMagusGUID = 0;
+                FrostMagusGUID = 0;
+                ArcaneMagusGUID = 0;
+				AppearDelay = true;
+                AppearDelay_Timer = 4000;
+				DoScriptText(SAY_MERGE, m_creature);
+			}else
+				return;
+		}
+
+		if ((Phase == 0) && (m_creature->GetHealth() <= (m_creature->GetMaxHealth() * 0.5)))
+        {     
+			Phase = 1;
+			m_creature->CastStop();
+			m_creature->SetVisibility(VISIBILITY_OFF);
+            m_creature->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE);
+			FireMagusGUID = SplitPersonality(MOB_FIRE_MAGUS);
+			FrostMagusGUID = SplitPersonality(MOB_FROST_MAGUS); 
+			ArcaneMagusGUID = SplitPersonality(MOB_ARCANE_MAGUS);
+			FireMagusDead = false;
+			FrostMagusDead = false;
+			ArcaneMagusDead = false;
+	        switch(rand()%2)
+	        {
+		        case 0: DoScriptText(SAY_SPLIT_1, m_creature); break;
+			    case 1: DoScriptText(SAY_SPLIT_2, m_creature); break;
+			}
+			return;
+	    }
+
+		if (HeroicMode && (Phase == 2) && (m_creature->GetHealth() <= (m_creature->GetMaxHealth() * 0.1))) 
+        {     
+			Phase = 3;
+			m_creature->CastStop();
+			m_creature->SetVisibility(VISIBILITY_OFF);
+            m_creature->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE);
+			FireMagusGUID = SplitPersonality(MOB_FIRE_MAGUS);
+			FrostMagusGUID = SplitPersonality(MOB_FROST_MAGUS); 
+			ArcaneMagusGUID = SplitPersonality(MOB_ARCANE_MAGUS);
+			FireMagusDead = false;
+			FrostMagusDead = false;
+			ArcaneMagusDead = false;
+	        switch(rand()%2)
+	        {
+		        case 0: DoScriptText(SAY_SPLIT_1, m_creature); break;
+			    case 1: DoScriptText(SAY_SPLIT_2, m_creature); break;
+			}
+			return;
+	    }
+
+		if (SPELL_ICE_NOVA_Timer < diff)
+        {
+			m_creature->CastStop();
+            if (Unit* target = SelectUnit(SELECT_TARGET_RANDOM, 0))
+                DoCast(target, HeroicMode ? SPELL_ICE_NOVA_H : SPELL_ICE_NOVA_N);
+            SPELL_ICE_NOVA_Timer = 15000;
+        }else SPELL_ICE_NOVA_Timer -=diff;
+
+        if (SPELL_GRAVITY_WELL_Timer < diff)
+        {
+			m_creature->CastStop();
+			if (Unit* target = m_creature->getVictim())
+                DoCast(target, SPELL_GRAVITY_WELL);
+		    SPELL_GRAVITY_WELL_Timer = 15000;
+        }else SPELL_GRAVITY_WELL_Timer -=diff;
+
+		if (SPELL_FIREBOMB_Timer < diff)
+        {
+            if (Unit* target = SelectUnit(SELECT_TARGET_RANDOM, 0))
+				DoCast(target, HeroicMode ? SPELL_FIREBOMB_H : SPELL_FIREBOMB_N);
+            SPELL_FIREBOMB_Timer = 2000;
+        }else SPELL_FIREBOMB_Timer -=diff;
+
+        DoMeleeAttackIfReady();    
     }
+
     void JustDied(Unit* killer)
-    {
+	{
         DoScriptText(SAY_DEATH, m_creature);
-    }
+		if (pInstance)
+	        pInstance->SetData(DATA_MAGUS_TELESTRA_EVENT, DONE);
+	}
+
     void KilledUnit(Unit *victim)
     {
-        if(victim == m_creature)
-            return;
         DoScriptText(SAY_KILL, m_creature);
     }
 };
