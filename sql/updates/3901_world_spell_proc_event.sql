fff5f8e2a66c6e1a15393e7cc7b6cb7d85f4c57a
QAston <none@none>
2009-06-10 17:17:03 +0200

    *Spell_proc_event entry for Glyph of Felhunter.
    --HG--
    branch : trunk

---
+++ sql/updates/3901_world_spell_proc_event.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_proc_event` WHERE `entry` IN (56249);
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
+(56249,	0x00,	5,	0x00000000,	0x00000000,	0x00000400,	0x00000000,	0x00000000,	0,	0,	0); -- Glyph of Felhunter
