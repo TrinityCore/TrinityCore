bb01747ad181cb07c76d1ea2cbaf0bbeae67004e
QAston <none@none>
2009-06-11 22:42:36 +0200

    *Fix Glyph of Icy Veins.
    --HG--
    branch : trunk
    rename : sql/updates/3914_world_spell_proc_event.sql => sql/updates/3927_world_spell_proc_event.sql

--- sql/updates/3914_world_spell_proc_event.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `spell_proc_event` WHERE `entry` IN (59088, 59089, 58388);
-INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
-(59088,	0x00,	4,	0x00000000,	0x00000002,	0x00000000,	0x00010000,	0x00000000,	0,	0,	0), -- Improved Spell Reflection
-(59089,	0x00,	4,	0x00000000,	0x00000002,	0x00000000,	0x00010000,	0x00000000,	0,	0,	0); -- Improved Spell Reflection
