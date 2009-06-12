332d23b48063e59645a9b72f4166847962ee0811
megamage <none@none>
2009-06-11 19:48:53 -0500

    [7994] Include in mangos.sql/characters.sql changes from sql/updates/7988_*.sql Author: VladimirMangos
        Also fix old typos in item_template data in mangos.sql
    
    --HG--
    branch : trunk

--- sql/characters.sql
+++ sql/characters.sql
@@ -451,6 +451,49 @@ LOCK TABLES `character_declinedname` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `character_equipmentsets`
+--
+
+DROP TABLE IF EXISTS `character_equipmentsets`;
+CREATE TABLE `character_equipmentsets` (
+  `guid` int(11) NOT NULL default '0',
+  `setguid` bigint(20) NOT NULL auto_increment,
+  `setindex` tinyint(4) NOT NULL default '0',
+  `name` varchar(100) NOT NULL,
+  `iconname` varchar(100) NOT NULL,
+  `item0` int(11) NOT NULL default '0',
+  `item1` int(11) NOT NULL default '0',
+  `item2` int(11) NOT NULL default '0',
+  `item3` int(11) NOT NULL default '0',
+  `item4` int(11) NOT NULL default '0',
+  `item5` int(11) NOT NULL default '0',
+  `item6` int(11) NOT NULL default '0',
+  `item7` int(11) NOT NULL default '0',
+  `item8` int(11) NOT NULL default '0',
+  `item9` int(11) NOT NULL default '0',
+  `item10` int(11) NOT NULL default '0',
+  `item11` int(11) NOT NULL default '0',
+  `item12` int(11) NOT NULL default '0',
+  `item13` int(11) NOT NULL default '0',
+  `item14` int(11) NOT NULL default '0',
+  `item15` int(11) NOT NULL default '0',
+  `item16` int(11) NOT NULL default '0',
+  `item17` int(11) NOT NULL default '0',
+  `item18` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`setguid`),
+  UNIQUE KEY `idx_set` (`guid`,`setguid`,`setindex`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `character_equipmentsets`
+--
+
+LOCK TABLES `character_equipmentsets` WRITE;
+/*!40000 ALTER TABLE `character_equipmentsets` DISABLE KEYS */;
+/*!40000 ALTER TABLE `character_equipmentsets` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `character_gifts`
 --
 
