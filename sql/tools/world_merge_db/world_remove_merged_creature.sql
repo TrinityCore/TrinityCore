c75bc47c7be9c0563d038920702dff9f70a0a3ba
megamage <none@none>
2009-06-13 20:45:01 -0500

    *Update db merging tool. Allow merge creature_template and model
    --HG--
    branch : trunk

--- sql/tools/world_merge_db/world_remove_merged_creature.sql
+++ sql/tools/world_merge_db/world_remove_merged_creature.sql
@@ -1,2 +1,8 @@
 DELETE FROM creature WHERE guid IN (SELECT guid FROM merge_creature);
 DROP TABLE IF EXISTS merge_creature;
+
+DELETE FROM creature_model_info WHERE modelid IN (SELECT modelid FROM merge_creature_model_info);
+DROP TABLE IF EXISTS merge_creature_model_info;
+
+DELETE FROM creature_template WHERE entry IN (SELECT entry FROM merge_creature_template);
+DROP TABLE IF EXISTS merge_creature_template;
