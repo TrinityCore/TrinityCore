2aceaa0cb9bfdf4068750bce3ee44a83a6d9b19c
Kudlaty <none@none>
2009-06-15 00:15:40 +0200

    Fix typo in last commit
    --HG--
    branch : trunk

---
+++ sql/updates/3999_sd2.sql
@@ -0,0 +1,5 @@
+DELETE FROM script_texts WHERE entry BETWEEN -1000342 AND -1000340;
+INSERT INTO script_texts (entry,content_default,sound,type,language,emote,comment) VALUES
+(-1000340, 'We made it! Thanks, $N. I couldn''t have gotten without you.', 0, 0, 0, 0, 'npc_deathstalker_erland - SAY_THANKS'),
+(-1000341, 'It''s good to see you again, Erland. What is your report?', 0, 0, 0, 0, 'npc_deathstalker_erland - SAY_RANE'),
+(-1000342, 'Masses of wolves are to the east, and whoever lived at Malden''s Orchard is gone.', 0, 0, 0, 0, 'npc_deathstalker_erland - SAY_ANSWER'),
