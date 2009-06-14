314a4f8f00213b25b48d2aab277a6a6babf0af48
megamage <none@none>
2009-06-14 10:06:41 -0500

    [8008] Fixed SMSG_RAID_INSTANCE_MESSAGE opcode. Author: tomrus88
    --HG--
    branch : trunk

--- src/game/Player.h
+++ src/game/Player.h
@@ -685,7 +685,8 @@ enum InstanceResetWarningType
     RAID_INSTANCE_WARNING_HOURS     = 1,                    // WARNING! %s is scheduled to reset in %d hour(s).
     RAID_INSTANCE_WARNING_MIN       = 2,                    // WARNING! %s is scheduled to reset in %d minute(s)!
     RAID_INSTANCE_WARNING_MIN_SOON  = 3,                    // WARNING! %s is scheduled to reset in %d minute(s). Please exit the zone or you will be returned to your bind location!
-    RAID_INSTANCE_WELCOME           = 4                     // Welcome to %s. This raid instance is scheduled to reset in %s.
+    RAID_INSTANCE_WELCOME           = 4,                    // Welcome to %s. This raid instance is scheduled to reset in %s.
+    RAID_INSTANCE_EXPIRED           = 5
 };
 
 // flags that use in movement check for example at spell casting
