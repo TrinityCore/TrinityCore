8480d94cf8892279fd546e5d2d5977e5616b40e2
QAston <none@none>
2009-06-15 22:34:31 +0200

    *Correct typo in latest commit.
    --HG--
    branch : trunk

--- sql/updates/4031_world_spell_proc_event.sql
+++ sql/updates/4031_world_spell_proc_event.sql
@@ -1,8 +1,5 @@
-DELETE FROM `spell_proc_event` WHERE `entry` IN (50880, 50884, 50885, 50886, 50887, 55610);
+DELETE FROM `spell_proc_event` WHERE `entry` IN (51698, 51700, 51701);
 INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
-( 50880, 0x00,  15, 0x00000000, 0x04000000, 0x00000000, 0x00000000, 0x00000000,   0,   0,   0), -- Icy Talons
-( 50884, 0x00,  15, 0x00000000, 0x04000000, 0x00000000, 0x00000000, 0x00000000,   0,   0,   0), -- Icy Talons
-( 50885, 0x00,  15, 0x00000000, 0x04000000, 0x00000000, 0x00000000, 0x00000000,   0,   0,   0), -- Icy Talons
-( 50886, 0x00,  15, 0x00000000, 0x04000000, 0x00000000, 0x00000000, 0x00000000,   0,   0,   0), -- Icy Talons
-( 50887, 0x00,  15, 0x00000000, 0x04000000, 0x00000000, 0x00000000, 0x00000000,   0,   0,   0), -- Icy Talons
-( 55610, 0x00,  15, 0x00000000, 0x04000000, 0x00000000, 0x00000000, 0x00000000,   0,   0,   0); -- Improved Icy Talons
+( 51698, 0x00,  0, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,   0,   0,   1), -- Honor Among Thieves
+( 51700, 0x00,  0, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,   0,   0,   1), -- Honor Among Thieves
+( 51701, 0x00,  0, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,   0,   0,   1), -- Honor Among Thieves
