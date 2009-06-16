a19f62b551ccddaa40eff65c8c7e972854aefa22
Machiavelli <none@none>
2009-06-16 18:26:24 +0200

    * Backport the Blood Furnace script from TC2 ( Do not merge into TC2 !! )
    --HG--
    branch : trunk

--- src/bindings/scripts/scripts/zone/hellfire_citadel/blood_furnace/boss_broggok.cpp
+++ src/bindings/scripts/scripts/zone/hellfire_citadel/blood_furnace/boss_broggok.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2006 - 2008 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+/* Copyright (C) 2006 - 2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -6,22 +6,24 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 /* ScriptData
 SDName: Boss_Broggok
-SD%Complete: 100
-SDComment:
+SD%Complete: 70
+SDComment: pre-event not made
 SDCategory: Hellfire Citadel, Blood Furnace
 EndScriptData */
 
 #include "precompiled.h"
+#include "def_blood_furnace.h"
+
 
 #define SAY_AGGRO               -1542008
 
@@ -33,8 +35,14 @@ EndScriptData */
 
 struct TRINITY_DLL_DECL boss_broggokAI : public ScriptedAI
 {
-    boss_broggokAI(Creature *c) : ScriptedAI(c) {}
+    boss_broggokAI(Creature *c) : ScriptedAI(c) 
+    {
+        pInstance = ((ScriptedInstance*)c->GetInstanceData());
+        Reset();
+    }
 
+    ScriptedInstance* pInstance;
+    
     uint32 AcidSpray_Timer;
     uint32 PoisonSpawn_Timer;
     uint32 PoisonBolt_Timer;
@@ -44,32 +52,45 @@ struct TRINITY_DLL_DECL boss_broggokAI : public ScriptedAI
         AcidSpray_Timer = 10000;
         PoisonSpawn_Timer = 5000;
         PoisonBolt_Timer = 7000;
+        ToggleDoors(0, DATA_DOOR4);
+        if(pInstance)
+            pInstance->SetData(TYPE_BROGGOK_EVENT, NOT_STARTED);
     }
 
-    void Aggro(Unit *who)
+    void EnterCombat(Unit *who)
     {
         DoScriptText(SAY_AGGRO, m_creature);
+        ToggleDoors(1, DATA_DOOR4);
+        if(pInstance)
+            pInstance->SetData(TYPE_BROGGOK_EVENT, IN_PROGRESS);
+    }
+
+    void JustSummoned(Creature *summoned)
+    {
+        summoned->setFaction(16);
+        summoned->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
+        summoned->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE);
+        summoned->CastSpell(summoned,SPELL_POISON,false,0,0,m_creature->GetGUID());
     }
 
     void UpdateAI(const uint32 diff)
     {
-
-        if (!UpdateVictim())
+        if (!UpdateVictim() )
             return;
 
-        if(AcidSpray_Timer < diff)
+        if (AcidSpray_Timer < diff)
         {
             DoCast(m_creature->getVictim(),SPELL_SLIME_SPRAY);
             AcidSpray_Timer = 4000+rand()%8000;
         }else AcidSpray_Timer -=diff;
 
-        if(PoisonBolt_Timer < diff)
+        if (PoisonBolt_Timer < diff)
         {
             DoCast(m_creature->getVictim(),SPELL_POISON_BOLT);
             PoisonBolt_Timer = 4000+rand()%8000;
         }else PoisonBolt_Timer -=diff;
 
-        if(PoisonSpawn_Timer < diff)
+        if (PoisonSpawn_Timer < diff)
         {
             DoCast(m_creature,SPELL_POISON_CLOUD);
             PoisonSpawn_Timer = 20000;
@@ -77,9 +98,31 @@ struct TRINITY_DLL_DECL boss_broggokAI : public ScriptedAI
 
         DoMeleeAttackIfReady();
     }
+    
+    void JustDied(Unit* who)
+    {   
+        ToggleDoors(0, DATA_DOOR4);
+        ToggleDoors(0, DATA_DOOR5);
+        if(pInstance)
+            pInstance->SetData(TYPE_BROGGOK_EVENT, DONE);
+    }
+    
+    void ToggleDoors(uint8 close, uint64 DOOR)
+    {
+        if (pInstance)
+        {
+            if (GameObject* Doors = GameObject::GetGameObject(*m_creature, pInstance->GetData64(DOOR)))
+            {
+                if (close == 1)
+                    Doors->SetGoState(GO_STATE_READY);                // Closed
+                else
+                    Doors->SetGoState(GO_STATE_ACTIVE);                // Open
+            }
+        }
+    }
 };
 
-CreatureAI* GetAI_boss_broggokAI(Creature *_Creature)
+CreatureAI* GetAI_boss_broggok(Creature *_Creature)
 {
     return new boss_broggokAI (_Creature);
 }
@@ -88,8 +131,7 @@ void AddSC_boss_broggok()
 {
     Script *newscript;
     newscript = new Script;
-    newscript->Name="boss_broggok";
-    newscript->GetAI = &GetAI_boss_broggokAI;
+    newscript->Name = "boss_broggok";
+    newscript->GetAI = &GetAI_boss_broggok;
     newscript->RegisterSelf();
 }
-
