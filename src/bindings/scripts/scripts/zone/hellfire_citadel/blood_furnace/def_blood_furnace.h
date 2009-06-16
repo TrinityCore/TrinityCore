70b33844cf7b43fbfa79b57756517774f1d7a399
Machiavelli <none@none>
2009-06-16 21:58:05 +0200

    Backed out changeset 49921bfa49f0 (1574) - Will have to modify the script later with a little more care.
    --HG--
    branch : trunk

--- src/bindings/scripts/scripts/zone/hellfire_citadel/blood_furnace/def_blood_furnace.h
+++ src/bindings/scripts/scripts/zone/hellfire_citadel/blood_furnace/def_blood_furnace.h
@@ -1,42 +1,10 @@
 /* Copyright (C) 2006 - 2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- */
+ * This program is free software licensed under GPL version 2
+ * Please see the included DOCS/LICENSE.TXT for more information */
 
 #ifndef DEF_BLOOD_FURNACE_H
 #define DEF_BLOOD_FURNACE_H
 
-#define DATA_THE_MAKER                  1
-#define DATA_BROGGOK                    2
-#define DATA_KELIDAN_THE_MAKER          3
-#define TYPE_THE_MAKER_EVENT            4
-#define TYPE_BROGGOK_EVENT              5
-#define TYPE_KELIDAN_THE_BREAKER_EVENT  6
-#define DATA_DOOR1                      7
-#define DATA_DOOR2                      8
-#define DATA_DOOR3                      9
-#define DATA_DOOR4                      10
-#define DATA_DOOR5                      11
-#define DATA_DOOR6                      12
-#define DATA_PRISON_CELL1               13
-#define DATA_PRISON_CELL2               14
-#define DATA_PRISON_CELL3               15
-#define DATA_PRISON_CELL4               16
-#define DATA_PRISON_CELL5               17
-#define DATA_PRISON_CELL6               18
-#define DATA_PRISON_CELL7               19
-#define DATA_PRISON_CELL8               20
+#define DATA_KELIDANEVENT           1
 #endif
-
 
