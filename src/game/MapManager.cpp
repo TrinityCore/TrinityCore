fe7c616b2d079f9b1edb15f9795eaf3b5c55b9a8
megamage <none@none>
2009-06-17 18:37:53 -0500

    [8031] Move player update code to Map::Update. Author: Ambal
    --HG--
    branch : trunk

--- src/game/MapManager.cpp
+++ src/game/MapManager.cpp
@@ -251,11 +251,6 @@ MapManager::Update(uint32 diff)
     if( !i_timer.Passed() )
         return;
 
-    sWorld.RecordTimeDiff(NULL);
-    ObjectAccessor::Instance().UpdatePlayers(i_timer.GetCurrent());
-    sWorld.RecordTimeDiff("UpdatePlayers");
-
-
     for(MapMapType::iterator iter=i_maps.begin(); iter != i_maps.end(); ++iter)
     {
         checkAndCorrectGridStatesArray();                   // debugging code, should be deleted some day
