3aaa3e247ea4a51b85283e10130abb4e137c11f0
megamage <none@none>
2009-06-18 17:07:40 -0500

    [8042] Speedup search creature/pet/vehicle by guid. Author: Ambal
    --HG--
    branch : trunk

--- src/game/ObjectAccessor.cpp
+++ src/game/ObjectAccessor.cpp
@@ -51,11 +51,14 @@ ObjectAccessor::~ObjectAccessor() {}
 Creature*
 ObjectAccessor::GetCreatureOrPetOrVehicle(WorldObject const &u, uint64 guid)
 {
-    if(Creature *unit = GetPet(guid))
-        return unit;
+    if(IS_PLAYER_GUID(guid))
+        return NULL;
 
-    if(Creature *unit = GetVehicle(guid))
-        return unit;
+    if(IS_PET_GUID(guid))
+        return GetPet(guid);
+
+    if(IS_VEHICLE_GUID(guid))
+        return GetVehicle(guid);
 
     return u.GetMap()->GetCreature(guid);
 }
