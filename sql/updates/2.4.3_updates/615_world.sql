c6c1284eb03354d503acd680958b6df29d022914
hunteee <none@none>
2009-06-18 19:31:55 +0200

    *Second part of previous commit.
    --HG--
    branch : trunk

--- sql/updates/2.4.3_updates/615_world.sql
+++ sql/updates/2.4.3_updates/615_world.sql
@@ -4,4 +4,3 @@ DELETE FROM spell_linked_spell WHERE `spell_trigger` IN (-49011, -49012);
 -- typo
 DELETE FROM `spell_linked_spell` WHERE `spell_trigger` IN (43468, 43648);
 INSERT INTO spell_linked_spell (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES (43648, 44007, 1, 'Storm Eye Safe Zone');
-
--- sql/updates/4105_8030_03_mangos_npc_trainer.sql
+++ sql/updates/4105_8030_03_mangos_npc_trainer.sql
@@ -1,4 +1,3 @@
 -- ALTER TABLE db_version CHANGE COLUMN required_8021_01_mangos_spell_proc_event required_8030_03_mangos_npc_trainer bit;
 
 DELETE FROM npc_trainer WHERE spell = 64904;
-
