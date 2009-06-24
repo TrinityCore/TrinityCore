2aa57fa880bc431a2605c6db66279704acefd35f
QAston <none@none>
2009-06-24 02:03:06 +0200

    *Add delete query for data in 4212_world.sql.(if you had problems with 4212 use 4222).
    --HG--
    branch : trunk

--- sql/updates/4212_world.sql
+++ sql/updates/4212_world.sql
@@ -1,17 +1,3 @@
 ALTER TABLE spell_pet_auras ADD effectId TINYINT( 3 ) UNSIGNED NOT NULL AFTER spell;
- 
 ALTER TABLE spell_pet_auras DROP PRIMARY KEY, ADD PRIMARY KEY(spell,effectId,pet);
- 
-INSERT INTO spell_pet_auras VALUES
-(56314, 0, 0, 57447),
-(56314, 1, 0, 57485),
-(56315, 0, 0, 57452),
-(56315, 1, 0, 57484),
-(56316, 0, 0, 57453),
-(56316, 1, 0, 57483),
-(56317, 0, 0, 57457),
-(56317, 1, 0, 57482),
-(56318, 0, 0, 57458),
-(56318, 1, 0, 57475);
 
-DELETE FROM spell_linked_spell where `spell_trigger` in (57447,57482,57453, 57457, 57458);
