d234b35a0c18d7f3b3120483ebe6845be22bead3
QAston <none@none>
2009-06-25 13:50:11 +0200

    *Fix typo causing problems with buying items.
    --HG--
    branch : trunk

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -17639,11 +17639,11 @@ bool Player::BuyItemFromVendor(uint64 vendorguid, uint32 item, uint8 count, uint
 {
     // cheating attempt
     if(count < 1) count = 1;
-    
+
     // cheating attempt
-    if(slot > MAX_BAG_SIZE)
+    if(slot > MAX_BAG_SIZE && slot !=NULL_SLOT)
         return false;
-        
+
     if(!isAlive())
         return false;
 
