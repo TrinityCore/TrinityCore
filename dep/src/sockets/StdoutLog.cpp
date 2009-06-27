41fbe4c3832143704ede776a1e2456c86b02c743
megamage <none@none>
2009-06-27 17:54:03 -0500

    [8080] Portability fixes for some Unix platforms. Author: VladimirMangos
        * Add #include <stdio.h> to some fiels where related functions call.
        * Avoid template dependent lookup for fields in class LockedQueue.
    
    --HG--
    branch : trunk

--- dep/src/sockets/StdoutLog.cpp
+++ dep/src/sockets/StdoutLog.cpp
@@ -27,6 +27,8 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
+#include <stdio.h>
+
 #ifdef _MSC_VER
 #pragma warning(disable:4786)
 #endif
