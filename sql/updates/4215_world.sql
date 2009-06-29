7b1e0b8e0d806939ac292603f86f895f16bc364a
Drahy <none@none>
2009-06-29 22:26:01 +0200

    * Second part of trinity sql coding style - now complete
    --HG--
    branch : trunk

--- sql/updates/4215_world.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `command` WHERE `name` IN ('modify tp');
- 
-INSERT INTO `command` VALUES
-('modify tp',1,'Syntax: .modify tp #amount\r\n\r\nSte free talent pointes for selected character or character\'s pet. It will be reset to default expected at next levelup/login/quest reward.');
