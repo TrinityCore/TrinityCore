e246e2e55f94a4dd68b9f521b49ed721936c284e
megamage <none@none>
2009-07-01 17:52:58 -0500

    [8097] Updated comment based on research. Author: ApoC
    --HG--
    branch : trunk

--- src/game/DBCStructure.h
+++ src/game/DBCStructure.h
@@ -490,7 +490,9 @@ struct AchievementCriteriaEntry
     //uint32 name_flags;                                    // 25
     uint32  completionFlag;                                 // 26
     uint32  groupFlag;                                      // 27
-    //uint32 unk1;                                          // 28
+    //uint32 unk1;                                          // 28 Alway appears with timed events
+                                                            // for timed spells it is spell id for
+                                                            // timed kills it is creature id
     uint32  timeLimit;                                      // 29 time limit in seconds
     //uint32 showOrder;                                     // 30 show order
 };
