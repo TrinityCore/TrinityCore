3e415740dda308a540168fe71bc7c8d47e2cdc7d
QAston <none@none>
2009-07-10 16:29:43 +0200

    *Fix compile - sorry for that:/.
    --HG--
    branch : trunk

--- src/game/GameObject.h
+++ src/game/GameObject.h
@@ -499,19 +499,7 @@ class TRINITY_DLL_SPEC GameObject : public WorldObject
 
         bool IsTransport() const;
 
-        void SetOwnerGUID(uint64 owner)
-        {
-            // Owner already found and different than expected owner - remove object from old owner
-            if (owner && GetOwnerGUID() && GetOwnerGUID() != owner)
-            {
-                if (Unit* owner = GetOwner())
-                    owner->RemoveGameObject(this, false);
-                else
-                    assert(false);
-            }
-            m_spawnedByDefault = false;                     // all object with owner is despawned after delay
-            SetUInt64Value(OBJECT_FIELD_CREATED_BY, owner);
-        }
+        void SetOwnerGUID(uint64 owner);
         uint64 GetOwnerGUID() const { return GetUInt64Value(OBJECT_FIELD_CREATED_BY); }
         Unit* GetOwner() const;
 
