33a720ddb4a29ee611a35a5096048ef9789499c4
QAston <none@none>
2009-07-14 16:39:20 +0200

    *spell_bonus_data for Glyph of Chains of Ice - by Zcuron
    --HG--
    branch : trunk

---
+++ sql/updates/4464_world_spell_bonus_data_TDB.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_bonus_data` WHERE `entry` = '58621';
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `comments`) VALUES
+(58621, 0, 0, 0.08, 'Death Knight - Glyph of Chains of Ice');
