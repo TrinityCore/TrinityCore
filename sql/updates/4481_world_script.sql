d6bd04ab0d813e99ab1c7d61d0d71c3da2fe0ae4
BroodWyrm <none@none>
2009-07-16 20:27:53 +0200

    * Add script for Val'kyr Battle-maiden - patch provided by Drethek * Add summonerguid to summons by spell
    --HG--
    branch : trunk

---
+++ sql/updates/4481_world_script.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_template` SET `ScriptName`='npc_valkyr_battle_maiden' WHERE (`entry`='28534');
+INSERT iGNORE INTO `spell_proc_event`  VALUES ( 51915, 0x00,   0, 0x00000000, 0x00000000, 0x00000000, 0x00000001, 0x00000000,   0, 100, 600);
