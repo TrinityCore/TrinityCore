454c734cd44c74e4d0fea965b633bda71ba0ced3
Rat <none@none>
2009-07-18 22:15:56 +0200

    *implemented basic Chat Channel saving *includes owner, banlist,announce,password, saving *todo:  -make it cacheble  -add a filter to not save some addon spec channels  -find someone who does these for me :)
    --HG--
    branch : trunk

---
+++ sql/updates/4498_characters_channels.sql
@@ -0,0 +1,14 @@
+-- ----------------------------
+-- Table structure for channels
+-- ----------------------------
+DROP TABLE IF EXISTS `channels`;
+CREATE TABLE `channels` (
+  `m_name` text NOT NULL,
+  `m_team` int(10) unsigned NOT NULL,
+  `m_ownerGUID` int(11) unsigned NOT NULL default '0',
+  `m_announce` tinyint(1) unsigned NOT NULL default '0',
+  `m_moderate` tinyint(1) unsigned NOT NULL default '0',
+  `m_password` text,
+  `BannedList` longtext,
+  PRIMARY KEY  (`m_name`(10),`m_team`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Access Requirements';
