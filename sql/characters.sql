a00473d79510f80ae1fd6b4ed882d0e696c22659
Anubisss <none@none>
2009-07-21 21:16:30 +0200

    *Add table channels to characters.sql
    --HG--
    branch : trunk

--- sql/characters.sql
+++ sql/characters.sql
@@ -267,6 +267,21 @@ LOCK TABLES `bugreport` WRITE;
 /*!40000 ALTER TABLE `bugreport` ENABLE KEYS */;
 UNLOCK TABLES;
 
+-- ----------------------------
+-- Table structure for channels
+-- ----------------------------
+DROP TABLE IF EXISTS `channels`;
+CREATE TABLE `channels` (
+  `m_name` text NOT NULL,
+  `m_team` int(10) unsigned NOT NULL,
+  `m_ownerGUID` int(11) unsigned NOT NULL default '0',
+  `m_announce` tinyint(1) unsigned NOT NULL default '0',
+  `m_moderate` tinyint(1) unsigned NOT NULL default '0',
+  `m_password` text,
+  `BannedList` longtext,
+  PRIMARY KEY  (`m_name`(10),`m_team`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Channel System';
+
 --
 -- Table structure for table `characters`
 --
