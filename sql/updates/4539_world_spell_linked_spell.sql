07a610edcdf5801a5ebbbd52b828ad3ed9b0011a
QAston <none@none>
2009-07-23 01:01:12 +0200

    *Add spell_linked_spell entry for Lichborne. *Add defines for SHADOW_DANCE and UNDEAD shapeshifts to prevent error display - cherrypicked from Astellar's shape patches.
    --HG--
    branch : trunk

---
+++ sql/updates/4539_world_spell_linked_spell.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger` IN (49039);
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES
+( 49039, 50397, 2, 'Lichborne - shapeshift');
