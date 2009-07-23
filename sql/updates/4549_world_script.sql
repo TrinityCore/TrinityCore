da32275c15ccfb36553f0f3cad9fe889c6588340
Anubisss <none@none>
2009-07-23 21:16:37 +0200

    *Fix a typo.
    --HG--
    branch : trunk

--- sql/updates/4549_world_script.sql
+++ sql/updates/4549_world_script.sql
@@ -1,4 +1,4 @@
 UPDATE `creature_template` SET `ScriptName`='npc_khunok_the_behemoth' WHERE (`entry`='25862');
 
---- DB CONTENT ----
+-- DB CONTENT --
 UPDATE `quest_template` SET `SrcSpell`='46232' WHERE (`entry`='11878');
