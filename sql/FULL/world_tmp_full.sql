3cd10ac4890596c5c593bcc635b8ee1eaba14283
Drahy <none@none>
2009-07-27 08:29:46 +0200

    * Fixed elevators in game * Fixed ugly typo in previous commit
    --HG--
    branch : trunk

--- sql/FULL/world_tmp_full.sql
+++ sql/FULL/world_tmp_full.sql
@@ -28,6 +28,62 @@ UPDATE creature_template SET speed = 1.0 WHERE entry = 23095; # molten_flame
 -- --------
 UPDATE `creature_template` SET `flags_extra` = 33 WHERE `entry` = 23576; /*no crush*/
 
+-- elevators
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=11898;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=11899;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=149045;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=149046;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=164871;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=175080;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=176080;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=176081;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=176082;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=176083;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=176084;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=176085;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=176086;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=176231;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=176244;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=176310;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=176495;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=177233;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=181056;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=181646;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=183169;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=183177;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=183202;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=183203;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=183407;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=183490;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=183788;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=184330;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=20649;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=20650;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=20651;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=20652;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=20653;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=20654;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=20655;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=20656;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=20657;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=20808;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=210335;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=210336;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=210349;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=211023;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=211024;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=211050;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=211051;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=211052;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=211053;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=4170;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=4171;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=47296;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=47297;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=80022;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=80023;
+UPDATE `gameobject_template` SET `flags`=40 WHERE `entry`=85556;
+
 -- --------
 -- PET
 -- --------
