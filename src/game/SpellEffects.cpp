ac7f18d4f72a0a95ee89c71a19c52813d1ee8f9f
QAston <none@none>
2009-07-30 00:05:36 +0200

    *Use basepoints for effect 140 only when non-null.
    --HG--
    branch : trunk

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -2028,7 +2028,10 @@ void Spell::EffectForceCast(uint32 i)
         return;
     }
 
-    unitTarget->CastCustomSpell(unitTarget, spellInfo->Id, &damage, NULL, NULL, true, NULL, NULL, m_originalCasterGUID);
+    if (damage)
+        unitTarget->CastCustomSpell(unitTarget, spellInfo->Id, &damage, NULL, NULL, true, NULL, NULL, m_originalCasterGUID);
+    else
+        unitTarget->CastSpell(unitTarget, spellInfo, true, NULL, NULL, m_originalCasterGUID);
 }
 
 void Spell::EffectTriggerSpell(uint32 i)
