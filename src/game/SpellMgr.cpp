672e57dddbb61ef2d934c3ccbc4dc861cf88bd04
megamage <none@none>
2009-07-30 11:06:31 +0800

    [8243] Fixed problems with unexpected set diminishion return level at arena for fear/turn spells. Author: VladimirMangos
    --HG--
    branch : trunk

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -2843,6 +2843,11 @@ DiminishingGroup GetDiminishingReturnsGroupForSpell(SpellEntry const* spellproto
     // Explicit Diminishing Groups
     switch(spellproto->SpellFamilyName)
     {
+        case SPELLFAMILY_GENERIC:
+            // some generic arena related spells have by some strange reason MECHANIC_TURN
+            if  (spellproto->Mechanic == MECHANIC_TURN)
+                return DIMINISHING_NONE;
+            break;
         case SPELLFAMILY_MAGE:
         {
             // Frostbite 0x80000000
