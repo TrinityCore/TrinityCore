05491d1c8dea7fff38784bcf170a99b0aaaaa305
megamage <none@none>
2009-07-31 11:33:48 +0800

    *Fixed /played chat command. Author: tomrus88
    --HG--
    branch : trunk

--- src/game/MiscHandler.cpp
+++ src/game/MiscHandler.cpp
@@ -1216,15 +1216,17 @@ void WorldSession::HandleWardenDataOpcode(WorldPacket& /*recv_data*/)
     */
 }
 
-void WorldSession::HandlePlayedTime(WorldPacket& /*recv_data*/)
+void WorldSession::HandlePlayedTime(WorldPacket& recv_data)
 {
-    uint32 TotalTimePlayed = GetPlayer()->GetTotalPlayedTime();
-    uint32 LevelPlayedTime = GetPlayer()->GetLevelPlayedTime();
+    CHECK_PACKET_SIZE(recv_data, 1);
 
-    WorldPacket data(SMSG_PLAYED_TIME, 9);
-    data << TotalTimePlayed;
-    data << LevelPlayedTime;
-    data << uint8(1);
+    uint8 unk1;
+    recv_data >> unk1;                                      // 0 or 1 expected
+
+    WorldPacket data(SMSG_PLAYED_TIME, 4 + 4 + 1);
+    data << uint32(_player->GetTotalPlayedTime());
+    data << uint32(_player->GetLevelPlayedTime());
+    data << uint8(unk1);                                    // 0 - will not show in chat frame
     SendPacket(&data);
 }
 
