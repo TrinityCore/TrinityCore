3a3e6d88d3ee839831477b2250028551777472a7
Rat <none@none>
2009-08-03 01:12:05 +0200

    *fix Lady Vashj *fix Toxic Sporebat's Toxic Pool spell
    --HG--
    branch : trunk

---
+++ sql/updates/4725_world_scripts.sql
@@ -0,0 +1,5 @@
+UPDATE `creature_template` SET `minlevel`='80' WHERE (`entry`='22207');
+UPDATE `creature_template` SET `maxlevel`='80' WHERE (`entry`='22207');
+UPDATE `creature_template` SET `faction_A`='14' WHERE (`entry`='22207');
+UPDATE `creature_template` SET `faction_H`='14' WHERE (`entry`='22207');
+UPDATE `creature_template` SET `flags_extra`='128' WHERE (`entry`='22207');
