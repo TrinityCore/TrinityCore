d7f7862d96c8086e3ea432e90530a329841317dc
Chaz Brown <iamparadox@netscape.net>
2009-08-07 02:47:25 -0400

    Fix knockback effect cause the creature's position client-server sync problem - By smellbee Details and comments on this patch found here: http://www.trinitycore.org/forum/project.php?issueid=4689
    --HG--
    branch : trunk

--- src/game/TargetedMovementGenerator.cpp
+++ src/game/TargetedMovementGenerator.cpp
@@ -205,7 +205,9 @@ TargetedMovementGenerator<T>::Update(T &owner, const uint32 & time_diff)
         return true;
     }
 
-    if (i_destinationHolder.UpdateTraveller(traveller, time_diff))
+    bool update_traveller = (owner.GetTypeId() == TYPEID_UNIT && !((Creature*)&owner)->isPet())? i_destinationHolder.UpdateTraveller(traveller, time_diff, true) : i_destinationHolder.UpdateTraveller(traveller, time_diff, false);
+    //if (i_destinationHolder.UpdateTraveller(traveller, time_diff))
+    if(update_traveller)
     {
         // put targeted movement generators on a higher priority
         //if (owner.GetObjectSize())
