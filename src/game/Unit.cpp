6ebfafeea203e34eaaad62a869a546c0d16ef5e3
megamage <none@none>
2009-08-08 17:58:58 -0500

    *Fix a crash caused by undismissed pet.
    --HG--
    branch : trunk

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -9138,9 +9138,7 @@ void Unit::RemoveAllControlled()
             && target->GetTypeId() == TYPEID_UNIT
             && ((Creature*)target)->HasSummonMask(SUMMON_MASK_SUMMON))
         {
-
-            if(!((TempSummon*)target)->isPet())
-                ((TempSummon*)target)->UnSummon();
+            ((TempSummon*)target)->UnSummon();
         }
         else
         {
