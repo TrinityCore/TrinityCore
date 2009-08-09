6fe9ce419eb6046bf7ddd114ec65434a6b445a69
maximius <none@none>
2009-08-08 22:13:01 -0700

    *Update Vault of Archavon, updates and FULL sqls are now accurate
    --HG--
    branch : trunk

---
+++ sql/updates/4841_world_scripts.sql
@@ -0,0 +1,4 @@
+UPDATE `creature_template` SET `ScriptName`='mob_archavon_warder' WHERE `entry`=32353;
+UPDATE `creature_template` SET `ScriptName`='boss_emalon' WHERE `entry`=33993;
+UPDATE `creature_template` SET `ScriptName`='mob_tempest_minion' WHERE `entry`=33998;
+UPDATE `instance_template` SET `ScriptName`='instance_archavon' WHERE `map`=624;
