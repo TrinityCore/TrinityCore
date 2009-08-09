b68a0972e0459ae1553a2d79d34a8c9b6665cbc7
maximius <none@none>
2009-08-08 23:27:18 -0700

    *ScriptName -> script (thanks Forgiven)
    --HG--
    branch : trunk

--- sql/FULL/world_scripts_full.sql
+++ sql/FULL/world_scripts_full.sql
@@ -1024,7 +1024,7 @@ UPDATE `creature_template` SET `ScriptName`='boss_archavon' WHERE `entry`=31125;
 UPDATE `creature_template` SET `ScriptName`='mob_archavon_warder' WHERE `entry`=32353;
 UPDATE `creature_template` SET `ScriptName`='boss_emalon' WHERE `entry`=33993;
 UPDATE `creature_template` SET `ScriptName`='mob_tempest_minion' WHERE `entry`=33998;
-UPDATE `instance_template` SET `ScriptName`='instance_archavon' WHERE `map`=624;
+UPDATE `instance_template` SET `script`='instance_archavon' WHERE `map`=624;
 
 /* WAILING CAVERNS */
 
