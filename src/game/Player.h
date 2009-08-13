9f38d67f2e4ddb23c55e508c615f4ecf36547ed0
megamage <none@none>
2009-08-12 20:57:35 -0500

    [8355] Implement QUEST_STATUS_FAILED and simplify related code.
        Use FailQuest for all quests that can fail and remove no longer needed function FailedTimedQuest.
    
        Signed-off-by: NoFantasy <nofantasy@nf.no>
    
    --HG--
    branch : trunk

--- src/game/Player.h
+++ src/game/Player.h
@@ -1222,7 +1222,6 @@ class MANGOS_DLL_SPEC Player : public Unit
         void IncompleteQuest( uint32 quest_id );
         void RewardQuest( Quest const *pQuest, uint32 reward, Object* questGiver, bool announce = true );
         void FailQuest( uint32 quest_id );
-        void FailTimedQuest( uint32 quest_id );
         bool SatisfyQuestSkillOrClass( Quest const* qInfo, bool msg );
         bool SatisfyQuestLevel( Quest const* qInfo, bool msg );
         bool SatisfyQuestLog( bool msg );
