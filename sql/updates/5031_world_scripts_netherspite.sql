7c81b694e96c70470fe01d81ef93a47f5aed9e42
megamage <none@none>
2009-08-14 18:41:10 -0500

    *Fix sql in 5031.
    --HG--
    branch : trunk

--- sql/updates/5031_world_scripts_netherspite.sql
+++ sql/updates/5031_world_scripts_netherspite.sql
@@ -1,8 +1,8 @@
 UPDATE `creature_template` SET `ScriptName`='boss_netherspite', `RegenHealth` = '1' WHERE `entry`='15689';
 UPDATE `creature_template` SET `faction_A` = '35', `faction_H` = '35', `modelid_A`='11686', `modelid_H`='11686', `unit_flags` = '33554496', `ScriptName` = '' WHERE `entry` IN ('17367','17368','17369');
 UPDATE `creature_template` SET `minlevel` = '70', `maxlevel` = '70',`flags_extra`= '2', `speed`= '0.0001', `faction_A` = '16', `faction_H` = '16', `ScriptName` = 'mob_eventai'  WHERE `entry` = '16697';
-DELETE FROM `eventai_scripts` WHERE `creature_id` = '16697';
-INSERT INTO `eventai_scripts`
+DELETE FROM `creature_ai_scripts` WHERE `creature_id` = '16697';
+INSERT INTO `creature_ai_scripts`
 (`id`,`creature_id`,`event_type`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action1_type`,`action1_param1`,`action1_param2`,`action1_param3`,`comment`) VALUES
 ('1669701', '16697',         '0',         '100',          '3',        '3000',        '3000',        '3000',        '3000',          '11',         '46264',             '0',             '2','Void Zone - Cast Void Zone Effect'),
 ('1669702', '16697',         '1',         '100',          '3',        '3000',        '3000',        '3000',        '3000',          '11',         '46264',             '0',             '2','Void Zone - Cast Void Zone Effect');
