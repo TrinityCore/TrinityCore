f37885c086be2c937afa62dfde868ebbcd97d79c
Kudlaty <none@none>
2009-08-14 21:58:29 +0200

    Merge [SD2] r1281 Added support for quest 12247. Patch by Azuritus r1282 Added support for quest 11708. Patch by Azuritus.       Additional cleanup and code style applied. r1283 Add support for quest 12807. Patch by Azuritus
    --HG--
    branch : trunk

--- src/bindings/scripts/scripts/zone/borean_tundra/borean_tundra.cpp
+++ src/bindings/scripts/scripts/zone/borean_tundra/borean_tundra.cpp
@@ -17,36 +17,92 @@
 /* ScriptData
 SDName: Borean_Tundra
 SD%Complete: 100
-SDComment:
+SDComment: Quest support: 11708. Taxi vendors.
 SDCategory: Borean Tundra
 EndScriptData */
 
 /* ContentData
-npc_tiare
+npc_fizzcrank_fullthrottle
 npc_surristrasz
+npc_tiare
 EndContentData */
 
 #include "precompiled.h"
 
 /*######
-## npc_tiare
+## npc_fizzcrank_fullthrottle
 ######*/
 
-#define GOSSIP_ITEM_TELEPORT    "Teleport me to Amber Ledge, please."
+#define GOSSIP_ITEM_GO_ON   "Go on."
+#define GOSSIP_ITEM_TELL_ME "Tell me what's going on out here, Fizzcrank."
 
-bool GossipHello_npc_tiare(Player* pPlayer, Creature* pCreature)
+enum
 {
-    pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_ITEM_TELEPORT, GOSSIP_SENDER_MAIN, GOSSIP_OPTION_GOSSIP);
+    GOSSIP_TEXTID_FIZZCRANK1    = 12456,
+    GOSSIP_TEXTID_FIZZCRANK2    = 12457,
+    GOSSIP_TEXTID_FIZZCRANK3    = 12458,
+    GOSSIP_TEXTID_FIZZCRANK4    = 12459,
+    GOSSIP_TEXTID_FIZZCRANK5    = 12460,
+    GOSSIP_TEXTID_FIZZCRANK6    = 12461,
+    GOSSIP_TEXTID_FIZZCRANK7    = 12462,
+    GOSSIP_TEXTID_FIZZCRANK8    = 12463,
+    GOSSIP_TEXTID_FIZZCRANK9    = 12464,
+
+    QUEST_THE_MECHAGNOMES       = 11708
+};
+
+bool GossipHello_npc_fizzcrank_fullthrottle(Player* pPlayer, Creature* pCreature)
+{
+    if (pCreature->isQuestGiver())
+        pPlayer->PrepareQuestMenu(pCreature->GetGUID());
+
+    if (pPlayer->GetQuestStatus(QUEST_THE_MECHAGNOMES) == QUEST_STATUS_INCOMPLETE)
+        pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_ITEM_TELL_ME, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+1);
+
     pPlayer->SEND_GOSSIP_MENU(pCreature->GetNpcTextId(), pCreature->GetGUID());
     return true;
 }
 
-bool GossipSelect_npc_tiare(Player* pPlayer, Creature* pCreature, uint32 sender, uint32 action)
+bool GossipSelect_npc_fizzcrank_fullthrottle(Player* pPlayer, Creature* pCreature, uint32 uiSender, uint32 uiAction)
 {
-    if (action == GOSSIP_OPTION_GOSSIP)
+    switch(uiAction)
     {
-        pPlayer->CLOSE_GOSSIP_MENU();
-        pPlayer->CastSpell(pPlayer,50135,true);
+        case GOSSIP_ACTION_INFO_DEF+1:
+            pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_ITEM_GO_ON, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
+            pPlayer->SEND_GOSSIP_MENU(GOSSIP_TEXTID_FIZZCRANK1, pCreature->GetGUID());
+            break;
+        case GOSSIP_ACTION_INFO_DEF+2:
+            pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_ITEM_GO_ON, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 3);
+            pPlayer->SEND_GOSSIP_MENU(GOSSIP_TEXTID_FIZZCRANK2, pCreature->GetGUID());
+            break;
+        case GOSSIP_ACTION_INFO_DEF+3:
+            pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_ITEM_GO_ON, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 4);
+            pPlayer->SEND_GOSSIP_MENU(GOSSIP_TEXTID_FIZZCRANK3, pCreature->GetGUID());
+            break;
+        case GOSSIP_ACTION_INFO_DEF+4:
+            pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_ITEM_GO_ON, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 5);
+            pPlayer->SEND_GOSSIP_MENU(GOSSIP_TEXTID_FIZZCRANK4, pCreature->GetGUID());
+            break;
+        case GOSSIP_ACTION_INFO_DEF+5:
+            pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_ITEM_GO_ON, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 6);
+            pPlayer->SEND_GOSSIP_MENU(GOSSIP_TEXTID_FIZZCRANK5, pCreature->GetGUID());
+            break;
+        case GOSSIP_ACTION_INFO_DEF+6:
+            pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_ITEM_GO_ON, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 7);
+            pPlayer->SEND_GOSSIP_MENU(GOSSIP_TEXTID_FIZZCRANK6, pCreature->GetGUID());
+            break;
+        case GOSSIP_ACTION_INFO_DEF+7:
+            pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_ITEM_GO_ON, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 8);
+            pPlayer->SEND_GOSSIP_MENU(GOSSIP_TEXTID_FIZZCRANK7, pCreature->GetGUID());
+            break;
+        case GOSSIP_ACTION_INFO_DEF+8:
+            pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_ITEM_GO_ON, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 9);
+            pPlayer->SEND_GOSSIP_MENU(GOSSIP_TEXTID_FIZZCRANK8, pCreature->GetGUID());
+            break;
+        case GOSSIP_ACTION_INFO_DEF+9:
+            pPlayer->SEND_GOSSIP_MENU(GOSSIP_TEXTID_FIZZCRANK9, pCreature->GetGUID());
+            pPlayer->AreaExploredOrEventHappens(QUEST_THE_MECHAGNOMES);
+            break;
     }
     return true;
 }
@@ -57,6 +113,11 @@ bool GossipSelect_npc_tiare(Player* pPlayer, Creature* pCreature, uint32 sender,
 
 #define GOSSIP_ITEM_FREE_FLIGHT "I'd like passage to the Transitus Shield."
 #define GOSSIP_ITEM_FLIGHT      "May I use a drake to fly elsewhere?"
+
+enum
+{
+    SPELL_ABMER_TO_COLDARRA     = 46064
+};
 
 bool GossipHello_npc_surristrasz(Player* pPlayer, Creature* pCreature)
 {
@@ -73,16 +134,46 @@ bool GossipHello_npc_surristrasz(Player* pPlayer, Creature* pCreature)
     return true;
 }
 
-bool GossipSelect_npc_surristrasz(Player* pPlayer, Creature* pCreature, uint32 sender, uint32 action)
+bool GossipSelect_npc_surristrasz(Player* pPlayer, Creature* pCreature, uint32 uiSender, uint32 uiAction)
 {
-    if (action == GOSSIP_OPTION_GOSSIP)
+    if (uiAction == GOSSIP_OPTION_GOSSIP)
     {
         pPlayer->CLOSE_GOSSIP_MENU();
-        pPlayer->CastSpell(pPlayer,46064,true);               //TaxiPath 795 (amber to coldarra)
+
+        //TaxiPath 795 (amber to coldarra)
+        pPlayer->CastSpell(pPlayer, SPELL_ABMER_TO_COLDARRA, true);
     }
-    if (action == GOSSIP_OPTION_TAXIVENDOR)
-    {
+
+    if (uiAction == GOSSIP_OPTION_TAXIVENDOR)
         pPlayer->GetSession()->SendTaxiMenu(pCreature);
+
+    return true;
+}
+
+/*######
+## npc_tiare
+######*/
+
+#define GOSSIP_ITEM_TELEPORT    "Teleport me to Amber Ledge, please."
+
+enum
+{
+    SPELL_TELEPORT_COLDARRA     = 50135
+};
+
+bool GossipHello_npc_tiare(Player* pPlayer, Creature* pCreature)
+{
+    pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_ITEM_TELEPORT, GOSSIP_SENDER_MAIN, GOSSIP_OPTION_GOSSIP);
+    pPlayer->SEND_GOSSIP_MENU(pCreature->GetNpcTextId(), pCreature->GetGUID());
+    return true;
+}
+
+bool GossipSelect_npc_tiare(Player* pPlayer, Creature* pCreature, uint32 uiSender, uint32 uiAction)
+{
+    if (uiAction == GOSSIP_OPTION_GOSSIP)
+    {
+        pPlayer->CLOSE_GOSSIP_MENU();
+        pPlayer->CastSpell(pPlayer, SPELL_TELEPORT_COLDARRA, true);
     }
     return true;
 }
@@ -236,15 +327,21 @@ void AddSC_borean_tundra()
     Script *newscript;
 
     newscript = new Script;
-    newscript->Name = "npc_tiare";
-    newscript->pGossipHello =  &GossipHello_npc_tiare;
-    newscript->pGossipSelect = &GossipSelect_npc_tiare;
+    newscript->Name = "npc_fizzcrank_fullthrottle";
+    newscript->pGossipHello = &GossipHello_npc_fizzcrank_fullthrottle;
+    newscript->pGossipSelect = &GossipSelect_npc_fizzcrank_fullthrottle;
     newscript->RegisterSelf();
 
     newscript = new Script;
     newscript->Name = "npc_surristrasz";
-    newscript->pGossipHello =  &GossipHello_npc_surristrasz;
+    newscript->pGossipHello = &GossipHello_npc_surristrasz;
     newscript->pGossipSelect = &GossipSelect_npc_surristrasz;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name = "npc_tiare";
+    newscript->pGossipHello = &GossipHello_npc_tiare;
+    newscript->pGossipSelect = &GossipSelect_npc_tiare;
     newscript->RegisterSelf();
 
     newscript = new Script;
---
+++ src/bindings/scripts/scripts/zone/grizzly_hills/grizzly_hills.cpp
@@ -0,0 +1,126 @@
+/* Copyright (C) 2006 - 2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/* ScriptData
+SDName: Grizzly_Hills
+SD%Complete: 80
+SDComment: Quest support: 12247
+SDCategory: Grizzly Hills
+EndScriptData */
+
+/* ContentData
+npc_orsonn_and_kodian
+EndContentData */
+
+#include "precompiled.h"
+
+#define GOSSIP_ITEM1 "You're free to go Orsonn, but first tell me what's wrong with the furbolg."
+#define GOSSIP_ITEM2 "What happened then?"
+#define GOSSIP_ITEM3 "Thank you, Son of Ursoc. I'll see what can be done."
+#define GOSSIP_ITEM4 "Who was this stranger?"
+#define GOSSIP_ITEM5 "Thank you, Kodian. I'll do what I can."
+
+enum
+{
+    GOSSIP_TEXTID_ORSONN1       = 12793,
+    GOSSIP_TEXTID_ORSONN2       = 12794,
+    GOSSIP_TEXTID_ORSONN3       = 12796,
+
+    GOSSIP_TEXTID_KODIAN1       = 12797,
+    GOSSIP_TEXTID_KODIAN2       = 12798,
+
+    NPC_ORSONN                  = 27274,
+    NPC_KODIAN                  = 27275,
+
+    //trigger creatures
+    NPC_ORSONN_CREDIT           = 27322,
+    NPC_KODIAN_CREDIT           = 27321,
+
+    QUEST_CHILDREN_OF_URSOC     = 12247
+};
+
+bool GossipHello_npc_orsonn_and_kodian(Player* pPlayer, Creature* pCreature)
+{
+    if (pCreature->isQuestGiver())
+        pPlayer->PrepareQuestMenu(pCreature->GetGUID());
+
+    if (pPlayer->GetQuestStatus(QUEST_CHILDREN_OF_URSOC) == QUEST_STATUS_INCOMPLETE)
+    {
+        switch(pCreature->GetEntry())
+        {
+            case NPC_ORSONN:
+                if (!pPlayer->GetReqKillOrCastCurrentCount(QUEST_CHILDREN_OF_URSOC, NPC_ORSONN_CREDIT))
+                {
+                    pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_ITEM1, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+1);
+                    pPlayer->SEND_GOSSIP_MENU(GOSSIP_TEXTID_ORSONN1, pCreature->GetGUID());
+                    return true;
+                }
+                break;
+            case NPC_KODIAN:
+                if (!pPlayer->GetReqKillOrCastCurrentCount(QUEST_CHILDREN_OF_URSOC, NPC_KODIAN_CREDIT))
+                {
+                    pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_ITEM4, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+4);
+                    pPlayer->SEND_GOSSIP_MENU(GOSSIP_TEXTID_KODIAN1, pCreature->GetGUID());
+                    return true;
+                }
+                break;
+        }
+    }
+
+    pPlayer->SEND_GOSSIP_MENU(pCreature->GetNpcTextId(), pCreature->GetGUID());
+    return true;
+}
+
+bool GossipSelect_npc_orsonn_and_kodian(Player* pPlayer, Creature* pCreature, uint32 uiSender, uint32 uiAction)
+{
+    switch(uiAction)
+    {
+        case GOSSIP_ACTION_INFO_DEF+1:
+            pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_ITEM2, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
+            pPlayer->SEND_GOSSIP_MENU(GOSSIP_TEXTID_ORSONN2, pCreature->GetGUID());
+            break;
+        case GOSSIP_ACTION_INFO_DEF+2:
+            pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_ITEM3, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 3);
+            pPlayer->SEND_GOSSIP_MENU(GOSSIP_TEXTID_ORSONN3, pCreature->GetGUID());
+            break;
+        case GOSSIP_ACTION_INFO_DEF+3:
+            pPlayer->CLOSE_GOSSIP_MENU();
+            pPlayer->TalkedToCreature(NPC_ORSONN_CREDIT, pCreature->GetGUID());
+            break;
+
+        case GOSSIP_ACTION_INFO_DEF+4:
+            pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_ITEM5, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 5);
+            pPlayer->SEND_GOSSIP_MENU(GOSSIP_TEXTID_KODIAN2, pCreature->GetGUID());
+            break;
+        case GOSSIP_ACTION_INFO_DEF+5:
+            pPlayer->CLOSE_GOSSIP_MENU();
+            pPlayer->TalkedToCreature(NPC_KODIAN_CREDIT, pCreature->GetGUID());
+            break;
+    }
+
+    return true;
+}
+
+void AddSC_grizzly_hills()
+{
+    Script* newscript;
+
+    newscript = new Script;
+    newscript->Name = "npc_orsonn_and_kodian";
+    newscript->pGossipHello = &GossipHello_npc_orsonn_and_kodian;
+    newscript->pGossipSelect = &GossipSelect_npc_orsonn_and_kodian;
+    newscript->RegisterSelf();
+}
