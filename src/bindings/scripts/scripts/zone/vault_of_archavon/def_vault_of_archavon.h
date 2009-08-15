f835633be0e4d16a043039876aa3fbed7edf5fb2
megamage <none@none>
2009-08-15 14:36:15 -0500

    *Update Emalon the Storm Watcher script. By riddick
    --HG--
    branch : trunk

--- src/bindings/scripts/scripts/zone/vault_of_archavon/def_vault_of_archavon.h
+++ src/bindings/scripts/scripts/zone/vault_of_archavon/def_vault_of_archavon.h
@@ -1,13 +1,8 @@
 #ifndef DEF_ARCHAVON_H
 #define DEF_ARCHAVON_H
 
-enum
-{
-    DATA_ARCHAVON           = 1,
-    DATA_EMALON             = 2,
-
-    DATA_ARCHAVON_EVENT     = 3,
-    DATA_EMALON_EVENT       = 4
-};
-
+#define DATA_ARCHAVON_EVENT			1
+#define DATA_EMALON_EVENT			2
+#define DATA_EMALON					3
+#define DATA_ARCHAVON				4
 #endif
