683848679649994f048a526be55f776644165827
Kudlaty <none@none>
2009-08-16 17:30:05 +0200

    Merge [SD2] r1306 Remove now empty zone folder and complete re-organize file/folder structure. Thanks to DasBlub, Zor and others for inspiration, help and suggestions.
    --HG--
    branch : trunk

---
+++ src/bindings/scripts/system/system.h
@@ -0,0 +1,8 @@
+/* Copyright (C) 2006 - 2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+* This program is free software licensed under GPL version 2
+* Please see the included DOCS/LICENSE.TXT for more information */
+
+#ifndef SC_SYSTEM_H
+#define SC_SYSTEM_H
+
+#endif
