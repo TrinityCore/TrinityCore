87c171f9541d63179e2be1051470af2811ca2936
Machiavelli <none@none>
2009-08-17 22:06:37 +0200

    Channel system update: * Fix bug that player does not have moderator priveliges when ownership was passed on to him after previous owner left the channel. * Comment out code + alter DB field that are responsible for storing ownerGUID in the database, making it no longer possible for an ex-owner to instantly regain ownership by rejoining the channel as this is not blizzlike. The code is commented out in case someone wants to make a configurable option out of it. * Cleanup some other code in affected files.
    --HG--
    branch : trunk

--- sql/characters.sql
+++ sql/characters.sql
@@ -274,7 +274,6 @@ DROP TABLE IF EXISTS `channels`;
 CREATE TABLE `channels` (
   `m_name` text NOT NULL,
   `m_team` int(10) unsigned NOT NULL,
-  `m_ownerGUID` int(11) unsigned NOT NULL default '0',
   `m_announce` tinyint(1) unsigned NOT NULL default '0',
   `m_moderate` tinyint(1) unsigned NOT NULL default '0',
   `m_password` text,
---
+++ sql/updates/5159_characters_channels.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `channels` DROP `m_ownerGUID`;
