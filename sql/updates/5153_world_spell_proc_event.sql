5a9493f8732ae2a3890b59491c53aaba627e106f
Nevan <none@none>
2009-08-17 19:23:17 +0200

    *Fix Glyph of Rake
    --HG--
    branch : trunk

---
+++ sql/updates/5153_world_spell_proc_event.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_proc_event` WHERE `entry` = 54821;
+INSERT INTO `spell_proc_event` ( `entry` , `SpellFamilyName` , `SpellFamilyMask0` , `procFlags`) VALUES
+(54821, 7, 0x00001000, 0x00000010); -- Glyph of Rake
