242958f45b4df58a24015b4f3a41b0f24d2f79d7
Nevan <none@none>
2009-08-17 03:28:52 +0200

    *Fix Frozen Power Talent
    --HG--
    branch : trunk

---
+++ sql/updates/5126_world_spell_proc_event.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_proc_event` WHERE `entry`IN(63373);
+INSERT INTO `spell_proc_event` (`entry`,`SpellFamilyName`,`SpellFamilyMask0`,`procFlags`) VALUES
+(63373,11,0x80000000,0x00010000), -- Frozen Power (Rank 1)
+(63374,11,0x80000000,0x00010000); -- Freeze Power (Rank 2)
