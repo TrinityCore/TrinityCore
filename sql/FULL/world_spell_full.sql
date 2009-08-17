9a7efc112a98f426bf65a84644c887a4a468996e
QAston <none@none>
2009-08-17 03:02:58 +0200

    *Backout unintentional change in world_spell_full from prev rev.
    --HG--
    branch : trunk

--- sql/FULL/world_spell_full.sql
+++ sql/FULL/world_spell_full.sql
@@ -2007,8 +2007,8 @@ INSERT INTO `npc_spellclick_spells` (`npc_entry`, `spell_id`, `quest_start`, `qu
 (28606, 52263, 12680, 1, 12680, 1),
 (28607, 52263, 12680, 1, 12680, 1),
 (28782, 52280, 12687, 1, 12687, 1), -- Unbound Charger
-(28833, 52447, 12701, 1, 12701, 1), -- Scarlet Cannon Master
-(28887, 52447, 12701, 1, 12701, 1);
+(28833, 52447, 12701, 1, 12701, 1); -- Scarlet Cannon Master
+# (28887, 52447, 12701, 1, 12701, 1); -- dead cannon
 
 DELETE FROM `npc_spellclick_spells` WHERE `npc_entry` IN(31883, 31893, 31894, 31895, 31896, 31897);
 INSERT INTO `npc_spellclick_spells` (npc_entry, spell_id, quest_start, quest_start_active, quest_end, cast_flags, aura_required, aura_forbidden, user_type) VALUES
@@ -2022,6 +2022,7 @@ INSERT INTO `npc_spellclick_spells` (npc_entry, spell_id, quest_start, quest_sta
 -- --------
 -- NAXXARAMAS
 -- --------
+
 DELETE FROM `spell_linked_spell` WHERE `spell_trigger` IN
 (28732,54097,-29865,-55053,-28169,28059,39088,-28059,-39088,28062,39090,28084,
 39091,-28084,-39091,28085,39093);
@@ -2127,11 +2128,19 @@ INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comm
 ( 54643,-54643, 2, 'Wintergrasp Defender Teleport');
 
 
--- lightwell
-REPLACE INTO `npc_spellclick_spells` (npc_entry, spell_id, quest_start, quest_start_active, quest_end, cast_flags, quest_status) VALUES
-(31883, 60123, 0, 0, 0, 2, 0),
-(31893, 60123, 0, 0, 0, 2, 0),
-(31894, 60123, 0, 0, 0, 2, 0),
-(31895, 60123, 0, 0, 0, 2, 0),
-(31896, 60123, 0, 0, 0, 2, 0),
-(31897, 60123, 0, 0, 0, 2, 0);
+-- temp
+
+UPDATE `creature_template` SET `ScriptName`='boss_netherspite', `RegenHealth` = '1' WHERE `entry`='15689';
+UPDATE `creature_template` SET `faction_A` = '35', `faction_H` = '35', `modelid_A`='11686', `modelid_H`='11686', `unit_flags` = '33554496', `ScriptName` = '' WHERE `entry` IN ('17367','17368','17369');
+UPDATE `creature_template` SET `minlevel` = '70', `maxlevel` = '70',`flags_extra`= '2', `speed`= '0.0001', `faction_A` = '16', `faction_H` = '16', `ScriptName` = 'mob_eventai'  WHERE `entry` = '16697';
+DELETE FROM `creature_ai_scripts` WHERE `creature_id` = '16697';
+INSERT INTO `creature_ai_scripts`
+(`id`,`creature_id`,`event_type`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action1_type`,`action1_param1`,`action1_param2`,`action1_param3`,`comment`) VALUES
+('1669701', '16697',         '0',         '100',          '3',        '3000',        '3000',        '3000',        '3000',          '11',         '46264',             '0',             '2','Void Zone - Cast Void Zone Effect'),
+('1669702', '16697',         '1',         '100',          '3',        '3000',        '3000',        '3000',        '3000',          '11',         '46264',             '0',             '2','Void Zone - Cast Void Zone Effect');
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger` IN ('-30421','-30422','-30423','38637','38638','38639');
+INSERT INTO `spell_linked_spell`(`spell_trigger`,`spell_effect`,`type`,`comment`) VALUES
+('-30421','38637','0','Netherspite\'s Perseverence'),
+('-30422','38638','0','Netherspite\'s Serenity'),
+('-30423','38639','0','Netherspite\'s Dominance');
+UPDATE `script_texts` SET `type` = '3' WHERE `entry` IN ('-1532089','-1532090');
