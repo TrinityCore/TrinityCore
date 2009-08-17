43cec406f41958a7fd5a177949ba0fdefe08ddb2
megamage <none@none>
2009-08-17 13:09:42 -0500

    *Fix dk quest chain until "The Scarlet Apocalypse"
    --HG--
    branch : trunk

--- sql/FULL/world_spell_full.sql
+++ sql/FULL/world_spell_full.sql
@@ -2018,6 +2018,26 @@ INSERT INTO `npc_spellclick_spells` (npc_entry, spell_id, quest_start, quest_sta
 
 update item_template set spellppmRate_1 = 1 where entry = 39371; -- persuader
 
+DELETE FROM `creature_questrelation` WHERE `quest` = 12754; 
+INSERT INTO `creature_questrelation` (`id`, `quest`) VALUES (28914, 12754); 
+DELETE FROM `creature_involvedrelation` WHERE `quest` = 12754; 
+INSERT INTO `creature_involvedrelation` (`id`, `quest`) VALUES (28914, 12754); 
+DELETE FROM `creature_questrelation` WHERE `quest` = 12755; 
+INSERT INTO `creature_questrelation` (`id`, `quest`) VALUES (28914, 12755);
+DELETE FROM `creature_involvedrelation` WHERE `quest` = 12755; 
+INSERT INTO `creature_involvedrelation` (`id`, `quest`) VALUES (29077, 12755);
+DELETE FROM `creature_questrelation` WHERE `quest` = 12756; 
+INSERT INTO `creature_questrelation` (`id`, `quest`) VALUES (29077, 12756);
+DELETE FROM `creature_involvedrelation` WHERE `quest` = 12756; 
+INSERT INTO `creature_involvedrelation` (`id`, `quest`) VALUES (28914, 12756); 
+DELETE FROM `creature_questrelation` WHERE `quest` = 12757; 
+INSERT INTO `creature_questrelation` (`id`, `quest`) VALUES (28914, 12757);
+
+UPDATE `quest_template` SET `PrevQuestId`=12751 WHERE `entry`=12754;
+
+update spell_area set quest_end = 12756 where spell=53081;
+
+
 -- --------
 -- NAXXARAMAS
 -- --------
@@ -2063,6 +2083,8 @@ INSERT INTO `spell_script_target` (`entry`, `type`, `targetEntry`) VALUES
 (27929, 1, 16060), # To Anchor 2 - Gothik
 (27935, 1, 16060), # To Anchor 1 - Gothik
 (27936, 1, 16060); # To Anchor 2 - Gothik
+
+
 
 -- --------
 -- ULDUAR
