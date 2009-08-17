3482c12c5d48dd862b0ba830fe1747a1b59fd161
Machiavelli <none@none>
2009-08-17 02:03:23 +0200

    *Send correct gossip icons for unlearn talent, unlearn pet talent and purchase dual talent. Dual talent still needs some changes to gossip to make blizzlike.
    --HG--
    branch : trunk

--- sql/FULL/world_script_texts.sql
+++ sql/FULL/world_script_texts.sql
@@ -2031,4 +2031,4 @@ INSERT INTO `npc_text` (`ID`, `text0_0`, `text0_1`, `lang0`, `prob0`, `em0_0`, `
 -- Dual Spec `npc_option` change
 UPDATE `npc_option` SET `id`=52,`action`=19 WHERE `id`=51 AND `action`=18 AND `option_text`='UNIT_NPC_FLAG_OUTDOORPVP';
 DELETE FROM `npc_option` WHERE `id`=51 AND `action`=16 AND `option_text`='Purchase a Dual Talent Specialization.';
-INSERT INTO `npc_option` (`id`, `gossip_id`, `npcflag`, `icon`, `action`, `box_money`, `coded`, `option_text`, `box_text`) values('51','0','16','2','18','10000000','0','Purchase a Dual Talent Specialization.','Are you sure you wish to purchase a Dual Talent Specialization?');
+INSERT INTO `npc_option` (`id`, `gossip_id`, `npcflag`, `icon`, `action`, `box_money`, `coded`, `option_text`, `box_text`) values('51','0','16','0','18','10000000','0','Purchase a Dual Talent Specialization.','Are you sure you wish to purchase a Dual Talent Specialization?');
