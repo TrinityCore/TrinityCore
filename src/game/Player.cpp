8a5da9df9678058b8cff021fb140154c80cdba5b
megamage <none@none>
2009-08-19 16:10:25 -0500

    *Fix a crash caused by ~Player
    --HG--
    branch : trunk

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -488,10 +488,6 @@ Player::~Player ()
     // it must be unloaded already in PlayerLogout and accessed only for loggined player
     //m_social = NULL;
 
-    // Player may still set map - remove it to correctly unload instances
-    if (FindMap())
-        ResetMap();
-
     // Note: buy back item already deleted from DB when player was saved
     for(uint8 i = 0; i < PLAYER_SLOTS_COUNT; ++i)
     {
