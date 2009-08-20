978198d7afd240a9ee4b6f3f258292c6b428afe4
megamage <none@none>
2009-08-19 23:02:54 -0500

    *Dismiss summoned vehicle when it is dead.
    --HG--
    branch : trunk

--- src/game/Vehicle.cpp
+++ src/game/Vehicle.cpp
@@ -144,6 +144,9 @@ void Vehicle::Update(uint32 diff)
     //310
     if(getPowerType() == POWER_ENERGY) // m_vehicleInfo->36
         ModifyPower(POWER_ENERGY, 100);
+
+    if(m_deathState == DEAD && !GetDBTableGUIDLow())
+        Dismiss();
 }
 
 bool Vehicle::Create(uint32 guidlow, Map *map, uint32 phaseMask, uint32 Entry, uint32 vehicleId, uint32 team, float x, float y, float z, float o, const CreatureData * data)
