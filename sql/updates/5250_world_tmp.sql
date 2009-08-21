317f5946e5f754fe80d9cccc58e0c5f0d1542de2
Chaz Brown <iamparadox@netscape.net>
2009-08-21 02:35:37 -0400

    Fix typos in 5250 sql that might ruin DBs other than UDB, if you are using a DB other than UDB and used this SQL, you may need to reinstall your DB, UDB users will be fine.
    --HG--
    branch : trunk

--- sql/updates/5250_world_tmp.sql
+++ sql/updates/5250_world_tmp.sql
@@ -1,7 +1,8 @@
-UPDATE `creature` SET `id` = '29519' WHERE `guid` IN ('128557','128558','128559','128561','128563','128740','128742','128743','128744','128747','128748','128749');
-
--- Temporary fix for The Endless Hunger
--- When the final fix is made in core/DB the following will revert it
+UPDATE `creature` SET `id` = '29519' WHERE `id` IN ('29520','29565','29566','29567');
+						    
+-- Temporary fix for The Endless Hunger		    
+-- When the final fix is made in core/DB the following will revert it for UDB
+-- May not fix YTDB or other DBs.
 
 -- UPDATE `creature` SET `id` = '29519' WHERE `guid` IN ('128557','128558','128559');
 -- UPDATE `creature` SET `id` = '29520' WHERE `guid` IN ('128561','128563');
