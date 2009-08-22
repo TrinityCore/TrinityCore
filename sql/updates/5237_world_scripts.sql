aafeb0a71deb9b787345b88aa4d2ee3a089aa399
Anubisss <none@none>
2009-08-22 03:12:35 +0200

    *Add a ; after a SQL query. By Sundark.
    --HG--
    branch : trunk

--- sql/updates/5237_world_scripts.sql
+++ sql/updates/5237_world_scripts.sql
@@ -3,4 +3,4 @@ UPDATE `creature_template` SET `ScriptName` = 'boss_runemaster_molgeim' WHERE `e
 UPDATE `creature_template` SET `ScriptName` = 'boss_stormcaller_brundir' WHERE `entry` = '32857';
 UPDATE `creature_template` SET `ScriptName` = 'mob_lightning_elemental' WHERE `entry` = '32958';
 UPDATE `creature_template` SET `ScriptName` = 'mob_rune_of_summoning' WHERE `entry` = '33051';
-UPDATE `instance_template` SET `script` = 'instance_ulduar' WHERE `map` = '603'
+UPDATE `instance_template` SET `script` = 'instance_ulduar' WHERE `map` = '603';
