9b3544c6f4dbc3c0b60ecee95ec5514124b4a00d
megamage <none@none>
2009-08-24 20:18:16 -0500

    [8407] Extract from guild bank handler functions for 3 cases and move code to Guild class. Author: VladimirMangos
        This mostly just move code and caller updates to use it from new place.
        More code chnages possible later.
    
    --HG--
    branch : trunk

--- src/game/WorldSession.h
+++ src/game/WorldSession.h
@@ -686,6 +686,7 @@ class TRINITY_DLL_SPEC WorldSession
         void HandleGuildBankDepositMoney(WorldPacket& recv_data);
         void HandleGuildBankWithdrawMoney(WorldPacket& recv_data);
         void HandleGuildBankSwapItems(WorldPacket& recv_data);
+
         void HandleGuildBankUpdateTab(WorldPacket& recv_data);
         void HandleGuildBankBuyTab(WorldPacket& recv_data);
         void HandleQueryGuildBankTabText(WorldPacket& recv_data);
