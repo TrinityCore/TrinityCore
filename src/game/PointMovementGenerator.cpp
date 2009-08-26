2d587591af133e3debb876e7f5c49cc0f9880145
megamage <none@none>
2009-08-26 13:39:11 -0500

    *Remove some unused code in movement generator.
    --HG--
    branch : trunk

--- src/game/PointMovementGenerator.cpp
+++ src/game/PointMovementGenerator.cpp
@@ -33,9 +33,6 @@ void PointMovementGenerator<T>::Initialize(T &unit)
     Traveller<T> traveller(unit);
     // knockback effect has UNIT_STAT_JUMPING set,so if here we disable sentmonstermove there will be creature position sync problem between client and server
     i_destinationHolder.SetDestination(traveller,i_x,i_y,i_z, true /* !unit.hasUnitState(UNIT_STAT_JUMPING)*/);
-
-    if (unit.GetTypeId() == TYPEID_UNIT && ((Creature*)&unit)->canFly())
-        unit.AddUnitMovementFlag(MOVEMENTFLAG_FLYING);
 }
 
 template<class T>
