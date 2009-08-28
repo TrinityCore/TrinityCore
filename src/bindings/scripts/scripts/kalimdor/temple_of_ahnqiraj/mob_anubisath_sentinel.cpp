e972e73b9276f09c44e971b8b8b974dea82c1580
QAston <none@none>
2009-08-28 17:55:30 +0200

    *Prevent assertion fail in mob_anubisath_sentinel script.
    --HG--
    branch : trunk

--- src/bindings/scripts/scripts/kalimdor/temple_of_ahnqiraj/mob_anubisath_sentinel.cpp
+++ src/bindings/scripts/scripts/kalimdor/temple_of_ahnqiraj/mob_anubisath_sentinel.cpp
@@ -310,7 +310,7 @@ Unit* SentinelAbilityAura::GetTriggerTarget() const
 }
 
 SentinelAbilityAura::SentinelAbilityAura(aqsentinelAI *abilityOwner, SpellEntry *spell, uint32 ability, uint32 eff)
-: Aura(spell, eff, NULL, abilityOwner->m_creature, abilityOwner->m_creature, NULL)
+: Aura(spell, eff, abilityOwner->m_creature, abilityOwner->m_creature, abilityOwner->m_creature, NULL)
 {
     aOwner = abilityOwner;
     abilityId = ability;
