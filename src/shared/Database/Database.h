bcc99c927bf1fc91845024151572dfc989e6ac94
megamage <none@none>
2009-09-01 17:09:39 -0500

    [8442] Fixed displaying return button in mails Author: arrai
    --HG--
    branch : trunk

--- src/shared/Database/Database.h
+++ src/shared/Database/Database.h
@@ -131,6 +131,7 @@ class TRINITY_DLL_SPEC Database
         // sets the result queue of the current thread, be careful what thread you call this from
         void SetResultQueue(SqlResultQueue * queue);
 
+        bool CheckRequiredField(char const* table_name, char const* required_name);
     private:
         bool m_logSQL;
         std::string m_logsDir;
