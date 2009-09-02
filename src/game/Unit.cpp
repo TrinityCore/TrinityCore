c2278faa3c3b4ad24bc655c4221ccc2d6b66776c
megamage <none@none>
2009-09-02 15:15:34 -0500

    *Fix the bug that Elemental Invisibility causes the creatures to constantly evade. Thanks to XTElite1 and simon
    --HG--
    branch : trunk

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -10597,8 +10597,10 @@ bool Unit::canAttack(Unit const* target, bool force) const
     if(!target->isAttackableByAOE() || target->hasUnitState(UNIT_STAT_DIED))
         return false;
 
-    if((m_invisibilityMask || target->m_invisibilityMask) && !canDetectInvisibilityOf(target))
-        return false;
+    // shaman totem quests: spell 8898, shaman can detect elementals but elementals cannot see shaman
+    if(m_invisibilityMask || target->m_invisibilityMask)
+        if(!canDetectInvisibilityOf(target) && !target->canDetectInvisibilityOf(target))
+            return false;
 
     if(target->GetVisibility() == VISIBILITY_GROUP_STEALTH && !canDetectStealthOf(target, GetDistance(target)))
         return false;
