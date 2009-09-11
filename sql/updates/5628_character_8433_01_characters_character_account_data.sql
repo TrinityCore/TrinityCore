30b1fed0beb52e0f8a06b106cf6d15a27350418f
maximius <none@none>
2009-09-11 15:34:25 -0700

    *COmment out MaNGOS specific data
    --HG--
    branch : trunk

--- sql/updates/5628_character_8433_01_characters_character_account_data.sql
+++ sql/updates/5628_character_8433_01_characters_character_account_data.sql
@@ -1,4 +1,4 @@
-ALTER TABLE character_db_version CHANGE COLUMN required_8433_01_characters_character_account_data required_8469_01_characters_character_spell bit;
+-- ALTER TABLE character_db_version CHANGE COLUMN required_8433_01_characters_character_account_data required_8469_01_characters_character_spell bit;
 
 DELETE FROM character_spell WHERE spell in (
  1178,  /* Bear Form (Passive) */
