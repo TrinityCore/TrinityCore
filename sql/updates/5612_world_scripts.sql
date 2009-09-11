5bcabb86c78da08850a936bd8705ba6d155ff19c
maximius <none@none>
2009-09-11 01:20:01 -0700

    *template, not tempate! sorry if you got import errors :(
    --HG--
    branch : trunk

--- sql/updates/5612_world_scripts.sql
+++ sql/updates/5612_world_scripts.sql
@@ -1,2 +1,2 @@
 
-UPDATE `creature_tempate` SET `ScriptName`='npc_razael_and_lyana',`npcflag`=`npcflag`|1 WHERE `entry` IN (23778,23998);
+UPDATE `creature_template` SET `ScriptName`='npc_razael_and_lyana',`npcflag`=`npcflag`|1 WHERE `entry` IN (23778,23998);
