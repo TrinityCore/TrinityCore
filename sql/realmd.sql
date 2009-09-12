1f95bf28f5d835167a5f8bf4c2419b24cc00b8e3
Kudlaty <none@none>
2009-09-13 00:02:14 +0200

    Add support for multirealm. Patch by streetrat.
    --HG--
    branch : trunk

--- sql/realmd.sql
+++ sql/realmd.sql
@@ -182,6 +182,7 @@ CREATE TABLE `realmlist` (
   `timezone` tinyint(3) unsigned NOT NULL default '0',
   `allowedSecurityLevel` tinyint(3) unsigned NOT NULL default '0',
   `population` float unsigned NOT NULL default '0',
+  `gamebuild` int(11) unsigned NOT NULL default '9947',
   PRIMARY KEY  (`id`),
   UNIQUE KEY `idx_name` (`name`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Realm System';
--- src/trinityrealm/RealmList.cpp
+++ src/trinityrealm/RealmList.cpp
@@ -44,7 +44,7 @@ void RealmList::Initialize(uint32 updateInterval)
     UpdateRealms(true);
 }
 
-void RealmList::UpdateRealm( uint32 ID, const std::string& name, const std::string& address, uint32 port, uint8 icon, uint8 color, uint8 timezone, AccountTypes allowedSecurityLevel, float popu)
+void RealmList::UpdateRealm( uint32 ID, const std::string& name, const std::string& address, uint32 port, uint8 icon, uint8 color, uint8 timezone, AccountTypes allowedSecurityLevel, float popu, uint32 build)
 {
     ///- Create new if not exist or update existed
     Realm& realm = m_realms[name];
@@ -61,6 +61,7 @@ void RealmList::UpdateRealm( uint32 ID, const std::string& name, const std::stri
     std::ostringstream ss;
     ss << address << ":" << port;
     realm.address   = ss.str();
+    realm.gamebuild = build;
 }
 
 void RealmList::UpdateIfNeed()
@@ -82,7 +83,7 @@ void RealmList::UpdateRealms(bool init)
 {
     sLog.outDetail("Updating Realm List...");
 
-    QueryResult *result = loginDatabase.Query( "SELECT id, name, address, port, icon, color, timezone, allowedSecurityLevel, population FROM realmlist WHERE color <> 3 ORDER BY name" );
+    QueryResult *result = loginDatabase.Query( "SELECT id, name, address, port, icon, color, timezone, allowedSecurityLevel, population, gamebuild FROM realmlist WHERE color <> 3 ORDER BY name" );
 
     ///- Circle through results and add them to the realm map
     if(result)
@@ -93,7 +94,7 @@ void RealmList::UpdateRealms(bool init)
 
             uint8 allowedSecurityLevel = fields[7].GetUInt8();
 
-            UpdateRealm(fields[0].GetUInt32(), fields[1].GetCppString(),fields[2].GetCppString(),fields[3].GetUInt32(),fields[4].GetUInt8(), fields[5].GetUInt8(), fields[6].GetUInt8(), (allowedSecurityLevel <= SEC_ADMINISTRATOR ? AccountTypes(allowedSecurityLevel) : SEC_ADMINISTRATOR), fields[8].GetFloat() );
+            UpdateRealm(fields[0].GetUInt32(), fields[1].GetCppString(),fields[2].GetCppString(),fields[3].GetUInt32(),fields[4].GetUInt8(), fields[5].GetUInt8(), fields[6].GetUInt8(), (allowedSecurityLevel <= SEC_ADMINISTRATOR ? AccountTypes(allowedSecurityLevel) : SEC_ADMINISTRATOR), fields[8].GetFloat(), fields[9].GetUInt32() );
             if(init)
                 sLog.outString("Added realm \"%s\".", fields[1].GetString());
         } while( result->NextRow() );
