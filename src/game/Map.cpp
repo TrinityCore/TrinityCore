8c94b29b09acc4cd20ea646f974052f9d3061faa
maximius <none@none>
2009-09-13 00:03:54 -0700

    *Missed a Boss Emote Command change
    --HG--
    branch : trunk

--- src/game/Map.cpp
+++ src/game/Map.cpp
@@ -2874,7 +2874,7 @@ void Map::ScriptsProcess()
 
                 uint64 unit_target = target ? target->GetGUID() : 0;
 
-                //datalong 0=normal say, 1=whisper, 2=yell, 3=emote text
+                //datalong 0=normal say, 1=whisper, 2=yell, 3=emote text, 4=boss emote text
                 switch(step.script->datalong)
                 {
                     case 0:                                 // Say
@@ -2894,6 +2894,9 @@ void Map::ScriptsProcess()
                     case 3:                                 // Emote text
                         ((Creature *)source)->TextEmote(step.script->dataint, unit_target);
                         break;
+                    case 4:                                 // Boss Emote text
+                        ((Creature *)source)->MonsterTextEmote(step.script->dataint, unit_target, true);
+                         break;
                     default:
                         break;                              // must be already checked at load
                 }
