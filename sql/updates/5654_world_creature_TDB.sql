49b6e6b037fdeb79ee4504e699997d22affc081a
maximius <none@none>
2009-09-14 12:09:22 -0700

    *Stoneclaw Totem & Glyph of Stoneclaw Totem by Themris and thenecromancer
    --HG--
    branch : trunk

---
+++ sql/updates/5654_world_creature_TDB.sql
@@ -0,0 +1,11 @@
+
+UPDATE `creature_template` SET `spell2`=55328 WHERE `entry`=3579;
+UPDATE `creature_template` SET `spell2`=55329 WHERE `entry`=3911;
+UPDATE `creature_template` SET `spell2`=55330 WHERE `entry`=3912;
+UPDATE `creature_template` SET `spell2`=55332 WHERE `entry`=3913;
+UPDATE `creature_template` SET `spell2`=55333 WHERE `entry`=7398;
+UPDATE `creature_template` SET `spell2`=55335 WHERE `entry`=7399;
+UPDATE `creature_template` SET `spell2`=55278 WHERE `entry`=15478;
+UPDATE `creature_template` SET `spell2`=58589 WHERE `entry`=31120;
+UPDATE `creature_template` SET `spell2`=58590 WHERE `entry`=31121;
+UPDATE `creature_template` SET `spell2`=58591 WHERE `entry`=31122;
