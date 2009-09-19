599d0fa65b2f28e99c3a5f6eca2e3b67f0f2e900
maximius <none@none>
2009-09-19 15:14:05 -0700

    *quest: "Long Forgotten Memories" *quest: "A Pawn on the Eternal Board" *World Event: AQ-War Effort *quest: "Eranikus, Tyrant of the Dream" *Sunken temple -> "Snake Puzzle event" *duskwood boss "Twilight Corrupter" for quest: "The Nightmare's Corruption" *All by kb_z, please tell me if I missed anything, and somebody please review the SQL.. a LOT Of stuff in this needs to go to TDB!
    --HG--
    branch : trunk

---
+++ sql/updates/5700_characters.sql
@@ -0,0 +1,3 @@
+
+DELETE `game_event_condition_save` FROM `game_event_condition_save` WHERE `event_id` in (22,75,76,77);
+DELETE `game_event_save` FROM `game_event_save` WHERE `event_id` in (22,75,76,77);
---
+++ src/bindings/scripts/scripts/eastern_kingdoms/sunken_temple/def_sunken_temple.h
@@ -0,0 +1,22 @@
+/* Copyright (C) 2006 - 2009 kb_z
+ * This program is free software licensed under GPL version 2
+ * Please see the included DOCS/LICENSE.TXT for more information */
+
+#ifndef DEF_SUNKEN_TEMPLE_H
+#define DEF_SUNKEN_TEMPLE_H
+
+#define TROLLBOSS1_DEATH 1
+#define TROLLBOSS2_DEATH 2
+#define TROLLBOSS3_DEATH 3
+#define TROLLBOSS4_DEATH 4
+#define TROLLBOSS5_DEATH 5
+#define TROLLBOSS6_DEATH 6
+#define JAMMALAN_DEATH 7
+#define MORPHAZ_DEATH 8
+#define HAZZAS_DEATH 9
+#define ERANIKUS_DEATH 10
+#define ATALALARION_DEATH 11 //optional
+
+#define EVENT_STATE 1
+#endif
+
