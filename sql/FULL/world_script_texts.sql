dc5f98a93b331243addb5e4b68b37812ce300db6
maximius <none@none>
2009-09-19 12:13:08 -0700

    *Wintergrasp: Prevent defenders team to click the relic. Patch by Spp. *Zum'Rah Area Trigger Script, Zum'Rah should become hostile when approached. By totoro. *Judgement of Light PPM based, not 100%. By Drevi. *Fix Deflection Exploit. By manuel, thanks to TheNecromancer and Gyullo. *Correct Wintergrasp Tenacity formulas, by Gyullo. *A Spirit Guide Escort Quest, code from SD2, patch by manuel. *TrullyOne/MeanMachine Waypoint System Restored. Patch by XTElite1.
    --HG--
    branch : trunk

--- sql/FULL/world_script_texts.sql
+++ sql/FULL/world_script_texts.sql
@@ -395,6 +395,9 @@ INSERT INTO `script_texts` (`npc_entry`,`entry`,`content_default`,`content_loc1`
    (18210,-1000486,'Ride the lightning, filth!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'maghar captive SAY_MAG_LIGHTNING'),
    (18210,-1000487,'FROST SHOCK!!!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'maghar captive SAY_MAG_SHOCK'),
    (18210,-1000488,'It is best that we split up now, in case they send more after us. Hopefully one of us will make it back to Garrosh. Farewell stranger.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'maghar captive SAY_MAG_COMPLETE'),
+   (17077,-1000496,'%s lifts its head into the air, as if listening for something.',0,2,0,0,'ancestral wolf EMOTE_WOLF_LIFT_HEAD'),
+   (17077,-1000497,'%s lets out a howl that rings across the mountains to the north and motions for you to follow.',0,2,0,0,'ancestral wolf EMOTE_WOLF_HOWL'),
+   (17077,-1000498,'Welcome, kind spirit. What has brought you to us?',0,0,0,0,'ancestral wolf SAY_WOLF_WELCOME'),
 
 -- -1 033 000 SHADOWFANG KEEP
    (0,-1033000,'Follow me and I''ll open the courtyard door for you.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,7,1,'prisoner ashcrombe SAY_FREE_AS'),
