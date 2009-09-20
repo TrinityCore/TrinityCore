afb56c1f0366364573ff2e54053cbcdf40097aea
maximius <none@none>
2009-09-20 02:01:04 -0700

    *Fix some typos, by Darkshines *More Fix About NPC dont regenerate HP, by Gyullo
    --HG--
    branch : trunk

--- sql/FULL/world_scripts_full.sql
+++ sql/FULL/world_scripts_full.sql
@@ -1333,7 +1333,7 @@ insert into `event_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, 
 update creature set spawnMask=0 where guid = 85587;
 
 -- Quest - The Warsong Farms
-DELETE FROM `creature_ai_scripts` WHERE `creature_id`=25669;
+DELETE FROM `creature_ai_scripts` WHERE `creature_id` IN (25669,25671,25672);
 INSERT INTO `creature_ai_scripts`
 (`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `action2_type`, `action2_param1`, `action2_param2`, `action2_param3`, `action3_type`, `action3_param1`, `action3_param2`, `action3_param3`, `comment`)
 VALUES
