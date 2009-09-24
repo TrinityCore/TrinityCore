a62447ab515a56b80b294c1e826e3be53de90c30
maximius <none@none>
2009-09-23 19:50:02 -0700

    *Krik'thir the Gatewatcher partial script by Manuel and Gyullo *Ahn'Kahet: Fixes for Prince Taldaram & code Achievement by Tartalo
    --HG--
    branch : trunk

--- src/bindings/scripts/system/ScriptLoader.cpp
+++ src/bindings/scripts/system/ScriptLoader.cpp
@@ -262,6 +262,7 @@ extern void AddSC_winterspring();
 
 //northrend
 extern void AddSC_instance_ahnkahet();          //Azjol-Nerub Ahn'kahet
+extern void AddSC_boss_krik_thir();
 extern void AddSC_boss_taldaram();
 extern void AddSC_boss_elder_nadox();
 extern void AddSC_boss_volazj();
@@ -669,6 +670,7 @@ void AddScripts()
 
     //northrend
     AddSC_instance_ahnkahet();          //Azjol-Nerub Ahn'kahet
+    AddSC_boss_krik_thir();
     AddSC_boss_taldaram();
     AddSC_boss_elder_nadox();
     AddSC_boss_volazj();
