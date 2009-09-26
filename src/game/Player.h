52573b1623b195727b41855a061f595e0e3afd65
maximius <none@none>
2009-09-26 12:26:02 -0700

    *Extend and comment the Player::FallGround function, it can now cause damage if required.
    --HG--
    branch : trunk

--- src/game/Player.h
+++ src/game/Player.h
@@ -1748,7 +1748,7 @@ class MANGOS_DLL_SPEC Player : public Unit
         Corpse *GetCorpse() const;
         void SpawnCorpseBones();
         void CreateCorpse();
-        bool FallGround(bool noDeath = false);
+        bool FallGround(uint8 FallMode = 0);
         void KillPlayer();
         uint32 GetResurrectionSpellId();
         void ResurrectPlayer(float restore_percent, bool applySickness = false);
