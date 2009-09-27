342b071d476f71f9467c56c46ac046eb906f7205
maximius <none@none>
2009-09-26 21:55:47 -0700

    *Instead of checking getVictim() to determine whether or not to regen health, check if the threat list is empty *Fix some incorrect/missing scriptname assignments, thanks Drahy
    --HG--
    branch : trunk

--- sql/FULL/world_scripts_full.sql
+++ sql/FULL/world_scripts_full.sql
@@ -521,12 +521,14 @@ UPDATE `instance_template` SET `script`='instance_gundrak' WHERE `map`=604;
 UPDATE `creature_template` SET `ScriptName`='boss_moorabi' WHERE `entry`=29305;
 /* Slad'ran */
 UPDATE `creature_template` SET `ScriptName`='boss_slad_ran' WHERE `entry`=29304;
-UPDATE `creature_template` SET `ScriptName`='mob_slad_ran_viper' WHERE `entry`=29304;
+UPDATE `creature_template` SET `ScriptName`='mob_slad_ran_viper' WHERE `entry`=29680;
 UPDATE `creature_template` SET `ScriptName`='mob_slad_ran_constrictor' WHERE `entry`=29713;
 /* Gal'darah */
 UPDATE `creature_template` SET `ScriptName`='boss_gal_darah' WHERE `entry`=29306;
 /* Drakkari Colossus */
 UPDATE `creature_template` SET `ScriptName`='boss_drakkari_colossus' WHERE `entry`=29307;
+/* Eck the Ferocious */
+UPDATE `creature_template` SET `ScriptName`='boss_eck' WHERE `entry`=29932;
 
 UPDATE `gameobject_template` SET `ScriptName`='go_gundrak_altar' WHERE `entry`=192518;
 UPDATE `gameobject_template` SET `ScriptName`='go_gundrak_altar' WHERE `entry`=192519;
