13f51d7f1465ee5a26d56432b65101c521bf47fb
Anubisss <none@none>
2009-10-02 15:47:32 +0200

    *Fix typos(correct script_texts ids). *Add some delete query.
    --HG--
    branch : trunk

--- sql/updates/5696_world.sql
+++ sql/updates/5696_world.sql
@@ -14,7 +14,7 @@ UPDATE `creature_template` SET `ScriptName`='npc_ancestral_wolf' WHERE `entry`=1
 -- This should be sent to TDB
 -- INSERT INTO `gameobject` VALUES (NULL, 300070, 530, 1,1,-20.236, 3803.41, 93.1402, 5.24994, 0, 0, 0.493946, -0.869492, 25, 0, 1);
 
-DELETE FROM `script_texts` WHERE `entry`=17077;
+DELETE FROM `script_texts` WHERE `npc_entry`=17077;
 INSERT INTO `script_texts` (`npc_entry`,`entry`,`content_default`,`sound`,`type`,`language`,`emote`,`comment`) VALUES
    (17077,-1000496,'%s lifts its head into the air, as if listening for something.',0,2,0,0,'ancestral wolf EMOTE_WOLF_LIFT_HEAD'),
    (17077,-1000497,'%s lets out a howl that rings across the mountains to the north and motions for you to follow.',0,2,0,0,'ancestral wolf EMOTE_WOLF_HOWL'),
