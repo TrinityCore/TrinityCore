5fc5212fd5d857a27ae18cd381f08560eab30395
maximius <none@none>
2009-10-14 19:17:03 -0700

    *Fix a typo in 5895_world_scripts.sql
    --HG--
    branch : trunk

--- sql/FULL/world_scripts_full.sql
+++ sql/FULL/world_scripts_full.sql
@@ -516,9 +516,9 @@ UPDATE `gameobject_template` SET `ScriptName`='go_gilded_brazier' WHERE `entry`=
 UPDATE `creature_template` SET `ScriptName`='npc_orsonn_and_kodian' WHERE `entry` IN (27274,27275);
 
 /* DRAK'THARON KEEP */
-UPDATE `instance_template` SET `script`=`instance_drak_tharon` WHERE `map`=600;
-UPDATE `creature_template` SET `ScriptName`=`boss_novos` WHERE `entry`=26631;
-UPDATE `creature_template` SET `ScriptName`=`mob_crystal_handler` WHERE `entry`=26627;
+UPDATE `instance_template` SET `script`='instance_drak_tharon' WHERE `map`=600;
+UPDATE `creature_template` SET `ScriptName`='boss_novos' WHERE `entry`=26631;
+UPDATE `creature_template` SET `ScriptName`='mob_crystal_handler' WHERE `entry`=26627;
 
 /* GRUUL'S LAIR */
 UPDATE `instance_template` SET `script`='instance_gruuls_lair' WHERE `map`=565;
