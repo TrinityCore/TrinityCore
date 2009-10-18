379eef6eb1fbf61112619201295a05b961285ed0
tartalo <none@none>
2009-10-18 17:03:19 +0200

    Nexus, The Occulus: initial instance script                     code cleansing                     complete boss script skels a bit
    --HG--
    branch : trunk

--- src/bindings/scripts/scripts/northrend/nexus/oculus/boss_drakos.cpp
+++ src/bindings/scripts/scripts/northrend/nexus/oculus/boss_drakos.cpp
@@ -10,35 +10,52 @@ Script Data End */
 update creature_template set scriptname = '' where entry = '';
 *** SQL END ***/
 #include "precompiled.h"
+#include "def_oculus.h"
 
-//Spells
-#define SPELL_MAGIC_PULL                           51336
-#define SPELL_THUNDERING_STOMP                     50774
-#define SPELL_THUNDERING_STOMP_2                   59370
+enum Spells
+{
+    SPELL_MAGIC_PULL                           = 51336,
+    SPELL_THUNDERING_STOMP                     = 50774,
+    SPELL_THUNDERING_STOMP_2                   = 59370
+};
 
 //not in db
-//Yell
-#define SAY_AGGRO                               -1578000
-#define SAY_KILL_1                              -1578001
-#define SAY_KILL_2                              -1578002
-#define SAY_KILL_3                              -1578003
-#define SAY_DEATH                               -1578004
-#define SAY_PULL_1                              -1578005
-#define SAY_PULL_2                              -1578006
-#define SAY_PULL_3                              -1578007
-#define SAY_PULL_4                              -1578008
-#define SAY_STOMP_1                             -1578009
-#define SAY_STOMP_2                             -1578010
-#define SAY_STOMP_3                             -1578011
+enum Yells
+{
+    SAY_AGGRO                               = -1578000,
+    SAY_KILL_1                              = -1578001,
+    SAY_KILL_2                              = -1578002,
+    SAY_KILL_3                              = -1578003,
+    SAY_DEATH                               = -1578004,
+    SAY_PULL_1                              = -1578005,
+    SAY_PULL_2                              = -1578006,
+    SAY_PULL_3                              = -1578007,
+    SAY_PULL_4                              = -1578008,
+    SAY_STOMP_1                             = -1578009,
+    SAY_STOMP_2                             = -1578010,
+    SAY_STOMP_3                             = -1578011
+};
 
 struct TRINITY_DLL_DECL boss_drakosAI : public ScriptedAI
 {
-    boss_drakosAI(Creature *c) : ScriptedAI(c) {}
+    boss_drakosAI(Creature *c) : ScriptedAI(c)
+    {
+        pInstance = c->GetInstanceData();
+    }
+    
+    ScriptedInstance* pInstance;
 
-    void Reset() {}
+    void Reset()
+    {
+        if (pInstance)
+            pInstance->SetData(DATA_DRAKOS_EVENT, NOT_STARTED);
+    }
     void EnterCombat(Unit* who)
     {
         DoScriptText(SAY_AGGRO, m_creature);
+        
+        if (pInstance)
+            pInstance->SetData(DATA_DRAKOS_EVENT, IN_PROGRESS);
     }
     void AttackStart(Unit* who) {}
     void MoveInLineOfSight(Unit* who) {}
@@ -53,6 +70,9 @@ struct TRINITY_DLL_DECL boss_drakosAI : public ScriptedAI
     void JustDied(Unit* killer)
     {
         DoScriptText(SAY_DEATH, m_creature);
+        
+        if (pInstance)
+            pInstance->SetData(DATA_DRAKOS_EVENT, DONE);
     }
     void KilledUnit(Unit *victim)
     {
--- src/bindings/scripts/scripts/northrend/nexus/oculus/def_oculus.h
+++ src/bindings/scripts/scripts/northrend/nexus/oculus/def_oculus.h
@@ -1,4 +1,28 @@
 #ifndef DEF_OCULUS_H
 #define DEF_OCULUS_H
 
+enum Data
+{
+    DATA_DRAKOS_EVENT,
+    DATA_VAROS_EVENT,
+    DATA_UROM_EVENT,
+    DATA_EREGOS_EVENT
+};
+
+enum Data64
+{
+    DATA_DRAKOS,
+    DATA_VAROS,
+    DATA_UROM,
+    DATA_EREGOS
+};
+
+enum Bosses
+{
+    CREATURE_DRAKOS        = 27654,
+    CREATURE_VAROS         = 27447,
+    CREATURE_UROM          = 27655,
+    CREATURE_EREGOS        = 27656
+};
+
 #endif
