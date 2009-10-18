ee33c1a7adee700181a553b2e8910f73cac985cf
tartalo <none@none>
2009-10-18 18:49:23 +0200

    Add missing sql from e12e891212c8 patch
    --HG--
    branch : trunk

--- sql/FULL/world_scripts_full.sql
+++ sql/FULL/world_scripts_full.sql
@@ -787,6 +787,8 @@ UPDATE `creature_template` SET `ScriptName`='boss_grobbulus' WHERE `entry`=15931
 UPDATE `creature_template` SET `ScriptName`='npc_grobbulus_poison_cloud' WHERE `entry`=16363;
 UPDATE `creature_template` SET `ScriptName`='boss_gluth' WHERE `entry`=15932;
 UPDATE `creature_template` SET `ScriptName`='boss_thaddius' WHERE `entry`=15928;
+UPDATE `creature_template` SET `ScriptName`='mob_stalagg' WHERE `entry`=15929;
+UPDATE `creature_template` SET `ScriptName`='mob_feugen' WHERE `entry`=15930;
 UPDATE `creature_template` SET `ScriptName`='boss_stalagg' WHERE `entry`=15929;
 UPDATE `creature_template` SET `ScriptName`='boss_fugen' WHERE `entry`=15930;
 UPDATE `creature_template` SET `ScriptName`='boss_sapphiron' WHERE `entry`=15989;
