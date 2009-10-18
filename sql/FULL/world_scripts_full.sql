5dc0490fd9476e28106c1f570225895ce1e2d951
tartalo <none@none>
2009-10-18 03:01:13 +0200

    Utgarde Pinnacle: Boss Palehoof script completion, by Tommassino. Code cleanup on Utgarde Pinnacle scripts
    --HG--
    branch : trunk

--- sql/FULL/world_scripts_full.sql
+++ sql/FULL/world_scripts_full.sql
@@ -1186,8 +1186,9 @@ UPDATE `creature_template` SET `ScriptName`='boss_skadi' WHERE `entry`=26693;
 UPDATE `creature_template` SET `ScriptName`='boss_ymiron' WHERE `entry`=26861;
 UPDATE `creature_template` SET `ScriptName`='mob_frenzied_worgen' WHERE `entry`=26683;
 UPDATE `creature_template` SET `ScriptName`='mob_ravenous_furbolg' WHERE `entry`=26684;
-UPDATE `creature_template` SET `ScriptName`='mob_ferocious_rhino' WHERE `entry`=26685;
-UPDATE `creature_template` SET `ScriptName`='mob_massive_jormungar' WHERE `entry`=26686;
+UPDATE `creature_template` SET `ScriptName`='mob_massive_jormungar' WHERE `entry`=26685;
+UPDATE `creature_template` SET `ScriptName`='mob_ferocious_rhino' WHERE `entry`=26686;
+UPDATE `creature_template` SET `ScriptName`='mob_palehoof_orb' WHERE `entry`=26688;
 UPDATE `instance_template` SET `script`='instance_utgarde_pinnacle' WHERE `map`=575;
 UPDATE `gameobject_template` SET `ScriptName`='go_palehoof_sphere'WHERE `entry`=188593;
 
@@ -1205,7 +1206,7 @@ UPDATE `creature_template` SET `ScriptName`='boss_lavanthor' WHERE `entry`=29312
 UPDATE `creature_template` SET `ScriptName`='boss_ichoron' WHERE `entry`=29313;
 UPDATE `creature_template` SET `ScriptName`='boss_zuramat' WHERE `entry`=29314;
 UPDATE `creature_template` SET `ScriptName`='boss_erekem' WHERE `entry`=29315;
-UPDATE `creature_template` SET `ScriptName`='mob_erekem_guard' WHERE `entry`=29395;
+UPDATE `creature_template` SET `ScriptName`='mob_erekem_guard' WHERE `entry`=32226;
 UPDATE `creature_template` SET `ScriptName`='boss_moragg' WHERE `entry`=29316;
 UPDATE `creature_template` SET `ScriptName`='boss_xevozz' WHERE `entry`=29266;
 UPDATE `creature_template` SET `ScriptName`='boss_cyanigosa' WHERE `entry`=31134;
--- src/bindings/scripts/scripts/northrend/utgarde_keep/utgarde_pinnacle/instance_pinnacle.cpp
+++ src/bindings/scripts/scripts/northrend/utgarde_keep/utgarde_pinnacle/instance_pinnacle.cpp
@@ -31,6 +31,7 @@ struct TRINITY_DLL_DECL instance_pinnacle : public ScriptedInstance
     uint64 uiRavenousFurbolg;
     uint64 uiFerociousRhino;
     uint64 uiMassiveJormungar;
+    uint64 uiPalehoofOrb;
 
     uint64 uiSvala;
 
@@ -62,9 +63,10 @@ struct TRINITY_DLL_DECL instance_pinnacle : public ScriptedInstance
             case 26861:    uiKingYmiron = pCreature->GetGUID();                     break;
             case 26683:    uiFrenziedWorgen = pCreature->GetGUID();                 break;
             case 26684:    uiRavenousFurbolg = pCreature->GetGUID();                break;
-            case 26685:    uiFerociousRhino = pCreature->GetGUID();                 break;
-            case 26686:    uiMassiveJormungar = pCreature->GetGUID();               break;
+            case 26685:    uiMassiveJormungar = pCreature->GetGUID();               break;
+            case 26686:    uiFerociousRhino = pCreature->GetGUID();                 break;
             case 29281:    uiSvala = pCreature->GetGUID();                          break;
+            case 26688:    uiPalehoofOrb = pCreature->GetGUID();                    break;
         }
     }
 
@@ -141,6 +143,7 @@ struct TRINITY_DLL_DECL instance_pinnacle : public ScriptedInstance
             case DATA_MOB_RAVENOUS_FURBOLG:   return uiRavenousFurbolg;
             case DATA_MOB_MASSIVE_JORMUNGAR:  return uiMassiveJormungar;
             case DATA_MOB_FEROCIOUS_RHINO:    return uiFerociousRhino;
+            case DATA_MOB_ORB:                return uiPalehoofOrb;
             case DATA_SVALA:                  return uiSvala;
             case DATA_GORTOK_PALEHOOF_SPHERE: return uiGortokPalehoofSphere;
         }
