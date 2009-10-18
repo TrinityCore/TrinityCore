b92204da851c3f99321b61ac096eb7eebde7d83e
maximius <none@none>
2009-10-18 03:17:03 -0700

    *Cleanup
    --HG--
    branch : trunk

--- sql/updates/5966_world_scripts.sql
+++ sql/updates/5966_world_scripts.sql
@@ -1,3 +1,4 @@
+
 UPDATE `creature_template` SET `ScriptName`='mob_massive_jormungar' WHERE `entry`=26685;
 UPDATE `creature_template` SET `ScriptName`='mob_ferocious_rhino' WHERE `entry`=26686;
 UPDATE `creature_template` SET `ScriptName`='mob_palehoof_orb' WHERE `entry`=26688;
