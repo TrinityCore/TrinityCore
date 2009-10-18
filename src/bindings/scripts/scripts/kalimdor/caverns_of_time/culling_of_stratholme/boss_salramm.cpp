708d12c7e623c69353ffd70c11a9cff7ea5c5257
tartalo <none@none>
2009-10-18 14:45:57 +0200

    Culling of Stratholme: Initial instance script                        Code cleansing                        More complete boss script skels
    --HG--
    branch : trunk

--- src/bindings/scripts/scripts/kalimdor/caverns_of_time/culling_of_stratholme/boss_salramm.cpp
+++ src/bindings/scripts/scripts/kalimdor/caverns_of_time/culling_of_stratholme/boss_salramm.cpp
@@ -10,53 +10,71 @@ Script Data End */
 update creature_template set scriptname = 'boss_salramm' where entry = '';
 *** SQL END ***/
 #include "precompiled.h"
+#include "def_culling_of_stratholme.h"
 
-//Spells
-#define SPELL_CURSE_OF_TWISTED_FLESH                58845
-#define SPELL_EXPLODE_GHOUL_N                       52480
-#define SPELL_EXPLODE_GHOUL_H                       58825
-#define SPELL_SHADOW_BOLT_N                         57725
-#define SPELL_SHADOW_BOLT_H                         58828
-#define SPELL_STEAL_FLESH                           52708
-#define SPELL_SUMMON_GHOULS                         52451
+enum Spells
+{
+    SPELL_CURSE_OF_TWISTED_FLESH                = 58845,
+    SPELL_EXPLODE_GHOUL                         = 52480,
+    H_SPELL_EXPLODE_GHOUL                       = 58825,
+    SPELL_SHADOW_BOLT                           = 57725,
+    H_SPELL_SHADOW_BOLT                         = 58828,
+    SPELL_STEAL_FLESH                           = 52708,
+    SPELL_SUMMON_GHOULS                         = 52451
+};
 
 //not in db
-//Yell
-#define SAY_AGGRO                                -1595032
-#define SAY_SPAWN                                -1595033
-#define SAY_SLAY_1                               -1595034
-#define SAY_SLAY_2                               -1595035
-#define SAY_SLAY_3                               -1595036
-#define SAY_DEATH                                -1595037
-#define SAY_EXPLODE_GHOUL_1                      -1595038
-#define SAY_EXPLODE_GHOUL_2                      -1595039
-#define SAY_STEAL_FLESH_1                        -1595040
-#define SAY_STEAL_FLESH_2                        -1595041
-#define SAY_STEAL_FLESH_3                        -1595042
-#define SAY_SUMMON_GHOULS_1                      -1595043
-#define SAY_SUMMON_GHOULS_2                      -1595044
+enum Yells
+{
+    SAY_AGGRO                                = -1595032,
+    SAY_SPAWN                                = -1595033,
+    SAY_SLAY_1                               = -1595034,
+    SAY_SLAY_2                               = -1595035,
+    SAY_SLAY_3                               = -1595036,
+    SAY_DEATH                                = -1595037,
+    SAY_EXPLODE_GHOUL_1                      = -1595038,
+    SAY_EXPLODE_GHOUL_2                      = -1595039,
+    SAY_STEAL_FLESH_1                        = -1595040,
+    SAY_STEAL_FLESH_2                        = -1595041,
+    SAY_STEAL_FLESH_3                        = -1595042,
+    SAY_SUMMON_GHOULS_1                      = -1595043,
+    SAY_SUMMON_GHOULS_2                      = -1595044
+};
 
 struct TRINITY_DLL_DECL boss_salrammAI : public ScriptedAI
 {
-    boss_salrammAI(Creature *c) : ScriptedAI(c) {}
+    boss_salrammAI(Creature *c) : ScriptedAI(c)
+    {
+        pInstance = c->GetInstanceData();
+    }
 
-    uint32 Curse_flesh_Timer,
-           Explode_ghoul_Timer,
-           Shadow_bolt_Timer,
-           Steal_flesh_Timer,
-           Summon_ghouls_Timer;
+    uint32 Curse_flesh_Timer;
+    uint32 Explode_ghoul_Timer;
+    uint32 Shadow_bolt_Timer;
+    uint32 Steal_flesh_Timer;
+    uint32 Summon_ghouls_Timer;
+    
+    ScriptedInstance* pInstance;
 
     void Reset()
     {
-         Curse_flesh_Timer =   30000;  //30s DBM
-         Explode_ghoul_Timer = 25000 + rand()%3000; //approx 6 sec after summon ghouls
-         Shadow_bolt_Timer =   8000  + rand()%4000; // approx 10s
-         Steal_flesh_Timer =   12345;
-         Summon_ghouls_Timer = 19000 + rand()%5000; //on a video approx 24s after aggro
+         Curse_flesh_Timer = 30000;  //30s DBM
+         Explode_ghoul_Timer = urand(25000,28000); //approx 6 sec after summon ghouls
+         Shadow_bolt_Timer = urand(8000,12000); // approx 10s
+         Steal_flesh_Timer = 12345;
+         Summon_ghouls_Timer = urand(19000,24000); //on a video approx 24s after aggro
+         
+         if (pInstance)
+             pInstance->SetData(DATA_SALRAMM_EVENT, NOT_STARTED);
     }
 
     void EnterCombat(Unit* who)
-        {DoScriptText(SAY_AGGRO, m_creature);}
+    {
+        DoScriptText(SAY_AGGRO, m_creature);
+        
+        if (pInstance)
+             pInstance->SetData(DATA_SALRAMM_EVENT, IN_PROGRESS);
+    }
 
     void AttackStart(Unit* who) {}
     void MoveInLineOfSight(Unit* who) {}
@@ -65,8 +83,6 @@ struct TRINITY_DLL_DECL boss_salrammAI : public ScriptedAI
         //Return since we have no target
         if (!UpdateVictim())
             return;
-
-        Unit* random_target = SelectUnit(SELECT_TARGET_RANDOM, 0);
 
         //Curse of twisted flesh timer
         if (Curse_flesh_Timer < diff)
@@ -78,27 +94,16 @@ struct TRINITY_DLL_DECL boss_salrammAI : public ScriptedAI
         //Shadow bolt timer
         if (Shadow_bolt_Timer < diff)
         {
-            if (random_target)
-                DoCast(random_target,SPELL_SHADOW_BOLT_N);
-            Shadow_bolt_Timer = 8000 + rand()%4000;
+            if (Unit* random_target = SelectUnit(SELECT_TARGET_RANDOM, 0))
+                DoCast(random_target, HEROIC(SPELL_SHADOW_BOLT, H_SPELL_SHADOW_BOLT));
+            Shadow_bolt_Timer = urand(8000,12000);
         }else Shadow_bolt_Timer -= diff;
 
         //Steal Flesh timer
         if (Steal_flesh_Timer < diff)
         {
-            switch(rand()%3)
-            {
-                case 0:
-                    DoScriptText(SAY_STEAL_FLESH_1, m_creature);
-                    break;
-                case 1:
-                    DoScriptText(SAY_STEAL_FLESH_2, m_creature);
-                    break;
-                case 2:
-                    DoScriptText(SAY_STEAL_FLESH_3, m_creature);
-                    break;
-            }
-            if (random_target)
+            DoScriptText(RAND(SAY_STEAL_FLESH_1,SAY_STEAL_FLESH_2,SAY_STEAL_FLESH_3), m_creature);
+            if (Unit* random_target = SelectUnit(SELECT_TARGET_RANDOM, 0))
                 DoCast(random_target,SPELL_STEAL_FLESH);
             Steal_flesh_Timer = 10000;
         }else Steal_flesh_Timer -= diff;
@@ -106,16 +111,9 @@ struct TRINITY_DLL_DECL boss_salrammAI : public ScriptedAI
         //Summon ghouls timer
         if (Summon_ghouls_Timer < diff)
         {
+            DoScriptText(RAND(SAY_SUMMON_GHOULS_1,SAY_SUMMON_GHOULS_2), m_creature);
             switch(rand()%2)
-            {
-                case 0:
-                    DoScriptText(SAY_SUMMON_GHOULS_1, m_creature);
-                    break;
-                case 1:
-                    DoScriptText(SAY_SUMMON_GHOULS_2, m_creature);
-                    break;
-            }
-            if (random_target)
+            if (Unit* random_target = SelectUnit(SELECT_TARGET_RANDOM, 0))
                 DoCast(random_target,SPELL_SUMMON_GHOULS);
             Summon_ghouls_Timer = 10000;
         }else Summon_ghouls_Timer -= diff;
@@ -124,7 +122,12 @@ struct TRINITY_DLL_DECL boss_salrammAI : public ScriptedAI
     }
 
     void JustDied(Unit* killer)
-    {DoScriptText(SAY_DEATH, m_creature);}
+    {
+        DoScriptText(SAY_DEATH, m_creature);
+        
+        if (pInstance)
+            pInstance->SetData(DATA_SALRAMM_EVENT, DONE);
+    }
 
     void KilledUnit(Unit *victim)
     {
--- src/bindings/scripts/scripts/kalimdor/caverns_of_time/culling_of_stratholme/instance_culling_of_stratholme.cpp
+++ src/bindings/scripts/scripts/kalimdor/caverns_of_time/culling_of_stratholme/instance_culling_of_stratholme.cpp
@@ -1,9 +1,153 @@
 #include "precompiled.h"
 #include "def_culling_of_stratholme.h"
+#define MAX_ENCOUNTER 5
+/* Culling of Stratholme encounters:
+0 - Meathook
+1 - Salramm the Fleshcrafter
+2 - Chrono-Lord Epoch 
+3 - Mal'Ganis 
+4 - Infinite Corruptor (Heroic only)
+*/
 
 struct TRINITY_DLL_DECL instance_culling_of_stratholme : public ScriptedInstance
 {
     instance_culling_of_stratholme(Map* pMap) : ScriptedInstance(pMap) {Initialize();};
+    
+    uint64 uiMeathook;
+    uint64 uiSalramm;
+    uint64 uiEpoch;
+    uint64 uiMalGanis;
+    uint64 uiInfinite;
+    
+    uint8 m_auiEncounter[MAX_ENCOUNTER];
+    std::string str_data;
+    
+    bool IsEncounterInProgress() const
+    {
+        for (uint8 i = 0; i < MAX_ENCOUNTER; ++i)
+            if (m_auiEncounter[i] == IN_PROGRESS) return true;
+
+        return false;
+    }
+    
+    void OnCreatureCreate(Creature* pCreature, bool add)
+    {
+        switch(pCreature->GetEntry())
+        {
+            case CREATURE_MEATHOOK:
+                uiMeathook = pCreature->GetGUID();
+                break;
+            case CREATURE_SALRAMM:
+                uiSalramm = pCreature->GetGUID();
+                break;
+            case CREATURE_EPOCH:
+                uiEpoch = pCreature->GetGUID();
+                break;
+            case CREATURE_MAL_GANIS:
+                uiMalGanis = pCreature->GetGUID();
+                break;
+            case CREATURE_INFINITE:
+                uiInfinite = pCreature->GetGUID();
+                break;
+        }
+    }
+    
+    void SetData(uint32 type, uint32 data)
+    {
+        switch(type)
+        {
+            case DATA_MEATHOOK_EVENT:
+                m_auiEncounter[0] = data;
+                break;
+            case DATA_SALRAMM_EVENT:
+                m_auiEncounter[1] = data;
+                break;
+            case DATA_EPOCH_EVENT:
+                m_auiEncounter[2] = data;
+                break;
+            case DATA_MAL_GANIS_EVENT:
+                m_auiEncounter[3] = data;
+                break;
+            case DATA_INFINITE_EVENT:
+                m_auiEncounter[4] = data;
+                break;
+        }
+        
+        if (data == DONE)
+            SaveToDB();
+    }
+    
+    uint32 GetData(uint32 type)
+    {
+        switch(type)
+        {
+            case DATA_MEATHOOK_EVENT:             return m_auiEncounter[0];
+            case DATA_SALRAMM_EVENT:              return m_auiEncounter[1];
+            case DATA_EPOCH_EVENT:                return m_auiEncounter[2];
+            case DATA_MAL_GANIS_EVENT:            return m_auiEncounter[3];
+            case DATA_INFINITE_EVENT:             return m_auiEncounter[4];
+        }
+        return 0;
+    }
+    
+    uint64 GetData64(uint32 identifier)
+    {
+        switch(identifier)
+        {
+            case DATA_MEATHOOK:                   return uiMeathook;
+            case DATA_SALRAMM:                    return uiSalramm;
+            case DATA_EPOCH:                      return uiEpoch;
+            case DATA_INFINITE:                   return uiInfinite;
+        }
+        return 0;
+    }
+    
+    std::string GetSaveData()
+    {
+        OUT_SAVE_INST_DATA;
+
+        std::ostringstream saveStream;
+        saveStream << "C S " << m_auiEncounter[0] << " " << m_auiEncounter[1] << " "
+            << m_auiEncounter[2] << " " << m_auiEncounter[3] << " " << m_auiEncounter[4];
+
+        str_data = saveStream.str();
+
+        OUT_SAVE_INST_DATA_COMPLETE;
+        return str_data;
+    }
+
+    void Load(const char* in)
+    {
+        if (!in)
+        {
+            OUT_LOAD_INST_DATA_FAIL;
+            return;
+        }
+
+        OUT_LOAD_INST_DATA(in);
+
+        char dataHead1, dataHead2;
+        uint16 data0,data1,data2, data3, data4;
+
+        std::istringstream loadStream(in);
+        loadStream >> dataHead1 >> dataHead2 >> data0 >> data1 >> data2 >> data3 >> data4;
+
+        if (dataHead1 == 'C' && dataHead2 == 'S')
+        {
+            m_auiEncounter[0] = data0;
+            m_auiEncounter[1] = data1;
+            m_auiEncounter[2] = data2;
+            m_auiEncounter[3] = data3;
+            m_auiEncounter[4] = data4;
+
+            for (uint8 i = 0; i < MAX_ENCOUNTER; ++i)
+                if (m_auiEncounter[i] == IN_PROGRESS)
+                    m_auiEncounter[i] = NOT_STARTED;
+
+        }else OUT_LOAD_INST_DATA_FAIL;
+
+        OUT_LOAD_INST_DATA_COMPLETE;
+    }
 };
 
 InstanceData* GetInstanceData_instance_culling_of_stratholme(Map* pMap)
