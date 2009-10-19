502d727b990b6ff1ca5734720657e9b74cea4aea
Drahy <none@none>
2009-10-19 12:18:21 +0200

    * Fixed some Sqls including 6008_world.sql
    --HG--
    branch : trunk

--- sql/updates/5970_world_scripts.sql
+++ sql/updates/5970_world_scripts.sql
@@ -1,4 +1,3 @@
-
 UPDATE `creature_template` SET `ScriptName`='boss_amanitar' WHERE `entry`=30258;
 UPDATE `creature_template` SET `ScriptName`='mob_amanitar_mushrooms' WHERE `entry` IN (30435,30391);
 UPDATE `creature_template` SET `ScriptName`='boss_hadronox' WHERE `entry`=28921;
