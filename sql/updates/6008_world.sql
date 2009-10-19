8a4aa8fe0254baf96ef9530a5ea90a2bc791e1e4
Rat <none@none>
2009-10-19 10:22:45 +0200

    *Headless horseman -only summon once / instance -fix fly movement -proper invis modelids for dummies
    --HG--
    branch : trunk

---
+++ sql/updates/6008_world.sql
@@ -0,0 +1,6 @@
+UPDATE `creature_template` SET `modelid_A`='11686',`modelid_H`='11686' WHERE `entry`=23537;
+UPDATE `creature_template` SET `modelid_A`='11686',`modelid_H`='11686' WHERE `entry`=23686;
+UPDATE `creature_template` SET `modelid_A`='11686',`modelid_H`='11686' WHERE `entry`=23758;
+UPDATE `creature_template` SET `modelid_A`='11686',`modelid_H`='11686' WHERE `entry`=24034;
+UPDATE `creature_template` SET `modelid_A`='11686',`modelid_H`='11686' WHERE `entry`=23775;
+
