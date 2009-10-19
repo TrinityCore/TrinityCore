11d7df3723d0dd8779ae8842bda46d31fc4a7ac9
Rat <none@none>
2009-10-19 13:16:04 +0200

    *fix sql
    --HG--
    branch : trunk

--- sql/updates/6008_world.sql
+++ sql/updates/6008_world.sql
@@ -1,5 +1,5 @@
-UPDATE `creature_template` SET `modelid1`='11686',`modelid3`='11686' WHERE `entry`=23537;
-UPDATE `creature_template` SET `modelid1`='11686',`modelid3`='11686' WHERE `entry`=23686;
-UPDATE `creature_template` SET `modelid1`='11686',`modelid3`='11686' WHERE `entry`=23758;
-UPDATE `creature_template` SET `modelid1`='11686',`modelid3`='11686' WHERE `entry`=24034;
-UPDATE `creature_template` SET `modelid1`='11686',`modelid3`='11686' WHERE `entry`=23775;
+UPDATE `creature_template` SET `modelid1`='11686' WHERE `entry`=23537;
+UPDATE `creature_template` SET `modelid1`='11686' WHERE `entry`=23686;
+UPDATE `creature_template` SET `modelid1`='11686' WHERE `entry`=23758;
+UPDATE `creature_template` SET `modelid1`='11686' WHERE `entry`=24034;
+UPDATE `creature_template` SET `modelid1`='11686' WHERE `entry`=23775;
