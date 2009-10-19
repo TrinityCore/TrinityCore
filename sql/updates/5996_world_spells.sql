65bb756d90d268c7588ede67b7747f01a7bc0d4c
Drahy <none@none>
2009-10-19 12:18:21 +0200

    * Fixed some Sqls including 6008_world.sql
    --HG--
    branch : trunk

--- sql/updates/5996_world_spells.sql
+++ sql/updates/5996_world_spells.sql
@@ -1,4 +1,3 @@
-
 -- Fix spell_proc_event for spell Arcane Blast, remove on Arcane Barrage or Arcane Explosion
 DELETE FROM `spell_proc_event` WHERE `entry` = 36032;
 INSERT INTO `spell_proc_event` (entry, SchoolMask, SpellFamilyName, SpellFamilyMask0, SpellFamilyMask1, SpellFamilyMask2, procFlags, procEx, ppmRate, CustomChance, Cooldown) VALUES
--- sql/updates/5997_world_scripts.sql
+++ sql/updates/5997_world_scripts.sql
@@ -1,5 +1,3 @@
-
-UPDATE `creature_template` SET `ScriptName`='boss_jedoga_shadowseeker' WHERE `entry`='29310' LIMIT 1;
-UPDATE `creature_template` SET `ScriptName`='mob_jedoga_initiand' WHERE `entry`='30114' LIMIT 1;
-UPDATE `creature_template` SET `ScriptName`='npc_jedogas_aufseher_trigger' WHERE `entry`='30181' LIMIT 1;
-
+UPDATE `creature_template` SET `ScriptName`='boss_jedoga_shadowseeker' WHERE `entry`='29310';
+UPDATE `creature_template` SET `ScriptName`='mob_jedoga_initiand' WHERE `entry`='30114';
+UPDATE `creature_template` SET `ScriptName`='npc_jedogas_aufseher_trigger' WHERE `entry`='30181';
