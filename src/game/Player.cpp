921224ed421482c58bfbab56a39a43e78ef62049
maximius <none@none>
2009-10-28 12:40:24 -0700

    *Fix 'slain' achievements for heroic mode dungeons, by Azuritus
    --HG--
    branch : trunk

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -14079,7 +14079,7 @@ void Player::KilledMonster( CreatureInfo const* cInfo, uint64 guid )
 void Player::KilledMonsterCredit( uint32 entry, uint64 guid )
 {
     uint32 addkillcount = 1;
-    GetAchievementMgr().UpdateAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_KILL_CREATURE, entry, addkillcount);
+    GetAchievementMgr().UpdateAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_KILL_CREATURE, guid ? GetMap()->GetCreature(guid)->GetEntry() : entry, addkillcount);
     for (uint8 i = 0; i < MAX_QUEST_LOG_SIZE; ++i )
     {
         uint32 questid = GetQuestSlotQuestId(i);
