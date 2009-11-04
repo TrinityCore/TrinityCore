a6b9e5ac20f73170ec6d3f4a17f5e8fea1e1918a
Machiavelli <none@none>
2009-11-04 13:04:00 +0100

    Remove / modify a bunch of SQL files that update template fields that should only be altered by database projects, or that just were blatant hacks. These "fixes" should be posted on the database project's forums.
    --HG--
    branch : trunk

--- sql/updates/6176_world_scripts.sql
+++ sql/updates/6176_world_scripts.sql
@@ -1,4 +1,4 @@
-UPDATE `creature_template` SET `AIName` = 'AOEAI',`flags_extra` = '0',`minlevel` = '70',`maxlevel` = '70',`maxlevel` = '70',`faction_A` = '14',`faction_H` = '14' WHERE `entry` IN ('23336','23069','23259');
+UPDATE `creature_template` SET `AIName` = 'AOEAI' WHERE `entry` IN ('23336','23069','23259');
 UPDATE `creature_template` SET `spell1` = '40029' WHERE `entry` = 23069;
 UPDATE `creature_template` SET `spell1` = '40610' WHERE `entry` = 23259;
 UPDATE `creature_template` SET `spell1` = '40836' WHERE `entry` = 23336;
