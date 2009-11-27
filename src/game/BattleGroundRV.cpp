e2009376e83346a161407a3966802caf58277d3d
Kudlaty <none@none>
2009-11-27 02:03:50 +0100

    Apply #413 Implementation of Ring of Valor (Orgrimmar Arena) Patch by Spp and Opterman
    --HG--
    branch : trunk

--- src/game/BattleGroundRV.cpp
+++ src/game/BattleGroundRV.cpp
@@ -1,6 +1,8 @@
 /*
  * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
  *
+ * Copyright (C) 2008-2009 Trinity <http://www.trinitycore.org/>
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -8,21 +10,25 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
 #include "BattleGround.h"
 #include "BattleGroundRV.h"
+#include "ObjectAccessor.h"
 #include "Language.h"
 #include "Player.h"
+#include "WorldPacket.h"
+#include "GameObject.h"
 
 BattleGroundRV::BattleGroundRV()
 {
+    m_BgObjects.resize(BG_RV_OBJECT_MAX);
 
     m_StartDelayTimes[BG_STARTING_EVENT_FIRST]  = BG_START_DELAY_1M;
     m_StartDelayTimes[BG_STARTING_EVENT_SECOND] = BG_START_DELAY_30S;
@@ -43,6 +49,47 @@ BattleGroundRV::~BattleGroundRV()
 void BattleGroundRV::Update(uint32 diff)
 {
     BattleGround::Update(diff);
+
+    if (getTimer() < diff)
+    {
+        uint32 i;
+        switch(getState())
+        {
+            case BG_RV_STATE_OPEN_FENCES:
+            {
+                setTimer(BG_RV_PILAR_TO_FIRE_TIMER);
+                setState(BG_RV_STATE_CLOSE_FIRE);
+                break;
+            }
+            case BG_RV_STATE_CLOSE_FIRE:
+                for (i = BG_RV_OBJECT_FIRE_1; i <= BG_RV_OBJECT_FIREDOOR_2; ++i)
+                    DoorClose(i);
+                setTimer(BG_RV_FIRE_TO_PILAR_TIMER);
+                setState(BG_RV_STATE_OPEN_PILARS);
+                break;
+            case BG_RV_STATE_OPEN_PILARS:
+                for(i = BG_RV_OBJECT_PILAR_1; i <= BG_RV_OBJECT_PULLEY_2; ++i)
+                    DoorOpen(i);
+                setTimer(BG_RV_PILAR_TO_FIRE_TIMER);
+                setState(BG_RV_STATE_OPEN_FIRE);
+                break;
+            case BG_RV_STATE_OPEN_FIRE:
+                for (i = BG_RV_OBJECT_FIRE_1; i <= BG_RV_OBJECT_FIREDOOR_2; ++i)
+                    DoorOpen(i);
+                setTimer(BG_RV_FIRE_TO_PILAR_TIMER);
+                setState(BG_RV_STATE_CLOSE_PILARS);
+                break;
+            case BG_RV_STATE_CLOSE_PILARS:
+                uint32 i;
+                for(i = BG_RV_OBJECT_PILAR_1; i <= BG_RV_OBJECT_PULLEY_2; ++i)
+                    DoorOpen(i);
+                setTimer(BG_RV_PILAR_TO_FIRE_TIMER);
+                setState(BG_RV_STATE_CLOSE_FIRE);
+                break;
+        }
+    }
+    else
+        setTimer(getTimer() - diff);
 }
 
 void BattleGroundRV::StartingEventCloseDoors()
@@ -51,6 +98,18 @@ void BattleGroundRV::StartingEventCloseDoors()
 
 void BattleGroundRV::StartingEventOpenDoors()
 {
+    // Buff respawn
+    SpawnBGObject(BG_RV_OBJECT_BUFF_1, 90);
+    SpawnBGObject(BG_RV_OBJECT_BUFF_2, 90);
+    // Open fences
+    DoorOpen(BG_RV_OBJECT_FENCE_1);
+    DoorOpen(BG_RV_OBJECT_FENCE_2);
+    // Elevators
+    DoorOpen(BG_RV_OBJECT_ELEVATOR_1);
+    DoorOpen(BG_RV_OBJECT_ELEVATOR_2);
+
+    setState(BG_RV_STATE_OPEN_FENCES);
+    setTimer(BG_RV_FIRST_TIMER);
 }
 
 void BattleGroundRV::AddPlayer(Player *plr)
@@ -60,22 +119,116 @@ void BattleGroundRV::AddPlayer(Player *plr)
     BattleGroundRVScore* sc = new BattleGroundRVScore;
 
     m_PlayerScores[plr->GetGUID()] = sc;
+
+    UpdateWorldState(BG_RV_WORLD_STATE_A, GetAlivePlayersCountByTeam(ALLIANCE));
+    UpdateWorldState(BG_RV_WORLD_STATE_H, GetAlivePlayersCountByTeam(HORDE));
 }
 
 void BattleGroundRV::RemovePlayer(Player * /*plr*/, uint64 /*guid*/)
 {
+    if (GetStatus() == STATUS_WAIT_LEAVE)
+        return;
+
+    UpdateWorldState(BG_RV_WORLD_STATE_A, GetAlivePlayersCountByTeam(ALLIANCE));
+    UpdateWorldState(BG_RV_WORLD_STATE_H, GetAlivePlayersCountByTeam(HORDE));
+
+    CheckArenaWinConditions();
 }
 
-void BattleGroundRV::HandleKillPlayer(Player* player, Player* killer)
+void BattleGroundRV::HandleKillPlayer(Player *player, Player *killer)
 {
+    if (GetStatus() != STATUS_IN_PROGRESS)
+        return;
+
+    if (!killer)
+    {
+        sLog.outError("BattleGroundRV: Killer player not found");
+        return;
+    }
+
     BattleGround::HandleKillPlayer(player, killer);
+
+    UpdateWorldState(BG_RV_WORLD_STATE_A, GetAlivePlayersCountByTeam(ALLIANCE));
+    UpdateWorldState(BG_RV_WORLD_STATE_H, GetAlivePlayersCountByTeam(HORDE));
+
+    CheckArenaWinConditions();
 }
 
-void BattleGroundRV::HandleAreaTrigger(Player * /*Source*/, uint32 /*Trigger*/)
+bool BattleGroundRV::HandlePlayerUnderMap(Player *player)
 {
+    player->TeleportTo(GetMapId(), 763.5, -284, 28.276, 2.422, false);
+    return true;
+}
+
+
+void BattleGroundRV::HandleAreaTrigger(Player *Source, uint32 Trigger)
+{
+    if (GetStatus() != STATUS_IN_PROGRESS)
+        return;
+
+    switch(Trigger)
+    {
+        case 5224:
+        case 5226:
+            break;
+        default:
+            sLog.outError("WARNING: Unhandled AreaTrigger in Battleground: %u", Trigger);
+            Source->GetSession()->SendAreaTriggerMessage("Warning: Unhandled AreaTrigger in Battleground: %u", Trigger);
+            break;
+    }
+}
+
+void BattleGroundRV::FillInitialWorldStates(WorldPacket &data)
+{
+    data << uint32(BG_RV_WORLD_STATE_A) << uint32(GetAlivePlayersCountByTeam(ALLIANCE));
+    data << uint32(BG_RV_WORLD_STATE_H) << uint32(GetAlivePlayersCountByTeam(HORDE));
+    data << uint32(BG_RV_WORLD_STATE) << uint32(1);
+}
+
+void BattleGroundRV::Reset()
+{
+    //call parent's class reset
+    BattleGround::Reset();
 }
 
 bool BattleGroundRV::SetupBattleGround()
 {
+    // Fence
+    if (   !AddObject(BG_RV_OBJECT_FENCE_1, BG_RV_OBJECT_TYPE_FENCE_1, 763.432373, -274.058197, 28.276695, 3.141593, 0, 0, 0, RESPAWN_IMMEDIATELY)
+        || !AddObject(BG_RV_OBJECT_FENCE_2, BG_RV_OBJECT_TYPE_FENCE_2, 763.432373, -294.419464, 28.276684, 3.141593, 0, 0, 0, RESPAWN_IMMEDIATELY)
+    // elevators
+        || !AddObject(BG_RV_OBJECT_ELEVATOR_1, BG_RV_OBJECT_TYPE_ELEVATOR_1, 763.536377, -294.535767, 0.505383, 3.141593, 0, 0, 0, RESPAWN_IMMEDIATELY)
+        || !AddObject(BG_RV_OBJECT_ELEVATOR_2, BG_RV_OBJECT_TYPE_ELEVATOR_2, 763.506348, -273.873352, 0.505383, 0.000000, 0, 0, 0, RESPAWN_IMMEDIATELY)
+    // buffs
+        || !AddObject(BG_RV_OBJECT_BUFF_1, BG_RV_OBJECT_TYPE_BUFF_1, 735.551819, -284.794678, 28.276682, 0.034906, 0, 0, 0, RESPAWN_IMMEDIATELY)
+        || !AddObject(BG_RV_OBJECT_BUFF_2, BG_RV_OBJECT_TYPE_BUFF_2, 791.224487, -284.794464, 28.276682, 2.600535, 0, 0, 0, RESPAWN_IMMEDIATELY)
+    // fire
+        || !AddObject(BG_RV_OBJECT_FIRE_1, BG_RV_OBJECT_TYPE_FIRE_1, 743.543457, -283.799469, 28.286655, 3.141593, 0, 0, 0, RESPAWN_IMMEDIATELY)
+        || !AddObject(BG_RV_OBJECT_FIRE_2, BG_RV_OBJECT_TYPE_FIRE_2, 782.971802, -283.799469, 28.286655, 3.141593, 0, 0, 0, RESPAWN_IMMEDIATELY)
+        || !AddObject(BG_RV_OBJECT_FIREDOOR_1, BG_RV_OBJECT_TYPE_FIREDOOR_1, 743.711060, -284.099609, 27.542587, 3.141593, 0, 0, 0, RESPAWN_IMMEDIATELY)
+        || !AddObject(BG_RV_OBJECT_FIREDOOR_2, BG_RV_OBJECT_TYPE_FIREDOOR_2, 783.221252, -284.133362, 27.535686, 0.000000, 0, 0, 0, RESPAWN_IMMEDIATELY)
+    // Gear
+        || !AddObject(BG_RV_OBJECT_GEAR_1, BG_RV_OBJECT_TYPE_GEAR_1, 763.664551, -261.872986, 26.686588, 0.000000, 0, 0, 0, RESPAWN_IMMEDIATELY)
+        || !AddObject(BG_RV_OBJECT_GEAR_2, BG_RV_OBJECT_TYPE_GEAR_2, 763.578979, -306.146149, 26.665222, 3.141593, 0, 0, 0, RESPAWN_IMMEDIATELY)
+    // Pulley
+        || !AddObject(BG_RV_OBJECT_PULLEY_1, BG_RV_OBJECT_TYPE_PULLEY_1, 700.722290, -283.990662, 39.517582, 3.141593, 0, 0, 0, RESPAWN_IMMEDIATELY)
+        || !AddObject(BG_RV_OBJECT_PULLEY_2, BG_RV_OBJECT_TYPE_PULLEY_2, 826.303833, -283.996429, 39.517582, 0.000000, 0, 0, 0, RESPAWN_IMMEDIATELY)
+    // Pilars
+        || !AddObject(BG_RV_OBJECT_PILAR_1, BG_RV_OBJECT_TYPE_PILAR_1, 763.632385, -306.162384, 25.909504, 3.141593, 0, 0, 0, RESPAWN_IMMEDIATELY)
+        || !AddObject(BG_RV_OBJECT_PILAR_2, BG_RV_OBJECT_TYPE_PILAR_2, 723.644287, -284.493256, 24.648525, 3.141593, 0, 0, 0, RESPAWN_IMMEDIATELY)
+        || !AddObject(BG_RV_OBJECT_PILAR_3, BG_RV_OBJECT_TYPE_PILAR_3, 763.611145, -261.856750, 25.909504, 0.000000, 0, 0, 0, RESPAWN_IMMEDIATELY)
+        || !AddObject(BG_RV_OBJECT_PILAR_4, BG_RV_OBJECT_TYPE_PILAR_4, 802.211609, -284.493256, 24.648525, 0.000000, 0, 0, 0, RESPAWN_IMMEDIATELY)
+/*
+    // Pilars Collision - Fixme: Use the collision pilars - should make u break LoS
+        || !AddObject(BG_RV_OBJECT_PILAR_COLLISION_1, BG_RV_OBJECT_TYPE_PILAR_COLLISION_1, 763.632385, -306.162384, 30.639660, 3.141593, 0, 0, 0, RESPAWN_IMMEDIATELY)
+        || !AddObject(BG_RV_OBJECT_PILAR_COLLISION_2, BG_RV_OBJECT_TYPE_PILAR_COLLISION_2, 723.644287, -284.493256, 32.382710, 0.000000, 0, 0, 0, RESPAWN_IMMEDIATELY)
+        || !AddObject(BG_RV_OBJECT_PILAR_COLLISION_3, BG_RV_OBJECT_TYPE_PILAR_COLLISION_3, 763.611145, -261.856750, 30.639660, 0.000000, 0, 0, 0, RESPAWN_IMMEDIATELY)
+        || !AddObject(BG_RV_OBJECT_PILAR_COLLISION_4, BG_RV_OBJECT_TYPE_PILAR_COLLISION_4, 802.211609, -284.493256, 32.382710, 3.141593, 0, 0, 0, RESPAWN_IMMEDIATELY)
+*/
+        )
+    {
+        sLog.outErrorDb("BatteGroundRV: Failed to spawn some object!");
+        return false;
+    }
     return true;
 }
--- src/game/BattleGroundRV.h
+++ src/game/BattleGroundRV.h
@@ -1,6 +1,8 @@
 /*
  * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
  *
+ * Copyright (C) 2008-2009 Trinity <http://www.trinitycore.org/>
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -8,24 +10,98 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 #ifndef __BATTLEGROUNDRV_H
 #define __BATTLEGROUNDRV_H
 
 class BattleGround;
 
+enum BattleGroundRVObjectTypes
+{
+    BG_RV_OBJECT_BUFF_1,
+    BG_RV_OBJECT_BUFF_2,
+    BG_RV_OBJECT_FIRE_1,
+    BG_RV_OBJECT_FIRE_2,
+    BG_RV_OBJECT_FIREDOOR_1,
+    BG_RV_OBJECT_FIREDOOR_2,
+
+    BG_RV_OBJECT_PILAR_1,
+    BG_RV_OBJECT_PILAR_3,
+    BG_RV_OBJECT_GEAR_1,
+    BG_RV_OBJECT_GEAR_2,
+
+    BG_RV_OBJECT_PILAR_2,
+    BG_RV_OBJECT_PILAR_4,
+    BG_RV_OBJECT_PULLEY_1,
+    BG_RV_OBJECT_PULLEY_2,
+/*
+    BG_RV_OBJECT_PILAR_COLLISION_1,
+    BG_RV_OBJECT_PILAR_COLLISION_2,
+    BG_RV_OBJECT_PILAR_COLLISION_3,
+    BG_RV_OBJECT_PILAR_COLLISION_4,
+*/
+    BG_RV_OBJECT_ELEVATOR_1,
+    BG_RV_OBJECT_ELEVATOR_2,
+    BG_RV_OBJECT_FENCE_1,
+    BG_RV_OBJECT_FENCE_2,
+    BG_RV_OBJECT_MAX,
+};
+
+enum BattleGroundRVObjects
+{
+    BG_RV_OBJECT_TYPE_BUFF_1                     = 184663,
+    BG_RV_OBJECT_TYPE_BUFF_2                     = 184664,
+    BG_RV_OBJECT_TYPE_FIRE_1                     = 192704,
+    BG_RV_OBJECT_TYPE_FIRE_2                     = 192705,
+
+    BG_RV_OBJECT_TYPE_FIREDOOR_2                 = 192387,
+    BG_RV_OBJECT_TYPE_FIREDOOR_1                 = 192388,
+    BG_RV_OBJECT_TYPE_PULLEY_1                   = 192389,
+    BG_RV_OBJECT_TYPE_PULLEY_2                   = 192390,
+    BG_RV_OBJECT_TYPE_FENCE_1                    = 192391,
+    BG_RV_OBJECT_TYPE_FENCE_2                    = 192392,
+    BG_RV_OBJECT_TYPE_GEAR_1                     = 192393,
+    BG_RV_OBJECT_TYPE_GEAR_2                     = 192394,
+    BG_RV_OBJECT_TYPE_ELEVATOR_1                 = 194582,
+    BG_RV_OBJECT_TYPE_ELEVATOR_2                 = 194586,
+/*
+    BG_RV_OBJECT_TYPE_PILAR_COLLISION_1          = 194580, // axe
+    BG_RV_OBJECT_TYPE_PILAR_COLLISION_2          = 194579, // arena
+    BG_RV_OBJECT_TYPE_PILAR_COLLISION_3          = 194581, // lightning
+    BG_RV_OBJECT_TYPE_PILAR_COLLISION_4          = 194578, // ivory
+*/
+    BG_RV_OBJECT_TYPE_PILAR_1                    = 194583, // axe
+    BG_RV_OBJECT_TYPE_PILAR_2                    = 194584, // arena
+    BG_RV_OBJECT_TYPE_PILAR_3                    = 194585, // lightning
+    BG_RV_OBJECT_TYPE_PILAR_4                    = 194587, // ivory
+};
+
+enum BattleGroundRVData
+{
+    BG_RV_STATE_OPEN_FENCES,
+    BG_RV_STATE_OPEN_PILARS,
+    BG_RV_STATE_CLOSE_PILARS,
+    BG_RV_STATE_OPEN_FIRE,
+    BG_RV_STATE_CLOSE_FIRE,
+    BG_RV_FIRE_TO_PILAR_TIMER                    = 20000,
+    BG_RV_PILAR_TO_FIRE_TIMER                    =  5000,
+    BG_RV_FIRST_TIMER                            = 20133,
+    BG_RV_WORLD_STATE_A                          = 0xe10,
+    BG_RV_WORLD_STATE_H                          = 0xe11,
+    BG_RV_WORLD_STATE                            = 0xe1a,
+};
+
 class BattleGroundRVScore : public BattleGroundScore
 {
     public:
         BattleGroundRVScore() {};
         virtual ~BattleGroundRVScore() {};
-        //TODO fix me
 };
 
 class BattleGroundRV : public BattleGround
@@ -41,10 +117,24 @@ class BattleGroundRV : public BattleGround
         virtual void AddPlayer(Player *plr);
         virtual void StartingEventCloseDoors();
         virtual void StartingEventOpenDoors();
+        virtual void Reset();
+        virtual void FillInitialWorldStates(WorldPacket &d);
 
         void RemovePlayer(Player *plr, uint64 guid);
         void HandleAreaTrigger(Player *Source, uint32 Trigger);
         bool SetupBattleGround();
         void HandleKillPlayer(Player* player, Player *killer);
+        bool HandlePlayerUnderMap(Player * plr);
+
+    private:
+        uint32 Timer;
+        uint32 State;
+
+    protected:
+        uint32 getTimer() { return Timer; };
+        void setTimer(uint32 timer) { Timer = timer; };
+
+        uint32 getState() { return State; };
+        void setState(uint32 state) { State = state; };
 };
 #endif
