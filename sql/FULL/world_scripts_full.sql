e38c5007cc7894441c146d3f3d99b05bee6db062
Kudlaty <none@none>
2009-11-27 04:47:47 +0100

    Apply #493. This add support for quest Adbuction. Patch by maanuel
    --HG--
    branch : trunk

--- sql/FULL/world_scripts_full.sql
+++ sql/FULL/world_scripts_full.sql
@@ -382,7 +382,7 @@ UPDATE `creature_template` SET `ScriptName`='npc_leryssa' WHERE `entry`=25251;
 UPDATE `creature_template` SET `ScriptName`='npc_thassarian' WHERE `entry`=26170;
 UPDATE `creature_template` SET `ScriptName`='npc_image_lich_king' WHERE `entry`=26203;
 UPDATE `creature_template` SET `ScriptName`='npc_counselor_talbot' WHERE `entry`=25301;
-
+UPDATE `creature_template` SET `ScriptName`='npc_beryl_sorcerer' WHERE `entry`=25316;
 
 
 /* BURNING STEPPES */
