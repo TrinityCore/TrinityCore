e83f90bfe34c3c7c5d46b4238eb189368d2c9fac
teacher <none@none>
2009-12-05 18:57:48 +0100

    And I should do like raczman, drink lots of coffee before committing. Thank you Xeptor for spotting silly SQL typo in 6397_world.sql
    --HG--
    branch : trunk

--- sql/updates/6397_world.sql
+++ sql/updates/6397_world.sql
@@ -1,4 +1,4 @@
-DELETE FROM `spell_script_target` WHERE `entry`=45364 AND `type`=1 AND `targetEntry`=25478
+DELETE FROM `spell_script_target` WHERE `entry`=45364 AND `type`=1 AND `targetEntry`=25478;
 DELETE FROM `spell_script_target` WHERE `entry`=45634;
 INSERT INTO `spell_script_target` (`entry`, `type`, `targetEntry`) VALUES
 (45634,1,25478);
