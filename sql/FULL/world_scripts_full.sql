7504175e24e3acb20e774c20df050556b0e5f528
maanuel <none@none>
2009-12-07 00:37:17 -0300

    Script for boss Xevozz from Violet Hold. Original author ckegg.
    --HG--
    branch : trunk

--- sql/FULL/world_scripts_full.sql
+++ sql/FULL/world_scripts_full.sql
@@ -1287,6 +1287,7 @@ UPDATE `creature_template` SET `ScriptName`='boss_erekem' WHERE `entry`=29315;
 UPDATE `creature_template` SET `ScriptName`='mob_erekem_guard' WHERE `entry`=32226;
 UPDATE `creature_template` SET `ScriptName`='boss_moragg' WHERE `entry`=29316;
 UPDATE `creature_template` SET `ScriptName`='boss_xevozz' WHERE `entry`=29266;
+UPDATE `creature_template` SET `ScriptName`='mob_ethereal_sphere' WHERE `entry`=29271;
 UPDATE `creature_template` SET `ScriptName`='boss_cyanigosa' WHERE `entry`=31134;
 UPDATE `instance_template` SET `script`='instance_violet_hold' WHERE `map`=608;
 
