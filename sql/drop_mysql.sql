2abcdcd7f8687782c8dfcaa5a35b98d6961395e1
spp <none@none>
2009-12-11 02:29:58 +0100

    Fix drop_mysql user drop query, by Anonymous contributor. Closes #5
    --HG--
    branch : trunk

--- sql/drop_mysql.sql
+++ sql/drop_mysql.sql
@@ -12,10 +12,10 @@ REVOKE ALL PRIVILEGES ON `realmd` . * FROM 'trinity'@'localhost';
 
 REVOKE GRANT OPTION ON `realmd` . * FROM 'trinity'@'localhost';
 
-DELETE FROM `user` WHERE CONVERT( User USING utf8 ) = CONVERT( 'trinity' USING utf8 ) AND CONVERT( Host USING utf8 ) = CONVERT( 'localhost' USING utf8 ) ;
+DROP USER 'trinity'@'localhost';
 
-DROP DATABASE IF EXISTS `world` ;
+DROP DATABASE IF EXISTS `world`;
 
-DROP DATABASE IF EXISTS `characters` ;
+DROP DATABASE IF EXISTS `characters`;
 
-DROP DATABASE IF EXISTS `realmd` ;
+DROP DATABASE IF EXISTS `realmd`;
