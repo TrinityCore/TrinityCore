f112e102c9af7d2256ade00ca43344544d43e96e
Brian <runningnak3d@gmail.com>
2009-12-11 13:35:17 -0700

    * This stuff does not belong in the core repo
    --HG--
    branch : trunk

--- sql/tools/characters_pet_data_cleanup.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM pet_aura           WHERE guid NOT IN (SELECT id FROM character_pet);
-DELETE FROM pet_spell          WHERE guid NOT IN (SELECT id FROM character_pet);
-DELETE FROM pet_spell_cooldown WHERE guid NOT IN (SELECT id FROM character_pet);
