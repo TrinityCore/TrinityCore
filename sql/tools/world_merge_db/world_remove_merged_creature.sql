c093a9735eeaac6554c2fa19e26b8cd7f73783b6
Brian <runningnak3d@gmail.com>
2009-12-11 13:35:17 -0700

    * This stuff does not belong in the core repo
    --HG--
    branch : trunk

--- sql/tools/world_merge_db/world_remove_merged_creature.sql
+++
@@ -1,8 +0,0 @@
-DELETE FROM creature WHERE guid IN (SELECT guid FROM merge_creature);
-DROP TABLE IF EXISTS merge_creature;
-
-DELETE FROM creature_model_info WHERE modelid IN (SELECT modelid FROM merge_creature_model_info);
-DROP TABLE IF EXISTS merge_creature_model_info;
-
-DELETE FROM creature_template WHERE entry IN (SELECT entry FROM merge_creature_template);
-DROP TABLE IF EXISTS merge_creature_template;
--- sql/tools/world_remove_all_destructible_buildings.sql
+++
@@ -1,1 +0,0 @@
-delete from gameobject where id IN (select entry from gameobject_template where type=33);
