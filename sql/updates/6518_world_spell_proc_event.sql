aeb3e27036c0dc91b9a9c692a22fed2ca3185051
Brian <runningnak3d@gmail.com>
2009-12-14 21:40:32 -0700

    * Add 20 second cooldown for Bloodworms * Thanks drevi * Did not add to the "full" files since they are going away * Closes #630
    --HG--
    branch : trunk

---
+++ sql/updates/6518_world_spell_proc_event.sql
@@ -0,0 +1,7 @@
+-- Add 20 second cooldown for Bloodworms
+DELETE FROM `spell_proc_event` WHERE `entry` IN (49027, 49542, 49543); 
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES 
+(49027, 0, 0, 0, 0, 0, 0, 0, 0, 3, 20),  -- Bloodworms rank 1 
+(49542, 0, 0, 0, 0, 0, 0, 0, 0, 6, 20),  -- Bloodworms rank 2 
+(49543, 0, 0, 0, 0, 0, 0, 0, 0, 9, 20);  -- Bloodworms rank 3
+
