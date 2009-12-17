60019dbd18b4cf43ba72c9f45a2abe9ebb3ae54b
win32 <none@none>
2009-12-17 14:31:32 +0200

    Correct spell proc for 16086 and rank Closes #56
    --HG--
    branch : trunk

---
+++ sql/updates/6549_world_spell_proc_event.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_proc_event` WHERE `entry` IN (16086,16544);
+INSERT INTO `spell_proc_event` VALUES 
+( 16086, 0x04, 11, 0x00000000, 0x00040000, 0x00000000, 0x00030000, 0x00000000,   0,   0,   0), -- Improved Fire Nova Totem (Rank 1)
+( 16544, 0x04, 11, 0x00000000, 0x00040000, 0x00000000, 0x00030000, 0x00000000,   0,   0,   0); -- Improved Fire Nova Totem (Rank 2)
