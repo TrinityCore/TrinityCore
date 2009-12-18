05c53e0334509ae58eb85e95686a8a9a9d30c253
nihal <none@none>
2009-12-18 20:05:45 +0100

    Sql cleanup. Moved all sql files up to rev 6507 to 3.1.3_old folder. Properly named some recent sql files following Trinity standard.
    --HG--
    branch : trunk

--- sql/updates/6589_characters_spell.sql
+++ sql/updates/6589_characters_spell.sql
@@ -1,4 +1,1 @@
 DELETE FROM `character_spell` WHERE `spell` IN (31892, 53720);
-DELETE FROM `character_spell_cooldown` WHERE `spell` IN (31892, 53720);
-DELETE FROM `character_aura` WHERE `spell` IN (31892, 53720);
-DELETE FROM `character_action` WHERE `action` IN (31892, 53720) AND `type`=0;
---
+++ sql/updates/6589_world_spell_bonus_data.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `spell_bonus_data` CHANGE COLUMN `entry` `entry` mediumint(8) unsigned NOT NULL DEFAULT '0';
