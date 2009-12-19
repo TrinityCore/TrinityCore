e13c23160fa584362281406b034a417cc5dc349e
n0n4m3 <none@none>
2009-12-19 10:29:12 +0100

    Restore work Auction
    --HG--
    branch : trunk

--- src/game/AuctionHouseHandler.cpp
+++ src/game/AuctionHouseHandler.cpp
@@ -162,8 +162,14 @@ void WorldSession::HandleAuctionSellItem(WorldPacket & recv_data)
 {
     uint64 auctioneer, item;
     uint32 etime, bid, buyout;
-    recv_data >> auctioneer >> item;
-    recv_data >> bid >> buyout >> etime;
+    recv_data >> auctioneer;
+    recv_data.read_skip<uint32>();                          // const 1?
+    recv_data >> item;
+    recv_data.read_skip<uint32>();                          // unk 3.2.2, const 1?
+    recv_data >> bid;
+    recv_data >> buyout;
+    recv_data >> etime;
+
     Player *pl = GetPlayer();
 
     if (!item || !bid || !etime)
