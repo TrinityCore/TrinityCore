656f4ceedd5c021075c160a66d619c508bb3ab29
n0n4m3 <none@none>
2009-12-25 15:23:34 +0100

    Fixed leave combat by timer. by laise
    --HG--
    branch : trunk

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -245,7 +245,7 @@ void Unit::Update(uint32 p_time)
         {
             // m_CombatTimer set at aura start and it will be freeze until aura removing
             if (m_CombatTimer <= p_time)
-                ClearInCombat();
+                CombatStop();
             else
                 m_CombatTimer -= p_time;
         }
