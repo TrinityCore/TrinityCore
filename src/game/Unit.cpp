aadbff5903ce9e4fb38acb7939bb426bc9e0e340
n0n4m3 <none@none>
2009-12-26 16:47:40 +0100

    Revert not needed changes(this implemented soon)
    --HG--
    branch : trunk

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -5566,10 +5566,6 @@ bool Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, AuraEffect* trigger
                 // Combustion
                 case 11129:
                 {
-                    Unit *caster = triggeredByAura->GetCaster();
-                    if (!caster || !damage)
-                        return false;
-
                     //last charge and crit
                     if (triggeredByAura->GetParentAura()->GetAuraCharges() <= 1 && (procEx & PROC_EX_CRITICAL_HIT) )
                     {
@@ -5577,7 +5573,7 @@ bool Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, AuraEffect* trigger
                         return true;                        // charge counting (will removed)
                     }
 
-                    CastSpell(this, 28682, true, castItem, triggeredByAura);
+                    CastSpell(this, 11129, true, castItem, triggeredByAura);
                     return (procEx & PROC_EX_CRITICAL_HIT);// charge update only at crit hits, no hidden cooldowns
                 }
                 // Glyph of Ice Block
