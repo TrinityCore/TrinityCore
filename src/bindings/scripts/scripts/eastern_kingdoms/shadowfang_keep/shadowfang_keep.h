089b2d6aa8d514ebb5808c3047f8d7401fb46f5f
_manuel_ <none@none>
2010-01-04 23:13:17 -0300

    Shadowfang Keep: Implemented event after Boss Fenrus the Devourer death.
    --HG--
    branch : trunk

--- src/bindings/scripts/scripts/eastern_kingdoms/shadowfang_keep/shadowfang_keep.h
+++ src/bindings/scripts/scripts/eastern_kingdoms/shadowfang_keep/shadowfang_keep.h
@@ -5,9 +5,13 @@
 #ifndef DEF_SHADOWFANG_H
 #define DEF_SHADOWFANG_H
 
-#define TYPE_FREE_NPC       1
-#define TYPE_RETHILGORE     2
-#define TYPE_FENRUS         3
-#define TYPE_NANDOS         4
+enum eData
+{
+    TYPE_FREE_NPC               = 1,
+    TYPE_RETHILGORE             = 2,
+    TYPE_FENRUS                 = 3,
+    TYPE_NANDOS                 = 4
+};
+
 #endif
 
