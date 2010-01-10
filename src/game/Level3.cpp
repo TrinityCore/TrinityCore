4bd6d06e26e6d09297139ce08abcc5225c430e1b
Astellar <none@none>
2010-01-10 22:01:18 +0300

    Fixed typo.
    --HG--
    branch : trunk

--- src/game/Level3.cpp
+++ src/game/Level3.cpp
@@ -4612,7 +4612,7 @@ bool ChatHandler::HandleNpcInfoCommand(const char* /*args*/)
     PSendSysMessage(LANG_NPCINFO_LOOT,  cInfo->lootid,cInfo->pickpocketLootId,cInfo->SkinLootId);
     PSendSysMessage(LANG_NPCINFO_DUNGEON_ID, target->GetInstanceId());
     PSendSysMessage(LANG_NPCINFO_PHASEMASK, target->GetPhaseMask());
-    PsendSysMessage(LANG_NPCINFO_ARMOR, target->GetArmor());
+    PSendSysMessage(LANG_NPCINFO_ARMOR, target->GetArmor());
     PSendSysMessage(LANG_NPCINFO_POSITION,float(target->GetPositionX()), float(target->GetPositionY()), float(target->GetPositionZ()));
     if(const CreatureData* const linked = target->GetLinkedRespawnCreatureData())
         if(CreatureInfo const *master = GetCreatureInfo(linked->id))
