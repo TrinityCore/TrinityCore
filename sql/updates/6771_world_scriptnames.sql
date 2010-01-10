eee3d2584851d0d0196ef54778e2df3ffcb911aa
_manuel_ <none@none>
2010-01-10 15:39:18 -0300

    Removed unnecessary sentence.
    --HG--
    branch : trunk

--- sql/updates/6771_world_scriptnames.sql
+++ sql/updates/6771_world_scriptnames.sql
@@ -6,9 +6,9 @@ UPDATE `creature_template` SET `ScriptName`='mob_toc5_mage' WHERE entry IN (3470
 UPDATE `creature_template` SET `ScriptName`='mob_toc5_shaman' WHERE entry IN (34701, 35571);
 UPDATE `creature_template` SET `ScriptName`='mob_toc5_hunter' WHERE entry IN (34657, 35570);
 UPDATE `creature_template` SET `ScriptName`='mob_toc5_rogue' WHERE entry IN (34703, 35617);
-UPDATE `creature_template` SET `ScriptName`='mob_toc5_risen_ghoul' WHERE entry IN (35545, 35564);
 UPDATE `creature_template` SET `ScriptName`='npc_risen_ghoul' WHERE `entry` IN (35545,35564);
 UPDATE `creature_template` SET `ScriptName`='boss_black_knight' WHERE `entry`=35451;
 UPDATE `creature_template` SET `ScriptName`='boss_eadric' WHERE `entry`=35119;
 UPDATE `creature_template` SET `ScriptName`='boss_paletress' WHERE `entry`=34928;
 UPDATE `creature_template` SET `ScriptName`='npc_memory' WHERE `entry` IN (35052,35041,35033,35046,35043,35047,35044,35039, 35034, 35049, 35030, 34942, 35050, 35042, 35045, 35037, 35031, 35038, 35029,35048,35032,35028,35040,35036,35051);
+
