2fb7cc0b45a6b44bb7f6b75bc881f095c1732900
Brian <runningnak3d@gmail.com>
2010-01-10 16:11:24 -0700

    * add armor output in .npc info
    --HG--
    branch : trunk

--- src/game/Language.h
+++ src/game/Language.h
@@ -886,7 +886,8 @@ enum TrinityStrings
     LANG_HEROIC                         = 5018,
     LANG_MOUNTABLE                      = 5019,
     LANG_NPCINFO_PHASEMASK              = 5020,
-    // Room for more Trinity strings      5021-9999
+    LANG_NPCINFO_ARMOR                  = 5021,
+    // Room for more Trinity strings      5022-9999
     // Used for GM Announcements
     LANG_GM_BROADCAST                    = 6613,
     LANG_GM_NOTIFY                       = 6614,
