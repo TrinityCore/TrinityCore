e4ee8eeefce290fd55af5bafd94333a88bdcf0c4
QAston <none@none>
2010-01-10 12:08:58 +0100

    * Use pair<uint32, uint64> lists instead of <Aura*> lists in Dispel/SpellSteal effects handlers (prevent reference to aura removed from unit) * Correctly fill EffectStealBeneficialBuff list with Stackable/Chargeable auras
    --HG--
    branch : trunk

--- src/game/Unit.h
+++ src/game/Unit.h
@@ -1562,8 +1562,8 @@ class TRINITY_DLL_SPEC Unit : public WorldObject
         void RemoveAurasDueToSpell(uint32 spellId, uint64 caster = NULL, uint8 reqEffMask = 0, AuraRemoveMode removeMode = AURA_REMOVE_BY_DEFAULT);
         void RemoveAuraFromStack(uint32 spellId, uint64 caster = NULL, AuraRemoveMode removeMode = AURA_REMOVE_BY_DEFAULT);
         inline void RemoveAuraFromStack(AuraMap::iterator &iter,AuraRemoveMode removeMode);
-        void RemoveAurasDueToSpellByDispel(Aura * aura, Unit *dispeller);
-        void RemoveAurasDueToSpellBySteal(Aura * aura, Unit *stealer);
+        void RemoveAurasDueToSpellByDispel(uint32 spellId, uint64 casterGUID, Unit *dispeller);
+        void RemoveAurasDueToSpellBySteal(uint32 spellId, uint64 casterGUID, Unit *stealer);
         void RemoveAurasDueToItemSpell(Item* castItem,uint32 spellId);
         void RemoveAurasByType(AuraType auraType, uint64 casterGUID = 0, Aura * except = NULL, bool negative = true, bool positive = true);
         void RemoveNotOwnSingleTargetAuras(uint32 newPhase = 0x0);
