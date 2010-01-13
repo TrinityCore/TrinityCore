a8746aab0572bb93109061509900c90cc22beb99
teacher <none@none>
2010-01-13 08:34:51 +0100

    Second part of sql files renaming not following Trinity standard. Update world.sql too. Final part later today to complete the work.
    --HG--
    branch : trunk

--- sql/updates/6877_world_script_texts.sql
+++ sql/updates/6877_world_script_texts.sql
@@ -5,4 +5,3 @@ INSERT INTO `script_texts` (`npc_entry`,`entry`,`content_default`,`content_loc1`
    (349,-1000466,'The Blackrock infestation is thick in these parts. I will do my best to keep the pace. Let''s go!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,''),
    (349,-1000467,'Marshal Marris, sir. Corporal Keeshan of the 12th Sabre Regiment returned from battle and reporting for duty!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,''),
    (349,-1000468,'Brave adventurer, thank you for rescuing me! I am sure Marshal Marris will reward your kind deed.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'');
-
