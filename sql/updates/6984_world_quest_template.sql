17b2dc697e586c4becc9c0d9408df7235e587aad
teacher <none@none>
2010-01-13 21:28:35 +0100

    Added long forgotten sql update for Rev6693 (Add support for RewSpellCast=-1. If -1 remove all auras applied to player at quest start). Thank you Warhead.
    --HG--
    branch : trunk

---
+++ sql/updates/6984_world_quest_template.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `quest_template` CHANGE `RewSpellCast1` `RewSpellCast1` int(11) SIGNED NOT NULL DEFAULT 0;
+ALTER TABLE `quest_template` CHANGE `RewSpellCast2` `RewSpellCast2` int(11) SIGNED NOT NULL DEFAULT 0;
+ALTER TABLE `quest_template` CHANGE `RewSpellCast3` `RewSpellCast3` int(11) SIGNED NOT NULL DEFAULT 0;
+ALTER TABLE `quest_template` CHANGE `RewSpellCast4` `RewSpellCast4` int(11) SIGNED NOT NULL DEFAULT 0;
