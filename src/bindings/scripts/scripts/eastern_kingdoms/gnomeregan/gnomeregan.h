a8b4a7bc378d651a2dde25f64a6b5a3927ee1815
_manuel_ <none@none>
2010-01-14 03:48:48 -0300

    Gnomeregan: Implemented Blastmaster Emi Shortfuse Event.
    --HG--
    branch : trunk

---
+++ src/bindings/scripts/scripts/eastern_kingdoms/gnomeregan/gnomeregan.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2010 Trinity <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#ifndef DEF_GNOMEREGAN_H
+#define DEF_GNOMEREGAN_H
+
+enum eGameObjects
+{
+    GO_CAVE_IN_LEFT     = 146085,
+    GO_CAVE_IN_RIGHT    = 146086
+};
+
+enum eCreatures
+{
+    NPC_BLASTMASTER_EMI_SHORTFUSE   = 7998,
+    NPC_CAVERNDEEP_AMBUSHER         = 6207,
+    NPC_GRUBBIS                     = 7361,
+    NPC_CHOMPER                     = 6215
+};
+
+enum eData
+{
+    TYPE_EVENT = 1
+};
+
+enum eData64
+{
+    DATA_GO_CAVE_IN_LEFT,
+    DATA_GO_CAVE_IN_RIGHT,
+    DATA_NPC_BASTMASTER_EMI_SHORTFUSE
+};
+
+#endif
