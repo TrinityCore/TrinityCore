4a1276e3d398b32f5c3bb04c2e64eb3acf3639ac
_manuel_ <none@none>
2010-01-15 22:41:56 -0300

    Dustwallow Marsh: Implemented improvements in scripts to make quest "What's Haunting Witch Hill?" more blizzlike.
    --HG--
    branch : trunk

---
+++ sql/updates/7053_world_script_texts.sql
@@ -0,0 +1,6 @@
+DELETE FROM `script_texts` WHERE entry IN (-1000469,-1000470,-1000471);
+INSERT INTO `script_texts` (`npc_entry`,`entry`,`content_default`,`content_loc1`,`content_loc2`,`content_loc3`,`content_loc4`,`content_loc5`,`content_loc6`,`content_loc7`,`content_loc8`,`sound`,`type`,`language`,`emote`,`comment`) VALUES
+   (23861,-1000469,'It is too late for us, living one. Take yourself and your friend away from here before you both are... claimed...',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,''),
+   (23861,-1000470,'Go away, whoever you are! Witch Hill is mine... mine!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,''),
+   (23861,-1000471,'It was... terrible... the demon...',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'');
+
