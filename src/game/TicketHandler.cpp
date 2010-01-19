0e286aaf028616a5567a339b491647efa678699d
Rat <none@none>
2010-01-19 16:37:49 +0100

    *Update Copyright times
    --HG--
    branch : trunk

--- src/game/TicketHandler.cpp
+++ src/game/TicketHandler.cpp
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2005-2009 MaNGOS
  *
- * Copyright (C) 2008-2009 Trinity
+ * Copyright (C) 2008-2010 Trinity
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
