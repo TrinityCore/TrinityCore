41bff63fe7012820e1fb9365a99a4540a0ef3cde
Rat <none@none>
2010-01-19 16:41:31 +0100

    *added placeholders for SmartAI
    --HG--
    branch : trunk

---
+++ src/game/ScriptedSmartAI.cpp
@@ -0,0 +1,8 @@
+/* Copyright (C) 2008-2010 Trinity <http://www.trinitycore.org/>
+ *
+ * Thanks to the original authors: ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ *
+ * This program is free software licensed under GPL version 2
+ * Please see the included DOCS/LICENSE.TXT for more information */
+
+#include "ScriptedSmartAI.h"
