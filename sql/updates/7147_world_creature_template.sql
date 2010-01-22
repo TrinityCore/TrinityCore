0a904a4d93a55bc5bfdd14c02368218fc58c6304
Brian <runningnak3d@gmail.com>
2010-01-22 10:21:25 -0700

    * Fixed a typo in 7147_world_creature_template.sql that prevented import
    --HG--
    branch : trunk

--- sql/updates/7147_world_creature_template.sql
+++ sql/updates/7147_world_creature_template.sql
@@ -1,6 +1,6 @@
 ALTER TABLE `creature_template`
 DROP COLUMN `dmg_Mod`,
-DROP COLUMN `rangeddmg_Mod`;
+DROP COLUMN `rangeddmg_Mod`,
 ADD COLUMN `mindmg` float NOT NULL DEFAULT '0' AFTER `rank`,
 ADD COLUMN `maxdmg` float NOT NULL DEFAULT '0' AFTER `mindmg`,
 ADD COLUMN `attackpower` int(10) unsigned NOT NULL DEFAULT '0' AFTER `dmgschool`,
