cfa8ff7bd6e001742f6149a84944966c85e2b1e2
Brian <runningnak3d@gmail.com>
2010-01-30 08:05:08 -0700

    * Set the default spawntimesecs for creatures / gameobjects added in game to 5 minutes * Was 25 seconds * Thanks Paradox
    --HG--
    branch : trunk

--- src/game/GameObject.cpp
+++ src/game/GameObject.cpp
@@ -49,7 +49,7 @@ GameObject::GameObject() : WorldObject(), m_goValue(new GameObjectValue)
 
     m_valuesCount = GAMEOBJECT_END;
     m_respawnTime = 0;
-    m_respawnDelayTime = 25;
+    m_respawnDelayTime = 300;
     m_lootState = GO_NOT_READY;
     m_spawnedByDefault = true;
     m_usetimes = 0;
