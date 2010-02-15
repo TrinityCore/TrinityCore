511891fdf7e2d002969cda3c57c8ab8837ecd143
Tartalo <none@none>
2010-02-15 18:06:32 +0100

    Frozen Halls, Forge of Souls: Initial instance script
    --HG--
    branch : trunk

---
+++ src/scripts/northrend/frozen_halls/forge_of_souls/forge_of_souls.h
@@ -0,0 +1,13 @@
+#ifndef DEF_FORGE_OF_SOULS_H
+#define DEF_FORGE_OF_SOULS_H
+enum Data
+{
+    DATA_BRONJAHM_EVENT,
+    DATA_DEVOURER_EVENT
+};
+enum Creatures
+{
+    CREATURE_BRONJAHM                             = 36497,
+    CREATURE_DEVOURER                             = 36502
+};
+#endif
