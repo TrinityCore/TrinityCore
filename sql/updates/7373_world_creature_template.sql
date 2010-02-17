08c6dae672257b5ccc9cca48b04dbec2e344fbf8
Tartalo <none@none>
2010-02-17 15:53:16 +0100

    Borean Tundra: Support for quest 11876 'Help Those That Cannot Help Themselves'
    --HG--
    branch : trunk

---
+++ sql/updates/7373_world_creature_template.sql
@@ -0,0 +1,2 @@
+UPDATE `creatyre_template` SET `scriptname`='npc_trapped_mammoth_calf' WHERE `entry`=25850;
+
---
+++ sql/updates/7373_world_item_template.sql
@@ -0,0 +1,2 @@
+UPDATE `item_template` SET `scriptname`='item_dehta_trap_smasher' WHERE `entry`=35228;
+
