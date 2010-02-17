26e2ab0fffeffe6baec35d6800aa2a2d0f53344e
Tartalo <none@none>
2010-02-17 16:00:18 +0100

    Fix typo
    --HG--
    branch : trunk

--- sql/updates/7373_world_creature_template.sql
+++ sql/updates/7373_world_creature_template.sql
@@ -1,2 +1,2 @@
-UPDATE `creatyre_template` SET `scriptname`='npc_trapped_mammoth_calf' WHERE `entry`=25850;
+UPDATE `creature_template` SET `scriptname`='npc_trapped_mammoth_calf' WHERE `entry`=25850;
 
