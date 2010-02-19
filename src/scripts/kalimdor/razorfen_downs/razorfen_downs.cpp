1da4996189c402f8176b561abc770df0feab0630
_manuel_ <manue.l@live.com.ar>
2010-02-19 15:12:46 -0800

    Razorfen downs: Implemented event from boss Tuten'kash. For a full fix see TDB Forums.
    --HG--
    branch : trunk

--- src/scripts/kalimdor/razorfen_downs/razorfen_downs.cpp
+++ src/scripts/kalimdor/razorfen_downs/razorfen_downs.cpp
@@ -26,6 +26,7 @@ npc_henry_stern
 EndContentData */
 
 #include "ScriptedPch.h"
+#include "razorfen_downs.h"
 
 /*###
 # npc_henry_stern
@@ -73,6 +74,75 @@ bool GossipSelect_npc_henry_stern (Player* pPlayer, Creature* pCreature, uint32 
     return true;
 }
 
+/*######
+## go_gong
+######*/
+
+bool GOHello_go_gong(Player* pPlayer, GameObject* pGO)
+{
+    //basic support, not blizzlike data is missing...
+    ScriptedInstance* pInstance = pGO->GetInstanceData();
+
+    if (pInstance)
+    {
+        pInstance->SetData(DATA_GONG_WAVES,pInstance->GetData(DATA_GONG_WAVES)+1);
+        return true;
+    }
+
+    return false;
+}
+
+enum eTombCreature
+{
+    SPELL_WEB                   = 745
+};
+
+struct npc_tomb_creatureAI : public ScriptedAI
+{
+    npc_tomb_creatureAI(Creature* pCreature) : ScriptedAI(pCreature)
+    {
+        pInstance = pCreature->GetInstanceData();
+    }
+
+    ScriptedInstance* pInstance;
+
+    uint32 uiWebTimer;
+
+    void Reset()
+    {
+        uiWebTimer = urand(5000,8000);
+    }
+
+    void UpdateAI(const uint32 uiDiff)
+    {
+        if (!UpdateVictim())
+            return;
+        
+        //from acid
+        if (m_creature->GetEntry() == CREATURE_TOMB_REAVER)
+        {
+            if (uiWebTimer <= uiDiff)
+            {
+                DoCast(m_creature->getVictim(), SPELL_WEB);
+                uiWebTimer = urand(7000,16000);
+            } else uiWebTimer -= uiDiff;
+        }
+
+        DoMeleeAttackIfReady();
+    }
+
+    void JustDied(Unit* pKiller)
+    {
+        if (pInstance)
+            pInstance->SetData(DATA_GONG_WAVES,pInstance->GetData(DATA_GONG_WAVES)+1);
+    }
+};
+
+CreatureAI* GetAI_npc_tomb_creature(Creature* pCreature)
+{
+    return new npc_tomb_creatureAI (pCreature);
+}
+
 void AddSC_razorfen_downs()
 {
     Script* newscript;
@@ -81,5 +151,15 @@ void AddSC_razorfen_downs()
     newscript->Name = "npc_henry_stern";
     newscript->pGossipHello = &GossipHello_npc_henry_stern;
     newscript->pGossipSelect = &GossipSelect_npc_henry_stern;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name = "go_gong";
+    newscript->pGOHello =           &GOHello_go_gong;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name = "npc_tomb_creature";
+    newscript->GetAI = &GetAI_npc_tomb_creature;
     newscript->RegisterSelf();
 }
