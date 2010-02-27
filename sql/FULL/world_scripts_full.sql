6146c337327788460661a42a0f536c2ebb592582
Trazom62 <none@none>
2010-02-27 16:41:44 +0100

    Fix Script Naxxramas/Kel Thusad. Thanks Ille. Fixes issue 650. Fixes issue 659. Fixes issue 809. Thanks PrinceCreed.
    script changes
    - added spawnpoints and spawn creatures in corners
    	* they react defensive, otherwise they assist the minions
    kelthuzad spawns
    - added functionality to area trigger
    	* fight begins when player enters green circle
    - added functionality to kelthuzad trigger
    	* trigger is activated when player enters green circle
    	* trigger disappears after 5s
    - added functionality to window portals
    	* at 45% portals open to let guardians in
    - chained can be 3 at once
    - added spells to chained
    	* for every class two spells
    - changed timers on enter combat
    - set combat reach to 2 for guardians
    - Fix emote when enter combat and when enter phase 2.
    
    NOTE: some db updates may still be necessary to have better locations of portals and EventAI updates.
    For more info, see sql in attachment of #659 and http://trinitydatabase.org/index.php?/topic/14304-boss-naxxramas-kelthuzad on tdb forum.
    
    --HG--
    branch : trunk

--- sql/FULL/world_scripts_full.sql
+++ sql/FULL/world_scripts_full.sql
@@ -878,6 +878,9 @@ UPDATE `creature_template` SET `ScriptName`='boss_kelthuzad' WHERE `entry`=15990
 UPDATE `creature_template` SET `ScriptName`='' WHERE `entry` IN (16062,30000);
 UPDATE `creature_template` SET `ScriptName`='trigger_periodic' WHERE `entry` IN (16474,16697,16129);
 
+DELETE FROM `areatrigger_scripts` WHERE `entry`=4112;
+INSERT INTO `areatrigger_scripts`(`entry`,`ScriptName`) VALUES(4112,'at_kelthuzad_center');
+
 /* NETHERSTORM */
 UPDATE `gameobject_template` SET `ScriptName`='go_manaforge_control_console' WHERE `entry` IN (183770,183956,184311,184312);
 UPDATE `creature_template` SET `ScriptName`='npc_manaforge_control_console' WHERE `entry` IN (20209,20417,20418,20440);
