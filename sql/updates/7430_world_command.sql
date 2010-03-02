70e77a796ddbf5457ef259627101e0b0f80e7a33
teacher <none@none>
2010-03-02 15:42:46 +0100

    Fixed import errors on recent sql update files... Added forgotten updates to world.sql... Added missing lines at the end of some sql files... Better format by adding DELETE FROM before any INSERT INTO update...
    --HG--
    branch : trunk

--- sql/updates/7430_world_command.sql
+++ sql/updates/7430_world_command.sql
@@ -1,1 +1,2 @@
-INSERT INTO command VALUES ('channel set public', 3, 'Syntax: .channel set public  $channel $public\r\n\r\nChange password-changing ability for a channel. 1 for possible, 0 for GM only.');
+DELETE FROM `command` WHERE name='channel set public';
+INSERT INTO `command` (`name`,`security`,`help`) VALUES ('channel set public', 3, 'Syntax: .channel set public  $channel $public\r\n\r\nChange password-changing ability for a channel. 1 for possible, 0 for GM only.');
