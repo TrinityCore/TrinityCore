81f6779102561df0719b0471b101bb5c15d2609a
p0wer <none@none>
2010-03-01 19:28:21 -0600

    Add command similar to pinfo but for gameobjects. Displays Entry, Type, DisplayId, and Name. Created in response to issue #768.
    --HG--
    branch : trunk

---
+++ sql/updates/7465_world_trinity_string.sql
@@ -0,0 +1,4 @@
+INSERT INTO trinity_string (`entry`, `content_default`) VALUES (5024, 'Entry: %u');
+INSERT INTO trinity_string (`entry`, `content_default`) VALUES (5025, 'Type: %u');
+INSERT INTO trinity_string (`entry`, `content_default`) VALUES (5026, 'DisplayID: %u');
+INSERT INTO trinity_string (`entry`, `content_default`) VALUES (5027, 'Name: %s');
