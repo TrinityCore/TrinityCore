b085d8eadc52f402bcef5aab789d0b922f2fdf38
Machiavelli <none@none>
2010-03-12 14:25:43 +0100

    Fix faulty debug logging and a crash caused by improper typecasting. Fixes issue #1068
    --HG--
    branch : trunk

--- src/game/MotionMaster.cpp
+++ src/game/MotionMaster.cpp
@@ -244,7 +244,7 @@ MotionMaster::MoveChase(Unit* target, float dist, float angle)
         DEBUG_LOG("Player (GUID: %u) chase to %s (GUID: %u)",
             i_owner->GetGUIDLow(),
             target->GetTypeId() == TYPEID_PLAYER ? "player" : "creature",
-            target->GetTypeId() == TYPEID_PLAYER ? i_owner->GetGUIDLow() : i_owner->ToCreature()->GetDBTableGUIDLow());
+            target->GetTypeId() == TYPEID_PLAYER ? target->GetGUIDLow() : target->ToPlayer()->GetDBTableGUIDLow());
         Mutate(new TargetedMovementGenerator<Player>(*target,dist,angle), MOTION_SLOT_ACTIVE);
     }
     else
