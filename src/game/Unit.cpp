10d198fb74cd50bcacdc367ab4ad0663bb14848f
reno <none@none>
2010-03-15 14:38:30 -0500

    attempt to stop mobs casting and fleeing while stunned or confused
    --HG--
    branch : trunk

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -14789,24 +14789,37 @@ void Unit::SetControlled(bool apply, UnitState state)
         if (hasUnitState(state))
             return;
 
-        addUnitState(state);
-
         switch(state)
         {
         case UNIT_STAT_STUNNED:
+	   {
+	    addUnitState(state);
             SetStunned(true);
+	    CastStop();
+	   }
             break;
         case UNIT_STAT_ROOT:
             if (!hasUnitState(UNIT_STAT_STUNNED))
-                SetRooted(true);
+		{
+		 addUnitState(state);
+                 SetRooted(true);
+	 	}
             break;
         case UNIT_STAT_CONFUSED:
             if (!hasUnitState(UNIT_STAT_STUNNED))
+		{
+		addUnitState(state);
                 SetConfused(true);
+		CastStop();
+		}
             break;
         case UNIT_STAT_FLEEING:
             if (!hasUnitState(UNIT_STAT_STUNNED | UNIT_STAT_CONFUSED))
+		{
+		addUnitState(state);
                 SetFeared(true);
+		CastStop();
+		}
             break;
         default:
             break;
