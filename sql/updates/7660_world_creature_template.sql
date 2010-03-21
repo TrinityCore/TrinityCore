e89db885fd36733a330982855c8d438d9e55b2e5
teacher <none@none>
2010-03-21 19:57:00 +0100

    Added missing sql file for rev 7656 (by Machiavelli). Removed DB content data from previous commit.
    --HG--
    branch : trunk

--- sql/updates/7660_world_creature_template.sql
+++ sql/updates/7660_world_creature_template.sql
@@ -1,3 +1,2 @@
 ALTER TABLE creature_template ADD COLUMN speed_run float NOT NULL default '1.14286' COMMENT 'Result of 8.0/7.0, most common value' AFTER speed;
-UPDATE creature_template SET speed=1 WHERE speed IS NULL;
 ALTER TABLE creature_template CHANGE COLUMN speed speed_walk FLOAT NOT NULL DEFAULT '1' COMMENT 'Result of 2.5/2.5, most common value';
