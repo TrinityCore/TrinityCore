2c34b6d376edf97e16902017c5fa8bd66d0162b1
Tartalo <none@none>
2010-03-21 18:32:50 +0100

    The Obsidiam Sanctum: Several updates, by Ouden Portal event, Flame Tsunami, Twilight Eggs, Twilights Fissure, Achievements in Hard Mode Closes issue #1015.
    --HG--
    branch : trunk

--- sql/FULL/world_scripts_full.sql
+++ sql/FULL/world_scripts_full.sql
@@ -914,15 +914,17 @@ UPDATE `creature_template` SET `ScriptName`='boss_drakos' WHERE `entry`=27654;
 UPDATE `creature_template` SET `ScriptName`='npc_oculus_drakes' WHERE `entry` IN (27657,27658,27659);
 
 /* OBSIDIAN SANCTUM */
-UPDATE `instance_template` SET `script`='instance_obsidian_sanctum' WHERE map=615;
-UPDATE creature_template SET ScriptName='boss_sartharion' WHERE entry=28860;
-UPDATE creature_template SET ScriptName='mob_vesperon' WHERE entry=30449;
-UPDATE creature_template SET ScriptName='mob_shadron' WHERE entry=30451;
-UPDATE creature_template SET ScriptName='mob_tenebron' WHERE entry=30452;
-UPDATE creature_template SET ScriptName='mob_twilight_eggs' WHERE entry=30882;
-UPDATE creature_template SET ScriptName='mob_twilight_whelp' WHERE entry IN (30890,31214);
-UPDATE creature_template SET ScriptName='mob_acolyte_of_shadron' WHERE entry=31218;
-UPDATE creature_template SET ScriptName='mob_acolyte_of_vesperon' WHERE entry=31219;
+UPDATE `instance_template` SET `script`='instance_obsidian_sanctum' WHERE `map`=615;
+UPDATE `creature_template` SET `ScriptName`='boss_sartharion' WHERE `entry`=28860;
+UPDATE `creature_template` SET `ScriptName`='mob_vesperon' WHERE `entry`=30449;
+UPDATE `creature_template` SET `ScriptName`='mob_shadron' WHERE `entry`=30451;
+UPDATE `creature_template` SET `ScriptName`='mob_tenebron' WHERE `entry`=30452;
+UPDATE `creature_template` SET `ScriptName`='mob_twilight_eggs' WHERE `entry`=30882;
+UPDATE `creature_template` SET `ScriptName`='mob_twilight_whelp' WHERE `entry` IN (30890,31214);
+UPDATE `creature_template` SET `ScriptName`='npc_twilight_fissure' WHERE `entry`=30641;
+UPDATE `creature_template` SET `ScriptName`='mob_acolyte_of_shadron' WHERE `entry`=31218;
+UPDATE `creature_template` SET `ScriptName`='mob_acolyte_of_vesperon' WHERE `entry`=31219;
+UPDATE `creature_template` SET `ScriptName`='npc_flame_tsunami' WHERE `entry`=30616;
 
 /* ONYXIA'S LAIR */
 UPDATE `creature_template` SET `ScriptName`='boss_onyxia' WHERE `entry`=10184;
