c45da17ef9378a21c290fd0126c7e14f266c75c6
Machiavelli <none@none>
2010-03-21 16:56:07 +0100

    Fix SQL's
    --HG--
    branch : trunk

--- sql/updates/7646_world_scriptname.sql
+++ sql/updates/7646_world_scriptname.sql
@@ -1,3 +1,3 @@
 UPDATE `creature_template` SET `ScriptName`='boss_drakos' WHERE `entry`=27654;
-UPDATE `creature_template` SET `ScriptName`='npcs_oculus_drakes' WHERE `entry` IN (27657,27658,27659);
+UPDATE `creature_template` SET `ScriptName`='npc_oculus_drakes' WHERE `entry` IN (27657,27658,27659);
 UPDATE `instance_template` SET `script`='instance_oculus' WHERE `map`=578;
