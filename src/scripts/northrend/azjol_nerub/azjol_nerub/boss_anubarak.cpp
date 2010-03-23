e29cc4cbbb1207ae75a53395f052e44fd191e3c9
Tartalo <none@none>
2010-03-24 00:13:00 +0100

    Azjol Nerub, Azjol Nerub: Cleanup
    --HG--
    branch : trunk

--- src/scripts/northrend/azjol_nerub/azjol_nerub/boss_anubarak.cpp
+++ src/scripts/northrend/azjol_nerub/azjol_nerub/boss_anubarak.cpp
@@ -1,75 +1,63 @@
 /*
- * Copyright (C) 2009 Trinity <http://www.trinitycore.org/>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- */
-
-/* ScriptData
-SDName: boss_anubarak
-SD%Complete: 90 %
-SDComment: Says are not implemented.
-SDCategory: Azjol Nerub
-EndScriptData */
-
+* Copyright (C) 2009 - 2010 TrinityCore <http://www.trinitycore.org/>
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
 
 #include "ScriptedPch.h"
 #include "azjol_nerub.h"
 
 enum Spells
 {
-    SPELL_CARRION_BEETLES                  =       53520,
-    SPELL_SUMMON_CARRION_BEETLES           =       53521,
-    SPELL_LEECHING_SWARM                   =       53467,
-
-    SPELL_IMPALE                           =       53454,
-    H_SPELL_IMPALE                         =       59446,
-
-    SPELL_POUND                            =       53472,
-    H_SPELL_POUND                          =       59433,
-
-    SPELL_SUBMERGE                         =       53421,
+    SPELL_CARRION_BEETLES                         = 53520,
+    SPELL_SUMMON_CARRION_BEETLES                  = 53521,
+    SPELL_LEECHING_SWARM                          = 53467,
+    SPELL_IMPALE                                  = 53454,
+    H_SPELL_IMPALE                                = 59446,
+    SPELL_POUND                                   = 53472,
+    H_SPELL_POUND                                 = 59433,
+    SPELL_SUBMERGE                                = 53421,
 };
 
 enum Creatures
 {
-    CREATURE_GUARDIAN                      =       29216,
-    CREATURE_VENOMANCER                    =       29217,
-    CREATURE_DATTER                        =       29213
+    CREATURE_GUARDIAN                             = 29216,
+    CREATURE_VENOMANCER                           = 29217,
+    CREATURE_DATTER                               = 29213
 };
 
 // not in db
 enum Yells
 {
-    SAY_INTRO                                  = -1601010,
-    SAY_AGGRO                                  = -1601000,
-    SAY_SLAY_1                                 = -1601001,
-    SAY_SLAY_2                                 = -1601002,
-    SAY_SLAY_3                                 = -1601003,
-    SAY_LOCUST_1                               = -1601005,
-    SAY_LOCUST_2                               = -1601006,
-    SAY_LOCUST_3                               = -1601007,
-    SAY_SUBMERGE_1                             = -1601008,
-    SAY_SUBMERGE_2                             = -1601009,
-    SAY_DEATH                                  = -1601004
+    SAY_INTRO                                     = -1601010,
+    SAY_AGGRO                                     = -1601000,
+    SAY_SLAY_1                                    = -1601001,
+    SAY_SLAY_2                                    = -1601002,
+    SAY_SLAY_3                                    = -1601003,
+    SAY_LOCUST_1                                  = -1601005,
+    SAY_LOCUST_2                                  = -1601006,
+    SAY_LOCUST_3                                  = -1601007,
+    SAY_SUBMERGE_1                                = -1601008,
+    SAY_SUBMERGE_2                                = -1601009,
+    SAY_DEATH                                     = -1601004
 };
-#define SPAWNPOINT_Z                               224.3
 
-float SpawnPoint[2][2] =
+const Position SpawnPoint[2] =
 {
-    {550.7, 282.8},
-    {551.1, 229.4},
+    { 550.7, 282.8, 224.3 },
+    { 551.1, 229.4, 224.3 },
 };
 
 struct boss_anub_arakAI : public ScriptedAI
@@ -81,36 +69,35 @@ struct boss_anub_arakAI : public ScriptedAI
 
     ScriptedInstance *pInstance;
 
-    bool Channeling;
-    bool HeroicMode;
-    bool Summoned_Guardian;
-    bool Summoned_Venomancer;
-    bool Summoned_Datter;
-    uint32 Phase;
-    uint32 Phase_Time;
+    bool bChanneling;
+    bool bGuardianSummoned;
+    bool bVenomancerSummoned;
+    bool bDatterSummoned;
+    uint8 uiPhase;
+    uint32 uiPhaseTimer;
 
-    uint32 SPELL_CARRION_BEETLES_Timer;
-    uint32 SPELL_LEECHING_SWARM_Timer;
-    uint32 SPELL_IMPALE_Timer;
-    uint32 SPELL_POUND_Timer;
-    uint32 SPELL_SUBMERGE_Timer;
-    uint32 UNDERGROUND_Timer;
-    uint32 VENOMANCER_Timer;
-    uint32 DATTER_Timer;
+    uint32 uiCarrionBeetlesTimer;
+    uint32 uiLeechingSwarmTimer;
+    uint32 uiImpaleTimer;
+    uint32 uiPoundTimer;
+    uint32 uiSubmergeTimer;
+    uint32 uiUndergroundTimer;
+    uint32 uiVenomancerTimer;
+    uint32 uiDatterTimer;
 
     SummonList lSummons;
 
     void Reset()
     {
 
-        SPELL_CARRION_BEETLES_Timer = 8000;
-        SPELL_LEECHING_SWARM_Timer = 20000;
-        SPELL_IMPALE_Timer = 9000;
-        SPELL_POUND_Timer = 15000;
+        uiCarrionBeetlesTimer = 8*IN_MILISECONDS;
+        uiLeechingSwarmTimer = 20*IN_MILISECONDS;
+        uiImpaleTimer = 9*IN_MILISECONDS;
+        uiPoundTimer = 15*IN_MILISECONDS;
 
-        Phase = 0;
-        Phase_Time = 0;
-        Channeling = false;
+        uiPhase = 0;
+        uiPhaseTimer = 0;
+        bChanneling = false;
 
         m_creature->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE|UNIT_FLAG_NOT_SELECTABLE);
         m_creature->RemoveAura(SPELL_SUBMERGE);
@@ -137,126 +124,125 @@ struct boss_anub_arakAI : public ScriptedAI
         if (!UpdateVictim())
             return;
 
-        if (Channeling == true)
+        if (bChanneling == true)
         {
             for (uint8 i = 0; i < 4; ++i)
                 DoCast(m_creature->getVictim(), SPELL_SUMMON_CARRION_BEETLES, true);
-            Channeling = false;
+            bChanneling = false;
         }
 
-        if (Phase == 1)
+        if (uiPhase == 1)
         {
-            if (SPELL_IMPALE_Timer <= diff)
+            if (uiImpaleTimer <= diff)
             {
                 if (Unit *pTarget = SelectTarget(SELECT_TARGET_RANDOM, 0, 100, true))
                     m_creature->CastSpell(pTarget, DUNGEON_MODE(SPELL_IMPALE,H_SPELL_IMPALE), true);
+                uiImpaleTimer = 9*IN_MILISECONDS;
+            } else uiImpaleTimer -= diff;
 
-                SPELL_IMPALE_Timer = 9000;
-            } else SPELL_IMPALE_Timer -= diff;
-
-            if (!Summoned_Guardian)
+            if (!bGuardianSummoned)
             {
                 for (uint8 i = 0; i < 2; ++i)
                 {
-                    if (Creature *Guardian = m_creature->SummonCreature(CREATURE_GUARDIAN,SpawnPoint[i][0],SpawnPoint[i][1],SPAWNPOINT_Z,0,TEMPSUMMON_CORPSE_DESPAWN,0))
+                    if (Creature *Guardian = m_creature->SummonCreature(CREATURE_GUARDIAN,SpawnPoint[i],TEMPSUMMON_CORPSE_DESPAWN,0))
                     {
                         Guardian->AddThreat(m_creature->getVictim(), 0.0f);
                         DoZoneInCombat(Guardian);
                     }
                 }
-                Summoned_Guardian = true;
+                bGuardianSummoned = true;
             }
 
-            if(!Summoned_Venomancer)
+            if(!bVenomancerSummoned)
             {
-                if (VENOMANCER_Timer <= diff)
+                if (uiVenomancerTimer <= diff)
                 {
-                    if (Phase_Time > 1)
+                    if (uiPhaseTimer > 1)
                     {
                         for (uint8 i = 0; i < 2; ++i)
                         {
-                            if (Creature *Venomancer = m_creature->SummonCreature(CREATURE_VENOMANCER,SpawnPoint[i][0],SpawnPoint[i][1],SPAWNPOINT_Z,0,TEMPSUMMON_CORPSE_DESPAWN,0))
+                            if (Creature *Venomancer = m_creature->SummonCreature(CREATURE_VENOMANCER,SpawnPoint[i],TEMPSUMMON_CORPSE_DESPAWN,0))
                             {
                                 Venomancer->AddThreat(m_creature->getVictim(), 0.0f);
                                 DoZoneInCombat(Venomancer);
                             }
                         }
-                        Summoned_Venomancer = true;
+                        bVenomancerSummoned = true;
                     }
-                } else VENOMANCER_Timer -= diff;
+                } else uiVenomancerTimer -= diff;
             }
 
-            if(!Summoned_Datter)
+            if(!bDatterSummoned)
             {
-                if (DATTER_Timer <= diff)
+                if (uiDatterTimer <= diff)
                 {
-                    if (Phase_Time > 2)
+                    if (uiPhaseTimer > 2)
                     {
                         for (uint8 i = 0; i < 2; ++i)
                         {
-                            if (Creature *Datter = m_creature->SummonCreature(CREATURE_DATTER,SpawnPoint[i][0],SpawnPoint[i][1],SPAWNPOINT_Z,0,TEMPSUMMON_CORPSE_DESPAWN,0))
+                            if (Creature *Datter = m_creature->SummonCreature(CREATURE_DATTER,SpawnPoint[i],TEMPSUMMON_CORPSE_DESPAWN,0))
                             {
                                 Datter->AddThreat(m_creature->getVictim(), 0.0f);
                                 DoZoneInCombat(Datter);
                             }
                         }
-                        Summoned_Datter = true;
+                        bDatterSummoned = true;
                     }
-                } else DATTER_Timer -= diff;
+                } else uiDatterTimer -= diff;
             }
 
-            if (UNDERGROUND_Timer <= diff)
+            if (uiUndergroundTimer <= diff)
             {
                 m_creature->RemoveAura(SPELL_SUBMERGE);
                 m_creature->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE|UNIT_FLAG_NOT_SELECTABLE);
-                Phase = 0;
-            } else UNDERGROUND_Timer -= diff;
+                uiPhase = 0;
+            } else uiUndergroundTimer -= diff;
         }
 
-        if (Phase == 0)
+        if (uiPhase == 0)
         {
-            if (SPELL_LEECHING_SWARM_Timer <= diff)
+            if (uiLeechingSwarmTimer <= diff)
             {
                 DoCast(m_creature, SPELL_LEECHING_SWARM, true);
-                SPELL_LEECHING_SWARM_Timer = 19000;
-            } else SPELL_LEECHING_SWARM_Timer -= diff;
+                uiLeechingSwarmTimer = 19*IN_MILISECONDS;
+            } else uiLeechingSwarmTimer -= diff;
 
-            if (SPELL_CARRION_BEETLES_Timer <= diff)
+            if (uiCarrionBeetlesTimer <= diff)
             {
-                Channeling = true;
+                bChanneling = true;
                 DoCastVictim(SPELL_CARRION_BEETLES);
-                SPELL_CARRION_BEETLES_Timer = 25000;
-            } else SPELL_CARRION_BEETLES_Timer -= diff;
+                uiCarrionBeetlesTimer = 25*IN_MILISECONDS;
+            } else uiCarrionBeetlesTimer -= diff;
 
-            if (SPELL_POUND_Timer <= diff)
+            if (uiPoundTimer <= diff)
             {
                  DoCastVictim(DUNGEON_MODE(SPELL_POUND, H_SPELL_POUND));
-                 SPELL_POUND_Timer = 16500;
-            } else SPELL_POUND_Timer -= diff;
+                 uiPoundTimer = 16.5*IN_MILISECONDS;
+            } else uiPoundTimer -= diff;
         }
 
-        if ((Phase_Time == 0 && m_creature->GetHealth()*100 / m_creature->GetMaxHealth() <= 75)
-            || (Phase_Time == 1 && m_creature->GetHealth()*100 / m_creature->GetMaxHealth() <= 50)
-            || (Phase_Time == 2 && m_creature->GetHealth()*100 / m_creature->GetMaxHealth() <= 25))
+        if ((uiPhaseTimer == 0 && HealthBelowPct(75))
+            || (uiPhaseTimer == 1 && HealthBelowPct(50))
+            || (uiPhaseTimer == 2 && HealthBelowPct(25)))
         {
-            ++Phase_Time;
+            ++uiPhaseTimer;
 
-            Summoned_Guardian = false;
-            Summoned_Venomancer = false;
-            Summoned_Datter = false;
+            bGuardianSummoned = false;
+            bVenomancerSummoned = false;
+            bDatterSummoned = false;
 
-            UNDERGROUND_Timer = 40000;
-            VENOMANCER_Timer = 25000;
-            DATTER_Timer = 32000;
+            uiUndergroundTimer = 40*IN_MILISECONDS;
+            uiVenomancerTimer = 25*IN_MILISECONDS;
+            uiDatterTimer = 32*IN_MILISECONDS;
 
             DoCast(m_creature, SPELL_SUBMERGE, false);
 
             m_creature->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE|UNIT_FLAG_NOT_SELECTABLE);
 
-            Phase = 1;
+            uiPhase = 1;
         }
 
-        if (Phase != 1)
+        if (uiPhase != 1)
             DoMeleeAttackIfReady();
     }
     
