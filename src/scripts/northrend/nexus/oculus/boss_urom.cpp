69579e38a5556abbbfa75a0048198f4ea6b177a9
Tartalo <none@none>
2010-03-27 22:32:17 +0100

    Nexus, The Oculus: Cleanup
    --HG--
    branch : trunk

--- src/scripts/northrend/nexus/oculus/boss_urom.cpp
+++ src/scripts/northrend/nexus/oculus/boss_urom.cpp
@@ -1,43 +1,48 @@
-/* Script Data Start
-SDName: Boss urom
-SDAuthor: LordVanMartin
-SD%Complete:  1%
-SDComment:
-SDCategory:
-Script Data End */
+/* Copyright (C) 2008 - 2010 TrinityCore <http://www.trinitycore.org>
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 
-/*** SQL START ***
-update creature_template set scriptname = '' where entry = '';
-*** SQL END ***/
 #include "ScriptedPch.h"
 #include "oculus.h"
 
 enum Spells
 {
-    SPELL_ARCANE_SHIELD                      = 53813, //Dummy --> Channeled, shields the caster from damage.
-    SPELL_EMPOWERED_ARCANE_EXPLOSION         = 51110,
-    SPELL_EMPOWERED_ARCANE_EXPLOSION_2       = 59377,
-    SPELL_FROSTBOMB                          = 51103, //Urom throws a bomb, hitting its target with the highest aggro which inflict directly 650 frost damage and drops a frost zone on the ground. This zone deals 650 frost damage per second and reduce the movement speed by 35%. Lasts 1 minute.
-    SPELL_SUMMON_MENAGERIE                   = 50476, //Summons an assortment of creatures and teleports the caster to safety.
-    SPELL_SUMMON_MENAGERIE_2                 = 50495,
-    SPELL_SUMMON_MENAGERIE_3                 = 50496,
-    SPELL_TELEPORT                           = 51112, //Teleports to the center of Oculus
-    SPELL_TIME_BOMB                          = 51121, //Deals arcane damage to a random player, and after 6 seconds, deals zone damage to nearby equal to the health missing of the target afflicted by the debuff.
-    SPELL_TIME_BOMB_2                        = 59376
+    SPELL_ARCANE_SHIELD                           = 53813, //Dummy --> Channeled, shields the caster from damage.
+    SPELL_EMPOWERED_ARCANE_EXPLOSION              = 51110,
+    SPELL_EMPOWERED_ARCANE_EXPLOSION_2            = 59377,
+    SPELL_FROSTBOMB                               = 51103, //Urom throws a bomb, hitting its target with the highest aggro which inflict directly 650 frost damage and drops a frost zone on the ground. This zone deals 650 frost damage per second and reduce the movement speed by 35%. Lasts 1 minute.
+    SPELL_SUMMON_MENAGERIE                        = 50476, //Summons an assortment of creatures and teleports the caster to safety.
+    SPELL_SUMMON_MENAGERIE_2                      = 50495,
+    SPELL_SUMMON_MENAGERIE_3                      = 50496,
+    SPELL_TELEPORT                                = 51112, //Teleports to the center of Oculus
+    SPELL_TIME_BOMB                               = 51121, //Deals arcane damage to a random player, and after 6 seconds, deals zone damage to nearby equal to the health missing of the target afflicted by the debuff.
+    SPELL_TIME_BOMB_2                             = 59376
 };
 //not in db
 enum Yells
 {
-    SAY_AGGRO                             = -1578012,
-    SAY_KILL_1                            = -1578013,
-    SAY_KILL_2                            = -1578014,
-    SAY_KILL_3                            = -1578015,
-    SAY_DEATH                             = -1578016,
-    SAY_EXPLOSION_1                       = -1578017,
-    SAY_EXPLOSION_2                       = -1578018,
-    SAY_SUMMON_1                          = -1578019,
-    SAY_SUMMON_2                          = -1578020,
-    SAY_SUMMON_3                          = -1578021
+    SAY_AGGRO                                     = -1578012,
+    SAY_KILL_1                                    = -1578013,
+    SAY_KILL_2                                    = -1578014,
+    SAY_KILL_3                                    = -1578015,
+    SAY_DEATH                                     = -1578016,
+    SAY_EXPLOSION_1                               = -1578017,
+    SAY_EXPLOSION_2                               = -1578018,
+    SAY_SUMMON_1                                  = -1578019,
+    SAY_SUMMON_2                                  = -1578020,
+    SAY_SUMMON_3                                  = -1578021
 };
 
 struct boss_uromAI : public ScriptedAI
