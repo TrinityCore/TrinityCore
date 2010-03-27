eb8415fd6a8b60ef0b39bedf13ab378f1719e14e
Tartalo <none@none>
2010-03-27 22:32:17 +0100

    Nexus, The Oculus: Cleanup
    --HG--
    branch : trunk

--- src/scripts/northrend/nexus/oculus/boss_varos.cpp
+++ src/scripts/northrend/nexus/oculus/boss_varos.cpp
@@ -1,41 +1,46 @@
-/* Script Data Start
-SDName: Boss varos
-SDAuthor: LordVanMartin
-SD%Complete:
-SDComment:
-SDCategory:
-Script Data End */
+/* Copyright (C) 2008 - 2010 TrinityCore <http://www.trinitycore.org>
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 
-/*** SQL START ***
-update creature_template set scriptname = '' where entry = '';
-*** SQL END ***/
 #include "ScriptedPch.h"
 #include "oculus.h"
 
 enum Spells
 {
-    SPELL_ENERGIZE_CORES                      = 50785, //Damage 5938 to 6562, effec2 Triggers 54069, effect3 Triggers 56251
-    SPELL_ENERGIZE_CORES_TRIGGER_1            = 54069,
-    SPELL_ENERGIZE_CORES_TRIGGER_2            = 56251,
-    SPELL_ENERGIZE_CORES_2                    = 59372, //Damage 9025 to 9975, effect2 Triggers 54069, effect 56251
-    SPELL_CALL_AZURE_RING_CAPTAIN             = 51002, //Effect    Send Event (12229)
-    SPELL_CALL_AZURE_RING_CAPTAIN_2           = 51006, //Effect    Send Event (10665)
-    SPELL_CALL_AZURE_RING_CAPTAIN_3           = 51007, //Effect    Send Event (18454)
-    SPELL_CALL_AZURE_RING_CAPTAIN_4           = 51008, //Effect    Send Event (18455)
-    SPELL_CALL_AMPLIFY_MAGIC                  = 51054,
-    SPELL_CALL_AMPLIFY_MAGIC_2                = 59371
+    SPELL_ENERGIZE_CORES                          = 50785, //Damage 5938 to 6562, effec2 Triggers 54069, effect3 Triggers 56251
+    SPELL_ENERGIZE_CORES_TRIGGER_1                = 54069,
+    SPELL_ENERGIZE_CORES_TRIGGER_2                = 56251,
+    SPELL_ENERGIZE_CORES_2                        = 59372, //Damage 9025 to 9975, effect2 Triggers 54069, effect 56251
+    SPELL_CALL_AZURE_RING_CAPTAIN                 = 51002, //Effect    Send Event (12229)
+    SPELL_CALL_AZURE_RING_CAPTAIN_2               = 51006, //Effect    Send Event (10665)
+    SPELL_CALL_AZURE_RING_CAPTAIN_3               = 51007, //Effect    Send Event (18454)
+    SPELL_CALL_AZURE_RING_CAPTAIN_4               = 51008, //Effect    Send Event (18455)
+    SPELL_CALL_AMPLIFY_MAGIC                      = 51054,
+    SPELL_CALL_AMPLIFY_MAGIC_2                    = 59371
 };
 //not in db
 enum Yells
 {
-    SAY_AGGRO                              = -1578022,
-    SAY_KILL_1                             = -1578023,
-    SAY_KILL_2                             = -1578024,
-    SAY_DEATH                              = -1578025,
-    SAY_STRIKE_1                           = -1578026,
-    SAY_STRIKE_2                           = -1578027,
-    SAY_STRIKE_3                           = -1578028,
-    SAY_SPAWN                              = -1578029
+    SAY_AGGRO                                     = -1578022,
+    SAY_KILL_1                                    = -1578023,
+    SAY_KILL_2                                    = -1578024,
+    SAY_DEATH                                     = -1578025,
+    SAY_STRIKE_1                                  = -1578026,
+    SAY_STRIKE_2                                  = -1578027,
+    SAY_STRIKE_3                                  = -1578028,
+    SAY_SPAWN                                     = -1578029
 };
 
 struct boss_varosAI : public ScriptedAI
