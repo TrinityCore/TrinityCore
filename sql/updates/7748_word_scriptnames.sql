2fd88cbe9576f568820160649e72c39d9599922a
_manuel_ <manue.l@live.com.ar>
2010-04-02 16:13:05 -0300

    Implemented: * Script at_aldurthar_gate for quests 13315 and 13351. * Script at_warsong_farms for quest 11686. * Script at_stormwright_shelf for quest 12741.
    Code from ScriptDev2.
    
    Before execute sql updates, apply TDB fix. New Fix Submissions > Core related DB content
    
    --HG--
    branch : trunk

---
+++ sql/updates/7748_word_scriptnames.sql
@@ -0,0 +1,3 @@
+UPDATE `areatrigger_scripts` SET `ScriptName`='at_aldurthar_gate' WHERE `entry` IN (5284,5285,5286,5287);
+UPDATE `areatrigger_scripts` SET `ScriptName`='at_warsong_farms' WHERE `entry` IN (4871,4872,4873);
+UPDATE `areatrigger_scripts` SET `ScriptName`='at_stormwright_shelf' WHERE `entry`=5108;
