f871e12fa4930af3b66c88996189e52e1ae2b23f
Tartalo <none@none>
2010-04-06 21:39:16 +0200

    Eastern Kingdoms, Alterac Valley: Register scripts on db
    --HG--
    branch : trunk

--- sql/FULL/world_scripts_full.sql
+++ sql/FULL/world_scripts_full.sql
@@ -163,6 +163,13 @@ UPDATE `creature_template` SET `ScriptName`='npc_wormhole' WHERE `entry`='35646'
 
 /* ALTERAC VALLEY */
 
+UPDATE `creature_template` SET `ScriptName`= 'boss_drekthar' WHERE `entry`=11946;
+UPDATE `creature_template` SET `ScriptName`= 'boss_vanndar' WHERE `entry`=11948;
+UPDATE `creature_template` SET `ScriptName`= 'boss_balinda' WHERE `entry`=11949;
+UPDATE `creature_template` SET `ScriptName`= 'mob_water_elemental' WHERE `entry`=25040;
+UPDATE `creature_template` SET `ScriptName`= 'boss_galvangar' WHERE `entry`=11947;
+UPDATE `creature_template` SET `ScriptName`= 'mob_av_marshal_or_warmaster' WHERE `entry` IN (14762,14763,14764,14765,14772,14773,14776,14777);
+
 /* ARATHI HIGHLANDS */
 UPDATE `creature_template` SET `ScriptName`='npc_professor_phizzlethorpe' WHERE `entry`=2768;
 
