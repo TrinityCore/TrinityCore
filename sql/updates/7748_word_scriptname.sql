4cb1197be7346125559f703b3d77da1f93729eed
teacher <none@none>
2010-04-08 16:29:29 +0200

    Added serverside spell as place holders for future development. Added some vehicleIDs and creature_template spells (by Xanadu and Mikebeck). Some fixes for proper sql format.
    --HG--
    branch : trunk

--- sql/updates/7748_word_scriptname.sql
+++ sql/updates/7748_word_scriptname.sql
@@ -1,3 +1,10 @@
-UPDATE `areatrigger_scripts` SET `ScriptName`='at_aldurthar_gate' WHERE `entry` IN (5284,5285,5286,5287);
-UPDATE `areatrigger_scripts` SET `ScriptName`='at_warsong_farms' WHERE `entry` IN (4871,4872,4873);
-UPDATE `areatrigger_scripts` SET `ScriptName`='at_stormwright_shelf' WHERE `entry`=5108;
+DELETE FROM `areatrigger_scripts` WHERE `entry` IN (5284,5285,5286,5287,4871,4872,4873,5108);
+INSERT INTO `areatrigger_scripts` (`entry`,`ScriptName`) VALUES 
+(5284, 'at_aldurthar_gate'),
+(5285, 'at_aldurthar_gate'),
+(5286, 'at_aldurthar_gate'),
+(5287, 'at_aldurthar_gate'),
+(4871, 'at_warsong_farms'),
+(4872, 'at_warsong_farms'),
+(4873, 'at_warsong_farms'),
+(5108, 'at_stormwright_shelf');
