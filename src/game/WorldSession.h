71724c27961fa438848e6c9c715fc1caa6d6b8cc
n0n4m3 <none@none>
2010-04-11 11:18:20 +0400

    Implement some opcodes. Big thx to TOM_RUS.
    --HG--
    branch : trunk

--- src/game/WorldSession.h
+++ src/game/WorldSession.h
@@ -76,22 +76,49 @@ struct AccountData
 enum PartyOperation
 {
     PARTY_OP_INVITE = 0,
-    PARTY_OP_LEAVE = 2
+    PARTY_OP_LEAVE = 2,
+    PARTY_OP_SWAP = 4
 };
 
 enum PartyResult
 {
-    PARTY_RESULT_OK                   = 0,
-    PARTY_RESULT_CANT_FIND_TARGET     = 1,
-    PARTY_RESULT_NOT_IN_YOUR_PARTY    = 2,
-    PARTY_RESULT_NOT_IN_YOUR_INSTANCE = 3,
-    PARTY_RESULT_PARTY_FULL           = 4,
-    PARTY_RESULT_ALREADY_IN_GROUP     = 5,
-    PARTY_RESULT_YOU_NOT_IN_GROUP     = 6,
-    PARTY_RESULT_YOU_NOT_LEADER       = 7,
-    PARTY_RESULT_TARGET_UNFRIENDLY    = 8,
-    PARTY_RESULT_TARGET_IGNORE_YOU    = 9,
-    PARTY_RESULT_INVITE_RESTRICTED    = 13
+    ERR_PARTY_RESULT_OK                 = 0,
+    ERR_BAD_PLAYER_NAME_S               = 1,
+    ERR_TARGET_NOT_IN_GROUP_S           = 2,
+    ERR_TARGET_NOT_IN_INSTANCE_S        = 3,
+    ERR_GROUP_FULL                      = 4,
+    ERR_ALREADY_IN_GROUP_S              = 5,
+    ERR_NOT_IN_GROUP                    = 6,
+    ERR_NOT_LEADER                      = 7,
+    ERR_PLAYER_WRONG_FACTION            = 8,
+    ERR_IGNORING_YOU_S                  = 9,
+    ERR_LFG_PENDING                     = 12,
+    ERR_INVITE_RESTRICTED               = 13,
+    ERR_GROUP_SWAP_FAILED               = 14,               // if (PartyOperation == PARTY_OP_SWAP) ERR_GROUP_SWAP_FAILED else ERR_INVITE_IN_COMBAT
+    ERR_INVITE_UNKNOWN_REALM            = 15,
+    ERR_INVITE_NO_PARTY_SERVER          = 16,
+    ERR_INVITE_PARTY_BUSY               = 17,
+    ERR_PARTY_TARGET_AMBIGUOUS          = 18,
+    ERR_PARTY_LFG_INVITE_RAID_LOCKED    = 19,
+    ERR_PARTY_LFG_BOOT_LIMIT            = 20,
+    ERR_PARTY_LFG_BOOT_COOLDOWN_S       = 21,
+    ERR_PARTY_LFG_BOOT_IN_PROGRESS      = 22,
+    ERR_PARTY_LFG_BOOT_TOO_FEW_PLAYERS  = 23,
+    ERR_PARTY_LFG_BOOT_NOT_ELIGIBLE_S   = 24,
+    ERR_RAID_DISALLOWED_BY_LEVEL        = 25,
+    ERR_PARTY_LFG_BOOT_IN_COMBAT        = 26,
+    ERR_VOTE_KICK_REASON_NEEDED         = 27,
+    ERR_PARTY_LFG_BOOT_DUNGEON_COMPLETE = 28,
+    ERR_PARTY_LFG_BOOT_LOOT_ROLLS       = 29,
+    ERR_PARTY_LFG_TELEPORT_IN_COMBAT    = 30
+};
+
+enum ChatRestrictionType
+{
+    ERR_CHAT_RESTRICTED = 0,
+    ERR_CHAT_THROTTLED  = 1,
+    ERR_USER_SQUELCHED  = 2,
+    ERR_YELL_RESTRICTED = 3
 };
 
 /// Player session in the World
@@ -555,6 +582,9 @@ class WorldSession
         bool processChatmessageFurtherAfterSecurityChecks(std::string&, uint32);
         void HandleMessagechatOpcode(WorldPacket& recvPacket);
         void SendPlayerNotFoundNotice(std::string name);
+        void SendPlayerAmbiguousNotice(std::string name);
+        void SendWrongFactionNotice();
+        void SendChatRestrictedNotice(ChatRestrictionType restriction);
         void HandleTextEmoteOpcode(WorldPacket& recvPacket);
         void HandleChatIgnoredOpcode(WorldPacket& recvPacket);
 
