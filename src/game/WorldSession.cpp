34718bfbd93ec89bec0d7be977d80a468004a292
click <none@none>
2010-04-12 14:24:10 +0200

    * Prevent possible memory leak in WorldSession (correction of order) Fix by Derex
    --HG--
    branch : trunk

--- src/game/WorldSession.cpp
+++ src/game/WorldSession.cpp
@@ -173,7 +173,7 @@ bool WorldSession::Update(uint32 /*diff*/)
     ///- Retrieve packets from the receive queue and call the appropriate handlers
     /// not proccess packets if socket already closed
     WorldPacket* packet;
-    while (_recvQueue.next(packet) && m_Socket && !m_Socket->IsClosed ())
+    while (m_Socket && !m_Socket->IsClosed() && _recvQueue.next(packet))
     {
         /*#if 1
         sLog.outError("MOEP: %s (0x%.4X)",
