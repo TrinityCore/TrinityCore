c1342997d535bdd8debee7d06630d9a831807df9
Trazom62 <none@none>
2010-04-25 21:31:55 +0200

    Fix resilience update for 3.3.x. Thanks dr.tenma. Fixes issue #198.
    --HG--
    branch : trunk

--- src/game/SpellAuraEffects.cpp
+++ src/game/SpellAuraEffects.cpp
@@ -1295,9 +1295,15 @@ void AuraEffect::PeriodicTick(Unit * target, Unit * caster) const
                 damage -= target->GetSpellCritDamageReduction(damage);
             }
 
-            // only from players
-            if (IS_PLAYER_GUID(GetCasterGUID()))
-                damage -= target->GetSpellDamageReduction(damage);
+            // Reduce damage from resilience for players and pets only.
+            // As of patch 3.3 pets inherit 100% of master resilience.
+            if (caster->GetSpellModOwner())
+                if (Player* modOwner = target->GetSpellModOwner())
+                {
+                    if (crit)
+                        damage -= modOwner->GetSpellCritDamageReduction(damage);
+                    damage -= modOwner->GetSpellDamageReduction(damage);
+                }
 
             caster->CalcAbsorbResist(target, GetSpellSchoolMask(GetSpellProto()), DOT, damage, &absorb, &resist, m_spellProto);
 
@@ -1368,9 +1374,15 @@ void AuraEffect::PeriodicTick(Unit * target, Unit * caster) const
                 damage = damageReductedArmor;
             }
 
-            // Reduce dot damage from resilience for players.
-            if (target->GetTypeId() == TYPEID_PLAYER)
-                damage-=target->GetSpellDamageReduction(damage);
+            // Reduce damage from resilience for players and pets only.
+            // As of patch 3.3 pets inherit 100% of master resilience.
+            if (caster->GetSpellModOwner())
+                if (Player* modOwner = target->GetSpellModOwner())
+                {
+                    if (crit)
+                        damage -= modOwner->GetSpellCritDamageReduction(damage);
+                    damage -= modOwner->GetSpellDamageReduction(damage);
+                }
 
             caster->CalcAbsorbResist(target, GetSpellSchoolMask(GetSpellProto()), DOT, damage, &absorb, &resist, m_spellProto);
 
