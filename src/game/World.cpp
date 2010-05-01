84616334e383bd74c7ccc554a50982b821b04f74
Spp <none@none>
2010-05-02 00:46:35 +0200

    Fix local change that wasn't supposed to be pushed...
    --HG--
    branch : trunk

--- src/game/World.cpp
+++ src/game/World.cpp
@@ -1711,7 +1711,7 @@ void World::SetInitialWorldSettings()
 
 void World::DetectDBCLang()
 {
-    uint16 m_lang_confid = sConfig.GetIntDefault("DBC.Locale", 255);
+    uint8 m_lang_confid = sConfig.GetIntDefault("DBC.Locale", 255);
 
     if (m_lang_confid != 255 && m_lang_confid >= MAX_LOCALE)
     {
