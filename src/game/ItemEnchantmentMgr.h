d6c056d6789525f645f1c2b453b7f66f04bf7852
Spp <none@none>
2010-05-04 17:51:11 +0200

    Add support to set RandomProperty / RandomSuffix to -1 in item_template
    --HG--
    branch : trunk

--- src/game/ItemEnchantmentMgr.h
+++ src/game/ItemEnchantmentMgr.h
@@ -24,7 +24,7 @@
 #include "Common.h"
 
 void LoadRandomEnchantmentsTable();
-uint32 GetItemEnchantMod(uint32 entry);
+uint32 GetItemEnchantMod(int32 entry);
 uint32 GenerateEnchSuffixFactor(uint32 item_id);
 #endif
 
