a525986976678afb6536f625746183e3ab46af20
Brian <runningnak3d@gmail.com>
2010-05-06 02:13:15 -0600

    * Fix the damn everloving neutral assist once again * Patch coutesy of PivoNRoll
    --HG--
    branch : trunk

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -11510,6 +11510,9 @@ bool Unit::canAttack(Unit const* target, bool force) const
     {
         if (IsFriendlyTo(target))
             return false;
+        if (GetTypeId()!=TYPEID_PLAYER)
+            if (!IsHostileTo(target))
+                return false;
     }
     else if (!IsHostileTo(target))
         return false;
