899736d89c3ad7bf26603ef96ae8351cf9810d2c
_manuel_ <manue.l@live.com.ar>
2010-05-13 11:09:10 -0300

    Changes in scripts for quest chain The Amphitheater of Anguish:
    - Added blizzlike content, like texts.
    - Corrected some spells and timers.
    - As i saw in offi can be more than 1 player doing different parts of the chain at the same time.
    - Added code to prevent the npc gets out of the ring.
    - Added support for The Amphitheater of Anguish: From Beyond.
    
    --HG--
    branch : trunk

---
+++ sql/updates/8179_world_script_texts.sql
@@ -0,0 +1,5 @@
+DELETE FROM `script_texts` WHERE `entry` IN (-1571039,-1571040,-1571041);
+INSERT INTO `script_texts` (`npc_entry`,`entry`,`content_default`,`content_loc1`,`content_loc2`,`content_loc3`,`content_loc4`,`content_loc5`,`content_loc6`,`content_loc7`,`content_loc8`,`sound`,`type`,`language`,`emote`,`comment`) VALUES
+   (30014,-1571039,'Yggdras emerges!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,3,0,0,''),
+   (30017,-1571040,'Stinkbeard comin'' for you, little ones!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,0,0,''),
+   (30007,-1571041,'Do you fell that folks? The air is cracking with energy! Than can only mean one thing...',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,0,0,'');
