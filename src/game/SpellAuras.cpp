0f0619ae0bb548f2b35c1a0e3b5286a083f331d4
Anubisss <none@none>
2010-05-16 18:49:51 +0200

    Fix spell Improved Devouring Plague's damage.
    Core part of the patch by PrinceCreed.
    SQL part of the patch by sinka.isi.
    
    Fixes issue #1965.
    Closes issue #1004.
    
    --HG--
    branch : trunk

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -976,7 +976,7 @@ void Aura::HandleAuraSpecificMods(AuraApplication const * aurApp, Unit * caster,
                     // Improved Devouring Plague
                     if (AuraEffect const * aurEff = caster->GetDummyAuraEffect(SPELLFAMILY_PRIEST, 3790, 1))
                     {
-                        int32 basepoints0 = aurEff->GetAmount() * GetEffect(0)->GetTotalTicks() * GetEffect(0)->GetAmount() / 100;
+                        int32 basepoints0 = aurEff->GetAmount() * GetEffect(0)->GetTotalTicks() * caster->SpellDamageBonus(target, GetSpellProto(), GetEffect(0)->GetAmount(), DOT) / 100;
                         caster->CastCustomSpell(target, 63675, &basepoints0, NULL, NULL, true, NULL, GetEffect(0));
                     }
                 }
