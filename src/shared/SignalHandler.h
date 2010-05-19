f871350bbdcd7db76e610e3462c93400c145984d
Anubisss <none@none>
2010-05-19 21:16:59 +0200

    Implement a signalhandler via ACE.
    Thanks to Diver for test compiling under WIN.
    
    --HG--
    branch : trunk

---
+++ src/shared/SignalHandler.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2005-2010 MaNGOS <http://getmangos.com/>
+ *
+ * Copyright (C) 2008-2010 Trinity <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef __SIGNAL_HANDLER_H__
+#define __SIGNAL_HANDLER_H__
+
+#include <ace/Event_Handler.h>
+
+namespace Trinity
+{
+
+/// Handle termination signals
+class SignalHandler : public ACE_Event_Handler
+{
+    public:
+        int handle_signal(int SigNum, siginfo_t* = NULL, ucontext_t* = NULL)
+        {
+            HandleSignal(SigNum);
+            return 0;
+        }
+        virtual void HandleSignal(int SigNum) {};
+};
+
+}
+
+#endif /* __SIGNAL_HANDLER_H__ */
