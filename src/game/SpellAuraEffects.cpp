7abdbf01b134e33a3b6fc3065d4a5cda25204c3d
Anubisss <none@none>
2010-05-20 22:01:45 +0200

    Fix talent Survival of the Fittest.
    Thanks to Aokromes for helping in the tests.
    
    Fixes issue #2034.
    
    --HG--
    branch : trunk

--- src/game/SpellAuraEffects.cpp
+++ src/game/SpellAuraEffects.cpp
@@ -2526,8 +2526,8 @@ void AuraEffect::HandleShapeshiftBoosts(Unit * target, bool apply) const
                     // Survival of the Fittest
                     if (AuraEffect const * aurEff = target->GetAuraEffect(SPELL_AURA_MOD_TOTAL_STAT_PERCENTAGE,SPELLFAMILY_DRUID, 961, 0))
                     {
-                        int32 bp = target->CalculateSpellDamage(target, aurEff->GetSpellProto(),2);
-                        target->CastCustomSpell(target, 62069,&bp, NULL, NULL, true, 0, this);
+                        int32 bp = 100 + aurEff->GetSpellProto()->CalculateSimpleValue(2);
+                        target->CastCustomSpell(target, 62069, &bp, NULL, NULL, true, 0, this);
                     }
                 break;
                 case FORM_MOONKIN:
