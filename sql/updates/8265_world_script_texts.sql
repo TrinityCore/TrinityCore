b3bd094785f24d8383ac0cf40fcab5b10c41e635
John Holiver <none@none>
2010-05-22 15:26:38 -0300

    Review of Onyxia's Lair Make it 95% perfect to LK version (fixes spawns, eruption, achievements and some more)
    Fixes issue #1706
    Fixes issue #1736d
    
    --HG--
    branch : trunk

---
+++ sql/updates/8265_world_script_texts.sql
@@ -0,0 +1,1 @@
+UPDATE `script_texts` SET `type`=3,`content_default`='%s takes in a deep breath...' WHERE `entry`=-1249004;
---
+++ sql/updates/8265_world_scriptname.sql
@@ -0,0 +1,2 @@
+UPDATE `instance_template` SET `script`='instance_onyxias_lair' WHERE `map`=249;
+UPDATE `creature_template` SET `AIName`='EventAI' WHERE `entry`=36561;
