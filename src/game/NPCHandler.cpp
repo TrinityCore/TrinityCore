09a455dcd1814e71da94aedd8f5325275a47a772
Liberate <none@none>
2010-05-24 21:10:48 +0200

    *Fix: Only show quests in the first page of gossip, instead of every page.
    --HG--
    branch : trunk

--- src/game/NPCHandler.cpp
+++ src/game/NPCHandler.cpp
@@ -333,7 +333,7 @@ void WorldSession::HandleGossipHelloOpcode(WorldPacket & recv_data)
     if (!sScriptMgr.GossipHello(_player, unit))
     {
 //        _player->TalkedToCreature(unit->GetEntry(), unit->GetGUID());
-        _player->PrepareGossipMenu(unit, unit->GetCreatureInfo()->GossipMenuId);
+        _player->PrepareGossipMenu(unit, unit->GetCreatureInfo()->GossipMenuId, true);
         _player->SendPreparedGossip(unit);
     }
 }
