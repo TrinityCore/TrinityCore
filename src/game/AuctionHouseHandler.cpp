680aed787ac2e57e857e41eb2716f9f595717582
click <none@none>
2010-05-25 16:15:22 +0200

    Slap auctioneers in the face telling them to search for items with partial names or suffixes - patch by MrSmite Will require more testing, but base functionality is there. Fixes issue #1510. Closes issue #1534. + Skip some excess (unused) packetdata sent from client during AH-usage (click)
    --HG--
    branch : trunk

--- src/game/AuctionHouseHandler.cpp
+++ src/game/AuctionHouseHandler.cpp
@@ -601,6 +601,8 @@ void WorldSession::HandleAuctionListItems(WorldPacket & recv_data)
     recv_data >> auctionSlotID >> auctionMainCategory >> auctionSubCategory;
     recv_data >> quality >> usable;
 
+    recv_data.read_skip(16);                                // unknown 16 bytes: 00 07 01 00 00 01 05 00 06 00 09 01 08 00 03 00
+
     Creature *pCreature = GetPlayer()->GetNPCIfCanInteractWith(guid,UNIT_NPC_FLAG_AUCTIONEER);
     if (!pCreature)
     {
@@ -643,7 +645,8 @@ void WorldSession::HandleAuctionListItems(WorldPacket & recv_data)
 void WorldSession::HandleAuctionListPendingSales(WorldPacket & recv_data)
 {
     sLog.outDebug("CMSG_AUCTION_LIST_PENDING_SALES");
-    recv_data.hexlike();
+
+    recv_data.read_skip<uint64>();
 
     uint32 count = 0;
 
