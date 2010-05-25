b362718b44af6992bcfa9aed4cf5be92ce68669f
Spp <spp@jorge.gr>
2010-05-25 04:29:59 +0200

    Add some missing %s on final dk quest. Patch by Aokromes
    --HG--
    branch : trunk

---
+++ sql/updates/8287_world_script_texts.sql
@@ -0,0 +1,14 @@
+UPDATE `script_texts` SET `content_default`='%s flee' WHERE `entry`='-1609272';
+UPDATE `script_texts` SET `content_default`='%s kneels in defeat before Tirion Fordring.' WHERE `entry`='-1609273';
+UPDATE `script_texts` SET `content_default`='%s arrives.' WHERE `entry`='-1609274';
+UPDATE `script_texts` SET `content_default`='%s becomes a shade of his past, and walks up to his father.' WHERE `entry`='-1609275';
+UPDATE `script_texts` SET `content_default`='%s hugs his father.' WHERE `entry`='-1609276';
+UPDATE `script_texts` SET `content_default`='%s disappears, and the Lich King appears.' WHERE `entry`='-1609277';
+UPDATE `script_texts` SET `content_default`='%s becomes himself again...and is now angry.' WHERE `entry`='-1609278';
+UPDATE `script_texts` SET `content_default`='%s casts a spell on Tirion.' WHERE `entry`='-1609279';
+UPDATE `script_texts` SET `content_default`='%s gasps for air.' WHERE `entry`='-1609280';
+UPDATE `script_texts` SET `content_default`='%s casts a powerful spell, killing the Defenders and knocking back the others.' WHERE `entry`='-1609281';
+UPDATE `script_texts` SET `content_default`='%s throws the Corrupted Ashbringer to Tirion, who catches it. Tirion becomes awash with Light, and the Ashbringer is cleansed.' WHERE `entry`='-1609282';
+UPDATE `script_texts` SET `content_default`='%s collapses.' WHERE `entry`='-1609283';
+UPDATE `script_texts` SET `content_default`='%s charges towards the Lich King, Ashbringer in hand and strikes the Lich King.' WHERE `entry`='-1609284';
+UPDATE `script_texts` SET `content_default`='%s disappears. Tirion walks over to where Darion lay' WHERE `entry`='-1609285';
