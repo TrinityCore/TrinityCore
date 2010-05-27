f966a928483c3e23ad919dc9fbd838c69c6d1654
Liberate <none@none>
2010-05-27 16:15:23 +0200

    Fixes a mistake in my previous commit regarding Mirror Image. Fire Blast should scale with 15% of the spell power, not 30%. Thanks A.Metaphysical.Drama for pointing this out. Also fixes a double entry in world.sql.
    --HG--
    branch : trunk

---
+++ sql/updates/8318_world_spell_bonus_data.sql
@@ -0,0 +1,2 @@
+DELETE FROM `spell_bonus_data` WHERE `entry`=59637;
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES (59637, 0.15, -1, 0, -1, 'Mage - Mirror Image Fire Blast');
