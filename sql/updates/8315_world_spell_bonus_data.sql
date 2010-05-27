127ed3499083d17b1d4e334bd685175808d0564c
Liberate <none@none>
2010-05-27 11:02:44 +0200

    Updates the Judgement of Light proc heal to the current patch. It does not scale with attack power and spell power anymore.
    --HG--
    branch : trunk

---
+++ sql/updates/8315_world_spell_bonus_data.sql
@@ -0,0 +1,2 @@
+DELETE FROM `spell_bonus_data` WHERE `entry` = 20267;
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES (20267, 0, 0, 0, 0, 'Paladin - Judgement of Light Proc');
