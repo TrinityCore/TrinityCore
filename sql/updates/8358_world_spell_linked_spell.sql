768952ae7105063b61148a5b177b453de3a564f1
John Holiver <none@none>
2010-06-01 00:37:59 -0300

    Added missing delete from r34f4db8886. Thnx kolapse01.
    --HG--
    branch : trunk

--- sql/updates/8358_world_spell_linked_spell.sql
+++ sql/updates/8358_world_spell_linked_spell.sql
@@ -1,3 +1,4 @@
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger` IN (69381,69378,69377);
 INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES 
 (69381, 72588, 1, 'Drums of the Wild'),
 (69378, 72586, 1, 'Drums of the Forgotten Kings'),
