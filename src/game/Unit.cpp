83aa24347cb2810f2221e742f6782ecc56143a67
John Holiver <none@none>
2010-06-02 18:30:57 -0300

    * Fix Glyph of Mend Pet (tnx L1ghtGu4rd from Mangos)
    --HG--
    branch : trunk

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -6606,6 +6606,12 @@ bool Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, AuraEffect* trigger
                 }
                 break;
             }
+            // Glyph of Mend Pet
+            if(dummySpell->Id == 57870)
+            {
+                pVictim->CastSpell(pVictim, 57894, true, NULL, NULL, GetGUID());
+                return true;
+            }
             break;
         }
         case SPELLFAMILY_PALADIN:
