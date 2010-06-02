c0f2a0eac02d47b22745151bd681fe7fd5927e5b
John Holiver <none@none>
2010-06-02 18:37:05 -0300

    * Fix Shaman T10 2 part bonus procing only for Riptide now
    --HG--
    branch : trunk

---
+++ sql/updates/8373_spell_proc_event.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_proc_event` WHERE `entry` IN (70807);
+INSERT INTO `spell_proc_event` (`entry`,`SchoolMask`,`SpellFamilyName`,`SpellFamilyMask0`,`SpellFamilyMask1`,`SpellFamilyMask2`,`procFlags`,`procEx`,`ppmRate`,`CustomChance`,`Cooldown`) VALUES 
+( 70807,    0,  11, 0x00000000, 0x00000000, 0x00000010, 0x00000000, 0x00000000,   0, 100,   0); -- Item - Shaman T10 Restoration 2P Bonus
