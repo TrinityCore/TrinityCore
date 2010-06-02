cba199fa204635bf69e4c16fff3de01ce43510c3
John Holiver <none@none>
2010-06-02 18:30:57 -0300

    * Fix Glyph of Mend Pet (tnx L1ghtGu4rd from Mangos)
    --HG--
    branch : trunk

---
+++ sql/updates/8372_spell_proc_event.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_proc_event` WHERE `entry` IN (57870);
+INSERT INTO `spell_proc_event` (`entry`,`SchoolMask`,`SpellFamilyName`,`SpellFamilyMask0`,`SpellFamilyMask1`,`SpellFamilyMask2`,`procFlags`,`procEx`,`ppmRate`,`CustomChance`,`Cooldown`) VALUES
+( 57870, 0x00,   9, 0x00800000, 0x00000000, 0x00000000, 0x00040000, 0x00000000,   0,   0,   0); -- Glyph of Mend Pet
