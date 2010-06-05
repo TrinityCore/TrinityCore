207dccc13de332674a53af5088daaa0ad58c3a3a
teacher <none@none>
2010-06-05 18:21:47 +0200

    Added conditions table to world.sql. Minor sql cleanup.
    --HG--
    branch : trunk

--- sql/world.sql
+++ sql/world.sql
@@ -705,6 +705,33 @@ INSERT INTO `command` VALUES
 /*!40000 ALTER TABLE `command` ENABLE KEYS */;
 UNLOCK TABLES;
 
+-- 
+-- Table structure for `conditions`
+-- 
+
+CREATE TABLE `conditions` (
+  `SourceTypeOrReferenceId` mediumint(8) NOT NULL DEFAULT '0',
+  `SourceGroup` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `SourceEntry` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `ElseGroup` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `ConditionTypeOrReference` mediumint(8) NOT NULL DEFAULT '0',
+  `ConditionValue1` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `ConditionValue2` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `ConditionValue3` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `ErrorTextId` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `Comment` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`SourceTypeOrReferenceId`,`SourceGroup`,`SourceEntry`,`ElseGroup`,`ConditionTypeOrReference`,`ConditionValue1`,`ConditionValue2`,`ConditionValue3`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Condition System';
+
+--
+-- Dumping data for table `creature`
+--
+
+LOCK TABLES `conditions` WRITE;
+/*!40000 ALTER TABLE `conditions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `conditions` ENABLE KEYS */;
+UNLOCK TABLES;
+
 --
 -- Table structure for table `creature`
 --
