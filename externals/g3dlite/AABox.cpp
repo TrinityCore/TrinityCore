8670fbe112b0dae84f9cf877f20447ba12ceaedf
click <none@none>
2010-06-08 08:04:26 +0200

    Get g3dlib, zlib and jemalloc to build again
    --HG--
    branch : trunk
    rename : opt/cleanup/tab2spaces.sh => contrib/cleanup/tab2spaces.sh
    rename : opt/cleanup/whitespace.sh => contrib/cleanup/whitespace.sh
    rename : opt/conf_merge/README => contrib/conf_merge/README
    rename : opt/conf_merge/index.php => contrib/conf_merge/index.php
    rename : opt/conf_merge/merge.php => contrib/conf_merge/merge.php
    rename : doc/AuctionHouseBot.txt => docs/AuctionHouseBot.txt
    rename : doc/DocStructure.dox => docs/DocStructure.dox
    rename : doc/Doxyfile.in => docs/Doxyfile.in
    rename : doc/EventAI.txt => docs/EventAI.txt
    rename : doc/HowToScript.txt => docs/HowToScript.txt
    rename : doc/TextTables.txt => docs/TextTables.txt
    rename : doc/UnixInstall.txt => docs/UnixInstall.txt
    rename : externals/jemalloc/include/internal/arena.h => externals/jemalloc/jemalloc/internal/arena.h
    rename : externals/jemalloc/include/internal/base.h => externals/jemalloc/jemalloc/internal/base.h
    rename : externals/jemalloc/include/internal/chunk.h => externals/jemalloc/jemalloc/internal/chunk.h
    rename : externals/jemalloc/include/internal/chunk_dss.h => externals/jemalloc/jemalloc/internal/chunk_dss.h
    rename : externals/jemalloc/include/internal/chunk_mmap.h => externals/jemalloc/jemalloc/internal/chunk_mmap.h
    rename : externals/jemalloc/include/internal/chunk_swap.h => externals/jemalloc/jemalloc/internal/chunk_swap.h
    rename : externals/jemalloc/include/internal/ckh.h => externals/jemalloc/jemalloc/internal/ckh.h
    rename : externals/jemalloc/include/internal/ctl.h => externals/jemalloc/jemalloc/internal/ctl.h
    rename : externals/jemalloc/include/internal/extent.h => externals/jemalloc/jemalloc/internal/extent.h
    rename : externals/jemalloc/include/internal/hash.h => externals/jemalloc/jemalloc/internal/hash.h
    rename : externals/jemalloc/include/internal/huge.h => externals/jemalloc/jemalloc/internal/huge.h
    rename : externals/jemalloc/include/internal/jemalloc_internal.h => externals/jemalloc/jemalloc/internal/jemalloc_internal.h
    rename : externals/jemalloc/include/internal/jemalloc_internal.h.in => externals/jemalloc/jemalloc/internal/jemalloc_internal.h.in
    rename : externals/jemalloc/include/internal/mb.h => externals/jemalloc/jemalloc/internal/mb.h
    rename : externals/jemalloc/include/internal/mutex.h => externals/jemalloc/jemalloc/internal/mutex.h
    rename : externals/jemalloc/include/internal/prof.h => externals/jemalloc/jemalloc/internal/prof.h
    rename : externals/jemalloc/include/internal/ql.h => externals/jemalloc/jemalloc/internal/ql.h
    rename : externals/jemalloc/include/internal/qr.h => externals/jemalloc/jemalloc/internal/qr.h
    rename : externals/jemalloc/include/internal/rb.h => externals/jemalloc/jemalloc/internal/rb.h
    rename : externals/jemalloc/include/internal/stats.h => externals/jemalloc/jemalloc/internal/stats.h
    rename : externals/jemalloc/include/internal/tcache.h => externals/jemalloc/jemalloc/internal/tcache.h
    rename : externals/jemalloc/include/internal/totally_not_p_r_n.h => externals/jemalloc/jemalloc/internal/totally_not_p_r_n.h
    rename : externals/jemalloc/include/jemalloc.h => externals/jemalloc/jemalloc/jemalloc.h
    rename : externals/jemalloc/include/jemalloc.h.in => externals/jemalloc/jemalloc/jemalloc.h.in
    rename : externals/jemalloc/include/jemalloc_defs.h => externals/jemalloc/jemalloc/jemalloc_defs.h
    rename : externals/jemalloc/include/jemalloc_defs.h.in => externals/jemalloc/jemalloc/jemalloc_defs.h.in

---
+++ externals/g3dlite/AABox.cpp
@@ -0,0 +1,366 @@
+/**
+  @file AABox.cpp
+
+  @maintainer Morgan McGuire, http://graphics.cs.williams.edu
+
+  @created 2004-01-10
+  @edited  2006-01-11
+*/
+
+#include "G3D/platform.h"
+#include "G3D/AABox.h"
+#include "G3D/Box.h"
+#include "G3D/Plane.h"
+#include "G3D/Sphere.h"
+#include "G3D/BinaryInput.h"
+#include "G3D/BinaryOutput.h"
+
+
+namespace G3D {
+
+const AABox& AABox::maxFinite() {
+    static const AABox b = AABox(Vector3::minFinite(), 
+                                 Vector3::maxFinite());
+    return b;
+}
+
+
+const AABox& AABox::large() {
+    static const AABox b = AABox(Vector3::minFinite() * 0.5f, 
+                                 Vector3::maxFinite() * 0.5f);
+    return b;
+}
+
+
+const AABox& AABox::inf() {
+    static const AABox b = AABox(-Vector3::inf(), Vector3::inf());
+    return b;
+}
+
+
+const AABox& AABox::zero() {
+    static const AABox b = AABox(Vector3::zero(), Vector3::zero());
+    return b;
+}
+
+
+void AABox::serialize(class BinaryOutput& b) const {
+    b.writeVector3(lo);
+    b.writeVector3(hi);
+}
+
+
+void AABox::deserialize(class BinaryInput& b) {
+    lo = b.readVector3();
+    hi = b.readVector3();
+}
+
+
+void AABox::split(const Vector3::Axis& axis, float location, AABox& low, AABox& high) const {
+    // Low, medium, and high along the chosen axis
+    float L = G3D::min(location, lo[axis]);
+    float M = G3D::min(G3D::max(location, lo[axis]), hi[axis]);
+    float H = G3D::max(location, hi[axis]);
+
+    // Copy over this box.
+    high = low = *this;
+
+    // Now move the split points along the special axis
+    low.lo[axis] = L;
+    low.hi[axis] = M;
+    high.lo[axis] = M;
+    high.hi[axis] = H;
+}
+
+
+Vector3 AABox::randomSurfacePoint() const {
+    Vector3 extent = hi - lo;
+    float aXY = extent.x * extent.y;
+    float aYZ = extent.y * extent.z;
+    float aZX = extent.z * extent.x;
+
+    float r = (float)uniformRandom(0.0f, aXY + aYZ + aZX);
+
+    // Choose evenly between positive and negative face planes
+    float d = ((float)uniformRandom(0, 1) < 0.5f) ? 0.0f : 1.0f;
+
+    // The probability of choosing a given face is proportional to
+    // its area.
+    if (r < aXY) {
+        return 
+            lo + 
+            Vector3(
+                (float)uniformRandom(0.0f, extent.x),
+                (float)uniformRandom(0.0f, extent.y),
+                d * extent.z);
+    } else if (r < aYZ) {
+        return 
+            lo + 
+            Vector3(
+                d * extent.x,
+                (float)uniformRandom(0, extent.y),
+                (float)uniformRandom(0, extent.z));
+    } else {
+        return 
+            lo + 
+            Vector3(
+                (float)uniformRandom(0, extent.x),
+                d * extent.y,
+                (float)uniformRandom(0, extent.z));
+    }
+}
+
+
+Vector3 AABox::randomInteriorPoint() const {
+    return Vector3(
+        (float)uniformRandom(lo.x, hi.x), 
+        (float)uniformRandom(lo.y, hi.y), 
+        (float)uniformRandom(lo.z, hi.z));
+}
+
+
+bool AABox::intersects(const AABox& other) const {
+    // Must be overlap along all three axes.
+    // Try to find a separating axis.
+
+    for (int a = 0; a < 3; ++a) {
+
+        //     |--------|
+        // |------|
+
+        if ((lo[a] > other.hi[a]) ||
+            (hi[a] < other.lo[a])) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int AABox::dummy = 0;
+
+bool AABox::culledBy(
+    const Array<Plane>&	plane,
+    int&		cullingPlane,
+    const uint32	_inMask,
+    uint32&             childMask) const {
+
+    uint32 inMask = _inMask;
+    assert(plane.size() < 31);
+
+    childMask = 0;
+
+    const bool finite = 
+        (abs(lo.x) < G3D::finf()) &&
+        (abs(hi.x) < G3D::finf()) &&
+        (abs(lo.y) < G3D::finf()) &&
+        (abs(hi.y) < G3D::finf()) &&
+        (abs(lo.z) < G3D::finf()) &&
+        (abs(hi.z) < G3D::finf());
+
+    // See if there is one plane for which all of the
+	// vertices are in the negative half space.
+    for (int p = 0; p < plane.size(); ++p) {
+
+		// Only test planes that are not masked
+		if ((inMask & 1) != 0) {
+		
+			Vector3 corner;
+
+            int numContained = 0;
+            int v = 0;
+
+            // We can early-out only if we have found one point on each
+            // side of the plane (i.e. if we are straddling).  That
+            // occurs when (numContained < v) && (numContained > 0)
+			for (v = 0; (v < 8) && ((numContained == v) || (numContained == 0)); ++v) {
+                // Unrolling these 3 if's into a switch decreases performance
+                // by about 2x
+				corner.x = (v & 1) ? hi.x : lo.x;
+				corner.y = (v & 2) ? hi.y : lo.y;
+				corner.z = (v & 4) ? hi.z : lo.z;
+        
+                if (finite) { // this branch is highly predictable
+                    if (plane[p].halfSpaceContainsFinite(corner)) {
+                        ++numContained;
+                    }
+                } else {
+                    if (plane[p].halfSpaceContains(corner)) {
+                        ++numContained;
+                    }
+                }
+			}
+
+			if (numContained == 0) {
+				// Plane p culled the box
+				cullingPlane = p;
+
+                // The caller should not recurse into the children,
+                // since the parent is culled.  If they do recurse,
+                // make them only test against this one plane, which
+                // will immediately cull the volume.
+                childMask = 1 << p;
+				return true;
+
+            } else if (numContained < v) {
+                // The bounding volume straddled the plane; we have
+                // to keep testing against this plane
+                childMask |= (1 << p);
+            }
+		}
+
+        // Move on to the next bit.
+		inMask = inMask >> 1;
+    }
+
+    // None of the planes could cull this box
+	cullingPlane = -1;
+    return false;
+}
+
+
+bool AABox::culledBy(
+    const Array<Plane>& plane,
+	int&				cullingPlane,
+	const uint32		_inMask) const {
+
+	uint32 inMask = _inMask;
+	assert(plane.size() < 31);
+    
+    const bool finite = 
+        (abs(lo.x) < G3D::finf()) &&
+        (abs(hi.x) < G3D::finf()) &&
+        (abs(lo.y) < G3D::finf()) &&
+        (abs(hi.y) < G3D::finf()) &&
+        (abs(lo.z) < G3D::finf()) &&
+        (abs(hi.z) < G3D::finf());
+
+    // See if there is one plane for which all of the
+	// vertices are in the negative half space.
+    for (int p = 0; p < plane.size(); ++p) {
+
+		// Only test planes that are not masked
+		if ((inMask & 1) != 0) {
+		
+			bool culled = true;
+			Vector3 corner;
+
+            int v;
+
+			// Assume this plane culls all points.  See if there is a point
+			// not culled by the plane... early out when at least one point
+            // is in the positive half space.
+			for (v = 0; (v < 8) && culled; ++v) {
+
+                // Unrolling these 3 if's into a switch decreases performance
+                // by about 2x
+				corner.x = (v & 1) ? hi.x : lo.x;
+				corner.y = (v & 2) ? hi.y : lo.y;
+				corner.z = (v & 4) ? hi.z : lo.z;
+        
+                if (finite) { // this branch is highly predictable
+                    culled = ! plane[p].halfSpaceContainsFinite(corner);
+                } else {
+                    culled = ! plane[p].halfSpaceContains(corner);
+                }
+			}
+
+			if (culled) {
+				// Plane p culled the box
+				cullingPlane = p;
+
+				return true;
+            }
+		}
+
+        // Move on to the next bit.
+		inMask = inMask >> 1;
+    }
+
+    // None of the planes could cull this box
+	cullingPlane = -1;
+    return false;
+}
+
+
+bool AABox::intersects(const class Sphere& sphere) const {
+    double d = 0; 
+
+    //find the square of the distance
+    //from the sphere to the box
+    for (int i = 0; i < 3; ++i) {
+        if (sphere.center[i] < lo[i]) {
+            d += square(sphere.center[i] - lo[i]);
+        } else if (sphere.center[i] > hi[i]) {
+            d += square(sphere.center[i] - hi[i]);
+        }
+    }
+
+    return d <= square(sphere.radius);
+}
+
+Vector3 AABox::corner(int index) const {
+
+    // default constructor inits all components to 0
+    Vector3 v;
+
+    switch (index)
+    {
+    case 0:
+        v.x = lo.x;
+        v.y = lo.y;
+        v.z = hi.z;
+        break;
+
+    case 1:
+        v.x = hi.x;
+        v.y = lo.y;
+        v.z = hi.z;
+        break;
+
+    case 2:
+        v.x = hi.x;
+        v.y = hi.y;
+        v.z = hi.z;
+        break;
+
+    case 3:
+        v.x = lo.x;
+        v.y = hi.y;
+        v.z = hi.z;
+        break;
+
+    case 4:
+        v.x = lo.x;
+        v.y = lo.y;
+        v.z = lo.z;
+        break;
+
+    case 5:
+        v.x = hi.x;
+        v.y = lo.y;
+        v.z = lo.z;
+        break;
+
+    case 6:
+        v.x = hi.x;
+        v.y = hi.y;
+        v.z = lo.z;
+        break;
+
+    case 7:
+        v.x = lo.x;
+        v.y = hi.y;
+        v.z = lo.z;
+        break;
+
+    default:
+        debugAssertM(false, "Invalid corner index");
+        break;
+    }
+
+    return v;
+}
+
+
+}
---
+++ externals/g3dlite/Triangle.cpp
@@ -0,0 +1,186 @@
+/**
+ @file Triangle.cpp
+ 
+ @maintainer Morgan McGuire, http://graphics.cs.williams.edu
+ 
+ @created 2001-04-06
+ @edited  2008-12-28
+
+ Copyright 2000-2009, Morgan McGuire.
+ All rights reserved.
+ */
+
+#include "G3D/platform.h"
+#include "G3D/Triangle.h"
+#include "G3D/Plane.h"
+#include "G3D/BinaryInput.h"
+#include "G3D/BinaryOutput.h"
+#include "G3D/debugAssert.h"
+#include "G3D/AABox.h"
+#include "G3D/Ray.h"
+
+namespace G3D {
+
+    
+void Triangle::init(const Vector3& v0, const Vector3& v1, const Vector3& v2) {
+
+    _plane = Plane(v0, v1, v2);
+    _vertex[0] = v0;
+    _vertex[1] = v1;
+    _vertex[2] = v2;
+
+    static int next[] = {1,2,0};
+
+    for (int i = 0; i < 3; ++i) {
+        const Vector3& e  = _vertex[next[i]] - _vertex[i];
+        edgeMagnitude[i]  = e.magnitude();
+
+        if (edgeMagnitude[i] == 0) {
+            edgeDirection[i] = Vector3::zero();
+        } else {
+            edgeDirection[i] = e / (float)edgeMagnitude[i];
+        }
+    }
+
+    _edge01 = _vertex[1] - _vertex[0];
+    _edge02 = _vertex[2] - _vertex[0];
+
+    _primaryAxis = _plane.normal().primaryAxis();
+    _area = 0.5f * edgeDirection[0].cross(edgeDirection[2]).magnitude() * (edgeMagnitude[0] * edgeMagnitude[2]);
+        //0.5f * (_vertex[1] - _vertex[0]).cross(_vertex[2] - _vertex[0]).dot(_plane.normal());
+}
+
+
+Triangle::Triangle() {
+    init(Vector3::zero(), Vector3::zero(), Vector3::zero());
+}
+    
+
+Triangle::Triangle(const Vector3& v0, const Vector3& v1, const Vector3& v2) {
+    init(v0, v1, v2);
+}
+
+    
+Triangle::~Triangle() {
+}
+
+
+Triangle::Triangle(class BinaryInput& b) {
+    deserialize(b);
+}
+
+
+void Triangle::serialize(class BinaryOutput& b) {
+    _vertex[0].serialize(b);
+    _vertex[1].serialize(b);
+    _vertex[2].serialize(b);
+}
+
+
+void Triangle::deserialize(class BinaryInput& b) {
+    _vertex[0].deserialize(b);
+    _vertex[1].deserialize(b);
+    _vertex[2].deserialize(b);
+    init(_vertex[0], _vertex[1], _vertex[2]);
+}
+
+
+float Triangle::area() const {
+    return _area;
+}
+
+
+const Vector3& Triangle::normal() const {
+    return _plane.normal();
+}
+
+
+const Plane& Triangle::plane() const {
+    return _plane;
+}
+
+
+Vector3 Triangle::center() const {
+    return (_vertex[0] + _vertex[1] + _vertex[2]) / 3.0;
+}
+
+Vector3 Triangle::randomPoint() const {
+    // Choose a random point in the parallelogram
+
+    float s = uniformRandom();
+    float t = uniformRandom();
+
+    if (t > 1.0f - s) {
+        // Outside the triangle; reflect about the
+        // diagonal of the parallelogram
+        t = 1.0f - t;
+        s = 1.0f - s;
+    }
+
+    return _edge01 * s + _edge02 * t + _vertex[0];
+}
+
+
+void Triangle::getBounds(AABox& out) const {
+    Vector3 lo = _vertex[0];
+    Vector3 hi = lo;
+
+    for (int i = 1; i < 3; ++i) {
+        lo = lo.min(_vertex[i]);
+        hi = hi.max(_vertex[i]);
+    }
+
+    out = AABox(lo, hi);
+}
+
+
+bool Triangle::intersect(const Ray& ray, float& distance, float baryCoord[3]) const {
+    static const float EPS = 1e-5f;
+
+    // See RTR2 ch. 13.7 for the algorithm.
+
+    const Vector3& e1 = edge01();
+    const Vector3& e2 = edge02();
+    const Vector3 p(ray.direction().cross(e2));
+    const float a = e1.dot(p);
+
+    if (abs(a) < EPS) {
+        // Determinant is ill-conditioned; abort early
+        return false;
+    }
+
+    const float f = 1.0f / a;
+    const Vector3 s(ray.origin() - vertex(0));
+    const float u = f * s.dot(p);
+
+    if ((u < 0.0f) || (u > 1.0f)) {
+        // We hit the plane of the m_geometry, but outside the m_geometry
+        return false;
+    }
+
+    const Vector3 q(s.cross(e1));
+    const float v = f * ray.direction().dot(q);
+
+    if ((v < 0.0f) || ((u + v) > 1.0f)) {
+        // We hit the plane of the triangle, but outside the triangle
+        return false;
+    }
+
+    const float t = f * e2.dot(q);
+
+    if ((t > 0.0f) && (t < distance)) {
+        // This is a new hit, closer than the previous one
+        distance = t;
+
+        baryCoord[0] = 1.0 - u - v;
+        baryCoord[1] = u;
+        baryCoord[2] = v;
+
+        return true;
+    } else {
+        // This hit is after the previous hit, so ignore it
+        return false;
+    }
+}
+
+} // G3D
