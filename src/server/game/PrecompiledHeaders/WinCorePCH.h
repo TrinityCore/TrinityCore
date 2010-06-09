1582dfc8b2cc7967eaa826ba339b421dda7e271d
Rat <none@none>
2010-06-09 14:05:50 +0200

    added revision.h O.o game.vcproj update , game.lib now buildable todo: auth+world server
    --HG--
    branch : trunk

--- src/server/game/PrecompiledHeaders/WinCorePCH.h
+++ src/server/game/PrecompiledHeaders/WinCorePCH.h
@@ -1,14 +1,14 @@
 //add here most rarely modified headers to speed up debug build compilation
-#include "WorldSocket.h"                                    // must be first to make ACE happy with ACE includes in it
-#include "Common.h"
+#include "..\Server\WorldSocket.h"                                    // must be first to make ACE happy with ACE includes in it
+#include "..\..\shared\Common.h"
 
-#include "MapManager.h"
-#include "Log.h"
-#include "ObjectAccessor.h"
-#include "ObjectDefines.h"
-#include "SQLStorage.h"
-#include "Opcodes.h"
-#include "SharedDefines.h"
-#include "ObjectMgr.h"
+#include "..\Maps\MapManager.h"
+#include "..\..\shared\Logging\Log.h"
+#include "..\Globals\ObjectAccessor.h"
+#include "..\Entities\Object\ObjectDefines.h"
+#include "..\..\shared\Database/SQLStorage.h"
+#include "..\Protocol\Opcodes.h"
+#include "..\Miscellaneous\SharedDefines.h"
+#include "..\Globals\ObjectMgr.h"
 
 #include "ScriptPCH.h"
