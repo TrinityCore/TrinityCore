cc4413f6b462cc5071cc44dbba27d329d5d43571
Tartalo <none@none>
2010-06-09 15:02:30 +0200

    Change binary names for tools/
    --HG--
    branch : trunk

--- src/server/shared/revision.h
+++ src/server/shared/revision.h
@@ -1,12 +1,12 @@
 #ifndef __REVISION_H__
 #define __REVISION_H__
- #define _BUILD_DIRECTIVE "Debug"
- #define _REVISION "8417"
- #define _HASH "47972987739a"
+ #define _BUILD_DIRECTIVE "Release"
+ #define _REVISION "8549"
+ #define _HASH "7c4cd473d7cc"
  #define _REVISION_DATE "*"
  #define _REVISION_TIME "*"
- #define FILEVER        0,0,8417,0
- #define PRODUCTVER     0,0,8417,0
- #define STRFILEVER     "0, 0, 8417, 47972987739a"
- #define STRPRODUCTVER  "0, 0, 8417, 47972987739a"
+ #define FILEVER        0,0,8549,0
+ #define PRODUCTVER     0,0,8549,0
+ #define STRFILEVER     "0, 0, 8549, 7c4cd473d7cc"
+ #define STRPRODUCTVER  "0, 0, 8549, 7c4cd473d7cc"
 #endif // __REVISION_H__
