725d1bf7c22567d29418521041ad1f0e58f80648
click <none@none>
2010-06-12 00:45:24 +0200

    Fix issues with missing the printf()-function on newer distributions (thanks to svannon and brian for helping)
    --HG--
    branch : trunk

--- src/tools/vmap3_extractor/mpq_libmpq04.h
+++ src/tools/vmap3_extractor/mpq_libmpq04.h
@@ -24,7 +24,7 @@ public:
     void close();
 
     void GetFileListTo(vector<string>& filelist) {
-    	uint32 filenum;
+    	uint32_t filenum;
     	if(libmpq__file_number(mpq_a, "(listfile)", &filenum)) return;
     	libmpq__off_t size, transferred;
 		libmpq__file_unpacked_size(mpq_a, filenum, &size);
