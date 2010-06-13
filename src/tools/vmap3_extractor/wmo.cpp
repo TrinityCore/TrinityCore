8a2a82d1e8c8fb71d2762490a2af7b925a29f6be
click <none@none>
2010-06-13 20:36:07 +0200

    port over some usability-fixes from lynx3d on the vmapextractor and remove an old header-relic
    --HG--
    branch : trunk

--- src/tools/vmap3_extractor/wmo.cpp
+++ src/tools/vmap3_extractor/wmo.cpp
@@ -1,4 +1,3 @@
-
 #include "vmapexport.h"
 #include "wmo.h"
 #include "vec3d.h"
@@ -443,7 +442,7 @@ WMOInstance::WMOInstance(MPQFile &f,const char* WmoInstName, uint32 mapID, uint3
     //-----------add_in _dir_file----------------
 
     char tempname[512];
-    sprintf(tempname, "Buildings/%s", WmoInstName);
+    sprintf(tempname, "%s/%s", szWorkDirWmo, WmoInstName);
     FILE *input;
     input = fopen(tempname, "r+b");
 
