47955ba8a8e3611d1713e0e7cbdbbde051702773
Tartalo <none@none>
2010-06-20 11:54:28 +0200

    Nicer approach for Repeteance fix using spell_linked_spell
    --HG--
    branch : trunk

---
+++ sql/updates/8658_world_spell_linked_spell.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger`=20066;
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES
+(20066, -61840, 0, 'Repentance');
+
