09dbd312ceb684d7c343bf9524131cc79b6c8260
Xanadu <none@none>
2010-06-23 05:04:24 +0200

    Mobs can no longer crit with spells. Closes issue #1704. Based on patch by PSZ.
    --HG--
    branch : trunk

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -10367,6 +10367,10 @@ int32 Unit::SpellBaseDamageBonusForVictim(SpellSchoolMask schoolMask, Unit *pVic
 
 bool Unit::isSpellCrit(Unit *pVictim, SpellEntry const *spellProto, SpellSchoolMask schoolMask, WeaponAttackType attackType) const
 {
+    // Mobs can't crit with spells.
+    if (IS_CREATURE_GUID(GetGUID()))
+        return false;
+    
     // not critting spell
     if ((spellProto->AttributesEx2 & SPELL_ATTR_EX2_CANT_CRIT))
         return false;
