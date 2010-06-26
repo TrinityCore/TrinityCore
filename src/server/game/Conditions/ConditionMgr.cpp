51f501de5d16876ce6c82fad325c0da602595e2b
Rat <none@none>
2010-06-26 10:57:19 +0200

    removed unneeded debug lines from conditions loading
    --HG--
    branch : trunk

--- src/server/game/Conditions/ConditionMgr.cpp
+++ src/server/game/Conditions/ConditionMgr.cpp
@@ -517,7 +517,6 @@ bool ConditionMgr::addToGossipMenus(Condition* cond)
             if ((*itr).second.entry == cond->mSourceGroup && (*itr).second.text_id == cond->mSourceEntry)
             {
                 (*itr).second.conditions.push_back(cond);
-                sLog.outDebug("addToGossipMenus: entry %u textId %u", cond->mSourceGroup, cond->mSourceEntry);
                 return true;
             }
         }
@@ -536,7 +535,6 @@ bool ConditionMgr::addToGossipMenuItems(Condition* cond)
             if ((*itr).second.menu_id == cond->mSourceGroup && (*itr).second.id == cond->mSourceEntry)
             {
                 (*itr).second.conditions.push_back(cond);
-                //sLog.outDebug("addToGossipMenuItems: menuId %u id %u", cond->mSourceGroup, cond->mSourceEntry);
                 return true;
             }
         }
