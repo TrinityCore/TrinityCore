012f498fa4f9b6d9df06b2c78c8d2e745a77251e
Tartalo <none@none>
2010-06-26 20:11:34 +0200

    Load&save gothik's door state on instancedata load&save Partial fix for issue #1244
    --HG--
    branch : trunk

--- src/server/scripts/Northrend/Naxxramas/instance_naxxramas.cpp
+++ src/server/scripts/Northrend/Naxxramas/instance_naxxramas.cpp
@@ -128,6 +128,8 @@ struct instance_naxxramas : public InstanceData
     uint64 uiKelthuzadTrigger;
     uint64 uiPortals[4];
 
+    GOState gothikDoorState;
+
     time_t minHorsemenDiedTime;
     time_t maxHorsemenDiedTime;
 
@@ -171,7 +173,9 @@ struct instance_naxxramas : public InstanceData
                     pSapphiron->AI()->DoAction(DATA_SAPPHIRON_BIRTH);
                 return;
             }
-            case GO_GOTHIK_GATE: GothikGateGUID = add ? pGo->GetGUID() : 0; break;
+            case GO_GOTHIK_GATE:
+                GothikGateGUID = add ? pGo->GetGUID() : 0;
+                break;
             case GO_HORSEMEN_CHEST: HorsemenChestGUID = add ? pGo->GetGUID() : 0; break;
             case GO_HORSEMEN_CHEST_HERO: HorsemenChestGUID = add ? pGo->GetGUID() : 0; break;
             case GO_KELTHUZAD_PORTAL01: uiPortals[0] = pGo->GetGUID(); break;
@@ -194,6 +198,7 @@ struct instance_naxxramas : public InstanceData
             case DATA_GOTHIK_GATE:
                 if (GameObject *pGothikGate = instance->GetGameObject(GothikGateGUID))
                     pGothikGate->SetGoState(GOState(value));
+                gothikDoorState = GOState(value);
                 break;
 
             case DATA_HORSEMEN0:
@@ -306,6 +311,21 @@ struct instance_naxxramas : public InstanceData
                 break;
         }
         return false;
+    }
+
+    std::string GetSaveData()
+    {
+        std::ostringstream saveStream;
+        saveStream << GetBossSaveData() << gothikDoorState;
+        return saveStream.str();
+    }
+
+    void Load(const char * data)
+    {
+        std::istringstream loadStream(LoadBossState(data));
+        uint32 buff;
+        loadStream >> buff;
+        gothikDoorState = GOState(buff);
     }
 };
 
