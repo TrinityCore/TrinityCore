4c59bf69dcfc2a58b62ce7daa8f87f5e2f503ab1
Tartalo <none@none>
2010-06-27 22:52:37 +0200

    Violet Hold: Add copyright note
    --HG--
    branch : trunk

--- src/server/scripts/Northrend/VioletHold/boss_cyanigosa.cpp
+++ src/server/scripts/Northrend/VioletHold/boss_cyanigosa.cpp
@@ -1,14 +1,21 @@
-/* Script Data Start
-SDName: Boss cyanigosa
-SDAuthor: LordVanMartin
-SD%Complete:
-SDComment:
-SDCategory:
-Script Data End */
+/*
+ * Copyright (C) 2009 - 2010 Trinity <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 
-/*** SQL START ***
-update creature_template set scriptname = '' where entry = '';
-*** SQL END ***/
 #include "ScriptPCH.h"
 #include "violet_hold.h"
 
