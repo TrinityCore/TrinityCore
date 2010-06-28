72de3dd6c42c9476b4d7f7d3f187ad1cf20b0414
click <none@none>
2010-06-28 17:12:36 +0200

    Remove hardcoded stacking for ammunition-types (should now stack properly)
    --HG--
    branch : trunk

--- src/server/game/Entities/Item/ItemPrototype.h
+++ src/server/game/Entities/Item/ItemPrototype.h
@@ -623,7 +623,7 @@ struct ItemPrototype
 
     uint32 GetMaxStackSize() const
     {
-        return (Stackable == 2147483647 || Stackable <= 0 || Stackable == 1000) ? uint32(0x7FFFFFFF-1) : uint32(Stackable);
+        return (Stackable == 2147483647 || Stackable <= 0) ? uint32(0x7FFFFFFF-1) : uint32(Stackable);
     }
 
     float getDPS() const
