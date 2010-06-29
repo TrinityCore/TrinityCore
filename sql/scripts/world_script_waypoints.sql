3737af6b46185b3583242413bc9c7de94a1d3565
teacher <none@none>
2010-06-29 13:22:56 +0200

    Added missing data to fulls and better format for sql update files.
    --HG--
    branch : trunk

--- sql/scripts/world_script_waypoints.sql
+++ sql/scripts/world_script_waypoints.sql
@@ -2257,3 +2257,13 @@ INSERT INTO `script_waypoint` (`entry`, `pointid`, `location_x`,`location_y`, `l
 (12717, 27, 3621.08, 1138.11, 10.369, 0, 'SAY_MUG_RETURN'),
 (12717, 28, 3615.48, 1145.53, 9.614, 0, ''),
 (12717, 29, 3607.19, 1152.72, 8.871, 0, '');
+
+-- Waypoints for Mimiron Inferno
+DELETE FROM `script_waypoint` WHERE `entry`=33370;
+INSERT INTO `script_waypoint` (`entry`, `pointid`, `location_x`, `location_y`, `location_z`, `waittime`, `point_comment`) VALUES
+(33370, 1, 331.674, -68.6878, 409.804, 0, '0'),
+(33370, 2, 274.578, -92.1829, 409.804, 0, '0'),
+(33370, 3, 226.433, -66.6652, 409.793, 0, '0'),
+(33370, 4, 206.092, -34.7447, 409.801, 0, '0'),
+(33370, 5, 240.208, 1.10346, 409.802, 0, '0'),
+(33370, 6, 337.199, 11.7051, 409.802, 0, '0');
--- sql/updates/8770_world_areatrigger_scripts.sql
+++ sql/updates/8770_world_areatrigger_scripts.sql
@@ -1,3 +1,4 @@
-INSERT INTO areatrigger_scripts VALUES
-(5369,'at_RX_214_repair_o_matic_station'),
-(5423,'at_RX_214_repair_o_matic_station');
+DELETE FROM `areatrigger_scripts` WHERE `entry` IN (5369,5423);
+INSERT INTO `areatrigger_scripts` (`entry`,`ScriptName`) VALUES
+(5369, 'at_RX_214_repair_o_matic_station'),
+(5423, 'at_RX_214_repair_o_matic_station');
