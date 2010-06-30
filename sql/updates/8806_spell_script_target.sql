12c87cd086446148c255fbdda019c0c5e4259749
Tartalo <none@none>
2010-06-30 19:42:25 +0200

    Remove sql content managed by TDB
    --HG--
    branch : trunk

--- sql/updates/8806_spell_script_target.sql
+++
@@ -1,5 +0,0 @@
--- Limit Flame Tsunami buff to Lava Blazes only
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=13 AND `SourceEntry`=60430;
-INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceEntry`,`ConditionTypeOrReference`,`ConditionValue1`,`ConditionValue2`) VALUES
-(13,60430,18,1,30643),
-(13,60430,18,1,31317);
