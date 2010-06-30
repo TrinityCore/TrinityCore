a2da847d2c1be565474c11d42c8501655f241da1
Tartalo <none@none>
2010-06-30 19:39:14 +0200

    Fix sql from rev 8806 for the new conditions system
    --HG--
    branch : trunk

--- sql/updates/8806_spell_script_target.sql
+++ sql/updates/8806_spell_script_target.sql
@@ -1,5 +1,5 @@
 -- Limit Flame Tsunami buff to Lava Blazes only
-DELETE FROM spell_script_target WHERE entry = 60430;
-INSERT INTO spell_script_target (entry, type, targetEntry) VALUES
-  (60430, 1, 30643),
-  (60430, 1, 31317);
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=13 AND `SourceEntry`=60430;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceEntry`,`ConditionTypeOrReference`,`ConditionValue1`,`ConditionValue2`) VALUES
+(13,60430,18,1,30643),
+(13,60430,18,1,31317);
