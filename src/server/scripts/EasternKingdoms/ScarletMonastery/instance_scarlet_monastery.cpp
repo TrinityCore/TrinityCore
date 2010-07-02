08661a23256943065830f82b8aed85cd0cee9c4e
click <none@none>
2010-07-02 16:13:40 +0200

    Adjust to use correct timeframe for script-copyrights
    --HG--
    branch : trunk

--- src/server/scripts/EasternKingdoms/ScarletMonastery/instance_scarlet_monastery.cpp
+++ src/server/scripts/EasternKingdoms/ScarletMonastery/instance_scarlet_monastery.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2006 - 2010 Trinity <http://www.trinitycore.org/>
+/* Copyright (C) 2008 - 2010 Trinity <http://www.trinitycore.org/>
  * Copyright (C) 2006 - 2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>.sourceforge.net/>
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
