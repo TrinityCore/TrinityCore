4c629200a12ff5456e6ec989ab5510a93bb10205
Shauren <none@none>
2010-07-02 14:24:05 +0200

    Fixed Chain Lightning overproccing with Lightning Overload, patch by DrTenma Closes issue #1198.
    --HG--
    branch : trunk

--- src/server/game/Miscellaneous/SharedDefines.h
+++ src/server/game/Miscellaneous/SharedDefines.h
@@ -287,7 +287,7 @@ const uint32 ItemQualityColors[MAX_ITEM_QUALITY] = {
 #define SPELL_ATTR_EX_NEGATIVE                    0x00000080            // 7
 #define SPELL_ATTR_EX_NOT_IN_COMBAT_TARGET        0x00000100            // 8 Spell req target not to be in combat state
 #define SPELL_ATTR_EX_UNK9                        0x00000200            // 9 melee spells
-#define SPELL_ATTR_EX_UNK10                       0x00000400            // 10 no generates threat on cast 100%? (old NO_INITIAL_AGGRO)
+#define SPELL_ATTR_EX_NO_THREAT                   0x00000400            // 10 no generates threat on cast 100% (old NO_INITIAL_AGGRO)
 #define SPELL_ATTR_EX_UNK11                       0x00000800            // 11 aura
 #define SPELL_ATTR_EX_UNK12                       0x00001000            // 12
 #define SPELL_ATTR_EX_UNK13                       0x00002000            // 13
