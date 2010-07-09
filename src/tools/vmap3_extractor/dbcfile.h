da7c15e955a7ba9efff62b0cae514ff05f5732fa
click <none@none>
2010-07-09 18:02:46 +0200

    Update VMap3 to "final" release (REEXTRACTION OF MAPS/VMAPS IS REQUIRED!) - thanks to Lynx3d (again) - add magic-header to .vmtile files - calculate waterlevels for non-flat surfaces in a more correct way - make MSVC shut up about float-issues - change logging around a bit (output function that the respective logentry comes from) - remove remaining Stormlib leftovers - set indoor/outdoor check to enabled by default (more blizzlike)
    --HG--
    branch : trunk

--- src/tools/vmap3_extractor/dbcfile.h
+++ src/tools/vmap3_extractor/dbcfile.h
@@ -1,13 +1,27 @@
+/*
+ * Copyright (C) 2008-2010 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2005-2010 MaNGOS <http://getmangos.com/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef DBCFILE_H
 #define DBCFILE_H
-#define __STORMLIB_SELF__
 
 #include <cassert>
 #include <string>
-//#include "StormLib.h"
 
-#undef min
-#undef max
 class DBCFile
 {
 public:
