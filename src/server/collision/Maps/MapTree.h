478946284d6949922267a24dae45e4ac4532ff56
click <none@none>
2010-07-10 05:14:05 +0200

    Fix incorrect blocked LOS-calculation on certain locations (by Lynx3d)
    --HG--
    branch : trunk

--- src/server/collision/Maps/MapTree.h
+++ src/server/collision/Maps/MapTree.h
@@ -57,7 +57,7 @@ namespace VMAP
             std::string iBasePath;
 
         private:
-            float getIntersectionTime(const G3D::Ray& pRay, float pMaxDist, bool pStopAtFirstHit) const;
+            bool getIntersectionTime(const G3D::Ray& pRay, float &pMaxDist, bool pStopAtFirstHit) const;
             //bool containsLoadedMapTile(unsigned int pTileIdent) const { return(iLoadedMapTiles.containsKey(pTileIdent)); }
         public:
             static std::string getTileFileName(uint32 mapID, uint32 tileX, uint32 tileY);
