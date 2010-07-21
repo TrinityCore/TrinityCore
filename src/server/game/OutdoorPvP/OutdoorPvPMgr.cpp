61946ea1c04fff6ace445f7f572e8bf0ac6c89be
click <none@none>
2010-07-21 12:39:51 +0200

    Remove possible memleak with lingering outdoor pvp capturepoints - patch by ge0rg
    --HG--
    branch : trunk

--- src/server/game/OutdoorPvP/OutdoorPvPMgr.cpp
+++ src/server/game/OutdoorPvP/OutdoorPvPMgr.cpp
@@ -36,10 +36,7 @@ OutdoorPvPMgr::~OutdoorPvPMgr()
 {
     //sLog.outDebug("Deleting OutdoorPvPMgr");
     for (OutdoorPvPSet::iterator itr = m_OutdoorPvPSet.begin(); itr != m_OutdoorPvPSet.end(); ++itr)
-    {
-        (*itr)->DeleteSpawns();
         delete *itr;
-    }
 }
 
 void OutdoorPvPMgr::InitOutdoorPvP()
