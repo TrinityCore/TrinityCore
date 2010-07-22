15c60f512ce6df8dec3917ce3d3fbf0bc6a405aa
Shauren <none@none>
2010-07-22 12:16:47 +0200

    Fix new character delete system. Those queries were not supposed to be there. patch by Az@zel
    --HG--
    branch : trunk

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -4499,10 +4499,6 @@ void Player::DeleteFromDB(uint64 playerguid, uint32 accountId, bool updateRealmC
             CharacterDatabase.PExecute("DELETE FROM character_gifts WHERE guid = '%u'",guid);
             CharacterDatabase.PExecute("DELETE FROM character_homebind WHERE guid = '%u'",guid);
             CharacterDatabase.PExecute("DELETE FROM character_instance WHERE guid = '%u'",guid);
-            CharacterDatabase.PExecute("DELETE FROM group_instance WHERE leaderGuid = '%u'",guid);
-            CharacterDatabase.PExecute("DELETE FROM groups WHERE leaderGuid = '%u'",guid);
-            CharacterDatabase.PExecute("DELETE FROM group_member WHERE leaderGuid = '%u' OR memberGuid = '%u'",guid,guid);
-            CharacterDatabase.PExecute("DELETE FROM groups WHERE leaderGuid = '%u'",guid);
             CharacterDatabase.PExecute("DELETE FROM character_inventory WHERE guid = '%u'",guid);
             CharacterDatabase.PExecute("DELETE FROM character_queststatus WHERE guid = '%u'",guid);
             CharacterDatabase.PExecute("DELETE FROM character_reputation WHERE guid = '%u'",guid);
