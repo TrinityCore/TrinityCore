bd1181ab628c3cfeb70ec176af0706e1393e4e44
Brian <runningnak3d@gmail.com>
2010-07-22 14:48:46 -0600

    * orientation information for the previous commit * Provided by crackm (thanks Zor)
    --HG--
    branch : trunk

---
+++ sql/updates/8988_world_playercreateinfo.sql
@@ -0,0 +1,12 @@
+
+UPDATE playercreateinfo SET orientation= 5.696318 WHERE race=4 and
+class<>6;
+UPDATE playercreateinfo SET orientation= 6.177156 WHERE race=3 and
+class<>6;
+UPDATE playercreateinfo SET orientation= 2.70526 WHERE race=5 and
+class<>6;
+UPDATE playercreateinfo SET orientation= 5.316046 WHERE race=10
+and class<>6;
+UPDATE playercreateinfo SET orientation= 2.083644 WHERE race=11
+and class<>6;
+UPDATE playercreateinfo SET orientation= 3.659973 WHERE class=6;
