da8479302348286c583b4abe919dcb4aea16ebc8
teacher <none@none>
2010-07-22 11:44:09 +0200

    Fixes import errors (updated insert statements to new table structures) to world_database.sql. Some random Trinity standards in sql syntax.
    --HG--
    branch : trunk

--- sql/updates/8971_characters_characters.sql
+++ sql/updates/8971_characters_characters.sql
@@ -2,4 +2,3 @@ ALTER TABLE `characters`
    ADD COLUMN `deleteInfos_Account` int(11) UNSIGNED default NULL AFTER actionBars,
    ADD COLUMN `deleteInfos_Name` varchar(12) default NULL AFTER deleteInfos_Account,
    ADD COLUMN `deleteDate` bigint(20) default NULL AFTER deleteInfos_Name;
-
