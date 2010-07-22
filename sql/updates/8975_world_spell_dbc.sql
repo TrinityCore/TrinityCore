df4b32ec74c17ed4b602811dd399adabd5deab9a
teacher <none@none>
2010-07-22 12:19:31 +0200

    Proper name for my previous sql update. Sorry for wasting rev numbers.
    --HG--
    branch : trunk

--- sql/updates/8975_world_spell_dbc.sql
+++ sql/updates/8975_world_spell_dbc.sql
@@ -1,4 +1,4 @@
 -- Add serverside spells place holders for future development
-DELETE FROM `spell_dbc` WHERE `Id` IN (70878);
+DELETE FROM `spell_dbc` WHERE `Id` IN (38406);
 INSERT INTO `spell_dbc` (`Id`,`Comment`) VALUES
 (38406, 'Quest 10721 RewSpellCast serverside spell');
