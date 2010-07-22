c8662c888b7b809189c3aedd2f4a7c0403f6456e
Brian <runningnak3d@gmail.com>
2010-07-22 13:41:19 -0600

    * Implemented DB cleaning on startup * Must be enabled in conf AND set in worldstates for what you want to clean * CLEANING_FLAG_ACHIEVEMENT_PROGRESS  = 0x1, * CLEANING_FLAG_SKILLS                = 0x2, * CLEANING_FLAG_SPELLS                = 0x4, * CLEANING_FLAG_TALENTS               = 0x8 * Set to 0xF to clean all 4 * Patch by hunuza
    --HG--
    branch : trunk

---
+++ src/server/game/Tools/CharacterDatabaseCleaner.h
@@ -0,0 +1,53 @@
+/*
+* Copyright (C) 2005-2010 MaNGOS <http://getmangos.com/>
+*
+* Copyright (C) 2008-2010 Trinity <http://www.trinitycore.org/>
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  
+USA
+*/
+
+#ifndef CHARACTERDATABASECLEANER_H
+#define CHARACTERDATABASECLEANER_H
+
+namespace CharacterDatabaseCleaner
+{
+    enum CleaningFlags
+    {
+        CLEANING_FLAG_ACHIEVEMENT_PROGRESS  = 0x1,
+        CLEANING_FLAG_SKILLS                = 0x2,
+        CLEANING_FLAG_SPELLS                = 0x4,
+        CLEANING_FLAG_TALENTS               = 0x8
+    };
+
+
+    void CleanDatabase();
+
+    void CheckUnique(const char* column, const char* table, bool 
+(*check)(uint32));
+
+    bool AchievementProgressCheck(uint32 criteria);
+    bool SkillCheck(uint32 skill);
+    bool SpellCheck(uint32 spell_id);
+    bool TalentCheck(uint32 talent_id);
+
+    void CleanCharacterAchievementProgress();
+    void CleanCharacterSkills();
+    void CleanCharacterSpell();
+    void CleanCharacterTalent();
+}
+
+#endif
+
