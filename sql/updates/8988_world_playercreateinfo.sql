912a099d491a58e068994c63a1055ab17af37634
Brian <runningnak3d@gmail.com>
2010-07-23 01:03:43 -0600

    * Proper Trinity SQL standards for recent orientation file * Update world_database.sql with that data
    --HG--
    branch : trunk

--- sql/updates/8988_world_playercreateinfo.sql
+++ sql/updates/8988_world_playercreateinfo.sql
@@ -1,12 +1,7 @@
 
-UPDATE playercreateinfo SET orientation= 5.696318 WHERE race=4 and
-class<>6;
-UPDATE playercreateinfo SET orientation= 6.177156 WHERE race=3 and
-class<>6;
-UPDATE playercreateinfo SET orientation= 2.70526 WHERE race=5 and
-class<>6;
-UPDATE playercreateinfo SET orientation= 5.316046 WHERE race=10
-and class<>6;
-UPDATE playercreateinfo SET orientation= 2.083644 WHERE race=11
-and class<>6;
-UPDATE playercreateinfo SET orientation= 3.659973 WHERE class=6;
+UPDATE `playercreateinfo` SET `orientation`=5.696318 WHERE `race`=4 AND `class`<>6;
+UPDATE `playercreateinfo` SET `orientation`=6.177156 WHERE `race`=3 AND `class`<>6;
+UPDATE `playercreateinfo` SET `orientation`=2.70526 WHERE `race`=5 AND `class`<>6;
+UPDATE `playercreateinfo` SET `orientation`=5.316046 WHERE `race`=10 AND `class`<>6;
+UPDATE `playercreateinfo` SET `orientation`=2.083644 WHERE `race`=11 AND `class`<>6;
+UPDATE `playercreateinfo` SET `orientation`=3.659973 WHERE `class`=6;
