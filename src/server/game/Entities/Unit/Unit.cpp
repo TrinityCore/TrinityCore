3a261563f82a41e914b872c9161865d6bc39c432
QAston <none@none>
2010-07-26 22:57:25 +0200

    *Add debug log to trac reason of a crash in Spell::DoAllEffectOnTarget.
    --HG--
    branch : trunk

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -16637,6 +16637,8 @@ void Unit::OutDebugInfo() const
     sLog.outString("GUID "UI64FMTD", entry %u, type %u, name %s", GetGUID(), GetEntry(), (uint32)GetTypeId(), GetName());
     sLog.outString("OwnerGUID "UI64FMTD", MinionGUID "UI64FMTD", CharmerGUID "UI64FMTD", CharmedGUID "UI64FMTD, GetOwnerGUID(), GetMinionGUID(), GetCharmerGUID(), GetCharmGUID());
     sLog.outString("In world %u, unit type mask %u", (uint32)(IsInWorld() ? 1 : 0), m_unitTypeMask);
+    if (IsInWorld())
+        sLog.outString("Mapid %u", GetMapId());
 
     sLog.outStringInLine("Summon Slot: ");
     for (uint32 i = 0; i < MAX_SUMMON_SLOT; ++i)
