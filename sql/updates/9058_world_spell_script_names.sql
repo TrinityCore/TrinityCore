1b382e5858fdaad7954e0486b23297d84ff57b39
Shauren <none@none>
2010-07-28 22:32:52 +0200

    Fix a typo in 9043_world_spell_scripts.sql and fix some horrible sql style in other files
    --HG--
    branch : trunk

--- sql/updates/9058_world_spell_script_names.sql
+++ sql/updates/9058_world_spell_script_names.sql
@@ -1,9 +1,9 @@
-DELETE FROM spell_script_names WHERE spell_id=-47540 AND ScriptName='spell_pri_penance';
-DELETE FROM spell_script_names WHERE spell_id=31231 AND ScriptName='spell_rog_cheat_death';
-DELETE FROM spell_script_names WHERE spell_id=51662 AND ScriptName='spell_rog_hunger_for_blood';
-DELETE FROM spell_script_names WHERE spell_id=14185 AND ScriptName='spell_rog_preparation';
-DELETE FROM spell_script_names WHERE spell_id=5938 AND ScriptName='spell_rog_shiv';
-INSERT INTO spell_script_names (spell_id, ScriptName) VALUES
+DELETE FROM `spell_script_names` WHERE `spell_id`=-47540 AND `ScriptName`='spell_pri_penance';
+DELETE FROM `spell_script_names` WHERE `spell_id`=31231 AND `ScriptName`='spell_rog_cheat_death';
+DELETE FROM `spell_script_names` WHERE `spell_id`=51662 AND `ScriptName`='spell_rog_hunger_for_blood';
+DELETE FROM `spell_script_names` WHERE `spell_id`=14185 AND `ScriptName`='spell_rog_preparation';
+DELETE FROM `spell_script_names` WHERE `spell_id`=5938 AND `ScriptName`='spell_rog_shiv';
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
 (-47540,'spell_pri_penance'),
 (31231,'spell_rog_cheat_death'),
 (51662,'spell_rog_hunger_for_blood'),
