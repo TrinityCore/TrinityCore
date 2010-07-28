669cbd9d66f88bb2ba99a305b4bd1cf66202baf7
Shauren <none@none>
2010-07-28 22:32:52 +0200

    Fix a typo in 9043_world_spell_scripts.sql and fix some horrible sql style in other files
    --HG--
    branch : trunk

--- sql/updates/9026_world_scriptname.sql
+++ sql/updates/9026_world_scriptname.sql
@@ -1,2 +1,2 @@
 -- script names
-update gameobject_template set scriptName = 'go_troll_cage' where entry >= 141070 and entry < 141075;
+UPDATE `gameobject_template` SET `scriptName`='go_troll_cage' WHERE `entry`>=141070 AND `entry`<141075;
