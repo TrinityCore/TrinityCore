83b4f5b2aaa1e8d0b855015a728b4e9f9754997d
click <none@none>
2010-07-29 09:00:55 +0200

    Fix windows-build and change some warnings if configuration-files can not be read properly - Add proper warning about missing [authserver] -tag in authserver configuration file if not found. - Add proper warning about missing [worldserver] -tag in worldserver configuration file if not found. Thanks to Qaston for finding the redefininition of strdup!
    --HG--
    branch : trunk

--- src/server/authserver/Main.cpp
+++ src/server/authserver/Main.cpp
@@ -165,7 +165,8 @@ extern int main(int argc, char **argv)
 
     if (!sConfig.SetSource(cfg_file))
     {
-        sLog.outError("Could not find configuration file %s.", cfg_file);
+        sLog.outError("\x1b[31mInvalid or missing configuration file :\x1b[0m %s", cfg_file);
+        sLog.outError("\x1b[31mVerify that the file exists and has \'\x1b[0m[authserver]\x1b[31m\' written in the top of the file!\x1b[0m");
         return 1;
     }
     sLog.Initialize();
