047284994a9fcbab8476b92e1efef2c215b4dbc9
Liberate <none@none>
2010-07-31 17:49:47 +0200

    *Fixes Necrosis and Blood-Caked Blade. They now only proc from melee auto-attacks. Fix by kerekeszoltan90. Fixes issue #3184
    --HG--
    branch : trunk

---
+++ sql/updates/9120_world_spell_proc_event.sql
@@ -0,0 +1,9 @@
+INSERT INTO `spell_proc_event`(`entry`,`SchoolMask`,`SpellFamilyName`,`SpellFamilyMask0`,`SpellFamilyMask1`,`SpellFamilyMask2`,`procFlags`,`procEx`,`ppmRate`,`CustomChance`,`Cooldown`) VALUES
+(51459,0,0,0,0,0,4,0,0,0,0), --Necrosis Rank 1
+(51462,0,0,0,0,0,4,0,0,0,0), --Necrosis Rank 2
+(51463,0,0,0,0,0,4,0,0,0,0), --Necrosis Rank 3
+(51464,0,0,0,0,0,4,0,0,0,0), --Necrosis Rank 4
+(51465,0,0,0,0,0,4,0,0,0,0), --Necrosis Rank 5
+(49219,0,0,0,0,0,4,0,0,0,0), --Blood-Caked Blade Rank 1
+(49627,0,0,0,0,0,4,0,0,0,0), --Blood-Caked Blade Rank 2
+(49628,0,0,0,0,0,4,0,0,0,0); --Blood-Caked Blade Rank 3
