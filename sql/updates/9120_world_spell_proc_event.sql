f4b30d1c659c8315a1ce198fd539d4eefd411967
QAston <none@none>
2010-08-01 03:39:43 +0200

    *Fix import of 9120_world_spell_proc_event.sql when using some weird tools - by taliesin.tc.
    --HG--
    branch : trunk

--- sql/updates/9120_world_spell_proc_event.sql
+++ sql/updates/9120_world_spell_proc_event.sql
@@ -1,10 +1,10 @@
 DELETE FROM `spell_proc_event` WHERE `entry` IN (51459,51462,51463,51464,51465,49219,49627,49628);
 INSERT INTO `spell_proc_event`(`entry`,`SchoolMask`,`SpellFamilyName`,`SpellFamilyMask0`,`SpellFamilyMask1`,`SpellFamilyMask2`,`procFlags`,`procEx`,`ppmRate`,`CustomChance`,`Cooldown`) VALUES
-(51459,0,0,0,0,0,4,0,0,0,0), --Necrosis Rank 1
-(51462,0,0,0,0,0,4,0,0,0,0), --Necrosis Rank 2
-(51463,0,0,0,0,0,4,0,0,0,0), --Necrosis Rank 3
-(51464,0,0,0,0,0,4,0,0,0,0), --Necrosis Rank 4
-(51465,0,0,0,0,0,4,0,0,0,0), --Necrosis Rank 5
-(49219,0,0,0,0,0,4,0,0,0,0), --Blood-Caked Blade Rank 1
-(49627,0,0,0,0,0,4,0,0,0,0), --Blood-Caked Blade Rank 2
-(49628,0,0,0,0,0,4,0,0,0,0); --Blood-Caked Blade Rank 3
+(51459,0,0,0,0,0,4,0,0,0,0), -- Necrosis Rank 1
+(51462,0,0,0,0,0,4,0,0,0,0), -- Necrosis Rank 2
+(51463,0,0,0,0,0,4,0,0,0,0), -- Necrosis Rank 3
+(51464,0,0,0,0,0,4,0,0,0,0), -- Necrosis Rank 4
+(51465,0,0,0,0,0,4,0,0,0,0), -- Necrosis Rank 5
+(49219,0,0,0,0,0,4,0,0,0,0), -- Blood-Caked Blade Rank 1
+(49627,0,0,0,0,0,4,0,0,0,0), -- Blood-Caked Blade Rank 2
+(49628,0,0,0,0,0,4,0,0,0,0); -- Blood-Caked Blade Rank 3
