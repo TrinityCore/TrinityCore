f2acefe6e90e3c3524ac267187c891cf2daf9bcb
silinoron <none@none>
2010-08-06 18:48:08 -0700

    Convert Alterac Valley to new format.
    --HG--
    branch : trunk

--- src/server/scripts/EasternKingdoms/AlteracValley/boss_drekthar.cpp
+++ src/server/scripts/EasternKingdoms/AlteracValley/boss_drekthar.cpp
@@ -41,99 +41,101 @@ enum Yells
     YELL_RANDOM5                                  = -1810007
 };
 
-struct boss_drektharAI : public ScriptedAI
+class boss_drekthar : public CreatureScript
 {
-    boss_drektharAI(Creature *c) : ScriptedAI(c) {}
+public:
+    boss_drekthar() : CreatureScript("boss_drekthar") { }
 
-    uint32 uiWhirlwindTimer;
-    uint32 uiWhirlwind2Timer;
-    uint32 uiKnockdownTimer;
-    uint32 uiFrenzyTimer;
-    uint32 uiYellTimer;
-    uint32 uiResetTimer;
-
-    void Reset()
+    struct boss_drektharAI : public ScriptedAI
     {
-        uiWhirlwindTimer = urand(1*IN_MILLISECONDS,20*IN_MILLISECONDS);
-        uiWhirlwind2Timer = urand(1*IN_MILLISECONDS,20*IN_MILLISECONDS);
-        uiKnockdownTimer = 12*IN_MILLISECONDS;
-        uiFrenzyTimer = 6*IN_MILLISECONDS;
-        uiResetTimer = 5*IN_MILLISECONDS;
-        uiYellTimer = urand(20*IN_MILLISECONDS,30*IN_MILLISECONDS); //20 to 30 seconds
-    }
+        boss_drektharAI(Creature *c) : ScriptedAI(c) {}
 
-    void EnterCombat(Unit * /*who*/)
-    {
-        DoScriptText(YELL_AGGRO, me);
-    }
+        uint32 uiWhirlwindTimer;
+        uint32 uiWhirlwind2Timer;
+        uint32 uiKnockdownTimer;
+        uint32 uiFrenzyTimer;
+        uint32 uiYellTimer;
+        uint32 uiResetTimer;
 
-    void JustRespawned()
-    {
-        Reset();
-        DoScriptText(YELL_RESPAWN, me);
-    }
-
-    void UpdateAI(const uint32 diff)
-    {
-        if (!UpdateVictim())
-            return;
-
-        if (uiWhirlwindTimer <= diff)
+        void Reset()
         {
-            DoCast(me->getVictim(), SPELL_WHIRLWIND);
-            uiWhirlwindTimer =  urand(8*IN_MILLISECONDS,18*IN_MILLISECONDS);
-        } else uiWhirlwindTimer -= diff;
-
-        if (uiWhirlwind2Timer <= diff)
-        {
-            DoCast(me->getVictim(), SPELL_WHIRLWIND2);
-            uiWhirlwind2Timer = urand(7*IN_MILLISECONDS,25*IN_MILLISECONDS);
-        } else uiWhirlwind2Timer -= diff;
-
-        if (uiKnockdownTimer <= diff)
-        {
-            DoCast(me->getVictim(), SPELL_KNOCKDOWN);
-            uiKnockdownTimer = urand(10*IN_MILLISECONDS,15*IN_MILLISECONDS);
-        } else uiKnockdownTimer -= diff;
-
-        if (uiFrenzyTimer <= diff)
-        {
-            DoCast(me->getVictim(), SPELL_FRENZY);
-            uiFrenzyTimer = urand(20*IN_MILLISECONDS,30*IN_MILLISECONDS);
-        } else uiFrenzyTimer -= diff;
-
-        if (uiYellTimer <= diff)
-        {
-            DoScriptText(RAND(YELL_RANDOM1,YELL_RANDOM2,YELL_RANDOM3,YELL_RANDOM4,YELL_RANDOM5), me);
-            uiYellTimer = urand(20*IN_MILLISECONDS,30*IN_MILLISECONDS); //20 to 30 seconds
-        } else uiYellTimer -= diff;
-
-        // check if creature is not outside of building
-        if (uiResetTimer <= diff)
-        {
-            if (me->GetDistance2d(me->GetHomePosition().GetPositionX(), me->GetHomePosition().GetPositionY()) > 50)
-            {
-                EnterEvadeMode();
-                DoScriptText(YELL_EVADE, me);
-            }
+            uiWhirlwindTimer = urand(1*IN_MILLISECONDS,20*IN_MILLISECONDS);
+            uiWhirlwind2Timer = urand(1*IN_MILLISECONDS,20*IN_MILLISECONDS);
+            uiKnockdownTimer = 12*IN_MILLISECONDS;
+            uiFrenzyTimer = 6*IN_MILLISECONDS;
             uiResetTimer = 5*IN_MILLISECONDS;
-        } else uiResetTimer -= diff;
+            uiYellTimer = urand(20*IN_MILLISECONDS,30*IN_MILLISECONDS); //20 to 30 seconds
+        }
 
-        DoMeleeAttackIfReady();
+        void EnterCombat(Unit * /*who*/)
+        {
+            DoScriptText(YELL_AGGRO, me);
+        }
+
+        void JustRespawned()
+        {
+            Reset();
+            DoScriptText(YELL_RESPAWN, me);
+        }
+
+        void UpdateAI(const uint32 diff)
+        {
+            if (!UpdateVictim())
+                return;
+
+            if (uiWhirlwindTimer <= diff)
+            {
+                DoCast(me->getVictim(), SPELL_WHIRLWIND);
+                uiWhirlwindTimer =  urand(8*IN_MILLISECONDS,18*IN_MILLISECONDS);
+            } else uiWhirlwindTimer -= diff;
+
+            if (uiWhirlwind2Timer <= diff)
+            {
+                DoCast(me->getVictim(), SPELL_WHIRLWIND2);
+                uiWhirlwind2Timer = urand(7*IN_MILLISECONDS,25*IN_MILLISECONDS);
+            } else uiWhirlwind2Timer -= diff;
+
+            if (uiKnockdownTimer <= diff)
+            {
+                DoCast(me->getVictim(), SPELL_KNOCKDOWN);
+                uiKnockdownTimer = urand(10*IN_MILLISECONDS,15*IN_MILLISECONDS);
+            } else uiKnockdownTimer -= diff;
+
+            if (uiFrenzyTimer <= diff)
+            {
+                DoCast(me->getVictim(), SPELL_FRENZY);
+                uiFrenzyTimer = urand(20*IN_MILLISECONDS,30*IN_MILLISECONDS);
+            } else uiFrenzyTimer -= diff;
+
+            if (uiYellTimer <= diff)
+            {
+                DoScriptText(RAND(YELL_RANDOM1,YELL_RANDOM2,YELL_RANDOM3,YELL_RANDOM4,YELL_RANDOM5), me);
+                uiYellTimer = urand(20*IN_MILLISECONDS,30*IN_MILLISECONDS); //20 to 30 seconds
+            } else uiYellTimer -= diff;
+
+            // check if creature is not outside of building
+            if (uiResetTimer <= diff)
+            {
+                if (me->GetDistance2d(me->GetHomePosition().GetPositionX(), me->GetHomePosition().GetPositionY()) > 50)
+                {
+                    EnterEvadeMode();
+                    DoScriptText(YELL_EVADE, me);
+                }
+                uiResetTimer = 5*IN_MILLISECONDS;
+            } else uiResetTimer -= diff;
+
+            DoMeleeAttackIfReady();
+        }
+    };
+
+
+    CreatureAI *OnGetAI(Creature *creature) const
+    {
+        return new boss_drektharAI(creature);
     }
 };
 
-
-CreatureAI* GetAI_boss_drekthar(Creature *_Creature)
-{
-    return new boss_drektharAI (_Creature);
-}
-
 void AddSC_boss_drekthar()
 {
-    Script *newscript;
-    newscript = new Script;
-    newscript->Name = "boss_drekthar";
-    newscript->GetAI = &GetAI_boss_drekthar;
-    newscript->RegisterSelf();
+    new boss_drekthar;
 }
