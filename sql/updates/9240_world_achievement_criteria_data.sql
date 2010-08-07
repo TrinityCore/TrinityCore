6513306b73514da10648d6f44ee407b6a96241f9
Shauren <none@none>
2010-08-07 18:37:20 +0200

    Use proper WHERE query in 9240_world_achievement_criteria_data.sql
    --HG--
    branch : trunk

--- sql/updates/9240_world_achievement_criteria_data.sql
+++ sql/updates/9240_world_achievement_criteria_data.sql
@@ -1,2 +1,2 @@
-UPDATE `achievement_criteria_data` SET `ScriptName`='achievement_school_of_hard_knocks' WHERE `criteria_id` IN (6641,6642,6643,6644);
-UPDATE `achievement_criteria_data` SET `ScriptName`='achievement_storm_glory' WHERE `criteria_id`=3693;
+UPDATE `achievement_criteria_data` SET `ScriptName`='achievement_school_of_hard_knocks' WHERE `criteria_id` IN (6641,6642,6643,6644) AND `type`=11;
+UPDATE `achievement_criteria_data` SET `ScriptName`='achievement_storm_glory' WHERE `criteria_id`=3693 AND `type`=11;
