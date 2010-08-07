955d2ca746b216c73386cd2919ad6b10b0fa083f
silinoron <none@none>
2010-08-06 17:23:01 -0700

    Convert scripts in the Custom directory to new format
    --HG--
    branch : trunk

--- src/server/scripts/Custom/npc_acherus_taxi.cpp
+++ src/server/scripts/Custom/npc_acherus_taxi.cpp
@@ -20,33 +20,33 @@
 
 #define GOSSIP_FLIGHT "I need a ride"
 
-bool GossipHello_npc_acherus_taxi(Player *pPlayer, Creature *pCreature)
+class npc_acherus_taxi : public CreatureScript
 {
-    pPlayer->SetTaxiCheater(true);
-    pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_FLIGHT, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
-    pPlayer->SEND_GOSSIP_MENU(9978, pCreature->GetGUID());
-    return true;
-}
+public:
+    npc_acherus_taxi() : CreatureScript("npc_acherus_taxi") { }
 
-bool GossipSelect_npc_acherus_taxi(Player *pPlayer, Creature * /*pCreature*/, uint32 /*uiSender*/, uint32 uiAction)
-{
-    if (uiAction == GOSSIP_ACTION_INFO_DEF + 1)
+    bool OnGossipHello(Player *pPlayer, Creature *pCreature)
     {
-        if (pPlayer->GetPositionZ() >= 316)
-            pPlayer->GetSession()->SendDoFlight(24446, 1053);
-        else
-            pPlayer->GetSession()->SendDoFlight(24446, 1054);
+        pPlayer->SetTaxiCheater(true);
+        pPlayer->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_FLIGHT, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
+        pPlayer->SEND_GOSSIP_MENU(9978, pCreature->GetGUID());
+        return true;
     }
-    return true;
-}
+
+    bool OnGossipSelect(Player *pPlayer, Creature * /*pCreature*/, uint32 /*uiSender*/, uint32 uiAction)
+    {
+        if (uiAction == GOSSIP_ACTION_INFO_DEF + 1)
+        {
+            if (pPlayer->GetPositionZ() >= 316)
+                pPlayer->GetSession()->SendDoFlight(24446, 1053);
+            else
+                pPlayer->GetSession()->SendDoFlight(24446, 1054);
+        }
+        return true;
+    }
+};
 
 void AddSC_npc_acherus_taxi()
 {
-    Script *newscript;
-
-    newscript = new Script;
-    newscript->Name = "npc_acherus_taxi";
-    newscript->pGossipHello = &GossipHello_npc_acherus_taxi;
-    newscript->pGossipSelect = &GossipSelect_npc_acherus_taxi;
-    newscript->RegisterSelf();
+    new npc_acherus_taxi;
 }
