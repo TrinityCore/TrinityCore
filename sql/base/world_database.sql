90f53c2f9b14b05dd3e4e005f5d9236c8a89f221
XTZGZoReX <none@none>
2010-08-07 17:30:14 +0200

    * Some cleanups in world_scripts_full.sql. * SQL updates for `battleground_template`.`Comment` (by Click), and `outdoorpvp_template` content.
    --HG--
    branch : trunk

--- sql/base/world_database.sql
+++ sql/base/world_database.sql
@@ -254,20 +254,20 @@ CREATE TABLE `battleground_template` (
 
 LOCK TABLES `battleground_template` WRITE;
 /*!40000 ALTER TABLE `battleground_template` DISABLE KEYS */;
-INSERT INTO `battleground_template` (`id`,`MinPlayersPerTeam`,`MaxPlayersPerTeam`,`MinLvl`,`MaxLvl`,`AllianceStartLoc`,`AllianceStartO`,`HordeStartLoc`,`HordeStartO`, `Weight`) VALUES
-(1,20,40,51,80,611,2.72532,610,2.27452,1),
-(2,5,10,10,80,769,3.14159,770,3.14159,1),
-(3,8,15,20,80,890,3.40156,889,0.263892,1),
-(4,0,2,10,80,929,0,936,3.14159,1),
-(5,0,2,10,80,939,0,940,3.14159,1),
-(6,0,2,10,80,0,0,0,0,1),
-(7,8,15,61,80,1103,3.40156,1104,0.263892,1),
-(8,0,2,10,80,1258,0,1259,3.14159,1),
-(9,7,15,71,80,1367,0,1368,0,1),
-(10,5,5,10,80,1362,0,1363,0,1),
-(11,5,5,10,80,1364,0,1365,0,1),
-(30,20,40,71,80,1485,0,1486,0,1),
-(32,10,10,0,80,0,0,0,0,1);
+INSERT INTO `battleground_template` (`id`,`MinPlayersPerTeam`,`MaxPlayersPerTeam`,`MinLvl`,`MaxLvl`,`AllianceStartLoc`,`AllianceStartO`,`HordeStartLoc`,`HordeStartO`, `Weight`, `Comment`) VALUES
+(1,20,40,51,80,611,2.72532,610,2.27452,1,'Alterac Valley'),
+(2,5,10,10,80,769,3.14159,770,3.14159,1,'Warsong Gulch'),
+(3,8,15,20,80,890,3.40156,889,0.263892,1,'Arathi Basin'),
+(4,0,2,10,80,929,0,936,3.14159,1,'Nagrand Arena'),
+(5,0,2,10,80,939,0,940,3.14159,1,'Blades''s Edge Arena'),
+(6,0,2,10,80,0,0,0,0,1,'All Arena'),
+(7,8,15,61,80,1103,3.40156,1104,0.263892,1,'Eye of The Storm'),
+(8,0,2,10,80,1258,0,1259,3.14159,1,'Ruins of Lordaeron'),
+(9,7,15,71,80,1367,0,1368,0,1,'Strand of the Ancients'),
+(10,5,5,10,80,1362,0,1363,0,1,'Dalaran Sewers'),
+(11,5,5,10,80,1364,0,1365,0,1,'The Ring of Valor'),
+(30,20,40,71,80,1485,0,1486,0,1,'Isle of Conquest'),
+(32,10,10,0,80,0,0,0,0,1,'Random battleground');
 /*!40000 ALTER TABLE `battleground_template` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -3862,6 +3862,13 @@ CREATE TABLE `outdoorpvp_template` (
 
 LOCK TABLES `outdoorpvp_template` WRITE;
 /*!40000 ALTER TABLE `outdoorpvp_template` DISABLE KEYS */;
+INSERT INTO `outdoorpvp_template` (`TypeId`, `Comment`) VALUES
+(1, 'Hellfire Peninsula');
+(2, 'Nagrand');
+(3, 'Terokkar Forest');
+(4, 'Zangarmarsh');
+(5, 'Silithus');
+(6, 'Eastern Plaguelands');
 /*!40000 ALTER TABLE `outdoorpvp_template` ENABLE KEYS */;
 UNLOCK TABLES;
 
