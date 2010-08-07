14583f5709e7a77345ac346af243a347c99e8a79
Supabad <none@none>
2010-08-07 13:06:10 +0200

    convert Mechanar to new format
    --HG--
    branch : trunk

--- src/server/scripts/Outland/TempestKeep/Mechanar/instance_mechanar.cpp
+++ src/server/scripts/Outland/TempestKeep/Mechanar/instance_mechanar.cpp
@@ -28,61 +28,67 @@ EndScriptData */
 
 #define MAX_ENCOUNTER      1
 
-struct instance_mechanar : public ScriptedInstance
+class instance_mechanar : public InstanceMapScript
 {
-    instance_mechanar(Map* pMap) : ScriptedInstance(pMap) {Initialize();};
-
-    uint32 m_auiEncounter[MAX_ENCOUNTER];
-
-    void Initialize()
-    {
-        memset(&m_auiEncounter, 0, sizeof(m_auiEncounter));
-    }
-
-    bool IsEncounterInProgress() const
-    {
-        for (uint8 i = 0; i < MAX_ENCOUNTER; ++i)
-            if (m_auiEncounter[i] == IN_PROGRESS)
-                return true;
-
-        return false;
-    }
-
-    uint32 GetData(uint32 type)
-    {
-        switch(type)
+    public:
+        instance_mechanar()
+            : InstanceMapScript("instance_mechanar")
         {
-        case DATA_NETHERMANCER_EVENT:   return m_auiEncounter[0];
         }
-
-        return false;
-    }
-
-    uint64 GetData64 (uint32 /*identifier*/)
-    {
-        return 0;
-    }
-
-    void SetData(uint32 type, uint32 data)
-    {
-        switch(type)
+        
+        struct instance_mechanar_InstanceMapScript : public ScriptedInstance
         {
-        case DATA_NETHERMANCER_EVENT:   m_auiEncounter[0] = data;   break;
-        }
-    }
+            instance_mechanar_InstanceMapScript(Map* pMap) : ScriptedInstance(pMap) { Initialize(); };
+            
+            uint32 m_auiEncounter[MAX_ENCOUNTER];
+            
+            void Initialize()
+            {
+                memset(&m_auiEncounter, 0, sizeof(m_auiEncounter));
+            }
+            
+            bool IsEncounterInProgress() const
+            {
+                for (uint8 i = 0; i < MAX_ENCOUNTER; ++i)
+                    if (m_auiEncounter[i] == IN_PROGRESS)
+                        return true;
+
+                return false;
+            }
+            
+            uint32 GetData(uint32 type)
+            {
+                switch(type)
+                {
+                case DATA_NETHERMANCER_EVENT:   return m_auiEncounter[0];
+                }
+
+                return false;
+            }
+
+            uint64 GetData64 (uint32 /*identifier*/)
+            {
+                return 0;
+            }
+
+            void SetData(uint32 type, uint32 data)
+            {
+                switch(type)
+                {
+                case DATA_NETHERMANCER_EVENT:   m_auiEncounter[0] = data;   break;
+                }
+            }
+        };
+            InstanceData* OnGetInstanceData(InstanceMap* pMap)
+            {
+                return new instance_mechanar_InstanceMapScript(pMap);
+            }
+        
 };
 
-InstanceData* GetInstanceData_instance_mechanar(Map* pMap)
-{
-    return new instance_mechanar(pMap);
-}
 
 void AddSC_instance_mechanar()
 {
-    Script *newscript;
-    newscript = new Script;
-    newscript->Name = "instance_mechanar";
-    newscript->GetInstanceData = &GetInstanceData_instance_mechanar;
-    newscript->RegisterSelf();
+    new instance_mechanar;
 }
 
