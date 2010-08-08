93dc9c2761fa90e766b87fd7fe23cf0fc73dd4bc
John Holiver <none@none>
2010-08-08 15:59:17 -0300

    [FIX] Intravenous Healing Potion. Thanks Gyullo.
    --HG--
    branch : trunk

--- sql/base/world_database.sql
+++ sql/base/world_database.sql
@@ -6321,7 +6321,9 @@ INSERT INTO `spell_linked_spell` (`spell_trigger`,`spell_effect`,`type`,`comment
 ( 69381, 72588, 1, 'Drums of the Wild'),
 ( 69378, 72586, 1, 'Drums of the Forgotten Kings'),
 ( 69377, 72590, 1, 'Runescroll of Fortitude'),
-( 50141, 50001, 0, 'Blood Oath to Blood Oath Aura');
+( 50141, 50001, 0, 'Blood Oath to Blood Oath Aura'),
+( 61263, 61267, 0, 'Intravenous Healing Effect'),
+( 61263, 61268, 0, 'Intravenous Mana Regeneration Effect');
 /*!40000 ALTER TABLE `spell_linked_spell` ENABLE KEYS */;
 UNLOCK TABLES;
 
