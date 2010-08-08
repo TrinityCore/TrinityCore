d88df48a55dc0e5c6b94d2ef9065a2ba6946ed40
XTZGZoReX <none@none>
2010-08-08 22:05:28 +0200

    * Fix SQL errors, again thanks to Machiavelli.
    --HG--
    branch : trunk

--- sql/updates/9237_world_outdoorpvp_template.sql
+++ sql/updates/9237_world_outdoorpvp_template.sql
@@ -1,8 +1,8 @@
 DELETE FROM `outdoorpvp_template` WHERE `TypeId` IN (1,2,3,4,5,6);
 INSERT INTO `outdoorpvp_template` (`TypeId`, `ScriptName`, `Comment`) VALUES
-(1, 'outdoorpvp_hp', 'Hellfire Peninsula');
-(2, 'outdoorpvp_na', 'Nagrand');
-(3, 'outdoorpvp_tf', 'Terokkar Forest');
-(4, 'outdoorpvp_zm', 'Zangarmarsh');
-(5, 'outdoorpvp_si', 'Silithus');
+(1, 'outdoorpvp_hp', 'Hellfire Peninsula'),
+(2, 'outdoorpvp_na', 'Nagrand'),
+(3, 'outdoorpvp_tf', 'Terokkar Forest'),
+(4, 'outdoorpvp_zm', 'Zangarmarsh'),
+(5, 'outdoorpvp_si', 'Silithus'),
 (6, 'outdoorpvp_ep', 'Eastern Plaguelands');
