0ce01cdb63bba202a0f0be6c7d0eac3dd6cb5df2
John Holiver <none@none>
2010-08-08 03:41:36 -0300

    Missed a new file from EasternKingdoms/Uldaman
    --HG--
    branch : trunk

---
+++ src/server/scripts/EasternKingdoms/Uldaman/uldaman.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2008-2010 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2006-2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DEF_ULDAMAN_H
+#define DEF_ULDAMAN_H
+
+enum eObjects
+{
+    GO_ARCHAEDAS_TEMPLE_DOOR            = 141869,
+    GO_ALTAR_OF_THE_KEEPER_TEMPLE_DOOR  = 124367,
+    GO_ANCIENT_VAULT_DOOR               = 124369,
+};
+
+#endif
