bbdaaf23b9b1d4a12d4d8cc26069aa1dbcbe4c31
Shauren <none@none>
2010-08-09 22:08:03 +0200

    achievement_criteria_data data type 11 is now handled by core team (as its purpose is scriptname only) Reimporting of SQL is not needed
    --HG--
    branch : trunk

--- sql/updates/9240_world_achievement_criteria_data.sql
+++ sql/updates/9240_world_achievement_criteria_data.sql
@@ -1,2 +1,7 @@
-UPDATE `achievement_criteria_data` SET `ScriptName`='achievement_school_of_hard_knocks' WHERE `criteria_id` IN (6641,6642,6643,6644) AND `type`=11;
-UPDATE `achievement_criteria_data` SET `ScriptName`='achievement_storm_glory' WHERE `criteria_id`=3693 AND `type`=11;
+DELETE FROM `achievement_criteria_data` WHERE `entry` IN (3693,6641,6642,6643,6644) AND `type`=11;
+INSERT INTO `achievement_criteria_data` (`criteria_id`,`type`,`value1`,`value2`,`ScriptName`) VALUES
+(3693,11,0,0, 'achievement_storm_glory'),
+(6641,11,0,0, 'achievement_school_of_hard_knocks'),
+(6642,11,0,0, 'achievement_school_of_hard_knocks'),
+(6643,11,0,0, 'achievement_school_of_hard_knocks'),
+(6644,11,0,0, 'achievement_school_of_hard_knocks');
