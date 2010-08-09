00f15cb8f2cb6b5eff4227cde255f23aa5759f84
Shauren <none@none>
2010-08-09 22:08:03 +0200

    achievement_criteria_data data type 11 is now handled by core team (as its purpose is scriptname only) Reimporting of SQL is not needed
    --HG--
    branch : trunk

--- sql/scripts/world_scripts_full.sql
+++ sql/scripts/world_scripts_full.sql
@@ -1654,7 +1654,12 @@ UPDATE `outdoorpvp_template` SET `ScriptName`='outdoorpvp_si' WHERE `TypeId`=5;
 UPDATE `outdoorpvp_template` SET `ScriptName`='outdoorpvp_ep' WHERE `TypeId`=6;
 
 /* ACHIEVEMENTS */
-UPDATE `achievement_criteria_data` SET `ScriptName`='achievement_school_of_hard_knocks' WHERE `criteria_id` IN (6641,6642,6643,6644);
-UPDATE `achievement_criteria_data` SET `ScriptName`='achievement_storm_glory' WHERE `criteria_id`=3693;
+DELETE FROM `type`=11;
+INSERT INTO `achievement_criteria_data` (`criteria_id`,`type`,`value1`,`value2`,`ScriptName`) VALUES
+(3693,11,0,0, 'achievement_storm_glory'),
+(6641,11,0,0, 'achievement_school_of_hard_knocks'),
+(6642,11,0,0, 'achievement_school_of_hard_knocks'),
+(6643,11,0,0, 'achievement_school_of_hard_knocks'),
+(6644,11,0,0, 'achievement_school_of_hard_knocks');
 
 /* EOF */
