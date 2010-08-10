85439d4fbbe763384f34c80dc7c7241c4a862d03
Spp <spp@jorge.gr>
2010-08-10 10:00:57 +0200

    *Fix typo in 9240 sql file *Fix typo in my previous commit
    Note to self: next time review the changes, not just C&P the delete from 9240*.sql
    
    --HG--
    branch : trunk

--- sql/scripts/world_scripts_full.sql
+++ sql/scripts/world_scripts_full.sql
@@ -1654,7 +1654,7 @@ UPDATE `outdoorpvp_template` SET `ScriptName`='outdoorpvp_si' WHERE `TypeId`=5;
 UPDATE `outdoorpvp_template` SET `ScriptName`='outdoorpvp_ep' WHERE `TypeId`=6;
 
 /* ACHIEVEMENTS */
-DELETE FROM `achievement_criteria_data` WHERE `entry` IN (3693,6641,6642,6643,6644) AND `type`=11;
+DELETE FROM `achievement_criteria_data` WHERE `criteria_id` IN (3693,6641,6642,6643,6644) AND `type`=11;
 INSERT INTO `achievement_criteria_data` (`criteria_id`,`type`,`value1`,`value2`,`ScriptName`) VALUES
 (3693,11,0,0, 'achievement_storm_glory'),
 (6641,11,0,0, 'achievement_school_of_hard_knocks'),
