5d89bcbbbf07f006d8a7875e51d2adfbe811ba81
Spp <none@none>
2010-08-14 15:00:04 +0200

    Dungeon Finder: Remove players from queue when accept to join a group This fixes players being multiple times in queue
    --HG--
    branch : trunk

--- src/server/game/Groups/Group.cpp
+++ src/server/game/Groups/Group.cpp
@@ -316,6 +316,7 @@ bool Group::AddMember(const uint64 &guid, const char* name)
     Player *player = sObjectMgr.GetPlayer(guid);
     if (player)
     {
+        sLFGMgr.Leave(player);
         if (!IsLeader(player->GetGUID()) && !isBGGroup())
         {
             // reset the new member's instances, unless he is currently in one of them
