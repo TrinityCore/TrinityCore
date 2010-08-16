40e641cc1c768628ddd555e3275ac7c9b69805ee
click <none@none>
2010-08-16 12:19:52 +0200

    It might actually help you all to have the PCH-files as well...
    --HG--
    branch : trunk

---
+++ src/server/shared/PrecompiledHeaders/sharedPCH.h
@@ -0,0 +1,14 @@
+//add here most rarely modified headers to speed up debug build compilation
+
+#include "Common.h"
+#include "Define.h"
+#include "Logging/Log.h"
+#include "DatabaseEnv.h"
+#include "Threading/Threading.h"
+#include "Database/DatabaseEnv.h"
+#include "Database/SqlDelayThread.h"
+#include "Database/SqlOperations.h"
+#include "Debugging/Errors.h"
+#include "Dynamic/TypeList.h"
+#include "Dynamic/UnorderedMap.h"
+
---
+++ src/server/worldserver/PrecompiledHeaders/worldPCH.h
@@ -0,0 +1,6 @@
+#include "Common.h"
+#include "World.h"
+#include "Log.h"
+#include "Database/DatabaseEnv.h"
+#include "Configuration/Config.h"
+#include "Util.h"
