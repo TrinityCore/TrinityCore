646808fcc53d0340c11a1289e7d2a0461da72564
teacher <none@none>
2010-08-17 09:34:59 +0200

    Proper insert statement for 9421_world_command.sql (also help description needs an update). Re-import it if you already have imported it a first time. Added recent sql update to full.
    --HG--
    branch : trunk

--- sql/updates/9421_world_command.sql
+++ sql/updates/9421_world_command.sql
@@ -1,4 +1,4 @@
-DELETE FROM `command` WHERE `name` IN ('summon', 'appear', 'groupsummon');
-UPDATE `command` SET `name`='summon' WHERE `name`='namego';
-UPDATE `command` SET `name`='appear' WHERE `name`='goname';
-UPDATE `command` SET `name`='groupsummon' WHERE `name`='groupgo';
+DELETE FROM `command` WHERE `name` IN ('namego','goname','groupgo','summon','appear','groupsummon');
+('appear',1,'Syntax: .appear [$charactername]\r\n\r\nTeleport to the given character. Either specify the character name or click on the character''s portrait, e.g. when you are in a group. Character can be offline.'),
+('groupsummon',1,'Syntax: .groupsummon [$charactername]\r\n\r\nTeleport the given character and his group to you. Teleported only online characters but original selected group member can be offline.'),
+('summon',1,'Syntax: .summon [$charactername]\r\n\r\nTeleport the given character to you. Character can be offline.');
