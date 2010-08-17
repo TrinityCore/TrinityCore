25cdfccaebf816420f1527176a1df9205374f2b9
click <none@none>
2010-08-17 16:04:50 +0200

    Core/SQL: Correction on SQL-file, make sure the SQL is properly formatted with a ; in the end.
    --HG--
    branch : trunk

--- sql/updates/9427_world_command.sql
+++ sql/updates/9427_world_command.sql
@@ -2,4 +2,4 @@ DELETE FROM `command` WHERE `name` IN ('reload gossip_menu','reload gossip_menu_
 INSERT INTO `command` (`name`,`security`,`help`) VALUES
 ('reload gossip_menu',3, 'Syntax: .reload gossip_menu\nReload gossip_menu table.'),
 ('reload gossip_menu_option',3, 'Syntax: .reload gossip_menu_option\nReload gossip_menu_option table.'),
-('reload gossip_scripts',3, 'Syntax: .reload gossip_scripts\nReload gossip_scripts table.'),
+('reload gossip_scripts',3, 'Syntax: .reload gossip_scripts\nReload gossip_scripts table.');
