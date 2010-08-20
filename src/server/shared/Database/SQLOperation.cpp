02e59ebdde249eb7595217dc690d8b94e5b6cc6f
click <none@none>
2010-08-20 11:48:12 +0200

    Core/Build: Fix non-PCH build, again... :P
    --HG--
    branch : trunk

--- src/server/shared/Database/SQLOperation.cpp
+++ src/server/shared/Database/SQLOperation.cpp
@@ -16,9 +16,9 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include "DatabaseEnv.h"
 #include "SQLOperation.h"
 #include "MySQLConnection.h"
-#include "Log.h"
 
 /*! Basic, ad-hoc queries. */
 BasicStatementTask::BasicStatementTask(const char* sql) :
