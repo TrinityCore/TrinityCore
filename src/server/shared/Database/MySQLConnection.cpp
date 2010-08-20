24d752e782f64b5f65166ebf1c108863e0aac61d
click <none@none>
2010-08-20 11:48:12 +0200

    Core/Build: Fix non-PCH build, again... :P
    --HG--
    branch : trunk

--- src/server/shared/Database/MySQLConnection.cpp
+++ src/server/shared/Database/MySQLConnection.cpp
@@ -15,9 +15,8 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-
 #include <mysql.h>
-#include "Common.h"
+#include "DatabaseEnv.h"
 #include "QueryResult.h"
 #include "SQLOperation.h"
 #include "MySQLConnection.h"
