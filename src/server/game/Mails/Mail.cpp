f7ad5b26243c44b37c496021b75d98bc72fcdf4a
click <none@none>
2010-08-21 09:53:04 +0200

    Core/Build: Clean up more nonPCH-issues + fix a warning in Timer.h (unsigned/signed cast)
    --HG--
    branch : trunk

--- src/server/game/Mails/Mail.cpp
+++ src/server/game/Mails/Mail.cpp
@@ -18,6 +18,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include "DatabaseEnv.h"
 #include "Mail.h"
 #include "WorldPacket.h"
 #include "WorldSession.h"
