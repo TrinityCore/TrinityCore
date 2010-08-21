18cdfe926171c272385c2fc53ce93b22983b5b7d
click <none@none>
2010-08-21 19:40:05 +0200

    Core: Remove warning-ignores from Common.h, and fix a shitload of retarded warnings that was hidden by it - NO MORE HIDING IN CORE THANKYOU!
    --HG--
    branch : trunk

--- src/server/shared/Database/SQLOperation.cpp
+++ src/server/shared/Database/SQLOperation.cpp
@@ -16,6 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include "Common.h"
 #include "DatabaseEnv.h"
 #include "SQLOperation.h"
 #include "MySQLConnection.h"
