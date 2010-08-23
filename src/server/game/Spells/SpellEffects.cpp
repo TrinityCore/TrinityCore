34384a38326d2fe8152d266566ebcf8dc6fccb3b
silinoron <none@none>
2010-08-23 15:51:34 -0700

    Spell 62124 should allow critical hits. Based on patch by Toni.Shocker (yes, again). Fixes issue #3554 Also get rid of GuildScript in TODO, as it already exists.
    --HG--
    branch : trunk

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -4138,8 +4138,11 @@ void Spell::EffectTaunt(uint32 /*i*/)
     if (m_spellInfo->Id == 62124)
     {
         int32 damageDone = int32(1 + m_caster->GetTotalAttackPowerValue(BASE_ATTACK) * 0.5f);
+        bool is_crit = m_caster->isSpellCrit(unitTarget, m_spellInfo, m_spellSchoolMask, m_attackType);
+        if (is_crit)
+            damageDone *= 2;
         m_caster->DealDamage(unitTarget, damageDone, NULL, SPELL_DIRECT_DAMAGE, SPELL_SCHOOL_MASK_HOLY, m_spellInfo, false);
-        m_caster->SendSpellNonMeleeDamageLog(unitTarget, m_spellInfo->Id, damageDone, SPELL_SCHOOL_MASK_HOLY, 0, 0, false, false, false);
+        m_caster->SendSpellNonMeleeDamageLog(unitTarget, m_spellInfo->Id, damageDone, SPELL_SCHOOL_MASK_HOLY, 0, 0, false, false, is_crit);
     }
 
     // Also use this effect to set the taunter's threat to the taunted creature's highest value
