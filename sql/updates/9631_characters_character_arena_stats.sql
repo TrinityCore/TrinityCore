63043b382cf638b7cfd562e2564990cef842274f
click <none@none>
2010-08-27 18:35:34 +0200

    Remove previously renamed SQL-updates - they're part of 9632, not 9631 - hg retardedness ftw.
    --HG--
    branch : trunk

--- sql/updates/9631_characters_character_arena_stats.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `character_arena_stats` (
-`guid`  int(10) NOT NULL ,
-`slot`  smallint(1) NOT NULL ,
-`personal_rating`  int(10) NOT NULL ,
-`matchmaker_rating`  int(10) NOT NULL ,
-PRIMARY KEY (`guid`, `slot`)
-);
