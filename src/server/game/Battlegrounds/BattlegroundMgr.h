a3a7369c016f8c2b2dabc4cd295e53f2d3cf7d34
Shocker <none@none>
2010-08-27 04:49:04 +0300

    Fix arena unit frames, original code by A Metaphysical Drama
    --HG--
    branch : trunk

--- src/server/game/Battlegrounds/BattlegroundMgr.h
+++ src/server/game/Battlegrounds/BattlegroundMgr.h
@@ -52,7 +52,7 @@ class BattlegroundMgr
         void BuildGroupJoinedBattlegroundPacket(WorldPacket *data, GroupJoinBattlegroundResult result);
         void BuildUpdateWorldStatePacket(WorldPacket *data, uint32 field, uint32 value);
         void BuildPvpLogDataPacket(WorldPacket *data, Battleground *bg);
-        void BuildBattlegroundStatusPacket(WorldPacket *data, Battleground *bg, uint8 QueueSlot, uint8 StatusID, uint32 Time1, uint32 Time2, uint8 arenatype);
+        void BuildBattlegroundStatusPacket(WorldPacket *data, Battleground *bg, uint8 QueueSlot, uint8 StatusID, uint32 Time1, uint32 Time2, uint8 arenatype, uint8 uiFrame = 1);
         void BuildPlaySoundPacket(WorldPacket *data, uint32 soundid);
         void SendAreaSpiritHealerQueryOpcode(Player *pl, Battleground *bg, const uint64& guid);
 
