d802407d8b18d3d01eeef8b5fa5212cdda5f4f74
click <none@none>
2010-08-28 21:42:07 +0200

    Buildsystem: Make options load BEFORE platformchecks, so they are preloaded correctly + add some more output to make it easier to find possible issues with the new layout
    --HG--
    branch : trunk

--- cmake/compiler/gcc/settings.cmake
+++ cmake/compiler/gcc/settings.cmake
@@ -2,14 +2,18 @@ add_definitions(-fno-delete-null-pointer-checks)
 
 if( USE_SFMT)
   add_definitions(-msse2)
+  message(STATUS "- GCC: SFMT enabled, SSE2 flag forced")
 endif()
 
 if( WITH_WARNINGS )
   add_definitions(-Wall -Wfatal-errors -Wextra)
+  message(STATUS "- GCC: All warnings enabled")
 else()
   add_definitions(--no-warnings)
+  message(STATUS "- GCC: All warnings disabled")
 endif()
 
 if( WITH_COREDEBUG )
   add_definitions(-ggdb3)
+  message(STATUS "- GCC: Debug-flags set (-ggdb3)")
 endif()
