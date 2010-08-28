847d0064d67a4ce9932a4d5dcf171587c9505a5c
click <none@none>
2010-08-28 21:51:28 +0200

    Buildsystem: Split out the resultlist from the options, and print it AFTER we've grabbed all the settings
    --HG--
    branch : trunk

---
+++ cmake/showoptions.cmake
@@ -0,0 +1,80 @@
+# output generic information about the core and buildtype chosen
+
+message("")
+message("* TrinityCore revision   : ${HG_REVISION}")
+if( UNIX )
+  message("* Build binaries in      : ${CMAKE_BUILD_TYPE} mode")
+endif()
+message("")
+
+# output information about installation-directories and locations
+
+message("* Install core to        : ${CMAKE_INSTALL_PREFIX}")
+if( UNIX )
+  message("* Install libraries to   : ${LIBSDIR}")
+  message("* Install configs to     : ${CONF_DIR}")
+endif()
+message("")
+
+# Show infomation about the options selected during configuration
+
+if( SERVERS )
+  message("* Build world/auth       : Yes (default)")
+else()
+  message("* Build world/authserver : No")
+endif()
+
+if( SCRIPTS )
+  message("* Build with scripts     : Yes (default)")
+  add_definitions(-DSCRIPTS)
+else()
+  message("* Build with scripts     : No")
+  set(USE_SCRIPTPCH 0)
+endif()
+
+if( TOOLS )
+  message("* Build map/vmap tools   : Yes")
+else()
+  message("* Build map/vmap tools   : No  (default)")
+endif()
+
+if( WITH_SQL )
+  message("* Install SQL-files      : Yes")
+else()
+  message("* Install SQL-files      : No  (default)")
+endif()
+
+if( USE_COREPCH )
+  message("* Build core w/PCH       : Yes (default)")
+else()
+  message("* Build core w/PCH       : No")
+endif()
+
+if( USE_SCRIPTPCH )
+  message("* Build scripts w/PCH    : Yes (default)")
+else()
+  message("* Build scripts w/PCH    : No")
+endif()
+
+if( USE_SFMT )
+  message("* Use SFMT for RNG       : Yes")
+  add_definitions(-DUSE_SFMT_FOR_RNG)
+else()
+  message("* Use SFMT for RNG       : No  (default)")
+endif()
+
+if( WITH_WARNINGS )
+  message("* Show all warnings      : Yes")
+else()
+  message("* Show compile-warnings  : No  (default)")
+endif()
+
+if( WITH_COREDEBUG )
+  message("* Use coreside debug     : Yes")
+  add_definitions(-DTRINITY_DEBUG)
+else()
+  message("* Use coreside debug     : No  (default)")
+endif()
+
+message("")
+
