b0d85ac1858ecc162adb08a8de1b3da68121ce64
click <none@none>
2010-08-30 11:50:48 +0200

    Buildsystem/MSVC: Reinstate regular flags, seems like C++-flags and C-flags doesn't mix very well on MSVC
    --HG--
    branch : trunk

--- cmake/compiler/msvc/settings.cmake
+++ cmake/compiler/msvc/settings.cmake
@@ -45,12 +45,8 @@ message(STATUS "- MSVC: Enabled extended object-support for debug-compiles")
 # disable warnings in Visual Studio 8 and above if not wanted
 if(NOT WITH_WARNINGS)
   if(MSVC AND NOT CMAKE_GENERATOR MATCHES "Visual Studio 7")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /wd4996 /wd4355 /wd4244 /wd4985 /wd4267 /wd4619")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4996 /wd4355 /wd4244 /wd4985 /wd4267 /wd4619")
     message(STATUS "- MSVC: Disabled generic compiletime warnings")
   endif()
 endif()
-
-# Copy CMAKE_CX_FLAGS to CMAKE_C_FLAGS to avoid inconsistency
-set(CMAKE_C_FLAGS "${CMAKE_CXX_FLAGS}")
-set(CMAKE_C_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG}")
-message(STATUS "- MSVC: Copied CXX_FLAGS to C_FLAGS (consistency)")
