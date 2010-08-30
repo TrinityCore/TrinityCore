5743e56e78b796bd4fd198e6e37b49248b4fc938
Shocker <none@none>
2010-08-30 09:26:28 +0300

    * Include needed SQL data for Lord Marrowgar / Deathbringer Saurfang * Small SQL correction from a previous commit, MySQL doesn't allow default value for a LONGTEXT on Windows, unlinke Linux where it's only a warning
    --HG--
    branch : trunk

--- sql/base/characters_database.sql
+++ sql/base/characters_database.sql
@@ -1320,7 +1320,7 @@ CREATE TABLE `gm_subsurveys` (
   `surveyid` int(10) NOT NULL,
   `subsurveyid` int(11) UNSIGNED NOT NULL DEFAULT '0',
   `rank` int(11) UNSIGNED NOT NULL DEFAULT '0',
-  `comment` longtext NOT NULL DEFAULT '',
+  `comment` longtext NOT NULL,
   PRIMARY KEY (`surveyid`,`subsurveyid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Player System';
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -1344,7 +1344,7 @@ CREATE TABLE `gm_surveys` (
   `surveyid` int(10) NOT NULL auto_increment,
   `player` int(11) UNSIGNED NOT NULL DEFAULT '0',
   `mainSurvey` int(11) UNSIGNED NOT NULL DEFAULT '0',
-  `overall_comment` longtext NOT NULL DEFAULT '',
+  `overall_comment` longtext NOT NULL,
   `timestamp` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`surveyid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Player System';
