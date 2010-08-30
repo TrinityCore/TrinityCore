88a0c007cee2e9a09d426acad136cbaf7f9b63bf
Machiavelli <none@none>
2010-08-30 19:19:47 +0200

    Wrap guild_bank_item save queries in transactions. Fixes a potential dataloss issue
    --HG--
    branch : trunk

--- src/server/game/Guilds/Guild.h
+++ src/server/game/Guilds/Guild.h
@@ -429,7 +429,7 @@ class Guild
         void   LoadGuildBankEventLogFromDB();
         void   DisplayGuildBankLogs(WorldSession *session, uint8 TabId);
         void   LogBankEvent(SQLTransaction& trans, uint8 EventType, uint8 TabId, uint32 PlayerGuidLow, uint32 ItemOrMoney, uint8 ItemStackCount=0, uint8 DestTabId=0);
-        bool   AddGBankItemToDB(uint32 GuildId, uint32 BankTab , uint32 BankTabSlot , uint32 GUIDLow, uint32 Entry);
+        bool   AddGBankItemToDB(uint32 GuildId, uint32 BankTab , uint32 BankTabSlot , uint32 GUIDLow, uint32 Entry, SQLTransaction& trans);
 
     protected:
         void AddRank(const std::string& name,uint32 rights,uint32 money);
