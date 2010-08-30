6d9c0e9e0a8a22d05286c8f6897f30fb6b6eb9c9
click <none@none>
2010-08-30 22:58:37 +0200

    Buildsystem: Clean up target link-definitions a bit
    --HG--
    branch : trunk

--- cmake/platform/settings.win32.cmake
+++ cmake/platform/settings.win32.cmake
@@ -1,5 +1,8 @@
 # Package overloads
 set(ACE_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/dep/acelite)
+set(ACE_LIBRARY "ace")
+set(BZIP2_LIBRARIES "bzip2")
+set(ZLIB_LIBRARIES "zlib")
 
 # check the CMake preload parameters (commented out by default)
 
