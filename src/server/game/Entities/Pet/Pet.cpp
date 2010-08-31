e32ece52ea801127941fd352980a2e7d7958a9c6
Shocker <none@none>
2010-08-31 20:40:43 +0300

    Pet level-synchronization with owner should be done before initializing stats, closes issue 3732, thanks moriquendu
    --HG--
    branch : trunk

--- src/server/game/Entities/Pet/Pet.cpp
+++ src/server/game/Entities/Pet/Pet.cpp
@@ -236,6 +236,7 @@ bool Pet::LoadPetFromDB(Player* owner, uint32 petentry, uint32 petnumber, bool c
     SetReactState(ReactStates(fields[6].GetUInt8()));
 
     SetCanModifyStats(true);
+    SynchronizeLevelWithOwner();
     InitStatsForLevel(petlevel);
 
     if (getPetType() == SUMMON_PET && !current)              //all (?) summon pets come with full health when called, but not when they are current
@@ -341,7 +342,6 @@ bool Pet::LoadPetFromDB(Player* owner, uint32 petentry, uint32 petnumber, bool c
 
     m_loading = false;
 
-    SynchronizeLevelWithOwner();
     return true;
 }
 
