2e7ba5e327f902ee8c1c1fdeca6f150a37ec70a9
silinoron <none@none>
2010-08-31 17:41:18 -0700

    Oooh, C++... How I love you.
    --HG--
    branch : trunk

--- src/server/game/Tickets/TicketMgr.cpp
+++ src/server/game/Tickets/TicketMgr.cpp
@@ -156,7 +156,7 @@ void TicketMgr::_AddOrUpdateGMTicket(GM_Ticket &ticket)
     ss << ticket.assignedToGM << ", '";
     ss << comment << "', ";
     ss << (ticket.completed ? 1 : 0) << ", ";
-    ss << ticket.escalated << ", ";
+    ss << uint32(ticket.escalated) << ", ";
     ss << (ticket.viewed ? 1 : 0) << ");";
     
     SQLTransaction trans = CharacterDatabase.BeginTransaction();
