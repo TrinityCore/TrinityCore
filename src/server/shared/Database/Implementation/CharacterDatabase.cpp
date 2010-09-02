cd182a1e8fea18c8c054f586bd369daa5bf085ba
Shauren <none@none>
2010-09-02 20:22:19 +0200

    Fixed build Fixed wrong header includes in new database files, thx Thyros
    --HG--
    branch : trunk

--- src/server/shared/Database/Implementation/CharacterDatabase.cpp
+++ src/server/shared/Database/Implementation/CharacterDatabase.cpp
@@ -16,7 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include "LoginDatabase.h"
+#include "CharacterDatabase.h"
 
 bool CharacterDatabaseConnection::Open(const std::string& infoString)
 {
