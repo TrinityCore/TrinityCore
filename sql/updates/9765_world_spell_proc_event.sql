fde0ad513a13f495fa1ccb52924e29540544a517
Shauren <none@none>
2010-09-04 23:09:46 +0200

    Core/Spells: Fixed Scent of Blood removal SQLs: Fixed typo in previous revision
    Closes issue #2668.
    
    --HG--
    branch : trunk

---
+++ sql/updates/9765_world_spell_proc_event.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_proc_event` WHERE `entry`=50421;
+INSERT INTO `spell_proc_event` (`entry`,`SchoolMask`,`SpellFamilyName`,`SpellFamilyMask0`,`SpellFamilyMask1`,`SpellFamilyMask2`,`procFlags`,`procEx`,`ppmRate`,`CustomChance`,`Cooldown`) VALUES
+(50421,0x00,0,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0,0,0); -- Scent of Blood
