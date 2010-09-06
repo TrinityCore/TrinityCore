5709007b021313358707ac6310c896c367154ad2
Shauren <none@none>
2010-09-06 11:27:00 +0200

    Core/Loot: Automatically distribute currency items when they drop, based on Destros' patch
    Closes issue #2913.
    
    --HG--
    branch : trunk

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -15262,10 +15262,9 @@ void Unit::Kill(Unit *pVictim, bool durabilityLoss)
 
     // Do KILL and KILLED procs. KILL proc is called only for the unit who landed the killing blow (and its owner - for pets and totems) regardless of who tapped the victim
     if (isPet() || isTotem())
-    {
         if (Unit *owner = GetOwner())
             owner->ProcDamageAndSpell(pVictim, PROC_FLAG_KILL, PROC_FLAG_NONE, PROC_EX_NONE, 0);
-    }
+
     ProcDamageAndSpell(pVictim, PROC_FLAG_KILL, PROC_FLAG_KILLED, PROC_EX_NONE, 0);
 
     // Proc auras on death - must be before aura/combat remove
