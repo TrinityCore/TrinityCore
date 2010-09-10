4f37c8a8f6b6de24f6f2e9f947fded9bb2f8ab6c
Spp <none@none>
2010-09-10 13:45:21 +0200

    Core/WorldSession: use extra param in SendPartyResult for future use
    --HG--
    branch : trunk

--- src/server/game/Server/Protocol/Handlers/GroupHandler.cpp
+++ src/server/game/Server/Protocol/Handlers/GroupHandler.cpp
@@ -47,13 +47,13 @@ class Aura;
     -FIX sending PartyMemberStats
 */
 
-void WorldSession::SendPartyResult(PartyOperation operation, const std::string& member, PartyResult res)
+void WorldSession::SendPartyResult(PartyOperation operation, const std::string& member, PartyResult res, uint32 val /* = 0 */)
 {
-    WorldPacket data(SMSG_PARTY_COMMAND_RESULT, (4+member.size()+1+4+4));
-    data << (uint32)operation;
+    WorldPacket data(SMSG_PARTY_COMMAND_RESULT, 4 + member.size() + 1 + 4 + 4);
+    data << uint32(operation);
     data << member;
-    data << (uint32)res;
-    data << uint32(0);                                      // LFD cooldown related (used with ERR_PARTY_LFG_BOOT_COOLDOWN_S and ERR_PARTY_LFG_BOOT_NOT_ELIGIBLE_S)
+    data << uint32(res);
+    data << uint32(val);                                    // LFD cooldown related (used with ERR_PARTY_LFG_BOOT_COOLDOWN_S and ERR_PARTY_LFG_BOOT_NOT_ELIGIBLE_S)
 
     SendPacket(&data);
 }
