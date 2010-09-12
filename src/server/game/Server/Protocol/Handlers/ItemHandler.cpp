80e6dc69040600cf08732675e773588decb13a52
click <none@none>
2010-09-12 02:59:19 +0200

    Core/ItemHandler: Force size on abs() value calaulation The size of the return value of abs() is natively defined, and can exceed 32 bits on x64 systems Fix by Zor
    --HG--
    branch : trunk

--- src/server/game/Server/Protocol/Handlers/ItemHandler.cpp
+++ src/server/game/Server/Protocol/Handlers/ItemHandler.cpp
@@ -424,7 +424,7 @@ void WorldSession::HandleItemQuerySingleOpcode(WorldPacket & recv_data)
         data << pProto->GemProperties;
         data << pProto->RequiredDisenchantSkill;
         data << pProto->ArmorDamageModifier;
-        data << abs(pProto->Duration);                      // added in 2.4.2.8209, duration (seconds)
+        data << uint32(abs(pProto->Duration));              // added in 2.4.2.8209, duration (seconds)
         data << pProto->ItemLimitCategory;                  // WotLK, ItemLimitCategory
         data << pProto->HolidayId;                          // Holiday.dbc?
         SendPacket(&data);
