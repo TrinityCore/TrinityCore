4132b9351184340a8688af7785db279ec0c6ab1b
Shauren <none@none>
2010-09-14 16:14:43 +0200

    Core/Vehicles: Removed hacky setting entry for multi-passenger mounts Core/Pools: Fixed compile
    --HG--
    branch : trunk

--- src/server/scripts/Northrend/Ulduar/ulduar/boss_flame_leviathan.cpp
+++ src/server/scripts/Northrend/Ulduar/ulduar/boss_flame_leviathan.cpp
@@ -369,7 +369,7 @@ public:
         void SpellHit(Unit* /*caster*/, const SpellEntry* pSpell)
         {
             if (pSpell->Id == SPELL_START_THE_ENGINE)
-                vehicle->InstallAllAccessories();
+                vehicle->InstallAllAccessories(me->GetEntry());
 
             if (pSpell->Id == SPELL_ELECTROSHOCK)
                 me->InterruptSpell(CURRENT_CHANNELED_SPELL);
