df7e63118361f82e52273c1ef29eb0bbc1ed9423
Spp <none@none>
2010-09-15 13:23:36 +0200

    Core: Fix warnings added in recent commits
    --HG--
    branch : trunk

--- src/server/game/Entities/Creature/Creature.h
+++ src/server/game/Entities/Creature/Creature.h
@@ -684,7 +684,7 @@ class Creature : public Unit, public GridObject<Creature>
         uint64 m_lootRecipient;
 
         /// Timers
-        uint32 m_corpseRemoveTime;                          // (msecs)timer for death or corpse disappearance
+        time_t m_corpseRemoveTime;                          // (msecs)timer for death or corpse disappearance
         time_t m_respawnTime;                               // (secs) time of next respawn
         uint32 m_respawnDelay;                              // (secs) delay between corpse disappearance and respawning
         uint32 m_corpseDelay;                               // (secs) delay between death and corpse disappearance
