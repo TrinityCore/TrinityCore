05ccd476be9bf9047aff2d4ff8f5fc5d02dc81f1
Shauren <none@none>
2010-09-16 19:58:06 +0200

    Core/Quests: Added index column for quest poi points forcing them to load in certain order
    --HG--
    branch : trunk

--- sql/base/world_database.sql
+++ sql/base/world_database.sql
@@ -16255,6 +16255,7 @@ DROP TABLE IF EXISTS `quest_poi_points`;
 CREATE TABLE `quest_poi_points` (
   `questId` int(10) UNSIGNED NOT NULL DEFAULT '0',
   `id` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `idx` int(10) unsigned NOT NULL DEFAULT '0',
   `x` int(10) NOT NULL DEFAULT '0',
   `y` int(10) NOT NULL DEFAULT '0',
   KEY `questId_id` (`questId`,`id`)
