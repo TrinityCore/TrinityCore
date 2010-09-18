ef9cb8000b7b200688cf1ed25b77980f6a8ff838
azazel <none@none>
2010-09-18 22:50:43 +0600

    Scripts/Spelsl: improve 24751 Trick or Treat spell and implement 24750 Trick spell.
    --HG--
    branch : trunk

---
+++ sql/updates/9977_world_spell_script_names.sql
@@ -0,0 +1,2 @@
+DELETE FROM `spell_script_names` WHERE `spell_id`=24750 AND `ScriptName`='spell_gen_trick';
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES (24750,'spell_gen_trick');
