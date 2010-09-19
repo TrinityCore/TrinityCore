19fa83c0ebf9359988ce030718403cb4ee8dd997
silinoron <none@none>
2010-09-18 19:49:17 -0700

    Core/DBLayer: Some more prepared statements in ObjectMgr. Core/ObjectMgr: Some additional cleanup.
    --HG--
    branch : trunk

--- src/server/shared/Database/Implementation/WorldDatabase.cpp
+++ src/server/shared/Database/Implementation/WorldDatabase.cpp
@@ -35,6 +35,7 @@ bool WorldDatabaseConnection::Open(const std::string& infoString)
     PrepareStatement(WORLD_LOAD_QUEST_POOLS, "SELECT entry, pool_entry FROM pool_quest");
     PrepareStatement(WORLD_DEL_CRELINKED_RESPAWN, "DELETE FROM creature_linked_respawn WHERE guid = ?");
     PrepareStatement(WORLD_REP_CRELINKED_RESPAWN, "REPLACE INTO creature_linked_respawn (guid,linkedGuid) VALUES (?, ?)");
+    PrepareStatement(WORLD_DEL_GAMEOBJECT_RESPAWN_TIMES, "DELETE FROM gameobject_respawn WHERE respawntime <= UNIX_TIMESTAMP(NOW())");
 
     return true;
 }
