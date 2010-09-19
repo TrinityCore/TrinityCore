6c0f3cff4f78f30685472f3e8d340a64671fbeaf
Machiavelli <none@none>
2010-09-19 12:36:48 +0200

    Core/DBLayer: Move QueryHolder and BasicStatement operations to their own file and remove SQLOperation.cpp from revision control
    --HG--
    branch : trunk

---
+++ src/server/shared/Database/AdhocStatement.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2008-2010 Trinity <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef _ADHOCSTATEMENT_H
+#define _ADHOCSTATEMENT_H
+
+#include <ace/Future.h>
+#include "SQLOperation.h"
+
+typedef ACE_Future<QueryResult> QueryResultFuture;
+/*! Raw, ad-hoc query. */
+class BasicStatementTask : public SQLOperation
+{
+    public:
+        BasicStatementTask(const char* sql);
+        BasicStatementTask(const char* sql, QueryResultFuture result);
+        ~BasicStatementTask();
+
+        bool Execute();
+
+    private:
+        const char* m_sql;      //- Raw query to be executed
+        bool m_has_result;
+        QueryResultFuture m_result;
+};
+
+#endif
