e6ddcbaa1b3da395e0e76034f3fafad033a34c90
Machiavelli <none@none>
2010-09-25 17:08:55 +0200

    Core/Player: Correctly send social list to player on login. Fixes display of number of online friends in the icon on the chat pane. Fixes issue #3807.
    --HG--
    branch : trunk

--- src/server/game/Entities/Player/SocialMgr.h
+++ src/server/game/Entities/Player/SocialMgr.h
@@ -126,7 +126,7 @@ class PlayerSocial
         void RemoveFromSocialList(uint32 friend_guid, bool ignore);
         void SetFriendNote(uint32 friend_guid, std::string note);
         // Packet send's
-        void SendSocialList();
+        void SendSocialList(Player* plr);
         // Misc
         bool HasFriend(uint32 friend_guid);
         bool HasIgnore(uint32 ignore_guid);
