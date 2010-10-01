f3a3408b86863ca7b21887770971e60a395c0a4e
runningnak3d <none@none>
2010-09-30 21:06:53 -0600

    Core/Misc: Set the hard level of the waypoint wisp to 255. If you for *SOME* reason had more than 255 waypoints, bad things (such as a client crash) would happen.
    --HG--
    branch : trunk

--- src/server/game/Chat/Commands/Level2.cpp
+++ src/server/game/Chat/Commands/Level2.cpp
@@ -3045,7 +3045,7 @@ bool ChatHandler::HandleWpShowCommand(const char* args)
             {
                 wpCreature->SetDisplayId(target->GetDisplayId());
                 wpCreature->SetFloatValue(OBJECT_FIELD_SCALE_X, 0.5);
-                wpCreature->SetLevel(point);
+                wpCreature->SetLevel(point > STRONG_MAX_LEVEL ? STRONG_MAX_LEVEL : point);
             }
         }
         while (result->NextRow());
