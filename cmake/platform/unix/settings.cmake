fdcdcdb3daf62c1f952e167a3ca582eb860edbd6
click <none@none>
2010-10-07 21:34:13 +0200

    Buildsystem: Clean up uninstall-target files a bit
    --HG--
    branch : trunk

--- cmake/platform/unix/settings.cmake
+++ cmake/platform/unix/settings.cmake
@@ -20,7 +20,7 @@ message(STATUS "UNIX: Configuring uninstall target")
 
 # create uninstaller target (allows for using "make uninstall")
 add_custom_target(uninstall
-  "${CMAKE_COMMAND}" -P "${CMAKE_CURRENT_BINARY_DIR}/cmake_uninstall.cmake"
+  "${CMAKE_COMMAND}" -P "${CMAKE_BINARY_DIR}/cmake_uninstall.cmake"
 )
 message(STATUS "UNIX: Created uninstall target")
 
