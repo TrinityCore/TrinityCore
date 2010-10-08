a288fb32af1e98144d98ca7aa1b8c865a5ca1d56
Supabad <none@none>
2010-10-08 18:26:37 +0200

    fix sql from previous commit
    --HG--
    branch : trunk
    rename : sql/updates/10181_world_script_texts.sql => sql/updates/10182_world_script_texts.sql
    rename : sql/updates/10181_world_spell_script_names.sql => sql/updates/10182_world_spell_script_names.sql

--- sql/updates/10182_world_spell_script_names.sql
+++ sql/updates/10182_world_spell_script_names.sql
@@ -1,4 +1,5 @@
 DELETE FROM `spell_script_names` WHERE `spell_id`=62717 AND `ScriptName`='spell_ignis_slag_pot';
+DELETE FROM `spell_script_names` WHERE `spell_id`=63477 AND `ScriptName`='spell_ignis_slag_pot';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (62717, 'spell_ignis_slag_pot'); -- boss Ignis 10man
-(62717, 'spell_ignis_slag_pot'); -- boss Ignis 25man
+(63477, 'spell_ignis_slag_pot'); -- boss Ignis 25man
