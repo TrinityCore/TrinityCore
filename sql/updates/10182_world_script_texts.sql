31f07f2660071bd3a240d7eec27af33dda98ed9c
Supabad <none@none>
2010-10-08 18:26:37 +0200

    fix sql from previous commit
    --HG--
    branch : trunk
    rename : sql/updates/10181_world_script_texts.sql => sql/updates/10182_world_script_texts.sql
    rename : sql/updates/10181_world_spell_script_names.sql => sql/updates/10182_world_spell_script_names.sql

--- sql/updates/10182_world_script_texts.sql
+++ sql/updates/10182_world_script_texts.sql
@@ -1,3 +1,3 @@
-DELETE FROM `script_texts` WHERE `entry` IN (-1571043,-1571042);
+DELETE FROM `script_texts` WHERE `entry` IN (-1603229);
 INSERT INTO `script_texts` (`npc_entry`,`entry`,`content_default`,`content_loc1`,`content_loc2`,`content_loc3`,`content_loc4`,`content_loc5`,`content_loc6`,`content_loc7`,`content_loc8`,`sound`,`type`,`language`,`emote`,`comment`) VALUES
 (33118,-1603229,'Ignis the Furnace Master begins to cast Flame Jets!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,3,0,0,'IGNIS EMOTE_JETS');
