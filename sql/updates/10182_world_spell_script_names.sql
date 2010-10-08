53de3eba56052f6908ba5af638b49b62b3052ccf
Supabad <none@none>
2010-10-08 18:37:59 +0200

    really fix sql this time... ty  svetilo12 for the notice
    --HG--
    branch : trunk
    rename : sql/updates/10182_world_spell_script_names.sql => sql/updates/10183_world_spell_script_names.sql

--- sql/updates/10182_world_spell_script_names.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM `spell_script_names` WHERE `spell_id`=62717 AND `ScriptName`='spell_ignis_slag_pot';
-DELETE FROM `spell_script_names` WHERE `spell_id`=63477 AND `ScriptName`='spell_ignis_slag_pot';
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
-(62717, 'spell_ignis_slag_pot'); -- boss Ignis 10man
-(63477, 'spell_ignis_slag_pot'); -- boss Ignis 25man
