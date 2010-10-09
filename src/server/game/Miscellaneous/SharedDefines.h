c82cb8efa2b7608e0dcf3e0b2347373406a77eb2
runningnak3d <none@none>
2010-10-08 19:05:52 -0600

    Core/Misc: Fix typo Thanks: jamieplucinski
    --HG--
    branch : trunk

--- src/server/game/Miscellaneous/SharedDefines.h
+++ src/server/game/Miscellaneous/SharedDefines.h
@@ -1318,7 +1318,7 @@ enum GameObjectDynamicLowFlags
     GO_DYNFLAG_LO_ANIMATE           = 0x02,                 // possibly more distinct animation of GO
     GO_DYNFLAG_LO_NO_INTERACT       = 0x04,                 // appears to disable interaction (not fully verified)
     GO_DYNFLAG_LO_SPARKLE           = 0x08,                 // makes GO sparkle
-}
+};
 
 enum TextEmotes
 {
