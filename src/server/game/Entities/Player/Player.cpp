66dec6d86fd235830a509f84e658d80d4c159dab
Shocker <none@none>
2010-10-12 21:05:37 +0300

    Core/Misc: Implement config option for energy regeneration rate, patch by Ramus, closes issue 4394
    --HG--
    branch : trunk

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -2236,7 +2236,7 @@ void Player::Regenerate(Powers power)
             }
         }   break;
         case POWER_ENERGY:                                  // Regenerate energy (rogue)
-            addvalue += 0.01f * m_regenTimer;
+            addvalue += 0.01f * m_regenTimer * sWorld.getRate(RATE_POWER_ENERGY);
             break;
         case POWER_RUNIC_POWER:
         {
