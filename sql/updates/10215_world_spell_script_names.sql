0f2ef38a789a80f14a175835ffc9fa92f315ccf9
Shocker <none@none>
2010-10-12 20:40:32 +0300

    Core/Spells: Attach the spell_creature_permanent_feign_death script to one more spell, 31261
    --HG--
    branch : trunk

---
+++ sql/updates/10215_world_spell_script_names.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` = 31261 AND `ScriptName`='spell_creature_permanent_feign_death';
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES 
+(31261, 'spell_creature_permanent_feign_death');
