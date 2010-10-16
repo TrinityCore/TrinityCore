28e9b376c890f066b95cfb03de44b146bb6ea460
Shocker <none@none>
2010-10-16 17:53:33 +0300

    Core/Spells: Really move spec changing InBattleground check to its correct location
    --HG--
    branch : trunk

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -5392,6 +5392,12 @@ SpellCastResult Spell::CheckCast(bool strict)
                 }
                 break;
             }
+            case SPELL_EFFECT_TALENT_SPEC_SELECT:
+                // can't change during already started arena/battleground
+                if (Battleground const* bg = m_caster->ToPlayer()->GetBattleground())
+                    if (bg->GetStatus() == STATUS_IN_PROGRESS)
+                        return SPELL_FAILED_NOT_IN_BATTLEGROUND;
+                break;
             default:
                 break;
         }
