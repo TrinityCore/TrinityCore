edf2ffe146ba07317aeadfa1b87fa7f80839d55e
Shocker <none@none>
2010-10-16 17:34:21 +0300

    Core/Misc: Also remove buffs from pet when joining arena to make sure a player won't enter with a prebuffed pet, fixes issue 4287
    --HG--
    branch : trunk

--- src/server/game/Battlegrounds/Battleground.cpp
+++ src/server/game/Battlegrounds/Battleground.cpp
@@ -1104,7 +1104,6 @@ void Battleground::AddPlayer(Player *plr)
     // add arena specific auras
     if (isArena())
     {
-        plr->RemoveArenaAuras();
         plr->RemoveArenaEnchantments(TEMP_ENCHANTMENT_SLOT);
         if (team == ALLIANCE)                                // gold
         {
