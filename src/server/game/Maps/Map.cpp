dc4a056a05eca223ede8aa33078ca3afd44c9af1
click <none@none>
2010-10-22 03:39:27 +0200

    Core/Grid: Fix infinity-loop in grid loading (patch by SilverIce)
    --HG--
    branch : trunk

--- src/server/game/Maps/Map.cpp
+++ src/server/game/Maps/Map.cpp
@@ -370,13 +370,13 @@ bool Map::EnsureGridLoaded(const Cell &cell)
     {
         sLog.outDebug("Loading grid[%u,%u] for map %u instance %u", cell.GridX(), cell.GridY(), GetId(), i_InstanceId);
 
+        setGridObjectDataLoaded(true,cell.GridX(), cell.GridY());
+
         ObjectGridLoader loader(*grid, this, cell);
         loader.LoadN();
 
         // Add resurrectable corpses to world object list in grid
         sObjectAccessor.AddCorpsesToGrid(GridPair(cell.GridX(),cell.GridY()),(*grid)(cell.CellX(), cell.CellY()), this);
-
-        setGridObjectDataLoaded(true,cell.GridX(), cell.GridY());
         return true;
     }
 
