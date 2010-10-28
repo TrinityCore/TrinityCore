d941933fdaffe5385c9e2e71fc2ac137b5b68228
Shocker <none@none>
2010-10-29 00:21:39 +0300

    Core/Spells: Deathfrost weapon enchantment shouldn't scale with spellpower
    --HG--
    branch : trunk

---
+++ sql/updates/10327_world_spell_bonus_data.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_bonus_data` WHERE `entry` = 46579;
+INSERT INTO `spell_bonus_data` (`entry`,`direct_bonus`,`dot_bonus`,`ap_bonus`,`ap_dot_bonus`,`comments`) VALUES
+(46579, 0, 0, 0, 0, 'Deathfrost - Enchantment');
