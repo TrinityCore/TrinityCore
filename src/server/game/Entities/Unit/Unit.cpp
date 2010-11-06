26d8e04351145e5ef02451cbb5f4ac2a3ee3c632
Machiavelli <none@none>
2010-11-06 10:35:46 +0100

    Core/Unit: Clear threat list on units when they die (fix players having aggro on corpses) Original suggestion by glorandwarf@yahoo.com
    Fixes issue #4667
    
    --HG--
    branch : trunk

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -12518,7 +12518,7 @@ bool Unit::CanHaveThreatList() const
         return false;
 
     // only alive units can have threat list
-    if (!isAlive())
+    if (!isAlive() || isDying())
         return false;
 
     // totems can not have threat list
