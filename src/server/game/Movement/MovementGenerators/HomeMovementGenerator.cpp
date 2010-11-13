e1f97886981726337095deecdb5791770587b31f
linencloth <none@none>
2010-11-13 23:51:23 +0100

    Core/MovementGenerators: Don't remove evade state when clearing unit states. Core/Spells: Don't start combat when the target is evading. Closes issue 1861
    --HG--
    branch : trunk

--- src/server/game/Movement/MovementGenerators/HomeMovementGenerator.cpp
+++ src/server/game/Movement/MovementGenerators/HomeMovementGenerator.cpp
@@ -54,7 +54,7 @@ HomeMovementGenerator<Creature>::_setTargetLocation(Creature & owner)
 
     uint32 travel_time = i_destinationHolder.SetDestination(traveller, x, y, z);
     modifyTravelTime(travel_time);
-    owner.clearUnitState(UNIT_STAT_ALL_STATE);
+    owner.clearUnitState(UNIT_STAT_ALL_STATE & ~UNIT_STAT_EVADE);
 }
 
 bool
