7811c89a93ea1608043c404b2173bf80155050a0
linencloth <none@none>
2010-11-13 23:51:23 +0100

    Core/MovementGenerators: Don't remove evade state when clearing unit states. Core/Spells: Don't start combat when the target is evading. Closes issue 1861
    --HG--
    branch : trunk

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -1321,7 +1321,7 @@ void Spell::DoAllEffectOnTarget(TargetInfo *target)
         }
     }
 
-    if (m_caster && !m_caster->IsFriendlyTo(unit) && !IsPositiveSpell(m_spellInfo->Id))
+    if (missInfo != SPELL_MISS_EVADE && m_caster && !m_caster->IsFriendlyTo(unit) && !IsPositiveSpell(m_spellInfo->Id))
     {
         m_caster->CombatStart(unit, !(m_spellInfo->AttributesEx3 & SPELL_ATTR_EX3_NO_INITIAL_AGGRO));
 
