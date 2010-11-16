95e956a0b5145ad0a260557ce9ae1290cdacad2d
click <none@none>
2010-11-16 21:27:25 +0100

    Core/Scripts: Convert more commands over to CommandScripts (moves gobject, honor, quest, reload, titles and wp commands to scripts) - Patch by Paradox (Also fixes a minor visual error on GM-listings (gm ingame/gm list) when the respective user had an R in his/her name). Closes issue 4792.
    --HG--
    branch : trunk

--- src/server/game/Chat/Commands/Debugcmds.cpp
+++ src/server/game/Chat/Commands/Debugcmds.cpp
@@ -16,25 +16,4 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "Common.h"
-#include "DatabaseEnv.h"
-#include "WorldPacket.h"
-#include "Vehicle.h"
-#include "Player.h"
-#include "Opcodes.h"
-#include "Chat.h"
-#include "Log.h"
-#include "Unit.h"
-#include "GossipDef.h"
-#include "Language.h"
-#include "BattlegroundMgr.h"
-#include <fstream>
-#include "ObjectMgr.h"
-#include "Cell.h"
-#include "CellImpl.h"
-#include "GridNotifiers.h"
-#include "GridNotifiersImpl.h"
-#include "SpellMgr.h"
-#include "ScriptMgr.h"
-
 // This file is deprecated
