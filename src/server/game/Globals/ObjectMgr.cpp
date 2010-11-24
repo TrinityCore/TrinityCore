98f94dd120e449dc8d99f13ac2da62161ef03fec
click <none@none>
2010-11-24 13:41:20 +0100

    Core/Reputation: Implement a more generic solution for reputation spillover (fix by Lynx3d, port by Kaelima) * Sorts out issues with factions 1037 and 1052 * Give a more correct client output when reputation is assigned to parent faction * reputation_spillover_template can still be used to override spillover from dbc Closes issue 4915.
    --HG--
    branch : trunk

--- src/server/game/Globals/ObjectMgr.cpp
+++ src/server/game/Globals/ObjectMgr.cpp
@@ -7156,7 +7156,7 @@ void ObjectMgr::LoadReputationSpilloverTemplate()
         bar.step();
 
         sLog.outString();
-        sLog.outErrorDb(">> Loaded `reputation_spillover_template`, table is empty!");
+        sLog.outString(">> Loaded `reputation_spillover_template`, table is empty.");
         return;
     }
 
