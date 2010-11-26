8f573393017343051d7b54f9d04e7db2d90772ce
click <none@none>
2010-11-26 01:35:32 +0100

    Core/PetHandler: Correction of SMSG_PET_NAME_QUERY response packet sizes and structure (fix by Ramus) (Client automatically handles re-queries if name is not supplied) Closes issue 4924.
    --HG--
    branch : trunk

--- src/server/game/Server/Protocol/Handlers/PetHandler.cpp
+++ src/server/game/Server/Protocol/Handlers/PetHandler.cpp
@@ -412,9 +412,9 @@ void WorldSession::SendPetNameQuery(uint64 petguid, uint32 petnumber)
     Creature* pet = ObjectAccessor::GetCreatureOrPetOrVehicle(*_player, petguid);
     if (!pet)
     {
-        WorldPacket data(SMSG_PET_NAME_QUERY_RESPONSE, (4+4+7+1));
+        WorldPacket data(SMSG_PET_NAME_QUERY_RESPONSE, (4+1+4+1));
         data << uint32(petnumber);
-        data << "Unknown";
+        data << uint8(0);
         data << uint32(0);
         data << uint8(0);
         _player->GetSession()->SendPacket(&data);
