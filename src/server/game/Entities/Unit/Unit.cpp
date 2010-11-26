ab719684d7798f34e85b072807b43f3f69513b92
click <none@none>
2010-11-26 02:02:05 +0100

    Core/Unit: Fix pets aggromode - do not set player in combat untill pet has actually done damage to target (fix by Kaelima) Closes issue 4894.
    --HG--
    branch : trunk

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -9389,7 +9389,7 @@ bool Unit::Attack(Unit *victim, bool meleeAttack)
     //if (GetTypeId() == TYPEID_UNIT)
     //    this->ToCreature()->SetCombatStartPosition(GetPositionX(), GetPositionY(), GetPositionZ());
 
-    if (GetTypeId() == TYPEID_UNIT)
+    if (GetTypeId() == TYPEID_UNIT && !this->ToCreature()->IsPet())
     {
         // should not let player enter combat by right clicking target
         SetInCombatWith(victim);
