96ab1a102f602050099d7b2487cca64e4eaeba22
Shauren <none@none>
2010-12-03 14:02:41 +0100

    Scripts/Trial of the Crusader: Fixed crashes in Gormok the Impaler
    Closes issue #4515.
    
    --HG--
    branch : trunk

--- src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/boss_lord_jaraxxus.cpp
+++ src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/boss_lord_jaraxxus.cpp
@@ -86,7 +86,8 @@ enum BossSpells
 /*######
 ## boss_jaraxxus
 ######*/
-class boss_jaraxxus : public CreatureScript
+
+class boss_jaraxxus : public CreatureScript
 {
 public:
     boss_jaraxxus() : CreatureScript("boss_jaraxxus") { }
@@ -248,7 +249,8 @@ public:
 
 };
 
-class mob_legion_flame : public CreatureScript
+
+class mob_legion_flame : public CreatureScript
 {
 public:
     mob_legion_flame() : CreatureScript("mob_legion_flame") { }
@@ -281,7 +283,8 @@ public:
 
 };
 
-class mob_infernal_volcano : public CreatureScript
+
+class mob_infernal_volcano : public CreatureScript
 {
 public:
     mob_infernal_volcano() : CreatureScript("mob_infernal_volcano") { }
@@ -358,7 +361,8 @@ public:
 
 };
 
-class mob_fel_infernal : public CreatureScript
+
+class mob_fel_infernal : public CreatureScript
 {
 public:
     mob_fel_infernal() : CreatureScript("mob_fel_infernal") { }
@@ -412,7 +416,8 @@ public:
 
 };
 
-class mob_nether_portal : public CreatureScript
+
+class mob_nether_portal : public CreatureScript
 {
 public:
     mob_nether_portal() : CreatureScript("mob_nether_portal") { }
@@ -483,7 +488,8 @@ public:
 
 };
 
-class mob_mistress_of_pain : public CreatureScript
+
+class mob_mistress_of_pain : public CreatureScript
 {
 public:
     mob_mistress_of_pain() : CreatureScript("mob_mistress_of_pain") { }
