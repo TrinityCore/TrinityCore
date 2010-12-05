6f128051b5e57a3c54bc8136e828ea0d3b8e21bf
QAston <none@none>
2010-12-05 20:43:09 +0100

    Core/Auras: backout r2bf34fd7ac and add alternative - not spell specific sollution for the problem.
    --HG--
    branch : trunk

--- src/server/game/Spells/SpellScript.cpp
+++ src/server/game/Spells/SpellScript.cpp
@@ -567,6 +567,7 @@ bool AuraScript::_IsDefaultActionPrevented()
         case AURA_SCRIPT_HOOK_EFFECT_PERIODIC:
             return m_defaultActionPrevented;
         default:
+            ASSERT(false && "m_defaultActionPrevented has incorrect value, or AuraScript::_IsDefaultActionPrevented is called in a wrong place");
             return false;
     }
 }
