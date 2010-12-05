e2c9453442d0bf5497d048305e0ff9c993c74ab5
Shauren <none@none>
2010-12-05 21:20:45 +0100

    Core/Auras: Small correction on PLAYER_FIELD_BYTES2 usage in SPELL_AURA_OVERRIDE_SPELLS (293)
    --HG--
    branch : trunk

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -6493,7 +6493,7 @@ void AuraEffect::HandleAuraOverrideSpells(AuraApplication const * aurApp, uint8 
 
     if (apply)
     {
-        target->SetUInt32Value(PLAYER_FIELD_BYTES2, overrideId);
+        target->SetUInt16Value(PLAYER_FIELD_BYTES2, 0, overrideId);
         if (OverrideSpellDataEntry const* overrideSpells = sOverrideSpellDataStore.LookupEntry(overrideId))
             for (uint8 i = 0; i < MAX_OVERRIDE_SPELL; ++i)
                 if (uint32 spellId = overrideSpells->spellId[i])
@@ -6501,7 +6501,7 @@ void AuraEffect::HandleAuraOverrideSpells(AuraApplication const * aurApp, uint8 
     }
     else
     {
-        target->SetUInt32Value(PLAYER_FIELD_BYTES2, 0);
+        target->SetUInt16Value(PLAYER_FIELD_BYTES2, 0, 0);
         if (OverrideSpellDataEntry const* overrideSpells = sOverrideSpellDataStore.LookupEntry(overrideId))
             for (uint8 i = 0; i < MAX_OVERRIDE_SPELL; ++i)
                 if (uint32 spellId = overrideSpells->spellId[i])
