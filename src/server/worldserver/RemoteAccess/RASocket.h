90328b42a4881ea140f4fe2929f20cb34db855db
Derex <derex101@gmail.com>
2010-12-10 01:01:44 +0200

    Rewrite remote access console using ACE
    --HG--
    branch : trunk

--- src/server/worldserver/RemoteAccess/RASocket.h
+++ src/server/worldserver/RemoteAccess/RASocket.h
@@ -23,48 +23,39 @@
 #ifndef _RASOCKET_H
 #define _RASOCKET_H
 
-#include "TcpSocket.h"
-
 #include "Common.h"
+
 #include <ace/Synch_Traits.h>
-
-#define RA_BUFF_SIZE 1024
-
-class ISocketHandler;
-
-typedef ACE_Atomic_Op<ACE_SYNCH_MUTEX, uint64> AtomicInt;
+#include <ace/Svc_Handler.h>
+#include <ace/SOCK_Stream.h>
+#include <ace/SOCK_Acceptor.h>
 
 /// Remote Administration socket
-class RASocket: public TcpSocket
+class RASocket: public ACE_Svc_Handler<ACE_SOCK_STREAM, ACE_MT_SYNCH>
 {
     public:
+        RASocket();
+        virtual ~RASocket();
 
-        RASocket(ISocketHandler& h);
-        ~RASocket();
-
-        void OnAccept();
-        void OnRead();
-
-    AtomicInt pendingCommands;
+        virtual int svc(void);
+        virtual int open(void * = 0);
+        virtual int handle_close(ACE_HANDLE = ACE_INVALID_HANDLE, ACE_Reactor_Mask = ACE_Event_Handler::ALL_EVENTS_MASK);
 
     private:
-        char buff[RA_BUFF_SIZE];
-        std::string szLogin;
-
-        unsigned int iInputLength;
-        bool bSecure;
-        //will protect from DOS, bruteforce attacks
-        //some 'smart' protection must be added for more security
-        uint8 iMinLevel;
-        enum
-        {
-            NONE,                                           //initial value
-            LG,                                             //only login was entered
-            OK,                                             //both login and pass were given, and they are correct and user have enough priv.
-        }stage;
+        int recv_line(std::string& out_line);
+        int recv_line(ACE_Message_Block& buffer);
+        int process_command(const std::string& command);
+        int authenticate();
+        int check_access_level(const std::string& user);
+        int check_password(const std::string& user, const std::string& pass);
+        int send(const std::string& line);
 
         static void zprint(void* callbackArg, const char * szText );
         static void commandFinished(void* callbackArg, bool success);
+
+    private:
+        /// Minimum security level required to connect
+        uint8 iMinLevel;
 };
 #endif
 /// @}
