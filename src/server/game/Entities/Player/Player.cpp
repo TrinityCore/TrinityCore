795053cc09784cc9ff5c1e2b4c1633efc83deeae
Liberate <none@none>
2010-12-14 03:58:59 +0100

    Core/Gameobjects: Make a chest not lootable when it still has a respawn time, to avoid cheating using third-party software.
    --HG--
    branch : trunk

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -8329,7 +8329,7 @@ void Player::SendLoot(uint64 guid, LootType loot_type)
 
         // not check distance for GO in case owned GO (fishing bobber case, for example)
         // And permit out of range GO with no owner in case fishing hole
-        if (!go || (loot_type != LOOT_FISHINGHOLE && (loot_type != LOOT_FISHING || go->GetOwnerGUID() != GetGUID()) && !go->IsWithinDistInMap(this,INTERACTION_DISTANCE)))
+        if (!go || (loot_type != LOOT_FISHINGHOLE && (loot_type != LOOT_FISHING || go->GetOwnerGUID() != GetGUID()) && !go->IsWithinDistInMap(this,INTERACTION_DISTANCE)) || go->GetRespawnTime())
         {
             SendLootRelease(guid);
             return;
