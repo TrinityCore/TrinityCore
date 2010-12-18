ed9708c1d8c81d6c9dee8a4e3bc255d962495bf0
silinoron <none@none>
2010-12-18 13:06:38 -0800

    Core/Auras: Fix Cold Blood handling on misses and parries. Patch by dr.tenma. Fixes issue #2291
    --HG--
    branch : trunk

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -1198,7 +1198,16 @@ void Spell::DoAllEffectOnTarget(TargetInfo *target)
     // Do not take combo points on dodge and miss
     if (m_needComboPoints && m_targets.getUnitTargetGUID() == target->targetGUID)
         if (missInfo != SPELL_MISS_NONE)
+        {
             m_needComboPoints = false;
+            // Restore spell mods for a miss/dodge/parry Cold Blood
+            // TODO: check how broad this rule should be
+            if (m_caster->GetTypeId() == TYPEID_PLAYER)
+                if ((missInfo == SPELL_MISS_MISS) ||
+                    (missInfo == SPELL_MISS_DODGE) ||
+                    (missInfo == SPELL_MISS_PARRY))
+                    m_caster->ToPlayer()->RestoreSpellMods(this, 14177);
+        }
 
     // Trigger info was not filled in spell::preparedatafortriggersystem - we do it now
     if (canEffectTrigger && !procAttacker && !procVictim)
