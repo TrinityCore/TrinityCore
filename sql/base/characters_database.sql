89f2870b97248558148667d7efdc86b76487407b
Shauren <none@none>
2010-12-20 14:55:36 +0100

    Core/Auras: Save cast item guids for auras to db
    --HG--
    branch : trunk

--- sql/base/characters_database.sql
+++ sql/base/characters_database.sql
@@ -461,6 +461,7 @@ DROP TABLE IF EXISTS `character_aura`;
 CREATE TABLE `character_aura` (
   `guid` int(11) unsigned NOT NULL default '0' COMMENT 'Global Unique Identifier',
   `caster_guid` bigint(20) unsigned NOT NULL default '0' COMMENT 'Full Global Unique Identifier',
+  `item_guid`  bigint(20) UNSIGNED NOT NULL DEFAULT '0',
   `spell` int(11) unsigned NOT NULL default '0',
   `effect_mask` tinyint(3) unsigned NOT NULL default '0',
   `recalculate_mask` tinyint(3) unsigned NOT NULL default '0',
@@ -474,7 +475,7 @@ CREATE TABLE `character_aura` (
   `maxduration` int(11) NOT NULL default '0',
   `remaintime` int(11) NOT NULL default '0',
   `remaincharges` tinyint(3) unsigned NOT NULL default '0',
-  PRIMARY KEY  (`guid`,`caster_guid`,`spell`,`effect_mask`)
+  PRIMARY KEY  (`guid`,`caster_guid`,`item_guid`,`spell`,`effect_mask`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Player System';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
