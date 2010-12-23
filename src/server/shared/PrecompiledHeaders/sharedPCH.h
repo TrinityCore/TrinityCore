3a8e833922040d0018080fb2e1ee754e7bf565d8
click <none@none>
2010-12-23 17:20:56 +0100

    Core: Adjust precompiled headers: Remove a few already defined/included headers in the shared PCH-file (already included from Common.h)
    --HG--
    branch : trunk

--- src/server/shared/PrecompiledHeaders/sharedPCH.h
+++ src/server/shared/PrecompiledHeaders/sharedPCH.h
@@ -1,11 +1,8 @@
 //add here most rarely modified headers to speed up debug build compilation
 
 #include "Common.h"
-#include "Define.h"
 #include "Log.h"
-#include "Threading.h"
 #include "DatabaseWorker.h"
 #include "SQLOperation.h"
 #include "Errors.h"
 #include "TypeList.h"
-#include "UnorderedMap.h"
