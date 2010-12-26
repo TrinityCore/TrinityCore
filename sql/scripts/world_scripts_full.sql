9de8466d84ce63e930657ef5e07bac8502ff8fd8
Shauren <none@none>
2010-12-26 17:00:31 +0100

    SQL: Added missing update for previous revision
    --HG--
    branch : trunk

--- sql/scripts/world_scripts_full.sql
+++ sql/scripts/world_scripts_full.sql
@@ -551,6 +551,7 @@ UPDATE `creature_template` SET `ScriptName`='mob_water_globule' WHERE `entry`=21
 UPDATE `creature_template` SET `ScriptName`='boss_the_lurker_below' WHERE `entry`=21217;
 UPDATE `creature_template` SET `ScriptName`='mob_coilfang_ambusher' WHERE `entry`=21865;
 UPDATE `creature_template` SET `ScriptName`='mob_coilfang_guardian' WHERE `entry`=21873;
+UPDATE `gameobject_template` SET `ScriptName`='go_strange_pool' WHERE `entry`=184956;
 /* Lady Vashj event */
 UPDATE `creature_template` SET `ScriptName`='boss_lady_vashj' WHERE `entry`=21212;
 UPDATE `creature_template` SET `ScriptName`='mob_enchanted_elemental' WHERE `entry`=21958;
