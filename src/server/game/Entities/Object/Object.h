6b12ec1f3e6fd6b2216051fca678d4b88a3aa60b
Machiavelli <machiavelli.trinity@gmail.com>
2010-12-29 12:09:57 +0100

    Fix build from 68c6189201b4aa111664

--- src/server/game/Entities/Object/Object.h
+++ src/server/game/Entities/Object/Object.h
@@ -517,6 +517,11 @@ struct MovementInfo
     uint32 GetMovementFlags() { return flags; }
     void AddMovementFlag(uint32 flag) { flags |= flag; }
     bool HasMovementFlag(uint32 flag) const { return flags & flag; }
+
+    uint16 GetExtraMovementFlags() { return flags2; }
+    void AddExtraMovementFlag(uint16 flag) { flags2 |= flags2; }
+    bool HasExtraMovementFlag(uint16 flag) const { return flags2 & flag; }
+
     void OutDebug();
 };
 
