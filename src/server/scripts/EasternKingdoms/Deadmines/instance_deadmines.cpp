957c69de835cc00e1edee7adb407c1e5a6e608f4
Machiavelli <machiavelli.trinity@gmail.com>
2011-01-01 15:01:13 +0100

    Update copyright note for 2011.
    Happy new year.

--- src/server/scripts/EasternKingdoms/Deadmines/instance_deadmines.cpp
+++ src/server/scripts/EasternKingdoms/Deadmines/instance_deadmines.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2010 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2008-2011 TrinityCore <http://www.trinitycore.org/>
  * Copyright (C) 2006-2008 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
  *
  * This program is free software; you can redistribute it and/or modify it
