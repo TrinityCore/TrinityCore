8bfc49cfa5e2ed331a5714e96db003d7d834ca4b
click <click@gonnamakeyou.com>
2011-01-05 17:55:57 +0100

    Core/Entities: Send SMSG_DISMOUNT notification to client when dismounting (mount/vehicle). Thanks to Vladimir.

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -11657,6 +11657,10 @@ void Unit::Unmount()
     SetUInt32Value(UNIT_FIELD_MOUNTDISPLAYID, 0);
     RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_MOUNT);
 
+    WorldPacket data(SMSG_DISMOUNT, 8);
+    data << GetPackGUID();
+    SendMessageToSet(&data, true);
+
     // only resummon old pet if the player is already added to a map
     // this prevents adding a pet to a not created map which would otherwise cause a crash
     // (it could probably happen when logging in after a previous crash)
