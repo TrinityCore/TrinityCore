8b78770dfb0bdcd310ac0c453c14693f457ac2c8
Machiavelli <machiavelli.trinity@gmail.com>
2011-01-08 19:07:13 +0100

    Core/DBLayer: Define prepared statements in an array per database instead of explicit calls to MySQL::PrepareStatement. Now the core will show the raw query (without bound arguments) in related log entries instead of PreparedStatement id: X on database Y.

--- src/server/shared/Database/Implementation/WorldDatabase.cpp
+++ src/server/shared/Database/Implementation/WorldDatabase.cpp
@@ -30,12 +30,13 @@ bool WorldDatabaseConnection::Open()
         LOAD YOUR PREPARED STATEMENTS HERE
         ##################################
     */
-    PrepareStatement(WORLD_LOAD_QUEST_POOLS, "SELECT entry, pool_entry FROM pool_quest");
-    PrepareStatement(WORLD_DEL_CRELINKED_RESPAWN, "DELETE FROM linked_respawn WHERE guid = ?", true);
-    PrepareStatement(WORLD_REP_CRELINKED_RESPAWN, "REPLACE INTO linked_respawn (guid,linkedGuid) VALUES (?, ?)", true);
-    PrepareStatement(WORLD_LOAD_CRETEXT, "SELECT entry, groupid, id, text, type, language, probability, emote, duration, sound FROM creature_text");
-    PrepareStatement(WORLD_LOAD_SMART_SCRIPTS,  "SELECT entryorguid, source_type, id, link, event_type, event_phase_mask, event_chance, event_flags, event_param1, event_param2, event_param3, event_param4, action_type, action_param1, action_param2, action_param3, action_param4, action_param5, action_param6, target_type, target_param1, target_param2, target_param3, target_x, target_y, target_z, target_o FROM smart_scripts ORDER BY entryorguid, source_type, id, link");
-    PrepareStatement(WORLD_LOAD_SMARTAI_WP,  "SELECT entry, pointid, position_x, position_y, position_z FROM waypoints ORDER BY entry, pointid");
+    for (uint32 index = 0; index < MAX_WORLDDATABASE_STATEMENTS; ++index)
+    {
+        PreparedStatementTable const& pst = WorldDatabasePreparedStatements[index];        
+        PrepareStatement(pst.index, pst.query, pst.type);
+    }
+
+    m_statementTable = WorldDatabasePreparedStatements;
 
     return true;
 }
