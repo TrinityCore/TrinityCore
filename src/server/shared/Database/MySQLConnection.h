b3d32c26c004dbe6979ab3cb8b3aa157b94cb271
Machiavelli <machiavelli.trinity@gmail.com>
2011-01-08 19:07:13 +0100

    Core/DBLayer: Define prepared statements in an array per database instead of explicit calls to MySQL::PrepareStatement. Now the core will show the raw query (without bound arguments) in related log entries instead of PreparedStatement id: X on database Y.

--- src/server/shared/Database/MySQLConnection.h
+++ src/server/shared/Database/MySQLConnection.h
@@ -60,6 +60,13 @@ struct MySQLConnectionInfo
     std::string port_or_socket;
 };
 
+struct PreparedStatementTable
+{
+    uint32 index;
+    const char* query;
+    ConnectionFlags type;
+};
+
 class MySQLConnection
 {
     template <class T> friend class DatabaseWorkerPool;
@@ -104,10 +111,11 @@ class MySQLConnection
 
         MYSQL* GetHandle()  { return m_Mysql; }
         MySQLPreparedStatement* GetPreparedStatement(uint32 index);
-        void PrepareStatement(uint32 index, const char* sql, bool async = false);
+        void PrepareStatement(uint32 index, const char* sql, ConnectionFlags flags);
 
     protected:
         std::vector<MySQLPreparedStatement*> m_stmts;       //! PreparedStatements storage
+        PreparedStatementTable const * m_statementTable;    //! Static index/query pairs
         bool                  m_reconnecting;               //! Are we reconnecting?
         
     private:
