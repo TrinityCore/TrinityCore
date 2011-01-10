529cd0bbdbaa68aace75bcbedd40811c0869704f
click <click@none>
2011-01-10 17:46:28 +0100

    Buildsystem: Add git versiontesting and path-testing to find git - also includes a notice if the version is not found/git is not installed correctly.

--- cmake/genrev.cmake
+++ cmake/genrev.cmake
@@ -8,19 +8,46 @@
 # WITHOUT ANY WARRANTY, to the extent permitted by law; without even the
 # implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
-execute_process(
-  COMMAND git describe --match init --dirty=+ --abbrev=12
-  WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
-  OUTPUT_VARIABLE rev_info
-  OUTPUT_STRIP_TRAILING_WHITESPACE
-  ERROR_QUIET
+set(_REQUIRED_GIT_VERSION "1.7")
+
+find_program(_GIT_EXEC
+  NAMES
+    git git.cmd
+  HINTS
+    ENV PATH
+  DOC "git installation path"
 )
+
+if(_GIT_EXEC)
+  execute_process(
+    COMMAND "${_GIT_EXEC}" --version
+    WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+    OUTPUT_VARIABLE _GIT_VERSION
+    ERROR_QUIET
+  )
+endif()
+
+# make sure we're using minimum the required version of git, so the "dirty-testing" will work properly
+ensure_version( "${_REQUIRED_GIT_VERSION}" "${_GIT_VERSION}" _GIT_VERSION_OK)
+
+if(_GIT_VERSION_OK)
+  execute_process(
+    COMMAND git describe --match init --dirty=+ --abbrev=12
+    WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+    OUTPUT_VARIABLE rev_info
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    ERROR_QUIET
+  )
+else()
+  message("")
+  message(STATUS "OUTDATED GIT-VERSION OR MISSING GIT-COMMAND - DID YOU FORGET TO INSTALL A RECENT VERSION?")
+  message(STATUS "FOR REVISION-IDS AND HASH-TAGGING TO WORK YOU NEED AT LEAST VERSION ${_REQUIRED_GIT_VERSION}.")
+endif()
 
 # Last minute check - ensure that we have a proper revision
 # If everything above fails (means the user has erased the git revision control directory or removed the origin/HEAD tag)
 if(NOT rev_info)
-  message("")
-  message(STATUS "WARNING - No revision-information found")
+  message(STATUS "No revision-information found, setting up for Archived-mode (revision 0, hash 0)")
 
   # Ok, since we have no valid ways of finding/setting the revision, let's force some defaults
   set(rev_hash_str "Archive")
