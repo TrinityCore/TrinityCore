89376afc3857d886df37a1ff7bfe583d8393f1d2
click <per.wilhelmsen@gmail.com>
2011-01-11 23:54:45 +0100

    Buildsystem: Fix a logical error in git revision-detection, and also make the warning text a little bit more "Hello Cpt.Obvious" obvious for users that can't read. (It now includes the word WARNING in the warnings)

--- cmake/showoptions.cmake
+++ cmake/showoptions.cmake
@@ -1,6 +1,5 @@
 # output generic information about the core and buildtype chosen
 
-message("")
 message("* TrinityCore revision   : ${rev_id_str} (${rev_hash_str})")
 if( UNIX )
   message("* Build binaries in      : ${CMAKE_BUILD_TYPE} mode")
