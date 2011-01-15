e505e9f0c0f9bdb566ee92ad72844373f326614f
Machiavelli <machiavelli.trinity@gmail.com>
2011-01-15 19:40:06 +0100

    Core/Spells: Implement SPELL_ATTR6_IGNORE_CROWD_CONTROL_TARGETS (0x00000100). Spells with this attribute flag (Avenger's Shield, Hammer of Righteousness, Shield of Righteousness, Felguard Cleave, Fel Iron Bomb) will ignore secondary targets that are under the effect of crowd control aura's.
    Thanks to Shauren for advices.

--- src/server/game/Miscellaneous/SharedDefines.h
+++ src/server/game/Miscellaneous/SharedDefines.h
@@ -487,7 +487,7 @@ enum SpellAttr6
     SPELL_ATTR6_UNK5                             = 0x00000020, //  5
     SPELL_ATTR6_UNK6                             = 0x00000040, //  6
     SPELL_ATTR6_UNK7                             = 0x00000080, //  7
-    SPELL_ATTR6_UNK8                             = 0x00000100, //  8
+    SPELL_ATTR6_IGNORE_CROWD_CONTROL_TARGETS     = 0x00000100, //  8
     SPELL_ATTR6_UNK9                             = 0x00000200, //  9
     SPELL_ATTR6_UNK10                            = 0x00000400, // 10
     SPELL_ATTR6_NOT_IN_RAID_INSTANCE             = 0x00000800, // 11 not usable in raid instance
