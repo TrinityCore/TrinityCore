134e6e265d6b7fb3a6b0516d8104dad9df6d7f27
SignFinder <dalex@mail.ru>
2011-01-21 21:54:20 +0100

    Fix proc Desolation dk's talent from all melee spells (must be procs only with BS)

---
+++ sql/fc_updates/facecore_updates_world.sql
@@ -0,0 +1,8 @@
+﻿-- Desolation procs only with BS fix.
+DELETE FROM `spell_proc_event` WHERE `entry` IN ('66799','66814','66815','66816','66817');
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
+('66799','0','15','4194304','0','0','0','0','0','0','0'), -- Desolation (Rank 1)
+('66814','0','15','4194304','0','0','0','0','0','0','0'), -- Desolation (Rank 2)
+('66815','0','15','4194304','0','0','0','0','0','0','0'), -- Desolation (Rank 3)
+('66816','0','15','4194304','0','0','0','0','0','0','0'), -- Desolation (Rank 4)
+('66817','0','15','4194304','0','0','0','0','0','0','0'); -- Desolation (Rank 5)
