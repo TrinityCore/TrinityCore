b2736618a62be9dd1ca917dd0698a37a63db6c85
SignFinder <dalex@mail.ru>
2011-01-22 11:18:16 +0100

    typo fix

--- sql/fc_updates/facecore_updates_world.sql
+++ sql/fc_updates/facecore_updates_world.sql
@@ -1,4 +1,4 @@
-﻿-- Desolation procs only with BS fix.
+-- Desolation procs only with BS fix.
 DELETE FROM `spell_proc_event` WHERE `entry` IN ('66799','66814','66815','66816','66817');
 INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
 ('66799','0','15','4194304','0','0','0','0','0','0','0'), -- Desolation (Rank 1)
@@ -10,5 +10,5 @@ INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `Spell
 -- Vampiric Touch dispel fix
 DELETE FROM `spell_bonus_data` WHERE `entry` IN ('34914', '64085');
 INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES
-('34914','0','0.4','0','0','Priest - Vampiric Touch');
+('34914','0','0.4','0','0','Priest - Vampiric Touch'),
 ('64085','1.2','0','0','0','Priest - Vampiric Touch');
