f6214ee77c21f4978f8cadc324523fffed25137f
SignFinder <dalex@mail.ru>
2011-01-24 10:05:54 +0100

    Fix Summon Infernal spell. Thanks inordon fod idea

--- sql/fc_updates/facecore_updates_world.sql
+++ sql/fc_updates/facecore_updates_world.sql
@@ -12,3 +12,6 @@ DELETE FROM `spell_bonus_data` WHERE `entry` IN ('34914', '64085');
 INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES
 ('34914','0','0.4','0','0','Priest - Vampiric Touch'),
 ('64085','1.2','0','0','0','Priest - Vampiric Touch');
+
+-- Fix Summon Infernal spell. Thanks inordon fod idea
+UPDATE `creature_template` SET flags_extra = 0 WHERE `entry` = 89; 
