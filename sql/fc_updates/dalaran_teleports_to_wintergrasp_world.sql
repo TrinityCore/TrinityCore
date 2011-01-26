c83a3c968861d5fcd3e405936cce2e30bacc4ac5
SignFinder <dalex@mail.ru>
2011-01-26 08:47:29 +0100

    Add teleports in Dalaran to wintergrasp and small rename for cmake

---
+++ sql/fc_updates/dalaran_teleports_to_wintergrasp_world.sql
@@ -0,0 +1,4 @@
+﻿DELETE FROM `gameobject` WHERE `id`=193772;
+INSERT INTO `gameobject` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`position_x`,`position_y`,`position_z`,`orientation`,`rotation0`,`rotation1`,`rotation2`,`rotation3`,`spawntimesecs`,`animprogress`,`state`) VALUES
+('', 193772, 571, 1, 65535, 5686.57, 772.921, 647.754, 5.62225, 0, 0, 0.324484, -0.945891, 600, 0, 1),
+('', 193772, 571, 1, 65535, 5930.82, 548.961, 640.632, 1.88506, 0, 0, 0.809047, 0.587744, 300, 0, 1);
