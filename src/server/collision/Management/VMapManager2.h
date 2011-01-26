58f515bde1e2b80320e9d0f38fb75e62a5ffca2a
WarHead <WarHead@UWoM.de>
2011-01-26 07:44:55 +0100

    Nachdem wir TrinityCore mal wieder "gefolgt" sind ( Umzug auf GIT :-( ), hier nun der Start der Reimplementierung unser eigenen Änderungen / Fixes / Erweiterungen. Dies Reimplementiert die Möglichkeit spezielle VMaps auszuschalten/ignorieren. Zahlreiche weitere Commits werden folgen...

--- src/server/collision/Management/VMapManager2.h
+++ src/server/collision/Management/VMapManager2.h
@@ -68,6 +68,8 @@ namespace VMAP
             ModelFileMap iLoadedModelFiles;
             InstanceTreeMap iInstanceMapTrees;
 
+            UNORDERED_MAP<unsigned int , bool> iIgnoreMapIds;
+
             bool _loadMap(uint32 pMapId, const std::string &basePath, uint32 tileX, uint32 tileY);
             /* void _unloadMap(uint32 pMapId, uint32 x, uint32 y); */
 
@@ -94,6 +96,7 @@ namespace VMAP
 
             bool processCommand(char * /*pCommand*/) { return false; }      // for debug and extensions
 
+            void preventMapsFromBeingUsed(const char* pMapIdString);
             bool getAreaInfo(unsigned int pMapId, float x, float y, float &z, uint32 &flags, int32 &adtId, int32 &rootId, int32 &groupId) const;
             bool GetLiquidLevel(uint32 pMapId, float x, float y, float z, uint8 ReqLiquidType, float &level, float &floor, uint32 &type) const;
 
--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -1119,6 +1119,7 @@ void World::LoadConfigSettings(bool reload)
     bool enableIndoor = sConfig->GetBoolDefault("vmap.enableIndoorCheck", true);
     bool enableLOS = sConfig->GetBoolDefault("vmap.enableLOS", true);
     bool enableHeight = sConfig->GetBoolDefault("vmap.enableHeight", true);
+    std::string ignoreMapIds = sConfig->GetStringDefault("vmap.ignoreMapIds", "");
     bool enablePetLOS = sConfig->GetBoolDefault("vmap.petLOS", true);
     std::string ignoreSpellIds = sConfig->GetStringDefault("vmap.ignoreSpellIds", "");
 
@@ -1127,8 +1128,9 @@ void World::LoadConfigSettings(bool reload)
 
     VMAP::VMapFactory::createOrGetVMapManager()->setEnableLineOfSightCalc(enableLOS);
     VMAP::VMapFactory::createOrGetVMapManager()->setEnableHeightCalc(enableHeight);
+    VMAP::VMapFactory::createOrGetVMapManager()->preventMapsFromBeingUsed(ignoreMapIds.c_str());
     VMAP::VMapFactory::preventSpellsFromBeingTestedForLoS(ignoreSpellIds.c_str());
-    sLog->outString("WORLD: VMap support included. LineOfSight:%i, getHeight:%i, indoorCheck:%i PetLOS:%i", enableLOS, enableHeight, enableIndoor, enablePetLOS);
+    sLog->outString("WORLD: VMap support included. LineOfSight: %i, getHeight: %i, indoorCheck: %i, ignoreMapIds: %s, ignoreSpellIds: %s, PetLOS: %i", enableLOS, enableHeight, enableIndoor, ignoreMapIds.c_str(), ignoreSpellIds.c_str(), enablePetLOS);
     sLog->outString("WORLD: VMap data directory is: %svmaps",m_dataPath.c_str());
 
     m_int_configs[CONFIG_MAX_WHO] = sConfig->GetIntDefault("MaxWhoListReturns", 49);
