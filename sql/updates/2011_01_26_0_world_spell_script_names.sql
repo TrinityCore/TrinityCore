afcfbd008cb8412199dd230356ea523075577caf
Shauren <shauren.trinity@gmail.com>
2011-01-28 14:33:12 +0100

    Scripts/Icecrown Citadel: Implemented Pact of the Darkfallen damage scaling (Blood-Queen Lana'thel)
    SQL: Renamed and split 2011_01_26_world_scriptname to meet the standards

---
+++ sql/updates/2011_01_26_0_world_spell_script_names.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_script_names` WHERE `spell_id`=51162 AND `ScriptName`='spell_eregos_planar_shift';
+INSERT INTO `spell_script_names`(`spell_id`,`ScriptName`) VALUES
+(51162,'spell_eregos_planar_shift');
