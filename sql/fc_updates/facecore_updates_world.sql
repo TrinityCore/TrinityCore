8fb450a50eae2f8bd5875c1a86d237a9f4391b07
SignFinder <dalex@mail.ru>
2011-02-04 08:35:43 +0100

    typo fix

--- sql/fc_updates/facecore_updates_world.sql
+++ sql/fc_updates/facecore_updates_world.sql
@@ -88,6 +88,6 @@ UPDATE `creature_template` SET `ScriptName` = 'npc_argent_champion' WHERE `entry
 DELETE FROM `creature_template_addon` WHERE `entry` = 33707;
 INSERT INTO `creature_template_addon` (`entry`, `mount`) VALUES ('33707', '14337');
 
---prevent bagouse varlock summon spell on battkegrounds
+-- prevent bagouse varlock's summon spell on battlegrounds
 DELETE FROM `disables` WHERE `sourceType`=0 and `entry` = 698;
 INSERT INTO `disables` VALUES (0, 698, (1+16+32), "489,529,529,566", "4572,4571,3417,3418,3820", "Ritual of Summoning spell on BG");
