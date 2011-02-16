ed91da2a73c69247dd735e4003b1db311d018450
gmlt.A <gmlta@yandex.ru>
2011-02-16 16:39:04 +0200

    Small changes in Pit of Saron
    * Scourgelord Tyrannus now unattackable until fixed

---
+++ sql/custom/world_pit_of_saron.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_template` SET `faction_A` = 14, `faction_H` = 14 WHERE `entry` IN ('36658', '36938'); -- Now faction is 35: boss locked in his start position so make it unattackable
+UPDATE `creature_template` SET `flags_extra` = 128 WHERE `entry` = 36731;
