12404f50d1b5dca54962ccc9b501e4c0f865b90a
SignFinder <dalex@mail.ru>
2011-02-16 08:43:05 +0100

    Core/Spells: Fix Anti-Magic Shell immunities. Thanks A-Metaphysical-Drama

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -11369,9 +11369,9 @@ bool Unit::IsImmunedToSpellEffect(SpellEntry const* spellInfo, uint32 index) con
         // Check for immune to application of harmful magical effects
         AuraEffectList const& immuneAuraApply = GetAuraEffectsByType(SPELL_AURA_MOD_IMMUNE_AURA_APPLY_SCHOOL);
         for (AuraEffectList::const_iterator iter = immuneAuraApply.begin(); iter != immuneAuraApply.end(); ++iter)
-            if (spellInfo->Dispel == DISPEL_MAGIC &&                                      // Magic debuff
-                ((*iter)->GetMiscValue() & GetSpellSchoolMask(spellInfo)) &&  // Check school
-                !IsPositiveEffect(spellInfo->Id, index))                                  // Harmful
+            if (spellInfo->DmgClass == SPELL_DAMAGE_CLASS_MAGIC && !(spellInfo->AttributesEx & SPELL_ATTR1_UNK9) && // Magical debuff
+                ((*iter)->GetMiscValue() & GetSpellSchoolMask(spellInfo)) &&                                        // Check school
+                !IsPositiveEffect(spellInfo->Id, index))         
                 return true;
     }
 
