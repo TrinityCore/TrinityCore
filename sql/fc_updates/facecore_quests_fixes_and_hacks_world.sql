cee5e30948cec38e22cc6283f65ca972fe4c84b7
SignFinder <signfinder@gmail.com>
2011-02-16 15:40:55 +0100

    DB/Quests: Fix mobs aggro for keldelar quest npc in Dalaran. Thanks 100parik

--- sql/fc_updates/facecore_quests_fixes_and_hacks_world.sql
+++ sql/fc_updates/facecore_quests_fixes_and_hacks_world.sql
@@ -1,4 +1,23 @@
 -- fix Junkboxes Needed quest available for all classes
 UPDATE `quest_template` SET `ZoneOrSort` = '0'  WHERE `entry` = '8249';
 
+-- fix mobs aggro for keldelar quest npc
+UPDATE `quest_template` SET `SrcSpell` = '70974' WHERE `entry` =20439;
+UPDATE `creature_template` SET `flags_extra` = '2' WHERE `entry` = 31885;
+UPDATE `creature_template` SET `flags_extra` = '2' WHERE `entry` = 31886;
+UPDATE `creature_template` SET `flags_extra` = '2' WHERE `entry` = 31557;
+UPDATE `creature_template` SET `flags_extra` = '2' WHERE `entry` = 32419;
+UPDATE `creature_template` SET `flags_extra` = '2' WHERE `entry` = 32420;
+UPDATE `creature_template` SET `flags_extra` = '2' WHERE `entry` = 32253;
+UPDATE `creature_template` SET `flags_extra` = '2' WHERE `entry` = 32415;
+UPDATE `creature_template` SET `flags_extra` = '2' WHERE `entry` = 32412;
+UPDATE `creature_template` SET `flags_extra` = '2' WHERE `entry` = 32251;
+UPDATE `creature_template` SET `flags_extra` = '2' WHERE `entry` = 32252;
 
+UPDATE `quest_template` SET `SrcSpell` = '70972' WHERE `entry` =24451;
+UPDATE `creature_template` SET `flags_extra` = '2' WHERE `entry` = 35507;
+UPDATE `creature_template` SET `flags_extra` = '2' WHERE `entry` = 31580;
+UPDATE `creature_template` SET `flags_extra` = '2' WHERE `entry` = 31579;
+UPDATE `creature_template` SET `flags_extra` = '2' WHERE `entry` = 35494;
+UPDATE `creature_template` SET `flags_extra` = '2' WHERE `entry` = 37942;
+UPDATE `creature_template` SET `flags_extra` = '2' WHERE `entry` = 33964;  
