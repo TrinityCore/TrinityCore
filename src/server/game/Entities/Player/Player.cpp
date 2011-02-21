b4ee305532c0e87cb999d2b52a223876f4adbc32
Vlad <vlad852@gmail.com>
2011-02-21 21:15:13 +0200

    Revert "Better solution for fix eploit."
    This reverts commit 0d6ccb9be1f39f8a84d2d020ddc151fbaeeeec68.

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -18115,7 +18115,7 @@ bool Player::CheckInstanceLoginValid()
     }
 
     // do checks for satisfy accessreqs, instance full, encounter in progress (raid), perm bind group != perm bind player
-    return sMapMgr->CanPlayerEnter(GetMap()->GetId(), this, false);
+    return sMapMgr->CanPlayerEnter(GetMap()->GetId(), this, true);
 }
 
 bool Player::_LoadHomeBind(PreparedQueryResult result)
