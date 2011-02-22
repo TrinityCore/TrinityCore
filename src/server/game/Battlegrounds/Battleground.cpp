cebf95d5036d329c64206821e6a103814aaa7e76
leak <leakzx@googlemail.com>
2011-02-22 16:52:38 +0100

    Core/Groups: Move scattered group object deletion to Disband()

--- src/server/game/Battlegrounds/Battleground.cpp
+++ src/server/game/Battlegrounds/Battleground.cpp
@@ -1034,7 +1034,6 @@ void Battleground::RemovePlayerAtLeave(uint64 guid, bool Transport, bool SendPac
             if (!group->RemoveMember(guid))                // group was disbanded
             {
                 SetBgRaid(team, NULL);
-                delete group;
             }
         }
         DecreaseInvitedCount(team);
