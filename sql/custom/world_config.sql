1e7a65c5ce75df18457a9622720a5927556759af
gmlt.A <gmlta@yandex.ru>
2011-02-24 19:08:03 +0200

    [SQL Config] One field from new db was lost

--- sql/custom/world_config.sql
+++ sql/custom/world_config.sql
@@ -6,6 +6,7 @@ CREATE TABLE `config` (
   `name` varchar(50) NOT NULL COMMENT 'Config parameter name.',
   `config_int` int(10) DEFAULT NULL,
   `config_float` float DEFAULT NULL,
+  `config_bool` tinyint(3) DEFAULT NULL,
   `config_string` varchar(50) DEFAULT NULL,
   `help` varchar(255) DEFAULT '',
   PRIMARY KEY (`name`)
