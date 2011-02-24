3afca7da8975c8ff7321050b13e3f878bf43e139
Azazel <azazel.kon@gmail.com>
2011-02-24 23:39:45 +0600

    CharDB Schema/Cleanup: cleanup account_data and character_account_data tables: * rename column account to accountId; * introduce prepared statements for both tables.

--- src/server/game/Accounts/AccountMgr.cpp
+++ src/server/game/Accounts/AccountMgr.cpp
@@ -75,7 +75,9 @@ AccountOpResult AccountMgr::DeleteAccount(uint32 accid)
 
     // table realm specific but common for all characters of account for realm
     CharacterDatabase.PExecute("DELETE FROM character_tutorial WHERE account = '%u'",accid);
-    CharacterDatabase.PExecute("DELETE FROM account_data WHERE account = '%u'",accid);
+    PreparedStatement* stmt = CharacterDatabase.GetPreparedStatement(CHAR_DEL_ACCOUNT_DATA);
+    stmt->setUInt32(0, accid);
+    CharacterDatabase.Execute(stmt);
 
     SQLTransaction trans = LoginDatabase.BeginTransaction();
 
