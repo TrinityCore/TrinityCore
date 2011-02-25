91b1586c38b37a7cd6d60cc26354f5c50e17a097
SignFinder <signfinder@gmail.com>
2011-02-25 07:16:35 +0100

    Core/Player: Prevent off PvP flag on battlegrounds and arenas. Tx Imprtat

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -18941,7 +18941,7 @@ void Player::UpdateContestedPvP(uint32 diff)
 
 void Player::UpdatePvPFlag(time_t currTime)
 {
-    if (!IsPvP())
+    if (!IsPvP() || InBattleground() || InArena()) 
         return;
     if (pvpInfo.endTimer == 0 || currTime < (pvpInfo.endTimer + 300))
         return;
