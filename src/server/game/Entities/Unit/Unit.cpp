fc79011f2dbb0a1406cc3ed26a65ee219b985b68
PrinceCreed <princecreed@gmail.com>
2011-03-01 09:01:18 +0100

    Core/Spells: Fix Combustion stack exploit (not test). Tx LihO

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -7934,6 +7934,17 @@ bool Unit::HandleAuraProc(Unit * pVictim, uint32 damage, Aura * triggeredByAura,
                     if (!caster || !damage)
                         return false;
 
+                    Aura * dummy = GetAura(28682);
+                    if (dummy && dummy->GetStackAmount() > 9 && (procEx & PROC_EX_CRITICAL_HIT) ){
+                        if(triggeredByAura->GetCharges() > 1){
+                            triggeredByAura->DropCharge(); // drop charge manually
+                            return false;
+                        }else{
+                            RemoveAurasDueToSpell(28682);
+                            return true;
+                        }
+                    }
+
                     //last charge and crit
                     if (triggeredByAura->GetCharges() <= 1 && (procEx & PROC_EX_CRITICAL_HIT))
                     {
