4eaccfaf21f2f2fc81694367cea56d4a86c3902f
WarHead <WarHead@UWoM.de>
2011-03-04 19:15:56 +0100

    Reimplementierung unserer PDK Fixes.

--- src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/boss_northrend_beasts.cpp
+++ src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/boss_northrend_beasts.cpp
@@ -306,6 +306,10 @@ public:
             if (m_bTargetDied || !UpdateVictim())
                 return;
 
+            if (Unit* pGormok = Unit::GetCreature(*me,m_pInstance->GetData64(NPC_GORMOK)))
+                if (!pGormok->isAlive())
+                    me->ForcedDespawn();
+
             if (Unit* pTarget = Unit::GetPlayer(*me, m_uiTargetGUID))
             {
                 if (!pTarget->isAlive())
--- src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/trial_of_the_crusader.cpp
+++ src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/trial_of_the_crusader.cpp
@@ -129,7 +129,7 @@ class npc_announcer_toc10 : public CreatureScript
             if (!instanceScript)
                 return true;
 
-            char const* _message = "We are ready!";
+            char const* _message = "Wir sind bereit!";
 
             if (player->isInCombat() || instanceScript->IsEncounterInProgress() || instanceScript->GetData(TYPE_EVENT))
                 return true;
