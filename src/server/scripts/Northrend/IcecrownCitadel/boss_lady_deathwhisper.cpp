6668975689b8d7bfeb9781e22f6b68e81cb1295e
WarHead <WarHead@UWoM.de>
2011-03-04 13:11:27 +0100

    ICC Raid: Todeswhispers Manaschild muss beim Spawn schon vorhanden, und auch beim Reset wieder da sein.

--- src/server/scripts/Northrend/IcecrownCitadel/boss_lady_deathwhisper.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_lady_deathwhisper.cpp
@@ -242,14 +242,18 @@ class boss_lady_deathwhisper : public CreatureScript
                 nextVengefulShadeTarget = 0;
                 darnavanGUID = 0;
                 DoCast(me, SPELL_SHADOW_CHANNELING);
+                DoCast(me, SPELL_MANA_BARRIER, true);
                 me->RemoveAurasDueToSpell(SPELL_BERSERK);
-                me->RemoveAurasDueToSpell(SPELL_MANA_BARRIER);
-                me->ApplySpellImmune(0, IMMUNITY_STATE, SPELL_AURA_MOD_TAUNT, false);
-                me->ApplySpellImmune(0, IMMUNITY_EFFECT, SPELL_EFFECT_ATTACK_ME, false);
+//                me->RemoveAurasDueToSpell(SPELL_MANA_BARRIER);
+//                me->ApplySpellImmune(0, IMMUNITY_STATE, SPELL_AURA_MOD_TAUNT, false);
+//                me->ApplySpellImmune(0, IMMUNITY_EFFECT, SPELL_EFFECT_ATTACK_ME, false);
             }
 
             void MoveInLineOfSight(Unit* who)
             {
+                if (!me->isInCombat() && !me->HasAura(SPELL_MANA_BARRIER))
+                    DoCast(me, SPELL_MANA_BARRIER, true);
+
                 if (!introDone && me->IsWithinDistInMap(who, 100.0f))
                 {
                     me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
@@ -302,7 +306,6 @@ class boss_lady_deathwhisper : public CreatureScript
                 Talk(SAY_AGGRO);
                 DoStartNoMovement(who);
                 me->RemoveAurasDueToSpell(SPELL_SHADOW_CHANNELING);
-                DoCast(me, SPELL_MANA_BARRIER, true);
 
                 instance->SetBossState(DATA_LADY_DEATHWHISPER, IN_PROGRESS);
             }
