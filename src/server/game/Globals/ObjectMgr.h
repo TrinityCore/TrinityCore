e9a2b8effc285550943d706cf2a04d83078a3289
Machiavelli <machiavelli.trinity@gmail.com>
2011-03-07 20:48:53 +0100

    Core/Vehicle: Use db table GuidLow instead of normal GuidLow (which may differ per instance) for vehicle_accessory definitions.

--- src/server/game/Globals/ObjectMgr.h
+++ src/server/game/Globals/ObjectMgr.h
@@ -809,10 +809,10 @@ class ObjectMgr
 
         VehicleAccessoryList const* GetVehicleAccessoryList(Vehicle* veh) const
         {
-            if (veh->GetBase()->ToCreature())
+            if (Creature* cre = veh->GetBase()->ToCreature())
             {
                 // Give preference to GUID-based accessories
-                VehicleAccessoryMap::const_iterator itr = m_VehicleAccessoryMap.find(veh->GetBase()->GetGUIDLow());
+                VehicleAccessoryMap::const_iterator itr = m_VehicleAccessoryMap.find(cre->GetDBTableGUIDLow());
                 if (itr != m_VehicleAccessoryMap.end())
                     return &itr->second;
             }
