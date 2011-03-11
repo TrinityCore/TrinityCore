7da86de9e896e1dd7ac4a219c020f7557a7bf24c
Vlad <vlad852@gmail.com>
2011-03-11 23:56:01 +0200

    Revert DS. Anyway nor playble. =\
    This reverts commit 08a4c5c76a29904049c8072cabea8f8b139b9b92.

--- src/server/game/Battlegrounds/Zones/BattlegroundDS.h
+++ src/server/game/Battlegrounds/Zones/BattlegroundDS.h
@@ -43,10 +43,10 @@ enum BattlegroundDSObjects
 };
 
 enum BattlegroundDSData
-{
-    BG_DS_WATERFALL_TIMER_MIN                    = 35000,
+{ // These values are NOT blizzlike... need the correct data!
+    BG_DS_WATERFALL_TIMER_MIN                    = 30000,
     BG_DS_WATERFALL_TIMER_MAX                    = 60000,
-    BG_DS_WATERFALL_DURATION                     = 30000,
+    BG_DS_WATERFALL_DURATION                     = 10000,
 };
 
 class BattlegroundDSScore : public BattlegroundScore
@@ -81,9 +81,6 @@ class BattlegroundDS : public Battleground
     private:
         uint32 m_waterTimer;
         bool m_waterfallActive;
-        bool m_knockbackCheck;
-        uint32 m_knockback;
-        void KnockBackPlayer(Unit *pPlayer, float angle, float horizontalSpeed, float verticalSpeed);
     protected:
         bool isWaterFallActive() { return m_waterfallActive; };
         void setWaterFallActive(bool active) { m_waterfallActive = active; };
