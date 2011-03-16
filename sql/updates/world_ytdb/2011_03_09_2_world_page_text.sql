04f4d0491d2611e358aa8477ceb67c3c3abbbff7
SignFinder <signfinder@gmail.com>
2011-03-16 08:48:43 +0100

    Removed support for TC updates for YTDB (no time)

--- sql/updates/world_ytdb/2011_03_09_2_world_page_text.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `page_text` WHERE `entry` IN (3281);
-INSERT INTO `page_text` (`entry`, `text`, `next_page`,`WDBVerified`) VALUES
-(3281, 'The Chieftain''s brother, Stormhoof gathered all his strength and set out to conquer the Storm Peaks from its rightful ruler, the North Wind.', 0,1);
-
