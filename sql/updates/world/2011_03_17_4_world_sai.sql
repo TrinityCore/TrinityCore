79357804fde28bb7e89667d8af0be7938e6c3927
Malcrom <none@none>
2011-03-17 11:45:20 +0100

    DB/SAI: Fel Cannon MKI SAI
    DB/SAI: Correct 2011_03_17_4 error

--- sql/updates/world/2011_03_17_4_world_sai.sql
+++ sql/updates/world/2011_03_17_4_world_sai.sql
@@ -4,7 +4,7 @@ SET @AURA := 17327;
 SET @SPELL1 := 37361;
 SET @SPELL2 := 24050;
 UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
-DELETE FROM `creature_ai_scripts` WHERE `creature_id`=0 AND `entryorguid`=@ENTRY;
+DELETE FROM `creature_ai_scripts` WHERE `creature_id`=@ENTRY;
 DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (@ENTRY,0,0,0,23,0,100,0,@AURA,0,2000,2000,11,@AURA,0,0,0,0,0,1,0,0,0,0,0,0,0, 'Lost Drakkari Spirit - Aura Spirit Particles not present - Add Aura Spirit Particles'),
