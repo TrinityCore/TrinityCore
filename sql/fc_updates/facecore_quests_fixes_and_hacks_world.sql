c6a7d25f3fd043d747bab17578e88c82323be2dc
SignFinder <signfinder@gmail.com>
2011-03-17 10:15:25 +0100

    Scripts/Quests: Fix quest Cold Hearted (12856). Tx zwerg

--- sql/fc_updates/facecore_quests_fixes_and_hacks_world.sql
+++ sql/fc_updates/facecore_quests_fixes_and_hacks_world.sql
@@ -21,3 +21,17 @@ UPDATE `creature_template` SET `flags_extra` = '2' WHERE `entry` = 31579;
 UPDATE `creature_template` SET `flags_extra` = '2' WHERE `entry` = 35494;
 UPDATE `creature_template` SET `flags_extra` = '2' WHERE `entry` = 37942;
 UPDATE `creature_template` SET `flags_extra` = '2' WHERE `entry` = 33964;  
+
+-- fix 12856 (Cold Hearted)
+-- captive proto-drake
+DELETE FROM `npc_spellclick_spells` WHERE `npc_entry`=29708;
+INSERT INTO `npc_spellclick_spells` (`npc_entry`,`spell_id`,`quest_start`,`quest_start_active`,`quest_end`,`cast_flags`,`aura_required`,`aura_forbidden`,`user_type`)
+VALUES (29708,55028,12856,1,12856,1,0,0,0);
+
+-- freed proto-drake
+DELETE FROM `creature_template_addon` WHERE `entry`=29709;
+INSERT INTO `creature_template_addon` (`entry`,`mount`,`bytes1`,`bytes2`,`emote`,`auras`) VALUES
+(29709,0,50331648,257,0, NULL);
+
+UPDATE `creature_template` SET `unit_flags`=`unit_flags`|16777216, `ScriptName`='' WHERE `entry`=29708;
+UPDATE `creature_template` SET `InhabitType`=7 WHERE `entry`=29709;
