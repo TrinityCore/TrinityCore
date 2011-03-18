c689749590e29abc5d3468f5111045c4066231a3
Shauren <shauren.trinity@gmail.com>
2011-03-18 18:31:17 +0100

    Scripts/Pit of Saron: Fixed Scourgelord Tyrannus intro being started every time areatrigger was stepped on

--- src/server/scripts/Northrend/FrozenHalls/PitOfSaron/pit_of_saron.h
+++ src/server/scripts/Northrend/FrozenHalls/PitOfSaron/pit_of_saron.h
@@ -23,12 +23,14 @@
 
 enum DataTypes
 {
+    // Encounter states and GUIDs
     DATA_GARFROST           = 0,
-    DATA_KRICK              = 1,
-    DATA_ICK                = 2,
-    DATA_TYRANNUS           = 3,
-    DATA_RIMEFANG           = 4,
+    DATA_ICK                = 1,
+    DATA_TYRANNUS           = 2,
 
+    // GUIDs
+    DATA_RIMEFANG           = 3,
+    DATA_KRICK              = 4,
     DATA_JAINA_SYLVANAS_1   = 5,    // GUID of either Jaina or Sylvanas part 1, depending on team, as it's the same spawn.
     DATA_JAINA_SYLVANAS_2   = 6,    // GUID of either Jaina or Sylvanas part 2, depending on team, as it's the same spawn.
     DATA_TYRANNUS_EVENT     = 7,
