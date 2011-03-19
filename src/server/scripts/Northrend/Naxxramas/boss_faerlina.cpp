1a4a618d81067969d7eec3725988a8bf0f845881
Shauren <shauren.trinity@gmail.com>
2011-03-19 10:54:16 +0100

    Core/Scripts: Code style cleanup in ScriptedAI and BossAI

--- src/server/scripts/Northrend/Naxxramas/boss_faerlina.cpp
+++ src/server/scripts/Northrend/Naxxramas/boss_faerlina.cpp
@@ -197,7 +197,7 @@ public:
 
         void Reset()
         {
-            if (getDifficulty() == RAID_DIFFICULTY_10MAN_NORMAL) {
+            if (GetDifficulty() == RAID_DIFFICULTY_10MAN_NORMAL) {
                 me->ApplySpellImmune(0, IMMUNITY_MECHANIC, SPELL_EFFECT_BIND, true);
                 me->ApplySpellImmune(0, IMMUNITY_MECHANIC, MECHANIC_CHARM, true);
             }
@@ -205,7 +205,7 @@ public:
 
         void JustDied(Unit * /*killer*/)
         {
-            if (pInstance && getDifficulty() == RAID_DIFFICULTY_10MAN_NORMAL)
+            if (pInstance && GetDifficulty() == RAID_DIFFICULTY_10MAN_NORMAL)
             {
                 if (Creature *pFaerlina = pInstance->instance->GetCreature(pInstance->GetData64(DATA_FAERLINA)))
                     DoCast(pFaerlina, SPELL_WIDOWS_EMBRACE);
