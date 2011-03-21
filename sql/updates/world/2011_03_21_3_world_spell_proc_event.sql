c01ff4da2a3c63b45b80ba8f82b4288d8a738fb0
Ramus <nikson.91@mail.ru>
2011-03-21 13:50:50 +0200

    Fix Combustion bug exploit Add forgotten file from previous commit

---
+++ sql/updates/world/2011_03_21_3_world_spell_proc_event.sql
@@ -0,0 +1,2 @@
+DELETE FROM `spell_proc_event` WHERE `entry`=11129;
+INSERT INTO `spell_proc_event` VALUES (11129, 0x04, 3, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0, 0, 0); -- Combustion
