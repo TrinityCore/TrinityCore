dab59b82333655adfdc24edd92b90f735bcc4b02
A Metaphysical Drama <a.metaphysical.drama@gmail.com>
2011-03-23 17:44:33 +0100

    Spells/DK: Fix Bloodworms heal

--- sql/fc_updates/facecore_updates_world.sql
+++ sql/fc_updates/facecore_updates_world.sql
@@ -260,3 +260,8 @@ INSERT INTO npc_spellclick_spells (`npc_entry`, `spell_id`, `quest_start`, `ques
 (35419, 68503, 0, 0, 0, 1, 0, 0, 0),
 (35431, 46598, 0, 0, 0, 1, 0, 0, 0),
 (35433, 46598, 0, 0, 0, 1, 0, 0, 0); 
+
+-- Bloodworm AI
+DELETE FROM `creature_ai_scripts` WHERE `creature_id` = 28017;
+INSERT INTO `creature_ai_scripts` (`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `action2_type`, `action2_param1`, `action2_param2`, `action2_param3`, `action3_type`, `action3_param1`, `action3_param2`, `action3_param3`, `comment`) VALUES
+(2801700, 28017, 4, 0, 100, 0, 0, 0, 0, 0, 11, 50453, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Bloodworm - Health Leech');
