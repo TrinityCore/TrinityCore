a49c9ec4c6b2d717ba0b207fe02fba78d361b136
Vlad <vlad852@gmail.com>
2011-03-24 16:40:15 +0200

    Add EffectMechanic MECHANIC_KNOCKOUT for all spells with EFFECT_KNOCK_BACK. It should fix immunity for bosses.

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -3517,6 +3517,11 @@ void SpellMgr::LoadSpellCustomAttr()
         {
             switch (spellInfo->Effect[j])
             {
+                case SPELL_EFFECT_KNOCK_BACK:
+                case SPELL_EFFECT_KNOCK_BACK_DEST:
+                    EffectMechanic[i] = MECHANIC_KNOCKOUT;
+                    count++;
+                    break;
                 case SPELL_EFFECT_SCHOOL_DAMAGE:
                 case SPELL_EFFECT_WEAPON_DAMAGE:
                 case SPELL_EFFECT_WEAPON_DAMAGE_NOSCHOOL:
