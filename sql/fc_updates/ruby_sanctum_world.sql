145bd0f06d1ab0d3ab3481a25195be5128e83e29
SignFinder <signfinder@gmail.com>
2011-03-25 08:27:46 +0100

    DB/Instances: Fix Halion spawn

--- sql/fc_updates/ruby_sanctum_world.sql
+++ sql/fc_updates/ruby_sanctum_world.sql
@@ -42,3 +42,8 @@ INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceGroup`,`SourceEntry`,
 
 -- Xerestrasza
 UPDATE `creature_template` SET `ScriptName`='mob_xerestrasza', `AIName` ='' WHERE `entry`=40429;
+
+-- fix Halion spawn
+DELETE FROM `creature` WHERE `id` = 39863;
+INSERT INTO `creature` (`id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `DeathState`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`) VALUES
+(39863, 724, 15, 1, 0, 0, 3144.93, 527.233, 72.8887, 0.110395, 300, 0, 0, 11156000, 0, 0, 0, 0, 0, 0);
