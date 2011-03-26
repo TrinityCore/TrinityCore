c7491b2194aa39b0ca9df5bc0dee78a0bbd998a2
Kapoeira <vertozor@gmail.com>
2011-03-26 02:28:31 -0400

    Core/Talents: Reapplied feral multiplier on Predatory Strikes.

--- src/server/game/Entities/Unit/StatSystem.cpp
+++ src/server/game/Entities/Unit/StatSystem.cpp
@@ -330,6 +330,8 @@ void Player::UpdateAttackPowerAndDamage(bool ranged)
                 ShapeshiftForm form = GetShapeshiftForm();
                 // Check if Predatory Strikes is skilled
                 float mLevelMult = 0.0f;
+				float mFeralMult = 0.0f;
+				bool applied = false;
                 switch (form)
                 {
                     case FORM_CAT:
@@ -344,7 +346,17 @@ void Player::UpdateAttackPowerAndDamage(bool ranged)
                             if ((*itr)->GetEffIndex() == 0 && (*itr)->GetSpellProto()->SpellIconID == 1563)
                             {
                                 mLevelMult = CalculatePctN(1.0f, (*itr)->GetAmount());
-                                break;
+								if(applied)
+									break;
+								applied = true;
+                            }
+                            // Predatory Strikes (effect 0)
+                            if ((*itr)->GetEffIndex() == 1 && (*itr)->GetSpellProto()->SpellIconID == 1563)
+                            {
+                                mFeralMult = CalculatePctN(1.0f, (*itr)->GetAmount());
+								if(applied)
+									break;
+								applied = true;
                             }
                         }
                         break;
@@ -355,10 +367,10 @@ void Player::UpdateAttackPowerAndDamage(bool ranged)
                 switch (form)
                 {
                     case FORM_CAT:
-                        val2 = getLevel() * (mLevelMult + 2.0f) + GetStat(STAT_STRENGTH) * 2.0f + GetStat(STAT_AGILITY) - 20.0f + m_baseFeralAP; break;
+                        val2 = getLevel() * (mLevelMult + 2.0f) + GetStat(STAT_STRENGTH) * 2.0f + GetStat(STAT_AGILITY) - 20.0f + (m_baseFeralAP * (mFeralMult + 1.0f)); break;
                     case FORM_BEAR:
                     case FORM_DIREBEAR:
-                        val2 = getLevel() * (mLevelMult + 3.0f) + GetStat(STAT_STRENGTH) * 2.0f - 20.0f + m_baseFeralAP; break;
+                        val2 = getLevel() * (mLevelMult + 3.0f) + GetStat(STAT_STRENGTH) * 2.0f - 20.0f + (m_baseFeralAP * (mFeralMult + 1.0f)); break;
                     case FORM_MOONKIN:
                         val2 = getLevel() * (mLevelMult + 1.5f) + GetStat(STAT_STRENGTH) * 2.0f - 20.0f + m_baseFeralAP; break;
                     default:
@@ -1092,7 +1104,7 @@ void Guardian::UpdateMaxHealth()
         case ENTRY_SUCCUBUS:    multiplicator = 9.1f;   break;
         case ENTRY_FELHUNTER:   multiplicator = 9.5f;   break;
         case ENTRY_FELGUARD:    multiplicator = 11.0f;  break;
-	    case ENTRY_BLOODWORM:   multiplicator = 1.0f;   break;
+        case ENTRY_BLOODWORM:   multiplicator = 1.0f;   break;
         default:                multiplicator = 10.0f;  break;
     }
 
