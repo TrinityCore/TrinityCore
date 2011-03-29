111d16fde84755c879cb4e71f9a0c72497a8afca
ZxBiohazardZx <zxbiohazardzx@gmail.com>
2011-03-29 11:22:28 +0200

    DB/Spawns: Increase some mining pools to more reasonable values

---
+++ sql/updates/world/2011_03_29_01_world_pool_template.sql
@@ -0,0 +1,14 @@
+-- update some maxcounts in spawnpools
+UPDATE `pool_template` SET `max_limit`=15 WHERE `pool`=896; -- Wintergrasp from 20 to 15
+UPDATE `pool_template` SET `max_limit`=35 WHERE `pool`=897; -- Icecrown from 20 to 35
+UPDATE `pool_template` SET `max_limit`=10 WHERE `pool`=2001; -- Alterac Mountains (outer section) from 4 to 10
+UPDATE `pool_template` SET `max_limit`=45 WHERE `pool`=2002; -- Arathi Highlands (Main Section) from 10 to 45
+UPDATE `pool_template` SET `max_limit`=25 WHERE `pool`=2004; -- Badlands from 9 to 25
+UPDATE `pool_template` SET `max_limit`=20 WHERE `pool`=2005; -- Blasted Lands from 8 to 20
+UPDATE `pool_template` SET `max_limit`=40 WHERE `pool`=2007; -- Burning Steppes from 10 to 40
+UPDATE `pool_template` SET `max_limit`=15 WHERE `pool`=2008; -- Dun Morogh from 12 to 15
+UPDATE `pool_template` SET `max_limit`=20 WHERE `pool`=2009; -- Duskwood from 16 to 20
+UPDATE `pool_template` SET `max_limit`=10 WHERE `pool`=2012; -- Eversong Woods from 6 to 10
+UPDATE `pool_template` SET `max_limit`=10 WHERE `pool`=2014; -- Hillsbrad Foothills (Main Section) from 5 to 10
+UPDATE `pool_template` SET `max_limit`= 3 WHERE `pool`=2016; -- Hillsbrad Foothills (Azurelode Mine) - Special Mineral GOs ONLY from 4 to 3
+UPDATE `pool_template` SET `max_limit`=20 WHERE `pool`=2018; -- Loch Modan from 9 to 20
