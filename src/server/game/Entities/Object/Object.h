73ffbd11c08830dde967ff4315003e02cd253a30
SignFinder <signfinder@gmail.com>
2011-04-01 07:39:54 +0200

    Temporary hack-fix of crash in Object.h:180. Tx blackmanos

--- src/server/game/Entities/Object/Object.h
+++ src/server/game/Entities/Object/Object.h
@@ -171,39 +171,39 @@ class Object
 
         const int32& GetInt32Value(uint16 index) const
         {
-            ASSERT(index < m_valuesCount || PrintIndexError(index , false));
+            //ASSERT(index < m_valuesCount || PrintIndexError(index , false));
             return m_int32Values[ index ];
         }
 
         const uint32& GetUInt32Value(uint16 index) const
         {
-            ASSERT(index < m_valuesCount || PrintIndexError(index , false));
+            //ASSERT(index < m_valuesCount || PrintIndexError(index , false));
             return m_uint32Values[ index ];
         }
 
         const uint64& GetUInt64Value(uint16 index) const
         {
-            ASSERT(index + 1 < m_valuesCount || PrintIndexError(index , false));
+            //ASSERT(index + 1 < m_valuesCount || PrintIndexError(index , false));
             return *((uint64*)&(m_uint32Values[ index ]));
         }
 
         const float& GetFloatValue(uint16 index) const
         {
-            ASSERT(index < m_valuesCount || PrintIndexError(index , false));
+            //ASSERT(index < m_valuesCount || PrintIndexError(index , false));
             return m_floatValues[ index ];
         }
 
         uint8 GetByteValue(uint16 index, uint8 offset) const
         {
-            ASSERT(index < m_valuesCount || PrintIndexError(index , false));
-            ASSERT(offset < 4);
+            //ASSERT(index < m_valuesCount || PrintIndexError(index , false));
+            //ASSERT(offset < 4);
             return *(((uint8*)&m_uint32Values[ index ])+offset);
         }
 
         uint16 GetUInt16Value(uint16 index, uint8 offset) const
         {
-            ASSERT(index < m_valuesCount || PrintIndexError(index , false));
-            ASSERT(offset < 2);
+            //ASSERT(index < m_valuesCount || PrintIndexError(index , false));
+            //ASSERT(offset < 2);
             return *(((uint16*)&m_uint32Values[ index ])+offset);
         }
 
