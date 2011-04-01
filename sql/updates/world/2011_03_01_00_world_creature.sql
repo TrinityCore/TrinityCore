f2fe3ec60c82d7cc5a214adb4a9eb475aa538a7a
Nay <dnpd.dd@gmail.com>
2011-04-01 22:30:09 +0100

    SQL: Fix typo on last commit. Thanks EdwinDW.

--- sql/updates/world/2011_03_01_00_world_creature.sql
+++ sql/updates/world/2011_03_01_00_world_creature.sql
@@ -1,1 +1,1 @@
-UPDATE `creature` SET `spawnMask`=`spawnMask`|2|1 WHERE `guid` BETWEEN 48304 AND 48304+6 AND `entry`=33233;
+UPDATE `creature` SET `spawnMask`=`spawnMask`|2|1 WHERE `guid` BETWEEN 48304 AND 48304+6 AND `id`=33233;
