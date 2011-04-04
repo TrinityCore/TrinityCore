93d4923408c19fe5e585b56852c78047714d2b07
runningnak3d <runningnak3d@gmail.com>
2011-04-04 10:51:55 -0400

    Core/BuildSystem: Removed -WITH_SQL Cmake option as it was confusing some users.

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -16,4 +16,3 @@ option(USE_COREPCH      "Use precompiled headers when compiling servers"        
 option(USE_SFMT         "Use SFMT as random numbergenerator"                          0)
 option(WITH_WARNINGS    "Show all warnings during compile"                            0)
 option(WITH_COREDEBUG   "Include additional debug-code in core"                       0)
-option(WITH_SQL         "Copy SQL files during installation"                          0)
