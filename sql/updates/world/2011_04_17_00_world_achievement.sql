19721514c3899153f6c58b234b4ba8e85d906b05
Shauren <shauren.trinity@gmail.com>
2011-04-17 14:55:32 +0200

    DB/Achievements: Fixed startup erros and rerunning possibility for previous achievement sql

--- sql/updates/world/2011_04_17_00_world_achievement.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM `disables` WHERE `sourceType` = 4 AND `entry` in (7625,6446,7628);
-DELETE FROM `achievement_criteria_data` WHERE `criteria_id` in(6446,7625) AND `type`=11;
-INSERT INTO `achievement_criteria_data` (`criteria_id`, `type`, `value1`, `value2`, `ScriptName`) VALUES 
-(6446,11,0,0,'achievement_bg_sa_drop_it'),
-(7625,11,0,0,'achievement_bg_sa_artillery_veteran'),
-(7628,11,0,0,'achievement_bg_sa_artillery_expert'),
-(7628,3,607,0,'');
