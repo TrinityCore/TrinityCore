6ad2278ff59cbfa6ff37dcf697123e8f0c2012a1
kaelima <kaelima@live.se>
2011-04-17 16:37:10 +0300

    Core/Spells: Move cloning spells to SpellScript

--- sql/scripts/world_scripts_full.sql
+++ sql/scripts/world_scripts_full.sql
@@ -1885,6 +1885,18 @@ INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
 ( 61177, 'spell_gen_profession_research'),
 ( 61288, 'spell_gen_profession_research'),
 ( 61756, 'spell_gen_profession_research'),
+( 45785, 'spell_generic_clone'),
+( 49889, 'spell_generic_clone'),
+( 50218, 'spell_generic_clone'),
+( 51719, 'spell_generic_clone'),
+( 57528, 'spell_generic_clone'),
+( 69828, 'spell_generic_clone'),
+( 41055, 'spell_generic_clone_weapon'),
+( 63416, 'spell_generic_clone_weapon'),
+( 69891, 'spell_generic_clone_weapon'),
+( 45206, 'spell_generic_clone_weapon'),
+( 69892, 'spell_generic_clone_weapon'),
+( 57593, 'spell_generic_clone_weapon'),
 -- instances
 -- Black Temple
 ( 41475, 'spell_boss_lady_malande_shield'),
