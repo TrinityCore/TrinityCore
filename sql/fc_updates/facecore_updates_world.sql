014d75788efc4f35c9a6965045b3ba581dc0c0ea
tobmaps <spambot42@yandex.ru>
2011-04-18 10:19:27 +0200

    Spells/Paladin: Fixed paladin's talent Blessing of Sanctuary. Now it will properly reduce taken damage and this effect will not stack with same effect from warrior's Vigilance.

--- sql/fc_updates/facecore_updates_world.sql
+++ sql/fc_updates/facecore_updates_world.sql
@@ -449,3 +449,11 @@ INSERT INTO `spell_proc_event` (`entry`,`procEx`) VALUES
 (49004,0x00000033),
 (49508,0x00000033),
 (49509,0x00000033);
+
+-- Fixed paladin's talent Blessing of Sanctuary
+DELETE FROM `spell_dbc` WHERE `id` = 20912;
+INSERT INTO `spell_dbc` (`Id`,`CastingTimeIndex`,`DurationIndex`,`RangeIndex`,`Effect1`,`EffectBasePoints1`,`EffectImplicitTargetA1`,`EffectApplyAuraName1`,`EffectMiscValue1`,`SpellFamilyName`,`Comment`) VALUES
+(20912,1,21,1,6,-3,1,87,127,10,'Blessing of Sanctuary Helper (SERVERSIDE)');
+-- Blessing of Sanctuary vs Vigilance
+UPDATE `spell_group` SET `spell_id` = 68066 WHERE `id` = 1091 and `spell_id` = 47930;
+UPDATE `spell_group` SET `spell_id` = 20912 WHERE `id` = 1092 and `spell_id` = 20911;
--- src/server/scripts/Spells/spell_paladin.cpp
+++ src/server/scripts/Spells/spell_paladin.cpp
@@ -29,6 +29,8 @@ enum PaladinSpells
     PALADIN_SPELL_DIVINE_PLEA                    = 54428,
     PALADIN_SPELL_BLESSING_OF_SANCTUARY_BUFF     = 67480,
 
+    PALADIN_SPELL_BLESSING_OF_SANCTUARY_HELPER   = 20912,
+
     PALADIN_SPELL_HOLY_SHOCK_R1                  = 20473,
     PALADIN_SPELL_HOLY_SHOCK_R1_DAMAGE           = 25912,
     PALADIN_SPELL_HOLY_SHOCK_R1_HEALING          = 25914,
@@ -189,12 +191,14 @@ public:
             Unit* pTarget = GetTarget();
             if (Unit* pCaster = GetCaster())
                 pCaster->CastSpell(pTarget, PALADIN_SPELL_BLESSING_OF_SANCTUARY_BUFF, true);
+            pTarget->CastSpell(pTarget, PALADIN_SPELL_BLESSING_OF_SANCTUARY_HELPER, true);
         }
 
         void HandleEffectRemove(AuraEffect const * /*aurEff*/, AuraEffectHandleModes /*mode*/)
         {
             Unit* pTarget = GetTarget();
             pTarget->RemoveAura(PALADIN_SPELL_BLESSING_OF_SANCTUARY_BUFF, GetCasterGUID());
+            pTarget->RemoveAura(PALADIN_SPELL_BLESSING_OF_SANCTUARY_HELPER);
         }
 
         void Register()
