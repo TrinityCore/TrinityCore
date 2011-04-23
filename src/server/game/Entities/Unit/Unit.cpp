e14678be45776ccc768ce7310079fd2bfe24d667
Shauren <shauren.trinity@gmail.com>
2011-04-23 10:24:44 +0200

    Core/Units: Made RemoveAllAurasExceptVehicle more generic, can now be used to remove all auras except specified type

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -4010,12 +4010,12 @@ void Unit::RemoveAllAurasRequiringDeadTarget()
     }
 }
 
-void Unit::RemoveAllAurasExceptVehicle()
+void Unit::RemoveAllAurasExceptType(AuraType type)
 {
     for (AuraApplicationMap::iterator iter = m_appliedAuras.begin(); iter != m_appliedAuras.end();)
     {
         Aura const* aura = iter->second->GetBase();
-        if (!IsSpellHaveAura(aura->GetSpellProto(), SPELL_AURA_CONTROL_VEHICLE))
+        if (!IsSpellHaveAura(aura->GetSpellProto(), type))
             _UnapplyAura(iter, AURA_REMOVE_BY_DEFAULT);
         else
             ++iter;
@@ -4024,7 +4024,7 @@ void Unit::RemoveAllAurasExceptVehicle()
     for (AuraMap::iterator iter = m_ownedAuras.begin(); iter != m_ownedAuras.end();)
     {
         Aura* aura = iter->second;
-        if (!IsSpellHaveAura(aura->GetSpellProto(), SPELL_AURA_CONTROL_VEHICLE))
+        if (!IsSpellHaveAura(aura->GetSpellProto(), type))
             RemoveOwnedAura(iter, AURA_REMOVE_BY_DEFAULT);
         else
             ++iter;
