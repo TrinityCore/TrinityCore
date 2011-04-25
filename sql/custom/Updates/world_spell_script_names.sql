bf83c575734760768335073b9376259936c732c0
Vlad <vlad852@gmail.com>
2011-04-25 23:08:34 +0300

    Revert "Fix Hard Boiled achievement" In off repo exist solution.
    This reverts commit ab9e33eb23ba6de77869fc2027c493d45edd2a21.

--- sql/custom/Updates/world_spell_script_names.sql
+++ sql/custom/Updates/world_spell_script_names.sql
@@ -1,10 +1,9 @@
-DELETE FROM `spell_script_names` WHERE `spell_id` IN ('16870', '19752', '53487', '54015', '61716', '62544', '62575', '62863', '62960', '69672');
+DELETE FROM `spell_script_names` WHERE `spell_id` IN ('16870', '19752', '53487', '54015', '62544', '62575', '62863', '62960', '69672');
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 ('16870', 'spell_clearcasting'),
 ('19752', 'spell_divine_intervention'),
 ('53487', 'spell_oracle_wolvar'),
 ('54015', 'spell_oracle_wolvar'),
-('61716', 'spell_item_rabbitcostume'),
 ('62544', 'spell_tournament_melee'),
 ('62575', 'spell_tournament_shield'),
 ('62863', 'spell_tournament_duel'),
