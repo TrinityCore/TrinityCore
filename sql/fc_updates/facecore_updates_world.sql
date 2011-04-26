b8b0c821fbe14f300c8f0a15807acabfe5782b04
Vlad <vlad852@gmail.com>
2011-04-26 11:20:59 +0200

    Achievements/Noblegarden: lost sql

--- sql/fc_updates/facecore_updates_world.sql
+++ sql/fc_updates/facecore_updates_world.sql
@@ -480,6 +480,7 @@ INSERT INTO `spell_proc_event` VALUES
 
 -- Fix Spring Fling achievement
 UPDATE `creature_template` SET `ScriptName` = 'npc_spring_rabbit' WHERE `entry` = 32791;
+UPDATE achievement_criteria_data SET value1='186' WHERE (criteria_id='9199') AND (type='6');
 
 -- Fix for Dual Specialisation learning
 DELETE FROM `gossip_scripts` WHERE id=50099;
