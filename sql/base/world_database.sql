aabf4ece49d5e52be7f78ebaf34650a08f180b03
Shocker <shocker@freakz.ro>
2011-05-04 08:53:45 +0300

    Core/Spells: Glyph of Shadowflame effect should be removed when Shadowflame is removed
    Closes #1359

--- sql/base/world_database.sql
+++ sql/base/world_database.sql
@@ -18095,6 +18095,8 @@ INSERT INTO `spell_linked_spell` (`spell_trigger`,`spell_effect`,`type`,`comment
 ( 20066,-61840, 0, 'Repentance'),
 ( 66235, 66233, 0, 'Ardent Defender Visuals'),
 ( 58875, 58876, 1, 'Spirit Walk'),
+(-47960,-63311, 0, 'Glyph of Shadowflame Rank 1'),
+(-61291,-63311, 0, 'Glyph of Shadowflame Rank 2'),
 -- Misc
 ( 55428, 55475, 0, 'Lifeblood (Rank 1)'),
 ( 55480, 55475, 0, 'Lifeblood (Rank 2)'),
