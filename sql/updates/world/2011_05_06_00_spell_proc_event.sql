89fe89e41eaec04031a55acf9009cbdc4ae34527
Shauren <shauren.trinity@gmail.com>
2011-05-06 22:20:08 +0200

    SQL: Corrected file naming

--- sql/updates/world/2011_05_06_00_spell_proc_event.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM `spell_proc_event` WHERE `entry` = 70844;
-INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
-(70844, 0, 4, 256, 0, 0, 0, 0, 0, 0, 0);
