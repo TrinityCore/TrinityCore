5bb0b2557731e40d982994a602971dcfecc81986
Supabad <supabad.trinity@gmail.com>
2011-05-08 18:09:40 +0200

    DB/SQL: fix typo in previous commit

--- sql/updates/world/2011_05_08_01_spell_script_names.sql
+++ sql/updates/world/2011_05_08_01_spell_script_names.sql
@@ -1,5 +1,5 @@
-DELETE `FROM spell_script_names` WHERE `spell_id` IN (63489,62274);
-INSERT `INTO spell_script_names`
-VALUES 
-(63489,'spell_shield_of_runes'),
-(62274,'spell_shield_of_runes');
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (63489,62274);
+INSERT INTO `spell_script_names` 
+VALUES
+(63489, 'spell_shield_of_runes'),
+(62274, 'spell_shield_of_runes');
