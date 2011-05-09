25bed2a4e139c2ac0ac8a08f544f35c51fff59c6
click <clickvd@gonnamakeyou.com>
2011-05-09 03:37:31 +0200

    Core: Clean up some more silly warnings

--- src/server/game/AI/ScriptedAI/ScriptedCreature.cpp
+++ src/server/game/AI/ScriptedAI/ScriptedCreature.cpp
@@ -111,8 +111,8 @@ bool SummonList::HasEntry(uint32 entry)
 ScriptedAI::ScriptedAI(Creature* pCreature) : CreatureAI(pCreature),
     me(pCreature),
     IsFleeing(false),
-    _isCombatMovementAllowed(true),
-    _evadeCheckCooldown(2500)
+    _evadeCheckCooldown(2500),
+    _isCombatMovementAllowed(true)
 {
     _isHeroic = me->GetMap()->IsHeroic();
     _difficulty = Difficulty(me->GetMap()->GetSpawnMode());
@@ -505,9 +505,11 @@ void Scripted_NoMovementAI::AttackStart(Unit* target)
         DoStartNoMovement(target);
 }
 
-BossAI::BossAI(Creature* creature, uint32 bossId) : ScriptedAI(creature)
-, _bossId(bossId), summons(creature), instance(creature->GetInstanceScript())
-, _boundary(instance ? instance->GetBossBoundary(bossId) : NULL)
+BossAI::BossAI(Creature* creature, uint32 bossId) : ScriptedAI(creature),
+    instance(creature->GetInstanceScript()),
+    summons(creature),
+    _boundary(instance ? instance->GetBossBoundary(bossId) : NULL),
+    _bossId(bossId)
 {
 }
 
