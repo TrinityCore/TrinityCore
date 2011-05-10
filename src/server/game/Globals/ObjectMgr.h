ded32f97e0fd7081f724cc617629f412e161f1ad
tobmaps <spambot42@yandex.ru>
2011-05-10 18:52:28 +0700

    Core/Player: Fix old issue with creating a character if using declined names. Thx to someone, whose name was lost on old forums

--- src/server/game/Globals/ObjectMgr.h
+++ src/server/game/Globals/ObjectMgr.h
@@ -1135,7 +1135,7 @@ class ObjectMgr
         static PetNameInvalidReason CheckPetName(const std::string& name);
         static bool IsValidCharterName(const std::string& name);
 
-        static bool CheckDeclinedNames(std::wstring mainpart, DeclinedName const& names);
+        static bool CheckDeclinedNames(std::wstring w_ownname, DeclinedName const& names);
 
         GameTele const* GetGameTele(uint32 id) const
         {
