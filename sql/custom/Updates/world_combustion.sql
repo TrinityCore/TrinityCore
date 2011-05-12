8230393e5ce13b668094cd23029bab04e4f505a2
Vlad <vlad852@gmail.com>
2011-05-12 16:00:23 +0300

    Remove world_combustion.sql update. Already in off repo.

--- sql/custom/Updates/world_combustion.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = '-11129';
-INSERT INTO `spell_linked_spell` VALUES ('-11129', '-28682', '0', 'Combustion');
-
-UPDATE `spell_proc_event` SET `SpellFamilyMask1` = `SpellFamilyMask1` &~ 0x8 WHERE `entry` = '11129';
