2d6912cd2e7ea19a781cc0cd8e4f68fda119c5ba
tobmaps <spambot42@yandex.ru>
2011-05-13 19:43:32 +0700

    Core/Auras: Properly fix SPELL_AURA_DAMAGE_SHIELD

---
+++ sql/updates/world/2011_05_13_01_world_spell_bonus_data.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_bonus_data` WHERE `entry` IN (467,7294);
+INSERT INTO `spell_bonus_data` VALUES
+(467,0.033,-1,-1,-1,'Druid - Thorns'),
+(7294,0.033,-1,-1,-1,'Paladin - Retribution Aura');
