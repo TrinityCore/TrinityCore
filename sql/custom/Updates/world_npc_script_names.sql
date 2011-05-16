0d9ec615975bef0b969eb1b48c260abcd2eb8f4f
Vlad <vlad852@gmail.com>
2011-05-17 00:06:22 +0300

    Fix quest A Blade Fit For A Champion.

--- sql/custom/Updates/world_npc_script_names.sql
+++ sql/custom/Updates/world_npc_script_names.sql
@@ -12,3 +12,9 @@ UPDATE `creature_template` SET `flags_extra` = 0, `AIName` = '', `ScriptName` = 
 
 -- npc_spring_rabbit for achievement Spring Fling
 UPDATE `creature_template` SET `ScriptName` = 'npc_spring_rabbit' WHERE `entry` = 32791;
+
+-- npc_lake_frog
+UPDATE `creature_template` SET `ScriptName` = 'npc_lake_frog' WHERE `entry` IN (33211,33224);
+UPDATE `creature_template` SET `gossip_menu_id` = 33220 WHERE `entry` = 33220;
+REPLACE INTO `gossip_menu_option` (`menu_id`, `option_text`, `option_id`, `npc_option_npcflag`, `action_script_id`) VALUES ('33220', 'Do you know, where I can find Ashwood Brand Sword?', '1', '1', '33220');
+REPLACE INTO `gossip_scripts` (`id`, `command`, `datalong`, `datalong2`) VALUES ('33220', '15', '62554', '3');
