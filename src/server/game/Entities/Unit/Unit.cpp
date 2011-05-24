db38dccb592fe1d9139ad81ba6668b62dbab4d80
gmlt.A <gmlta@yandex.ru>
2011-05-24 07:14:25 +0200

    Spells/Warrior: Implement Warrior T10 Melee 4P Bonus. Tx Filipoliko

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -8959,6 +8959,22 @@ bool Unit::HandleProcTriggerSpell(Unit *pVictim, uint32 damage, AuraEffect* trig
                 }
             break;
         }
+        case 46916:  // Slam!
+        case 52437:  // Sudden Death
+        {
+            // Item - Warrior T10 Melee 4P Bonus
+            if (AuraEffect const * aurEff = GetAuraEffect(70847, EFFECT_0))
+            {
+                uint32 amount = aurEff->GetAmount();
+                if (roll_chance_i(amount))
+                    CastSpell(this, 70849, true, castItem, triggeredByAura); // Extra Charge!
+                if (roll_chance_i(amount))
+                    CastSpell(this, 71072, true, castItem, triggeredByAura); // Slam GCD Reduced
+                if (roll_chance_i(amount))
+                    CastSpell(this, 71069, true, castItem, triggeredByAura); // Execute GCD Reduced
+            }
+            break;
+        }
         // Sword and Board
         case 50227:
         {
