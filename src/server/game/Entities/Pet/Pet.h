d7653c605181278d20d727c208b54726e6540b39
tobmaps <spambot42@yandex.ru>
2011-05-31 05:47:17 +0700

    Core/Players: Pet talents can be reset at a pet trainer for free, original idea by rsa

--- src/server/game/Entities/Pet/Pet.h
+++ src/server/game/Entities/Pet/Pet.h
@@ -208,17 +208,14 @@ class Pet : public Guardian
 
         void InitPetCreateSpells();
 
-        bool resetTalents(bool no_cost = false);
+        bool resetTalents();
         static void resetTalentsForAllPetsOf(Player* owner, Pet* online_pet = NULL);
-        uint32 resetTalentsCost() const;
         void InitTalentForLevel();
 
         uint8 GetMaxTalentPointsForLevel(uint8 level);
         uint8 GetFreeTalentPoints() { return GetByteValue(UNIT_FIELD_BYTES_1, 1); }
         void SetFreeTalentPoints(uint8 points) { SetByteValue(UNIT_FIELD_BYTES_1, 1, points); }
 
-        uint32  m_resetTalentsCost;
-        time_t  m_resetTalentsTime;
         uint32  m_usedTalentCount;
 
         const uint64& GetAuraUpdateMaskForRaid() const { return m_auraRaidUpdateMask; }
