101cce4bafdb02b77f5f407f74603ba73fe1036b
gmltA <gmltt@yandex.ru>
2011-05-30 06:31:47 -0700

    Typo in comment

--- src/server/game/Spells/Spell.h
+++ src/server/game/Spells/Spell.h
@@ -609,7 +609,7 @@ class Spell
         GameObject* focusObject;
 
         // Damage and healing in effects need just calculate
-        int32 m_damage;           // Damge        in effects count here
+        int32 m_damage;           // Damage       in effects count here
         int32 m_healing;          // Healing      in effects count here
         int32 m_final_damage;     // Final damage in effects count here
 
