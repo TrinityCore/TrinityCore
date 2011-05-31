55f3bfa25a03f8dceeedde3211b8e1efcee74885
tobmaps <spambot42@yandex.ru>
2011-06-01 01:58:32 +0700

    Core/Spells: Drop dead code and fix some SQL updates

--- sql/updates/world/2011_05_31_00_world_spell_script_names.sql
+++ sql/updates/world/2011_05_31_00_world_spell_script_names.sql
@@ -1,1 +1,2 @@
-INSERT INTO `spell_script_names`(`spell_id`, `ScriptName`) VALUES ('24778', 'spell_dream_fog_sleep');
+DELETE FROM `spell_script_names` WHERE `spell_id` = 24778;
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES (24778, 'spell_dream_fog_sleep');
