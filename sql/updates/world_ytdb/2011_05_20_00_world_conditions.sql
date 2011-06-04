60a7919496f3e806b3106ff248331098d9d52a44
SignFinder <signfinder@gmail.com>
2011-06-04 13:15:50 +0200

    DB/YTDB: Add needed updates for YTDB revision 600

--- sql/updates/world_ytdb/2011_05_20_00_world_conditions.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=18 AND `SourceEntry`=23417;
-INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceGroup`,`SourceEntry`,`ElseGroup`,`ConditionTypeOrReference`,`ConditionValue1`,`ConditionValue2`,`ConditionValue3`,`ErrorTextId`,`ScriptName`,`Comment`) VALUES
-(18,0,23417,0,24,1,16975,0,0, '', 'Sanctified Crystal - Uncontrolled Voidwalker target');
--- sql/updates/world_ytdb/2011_05_22_03_world_gameobject_template.sql
+++
@@ -1,1 +0,0 @@
-UPDATE `gameobject_template` SET `flags`=`flags`|32,`faction`=35 WHERE `entry`=195527; -- Argent Coliseum Floor
