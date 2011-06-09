15160dfb19ba77b75c6a52f053e64edf3c9ce73f
SignFinder <signfinder@gmail.com>
2011-06-09 08:56:07 +0200

    DB/updates: small sql reorganisation ans visible fix of Keldelar NPC

--- sql/updates/fc_updates/oldbases_fix_characters.sql
+++
@@ -1,6 +0,0 @@
--- fix 1 for old created bases 
-ALTER TABLE `characters` DROP COLUMN `latency`;
-ALTER TABLE `characters` ADD COLUMN `latency` mediumint(8) unsigned NOT NULL DEFAULT '0' AFTER `power7`;
-
--- fix 2 for old created bases 
-ALTER TABLE `character_pet` DROP COLUMN `talentpoints`;
