93c2e71d8d7cdd71f3a4a21f996df955710d847b
SignFinder <signfinder@gmail.com>
2011-06-16 14:40:31 +0200

    DB/YTDB: Add needed updates for YTDB revision 602

--- sql/updates/world_ytdb/2011_06_01_02_world_gameobject_template.sql
+++
@@ -1,9 +0,0 @@
--- Set proper flags for Flame of Imbel, Flame of Lahassa, Flame of Samha and Flame of Byltan
-UPDATE `gameobject_template` SET `flags`=`flags`&~4 WHERE `entry` IN (142185,142187,142186,142188);
--- Set loot template for the gameobjects above
-DELETE FROM `gameobject_loot_template` WHERE `entry` IN (6711,6709,6708,6710);
-INSERT INTO `gameobject_loot_template` (`entry`,`item`,`ChanceOrQuestChance`,`lootmode`,`groupid`,`mincountOrRef`,`maxcount`) VALUES
-(6711,9258,100,1,0,1,1),
-(6709,9256,100,1,0,1,1),
-(6708,9255,100,1,0,1,1),
-(6710,9257,100,1,0,1,1);
--- sql/updates/world_ytdb/2011_06_02_00_world_gameobject_template.sql
+++
@@ -1,2 +0,0 @@
--- Set sniffed flag for Bristlelimb Cage
-UPDATE `gameobject_template` SET `flags`=32 WHERE `entry`=181714;
