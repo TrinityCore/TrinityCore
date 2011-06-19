36d44b15b552b216842482410adea876241bc5ca
Joeri Thissen <msn@chocochaos.com>
2011-06-19 14:35:41 +0200

    Switches around orientation and map so that map is now the 4th and orientation the 5th argument.
    .go xyz #x #y [#z [#mapid [#orientation]]]

--- sql/updates/world/2011_06_14_00_world_command.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `command` WHERE `name` = "go xy";
-UPDATE `command` SET `help` = "Syntax: .go xyz #x #y [#z [#orientation [#mapid]]]
-
-Teleport player to point with (#x,#y,#z) coordinates with orientation #orientation at map #mapid. If z is not provided, ground/water level will be used. If #orientation is not provided, the current orientation will be used. If mapid is not provided, the current map will be used." WHERE `name` = "go xyz";
