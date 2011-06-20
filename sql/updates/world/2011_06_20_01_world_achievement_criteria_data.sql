2df209eb79747919f51df1132b1f4faffbfee2e7
Lopin <davca.hr@seznam.cz>
2011-06-20 17:47:20 +0200

    DB/SQL: Better queries deletion, thanks Dasio for pointing it out

--- sql/updates/world/2011_06_20_01_world_achievement_criteria_data.sql
+++ sql/updates/world/2011_06_20_01_world_achievement_criteria_data.sql
@@ -1,4 +1,4 @@
-DELETE FROM `disables` WHERE `entry`=7598;
+DELETE FROM `disables` WHERE `entry`=7598 AND `sourceType`=4;
 DELETE FROM `achievement_criteria_data` WHERE `criteria_id`=7598 AND `type`=11;
 INSERT INTO `achievement_criteria_data` (`criteria_id`,`type`,`value1`,`value2`,`ScriptName`)
 VALUES
--- sql/updates/world/2011_06_20_04_world_achievement_criteria_data.sql
+++ sql/updates/world/2011_06_20_04_world_achievement_criteria_data.sql
@@ -1,4 +1,4 @@
-DELETE FROM `disables` WHERE `entry` IN (7264,7548);
+DELETE FROM `disables` WHERE `entry` IN (7264,7548) AND `sourceType`=4;
 DELETE FROM `achievement_criteria_data` WHERE `criteria_id` IN (7264,7548) AND `type`=11;
 INSERT INTO `achievement_criteria_data` (`criteria_id`,`type`,`value1`,`value2`,`ScriptName`)
 VALUES
