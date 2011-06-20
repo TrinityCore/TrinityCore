030efd5af8b12a841f69cdab7082b17f5bfbf8be
Lopin <davca.hr@seznam.cz>
2011-06-20 10:21:50 +0200

    Core/Achievements: Fixed achievement King's Bane in King Ymiron's encounter

---
+++ sql/updates/world/2011_06_20_01_world_achievement_criteria_data.sql
@@ -0,0 +1,5 @@
+DELETE FROM `disables` WHERE `entry`=7598;
+DELETE FROM `achievement_criteria_data` WHERE `criteria_id`=7598 AND `type`=11;
+INSERT INTO `achievement_criteria_data` (`criteria_id`,`type`,`value1`,`value2`,`ScriptName`)
+VALUES
+(7598,11,0,0,'achievement_kings_bane');
