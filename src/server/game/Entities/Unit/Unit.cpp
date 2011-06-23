41b02b86d9708145b1e97787a0ec7054217ae083
tobmaps <spambot42@yandex.ru>
2011-06-24 03:18:06 +0700

    Core/Spells: Allow direct damage effect of Seal of Vengeance and Seal of Corruption to proc from Shield of Righteousness

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -6808,7 +6808,8 @@ bool Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, AuraEffect* trigger
 
                     // At melee attack or Hammer of the Righteous spell damage considered as melee attack
                     bool stacker = !procSpell || procSpell->Id == 53595;
-                    bool damager = procSpell && procSpell->EquippedItemClass == ITEM_CLASS_WEAPON;
+                    // spells with SPELL_DAMAGE_CLASS_MELEE excluding Judgements
+                    bool damager = procSpell && procSpell->EquippedItemClass != -1;
 
                     if (!stacker && !damager)
                         return false;
@@ -6839,7 +6840,8 @@ bool Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, AuraEffect* trigger
 
                     // At melee attack or Hammer of the Righteous spell damage considered as melee attack
                     bool stacker = !procSpell || procSpell->Id == 53595;
-                    bool damager = procSpell && procSpell->EquippedItemClass == ITEM_CLASS_WEAPON;
+                    // spells with SPELL_DAMAGE_CLASS_MELEE excluding Judgements
+                    bool damager = procSpell && procSpell->EquippedItemClass != -1;
 
                     if (!stacker && !damager)
                         return false;
