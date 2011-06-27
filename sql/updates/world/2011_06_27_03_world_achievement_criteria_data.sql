cdcf5be8d1c7edb41f01a91a61e5f3ade57fc20a
Lopin <davca.hr@seznam.cz>
2011-06-27 18:50:43 +0200

    DB/SQL: Fixed import of SQLs after recent commits

--- sql/updates/world/2011_06_27_03_world_achievement_criteria_data.sql
+++ sql/updates/world/2011_06_27_03_world_achievement_criteria_data.sql
@@ -1,4 +1,4 @@
-DELETE FROM `achievement_criteria_data` WHERE `criteria_id` IN (10447,10448,10449,10459,10460,10461) AND `type` IN (11,18);
+DELETE FROM `achievement_criteria_data` WHERE `criteria_id` IN (10447,10448,10449,10459,10460,10461) AND `type` IN (11,12,18);
 INSERT INTO `achievement_criteria_data` (`criteria_id`,`type`,`value1`,`value2`,`ScriptName`)
 VALUES
 (10447,11,0,0,'achievement_knock_on_wood'),
