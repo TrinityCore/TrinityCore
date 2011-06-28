e1f75b22657d8cc8e4fca1379b668a97d9b1a620
locknes <locknes91@windowslive.com>
2011-06-28 15:48:04 +0200

    Fix Sindragosa trash damage

---
+++ sql/ChaosCore/v4_world_Frostwing Whelp_damage.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_template` SET `mindmg`=391, `maxdmg`=585, `attackpower`=146, `dmg_multiplier`=10.8, `minrangedmg`=313, `maxrangedmg`=468, `rangedattackpower`=117 WHERE `entry`=37532;
+UPDATE `creature_template` SET `mindmg`=391, `maxdmg`=585, `attackpower`=146, `dmg_multiplier`=16.1, `minrangedmg`=313, `maxrangedmg`=468, `rangedattackpower`=117 WHERE `entry`=38151;
