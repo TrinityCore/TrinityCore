bdba5cd900435888afc3b276ad4e0f6d1419e5b1
Shocker <shocker@freakz.ro>
2011-06-29 16:36:54 +0300

    Core/Commands: Move .gps to command scripts and add .wpgps command, used for generating partial SQL queries for pathing

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -59,6 +59,7 @@ void AddSC_reload_commandscript();
 void AddSC_tele_commandscript();
 void AddSC_titles_commandscript();
 void AddSC_wp_commandscript();
+void AddSC_gps_commandscript();
 
 #ifdef SCRIPTS
 //world
@@ -658,6 +659,7 @@ void AddCommandScripts()
     AddSC_tele_commandscript();
     AddSC_titles_commandscript();
     AddSC_wp_commandscript();
+    AddSC_gps_commandscript();
 }
 
 void AddWorldScripts()
