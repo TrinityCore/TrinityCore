7cad3050e83b2272f18c635f235102f876af8796
locknes <locknes91@windowslive.com>
2011-07-02 14:40:06 +0200

    Mechanic imune update for Val'kyr thx to paff close #18

--- sql/ChaosCore/v4_world_icc_valkyre.sql
+++ sql/ChaosCore/v4_world_icc_valkyre.sql
@@ -1,1 +1,3 @@
 UPDATE creature_template SET unit_flags=512 WHERE entry IN (36609,39120,39121,39122);
+UPDATE creature_template SET mechanic_immune_mask=2 WHERE entry IN (36609,39120,39121,39122);
+UPDATE creature_template SET speed_walk=1.2 WHERE entry IN (36609,39120,39121,39122);
