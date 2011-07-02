c2b63cac6e5d852f8a9cecb85f87b284b45a626a
locknes <locknes91@windowslive.com>
2011-07-02 14:44:18 +0200

    Add missing spawn thx to paff close #21

---
+++ sql/ChaosCore/v4_world_spawns.sql
@@ -0,0 +1,14 @@
+INSERT INTO gameobject (guid, id, map, spawnMask, phaseMask, position_x, position_y, position_z, orientation, rotation0, rotation1, rotation2, rotation3, spawntimesecs, animprogress, state) VALUES
+(0, 154357, 0, 1, 1, -9357.31, -2235.44, 45.8161, 3.58651, 0, 0, 0.975358, -0.22063, 300, 0, 1),
+(0, 154357, 0, 1, 1, -9366.56, -2093.91, 45.2099, 2.51444, 0, 0, 0.951236, 0.308463, 300, 0, 1),
+(0, 154357, 0, 1, 1, -9345.06, -2048.99, 40.6823, 1.27351, 0, 0, 0.594589, 0.80403, 300, 0, 1),
+(0, 154357, 0, 1, 1, -9357.07, -2123.68, 43.5186, 4.62324, 0, 0, 0.737915, -0.674894, 300, 0, 1);
+
+INSERT INTO creature (guid, id, map, spawnMask, phaseMask, modelid, equipment_id, position_x, position_y, position_z, orientation, spawntimesecs, spawndist, currentwaypoint, curhealth, curmana, MovementType, npcflag, unit_flags, dynamicflags) VALUES
+(0, 16817, 0, 1, 1, 0, 0, -8831.18, 859.479, 98.9684, 5.92762, 300, 0, 0, 3052, 0, 0, 0, 0, 0);
+
+INSERT INTO creature (guid, id, map, spawnMask, phaseMask, modelid, equipment_id, position_x, position_y, position_z, orientation, spawntimesecs, spawndist, currentwaypoint, curhealth, curmana, MovementType, npcflag, unit_flags, dynamicflags) VALUES
+(0, 16817, 0, 1, 1, 0, 0, -8831.18, 859.479, 98.9684, 5.92762, 300, 0, 0, 3052, 0, 0, 0, 0, 0);
+
+INSERT INTO creature (guid, id, map, spawnMask, phaseMask, modelid, equipment_id, position_x, position_y, position_z, orientation, spawntimesecs, spawndist, currentwaypoint, curhealth, curmana, MovementType, npcflag, unit_flags, dynamicflags) VALUES
+(0, 15402, 530, 1, 1, 0, 463, 8719.32, -7174.39, 45.7225, 1.49643, 300, 0, 0, 328, 350, 0, 0, 0, 0);
