acd00ca4e29a5cd41b5a1b6eb002a1ef96b6e7c9
Ruben Motta Mussi de Souza <rubenmussi@hotmail.com>
2011-07-03 16:02:43 -0300

    -- Added proper management to ulduar loot changes, instead of many files for each keeper we will use now only one file.
    -- Fix a mistake on thorim cache enum (source: sniff)

--- sql/ChaosCore/v4_world_ulduar_loot.sql
+++ sql/ChaosCore/v4_world_ulduar_loot.sql
@@ -1,7 +1,67 @@
--- Freya
--- TODO: Fix the loot on remaining caches
--- Add Missing GO's templates for chests
+-- -------------------
+-- ----ULDUAR LOOTS---
+-- -------------------
 
+
+-- -------------------------------
+-- ----------Thorim loot 25m------
+-- --------------------------------
+-- TODO: Fix the loot on remaining caches
+DELETE FROM reference_loot_template WHERE `entry` =  26956; -- Hardmode + normal itens used for reference
+INSERT INTO reference_loot_template VALUES
+(26956, 45463, 0, 1, 1, 1, 1),
+(26956, 45466, 0, 1, 1, 1, 1),
+(26956, 45467, 0, 1, 1, 1, 1),
+(26956, 45468, 0, 1, 1, 1, 1),
+(26956, 45469, 0, 1, 1, 1, 1),
+(26956, 45470, 0, 1, 2, 1, 1),
+(26956, 45471, 0, 1, 2, 1, 1),
+(26956, 45472, 0, 1, 2, 1, 1),
+(26956, 45473, 0, 1, 2, 1, 1),
+(26956, 45474, 0, 1, 2, 1, 1),
+(26956, 45570, 0, 1, 2, 1, 1);
+DELETE FROM `gameobject_loot_template` WHERE `entry` = 26955; -- Chest 25 man normal mode
+INSERT INTO `gameobject_loot_template` VALUES 
+(26955, 3, 10, 1, 0, -34154, 1), -- Random Recipes
+(26955, 45038, 18, 1, 0, 1, 1), -- Fragment of Val'anyr
+(26955, 45087, 75, 1, 0, 1, 1), -- Runed Orb
+(26955, 1, 100, 1, 1, -26956, 3), -- 3 Normal itens
+(26955, 45638, 0, 1, 3, 1, 1), -- T8
+(26955, 45639, 0, 1, 3, 1, 1), -- T8
+(26955, 45640, 0, 1, 3, 1, 1), -- T8
+(26955, 47241, 100, 1, 0, 1, 1); -- Emblem of triumph x1
+UPDATE `gameobject_template` SET `data1` = 26955 WHERE `entry` = 194314;
+-- Hard Mode
+DELETE FROM `gameobject_loot_template` WHERE `entry` = 40000; -- Chest 25 man Hard mode
+INSERT INTO `gameobject_loot_template` VALUES 
+(40000, 3, 10, 1, 0, -34154, 1), -- Random Recipes
+(40000, 45038, 100, 1, 0, 1, 1), -- Fragment of Val'anyr (100% drop chance on hardmode to incentivate people to run ulduar :D)
+(40000, 45087, 75, 1, 0, 1, 1), -- Runed orb
+(40000, 45638, 0, 1, 3, 1, 1), -- T8
+(40000, 45639, 0, 1, 3, 1, 1), -- T8
+(40000, 45640, 0, 1, 3, 1, 1), -- T8
+(40000, 1, 100, 1, 1, -26956, 3), -- 3 Normal itens
+(40000, 2, 100, 1, 2, -26956, 3), -- 3 Hard Mode itens
+(40000, 45817, -100, 1, 0, 1, 1), -- Sigil Thorim
+(40000, 47241, 100, 1, 0, 1, 1); -- Emblem of Triumph x1
+UPDATE `gameobject_template` SET `data1` = 40000 WHERE `entry` = 194315;
+
+-- -------------------------------
+-- ------------HODIR--------------
+-- -------------------------------
+-- Hodir rare chest Spawn
+DELETE FROM `gameobject` WHERE `id`=194200;
+INSERT INTO `gameobject` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`position_x`,`position_y`,`position_z`,`orientation`,`rotation0`,`rotation1`,`rotation2`,`rotation3`,`spawntimesecs`,`animprogress`,`state`) VALUES
+(NULL, 194200, 603, 3, 1, 2029.19, -192.403, 432.687, 4.44005, 0, 0, 0.79655, -0.604572, -604800, 255, 1);
+DELETE FROM `gameobject` WHERE `id`=194201;
+INSERT INTO `gameobject` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`position_x`,`position_y`,`position_z`,`orientation`,`rotation0`,`rotation1`,`rotation2`,`rotation3`,`spawntimesecs`,`animprogress`,`state`) VALUES
+(NULL, 194201, 603, 3, 1, 2029.19, -192.403, 432.687, 4.44005, 0, 0, 0.79655, -0.604572, -604800, 255, 1);
+
+
+-- -------------------------------
+-- -------------FREYA-------------
+-- -------------------------------
+-- Add Missing GO's templates for chests
 -- Cache with 2 elders alive in 10 man mode
 DELETE FROM gameobject_template WHERE `entry` = 194326;
 INSERT INTO gameobject_template (`entry`, `type`, `displayId`, `name`, `size`, `data0`, `data1`, `data3`, `data6`, `data11`, `data13`, `data15`, `WDBVerified`) VALUES
@@ -140,3 +200,31 @@ INSERT INTO gameobject_loot_template VALUES
 (26962, 45814, -100, 2, 0, 1, 1), -- Sigil Freya
 (26962, 47241, 100, 1, 0, 4, 4); -- Emblem of Triumph x4
 UPDATE gameobject_template SET `data1` = 26962 WHERE `entry` = 194331;
+
+-- ----------------------------
+-- ------UPDATES---------------
+-- ----------------------------
+-- Drop unused  reference loot rows
+DELETE FROM `reference_loot_template` WHERE `entry` IN (12020,12021,12022,34126,34127);
+
+-- Add/fix proper loot conditions to greymane's custom loot fixes
+UPDATE `conditions` SET `SourceGroup` = 27081 WHERE SourceEntry = 45788; -- Freya Sigil 10M
+UPDATE `conditions` SET `SourceGroup` = 27086 WHERE SourceEntry = 45787; -- Mimiron Sigil 10M
+UPDATE `conditions` SET `SourceGroup` = 27074 WHERE SourceEntry = 45784; -- Thorim Sigil 10M
+UPDATE `conditions` SET `SourceGroup` = 26962 WHERE SourceEntry = 45814; -- Freya Sigil 25M
+UPDATE `conditions` SET `SourceGroup` = 26967 WHERE SourceEntry = 45816; -- Mimiron Sigil 25M
+
+-- Fix proper lootmode on freya's sigil
+UPDATE `gameobject_loot_template` SET `lootmode` = 1 WHERE `item` IN (45788,45814) AND `entry` IN (27081,26962);
+-- Add Condition for hodirs sigil drop
+DELETE FROM `conditions` WHERE `SourceEntry` IN (45786,45815);
+INSERT INTO `conditions` VALUES
+(4,27069,45786,0,9,13609,0,0,0,'',NULL),
+(4,26950,45815,0,9,13822,0,0,0,'',NULL);
+-- Handle New Condition on thorim hasdmode chest, since i changed the chest on his script
+UPDATE `gameobject_template` SET `questItem1` = 0 WHERE `entry` = 194314;
+UPDATE `gameobject_template` SET `questitem1` = 45817 WHERE `entry` = 194315; 
+UPDATE `conditions` SET SourceGroup = 40000 WHERE SourceGroup = 26955;
+-- Delete unused reference loot for t8 tokens on thorim cache
+DELETE FROM `reference_loot_template` WHERE `entry` = 34181;
+
