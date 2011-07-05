36b11b8a7c1e09365813c89403ca7802f5c4a340
gmlt.A <gmlta@yandex.ru>
2011-07-05 15:42:19 +0300

    Restore old fixes
    	* Possible fix for Blood Queen's behavior while raid member has quest 24879
    	* Fix bug by commenting dangerous code

--- src/server/scripts/Northrend/IcecrownCitadel/boss_blood_prince_council.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_blood_prince_council.cpp
@@ -240,6 +240,8 @@ class boss_blood_council_controller : public CreatureScript
                     _invocationOrder[2] = InvocationData(instance->GetData64(DATA_PRINCE_TALDARAM_GUID), SPELL_INVOCATION_OF_BLOOD_TALDARAM, EMOTE_TALDARAM_INVOCATION, 71081);
                 }
 
+                /* Causes bug! This aura is applied automaticaly when enemy casts SPELL_SHADOW_PRISON => this code is useless
+                Dummy dissapears when enemy dies or will be removed in JustDied, but if enemy alive, spell will be on player even outside instance.
                 if (IsHeroic())
                 {
                     Map::PlayerList const &PlList = me->GetMap()->GetPlayers();
@@ -257,7 +259,7 @@ class boss_blood_council_controller : public CreatureScript
                                 player->AddAura(SPELL_SHADOW_PRISON_DUMMY, player);
                         }
                     }
-                }
+                }*/
             }
 
             void SetData(uint32 /*type*/, uint32 data)
