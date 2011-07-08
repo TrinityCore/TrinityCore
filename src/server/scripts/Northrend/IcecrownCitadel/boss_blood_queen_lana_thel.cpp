00e02f683c2ee93f443a56cb0d496728a004d2eb
Shauren <shauren.trinity@gmail.com>
2011-07-08 18:15:57 +0200

    Scripts/Icecrown Citadel
    * Lord Marrowgar's Coldflame will now spread slower
    * Lord Marrowgar will no longer attempt to target tanks with Bone Spike Graveyard
    * Valithria Dreamwalker will now properly cast Dreamwalker's Rage and kill enemy creatures
    * Blood-Queen Lana'Thel will now warn the player who is under effects of Frenzied Bloodthirst

--- src/server/scripts/Northrend/IcecrownCitadel/boss_blood_queen_lana_thel.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_blood_queen_lana_thel.cpp
@@ -587,6 +587,7 @@ class spell_blood_queen_frenzied_bloodthirst : public SpellScriptLoader
 
             void Register()
             {
+                OnEffectApply += AuraEffectApplyFn(spell_blood_queen_frenzied_bloodthirst_AuraScript::OnApply, EFFECT_0, SPELL_AURA_OVERRIDE_SPELLS, AURA_EFFECT_HANDLE_REAL);
                 AfterEffectRemove += AuraEffectRemoveFn(spell_blood_queen_frenzied_bloodthirst_AuraScript::OnRemove, EFFECT_0, SPELL_AURA_OVERRIDE_SPELLS, AURA_EFFECT_HANDLE_REAL);
             }
         };
