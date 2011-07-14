ebc1baf68424993267554c9c001169fcc0a0cdb2
DrTenma <dr.tenma@yahoo.com>
2011-07-15 04:58:14 +0700

    Core/Spells: Fix talent Improved Unholy Presence
    Closes #411

--- src/server/game/Spells/Auras/SpellAuras.cpp
+++ src/server/game/Spells/Auras/SpellAuras.cpp
@@ -1645,7 +1645,7 @@ void Aura::HandleAuraSpecificMods(AuraApplication const* aurApp, Unit* caster, b
                     }
                 }
 
-                uint32 presence=GetId();
+                uint32 presence = GetId();
                 if (apply)
                 {
                     // Blood Presence bonus
@@ -1653,7 +1653,7 @@ void Aura::HandleAuraSpecificMods(AuraApplication const* aurApp, Unit* caster, b
                         target->CastSpell(target, 63611, true);
                     else if (bloodPresenceAura)
                     {
-                        int32 basePoints1=bloodPresenceAura->GetAmount();
+                        int32 basePoints1 = bloodPresenceAura->GetAmount();
                         target->CastCustomSpell(target, 63611, NULL, &basePoints1, NULL, true, 0, bloodPresenceAura);
                     }
                     // Frost Presence bonus
@@ -1672,13 +1672,12 @@ void Aura::HandleAuraSpecificMods(AuraApplication const* aurApp, Unit* caster, b
                             // Not listed as any effect, only base points set
                             int32 basePoints0 = SpellMgr::CalculateSpellEffectAmount(unholyPresenceAura->GetSpellProto(), 1);
                             target->CastCustomSpell(target, 63622, &basePoints0 , &basePoints0, &basePoints0, true, 0, unholyPresenceAura);
-                            target->CastCustomSpell(target, 65095, &basePoints0 , NULL, NULL, true, 0, unholyPresenceAura);
                         }
                         target->CastSpell(target, 49772, true);
                     }
                     else if (unholyPresenceAura)
                     {
-                        int32 basePoints0=unholyPresenceAura->GetAmount();
+                        int32 basePoints0 = unholyPresenceAura->GetAmount();
                         target->CastCustomSpell(target, 49772, &basePoints0, NULL, NULL, true, 0, unholyPresenceAura);
                     }
                 }
@@ -1692,10 +1691,7 @@ void Aura::HandleAuraSpecificMods(AuraApplication const* aurApp, Unit* caster, b
                     if (presence == 48265 || unholyPresenceAura)
                     {
                         if (presence == 48265 && unholyPresenceAura)
-                        {
                             target->RemoveAurasDueToSpell(63622);
-                            target->RemoveAurasDueToSpell(65095);
-                        }
                         target->RemoveAurasDueToSpell(49772);
                     }
                 }
