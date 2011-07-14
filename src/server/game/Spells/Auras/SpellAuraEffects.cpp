8b1e0dd2e7c198acaa141281fa9adca535ef0ebc
DrTenma <dr.tenma@yahoo.com>
2011-07-15 04:58:14 +0700

    Core/Spells: Fix talent Improved Unholy Presence
    Closes #411

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -3897,6 +3897,8 @@ void AuraEffect::HandleModPowerRegen(AuraApplication const* aurApp, uint8 mode, 
     // Update manaregen value
     if (GetMiscValue() == POWER_MANA)
         target->ToPlayer()->UpdateManaRegen();
+    else if (GetMiscValue() == POWER_RUNE)
+        target->ToPlayer()->UpdateRuneRegen(RuneType(GetMiscValueB()));
     // other powers are not immediate effects - implemented in Player::Regenerate, Creature::Regenerate
 }
 
