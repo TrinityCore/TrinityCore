36c343e9e869fa934f53f35dbe72172144a968d9
locknes <locknes91@windowslive.com>
2011-07-19 19:29:41 +0200

    update EOE thx to zwerg

--- src/server/scripts/Northrend/Nexus/EyeOfEternity/eye_of_eternity.h
+++ src/server/scripts/Northrend/Nexus/EyeOfEternity/eye_of_eternity.h
@@ -20,53 +20,48 @@
 
 enum InstanceData
 {
-    DATA_MALYGOS_EVENT,
+    BOSS_MALYGOS,
     MAX_ENCOUNTER,
-
-    DATA_VORTEX_HANDLING,
-    DATA_POWER_SPARKS_HANDLING
 };
 
-enum InstanceData64
+enum Data64
 {
-    DATA_TRIGGER,
     DATA_MALYGOS,
     DATA_PLATFORM
 };
 
-enum InstanceNpcs
+enum Creatures
 {
-    NPC_MALYGOS             = 28859,
-    NPC_VORTEX_TRIGGER      = 30090,
-    NPC_PORTAL_TRIGGER      = 30118,
-    NPC_POWER_SPARK         = 30084,
-    NPC_HOVER_DISK_MELEE    = 30234,
-    NPC_HOVER_DISK_CASTER   = 30248,
-    NPC_ARCANE_OVERLOAD     = 30282,
-    NPC_WYRMREST_SKYTALON   = 30161,
-    NPC_ALEXSTRASZA         = 32295
+    NPC_MALYGOS                 = 28859,
+    NPC_VORTEX                  = 30090,
+    NPC_POWER_SPARK             = 30084,
+    NPC_ARCANE_OVERLOAD         = 30282,
+    NPC_SURGE_OF_POWER          = 30334,
+    NPC_NEXUS_LORD              = 30245,
+    NPC_SCION_OF_ETERNITY       = 30249,
+    NPC_STATIC_FIELD            = 30592,
+    NPC_WYRMREST_SKYTALON       = 30161,
+    NPC_ALEXSTRASZA             = 32295,
+    NPC_ALEXSTRASZAS_GIFT       = 32448,
+    NPC_HOVER_DISK_CASTER       = 30234,
+    NPC_HOVER_DISK_MELEE        = 30248
 };
 
-enum InstanceGameObjects
+enum GameObjects
 {
-    GO_NEXUS_RAID_PLATFORM      = 193070,
+    GO_PLATFORM                 = 193070,
     GO_EXIT_PORTAL              = 193908,
-    GO_FOCUSING_IRIS            = 193958,
-    GO_ALEXSTRASZA_S_GIFT       = 193905,
-    GO_ALEXSTRASZA_S_GIFT_2     = 193967
+    GO_FOCUSING_IRIS_10         = 193958,
+    GO_FOCUSING_IRIS_25         = 193960,
+    GO_ALEXSTRASZAS_GIFT_10     = 193905,
+    GO_ALEXSTRASZAS_GIFT_25     = 193967,
+    GO_HEART_OF_MAGIC_10        = 194158,
+    GO_HEART_OF_MAGIC_25        = 194159
 };
 
-enum InstanceEvents
+enum AchievementData
 {
-    EVENT_FOCUSING_IRIS = 20711
-};
-
-enum InstanceSpells
-{
-    SPELL_VORTEX_4          = 55853, // damage | used to enter to the vehicle
-    SPELL_VORTEX_5          = 56263, // damage | used to enter to the vehicle
-    SPELL_PORTAL_OPENED     = 61236,
-    SPELL_RIDE_RED_DRAGON   = 56071,
+    ACHIEV_TIMED_START_EVENT    = 20387
 };
 
 #endif
