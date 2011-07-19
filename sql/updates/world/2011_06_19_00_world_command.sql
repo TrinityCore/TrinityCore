1fb72cf8da882ea3141f033ff43ee116a266ae1e
Joeri Thissen <msn@chocochaos.com>
2011-07-19 13:43:35 +0200

    Perhaps a better way to commit sql changes.

--- sql/updates/world/2011_06_19_00_world_command.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `command` WHERE `name` = "go xy";
-UPDATE `command` SET `help` = "Syntax: .go xyz #x #y [#z [#mapid [#orientation]]]
-
-Teleport player to point with (#x,#y,#z) coordinates at map #mapid with orientation #orientation. If z is not provided, ground/water level will be used. If mapid is not provided, the current map will be used. If #orientation is not provided, the current orientation will be used." WHERE `name` = "go xyz";
