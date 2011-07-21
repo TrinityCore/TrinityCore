7c7a00b360c5b12bac599685611633f27a02f0cb
tobmaps <spambot42@yandex.ru>
2011-07-22 06:04:10 +0700

    Scripts/Trial of the Crusader
    * Use spells instead of scripts to handle summons used by Lord Jaraxxus
    * Fix stacks for spell Nether Power used by Lord Jaraxxus
    * Make some triggers invisible
    
    Closes #829

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -4272,6 +4272,16 @@ void SpellMgr::LoadSpellCustomAttr()
             spellInfo->AuraInterruptFlags = AURA_INTERRUPT_FLAG_HITBYSPELL | AURA_INTERRUPT_FLAG_TAKE_DAMAGE;
             ++count;
             break;
+        // TRIAL OF THE CRUSADER SPELLS
+        case 66258: // Infernal Eruption (10N)
+        case 67901: // Infernal Eruption (25N)
+            // increase duration from 15 to 18 seconds because caster is already
+            // unsummoned when spell missile hits the ground so nothing happen in result
+            spellInfo->DurationIndex = 85;
+            ++count;
+            break;
+        // ENDOF TRIAL OF THE CRUSADER SPELLS
+        //
         // ULDUAR SPELLS
         //
         case 63342: // Focused Eyebeam Summon Trigger (Kologarn)
