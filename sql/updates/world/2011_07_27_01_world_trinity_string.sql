68da510641c1b89ad5e11c566c53bbf0b71d9276
tobmaps <spambot42@yandex.ru>
2011-08-06 19:58:28 +0700

    SQL: Cleanup in SQL updates

--- sql/updates/world/2011_07_27_01_world_trinity_string.sql
+++ sql/updates/world/2011_07_27_01_world_trinity_string.sql
@@ -1,2 +1,3 @@
+DELETE FROM `trinity_string` WHERE `entry` = 283;
 INSERT INTO `trinity_string` (`entry`,`content_default`) VALUES 
 (283,'You have disabled %s\'s chat for %u minutes, effective at the player\'s next login. Reason: %s.');
