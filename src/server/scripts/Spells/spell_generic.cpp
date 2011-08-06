1024fe8ac77b29f1f0dd0c391c4621828129ae11
Machiavelli <machiavelli.trinity@gmail.com>
2011-08-06 19:39:45 +0200

    Core/Spells: Add GetTriggeringSpell() method to SpellScript and use it to properly filter server side spell 18350's script.

--- src/server/scripts/Spells/spell_generic.cpp
+++ src/server/scripts/Spells/spell_generic.cpp
@@ -1210,6 +1210,7 @@ class spell_gen_soul_preserver : public SpellScriptLoader
 {
     enum Spells
     {
+        SOUL_PRESERVER          = 60510,
         HEALING_TRANCE_DRUID    = 60512,
         HEALING_TRANCE_PALADIN  = 60513,
         HEALING_TRANCE_PRIEST   = 60514,
@@ -1222,6 +1223,13 @@ class spell_gen_soul_preserver : public SpellScriptLoader
         class spell_gen_soul_preserver_SpellScript : public SpellScript
         {
             PrepareSpellScript(spell_gen_soul_preserver_SpellScript);
+
+            bool Load()
+            {
+                // 18350 is a common placeholder for trigered spells
+                // this script only applies to 60510
+                return GetTriggeringSpell()->Id == SOUL_PRESERVER;
+            }
 
             void HandleScript(SpellEffIndex /*effIndex*/)
             {
