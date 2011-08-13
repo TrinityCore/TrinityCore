d3e05bc9b87e4049534b92d7bc6bcfca6a2fbdb5
hacknowledge <hacknowledge101@googlemail.com>
2011-08-13 23:58:33 +0200

    Core/Spells Disables environmental damage to spirit of redemption form of priest Core: Added check for AOE attackability to environmental damage Fixes #2531 Reduced check for environmental damage to whether unit is attackable by AOE. check for GM and death state is included in this check
    Fixes #2531

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -2753,6 +2753,8 @@ class Player : public Unit, public GridObject<Player>
         bool IsHasDelayedTeleport() const { return m_bHasDelayedTeleport; }
         void SetDelayedTeleportFlag(bool setting) { m_bHasDelayedTeleport = setting; }
 
+        bool IsImmuneToEnvironmentalDamage();
+
         void ScheduleDelayedOperation(uint32 operation)
         {
             if (operation < DELAYED_END)
