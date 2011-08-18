f67e4c5e9b85e2944f8ec2b41c2610430d4e75f3
Machiavelli <machiavelli.trinity@gmail.com>
2011-08-18 20:47:36 +0200

    Script/Spells: Fix spell 64414 / Load into catapult
    Also fix SQL file naming from this morning.

--- sql/updates/world/2011_08_18_02_world_achievement_criteria_data.sql
+++ sql/updates/world/2011_08_18_02_world_achievement_criteria_data.sql
@@ -1,7 +1,6 @@
-DELETE FROM `achievement_criteria_data` WHERE `criteria_id` IN (12740,12741,12744,12993) AND `type`=12;
+DELETE FROM `achievement_criteria_data` WHERE `criteria_id` IN (12747,12748,12749) AND `type`=12;
 INSERT INTO `achievement_criteria_data` (`criteria_id`,`type`,`value1`,`value2`,`ScriptName`)
 VALUES
-(12740,12,0,0,''),
-(12741,12,0,0,''),
-(12744,12,0,0,''),
-(12993,12,1,0,'');
+(12747,12,1,0,''),
+(12748,12,1,0,''),
+(12749,12,1,0,'');
