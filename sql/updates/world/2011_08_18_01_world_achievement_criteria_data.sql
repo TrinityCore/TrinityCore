957dccadb04ccbb3a67406dc7a04df5358efd809
horn <horn@trinitycore.contributor>
2011-08-18 22:42:32 +0700

    Scripts/The Pit of Saron: Prevent heroic final achievement gain in normal mode

---
+++ sql/updates/world/2011_08_18_01_world_achievement_criteria_data.sql
@@ -0,0 +1,6 @@
+DELETE FROM `achievement_criteria_data` WHERE `criteria_id` IN (12747,12748,12749) AND `type`=12;
+INSERT INTO `achievement_criteria_data` (`criteria_id`,`type`,`value1`,`value2`,`ScriptName`)
+VALUES
+(12747,12,1,0,''),
+(12748,12,1,0,''),
+(12749,12,1,0,'');
