78ebe93287ebd3b0aa41a770a7b3de02d0251f32
Lopin <davca.hr@seznam.cz>
2011-08-20 13:18:17 +0200

    Scripts/Naxxramas: Fixed reseting of Just Can't Get Enough achievement. Thanks to Shauren for notice

--- src/server/scripts/Northrend/Naxxramas/boss_kelthuzad.cpp
+++ src/server/scripts/Northrend/Naxxramas/boss_kelthuzad.cpp
@@ -269,8 +269,10 @@ public:
         boss_kelthuzadAI(Creature* c) : BossAI(c, BOSS_KELTHUZAD), spawns(c)
         {
             uiFaction = me->getFaction();
+            instance = me->GetInstanceScript();
         }
 
+        InstanceScript* instance;
         uint32 Phase;
         uint32 uiGuardiansOfIcecrownTimer;
         uint32 uiFaction;
@@ -306,6 +308,9 @@ public:
             spawns.DespawnAll();
 
             FindGameObjects();
+
+            if (instance)
+                instance->SetData(DATA_ABOMINATION_KILLED, 0);
 
             if (GameObject* pKTTrigger = me->GetMap()->GetGameObject(KTTriggerGUID))
             {
