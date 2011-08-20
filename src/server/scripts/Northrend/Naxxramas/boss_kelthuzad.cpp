506c15de3225512fbfc1e1ae2a0c0b9daa278ab2
Shauren <shauren.trinity@gmail.com>
2011-08-20 15:45:25 +0200

    Scripts/Naxxramas: Removed unneeded lines

--- src/server/scripts/Northrend/Naxxramas/boss_kelthuzad.cpp
+++ src/server/scripts/Northrend/Naxxramas/boss_kelthuzad.cpp
@@ -269,10 +269,8 @@ public:
         boss_kelthuzadAI(Creature* c) : BossAI(c, BOSS_KELTHUZAD), spawns(c)
         {
             uiFaction = me->getFaction();
-            instance = me->GetInstanceScript();
         }
 
-        InstanceScript* instance;
         uint32 Phase;
         uint32 uiGuardiansOfIcecrownTimer;
         uint32 uiFaction;
