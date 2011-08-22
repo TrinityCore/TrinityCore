e0b0b0c0b624f2c557611e42dc6f794d39edde66
Kapoeira <vertozor@gmail.com>
2011-08-21 23:25:34 -0400

    Wrong stuff to revert.

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -5405,7 +5405,6 @@ bool Unit::HandleDummyAuraProc(Unit* victim, uint32 damage, AuraEffect* triggere
                 {
                     triggered_spell_id = 48503;
                     basepoints0 = triggerAmount;
-                    target = this;
                     break;
                 }
                 // Kill command
