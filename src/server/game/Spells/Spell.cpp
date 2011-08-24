7f7ca27aa615c601f817770727b6db6780e9396c
hacknowledge <hacknowledge101@googlemail.com>
2011-08-25 00:41:02 +0200

    white space correction

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -3290,7 +3290,7 @@ void Spell::handle_immediate()
     TakeCastItem();
 
     // handle ammo consumption for Hunter's volley spell
-	if (m_spellInfo->IsRangedWeaponSpell() && m_spellInfo->IsChanneled())
+    if (m_spellInfo->IsRangedWeaponSpell() && m_spellInfo->IsChanneled())
         TakeAmmo();
 
 
