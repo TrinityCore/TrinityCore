ce8ce369a5976ee8d735eafe1c02cb4889d12697
Kapoeira <vertozor@gmail.com>
2011-08-24 03:55:44 -0400

    Revert "torev"
    This reverts commit 3ba882771615b7792ed9845b1187de55ac24cde0.

--- src/server/game/Spells/SpellInfo.cpp
+++ src/server/game/Spells/SpellInfo.cpp
@@ -497,7 +497,6 @@ int32 SpellEffectInfo::CalcValue(Unit const* caster, int32 const* bp, Unit const
             //value = int32(value * (int32)getLevel() / (int32)(_spellInfo->spellLevel ? _spellInfo->spellLevel : 1));
     }
 
-    sLog->outString("Computed value is %u", value);
     return int32(value);
 }
 
