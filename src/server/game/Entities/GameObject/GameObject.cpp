bef1e81e90b608aee1722e022a7dac23508175f6
cyberbrest <cyberbrest@trinity.contrib>
2011-08-29 23:49:46 +0200

    Core/Spells: Set summoner guid for GO cast triggers to allow proper hostility checks on cast

--- src/server/game/Entities/GameObject/GameObject.cpp
+++ src/server/game/Entities/GameObject/GameObject.cpp
@@ -1631,6 +1631,8 @@ void GameObject::CastSpell(Unit* target, uint32 spellId)
     if (Unit* owner = GetOwner())
     {
         trigger->setFaction(owner->getFaction());
+        // needed for GO casts for proper target validation checks
+        trigger->SetUInt64Value(UNIT_FIELD_SUMMONEDBY, owner->GetGUID());
         trigger->CastSpell(target ? target : trigger, spellInfo, true, 0, 0, owner->GetGUID());
     }
     else
