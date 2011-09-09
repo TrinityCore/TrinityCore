9770fdb1004aa8ea776644e92b7496a5ef85eded
WarHead <WarHead@UWoM.de>
2011-09-09 11:05:45 +0200

    Creature: Korrigierten FallGround() Hack aus https://github.com/TrinityCore/TrinityCore/issues/2858 hinzugefügt.

--- src/server/game/Entities/Creature/Creature.cpp
+++ src/server/game/Entities/Creature/Creature.cpp
@@ -49,6 +49,8 @@
 #include "Vehicle.h"
 #include "SpellAuraEffects.h"
 #include "Group.h"
+#include <IVMapManager.h>
+
 // apply implementation of the singletons
 
 TrainerSpell const* TrainerSpellData::Find(uint32 spell_id) const
@@ -1561,6 +1563,9 @@ bool Creature::FallGround()
     // use larger distance for vmap height search than in most other cases
     float ground_Z = GetMap()->GetHeight(x, y, z, true, MAX_FALL_DISTANCE);
     if (fabs(ground_Z - z) < 0.1f)
+        return false;
+
+    if (ground_Z == VMAP_INVALID_HEIGHT_VALUE)
         return false;
 
     GetMotionMaster()->MoveFall(ground_Z, EVENT_FALL_GROUND);
