f30321ef1eda0d1a2094a890d56299b303576c33
kiper <kiperek@o2.pl>
2011-09-15 01:11:50 +0200

    Fix typo in sql file from previous commit

--- sql/updates/world/2011_09_15_00_world_spell_script_names.sql
+++ sql/updates/world/2011_09_15_00_world_spell_script_names.sql
@@ -1,4 +1,4 @@
-DELETE FROM `spell_script_names` WHERE `spell_id` IN (53475,53487,53487,54015);
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (53475,53487,54015);
 INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
 (53475, 'spell_gen_oracle_wolvar_reputation'), -- Set Oracle Faction Friendly
 (53487, 'spell_gen_oracle_wolvar_reputation'), -- Set Wolvar Faction Honored
