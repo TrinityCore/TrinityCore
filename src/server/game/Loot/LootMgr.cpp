9ad5eb9be877e20ec31548f7d96da7d0ff71fce4
Shocker <shocker@freakz.ro>
2011-09-16 00:37:04 +0300

    Core/Misc: Revert the loot-related code from 74616464f2, the iterator is needed later

--- src/server/game/Loot/LootMgr.cpp
+++ src/server/game/Loot/LootMgr.cpp
@@ -1075,7 +1075,9 @@ void LootTemplate::LootGroup::Process(Loot& loot, uint16 lootMode) const
         if (item == NULL && !EqualPossibleDrops.empty()) // If nothing selected yet - an item is taken from equal-chanced part
         {
             itemSource = 2;
-            item = const_cast<LootStoreItem*>(&SelectRandomContainerElement(EqualPossibleDrops));
+            itr = EqualPossibleDrops.begin();
+            std::advance(itr, irand(0, EqualPossibleDrops.size()-1));
+            item = &*itr;
         }
         // finish rolling
 
