a72f459f759e4a3311c18f8453a77e7c4d60f05b
LihO <liho@posterus.cz>
2011-09-17 20:17:41 +0200

    Core/Groups: Refactored sending of update packets.
    Now it is possible to send update packet to single player.

--- src/server/game/Groups/Group.h
+++ src/server/game/Groups/Group.h
@@ -260,6 +260,7 @@ class Group
         //void SendInit(WorldSession* session);
         void SendTargetIconList(WorldSession* session);
         void SendUpdate();
+        void SendUpdateToPlayer(uint64 playerGUID, MemberSlot* slot = NULL);
         void UpdatePlayerOutOfRange(Player* pPlayer);
                                                             // ignore: GUID of player that will be ignored
         void BroadcastPacket(WorldPacket* packet, bool ignorePlayersInBGRaid, int group=-1, uint64 ignore=0);
