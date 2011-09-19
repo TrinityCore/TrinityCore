dd83f51dcd6ecfe8d7a9da2ff72214a2ac73db63
Shauren <shauren.trinity@gmail.com>
2011-09-19 12:21:36 +0200

    Scripts/Icecrown Citadel
    * Traps after Deathbringer Saurfang will not turn off anymore
    * Fixed Valithria Dreamwalker adds not attacking players
    
    Closes #2343
    Closes #2485
    Closes #2815

--- src/server/scripts/Northrend/IcecrownCitadel/icecrown_citadel_teleport.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/icecrown_citadel_teleport.cpp
@@ -41,6 +41,7 @@ class icecrown_citadel_teleport : public GameObjectScript
                     player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Teleport to the Deathbringer's Rise.", GOSSIP_SENDER_ICC_PORT, DEATHBRINGER_S_RISE_TELEPORT);
                 if (instance->GetData(DATA_COLDFLAME_JETS) == DONE)
                     player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Teleport to the Upper Spire.", GOSSIP_SENDER_ICC_PORT, UPPER_SPIRE_TELEPORT);
+                // TODO: Gauntlet event before Sindragosa
                 if (instance->GetBossState(DATA_VALITHRIA_DREAMWALKER) == DONE)
                     player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Teleport to Sindragosa's Lair", GOSSIP_SENDER_ICC_PORT, SINDRAGOSA_S_LAIR_TELEPORT);
             }
