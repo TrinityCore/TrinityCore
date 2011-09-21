adf16c718e7b62f4d8effd6022ce5fc98de21caf
Rochet2 <rochet2@post.com>
2011-09-21 10:04:41 -0300

    Scripts/Commands: Added .developer command to show <Dev> tag in nickname

---
+++ sql/updates/world/2011_09_21_02_world_command.sql
@@ -0,0 +1,3 @@
+DELETE FROM command WHERE name = 'developer';
+INSERT INTO command VALUES
+('developer', 3, 'Syntax: .developer [on/off]\r\n\r\nEnable or Disable in game Dev tag or show current state if on/off not provided.');
