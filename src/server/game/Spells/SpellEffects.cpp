154c7ce79c15971d1b5bae033a67f41bc33945fb
Shauren <shauren.trinity@gmail.com>
2011-09-22 20:16:47 +0200

    Core/Spells: Fixed Brewfest - Attack Keg spell damage

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -422,6 +422,7 @@ void Spell::SpellDamageSchoolDmg(SpellEffIndex effIndex)
                     case 20625: // Ritual of Doom Sacrifice
                     case 29142: // Eyesore Blaster
                     case 35139: // Throw Boom's Doom
+                    case 42393: // Brewfest - Attack Keg
                     case 55269: // Deathly Stare
                     case 56578: // Rapid-Fire Harpoon
                     case 62775: // Tympanic Tantrum
