780747f5d39cd05e22d28f80eed7eda18453e0b8
megamage <none@none.none>
2011-09-26 09:14:12 -0400

    Missing part of the previous commit.

--- src/server/scripts/Northrend/Nexus/Nexus/boss_keristrasza.cpp
+++ src/server/scripts/Northrend/Nexus/Nexus/boss_keristrasza.cpp
@@ -240,6 +240,7 @@ class spell_intense_cold : public SpellScriptLoader
                 if (aurEff->GetBase()->GetStackAmount() < 2)
                     return;
                 Unit* caster = GetCaster();
+                //TODO: the caster should be boss but not the player
                 if (!caster || !caster->GetAI())
                     return;
                 caster->GetAI()->SetGUID(GetTarget()->GetGUID(), DATA_INTENSE_COLD);
