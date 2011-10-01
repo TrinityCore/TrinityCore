85f8653d21fff7d7b3c9fe97590ec9c4a770b800
Discover- <amort11@hotmail.com>
2011-10-01 18:06:17 +0100

    DB/SAI: Re-fix quest Stunning Defeat at the Ring (convert EAI to SAI)
    Closes #2618

--- sql/updates/world/2011_10_01_06_world_misc.sql
+++ sql/updates/world/2011_10_01_06_world_misc.sql
@@ -26,7 +26,7 @@ INSERT INTO `gossip_menu_option` (`menu_id`,`id`,`option_icon`,`option_text`,`op
 
 -- Commander Ruusk SAI
 UPDATE `creature_template` SET `gossip_menu_id`=8336,`AIName`='SmartAI' WHERE `entry`=20563;
-DELETE FROM `smart_scripts` WHERE `entryorguid`=20563;
+DELETE FROM `smart_scripts` WHERE `entryorguid`=20563 AND `source_type`=0;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (20563,0,0,1,62,0,100,0,8338,0,0,0,72,0,0,0,0,0,0,7,0,0,0,0,0,0,0,'Commander Ruusk - On Gossip Select - Close Gossip'),
 (20563,0,1,0,61,0,100,0,0,0,0,0,26,10577,0,0,0,0,0,7,0,0,0,0,0,0,0,'Commander Ruusk - On Gossip Select (link) - Quest Credit');
