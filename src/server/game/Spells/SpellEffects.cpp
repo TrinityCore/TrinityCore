8919b9cb4c08e6960f844a90472dfeb5b2caa5bf
Shauren <shauren.trinity@gmail.com>
2011-10-06 14:22:11 +0200

    Core/Spells: Fixed typo breaking SPELL_AURA_MOD_AOE_DAMAGE_AVOIDANCE and SPELL_AURA_MOD_CREATURE_AOE_DAMAGE_AVOIDANCE

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -309,7 +309,7 @@ void Spell::EffectEnvironmentalDMG(SpellEffIndex /*effIndex*/)
 
 void Spell::EffectSchoolDMG(SpellEffIndex effIndex)
 {
-    if (effectHandleMode == SPELL_EFFECT_HANDLE_LAUNCH_TARGET)
+    if (effectHandleMode != SPELL_EFFECT_HANDLE_LAUNCH_TARGET)
         return;
 
     bool apply_direct_bonus = true;
