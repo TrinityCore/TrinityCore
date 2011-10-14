dbb42820aa3b7d5e938086d1ca95ce08b9581393
SignFinder <signfinder@gmail.com>
2011-10-14 15:18:22 +0400

    Core/Mechanic: Fix items with ITEM_MOD_SPELL_PENETRATION

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -1595,7 +1595,7 @@ void Unit::CalcAbsorbResist(Unit* victim, SpellSchoolMask schoolMask, DamageEffe
         float ignoredResistance = float(GetTotalAuraModifierByMiscMask(SPELL_AURA_MOD_TARGET_RESISTANCE, schoolMask));
         if (Player* player = ToPlayer())
             ignoredResistance += float(player->GetSpellPenetrationItemMod());
-        float victimResistance = baseVictimResistance + ignoredResistance;
+        float victimResistance = baseVictimResistance - ignoredResistance;
 
         static const uint32 BOSS_LEVEL = 83;
         static const float BOSS_RESISTANCE_CONSTANT = 510.0;
