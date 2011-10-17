b84250abc5195ddde58706d0de7ec867627e78ef
Liberate <tbaart@gmail.com>
2011-10-17 03:06:35 +0200

    Core/Movement: Fix Point Movement after a stun/root. The NPC should now correctly start moving again.

--- src/server/game/Movement/MovementGenerators/PointMovementGenerator.cpp
+++ src/server/game/Movement/MovementGenerators/PointMovementGenerator.cpp
@@ -58,6 +58,10 @@ bool PointMovementGenerator<T>::Update(T &unit, const uint32 diff)
         arrived = true;
         return false;
     }
+    else if (!unit.HasUnitState(UNIT_STAT_MOVE))
+    {
+        i_destinationHolder.StartTravel(traveller);
+    }
 
     return true;
 }
