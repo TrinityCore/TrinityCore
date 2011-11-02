c0c6000a4479f26fe2452a25846d2303a09533b8
tobmaps <spambot42@yandex.ru>
2011-11-02 19:13:51 +0700

    Core/Misc: Fix spelling typo

--- src/server/game/Entities/Creature/Creature.h
+++ src/server/game/Entities/Creature/Creature.h
@@ -156,7 +156,7 @@ struct CreatureTemplate
         else if (type_flags & CREATURE_TYPEFLAGS_MININGLOOT)
             return SKILL_MINING;
         else if (type_flags & CREATURE_TYPEFLAGS_ENGINEERLOOT)
-            return SKILL_ENGINERING;
+            return SKILL_ENGINEERING;
         else
             return SKILL_SKINNING;                          // normal case
     }
