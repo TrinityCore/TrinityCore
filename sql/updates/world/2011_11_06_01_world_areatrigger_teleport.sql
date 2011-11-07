92199c02c917f68ee0b1fb9bcec36cc5fea73e5e
Aokromes <aokromes@gmail.com>
2011-11-07 17:12:19 +0100

    DB:Areatrigger teleport: Fix orientation of triggers from AQ40

---
+++ sql/updates/world/2011_11_06_01_world_areatrigger_teleport.sql
@@ -0,0 +1,3 @@
+-- Correct AQ40's areatrigger_teleport orientation
+UPDATE `areatrigger_teleport` SET `target_position_z`='129.330612', `target_orientation`='0.929912' WHERE `id`=4010;
+UPDATE `areatrigger_teleport` SET `target_orientation`='4.030663' WHERE `id`=4012;
