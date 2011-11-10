59004f0edecb6db4c3215009d61eaf9a7f18f283
megamage <none@none.none>
2011-11-10 17:31:45 -0500

    Fix the crash when removing object from an unloaded grid. Object should always be unlinked from grid no matter if the grid is loaded.

--- src/server/game/Maps/Map.h
+++ src/server/game/Maps/Map.h
@@ -526,9 +526,6 @@ class Map : public GridRefManager<NGridType>
             void AddToGrid(T* object, Cell const& cell);
 
         template<class T>
-            void RemoveFromGrid(T* object, Cell const& cell);
-
-        template<class T>
             void DeleteFromWorld(T*);
 
         template<class T>
