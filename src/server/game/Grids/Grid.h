7cc4930439135d73d1db65bfa7ab891e50d0bc2c
megamage <none@none.none>
2011-11-10 10:18:32 -0500

    Add some ASSERT in add/remove obj to/from grid.

--- src/server/game/Grids/Grid.h
+++ src/server/game/Grids/Grid.h
@@ -58,16 +58,19 @@ class Grid
          */
         template<class SPECIFIC_OBJECT> void AddWorldObject(SPECIFIC_OBJECT *obj)
         {
-            if (!i_objects.template insert<SPECIFIC_OBJECT>(obj))
-                ASSERT(false);
+            ASSERT(!obj->GetGridRef().isValid());
+            i_objects.template insert<SPECIFIC_OBJECT>(obj);
+            ASSERT(obj->GetGridRef().isValid());
         }
 
         /** an object of interested exits the grid
          */
+        //Actually an unlink is enough, no need to go through the container
         template<class SPECIFIC_OBJECT> void RemoveWorldObject(SPECIFIC_OBJECT *obj)
         {
-            if (!i_objects.template remove<SPECIFIC_OBJECT>(obj))
-                ASSERT(false);
+            ASSERT(obj->GetGridRef().isValid());
+            i_objects.template remove<SPECIFIC_OBJECT>(obj);
+            ASSERT(!obj->GetGridRef().isValid());
         }
 
         /** Refreshes/update the grid. This required for remote grids.
@@ -104,16 +107,18 @@ class Grid
          */
         template<class SPECIFIC_OBJECT> void AddGridObject(SPECIFIC_OBJECT *obj)
         {
-            if (!i_container.template insert<SPECIFIC_OBJECT>(obj))
-                ASSERT(false);
+            ASSERT(!obj->GetGridRef().isValid());
+            i_container.template insert<SPECIFIC_OBJECT>(obj);
+            ASSERT(obj->GetGridRef().isValid());
         }
 
         /** Removes a containter type object from the grid
          */
         template<class SPECIFIC_OBJECT> void RemoveGridObject(SPECIFIC_OBJECT *obj)
         {
-            if (!i_container.template remove<SPECIFIC_OBJECT>(obj))
-                ASSERT(false);
+            ASSERT(obj->GetGridRef().isValid());
+            i_container.template remove<SPECIFIC_OBJECT>(obj);
+            ASSERT(!obj->GetGridRef().isValid());
         }
 
         /*bool NoWorldObjectInGrid() const
