0673a955b9929129be2010c953750d003f00fc10
tobmaps <spambot42@yandex.ru>
2011-11-12 04:40:44 +0700

    Core/Spells: Fixed resist calculations for spells with multiple schools. Thx to Shauren for tips
    Closes #3875

--- src/server/game/Entities/Vehicle/Vehicle.cpp
+++ src/server/game/Entities/Vehicle/Vehicle.cpp
@@ -177,7 +177,7 @@ void Vehicle::ApplyAllImmunities()
         case 244: // Wintergrasp
         case 510: // Isle of Conquest
             _me->SetControlled(true, UNIT_STAT_ROOT);
-            // why we need to apply this? we can simple add immunities to slow mechamic in DB
+            // why we need to apply this? we can simple add immunities to slow mechanic in DB
             _me->ApplySpellImmune(0, IMMUNITY_STATE, SPELL_AURA_MOD_DECREASE_SPEED, true);
             break;
         default:
