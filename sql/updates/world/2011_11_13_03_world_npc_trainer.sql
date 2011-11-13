7dec496e1844297aab9bbc44ebf33f65e9492667
zxbiohazardzx <zxbiohazardzx@gmail.com>
2011-11-14 00:10:35 +0100

    DB/Loot: Bloodfen Feathers should also drop from other Bloodfen Raptors
    fixes #3842

--- sql/updates/world/2011_11_13_03_world_npc_trainer.sql
+++ sql/updates/world/2011_11_13_03_world_npc_trainer.sql
@@ -1,5 +1,5 @@
 -- Remove Manual: Netherweave Bandage & and Manual: Heavy Netherweave Bandage from vendors (Patch 3.1.0)
-DELETE FROM npc_vendor WHERE item IN (21992,21991);
+DELETE FROM `npc_vendor` WHERE `item` IN (21992,21991);
 -- Update reqskillvalue for Netherweave Bandage & Heavy Netherweave Bandage (Patch 3.1.0)
-UPDATE npc_trainer SET reqskillvalue=300 WHERE spell=27032;
-UPDATE npc_trainer SET reqskillvalue=330 WHERE spell=27033;
+UPDATE `npc_trainer` SET `reqskillvalue`=300 WHERE spell=27032;
+UPDATE `npc_trainer` SET `reqskillvalue`=330 WHERE spell=27033;
