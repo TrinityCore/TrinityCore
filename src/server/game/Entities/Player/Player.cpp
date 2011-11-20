28cc82deb5f7a461b2bc06445ee6421412130728
Kandera <kanderacutie@hotmail.com>
2011-11-20 07:28:02 -0500

    Core/Spells: Bladestorm (fix issue with changing between weapons canceling bladestorm)

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -22224,6 +22224,12 @@ void Player::RemoveItemDependentAurasAndCasts(Item* pItem)
             continue;
         }
 
+        if (aura->GetId() == 46924)
+        {
+            ++itr;
+            continue;
+        }
+
         // no alt item, remove aura, restart check
         RemoveOwnedAura(itr);
     }
