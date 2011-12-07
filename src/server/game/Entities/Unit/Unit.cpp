bd80d35e1749864e0c308d4ce5b95b2dbb205c24
Giuseppe Montesanto <montesanto.giuseppe@live.it>
2011-12-08 00:50:03 +0100

    Ignore effects with mechanic on 'IsImmunedToSpell'

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -11461,9 +11461,8 @@ bool Unit::IsImmunedToSpell(SpellInfo const* spellInfo)
     {
         // State/effect immunities applied by aura expect full spell immunity
         // Ignore effects with mechanic, they are supposed to be checked separately
-        if (!spellInfo->Effects[i].Mechanic)
-            if (!IsImmunedToSpellEffect(spellInfo, i))
-                immuneToAllEffects = false;
+        if (spellInfo->Effects[i].Mechanic || (!spellInfo->Effects[i].Mechanic && !IsImmunedToSpellEffect(spellInfo, i)))
+            immuneToAllEffects = false;
     }
     if (immuneToAllEffects) //Return immune only if the target is immune to all spell effects.
         return true;
