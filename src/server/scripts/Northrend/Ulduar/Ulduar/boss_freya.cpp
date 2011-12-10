d4921e0f259e3e9b8cb40fac992c3dcf884d219b
Shocker <shocker@freakz.ro>
2011-12-10 20:05:51 +0200

    Core/Misc: Small english fix

--- src/server/scripts/Northrend/Ulduar/Ulduar/boss_freya.cpp
+++ src/server/scripts/Northrend/Ulduar/Ulduar/boss_freya.cpp
@@ -526,7 +526,7 @@ class boss_freya : public CreatureScript
             {
                 uint8 n = 0;
 
-                // Handling recieved data
+                // Handling received data
                 for (uint8 i = 0; i < 5; ++i)                                    // We have created "instances" for keeping informations about last 6 death lashers - needed because of respawning
                 {
                     deforestation[i][0] = deforestation[(i + 1)][0];             // Time
