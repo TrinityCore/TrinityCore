94e85c34d3187de243984e46e1cc6d9ef3361529
Nay <dnpd.dd@gmail.com>
2011-12-22 21:21:41 +0000

    Core/Spell: Enable and 'fix' SMSG_SET_PCT/FLAG_SPELL_MODIFIER

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -19730,29 +19730,29 @@ bool Player::IsAffectedBySpellmod(SpellInfo const* spellInfo, SpellModifier* mod
 void Player::AddSpellMod(SpellModifier* mod, bool apply)
 {
     sLog->outDebug(LOG_FILTER_SPELLS_AURAS, "Player::AddSpellMod %d", mod->spellId);
-    uint16 Opcode = (mod->type == SPELLMOD_FLAT) ? SMSG_SET_FLAT_SPELL_MODIFIER : SMSG_SET_PCT_SPELL_MODIFIER;
+    uint16 opcode = (mod->type == SPELLMOD_FLAT) ? SMSG_SET_FLAT_SPELL_MODIFIER : SMSG_SET_PCT_SPELL_MODIFIER;
 
     int i = 0;
     flag96 _mask = 0;
     for (int eff = 0; eff < 96; ++eff)
     {
-        if (eff != 0 && eff%32 == 0)
+        if (eff != 0 && eff % 32 == 0)
             _mask[i++] = 0;
 
-        _mask[i] = uint32(1) << (eff-(32*i));
+        _mask[i] = uint32(1) << (eff - (32 * i));
         if (mod->mask & _mask)
         {
             int32 val = 0;
             for (SpellModList::iterator itr = m_spellMods[mod->op].begin(); itr != m_spellMods[mod->op].end(); ++itr)
-            {
                 if ((*itr)->type == mod->type && (*itr)->mask & _mask)
                     val += (*itr)->value;
-            }
             val += apply ? mod->value : -(mod->value);
-            WorldPacket data(Opcodes(Opcode), (1+1+4));
+            WorldPacket data(Opcodes(opcode), (1+1+4));
             data << uint8(eff);
             data << uint8(mod->op);
             data << int32(val);
+            if (opcode == SMSG_SET_PCT_SPELL_MODIFIER)
+                data << uint8(0);                             // 4.x unk
             SendDirectMessage(&data);
         }
     }
