3363fc8eb17f6bd0fe50670c1b7a42d5a805c227
kaelima <kaelima@live.se>
2011-12-22 13:22:18 +0100

    Core/Spells: Fix two signed / unsigned warnings from f8a13b401d132c6e8e86fb76244c0586378bc189

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -3485,7 +3485,7 @@ void Spell::update(uint32 difftime)
         {
             if (m_timer)
             {
-                if (difftime >= m_timer)
+                if (difftime >= (uint32)m_timer)
                     m_timer = 0;
                 else
                     m_timer -= difftime;
@@ -3494,7 +3494,8 @@ void Spell::update(uint32 difftime)
             if (m_timer == 0 && !IsNextMeleeSwingSpell() && !IsAutoRepeat())
                 // don't CheckCast for instant spells - done in spell::prepare, skip duplicate checks, needed for range checks for example
                 cast(!m_casttime);
-        } break;
+            break;
+        }
         case SPELL_STATE_CASTING:
         {
             if (m_timer > 0)
@@ -3507,7 +3508,7 @@ void Spell::update(uint32 difftime)
                     finish();
                 }
 
-                if (difftime >= m_timer)
+                if (difftime >= (uint32)m_timer)
                     m_timer = 0;
                 else
                     m_timer -= difftime;
@@ -3552,10 +3553,10 @@ void Spell::update(uint32 difftime)
 
                 finish();
             }
-        } break;
+            break;
+        }
         default:
-        {
-        }break;
+            break;
     }
 }
 
