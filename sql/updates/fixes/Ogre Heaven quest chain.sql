0d5ef43272fc3202540fd327c3be73e299a7fd98
nelegalno <nelegalno@yopmail.com>
2011-12-22 18:18:54 +0200

    Ogre Heaven fix PrevQuestID by shlomi1515
    fixed

--- sql/updates/fixes/Ogre Heaven quest chain.sql
+++ sql/updates/fixes/Ogre Heaven quest chain.sql
@@ -1,4 +1,4 @@
 -- Ogre Heaven fix PrevQuestID by shlomi1515
 -- Closes #2732
 
-UPDATE quest_template SET PrevQuestID='11000' WHERE entry='11009';
+UPDATE quest_template SET PrevQuestID='11000' WHERE ID='11009';
