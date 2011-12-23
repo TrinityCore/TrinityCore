a6cf8949cdf12985556b22c308adfe69d2b4b4e7
SignFinder <signfinder@gmail.com>
2011-12-23 15:33:28 +0400

    DB/YTDB: Fix repetable of event for object 185861

--- sql/updates/fc_updates/fixes_for_ytdb_world.sql
+++ sql/updates/fc_updates/fixes_for_ytdb_world.sql
@@ -195,3 +195,6 @@ UPDATE creature_template SET unit_flags = '0' WHERE entry IN (36853,38267,38266,
 
 -- Fix On Metzen! Achievement (20.12.2011)
 UPDATE `smart_scripts` SET `event_param1`=6763 WHERE  `entryorguid` =15664;
+
+-- Fix repetable of event for object 185861 (23.12.2011)
+UPDATE `creature_ai_scripts` SET `event_flags`=1 WHERE `creature_id`=23119;
