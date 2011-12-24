c284c3404a33b98594a20c9916b29329dfa540b9
Shocker <shocker@freakz.ro>
2011-12-24 15:16:00 +0200

    SQL: Correct column names in recent SQL

--- sql/updates/world/2011_12_24_01_world_mail_loot_template.sql
+++ sql/updates/world/2011_12_24_01_world_mail_loot_template.sql
@@ -1,41 +1,41 @@
 -- Reward from Treats for Greatfather Winter (horde)
 SET @mail:=102; -- Set in DBC
-UPDATE quest_template SET RewMailTemplateId=@mail, RewMailDelaySecs=86400 WHERE entry IN (6962); -- Set mail delivery
+UPDATE quest_template SET RewardMailTemplateId=@mail, RewardMailDelay=86400 WHERE Id IN (6962); -- Set mail delivery
 DELETE FROM mail_loot_template WHERE entry=@mail;
 INSERT INTO mail_loot_template (entry,item,ChanceOrQuestChance,lootmode,groupid,mincountOrRef,maxcount) VALUES
 (@mail,17685,100,1,0,1,1); -- Attach item to mail
 
 -- Reward from Treats for Greatfather Winter (alliance)
 SET @mail:=118; -- Set in DBC
-UPDATE quest_template SET RewMailTemplateId=@mail, RewMailDelaySecs=86400 WHERE entry IN (7025); -- Set mail delivery
+UPDATE quest_template SET RewardMailTemplateId=@mail, RewardMailDelay=86400 WHERE Id IN (7025); -- Set mail delivery
 DELETE FROM mail_loot_template WHERE entry=@mail;
 INSERT INTO mail_loot_template (entry,item,ChanceOrQuestChance,lootmode,groupid,mincountOrRef,maxcount) VALUES
 (@mail,17685,100,1,0,1,1); -- Attach item to mail
 
 -- Reward from Stolen Winter Veil Treats (horde)
 SET @mail:=108; -- Set in DBC
-UPDATE quest_template SET RewMailTemplateId=@mail, RewMailDelaySecs=86400 WHERE entry IN (6963); -- Set mail delivery
+UPDATE quest_template SET RewardMailTemplateId=@mail, RewardMailDelay=86400 WHERE Id IN (6963); -- Set mail delivery
 DELETE FROM mail_loot_template WHERE entry=@mail;
 INSERT INTO mail_loot_template (entry,item,ChanceOrQuestChance,lootmode,groupid,mincountOrRef,maxcount) VALUES
 (@mail,17712,100,1,0,1,1); -- Attach item to mail
 
 -- Reward from Stolen Winter Veil Treats (alliance)
 SET @mail:=117; -- Set in DBC
-UPDATE quest_template SET RewMailTemplateId=@mail, RewMailDelaySecs=86400 WHERE entry IN (7042); -- Set mail delivery
+UPDATE quest_template SET RewardMailTemplateId=@mail, RewardMailDelay=86400 WHERE Id IN (7042); -- Set mail delivery
 DELETE FROM mail_loot_template WHERE entry=@mail;
 INSERT INTO mail_loot_template (entry,item,ChanceOrQuestChance,lootmode,groupid,mincountOrRef,maxcount) VALUES
 (@mail,17712,100,1,0,1,1); -- Attach item to mail
 
 -- Reward from Metzen the Reindeer (horde)
 SET @mail:=122; -- Set in DBC
-UPDATE quest_template SET RewMailTemplateId=@mail, RewMailDelaySecs=86400 WHERE entry IN (8746); -- Set mail delivery
+UPDATE quest_template SET RewardMailTemplateId=@mail, RewardMailDelay=86400 WHERE Id IN (8746); -- Set mail delivery
 DELETE FROM mail_loot_template WHERE entry=@mail;
 INSERT INTO mail_loot_template (entry,item,ChanceOrQuestChance,lootmode,groupid,mincountOrRef,maxcount) VALUES
 (@mail,21216,100,1,0,1,1); -- Attach item to mail
 
 -- Reward from Metzen the Reindeer (alliance)
 SET @mail:=161; -- Set in DBC
-UPDATE quest_template SET RewMailTemplateId=@mail, RewMailDelaySecs=86400 WHERE entry IN (8762); -- Set mail delivery
+UPDATE quest_template SET RewardMailTemplateId=@mail, RewardMailDelay=86400 WHERE Id IN (8762); -- Set mail delivery
 DELETE FROM mail_loot_template WHERE entry=@mail;
 INSERT INTO mail_loot_template (entry,item,ChanceOrQuestChance,lootmode,groupid,mincountOrRef,maxcount) VALUES
 (@mail,21216,100,1,0,1,1); -- Attach item to mail
