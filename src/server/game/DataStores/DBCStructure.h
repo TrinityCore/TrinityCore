710f5565041bd0aeac7a849d49be570398ce78f6
Shocker <shocker@freakz.ro>
2011-12-26 04:44:22 +0200

    Core/Achievements: Start implementing DBC achievement criteria conditions and remove db data and code for outdated DB achievement criteria datas

--- src/server/game/DataStores/DBCStructure.h
+++ src/server/game/DataStores/DBCStructure.h
@@ -526,8 +526,8 @@ struct AchievementCriteriaEntry
     uint32  showOrder;                                      // 14       m_ui_order  also used in achievement shift-links as index in state bitmask
     //uint32 unk1;                                          // 15 only one value, still unknown
     //uint32 unk2;                                          // 16 all zeros
-    uint32 moreRequirement[3];                              // 17-19
-    uint32 moreRequirementValue[3];                         // 20-22
+    uint32 additionalConditionType[MAX_ADDITIONAL_CRITERIA_CONDITIONS];     // 17-19
+    uint32 additionalConditionValue[MAX_ADDITIONAL_CRITERIA_CONDITIONS];    // 20-22
 };
 
 struct AreaTableEntry
