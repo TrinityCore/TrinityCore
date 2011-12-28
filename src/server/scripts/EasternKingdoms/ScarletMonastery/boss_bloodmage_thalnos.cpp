4fb634f9a50638e956423b7f091bc5795b784035
Spp <spp@jorge.gr>
2011-12-28 09:16:06 +0100

    Fix missing changes from previous merge

--- src/server/scripts/EasternKingdoms/ScarletMonastery/boss_bloodmage_thalnos.cpp
+++ src/server/scripts/EasternKingdoms/ScarletMonastery/boss_bloodmage_thalnos.cpp
@@ -92,7 +92,7 @@ public:
             if (FlameShock_Timer <= diff)
             {
                 DoCast(me->getVictim(), SPELL_FLAMESHOCK);
-                FlameShock_Timer = rand(10000, 15000);
+                FlameShock_Timer = urand(10000, 15000);
             } else FlameShock_Timer -= diff;
 
             //FlameSpike_Timer
