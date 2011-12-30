04d6fbdd328497d453fe45e3a5a05eb3b0433e4b
nelegalno <nelegalno@yopmail.com>
2011-12-30 15:28:51 +0200

    Revert "The Cleansing quest fix by shlomi1515"
    This reverts commit c38a24719a59f4d62e2977d912cdec674f540afa.

--- sql/updates/fixes2/The Cleansing event script.sql
+++
@@ -1,9 +0,0 @@
--- The Cleansing fix by shlomi1515
--- Closes #3582
-
-DELETE FROM gameobject WHERE guid=150377;
-UPDATE gameobject_template SET data2=11322 WHERE entry=186649;
-
-DELETE FROM event_scripts WHERE id=11322;
-INSERT INTO event_scripts VALUES 
-(11322,1,8,27959,0,0,0,0,0,0);
