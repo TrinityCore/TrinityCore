3a7ec6ef963f4dc66ff1cb018da013c82c7c1720
E. van Harten <evharten@snelmail.nl>
2012-01-03 10:43:08 +0100

    Quest Fix Priest (NightElf/Dwarf)

---
+++ sql/updates/world/2012_01_02_00_world_creature_template.sql
@@ -0,0 +1,5 @@
+-- [Q] Garments of the Moon (5621) and Garments of the Light (5625)
+SET @ENTRY_SHAYA := 12429; -- Sentinel Shaya
+SET @ENTRY_DOLF := 12427; -- Mountaineer Dolf
+
+UPDATE `creature_template` SET `unit_flags`=`8` WHERE `entry` IN (@ENTRY_SHAYA, @ENTRY_DOLF);
