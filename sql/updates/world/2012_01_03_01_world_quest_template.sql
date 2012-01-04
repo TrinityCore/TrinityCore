09aa2c0a84a72a5307aa264d7ad08dbdc9de57cc
malcrom <malcromdev@gmail.com>
2012-01-04 11:25:45 +0100

    DB/Creatures: set correct faction for frostmane troll whelp
    closes #1105

--- sql/updates/world/2012_01_03_01_world_quest_template.sql
+++ sql/updates/world/2012_01_03_01_world_quest_template.sql
@@ -1,1 +1,1 @@
-UPDATE quest_template SET OfferRewardText='<Getry looks you over.>$B$BThis is all Ickoris could send?' WHERE entry=11703;
+UPDATE `quest_template` SET `OfferRewardText`='<Getry looks you over.>$B$BThis is all Ickoris could send?' WHERE `id`=11703;
