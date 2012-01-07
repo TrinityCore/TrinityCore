15d876347892e372276f3941ef755f61d8d1b77f
Subv2112 <s.v.h21@hotmail.com>
2012-01-06 22:24:11 -0500

    Core/SAI: Fixed the crash with using SMART_ACTION_ATTACK_START inside a TimedActionList, closes #4650
    Signed-off-by: Subv2112 <s.v.h21@hotmail.com>

--- src/server/game/AI/SmartScripts/SmartScriptMgr.h
+++ src/server/game/AI/SmartScripts/SmartScriptMgr.h
@@ -824,7 +824,7 @@ struct SmartAction
         struct
         {
             uint32 id;
-            uint32 dontResume;
+            uint32 unused;
             uint32 timerType;
         } timedActionList;
 
