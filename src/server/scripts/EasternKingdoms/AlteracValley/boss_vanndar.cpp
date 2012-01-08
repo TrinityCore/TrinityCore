c1d9f83e6c05472ce604663ea602df8b266c0931
malcrom <malcromdev@gmail.com>
2012-01-08 14:20:11 +0100

    Core/Script: update creature_text for Twilight Corruptor from sniffs
    closes #4697

--- src/server/scripts/EasternKingdoms/AlteracValley/boss_vanndar.cpp
+++ src/server/scripts/EasternKingdoms/AlteracValley/boss_vanndar.cpp
@@ -23,7 +23,8 @@ enum Yells
     YELL_EVADE                                    = 1,
     YELL_RESPAWN1                                 = -1810010, // no creature_text
     YELL_RESPAWN2                                 = -1810011, // no creature_text
-    YELL_RANDOM                                   = 3
+    YELL_RANDOM                                   = 2,
+	YELL_SPELL                                    = 3,
 };
 
 enum Spells
