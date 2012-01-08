955824009c6669bbfb33d9a0ebf69bd3e9020cd4
malcrom <malcromdev@gmail.com>
2012-01-08 14:20:11 +0100

    Core/Script: update creature_text for Twilight Corruptor from sniffs
    closes #4697

---
+++ sql/updates/world/2011_01_08_12_world_creature_text.sql
@@ -0,0 +1,6 @@
+-- Twilight Corrupter say text
+DELETE FROM `creature_text` WHERE `entry` IN (15625);
+INSERT INTO `creature_text` (`entry`,`groupid`,`id`,`text`,`type`,`language`,`probability`,`emote`,`duration`,`sound`,`comment`) VALUES
+(15625,0,0, 'Come, $N. See what the Nightmare brings...',14,0,100,0,0,0, 'Twilight Corrupter'),
+(15625,1,0, 'The Nightmare cannot be stopped!',14,0,100,0,0,0, 'Twilight Corrupter'),
+(15625,2,0, 'Twilight Corrupter squeezes the last bit of life out of $N and swallows their soul.',16,0,100,0,0,0, 'Twilight Corrupter');
