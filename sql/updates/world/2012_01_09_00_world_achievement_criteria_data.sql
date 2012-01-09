f850baeb30e2532d2fb52fbd49368f313b73fe04
kaelima <kaelima@live.se>
2012-01-09 10:07:46 +0100

    Script/Naxxramas: Fixed "A Spore Loser", texts and timers for Loatheb encounter.
    Creds to Warpten, thanks.

---
+++ sql/updates/world/2012_01_09_00_world_achievement_criteria_data.sql
@@ -0,0 +1,5 @@
+DELETE FROM `disables` WHERE `sourceType`=4 AND `entry` IN (7612,7613);
+DELETE FROM `achievement_criteria_data` WHERE `criteria_id` IN (7612,7613);
+INSERT INTO `achievement_criteria_data` (`criteria_id`,`type`,`value1`,`value2`,`ScriptName`) VALUES
+(7612,11,0,0, 'achievement_spore_loser'),
+(7613,11,0,0, 'achievement_spore_loser');
