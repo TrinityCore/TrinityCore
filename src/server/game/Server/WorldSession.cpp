de6a45b20795855b93be2fbe86c2bba0ab5387e4
Rat <gmstreetrat@gmail.com>
2012-01-10 15:26:12 +0100

    Core/WorldSession: commented null opcode tracer

--- src/server/game/Server/WorldSession.cpp
+++ src/server/game/Server/WorldSession.cpp
@@ -161,9 +161,9 @@ void WorldSession::SendPacket(WorldPacket const* packet)
 
     if (packet->GetOpcode() == NULL_OPCODE || packet->GetOpcode() == UNKNOWN_OPCODE)
     {
-        sLog->outError("Prevented sending of %s. Trace:", packet->GetOpcode() == NULL_OPCODE ? "NULL_OPCODE" : "UNKNOWN_OPCODE");
-        ACE_Stack_Trace trace;
-        sLog->outError("%s", trace.c_str());
+        sLog->outError("Prevented sending of %s", packet->GetOpcode() == NULL_OPCODE ? "NULL_OPCODE" : "UNKNOWN_OPCODE");
+        //ACE_Stack_Trace trace;
+        //sLog->outError("%s", trace.c_str());
         return;
     }
 
