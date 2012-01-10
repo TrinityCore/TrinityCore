c398f2098805fe6030dc9c08543f13cdef5fd043
Shocker <shocker@freakz.ro>
2012-01-11 01:08:35 +0200

    Core/Achievements: Add SQLs for recent pull

---
+++ sql/updates/world/2012_01_11_00_world_spell_script_names.sql
@@ -0,0 +1,6 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (28062, 28085, 39090, 39093);
+INSERT INTO `spell_script_names`(`spell_id`, `scriptname`) VALUES
+(28062, "spell_thaddius_pos_neg_charge"),
+(28085, "spell_thaddius_pos_neg_charge"),
+(39090, "spell_thaddius_pos_neg_charge"),
+(39093, "spell_thaddius_pos_neg_charge");
