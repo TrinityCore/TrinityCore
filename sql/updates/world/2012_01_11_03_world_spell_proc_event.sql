c696112d73957161acf4f70ff605e22b2780da6f
cemak <mightyhaza@yandex.ru>
2012-01-12 20:00:03 +0400

    Core/Spells: Fix proc Furious Attacks.
    Signed-off-by: cemak <mightyhaza@yandex.ru>

---
+++ sql/updates/world/2012_01_11_03_world_spell_proc_event.sql
@@ -0,0 +1,5 @@
+DELETE FROM `spell_proc_event` WHERE entry IN (46910, 46911);
+INSERT into `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) values
+('46910','0','0','0','0','0','4','1','0','0','0');
+INSERT into `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) values
+('46911','0','0','0','0','0','4','1','0','0','0');
