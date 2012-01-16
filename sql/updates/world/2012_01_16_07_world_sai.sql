50e5432ccc458d1468a20a03ff032b47585f8dad
zxbiohazardzx <zxbiohazardzx@gmail.com>
2012-01-16 15:04:14 +0100

    DB/error: set correct column names

--- sql/updates/world/2012_01_16_07_world_sai.sql
+++ sql/updates/world/2012_01_16_07_world_sai.sql
@@ -5,7 +5,7 @@ SET @QUEST := 1447;
 SET @SPELL_PUMMEL := 12555;
 UPDATE `creature_template` SET `AIName`='SmartAI',`ScriptName`='' WHERE `entry`=@ENTRY;
 UPDATE `quest_template` SET `StartScript`=0,`CompleteScript`=0,`EmoteOnComplete`=11 WHERE `id`=@QUEST; -- ONESHOT_LAUGH
-UPDATE `quest_template` SET `CompleteEmote`=14 WHERE `entry`=1246; -- Previous version should make him rude against player
+UPDATE `quest_template` SET `EmoteOnComplete`=14 WHERE `id`=1246; -- Previous version should make him rude against player
 DELETE FROM `quest_start_scripts` WHERE `id`=@QUEST;
 DELETE FROM `smart_scripts` WHERE `entryorguid` IN (@ENTRY,@ENTRY*100,@ENTRY*100+1);
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
