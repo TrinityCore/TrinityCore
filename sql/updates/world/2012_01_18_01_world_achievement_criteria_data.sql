d201f2bbf1de2827f9e6aa330cc5636624bbaacb
Vincent-Core <Vincent_Michael@gmx.de>
2012-01-18 12:16:45 +0100

    Core/Achievements: Fixed achievement "Let's Get This Done" in Arathi Basin

---
+++ sql/updates/world/2012_01_18_01_world_achievement_criteria_data.sql
@@ -0,0 +1,3 @@
+DELETE FROM `achievement_criteria_data` WHERE `criteria_id` = 1237;
+INSERT INTO `achievement_criteria_data` (`criteria_id`, `type`, `value1`, `value2`, `ScriptName`) VALUES
+(1237, 0, 0, 0, '');
