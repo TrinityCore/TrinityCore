3b730500e3fcdafe34935f92ed8113a3a7f7d023
SignFinder <signfinder@gmail.com>
2012-01-19 10:02:36 +0400

    Core/Instances: Homebind timer set to 1 for disable steal loot

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -21062,7 +21062,7 @@ void Player::UpdateHomebindTime(uint32 time)
     else
     {
         // instance is invalid, start homebind timer
-        m_HomebindTimer = 60000;
+        m_HomebindTimer = 1;
         // send message to player
         WorldPacket data(SMSG_RAID_GROUP_ONLY, 4+4);
         data << uint32(m_HomebindTimer);
