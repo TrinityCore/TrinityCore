6521f7362ff3f0feea5decc1de5922ca6231232b
Chaplain <aionthefirst@gmail.com>
2012-01-25 20:40:59 +0300

    Core/Unit: More correct MotionMaster handling in Unit::SetCharmedBy() * fixes spell Eyes of the Beast

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -15817,6 +15817,8 @@ bool Unit::SetCharmedBy(Unit* charmer, CharmType type, AuraApplication const* au
     if (GetTypeId() == TYPEID_UNIT)
     {
         ToCreature()->AI()->OnCharmed(true);
+        GetMotionMaster()->Clear(false);
+        StopMoving();
         GetMotionMaster()->MoveIdle();
     }
     else
