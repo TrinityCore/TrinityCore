0e2ae5029ab2051e763fe1507eaf49081d5dcbd0
Subv2112 <s.v.h21@hotmail.com>
2012-02-04 19:51:39 -0500

    SQL: Added missing spawn of Spirit Healer in Desolace Author: The-Legend

---
+++ sql/updates/world/2012_02_04_00_world_creature.sql
@@ -0,0 +1,4 @@
+-- Adds missing spirit healer in Desolace
+SET @GUID := 40269;
+INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`) VALUES
+(@GUID, 6491, 1, 1, 1, 0, 0, -452.182, 2513.69, 92.8852, 6.1809, 300, 0, 0, 4120, 0, 0, 0, 0, 0);
