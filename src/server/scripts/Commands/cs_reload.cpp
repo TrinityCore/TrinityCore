13bea234094117258e5cb70d3c21c7ba53007a4b
Aokromes <jipr@hotmail.com>
2012-02-06 18:01:12 +0100

    Send Global GM message when reloading waypoint_data table like other reloads.

--- src/server/scripts/Commands/cs_reload.cpp
+++ src/server/scripts/Commands/cs_reload.cpp
@@ -1000,7 +1000,7 @@ public:
         sWaypointMgr->Load();
 
         if (*args != 'a')
-            sLog->outString("DB Table 'waypoint_data' reloaded.");
+            handler->SendGlobalGMSysMessage("DB Table 'waypoint_data' reloaded.");
 
         return true;
     }
