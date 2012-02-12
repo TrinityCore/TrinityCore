6bf665862b312437b69cc0644ae91cfa0e1e91fd
Go6o <aconstantgoal@abv.bg>
2012-02-12 02:02:29 +0200

    Proper fix by Vincent-Michael of Volunteer's Work

--- src/server/scripts/Northrend/AzjolNerub/Ahnkahet/boss_jedoga_shadowseeker.cpp
+++ src/server/scripts/Northrend/AzjolNerub/Ahnkahet/boss_jedoga_shadowseeker.cpp
@@ -159,7 +159,7 @@ public:
         void DoAction(int32 const action)
         {
             if (action == ACTION_INITIAND_KILLED)
-                volunteerWork = true;
+                volunteerWork = false;
         }
 
         uint32 GetData(uint32 type)
@@ -400,8 +400,8 @@ public:
                 if (Creature* boss = ObjectAccessor::GetCreature(*me, instance->GetData64(DATA_JEDOGA_SHADOWSEEKER)))
                 {
                     if (!CAST_AI(boss_jedoga_shadowseeker::boss_jedoga_shadowseekerAI, boss->AI())->bOpFerok)
-                        CAST_AI(boss_jedoga_shadowseeker::boss_jedoga_shadowseekerAI, boss->AI())->bOpFerokFail = true;
-
+                    CAST_AI(boss_jedoga_shadowseeker::boss_jedoga_shadowseekerAI, boss->AI())->bOpFerokFail = true;
+                    if (Killer->GetTypeId() == TYPEID_PLAYER)
                     boss->AI()->DoAction(ACTION_INITIAND_KILLED);
                 }
 
