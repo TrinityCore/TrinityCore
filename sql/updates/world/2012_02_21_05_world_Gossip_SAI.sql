a07e740c811eeb27afb82b94e5ba4759ccc97ac9
Malcrom <malcromdev@gmail.com>
2012-02-21 20:07:36 -0330

    Fix typo on previous commit

--- sql/updates/world/2012_02_21_05_world_Gossip_SAI.sql
+++ sql/updates/world/2012_02_21_05_world_Gossip_SAI.sql
@@ -19,6 +19,7 @@ UPDATE `creature_template` SET `AIName`='SmartAI',`ScriptName`='' WHERE `entry`=
 DELETE FROM `smart_scripts` WHERE (`entryorguid`=@ENTRY AND `source_type`=0);
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (@ENTRY,0,0,1,62,0,100,0,1285,0,0,0,26,3377,0,0,0,0,0,7,0,0,0,0,0,0,0,'Zamael Lunthistle - On Gossip Option select - quest credit'),
+(@ENTRY,0,1,0,61,0,100,0,0,0,0,0,72,0,0,0,0,0,0,7,0,0,0,0,0,0,0,'Zamael Lunthistle - On Gossip Option select - close gossip');
 
 DELETE FROM `gossip_menu` WHERE `entry`=1285 AND `text_id`=1920;
 INSERT INTO `gossip_menu` (`entry`,`text_id`) VALUES (1285,1920);
