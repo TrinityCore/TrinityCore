ce819b21b97f8abd15ed81a82bb181fc67be219f
ZxBiohazardZx <zxbiohazardzx@gmail.com>
2012-02-21 20:29:02 +0100

    Renumber the file & typofix on 2012_02_21_creature_loot_template

--- sql/updates/world/2012_02_21_02_world_creature_loot_template.sql
+++ sql/updates/world/2012_02_21_02_world_creature_loot_template.sql
@@ -1,4 +1,4 @@
-UPDATE `creature_template` SET `lootid`=`entry`  WHERE `entry` IN (38340,38341,38342,38032,37917,38016,38023,37214,38030,38006,37984);
+UPDATE `creature_template` SET `lootid`=`entry`  WHERE `entry` IN (38032,37917,38016,38023,37214,38030,38006,37984);
 DELETE FROM `creature_loot_template` WHERE `entry`IN (38032,37917,38016,38023,37214,38030,38006,37984);
 INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
 (38032,1,100,1,0,-45009,1), -- Crown Sprayer
