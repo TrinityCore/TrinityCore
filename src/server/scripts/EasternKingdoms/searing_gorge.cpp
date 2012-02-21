74fc13596c6552a984295670dbd1ca3190643dd5
Malcrom <malcromdev@gmail.com>
2012-02-21 20:03:12 -0330

    DB/Scripts: Remove 2 cpp scripts in Searing Gorge and replace with SAI.

--- src/server/scripts/EasternKingdoms/searing_gorge.cpp
+++ src/server/scripts/EasternKingdoms/searing_gorge.cpp
@@ -19,14 +19,12 @@
 /* ScriptData
 SDName: Searing_Gorge
 SD%Complete: 80
-SDComment: Quest support: 3377, 3441 (More accurate info on Kalaran needed). Lothos Riftwaker teleport to Molten Core.
+SDComment: Quest support: 3441 (More accurate info on Kalaran needed).
 SDCategory: Searing Gorge
 EndScriptData */
 
 /* ContentData
 npc_kalaran_windblade
-npc_lothos_riftwaker
-npc_zamael_lunthistle
 EndContentData */
 
 #include "ScriptPCH.h"
@@ -81,99 +79,10 @@ public:
 };
 
 /*######
-## npc_lothos_riftwaker
-######*/
-
-#define GOSSIP_HELLO_LR "Teleport me to the Molten Core"
-
-class npc_lothos_riftwaker : public CreatureScript
-{
-public:
-    npc_lothos_riftwaker() : CreatureScript("npc_lothos_riftwaker") { }
-
-    bool OnGossipSelect(Player* player, Creature* /*creature*/, uint32 /*uiSender*/, uint32 uiAction)
-    {
-        player->PlayerTalkClass->ClearMenus();
-        if (uiAction == GOSSIP_ACTION_INFO_DEF + 1)
-        {
-            player->CLOSE_GOSSIP_MENU();
-            player->TeleportTo(409, 1096, -467, -104.6f, 3.64f);
-        }
-
-        return true;
-    }
-
-    bool OnGossipHello(Player* player, Creature* creature)
-    {
-        if (creature->isQuestGiver())
-            player->PrepareQuestMenu(creature->GetGUID());
-
-        if (player->GetQuestRewardStatus(7487) || player->GetQuestRewardStatus(7848))
-            player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_HELLO_LR, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
-
-        player->SEND_GOSSIP_MENU(player->GetGossipTextId(creature), creature->GetGUID());
-
-        return true;
-    }
-
-};
-
-/*######
-## npc_zamael_lunthistle
-######*/
-
-#define GOSSIP_HELLO_ZL     "Tell me your story"
-#define GOSSIP_SELECT_ZL1   "Please continue..."
-#define GOSSIP_SELECT_ZL2   "Goodbye"
-
-class npc_zamael_lunthistle : public CreatureScript
-{
-public:
-    npc_zamael_lunthistle() : CreatureScript("npc_zamael_lunthistle") { }
-
-    bool OnGossipSelect(Player* player, Creature* creature, uint32 /*uiSender*/, uint32 uiAction)
-    {
-        player->PlayerTalkClass->ClearMenus();
-        switch (uiAction)
-        {
-            case GOSSIP_ACTION_INFO_DEF:
-                player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_SELECT_ZL1, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
-                player->SEND_GOSSIP_MENU(1921, creature->GetGUID());
-                break;
-            case GOSSIP_ACTION_INFO_DEF+1:
-                player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_SELECT_ZL2, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
-                player->SEND_GOSSIP_MENU(1922, creature->GetGUID());
-                break;
-            case GOSSIP_ACTION_INFO_DEF+2:
-                player->CLOSE_GOSSIP_MENU();
-                player->AreaExploredOrEventHappens(3377);
-                break;
-        }
-        return true;
-    }
-
-    bool OnGossipHello(Player* player, Creature* creature)
-    {
-        if (creature->isQuestGiver())
-            player->PrepareQuestMenu(creature->GetGUID());
-
-        if (player->GetQuestStatus(3377) == QUEST_STATUS_INCOMPLETE)
-            player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_HELLO_ZL, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF);
-
-        player->SEND_GOSSIP_MENU(1920, creature->GetGUID());
-
-        return true;
-    }
-
-};
-
-/*######
 ##
 ######*/
 
 void AddSC_searing_gorge()
 {
     new npc_kalaran_windblade();
-    new npc_lothos_riftwaker();
-    new npc_zamael_lunthistle();
 }
