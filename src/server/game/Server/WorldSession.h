dd05f1dcd278a2a50880d10a5df1549b4afbcc55
Kandera <KanderaDev@gmail.com>
2012-02-22 16:32:51 -0500

    fix crash with groups, added enum for battlfield leave reasons

--- src/server/game/Server/WorldSession.h
+++ src/server/game/Server/WorldSession.h
@@ -125,6 +125,16 @@ enum PartyResult
     ERR_PARTY_LFG_TELEPORT_IN_COMBAT    = 30
 };
 
+
+enum BFLeaveReason
+{
+    BF_LEAVE_REASON_CLOSE     = 0x00000001,
+    //BF_LEAVE_REASON_UNK1      = 0x00000002, (not used)
+    //BF_LEAVE_REASON_UNK2      = 0x00000004, (not used)
+    BF_LEAVE_REASON_EXITED    = 0x00000008,
+    BF_LEAVE_REASON_LOW_LEVEL = 0x00000010,
+};
+
 enum ChatRestrictionType
 {
     ERR_CHAT_RESTRICTED = 0,
