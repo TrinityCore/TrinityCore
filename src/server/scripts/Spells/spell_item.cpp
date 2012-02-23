c27a0333ceaeeb9c499f54670cbc40d4ce8cd9c5
Kandera <KanderaDev@gmail.com>
2012-02-23 12:06:43 -0500

    Core/Scripts: script despawn for dummy effect of muisek vessels

--- src/server/scripts/Spells/spell_item.cpp
+++ src/server/scripts/Spells/spell_item.cpp
@@ -1994,6 +1994,34 @@ class spell_item_refocus : public SpellScriptLoader
         }
 };
 
+class spell_item_muisek_vessel : public SpellScriptLoader
+{
+    public:
+        spell_item_muisek_vessel() : SpellScriptLoader("spell_item_muisek_vessel") { }
+
+        class spell_item_muisek_vessel_SpellScript : public SpellScript
+        {
+            PrepareSpellScript(spell_item_muisek_vessel_SpellScript);
+
+            void HandleDummy(SpellEffIndex /*effIndex*/)
+            {
+                if (Creature* target = GetHitCreature())
+                    if (target->isDead())
+                        target->ForcedDespawn();
+            }
+
+            void Register()
+            {
+                OnEffectHitTarget += SpellEffectFn(spell_item_muisek_vessel_SpellScript::HandleDummy, EFFECT_0, SPELL_EFFECT_DUMMY);
+            }
+        };
+
+        SpellScript* GetSpellScript() const
+        {
+            return new spell_item_muisek_vessel_SpellScript();
+        }
+};
+
 void AddSC_item_spell_scripts()
 {
     // 23074 Arcanite Dragonling
@@ -2045,4 +2073,5 @@ void AddSC_item_spell_scripts()
     new spell_item_unusual_compass();
     new spell_item_uded();
     new spell_item_chicken_cover();
+    new spell_item_muisek_vessel();
 }
