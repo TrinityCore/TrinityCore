ef30e75296701397503bb5435d6dbdc07b781301
Souler <soulerhyd@gmail.com>
2012-02-25 17:27:19 +0100

    Core/Arenas/Dalaran Sewers: Players who stay on the pipe after the battle has begun should be knocked into the arena.
    Also corrected the orientation of horde starting position

--- src/server/game/Battlegrounds/Zones/BattlegroundDS.h
+++ src/server/game/Battlegrounds/Zones/BattlegroundDS.h
@@ -42,12 +42,34 @@ enum BattlegroundDSObjects
     BG_DS_OBJECT_TYPE_BUFF_2    = 184664
 };
 
+enum BattlegroundDSCreatureTypes
+{
+    BG_DS_NPC_WATERFALL_KNOCKBACK = 0,
+    BG_DS_NPC_PIPE_KNOCKBACK_1    = 1,
+    BG_DS_NPC_PIPE_KNOCKBACK_2    = 2,
+    BG_DS_NPC_MAX                 = 3
+};
+
+enum BattlegroundDSCreatures
+{
+    BG_DS_NPC_TYPE_WATER_SPOUT    = 28567,
+};
+
+enum BattlegroundDSSpells
+{
+    BG_DS_SPELL_FLUSH             = 57405, // Visual and target selector for the starting knockback from the pipe
+    BG_DS_SPELL_FLUSH_KNOCKBACK   = 61698, // Knockback effect for previous spell (triggered, not need to be casted)
+};
+
 enum BattlegroundDSData
 { // These values are NOT blizzlike... need the correct data!
     BG_DS_WATERFALL_TIMER_MIN                    = 30000,
     BG_DS_WATERFALL_TIMER_MAX                    = 60000,
     BG_DS_WATERFALL_WARNING_DURATION             = 7000,
     BG_DS_WATERFALL_DURATION                     = 10000,
+    BG_DS_PIPE_KNOCKBACK_FIRST_DELAY             = 5000,
+    BG_DS_PIPE_KNOCKBACK_DELAY                   = 3000,
+    BG_DS_PIPE_KNOCKBACK_TOTAL_COUNT             = 2,
 
     BG_DS_WATERFALL_STATUS_WARNING               = 1, // Water starting to fall, but no LoS Blocking nor movement blocking
     BG_DS_WATERFALL_STATUS_ON                    = 2, // LoS and Movement blocking active
@@ -83,12 +105,18 @@ class BattlegroundDS : public Battleground
     private:
         uint32 _waterfallTimer;
         uint8 _waterfallStatus;
+        uint32 _pipeKnockBackTimer;
+        uint8 _pipeKnockBackCount;
 
         virtual void PostUpdateImpl(uint32 diff);
     protected:
         uint32 getWaterFallStatus() { return _waterfallStatus; };
-        void setWaterFallStatus(uint32 status) { _waterfallStatus = status; };
-        void setWaterFallTimer(uint32 timer) { _waterfallTimer = timer; };
+        void setWaterFallStatus(uint8 status) { _waterfallStatus = status; };
         uint32 getWaterFallTimer() { return _waterfallTimer; };
+        void setWaterFallTimer(uint32 timer) { _waterfallTimer = timer; };
+        uint8 getPipeKnockBackCount() { return _pipeKnockBackCount; };
+        void setPipeKnockBackCount(uint8 count) { _pipeKnockBackCount = count; };
+        uint32 getPipeKnockBackTimer() { return _pipeKnockBackTimer; };
+        void setPipeKnockBackTimer(uint32 timer) { _pipeKnockBackTimer = timer; };
 };
 #endif
