4336928e2ec554f32730d78b8697172e3047e0f7
Subv <s.v.h21@hotmail.com>
2012-03-04 08:23:19 -0500

    Core/Collisions: Reverted a commit about M2 models. closes #5312 and probably others

--- src/server/collision/Models/WorldModel.cpp
+++ src/server/collision/Models/WorldModel.cpp
@@ -421,9 +421,6 @@ namespace VMAP
 
     bool WorldModel::IntersectRay(const G3D::Ray &ray, float &distance, bool stopAtFirstHit) const
     {
-        // M2 models are not taken into account for LoS calculation
-        if (Flags & MOD_M2)
-            return false;
         // small M2 workaround, maybe better make separate class with virtual intersection funcs
         // in any case, there's no need to use a bound tree if we only have one submodel
         if (groupModels.size() == 1)
