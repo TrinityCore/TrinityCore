1776b16773cbff1262ef62268b7a6827ac002c07
Discover- <amort11@hotmail.com>
2012-03-04 13:19:47 +0100

    DB/Misc: Fix SQL of c48a1ad17db9b623795d24bbe83d77247b6ef756

--- sql/updates/world/2012_03_04_00_world_misc.sql
+++ sql/updates/world/2012_03_04_00_world_misc.sql
@@ -4,10 +4,10 @@ INSERT INTO `creature_template_addon` (`entry`,`mount`,`bytes1`,`bytes2`,`emote`
 (19913,0,0,0,0,'39312');
 
 -- Scriptnames for areatriggers
-SET @AT_AREA_52_SOUTHSET := 4472;
-SET @AT_AREA_52_NORTHSET := 4466;
-SET @AT_AREA_52_WESTSET := 4471;
-SET @AT_AREA_52_EASTSET := 4422;
-DELETE FROM `areatrigger_scripts` WHERE `entry` IN (@AT_AREA_52_SOUTH,@AT_AREA_52_NORTH,@AT_AREA_52_WEST,@AT_AREA_52_EAST)
+SET @AT_AREA_52_SOUTH := 4472;
+SET @AT_AREA_52_NORTH := 4466;
+SET @AT_AREA_52_WEST := 4471;
+SET @AT_AREA_52_EAST := 4422;
+DELETE FROM `areatrigger_scripts` WHERE `entry` IN (@AT_AREA_52_SOUTH,@AT_AREA_52_NORTH,@AT_AREA_52_WEST,@AT_AREA_52_EAST);
 INSERT INTO `areatrigger_scripts` (`entry`,`ScriptName`) VALUES
 (@AT_AREA_52_SOUTH,"at_area_52_entrance"),(@AT_AREA_52_NORTH,"at_area_52_entrance"),(@AT_AREA_52_WEST,"at_area_52_entrance"),(@AT_AREA_52_EAST,"at_area_52_entrance");
