45946e23bac57b1084d7bd42daff1445cb8957e2
click <click@gonnamakeyou.com>
2012-03-04 21:38:57 +0100

    Core: Adjust parameter output values to avoid excessive warning outputs on GCC and some other minor warnings

--- src/server/scripts/Spells/spell_dk.cpp
+++ src/server/scripts/Spells/spell_dk.cpp
@@ -769,7 +769,7 @@ class spell_dk_death_grip : public SpellScriptLoader
         {
             PrepareSpellScript(spell_dk_death_grip_SpellScript);
 
-            void HandleDummy(SpellEffIndex effIndex)
+            void HandleDummy(SpellEffIndex /*effIndex*/)
             {
                 int32 damage = GetEffectValue();
                 Position const* pos = GetTargetDest();
