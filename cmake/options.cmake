7bd9995172931181051ffcbaa0ab9552ae5ac6ea
Shauren <shauren.trinity@gmail.com>
2012-03-04 22:43:13 +0100

    Buildsystem/CMake: Changed TOOLS building back to off by default

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -10,7 +10,7 @@
 
 option(SERVERS          "Build worldserver and authserver"                            1)
 option(SCRIPTS          "Build core with scripts included"                            1)
-option(TOOLS            "Build map/vmap extraction/assembler tools"                   1)
+option(TOOLS            "Build map/vmap extraction/assembler tools"                   0)
 option(USE_SCRIPTPCH    "Use precompiled headers when compiling scripts"              1)
 option(USE_COREPCH      "Use precompiled headers when compiling servers"              1)
 option(USE_SFMT         "Use SFMT as random numbergenerator"                          1)
