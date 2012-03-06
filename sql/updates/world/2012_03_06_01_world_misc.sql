ea28c6feecb90340926f3302b108350364272850
leak <leak@bitmx.net>
2012-03-06 20:34:09 +0100

    DB/Schema: Convert all world db tables to use MYISAM storage engine

---
+++ sql/updates/world/2012_03_06_01_world_misc.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `warden_checks` ENGINE=MYISAM;
+ALTER TABLE `achievement_dbc` ENGINE=MYISAM;
+ALTER TABLE `character_queststatus_seasonal` ENGINE=MYISAM;
+ALTER TABLE `creature_text` ENGINE=MYISAM;
+ALTER TABLE `game_event_seasonal_questrelation` ENGINE=MYISAM;
