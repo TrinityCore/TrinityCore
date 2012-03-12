d1737b73d476063aca3829fe8334ab5612ce9b4b
root <root@wow.(none)>
2012-03-12 13:54:48 +0100

    Quest 2118 adjustment

--- sql/updates/world/2012_03_12_00_world_gameobject_template.sql
+++ sql/updates/world/2012_03_12_00_world_gameobject_template.sql
@@ -1,1 +1,1 @@
-UPDATE `gameobject_template` SET `data2`=1 WHERE  `entry`=109515 LIMIT 1;
+UPDATE `gameobject_template` SET `data2`=5 WHERE  `entry`=109515 LIMIT 1;
--- sql/updates/world/2012_03_12_00_world_spell_script_names.sql
+++ sql/updates/world/2012_03_12_00_world_spell_script_names.sql
@@ -1,2 +1,2 @@
 DELETE FROM `spell_script_names` WHERE `spell_id`=9439;
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (9439, 'spell_bear_trap_capture');
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (9439, 'spell_bear_capture_trap');
