0411dc80be6d89fbb5a7503323bbbff0462398f2
cemak <mightyhaza@yandex.ru>
2012-03-17 19:08:09 +0400

    sorry my bad
    Signed-off-by: cemak <mightyhaza@yandex.ru>

--- src/server/scripts/Northrend/FrozenHalls/PitOfSaron/pit_of_saron.cpp
+++ src/server/scripts/Northrend/FrozenHalls/PitOfSaron/pit_of_saron.cpp
@@ -880,7 +880,7 @@ class at_pit_of_saron_start : public AreaTriggerScript
         bool OnTrigger(Player* player, AreaTriggerEntry const* areaTrigger)
         {
             InstanceScript* instance = player->GetInstanceScript();
-            if(instance->GetData(DATA_TYRANNUS_START) == IN_PROGRESS && instance->GetData(DATA_TYRANNUS_START) == DONE || player->isGameMaster() || !instance)
+            if(instance->GetData(DATA_TYRANNUS_START) == IN_PROGRESS || instance->GetData(DATA_TYRANNUS_START) == DONE || player->isGameMaster() || !instance)
                       return false;
                       
                instance->SetData(DATA_TYRANNUS_START, IN_PROGRESS);
