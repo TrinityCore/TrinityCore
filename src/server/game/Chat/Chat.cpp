9ed40014fd5819d3ff1bc697aaaca9add5dbaad4
Shauren <shauren.trinity@gmail.com>
2012-03-24 16:44:01 +0100

    Core/DBLayer: Match field type reads in core with data types in db for prepared statements

--- src/server/game/Chat/Chat.cpp
+++ src/server/game/Chat/Chat.cpp
@@ -455,7 +455,7 @@ ChatCommand* ChatHandler::getCommandTable()
                 Field* fields = result->Fetch();
                 std::string name = fields[0].GetString();
 
-                SetDataForCommandInTable(commandTableCache, name.c_str(), fields[1].GetUInt16(), fields[2].GetString(), name);
+                SetDataForCommandInTable(commandTableCache, name.c_str(), fields[1].GetUInt8(), fields[2].GetString(), name);
 
             } while (result->NextRow());
         }
