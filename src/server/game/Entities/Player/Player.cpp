24798414bf9ce563784a852725c1a66219fc0b0c
Shauren <shauren.trinity@gmail.com>
2012-03-28 00:03:11 +0200

    Core/Players: Fixed copy paste mistake

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -19531,7 +19531,6 @@ void Player::_SaveStats(SQLTransaction& trans)
     stmt->setUInt32(index++, GetUInt32Value(UNIT_FIELD_RANGED_ATTACK_POWER));
     stmt->setUInt32(index++, GetBaseSpellPowerBonus());
     stmt->setUInt32(index++, GetUInt32Value(PLAYER_FIELD_COMBAT_RATING_1 + CR_CRIT_TAKEN_SPELL));
-    stmt->setUInt32(index++, GetMaxHealth());
 
     trans->Append(stmt);
 }
