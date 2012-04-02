a072e270866551fec39a8373bd199bba1eabff11
Shutok <shutok2@hotmail.com>
2012-04-02 11:12:33 +0300

    Update src/server/shared/SystemConfig.h

--- src/server/shared/SystemConfig.h
+++ src/server/shared/SystemConfig.h
@@ -24,7 +24,7 @@
 #include "Define.h"
 #include "revision.h"
 
-#define _PACKAGENAME "Core: Flame Core\nBased on: Jacob Core\nMain Developer: Denis aka jacob\n"
+#define _PACKAGENAME "Servidor Encendido"
 
 #if TRINITY_ENDIAN == TRINITY_BIGENDIAN
 # define _ENDIAN_STRING "big-endian"
