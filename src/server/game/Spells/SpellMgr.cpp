a0744978804f7e77b61c683712bacb1cd632ed2c
Nay <dnpd.dd@gmail.com>
2012-04-06 19:33:32 +0100

    Core&DB/Spells: Fix Warr T8 4p bonus
    - Added missing spell to DB
    - Corrected DBC data that is, very likely, wrong
    Closes #2386

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -3238,7 +3238,8 @@ void SpellMgr::LoadDbcDataCorrections()
                 spellInfo->EffectDieSides[0] = 0; // was 1, that should probably mean seat 0, but instead it's treated as spell 1
                 spellInfo->EffectBasePoints[0] = 52391; // Ride Vehicle (forces seat 0)
                 break;
-            case 64745:
+            case 64745: // Item - Death Knight T8 Tank 4P Bonus
+            case 64936: // Item - Warrior T8 Protection 4P Bonus
                 spellInfo->EffectBasePoints[0] = 100; // 100% chance of procc'ing, not -10% (chance calculated in PrepareTriggersExecutedOnHit)
                 break;
             case 19970: // Entangling Roots (Rank 6) -- Nature's Grasp Proc
