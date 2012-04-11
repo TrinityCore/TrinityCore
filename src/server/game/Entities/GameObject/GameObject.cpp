a998e9e58a42a1a6ad38fc13da1968c9ee6a4b31
Subv <s.v.h21@hotmail.com>
2012-04-10 20:12:13 -0500

    Collision/GameObjects: Corrected a possible logic fail
    Signed-off-by: Subv <s.v.h21@hotmail.com>

--- src/server/game/Entities/GameObject/GameObject.cpp
+++ src/server/game/Entities/GameObject/GameObject.cpp
@@ -135,7 +135,7 @@ void GameObject::AddToWorld()
         sObjectAccessor->AddObject(this);
         bool startOpen = (GetGoType() == GAMEOBJECT_TYPE_DOOR || GetGoType() == GAMEOBJECT_TYPE_BUTTON ? GetGOInfo()->door.startOpen : false);
         // The state can be changed after GameObject::Create but before GameObject::AddToWorld
-        bool toggledState = GetGoState() == GO_STATE_READY;
+        bool toggledState = GetGOData() ? GetGOData()->go_state == GO_STATE_READY : false;
         if (m_model)
             GetMap()->Insert(*m_model);
         if (startOpen ^ toggledState)
