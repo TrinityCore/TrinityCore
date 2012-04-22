ec6fd069ecfdf1bb562238b2a2349236e0c681a3
Faq <ainarsh@gmail.com>
2012-04-22 18:07:50 +0300

    [gc13] Libram of Tolerance & Libram of the Lightbringer

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -3234,6 +3234,10 @@ void SpellMgr::LoadDbcDataCorrections()
                         // this needs research on modifier applying rules, does not seem to be in Attributes fields
                 spellInfo->EffectSpellClassMask[0] = flag96(0x00000040, 0x00000000, 0x00000000);
                 break;
+            case 34231: // Increased Holy Light Healing (Item - Libram of the Lightbringer)
+            case 60792: // Libram of Tolerance
+                spellInfo->EffectSpellClassMask[0] = flag96(0x80000000, 0x00000000, 0x00000000);
+                break;
             case 63163: // Apply Enchanted Bridle (Argent Tournament)
                 spellInfo->EffectDieSides[0] = 0; // was 1, that should probably mean seat 0, but instead it's treated as spell 1
                 spellInfo->EffectBasePoints[0] = 52391; // Ride Vehicle (forces seat 0)
--- src/server/shared/revision_gc.h
+++ src/server/shared/revision_gc.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_gc_H__
 #define __REVISION_gc_H__
- #define REVISION_gc "12"
+ #define REVISION_gc "13"
 #endif // __REVISION_gc_H__
