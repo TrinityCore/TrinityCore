4a53d13a0dd1670107660ddf047fb0719eee5777
Shauren <shauren.trinity@gmail.com>
2012-04-24 17:19:41 +0200

    Core/Vehicles: Fixed creature passengers falling off vehicles at client side

--- src/server/game/Entities/Vehicle/Vehicle.cpp
+++ src/server/game/Entities/Vehicle/Vehicle.cpp
@@ -285,9 +285,6 @@ void Vehicle::InstallAccessory(uint32 entry, int8 seatId, bool minion, uint8 typ
         //    return;         // Something went wrong in the spellsystem
         //}
 
-        // This is not good, we have to send update twice
-        accessory->SendMovementFlagUpdate();
-
         if (GetBase()->GetTypeId() == TYPEID_UNIT)
             sScriptMgr->OnInstallAccessory(this, accessory);
     }
@@ -352,6 +349,7 @@ bool Vehicle::AddPassenger(Unit* unit, int8 seatId)
     unit->m_movementInfo.t_pos.m_orientation = 0;
     unit->m_movementInfo.t_time = 0; // 1 for player
     unit->m_movementInfo.t_seat = seat->first;
+    unit->m_movementInfo.t_guid = _me->GetGUID();
 
     if (_me->GetTypeId() == TYPEID_UNIT
         && unit->GetTypeId() == TYPEID_PLAYER
