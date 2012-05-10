4aa2032cd4ea0647168c3bdedb09172c9915ac64
Discover- <amort11@hotmail.com>
2012-05-10 13:15:09 +0200

    Database/Errors: Fix another SQL error from b5ff1633541bb1d7d2c4f2efd26a8a53544ae4eb :thumbsdown:

--- sql/updates/world/2012_05_09_03_world_misc.sql
+++ sql/updates/world/2012_05_09_03_world_misc.sql
@@ -13,7 +13,7 @@ SET @SPELL3  := 45104; -- Shadow Channelling
 DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (@ENTRY,0,0,0,0,0,100,6,100,100,3000,3000,11,@SPELL1,0,0,0,0,0,2,0,0,0,0,0,0,0, 'Deathwhisper Necrolyte - Combat - Cast Shadow Bolt'),
-(@ENTRY,0,1,0,0,0,100,6,9000,9000,24000,24000,11,@SPELL2,0,0,0,0,0,5,0,0,0,0,0,0,0, 'Deathwhisper Necrolyte - Combat - Cast Conversion Beam'),
+(@ENTRY,0,1,0,0,0,100,6,9000,9000,24000,24000,11,@SPELL2,0,0,0,0,0,5,0,0,0,0,0,0,0, 'Deathwhisper Necrolyte - Combat - Cast Conversion Beam');
 
 -- Deathwhisper Necrolyte pathing
 SET @NPC := 202231;
