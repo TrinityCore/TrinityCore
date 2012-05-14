TrinityCore/TrinityCore

commit 72d00c476ca6b6e2d48f5fdfaae48bf383e44ef0

Core/scripts: Add spell enums for boss sara.

src/server/scripts/Northrend/Ulduar/Ulduar/boss_yoggsaron.cpp
@@ -18,12 +18,27 @@
 #include "ScriptMgr.h"
 #include "ulduar.h"
 
-enum Sara_Yells
+enum Sara
 {
+    // text
     YELL_SARA_PREFIGHT                          = 0,
     YELL_COMBAT_PHASE_1                         = 1,
     YELL_COMBAT_PHASE_2                         = 2,
     YELL_SLAY                                   = 3,
+
+    // Phase 1 spells
+    SPELL_SARAS_BLESSING                        = 63745, // Target Self
+    SPELL_SARAS_ANGER                           = 63147, // Target Entry 33136
+    SPELL_SARAS_ANGER                           = 63744, // Target Entry 33136
+    SPELL_SARAS_FEVOR                           = 63138, // Target Player
+    SPELL_SARAS_FEVOR                           = 63747, // Target Player
+    SPELL_SARAS_BLESSING                        = 63134, // Target Player
+
+    // Phase 2 spells
+    SPELL_PHYCHOSIS                             = 63795, // Target Self
+    SPELL_MALADY_OF_THE_MIND                    = 63830, // Target Self
+    SPELL_DEATH_RAY                             = 63891, // Target Self
+    SPELL_BRAIN_LINK                            = 63802, // Target Self
 };
 
 enum YoggSaron_Yells

commit 19a9edcf7ea1fa0f2231740ae2a307fbbd3c1c23

Fix compile problems caused by 72d00c476ca6b6e2d48f5fdfaae48bf383e44ef0

src/server/scripts/Northrend/Ulduar/Ulduar/boss_yoggsaron.cpp
@@ -27,12 +27,12 @@ enum Sara
     YELL_SLAY                                   = 3,
 
     // Phase 1 spells
-    SPELL_SARAS_BLESSING                        = 63745, // Target Self
-    SPELL_SARAS_ANGER                           = 63147, // Target Entry 33136
-    SPELL_SARAS_ANGER                           = 63744, // Target Entry 33136
-    SPELL_SARAS_FEVOR                           = 63138, // Target Player
-    SPELL_SARAS_FEVOR                           = 63747, // Target Player
-    SPELL_SARAS_BLESSING                        = 63134, // Target Player
+    SPELL_SARAS_ANGER_1                         = 63147, // Target Entry 33136
+    SPELL_SARAS_ANGER_2                         = 63744, // Target Entry 33136
+    SPELL_SARAS_FEVOR_1                         = 63138, // Target Player
+    SPELL_SARAS_FEVOR_2                         = 63747, // Target Player
+    SPELL_SARAS_BLESSING_1                      = 63134, // Target Player
+    SPELL_SARAS_BLESSING_2                      = 63745, // Target Self
 
     // Phase 2 spells
     SPELL_PHYCHOSIS                             = 63795, // Target Self