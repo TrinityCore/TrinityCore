124b650808f9ced66d48f5a290814be25ce345f2
Valcorb <valcorb@hotmail.com>
2012-05-28 02:43:41 +0200

    DB\Errors: Fix DB errors from previous commit(s)

---
+++ sql/updates/world/2012_05_28_world_creature_ai_scripts.sql
@@ -0,0 +1,3 @@
+-- Fix recent DB errors
+DELETE FROM `creature_ai_scripts` WHERE `creature_id`=27135; -- Attunement To Dalaran Kill Credit
+DELETE FROM `creature_ai_scripts` WHERE `creature_id`=32400; -- Tukemuth
