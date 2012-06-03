e1f97d94c60f3968f43011ecbb9a61f0720e17b5
Nay <dnpd.dd@gmail.com>
2012-06-03 22:08:10 +0100

    DB/Quests: Update OfferRewardText and RequestItemsText for some quests
    P.S This introduces less new content than what it appears to be, most
     updates are simple typo fixes or removing or adding an extra space

--- sql/updates/world/2012_06_03_00_world_sai.sql
+++ sql/updates/world/2012_06_03_00_world_sai.sql
@@ -3,7 +3,7 @@ SET @CARPET := 25460;
 SET @Q_CarpetRide := 11636;
 
 DELETE FROM `smart_scripts` WHERE `entryorguid` IN (@YANNI,@CARPET) AND `source_type`=0;
-DELETE FROM `smart_scripts` WHERE `entryorguid` IN (@CARPET*100,@CARPET*101,@CARPET*100+1) AND `source_type`=9;
+DELETE FROM `smart_scripts` WHERE `entryorguid` IN (@CARPET*100,@CARPET*100+1) AND `source_type`=9;
 INSERT INTO `smart_scripts`(`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`)VALUES
 (@YANNI,0,0,0,62,0,100,0,9162,0,0,0,11,45600,0,0,0,0,0,7,0,0,0,0,0,0,0, 'Yanni - Cast Call of the Carpet on player'),
 (@YANNI,0,1,0,62,0,100,0,9162,0,0,0,72,0,0,0,0,0,0,7,0,0,0,0,0,0,0, 'Yanni - On Gossip option - Close gossip'),
