a45a287a5aa906786a65545437d614edc4736861
LordJZ <a553r7fa1l3d@gmail.com>
2012-06-05 15:24:15 +0400

    Load art.MPQ in mapextractor

--- src/tools/map_extractor/System.cpp
+++ src/tools/map_extractor/System.cpp
@@ -79,6 +79,7 @@ uint32 CONF_TargetBuild = 15595;              // 4.3.4.15595
 char const* CONF_mpq_list[]=
 {
     "world.MPQ",
+    "art.MPQ",
     "world2.MPQ",
     "expansion1.MPQ",
     "expansion2.MPQ",
