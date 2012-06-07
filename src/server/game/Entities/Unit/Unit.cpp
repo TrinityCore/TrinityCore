TrinityCore/TrinityCore

commit fda086609ca3f352accb3d5ac8c8942fe7593a91

Core/Spells: do not apply unit_state_casting for instant cast spells.

src/server/game/Entities/Unit/Unit.cpp
@@ -2893,7 +2893,8 @@ void Unit::SetCurrentCastedSpell(Spell* pSpell)
                     InterruptSpell(CURRENT_AUTOREPEAT_SPELL);
                 m_AutoRepeatFirstCast = true;
             }
-            AddUnitState(UNIT_STATE_CASTING);
+            if (m_currentSpells[CURRENT_GENERIC_SPELL]->m_spellInfo->CalcCastTime(this) > 0 && IsNonMeleeSpellCasted(false, false, true))
+                AddUnitState(UNIT_STATE_CASTING);
         } break;
 
         case CURRENT_CHANNELED_SPELL:

commit da231bca0949935ccc1c1668083cd270a11f0e76

Core/Spells: fix crash introduced in fda086609ca3f352accb3d5ac8c8942fe7593a91 (thx vincent-michael)

src/server/game/Entities/Unit/Unit.cpp
@@ -2893,7 +2893,7 @@ void Unit::SetCurrentCastedSpell(Spell* pSpell)
                     InterruptSpell(CURRENT_AUTOREPEAT_SPELL);
                 m_AutoRepeatFirstCast = true;
             }
-            if (m_currentSpells[CURRENT_GENERIC_SPELL]->m_spellInfo->CalcCastTime(this) > 0 && IsNonMeleeSpellCasted(false, false, true))
+            if ((m_currentSpells[CURRENT_GENERIC_SPELL] && m_currentSpells[CURRENT_GENERIC_SPELL]->m_spellInfo->CalcCastTime(this) > 0) && IsNonMeleeSpellCasted(false, false, true))
                 AddUnitState(UNIT_STATE_CASTING);
         } break;
 