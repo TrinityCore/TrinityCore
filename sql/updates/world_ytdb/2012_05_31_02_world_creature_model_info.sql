2f60bebcc8ced65f2237cb94599a111c4770d1ae
SignFinder <signfinder@gmail.com>
2012-06-14 14:02:25 +0400

    DB/YTDB: Add needed for YTDB623 updates.

--- sql/updates/world_ytdb/2012_05_31_02_world_creature_model_info.sql
+++
@@ -1,4 +0,0 @@
--- Grunt Ounda should only have a female model
-UPDATE `creature_model_info` SET
- `modelid_other_gender` = 0
-WHERE `modelid` = 4260;
--- sql/updates/world_ytdb/2012_05_31_10_world_creature_template.sql
+++
@@ -1,2 +0,0 @@
--- update the book to always drop of ragnaros
-UPDATE `creature_loot_template` SET `ChanceOrQuestChance`=-100 WHERE `entry`=11502 AND `item`=21110;
