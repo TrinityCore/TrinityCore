f63328dcbe5d3490c8b0235cf0c92bc1628d1aea
Kandera <KanderaDev@gmail.com>
2012-06-22 12:23:53 -0400

    Core/LFG: commented out a previous change that was causing issues with lfg dungeon locks.

--- src/server/game/DungeonFinding/LFGMgr.cpp
+++ src/server/game/DungeonFinding/LFGMgr.cpp
@@ -433,8 +433,8 @@ void LFGMgr::InitializeLockedDungeons(Player* player)
             locktype = LFG_LOCKSTATUS_RAID_LOCKED;
         else if (dungeon->difficulty > DUNGEON_DIFFICULTY_NORMAL && player->GetBoundInstance(dungeon->map, Difficulty(dungeon->difficulty)))
         {
-            if (!player->GetGroup() || !player->GetGroup()->isLFGGroup() || GetDungeon(player->GetGroup()->GetGUID(), true) != dungeon->ID || GetState(player->GetGroup()->GetGUID()) != LFG_STATE_DUNGEON)
-                locktype = LFG_LOCKSTATUS_RAID_LOCKED;
+            //if (!player->GetGroup() || !player->GetGroup()->isLFGGroup() || GetDungeon(player->GetGroup()->GetGUID(), true) != dungeon->ID || GetState(player->GetGroup()->GetGUID()) != LFG_STATE_DUNGEON)
+            locktype = LFG_LOCKSTATUS_RAID_LOCKED;
         }
         else if (dungeon->minlevel > level)
             locktype = LFG_LOCKSTATUS_TOO_LOW_LEVEL;
