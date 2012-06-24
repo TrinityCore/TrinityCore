ab878d99716cb6cc5897459fbcfbda07564ee242
Malcrom <malcromdev@gmail.com>
2012-06-24 19:45:58 -0230

    DB/Core/Gossip: Add Gossip for Alliance & Horde bonfires, fix gameobject faction, fix quest racerequirements, fix spell 45437 to be cast upon bonfire. Spawning left to do.

--- src/server/scripts/World/go_scripts.cpp
+++ src/server/scripts/World/go_scripts.cpp
@@ -1275,6 +1275,28 @@ public:
     }
 };
 
+/*######
+## go_midsummer_bonfire
+######*/
+
+enum eMidsummerBonfire
+{
+    STAMP_OUT_BONFIRE_QUEST_COMPLETE    = 45458,
+};
+
+class go_midsummer_bonfire : public GameObjectScript
+{
+public:
+    go_midsummer_bonfire() : GameObjectScript("go_midsummer_bonfire") { }
+
+    bool OnGossipSelect(Player* player, GameObject* /*go*/, uint32 /*sender*/, uint32 /*action*/)
+    {
+        player->CastSpell(player, STAMP_OUT_BONFIRE_QUEST_COMPLETE, true);
+        player->CLOSE_GOSSIP_MENU();
+        return false;
+    }
+};
+
 void AddSC_go_scripts()
 {
     new go_cat_figurine;
@@ -1316,4 +1338,5 @@ void AddSC_go_scripts()
     new go_large_gjalerbron_cage;
     new go_veil_skith_cage;
     new go_frostblade_shrine;
+    new go_midsummer_bonfire;
 }
