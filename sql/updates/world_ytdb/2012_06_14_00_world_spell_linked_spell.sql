f2970c1962c3b89b501f227238b71cc0f94c7637
SignFinder <signfinder@gmail.com>
2012-06-27 12:48:11 +0400

    DB/YTDB: Add needed for YTDB624 updates.

--- sql/updates/world_ytdb/2012_06_14_00_world_spell_linked_spell.sql
+++
@@ -1,5 +0,0 @@
--- Fix Surge of Adrenaline
-DELETE FROM `spell_linked_spell` WHERE `spell_effect`=68667;
-INSERT INTO `spell_linked_spell` (`spell_trigger`,`spell_effect`,`type`,`comment`) VALUES
-(-66683,68667,0, 'Icehowl - Surge of Adrenaline'),
-(-67660,68667,0, 'Icehowl - Surge of Adrenaline');
--- sql/updates/world_ytdb/2012_06_14_05_world_spelldifficulty_dbc.sql
+++
@@ -1,6 +0,0 @@
-SET @DIFF := 3268; -- set by TDB team
-DELETE FROM `spelldifficulty_dbc` WHERE `id` BETWEEN @DIFF+0 AND @DIFF+2;
-INSERT INTO `spelldifficulty_dbc` (`id`,`spellid0`,`spellid1`,`spellid2`,`spellid3`) VALUES
-(@DIFF+0,50811,61547,0,0),
-(@DIFF+1,57762,59990,0,0),
-(@DIFF+2,57763,61489,0,0);
