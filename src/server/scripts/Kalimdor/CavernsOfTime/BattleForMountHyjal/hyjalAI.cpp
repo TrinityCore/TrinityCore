f34bbf520ee70497af754e50330fe5057a1e0360
Subv <s.v.h21@hotmail.com>
2012-07-01 16:36:12 -0500

    Core/Scripts: More missing includes

--- src/server/scripts/Kalimdor/CavernsOfTime/BattleForMountHyjal/hyjalAI.cpp
+++ src/server/scripts/Kalimdor/CavernsOfTime/BattleForMountHyjal/hyjalAI.cpp
@@ -27,6 +27,7 @@ EndScriptData */
 #include "ScriptedCreature.h"
 #include "ScriptedEscortAI.h"
 #include "GridNotifiers.h"
+#include "GridNotifiersImpl.h"
 #include "Cell.h"
 #include "CellImpl.h"
 #include "hyjalAI.h"
