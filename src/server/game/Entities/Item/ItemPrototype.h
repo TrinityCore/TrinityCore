7e2441d06c492594ca4e9ef6ef18b79464eae61a
Kandera <KanderaDev@gmail.com>
2012-07-05 13:18:40 -0400

    Core/Loot: fix a typo from previous commit. (thx joschiwald)

--- src/server/game/Entities/Item/ItemPrototype.h
+++ src/server/game/Entities/Item/ItemPrototype.h
@@ -196,7 +196,7 @@ enum ItemFlagsCustom
 {
     ITEM_FLAGS_CU_DURATION_REAL_TIME    = 0x0001,   // Item duration will tick even if player is offline
     ITEM_FLAGS_CU_IGNORE_QUEST_STATUS   = 0x0002,   // No quest status will be checked when this item drops
-    ITEM_FLAGS_CU_FOLLOW_LOOT_RULES     = 0x0003,   // Item will always follow group/master/need before greed looting rules
+    ITEM_FLAGS_CU_FOLLOW_LOOT_RULES     = 0x0004,   // Item will always follow group/master/need before greed looting rules
 };
 
 enum BAG_FAMILY_MASK
