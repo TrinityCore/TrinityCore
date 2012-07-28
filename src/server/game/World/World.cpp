ba3fc70c091525adb3347e287e865746372f269b
blaskred <detso@rambler.ru>
2012-07-29 02:38:28 +0600

    LFG
    Signed-off-by: blaskred <detso@rambler.ru>

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -1218,6 +1218,9 @@ void World::LoadConfigSettings(bool reload)
     m_bool_configs[CONFIG_OUTDOORPVP_WINTERGRASP_ANTIFARM_ENABLE]  = ConfigMgr::GetBoolDefault("OutdoorPvP.Wintergrasp.Antifarm.Enable", false);
     m_int_configs[CONFIG_OUTDOORPVP_WINTERGRASP_ANTIFARM_ATK]  = ConfigMgr::GetIntDefault("OutdoorPvP.Wintergrasp.Antifarm.Atk", 5);
     m_int_configs[CONFIG_OUTDOORPVP_WINTERGRASP_ANTIFARM_DEF]  = ConfigMgr::GetIntDefault("OutdoorPvP.Wintergrasp.Antifarm.Def", 5);
+	
+     // Management for channels with flag CHANNEL_DBC_FLAG_CITY_ONLY.  
+    m_bool_configs[CONFIG_CHANNEL_ON_CITY_ONLY_FLAG]  = ConfigMgr::GetBoolDefault("Channel.CityOnlyFlag", true);
 
     // call ScriptMgr if we're reloading the configuration
     if (reload)
