230d8ea0d43a2055f446087989fa75ffc434bb46
Nay <dnpd.dd@gmail.com>
2012-08-03 22:55:18 +0100

    Whitespace is the root of all evil (version 2)

--- src/server/scripts/Commands/cs_disable.cpp
+++ src/server/scripts/Commands/cs_disable.cpp
@@ -14,27 +14,27 @@
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
 /* ScriptData
 Name: disable_commandscript
 %Complete: 100
 Comment: All disable related commands
 Category: commandscripts
 EndScriptData */
- 
+
 #include "ScriptMgr.h"
 #include "ObjectMgr.h"
 #include "Chat.h"
 #include "DisableMgr.h"
- 
+
 class disable_commandscript : public CommandScript
 {
 public:
     disable_commandscript() : CommandScript("disable_commandscript") { }
- 
+
     ChatCommand* GetCommands() const
     {
- 
+
         static ChatCommand disableCommandTable[] =
         {
             { "spell",          SEC_GAMEMASTER,     false, &HandleDisableSpellCommand,         "", NULL },
@@ -45,7 +45,7 @@ public:
             { "vmap",           SEC_GAMEMASTER,     false, &HandleDisableVmapCommand,          "", NULL },
             { NULL,             0,                  false, NULL,                               "", NULL }
         };
- 
+
         static ChatCommand commandTable[] =
         {
             { "disable",        SEC_GAMEMASTER,     false, NULL,                "", disableCommandTable },
@@ -53,8 +53,8 @@ public:
         };
         return commandTable;
     }
- 
-   
+
+
     static void HandleDisables(ChatHandler* handler, char const* args, uint8 disableType)
     {
         char* cEntry = strtok((char*)args, " ");
@@ -63,30 +63,30 @@ public:
             handler->SendSysMessage("No entry specified.");
             return;
         }
- 
+
         char* cFlags = strtok(NULL, " ");
         if (!cFlags || !atoi(cFlags))
         {
             handler->SendSysMessage("No flags specified.");
             return;
         }
-       
+
         char* cComment = strtok(NULL, "");
         if (!cComment)
         {
             handler->SendSysMessage("No comment specified.");
             return;
         }
- 
+
         std::string entryStr = cEntry;
         std::string disableComment = cComment;
         uint32 entry = (uint32)atoi(cEntry);
         uint8 flags = atoi(cFlags);
- 
-       
- 
+
+
+
         std::string disableTypeStr = "";
- 
+
         switch (disableType)
         {
             case DISABLE_TYPE_SPELL:
@@ -108,87 +108,87 @@ public:
                 disableTypeStr = "vmap";
                 break;
         }
-       
+
         PreparedStatement* stmt = WorldDatabase.GetPreparedStatement(WORLD_SEL_DISABLES);
         stmt->setUInt32(0, entry);
-        stmt->setUInt8(1, disableType);  
+        stmt->setUInt8(1, disableType);
         PreparedQueryResult result = WorldDatabase.Query(stmt);
-       
- 
+
+
         if (result)
         {
             handler->PSendSysMessage("This %s (id %u) is already disabled.", disableTypeStr.c_str(), entry);
             return;
         }
-     
+
         PreparedStatement* stmt2 = WorldDatabase.GetPreparedStatement(WORLD_INS_DISABLES);
         stmt2->setUInt32(0, entry);
         stmt2->setUInt8(1, disableType);
         stmt2->setUInt16(2, flags);
         stmt2->setString(3, disableComment);
         WorldDatabase.Execute(stmt2);
-               
+
         handler->PSendSysMessage("Disabled %s %u for reason %s", disableTypeStr.c_str(), entry, disableComment.c_str());
         return;
-       
+
     }
-   
+
     static bool HandleDisableSpellCommand(ChatHandler* handler, char const* args)
     {
         if (!*args)
             return false;
-       
+
         HandleDisables(handler, args, DISABLE_TYPE_SPELL);
         return true;
     }
- 
+
     static bool HandleDisableMapCommand(ChatHandler* handler, char const* args)
     {
         if (!*args)
             return false;
-       
+
         HandleDisables(handler, args, DISABLE_TYPE_MAP);
         return true;
     }
- 
+
     static bool HandleDisableBattlegroundCommand(ChatHandler* handler, char const* args)
     {
         if (!*args)
             return false;
-       
+
         HandleDisables(handler, args, DISABLE_TYPE_BATTLEGROUND);
         return true;
     }
- 
+
     static bool HandleDisableAchievementCriteriaCommand(ChatHandler* handler, char const* args)
     {
         if (!*args)
             return false;
-       
+
         HandleDisables(handler, args, DISABLE_TYPE_ACHIEVEMENT_CRITERIA);
         return true;
     }
- 
+
     static bool HandleDisableOutdoorPvPCommand(ChatHandler* handler, char const* args)
     {
         if (!*args)
             return false;
-       
+
         HandleDisables(handler, args, DISABLE_TYPE_OUTDOORPVP);
         return true;
     }
- 
+
     static bool HandleDisableVmapCommand(ChatHandler* handler, char const* args)
     {
         if (!*args)
             return false;
-       
+
         HandleDisables(handler, args, DISABLE_TYPE_VMAP);
         return true;
     }
- 
+
 };
- 
+
 void AddSC_disable_commandscript()
 {
     new disable_commandscript();
