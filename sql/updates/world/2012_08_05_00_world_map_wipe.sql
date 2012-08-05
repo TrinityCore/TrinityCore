dbf595c6de446cebd0ec800afbb52eda4cc635ec
Northstrider <Dreadkiller@gmx.de>
2012-08-05 14:02:18 +0200

    DB/Wipe: more customizable sql to delete guid specific entrys from other tables related to map 0 and 1

--- sql/updates/world/2012_08_05_00_world_map_wipe.sql
+++ sql/updates/world/2012_08_05_00_world_map_wipe.sql
@@ -1,2 +1,11 @@
 DELETE FROM `gameobject` WHERE map IN (0, 1);
-DELETE FROM `creature` WHERE map IN (0, 1);
+
+DROP TABLE IF EXISTS `export_id`;
+CREATE TABLE `export_id` (`id` INT(10) UNSIGNED NULL,  PRIMARY KEY (`id`) ) COLLATE='latin1_swedish_ci' ENGINE=InnoDB ROW_FORMAT=DEFAULT;
+
+REPLACE INTO `export_id` SELECT `guid` FROM `creature` WHERE `map` IN (0, 1);
+
+DELETE FROM `creature_addon` WHERE `guid` IN (SELECT id FROM export_id);
+DELETE FROM `creature` WHERE `map` IN (0, 1);
+
+DROP TABLE IF EXISTS `export_id`;
