dd8cd53219356c4de1cfd8d105e1c6470f9f526b
Nay <dnpd.dd@gmail.com>
2012-08-08 19:02:43 +0100

    SQL: Sync auth_database.sql as well

--- sql/base/auth_database.sql
+++ sql/base/auth_database.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.22, for Win64 (x86)
+-- MySQL dump 10.13  Distrib 5.5.21, for Win64 (x86)
 --
 -- Host: localhost    Database: auth_4x
 -- ------------------------------------------------------
--- Server version	5.5.22
+-- Server version	5.5.21
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -143,6 +143,7 @@ CREATE TABLE `logs` (
   `time` int(10) unsigned NOT NULL,
   `realm` int(10) unsigned NOT NULL,
   `type` tinyint(3) unsigned NOT NULL,
+  `level` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `string` text CHARACTER SET latin1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -249,4 +250,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-05-30 12:52:33
+-- Dump completed on 2012-08-08 19:01:34
