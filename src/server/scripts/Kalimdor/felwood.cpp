fe4fdaf56b63be45c608dc669d6c04525fd92ba5
Vincent-Michael <Vincent_Michael@gmx.de>
2012-08-12 17:22:45 +0200

    Scripts/Some: Cleanup for old scripts, not exist Quest and etc. in Cata.

--- src/server/scripts/Kalimdor/felwood.cpp
+++ src/server/scripts/Kalimdor/felwood.cpp
@@ -19,87 +19,17 @@
 /* ScriptData
 SDName: Felwood
 SD%Complete: 95
-SDComment: Quest support: 4101, 4102
+SDComment: Quest support:
 SDCategory: Felwood
 EndScriptData */
 
 /* ContentData
-npcs_riverbreeze_and_silversky
 EndContentData */
 
 #include "ScriptMgr.h"
 #include "ScriptedCreature.h"
 #include "ScriptedGossip.h"
 
-/*######
-## npcs_riverbreeze_and_silversky
-######*/
-
-#define GOSSIP_ITEM_BEACON  "Please make me a Cenarion Beacon"
-
-enum RiverbreezeAndSilversky
-{
-    SPELL_CENARION_BEACON       = 15120,
-
-    NPC_ARATHANDRIS_SILVERSKY   = 9528,
-    NPC_MAYBESS_RIVERBREEZE     = 9529,
-
-    QUEST_CLEASING_FELWOOD_A    = 4101,
-    QUEST_CLEASING_FELWOOD_H    = 4102
-};
-
-class npcs_riverbreeze_and_silversky : public CreatureScript
-{
-public:
-    npcs_riverbreeze_and_silversky() : CreatureScript("npcs_riverbreeze_and_silversky") { }
-
-    bool OnGossipSelect(Player* player, Creature* creature, uint32 /*sender*/, uint32 action)
-    {
-        player->PlayerTalkClass->ClearMenus();
-        if (action == GOSSIP_ACTION_INFO_DEF+1)
-        {
-            player->CLOSE_GOSSIP_MENU();
-            creature->CastSpell(player, SPELL_CENARION_BEACON, false);
-        }
-        return true;
-    }
-
-    bool OnGossipHello(Player* player, Creature* creature)
-    {
-        if (creature->isQuestGiver())
-            player->PrepareQuestMenu(creature->GetGUID());
-
-        uint32 creatureId = creature->GetEntry();
-
-        if (creatureId == NPC_ARATHANDRIS_SILVERSKY)
-        {
-            if (player->GetQuestRewardStatus(QUEST_CLEASING_FELWOOD_A))
-            {
-                player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_ITEM_BEACON, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+1);
-                player->SEND_GOSSIP_MENU(2848, creature->GetGUID());
-            } else if (player->GetTeam() == HORDE)
-            player->SEND_GOSSIP_MENU(2845, creature->GetGUID());
-            else
-                player->SEND_GOSSIP_MENU(2844, creature->GetGUID());
-        }
-
-        if (creatureId == NPC_MAYBESS_RIVERBREEZE)
-        {
-            if (player->GetQuestRewardStatus(QUEST_CLEASING_FELWOOD_H))
-            {
-                player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, GOSSIP_ITEM_BEACON, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+1);
-                player->SEND_GOSSIP_MENU(2849, creature->GetGUID());
-            } else if (player->GetTeam() == ALLIANCE)
-            player->SEND_GOSSIP_MENU(2843, creature->GetGUID());
-            else
-                player->SEND_GOSSIP_MENU(2842, creature->GetGUID());
-        }
-
-        return true;
-    }
-};
-
 void AddSC_felwood()
 {
-    new npcs_riverbreeze_and_silversky();
 }
