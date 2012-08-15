60e608417eee5ad1822592d226c9b2e65096f98c
Nay <dnpd.dd@gmail.com>
2012-08-16 00:29:05 +0100

    Core/Spell: Update some comments in SpellEffect enum

--- src/server/game/Miscellaneous/SharedDefines.h
+++ src/server/game/Miscellaneous/SharedDefines.h
@@ -712,7 +712,7 @@ enum SpellEffects
     SPELL_EFFECT_INSTAKILL                          = 1,
     SPELL_EFFECT_SCHOOL_DAMAGE                      = 2,
     SPELL_EFFECT_DUMMY                              = 3,
-    SPELL_EFFECT_PORTAL_TELEPORT                    = 4, // Unused (4.2.2)
+    SPELL_EFFECT_PORTAL_TELEPORT                    = 4, // Unused (4.3.4)
     SPELL_EFFECT_TELEPORT_UNITS                     = 5,
     SPELL_EFFECT_APPLY_AURA                         = 6,
     SPELL_EFFECT_ENVIRONMENTAL_DAMAGE               = 7,
@@ -721,9 +721,9 @@ enum SpellEffects
     SPELL_EFFECT_HEAL                               = 10,
     SPELL_EFFECT_BIND                               = 11,
     SPELL_EFFECT_PORTAL                             = 12,
-    SPELL_EFFECT_RITUAL_BASE                        = 13, // Unused (4.2.2)
-    SPELL_EFFECT_RITUAL_SPECIALIZE                  = 14, // Unused (4.2.2)
-    SPELL_EFFECT_RITUAL_ACTIVATE_PORTAL             = 15, // Unused (4.2.2)
+    SPELL_EFFECT_RITUAL_BASE                        = 13, // Unused (4.3.4)
+    SPELL_EFFECT_RITUAL_SPECIALIZE                  = 14, // Unused (4.3.4)
+    SPELL_EFFECT_RITUAL_ACTIVATE_PORTAL             = 15, // Unused (4.3.4)
     SPELL_EFFECT_QUEST_COMPLETE                     = 16,
     SPELL_EFFECT_WEAPON_DAMAGE_NOSCHOOL             = 17,
     SPELL_EFFECT_RESURRECT                          = 18,
@@ -759,8 +759,8 @@ enum SpellEffects
     SPELL_EFFECT_STEALTH                            = 48,
     SPELL_EFFECT_DETECT                             = 49,
     SPELL_EFFECT_TRANS_DOOR                         = 50,
-    SPELL_EFFECT_FORCE_CRITICAL_HIT                 = 51, // Unused (4.2.2)
-    SPELL_EFFECT_GUARANTEE_HIT                      = 52, // Unused (4.2.2)
+    SPELL_EFFECT_FORCE_CRITICAL_HIT                 = 51, // Unused (4.3.4)
+    SPELL_EFFECT_GUARANTEE_HIT                      = 52, // Unused (4.3.4)
     SPELL_EFFECT_ENCHANT_ITEM                       = 53,
     SPELL_EFFECT_ENCHANT_ITEM_TEMPORARY             = 54,
     SPELL_EFFECT_TAMECREATURE                       = 55,
@@ -830,7 +830,7 @@ enum SpellEffects
     SPELL_EFFECT_APPLY_AREA_AURA_PET                = 119,
     SPELL_EFFECT_TELEPORT_GRAVEYARD                 = 120,
     SPELL_EFFECT_NORMALIZED_WEAPON_DMG              = 121,
-    SPELL_EFFECT_122                                = 122, // Unused (4.2.2)
+    SPELL_EFFECT_122                                = 122, // Unused (4.3.4)
     SPELL_EFFECT_SEND_TAXI                          = 123,
     SPELL_EFFECT_PULL_TOWARDS                       = 124,
     SPELL_EFFECT_MODIFY_THREAT_PERCENT              = 125,
@@ -871,7 +871,7 @@ enum SpellEffects
     SPELL_EFFECT_160                                = 160,
     SPELL_EFFECT_TALENT_SPEC_COUNT                  = 161,
     SPELL_EFFECT_TALENT_SPEC_SELECT                 = 162,
-    SPELL_EFFECT_163                                = 163,  // Unused (4.2.2)
+    SPELL_EFFECT_163                                = 163, // Unused (4.3.4)
     SPELL_EFFECT_REMOVE_AURA                        = 164,
     SPELL_EFFECT_165                                = 165,
     SPELL_EFFECT_166                                = 166,
@@ -879,17 +879,17 @@ enum SpellEffects
     SPELL_EFFECT_168                                = 168,
     SPELL_EFFECT_169                                = 169,
     SPELL_EFFECT_170                                = 170,
-    SPELL_EFFECT_171                                = 171,  // Summons gamebject
-    SPELL_EFFECT_172                                = 172,  // Aoe ressurection
-    SPELL_EFFECT_UNLOCK_GUILD_VAULT_TAB             = 173,  // Guild tab unlocked (guild perk)
-    SPELL_EFFECT_174                                = 174,  //
-    SPELL_EFFECT_175                                = 175,  // Unused (4.2.2)
-    SPELL_EFFECT_176                                = 176,  // Some kind of sanctuary effect (Vanish)
+    SPELL_EFFECT_171                                = 171, // Summons gamebject
+    SPELL_EFFECT_172                                = 172, // Aoe ressurection
+    SPELL_EFFECT_UNLOCK_GUILD_VAULT_TAB             = 173, // Guild tab unlocked (guild perk)
+    SPELL_EFFECT_174                                = 174,
+    SPELL_EFFECT_175                                = 175, // Unused (4.3.4)
+    SPELL_EFFECT_176                                = 176, // Some kind of sanctuary effect (Vanish)
     SPELL_EFFECT_177                                = 177,
-    SPELL_EFFECT_178                                = 178,
+    SPELL_EFFECT_178                                = 178, // Unused (4.3.4)
     SPELL_EFFECT_179                                = 179,
-    SPELL_EFFECT_180                                = 180,
-    SPELL_EFFECT_181                                = 181,
+    SPELL_EFFECT_180                                = 180, // Unused (4.3.4)
+    SPELL_EFFECT_181                                = 181, // Unused (4.3.4)
     SPELL_EFFECT_182                                = 182,
     TOTAL_SPELL_EFFECTS                             = 183,
 };
