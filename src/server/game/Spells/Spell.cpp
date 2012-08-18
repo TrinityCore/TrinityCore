181c9e7d30f125c9e440b9aa9acdc35c5b6eb3e3
cyberbrest <cyberbrest@cyberbrest.com>
2012-08-19 00:15:06 +0300

    [PacketIO] correct struct for SendSpellGo

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -4010,7 +4010,21 @@ void Spell::SendSpellGo()
     if (m_targets.GetTargetMask() & TARGET_FLAG_DEST_LOCATION)
     {
         data << uint8(0);
+
+        WorldLocation const* loc =  m_targets.GetDstPos();
+        if(loc)
+        {
+            data << float(loc->m_positionX);
+            data << float(loc->m_positionY);
+            data << float(loc->m_positionZ);
+        }else
+        {
+            data << float(0);
+            data << float(0);
+            data << float(0);
+        }
     }
+    WorldLocation const* loc =  m_targets.GetDstPos();
 
     if (m_targets.GetTargetMask() & TARGET_FLAG_EXTRA_TARGETS)
     {
