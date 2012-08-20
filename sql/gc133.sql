dbb257c1a2a14c446347b20ae0a1c7d07e08a8a2
tibbi <tibbi@centrum.sk>
2012-08-20 23:12:35 +0100

    [gc133] adding immunities to 25m version Ulduar npcs

--- sql/gc133.sql
+++ sql/gc133.sql
@@ -549,26 +549,32 @@ INSERT INTO `script_texts` VALUES
 -- Update mechanic immunity flags.
 UPDATE `creature_template` SET `mechanic_immune_mask`=650853247, `flags_extra`=1 WHERE `entry` IN 
 (
-33113,   -- Flame Leviathan
-33118,   -- Ignis the Furnace Master
-33186,   -- Razorscale
-33293,   -- XT002
-32867,   -- Steelbreaker
-32927,   -- Runemaster Molgeim
-32930,   -- Kologarn
-32906,   -- Freya
-32845,   -- Hodir
-33350,   -- Mimiron
-33432,   -- Leviathan MK II
-33651,   -- VX-001
-33670,   -- Aerial Unit
-32865,   -- Thorim
-33271,   -- General Vezax
-33524,   -- Saronite Animus
-33288,   -- Yogg-Saron
-33134,   -- Sara
-33890    -- Brain of Yogg-Saron
+33113, 34003,  -- Flame Leviathan
+33118, 33190,  -- Ignis the Furnace Master
+33186, 33724,  -- Razorscale
+33293, 33885,  -- XT002
+32867, 33693,  -- Steelbreaker
+32927, 33692,  -- Runemaster Molgeim
+32930, 33909,  -- Kologarn
+32906, 33360,  -- Freya
+32845, 32846,  -- Hodir
+33350,         -- Mimiron
+33432, 34106,  -- Leviathan MK II
+33651, 34108,  -- VX-001
+33670, 34109,  -- Aerial Unit
+32865, 33147,  -- Thorim
+33271, 33449,  -- General Vezax
+33524, 34152,  -- Saronite Animus
+33288, 33955,  -- Yogg-Saron
+33134, 34332,  -- Sara
+33890, 33954   -- Brain of Yogg-Saron
 );
+
+-- updating giant immunities
+UPDATE creature_template SET mechanic_immune_mask = 650854239 WHERE entry IN (33149, 33148);
+
+-- assigning 25m version of Brain of Yogg-Saron to the normal version
+UPDATE creature_template SET difficulty_entry_1 = 33954 WHERE entry = 33890;
 
 -- Auriaya should be interruptible, due to Sentinel Blast
 UPDATE `creature_template` SET `mechanic_immune_mask`=617298815 WHERE `entry` IN (33515);
