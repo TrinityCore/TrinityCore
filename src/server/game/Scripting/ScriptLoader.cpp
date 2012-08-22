0b1ef5f2a1c9e53280c3691f3a027fa60424e064
Gary <fishermanpl@gmail.com>
2012-08-22 18:19:30 +0200

    [6] Add script hunterpet vendor.

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -23,6 +23,8 @@ void AddSC_example_escort();
 void AddSC_example_gossip_codebox();
 void AddSC_example_misc();
 void AddSC_example_commandscript();
+//Hunter Pet Vendor
+void AddSC_npc_hunterpetvendor();
 
 // spells
 void AddSC_deathknight_spell_scripts();
@@ -1267,6 +1269,8 @@ void AddCustomScripts()
 {
 #ifdef SCRIPTS
     /* This is where custom scripts should be added. */
+    //Hunter Pet Vendor
+    AddSC_npc_hunterpetvendor();
 
 #endif
 }
