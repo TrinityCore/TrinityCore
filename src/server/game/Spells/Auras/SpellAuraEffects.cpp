8f24fc8be12cbb98bba45c3f99ad6df23f07a7c5
Gary <fishermanpl@gmail.com>
2012-08-22 18:04:42 +0200

    [4] Fix Gnomeregan Pride.

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -2219,6 +2219,10 @@ void AuraEffect::HandleAuraTransform(AuraApplication const* aurApp, uint8 mode, 
                     case 42365:
                         target->SetDisplayId(21723);
                         break;
+                    // Gnomeregan Pride
+                    case 75531:
+                        target->SetDisplayId(31654);
+                        break;
                     // Dread Corsair
                     case 50517:
                     // Corsair Costume
