42972052955970c2b4a1bff98591add1140bc5f9
Tuxity <kevin.darcel@gmail.com>
2012-08-23 17:13:54 +0200

    Core/AuctionHouse: Fix few coding style typos

--- src/server/game/Server/WorldSession.h
+++ src/server/game/Server/WorldSession.h
@@ -355,7 +355,7 @@ class WorldSession
         void SendAuctionCommandResult(AuctionEntry* auction, uint32 Action, uint32 ErrorCode, uint32 bidError = 0);
         void SendAuctionBidderNotification(uint32 location, uint32 auctionId, uint64 bidder, uint32 bidSum, uint32 diff, uint32 item_template);
         void SendAuctionOwnerNotification(AuctionEntry* auction);
-        void SendAuctionRemovedNotification(uint32 auctionId, uint32 item_template, int32 randomPropertyId);
+        void SendAuctionRemovedNotification(uint32 auctionId, uint32 itemEntry, int32 randomPropertyId);
 
         //Item Enchantment
         void SendEnchantmentLog(uint64 Target, uint64 Caster, uint32 ItemID, uint32 SpellID);
