ce5352ca1db42a1582d2d8a284dc260a91192641
Gary <fishermanpl@gmail.com>
2012-08-27 19:21:00 +0200

    Change Info Repo.

--- src/server/scripts/Commands/cs_server.cpp
+++ src/server/scripts/Commands/cs_server.cpp
@@ -114,9 +114,9 @@ public:
         uint32 updateTime           = sWorld->GetUpdateTime();
 
         //handler->SendSysMessage(_FULLVERSION);
-        handler->PSendSysMessage("Core: BlizzNetCore");
+        handler->PSendSysMessage("Core: GaryMoveOut");
         handler->PSendSysMessage("Rev: XX.08.2012");
-        handler->PSendSysMessage("Sponsors: http://BlizzNet.PL");
+        handler->PSendSysMessage("Sponsors: BlizzNet.PL / xGame.PL / JarSat.PL");
         handler->PSendSysMessage(LANG_CONNECTED_PLAYERS, playersNum, maxPlayersNum);
         handler->PSendSysMessage(LANG_CONNECTED_USERS, activeClientsNum, maxActiveClientsNum, queuedClientsNum, maxQueuedClientsNum);
         handler->PSendSysMessage(LANG_UPTIME, uptime.c_str());
