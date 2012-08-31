0f76a44a0cf69980971a40b4a7146ff2df17a3d5
cyberbrest <cyberbrest@cyberbrest.com>
2012-08-31 22:25:46 +0300

    typo

--- sql/updates/world/2012_08_31_01_world_phase_template.sql
+++ sql/updates/world/2012_08_31_01_world_phase_template.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS `phase_template`;
 CREATE TABLE `phase_template` (
   `id` mediumint(8) unsigned NOT NULL COMMENT 'id from Phase.dbc',
-  `phaseMask` smallint(5) unsigned NOT NULL DEFAULT '-1',
-  `map` smallint(5) NOT NULL DEFAULT '0',
+  `phaseMask` smallint(5) unsigned NOT NULL DEFAULT '1',
+  `map` smallint(5) NOT NULL DEFAULT '-1',
   `terrainSwap` smallint(5) unsigned NOT NULL DEFAULT '0',
   `unkFirstCounter` smallint(5) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
