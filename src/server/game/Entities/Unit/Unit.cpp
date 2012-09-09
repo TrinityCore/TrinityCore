5c2c7c680dacf746aae9cfb5f42a7daffa342ffc
Shauren <shauren.trinity@gmail.com>
2012-09-09 19:56:00 +0200

    Core/Vehicles: Improved vehicle passenger position calculation (still not complete)

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -406,15 +406,8 @@ void Unit::UpdateSplineMovement(uint32 t_diff)
             pos.m_positionZ = loc.z;
             pos.m_orientation = loc.orientation;
 
-            if (Unit* vehicle = GetVehicleBase())
-            {
-                loc.x += vehicle->GetPositionX();
-                loc.y += vehicle->GetPositionY();
-                loc.z += vehicle->GetPositionZMinusOffset();
-                loc.orientation = vehicle->GetOrientation();
-            }
-            else if (Transport* trans = GetTransport())
-                trans->CalculatePassengerPosition(loc.x, loc.y, loc.z, loc.orientation);
+            if (TransportBase* transport = GetDirectTransport())
+                transport->CalculatePassengerPosition(loc.x, loc.y, loc.z, loc.orientation);
         }
 
         UpdatePosition(loc.x, loc.y, loc.z, loc.orientation);
@@ -16287,6 +16280,13 @@ uint64 Unit::GetTransGUID() const
         return GetTransport()->GetGUID();
 
     return 0;
+}
+
+TransportBase* Unit::GetDirectTransport() const
+{
+    if (Vehicle* veh = GetVehicle())
+        return veh;
+    return GetTransport();
 }
 
 bool Unit::IsInPartyWith(Unit const* unit) const
