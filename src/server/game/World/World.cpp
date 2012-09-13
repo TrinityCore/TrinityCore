10fd35dfb5598b0ead884710fdfa0fa5c3f2dfaf
Innerxe <gamerzlove@gmail.com>
2012-09-13 15:27:55 -0500

    Sistema de Títulos PVP

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -848,6 +848,19 @@ void World::LoadConfigSettings(bool reload)
         sLog->outError(LOG_FILTER_SERVER_LOADING, "MinPetitionSigns (%i) must be in range 0..9. Set to 9.", m_int_configs[CONFIG_MIN_PETITION_SIGNS]);
         m_int_configs[CONFIG_MIN_PETITION_SIGNS] = 9;
     }
+	
+	rate_values[RATE_PVP_RANK_EXTRA_HONOR] = ConfigMgr::GetFloatDefault("PvPRank.Rate.ExtraHonor", 1);
+	std::string s_pvp_ranks = ConfigMgr::GetStringDefault("PvPRank.HKPerRank", "10,50,100,200,450,750,1300,2000,3500,6000,9500,15000,21000,30000");
+	char *c_pvp_ranks = const_cast<char*>(s_pvp_ranks.c_str());
+	for (int i = 0; i !=HKRANKMAX; i++)
+	{
+		if (i==0)
+			pvp_ranks[0] = 0;
+		else if (i==1)
+			pvp_ranks[1] = atoi(strtok (c_pvp_ranks, ","));
+		else
+			pvp_ranks[i] = atoi(strtok (NULL, ","));
+	}
 
     m_int_configs[CONFIG_GM_LOGIN_STATE]        = ConfigMgr::GetIntDefault("GM.LoginState", 2);
     m_int_configs[CONFIG_GM_VISIBLE_STATE]      = ConfigMgr::GetIntDefault("GM.Visible", 2);
