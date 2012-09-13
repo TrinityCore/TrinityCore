ed1ece2bb78d6e473872ff1691343295cd573309
cyberbrest <cyberbrest@cyberbrest.com>
2012-09-13 16:16:33 +0100

    Core/Quest: allow to complete quest by new auto-submit system with getting next quest in chain.
    Closes #7752
    
    (Modified)

--- src/server/game/Quests/QuestDef.h
+++ src/server/game/Quests/QuestDef.h
@@ -148,6 +148,11 @@ enum __QuestFlags
     QUEST_FLAGS_SPECIAL_ITEM   = 0x00020000,                // has something to do with RequiredItemId and SourceItemId
     QUEST_FLAGS_OBJ_TEXT       = 0x00040000,                // use Objective text as Complete text
     QUEST_FLAGS_AUTO_ACCEPT    = 0x00080000,                // The client recognizes this flag as auto-accept. However, NONE of the current quests (3.3.5a) have this flag. Maybe blizz used to use it, or will use it in the future.
+    QUEST_FLAGS_AUTO_SUBMIT    = 0x00100000,                // Quests with this flag player submit automatically by special button in player gui
+    QUEST_FLAGS_AUTO_TAKE      = 0x00200000,                // Automatically suggestion of accepting quest. Not from npc.
+    //QUEST_FLAGS_UNK2           = 0x00400000,
+    //QUEST_FLAGS_UNK3           = 0x00800000,                // Found in quest 14069
+    //QUEST_FLAGS_UNK4           = 0x01000000,
 
     // ... 4.x added flags up to 0x80000000 - all unknown for now
 };
