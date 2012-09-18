715e092b3df92bfe68a5098a25c737e23698b521
Gary <fishermanpl@gmail.com>
2012-09-18 13:45:59 +0200

    [36] [Censor] Update vulgar slang.

--- src/server/scripts/Custom/chat_censure.cpp
+++ src/server/scripts/Custom/chat_censure.cpp
@@ -40,8 +40,9 @@ void CheckMessage(Player* player, std::string& msg, uint32 lang, Player* /*recei
     std::string lower = msg;
     std::transform(lower.begin(), lower.end(), lower.begin(), ::tolower);
  
-    uint8 cheksSize = 11;
+    uint8 cheksSize = 36;
     std::string checks[cheksSize];
+    // Strony (Sites)
     checks[0] ="http://";
     checks[1] =".com";
     checks[2] =".www";
@@ -51,13 +52,40 @@ void CheckMessage(Player* player, std::string& msg, uint32 lang, Player* /*recei
     checks[6] ="www.";
     checks[7] ="wow-";
     checks[8] ="-wow";
-    checks[9] ="rondor";
-    checks[10] =".pl";
+    checks[9] =".pl";
+    // Polskie przekleñstwa (Polish curses)
+    checks[10] ="chuj";
+    checks[11] ="huj";
+    checks[12] ="kurw";
+    checks[13] ="jeba";
+    checks[14] ="cip";
+    checks[15] ="gej";
+    checks[16] ="cwel";
+    checks[17] ="pizd";
+    checks[18] ="pierdo";
+    // English curses
+    checks[19] ="anal";
+    checks[20] ="ass";
+    checks[21] ="bitch";
+    checks[22] ="clit";
+    checks[23] ="cock";
+    checks[24] ="cum";
+    checks[25] ="cunt";
+    checks[26] ="dick";
+    checks[27] ="faggot";
+    checks[28] ="fuck";
+    checks[29] ="gay";
+    checks[30] ="lesbian";
+    checks[31] ="penis";
+    checks[32] ="prick";
+    checks[33] ="slut";
+    checks[34] ="twat";
+    checks[35] ="whore";
     for (int i = 0; i < cheksSize; ++i)
         if (lower.find(checks[i]) != std::string::npos)
         {
             msg = "";
-            ChatHandler(player).PSendSysMessage("Advertising is not allowed!");         
+            ChatHandler(player).PSendSysMessage("Advertising and vulgar behavior is not allowed!");         
             return;
         }
 } 
