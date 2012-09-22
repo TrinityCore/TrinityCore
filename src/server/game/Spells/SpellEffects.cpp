50be08f01bec46f8915dff08d03af50eb3b4b140
Warpten <vertozor@gmail.com>
2012-09-22 21:28:37 +0200

    Core/Spells: Fixed Soulwell getting two charges removed (or more) per click.

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -2001,7 +2001,8 @@ void Spell::SendLoot(uint64 guid, LootType loottype)
         if (sScriptMgr->OnGossipHello(player, gameObjTarget))
             return;
 
-        gameObjTarget->AI()->GossipHello(player);
+        if (gameObjTarget->AI()->GossipHello(player))
+            return;
 
         switch (gameObjTarget->GetGoType())
         {
