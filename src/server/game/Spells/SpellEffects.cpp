198284a14539c134ce89aab97234307f66868b55
Gary <fishermanpl@gmail.com>
2012-09-28 18:49:30 +0200

    [40] Fix Pet CD After Summon.

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -3081,6 +3081,7 @@ void Spell::EffectSummonPet(SpellEffIndex effIndex)
     // if pet requested type already exist
     if (OldSummon)
     {
+        OldSummon->m_CreatureSpellCooldowns.clear();
         if (petentry == 0 || OldSummon->GetEntry() == petentry)
         {
             // pet in corpse state can't be summoned
