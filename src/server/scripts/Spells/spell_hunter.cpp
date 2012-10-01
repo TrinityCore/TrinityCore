1801fe0ffe72b3de84dde4ab4c3ce09244ad4b95
Gary <fishermanpl@gmail.com>
2012-10-01 20:10:18 +0200

    [54] [Spells/Hunter] Fix Misdirection.

--- src/server/scripts/Spells/spell_hunter.cpp
+++ src/server/scripts/Spells/spell_hunter.cpp
@@ -574,7 +574,8 @@ class spell_hun_misdirection : public SpellScriptLoader
             void OnRemove(AuraEffect const* /*aurEff*/, AuraEffectHandleModes /*mode*/)
             {
                 if (Unit* caster = GetCaster())
-                    if (GetTargetApplication()->GetRemoveMode() != AURA_REMOVE_BY_DEFAULT)
+                    //if (GetTargetApplication()->GetRemoveMode() != AURA_REMOVE_BY_DEFAULT)
+                    if (!GetDuration())
                         caster->SetReducedThreatPercent(0, 0);
             }
 
