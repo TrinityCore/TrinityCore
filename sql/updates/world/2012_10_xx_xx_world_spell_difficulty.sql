f3a9e2c9374572a517331eb9d998109237e6ca4c
Retriman <nicolas_katre@hotmail.com>
2012-10-02 23:33:18 -0400

    Core/Script: Dungeon Ahnkahet Boss Elder Nadox. dammit fix typo's in sql :(

--- sql/updates/world/2012_10_xx_xx_world_spell_difficulty.sql
+++ sql/updates/world/2012_10_xx_xx_world_spell_difficulty.sql
@@ -2,7 +2,3 @@
 DELETE FROM `spelldifficulty_dbc` WHERE `id` =56130;
 INSERT INTO `spelldifficulty_dbc`(`id`,`spellid0`,`spellid1`) VALUES
 (56130,56130,59467); -- Spell Pound 
-
-DELETE FROM `spell_script_names` WHERE `spell_id`=56153;
-INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
-(56153, 'spell_elder_nadox_guardian');
--- sql/updates/world/2012_10_xx_xx_world_spell_script_names.sql
+++ sql/updates/world/2012_10_xx_xx_world_spell_script_names.sql
@@ -2,4 +2,3 @@
 DELETE FROM `spell_script_names` WHERE `spell_id`=56153;
 INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
 (56153, 'spell_elder_nadox_guardian');
-
