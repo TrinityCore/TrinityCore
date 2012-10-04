12dbf1130990ee32d1210ce33bd14afe0e68fba9
Gary <fishermanpl@gmail.com>
2012-10-04 13:36:24 +0200

    [37] Removes Master's Call stun immunity.

--- sql/GaryMoveOut/gary_world.sql
+++ sql/GaryMoveOut/gary_world.sql
@@ -161,3 +161,6 @@ UPDATE `creature_template` SET `ScriptName` = 'npc_lake_frog' WHERE `entry` IN (
 UPDATE `creature_template` SET `gossip_menu_id` = 33220 WHERE `entry` = 33220;
 REPLACE INTO `gossip_menu_option` (`menu_id`, `option_text`, `option_id`, `npc_option_npcflag`, `action_script_id`) VALUES ('33220', 'Do you know, where I can find Ashwood Brand Sword?', '1', '1', '33220');
 REPLACE INTO `gossip_scripts` (`id`, `command`, `datalong`, `datalong2`) VALUES ('33220', '15', '62554', '3');
+
+-- Removes Master's Call stun immunity.
+INSERT INTO `spell_linked_spell`(`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES (54216,-56651,1,'Removes Master''s Call stun immunity');
