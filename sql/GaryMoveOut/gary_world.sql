80771de2afe5038cf274d0c47b89016d635b37fd
Gary <fishermanpl@gmail.com>
2012-10-08 15:31:04 +0200

    [59] --> Fix Sap removes Stealth.

--- sql/GaryMoveOut/gary_world.sql
+++ sql/GaryMoveOut/gary_world.sql
@@ -164,3 +164,11 @@ REPLACE INTO `gossip_scripts` (`id`, `command`, `datalong`, `datalong2`) VALUES 
 
 -- Removes Master's Call stun immunity.
 INSERT INTO `spell_linked_spell`(`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES (54216,-56651,1,'Removes Master''s Call stun immunity');
+
+-- Sap removes Stealth.
+INSERT INTO spell_linked_spell (spell_trigger, spell_effect, type,
+comment) VALUES 
+(6770, -1784, 1, 'Sap(rank 1) removes Stealth'),
+(2070, -1784, 1, 'Sap(rank 2) removes Stealth'),
+(11297, -1784, 1, 'Sap(rank 3) removes Stealth'),
+(51724, -1784, 1, 'Sap(rank 4) removes Stealth');
