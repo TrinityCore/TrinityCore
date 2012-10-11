d76adf4d27477a0c385860766166d8cd97694745
tibbi <tibbi@centrum.sk>
2012-10-11 20:08:33 +0100

    [gc324] make all 15v15 BGs require only 5 players per faction

--- sql/gc/gc324.sql
+++ sql/gc/gc324.sql
@@ -1339,3 +1339,6 @@ UPDATE creature_template SET difficulty_entry_3 = 37291 WHERE entry = 14772;
 UPDATE creature_template SET minlevel = 85, maxlevel = 85, faction_A = 1214, faction_H = 1214, speed_walk = 1.76, mindmg = 614, maxdmg = 820, attackpower = 745, dmg_multiplier = 13, baseattacktime = 2000, unit_flags = 4096, dynamicflags = 8, MovementType = 1, equipment_id = 2064 WHERE entry = 37291;
 UPDATE creature_template SET difficulty_entry_3 = 37468 WHERE entry = 14777;
 UPDATE creature_template SET minlevel = 85, maxlevel = 85, faction_A = 1214, faction_H = 1214, speed_walk = 1.76, mindmg = 614, maxdmg = 820, attackpower = 745, dmg_multiplier = 13, baseattacktime = 2000, unit_flags = 4096, dynamicflags = 8, MovementType = 1, equipment_id = 2064 WHERE entry = 37468;
+
+-- misc battleground queries
+UPDATE battleground_template SET MinPlayersPerTeam = 5 WHERE id IN (3, 7, 9);
