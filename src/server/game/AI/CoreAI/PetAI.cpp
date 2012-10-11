b019f784fb1c3244d192e9a9359fec9c8840b6e9
Retriman <nicolas_katre@hotmail.com>
2012-10-11 01:36:59 -0400

    Core/Pet: *if the owner does not see the attacker can not attack the pet

--- src/server/game/AI/CoreAI/PetAI.cpp
+++ src/server/game/AI/CoreAI/PetAI.cpp
@@ -105,7 +105,8 @@ void PetAI::UpdateAI(const uint32 diff)
             return;
         }
 
-        DoMeleeAttackIfReady();
+        if (me->getVictim()->IsVisible() && me->GetOwner()->getVictim() && me->GetOwner()->getVictim()->IsVisible())
+            DoMeleeAttackIfReady();
     }
     else if (owner && me->GetCharmInfo()) //no victim
     {
