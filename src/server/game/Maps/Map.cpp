d13d5e5bf0bc9cc2974ddeff19f2b14bfdd3e5e1
Gary <fishermanpl@gmail.com>
2012-10-12 15:07:50 +0200

    [62] Core/Maps: Merge some changes to GetHeight Calculation.

--- src/server/game/Maps/Map.cpp
+++ src/server/game/Maps/Map.cpp
@@ -1659,8 +1659,17 @@ float Map::GetHeight(float x, float y, float z, bool checkVMap /*= true*/, float
         else
             return vmapHeight;                              // we have only vmapHeight (if have)
     }
+    else
+    {
+        if (!checkVMap)
+            return mapHeight; // explicitly use map data (if have)
+        else if (mapHeight > INVALID_HEIGHT && (z < mapHeight + 2 || z == MAX_HEIGHT))
+            return mapHeight; // explicitly use map data if original z < mapHeight but map found (z+2 > mapHeight)
+        else
+            return VMAP_INVALID_HEIGHT_VALUE; // we not have any height
+    }
 
-    return mapHeight;                               // explicitly use map data
+    // return mapHeight; // explicitly use map data
 }
 
 inline bool IsOutdoorWMO(uint32 mogpFlags, int32 /*adtId*/, int32 /*rootId*/, int32 /*groupId*/, WMOAreaTableEntry const* wmoEntry, AreaTableEntry const* atEntry)
