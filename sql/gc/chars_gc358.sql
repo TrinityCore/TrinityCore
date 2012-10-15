30d715d41958bafa5b3fc857588b0edaf5d0b125
tibbi <tibbi@centrum.sk>
2012-10-15 13:26:49 +0100

    [gc358] small sql correction for warden

--- sql/gc/chars_gc358.sql
+++ sql/gc/chars_gc358.sql
@@ -1,3 +1,4 @@
 -- warden settings
+DELETE FROM `warden_action` WHERE `wardenID` IN (786, 209);
 INSERT INTO `warden_action` (`wardenId`, `action`) VALUES ('786', '2');
 INSERT INTO `warden_action` (`wardenId`, `action`) VALUES ('209', '2');
--- src/server/shared/revision_gc.h
+++ src/server/shared/revision_gc.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_gc_H__
 #define __REVISION_gc_H__
- #define REVISION_gc "357"
+ #define REVISION_gc "358"
 #endif // __REVISION_gc_H__
