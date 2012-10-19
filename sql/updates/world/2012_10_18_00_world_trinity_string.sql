307af70b2d390d09f6db62dd2cdfaeae1aee5bf6
Spp <spp@jorge.gr>
2012-10-19 17:36:24 +0200

    Move LFG string out of Custom patch range

--- sql/updates/world/2012_10_18_00_world_trinity_string.sql
+++ sql/updates/world/2012_10_18_00_world_trinity_string.sql
@@ -1,16 +1,17 @@
-REPLACE INTO `trinity_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`)
+DELETE FROM `trinity_string` WHERE `entry` IN (9986, 9987, 9989, 9990, 9991, 9992, 9993, 9994, 9995, 9996, 9997, 9998, 9999);
+INSERT INTO `trinity_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`)
 VALUES
-  (11015, 'Error', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-  (11014, 'None', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-  (11013, 'Leader', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-  (11012, 'Dps', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-  (11011, 'Healer', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-  (11010, 'Tank', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-  (11009, 'Raid browser', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-  (11008, 'Finished dungeon', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-  (11007, 'In dungeon', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-  (11006, 'Vote kick', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-  (11005, 'Proposal', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-  (11004, 'Queued', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-  (11003, 'Role check', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-  (11002, 'None', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+  (9986, 'Error', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+  (9987, 'None', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+  (9988, 'Leader', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+  (9989, 'Dps', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+  (9990, 'Healer', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+  (9991, 'Tank', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+  (9992, 'Raid browser', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+  (9993, 'Finished dungeon', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+  (9994, 'In dungeon', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+  (9995, 'Vote kick', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+  (9996, 'Proposal', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+  (9997, 'Queued', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+  (9998, 'Role check', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+  (9999, 'None', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
