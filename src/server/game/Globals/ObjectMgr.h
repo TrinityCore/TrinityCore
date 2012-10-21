ac0f11b943ab197bd529a61e63a1d6ba504b428d
saqirmdev <saqirm@gmail.com>
2012-10-21 16:21:26 +0200

    Fix Build Transmogrification

--- src/server/game/Globals/ObjectMgr.h
+++ src/server/game/Globals/ObjectMgr.h
@@ -400,6 +400,9 @@ typedef UNORDERED_MAP<uint32, PointOfInterestLocale> PointOfInterestLocaleContai
 typedef std::multimap<uint32, uint32> QuestRelations;
 typedef std::pair<QuestRelations::const_iterator, QuestRelations::const_iterator> QuestRelationBounds;
 
+typedef std::map<uint32, uint32> FakeItemsContainer;
+
+
 struct PetLevelInfo
 {
     PetLevelInfo() : health(0), mana(0) { for (uint8 i=0; i < MAX_STATS; ++i) stats[i] = 0; }
@@ -639,6 +642,11 @@ class ObjectMgr
             return NULL;
         }
 
+        uint32 GetFakeItemEntry(uint32 itemGuid);
+        void SetFekeItem(uint32 itemGuid, uint32 fakeEntry);
+        void RemoveFakeItem(uint32 itemGuid);
+
+
         InstanceTemplate const* GetInstanceTemplate(uint32 mapId);
 
         PetLevelInfo const* GetPetLevelInfo(uint32 creature_id, uint8 level) const;
@@ -861,6 +869,7 @@ class ObjectMgr
         void LoadGameobjects();
         void LoadItemTemplates();
         void LoadItemLocales();
+        void LoadFakeItems();
         void LoadItemSetNames();
         void LoadItemSetNameLocales();
         void LoadQuestLocales();
@@ -1271,6 +1280,7 @@ class ObjectMgr
         ItemTemplateContainer _itemTemplateStore;
         ItemLocaleContainer _itemLocaleStore;
         ItemSetNameLocaleContainer _itemSetNameLocaleStore;
+        FakeItemsContainer _fakeItemsStore;
         QuestLocaleContainer _questLocaleStore;
         NpcTextLocaleContainer _npcTextLocaleStore;
         PageTextLocaleContainer _pageTextLocaleStore;
