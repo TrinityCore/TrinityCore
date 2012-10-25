10e24977edfb16b06578e5509e78fbac1ae8b9a2
tibbi <tibbi@centrum.sk>
2012-10-25 22:27:36 +0100

    [gc456] compile fix

--- src/server/scripts/Events/childrens_week.cpp
+++ src/server/scripts/Events/childrens_week.cpp
@@ -15,7 +15,6 @@
 * with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "event.h"
 #include "ScriptMgr.h"
 #include "ScriptedCreature.h"
 
--- src/server/shared/revision_gc.h
+++ src/server/shared/revision_gc.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_gc_H__
 #define __REVISION_gc_H__
- #define REVISION_gc "455"
+ #define REVISION_gc "456"
 #endif // __REVISION_gc_H__
