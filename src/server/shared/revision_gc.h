3f3e5cdde08eb66f1adb5671e210523fe705a82f
Gary <fishermanpl@gmail.com>
2012-10-25 12:13:14 +0200

    Fix style file.

--- src/server/shared/revision_gc.h
+++ src/server/shared/revision_gc.h
@@ -1,8 +1,4 @@
 #ifndef __REVISION_gc_H__
 #define __REVISION_gc_H__
-<<<<<<< HEAD
  #define REVISION_gc "449"
-=======
- #define REVISION_gc "442"
->>>>>>> parent of 0291305... [gc443] ICC - make only the first 3 bosses (excluding gunship battle) accessible
 #endif // __REVISION_gc_H__
