876ad50dcde63eb29c51d54e126c9d66508183ac
Spp <spp@jorge.gr>
2012-10-26 14:30:52 +0200

    Core/Misc: Tweaked ThreatContainer internals (and many cosmetic changes here and there)

--- src/server/scripts/EasternKingdoms/SunwellPlateau/boss_felmyst.cpp
+++ src/server/scripts/EasternKingdoms/SunwellPlateau/boss_felmyst.cpp
@@ -512,7 +512,6 @@ public:
             }
         }
     };
-
 };
 
 class mob_felmyst_vapor : public CreatureScript
@@ -545,7 +544,6 @@ public:
                     AttackStart(target);
         }
     };
-
 };
 
 class mob_felmyst_trail : public CreatureScript
@@ -573,7 +571,6 @@ public:
         void MoveInLineOfSight(Unit* /*who*/) {}
         void UpdateAI(const uint32 /*diff*/) {}
     };
-
 };
 
 void AddSC_boss_felmyst()
