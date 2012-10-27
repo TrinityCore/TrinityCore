33db9acafa080ea3b63c3078106835a1f1e169d8
tibbi <tibbi@centrum.sk>
2012-10-27 23:52:52 +0100

    add a missing item at player_factionchange_items

---
+++ sql/updates/world/2012_10_28_00_world_player_factionchange_items.sql
@@ -0,0 +1,3 @@
+DELETE FROM `player_factionchange_items` WHERE `alliance_id` = 15198;
+INSERT INTO `player_factionchange_items` (`race_A`, `alliance_id`, `commentA`, `race_H`, `horde_id`, `commentH`) VALUES
+(0, 15198, 'Knight\'s Colors', 0, 15199, 'Stone Guard\'s Herald');
