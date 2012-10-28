1327975613ed0d9123405d14f8a38eb18d9268e8
CeIa <thijs.emmerink@gmail.com>
2012-10-29 00:13:16 +0100

    Events/Achievement: fix 'Out With It'
    Credits to @darkman1983

--- src/server/scripts/Spells/spell_generic.cpp
+++ src/server/scripts/Spells/spell_generic.cpp
@@ -561,7 +561,10 @@ enum TrickOrTreatSpells
 {
     SPELL_TRICK                 = 24714,
     SPELL_TREAT                 = 24715,
-    SPELL_TRICKED_OR_TREATED    = 24755
+    SPELL_TRICKED_OR_TREATED    = 24755,
+    SPELL_TRICKY_TREAT_SPEED    = 42919,	
+    SPELL_TRICKY_TREAT_TRIGGER  = 42965,
+    SPELL_UPSET_TUMMY           = 42966
 };
 
 class spell_gen_trick_or_treat : public SpellScriptLoader
@@ -599,6 +602,45 @@ class spell_gen_trick_or_treat : public SpellScriptLoader
         SpellScript* GetSpellScript() const
         {
             return new spell_gen_trick_or_treat_SpellScript();
+        }
+};
+
+class spell_gen_tricky_treat : public SpellScriptLoader
+{
+    public:
+        spell_gen_tricky_treat() : SpellScriptLoader("spell_gen_tricky_treat") {}
+
+        class spell_gen_tricky_treat_SpellScript : public SpellScript
+        {
+            PrepareSpellScript(spell_gen_tricky_treat_SpellScript);
+
+            bool Validate(SpellInfo const* /*spellEntry*/)
+            {
+                if (!sSpellMgr->GetSpellInfo(SPELL_TRICKY_TREAT_SPEED))
+                    return false;
+                if (!sSpellMgr->GetSpellInfo(SPELL_TRICKY_TREAT_TRIGGER))
+                    return false;
+                if (!sSpellMgr->GetSpellInfo(SPELL_UPSET_TUMMY))
+                    return false;
+                return true;
+            }
+
+            void HandleScript(SpellEffIndex /*effIndex*/)
+            {
+                if (Unit* caster = GetCaster())
+                    if (caster->HasAura(SPELL_TRICKY_TREAT_TRIGGER) && caster->GetAuraCount(SPELL_TRICKY_TREAT_SPEED) > 3 && roll_chance_i(33))
+                        caster->CastSpell(caster, SPELL_UPSET_TUMMY, true);
+            }
+
+            void Register()
+            {
+                OnEffectHitTarget += SpellEffectFn(spell_gen_tricky_treat_SpellScript::HandleScript, EFFECT_0, SPELL_EFFECT_SCRIPT_EFFECT);
+            }
+        };
+
+        SpellScript* GetSpellScript() const
+        {
+            return new spell_gen_tricky_treat_SpellScript();
         }
 };
 
@@ -3315,6 +3357,7 @@ void AddSC_generic_spell_scripts()
     new spell_gen_remove_flight_auras();
     new spell_gen_trick();
     new spell_gen_trick_or_treat();
+    new spell_gen_tricky_treat();
     new spell_creature_permanent_feign_death();
     new spell_pvp_trinket_wotf_shared_cd();
     new spell_gen_animal_blood();
