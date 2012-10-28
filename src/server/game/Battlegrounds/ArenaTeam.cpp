798f95b75c3816c3aa320d8f9b07d7252177bbde
Shocker <shocker@freakz.ro>
2012-10-29 00:37:08 +0200

    Core/Arena: Fix arena team existing name checking on team create

--- src/server/game/Battlegrounds/ArenaTeam.cpp
+++ src/server/game/Battlegrounds/ArenaTeam.cpp
@@ -46,7 +46,7 @@ bool ArenaTeam::Create(uint64 captainGuid, uint8 type, std::string teamName, uin
         return false;
 
     // Check if arena team name is already taken
-    if (sArenaTeamMgr->GetArenaTeamByName(TeamName))
+    if (sArenaTeamMgr->GetArenaTeamByName(teamName))
         return false;
 
     // Generate new arena team id
