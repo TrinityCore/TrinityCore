191b54fc2ebe6c7a5112b610f8e4ae3d24e28c6f
CeIa <thijs.emmerink@gmail.com>
2012-10-29 16:31:57 +0100

    Commit fix 2

--- sql/updates/world/2012_10_29_00_world_misc.sql
+++ sql/updates/world/2012_10_29_00_world_misc.sql
@@ -1,4 +1,4 @@
-DELETE FROM `spell_script_names` WHERE `spell_id`=44436;
+DELETE FROM `spell_script_names` WHERE `spell_id` IN(24750, 24751, 44436);
 INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
 (24750, 'spell_trick'),
 (24751, 'spell_trick_or_treat'),
@@ -10,6 +10,6 @@ INSERT INTO `spell_linked_spell` (`spell_trigger`,`spell_effect`,`type`,`comment
 (42966,-42965,0, 'Hallows End - Upset Tummy removes Tricky Treat'),
 (42966,-42919,0, 'Hallows End - Upset Tummy removes Tricky Treat');
 
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=17 AND `SourceEntry`=44436;
-INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceEntry`,`ConditionTypeOrReference`,`ConditionValue1`) VALUES
-(17, 44436, 11, 42966);
+DELETE FROM `conditions` WHERE `SourceEntry`=44436 AND `SourceTypeOrReferenceId`=17;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceGroup`,`SourceEntry`,`SourceId`,`ElseGroup`,`ConditionTypeOrReference`,`ConditionTarget`,`ConditionValue1`,`ConditionValue2`,`ConditionValue3`,`NegativeCondition`,`ErrorTextId`,`ScriptName`,`Comment`) VALUES
+(17, 0, 44436, 0, 0, 1, 0, 42966, 0, 0, 1, 0, '','Tricky Treat casted only if Upset Tummy aura is missing');
