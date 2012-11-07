05dd9c63b936f92bd80704bec39378147e66a186
zamalaev <zamalaev88@mail.ru>
2012-11-07 14:24:24 +0300

    Core/Script: fix goblin quest 14069 and 28414

---
+++ sql/updates/custom/r_2.sql
@@ -0,0 +1,5 @@
+-- fix q 14069
+UPDATE `creature_template` SET `ScriptName` = 'npc_defiant_troll' WHERE `entry` = '34830';
+
+-- fix q 28414
+UPDATE `creature_template` SET `ScriptName` = 'npc_fourth_and_goal_target' WHERE `entry` = '37203';
