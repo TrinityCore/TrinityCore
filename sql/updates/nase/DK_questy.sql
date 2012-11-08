9c121b5bab8c28ab01657462ee89e13bdde7c703
Dessus <honza-master@seznam.cz>
2012-11-08 20:36:02 +0100

    dk q, minioni a savage

---
+++ sql/updates/nase/DK_questy.sql
@@ -0,0 +1,2 @@
+-- faze nejde
+UPDATE `world`.`quest_template` SET `Method` = '0' WHERE `quest_template`.`Id` =12687;
---
+++ sql/updates/nase/savage_set_honors.sql
@@ -0,0 +1,2 @@
+UPDATE npc_vendor SET ExtendedCost = 2403 WHERE ExtendedCost = 2569 AND entry IN(32832, 32834);
+UPDATE npc_vendor SET ExtendedCost = 2289 WHERE ExtendedCost = 2570 AND entry IN(32832, 32834);
