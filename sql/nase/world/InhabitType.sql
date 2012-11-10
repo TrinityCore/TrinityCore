c9ab66a749d2917a93d930b27142b4dc4b4e16fb
Saqirmdev <1king2@azet.sk>
2012-11-10 22:55:19 +0100

    Update Readme File Change SQL files and Add InhabitType

---
+++ sql/nase/world/InhabitType.sql
@@ -0,0 +1,6 @@
+
+
+
+UPDATE creature_template SET InhabitType=7 WHERE name LIKE '%flying%'; 
+
+UPDATE `creature_template` SET `InhabitType`=7 WHERE  `entry` in (16168, 3869, 16446, 17906, 24440, 24624, 26491, 26517, 27829, 28749, 29239, 28874, 121212, 8534, 29576, 37544, 38481, 31040);
--- sql/nase/world/mmaps_Disables_and_Point.sql
+++ sql/nase/world/mmaps_Disables_and_Point.sql
@@ -1,6 +1,4 @@
 REPLACE INTO `creature_template` VALUES (121212, 0, 0, 0, 0, 0, 10045, 0, 0, 0, 'Mmaps', 'Waypoint', NULL, 0, 1, 1, 2, 35, 35, 0, 1, 1.14286, 0.5, 0, 422, 586, 0, 642, 1, 2000, 2200, 2, 0, 2048, 8, 0, 0, 0, 0, 0, 345, 509, 103, 6, 72, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 7, 9, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, '', 12340);
 
-UPDATE `creature_template` SET `InhabitType`=7 WHERE  `entry` in (16168, 3869, 16446, 17906, 24440, 24624, 26491, 26517, 27829, 28749, 29239, 28874, 121212, 8534, 29576, 37544, 38481, 31040); 
-
 REPLACE INTO `disables` VALUES (7, 650, 8, '', '', 'Mmaps - Trial of the Champion');
 REPLACE INTO `disables` VALUES (7, 649, 8, '', '', 'Mmaps - Trial of the Crusader');
