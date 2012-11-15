bb45d10c214a6a628eac3b58eb7652bf133dc995
Saqirmdev <1king2@azet.sk>
2012-11-15 20:49:27 +0100

    Update AI Icecrown Citadel

--- src/server/scripts/Northrend/IcecrownCitadel/boss_the_lich_king.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_the_lich_king.cpp
@@ -1478,7 +1478,7 @@ class npc_valkyr_shadowguard : public CreatureScript
                 _events.Reset();
                 me->SetReactState(REACT_PASSIVE);
                 DoCast(me, SPELL_WINGS_OF_THE_DAMNED, false);
-                me->SetSpeed(MOVE_FLIGHT, 0.642857f, true);
+                me->SetSpeed(MOVE_FLIGHT, 0.542857f, true);
             }
 
             void IsSummonedBy(Unit* /*summoner*/)
