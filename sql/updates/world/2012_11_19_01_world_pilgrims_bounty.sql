236863f4426f2e2b4cc94baa4beb04749e7eb13b
ZxBiohazardZx <zxbiohazardzx@gmail.com>
2012-11-19 21:20:56 +0100

    Commit: change SQL name to match indexing

--- sql/updates/world/2012_11_19_01_world_pilgrims_bounty.sql
+++ sql/updates/world/2012_11_19_01_world_pilgrims_bounty.sql
@@ -4,7 +4,7 @@
 SET @Event = 26; -- game_event.entry - Set by TDB
 DELETE FROM `game_event` WHERE `eventEntry`=@Event;
 INSERT INTO `game_event` (`eventEntry`,`start_time`,`end_time`,`occurence`,`length`,`holiday`,`description`,`world_event`) VALUES
-(@Event,'2011-11-20 01:00:00','2020-12-31 05:00:00',525600,10020,404, 'Pilgrim''s Bounty',0);
+(@Event,'2011-11-18 01:00:00','2020-12-31 05:00:00',525600,10020,404, 'Pilgrim''s Bounty',0);
 -- ------------------
 -- -- Event Spawns --
 -- ------------------
