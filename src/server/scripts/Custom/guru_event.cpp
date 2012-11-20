d205bd1cd90701ef71e569d7398da6d0aaabf6ed
Dessus <honza-master@seznam.cz>
2012-11-20 20:40:45 +0100

    Guru event ...

--- src/server/scripts/Custom/guru_event.cpp
+++ src/server/scripts/Custom/guru_event.cpp
@@ -3,7 +3,14 @@
 */
 
 #include "ScriptPCH.h"
+#include "Group.h"
 
+enum guruQuests{
+	GURU_QUEST_2VX = 100001,
+	GURU_QUEST_3VX = 100002,
+	GURU_QUEST_5VX = 100003,
+	GURU_QUEST_7VX = 100004
+};
 
 enum EncounterTypes{
 	GURU_2VX	= 1,
@@ -12,17 +19,40 @@ enum EncounterTypes{
 	GURU_7VX	= 4
 };
 
-struct GuruAnnouncerMenus{
+enum GuruActions{
+	GURU_ACTION_2VX		= 10,	// 2VX battle start
+	GURU_ACTION_3VX		= 11,	// 3VX battle start
+	GURU_ACTION_5VX		= 12,	// 5VX battle start
+	GURU_ACTION_7VX		= 13	// 7VX battle start
+};
+
+
+
+// yells
+struct GuruAnnouncerYells{
 	std::string text;
 	EncounterTypes encounterType;
 
 };
-
-static GuruAnnouncerMenus _messages[] = {
+static GuruAnnouncerYells yells[] = {
 	{"You've chosen a battle against 2 Heroes! It's the easiest battle there so a reward will not be the best! Good Luck and Have Fun!", GURU_2VX},
 	{"You've chosen a battle against 3 Heroes! Good Luck and Have Fun!", GURU_3VX},
 	{"You've chosen a battle against 3 Heroes! Good Luck and Have Fun!", GURU_5VX},
 	{"You've chosen a battle against 7 Heroes! It's the hardest battle there so a reward will be the best! Good Luck and Have Fun!", GURU_7VX}
+
+};
+
+// gossip menu texts
+struct GuruAnnouncerMenus{
+	std::string text;
+	GuruActions action;
+
+};
+static GuruAnnouncerMenus gossip[] = {
+	{"Summon 2 heroes!", GURU_ACTION_2VX},
+	{"Summon 3 heroes!", GURU_ACTION_3VX},
+	{"Summon 5 heroes!", GURU_ACTION_5VX},
+	{"Summon 7 heroes!", GURU_ACTION_7VX}
 
 };
 
@@ -32,18 +62,63 @@ class npc_guru_announcer : public CreatureScript
 public:
     npc_guru_announcer() : CreatureScript("npc_guru_announcer") { }
 
+	bool addGossipMenu(Player* player, Creature* creature){
+
+
+		Group* group = player->GetGroup();
+		
+
+		if(player->isInCombat())
+        {
+            player->CLOSE_GOSSIP_MENU();
+            creature->MonsterWhisper("You are in a combat!", player->GetGUID());
+			return false;
+        }
+        else
+        {
+			if(group){
+				if(!group->IsLeader(player->GetGUID())){
+					creature->MonsterWhisper("You are not a party leader!", player->GetGUID());	
+					player->CLOSE_GOSSIP_MENU();
+					return false;
+				}
+
+			}
+			if(CheckEventState()){
+				player->ADD_GOSSIP_ITEM(7, "Yes! I Want to start the battle!", 1, 1);
+			}
+        }
+        player->SEND_GOSSIP_MENU(60000, creature->GetGUID());	
+		return true;
+	}
+
+
+
+
     bool OnGossipHello(Player *player, Creature *_creature)
     {
-        
+        addGossipMenu(player, _creature);
         return true;
     }
 
-
-    
     bool OnGossipSelect(Player *player, Creature *_creature, uint32 sender, uint32 uiAction)
     {
+		if(uiAction == 1){
+			// select count of heroes
+			int len = sizeof(gossip) / sizeof(int);
+			for(int i = 0; i < len; i++){
+				player->ADD_GOSSIP_ITEM(7, gossip[i].text, gossip[i].action, 2);
+			}
+		}
         return true;
     }
+
+
+	bool CheckEventState(){
+		return true;
+	
+	}
+
 };
 
 class npc_guru_shaman : public CreatureScript
@@ -178,6 +253,8 @@ public:
         return true;
     }
 };
+
+
 
 class npc_guru_paladin : public CreatureScript
 {
