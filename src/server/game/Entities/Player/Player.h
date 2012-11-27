7749727ed8425b4ae48fa42487877c96116745dc
Spp <spp@jorge.gr>
2012-11-27 07:44:27 +0100

    Core/Units: make SetHover virtual

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -2279,8 +2279,6 @@ class Player : public Unit, public GridObject<Player>
             m_mover->m_movedPlayer = this;
         }
 
-        bool SetHover(bool enable);
-
         void SetSeer(WorldObject* target) { m_seer = target; }
         void SetViewpoint(WorldObject* target, bool apply);
         WorldObject* GetViewpoint() const;
