83591fb7a69e2267a634db1ca3fd9bc775395a84
tibbi <tibbi@centrum.sk>
2012-11-27 16:45:39 +0100

    [gc516] add linked spell to Gas Spore and Slime Spray, author Xanvial (?)

--- sql/gc/gc516.sql
+++ sql/gc/gc516.sql
@@ -1475,6 +1475,18 @@ INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `l
 UPDATE `reference_loot_template` SET `ChanceOrQuestChance` = 0.3 WHERE `entry` = 35069 AND `mincountOrRef` = -35068;
 UPDATE `reference_loot_template` SET `ChanceOrQuestChance` = 0.2 WHERE `entry` = 35069 AND `mincountOrRef` = -35074;
 
+-- Add linked spell to Gas Spore and Slime Spray
+DELETE FROM `spell_linked_spell` WHERE `spell_effect` IN (72144,72145);
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES
+(69291, 72144, 1, 'Festergut: Apply Orange Blight Residue'),
+(72101, 72144, 1, 'Festergut: Apply Orange Blight Residue'),
+(72102, 72144, 1, 'Festergut: Apply Orange Blight Residue'),
+(72103, 72144, 1, 'Festergut: Apply Orange Blight Residue'),
+(69507, 72145, 1, 'Rotface: Apply Green Blight Residue'),
+(71213, 72145, 1, 'Rotface: Apply Green Blight Residue'),
+(73189, 72145, 1, 'Rotface: Apply Green Blight Residue'),
+(73190, 72145, 1, 'Rotface: Apply Green Blight Residue');
+
 ####################
 -- Forge of Souls ##
 ####################
