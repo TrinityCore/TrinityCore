d2fb1ad579d3f51cdc8bacc62ac9449cf6ac5aac
Saqirmdev <1king2@azet.sk>
2012-11-27 17:45:09 +0100

    Missing Commit

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -24,6 +24,10 @@ void AddSC_example_gossip_codebox();
 void AddSC_example_misc();
 void AddSC_example_commandscript();
 
+//Custom
+void spell_mage_flame_orb();
+void spell_mage_frostfire_orb();
+
 // spells
 void AddSC_deathknight_spell_scripts();
 void AddSC_druid_spell_scripts();
@@ -1264,6 +1268,7 @@ void AddCustomScripts()
 {
 #ifdef SCRIPTS
     /* This is where custom scripts should be added. */
-
+    AddSC_spell_mage_frostfire_orb();
+	AddSC_spell_mage_flame_orb();
 #endif
 }
