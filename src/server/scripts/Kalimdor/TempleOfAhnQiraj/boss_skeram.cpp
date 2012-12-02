a73a7dc43ffb8b65dcfab3b79976ea7b49634524
Gacko <gacko28@gmx.de>
2012-12-02 19:13:57 +0100

    Core/DB: Creature text for 37 script files

--- src/server/scripts/Kalimdor/TempleOfAhnQiraj/boss_skeram.cpp
+++ src/server/scripts/Kalimdor/TempleOfAhnQiraj/boss_skeram.cpp
@@ -29,19 +29,18 @@ EndScriptData */
 #include "Group.h"
 #include "Player.h"
 
-#define SAY_AGGRO1                  -1531000
-#define SAY_AGGRO2                  -1531001
-#define SAY_AGGRO3                  -1531002
-#define SAY_SLAY1                   -1531003
-#define SAY_SLAY2                   -1531004
-#define SAY_SLAY3                   -1531005
-#define SAY_SPLIT                   -1531006
-#define SAY_DEATH                   -1531007
-
-#define SPELL_ARCANE_EXPLOSION      25679
-#define SPELL_EARTH_SHOCK           26194
-#define SPELL_TRUE_FULFILLMENT      785
-#define SPELL_BLINK                 28391
+enum Skeram
+{
+    SAY_AGGRO                   = 0,
+    SAY_SLAY                    = 1,
+    SAY_SPLIT                   = 2,
+    SAY_DEATH                   = 3,
+    
+    SPELL_ARCANE_EXPLOSION      = 25679,
+    SPELL_EARTH_SHOCK           = 26194,
+    SPELL_TRUE_FULFILLMENT      = 785,
+    SPELL_BLINK                 = 28391
+};
 
 class ov_mycoordinates
 {
@@ -104,20 +103,20 @@ public:
 
         void KilledUnit(Unit* /*victim*/)
         {
-            DoScriptText(RAND(SAY_SLAY1, SAY_SLAY2, SAY_SLAY3), me);
+            Talk(SAY_SLAY);
         }
 
         void JustDied(Unit* /*killer*/)
         {
             if (!IsImage)
-                DoScriptText(SAY_DEATH, me);
+                Talk(SAY_DEATH);
         }
 
         void EnterCombat(Unit* /*who*/)
         {
             if (IsImage || Images75)
                 return;
-            DoScriptText(RAND(SAY_AGGRO1, SAY_AGGRO2, SAY_AGGRO3), me);
+            Talk(SAY_AGGRO);
         }
 
         void UpdateAI(const uint32 diff)
@@ -209,7 +208,7 @@ public:
 
         void DoSplit(int atPercent /* 75 50 25 */)
         {
-            DoScriptText(SAY_SPLIT, me);
+            Talk(SAY_SPLIT);
 
             ov_mycoordinates *place1 = new ov_mycoordinates(-8340.782227f, 2083.814453f, 125.648788f, 0);
             ov_mycoordinates *place2 = new ov_mycoordinates(-8341.546875f, 2118.504639f, 133.058151f, 0);
