c39685696d114655c26cc13beac07bc55b693597
Vincent-Michael <Vincent_Michael@gmx.de>
2012-12-04 11:23:10 +0100

    Core/Quest: Removed table quest_start_scripts

--- src/server/game/Quests/QuestDef.h
+++ src/server/game/Quests/QuestDef.h
@@ -244,7 +244,6 @@ class Quest
         uint32 GetPointOpt() const { return PointOption; }
         uint32 GetIncompleteEmote() const { return EmoteOnIncomplete; }
         uint32 GetCompleteEmote() const { return EmoteOnComplete; }
-        uint32 GetQuestStartScript() const { return StartScript; }
         uint32 GetQuestCompleteScript() const { return CompleteScript; }
         bool   IsRepeatable() const { return Flags & QUEST_TRINITY_FLAGS_REPEATABLE; }
         bool   IsAutoAccept() const;
@@ -355,7 +354,6 @@ class Quest
         uint32 PointOption;
         uint32 EmoteOnIncomplete;
         uint32 EmoteOnComplete;
-        uint32 StartScript;
         uint32 CompleteScript;
 };
 
