b7df0c4f60c60773434366d2f82689cf206de7b9
Gary <fishermanpl@gmail.com>
2012-12-07 13:29:58 +0100

    WTF...

--- src/server/scripts/Spells/spell_warrior.cpp
+++ src/server/scripts/Spells/spell_warrior.cpp
@@ -340,7 +340,6 @@ class spell_warr_execute_bonus : public SpellScriptLoader
                     return false;
                 return true;
             }
-
             void HandleDummy(SpellEffIndex /* effIndex */)
             {
                 int32 bp0 = GetEffectValue();
@@ -378,6 +377,67 @@ class spell_warr_execute_bonus : public SpellScriptLoader
         AuraScript* GetAuraScript() const
         {
             return new spell_warr_execute_bonus_AuraScript();
+        }
+};
+
+enum Execute
+{
+    SPELL_EXECUTE               = 20647,
+    SPELL_GLYPH_OF_EXECUTION    = 58367,
+    ICON_ID_SUDDEN_DEATH        = 1989,
+};
+
+class spell_warr_execute : public SpellScriptLoader
+{
+    public:
+        spell_warr_execute() : SpellScriptLoader("spell_warr_execute") { }
+
+        class spell_warr_execute_SpellScript : public SpellScript
+        {
+            PrepareSpellScript(spell_warr_execute_SpellScript);
+
+            bool Validate(SpellInfo const* /*SpellEntry*/)
+            {
+                if (!sSpellMgr->GetSpellInfo(SPELL_EXECUTE) || !sSpellMgr->GetSpellInfo(SPELL_GLYPH_OF_EXECUTION))
+                    return false;
+                return true;
+            }
+            void HandleDummy(SpellEffIndex effIndex)
+            {
+                Unit* caster = GetCaster();
+                if (Unit* target = GetHitUnit())
+                {
+                    SpellInfo const* spellInfo = GetSpellInfo();
+                    int32 rageUsed = std::min<int32>(300 - spellInfo->CalcPowerCost(caster, SpellSchoolMask(spellInfo->SchoolMask)), caster->GetPower(POWER_RAGE));
+                    int32 newRage = std::max<int32>(0, caster->GetPower(POWER_RAGE) - rageUsed);
+
+                    // Sudden Death rage save
+                    if (AuraEffect* aurEff = caster->GetAuraEffect(SPELL_AURA_PROC_TRIGGER_SPELL, SPELLFAMILY_GENERIC, ICON_ID_SUDDEN_DEATH, EFFECT_0))
+                    {
+                        int32 ragesave = aurEff->GetSpellInfo()->Effects[EFFECT_1].CalcValue() * 10;
+                        newRage = std::max(newRage, ragesave);
+                    }
+
+                    caster->SetPower(POWER_RAGE, uint32(newRage));
+                    // Glyph of Execution bonus
+                    if (AuraEffect* aurEff = caster->GetAuraEffect(SPELL_GLYPH_OF_EXECUTION, EFFECT_0))
+                        rageUsed += aurEff->GetAmount() * 10;
+
+
+                    int32 bp = GetEffectValue() + int32(rageUsed * spellInfo->Effects[effIndex].DamageMultiplier + caster->GetTotalAttackPowerValue(BASE_ATTACK) * 0.2f);
+                    caster->CastCustomSpell(target,SPELL_EXECUTE,&bp,0,0,true,0,0,GetOriginalCaster()->GetGUID());
+                }
+            }
+
+            void Register()
+            {
+                OnEffectHitTarget += SpellEffectFn(spell_warr_execute_SpellScript::HandleDummy, EFFECT_0, SPELL_EFFECT_DUMMY);
+            }
+        };
+
+        SpellScript* GetSpellScript() const
+        {
+            return new spell_warr_execute_SpellScript();
         }
 };
 
@@ -538,6 +598,7 @@ void AddSC_warrior_spell_scripts()
     new spell_warr_deep_wounds();
     new spell_warr_charge();
     new spell_warr_slam();
+    new spell_warr_execute_bonus();
     new spell_warr_execute();
     new spell_warr_concussion_blow();
     new spell_warr_bloodthirst();
