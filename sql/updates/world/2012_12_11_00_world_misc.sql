b60aa2acdcb1cb09f981427c29b5a1e4bd7e2982
thebillkidy <thebillkidy@hotmail.com>
2012-12-11 22:24:20 +0100

    Updated to match structure?

--- sql/updates/world/2012_12_11_00_world_misc.sql
+++ sql/updates/world/2012_12_11_00_world_misc.sql
@@ -4,24 +4,12 @@
 -- -------------------------
 ALTER TABLE `creature_classlevelstats`
 	ADD PRIMARY KEY (`level`, `class`);
-	
-ALTER TABLE `event_scripts`
-	ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `game_event_arena_seasons`
 	ADD PRIMARY KEY (`eventEntry`, `season`);
 
-ALTER TABLE `gameobject_scripts`
-	ADD PRIMARY KEY (`id`);
-
 ALTER TABLE `playercreateinfo_item`
 	ADD PRIMARY KEY (`race`, `class`, `itemid`);
-
-ALTER TABLE `quest_end_scripts`
-	ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `quest_start_scripts`
-	ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `spell_enchant_proc_data`
 	ADD PRIMARY KEY (`entry`);
@@ -31,9 +19,6 @@ ALTER TABLE `spell_linked_spell`
 
 ALTER TABLE `spell_script_names`
 	ADD PRIMARY KEY (`spell_id`, `ScriptName`);
-	
-ALTER TABLE `spell_scripts`
-	ADD PRIMARY KEY (`id`, `datalong`, `datalong2`);
 	
 ALTER TABLE `version`
 	ADD PRIMARY KEY (`core_version`);
