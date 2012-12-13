d91145b0cf601d3db764239bd35d10dbb29962ff
SignFinder <signfinder@gmail.com>
2012-12-13 14:37:42 +0400

    DB/YTDB: Add needed for YTDB632 updates.

--- sql/updates/world_ytdb/2012_11_13_02_world_creature.sql
+++
@@ -1,3 +0,0 @@
--- Fix [Q]{A/H} Meet At The Grave
-UPDATE `creature_template_addon` SET `auras`='10848' WHERE `entry`=9299; -- allow to be seen in world of death
-UPDATE `creature_template` SET `npcflag`=32770,`unit_flags`=768 WHERE `entry`=9299; -- makes unseen in world of living
