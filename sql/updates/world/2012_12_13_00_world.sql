fef3b31f5451b3dec95d15e3c6a1d521310bcc0d
Nay <dnpd.dd@gmail.com>
2012-12-14 01:16:14 +0000

    DB/World: Correct some waypoint_data and properly add PK to this table

--- sql/updates/world/2012_12_13_00_world.sql
+++ sql/updates/world/2012_12_13_00_world.sql
@@ -1,7 +1,4 @@
-DELETE FROM `waypoint_data` WHERE `id`=389230 AND `point`=11 AND `position_z`=21.8886; -- duplicated point with wrong `point`
-
 ALTER TABLE `creature_classlevelstats` ADD PRIMARY KEY (`level`, `class`);
 ALTER TABLE `playercreateinfo_item` ADD PRIMARY KEY (`race`, `class`, `itemid`); -- already got (race, class) index
 ALTER TABLE `spell_enchant_proc_data` ADD PRIMARY KEY (`entry`);
 ALTER TABLE `version` ADD PRIMARY KEY (`core_version`);
-ALTER TABLE `waypoint_data` ADD PRIMARY KEY (`id`, `point`);
