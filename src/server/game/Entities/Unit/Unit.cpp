9e580df27a365c0f8b32e0b5f19cb52b7d7e939c
Gacko <gacko28@gmx.de>
2012-12-22 16:42:46 +0100

    Core/Spell: Shadows Fate crash fix

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -5598,18 +5598,18 @@ bool Unit::HandleDummyAuraProc(Unit* victim, uint32 damage, AuraEffect* triggere
                         ok = true;
                     }
 
-                    // The spell usually only hits one target, but it should hit every player
-                    // which applied Shadows Fate at least once to the victim.
-                    if (target->GetTypeId() == TYPEID_PLAYER && target->ToPlayer()->GetQuestStatus(24547) == QUEST_STATUS_INCOMPLETE)
+                    // TODO: The aura should be applieable by multiple players
+                    Unit* caster = triggeredByAura->GetCaster();
+                    if (caster && caster->GetTypeId() == TYPEID_PLAYER && caster->ToPlayer()->GetQuestStatus(24547) == QUEST_STATUS_INCOMPLETE)
                     {
-                        triggered_spell_id = 71203;
+                        CastSpell(caster, 71203, true);
                         ok = true;
                     }
 
                     if (!ok)
                         return false;
 
-                    break;
+                    return true;
                 }
                 // Essence of the Blood Queen
                 case 70871:
