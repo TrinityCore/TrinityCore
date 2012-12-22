feed631c7f9a8ad89b30e565784c55772a96a593
Vincent_Michael <Vincent_Michael@gmx.de>
2012-12-22 01:13:56 +0100

    Core: Fix more some codestyle

--- src/server/scripts/Kalimdor/moonglade.cpp
+++ src/server/scripts/Kalimdor/moonglade.cpp
@@ -323,7 +323,7 @@ public:
         }
 
         void IsSummonedBy(Unit* /*summoner*/)
-        {            
+        {
             std::list<Player*> playerOnQuestList;
             Trinity::AnyPlayerInObjectRangeCheck checker(me, 5.0f);
             Trinity::PlayerListSearcher<Trinity::AnyPlayerInObjectRangeCheck> searcher(me, playerOnQuestList, checker);
