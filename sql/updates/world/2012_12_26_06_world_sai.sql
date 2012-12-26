7a96d8fa0830582ed69567bb53902cce6d367f79
Malcrom <malcromdev@gmail.com>
2012-12-26 14:52:41 -0330

    DB/SAI: Fix flags on previous commit

--- sql/updates/world/2012_12_26_06_world_sai.sql
+++ sql/updates/world/2012_12_26_06_world_sai.sql
@@ -200,10 +200,10 @@ UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
 DELETE FROM `creature_ai_scripts` WHERE `creature_id`=@ENTRY;
 DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
-(@ENTRY,0,0,0,4,0,15,1,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0, 'Defias Henchman - On Aggro - Say random text'),
-(@ENTRY,0,1,0,13,0,100,1,3200,13800,0,0,11,8242,0,0,0,0,0,2,0,0,0,0,0,0,0, 'Defias Henchman - Combat - Cast Shield Slam'),
-(@ENTRY,0,2,0,0,0,100,1,3600,18800,0,0,11,3248,0,0,0,0,0,1,0,0,0,0,0,0,0, 'Defias Henchman - Combat - Cast Improved Blocking'),
-(@ENTRY,0,3,0,2,0,100,1,0,15,0,0,25,1,0,0,0,0,0,1,0,0,0,0,0,0,0, 'Defias Henchman - HP@15% - Flee');
+(@ENTRY,0,0,0,4,0,15,3,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0, 'Defias Henchman - On Aggro - Say random text'),
+(@ENTRY,0,1,0,13,0,100,3,3200,13800,0,0,11,8242,0,0,0,0,0,2,0,0,0,0,0,0,0, 'Defias Henchman - Combat - Cast Shield Slam'),
+(@ENTRY,0,2,0,0,0,100,3,3600,18800,0,0,11,3248,0,0,0,0,0,1,0,0,0,0,0,0,0, 'Defias Henchman - Combat - Cast Improved Blocking'),
+(@ENTRY,0,3,0,2,0,100,3,0,15,0,0,25,1,0,0,0,0,0,1,0,0,0,0,0,0,0, 'Defias Henchman - HP@15% - Flee');
 
 -- SAI for Defias Digger
 SET @ENTRY := 824;
@@ -211,8 +211,8 @@ UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
 DELETE FROM `creature_ai_scripts` WHERE `creature_id`=@ENTRY;
 DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
-(@ENTRY,0,0,0,4,0,15,1,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0, 'Defias Digger - On Aggro - Say random text'),
-(@ENTRY,0,1,0,2,0,100,1,0,15,0,0,25,1,0,0,0,0,0,1,0,0,0,0,0,0,0, 'Defias Digger - HP@15% - Flee');
+(@ENTRY,0,0,0,4,0,15,3,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0, 'Defias Digger - On Aggro - Say random text'),
+(@ENTRY,0,1,0,2,0,100,3,0,15,0,0,25,1,0,0,0,0,0,1,0,0,0,0,0,0,0, 'Defias Digger - HP@15% - Flee');
 
 -- SAI for Surena Caledon
 SET @ENTRY := 881;
