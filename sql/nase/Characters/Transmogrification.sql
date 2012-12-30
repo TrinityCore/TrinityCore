ca2a2f38c63f00461f6c99356d5a653d66db9aa9
Saqirmdev <Saqirm@gmail.com>
2012-12-30 14:58:54 +0100

    Add req. SQL

--- sql/nase/Characters/Transmogrification.sql
+++ sql/nase/Characters/Transmogrification.sql
@@ -1,8 +1,18 @@
 
 
 DROP TABLE IF EXISTS `fake_items`;
-CREATE TABLE `fake_items` (
-  `guid` int(11) NOT NULL,
-  `fakeEntry` int(11) NOT NULL,
-  PRIMARY KEY (`guid`)
-);
+CREATE TABLE `custom_transmogrification` (
+
+`GUID` INT(10) UNSIGNED NOT NULL COMMENT 'Item guidLow',
+`FakeEntry` INT(10) UNSIGNED NOT NULL COMMENT 'Item entry',
+
+`Owner` INT(10) UNSIGNED NOT NULL COMMENT 'Player guidLow',
+PRIMARY KEY (`GUID`)
+)
+
+
+COMMENT='version 4.0'
+
+COLLATE='latin1_swedish_ci'
+
+ENGINE=InnoDB;
--- sql/nase/world/Transmogrify.sql
+++
@@ -1,5 +0,0 @@
-REPLACE INTO `creature_template` VALUES (190001, 0, 0, 0, 0, 0, 15998, 0, 0, 0, 'Transmogrify Master', '', 'Speak', 50000, 71, 71, 0, 35, 35, 3, 1, 1.14286, 1.25, 1, 0, 0, 0, 0, 1, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 138936390, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 1, 1.56, 1.56, 1.56, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 2, 'npc_transmogrify', 1);
-
-INSERT INTO `gossip_menu` VALUES (51000, 51000);
-INSERT INTO npc_text (ID, text0_0, em0_1) VALUES
-(51000, 'Put in the first slot of bag item, that you want to transmogrify. In the second slot, put item with perfect display.', 0);
