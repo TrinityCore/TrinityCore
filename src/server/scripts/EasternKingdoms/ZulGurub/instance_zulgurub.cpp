58f9bdd3bd545f8adb19a7a1c302c39076919143
Vincent_Michael <Vincent_Michael@gmx.de>
2012-12-30 03:01:12 +0100

    Core/ZulGurub: Added script for Bloodlord Mandokir ToDo: Remove hack :P

--- src/server/scripts/EasternKingdoms/ZulGurub/instance_zulgurub.cpp
+++ src/server/scripts/EasternKingdoms/ZulGurub/instance_zulgurub.cpp
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) 2008-2012 TrinityCore <http://www.trinitycore.org/>
- * Copyright (C) 2006-2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -20,6 +19,16 @@
 #include "InstanceScript.h"
 #include "zulgurub.h"
 
+DoorData const doorData[] =
+{
+    { GO_VENOXIS_COIL,                  DATA_VENOXIS,   DOOR_TYPE_ROOM, BOUNDARY_NONE },
+    { GO_ARENA_DOOR_1,                  DATA_MANDOKIR,  DOOR_TYPE_ROOM, BOUNDARY_NONE },
+    { GO_FORCEFIELD,                    DATA_KILNARA,   DOOR_TYPE_ROOM, BOUNDARY_NONE },
+    { GO_ZANZIL_DOOR,                   DATA_ZANZIL,    DOOR_TYPE_ROOM, BOUNDARY_NONE },
+    //{ GO_THE_CACHE_OF_MADNESS_DOOR,     DATA_xxxxxxx,   DOOR_TYPE_ROOM, BOUNDARY_NONE },
+    { 0,                                0,              DOOR_TYPE_ROOM, BOUNDARY_NONE }
+};
+
 class instance_zulgurub : public InstanceMapScript
 {
     public:
@@ -30,6 +39,7 @@ class instance_zulgurub : public InstanceMapScript
             instance_zulgurub_InstanceMapScript(Map* map) : InstanceScript(map)
             {
                 SetBossNumber(EncounterCount);
+                LoadDoorData(doorData);
                 venoxisGUID         = 0;
                 mandokirGUID        = 0;
                 kilnaraGUID         = 0;
@@ -75,6 +85,38 @@ class instance_zulgurub : public InstanceMapScript
                         break;
                     case NPC_JINDO_TRIGGER:
                         jindoTiggerGUID = creature->GetGUID();
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            void OnGameObjectCreate(GameObject* go)
+            {
+                switch (go->GetEntry())
+                {
+                    case GO_VENOXIS_COIL:
+                    case GO_ARENA_DOOR_1:
+                    case GO_FORCEFIELD:
+                    case GO_ZANZIL_DOOR:
+                    case GO_THE_CACHE_OF_MADNESS_DOOR:
+                        AddDoor(go, true);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            void OnGameObjectRemove(GameObject* go)
+            {
+                switch (go->GetEntry())
+                {
+                    case GO_VENOXIS_COIL:
+                    case GO_ARENA_DOOR_1:
+                    case GO_FORCEFIELD:
+                    case GO_ZANZIL_DOOR:
+                    case GO_THE_CACHE_OF_MADNESS_DOOR:
+                        AddDoor(go, false);
                         break;
                     default:
                         break;
