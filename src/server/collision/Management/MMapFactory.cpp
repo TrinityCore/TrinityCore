08f62d8313e05ed6eb1767eed07a7e31533e9bb6
joschiwald <joschiwald@online.de>
2012-12-30 21:09:21 +0100

    Core/MMaps: use DisableMgr to disable pathfinding on a specific map

--- src/server/collision/Management/MMapFactory.cpp
+++ src/server/collision/Management/MMapFactory.cpp
@@ -19,16 +19,13 @@
 #include "MMapFactory.h"
 #include "World.h"
 #include "Config.h"
-#include <set>
+#include "DisableMgr.h"
 
 namespace MMAP
 {
     // ######################## MMapFactory ########################
     // our global singleton copy
     MMapManager *g_MMapManager = NULL;
-
-    // stores list of mapids which do not use pathfinding
-    std::set<uint32>* g_mmapDisabledIds = NULL;
 
     MMapManager* MMapFactory::createOrGetMMapManager()
     {
@@ -38,39 +35,14 @@ namespace MMAP
         return g_MMapManager;
     }
 
-    void MMapFactory::preventPathfindingOnMaps(const char* ignoreMapIds)
-    {
-        if (!g_mmapDisabledIds)
-            g_mmapDisabledIds = new std::set<uint32>();
-
-        uint32 strLenght = strlen(ignoreMapIds)+1;
-        char* mapList = new char[strLenght];
-        memcpy(mapList, ignoreMapIds, sizeof(char)*strLenght);
-
-        char* idstr = strtok(mapList, ",");
-        while (idstr)
-        {
-            g_mmapDisabledIds->insert(uint32(atoi(idstr)));
-            idstr = strtok(NULL, ",");
-        }
-
-        delete[] mapList;
-    }
-
     bool MMapFactory::IsPathfindingEnabled(uint32 mapId)
     {
         return sWorld->getBoolConfig(CONFIG_ENABLE_MMAPS)
-            && g_mmapDisabledIds->find(mapId) == g_mmapDisabledIds->end();
+            && !DisableMgr::IsDisabledFor(DISABLE_TYPE_MMAP, mapId, NULL, MMAP_DISABLE_PATHFINDING);
     }
 
     void MMapFactory::clear()
     {
-        if (g_mmapDisabledIds)
-        {
-            delete g_mmapDisabledIds;
-            g_mmapDisabledIds = NULL;
-        }
-
         if (g_MMapManager)
         {
             delete g_MMapManager;
