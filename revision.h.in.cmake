11e122778465bf134ba402bca495a201594ace3c
Vincent_Michael <Vincent_Michael@gmx.de>
2013-01-01 12:57:23 +0100

    Update more copyright note for 2013. (alcohol sucks)

--- revision.h.in.cmake
+++ revision.h.in.cmake
@@ -3,7 +3,7 @@
  #define _HASH                      "@rev_hash@"
  #define _DATE                      "@rev_date@"
  #define VER_COMPANYNAME_STR        "TrinityCore Developers"
- #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2012 TrinityCore"
+ #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2013 TrinityCore"
  #define VER_FILEVERSION            0,0,0
  #define VER_FILEVERSION_STR        "@rev_date@ (@rev_hash@)"
  #define VER_PRODUCTVERSION         VER_FILEVERSION
