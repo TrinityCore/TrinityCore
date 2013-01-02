0c85ec97caa9808dda14dc8ef0823b9df27f5735
Spp <spp@jorge.gr>
2013-01-02 21:23:54 +0100

    Core/Misc: Missing changes from 8968dbc (Partial revert of 2292025) Closes #8843

--- src/server/game/Entities/Object/ObjectDefines.h
+++ src/server/game/Entities/Object/ObjectDefines.h
@@ -23,18 +23,18 @@
 
 enum HighGuid
 {
-    HIGHGUID_ITEM           = 0x400,                       // blizz 4000
-    HIGHGUID_CONTAINER      = 0x400,                       // blizz 4000
-    HIGHGUID_PLAYER         = 0x000,                       // blizz 0000
-    HIGHGUID_GAMEOBJECT     = 0xF11,                       // blizz F110
-    HIGHGUID_TRANSPORT      = 0xF12,                       // blizz F120 (for GAMEOBJECT_TYPE_TRANSPORT)
-    HIGHGUID_UNIT           = 0xF13,                       // blizz F130
-    HIGHGUID_PET            = 0xF14,                       // blizz F140
-    HIGHGUID_VEHICLE        = 0xF15,                       // blizz F550
-    HIGHGUID_DYNAMICOBJECT  = 0xF10,                       // blizz F100
+    HIGHGUID_ITEM           = 0x4000,                      // blizz 4000
+    HIGHGUID_CONTAINER      = 0x4000,                      // blizz 4000
+    HIGHGUID_PLAYER         = 0x0000,                      // blizz 0000
+    HIGHGUID_GAMEOBJECT     = 0xF110,                      // blizz F110
+    HIGHGUID_TRANSPORT      = 0xF120,                      // blizz F120 (for GAMEOBJECT_TYPE_TRANSPORT)
+    HIGHGUID_UNIT           = 0xF130,                      // blizz F130
+    HIGHGUID_PET            = 0xF140,                      // blizz F140
+    HIGHGUID_VEHICLE        = 0xF150,                      // blizz F550
+    HIGHGUID_DYNAMICOBJECT  = 0xF100,                      // blizz F100
     HIGHGUID_CORPSE         = 0xF101,                      // blizz F100
-    HIGHGUID_MO_TRANSPORT   = 0x1FC,                       // blizz 1FC0 (for GAMEOBJECT_TYPE_MO_TRANSPORT)
-    HIGHGUID_GROUP          = 0x1F5
+    HIGHGUID_MO_TRANSPORT   = 0x1FC0,                      // blizz 1FC0 (for GAMEOBJECT_TYPE_MO_TRANSPORT)
+    HIGHGUID_GROUP          = 0x1F05
 };
 
 // used for creating values for respawn for example
