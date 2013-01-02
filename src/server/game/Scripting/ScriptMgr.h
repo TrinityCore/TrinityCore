022dac9c4a1f4870f5d690bf8b536e68c539023d
Ellie <ellie1337@gmail.com>
2013-01-02 15:46:31 -0800

    Core/Scripting: Created a hook for when a player is about to be saved.

--- src/server/game/Scripting/ScriptMgr.h
+++ src/server/game/Scripting/ScriptMgr.h
@@ -746,6 +746,9 @@ class PlayerScript : public UnitScript
         // Called when a player is deleted.
         virtual void OnDelete(uint64 /*guid*/) { }
 
+        // Called whe is a player is about to be saved.
+        virtual void OnSave(Player* /*player*/) { }
+
         // Called when a player is bound to an instance
         virtual void OnBindToInstance(Player* /*player*/, Difficulty /*difficulty*/, uint32 /*mapId*/, bool /*permanent*/) { }
 
@@ -1022,6 +1025,7 @@ class ScriptMgr
         void OnPlayerLogout(Player* player);
         void OnPlayerCreate(Player* player);
         void OnPlayerDelete(uint64 guid);
+        void OnPlayerSave(Player* player);
         void OnPlayerBindToInstance(Player* player, Difficulty difficulty, uint32 mapid, bool permanent);
         void OnPlayerUpdateZone(Player* player, uint32 newZone, uint32 newArea);
 
