88a30801f579afb69023a637a12d8fc30996206b
Malcrom <malcromdev@gmail.com>
2013-01-04 01:00:51 -0330

    Core/Scripting: Add Kirtonos The Herald to c++ scripting. Combat still not finished.

--- src/server/scripts/EasternKingdoms/Scholomance/scholomance.h
+++ src/server/scripts/EasternKingdoms/Scholomance/scholomance.h
@@ -19,13 +19,31 @@
 #ifndef DEF_SCHOLOMANCE_H
 #define DEF_SCHOLOMANCE_H
 
-#define TYPE_GANDLING                       1
-#define DATA_DOCTORTHEOLENKRASTINOV_DEATH   2
-#define DATA_INSTRUCTORMALICIA_DEATH        3
-#define DATA_LADYILLUCIABAROV_DEATH         4
-#define DATA_LORDALEXEIBAROV_DEATH          5
-#define DATA_LOREKEEPERPOLKELT_DEATH        6
-#define DATA_THERAVENIAN_DEATH              7
-#define TYPE_KIRTONOS                       8
-#endif
+uint32 const MAX_ENCOUNTER = 2;
 
+enum DataTypes
+{
+    TYPE_GANDLING                       = 1,
+    DATA_DOCTORTHEOLENKRASTINOV_DEATH   = 2,
+    DATA_INSTRUCTORMALICIA_DEATH        = 3,
+    DATA_LADYILLUCIABAROV_DEATH         = 4,
+    DATA_LORDALEXEIBAROV_DEATH          = 5,
+    DATA_LOREKEEPERPOLKELT_DEATH        = 6,
+    DATA_THERAVENIAN_DEATH              = 7,
+    TYPE_KIRTONOS                       = 8
+};
+
+enum GameobjectIds
+{
+    GO_GATE_KIRTONOS                    = 175570,
+    GO_GATE_GANDLING                    = 177374,
+    GO_GATE_MALICIA                     = 177375,
+    GO_GATE_THEOLEN                     = 177377,
+    GO_GATE_POLKELT                     = 177376,
+    GO_GATE_RAVENIAN                    = 177372,
+    GO_GATE_BAROV                       = 177373,
+    GO_GATE_ILLUCIA                     = 177371,
+    GO_BRAZIER_OF_THE_HERALD            = 175564
+};
+
+#endif
