b0955c30215574a335c78d89becea988fdef5e64
Nyeriah <sarah.trysan@live.com>
2013-01-05 13:12:19 -0200

    some style alignment

--- src/server/scripts/Northrend/FrozenHalls/HallsOfReflection/halls_of_reflection.h
+++ src/server/scripts/Northrend/FrozenHalls/HallsOfReflection/halls_of_reflection.h
@@ -20,95 +20,95 @@
 
 enum Data
 {
-    MAX_ENCOUNTER          = 4,
+    MAX_ENCOUNTER                  = 4,
 
-    DATA_FALRIC_EVENT      = 1,
-    DATA_MARWYN_EVENT      = 2,
-    DATA_FROSWORN_EVENT    = 3,
-    DATA_LICHKING_EVENT    = 4,
+    DATA_FALRIC_EVENT              = 1,
+    DATA_MARWYN_EVENT              = 2,
+    DATA_FROSWORN_EVENT            = 3,
+    DATA_LICHKING_EVENT            = 4,
 
-    DATA_INTRO_EVENT       = 5,
-    DATA_TEAM_IN_INSTANCE  = 6,
-    DATA_WAVE_COUNT        = 7,
-    DATA_WAVE_STATE       = 8,
+    DATA_INTRO_EVENT               = 5,
+    DATA_TEAM_IN_INSTANCE          = 6,
+    DATA_WAVE_COUNT                = 7,
+    DATA_WAVE_STATE                = 8,
 
-    DATA_SUMMONS           = 9,
-    DATA_ICE_WALL_1        = 10,
-    DATA_ICE_WALL_2        = 11,
-    DATA_ICE_WALL_3        = 12,
-    DATA_ICE_WALL_4        = 13,
+    DATA_SUMMONS                   = 9,
+    DATA_ICE_WALL_1                = 10,
+    DATA_ICE_WALL_2                = 11,
+    DATA_ICE_WALL_3                = 12,
+    DATA_ICE_WALL_4                = 13,
 
-    DATA_PHASE             = 14,
+    DATA_PHASE                     = 14,
 };
 
 enum Data64
 {
-    DATA_FALRIC            = 101,
-    DATA_MARWYN            = 102,
-    DATA_LICHKING          = 103,
-    DATA_ESCAPE_LIDER      = 104,
-    DATA_FROSTMOURNE       = 105,
-    DATA_FRONT_DOOR        = 106,
-    DATA_FROSTWORN_DOOR    = 107,
-    DATA_ARTHAS_DOOR       = 108,
+    DATA_FALRIC                    = 101,
+    DATA_MARWYN                    = 102,
+    DATA_LICHKING                  = 103,
+    DATA_ESCAPE_LIDER              = 104,
+    DATA_FROSTMOURNE               = 105,
+    DATA_FRONT_DOOR                = 106,
+    DATA_FROSTWORN_DOOR            = 107,
+    DATA_ARTHAS_DOOR               = 108,
 };
 
 enum Creatures
 {
-    NPC_FALRIC                                    = 38112,
-    NPC_MARWYN                                    = 38113,
-    NPC_FROSTWORN_GENERAL                         = 36723,
-    NPC_REFLECTION                                = 37107,
-    NPC_LICH_KING_EVENT                           = 36954,
-    NPC_QUELDELAR                                 = 37158,
-    BOSS_LICH_KING                                = 37226,
+    NPC_FALRIC                     = 38112,
+    NPC_MARWYN                     = 38113,
+    NPC_FROSTWORN_GENERAL          = 36723,
+    NPC_REFLECTION                 = 37107,
+    NPC_LICH_KING_EVENT            = 36954,
+    NPC_QUELDELAR                  = 37158,
+    BOSS_LICH_KING                 = 37226,
 
-    NPC_UTHER                                     = 37225,
-    NPC_JAINA_PART1                               = 37221,
-    NPC_JAINA_OUTRO                               = 36955,
-    NPC_SYLVANAS_PART1                            = 37223,
-    NPC_SYLVANA_OUTRO                             = 37554,
-    NPC_BARTLETT                                  = 37182,
-    NPC_KORM                                      = 37833,
+    NPC_UTHER                      = 37225,
+    NPC_JAINA_PART1                = 37221,
+    NPC_JAINA_OUTRO                = 36955,
+    NPC_SYLVANAS_PART1             = 37223,
+    NPC_SYLVANA_OUTRO              = 37554,
+    NPC_BARTLETT                   = 37182,
+    NPC_KORM                       = 37833,
 
-    NPC_WAVE_MERCENARY                            = 38177,
-    NPC_WAVE_FOOTMAN                              = 38173,
-    NPC_WAVE_RIFLEMAN                             = 38176,
-    NPC_WAVE_PRIEST                               = 38175,
-    NPC_WAVE_MAGE                                 = 38172,
-    NPC_WAVE_LOOT_DUMMY                           = 37906,
+    NPC_WAVE_MERCENARY             = 38177,
+    NPC_WAVE_FOOTMAN               = 38173,
+    NPC_WAVE_RIFLEMAN              = 38176,
+    NPC_WAVE_PRIEST                = 38175,
+    NPC_WAVE_MAGE                  = 38172,
+    NPC_WAVE_LOOT_DUMMY            = 37906,
 
-    NPC_ICE_WALL                                  = 37014,
-    NPC_RAGING_GNOUL                              = 36940,
-    NPC_RISEN_WITCH_DOCTOR                        = 36941,
-    NPC_ABON                                      = 37069,
+    NPC_ICE_WALL                   = 37014,
+    NPC_RAGING_GNOUL               = 36940,
+    NPC_RISEN_WITCH_DOCTOR         = 36941,
+    NPC_ABON                       = 37069,
 
 };
 
 enum GameObjects
 {
-    GO_FROSTMOURNE               = 202302,
-    GO_FROSTMOURNE_ALTAR         = 202236,
-    GO_FRONT_DOOR                = 201976,
-    GO_FROSTWORN_DOOR            = 197341,
-    GO_ARTHAS_DOOR               = 197342,
-    GO_RUN_DOOR                  = 197343,
+    GO_FROSTMOURNE                 = 202302,
+    GO_FROSTMOURNE_ALTAR           = 202236,
+    GO_FRONT_DOOR                  = 201976,
+    GO_FROSTWORN_DOOR              = 197341,
+    GO_ARTHAS_DOOR                 = 197342,
+    GO_RUN_DOOR                    = 197343,
 
-    GO_ICE_WALL_1                = 201385,
-    GO_ICE_WALL_2                = 201885,
-    GO_ICE_WALL_3                = 202396,
-    GO_ICE_WALL_4                = 500001,
-    GO_CAVE                      = 201596,
+    GO_ICE_WALL_1                  = 201385,
+    GO_ICE_WALL_2                  = 201885,
+    GO_ICE_WALL_3                  = 202396,
+    GO_ICE_WALL_4                  = 500001,
+    GO_CAVE                        = 201596,
 
-    GO_STAIRS_SKYBREAKER         = 201709,
-    GO_SKYBREAKER                = 500002,
-    GO_STAIRS_ORGRIM_HAMMER      = 202211,
-    GO_ORGRIM_HAMMER             = 500003,
-    GO_PORTAL                    = 202079,
-    GO_CAPTAIN_CHEST_1           = 202212, //3145
-    GO_CAPTAIN_CHEST_2           = 201710, //30357
-    GO_CAPTAIN_CHEST_3           = 202337, //3246
-    GO_CAPTAIN_CHEST_4           = 202336, //3333
+    GO_STAIRS_SKYBREAKER           = 201709,
+    GO_SKYBREAKER                  = 500002,
+    GO_STAIRS_ORGRIM_HAMMER        = 202211,
+    GO_ORGRIM_HAMMER               = 500003,
+    GO_PORTAL                      = 202079,
+    GO_CAPTAIN_CHEST_1             = 202212, //3145
+    GO_CAPTAIN_CHEST_2             = 201710, //30357
+    GO_CAPTAIN_CHEST_3             = 202337, //3246
+    GO_CAPTAIN_CHEST_4             = 202336, //3333
 };
 
 enum Achievements
@@ -140,8 +140,8 @@ const Position OutroSpawns[2] =
 
 enum HorWorldStates
 {
-    WORLD_STATE_HOR                               = 4884,
-    WORLD_STATE_HOR_WAVE_COUNT                    = 4882,
+    WORLD_STATE_HOR                = 4884,
+    WORLD_STATE_HOR_WAVE_COUNT     = 4882,
 };
 
 // Common actions from Instance Script to Boss Script
