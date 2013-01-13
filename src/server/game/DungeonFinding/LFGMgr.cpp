7a3330ac0c8e79bee52319ad2b434d055870f416
Spp <spp@jorge.gr>
2013-01-13 16:15:28 +0100

    Core/Dungeon finder: Fix teleport out after dungeon is finished Closes 8914

--- src/server/game/DungeonFinding/LFGMgr.cpp
+++ src/server/game/DungeonFinding/LFGMgr.cpp
@@ -1563,7 +1563,6 @@ void LFGMgr::FinishDungeon(uint64 gguid, const uint32 dungeonId)
         LfgPlayerRewardData data = LfgPlayerRewardData(dungeon->Entry(), GetDungeon(gguid, false), done, quest);
         player->GetSession()->SendLfgPlayerReward(data);
     }
-    SetDungeon(gguid, 0);
 }
 
 // --------------------------------------------------------------------------//
@@ -1629,6 +1628,18 @@ LfgState LFGMgr::GetState(uint64 guid)
         state = PlayersStore[guid].GetState();
 
     sLog->outTrace(LOG_FILTER_LFG, "LFGMgr::GetState: [" UI64FMTD "] = %u", guid, state);
+    return state;
+}
+
+LfgState LFGMgr::GetOldState(uint64 guid)
+{
+    LfgState state;
+    if (IS_GROUP_GUID(guid))
+        state = GroupsStore[guid].GetOldState();
+    else
+        state = PlayersStore[guid].GetOldState();
+
+    sLog->outTrace(LOG_FILTER_LFG, "LFGMgr::GetOldState: [" UI64FMTD "] = %u", guid, state);
     return state;
 }
 
