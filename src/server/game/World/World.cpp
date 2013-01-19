a45f7fd878ba9060c3b4a38f353ac0de4b22c388
MrSmite <mrsmite@att.net>
2013-01-19 02:14:20 -0500

    Adds CONF option to show quest levels in dialogs.
    Example: [13] Westfall Stew
    
    Default = 0 (off). Make sure to set UI.ShowQuestLevelsInDialogs to 1 to turn it on
    
    NOTES:
    
    * DO NOT supply the quest level when using commands such as .lookup quest, it
    is not actually part of the title (even though it will be printed in the result)
    
    * Also adds the level in the quest tracker and quest log
    
    * If locale is used, the level is added after the proper title is looked up
    
    * LUA Mods that offer this feature do not work properly with TC, most will not put the level in all dialogs. Users who have such mods should disable the mod's feature or they may see the quest level listed twice.
    
    Example: [13][13] Westfall Stew

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -1224,6 +1224,7 @@ void World::LoadConfigSettings(bool reload)
     // misc
     m_bool_configs[CONFIG_PDUMP_NO_PATHS] = ConfigMgr::GetBoolDefault("PlayerDump.DisallowPaths", true);
     m_bool_configs[CONFIG_PDUMP_NO_OVERWRITE] = ConfigMgr::GetBoolDefault("PlayerDump.DisallowOverwrite", true);
+    m_bool_configs[CONFIG_UI_QUESTLEVELS_IN_DIALOGS] = ConfigMgr::GetBoolDefault("UI.ShowQuestLevelsInDialogs", false);
 
     // call ScriptMgr if we're reloading the configuration
     m_bool_configs[CONFIG_WINTERGRASP_ENABLE] = ConfigMgr::GetBoolDefault("Wintergrasp.Enable", false);
