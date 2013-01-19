a5eb245fcf49aef7c280164dfca99c445b2c424e
MrSmite <mrsmite@att.net>
2013-01-19 02:14:20 -0500

    Adds CONF option to show quest levels in dialogs.
    Example: [13] Westfall Stew
    
    Default = 0 (off). Make sure to set UI.ShowQuestLevelsInDialogs to 1 to turn it on
    
    NOTES:
    
    * DO NOT supply the quest level when using commands such as .lookup quest, it
    is not actually part of the title (even though it will be printed in the result)
    
    * Also adds the level in the quest tracker and quest log
    
    * If locale is used, the level is added after the proper title is looked up
    
    * LUA Mods that offer this feature do not work properly with TC, most will not put the level in all dialogs. Users who have such mods should disable the mod's feature or they may see the quest level listed twice.
    
    Example: [13][13] Westfall Stew

--- src/server/game/World/World.h
+++ src/server/game/World/World.h
@@ -165,6 +165,7 @@ enum WorldBoolConfigs
     CONFIG_WARDEN_ENABLED,
     CONFIG_ENABLE_MMAPS,
     CONFIG_WINTERGRASP_ENABLE,
+    CONFIG_UI_QUESTLEVELS_IN_DIALOGS,     // Should we add quest levels to the title in the NPC dialogs?
     BOOL_CONFIG_VALUE_COUNT
 };
 
