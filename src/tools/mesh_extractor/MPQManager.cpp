f144df04da5efa3de1b837f2a21cf633e1713769
Shauren <shauren.trinity@gmail.com>
2013-01-22 19:07:40 +0100

    Tools: Build fix

--- src/tools/mesh_extractor/MPQManager.cpp
+++ src/tools/mesh_extractor/MPQManager.cpp
@@ -14,7 +14,7 @@ char* MPQManager::Files[] = {
     "patch-3.MPQ"
 };
 
-char* MPQManager::Languages[] = { "enGB", "enUS", "deDE", "esES", "frFR", "koKR", "zhCN", "zhTW", "enCN", "enTW", "esMX", "ruRU" };
+char const* MPQManager::Languages[] = { "enGB", "enUS", "deDE", "esES", "frFR", "koKR", "zhCN", "zhTW", "enCN", "enTW", "esMX", "ruRU" };
 
 void MPQManager::Initialize()
 {
--- src/tools/mesh_extractor/Utils.h
+++ src/tools/mesh_extractor/Utils.h
@@ -15,7 +15,7 @@
 struct WorldModelDefinition;
 class DoodadInstance;
 
-#define ASSERT(assertion) { if (!(assertion)) { ACE_Stack_Trace st; fprintf(stderr, "\n%s:%i in %s ASSERTION FAILED:\n  %s\n", __FILE__, __LINE__, __FUNCTION__, #assertion, st.c_str()); *((volatile int*)NULL) = 0; } }
+#define ASSERT(assertion) { if (!(assertion)) { ACE_Stack_Trace st; fprintf(stderr, "\n%s:%i in %s ASSERTION FAILED:\n  %s\n%s\n", __FILE__, __LINE__, __FUNCTION__, #assertion, st.c_str()); *((volatile int*)NULL) = 0; } }
 
 struct Vector3
 {
