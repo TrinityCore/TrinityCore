58193063357f42b83aed0992a4f999bd6de12e23
ZxBiohazardZx <zxbiohazardzx@gmail.com>
2013-01-27 17:41:26 +0100

    Fix/Commit: push final file, not the workingcopy

--- sql/updates/world/2013_01_27_00_world_creature.sql
+++ sql/updates/world/2013_01_27_00_world_creature.sql
@@ -1,7 +1,8 @@
-DELETE FROM `smart_scripts` WHERE `entryorguid`=3094 AND sourcetype=0
+DELETE FROM `smart_scripts` WHERE `entryorguid`=3094 AND sourcetype=0;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
 (3094,0,0,0,11,0,100,0,0,0,0,0,11,4986,2,0,0,0,0,1,0,0,0,0,0,0,0,'Unseen - On Respawn - Cast Unseen'); 
 -- Spawns for Unseen from Sniffs
+SET @GUID:=100173;
 DELETE FROM `creature` WHERE `id`=3094;
 INSERT INTO `creature` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`MovementType`,`npcflag`,`unit_flags`,`dynamicflags`) VALUES
 (@GUID   ,3094,0,1,1,0,0,-10694.8,345.662,44.4895,2.09338,300,0,0,2215,0,0,0,0,0),
