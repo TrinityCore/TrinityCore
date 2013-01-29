5a2a0f3280b3a2905ec8c71a6200f46b49f4ab7f
click <click@gonnamakeyou.com>
2013-01-29 23:18:25 +0100

    Buildsystem/CMake: Revoke commit 7b49da48f9c3c21406fbe350de170757ea6e285f - it creates unwanted results when used with PCh on *nix platforms.

--- cmake/compiler/gcc/settings.cmake
+++ cmake/compiler/gcc/settings.cmake
@@ -1,5 +1,5 @@
 # Set build-directive (used in core to tell which buildtype we used)
-add_definitions(-D_BUILD_DIRECTIVE="${CMAKE_BUILD_TYPE}")
+add_definitions(-D_BUILD_DIRECTIVE='"${CMAKE_BUILD_TYPE}"')
 
 if(PLATFORM EQUAL 32)
   # Required on 32-bit systems to enable SSE2 (standard on x64)
