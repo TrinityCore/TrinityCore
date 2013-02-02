e65f738733698c193e182f20b51253f67d8c80f2
Vincent_Michael <Vincent_Michael@gmx.de>
2013-02-02 19:06:47 +0100

    Core/Misc: Small code optimisations

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -15299,26 +15299,26 @@ void Unit::SetControlled(bool apply, UnitState state)
             case UNIT_STATE_STUNNED:
                 if (HasAuraType(SPELL_AURA_MOD_STUN))
                     return;
-                else
-                    SetStunned(false);
+
+                SetStunned(false);
                 break;
             case UNIT_STATE_ROOT:
                 if (HasAuraType(SPELL_AURA_MOD_ROOT) || GetVehicle())
                     return;
-                else
-                    SetRooted(false);
+
+                SetRooted(false);
                 break;
             case UNIT_STATE_CONFUSED:
                 if (HasAuraType(SPELL_AURA_MOD_CONFUSE))
                     return;
-                else
-                    SetConfused(false);
+
+                SetConfused(false);
                 break;
             case UNIT_STATE_FLEEING:
                 if (HasAuraType(SPELL_AURA_MOD_FEAR))
                     return;
-                else
-                    SetFeared(false);
+
+                SetFeared(false);
                 break;
             default:
                 return;
