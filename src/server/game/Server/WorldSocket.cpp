623904e661e289b009cdde2d1c308d9de71b3d2f
Shauren <shauren.trinity@gmail.com>
2013-02-08 01:03:56 +0100

    Core/Auth: Reverted part of fb43a92cc2aaffab42efebf025b6a12c01af8fde - session key is still needed after logging in to be able to switch realms

--- src/server/game/Server/WorldSocket.cpp
+++ src/server/game/Server/WorldSocket.cpp
@@ -933,12 +933,6 @@ int WorldSocket::HandleAuthSession(WorldPacket& recvPacket)
     // NOTE ATM the socket is single-threaded, have this in mind ...
     ACE_NEW_RETURN(m_Session, WorldSession(id, this, AccountTypes(security), expansion, mutetime, locale, recruiter, isRecruiter), -1);
 
-    stmt = LoginDatabase.GetPreparedStatement(LOGIN_UPD_CLEAR_SESSIONKEY);
-
-    stmt->setUInt32(0, id);
-
-    LoginDatabase.Execute(stmt);
-
     m_Crypt.Init(&k);
 
     m_Session->LoadGlobalAccountData();
