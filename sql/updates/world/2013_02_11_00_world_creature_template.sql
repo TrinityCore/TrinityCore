12359b993d5bff183e59e1355f5d291ac0ca45a6
Nay <dnpd.dd@gmail.com>
2013-02-11 03:10:02 +0000

    DB/NPCs: Set trigger flags_extra to a bunch of quest credit markers
    God damn Aokromes, always making me do work and stuff...

---
+++ sql/updates/world/2013_02_11_00_world_creature_template.sql
@@ -0,0 +1,26 @@
+UPDATE `creature_template` SET `flags_extra`=`flags_extra`|128 WHERE `entry` IN (
+  17413, -- Sedai Quest Credit Marker
+  21121, -- Bonechewer Quest credit marker
+  24887, -- Fengir Quest Credit
+  24888, -- Isuldof Quest Credit
+  24889, -- Rodin Quest Credit
+  24890, -- Windan Quest Credit
+  27471, -- Forgotten Rifleman Quest Credit
+  27472, -- Forgotten Peasant Quest Credit
+  27473, -- Forgotten Knight Quest Credit
+  27474, -- Captain Luc D'Merud Quest Credit
+  27879, -- Frostmourne Cavern Quest Credit
+  38503, -- Blood Infusion Quest Credit Bunny
+  38546, -- Frost Infusion Quest Credit
+  38547, -- Sindragosa Quest Credit
+  39355, -- [DND] Salute Quest Credit Bunny
+  39356, -- [DND] Roar Quest Credit Bunny	
+  39361, -- [DND] Dance Quest Credit Bunny
+  39362, -- [DND] Cheer Quest Credit Bunny
+  39683, -- [DND] Quest Credit Bunny - Eject
+  39691, -- [DND] Quest Credit Bunny - Move 1
+  39692, -- [DND] Quest Credit Bunny - Move 2
+  39695, -- [DND] Quest Credit Bunny - Move 3
+  39703, -- [DND] Quest Credit Bunny - Attack
+  40428  -- [DND] Quest Credit Bunny - ET Battle
+);
