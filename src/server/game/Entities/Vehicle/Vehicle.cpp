5290b5be6381dd0299d96ec1e337a76f33116669
Nay <dnpd.dd@gmail.com>
2013-02-16 17:04:44 +0000

    Say NO to whitespace

--- src/server/game/Entities/Vehicle/Vehicle.cpp
+++ src/server/game/Entities/Vehicle/Vehicle.cpp
@@ -338,7 +338,7 @@ bool Vehicle::AddPassenger(Unit* unit, int8 seatId)
     // exits the vehicle will dismiss. That's why the actual adding the passenger to the vehicle is scheduled
     // asynchronously, so it can be cancelled easily in case the vehicle is uninstalled meanwhile.
     SeatMap::iterator seat;
-    sLog->outDebug(LOG_FILTER_VEHICLES, "Unit %s scheduling enter vehicle entry %u id %u dbguid %u seat %d", 
+    sLog->outDebug(LOG_FILTER_VEHICLES, "Unit %s scheduling enter vehicle entry %u id %u dbguid %u seat %d",
         unit->GetName().c_str(), _me->GetEntry(), _vehicleInfo->m_ID, _me->GetGUIDLow(), seatId);
     VehicleJoinEvent* e = new VehicleJoinEvent(this, unit);
     unit->m_Events.AddEvent(e, unit->m_Events.CalculateTime(0));
@@ -525,7 +525,7 @@ void Vehicle::CalculatePassengerOffset(float& x, float& y, float& z, float& o)
     y = (iny - inx * tan(GetBase()->GetOrientation())) / (cos(GetBase()->GetOrientation()) + std::sin(GetBase()->GetOrientation()) * tan(GetBase()->GetOrientation()));
     x = (inx + iny * tan(GetBase()->GetOrientation())) / (cos(GetBase()->GetOrientation()) + std::sin(GetBase()->GetOrientation()) * tan(GetBase()->GetOrientation()));
 }
-    
+
 void Vehicle::CancelJoinEvent(VehicleJoinEvent* e)
 {
     e->to_Abort = true;
@@ -593,9 +593,9 @@ bool VehicleJoinEvent::Execute(uint64, uint32)
     if (Target->GetBase()->GetTypeId() == TYPEID_UNIT)
         sScriptMgr->OnAddPassenger(Target, Passenger, Seat->first);
 
-    return true; 
+    return true;
 }
-    
+
 void VehicleJoinEvent::Abort(uint64)
 {
     Passenger->m_vehicle = NULL;
