6d1885e51d3115aa515f728b052f7e12eac802c4
Saqirmdev <Saqirm@gmail.com>
2013-03-03 21:57:14 +0100

    BG/CF - Update Battleground Crossfraction Traps activate

--- src/server/scripts/Spells/spell_hunter.cpp
+++ src/server/scripts/Spells/spell_hunter.cpp
@@ -364,12 +364,17 @@ class spell_hun_masters_call : public SpellScriptLoader
             {
                 if (Unit* ally = GetHitUnit())
                     if (Player* caster = GetCaster()->ToPlayer())
+					{
+
+					  player->CastSpell(player, SPELL_HUNTER_MASTERS_CALL_TRIGGERED, true);
+
                         if (Pet* target = caster->GetPet())
                         {
                             TriggerCastFlags castMask = TriggerCastFlags(TRIGGERED_FULL_MASK & ~TRIGGERED_IGNORE_CASTER_AURASTATE);
                             target->CastSpell(ally, GetEffectValue(), castMask);
                             target->CastSpell(ally, GetSpellInfo()->Effects[EFFECT_0].CalcValue(), castMask);
                         }
+					}
             }
 
             void HandleScriptEffect(SpellEffIndex /*effIndex*/)
