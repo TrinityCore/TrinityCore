906bb591af308681d740b91fd39696a006f73c48
ZxBiohazardZx <zxbiohazardzx@gmail.com>
2013-03-04 13:00:03 +0100

    DB/Quest: fix some questrequirements in BE start zone by Praxious, Closes #9156
    Commit: fix naming on previous file, fix typo in 2013_03_04_00

--- sql/updates/world/2013_03_04_00_world_gameobject.sql
+++ sql/updates/world/2013_03_04_00_world_gameobject.sql
@@ -2,6 +2,6 @@ DELETE FROM `gameobject_template` WHERE `entry`=180055;
 INSERT INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `IconName`, `castBarCaption`, `unk1`, `faction`, `flags`, `size`, `questItem1`, `questItem2`, `questItem3`, `questItem4`, `questItem5`, `questItem6`, `data0`, `data1`, `data2`, `data3`, `data4`, `data5`, `data6`, `data7`, `data8`, `data9`, `data10`, `data11`, `data12`, `data13`, `data14`, `data15`, `data16`, `data17`, `data18`, `data19`, `data20`, `data21`, `data22`, `data23`, `AIName`, `ScriptName`, `WDBVerified`) VALUES
 (180055,2,318,'Mysterious Wailing Caverns Chest','',,'',0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'',0);
 
-DELETE FROM `gameobejct` WHERE `id`=180055;
+DELETE FROM `gameobject` WHERE `id`=180055;
 INSERT INTO `gameobject` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`position_x`,`position_y`,`position_z`,`orientation`,`rotation0`,`rotation1`,`rotation2`,`rotation3`,`spawntimesecs`,`animprogress`,`state`)
 (4598,180055,43,1,1,-97.6,173.8,-79,2.08,0,0,0,0,43200,100,1);
---
+++ sql/updates/world/2013_03_04_02_world_quest_template.sql
@@ -0,0 +1,2 @@
+UPDATE `quest_template` SET `PrevQuestId`=0 WHERE `Id`=8345;
+UPDATE `quest_template` SET `NextQuestId`=0 WHERE `Id`=8330;
