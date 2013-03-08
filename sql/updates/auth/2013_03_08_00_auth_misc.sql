524a10d6286473afc3216b184738c81379479b74
Spp <spp@jorge.gr>
2013-03-08 14:05:51 +0100

    Typo fix... ¬¬

--- sql/updates/auth/2013_03_08_00_auth_misc.sql
+++ sql/updates/auth/2013_03_08_00_auth_misc.sql
@@ -1,8 +1,8 @@
 -- Delete 'Two side mail interaction', 'Allow say chat between factions' and 'Allow channel chat between factions' from Player group
 DELETE FROM `rbac_group_roles` WHERE `groupId` = 1 AND `roleId` IN (19, 28, 30);
 
--- Delete 'Join channels withou announce' from Moderators group
-DELETE FROM `rbac_group_roles` WHERE `groupId` = 2 AND `roleId` IN (31);
+-- Delete 'Join channels without announce' from Moderators+ group
+DELETE FROM `rbac_group_roles` WHERE `roleId` = 31;
 
 -- Add 'Allow two side charater accounts'
 DELETE FROM `rbac_group_roles` WHERE `roleId` = 34;
