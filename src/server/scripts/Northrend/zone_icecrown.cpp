17061cb9e52d409f70dbe063b73cae9cd02c7db3
Trista <aconstantgoal@abv.bg>
2013-03-08 04:45:15 +0200

    Scripts/Icecrown: Add script* support for Borrowed Technology/Volatility quests
    * There are heavy core problems that make it not fully functional for now.
    1) Enter vehicle of 31578 is being overriding the vehicle aura of the accessory so it kicks it out. (both get applied to 1, but instead decoy should be to 0).
    2) Even if mentioned above get fixed the accessory is supposed to implicitly hit another passenger with explosion
    and no seat flags for it even unknown.
    3) Armored Decoy doesn't keep emote from freeze mechanic when enter vehicle connected with a not supported flag.
    4) Vehicle speed seems too fast (there is some general vehicle not correct speed problem with spline being involved).
    
    Connected with: #5757

--- src/server/scripts/Northrend/zone_icecrown.cpp
+++ src/server/scripts/Northrend/zone_icecrown.cpp
@@ -32,6 +32,7 @@ EndContentData */
 #include "ScriptedGossip.h"
 #include "SpellAuras.h"
 #include "Player.h"
+#include "TemporarySummon.h"
 
 /*######
 ## npc_arete
@@ -874,6 +875,92 @@ public:
     }
 };
 
+/*######
+## Borrowed Technology - Id: 13291, The Solution Solution (daily) - Id: 13292, Volatility - Id: 13239, Volatiliy - Id: 13261 (daily)
+######*/
+
+enum BorrowedTechnologyAndVolatility
+{
+    // Spells
+    SPELL_GRAB             = 59318,
+    SPELL_PING_BUNNY       = 59375,
+    SPELL_IMMOLATION       = 54690,
+    SPELL_EXPLOSION        = 59335,
+    SPELL_RIDE             = 56687,
+
+    // Points
+    POINT_GRAB_DECOY       = 1,
+    POINT_FLY_AWAY         = 2,
+
+    // Events
+    EVENT_FLY_AWAY         = 1
+};
+
+class npc_frostbrood_skytalon : public CreatureScript
+{
+    public:
+        npc_frostbrood_skytalon() : CreatureScript("npc_frostbrood_skytalon") { }
+
+        struct npc_frostbrood_skytalonAI : public VehicleAI
+        {
+            npc_frostbrood_skytalonAI(Creature* creature) : VehicleAI(creature) { }
+
+            EventMap events;
+
+            void IsSummonedBy(Unit* summoner)
+            {
+                me->GetMotionMaster()->MovePoint(POINT_GRAB_DECOY, summoner->GetPositionX(), summoner->GetPositionY(), summoner->GetPositionZ());
+            }
+
+            void MovementInform(uint32 type, uint32 id)
+            {
+                if (type != POINT_MOTION_TYPE)
+                    return;
+
+                if (id == POINT_GRAB_DECOY)
+                    if (TempSummon* summon = me->ToTempSummon())
+                        if (Unit* summoner = summon->GetSummoner())
+                            DoCast(summoner, SPELL_GRAB); 
+            }
+
+            void UpdateAI(uint32 diff)
+            {
+                VehicleAI::UpdateAI(diff);
+                events.Update(diff);
+
+                while (uint32 eventId = events.ExecuteEvent())
+                {
+                    if (eventId == EVENT_FLY_AWAY)
+                    {
+                        Position randomPosOnRadius;
+                        randomPosOnRadius.m_positionZ = (me->GetPositionZ() + 40.0f);
+                        me->GetNearPoint2D(randomPosOnRadius.m_positionX, randomPosOnRadius.m_positionY, 40.0f, me->GetAngle(me));
+                        me->GetMotionMaster()->MovePoint(POINT_FLY_AWAY, randomPosOnRadius);
+                    }
+                }
+            }
+
+            void SpellHit(Unit* /*caster*/, SpellInfo const* spell)
+            {
+                switch (spell->Id)
+                {
+                    case SPELL_EXPLOSION:
+                        DoCast(me, SPELL_IMMOLATION);
+                        break;
+                    case SPELL_RIDE:
+                        DoCastAOE(SPELL_PING_BUNNY);
+                        events.ScheduleEvent(EVENT_FLY_AWAY, 100);
+                        break;
+                }
+            }
+        };
+
+        CreatureAI* GetAI(Creature* creature) const
+        {
+            return new npc_frostbrood_skytalonAI(creature);
+        }
+};
+
 void AddSC_icecrown()
 {
     new npc_arete;
@@ -883,4 +970,5 @@ void AddSC_icecrown()
     new npc_vereth_the_cunning;
     new npc_tournament_training_dummy;
     new npc_blessed_banner();
+    new npc_frostbrood_skytalon();
 }
