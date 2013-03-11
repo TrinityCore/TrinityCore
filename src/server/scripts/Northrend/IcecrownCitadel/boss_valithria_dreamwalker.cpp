7ee8019ae7ad8fd3f73f77a89d435497db9e7182
Gary <fishermanpl@gmail.com>
2013-03-11 01:56:53 +0100

    Fix valithria dreamwalker by balacas.

--- src/server/scripts/Northrend/IcecrownCitadel/boss_valithria_dreamwalker.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_valithria_dreamwalker.cpp
@@ -962,7 +962,8 @@ class npc_gluttonous_abomination : public CreatureScript
 
             void JustDied(Unit* /*killer*/)
             {
-                DoCast(me, SPELL_ROT_WORM_SPAWNER, true);
+                if (!killer->GetEntry())
+                    DoCast(me, SPELL_ROT_WORM_SPAWNER, true);
             }
 
             void UpdateAI(uint32 diff)
