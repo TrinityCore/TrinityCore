4fe545203c91579a0c224b75130f9d838ae80c67
Gary <fishermanpl@gmail.com>
2013-03-11 01:59:08 +0100

    Fix compile...

--- src/server/scripts/Northrend/IcecrownCitadel/boss_valithria_dreamwalker.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_valithria_dreamwalker.cpp
@@ -960,7 +960,7 @@ class npc_gluttonous_abomination : public CreatureScript
                 _events.ScheduleEvent(EVENT_GUT_SPRAY, urand(10000, 13000));
             }
 
-            void JustDied(Unit* /*killer*/)
+            void JustDied(Unit* killer)
             {
                 if (!killer->GetEntry())
                     DoCast(me, SPELL_ROT_WORM_SPAWNER, true);
