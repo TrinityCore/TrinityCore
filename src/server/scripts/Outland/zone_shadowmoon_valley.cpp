9ae1ef2cf857828ad6c277ce3364ab38998b0bab
Gacko <gacko28@gmx.de>
2013-03-12 21:52:34 +0100

    Script/Spell: Unlocking Zuluhed's Chains
    Closes #996

--- src/server/scripts/Outland/zone_shadowmoon_valley.cpp
+++ src/server/scripts/Outland/zone_shadowmoon_valley.cpp
@@ -1891,16 +1891,11 @@ class spell_unlocking_zuluheds_chains : public SpellScriptLoader
         {
             PrepareSpellScript(spell_unlocking_zuluheds_chains_SpellScript);
 
-            bool Load()
-            {
-                return GetCaster()->GetTypeId() == TYPEID_PLAYER;
-            }
-
             void HandleAfterHit()
             {
-                Player* caster = GetCaster()->ToPlayer();
-                if (caster->GetQuestStatus(QUEST_ZULUHED) == QUEST_STATUS_INCOMPLETE)
-                    caster->KilledMonsterCredit(NPC_KARYNAKU, 0);
+                if (GetCaster()->GetTypeId() == TYPEID_PLAYER)
+                    if (Creature* karynaku = GetCaster()->FindNearestCreature(NPC_KARYNAKU, 15.0f))
+                        GetCaster()->ToPlayer()->CastedCreatureOrGO(NPC_KARYNAKU, karynaku->GetGUID(), GetSpellInfo()->Id);
             }
 
             void Register()
