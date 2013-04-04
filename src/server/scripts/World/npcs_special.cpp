d42c86b8618ff3b3e72b2282b9cbcdd546a17ca6
Gary <fishermanpl@gmail.com>
2013-04-04 12:27:40 +0200

    Revert 'Fix - Ebon Gargoyle Target'.

--- src/server/scripts/World/npcs_special.cpp
+++ src/server/scripts/World/npcs_special.cpp
@@ -1920,8 +1920,6 @@ public:
         npc_ebon_gargoyleAI(Creature* creature) : CasterAI(creature) {}
 
         uint32 despawnTimer;
-        Unit* owner;
-        Unit* target;
 
         void InitializeAI()
         {
@@ -1929,10 +1927,19 @@ public:
             uint64 ownerGuid = me->GetOwnerGUID();
             if (!ownerGuid)
                 return;
-            owner = me->GetOwner();
             // Not needed to be despawned now
             despawnTimer = 0;
-            target = NULL;
+            // Find victim of Summon Gargoyle spell
+            std::list<Unit*> targets;
+            Trinity::AnyUnfriendlyUnitInObjectRangeCheck u_check(me, me, 30);
+            Trinity::UnitListSearcher<Trinity::AnyUnfriendlyUnitInObjectRangeCheck> searcher(me, targets, u_check);
+            me->VisitNearbyObject(30, searcher);
+            for (std::list<Unit*>::const_iterator iter = targets.begin(); iter != targets.end(); ++iter)
+                if ((*iter)->GetAura(49206, ownerGuid))
+                {
+                    me->Attack((*iter), false);
+                    break;
+                }
         }
 
         void JustDied(Unit* /*killer*/)
@@ -1984,30 +1991,6 @@ public:
                     me->DespawnOrUnsummon();
                 return;
             }
-
-            if (!target)
-            {
-                // Find victim of Summon Gargoyle spell
-                std::list<Unit*> targets;
-                Trinity::AnyUnfriendlyUnitInObjectRangeCheck u_check(me, me, 30);
-                Trinity::UnitListSearcher<Trinity::AnyUnfriendlyUnitInObjectRangeCheck> searcher(me, targets, u_check);
-                me->VisitNearbyObject(30, searcher);
-                for (std::list<Unit*>::const_iterator iter = targets.begin(); iter != targets.end(); ++iter)
-                {
-                    if ((*iter)->HasAura(49206, me->GetOwnerGUID()))
-                    {
-                        target = (*iter);
-                        AttackStart(target);
-                        break;
-                    }
-                }
-            }
-            else
-                if (target && target->isAlive())
-                {
-                    AttackStart(target);
-                }
-
             CasterAI::UpdateAI(diff);
         }
     };
