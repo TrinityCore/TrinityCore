0a863e8404b93501ff9c332db014e32c9e8ca0fc
Gary <fishermanpl@gmail.com>
2013-04-09 15:05:41 +0200

    [173] Fix - Bladestorm - Knockback Effect. (Thx Demonid)

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -16508,6 +16508,10 @@ void Unit::KnockbackFrom(float x, float y, float speedXY, float speedZ)
     }
     else
     {
+        // Bladestorm
+        if (player->HasAura(46924))
+            return;
+
         float vcos, vsin;
         GetSinCos(x, y, vsin, vcos);
 
