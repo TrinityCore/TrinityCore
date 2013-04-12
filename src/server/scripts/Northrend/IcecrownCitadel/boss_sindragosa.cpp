6dbc3c8c3d75638d292a0ee263a9de7d6568c0f7
Gacko <gacko28@gmx.de>
2013-04-12 17:34:43 +0200

    Script/Spell: Apply Frost Imbued Blade only in 25 man mode
    Note: Quest credit spells are already casted in 25 man mode only.

--- src/server/scripts/Northrend/IcecrownCitadel/boss_sindragosa.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_sindragosa.cpp
@@ -1131,7 +1131,8 @@ class spell_sindragosa_frost_breath : public SpellScriptLoader
                 if (!target)
                     return;
 
-                if (target->GetQuestStatus(QUEST_FROST_INFUSION) != QUEST_STATUS_INCOMPLETE)
+                // Check difficulty and quest status
+                if (!(target->GetRaidDifficulty() & RAID_DIFFICULTY_MASK_25MAN) || target->GetQuestStatus(QUEST_FROST_INFUSION) != QUEST_STATUS_INCOMPLETE)
                     return;
 
                 // Check if player has Shadow's Edge equipped and not ready for infusion
