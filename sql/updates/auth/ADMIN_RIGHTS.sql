17b68cd18dd94285997f74cfc3a3936f4e15b9f4
Northstrider <Dreadkiller@gmx.de>
2013-05-02 21:37:29 +0200

    DB: added sql for instant admin rights for that shit rbac system

---
+++ sql/updates/auth/ADMIN_RIGHTS.sql
@@ -0,0 +1,10 @@
+﻿
+-- adding full Admin rights to account id 1
+SET @ACCOUNTID :=1;
+DELETE FROM account_access WHERE id = @ACCOUNTID;
+INSERT INTO account_access VALUES (@ACCOUNTID, 4, -1);
+DELETE FROM rbac_account_groups WHERE accountId = @ACCOUNTID;
+INSERT INTO rbac_account_groups (accountId, groupId, realmId) VALUES (@ACCOUNTID, 1, -1);
+INSERT INTO rbac_account_groups (accountId, groupId, realmId) VALUES (@ACCOUNTID, 2, -1);
+INSERT INTO rbac_account_groups (accountId, groupId, realmId) VALUES (@ACCOUNTID, 3, -1);
+INSERT INTO rbac_account_groups (accountId, groupId, realmId) VALUES (@ACCOUNTID, 4, -1);
