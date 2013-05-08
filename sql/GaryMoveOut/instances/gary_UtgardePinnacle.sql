36f78a9944d8249f534cf7374f272a11434b0401
Gary <fishermanpl@gmail.com>
2013-05-08 15:50:33 +0200

    Add sql...

---
+++ sql/GaryMoveOut/instances/gary_UtgardePinnacle.sql
@@ -0,0 +1,3 @@
+-- Update InhabitType for Skadi and Grauf. (Very Hacky, It's under the "It Works" Category.)
+UPDATE `creature_template` SET `InhabitType`=7 WHERE `entry`=26893;
+UPDATE `creature_template` SET `InhabitType`=7 WHERE `entry`=26693;
