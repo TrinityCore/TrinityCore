2e13b5541373d9a58c6608e95b58d984c4aaba56
Rochet2 <tqbattlenet@gmail.com>
2013-05-12 15:58:56 +0300

    UPDATED: Eluna

--- src/server/LuaEngine/UnitMethods.h
+++ src/server/LuaEngine/UnitMethods.h
@@ -826,7 +826,7 @@ public:
     {
         TO_PLAYER_BOOL();
 
-        sEluna->PushBoolean(L, player->GetSession()->IsARecruiter());
+        sEluna->PushBoolean(L, player->GetSession()->IsARecruiter() || (player->GetSession()->GetRecruiterId() != 0));
         return 1;
     }
 
