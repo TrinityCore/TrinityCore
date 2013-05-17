b0dac2fb5d61c68e8566bde7ebfc534db860a10f
Shauren <shauren.trinity@gmail.com>
2013-05-17 20:39:53 +0200

    Core/Misc: Fixed a bunch of issues found by static analysis

--- src/server/game/DataStores/DBCStores.cpp
+++ src/server/game/DataStores/DBCStores.cpp
@@ -902,7 +902,7 @@ LFGDungeonEntry const* GetLFGDungeon(uint32 mapId, Difficulty difficulty)
         if (!dungeon)
             continue;
 
-        if (dungeon->map == (int)mapId && Difficulty(dungeon->difficulty) == difficulty)
+        if (dungeon->map == int32(mapId) && Difficulty(dungeon->difficulty) == difficulty)
             return dungeon;
     }
 
