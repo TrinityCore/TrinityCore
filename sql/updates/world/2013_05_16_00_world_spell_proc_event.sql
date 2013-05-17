3508ba4961685d7c922646d963c2121dba3cd801
mik1893 <michele.roscelli@gmail.com>
2013-05-17 17:43:56 +0200

    SQL/SpellProc: modified SQL to have correct format

--- sql/updates/world/2013_05_16_00_world_spell_proc_event.sql
+++ sql/updates/world/2013_05_16_00_world_spell_proc_event.sql
@@ -1,3 +1,4 @@
-DELETE FROM spell_proc_event WHERE entry in(56333, 56336);
-INSERT INTO `spell_proc_event` VALUES (56333, 0, 0, 0, 134217728, 147456, 262144, 0, 0, 10, 0);
-INSERT INTO `spell_proc_event` VALUES (56336, 0, 0, 0, 134217728, 147456, 262144, 0, 0, 20, 0);
+DELETE FROM `spell_proc_event` WHERE `entry` IN (56333, 56336);
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
+(56333, 0, 0, 0, 134217728, 147456, 262144, 0, 0, 10, 0),
+(56336, 0, 0, 0, 134217728, 147456, 262144, 0, 0, 20, 0);
