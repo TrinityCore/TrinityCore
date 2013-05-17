130a22e74f5a5fddcbbeb4fdbdffcc5594f95d58
mik1893 <michele.roscelli@gmail.com>
2013-05-17 17:08:50 +0200

    SQL/SpellProc: fix TNT proc on Black Arrow

---
+++ sql/updates/world/2013_05_16_00_world_spell_proc_event.sql
@@ -0,0 +1,3 @@
+DELETE FROM spell_proc_event WHERE entry in(56333, 56336);
+INSERT INTO `spell_proc_event` VALUES (56333, 0, 0, 0, 134217728, 147456, 262144, 0, 0, 10, 0);
+INSERT INTO `spell_proc_event` VALUES (56336, 0, 0, 0, 134217728, 147456, 262144, 0, 0, 20, 0);
