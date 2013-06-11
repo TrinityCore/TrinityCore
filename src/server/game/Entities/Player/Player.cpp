ef15fe8ed28cb2d65574e8c5c755d39f8a06a994
Shauren <shauren.trinity@gmail.com>
2013-06-11 15:50:08 +0200

    Core/Instances: Fixed a bug that caused switching group leader inside active instance to cause players not being saved when a boss was killed.
    Closes #5109

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -18698,7 +18698,9 @@ void Player::ConvertInstancesToGroup(Player* player, Group* group, bool switchLe
     {
         for (BoundInstancesMap::iterator itr = player->m_boundInstances[i].begin(); itr != player->m_boundInstances[i].end();)
         {
-            group->BindToInstance(itr->second.save, itr->second.perm, false);
+            if (!switchLeader || !group->GetBoundInstance(itr->second.save->GetDifficulty(), itr->first))
+                group->BindToInstance(itr->second.save, itr->second.perm, false);
+
             // permanent binds are not removed
             if (switchLeader && !itr->second.perm)
             {
