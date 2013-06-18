405e550a4a991eb5d05e7b637781fc6ad2c6e0ff
dreamwowcom <soporte@dream-wow.com>
2013-06-19 00:51:24 +0200

    Revert "Prueba de reparacion del bug visual de movimiento"
    This reverts commit 44b02a27cf452ed6fd9b09410c2ab10b7eaad219.

--- src/server/game/Handlers/MovementHandler.cpp
+++ src/server/game/Handlers/MovementHandler.cpp
@@ -550,29 +550,17 @@ void WorldSession::HandleMountSpecialAnimOpcode(WorldPacket& /*recvData*/)
 void WorldSession::HandleMoveKnockBackAck(WorldPacket& recvData)
 {
     TC_LOG_DEBUG(LOG_FILTER_NETWORKIO, "CMSG_MOVE_KNOCK_BACK_ACK");
-	
-    uint64 guid;
-    recv_data.readPackGUID(guid);
-
-    if (_player->m_mover->GetGUID() != guid)
-        return;
-
-    recv_data.read_skip<uint32>();
 
     MovementInfo movementInfo;
-    ReadMovementInfo(recv_data, &movementInfo);
+    GetPlayer()->ReadMovementInfo(recvData, &movementInfo);
+
+    if (_player->m_mover->GetGUID() != movementInfo.guid)
+        return;
+
     _player->m_movementInfo = movementInfo;
 
-    WorldPacket data(MSG_MOVE_KNOCK_BACK, 66);
-    data.appendPackGUID(guid);
-    _player->BuildMovementPacket(&data);
-
-    // Información específica para cualquier movimiento.
-    data << movementInfo.j_sinAngle;
-    data << movementInfo.j_cosAngle;
-    data << movementInfo.j_xyspeed;
-    data << movementInfo.j_zspeed;
-
+    WorldPacket data(SMSG_MOVE_UPDATE_KNOCK_BACK, 66);
+    _player->WriteMovementInfo(data);
     _player->SendMessageToSet(&data, false);
 }
 
@@ -582,7 +570,7 @@ void WorldSession::HandleMoveHoverAck(WorldPacket& recvData)
 
     uint64 guid;                                            // guid - unused
     recvData.readPackGUID(guid);
-		
+
     recvData.read_skip<uint32>();                          // unk
 
     MovementInfo movementInfo;
