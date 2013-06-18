b4c3ab43d39d26ac6cf3f0fd238d454b6686603f
kaelima <kaelima@live.se>
2013-06-19 00:41:40 +0200

    Scripts: Prevented several KilledUnit texts to yelled for non-players such as pets

--- src/server/scripts/Kalimdor/CavernsOfTime/CullingOfStratholme/boss_meathook.cpp
+++ src/server/scripts/Kalimdor/CavernsOfTime/CullingOfStratholme/boss_meathook.cpp
@@ -125,7 +125,7 @@ public:
 
         void KilledUnit(Unit* victim)
         {
-            if (victim == me)
+            if (victim->GetTypeId() != TYPEID_PLAYER)
                 return;
 
             Talk(SAY_SLAY);
