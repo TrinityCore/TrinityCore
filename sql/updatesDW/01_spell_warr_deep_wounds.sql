02c0635dd26841c0fb87cc7e537558ece10b9155
Niebla30 <jwladi@gmail.com>
2013-06-20 19:38:06 +0200

    Añadida carpeta updatesDW a sql para poder meter los sql que vengan generados directamente por modificaciones del core. Añadidos los sql de los commits anteriores

---
+++ sql/updatesDW/01_spell_warr_deep_wounds.sql
@@ -0,0 +1,2 @@
+-- Este hechizo no es deep wounds, es un aura efecto de deep wounds rango 1
+Delete from spell_script_names where spell_id = -12162;
---
+++ sql/updatesDW/02_spell_item_flask_of_enhancement.sql
@@ -0,0 +1,3 @@
+-- Agregar esto para que funcione el script:
+DELETE FROM spell_script_names WHERE spell_id = 79637;
+INSERT INTO spell_script_names VALUES (79637, 'spell_item_flask_of_enhancement');
