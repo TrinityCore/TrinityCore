a2855e84c50167f19bcd7d616b07497809d130b7
Shauren <shauren.trinity@gmail.com>
2013-06-23 12:55:54 +0200

    Core/PacketIO: Enabled more opcodes

--- src/server/game/Guilds/Guild.cpp
+++ src/server/game/Guilds/Guild.cpp
@@ -1366,6 +1366,30 @@ bool Guild::SetName(std::string const& name)
     stmt->setString(0, m_name);
     stmt->setUInt32(1, GetId());
     CharacterDatabase.Execute(stmt);
+
+    ObjectGuid guid = GetGUID();
+    WorldPacket data(SMSG_GUILD_RENAMED, 24 + 8 + 1);
+    data.WriteBit(guid[5]);
+    data.WriteBits(name.length(), 8);
+    data.WriteBit(guid[4]);
+    data.WriteBit(guid[0]);
+    data.WriteBit(guid[6]);
+    data.WriteBit(guid[3]);
+    data.WriteBit(guid[1]);
+    data.WriteBit(guid[7]);
+    data.WriteBit(guid[2]);
+
+    data.WriteByteSeq(guid[3]);
+    data.WriteByteSeq(guid[2]);
+    data.WriteByteSeq(guid[7]);
+    data.WriteByteSeq(guid[1]);
+    data.WriteByteSeq(guid[0]);
+    data.WriteByteSeq(guid[6]);
+    data.WriteString(name);
+    data.WriteByteSeq(guid[4]);
+    data.WriteByteSeq(guid[5]);
+
+    BroadcastPacket(&data);
     return true;
 }
 
