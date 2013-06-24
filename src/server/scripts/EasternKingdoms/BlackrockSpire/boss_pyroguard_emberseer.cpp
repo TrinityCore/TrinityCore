d9c819a65d6812506e03e6f7ac9781b2fc0c4c41
Malcrom <malcromdev@gmail.com>
2013-06-23 23:07:08 -0230

    Core/Scripting: tcbuild is being a bitch and SD2 had nothing to do with this script.

--- src/server/scripts/EasternKingdoms/BlackrockSpire/boss_pyroguard_emberseer.cpp
+++ src/server/scripts/EasternKingdoms/BlackrockSpire/boss_pyroguard_emberseer.cpp
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) 2008-2013 TrinityCore <http://www.trinitycore.org/>
- * Copyright (C) 2006-2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -165,7 +164,7 @@ public:
             }
         }
 
-        void SpellHit(Unit* /*caster*/, const SpellInfo* spell)
+        void SpellHit(Unit* /*caster*/, SpellInfo const* spell)
         {
             if (spell->Id == SPELL_ENCAGE_EMBERSEER)
                 if (me->GetAuraCount(SPELL_ENCAGED_EMBERSEER) == 0)
