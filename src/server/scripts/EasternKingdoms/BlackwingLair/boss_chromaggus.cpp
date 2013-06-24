4bfdda98ea95773118d97a5855e5ce0a28471b75
Malcrom <malcromdev@gmail.com>
2013-06-24 13:48:02 -0230

    Core/Scripting: Updated BWL Scripts. Thanks to gerripeach for the base. Closes #4952 #9283

--- src/server/scripts/EasternKingdoms/BlackwingLair/boss_chromaggus.cpp
+++ src/server/scripts/EasternKingdoms/BlackwingLair/boss_chromaggus.cpp
@@ -16,15 +16,9 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* ScriptData
-SDName: Boss_Chromaggus
-SD%Complete: 95
-SDComment: Chromatic Mutation disabled due to lack of core support
-SDCategory: Blackwing Lair
-EndScriptData */
-
 #include "ScriptMgr.h"
 #include "ScriptedCreature.h"
+#include "blackwing_lair.h"
 
 enum Emotes
 {
@@ -34,22 +28,22 @@ enum Emotes
 
 enum Spells
 {
-    //These spells are actually called elemental shield
-    //What they do is decrease all damage by 75% then they increase
-    //One school of damage by 1100%
+    // These spells are actually called elemental shield
+    // What they do is decrease all damage by 75% then they increase
+    // One school of damage by 1100%
     SPELL_FIRE_VULNERABILITY                               = 22277,
     SPELL_FROST_VULNERABILITY                              = 22278,
     SPELL_SHADOW_VULNERABILITY                             = 22279,
     SPELL_NATURE_VULNERABILITY                             = 22280,
     SPELL_ARCANE_VULNERABILITY                             = 22281,
-    //Other spells
+    // Other spells
     SPELL_INCINERATE                                       = 23308,   //Incinerate 23308, 23309
     SPELL_TIMELAPSE                                        = 23310,   //Time lapse 23310, 23311(old threat mod that was removed in 2.01)
     SPELL_CORROSIVEACID                                    = 23313,   //Corrosive Acid 23313, 23314
     SPELL_IGNITEFLESH                                      = 23315,   //Ignite Flesh 23315, 23316
     SPELL_FROSTBURN                                        = 23187,   //Frost burn 23187, 23189
-    //Brood Affliction 23173 - Scripted Spell that cycles through all targets within 100 yards and has a chance to cast one of the afflictions on them
-    //Since Scripted spells arn't coded I'll just write a function that does the same thing
+    // Brood Affliction 23173 - Scripted Spell that cycles through all targets within 100 yards and has a chance to cast one of the afflictions on them
+    // Since Scripted spells arn't coded I'll just write a function that does the same thing
     SPELL_BROODAF_BLUE                                     = 23153,   //Blue affliction 23153
     SPELL_BROODAF_BLACK                                    = 23154,   //Black affliction 23154
     SPELL_BROODAF_RED                                      = 23155,   //Red affliction 23155 (23168 on death)
@@ -60,27 +54,31 @@ enum Spells
     SPELL_ENRAGE                                           = 28747
 };
 
+enum Events
+{
+    EVENT_SHIMMER       = 1,
+    EVENT_BREATH_1      = 2,
+    EVENT_BREATH_2      = 3,
+    EVENT_AFFLICTION    = 4,
+    EVENT_FRENZY        = 5
+};
+
 class boss_chromaggus : public CreatureScript
 {
 public:
     boss_chromaggus() : CreatureScript("boss_chromaggus") { }
 
-    CreatureAI* GetAI(Creature* creature) const
+    struct boss_chromaggusAI : public BossAI
     {
-        return new boss_chromaggusAI (creature);
-    }
-
-    struct boss_chromaggusAI : public ScriptedAI
-    {
-        boss_chromaggusAI(Creature* creature) : ScriptedAI(creature)
+        boss_chromaggusAI(Creature* creature) : BossAI(creature, BOSS_CHROMAGGUS)
         {
-            //Select the 2 breaths that we are going to use until despawned
-            //5 possiblities for the first breath, 4 for the second, 20 total possiblites
-            //This way we don't end up casting 2 of the same breath
-            //TL TL would be stupid
+            // Select the 2 breaths that we are going to use until despawned
+            // 5 possiblities for the first breath, 4 for the second, 20 total possiblites
+            // This way we don't end up casting 2 of the same breath
+            // TL TL would be stupid
             switch (urand(0, 19))
             {
-                //B1 - Incin
+                // B1 - Incin
                 case 0:
                     Breath1_Spell = SPELL_INCINERATE;
                     Breath2_Spell = SPELL_TIMELAPSE;
@@ -98,7 +96,7 @@ public:
                     Breath2_Spell = SPELL_FROSTBURN;
                     break;
 
-                    //B1 - TL
+                    // B1 - TL
                 case 4:
                     Breath1_Spell = SPELL_TIMELAPSE;
                     Breath2_Spell = SPELL_INCINERATE;
@@ -174,117 +172,97 @@ public:
             EnterEvadeMode();
         }
 
-        uint32 Breath1_Spell;
-        uint32 Breath2_Spell;
-        uint32 CurrentVurln_Spell;
-
-        uint32 Shimmer_Timer;
-        uint32 Breath1_Timer;
-        uint32 Breath2_Timer;
-        uint32 Affliction_Timer;
-        uint32 Frenzy_Timer;
-        bool Enraged;
-
         void Reset()
         {
-            CurrentVurln_Spell = 0;                             //We use this to store our last vulnerabilty spell so we can remove it later
+            _Reset();
 
-            Shimmer_Timer = 0;                                  //Time till we change vurlnerabilites
-            Breath1_Timer = 30000;                              //First breath is 30 seconds
-            Breath2_Timer = 60000;                              //Second is 1 minute so that we can alternate
-            Affliction_Timer = 10000;                           //This is special - 5 seconds means that we cast this on 1 player every 5 sconds
-            Frenzy_Timer = 15000;
-
+            CurrentVurln_Spell = 0;     // We use this to store our last vulnerabilty spell so we can remove it later
             Enraged = false;
         }
 
-        void EnterCombat(Unit* /*who*/) {}
+        void EnterCombat(Unit* /*who*/)
+        {
+            if (instance && instance->GetBossState(BOSS_FLAMEGOR) != DONE)
+            {
+                EnterEvadeMode();
+                return;
+            }
+            _EnterCombat();
+
+            events.ScheduleEvent(EVENT_SHIMMER, 0);
+            events.ScheduleEvent(EVENT_BREATH_1, 30000);
+            events.ScheduleEvent(EVENT_BREATH_2, 60000);
+            events.ScheduleEvent(EVENT_AFFLICTION, 10000);
+            events.ScheduleEvent(EVENT_FRENZY, 15000);
+        }
 
         void UpdateAI(uint32 diff)
         {
             if (!UpdateVictim())
                 return;
 
-            //Shimmer_Timer Timer
-            if (Shimmer_Timer <= diff)
+            events.Update(diff);
+
+            if (me->HasUnitState(UNIT_STATE_CASTING))
+                return;
+
+            while (uint32 eventId = events.ExecuteEvent())
             {
-                //Remove old vulnerabilty spell
-                if (CurrentVurln_Spell)
-                    me->RemoveAurasDueToSpell(CurrentVurln_Spell);
-
-                //Cast new random vulnerabilty on self
-                uint32 spell = RAND(SPELL_FIRE_VULNERABILITY, SPELL_FROST_VULNERABILITY,
-                    SPELL_SHADOW_VULNERABILITY, SPELL_NATURE_VULNERABILITY, SPELL_ARCANE_VULNERABILITY);
-
-                DoCast(me, spell);
-                CurrentVurln_Spell = spell;
-
-                Talk(EMOTE_SHIMMER);
-                Shimmer_Timer = 45000;
-            } else Shimmer_Timer -= diff;
-
-            //Breath1_Timer
-            if (Breath1_Timer <= diff)
-            {
-                DoCastVictim(Breath1_Spell);
-                Breath1_Timer = 60000;
-            } else Breath1_Timer -= diff;
-
-            //Breath2_Timer
-            if (Breath2_Timer <= diff)
-            {
-                DoCastVictim(Breath2_Spell);
-                Breath2_Timer = 60000;
-            } else Breath2_Timer -= diff;
-
-            //Affliction_Timer
-            if (Affliction_Timer <= diff)
-            {
-                ThreatContainer::StorageType threatlist = me->getThreatManager().getThreatList();
-                for (ThreatContainer::StorageType::const_iterator i = threatlist.begin(); i != threatlist.end(); ++i)
+                switch (eventId)
                 {
-                    if ((*i) && (*i)->GetSource())
-                    {
-                        if (Unit* unit = Unit::GetUnit(*me, (*i)->getUnitGuid()))
+                    case EVENT_SHIMMER:
                         {
-                            //Cast affliction
-                            DoCast(unit, RAND(SPELL_BROODAF_BLUE, SPELL_BROODAF_BLACK,
-                                               SPELL_BROODAF_RED, SPELL_BROODAF_BRONZE, SPELL_BROODAF_GREEN), true);
+                            // Remove old vulnerabilty spell
+                            if (CurrentVurln_Spell)
+                                me->RemoveAurasDueToSpell(CurrentVurln_Spell);
 
-                            //Chromatic mutation if target is effected by all afflictions
-                            if (unit->HasAura(SPELL_BROODAF_BLUE)
-                                && unit->HasAura(SPELL_BROODAF_BLACK)
-                                && unit->HasAura(SPELL_BROODAF_RED)
-                                && unit->HasAura(SPELL_BROODAF_BRONZE)
-                                && unit->HasAura(SPELL_BROODAF_GREEN))
+                            // Cast new random vulnerabilty on self
+                            uint32 spell = RAND(SPELL_FIRE_VULNERABILITY, SPELL_FROST_VULNERABILITY, SPELL_SHADOW_VULNERABILITY, SPELL_NATURE_VULNERABILITY, SPELL_ARCANE_VULNERABILITY);
+                            DoCast(me, spell);
+                            CurrentVurln_Spell = spell;
+                            Talk(EMOTE_SHIMMER);
+                            events.ScheduleEvent(EVENT_SHIMMER, 45000);
+                            break;
+                        }
+                    case EVENT_BREATH_1:
+                            DoCastVictim(Breath1_Spell);
+                            events.ScheduleEvent(EVENT_BREATH_1, 60000);
+                            break;
+                    case EVENT_BREATH_2:
+                            DoCastVictim(Breath2_Spell);
+                            events.ScheduleEvent(EVENT_BREATH_2, 60000);
+                            break;
+                    case EVENT_AFFLICTION:
+                        {
+                            Map::PlayerList const &players = me->GetMap()->GetPlayers();
+                            for (Map::PlayerList::const_iterator itr = players.begin(); itr != players.end(); ++itr)
                             {
-                                //target->RemoveAllAuras();
-                                //DoCast(target, SPELL_CHROMATIC_MUT_1);
+                                if (Player* player = itr->GetSource()->ToPlayer())
+                                {
+                                    DoCast(player, RAND(SPELL_BROODAF_BLUE, SPELL_BROODAF_BLACK, SPELL_BROODAF_RED, SPELL_BROODAF_BRONZE, SPELL_BROODAF_GREEN), true);
 
-                                //Chromatic mutation is causing issues
-                                //Assuming it is caused by a lack of core support for Charm
-                                //So instead we instant kill our target
+                                        if (player->HasAura(SPELL_BROODAF_BLUE) &&
+                                            player->HasAura(SPELL_BROODAF_BLACK) &&
+                                            player->HasAura(SPELL_BROODAF_RED) &&
+                                            player->HasAura(SPELL_BROODAF_BRONZE) &&
+                                            player->HasAura(SPELL_BROODAF_GREEN))
+                                        {
+                                            DoCast(player, SPELL_CHROMATIC_MUT_1);
+                                        }
 
-                                //WORKAROUND
-                                if (unit->GetTypeId() == TYPEID_PLAYER)
-                                    unit->CastSpell(unit, 5, false);
+                                }
                             }
                         }
-                    }
+                        events.ScheduleEvent(EVENT_AFFLICTION, 10000);
+                        break;
+                    case EVENT_FRENZY:
+                        DoCast(me, SPELL_FRENZY);
+                        events.ScheduleEvent(EVENT_FRENZY, urand(10000, 15000));
+                        break;
                 }
+            }
 
-                Affliction_Timer = 10000;
-            } else Affliction_Timer -= diff;
-
-            //Frenzy_Timer
-            if (Frenzy_Timer <= diff)
-            {
-                DoCast(me, SPELL_FRENZY);
-                Talk(EMOTE_FRENZY);
-                Frenzy_Timer = urand(10000, 15000);
-            } else Frenzy_Timer -= diff;
-
-            //Enrage if not already enraged and below 20%
+            // Enrage if not already enraged and below 20%
             if (!Enraged && HealthBelowPct(20))
             {
                 DoCast(me, SPELL_ENRAGE);
@@ -293,7 +271,18 @@ public:
 
             DoMeleeAttackIfReady();
         }
+
+    private:
+        uint32 Breath1_Spell;
+        uint32 Breath2_Spell;
+        uint32 CurrentVurln_Spell;
+        bool Enraged;
     };
+
+    CreatureAI* GetAI(Creature* creature) const
+    {
+        return new boss_chromaggusAI (creature);
+    }
 };
 
 void AddSC_boss_chromaggus()
