b5744bb8d50110d9c0224a39f7f726d2d23afda5
Aokromes <jipr@hotmail.com>
2013-07-01 08:17:16 +0200

    DB/Misc: Typo fix

--- sql/updates/world/2013_06_30_16_world_misc.sql
+++ sql/updates/world/2013_06_30_16_world_misc.sql
@@ -5,11 +5,11 @@ DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@WAROSH;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (@WAROSH,0,0,0,1,0,100,0,20000,20000,20000,20000,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0, 'Warosh -  OOC - Say 0');
 -- Text for Warosh
-DELETE FROM `creature_text` WHERE `entry` IN (WAROSH);
+DELETE FROM `creature_text` WHERE `entry` IN (@WAROSH);
 INSERT INTO `creature_text` (`entry`,`groupid`,`id`,`text`,`type`,`language`,`probability`,`emote`,`duration`,`sound`,`comment`) VALUES
-(WAROSH,0,0,'%s cries.',16,0,100,0,0,0,'Warosh'),
-(WAROSH,0,1,'%s snorts.',16,0,100,0,0,0,'Warosh'),
-(WAROSH,0,2,'%s grumbles.',16,0,100,0,0,0,'Warosh');
+(@WAROSH,0,0,'%s cries.',16,0,100,0,0,0,'Warosh'),
+(@WAROSH,0,1,'%s snorts.',16,0,100,0,0,0,'Warosh'),
+(@WAROSH,0,2,'%s grumbles.',16,0,100,0,0,0,'Warosh');
 
 -- Add missing and path Warosh Entry: 10799
 SET @NPC := 17;
