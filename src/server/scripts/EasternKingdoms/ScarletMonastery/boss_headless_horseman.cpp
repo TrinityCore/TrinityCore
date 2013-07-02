11d59f20a4b3562e0dcc5f0be06d4524da6770b5
Aokromes <jipr@hotmail.com>
2013-07-02 17:38:07 +0200

    Core/Scripting: Allow Headless Horseman to be attacked
    Fix by digz6666.
    Note: this script needs rewrite, any pull request doing it is welcome.
    Closes #3568

--- src/server/scripts/EasternKingdoms/ScarletMonastery/boss_headless_horseman.cpp
+++ src/server/scripts/EasternKingdoms/ScarletMonastery/boss_headless_horseman.cpp
@@ -435,6 +435,7 @@ public:
                 headGUID = 0;
             }
 
+            me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_IMMUNE_TO_PC);
             //if (instance)
             //    instance->SetData(DATA_HORSEMAN_EVENT, NOT_STARTED);
         }
