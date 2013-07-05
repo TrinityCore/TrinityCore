17f031f12d7fcf875f9a69a396d0f673b5c09ca3
Vincent-Michael <Vincent_Michael@gmx.de>
2013-07-05 17:19:29 +0200

    Misc: - Fix missing newlines - Fix some startup errors

--- sql/updates/world/2013_07_04_02_world_misc.sql
+++ sql/updates/world/2013_07_04_02_world_misc.sql
@@ -1,4 +1,3 @@
 UPDATE `creature_template` SET `ScriptName` = REPLACE(`ScriptName`, 'mob_', 'npc_') WHERE `ScriptName` LIKE 'mob\_%';
 UPDATE `creature_template` SET `ScriptName` = REPLACE(`ScriptName`, 'mobs_', 'npc_') WHERE `ScriptName` LIKE 'mobs\_%';
 UPDATE `spell_script_names` SET `ScriptName`='spell_trash_npc_glacial_strike' WHERE `ScriptName`='spell_trash_mob_glacial_strike';
-
