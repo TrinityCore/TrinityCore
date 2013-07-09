293a9757eb7f5315cbfee0f489faa674898ce9ce
Malcrom <malcromdev@gmail.com>
2013-07-09 07:10:48 -0230

    Core / Scripting: Coverted a couple Shattered Halls npc scripts to use events

--- src/server/scripts/Outland/HellfireCitadel/ShatteredHalls/boss_nethekurse.cpp
+++ src/server/scripts/Outland/HellfireCitadel/ShatteredHalls/boss_nethekurse.cpp
@@ -66,6 +66,12 @@ enum SetData
     SETDATA_PEON_DEATH         = 2
 };
 
+enum Events
+{
+    // Fel Orc Convert
+    EVENT_HEMORRHAGE           = 1
+};
+
 // ########################################################
 // Grand Warlock Nethekurse
 // ########################################################
@@ -314,13 +320,14 @@ class npc_fel_orc_convert : public CreatureScript
             void Reset() OVERRIDE
             {
                 me->SetNoCallAssistance(true);              //we don't want any assistance (WE R HEROZ!)
-                Hemorrhage_Timer = 3000;
             }
 
             void MoveInLineOfSight(Unit* /*who*/) OVERRIDE { }
 
             void EnterCombat(Unit* /*who*/) OVERRIDE
             {
+                events.ScheduleEvent(EVENT_HEMORRHAGE, 3000);
+
                 if (instance)
                     if (Creature* Kurse = Unit::GetCreature(*me, instance->GetData64(NPC_GRAND_WARLOCK_NETHEKURSE)))
                         if (Kurse && me->IsWithinDist(Kurse, 45.0f))
@@ -344,18 +351,20 @@ class npc_fel_orc_convert : public CreatureScript
                 if (!UpdateVictim())
                     return;
 
-                if (Hemorrhage_Timer <= diff)
+                events.Update(diff);
+
+                if (uint32 EVENT_HEMORRHAGE = events.ExecuteEvent())
                 {
                     DoCastVictim(SPELL_HEMORRHAGE);
-                    Hemorrhage_Timer = 15000;
-                } else Hemorrhage_Timer -= diff;
+                    events.ScheduleEvent(EVENT_HEMORRHAGE, 15000);
+                }
 
                 DoMeleeAttackIfReady();
             }
 
             private:
                 InstanceScript* instance;
-                uint32 Hemorrhage_Timer;
+                EventMap events;
         };
 
         CreatureAI* GetAI(Creature* creature) const OVERRIDE
