2005be04740406f17bd8af8569f2cd75aa0614d4
Warpten <vertozor@gmail.com>
2013-07-10 21:44:08 +0200

    Shared/Utils: Fixed ApplyPercentModFloatVar.
    This has been around since 2010-12-11 :)

--- src/server/shared/Utilities/Util.h
+++ src/server/shared/Utilities/Util.h
@@ -114,7 +114,8 @@ inline void ApplyPercentModFloatVar(float& var, float val, bool apply)
 {
     if (val == -100.0f)     // prevent set var to zero
         val = -99.99f;
-    var *= (apply ? (100.0f + val) / 100.0f : 100.0f / (100.0f + val));
+
+    var *= apply ? (100.0f + val) / 100.0f : (100.0f - val) / 100.0f;
 }
 
 // Percentage calculation
