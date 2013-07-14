6cc547bc2dd666269597a436871700a7579ab178
Warpten <vertozor@gmail.com>
2013-07-14 16:46:11 +0200

    Core/Players: Dueling players in your party / raid should not make them always detectable.
    Fixes seeing stealthed rogues & druids, as well as invisible mages in duels.

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -22106,10 +22106,10 @@ bool Player::IsAlwaysDetectableFor(WorldObject const* seer) const
 
     if (const Player* seerPlayer = seer->ToPlayer())
         if (IsGroupVisibleFor(seerPlayer))
-            return true;
+            return !(seerPlayer->duel && seerPlayer->duel->startTime != 0 && seerPlayer->duel->opponent == this);
 
-     return false;
- }
+    return false;
+}
 
 bool Player::IsVisibleGloballyFor(Player const* u) const
 {
