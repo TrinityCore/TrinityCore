8b0cea7a5eea1e61d9c1f179d48bd85a61c8c5ca
Nay <dnpd.dd@gmail.com>
2013-07-14 18:46:48 +0100

    SQL: Prevent a warning in 2013_07_14_01_world_item_template.sql

--- sql/updates/world/2013_07_14_01_world_item_template.sql
+++ sql/updates/world/2013_07_14_01_world_item_template.sql
@@ -1,5 +1,3 @@
-ALTER TABLE `item_template` CHANGE `Flags` `Flags` int(10) unsigned NOT NULL DEFAULT '0';
-
 UPDATE `item_template` SET `Flags`=0x80000000 WHERE `entry`=23614;
 UPDATE `item_template` SET `Flags`=0x80000800 WHERE `entry`=28635;
 UPDATE `item_template` SET `Flags`=0x80000800 WHERE `entry`=28636;
@@ -12,3 +10,5 @@ UPDATE `item_template` SET `Flags`=0x80000040 WHERE `entry`=40652;
 UPDATE `item_template` SET `Flags`=0x80000040 WHERE `entry`=40970;
 UPDATE `item_template` SET `Flags`=0x80000800 WHERE `entry`=41843;
 UPDATE `item_template` SET `Flags`=0x80000800 WHERE `entry`=42782;
+
+ALTER TABLE `item_template` CHANGE `Flags` `Flags` int(10) unsigned NOT NULL DEFAULT '0';
