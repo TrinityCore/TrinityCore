4124d91bae0358fa3db00d61b037fc465b83f35f
snakeice <snakeice@gmail.com>
2013-07-19 07:59:13 -0300

    Fixing SQL semicolons and Uppercase

--- sql/updates/world/2013_07_17_00_world_misc.sql
+++ sql/updates/world/2013_07_17_00_world_misc.sql
@@ -1,23 +1,23 @@
 -- areatrigger for the shadow throne
-DELETE FROM `areatrigger_scripts` WHERE `entry` = 5605
+DELETE FROM `areatrigger_scripts` WHERE `entry` = 5605;
 INSERT INTO `areatrigger_scripts` (`entry`,`scriptname`) VALUES (5605,'at_shadow_throne');
 
 -- jaina's equipment
-DELETE FROM `creature_equip_template` WHERE `entry` = 36955 AND `id` = 1
+DELETE FROM `creature_equip_template` WHERE `entry` = 36955 AND `id` = 1;
 INSERT INTO `creature_equip_template` (`entry`, `id`, `itementry1`, `itementry2`,`itementry3`) VALUES (36955,1,2177,12869,0);
 
 -- jaina's gossip menu (the same one FROM sylvanas)
-update `creature_template` SET `gossip_menu_id` = 10909 WHERE `entry` = 36955;
+UPDATE `creature_template` SET `gossip_menu_id` = 10909 WHERE `entry` = 36955;
 
 -- jaina's scriptai part 2
-update `creature_template` SET `ScriptName` = 'npc_jaina_or_sylvanas_escape_hor' WHERE `entry` in (36955,37554);
+UPDATE `creature_template` SET `ScriptName` = 'npc_jaina_or_sylvanas_escape_hor' WHERE `entry` in (36955,37554);
 
 -- Raging Gnoul scriptai
-update `creature_template` SET `ScriptName` = 'npc_raging_gnoul' WHERE `entry` = 36940;
+UPDATE `creature_template` SET `ScriptName` = 'npc_raging_gnoul' WHERE `entry` = 36940;
 
 -- Risen Witch Doctor scriptai
-update `creature_template` SET `ScriptName` = 'npc_risen_witch_doctor' WHERE `entry` = 36941;
+UPDATE `creature_template` SET `ScriptName` = 'npc_risen_witch_doctor' WHERE `entry` = 36941;
 
 -- LumberINg Abomination scriptai
-update `creature_template` SET `ScriptName` = 'npc_lumbering_abomination' WHERE `entry` = 37069;
+UPDATE `creature_template` SET `ScriptName` = 'npc_lumbering_abomination' WHERE `entry` = 37069;
 
