7031c26441b5d15cefb3c0ec98f734386e5c8e14
Vincent-Michael <Vincent_Michael@gmx.de>
2013-07-19 22:41:14 +0200

    SQL: Fix sql fail (thx Sar777)

--- sql/updates/world/2013_07_19_00_world_spell_script_misc_434.sql
+++ sql/updates/world/2013_07_19_00_world_spell_script_misc_434.sql
@@ -1,7 +1,7 @@
 DELETE FROM `spell_script_names` WHERE `spell_id` IN (-52752,55440);
 INSERT INTO `spell_script_names`(`spell_id`, `ScriptName`) VALUES
 (-52752,'spell_sha_ancestral_awakening'),
-(55440,'spell_sha_ancestral_awakening');
+(55440,'spell_sha_glyph_of_healing_wave');
 
 DELETE FROM `spell_ranks` WHERE `first_spell_id`=52752;
 INSERT INTO `spell_ranks` (`first_spell_id`, `spell_id`, `rank`) VALUES
