4c88602a88bb9964b1d05a273d0b638828273564
Vincent-Michael <Vincent_Michael@gmx.de>
2013-07-20 18:24:47 +0200

    DB/Spells: Fix typo in 275299150b53cb66e456cdaae8cc8045638c938c

--- sql/updates/world/2013_07_20_04_world_spell_script_misc_434.sql
+++ sql/updates/world/2013_07_20_04_world_spell_script_misc_434.sql
@@ -4,4 +4,4 @@ INSERT INTO `spell_script_names`(`spell_id`, `ScriptName`) VALUES
 
 DELETE FROM `spell_proc_event` WHERE `entry`=32216;
 INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
-(32216, 0, 4, 0, 256, 0, 16, 0, 0, 0, 0);
+(32216, 0, 4, 0, 256, 0, 16, 0, 0, 100, 0);
