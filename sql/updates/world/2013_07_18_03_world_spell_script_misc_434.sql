d4736f2613da51676347466ed378be4a6bf3aa9d
Roifok <skunkyfok@gmail.com>
2013-07-20 22:04:15 +0200

    Revert "Cleaning all the code"
    This reverts commit d11c161de774b5ea9e07c30dc878c1b761696de5.

---
+++ sql/updates/world/2013_07_18_03_world_spell_script_misc_434.sql
@@ -0,0 +1,5 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (88747,88751,774);
+INSERT INTO `spell_script_names`(`spell_id`, `ScriptName`) VALUES
+(88747,'spell_druid_wild_mushroom'),
+(88751,'spell_druid_wild_mushroom_detonate'),
+(774,'spell_druid_rejuvenation');
