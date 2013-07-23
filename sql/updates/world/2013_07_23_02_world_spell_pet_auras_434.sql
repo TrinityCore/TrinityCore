5edd13e85d0e608647d10dd8d336c25facad23d2
Vincent-Michael <Vincent_Michael@gmx.de>
2013-07-23 15:07:20 +0200

    Revert "Core/Spells: Fix hunter talent "Kindred Spirits" for 4.3.4"
    This reverts commit 97608aeff754ef94b73b868437d8f41ccace24be.

--- sql/updates/world/2013_07_23_02_world_spell_pet_auras_434.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `spell_pet_auras WHERE `spell` IN (56314,56315);
-INSERT INTO `spell_pet_auras` (`spell`, `effectId`, `pet`, `aura`) VALUES
-(56314, 0, 0, 88680),
-(56315 , 0, 0, 88680);
