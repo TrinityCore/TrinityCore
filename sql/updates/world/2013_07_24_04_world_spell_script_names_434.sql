2f98d1a451b6466a6853c679a54cbf657e6aecb7
Warpten <vertozor@gmail.com>
2013-07-24 15:01:38 +0200

    Ctrl+S, i SERIOUSLY HATE YOU

--- sql/updates/world/2013_07_24_04_world_spell_script_names_434.sql
+++ sql/updates/world/2013_07_24_04_world_spell_script_names_434.sql
@@ -1,6 +1,6 @@
-DELETE FROM spell_script_names WHERE ScriptName LIKE "spell_warlock_soul_swap%";
+DELETE FROM spell_script_names WHERE ScriptName LIKE "spell_warl_soul_swap%";
 INSERT INTO spell_script_names (spell_id, ScriptName) VALUES
-(86121, "spell_warlock_soul_swap"),
-(86211, "spell_warlock_soul_swap_override"),
-(92795, "spell_warlock_soul_swap_dot_marker"),
-(86213, "spell_warlock_soul_swap_exhale");
+(86121, "spell_warl_soul_swap"),
+(86211, "spell_warl_soul_swap_override"),
+(92795, "spell_warl_soul_swap_dot_marker"),
+(86213, "spell_warl_soul_swap_exhale");
