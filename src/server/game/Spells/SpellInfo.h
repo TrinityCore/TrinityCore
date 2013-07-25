9214e5a7d3c1613ed3933edcabd0dc2a3950dcc6
joschiwald <joschiwald@online.de>
2013-07-25 18:32:00 +0200

    Core/Spells: fix uninitialised value

--- src/server/game/Spells/SpellInfo.h
+++ src/server/game/Spells/SpellInfo.h
@@ -198,7 +198,7 @@ class SpellImplicitTargetInfo
 private:
     Targets _target;
 public:
-    SpellImplicitTargetInfo() {}
+    SpellImplicitTargetInfo() : _target(Targets(0)) { }
     SpellImplicitTargetInfo(uint32 target);
 
     bool IsArea() const;
