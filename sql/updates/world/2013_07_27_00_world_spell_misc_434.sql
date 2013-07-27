56b50bf5b17ccca9bcca0330220e4d9fe4428bd5
Vincent-Michael <Vincent_Michael@gmx.de>
2013-07-27 03:07:05 +0200

    DB/Spells: Fix typo in cfb79667e6c2a0b524e1162b59fe21e556c30be9

--- sql/updates/world/2013_07_27_00_world_spell_misc_434.sql
+++ sql/updates/world/2013_07_27_00_world_spell_misc_434.sql
@@ -1,6 +1,6 @@
 DELETE FROM `spell_proc_event` WHERE `entry`=-84583;
 INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
-(-84583, 0, 4, 0x22200004, 0, 0, 0, 0, 0, 0, 0);
+(-84583, 0, 4, 0x2000000, 0, 0, 0, 0, 0, 0, 0);
 
 DELETE FROM `spell_script_names` WHERE `spell_id`=-84583;
 INSERT INTO `spell_script_names`(`spell_id`, `ScriptName`) VALUES
