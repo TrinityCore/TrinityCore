0f46bf50224a49f7d2745f4e40a6175b9e9de5ce
Nyeriah <sarah.trysan@live.com>
2013-08-05 02:25:31 -0300

    [TW046] - Core/Scripts: ToCr - Fix Anub'arak's respawn delay
    Should be 1 week, otherwise he respawns in 5 minutes. REWRITE ME if there's a better way to do this. For as far as I know, for temp summons...

--- src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/trial_of_the_crusader.cpp
+++ src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/trial_of_the_crusader.cpp
@@ -210,7 +210,7 @@ class npc_announcer_toc10 : public CreatureScript
                 Creature* anubArak = Unit::GetCreature(*creature, instance->GetData64(NPC_ANUBARAK));
                 if (!anubArak || !anubArak->IsAlive())
                     anubArak = creature->SummonCreature(NPC_ANUBARAK, AnubarakLoc[0].GetPositionX(), AnubarakLoc[0].GetPositionY(), AnubarakLoc[0].GetPositionZ(), 3, TEMPSUMMON_CORPSE_TIMED_DESPAWN, DESPAWN_TIME);
-
+                
                 instance->SetBossState(BOSS_ANUBARAK, NOT_STARTED);
 
                 if (creature->IsVisible())
@@ -332,8 +332,11 @@ class boss_lich_king_toc : public CreatureScript
                             _instance->SetBossState(BOSS_LICH_KING, DONE);
                             Creature* temp = Unit::GetCreature(*me, _instance->GetData64(NPC_ANUBARAK));
                             if (!temp || !temp->IsAlive())
+                            {
                                 temp = me->SummonCreature(NPC_ANUBARAK, AnubarakLoc[0].GetPositionX(), AnubarakLoc[0].GetPositionY(), AnubarakLoc[0].GetPositionZ(), 3, TEMPSUMMON_CORPSE_TIMED_DESPAWN, DESPAWN_TIME);
-
+                                temp->SetRespawnDelay(7*DAY);
+                            }
+                            
                             _instance->SetData(TYPE_EVENT, 0);
 
                             me->DespawnOrUnsummon();
