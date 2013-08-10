ba6e62fd6ec00dcd03ae1fa5332a8499ec139ca0
Vincent-Michael <Vincent_Michael@gmx.de>
2013-08-10 16:35:06 +0200

    Scripted/BaradinHold: Implemented Pit Lord Argaloth Note: Need some update with sniff

---
+++ src/server/scripts/EasternKingdoms/BaradinHold/boss_pit_lord_argaloth.cpp
@@ -0,0 +1,200 @@
+/*
+* Copyright (C) 2008-2013 TrinityCore <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "ScriptMgr.h"
+#include "ScriptedCreature.h"
+#include "SpellScript.h"
+#include "baradin_hold.h"
+
+/* TODO:
+- Fel Firestorm need completion
+- Need Correct timer
+*/
+
+enum Spells
+{
+    SPELL_METEOR_SLASH          = 88942,
+    SPELL_CONSUMING_DARKNESS    = 88954,
+    SPELL_FEL_FIRESTORM         = 88972,
+    SPELL_BERSEKER              = 47008
+};
+
+enum Events
+{
+    EVENT_METEOR_SLASH          = 1,
+    EVENT_CONSUMING_DARKNESS    = 2,
+    EVENT_BERSEKER              = 3
+};
+
+class boss_pit_lord_argaloth : public CreatureScript
+{
+    public:
+        boss_pit_lord_argaloth() : CreatureScript("boss_pit_lord_argaloth") { }
+
+        struct boss_pit_lord_argalothAI : public BossAI
+        {
+            boss_pit_lord_argalothAI(Creature* creature) : BossAI(creature, DATA_ARGALOTH) { }
+
+            void EnterCombat(Unit* /*who*/) OVERRIDE
+            {
+                _EnterCombat();
+                instance->SendEncounterUnit(ENCOUNTER_FRAME_ENGAGE, me);
+                events.ScheduleEvent(EVENT_METEOR_SLASH, urand(10 * IN_MILLISECONDS, 20 * IN_MILLISECONDS));
+                events.ScheduleEvent(EVENT_CONSUMING_DARKNESS, urand(20 * IN_MILLISECONDS, 25 * IN_MILLISECONDS));
+                events.ScheduleEvent(SPELL_BERSEKER, 5 * MINUTE * IN_MILLISECONDS);
+            }
+
+            void EnterEvadeMode() OVERRIDE
+            {
+                me->GetMotionMaster()->MoveTargetedHome();
+                instance->SendEncounterUnit(ENCOUNTER_FRAME_DISENGAGE, me);
+                _DespawnAtEvade();
+            }
+
+            void DamageTaken(Unit* /*attacker*/, uint32& damage) OVERRIDE
+            {
+                if (me->HealthBelowPctDamaged(33, damage) ||
+                    me->HealthBelowPctDamaged(66, damage))
+                {
+                    DoCastAOE(SPELL_FEL_FIRESTORM);
+                }
+            }
+
+            void JustDied(Unit* /*killer*/) OVERRIDE
+            {
+                _JustDied();
+                instance->SendEncounterUnit(ENCOUNTER_FRAME_DISENGAGE, me);
+            }
+
+            void UpdateAI(uint32 diff) OVERRIDE
+            {
+                if (!UpdateVictim())
+                    return;
+
+                events.Update(diff);
+
+                if (me->HasUnitState(UNIT_STATE_CASTING))
+                    return;
+
+                while (uint32 eventId = events.ExecuteEvent())
+                {
+                    switch (eventId)
+                    {
+                        case EVENT_METEOR_SLASH:
+                            DoCastAOE(SPELL_METEOR_SLASH);
+                            events.ScheduleEvent(EVENT_METEOR_SLASH, urand(15 * IN_MILLISECONDS, 20 * IN_MILLISECONDS));
+                            break;
+                        case EVENT_CONSUMING_DARKNESS:
+                            DoCastAOE(SPELL_CONSUMING_DARKNESS, true);
+                            events.ScheduleEvent(EVENT_CONSUMING_DARKNESS, urand(20 * IN_MILLISECONDS, 25 * IN_MILLISECONDS));
+                            break;
+                        case SPELL_BERSEKER:
+                            DoCast(me, EVENT_BERSEKER, true);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                DoMeleeAttackIfReady();
+            }
+        };
+
+        CreatureAI* GetAI(Creature* creature) const OVERRIDE
+        {
+            return GetBaradinHoldAI<boss_pit_lord_argalothAI>(creature);
+        }
+};
+
+// 88954 / 95173 - Consuming Darkness
+class spell_argaloth_consuming_darkness : public SpellScriptLoader
+{
+    public:
+        spell_argaloth_consuming_darkness() : SpellScriptLoader("spell_argaloth_consuming_darkness") { }
+
+        class spell_argaloth_consuming_darkness_SpellScript : public SpellScript
+        {
+            PrepareSpellScript(spell_argaloth_consuming_darkness_SpellScript);
+
+            void FilterTargets(std::list<WorldObject*>& targets)
+            {
+                Trinity::Containers::RandomResizeList(targets, GetCaster()->GetMap()->Is25ManRaid() ? 8 : 3);
+            }
+
+            void Register() OVERRIDE
+            {
+                OnObjectAreaTargetSelect += SpellObjectAreaTargetSelectFn(spell_argaloth_consuming_darkness_SpellScript::FilterTargets, EFFECT_0, TARGET_UNIT_SRC_AREA_ENEMY);
+            }
+        };
+
+        SpellScript* GetSpellScript() const OVERRIDE
+        {
+            return new spell_argaloth_consuming_darkness_SpellScript();
+        }
+};
+
+// 88942 / 95172 - Meteor Slash
+class spell_argaloth_meteor_slash : public SpellScriptLoader
+{
+    public:
+        spell_argaloth_meteor_slash() : SpellScriptLoader("spell_argaloth_meteor_slash") { }
+
+        class spell_argaloth_meteor_slash_SpellScript : public SpellScript
+        {
+            PrepareSpellScript(spell_argaloth_meteor_slash_SpellScript);
+
+            bool Load() OVERRIDE
+            {
+                _targetCount = 0;
+                return true;
+            }
+
+            void CountTargets(std::list<WorldObject*>& targets)
+            {
+                _targetCount = targets.size();
+            }
+
+            void SplitDamage()
+            {
+                if (!_targetCount)
+                    return;
+
+                SetHitDamage(GetHitDamage() / _targetCount);
+            }
+
+            void Register() OVERRIDE
+            {
+                OnObjectAreaTargetSelect += SpellObjectAreaTargetSelectFn(spell_argaloth_meteor_slash_SpellScript::CountTargets, EFFECT_0, TARGET_UNIT_DEST_AREA_ENEMY);
+                OnHit += SpellHitFn(spell_argaloth_meteor_slash_SpellScript::SplitDamage);
+            }
+
+        private:
+            uint32 _targetCount;
+        };
+
+        SpellScript* GetSpellScript() const OVERRIDE
+        {
+            return new spell_argaloth_meteor_slash_SpellScript();
+        }
+};
+
+void AddSC_boss_pit_lord_argaloth()
+{
+    new boss_pit_lord_argaloth();
+    new spell_argaloth_consuming_darkness();
+    new spell_argaloth_meteor_slash();
+}
--- src/server/scripts/EasternKingdoms/BaradinHold/instance_baradin_hold.cpp
+++ src/server/scripts/EasternKingdoms/BaradinHold/instance_baradin_hold.cpp
@@ -27,142 +27,141 @@ DoorData const doorData[] =
 
 class instance_baradin_hold: public InstanceMapScript
 {
-public:
-    instance_baradin_hold() : InstanceMapScript("instance_baradin_hold", 757) { }
+    public:
+        instance_baradin_hold() : InstanceMapScript(BHScriptName, 757) { }
 
-    InstanceScript* GetInstanceScript(InstanceMap* map) const
-    {
-        return new instance_baradin_hold_InstanceMapScript(map);
-    }
-
-    struct instance_baradin_hold_InstanceMapScript: public InstanceScript
-    {
-        instance_baradin_hold_InstanceMapScript(InstanceMap* map) : InstanceScript(map)
+        struct instance_baradin_hold_InstanceMapScript: public InstanceScript
         {
-            SetBossNumber(MAX_ENCOUNTERS);
-            LoadDoorData(doorData);
-            _argalothGUID = 0;
-            _occutharGUID = 0;
-            _alizabalGUID = 0;
-            _argalothDoor = 0;
-            _occutharDoor = 0;
-        }
-
-        void Initialize()
-        {
-        }
-
-        void OnCreatureCreate(Creature* creature)
-        {
-            switch(creature->GetEntry())
+            instance_baradin_hold_InstanceMapScript(InstanceMap* map) : InstanceScript(map)
             {
-                case BOSS_ARGALOTH:
-                    _argalothGUID = creature->GetGUID();
-                    break;
-                case BOSS_OCCUTHAR:
-                    _occutharGUID = creature->GetGUID();
-                    break;
-                case BOSS_ALIZABAL:
-                    _alizabalGUID = creature->GetGUID();
-                    break;
-            }
-        }
+                SetBossNumber(EncounterCount);
+                LoadDoorData(doorData);
 
-        void OnGameObjectCreate(GameObject* go)
-        {
-            switch(go->GetEntry())
-            {
-                case GO_ARGALOTH_DOOR:
-                    _argalothDoor = go->GetGUID();
-                    AddDoor(go, true);
-                    break;
-                case GO_OCCUTHAR_DOOR:
-                    _occutharDoor = go->GetGUID();
-                    AddDoor(go, true);
-                    break;
-            }
-        }
-
-        uint64 GetData64(uint32 data) const
-        {
-            switch (data)
-            {
-                case DATA_ARGALOTH:
-                    return _argalothGUID;
-                case DATA_OCCUTHAR:
-                    return _occutharGUID;
-                case DATA_ALIZABAL:
-                    return _alizabalGUID;
-                default:
-                    break;
+                ArgalothGUID = 0;
+                OccutharGUID = 0;
+                AlizabalGUID = 0;
+                ArgalothDoor = 0;
+                OccutharDoor = 0;
             }
 
-            return 0;
-        }
-
-        void OnGameObjectRemove(GameObject* go)
-        {
-            switch(go->GetEntry())
+            void OnCreatureCreate(Creature* creature)
             {
-                case GO_ARGALOTH_DOOR:
-                    AddDoor(go, false);
-                    break;
-                case GO_OCCUTHAR_DOOR:
-                    AddDoor(go, false);
-                    break;
-            }
-        }
-
-        std::string GetSaveData()
-        {
-            OUT_SAVE_INST_DATA;
-
-            std::ostringstream saveStream;
-            saveStream << "B H " << GetBossSaveData();
-
-            OUT_SAVE_INST_DATA_COMPLETE;
-            return saveStream.str();
-        }
-
-        void Load(const char* in)
-        {
-            if (!in)
-            {
-                OUT_LOAD_INST_DATA_FAIL;
-                return;
-            }
-
-            OUT_LOAD_INST_DATA(in);
-
-            char dataHead1, dataHead2;
-
-            std::istringstream loadStream(in);
-            loadStream >> dataHead1 >> dataHead2;
-
-            if (dataHead1 == 'B' && dataHead2 == 'H')
-            {
-                for (uint8 i = 0; i < MAX_ENCOUNTERS; ++i)
+                switch(creature->GetEntry())
                 {
-                    uint32 tmpState;
-                    loadStream >> tmpState;
-                    if (tmpState == IN_PROGRESS || tmpState > SPECIAL)
-                        tmpState = NOT_STARTED;
+                    case BOSS_ARGALOTH:
+                        ArgalothGUID = creature->GetGUID();
+                        break;
+                    case BOSS_OCCUTHAR:
+                        OccutharGUID = creature->GetGUID();
+                        break;
+                    case BOSS_ALIZABAL:
+                        AlizabalGUID = creature->GetGUID();
+                        break;
+                }
+            }
 
-                    SetBossState(i, EncounterState(tmpState));
+            void OnGameObjectCreate(GameObject* go)
+            {
+                switch(go->GetEntry())
+                {
+                    case GO_ARGALOTH_DOOR:
+                        ArgalothDoor = go->GetGUID();
+                        AddDoor(go, true);
+                        break;
+                    case GO_OCCUTHAR_DOOR:
+                        OccutharDoor = go->GetGUID();
+                        AddDoor(go, true);
+                        break;
+                }
+            }
+
+            uint64 GetData64(uint32 data) const
+            {
+                switch (data)
+                {
+                    case DATA_ARGALOTH:
+                        return ArgalothGUID;
+                    case DATA_OCCUTHAR:
+                        return OccutharGUID;
+                    case DATA_ALIZABAL:
+                        return AlizabalGUID;
+                    default:
+                        break;
                 }
 
-            } else OUT_LOAD_INST_DATA_FAIL;
+                return 0;
+            }
 
-            OUT_LOAD_INST_DATA_COMPLETE;
+            void OnGameObjectRemove(GameObject* go)
+            {
+                switch(go->GetEntry())
+                {
+                    case GO_ARGALOTH_DOOR:
+                        AddDoor(go, false);
+                        break;
+                    case GO_OCCUTHAR_DOOR:
+                        AddDoor(go, false);
+                        break;
+                }
+            }
+
+            std::string GetSaveData()
+            {
+                OUT_SAVE_INST_DATA;
+
+                std::ostringstream saveStream;
+                saveStream << "B H " << GetBossSaveData();
+
+                OUT_SAVE_INST_DATA_COMPLETE;
+                return saveStream.str();
+            }
+
+            void Load(const char* in)
+            {
+                if (!in)
+                {
+                    OUT_LOAD_INST_DATA_FAIL;
+                    return;
+                }
+
+                OUT_LOAD_INST_DATA(in);
+
+                char dataHead1, dataHead2;
+
+                std::istringstream loadStream(in);
+                loadStream >> dataHead1 >> dataHead2;
+
+                if (dataHead1 == 'B' && dataHead2 == 'H')
+                {
+                    for (uint8 i = 0; i < EncounterCount; ++i)
+                    {
+                        uint32 tmpState;
+                        loadStream >> tmpState;
+                        if (tmpState == IN_PROGRESS || tmpState > SPECIAL)
+                            tmpState = NOT_STARTED;
+
+                        SetBossState(i, EncounterState(tmpState));
+                    }
+
+                }
+                else
+                    OUT_LOAD_INST_DATA_FAIL;
+
+                OUT_LOAD_INST_DATA_COMPLETE;
+            }
+
+        protected:
+            uint64 ArgalothGUID;
+            uint64 OccutharGUID;
+            uint64 AlizabalGUID;
+            uint64 ArgalothDoor;
+            uint64 OccutharDoor;
+        };
+
+        InstanceScript* GetInstanceScript(InstanceMap* map) const
+        {
+            return new instance_baradin_hold_InstanceMapScript(map);
         }
-
-    private:
-        uint64 _argalothGUID;
-        uint64 _occutharGUID;
-        uint64 _alizabalGUID;
-        uint64 _argalothDoor;
-        uint64 _occutharDoor;
-    };
 };
 
 void AddSC_instance_baradin_hold()
