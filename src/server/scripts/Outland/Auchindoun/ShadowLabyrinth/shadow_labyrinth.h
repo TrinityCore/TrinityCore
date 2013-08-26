3a45f0ef942a1087f1b4644fc60ee4c8d3324b45
joschiwald <joschiwald@online.de>
2013-08-26 18:52:29 +0200

    Scripts/Auchindoun: add missing InstanceScripts and cleanup some boss scripts

--- src/server/scripts/Outland/Auchindoun/ShadowLabyrinth/shadow_labyrinth.h
+++ src/server/scripts/Outland/Auchindoun/ShadowLabyrinth/shadow_labyrinth.h
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) 2008-2013 TrinityCore <http://www.trinitycore.org/>
- * Copyright (C) 2006-2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -16,26 +15,49 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef DEF_SHADOW_LABYRINTH_H
-#define DEF_SHADOW_LABYRINTH_H
+#ifndef SHADOW_LABYRINTH_H_
+#define SHADOW_LABYRINTH_H_
 
-uint32 const EncounterCount             = 5;
+#define SLScriptName "instance_shadow_labyrinth"
+
+uint32 const EncounterCount = 4;
 
 enum DataTypes
 {
-    TYPE_HELLMAW                        = 1,
-    TYPE_OVERSEER                       = 2,
-    DATA_BLACKHEARTTHEINCITEREVENT      = 3,
-    DATA_GRANDMASTERVORPILEVENT         = 4,
-    DATA_MURMUREVENT                    = 5,
-    DATA_GRANDMASTERVORPIL              = 6
+    // Encounter States/Boss GUIDs
+    DATA_AMBASSADOR_HELLMAW             = 0,
+    DATA_BLACKHEART_THE_INCITER         = 1,
+    DATA_GRANDMASTER_VORPIL             = 2,
+    DATA_MURMUR                         = 3,
+
+    // Additional Data
+    DATA_FEL_OVERSEER                   = 4
 };
 
-enum Objects
+enum CreatureIds
 {
-    REFECTORY_DOOR                      = 183296, // door opened when blackheart the inciter dies
-    SCREAMING_HALL_DOOR                 = 183295  // door opened when grandmaster vorpil dies
+    NPC_AMBASSADOR_HELLMAW              = 18731,
+    NPC_GRANDMASTER_VORPIL              = 18732,
+    NPC_FEL_OVERSEER                    = 18796
 };
 
-#endif
+enum GameObjectIds
+{
+    GO_REFECTORY_DOOR                   = 183296, // door opened when blackheart the inciter dies
+    GO_SCREAMING_HALL_DOOR              = 183295  // door opened when grandmaster vorpil dies
+};
+
+enum Misc
+{
+    ACTION_AMBASSADOR_HELLMAW_INTRO     = 1,
+    ACTION_AMBASSADOR_HELLMAW_BANISH    = 2,
+};
+
+template<class AI>
+AI* GetShadowLabyrinthAI(Creature* creature)
+{
+    return GetInstanceAI<AI>(creature, SLScriptName);
+}
+
+#endif // SHADOW_LABYRINTH_H_
 
