67a2b159ce99510af73e679bb73ed5eaefcdc098
Vincent-Michael <Vincent_Michael@gmx.de>
2013-08-29 21:28:32 +0200

    Core/Quest: Fix typo in 2f78bc2b5da8e7ff61740dc1e5406f7554eb02d1

--- src/server/game/Quests/QuestDef.h
+++ src/server/game/Quests/QuestDef.h
@@ -78,7 +78,7 @@ enum QuestShareMessages
     QUEST_PARTY_MSG_NOT_IN_PARTY            = 10
 };
 
-enum __QuestTradeSkill
+enum QuestTradeSkill
 {
     QUEST_TRSKILL_NONE           = 0,
     QUEST_TRSKILL_ALCHEMY        = 1,
@@ -109,7 +109,7 @@ enum QuestStatus
     MAX_QUEST_STATUS
 };
 
-enum __QuestGiverStatus
+enum QuestGiverStatus
 {
     DIALOG_STATUS_NONE                     = 0x000,
     DIALOG_STATUS_UNK                      = 0x001,
@@ -156,7 +156,7 @@ enum QuestFlags
     // ... 4.x added flags up to 0x80000000 - all unknown for now
 };
 
-enum __QuestSpecialFlags
+enum QuestSpecialFlags
 {
     QUEST_SPECIAL_FLAGS_NONE                 = 0x000,
     // Trinity flags for set SpecialFlags in DB if required but used only at server
