62d1ee680b5cd7fc1053527332c3859cf621c727
Nay <dnpd.dd@gmail.com>
2013-08-29 15:36:38 +0100

    Core/Quests: Drop world.quest_template.RequiredSpellCastX
    These were non-wdb fields that didn't have much use.
    Most quests that were using these fields (not many) should still work,
    however, if you find any quest that worked before and is now broken
    please report it in the issue tracker and it will be promptly fixed.

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -16267,10 +16267,6 @@ void Player::KilledMonsterCredit(uint32 entry, uint64 guid /*= 0*/)
                     if (qInfo->RequiredNpcOrGo[j] <= 0)
                         continue;
 
-                    // skip Cast at creature objective
-                    if (qInfo->RequiredSpellCast[j] != 0)
-                        continue;
-
                     uint32 reqkill = qInfo->RequiredNpcOrGo[j];
 
                     if (reqkill == real_entry)
@@ -16360,10 +16356,6 @@ void Player::CastedCreatureOrGO(uint32 entry, uint64 guid, uint32 spell_id)
             {
                 for (uint8 j = 0; j < QUEST_OBJECTIVES_COUNT; ++j)
                 {
-                    // skip kill creature objective (0) or wrong spell casts
-                    if (qInfo->RequiredSpellCast[j] != spell_id)
-                        continue;
-
                     uint32 reqTarget = 0;
 
                     if (isCreature)
@@ -16443,8 +16435,8 @@ void Player::TalkedToCreature(uint32 entry, uint64 guid)
             {
                 for (uint8 j = 0; j < QUEST_OBJECTIVES_COUNT; ++j)
                 {
-                                                            // skip spell casts and Gameobject objectives
-                    if (qInfo->RequiredSpellCast[j] > 0 || qInfo->RequiredNpcOrGo[j] < 0)
+                    // skip gameobject objectives
+                    if (qInfo->RequiredNpcOrGo[j] < 0)
                         continue;
 
                     uint32 reqTarget = 0;
