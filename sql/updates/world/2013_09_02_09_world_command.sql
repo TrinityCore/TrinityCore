79f446364c7d7a73138547fa215b25df574f7a10
Aokromes <jipr@hotmail.com>
2013-09-02 16:29:25 +0200

    Fix SQL typo after RBAC commits

--- sql/updates/world/2013_09_02_09_world_command.sql
+++ sql/updates/world/2013_09_02_09_world_command.sql
@@ -9,4 +9,4 @@ UPDATE `command` SET `permission` = @id+2 WHERE `name` = 'guild delete';
 UPDATE `command` SET `permission` = @id+3 WHERE `name` = 'guild invite';
 UPDATE `command` SET `permission` = @id+4 WHERE `name` = 'guild uninvite';
 UPDATE `command` SET `permission` = @id+5 WHERE `name` = 'guild rank';
-UPDATE `command` SET `permission` = @id+6 WHERE `name` = 'guild rename');
+UPDATE `command` SET `permission` = @id+6 WHERE `name` = 'guild rename';
