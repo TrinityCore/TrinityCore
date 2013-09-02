7e5ea24e6edad9a0e462b618ff301e411981c484
Spp <spp@jorge.gr>
2013-09-02 13:19:59 +0200

    Compile Fix (and edit of last 2 sqls to add the missing permissions)

--- sql/updates/world/2013_09_02_15_world_command.sql
+++ sql/updates/world/2013_09_02_15_world_command.sql
@@ -1,6 +1,6 @@
 /* cs_lookup.cpp */
 
-SET @id = 436;
+SET @id = 442;
 
 -- Update command table with new RBAC permissions
 UPDATE `command` SET `permission` = @id+0 WHERE `name` = 'lookup';
@@ -13,9 +13,13 @@ UPDATE `command` SET `permission` = @id+6 WHERE `name` = 'lookup itemset';
 UPDATE `command` SET `permission` = @id+7 WHERE `name` = 'lookup object';
 UPDATE `command` SET `permission` = @id+8 WHERE `name` = 'lookup quest';
 UPDATE `command` SET `permission` = @id+9 WHERE `name` = 'lookup player';
-UPDATE `command` SET `permission` = @id+10 WHERE `name` = 'lookup skill';
-UPDATE `command` SET `permission` = @id+11 WHERE `name` = 'lookup spell';
-UPDATE `command` SET `permission` = @id+12 WHERE `name` = 'lookup taxinode';
-UPDATE `command` SET `permission` = @id+13 WHERE `name` = 'lookup tele';
-UPDATE `command` SET `permission` = @id+14 WHERE `name` = 'lookup title';
-UPDATE `command` SET `permission` = @id+15 WHERE `name` = 'lookup map');
+UPDATE `command` SET `permission` = @id+10 WHERE `name` = 'lookup player ip';
+UPDATE `command` SET `permission` = @id+11 WHERE `name` = 'lookup player account';
+UPDATE `command` SET `permission` = @id+12 WHERE `name` = 'lookup player email';
+UPDATE `command` SET `permission` = @id+13 WHERE `name` = 'lookup skill';
+UPDATE `command` SET `permission` = @id+14 WHERE `name` = 'lookup spell';
+UPDATE `command` SET `permission` = @id+15 WHERE `name` = 'lookup spell id';
+UPDATE `command` SET `permission` = @id+16 WHERE `name` = 'lookup taxinode';
+UPDATE `command` SET `permission` = @id+17 WHERE `name` = 'lookup tele';
+UPDATE `command` SET `permission` = @id+18 WHERE `name` = 'lookup title';
+UPDATE `command` SET `permission` = @id+19 WHERE `name` = 'lookup map');
