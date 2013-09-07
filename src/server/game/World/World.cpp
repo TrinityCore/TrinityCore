9578b5f87a4aa96e120b78f410ea90e141903dd7
jackpoz <giacomopoz@gmail.com>
2013-09-07 14:37:54 +0200

    Core/Thread: Fix race condition converting time values to local time
    Replace thread-unsafe localtime() http://www.cplusplus.com/reference/ctime/localtime/ with thread-safe portable ACE_OS::localtime_r() .
    
    Helgrind log:
     Possible data race during read of size 4 at 0x6F183C0 by thread #1
     Locks held: none
      at 0x14E72E3: World::InitDailyQuestResetTime() (World.cpp:2772)
      by 0x14E3A01: World::SetInitialWorldSettings() (World.cpp:1790)
      by 0x101122A: Master::Run() (Master.cpp:164)
      by 0x101740C: main (Main.cpp:142)
    
     This conflicts with a previous write of size 4 by thread #2
     Locks held: none
      at 0x6C2D3BA: __tzfile_compute (tzfile.c:797)
      by 0x6C2D036: __tz_convert (tzset.c:627)
      by 0x164146C: LogMessage::getTimeStr(long) (Appender.cpp:23)
      by 0x1641550: LogMessage::getTimeStr() (Appender.cpp:31)
      by 0x1641722: Appender::write(LogMessage&) (Appender.cpp:80)
      by 0x1633FCE: Logger::write(LogMessage&) (Logger.cpp:83)
      by 0x16433D8: LogOperation::call() (LogOperation.cpp:29)
      by 0x16428A4: LogWorker::svc() (LogWorker.cpp:45)

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -1730,7 +1730,9 @@ void World::SetInitialWorldSettings()
     //mailtimer is increased when updating auctions
     //one second is 1000 -(tested on win system)
     /// @todo Get rid of magic numbers
-    mail_timer = ((((localtime(&m_gameTime)->tm_hour + 20) % 24)* HOUR * IN_MILLISECONDS) / m_timers[WUPDATE_AUCTIONS].GetInterval());
+    tm localTm;
+    ACE_OS::localtime_r(&m_gameTime, &localTm);
+    mail_timer = ((((localTm.tm_hour + 20) % 24)* HOUR * IN_MILLISECONDS) / m_timers[WUPDATE_AUCTIONS].GetInterval());
                                                             //1440
     mail_timer_expires = ((DAY * IN_MILLISECONDS) / (m_timers[WUPDATE_AUCTIONS].GetInterval()));
     TC_LOG_INFO(LOG_FILTER_SERVER_LOADING, "Mail timer set to: " UI64FMTD ", mail return is called every " UI64FMTD " minutes", uint64(mail_timer), uint64(mail_timer_expires));
@@ -2769,7 +2771,8 @@ void World::InitDailyQuestResetTime()
     // client built-in time for reset is 6:00 AM
     // FIX ME: client not show day start time
     time_t curTime = time(NULL);
-    tm localTm = *localtime(&curTime);
+    tm localTm;
+    ACE_OS::localtime_r(&curTime, &localTm);
     localTm.tm_hour = 6;
     localTm.tm_min  = 0;
     localTm.tm_sec  = 0;
@@ -2802,7 +2805,8 @@ void World::InitRandomBGResetTime()
 
     // generate time by config
     time_t curTime = time(NULL);
-    tm localTm = *localtime(&curTime);
+    tm localTm;
+    ACE_OS::localtime_r(&curTime, &localTm);
     localTm.tm_hour = getIntConfig(CONFIG_RANDOM_BG_RESET_HOUR);
     localTm.tm_min = 0;
     localTm.tm_sec = 0;
@@ -2829,7 +2833,8 @@ void World::InitGuildResetTime()
 
     // generate time by config
     time_t curTime = time(NULL);
-    tm localTm = *localtime(&curTime);
+    tm localTm;
+    ACE_OS::localtime_r(&curTime, &localTm);
     localTm.tm_hour = getIntConfig(CONFIG_GUILD_RESET_HOUR);
     localTm.tm_min = 0;
     localTm.tm_sec = 0;
@@ -2913,7 +2918,8 @@ void World::ResetMonthlyQuests()
 
     // generate time
     time_t curTime = time(NULL);
-    tm localTm = *localtime(&curTime);
+    tm localTm;
+    ACE_OS::localtime_r(&curTime, &localTm);
 
     int month   = localTm.tm_mon;
     int year    = localTm.tm_year;
