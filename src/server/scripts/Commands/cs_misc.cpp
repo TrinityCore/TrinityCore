83cb44d79f9fb266d426b251a96dd7053da0187f
Spp <spp@jorge.gr>
2013-09-10 12:07:03 +0200

    Core/RBAC: Move pet commands to RBAC (using individual permissions) - Pet commands moved from cs_misc.cpp to own file

--- src/server/scripts/Commands/cs_misc.cpp
+++ src/server/scripts/Commands/cs_misc.cpp
@@ -44,13 +44,6 @@ public:
 
     ChatCommand* GetCommands() const OVERRIDE
     {
-        static ChatCommand petCommandTable[] =
-        {
-            { "create",             RBAC_PERM_GAMEMASTER_COMMANDS,         false, &HandleCreatePetCommand,             "", NULL },
-            { "learn",              RBAC_PERM_GAMEMASTER_COMMANDS,         false, &HandlePetLearnCommand,              "", NULL },
-            { "unlearn",            RBAC_PERM_GAMEMASTER_COMMANDS,         false, &HandlePetUnlearnCommand,            "", NULL },
-            { NULL,                 0,                      false, NULL,                                "", NULL }
-        };
         static ChatCommand sendCommandTable[] =
         {
             { "items",              RBAC_PERM_ADMINISTRATOR_COMMANDS,      true,  &HandleSendItemsCommand,             "", NULL },
@@ -94,7 +87,6 @@ public:
             { "pinfo",              RBAC_PERM_GAMEMASTER_COMMANDS,         true,  &HandlePInfoCommand,                 "", NULL },
             { "respawn",            RBAC_PERM_ADMINISTRATOR_COMMANDS,      false, &HandleRespawnCommand,               "", NULL },
             { "send",               RBAC_PERM_MODERATOR_COMMANDS,          true,  NULL,                                "", sendCommandTable },
-            { "pet",                RBAC_PERM_GAMEMASTER_COMMANDS,         false, NULL,                                "", petCommandTable },
             { "mute",               RBAC_PERM_MODERATOR_COMMANDS,          true,  &HandleMuteCommand,                  "", NULL },
             { "unmute",             RBAC_PERM_MODERATOR_COMMANDS,          true,  &HandleUnmuteCommand,                "", NULL },
             { "movegens",           RBAC_PERM_ADMINISTRATOR_COMMANDS,      false, &HandleMovegensCommand,              "", NULL },
@@ -2487,145 +2479,6 @@ public:
         // Confirmation message
         std::string nameLink = handler->GetNameLink(player);
         handler->PSendSysMessage(LANG_SENDMESSAGE, nameLink.c_str(), msgStr);
-
-        return true;
-    }
-
-    static bool HandleCreatePetCommand(ChatHandler* handler, char const* /*args*/)
-    {
-        Player* player = handler->GetSession()->GetPlayer();
-        Creature* creatureTarget = handler->getSelectedCreature();
-
-        if (!creatureTarget || creatureTarget->IsPet() || creatureTarget->GetTypeId() == TYPEID_PLAYER)
-        {
-            handler->PSendSysMessage(LANG_SELECT_CREATURE);
-            handler->SetSentErrorMessage(true);
-            return false;
-        }
-
-        CreatureTemplate const* creatrueTemplate = sObjectMgr->GetCreatureTemplate(creatureTarget->GetEntry());
-        // Creatures with family 0 crashes the server
-        if (!creatrueTemplate->family)
-        {
-            handler->PSendSysMessage("This creature cannot be tamed. (family id: 0).");
-            handler->SetSentErrorMessage(true);
-            return false;
-        }
-
-        if (player->GetPetGUID())
-        {
-            handler->PSendSysMessage("You already have a pet");
-            handler->SetSentErrorMessage(true);
-            return false;
-        }
-
-        // Everything looks OK, create new pet
-        Pet* pet = new Pet(player, HUNTER_PET);
-        if (!pet->CreateBaseAtCreature(creatureTarget))
-        {
-            delete pet;
-            handler->PSendSysMessage("Error 1");
-            return false;
-        }
-
-        creatureTarget->setDeathState(JUST_DIED);
-        creatureTarget->RemoveCorpse();
-        creatureTarget->SetHealth(0); // just for nice GM-mode view
-
-        pet->SetUInt64Value(UNIT_FIELD_CREATEDBY, player->GetGUID());
-        pet->SetUInt32Value(UNIT_FIELD_FACTIONTEMPLATE, player->getFaction());
-
-        if (!pet->InitStatsForLevel(creatureTarget->getLevel()))
-        {
-            TC_LOG_ERROR(LOG_FILTER_GENERAL, "InitStatsForLevel() in EffectTameCreature failed! Pet deleted.");
-            handler->PSendSysMessage("Error 2");
-            delete pet;
-            return false;
-        }
-
-        // prepare visual effect for levelup
-        pet->SetUInt32Value(UNIT_FIELD_LEVEL, creatureTarget->getLevel()-1);
-
-        pet->GetCharmInfo()->SetPetNumber(sObjectMgr->GeneratePetNumber(), true);
-        // this enables pet details window (Shift+P)
-        pet->InitPetCreateSpells();
-        pet->SetFullHealth();
-
-        pet->GetMap()->AddToMap(pet->ToCreature());
-
-        // visual effect for levelup
-        pet->SetUInt32Value(UNIT_FIELD_LEVEL, creatureTarget->getLevel());
-
-        player->SetMinion(pet, true);
-        pet->SavePetToDB(PET_SAVE_AS_CURRENT);
-        player->PetSpellInitialize();
-
-        return true;
-    }
-
-    static bool HandlePetLearnCommand(ChatHandler* handler, char const* args)
-    {
-        if (!*args)
-            return false;
-
-        Player* player = handler->GetSession()->GetPlayer();
-        Pet* pet = player->GetPet();
-
-        if (!pet)
-        {
-            handler->PSendSysMessage("You have no pet");
-            handler->SetSentErrorMessage(true);
-            return false;
-        }
-
-        uint32 spellId = handler->extractSpellIdFromLink((char*)args);
-
-        if (!spellId || !sSpellMgr->GetSpellInfo(spellId))
-            return false;
-
-        // Check if pet already has it
-        if (pet->HasSpell(spellId))
-        {
-            handler->PSendSysMessage("Pet already has spell: %u", spellId);
-            handler->SetSentErrorMessage(true);
-            return false;
-        }
-
-        // Check if spell is valid
-        SpellInfo const* spellInfo = sSpellMgr->GetSpellInfo(spellId);
-        if (!spellInfo || !SpellMgr::IsSpellValid(spellInfo))
-        {
-            handler->PSendSysMessage(LANG_COMMAND_SPELL_BROKEN, spellId);
-            handler->SetSentErrorMessage(true);
-            return false;
-        }
-
-        pet->learnSpell(spellId);
-
-        handler->PSendSysMessage("Pet has learned spell %u", spellId);
-        return true;
-    }
-
-    static bool HandlePetUnlearnCommand(ChatHandler* handler, char const* args)
-    {
-        if (!*args)
-            return false;
-
-        Player* player = handler->GetSession()->GetPlayer();
-        Pet* pet = player->GetPet();
-        if (!pet)
-        {
-            handler->PSendSysMessage("You have no pet");
-            handler->SetSentErrorMessage(true);
-            return false;
-        }
-
-        uint32 spellId = handler->extractSpellIdFromLink((char*)args);
-
-        if (pet->HasSpell(spellId))
-            pet->removeSpell(spellId, false);
-        else
-            handler->PSendSysMessage("Pet doesn't have that spell");
 
         return true;
     }
