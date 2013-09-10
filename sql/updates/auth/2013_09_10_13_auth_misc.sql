489012b08f995f846f46c1f03dc9b178c6122a34
Spp <spp@jorge.gr>
2013-09-10 15:47:17 +0200

    Core/RBAC: Fix typos in recent SQL files

--- sql/updates/auth/2013_09_10_13_auth_misc.sql
+++ sql/updates/auth/2013_09_10_13_auth_misc.sql
@@ -11,14 +11,14 @@ INSERT INTO `rbac_permissions` (`id`, `name`) VALUES
 (@id+3, 'titles remove'),
 (@id+4, 'titles set'),
 (@id+5, 'titles set mask'),
-(@id+6, 'titles wp'),
-(@id+7, 'titles wp add'),
-(@id+8, 'titles wp event'),
-(@id+9, 'titles wp load'),
-(@id+10, 'titles wp modify'),
-(@id+11, 'titles wp unload'),
-(@id+12, 'titles wp reload'),
-(@id+13, 'titles wp show');
+(@id+6, 'wp'),
+(@id+7, 'wp add'),
+(@id+8, 'wp event'),
+(@id+9, 'wp load'),
+(@id+10, 'wp modify'),
+(@id+11, 'wp unload'),
+(@id+12, 'wp reload'),
+(@id+13, 'wp show');
 
 -- Add permissions to "corresponding Commands Role"
 DELETE FROM `rbac_role_permissions` WHERE `permissionId` BETWEEN @id AND @id+13;
