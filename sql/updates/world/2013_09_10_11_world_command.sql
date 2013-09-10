abbc6b2760a0f8117761e303c999f375c9ab4317
Spp <spp@jorge.gr>
2013-09-10 14:57:01 +0200

    Core/RBAC: Move server commands to RBAC (using individual permissions)

--- sql/updates/world/2013_09_10_11_world_command.sql
+++ sql/updates/world/2013_09_10_11_world_command.sql
@@ -3,11 +3,11 @@
 SET @id = 710;
 
 -- Update command table with new RBAC permissions
-UPDATE `command` SET `permission` = @id+0 WHERE `name` = 'reload';
-UPDATE `command` SET `permission` = @id+1 WHERE `name` = 'reload achievements';
-UPDATE `command` SET `permission` = @id+2 WHERE `name` = 'reload honor';
-UPDATE `command` SET `permission` = @id+3 WHERE `name` = 'reload level';
-UPDATE `command` SET `permission` = @id+4 WHERE `name` = 'reload spells';
-UPDATE `command` SET `permission` = @id+5 WHERE `name` = 'reload stats';
-UPDATE `command` SET `permission` = @id+6 WHERE `name` = 'reload talents';
-UPDATE `command` SET `permission` = @id+7 WHERE `name` = 'reload all';
+UPDATE `command` SET `permission` = @id+0 WHERE `name` = 'reset';
+UPDATE `command` SET `permission` = @id+1 WHERE `name` = 'reset achievements';
+UPDATE `command` SET `permission` = @id+2 WHERE `name` = 'reset honor';
+UPDATE `command` SET `permission` = @id+3 WHERE `name` = 'reset level';
+UPDATE `command` SET `permission` = @id+4 WHERE `name` = 'reset spells';
+UPDATE `command` SET `permission` = @id+5 WHERE `name` = 'reset stats';
+UPDATE `command` SET `permission` = @id+6 WHERE `name` = 'reset talents';
+UPDATE `command` SET `permission` = @id+7 WHERE `name` = 'reset all';
