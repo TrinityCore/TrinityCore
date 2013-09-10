0ed83024e0c4771f82096dcb9da032c7adcdad53
Spp <spp@jorge.gr>
2013-09-10 15:47:17 +0200

    Core/RBAC: Fix typos in recent SQL files

--- sql/updates/world/2013_09_10_15_world_command.sql
+++ sql/updates/world/2013_09_10_15_world_command.sql
@@ -3,22 +3,17 @@
 SET @id = 761;
 
 -- Update command table with new RBAC permissions
-UPDATE `command` SET `permission` = @id+0 WHERE `name` = 'ticket';
-UPDATE `command` SET `permission` = @id+1 WHERE `name` = 'ticket assign';
-UPDATE `command` SET `permission` = @id+2 WHERE `name` = 'ticket close';
-UPDATE `command` SET `permission` = @id+3 WHERE `name` = 'ticket closedlist';
-UPDATE `command` SET `permission` = @id+4 WHERE `name` = 'ticket comment';
-UPDATE `command` SET `permission` = @id+5 WHERE `name` = 'ticket complete';
-UPDATE `command` SET `permission` = @id+6 WHERE `name` = 'ticket delete';
-UPDATE `command` SET `permission` = @id+7 WHERE `name` = 'ticket escalate';
-UPDATE `command` SET `permission` = @id+8 WHERE `name` = 'ticket escalatedlist';
-UPDATE `command` SET `permission` = @id+9 WHERE `name` = 'ticket list';
-UPDATE `command` SET `permission` = @id+10 WHERE `name` = 'ticket onlinelist';
-UPDATE `command` SET `permission` = @id+11 WHERE `name` = 'ticket reset';
-UPDATE `command` SET `permission` = @id+12 WHERE `name` = 'ticket response';
-UPDATE `command` SET `permission` = @id+13 WHERE `name` = 'ticket response append';
-UPDATE `command` SET `permission` = @id+14 WHERE `name` = 'ticket response appendln';
-UPDATE `command` SET `permission` = @id+15 WHERE `name` = 'ticket togglesystem';
-UPDATE `command` SET `permission` = @id+16 WHERE `name` = 'ticket unassign';
-UPDATE `command` SET `permission` = @id+17 WHERE `name` = 'ticket viewid';
-UPDATE `command` SET `permission` = @id+18 WHERE `name` = 'ticket viewname';
+UPDATE `command` SET `permission` = @id+0 WHERE `name` = 'titles';
+UPDATE `command` SET `permission` = @id+1 WHERE `name` = 'titles add';
+UPDATE `command` SET `permission` = @id+2 WHERE `name` = 'titles current';
+UPDATE `command` SET `permission` = @id+3 WHERE `name` = 'titles remove';
+UPDATE `command` SET `permission` = @id+4 WHERE `name` = 'titles set';
+UPDATE `command` SET `permission` = @id+5 WHERE `name` = 'titles set mask';
+UPDATE `command` SET `permission` = @id+6 WHERE `name` = 'wp';
+UPDATE `command` SET `permission` = @id+7 WHERE `name` = 'wp add';
+UPDATE `command` SET `permission` = @id+8 WHERE `name` = 'wp event';
+UPDATE `command` SET `permission` = @id+9 WHERE `name` = 'wp load';
+UPDATE `command` SET `permission` = @id+10 WHERE `name` = 'wp modify';
+UPDATE `command` SET `permission` = @id+11 WHERE `name` = 'wp unload';
+UPDATE `command` SET `permission` = @id+12 WHERE `name` = 'wp reload';
+UPDATE `command` SET `permission` = @id+13 WHERE `name` = 'wp show';
