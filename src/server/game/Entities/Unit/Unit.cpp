7f3240677a6ffd57eb3286993d0202c70c4fd6bc
Warpten <vertozor@gmail.com>
2013-09-19 17:39:09 +0200

    Core/Units: Fixed Moonkin forms for Trolls and Worgens, as well as epic Flight forms.
    Also (hack?)fixed Glyph of Arctic Wolf.

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -14931,8 +14931,18 @@ uint32 Unit::GetModelForForm(ShapeshiftForm form) const
                 return 20872;
             case FORM_FLIGHT_EPIC:
                 if (Player::TeamForRace(getRace()) == ALLIANCE)
-                    return 21243;
+                    return (getRace() == RACE_WORGEN ? 37729 : 21243);
+                if (getRace() == RACE_TROLL)
+                    return 37730;
                 return 21244;
+            case FORM_MOONKIN:
+                if (getRace() == RACE_TROLL)
+                    return 37174;
+                if (getRace() == RACE_WORGEN)
+                    return 37173;
+            case FORM_GHOSTWOLF:
+                if (HasAura(58135)) //! Glyph of Arctic Wolf
+                    return 27312;
             default:
                 break;
         }
