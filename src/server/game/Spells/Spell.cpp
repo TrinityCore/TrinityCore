2f4be6cbe156859bf38981438a272dd37eb42a17
dreamwowcom <soporte@dream-wow.com>
2013-09-19 16:30:48 +0200

    Fix correcto para 337f47be2f7d83d42da0bd819c97b926ffbfc71d

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -1449,9 +1449,9 @@ void Spell::SelectImplicitCasterDestTargets(SpellEffIndex effIndex, SpellImplici
 
 void Spell::SelectImplicitTargetDestTargets(SpellEffIndex effIndex, SpellImplicitTargetInfo const& targetType)
 {
+    ASSERT(m_targets.GetObjectTarget() && "Spell::SelectImplicitTargetDestTargets - no explicit object target available!");
     WorldObject* target = m_targets.GetObjectTarget();
-    if (!target)
-        return;
+    
     switch (targetType.GetTarget())
     {
         case TARGET_DEST_TARGET_ENEMY:
