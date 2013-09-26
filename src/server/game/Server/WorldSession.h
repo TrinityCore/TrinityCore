fceadc942d43436945cff3c7c3dfd67ae0e36dce
jackpoz <giacomopoz@gmail.com>
2013-09-26 20:03:39 +0200

    Core/Movement: Port Ascent r1637 http://sourceforge.net/p/ascentemubackup/code/1637/ to TrinityCore
    Port some movement packet fixes, taking into account client/server connection lag.
    Credits to original author, check http://sourceforge.net/p/ascentemubackup/code/1637/ for more details.
    MOVEMENT_PACKET_TIME_DELAY default value is currently set to 0, feel free to set it to different values and report feedback.

--- src/server/game/Server/WorldSession.h
+++ src/server/game/Server/WorldSession.h
@@ -352,6 +352,7 @@ class WorldSession
 
         uint32 GetLatency() const { return m_latency; }
         void SetLatency(uint32 latency) { m_latency = latency; }
+        void ResetClientTimeDelay() { m_clientTimeDelay = 0; }
         uint32 getDialogStatus(Player* player, Object* questgiver, uint32 defstatus);
 
         time_t m_timeOutTime;
@@ -987,6 +988,7 @@ class WorldSession
         LocaleConstant m_sessionDbcLocale;
         LocaleConstant m_sessionDbLocaleIndex;
         uint32 m_latency;
+        uint32 m_clientTimeDelay;
         AccountData m_accountData[NUM_ACCOUNT_DATA_TYPES];
         uint32 m_Tutorials[MAX_ACCOUNT_TUTORIAL_VALUES];
         bool   m_TutorialsChanged;
