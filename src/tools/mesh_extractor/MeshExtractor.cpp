3ed0a11679a97aba9c88fde38a431aa3878bb759
Sebastian Valle <s.v.h21@hotmail.com>
2013-09-30 21:08:33 -0500

    Tools/MeshExtractor: Do not delete the polygons before we are done using them.

--- src/tools/mesh_extractor/MeshExtractor.cpp
+++ src/tools/mesh_extractor/MeshExtractor.cpp
@@ -378,8 +378,8 @@ int main(int argc, char* argv[])
 
     if (extractFlags & Constants::EXTRACT_FLAG_TEST)
     {
-        float start[] = { -230.133f, 191.085f, -24.9191f };
-        float end[] = { -396.309f, 150.828f, 7.82184f };
+        float start[] = { -44.641f, -34.606f, -1.045f };
+        float end[] = { -66.18f, 20.222f, -1.128f };
 
         //
         float m_spos[3];
@@ -408,7 +408,7 @@ int main(int argc, char* argv[])
         dtPolyRef m_startRef;
         dtPolyRef m_endRef;
 
-        FILE* mmap = fopen("mmaps/389.mmap", "rb");
+        FILE* mmap = fopen("mmaps/555.mmap", "rb");
         dtNavMeshParams params;
         int count = fread(&params, sizeof(dtNavMeshParams), 1, mmap);
         fclose(mmap);
@@ -427,7 +427,7 @@ int main(int argc, char* argv[])
             for (int j = 0; j <= 32; ++j)
             {
                 char buff[100];
-                sprintf(buff, "mmaps/389%02i%02i.mmtile", i, j);
+                sprintf(buff, "mmaps/555%02i%02i.mmtile", i, j);
                 LoadTile(navMesh, buff);
             }
         }
