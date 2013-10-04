e62f64b5912fb1b249680d0bd026b8e4c7d7a022
Sebastian Valle <s.v.h21@hotmail.com>
2013-10-04 10:25:44 -0500

    Tools/MeshExtractor: Fixed loading of locale patch MPQs (This fixes DBC extraction)

--- src/tools/mesh_extractor/MPQManager.cpp
+++ src/tools/mesh_extractor/MPQManager.cpp
@@ -14,6 +14,12 @@ char const* MPQManager::Files[] = {
     "patch-3.MPQ"
 };
 
+char const* MPQManager::LocalePatchFiles[] = {
+    "Data/%s/patch-%s.MPQ",
+    "Data/%s/patch-%s-2.MPQ",
+    "Data/%s/patch-%s-3.MPQ"
+};
+
 char const* MPQManager::Languages[] = { "enGB", "enUS", "deDE", "esES", "frFR", "koKR", "zhCN", "zhTW", "enCN", "enTW", "esMX", "ruRU" };
 
 void MPQManager::Initialize()
@@ -31,7 +37,6 @@ void MPQManager::Initialize()
 void MPQManager::InitializeDBC()
 {
     BaseLocale = -1;
-    std::string fileName;
     uint32 size = sizeof(Languages) / sizeof(char*);
     MPQArchive* _baseLocale = NULL;
     for (uint32 i = 0; i < size; ++i)
@@ -41,20 +46,34 @@ void MPQManager::InitializeDBC()
         if (file)
         {
             if (BaseLocale == -1)
-            {
                 BaseLocale = i;
-                _baseLocale = new MPQArchive(_fileName.c_str());
-                fileName = _fileName;
-                LocaleFiles[i] = _baseLocale;
+
+            // Load the base locale file
+            MPQArchive* arch = new MPQArchive(_fileName.c_str());
+            LocaleFiles[i].push_front(arch);
+            
+            Archives.push_front(arch); // For lookup in GetFile
+
+            // Load the locale patches
+            for (uint32 j = 0; j < sizeof(LocalePatchFiles) / sizeof(char*); ++j)
+            {
+                char patchName[100];
+                sprintf(patchName, LocalePatchFiles[j], Languages[i], Languages[i]);
+                FILE* patch = fopen(patchName, "rb");
+                if (file)
+                {
+                    MPQArchive* archP = new MPQArchive(patchName);
+                    LocaleFiles[i].push_front(archP);
+                    Archives.push_front(archP); // For lookup in GetFile
+                    fclose(patch);
+                }
             }
-            else
-                LocaleFiles[i] = new MPQArchive(_fileName.c_str());
 
             AvailableLocales.insert(i);
             printf("Detected locale: %s\n", Languages[i]);
         }
     }
-    Archives.push_front(_baseLocale);
+
     if (BaseLocale == -1)
     {
         printf("No locale data detected. Please make sure that the executable is in the same folder as your WoW installation.\n");
@@ -77,6 +96,46 @@ DBC* MPQManager::GetDBC(const std::string& name )
 {
     std::string path = "DBFilesClient\\" + name + ".dbc";
     return new DBC(GetFile(path));
+}
+
+FILE* MPQManager::GetFileFromLocale( const std::string& path, uint32 locale )
+{
+    ACE_GUARD_RETURN(ACE_Thread_Mutex, g, mutex, NULL);
+    std::deque<MPQArchive*> files = LocaleFiles[locale];
+    FILE* ret = NULL;
+    for (std::deque<MPQArchive*>::iterator itr = files.begin(); itr != files.end(); ++itr)
+    {
+        mpq_archive* mpq_a = (*itr)->mpq_a;
+
+        uint32_t filenum;
+        if(libmpq__file_number(mpq_a, path.c_str(), &filenum))
+            continue;
+        libmpq__off_t transferred;
+        libmpq__off_t size = 0;
+        libmpq__file_unpacked_size(mpq_a, filenum, &size);
+
+        // HACK: in patch.mpq some files don't want to open and give 1 for filesize
+        if (size <= 1)
+            continue;
+
+        char* buffer = new char[size];
+
+        //libmpq_file_getdata
+        libmpq__file_read(mpq_a, filenum, (unsigned char*)buffer, size, &transferred);
+        
+        // Pack the return into a FILE stream
+        ret = tmpfile();
+        if (!ret)
+        {
+            printf("Could not create temporary file. Please run as Administrator or root\n");
+            exit(1);
+        }
+        fwrite(buffer, sizeof(uint8), size, ret);
+        fseek(ret, 0, SEEK_SET);
+        delete[] buffer;
+        break;
+    }
+    return ret;
 }
 
 FILE* MPQManager::GetFileFrom(const std::string& path, MPQArchive* file )
