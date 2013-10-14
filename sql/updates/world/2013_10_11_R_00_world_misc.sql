ad317f66bd4824747ac3bda1752940ca9fe5cfc6
d-sat <peter.schmidt640@yahoo.de>
2013-10-14 23:37:01 +0200

    Included Sniffs. Thanks to Aokromes.

--- sql/updates/world/2013_10_11_R_00_world_misc.sql
+++ sql/updates/world/2013_10_11_R_00_world_misc.sql
@@ -7,11 +7,12 @@ UPDATE `creature_template` SET `ScriptName` = 'npc_grauf' WHERE `entry` = 26893;
 DELETE FROM `creature_text` WHERE `entry`=26693 AND `groupid`=2 AND `type`=41;
 DELETE FROM `creature_text` WHERE `entry`=26693 AND `groupid`=4 AND `type`=41;
 INSERT INTO `creature_text` (`entry`, `groupid`, `id`, `text`, `type`, `language`, `probability`, `emote`, `duration`, `sound`, `comment`) VALUES
-(26893, 2, 0, '%s in within range of the harpoon launchers!', 41, 0, 100, 0, 0, 0, 'Grauf EMOTE_RANGE');
+(26893, 2, 0, '%s in within range of the harpoon launchers!', 41, 0, 100, 0, 0, 0, 'Grauf EMOTE_RANGE'),
+(26893, 1, 0, '%s takes a deep breath.', 41, 0, 100, 0, 0, 0, 'Grauf EMOTE_BREATH');
 
 DELETE FROM `spell_target_position` WHERE `id` = 61790;
 INSERT INTO `spell_target_position` (`id`, `effIndex`, `target_map`, `target_position_x`, `target_position_y`, `target_position_z`, `target_orientation`) VALUES 
-('61790', '0', '575', '480.84', '-511.95', '104.72', '2.71');
+('61790', '0', '575', '476.799', '-511.167', '104.723', '0');
 
 DELETE FROM `spelldifficulty_dbc` WHERE `spellid0` = 50228;
 INSERT INTO `spelldifficulty_dbc` (`id`, `spellid0`, `spellid1`, `spellid2`, `spellid3`) VALUES 
--- src/server/scripts/Northrend/UtgardeKeep/UtgardePinnacle/boss_skadi.cpp
+++ src/server/scripts/Northrend/UtgardeKeep/UtgardePinnacle/boss_skadi.cpp
@@ -25,29 +25,64 @@ enum Yells
 {
     SAY_AGGRO                           = 0,
     SAY_KILL                            = 1,
-    EMOTE_RANGE                         = 2,
     SAY_DEATH                           = 3,
     SAY_DRAKE_DEATH                     = 5,
-    //EMOTE_BREATH                        = 5,
-    SAY_DRAKE_BREATH                    = 6
+    SAY_DRAKE_BREATH                    = 6,
+    EMOTE_BREATH                        = 1,
+    EMOTE_RANGE                         = 2
 };
 
 static Position SpawnLoc = {468.931f, -513.555f, 104.723f, 0};
-static Position Location[]=
+
+static Position Location[] =
 {
-    // Boss
-    {341.740997f, -516.955017f, 104.66900f, 0}, // 0
-    {293.299f, -505.95f, 142.03f, 0},           // 1
-    {301.664f, -535.164f, 146.097f, 0},         // 2
-    {521.031006f, -544.667847f, 128.80064f, 0}, // 3
-    {477.311981f, -509.296814f, 104.72308f, 0}, // 4
-    {341.740997f, -516.955017f, 104.66900f, 0}, // 5
-    {341.740997f, -516.955017f, 104.66900f, 0}, // 6
-    {341.740997f, -516.955017f, 104.66900f, 0}, // 7
+    // Skadi
+    {343.0203f, -507.3246f, 104.567f,  0}, // 0 24:31
+    //Grauf
+    {310.2165f, -510.5302f, 120.5487f, 0}, // 1 24:36
+    {300.8685f, -520.3249f, 133.3664f, 0}, // 2
+    {298.3447f, -529.4818f, 137.2831f, 0}, // 3
+    {309.0959f, -540.3195f, 134.9774f, 0}, // 4
+    {323.6415f, -547.9715f, 130.3106f, 0}, // 5
+    {357.078f,  -549.1597f, 116.3105f, 0}, // 6
+    {401.2573f, -550.2768f, 114.9216f, 0}, // 7
+    {464.5062f, -555.9442f, 114.4494f, 0}, // 8
+    {496.192f,  -556.9628f, 114.8661f, 0}, // 9
+    {523.2011f, -548.9916f, 114.8661f, 0}, // 10
+    //
+    {523.2011f, -548.9916f, 114.8661f, 0}, // 11 24:52 -> within Range
+    //
+    {496.434f,  -517.578f,  120.0f,    0}, // 12 24:57
+    //
+    {496.434f,  -517.578f,  120.0f,    0}, // 13 25:02 -> breath
+    //
+    {453.1242f, -517.17f,   120.0273f, 0}, // 14 25:04
+    {388.3312f, -514.3768f, 121.1193f, 0}, // 15
+    {340.2752f, -512.0926f, 122.3138f, 0}, // 16
+    {313.0756f, -509.138f,  125.175f,  0}, // 17
+    {296.6964f, -522.6705f, 133.7026f, 0}, // 18
+    {301.2957f, -549.4583f, 137.4249f, 0}, // 19
+    {335.0746f, -552.1211f, 119.8416f, 0}, // 20
+    {397.6237f, -553.9203f, 119.8416f, 0}, // 21
+    {459.6321f, -558.0078f, 119.8416f, 0}, // 22
+    {505.5458f, -568.7815f, 119.8416f, 0}, // 23
+    {518.099f,  -560.8499f, 119.8416f, 0}, // 24
+    {520.4827f, -541.5633f, 119.8416f, 0}, // 25
+    //
+    {520.4827f, -541.5633f, 119.8416f, 0},// 26 25:29 -> range
+    //
+    {496.434f,  -517.578f,  120.0f,    0},// 27 25:33
+    //
+    {496.434f,  -517.578f,  120.0f,    0},// 28 25:37 -> breath
+    //
+    {496.434f,  -517.578f,  120.0f,    0},// 29 25:38
+    //
+    {496.434f,  -517.578f,  106.8871f, 0},// 30 25:38
     // Breach Zone
-    {485.4577f, -511.2515f, 115.3011f, 0},      //8
-    {435.1892f, -514.5232f, 118.6719f, 0},      //9
-    {413.9327f, -540.9407f, 138.2614f, 0},      //10
+    {485.4577f, -511.2515f, 115.3011f, 0},      //31
+    {435.1892f, -514.5232f, 118.6719f, 0},      //32
+    {413.9327f, -540.9407f, 138.2614f, 0}, //33
+    {341.740997f, -516.955017f, 104.66900f, 3.0f}     // 34 Grauf Spawn
 };
 
 enum Phases
@@ -75,9 +110,10 @@ enum Spells
     SPELL_RIDE_VEHICLE      = 61791,
     // PHASE_FLYING
     SPELL_HARPOON_DAMAGE    = 48642, 
-    SPELL_FREEZING_CLOUD    = 47579,
     SPELL_FREEZING_CLOUD_L  = 47590,
     SPELL_FREEZING_CLOUD_R  = 47592,
+    SPELL_FREEZING_CLOUD_1  = 47594,
+    SPELL_FREEZING_CLOUD_2  = 60020,
     SPELL_SKADI_TELEPORT    = 61790,
     // PHASE_GROUND
     SPELL_CRUSH             = 50234,
@@ -127,7 +163,7 @@ public:
             me->SetSpeed(MOVE_FLIGHT, 3.0f);
 
             if ((Unit::GetCreature(*me, instance->GetData64(DATA_NPC_GRAUF)) == NULL) && !me->IsMounted())
-                me->SummonCreature(NPC_GRAUF, Location[0].GetPositionX(), Location[0].GetPositionY(), Location[0].GetPositionZ(), 3.0f);
+                me->SummonCreature(NPC_GRAUF, Location[34]);
             if (instance)  
                 instance->DoStopTimedAchievement(ACHIEVEMENT_TIMED_TYPE_EVENT, ACHIEV_TIMED_START_EVENT);
         }
@@ -137,17 +173,18 @@ public:
             me->Dismount();
             me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE | UNIT_FLAG_NON_ATTACKABLE);
             if (!Unit::GetCreature(*me, instance->GetData64(DATA_NPC_GRAUF)))
-                me->SummonCreature(NPC_GRAUF, Location[0].GetPositionX(), Location[0].GetPositionY(), Location[0].GetPositionZ(), 3.0f);
+                me->SummonCreature(NPC_GRAUF, Location[34]);
         }
 
         void EnterCombat(Unit* /*who*/) OVERRIDE
         {
              _EnterCombat();
             Talk(SAY_AGGRO);
-            me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE | UNIT_FLAG_NON_ATTACKABLE);
+            me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE | UNIT_FLAG_IMMUNE_TO_PC | UNIT_FLAG_UNK_6);
             events.SetPhase(PHASE_INTRO);
             events.ScheduleEvent(EVENT_MOUNT, 1 * IN_MILLISECONDS);
-
+            events.SetPhase(PHASE_INTRO);
+            events.ScheduleEvent(EVENT_MOUNT, 1 * IN_MILLISECONDS);
             if (instance)
                 instance->DoStartTimedAchievement(ACHIEVEMENT_TIMED_TYPE_EVENT, ACHIEV_TIMED_START_EVENT);
         }
@@ -181,7 +218,9 @@ public:
                 events.ScheduleEvent(EVENT_CRUSH, 8000, 0, PHASE_GROUND);
                 events.ScheduleEvent(EVENT_POISONED_SPEAR, 10000, 0, PHASE_GROUND);
                 events.ScheduleEvent(EVENT_WHIRLWIND, 20000, 0, PHASE_GROUND);
-                me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE | UNIT_FLAG_NON_ATTACKABLE );
+                me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE | UNIT_FLAG_IMMUNE_TO_PC);
+                me->RemoveFlag(UNIT_FIELD_FLAGS_2, UNIT_FLAG2_PLAY_DEATH_ANIM);
+                me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_PET_IN_COMBAT);
             }
         }
 
@@ -197,10 +236,13 @@ public:
             _JustDied();
         }
 
-        void UpdateAI(uint32 diff) OVERRIDE
+        void JustDied(Unit* /*killer*/) OVERRIDE
         {
             events.Update(diff);
 
+            if((events.IsInPhase(PHASE_FLYING) || events.IsInPhase(PHASE_GROUND)) && !UpdateVictim())
+
+        void UpdateAI(uint32 diff) OVERRIDE
             if((events.IsInPhase(PHASE_FLYING) || events.IsInPhase(PHASE_GROUND)) && !UpdateVictim())
                 return;
 
@@ -213,6 +255,7 @@ public:
                 {
                 case EVENT_MOUNT:                    
                     me->CastSpell(me->GetUnit(*me, instance->GetData64(DATA_NPC_GRAUF)), SPELL_RIDE_VEHICLE);
+                    me->SetFlag(UNIT_FIELD_FLAGS_2, UNIT_FLAG2_PLAY_DEATH_ANIM | UNIT_FLAG2_REGENERATE_POWER);
                     events.SetPhase(PHASE_FLYING);
                     events.ScheduleEvent(EVENT_SUMMON, 10000, 0, PHASE_FLYING);
                     break;
@@ -230,6 +273,7 @@ public:
                     events.ScheduleEvent(EVENT_POISONED_SPEAR, 10000, 0, PHASE_GROUND);
                     break;
                 case EVENT_WHIRLWIND:
+                    me->AI()->SelectTarget(SELECT_TARGET_BOTTOMAGGRO);
                     DoCastAOE(SPELL_WHIRLWIND);
                     events.ScheduleEvent(EVENT_WHIRLWIND, 23000, 0, PHASE_GROUND);
                     break;                        
@@ -257,6 +301,7 @@ public:
         } 
     };
     CreatureAI* GetAI(Creature* creature) const OVERRIDE
+};
     {
         return new boss_skadiAI(creature);
     }
@@ -266,13 +311,25 @@ class npc_grauf : public CreatureScript
 {
 public:
     npc_grauf() : CreatureScript("npc_grauf") { }
+        }
 
     struct npc_graufAI : public ScriptedAI
+        EventMap events;
+        uint32 m_uiWaypointId;
+        std::list<Creature*> CloudTriggerList;
+        uint8 harpoonsHit;
+
+        void Reset() OVERRIDE
     {
         npc_graufAI(Creature* creature) : ScriptedAI(creature)
         {
             SetCombatMovement(false);
             instance = me->GetInstanceScript();
+            
+            if(Creature* pSkadi = Unit::GetCreature(*me, DATA_SKADI_THE_RUTHLESS))
+            {
+                pSkadi->AI()->EnterEvadeMode();
+            me->SetCanFly(false);
         }
 
         InstanceScript* instance;
@@ -280,29 +337,13 @@ public:
         uint32 m_uiWaypointId;
         std::list<Creature*> CloudTriggerList;
         uint8 harpoonsHit;
+        
 
         void Reset() OVERRIDE
         {
             events.SetPhase(PHASE_INTRO);
             m_uiWaypointId = 0;
             harpoonsHit = 0;
-        }
-
-        void JustReachedHome() OVERRIDE
-        {
-            if(Creature* pSkadi = Unit::GetCreature(*me, DATA_SKADI_THE_RUTHLESS))
-            {
-                pSkadi->AI()->EnterEvadeMode();
-            }
-            me->SetCanFly(false);
-        }
-
-        void SpellHit(Unit* /*caster*/, const SpellInfo* spell) OVERRIDE
-        {
-            if(!instance)
-                return;
-
-            if(spell->Id == SPELL_HARPOON_DAMAGE)
             {
                 if(me->GetHealth() <= me->GetMaxHealth() * (IsHeroic() ? 0.17f : 0.27f))
                 {
@@ -317,24 +358,27 @@ public:
                 harpoonsHit++;
             }
 
-            if(spell->Id == SPELL_RIDE_VEHICLE)
+        void JustReachedHome() OVERRIDE
             {
-                events.SetPhase(PHASE_FLYING);
-                events.ScheduleEvent(EVENT_MOVE, 1000, 0, PHASE_FLYING);
-                me->SetCanFly(true);
-                me->SetDisableGravity(true);
-                me->SetHover(true);
+            if(Creature* pSkadi = Unit::GetCreature(*me, DATA_SKADI_THE_RUTHLESS))
+            {
+                pSkadi->AI()->EnterEvadeMode();
+            }
+            me->SetDisableGravity(false);
                 me->SetWalk(false);
             }
         }
 
-        void CastFreezingCloud(bool triggerLeft) {
+        void SpellHit(Unit* /*caster*/, const SpellInfo* spell) OVERRIDE
             me->GetCreatureListWithEntryInGrid(CloudTriggerList, NPC_TRIGGER, 200.0f);
             if(!CloudTriggerList.empty())
             {
-                for (std::list<Creature*>::const_iterator itr = CloudTriggerList.begin(); itr != CloudTriggerList.end(); ++itr)
+            if(!instance)
+                return;
+
+            if(spell->Id == SPELL_HARPOON_DAMAGE)
                 {
-                    if (Creature* trigger = *itr)
+                if(me->GetHealth() <= me->GetMaxHealth() * (IsHeroic() ? 0.17f : 0.27f))
                     {
                         if (trigger->GetPositionY() > -511.0f && triggerLeft == true)
                             trigger->CastSpell(trigger, SPELL_FREEZING_CLOUD, true);
@@ -366,53 +410,133 @@ public:
                 case EVENT_MOVE:
                     switch (m_uiWaypointId)
                     {
-                    case 0:
-                        me->GetMotionMaster()->MovePoint(0, Location[1].GetPositionX(), Location[1].GetPositionY(), Location[1].GetPositionZ());
-                        events.ScheduleEvent(EVENT_MOVE, 5*IN_MILLISECONDS, 0, PHASE_FLYING);
-                        break;
+                    if(Creature* pSkadi = Unit::GetCreature(*me, instance->GetData64(DATA_SKADI_THE_RUTHLESS)))
+                    {
+                        me->RemoveAllAuras();
+                        pSkadi->CastSpell(pSkadi, SPELL_SKADI_TELEPORT, true);
+                        pSkadi->AI()->Talk(SAY_DRAKE_DEATH);
+                    }
+                }
+                me->DealDamage(me, me->GetMaxHealth() * (IsHeroic() ? 0.17f : 0.25f), NULL, DIRECT_DAMAGE, SPELL_SCHOOL_MASK_NONE, NULL, false);
+                harpoonsHit++;
+            }
+            if(spell->Id == SPELL_RIDE_VEHICLE)
+            {
+                events.SetPhase(PHASE_FLYING);
+                events.ScheduleEvent(EVENT_MOVE, 1000, 0, PHASE_FLYING);
+                me->SetCanFly(true);
+                me->SetDisableGravity(true);
+                me->SetHover(true);
+                me->SetWalk(false);
+            }
+        }
+
+        void MovementInform(uint32 type, uint32 id) OVERRIDE
+            {
+                if (type != POINT_MOTION_TYPE)
+                    return;
+
+                switch (id)
+                {
                     case 1:
-                        me->GetMotionMaster()->MovePoint(0, Location[2].GetPositionX(), Location[2].GetPositionY(), Location[2].GetPositionZ());
-                        events.ScheduleEvent(EVENT_MOVE, 2*IN_MILLISECONDS, 0, PHASE_FLYING);
+                        Talk(EMOTE_RANGE);
                         break;
                     case 2:
-                        me->GetMotionMaster()->MovePoint(0, Location[3].GetPositionX(), Location[3].GetPositionY(), Location[3].GetPositionZ());
-                        events.ScheduleEvent(EVENT_MOVE, 1.5*IN_MILLISECONDS, 0, PHASE_FLYING);
+                        Talk(EMOTE_BREATH);
+                        CastFreezingCloud();
                         break;
-                    case 3:
-                        me->GetMotionMaster()->MovePoint(0, Location[8].GetPositionX(), Location[8].GetPositionY(), Location[8].GetPositionZ());
-                        if(Creature* pSkadi = Unit::GetCreature(*me, instance->GetData64(DATA_SKADI_THE_RUTHLESS)))
+                    default:
+                        break;
+                }
+            }
+
+        void CastFreezingCloud() {
+        void CastFreezingCloud() {
+            me->GetCreatureListWithEntryInGrid(CloudTriggerList, NPC_TRIGGER, 200.0f);
+            bool triggerLeft = urand(0,1);
+            if(!CloudTriggerList.empty())
+            {
+                for (std::list<Creature*>::const_iterator itr = CloudTriggerList.begin(); itr != CloudTriggerList.end(); ++itr)
+                {
+                    if (Creature* trigger = *itr)
+                    {
+                        if (trigger->GetPositionY() > -511.0f && triggerLeft == true)
                         {
-                            pSkadi->AI()->Talk(SAY_DRAKE_BREATH);
+                            trigger->CastSpell(trigger, SPELL_FREEZING_CLOUD_1, true);
+                            trigger->CastSpell(trigger, SPELL_FREEZING_CLOUD_2, true);
                         }
-                        events.ScheduleEvent(EVENT_MOVE, 2.5*IN_MILLISECONDS, 0, PHASE_FLYING);
-                        break;
-                    case 4:
-                        me->GetMotionMaster()->MovePoint(0, Location[9].GetPositionX(), Location[9].GetPositionY(), Location[9].GetPositionZ());
-                        switch(urand(0,1))
+                        else if (trigger->GetPositionY() < -511.0f && triggerLeft == false)
                         {
-                        case 0 : 
-                            DoCast(SPELL_FREEZING_CLOUD_L);
-                            CastFreezingCloud(true);
-                            break;
-                        case 1 :
-                            DoCast(SPELL_FREEZING_CLOUD_R);
-                            CastFreezingCloud(false);
-                            break;
+                            trigger->CastSpell(trigger, SPELL_FREEZING_CLOUD_1, true);
+                            trigger->CastSpell(trigger, SPELL_FREEZING_CLOUD_2, true);
                         }
-                        events.ScheduleEvent(EVENT_STOP_CLOUD, 5*IN_MILLISECONDS, 0, PHASE_FLYING);
-                        events.ScheduleEvent(EVENT_MOVE, 2*IN_MILLISECONDS, 0, PHASE_FLYING);
-                        break;
-                    case 5:
-                        me->GetMotionMaster()->MovePoint(0, Location[10].GetPositionX(), Location[10].GetPositionY(), Location[10].GetPositionZ());
-                        events.ScheduleEvent(EVENT_MOVE, 3*IN_MILLISECONDS, 0, PHASE_FLYING);
-                        break;
-                    case 6:
-                        me->GetMotionMaster()->MovePoint(0, Location[3].GetPositionX(), Location[3].GetPositionY(), Location[3].GetPositionZ());
-                        m_uiWaypointId = 2;
-                        events.ScheduleEvent(EVENT_MOVE, 15*IN_MILLISECONDS, 0, PHASE_FLYING);
-                        break;
+                        }
                     }
-                    m_uiWaypointId++; 
+                }
+            }
+        void UpdateAI(uint32 diff) OVERRIDE
+                        {
+            events.Update(diff);
+
+            //if (me->GetPositionX() >= 519)
+            //{
+            //    me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE | UNIT_FLAG_NON_ATTACKABLE);
+            //    Talk(EMOTE_RANGE);
+            //}
+            //else
+            //{
+            //    me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE | UNIT_FLAG_NON_ATTACKABLE);
+            //}
+
+            while (uint32 eventId = events.ExecuteEvent())
+            {
+                switch (eventId)
+                {
+                case EVENT_MOVE:
+                    switch(m_uiWaypointId)
+                    {
+                        case 0:
+                            me->GetMotionMaster()->MovePoint(0, Location[10]);
+                            events.ScheduleEvent(EVENT_MOVE, 15*IN_MILLISECONDS, 0, PHASE_FLYING);
+                            break;
+                        case 1:
+                            me->GetMotionMaster()->MovePoint(1, Location[11]);
+                            
+                            events.ScheduleEvent(EVENT_MOVE, 5*IN_MILLISECONDS, 0, PHASE_FLYING);
+                            break;
+                        case 2:
+                            me->GetMotionMaster()->MovePoint(0, Location[12]);
+                            events.ScheduleEvent(EVENT_MOVE, 5*IN_MILLISECONDS, 0, PHASE_FLYING);
+                            break;
+                        case 3:
+                            me->GetMotionMaster()->MovePoint(2, Location[13]);
+                            events.ScheduleEvent(EVENT_STOP_CLOUD, 5*IN_MILLISECONDS,0, PHASE_FLYING);
+                            events.ScheduleEvent(EVENT_MOVE, 2*IN_MILLISECONDS, 0, PHASE_FLYING);
+                            break;
+                        case 4:
+                            me->GetMotionMaster()->MovePoint(0, Location[25]);
+                            events.ScheduleEvent(EVENT_MOVE, 25*IN_MILLISECONDS, 0, PHASE_FLYING);
+                            break;
+                        case 5:
+                            me->GetMotionMaster()->MovePoint(1, Location[26]);
+                            events.ScheduleEvent(EVENT_MOVE, 4*IN_MILLISECONDS, 0, PHASE_FLYING);
+                            break;
+                        case 6:
+                            me->GetMotionMaster()->MovePoint(0, Location[27]);
+                            events.ScheduleEvent(EVENT_MOVE, 4*IN_MILLISECONDS, 0, PHASE_FLYING);
+                            break;
+                        case 7:
+                            me->GetMotionMaster()->MovePoint(1, Location[28]);
+                            events.ScheduleEvent(EVENT_STOP_CLOUD, 5*IN_MILLISECONDS,0, PHASE_FLYING);
+                            events.ScheduleEvent(EVENT_MOVE, 1*IN_MILLISECONDS, 0, PHASE_FLYING);
+                            break;
+                        case 8:
+                            me->GetMotionMaster()->MovePoint(0, Location[29]);
+                            m_uiWaypointId = 1;
+                            events.ScheduleEvent(EVENT_MOVE, 4*IN_MILLISECONDS, 0, PHASE_FLYING);
+                            break;
+                    }
+                    m_uiWaypointId++;
                     break;
                 case EVENT_STOP_CLOUD:
                     me->RemoveAurasDueToSpell(SPELL_FREEZING_CLOUD_L);
