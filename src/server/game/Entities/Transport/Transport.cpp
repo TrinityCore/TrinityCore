81ec99c95e326ec1b98fff724f387dd077999368
Shauren <shauren.trinity@gmail.com>
2013-10-16 18:59:34 +0200

    Build fix

--- src/server/game/Entities/Transport/Transport.cpp
+++ src/server/game/Entities/Transport/Transport.cpp
@@ -66,7 +66,7 @@ bool Transport::Create(uint32 guidlow, uint32 entry, uint32 mapid, float x, floa
     TransportTemplate const* tInfo = sTransportMgr->GetTransportTemplate(entry);
     if (!tInfo)
     {
-        TC_LOG_ERROR(LOG_FILTER_SQL, "Transport %u (name: %s) will not be created, missing `transport_template` entry.", entry, goinfo->name);
+        TC_LOG_ERROR(LOG_FILTER_SQL, "Transport %u (name: %s) will not be created, missing `transport_template` entry.", entry, goinfo->name.c_str());
         return false;
     }
 
