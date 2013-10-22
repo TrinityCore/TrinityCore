04d32db2e43dd2cb7b4db539cbf1c6043d693c62
jackpoz <giacomopoz@gmail.com>
2013-10-22 22:59:47 +0200

    Core/IoC: Fix uninitialized timer
    Fix uninitialized Docks timer in Isle Of Conquest .
    
    Valgrind log:
     Conditional jump or move depends on uninitialised value(s)
      at : BattlegroundIC::HandleCapturedNodes(ICNodePoint*, bool) (BattlegroundIC.cpp:665)
      by : BattlegroundIC::PostUpdateImpl(unsigned int) (BattlegroundIC.cpp:228)
      by : Battleground::Update(unsigned int) (Battleground.cpp:300)
      by : BattlegroundMgr::Update(unsigned int) (BattlegroundMgr.cpp:93)
      by : World::Update(unsigned int) (World.cpp:2062)

--- src/server/game/Battlegrounds/Zones/BattlegroundIC.cpp
+++ src/server/game/Battlegrounds/Zones/BattlegroundIC.cpp
@@ -45,6 +45,7 @@ BattlegroundIC::BattlegroundIC()
 
     closeFortressDoorsTimer = CLOSE_DOORS_TIME; // the doors are closed again... in a special way
     doorsClosed = false;
+    docksTimer = DOCKS_UPDATE_TIME;
     resourceTimer = IC_RESOURCE_TIME;
 
     for (uint8 i = NODE_TYPE_REFINERY; i < MAX_NODE_TYPES; ++i)
