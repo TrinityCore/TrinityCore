f08c69820ccac5389b65bc2527d172149a866e47
Timh52280 <timh@live.com.mx>
2013-10-31 11:32:37 -0600

    Agregue los delete en los sql

--- sql/fixes_spells/spell_warl_bane_of_doom_coeff_correction.sql
+++ sql/fixes_spells/spell_warl_bane_of_doom_coeff_correction.sql
@@ -1,1 +1,2 @@
+delete from `spell_bonus_data` where `entry` = '603';
 insert into `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) values('603','0','0.88','-1','-1','Warlock - Bane of Doom');
