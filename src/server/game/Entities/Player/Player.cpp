96aa82a87cfac26987982d4b434a0f27a515e182
Nyeriah <sarah.trysan@live.com>
2013-11-30 10:39:26 -0200

    [TW082] - Core/Players: Saves players after turning in quest to avoid data mismatch in case of crashes
    E.g players turn in quest, server crashes, they'll still have the items necessary to turn in their quest at inventory but quest will already have rewarded status as only status was being saved. They'd however have lost quest rewards, which at some cases would prevent progressing in quest chains and other bad things. [Needs only to check out if this doesn't affect performance much, if it does, then I'll think of another way to do this (saving only the needed player stats)].

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -15362,6 +15362,9 @@ void Player::RewardQuest(Quest const* quest, uint32 reward, Object* questGiver, 
 
     //lets remove flag for delayed teleports
     SetCanDelayTeleport(false);
+
+    // Save the player after quest is rewarded to avoid bad behaviors in case of server crashes.
+    this->SaveToDB();
 }
 
 void Player::FailQuest(uint32 questId)
