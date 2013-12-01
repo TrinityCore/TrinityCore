02c03201bf825a4fc7c763cad3cbcd91e6df6c53
MitchesD <majklprofik@seznam.cz>
2013-12-01 16:52:54 +0100

    Scripts/Jeklik: fixed flying in first phase, reworked spawn and rewritten to EventMap

---
+++ sql/updates/world/2013_12_01_06_world_creature.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_template` SET `InhabitType` = 5 WHERE `entry` = 11368; -- Bloodseeker Bat
+UPDATE `creature_template` SET `InhabitType` = 7 WHERE `entry` = 14965; -- Frenzied Bloodseeker Bat
