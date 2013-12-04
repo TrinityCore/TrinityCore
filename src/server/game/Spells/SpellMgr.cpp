f5c15a0689dbf36c485307e9d3ce7854f053eda4
Nyeriah <sarah.trysan@live.com>
2013-12-04 21:21:47 -0200

    [TW094] - Core/Spells: Allow Death and Decay's trigger to damage invisible targets
    Taken from tc's tracker.

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -3782,6 +3782,9 @@ void SpellMgr::LoadSpellInfoCorrections()
             case 18756:
                 spellInfo->Effects[0].TargetA = SpellImplicitTargetInfo(TARGET_UNIT_CASTER);
                 break;
+            case 52212: // Death Knight's Death and Decay triggered spell
+                spellInfo->AttributesEx6 |= SPELL_ATTR6_CAN_TARGET_INVISIBLE;
+                break;
             // ISLE OF CONQUEST SPELLS
             //
             case 66551: // Teleport
