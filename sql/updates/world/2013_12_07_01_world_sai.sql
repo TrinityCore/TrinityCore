32c82a5da4189b2309c27d231f4f25ef8f199b6d
Malcrom <malcromdev@gmail.com>
2013-12-07 23:11:41 -0330

    DB/SAI: Fix my stupid mistake

--- sql/updates/world/2013_12_07_01_world_sai.sql
+++ sql/updates/world/2013_12_07_01_world_sai.sql
@@ -1,3 +1,3 @@
 -- Remove Springpaw Stalker & Elder Springpaw SAI
 UPDATE `creature_template` SET `AIName`= '' WHERE `entry`IN (15651,15652);
-DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY IN (15651,15652);
+DELETE FROM `smart_scripts` WHERE `entryorguid` IN (15651,15652);
