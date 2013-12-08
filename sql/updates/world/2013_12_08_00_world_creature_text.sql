0e37faf2022aed2b54f337950e8a209e6538613f
Malcrom <malcromdev@gmail.com>
2013-12-08 15:10:28 -0330

    Scripting/Razorfen Downs: That emote shouldn't have slipped in there.

--- sql/updates/world/2013_12_08_00_world_creature_text.sql
+++ sql/updates/world/2013_12_08_00_world_creature_text.sql
@@ -3,6 +3,6 @@ SET @ENTRY := 8567;
 DELETE FROM `creature_text` WHERE `entry`=@ENTRY;
 INSERT INTO `creature_text` (`entry`,`groupid`,`id`,`text`,`type`,`language`,`probability`,`emote`,`duration`,`sound`,`comment`) VALUES
 (@ENTRY,0,0,'Me smell stench of the living!',14,0,100,0,0,5823, 'Glutton - SAY_AGGRO'),
-(@ENTRY,1,0,'Me feast on you all!',14,0,100,1,0,5824, 'Glutton - SAY_SLAY'),
+(@ENTRY,1,0,'Me feast on you all!',14,0,100,0,0,5824, 'Glutton - SAY_SLAY'),
 (@ENTRY,2,0,'%s is getting hungry!',16,0,100,0,0,0, 'Glutton - EMOTE_50%'),
 (@ENTRY,3,0,'%s is getting VERY hungry!',16,0,100,0,0,0, 'Glutton - EMOTE 15%');
