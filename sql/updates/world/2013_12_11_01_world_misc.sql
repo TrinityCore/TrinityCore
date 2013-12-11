5fa0b7c438332789218959c7cd91642b5d0626cd
Malcrom <malcromdev@gmail.com>
2013-12-11 15:34:57 -0330

    DB/Misc: fixed comment in one SAI script.

--- sql/updates/world/2013_12_11_01_world_misc.sql
+++ sql/updates/world/2013_12_11_01_world_misc.sql
@@ -34,7 +34,7 @@ SET @ENTRY := 21811;
 UPDATE `creature_template` SET `AIName`="SmartAI" WHERE `entry`=@ENTRY;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=0;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
-(@ENTRY,0,0,0,0,0,100,0,0,0,2300,3900,11,34083,64,0,0,0,0,2,0,0,0,0,0,0,0,"Wyrmcult Poacher - Combat - Cast Fireball");
+(@ENTRY,0,0,0,0,0,100,0,0,0,2300,3900,11,34083,64,0,0,0,0,2,0,0,0,0,0,0,0,"Wyrmcult Broodling - Combat - Cast Fireball");
 
 -- Remove aura from addon being applied in SAI
 DELETE FROM `creature_addon` WHERE `guid` IN (SELECT guid FROM creature WHERE id=34947);
