39331ea7b9e906b67378a1be6c0c694d264eda3a
Spp <spp@jorge.gr>
2013-12-19 10:34:17 +0100

    Core/Logging: Use logger cache for speed up logger filter search

--- src/server/shared/Logging/Log.cpp
+++ src/server/shared/Logging/Log.cpp
@@ -267,7 +267,7 @@ void Log::vlog(std::string const& filter, LogLevel level, char const* str, va_li
     write(new LogMessage(level, filter, text));
 }
 
-void Log::write(LogMessage* msg) const
+void Log::write(LogMessage* msg)
 {
     Logger const* logger = GetLoggerByType(msg->type);
     msg->text.append("\n");
@@ -376,6 +376,7 @@ void Log::Close()
     delete worker;
     worker = NULL;
     loggers.clear();
+    cachedLoggers.clear();
     for (AppenderMap::iterator it = appenders.begin(); it != appenders.end(); ++it)
     {
         delete it->second;
