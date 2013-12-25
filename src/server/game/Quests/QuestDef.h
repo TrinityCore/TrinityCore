01a43e6a300a0321bf11082f2eecfb0e8ecc2e1b
Malcrom <malcromdev@gmail.com>
2013-12-25 14:31:28 -0330

    Core/QuestDef: Well I clicked revert but Git didn't play along.

--- src/server/game/Quests/QuestDef.h
+++ src/server/game/Quests/QuestDef.h
@@ -109,15 +109,15 @@ enum QuestStatus
 enum QuestGiverStatus
 {
     DIALOG_STATUS_NONE                     = 0,
-    DIALOG_STATUS_UNAVAILABLE              = 2,
-    DIALOG_STATUS_LOW_LEVEL_AVAILABLE      = 3,
-    DIALOG_STATUS_LOW_LEVEL_REWARD_REP     = 4,
-    DIALOG_STATUS_LOW_LEVEL_AVAILABLE_REP  = 5,
-    DIALOG_STATUS_INCOMPLETE               = 6,
-    DIALOG_STATUS_REWARD_REP               = 7,
-    DIALOG_STATUS_AVAILABLE_REP            = 8,
-    DIALOG_STATUS_AVAILABLE                = 9,
-    DIALOG_STATUS_REWARD2                  = 10,             // no yellow dot on minimap
+    DIALOG_STATUS_UNAVAILABLE              = 1,
+    DIALOG_STATUS_LOW_LEVEL_AVAILABLE      = 2,
+    DIALOG_STATUS_LOW_LEVEL_REWARD_REP     = 3,
+    DIALOG_STATUS_LOW_LEVEL_AVAILABLE_REP  = 4,
+    DIALOG_STATUS_INCOMPLETE               = 5,
+    DIALOG_STATUS_REWARD_REP               = 6,
+    DIALOG_STATUS_AVAILABLE_REP            = 7,
+    DIALOG_STATUS_AVAILABLE                = 8,
+    DIALOG_STATUS_REWARD2                  = 9,             // no yellow dot on minimap
     DIALOG_STATUS_REWARD                   = 10,            // yellow dot on minimap
 
     // Custom value meaning that script call did not return any valid quest status
