72952e6b5e5bf1dd096cbd0b6c817adc09b4429e
Nawuko <patrick.loewer@gmail.com>
2013-12-30 20:06:45 +0100

    Core: user defined literals need a space in front (according to the new standard). compile fix for gcc

--- src/server/shared/Define.h
+++ src/server/shared/Define.h
@@ -70,13 +70,8 @@
 #  define ATTR_DEPRECATED
 #endif //COMPILER == COMPILER_GNU
 
-#if COMPILER_HAS_CPP11_SUPPORT
-#  define OVERRIDE override
-#  define FINAL final
-#else
-#  define OVERRIDE
-#  define FINAL
-#endif //COMPILER_HAS_CPP11_SUPPORT
+#define OVERRIDE
+#define FINAL
 
 #define UI64FMTD ACE_UINT64_FORMAT_SPECIFIER
 #define UI64LIT(N) ACE_UINT64_LITERAL(N)
