7a8c0cb1f65182106e90d750767938e8522a12c9
Zabustak <Zabustak@gmail.com>
2013-12-31 00:14:20 +0100

    Show Kick, Mute and Ban in world

---
+++ sql/updates/world/2013_12_30_01_trinity_string.sql
@@ -0,0 +1,9 @@
+
+DELETE FROM trinity_string where entry in (11002, 11003, 11004, 11005, 11006, 11007);
+INSERT INTO `trinity_string` VALUES
+('11002', '|cffff0000[Server]: |cffFFFFFF%s |cFF00ccffhas kicked |cffFFFFFF%s, |cFF00ccffreason: |cffFFFFFF%s|cFF00ccff. |r', '', '', '', '', '', '', '', ''),
+('11003', '|cffff0000[Server]: |cffFFFFFF%s |cFF00ccffhas muted |cffFFFFFF%s |cFF00ccfffor |cffFFFFFF%u minutes,|cFF00ccffreason: |cffFFFFFF%s|cFF00ccff. |r', '', '', '', '', '', '', '', ''),
+('11004', '|cffff0000[Server]: |cffFFFFFF%s |cFF00ccffhas banned character |cffFFFFFF%s |cFF00ccfffor |cffFFFFFF%s, |cFF00ccffreason: |cffFFFFFF%s|cFF00ccff. |r', '', '', '', '', '', '', '', ''),
+('11005', '|cffff0000[Server]: |cffFFFFFF%s |cFF00ccffhas banned character |cffFFFFFF%s |cFF00ccffpermanetly, reason: |cffFFFFFF%s|cFF00ccff. |r', '', '', '', '', '', '', '', ''),
+('11006', '|cffff0000[Server]: |cffFFFFFF%s |cFF00ccffhas banned |cffFFFFFF%s |cFF00ccfffor |cffFFFFFF%s, |cFF00ccffreason: |cffFFFFFF%s|cFF00ccff. |r', '', '', '', '', '', '', '', ''),
+('11007', '|cffff0000[Server]: |cffFFFFFF%s |cFF00ccffhas banned |cffFFFFFF%s |cFF00ccffpermanetly, reason: |cffFFFFFF%s|cFF00ccff. |r', '', '', '', '', '', '', '', '');
