20004050bcd9396f504e3e33138b734d96de5238
Vincent_Michael <Vincent_Michael@gmx.de>
2014-01-01 00:07:53 +0100

    Update copyright note for 2014.
    Happy new year.

--- src/server/game/Maps/TransportMgr.h
+++ src/server/game/Maps/TransportMgr.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2012 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2008-2014 TrinityCore <http://www.trinitycore.org/>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
