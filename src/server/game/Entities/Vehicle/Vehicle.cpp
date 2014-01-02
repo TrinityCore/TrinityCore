e66b8da274f98747cae8945d95c8e40e7647e1a5
Shauren <shauren.trinity@gmail.com>
2014-01-02 14:20:05 +0100

    Core/Vehicles
    * Fixed vehicle orientation on transports
    * Fixed exiting vehicles on moving transports

--- src/server/game/Entities/Vehicle/Vehicle.cpp
+++ src/server/game/Entities/Vehicle/Vehicle.cpp
@@ -521,8 +521,13 @@ Vehicle* Vehicle::RemovePassenger(Unit* unit)
 
     if (_me->IsInWorld())
     {
-        unit->RemoveUnitMovementFlag(MOVEMENTFLAG_ONTRANSPORT);
-        unit->m_movementInfo.transport.Reset();
+        if (!_me->GetTransport())
+        {
+            unit->RemoveUnitMovementFlag(MOVEMENTFLAG_ONTRANSPORT);
+            unit->m_movementInfo.transport.Reset();
+        }
+        else
+            unit->m_movementInfo.transport = _me->m_movementInfo.transport;
     }
 
     // only for flyable vehicles
