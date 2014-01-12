5543e684eed91ce4fd9122d41a8fb194cbca0980
Vincent_Michael <Vincent_Michael@gmx.de>
2014-01-12 02:02:02 +0100

    Fix non pch build

--- src/server/scripts/EasternKingdoms/zone_burning_steppes.cpp
+++ src/server/scripts/EasternKingdoms/zone_burning_steppes.cpp
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) 2008-2014 TrinityCore <http://www.trinitycore.org/>
- * Copyright (C) 2006-2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -15,45 +14,6 @@
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
-/* ScriptData
-SDName: Burning_Steppes
-SD%Complete: 0
-SDComment:
-SDCategory: Burning Steppes
-EndScriptData */
-
-enum RaggedJohn
-{
-    QUEST_THE_TRUE_MASTERS        = 4224,
-    QUEST_MOTHERS_MILK            = 4866,
-    SPELL_MOTHERS_MILK            = 16468,
-    SPELL_WICKED_MILKING          = 16472
-};
-
-    struct npc_ragged_johnAI : public ScriptedAI
-    {
-        npc_ragged_johnAI(Creature* creature) : ScriptedAI(creature) { }
-
-        void Reset() OVERRIDE { }
-
-        void MoveInLineOfSight(Unit* who) OVERRIDE
-        {
-            if (who->HasAura(SPELL_MOTHERS_MILK))
-            {
-                if (who->GetTypeId() == TYPEID_PLAYER && me->IsWithinDistInMap(who, 15) && who->isInAccessiblePlaceFor(me))
-                {
-                    DoCast(who, SPELL_WICKED_MILKING);
-                    if (Player* player = who->ToPlayer())
-                        player->AreaExploredOrEventHappens(QUEST_MOTHERS_MILK);
-                }
-            }
-
-            ScriptedAI::MoveInLineOfSight(who);
-        }
-
-        void EnterCombat(Unit* /*who*/) OVERRIDE { }
-    };
 
 void AddSC_burning_steppes()
 {
