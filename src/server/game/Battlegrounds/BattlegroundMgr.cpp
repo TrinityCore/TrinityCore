ebd41d3d554cee84f346131ad030326c93d1e6be
zorix <zajast@o2.pl>
2014-01-22 00:41:23 +0100

    Core/Battleground: Offline players will displayed on scoreboard

--- src/server/game/Battlegrounds/BattlegroundMgr.cpp
+++ src/server/game/Battlegrounds/BattlegroundMgr.cpp
@@ -379,6 +379,7 @@ void BattlegroundMgr::BuildPvpLogDataPacket(WorldPacket* data, Battleground* bg)
     size_t count_pos = data->bitwpos();
     data->WriteBits(0, 21);
     uint32 count = 0;
+    BattlegroundPlayerMap const& bgPlayers = bg->GetPlayers();
     for (Battleground::BattlegroundScoreMap::const_iterator itr = bg->GetPlayerScoresBegin(); itr != bg->GetPlayerScoresEnd(); ++itr)
     {
         if (!bg->IsPlayerInBattleground(itr->first))
@@ -387,11 +388,24 @@ void BattlegroundMgr::BuildPvpLogDataPacket(WorldPacket* data, Battleground* bg)
             continue;
         }
 
-        Player* player = ObjectAccessor::FindPlayer(itr->first);
-        if (!player)
+        uint32 team;
+        int32  primaryTree;
+        if (Player* player = ObjectAccessor::FindPlayer(itr->first))
         {
-            TC_LOG_ERROR(LOG_FILTER_BATTLEGROUND, "Player " UI64FMTD " has scoreboard entry for battleground %u but is not in world!", itr->first, bg->GetTypeID(true));
-            continue;
+            team = player->GetBGTeam();
+            primaryTree = player->GetPrimaryTalentTree(player->GetActiveSpec());
+        }
+        else
+        {
+            BattlegroundPlayerMap::const_iterator itr2 = bgPlayers.find(itr->first);
+            if (itr2 == bgPlayers.end())
+            {
+                TC_LOG_ERROR(LOG_FILTER_BATTLEGROUND, "Player " UI64FMTD " has scoreboard entry for battleground %u but do not have battleground data!", itr->first, bg->GetTypeID(true));
+                continue;
+            }
+
+            team = itr2->second.Team;
+            primaryTree = itr2->second.PrimaryTree;
         }
 
         ObjectGuid playerGUID = itr->first;
@@ -409,7 +423,7 @@ void BattlegroundMgr::BuildPvpLogDataPacket(WorldPacket* data, Battleground* bg)
         data->WriteBit(playerGUID[5]);
         data->WriteBit(playerGUID[1]);
         data->WriteBit(playerGUID[6]);
-        data->WriteBit(player->GetBGTeam() == HORDE ? 0 : 1);
+        data->WriteBit(team == HORDE ? 0 : 1);
         data->WriteBit(playerGUID[7]);
 
         buff << uint32(score->HealingDone);             // healing done
@@ -431,7 +445,7 @@ void BattlegroundMgr::BuildPvpLogDataPacket(WorldPacket* data, Battleground* bg)
         buff.WriteByteSeq(playerGUID[1]);
         buff.WriteByteSeq(playerGUID[6]);
 
-        buff << int32(player->GetPrimaryTalentTree(player->GetActiveSpec()));
+        buff << int32(primaryTree);
 
         switch (bg->GetTypeID(true))                             // Custom values
         {
