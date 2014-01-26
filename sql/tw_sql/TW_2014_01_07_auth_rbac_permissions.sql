26189dce5b0c14f717a7a0eae56a3f5efcffb770
pr0d1gy <pr0d1gy.bc@gmail.com>
2014-01-26 23:03:34 +0200

    Fix new rbac file.

--- sql/tw_sql/TW_2014_01_07_auth_rbac_permissions.sql
+++ sql/tw_sql/TW_2014_01_07_auth_rbac_permissions.sql
@@ -1,64 +1,33 @@
 /* add rbac permissions to new commands */
- 
-INSERT INTO `rbac_permissions` (`id`, `name`) VALUES
-(1002,   'Command: qc');
- 
-INSERT INTO `rbac_permissions` (`id`, `name`) VALUES
-(1003,   'Command: qc status');
- 
-INSERT INTO `rbac_permissions` (`id`, `name`) VALUES
-(1004,   'Command: qc add');
- 
-INSERT INTO `rbac_permissions` (`id`, `name`) VALUES
-(1005,   'Command: qc del');
- 
-INSERT INTO `rbac_permissions` (`id`, `name`) VALUES
-(1006,   'Command: online');
- 
-INSERT INTO `rbac_permissions` (`id`, `name`) VALUES
-(1007,   'Command: online account');
- 
-INSERT INTO `rbac_permissions` (`id`, `name`) VALUES
-(1008,   'Command: online character');
- 
-INSERT INTO `rbac_permissions` (`id`, `name`) VALUES
-(1009,   'Command: instance setdata');
- 
-INSERT INTO `rbac_permissions` (`id`, `name`) VALUES
-(1010,   'Command: instance getdata');
+DELETE FROM `rbac_permissions` WHERE `id` IN (1002,1003,1004,1005,1006,1007,1008,1009,1010);
+INSERT INTO `rbac_permissions` (`id`,`name`) VALUES
+(1002,'Command: qc'),
+(1003,'Command: qc status'),
+(1004,'Command: qc add'),
+(1005,'Command: qc del'),
+(1006,'Command: online'),
+(1007,'Command: online account'),
+(1008,'Command: online character'),
+(1009,'Command: instance setdata'),
+(1010,'Command: instance getdata');
  
 /* adds qc to players */
-INSERT INTO `rbac_linked_permissions` VALUES
-(195, 1002);
- 
+DELETE FROM `rbac_linked_permissions` WHERE `id` IN (1002,1003,1004,1005,1006,1007,1008,1009,1010);
+INSERT INTO `rbac_linked_permissions` (`id`,`linkedId`) VALUES
+(195, 1002),
 /* adds qc status to players */
-INSERT INTO `rbac_linked_permissions` VALUES
-(195, 1003);
- 
+(195, 1003), 
 /* adds qc add to GMs */
-INSERT INTO `rbac_linked_permissions` VALUES
-(193, 1004);
- 
+(193, 1004),
 /* adds qc del to GMs */
-INSERT INTO `rbac_linked_permissions` VALUES
-(193, 1005);
- 
+(193, 1005),
 /* adds online to mods+ */
-INSERT INTO `rbac_linked_permissions` VALUES
-(195, 1006);
- 
+(195, 1006), 
 /* adds online account to mods+ */
-INSERT INTO `rbac_linked_permissions` VALUES
-(194, 1007);
- 
+(194, 1007), 
 /* adds online character to mods+ */
-INSERT INTO `rbac_linked_permissions` VALUES
-(194, 1008);
- 
+(194, 1008), 
 /* adds instance setdata to GMs */
-INSERT INTO `rbac_linked_permissions` VALUES
-(193, 1009);
- 
+(193, 1009), 
 /* adds instance getdata to GMs */
-INSERT INTO `rbac_linked_permissions` VALUES
 (193, 1010);
