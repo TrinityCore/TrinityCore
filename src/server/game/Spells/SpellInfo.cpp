88b2a8577ba4b9380eb85debf247e504257f56ad
joschiwald <joschiwald.trinity@gmail.com>
2014-01-27 19:50:07 +0100

    Core/Spells: update judgement spellscript thx @Xanvial
    Closes #10562
    Closes #9477
    Closes #9801

--- src/server/game/Spells/SpellInfo.cpp
+++ src/server/game/Spells/SpellInfo.cpp
@@ -2169,8 +2169,8 @@ SpellSpecificType SpellInfo::GetSpellSpecific() const
             if (SpellFamilyFlags[0] & 0x00002190)
                 return SPELL_SPECIFIC_HAND;
 
-            // Judgement of Wisdom, Judgement of Light, Judgement of Justice
-            if (Id == 20184 || Id == 20185 || Id == 20186)
+            // Judgement
+            if (Id == 20271)
                 return SPELL_SPECIFIC_JUDGEMENT;
 
             // only paladin auras have this (for palaldin class family)
