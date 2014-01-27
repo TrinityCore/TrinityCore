6c17bcf928799b19d724a2c4862cb7bc0321dad7
Aokromes <jipr@hotmail.com>
2014-01-27 20:57:43 +0100

    DB/Creature: Fix some factions
    Updates #11501

---
+++ sql/updates/world/2014_01_27_15_world_creature_template.sql
@@ -0,0 +1,3 @@
+--
+UPDATE `creature_template` SET `faction_A` = 26, `faction_H` = 26 WHERE `entry` IN (37469,37470,37473);
+UPDATE `creature_template` SET `faction_A` = 1214, `faction_H` = 1214 WHERE `entry` IN (13397,22745,32016);
