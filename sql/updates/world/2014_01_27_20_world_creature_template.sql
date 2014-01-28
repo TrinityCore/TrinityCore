b59559999d6c94d1b9b86ca182e972a8d3b06f1a
Aokromes <jipr@hotmail.com>
2014-01-28 06:29:17 +0100

    DB/Creature: Typo fix.

--- sql/updates/world/2014_01_27_20_world_creature_template.sql
+++ sql/updates/world/2014_01_27_20_world_creature_template.sql
@@ -1,3 +1,3 @@
 --
 UPDATE `creature_template` SET `faction_A` = 1216, `faction_H` = 1216 WHERE `entry` IN (13331,13422,22588,22608,31932,32125);
-UPDATE `creature_template` SET `faction_A` = 1214, `faction_H` = 1214 WHERE `entry` IN (13088,13316,22684,2273331950,32043,13332,22589,32126);
+UPDATE `creature_template` SET `faction_A` = 1214, `faction_H` = 1214 WHERE `entry` IN (13088,13316,22684,22733,31950,32043,13332,22589,32126);
