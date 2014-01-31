0bb41038a9d8a10cbda11cb3986aefff7e1aa9cc
Magnifikator <user385@ximbo.net>
2014-01-31 15:53:05 +0000

    DB/item_template: Set defaults of spellcharges_X to 0 instead of NULL
    Closes #10748
    
    Signed-off-by: Duarte Duarte <dnpd.dd@gmail.com>

---
+++ sql/updates/world/2014_01_31_01_world_item_template.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `item_template` CHANGE `spellcharges_1` `spellcharges_1` SMALLINT( 6 ) NOT NULL DEFAULT '0',
+                            CHANGE `spellcharges_2` `spellcharges_2` SMALLINT( 6 ) NOT NULL DEFAULT '0',
+                            CHANGE `spellcharges_3` `spellcharges_3` SMALLINT( 6 ) NOT NULL DEFAULT '0',
+                            CHANGE `spellcharges_4` `spellcharges_4` SMALLINT( 6 ) NOT NULL DEFAULT '0',
+                            CHANGE `spellcharges_5` `spellcharges_5` SMALLINT( 6 ) NOT NULL DEFAULT '0';
