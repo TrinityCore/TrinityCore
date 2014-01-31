7f52bf52e44dd7d65638d4f3217da5d0ca5ebec3
Duarte Duarte <dnpd.dd@gmail.com>
2014-01-31 16:35:22 +0000

    SQL: Update 2014_01_31_01_world_item_template.sql

--- sql/updates/world/2014_01_31_01_world_item_template.sql
+++ sql/updates/world/2014_01_31_01_world_item_template.sql
@@ -1,3 +1,9 @@
+UPDATE `item_template` SET `spellcharges_1`=0 WHERE `spellcharges_1` IS NULL;
+UPDATE `item_template` SET `spellcharges_2`=0 WHERE `spellcharges_2` IS NULL;
+UPDATE `item_template` SET `spellcharges_3`=0 WHERE `spellcharges_3` IS NULL;
+UPDATE `item_template` SET `spellcharges_4`=0 WHERE `spellcharges_4` IS NULL;
+UPDATE `item_template` SET `spellcharges_5`=0 WHERE `spellcharges_5` IS NULL;
+
 ALTER TABLE `item_template` CHANGE `spellcharges_1` `spellcharges_1` SMALLINT( 6 ) NOT NULL DEFAULT '0',
                             CHANGE `spellcharges_2` `spellcharges_2` SMALLINT( 6 ) NOT NULL DEFAULT '0',
                             CHANGE `spellcharges_3` `spellcharges_3` SMALLINT( 6 ) NOT NULL DEFAULT '0',
