1acb3f6534148f2cf0e0880db84053a746492c81
Dr-J <daniel.jarrott0@gmail.com>
2014-02-03 20:22:50 +0000

    Update 2014_02_02_01_world_sai.sql

--- sql/updates/world/2014_02_02_01_world_sai.sql
+++ sql/updates/world/2014_02_02_01_world_sai.sql
@@ -106,7 +106,6 @@ INSERT INTO `creature_equip_template` (`entry`, `id`, `itemEntry1`, `itemEntry2`
 (33235, 1, 1903, 0, 25972), -- 33235
 (33956, 1, 1903, 0, 25972), -- 33956
 (33957, 1, 1903, 0, 25972); -- 33956
-
 -- Prospector Doren/Loren Gossip
 UPDATE `creature_template` SET `gossip_menu_id`=10393,`npcflag`=2 WHERE  `entry`IN(33956,33957);
 DELETE FROM `gossip_menu` WHERE `entry`=10393;
