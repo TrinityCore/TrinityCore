b7fbf46fff46c32fe87b55d18767f08f02aab57c
Ukulutl <gascart@gmail.com>
2014-02-07 08:17:40 +0100

    Add goblin mount vendor

--- src/server/scripts/World/npcs_special.cpp
+++ src/server/scripts/World/npcs_special.cpp
@@ -1211,6 +1211,11 @@ public:
                     player->SEND_GOSSIP_MENU(10239, creature->GetGUID());
                 else canBuy = true;
                 break;
+            case 48510:                                         //Kall Worthaton
+                if (player->GetReputationRank(1133) != REP_EXALTED && race != RACE_GOBLIN)
+                    player->SEND_GOSSIP_MENU(17494, creature->GetGUID());
+                else canBuy = true;
+                break;
         }
 
         if (canBuy)
