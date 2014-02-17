66c0aa50978c11b3fefe7fb39782073193ac42ce
Ascathor <Break_the_Chain@web.de>
2014-02-17 02:01:35 +0100

    Core/Cmake: Allow settings to detach custom scripts Allow to remove coupled custom scripts to regular scripts on build. Meaning, if you change custom scripts and have this enabled, only custom scripts are rebuilt and core scripts are untouched. Disabled by default. Original Code by @LilleCarl.

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -8,12 +8,13 @@
 # WITHOUT ANY WARRANTY, to the extent permitted by law; without even the
 # implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
-option(SERVERS          "Build worldserver and authserver"                            1)
-option(SCRIPTS          "Build core with scripts included"                            1)
-option(TOOLS            "Build map/vmap/mmap extraction/assembler tools"              0)
-option(USE_SCRIPTPCH    "Use precompiled headers when compiling scripts"              1)
-option(USE_COREPCH      "Use precompiled headers when compiling servers"              1)
-option(WITH_WARNINGS    "Show all warnings during compile"                            0)
-option(WITH_COREDEBUG   "Include additional debug-code in core"                       0)
-option(WITH_MESHEXTRACTOR "Build meshextractor (alpha)"                               0)
-option(WITHOUT_GIT      "Disable the GIT testing routines"                            0)
+option(SERVERS          "Build worldserver and authserver"                              1)
+option(SCRIPTS          "Build core with scripts included"                              1)
+option(TOOLS            "Build map/vmap/mmap extraction/assembler tools"                0)
+option(USE_SCRIPTPCH    "Use precompiled headers when compiling scripts"                1)
+option(USE_COREPCH      "Use precompiled headers when compiling servers"                1)
+option(DETACH_CSCRIPTS  "Detach custom scripts to compile individually to core scripts" 0)
+option(WITH_WARNINGS    "Show all warnings during compile"                              0)
+option(WITH_COREDEBUG   "Include additional debug-code in core"                         0)
+option(WITH_MESHEXTRACTOR "Build meshextractor (alpha)"                                 0)
+option(WITHOUT_GIT      "Disable the GIT testing routines"                              0)
