2c479b7ffe179cd4cd8e053979f88006db473e0a
Aokromes <Aokromes@users.noreply.github.com>
2014-02-20 04:13:52 +0100

    DB/Quest: Update OfferRewardText for quest 396
    Notice: i realize of the typo, but wowhead shows that text, if anyone have a prof of it's wowhead typo i will fix it.
    Closes #11534

---
+++ sql/updates/world/2014_02_20_03_world_quest_template.sql
@@ -0,0 +1,2 @@
+--
+UPDATE `quest_template` SET `OfferRewardText`='You are to be commended for the service you have done for Stormwind, $N. If VanCleef had been successful in his machinations, he would no doubt have done considerable harm to the kingdom - something we must obviously avoid.$B$BTake this, $C, as a token of the my appreciation.' WHERE `Id`=396;
