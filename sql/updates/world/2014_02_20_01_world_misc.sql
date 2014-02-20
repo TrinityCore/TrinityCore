537a6df782382c0a812baa24351c672e082691f6
Aokromes <Aokromes@users.noreply.github.com>
2014-02-20 02:08:51 +0100

    DB/Misc: Coding standards

--- sql/updates/world/2014_02_20_01_world_misc.sql
+++ sql/updates/world/2014_02_20_01_world_misc.sql
@@ -1,11 +1,12 @@
 --
-INSERT INTO `page_text` (`entry`, `text`) VALUES (124, 'Missing WDB data.');
-INSERT INTO `page_text` (`entry`, `text`) VALUES (2875, 'Missing WDB data.');
-INSERT INTO `page_text` (`entry`, `text`) VALUES (3560, 'Missing WDB data.');
-INSERT INTO `page_text` (`entry`, `text`) VALUES (2898, 'Missing WDB data.');
-INSERT INTO `page_text` (`entry`, `text`) VALUES (3581, 'Missing WDB data.');
-INSERT INTO `page_text` (`entry`, `text`) VALUES (3583, 'Missing WDB data.');
-INSERT INTO `page_text` (`entry`, `text`) VALUES (3587, 'Missing WDB data.');
-INSERT INTO `page_text` (`entry`, `text`) VALUES (3588, 'Missing WDB data.');
-INSERT INTO `page_text` (`entry`, `text`) VALUES (3589, 'Missing WDB data.');
-UPDATE item_template SET BagFamily = 0 WHERE entry IN (43651, 41811, 41804, 39527, 39526, 38625, 45860, 45120, 7187, 45863, 39148, 46055, 46054, 16082);
+INSERT INTO `page_text` (`entry`, `text`) VALUES 
+(124, 'Missing WDB data.'),
+(2875, 'Missing WDB data.'),
+(3560, 'Missing WDB data.'),
+(2898, 'Missing WDB data.'),
+(3581, 'Missing WDB data.'),
+(3583, 'Missing WDB data.'),
+(3587, 'Missing WDB data.'),
+(3588, 'Missing WDB data.'),
+(3589, 'Missing WDB data.');
+UPDATE `item_template` SET `BagFamily` = 0 WHERE `entry` IN (43651, 41811, 41804, 39527, 39526, 38625, 45860, 45120, 7187, 45863, 39148, 46055, 46054, 16082);
