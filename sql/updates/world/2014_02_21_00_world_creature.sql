8194d6127a8580cb6b59019b31a40fc18588afb8
msoky <msoky9@gmail.com>
2014-02-21 12:45:12 +0100

    DB/Creature: Fix db errors
    DB/Creature: Fix db errors

---
+++ sql/updates/world/2014_02_21_00_world_creature.sql
@@ -0,0 +1,3 @@
+-- db errors
+UPDATE `creature_template` SET `exp`=0 WHERE `entry` IN (13096,12047,13116,37236,13116,37323,13546,13777,14284,37409,14285,14848);
+UPDATE `creature_template` SET `exp`=1 WHERE `entry` IN (20536,20539);
