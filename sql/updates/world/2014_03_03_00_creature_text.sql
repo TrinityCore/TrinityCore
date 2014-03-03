d6e63c9690f818b006e6db61b12b27faf139dbf3
Aokromes <Aokromes@users.noreply.github.com>
2014-03-03 02:16:32 +0100

    DB/Creature: Commander Stoutbeard's yell
    Closes #7948

---
+++ sql/updates/world/2014_03_03_00_creature_text.sql
@@ -0,0 +1,3 @@
+--
+UPDATE `creature_text` SET `text`='What? Where in the...don\'t just stand around, lads! Kill somebody!' WHERE `entry`=26796 `groupid`=0;
+UPDATE `creature_text` SET `text`='Is that all you...got...' WHERE `entry`=26796 AND `groupid`=2;
