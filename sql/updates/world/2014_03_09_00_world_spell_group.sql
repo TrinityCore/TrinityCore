e811b9f2cc6b9360c3240fc92049e8c56f3992f8
Aokromes <Aokromes@users.noreply.github.com>
2014-03-09 19:48:43 +0100

    DB/Spells: Fix Thunderclap bugging Infected Wounds
    By untaught. Closes #6291.

---
+++ sql/updates/world/2014_03_09_00_world_spell_group.sql
@@ -0,0 +1,7 @@
+--
+UPDATE `spell_group` SET `spell_id`=58179 WHERE `id`=1061;
+DELETE FROM `spell_ranks` WHERE `first_spell_id`=58179;
+INSERT INTO `spell_ranks` (`first_spell_id`, `spell_id`, `rank`) VALUES
+(58179,58179,1), --Infected Wounds (Rank 1)
+(58179,58180,2), --Infected Wounds (Rank 2)
+(58179,58181,3); --Infected Wounds (Rank 3)
