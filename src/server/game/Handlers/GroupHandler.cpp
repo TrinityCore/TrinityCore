3fe557a56f2853eff061824de8455ea23eb21ebc
Dehravor <dehravor@gmail.com>
2014-03-16 19:12:01 +0100

    Core/Loot: Implement round robin for under threshold items in case of master loot
    Thanks @Shauren for pointing out

--- src/server/game/Handlers/GroupHandler.cpp
+++ src/server/game/Handlers/GroupHandler.cpp
@@ -438,6 +438,7 @@ void WorldSession::HandleLootMethodOpcode(WorldPacket& recvData)
 
     // everything's fine, do it
     group->SetLootMethod((LootMethod)lootMethod);
+    group->SetMasterLooterGuid(lootMaster);
     group->SetLooterGuid(lootMaster);
     group->SetLootThreshold((ItemQualities)lootThreshold);
     group->SendUpdate();
