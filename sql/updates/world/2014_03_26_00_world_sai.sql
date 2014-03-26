28af7cb17ae84e4996aee2a4b2828ac309d3dcc9
Gacko <gacko28@gmx.de>
2014-03-26 18:51:09 +0100

    Fix non-pch build and remove blank line

--- sql/updates/world/2014_03_26_00_world_sai.sql
+++ sql/updates/world/2014_03_26_00_world_sai.sql
@@ -3,4 +3,3 @@ DELETE FROM `smart_scripts` WHERE `entryorguid`=24601 AND `source_type`=0 AND `i
 DELETE FROM `spell_script_names` WHERE `spell_id`=46374;
 INSERT INTO `spell_script_names`(`spell_id`,`ScriptName`) VALUE
 (46374,'spell_windsoul_totem_aura');
-
