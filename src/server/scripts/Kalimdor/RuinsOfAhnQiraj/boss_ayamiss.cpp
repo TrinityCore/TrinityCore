6c8c84bdf9c7d20129b47720870176d221f845ee
jackpoz <giacomopoz@gmail.com>
2014-04-01 22:49:15 +0200

    Core/Misc: Fix uninitialized values
    Fix Position members not being always initialized.
    Valgrind log:
     at : Position::NormalizeOrientation(float) (Object.h:388)
     by : Position::SetOrientation(float) (Object.h:315)
     by : Position::Relocate(Position const&) (Object.h:310)
     by : spell_ulduar_stone_grip::spell_ulduar_stone_grip_AuraScript::OnRemoveVehicle(AuraEffect const*, AuraEffectHandleModes) (boss_kologarn.cpp:592)

--- src/server/scripts/Kalimdor/RuinsOfAhnQiraj/boss_ayamiss.cpp
+++ src/server/scripts/Kalimdor/RuinsOfAhnQiraj/boss_ayamiss.cpp
@@ -208,11 +208,13 @@ class boss_ayamiss : public CreatureScript
                             events.ScheduleEvent(EVENT_SWARMER_ATTACK, 60000);
                             break;
                         case EVENT_SUMMON_SWARMER:
+                        {
                             Position Pos;
                             me->GetRandomPoint(SwarmerPos, 80.0f, Pos);
                             me->SummonCreature(NPC_SWARMER, Pos);
                             events.ScheduleEvent(EVENT_SUMMON_SWARMER, 5000);
                             break;
+                        }
                         case EVENT_TRASH:
                             DoCastVictim(SPELL_TRASH);
                             events.ScheduleEvent(EVENT_TRASH, urand(5000, 7000));
