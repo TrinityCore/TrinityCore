335fe8e1b05edfe41d29fee92ee32f356270cff9
Warpten <vertozor@gmail.com>
2014-04-10 14:28:58 +0200

    Scripts/Ruby Sanctum: Fixed Halion engaging combat outside of his Flame Ring.

--- src/server/scripts/Northrend/ChamberOfAspects/RubySanctum/boss_halion.cpp
+++ src/server/scripts/Northrend/ChamberOfAspects/RubySanctum/boss_halion.cpp
@@ -394,6 +394,13 @@ class boss_halion : public CreatureScript
                 if (events.IsInPhase(PHASE_TWO))
                     return;
 
+                // Rough radius, it is not an exactly perfect circle
+                if (me->GetDistance2d(HalionControllerSpawnPos.GetPositionX(), HalionControllerSpawnPos.GetPositionY()) > 48.5f)
+                {
+                    EnterEvadeMode();
+                    return;
+                }
+
                 generic_halionAI::UpdateAI(diff);
             }
 
