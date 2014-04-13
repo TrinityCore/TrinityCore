f7c774cde8244486511296a84bda31786350513c
jackpoz <giacomopoz@gmail.com>
2014-04-13 15:01:20 +0200

    Scripts/Commands: Fix memory leak in ".gobject add" command
    Valgrind log:
     158 (104 direct, 54 indirect) bytes in 1 blocks are definitely lost in loss record 24 of 73
     at operator new(unsigned long) (vg_replace_malloc.c:319)
     by GameObjectModel::Create(GameObject const&) (GameObjectModel.cpp:152)
     by GameObject::Create(unsigned int, unsigned int, Map*, unsigned int, float, float, float, float, float, float, float, float, unsigned int, GOState, unsigned int) (GameObject.cpp:237)
     by gobject_commandscript::HandleGameObjectAddCommand(ChatHandler*, char const*) (cs_gobject.cpp:155)

--- src/server/scripts/Commands/cs_gobject.cpp
+++ src/server/scripts/Commands/cs_gobject.cpp
@@ -166,7 +166,9 @@ public:
 
         // fill the gameobject data and save to the db
         object->SaveToDB(map->GetId(), (1 << map->GetSpawnMode()), player->GetPhaseMaskForSpawn());
+        delete object;
 
+        object = new GameObject();
         // this will generate a new guid if the object is in an instance
         if (!object->LoadGameObjectFromDB(guidLow, map))
         {
