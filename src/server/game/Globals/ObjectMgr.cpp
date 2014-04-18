aa2e1e5ea04669413430063edaddfdba336ba24b
Vincent-Michael <Vincent_Michael_Michael@gmx.de>
2014-04-18 16:55:12 +0200

    Core/Misc: Added missing field in npc_text table for loading

--- src/server/game/Globals/ObjectMgr.cpp
+++ src/server/game/Globals/ObjectMgr.cpp
@@ -5236,6 +5236,8 @@ void ObjectMgr::LoadGossipText()
             gText.Options[i].Text_0           = fields[cic++].GetString();
             gText.Options[i].Text_1           = fields[cic++].GetString();
 
+			gText.Options[i].BroadcastTextID  = fields[cic++].GetUInt32(); // Need for correct loading order
+
             gText.Options[i].Language         = fields[cic++].GetUInt8();
             gText.Options[i].Probability      = fields[cic++].GetFloat();
 
