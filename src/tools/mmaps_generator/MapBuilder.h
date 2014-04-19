93a68a66c3009ea86569076ea05a39ce87dfb78d
jackpoz <giacomopoz@gmail.com>
2014-04-19 15:35:13 +0200

    Core/MMAPs: Improve NPC behavior with fences and high climbs
    Increase the max walkable angle and max walkable climb in MMAPs generator to allow NPCs to jump on fences and climb steps.
    This requires MMAPs to be extracted again with mmaps_generator, old extracted MMAPs are still compatible but will not provide the benefits of this commit.
    Fixes #9965

--- src/tools/mmaps_generator/MapBuilder.h
+++ src/tools/mmaps_generator/MapBuilder.h
@@ -61,7 +61,7 @@ namespace MMAP
     class MapBuilder
     {
         public:
-            MapBuilder(float maxWalkableAngle   = 55.f,
+            MapBuilder(float maxWalkableAngle   = 70.f,
                 bool skipLiquid          = false,
                 bool skipContinents      = false,
                 bool skipJunkMaps        = true,
