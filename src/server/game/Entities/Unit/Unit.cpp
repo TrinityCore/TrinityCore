db6890bb2096d15219bfd5a0059b4a363e7b00d5
ZackTsuna <Dreadkiller@gmx.de>
2014-04-20 17:50:26 +0200

    Core/Units: don't set power type if old power type is the same

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -7210,6 +7210,9 @@ bool Unit::HandleOverrideClassScriptAuraProc(Unit* victim, uint32 /*damage*/, Au
 
 void Unit::setPowerType(Powers new_powertype)
 {
+    if (getPowerType() == new_powertype)
+        return;
+
     SetByteValue(UNIT_FIELD_BYTES_0, 3, new_powertype);
 
     if (GetTypeId() == TYPEID_PLAYER)
