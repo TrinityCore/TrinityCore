cb47c319c5da75f4f99d1a92a4066264956c1845
Aokromes <Aokromes@users.noreply.github.com>
2014-04-20 19:10:50 +0200

    DB/Creature: Fix Earthcaller Ryga
    By untaught, closes #11827

---
+++ sql/updates/world/2014_04_20_02_creature_text.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `creature_text` WHERE `entry`=17123;
+INSERT INTO `creature_text` (`entry`, `groupid`, `id`, `text`, `type`, `language`, `probability`, `emote`, `duration`, `sound`, `comment`, `broadcasttextid`) VALUES
+(17123,0,0,'Welcome, kind spirit. What has brought you to us?',12,0,100,0,0,0,'',13512);
