TrinityCore/TrinityCore

commit 6bd36d394e5b120421c8ce6b893d90a29591089b

[TW292] - Core/Scripts: ToCh - Update ToCh scripts and make them way more trinitysh
Also fixes a crash when dismounting ToCh vehicles outside the instance

src/server/scripts/Custom/TrialOfTheChampion/TW_boss_black_knight.cpp
@@ -175,14 +175,11 @@ public:
             iveHadWorse = true;
             pAnnouncer = NULL;
 
-            if (instance)
-            {
-                if (GameObject* go = GameObject::GetGameObject(*me, instance->GetData64(DATA_MAIN_GATE)))
-                    instance->HandleGameObject(go->GetGUID(), false);
+            if (GameObject* go = GameObject::GetGameObject(*me, instance->GetData64(DATA_MAIN_GATE)))
+                instance->HandleGameObject(go->GetGUID(), false);
 
-                if (GameObject* go = GameObject::GetGameObject(*me, instance->GetData64(DATA_MAIN_GATE1)))
-                    instance->HandleGameObject(go->GetGUID(), true);
-            }
+            if (GameObject* go = GameObject::GetGameObject(*me, instance->GetData64(DATA_MAIN_GATE1)))
+                instance->HandleGameObject(go->GetGUID(), true);
 
             if (bEventInBattle)
             {
@@ -495,16 +492,13 @@ public:
                     varian->AI()->Talk(SAY_AGGRO_OUTRO);
             }
 
-            if (instance)
-            {
-                if (GameObject* go = GameObject::GetGameObject(*me, instance->GetData64(DATA_MAIN_GATE1)))
-                    instance->HandleGameObject(go->GetGUID(), false);
+            if (GameObject* gate = ObjectAccessor::GetGameObject(*me, instance->GetData64(DATA_MAIN_GATE1)))
+                instance->HandleGameObject(gate->GetGUID(), false);
 
-                if (GameObject* go = GameObject::GetGameObject(*me, instance->GetData64(DATA_MAIN_GATE)))
-                    instance->HandleGameObject(go->GetGUID(), false);
+            if (GameObject* gate = ObjectAccessor::GetGameObject(*me, instance->GetData64(DATA_MAIN_GATE)))
+                instance->HandleGameObject(gate->GetGUID(), false);
 
-                instance->SetData(BOSS_BLACK_KNIGHT, IN_PROGRESS);
-            }
+            instance->SetData(BOSS_BLACK_KNIGHT, IN_PROGRESS);
         }
 
         void KilledUnit(Unit* /*victim*/) OVERRIDE
@@ -558,21 +552,15 @@ public:
         {
             DoCast(me, SPELL_KILL_CREDIT);
             Talk(SAY_DEATH);
+            
             if (TempSummon* summ = me->ToTempSummon())
                 summ->SetTempSummonType(TEMPSUMMON_DEAD_DESPAWN);
 
-            if (instance)
-            {
-                instance->SetData(BOSS_BLACK_KNIGHT, DONE);
+            instance->SetData(BOSS_BLACK_KNIGHT, DONE);
+            instance->DoCastSpellOnPlayers(SPELL_KILL_CREDIT);
 
-                instance->DoCastSpellOnPlayers(SPELL_KILL_CREDIT);
-
-                // Instance encounter counting mechanics
-                //instance->UpdateEncounterState(ENCOUNTER_CREDIT_CAST_SPELL, 68663, me);
-
-                if (GameObject* go = GameObject::GetGameObject(*me, instance->GetData64(DATA_MAIN_GATE1)))
-                    instance->HandleGameObject(go->GetGUID(), true);
-            }
+            if (GameObject* gate = ObjectAccessor::GetGameObject(*me, instance->GetData64(DATA_MAIN_GATE1)))
+                instance->HandleGameObject(gate->GetGUID(), true);
         }
         private:
             EventMap _events;
@@ -580,7 +568,7 @@ public:
 
     CreatureAI* GetAI(Creature* creature) const
     {
-        return new TW_boss_black_knightAI (creature);
+        return GetTWTrialOfTheChampionAI<TW_boss_black_knightAI>(creature);
     }
 
 };
@@ -623,14 +611,14 @@ public:
         void SpellHitTarget(Unit* target, const SpellInfo* spell) OVERRIDE
         {
             if (target->GetTypeId() == TYPEID_PLAYER && (spell->Id == GHOUL_EXPLODE_DAMAGE || spell->Id == H_GHOUL_EXPLODE_DAMAGE || spell->Id == SPELL_GHOUL_EXPLODE))
-                if (Creature* knight = me->FindNearestCreature(NPC_BLACK_KNIGHT, 200.0f))
+                if (Creature* knight = ObjectAccessor::GetCreature(*me, instance->GetData64(DATA_BLACK_KNIGHT)))
                     knight->AI()->SetData(DATA_IVE_HAD_WORSE, false);
         }
     };
 
     CreatureAI* GetAI(Creature* creature) const
     {
-        return new TW_npc_risen_ghoulAI(creature);
+        return GetTWTrialOfTheChampionAI<TW_npc_risen_ghoulAI>(creature);
     }
 };
 
@@ -683,7 +671,7 @@ public:
 
     CreatureAI* GetAI(Creature* creature) const
     {
-        return new TW_npc_risen_announcerAI(creature);
+        return GetTWTrialOfTheChampionAI<TW_npc_risen_announcerAI>(creature);
     }
 };
 
@@ -753,7 +741,7 @@ public:
 
     CreatureAI* GetAI(Creature* creature) const
     {
-        return new TW_npc_black_knight_skeletal_gryphonAI(creature);
+        return GetTWTrialOfTheChampionAI<TW_npc_black_knight_skeletal_gryphonAI>(creature);
     }
 };
 

src/server/scripts/Custom/TrialOfTheChampion/TW_boss_argent_challenge.cpp
@@ -202,16 +202,13 @@ class TW_boss_eadric : public CreatureScript
                          return;
                 }
                 
-                if (instance)
-                {
-                    GameObject* GO = GameObject::GetGameObject(*me, instance->GetData64(DATA_MAIN_GATE1));
-                    if (GO)
-                        instance->HandleGameObject(GO->GetGUID(),true);
-                    Creature* announcer=pMap->GetCreature(instance->GetData64(DATA_ANNOUNCER));
-                    instance->SetData(DATA_ARGENT_SOLDIER_DEFEATED,0);
-                    announcer->SetFlag(UNIT_NPC_FLAGS, UNIT_NPC_FLAG_GOSSIP);
-                }
+                if (GameObject* gate = ObjectAccessor::GetGameObject(*me, instance->GetData64(DATA_MAIN_GATE1)))
+                    instance->HandleGameObject(gate->GetGUID(), true);
 
+                if (Creature* announcer = ObjectAccessor::GetCreature(*me, instance->GetData64(DATA_ANNOUNCER)))
+                    announcer->SetFlag(UNIT_NPC_FLAGS, UNIT_NPC_FLAG_GOSSIP);
+
+                instance->SetData(DATA_ARGENT_SOLDIER_DEFEATED, 0);
                 me->RemoveFromWorld();
             }
         }
@@ -231,12 +228,11 @@ class TW_boss_eadric : public CreatureScript
                 Talk(SAY_EADRIC_DEFEATED);
                 me->setFaction(35);
                 bDone = true;
-                if (GameObject* pGO = GameObject::GetGameObject(*me, instance->GetData64(DATA_MAIN_GATE)))
-                    instance->HandleGameObject(pGO->GetGUID(),true);
-                if (GameObject* pGO = GameObject::GetGameObject(*me, instance->GetData64(DATA_MAIN_GATE1)))
-                    instance->HandleGameObject(pGO->GetGUID(),true);
-                if (instance)
-                    instance->SetData(BOSS_ARGENT_CHALLENGE_E, DONE);
+                if (GameObject* gate = ObjectAccessor::GetGameObject(*me, instance->GetData64(DATA_MAIN_GATE)))
+                    instance->HandleGameObject(gate->GetGUID(),true);
+                if (GameObject* gate = ObjectAccessor::GetGameObject(*me, instance->GetData64(DATA_MAIN_GATE1)))
+                    instance->HandleGameObject(gate->GetGUID(),true);
+                instance->SetData(BOSS_ARGENT_CHALLENGE_E, DONE);
                 HandleInstanceBind(me);
             }
         }
@@ -283,8 +279,8 @@ class TW_boss_eadric : public CreatureScript
             {
                 me->GetMotionMaster()->MovePoint(0,746.843f, 695.68f, 412.339f);
                 bDone = false;
-                if (GameObject* pGO = GameObject::GetGameObject(*me, instance->GetData64(DATA_MAIN_GATE)))
-                    instance->HandleGameObject(pGO->GetGUID(),false);
+                if (GameObject* gate = ObjectAccessor::GetGameObject(*me, instance->GetData64(DATA_MAIN_GATE)))
+                    instance->HandleGameObject(gate->GetGUID(),false);
             } else uiResetTimer -= uiDiff;
 
             if (!UpdateVictim())
@@ -327,7 +323,7 @@ class TW_boss_eadric : public CreatureScript
 
     CreatureAI* GetAI(Creature* creature) const
     {
-        return new TW_boss_eadricAI(creature);
+        return GetTWTrialOfTheChampionAI<TW_boss_eadricAI>(creature);
     };
 };
 
@@ -392,16 +388,13 @@ class TW_boss_paletress : public CreatureScript
                         return;
                 }
 
-                if (pInstance)
-                {
-                    GameObject* GO = GameObject::GetGameObject(*me, pInstance->GetData64(DATA_MAIN_GATE1));
-                    if(GO)
-                       pInstance->HandleGameObject(GO->GetGUID(),true);
-                    Creature* announcer = pMap->GetCreature(pInstance->GetData64(DATA_ANNOUNCER));
-                    pInstance->SetData(DATA_ARGENT_SOLDIER_DEFEATED,0);
-                    announcer->SetFlag(UNIT_NPC_FLAGS, UNIT_NPC_FLAG_GOSSIP);
-                }
+                if (GameObject* gate = ObjectAccessor::GetGameObject(*me, pInstance->GetData64(DATA_MAIN_GATE1)))
+                    pInstance->HandleGameObject(gate->GetGUID(), true);
 
+                if (Creature* announcer = ObjectAccessor::GetCreature(*me, pInstance->GetData64(DATA_ANNOUNCER)))
+                    announcer->SetFlag(UNIT_NPC_FLAGS, UNIT_NPC_FLAG_GOSSIP);
+
+                pInstance->SetData(DATA_ARGENT_SOLDIER_DEFEATED, 0);
                 me->RemoveFromWorld();
             }
         }
@@ -437,14 +430,14 @@ class TW_boss_paletress : public CreatureScript
                 Talk(SAY_PALETRESS_DEFEATED);
                 me->setFaction(35);
                 bDone = true;
-                if (GameObject* pGO = GameObject::GetGameObject(*me, pInstance->GetData64(DATA_MAIN_GATE)))
-                    pInstance->HandleGameObject(pGO->GetGUID(),true);
-                if (GameObject* pGO = GameObject::GetGameObject(*me, pInstance->GetData64(DATA_MAIN_GATE1)))
-                    pInstance->HandleGameObject(pGO->GetGUID(),true);
+                if (GameObject* gate = ObjectAccessor::GetGameObject(*me, pInstance->GetData64(DATA_MAIN_GATE)))
+                    pInstance->HandleGameObject(gate->GetGUID(),true);
+                if (GameObject* gate = ObjectAccessor::GetGameObject(*me, pInstance->GetData64(DATA_MAIN_GATE1)))
+                    pInstance->HandleGameObject(gate->GetGUID(),true);
                 pInstance->SetData(BOSS_ARGENT_CHALLENGE_P, DONE);
                 HandleInstanceBind(me);
 
-                if (Creature* memory = me->GetMap()->ToInstanceMap()->GetCreature(MemoryGUID))
+                if (Creature* memory = ObjectAccessor::GetCreature(*me, MemoryGUID))
                     HandleSpellOnPlayersInInstanceToC5(memory, SPELL_CONFESSOR_ACHIEVEMENT);
             }
         }
@@ -453,7 +446,6 @@ class TW_boss_paletress : public CreatureScript
         {
             if (MovementType != POINT_MOTION_TYPE)
                 return;
-
         }
 
         void UpdateAI(uint32 uiDiff)
@@ -462,8 +454,8 @@ class TW_boss_paletress : public CreatureScript
             {
                 me->GetMotionMaster()->MovePoint(0, 746.843f, 695.68f, 412.339f);
                 bDone = false;
-                if (GameObject* pGO = GameObject::GetGameObject(*me, pInstance->GetData64(DATA_MAIN_GATE)))
-                    pInstance->HandleGameObject(pGO->GetGUID(),false);
+                if (GameObject* gate = ObjectAccessor::GetGameObject(*me, pInstance->GetData64(DATA_MAIN_GATE)))
+                    pInstance->HandleGameObject(gate->GetGUID(),false);
             } else uiResetTimer -= uiDiff;
 
             if (!UpdateVictim())
@@ -507,7 +499,7 @@ class TW_boss_paletress : public CreatureScript
                             DoCast(me,DUNGEON_MODE(SPELL_RENEW,SPELL_RENEW_H));
                             break;
                         case 1:
-                            if (Creature* pMemory = Unit::GetCreature(*me, MemoryGUID))
+                            if (Creature* pMemory = ObjectAccessor::GetCreature(*me, MemoryGUID))
                                 if (pMemory->IsAlive())
                                     DoCast(pMemory, DUNGEON_MODE(SPELL_RENEW,SPELL_RENEW_H));
                             break;
@@ -539,7 +531,7 @@ class TW_boss_paletress : public CreatureScript
 
     CreatureAI* GetAI(Creature* creature) const
     {
-        return new TW_boss_paletressAI(creature);
+        return GetTWTrialOfTheChampionAI<TW_boss_paletressAI>(creature);
     };
 };
 
@@ -613,7 +605,7 @@ class TW_npc_memory : public CreatureScript
 
     CreatureAI* GetAI(Creature* creature) const
     {
-        return new TW_npc_memoryAI(creature);
+        return GetTWTrialOfTheChampionAI<TW_npc_memoryAI>(creature);
     };
 };
 
@@ -631,8 +623,8 @@ class TW_npc_argent_soldier : public CreatureScript
             me->SetReactState(REACT_PASSIVE);
             me->SetFlag(UNIT_FIELD_FLAGS,UNIT_FLAG_NON_ATTACKABLE);
             me->SetFlag(UNIT_FIELD_FLAGS,UNIT_FLAG_NOT_SELECTABLE);
-            if (GameObject* pGO = GameObject::GetGameObject(*me, pInstance->GetData64(DATA_MAIN_GATE)))
-                pInstance->HandleGameObject(pGO->GetGUID(),true);
+            if (GameObject* gate = ObjectAccessor::GetGameObject(*me, pInstance->GetData64(DATA_MAIN_GATE)))
+                pInstance->HandleGameObject(gate->GetGUID(),true);
             SetDespawnAtEnd(false);
             uiWaypoint = 0;
             bStarted = false;
@@ -705,8 +697,8 @@ class TW_npc_argent_soldier : public CreatureScript
                         me->SetReactState(REACT_AGGRESSIVE);
                         me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE|UNIT_FLAG_NOT_SELECTABLE);
                         bStarted = true;
-                        if (GameObject* pGO = GameObject::GetGameObject(*me, pInstance->GetData64(DATA_MAIN_GATE)))
-                            pInstance->HandleGameObject(pGO->GetGUID(),false);
+                        if (GameObject* gate = ObjectAccessor::GetGameObject(*me, pInstance->GetData64(DATA_MAIN_GATE)))
+                            pInstance->HandleGameObject(gate->GetGUID(),false);
                         break;
                 }
 
@@ -850,14 +842,13 @@ class TW_npc_argent_soldier : public CreatureScript
 
         void JustDied(Unit* killer) OVERRIDE
         {
-            if (pInstance)
-                pInstance->SetData(DATA_ARGENT_SOLDIER_DEFEATED,pInstance->GetData(DATA_ARGENT_SOLDIER_DEFEATED) + 1);
+            pInstance->SetData(DATA_ARGENT_SOLDIER_DEFEATED,pInstance->GetData(DATA_ARGENT_SOLDIER_DEFEATED) + 1);
         }
     };
 
     CreatureAI* GetAI(Creature* creature) const
     {
-        return new TW_npc_argent_soldierAI(creature);
+        return GetTWTrialOfTheChampionAI<TW_npc_argent_soldierAI>(creature);
     };
 };
 

src/server/scripts/Custom/TrialOfTheChampion/TW_boss_grand_champions.cpp
@@ -262,9 +262,6 @@ class TW_generic_vehicleAI_toc5 : public CreatureScript
 
         void WaypointReached(uint32 i) OVERRIDE
         {
-            if (!instance)
-                return;
-
             switch(i)
             {
                 case 2:
@@ -456,7 +453,7 @@ class TW_generic_vehicleAI_toc5 : public CreatureScript
 
     CreatureAI* GetAI(Creature* creature) const
     {
-        return new TW_generic_vehicleAI_toc5AI(creature);
+        return GetTWTrialOfTheChampionAI<TW_generic_vehicleAI_toc5AI>(creature);
     }
 };
 
@@ -531,11 +528,11 @@ class TW_boss_warrior_toc5 : public CreatureScript
                 Talk(WARNING_WEAPONS);
                 me->RemoveAura(64723); // [DND] ReadyJoust Pose Effect	
 
-                if (instance && me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_1))
+                if (me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_1))
                     me->SetHomePosition(739.678f, 662.541f, 413.395f, 4.49f);
-                else if (instance && me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_2))
+                else if (me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_2))
                          me->SetHomePosition(746.71f, 661.02f, 412.695f, 4.6f);
-                else if (instance && me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_3))
+                else if (me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_3))
                          me->SetHomePosition(754.34f, 660.70f, 413.395f, 4.79f);
 
                 me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
@@ -597,8 +594,7 @@ class TW_boss_warrior_toc5 : public CreatureScript
                 hasBeenInCombat = false;
                 Talk(SAY_CHAMPION_DEFEAT);
 
-                if (instance)
-                    instance->SetData(BOSS_GRAND_CHAMPIONS, DONE);
+                instance->SetData(BOSS_GRAND_CHAMPIONS, DONE);
 
                 // Instance encounter counting mechanics
                 if (!bCredit)
@@ -613,17 +609,11 @@ class TW_boss_warrior_toc5 : public CreatureScript
                 HandleInstanceBind(me);
             }
         }
-
-        void JustDied(Unit* /*killer*/)
-        {
-           if (instance)
-                instance->SetData(BOSS_GRAND_CHAMPIONS, DONE);
-        }
     };
 
     CreatureAI* GetAI(Creature* creature) const
     {
-        return new TW_boss_warrior_toc5AI(creature);
+        return GetTWTrialOfTheChampionAI<TW_boss_warrior_toc5AI>(creature);
     }
 };
 
@@ -693,15 +683,14 @@ class TW_boss_mage_toc5 : public CreatureScript
                 bDone = true;
                 me->RemoveAura(64723); // [DND] ReadyJoust Pose Effect
 
-                if (instance && me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_1))
+                if (me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_1))
                     me->SetHomePosition(739.678f, 662.541f, 413.395f, 4.49f);
-                else if (instance && me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_2))
+                else if (me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_2))
                          me->SetHomePosition(746.71f, 661.02f, 412.695f, 4.6f);
-                else if (instance && me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_3))
+                else if (me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_3))
                          me->SetHomePosition(754.34f, 660.70f, 413.395f, 4.79f);
-
-                if (instance)
-                    instance->SetData(BOSS_GRAND_CHAMPIONS, IN_PROGRESS);
+                
+                instance->SetData(BOSS_GRAND_CHAMPIONS, IN_PROGRESS);
 
                 me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
 
@@ -757,8 +746,7 @@ class TW_boss_mage_toc5 : public CreatureScript
                 hasBeenInCombat = false;
                 Talk(SAY_CHAMPION_DEFEAT);
 
-                if (instance)
-                    instance->SetData(BOSS_GRAND_CHAMPIONS, DONE);
+                instance->SetData(BOSS_GRAND_CHAMPIONS, DONE);
 
                 // Instance encounter counting mechanics
                 if (!bCredit)
@@ -776,7 +764,7 @@ class TW_boss_mage_toc5 : public CreatureScript
 
     CreatureAI* GetAI(Creature* creature) const
     {
-        return new TW_boss_mage_toc5AI(creature);
+        return GetTWTrialOfTheChampionAI<TW_boss_mage_toc5AI>(creature);
     };
 };
 
@@ -852,15 +840,14 @@ class TW_boss_shaman_toc5 : public CreatureScript
 
                 me->RemoveAura(64723); // [DND] ReadyJoust Pose Effect
 
-                if (instance && me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_1))
+                if (me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_1))
                     me->SetHomePosition(739.678f,662.541f,413.395f,4.49f);
-                else if (instance && me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_2))
+                else if (me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_2))
                          me->SetHomePosition(746.71f,661.02f,412.695f,4.6f);
-                else if (instance && me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_3))
+                else if (me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_3))
                          me->SetHomePosition(754.34f,660.70f,413.395f,4.79f);
 
-                if (instance)
-                    instance->SetData(BOSS_GRAND_CHAMPIONS, IN_PROGRESS);
+                instance->SetData(BOSS_GRAND_CHAMPIONS, IN_PROGRESS);
 
                 EnterEvadeMode();
                 bHome = true;
@@ -876,12 +863,12 @@ class TW_boss_shaman_toc5 : public CreatureScript
                 switch (eventId)
                 {
                     case EVENT_EARTH_SHIELD:
-                        DoCast(me,SPELL_EARTH_SHIELD);
+                        DoCast(me, SPELL_EARTH_SHIELD);
                         events.ScheduleEvent(EVENT_EARTH_SHIELD, urand(40000,45000), 0, PHASE_COMBAT);
                         break;
                     case EVENT_CHAIN_LIGHTNING:
                         if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM,0))
-                            DoCast(target,DUNGEON_MODE(SPELL_CHAIN_LIGHTNING,SPELL_CHAIN_LIGHTNING_H));
+                            DoCast(target, DUNGEON_MODE(SPELL_CHAIN_LIGHTNING, SPELL_CHAIN_LIGHTNING_H));
                         events.ScheduleEvent(EVENT_CHAIN_LIGHTNING, 23000, 0, PHASE_COMBAT);
                         break;
                     case EVENT_HEALING_WAVE:
@@ -920,8 +907,7 @@ class TW_boss_shaman_toc5 : public CreatureScript
                 hasBeenInCombat = false;
                 Talk(SAY_CHAMPION_DEFEAT);
 
-                if (instance)
-                    instance->SetData(BOSS_GRAND_CHAMPIONS, DONE);
+                instance->SetData(BOSS_GRAND_CHAMPIONS, DONE);
 
                 // Instance encounter counting mechanics
                 if (!bCredit)
@@ -940,7 +926,7 @@ class TW_boss_shaman_toc5 : public CreatureScript
 
     CreatureAI* GetAI(Creature* creature) const
     {
-        return new TW_boss_shaman_toc5AI(creature);
+        return GetTWTrialOfTheChampionAI<TW_boss_shaman_toc5AI>(creature);
     }
 };
 
@@ -990,15 +976,10 @@ class TW_boss_hunter_toc5 : public CreatureScript
                         return;
                 }
 
-                if (instance)
-                    instance->SetData(BOSS_GRAND_CHAMPIONS, NOT_STARTED);
+                instance->SetData(BOSS_GRAND_CHAMPIONS, NOT_STARTED);
 
-                if (instance)
-                {
-                    GameObject* GO = GameObject::GetGameObject(*me, instance->GetData64(DATA_MAIN_GATE1));
-                    if (GO)
-                        instance->HandleGameObject(GO->GetGUID(),true);
-                }
+                if (GameObject* gate = GameObject::GetGameObject(*me, instance->GetData64(DATA_MAIN_GATE1)))
+                    instance->HandleGameObject(gate->GetGUID(), true);
 
                 me->RemoveFromWorld();
                 events.SetPhase(PHASE_IDLE);
@@ -1038,15 +1019,14 @@ class TW_boss_hunter_toc5 : public CreatureScript
 
                 me->RemoveAura(64723); // [DND] ReadyJoust Pose Effect
 
-                if (instance && me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_1))
+                if (me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_1))
                     me->SetHomePosition(739.678f,662.541f,413.395f,4.49f);
-                else if (instance && me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_2))
+                else if (me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_2))
                          me->SetHomePosition(746.71f,661.02f,412.695f,4.6f);
-                else if (instance && me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_3))
+                else if (me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_3))
                          me->SetHomePosition(754.34f,660.70f,413.395f,4.79f);
 
-                if (instance)
-                    instance->SetData(BOSS_GRAND_CHAMPIONS, IN_PROGRESS);
+                instance->SetData(BOSS_GRAND_CHAMPIONS, IN_PROGRESS);
 
                 EnterEvadeMode();
                 bHome = true;
@@ -1132,8 +1112,7 @@ class TW_boss_hunter_toc5 : public CreatureScript
                 hasBeenInCombat = false;
                 Talk(SAY_CHAMPION_DEFEAT);
 
-                if (instance)
-                    instance->SetData(BOSS_GRAND_CHAMPIONS, DONE);
+                instance->SetData(BOSS_GRAND_CHAMPIONS, DONE);
 
                 // Instance encounter counting mechanics
                 if (!bCredit)
@@ -1152,7 +1131,7 @@ class TW_boss_hunter_toc5 : public CreatureScript
 
     CreatureAI* GetAI(Creature* creature) const
     {
-        return new TW_boss_hunter_toc5AI(creature);
+        return GetTWTrialOfTheChampionAI<TW_boss_hunter_toc5AI>(creature);
     }
 };
 
@@ -1198,15 +1177,10 @@ class TW_boss_rogue_toc5 : public CreatureScript
                         return;
                 }
 
-                if (instance)
-                    instance->SetData(BOSS_GRAND_CHAMPIONS, NOT_STARTED);
+                instance->SetData(BOSS_GRAND_CHAMPIONS, NOT_STARTED);
 
-                if (instance)
-                {
-                    GameObject* GO = GameObject::GetGameObject(*me, instance->GetData64(DATA_MAIN_GATE1));
-                    if (GO)
-                        instance->HandleGameObject(GO->GetGUID(),true);
-                }
+                if (GameObject* gate = GameObject::GetGameObject(*me, instance->GetData64(DATA_MAIN_GATE1)))
+                    instance->HandleGameObject(gate->GetGUID(),true);
 
                 events.SetPhase(PHASE_IDLE);
                 me->RemoveFromWorld();
@@ -1246,15 +1220,14 @@ class TW_boss_rogue_toc5 : public CreatureScript
 
                 me->RemoveAura(64723); // [DND] ReadyJoust Pose Effect
 
-                if (instance && me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_1))
+                if (me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_1))
                     me->SetHomePosition(739.678f,662.541f,413.395f,4.49f);
-                else if (instance && me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_2))
+                else if (me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_2))
                          me->SetHomePosition(746.71f,661.02f,412.695f,4.6f);
-                else if (instance && me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_3))
+                else if (me->GetGUID() == instance->GetData64(DATA_GRAND_CHAMPION_3))
                          me->SetHomePosition(754.34f,660.70f,413.395f,4.79f);
 
-                if (instance)
-                    instance->SetData(BOSS_GRAND_CHAMPIONS, IN_PROGRESS);
+                instance->SetData(BOSS_GRAND_CHAMPIONS, IN_PROGRESS);
 
                 me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
 
@@ -1305,8 +1278,7 @@ class TW_boss_rogue_toc5 : public CreatureScript
                 hasBeenInCombat = false;
                 Talk(SAY_CHAMPION_DEFEAT);
 
-                if (instance)
-                    instance->SetData(BOSS_GRAND_CHAMPIONS, DONE);
+                instance->SetData(BOSS_GRAND_CHAMPIONS, DONE);
 
                 // Instance encounter counting mechanics
                 if (!bCredit)
@@ -1325,7 +1297,7 @@ class TW_boss_rogue_toc5 : public CreatureScript
 
     CreatureAI* GetAI(Creature* creature) const
     {
-        return new TW_boss_rogue_toc5AI(creature);
+        return GetTWTrialOfTheChampionAI<TW_boss_rogue_toc5AI>(creature);
     }
 };
 

commit bb74bda66c8174f2847dc497d4a1e9cca837fe8f

[TW295] - Core/Scripts: ToC5 - Fixes crash introduced with https://github.com/TrueWoW/TrinityCore/commit/6bd36d394e5b120421c8ce6b893d90a29591089b
Needs to be further investigated why the data is returning a null value

src/server/scripts/Custom/TrialOfTheChampion/TW_boss_black_knight.cpp
@@ -611,7 +611,7 @@ public:
         void SpellHitTarget(Unit* target, const SpellInfo* spell) OVERRIDE
         {
             if (target->GetTypeId() == TYPEID_PLAYER && (spell->Id == GHOUL_EXPLODE_DAMAGE || spell->Id == H_GHOUL_EXPLODE_DAMAGE || spell->Id == SPELL_GHOUL_EXPLODE))
-                if (Creature* knight = ObjectAccessor::GetCreature(*me, instance->GetData64(DATA_BLACK_KNIGHT)))
+                if (Creature* knight = me->FindNearestCreature(NPC_BLACK_KNIGHT, 200.0f))
                     knight->AI()->SetData(DATA_IVE_HAD_WORSE, false);
         }
     };