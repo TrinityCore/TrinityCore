599f5cfd953bacff6001fd544eef8ae6b4be4869
Vincent-Michael <Vincent_Michael@gmx.de>
2014-04-28 09:19:37 +0200

    Core/Creature: Missed changes in 345ce2caf9f37be2dfa31e8bddf4122faa189032

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -15952,10 +15952,7 @@ void Unit::RestoreFaction()
         }
 
         if (CreatureTemplate const* cinfo = ToCreature()->GetCreatureTemplate())  // normal creature
-        {
-            FactionTemplateEntry const* faction = GetFactionTemplateEntry();
-            setFaction((faction && faction->friendlyMask & 0x004) ? cinfo->faction : cinfo->faction);
-        }
+            setFaction(cinfo->faction);
     }
 }
 
