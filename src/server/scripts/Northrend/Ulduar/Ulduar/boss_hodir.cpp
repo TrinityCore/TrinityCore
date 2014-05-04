3c1e29960a0749233064da50b429fe396b6fc598
Nyeriah <sarah.trysan@live.com>
2014-05-04 20:53:04 -0300

    [TW302] - Core/Scripts: Ulduar - add a probably futile check that 'may' fix Hodir's encounter state not being set properly after completed
    as Reset() is called when he turns friendly

--- src/server/scripts/Northrend/Ulduar/Ulduar/boss_hodir.cpp
+++ src/server/scripts/Northrend/Ulduar/Ulduar/boss_hodir.cpp
@@ -324,6 +324,9 @@ class boss_hodir : public CreatureScript
 
             void Reset() override
             {
+                if (instance->GetBossState(BOSS_HODIR) == DONE)
+                    return;
+
                 _Reset();
                 me->SetReactState(REACT_PASSIVE);
 
