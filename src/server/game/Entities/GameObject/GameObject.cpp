87cd5dce2f43b8fd1c70ec97075b0c4a5be889ef
Gacko <gacko28@gmx.de>
2014-05-05 18:39:53 +0200

    Core/GameObject: Store unit in GameObject::SetLootState - required for trap activation.

--- src/server/game/Entities/GameObject/GameObject.cpp
+++ src/server/game/Entities/GameObject/GameObject.cpp
@@ -2015,6 +2015,7 @@ void GameObject::SetDestructibleState(GameObjectDestructibleState state, Player*
 void GameObject::SetLootState(LootState state, Unit* unit)
 {
     m_lootState = state;
+    m_lootStateUnit = unit;
     AI()->OnStateChanged(state, unit);
     sScriptMgr->OnGameObjectLootStateChanged(this, state, unit);
     if (m_model)
