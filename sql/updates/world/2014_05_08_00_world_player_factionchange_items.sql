9ee64f4daa52f59316be09403f8507b2c8f5d384
cr4st <crest@mwow.eu>
2014-05-08 08:08:11 +0200

    [DB/FactionchangeItems] T9 warrior (alliance) to T9 shaman (horde)
    T9 alliance with a horde character you'll get the t9 shaman horde in the
    fraction change.

---
+++ sql/updates/world/2014_05_08_00_world_player_factionchange_items.sql
@@ -0,0 +1,29 @@
+--  T9 Warrior (Alliance) to T9 Shaman (Horde) is not correct-- 
+-- alliance_id 48357 Thrall's Grips of Triumph (H) - horde_id 48384 - Wrynn's Gauntlets of Triumph (H)
+DELETE FROM `player_factionchange_items` WHERE `alliance_id` = 48357 AND `horde_id` = 48384;
+-- alliance_id 48358 Thrall's Faceguard of Triumph (H) - horde_id 48383 - Wrynn's Helmet of Triumph (H)
+DELETE FROM `player_factionchange_items` WHERE `alliance_id` = 48358 AND `horde_id` = 48383;
+-- alliance_id 48359 Thrall's War-Kilt of Triumph (H) - horde_id 48382 - Wrynn's Legplates of Triumph (H)
+DELETE FROM `player_factionchange_items` WHERE `alliance_id` = 48359 AND `horde_id` = 48382;
+-- alliance_id 48360 Thrall's Shoulderguards of Triumph (H) - horde_id 48381 - Wrynn's Shoulderplates of Triumph (H)
+DELETE FROM `player_factionchange_items` WHERE `alliance_id` = 48360 AND `horde_id` = 48381;
+-- alliance_id 48361 Thrall's Shoulderguards of Triumph - horde_id 48380 - Wrynn's Shoulderplates of Triumph
+DELETE FROM `player_factionchange_items` WHERE `alliance_id` = 48361 AND `horde_id` = 48380;
+-- alliance_id 48362 Thrall's War-Kilt of Triumph - horde_id 48379 - Wrynn's Legplates of Triumph
+DELETE FROM `player_factionchange_items` WHERE `alliance_id` = 48362 AND `horde_id` = 48379;
+-- alliance_id 48363 Thrall's Faceguard of Triumph - horde_id 48378 - Wrynn's Helmet of Triumph
+DELETE FROM `player_factionchange_items` WHERE `alliance_id` = 48363 AND `horde_id` = 48378;
+-- alliance_id 48364 Thrall's Grips of Triumph - horde_id 48377 - Wrynn's Gauntlets of Triumph
+DELETE FROM `player_factionchange_items` WHERE `alliance_id` = 48364 AND `horde_id` = 48377;
+-- alliance_id 48365 Thrall's Chestguard of Triumph - horde_id 48376 - Wrynn's Battleplate of Triumph
+DELETE FROM `player_factionchange_items` WHERE `alliance_id` = 48365 AND `horde_id` = 48376;
+-- alliance_id 48366 Thrall's Chestguard of Conquest - horde_id 48372 - Wrynn's Battleplate of Conquest
+DELETE FROM `player_factionchange_items` WHERE `alliance_id` = 48366 AND `horde_id` = 48372;
+-- alliance_id 48367 Thrall's Grips of Conquest - horde_id 48375 - Wrynn's Gauntlets of Conquest
+DELETE FROM `player_factionchange_items` WHERE `alliance_id` = 48367 AND `horde_id` = 48375;
+-- alliance_id 48368 Thrall's Faceguard of Conquest - horde_id 48371 - Wrynn's Helmet of Conquest
+DELETE FROM `player_factionchange_items` WHERE `alliance_id` = 48368 AND `horde_id` = 48371;
+-- alliance_id 48369 Thrall's War-Kilt of Conquest - horde_id 48373 - Wrynn's Legplates of Conquest
+DELETE FROM `player_factionchange_items` WHERE `alliance_id` = 48369 AND `horde_id` = 48373;
+-- alliance_id 48370 Thrall's Shoulderguards of Conquest - horde_id 48374 - Wrynn's Shoulderplates of Conquest
+DELETE FROM `player_factionchange_items` WHERE `alliance_id` = 48370 AND `horde_id` = 48374;
