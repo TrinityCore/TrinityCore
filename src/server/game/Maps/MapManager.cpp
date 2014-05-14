9b336ddadc4f4ea60eaf855e125e58c1e68df7bc
Rochet2 <tqbattlenet@gmail.com>
2014-05-14 15:08:50 +0300

    Eluna

--- src/server/game/Maps/MapManager.cpp
+++ src/server/game/Maps/MapManager.cpp
@@ -51,7 +51,7 @@ void MapManager::Initialize()
     Map::InitStateMachine();
 
     int num_threads(sWorld->getIntConfig(CONFIG_NUMTHREADS));
-    // Start mtmaps if needed.
+#if ELUNA
     if (num_threads > 1)
     {
         // Force 1 thread for Eluna as lua is single threaded. By default thread count is 1
@@ -59,6 +59,8 @@ void MapManager::Initialize()
         TC_LOG_ERROR("maps", "Map update threads set to %i, when Eluna only allows 1, changing to 1", num_threads);
         num_threads = 1;
     }
+#endif
+    // Start mtmaps if needed.
     if (num_threads > 0 && m_updater.activate(num_threads) == -1)
         abort();
 }
