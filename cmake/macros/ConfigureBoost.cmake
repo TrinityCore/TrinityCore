896ce0caf341114c974332a0abb06e25b183ad30
leak <leak@bitmx.net>
2014-05-17 20:27:39 +0200

    Replace authserver ACE related code with Boost/C++11

--- cmake/macros/ConfigureBoost.cmake
+++ cmake/macros/ConfigureBoost.cmake
@@ -1,3 +1,13 @@
+macro(get_WIN32_WINNT version)
+    if (WIN32 AND CMAKE_SYSTEM_VERSION)
+        set(ver ${CMAKE_SYSTEM_VERSION})
+        string(REPLACE "." "" ver ${ver})
+        string(REGEX REPLACE "([0-9])" "0\\1" ver ${ver})
+
+        set(${version} "0x${ver}")
+    endif()
+endmacro()
+
 if(WIN32)
  set(BOOST_DEBUG ON)
   if(DEFINED ENV{BOOST_ROOT})
@@ -12,9 +22,12 @@ if(WIN32)
   set(Boost_USE_STATIC_LIBS        ON)
   set(Boost_USE_MULTITHREADED      ON)
   set(Boost_USE_STATIC_RUNTIME     OFF)
+  
+  get_WIN32_WINNT(ver)
+  add_definitions(-D_WIN32_WINNT=${ver})
 endif()
 
-find_package(Boost 1.55 REQUIRED atomic chrono date_time exception system thread)
+find_package(Boost 1.55 REQUIRED atomic chrono date_time exception regex system thread)
 
 if(Boost_FOUND)
   include_directories(${Boost_INCLUDE_DIRS})
