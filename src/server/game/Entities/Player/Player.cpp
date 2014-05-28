718ca32f7e2210c6ce3178801a862f02cd3956d9
Trisjdc <trisjdc@gmail.com>
2014-05-28 22:05:34 +0100

    Core/Players: Fix GetNPCIfCanInteractWith when dealing with creatures of neutral reputation/forced friendly reputation

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -2741,11 +2741,8 @@ Creature* Player::GetNPCIfCanInteractWith(uint64 guid, uint32 npcflagmask)
         return NULL;
 
     // not unfriendly
-    if (FactionTemplateEntry const* factionTemplate = sFactionTemplateStore.LookupEntry(creature->getFaction()))
-        if (factionTemplate->faction)
-            if (FactionEntry const* faction = sFactionStore.LookupEntry(factionTemplate->faction))
-                if (faction->reputationListID >= 0 && GetReputationMgr().GetRank(faction) <= REP_UNFRIENDLY)
-                    return NULL;
+    if (creature->GetReactionTo(this) <= REP_HOSTILE)
+        return NULL;
 
     // not too far
     if (!creature->IsWithinDistInMap(this, INTERACTION_DISTANCE))
