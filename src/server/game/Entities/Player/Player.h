ab62fe30bc399fcc717d0c37df220d7b57a7c043
Ankso <misterankso@gmail.com>
2014-05-30 01:59:38 +0200

    Core/Players: Update the player's zone and area after update the player's position server side.

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -1675,7 +1675,8 @@ class Player : public Unit, public GridObject<Player>
         void UpdatePvP(bool state, bool override=false);
         void UpdateZone(uint32 newZone, uint32 newArea);
         void UpdateArea(uint32 newArea);
-
+        void SetNeedsZoneUpdate(bool needsUpdate) { m_needsZoneUpdate = needsUpdate; }
+        
         void UpdateZoneDependentAuras(uint32 zone_id);    // zones
         void UpdateAreaDependentAuras(uint32 area_id);    // subzones
 
@@ -2561,6 +2562,8 @@ class Player : public Unit, public GridObject<Player>
         bool IsAlwaysDetectableFor(WorldObject const* seer) const;
 
         uint8 m_grantableLevels;
+        
+        bool m_needsZoneUpdate;
 
     private:
         // internal common parts for CanStore/StoreItem functions
