87b1045de02ddb6c0767be38f9ec9ac18d19ff40
Subv <subv2112@gmail.com>
2014-06-02 15:53:54 -0500

    Core/Phases: Added missing sql and changed how phase_definitions will work, implementation still missing

--- src/server/game/Globals/ObjectMgr.h
+++ src/server/game/Globals/ObjectMgr.h
@@ -151,10 +151,8 @@ struct PhaseDefinition
 {
     uint32 zoneId;
     uint32 entry;
-    uint32 phasemask;
     uint32 phaseId;
-    uint32 terrainswapmap;
-    uint8 flags;
+    uint32 phaseGroup;
 };
 
 typedef std::list<PhaseDefinition> PhaseDefinitionContainer;
