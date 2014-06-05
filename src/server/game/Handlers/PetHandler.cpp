db2d9d9ee07a0ed64bea8cd36673a3ac1ca7abd7
jackpoz <giacomopoz@gmail.com>
2014-06-05 22:50:11 +0200

    Core/Talents: Align server checks to Client limits
    Set the max amount of talents to process in CMSG_LEARN_PREVIEW_TALENTS_PET to 30 ( 24 talents rounded up ).

--- src/server/game/Handlers/PetHandler.cpp
+++ src/server/game/Handlers/PetHandler.cpp
@@ -868,7 +868,10 @@ void WorldSession::HandleLearnPreviewTalentsPet(WorldPacket& recvData)
 
     uint32 talentId, talentRank;
 
-    for (uint32 i = 0; i < talentsCount; ++i)
+    // Client has max 24 talents, rounded up : 30
+    const int MaxTalentsCount = 30;
+
+    for (uint32 i = 0; i < talentsCount && i < MaxTalentsCount; ++i)
     {
         recvData >> talentId >> talentRank;
 
@@ -876,4 +879,6 @@ void WorldSession::HandleLearnPreviewTalentsPet(WorldPacket& recvData)
     }
 
     _player->SendTalentsInfoData(true);
+
+    recvData.rfinish();
 }
