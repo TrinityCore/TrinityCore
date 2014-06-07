91052574acb49a791ebfc958c0776e19dd578a71
Trisjdc <trisjdc@gmail.com>
2014-06-07 23:43:11 +0100

    Core/Auras: Expanding functionality of SPELL_GROUP_STACK_RULE_EXCLUSIVE_SAME_EFFECT to be able to take care of aura effects inside their handlers
    Currently handled:
    - SPELL_AURA_MOD_RESISTANCE_PCT
    - SPELL_AURA_MOD_STAT
    - SPELL_AURA_MELEE_SLOW
    - SPELL_AURA_MOD_MELEE_HASTE
    - SPELL_AURA_MOD_DAMAGE_PERCENT_DONE
    
    Necessary to allow stack rules such as BoK/BoS/Bok-Drums and others to be all in a target with all of the aura effects, yet with only the 'highest' effect active

--- src/server/game/Entities/Unit/Unit.h
+++ src/server/game/Entities/Unit/Unit.h
@@ -2152,6 +2152,8 @@ class Unit : public WorldObject
         time_t GetLastDamagedTime() const { return _lastDamagedTime; }
         void SetLastDamagedTime(time_t val) { _lastDamagedTime = val; }
 
+        int32 GetHighestExclusiveSameEffectSpellGroupValue(AuraEffect const* aurEff, AuraType auraType, bool sameMiscValue = false) const;
+
     protected:
         explicit Unit (bool isWorldObject);
 
