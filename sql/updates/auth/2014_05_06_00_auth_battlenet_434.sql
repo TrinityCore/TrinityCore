98258679e9bd30a7b7255fea043923bfcc5ddcf9
Shauren <shauren.trinity@gmail.com>
2014-06-11 20:54:18 +0200

    SQL: Reordered statements to fix import

--- sql/updates/auth/2014_05_06_00_auth_battlenet_434.sql
+++ sql/updates/auth/2014_05_06_00_auth_battlenet_434.sql
@@ -1,7 +1,3 @@
-ALTER TABLE `account`
-  ADD `battlenet_account` int(10) unsigned DEFAULT NULL AFTER `recruiter`,
-  ADD CONSTRAINT `fk_bnet_acc` FOREIGN KEY (`battlenet_account`) REFERENCES `battlenet_accounts` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
 ALTER TABLE `realmlist`
   ADD `Region` tinyint(3) UNSIGNED NOT NULL DEFAULT 2 AFTER `gamebuild`,
   ADD `Battlegroup` tinyint(3) UNSIGNED NOT NULL DEFAULT 0 AFTER `Region`;
@@ -146,3 +142,6 @@ INSERT INTO `battlenet_modules` VALUES
 /*!40000 ALTER TABLE `battlenet_modules` ENABLE KEYS */;
 UNLOCK TABLES;
 
+ALTER TABLE `account`
+  ADD `battlenet_account` int(10) unsigned DEFAULT NULL AFTER `recruiter`,
+  ADD CONSTRAINT `fk_bnet_acc` FOREIGN KEY (`battlenet_account`) REFERENCES `battlenet_accounts` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
