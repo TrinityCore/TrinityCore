640abe2e52a16eb80b309055c6d10d1821fd422d
Trisjdc <trisjdc@gmail.com>
2014-06-12 20:13:07 +0100

    Quick fixup for previous commit

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -12888,7 +12888,7 @@ void Unit::ModSpellCastTime(SpellInfo const* spellInfo, int32 & castTime, Spell*
     if (!spellInfo || castTime < 0)
         return;
 
-    if (spellInfo->IsChanneled() && !spellInfo->AttributesEx5 & SPELL_ATTR5_HASTE_AFFECT_DURATION)
+    if (spellInfo->IsChanneled() && !(spellInfo->AttributesEx5 & SPELL_ATTR5_HASTE_AFFECT_DURATION))
         return;
 
     // called from caster
