11d545f01d958f19861a9b50003ed7e7f05d794d
Subv <subv2112@gmail.com>
2014-06-22 13:46:21 -0500

    Core/Build: Fixed build.

--- src/server/game/AI/CreatureAIFactory.h
+++ src/server/game/AI/CreatureAIFactory.h
@@ -49,7 +49,6 @@ CreatureAIFactory<REAL_AI>::Create(void* data) const
 
 typedef FactoryHolder<CreatureAI> CreatureAICreator;
 typedef FactoryHolder<CreatureAI>::FactoryHolderRegistry CreatureAIRegistry;
-typedef FactoryHolder<CreatureAI>::FactoryHolderRepository CreatureAIRepository;
 
 //GO
 struct SelectableGameObjectAI : public FactoryHolder<GameObjectAI>, public Permissible<GameObject>
@@ -77,5 +76,4 @@ GameObjectAIFactory<REAL_GO_AI>::Create(void* data) const
 
 typedef FactoryHolder<GameObjectAI> GameObjectAICreator;
 typedef FactoryHolder<GameObjectAI>::FactoryHolderRegistry GameObjectAIRegistry;
-typedef FactoryHolder<GameObjectAI>::FactoryHolderRepository GameObjectAIRepository;
 #endif
