60e12676237a39afbaf08b249e38cbf09d08b4f0
Subv <subv2112@gmail.com>
2014-06-22 13:46:21 -0500

    Core/Build: Fixed build.

--- src/server/game/Movement/MovementGenerator.h
+++ src/server/game/Movement/MovementGenerator.h
@@ -92,5 +92,4 @@ struct MovementGeneratorFactory : public SelectableMovement
 
 typedef FactoryHolder<MovementGenerator, MovementGeneratorType> MovementGeneratorCreator;
 typedef FactoryHolder<MovementGenerator, MovementGeneratorType>::FactoryHolderRegistry MovementGeneratorRegistry;
-typedef FactoryHolder<MovementGenerator, MovementGeneratorType>::FactoryHolderRepository MovementGeneratorRepository;
 #endif
