4ddc78754cc2e86deb136623acce27510330b5d3
leak <leak@bitmx.net>
2014-06-24 19:13:29 +0200

    Removed ACE dependencies from LogWorker

--- src/server/shared/Logging/LogWorker.h
+++ src/server/shared/Logging/LogWorker.h
@@ -19,29 +19,19 @@
 #define LOGWORKER_H
 
 #include "LogOperation.h"
+#include "LockedQueue.h"
 
-#include <ace/Task.h>
-#include <ace/Activation_Queue.h>
-
-class LogWorker: protected ACE_Task_Base
+class LogWorker
 {
     public:
-        LogWorker();
+        LogWorker() {};
         ~LogWorker();
 
-        typedef ACE_Message_Queue_Ex<LogOperation, ACE_MT_SYNCH> LogMessageQueueType;
-
-        enum
-        {
-            HIGH_WATERMARK = 8 * 1024 * 1024,
-            LOW_WATERMARK  = 8 * 1024 * 1024
-        };
-
-        int enqueue(LogOperation *op);
+        void enqueue(LogOperation& op);
 
     private:
         virtual int svc();
-        LogMessageQueueType m_queue;
+        LockedQueue<LogOperation> m_queue;
 };
 
 #endif
