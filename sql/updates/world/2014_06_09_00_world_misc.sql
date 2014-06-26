87409e078ec3e4075e19cf14ad74bef959ca1f16
untaught <untaugh@mail.bg>
2014-06-26 22:11:09 +0300

    Remove spawn part for trap objects

--- sql/updates/world/2014_06_09_00_world_misc.sql
+++ sql/updates/world/2014_06_09_00_world_misc.sql
@@ -1,15 +1,3 @@
-SET @GUID = 13278;
-
-DELETE FROM `gameobject` WHERE guid BETWEEN @GUID+0 AND @GUID+6;
-INSERT INTO `gameobject` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES
-(@GUID+0,194955,571,1,1,5988.04,4473.78,-87.3617,1.74533,0,0,0.766045,0.642787,180,255,1),
-(@GUID+1,194955,571,1,1,6073.83,4463.66,-84.9872,-0.820303,0,0,-0.398748,0.91706,180,255,1),
-(@GUID+2,194955,571,1,1,6130.8,4462.61,-84.544,2.75761,0,0,0.981626,0.190814,180,255,1),
-(@GUID+3,194955,571,1,1,6075.99,4523.62,-81.0092,0.698132,0,0,0.34202,0.939693,180,255,1),
-(@GUID+4,194955,571,1,1,6012.03,4517.3,-86.8614,0.872664,0,0,0.422618,0.906308,180,255,1),
-(@GUID+5,194955,571,1,1,6092.64,4425.49,-83.9704,-2.72271,0,0,-0.978147,0.207914,180,255,1),
-(@GUID+6,194955,571,1,1,6102.15,4497.03,-81.3401,-2.63544,0,0,-0.968147,0.250383,180,255,1);
-
 UPDATE `gameobject_template` SET `AIName`='SmartGameObjectAI' WHERE  `entry`=190590;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=190590 AND  `source_type`=1;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
