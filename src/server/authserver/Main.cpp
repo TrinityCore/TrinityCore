b00e70cf8d942cbe04fd2d0fac1475b6a0e3842d
jackpoz <giacomopoz@gmail.com>
2014-06-28 17:55:40 +0200

    Auth/Misc: Move OpenSSL version log from Warning to Information

--- src/server/authserver/Main.cpp
+++ src/server/authserver/Main.cpp
@@ -115,7 +115,7 @@ extern int main(int argc, char** argv)
     TC_LOG_INFO("server.authserver", "<Ctrl-C> to stop.\n");
     TC_LOG_INFO("server.authserver", "Using configuration file %s.", configFile);
 
-    TC_LOG_WARN("server.authserver", "%s (Library: %s)", OPENSSL_VERSION_TEXT, SSLeay_version(SSLEAY_VERSION));
+    TC_LOG_INFO("server.authserver", "%s (Library: %s)", OPENSSL_VERSION_TEXT, SSLeay_version(SSLEAY_VERSION));
 
 #if defined (ACE_HAS_EVENT_POLL) || defined (ACE_HAS_DEV_POLL)
     ACE_Reactor::instance(new ACE_Reactor(new ACE_Dev_Poll_Reactor(ACE::max_handles(), 1), 1), true);
