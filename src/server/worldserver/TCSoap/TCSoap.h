51a8517c79d53c0d0fcb0ea3ff220829ac386334
Subv <subv2112@gmail.com>
2014-07-01 22:01:10 -0500

    Removed ACE from TCSoap
    Untested.
    
    Note: That was some rather awful use of a semaphore

--- src/server/worldserver/TCSoap/TCSoap.h
+++ src/server/worldserver/TCSoap/TCSoap.h
@@ -19,18 +19,16 @@
 #define _TCSOAP_H
 
 #include "Define.h"
+#include <mutex>
 
-#include <ace/Semaphore.h>
-#include <ace/Task.h>
-
-void process_message(ACE_Message_Block* mb);
+void process_message(struct soap* soap_message);
 void TCSoapThread(const std::string& host, uint16 port);
 
 class SOAPCommand
 {
     public:
         SOAPCommand():
-            pendingCommands(0, USYNC_THREAD, "pendingCommands"), m_success(false)
+            m_success(false)
         {
         }
 
@@ -43,7 +41,7 @@ class SOAPCommand
             m_printBuffer += msg;
         }
 
-        ACE_Semaphore pendingCommands;
+        std::mutex pendingCommands;
 
         void setCommandSuccess(bool val)
         {
