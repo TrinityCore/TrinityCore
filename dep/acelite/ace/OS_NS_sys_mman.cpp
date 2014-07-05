e4b56e2930348e4530399fae3fe0eef0fc939128
leak <leak@bitmx.net>
2014-07-06 01:42:12 +0200

    Completely remove ACE as dependency for TC - kkthxbye

--- dep/acelite/ace/OS_NS_sys_mman.cpp
+++
@@ -1,8 +0,0 @@
-// $Id: OS_NS_sys_mman.cpp 92837 2010-12-08 17:31:02Z johnnyw $
-
-#include "ace/OS_NS_sys_mman.h"
-
-#if !defined (ACE_HAS_INLINED_OSCALLS)
-# include "ace/OS_NS_sys_mman.inl"
-#endif /* ACE_HAS_INLINED_OSCALLS */
-
