b3266ad3c034e6588972c676ca2483bc049d8af0
Vincent-Michael <Vincent_Michael@gmx.de>
2014-07-10 00:41:55 +0200

    DB/Creature: Spawn Agatha for first undead quests

---
+++ sql/updates/world/2014_07_10_00_world_creature_434.sql
@@ -0,0 +1,7 @@
+DELETE FROM `creature_template_addon` WHERE `entry`=49044;
+INSERT INTO `creature_template_addon` (`entry`, `mount`, `bytes1`, `bytes2`, `auras`) VALUES
+(49044, 0, 0x3010000, 0x1, '49414');
+
+DELETE FROM `creature` WHERE `id`=49044;
+INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `phaseId`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `MovementType`) VALUES
+(xxxxxxx, 49044, 0, 1, 1, '', 1700.2, 1703.361, 137.1976, 1.675516, 120, 0, 0);
