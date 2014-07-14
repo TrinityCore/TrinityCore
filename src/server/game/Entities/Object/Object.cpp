0473a106bb25a67944a3ea1803dc2649706b1aa9
joschiwald <joschiwald.trinity@gmail.com>
2014-07-14 17:59:22 +0200

    Core/Battlefield: move complex methods from BattlefieldWG.h to BattlefieldWG.cpp

--- src/server/game/Entities/Object/Object.cpp
+++ src/server/game/Entities/Object/Object.cpp
@@ -48,8 +48,8 @@
 #include "MovementPacketBuilder.h"
 #include "DynamicTree.h"
 #include "Group.h"
-#include "Battlefield.h"
 #include "BattlefieldMgr.h"
+#include "Battleground.h"
 #include "Chat.h"
 
 uint32 GuidHigh2TypeId(uint32 guid_hi)
