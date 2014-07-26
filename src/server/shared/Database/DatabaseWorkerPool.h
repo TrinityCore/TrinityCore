5ce15d2f129775b85d2f4934763452aef6efb4d6
Shauren <shauren.trinity@gmail.com>
2014-07-26 16:10:05 +0200

    Core/DBLayer: Fixed possible crashes with async queries retrieving result
    Closes #12655

--- src/server/shared/Database/DatabaseWorkerPool.h
+++ src/server/shared/Database/DatabaseWorkerPool.h
@@ -297,8 +297,9 @@ class DatabaseWorkerPool
         QueryResultFuture AsyncQuery(const char* sql)
         {
             BasicStatementTask* task = new BasicStatementTask(sql, true);
+            QueryResultFuture result = task->GetFuture();
             Enqueue(task);
-            return task->GetFuture();         //! Actual return value has no use yet
+            return result;  //! Actual return value has no use yet
         }
 
         //! Enqueues a query in string format -with variable args- that will set the value of the QueryResultFuture return object as soon as the query is executed.
@@ -320,8 +321,9 @@ class DatabaseWorkerPool
         PreparedQueryResultFuture AsyncQuery(PreparedStatement* stmt)
         {
             PreparedStatementTask* task = new PreparedStatementTask(stmt, true);
+            PreparedQueryResultFuture result = task->GetFuture();
             Enqueue(task);
-            return task->GetFuture();
+            return result;
         }
 
         //! Enqueues a vector of SQL operations (can be both adhoc and prepared) that will set the value of the QueryResultHolderFuture
@@ -331,8 +333,9 @@ class DatabaseWorkerPool
         QueryResultHolderFuture DelayQueryHolder(SQLQueryHolder* holder)
         {
             SQLQueryHolderTask* task = new SQLQueryHolderTask(holder);
+            QueryResultHolderFuture result = task->GetFuture();
             Enqueue(task);
-            return task->GetFuture();
+            return result;
         }
 
         /**
