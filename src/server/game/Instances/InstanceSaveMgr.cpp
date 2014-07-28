9138dc1349165f024853dfe32792ceb95a7fcd7e
Shauren <shauren.trinity@gmail.com>
2014-07-28 23:32:53 +0200

    Core/Instances: Explicitly unload InstanceSaveMgr before MapManager, fixes invalid memory access at shutdown
    Closes #12635

--- src/server/game/Instances/InstanceSaveMgr.cpp
+++ src/server/game/Instances/InstanceSaveMgr.cpp
@@ -39,8 +39,10 @@ uint16 InstanceSaveManager::ResetTimeDelay[] = {3600, 900, 300, 60};
 
 InstanceSaveManager::~InstanceSaveManager()
 {
-    // it is undefined whether this or objectmgr will be unloaded first
-    // so we must be prepared for both cases
+}
+
+void InstanceSaveManager::Unload()
+{
     lock_instLists = true;
     for (InstanceSaveHashMap::iterator itr = m_instanceSaveById.begin(); itr != m_instanceSaveById.end(); ++itr)
     {
