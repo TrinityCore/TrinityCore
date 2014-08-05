719f3c9525cbe7ed87491dd7bd2ad3a47ad0db5b
Aokromes <Aokromes@users.noreply.github.com>
2014-08-05 05:49:10 +0200

    DB/Misc: A Bloodstained Journal Page
    By Crysicle, closes #12722

---
+++ sql/updates/world/2014_08_05_02_world_page_text.sql
@@ -0,0 +1,2 @@
+--A Bloodstained Journal Page
+UPDATE `page_text` SET `text` = '. . .downward spiral of despair. First she mocks me and now she is engaged. The ungracious charlatan was pretending to love when truly she desired to hurt me all along. A black void lurks with in me now and it grows with each waking moment. The blood I shall spill pales in comparison to the tears I have shed. . .' WHERE `entry` = 24;
