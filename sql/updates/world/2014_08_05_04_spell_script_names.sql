5cb222ef3454452dd73c271ddc8b128784ee538d
Portgas <saiifii@live.de>
2014-08-05 03:11:08 +0200

    Core/Spells: Fixed Druid - Harmony Mastery

---
+++ sql/updates/world/2014_08_05_04_spell_script_names.sql
@@ -0,0 +1,5 @@
+REPLACE INTO `spell_proc_event` (`entry`, `SpellFamilyName`, `procFlags`, `CustomChance`) VALUES 
+('77495', '7', '16384', '100');
+
+REPLACE INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+('77495', 'spell_dru_mastery_harmony');
