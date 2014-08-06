0f07dcfea6585d35e3a73a99e9f26131a766e9a2
jackpoz <giacomopoz@gmail.com>
2014-08-06 20:55:43 +0200

    Core/Scripts: Fix possible crash
    Fix possible NULL dereference crash spotted by Coverity.

--- src/server/game/Scripting/MapScripts.cpp
+++ src/server/game/Scripting/MapScripts.cpp
@@ -824,10 +824,9 @@ void Map::ScriptsProcess()
                 }
 
                 Creature* cTarget = NULL;
-                if (source) //using grid searcher
+                WorldObject* wSource = dynamic_cast <WorldObject*> (source);
+                if (wSource) //using grid searcher
                 {
-                    WorldObject* wSource = dynamic_cast <WorldObject*> (source);
-
                     CellCoord p(Trinity::ComputeCellCoord(wSource->GetPositionX(), wSource->GetPositionY()));
                     Cell cell(p);
 
