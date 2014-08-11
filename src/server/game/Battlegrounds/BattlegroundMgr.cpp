TrinityCore/TrinityCore

commit 10bf6dbb0da76f086caafb18cf7d48cbdc3787a8

[TW351] - Implement rewritten RBG selection script

src/server/game/Battlegrounds/BattlegroundMgr.cpp
@@ -975,7 +975,103 @@ BattlegroundTypeId BattlegroundMgr::GetRandomBG(BattlegroundTypeId bgTypeId)
         if (selectionWeights.size() == 1)
             return selectionWeights.begin()->first;
 
-        if (weight)
+        if (weight && bgTypeId == BATTLEGROUND_RB)
+        {
+    		uint32 playersInBG;
+    		uint32 playersInBG1;
+    		uint32 playersInBG2;
+    		uint32 playersInBG3;
+    		QueryResult result = CharacterDatabase.Query("select count(guid) from characters where level=80 and online=1 and (map=489 or map=566 or map=529 or map=607 or map=30 or map=628)");
+    		if (result)
+    		{
+    			do
+    			{
+    				Field* fields = result->Fetch();
+    				playersInBG = fields[0].GetUInt32();
+    			}
+    			while (result->NextRow());
+    		}
+    		QueryResult result1 = CharacterDatabase.Query("select count(guid) from characters where level=80 and online=1 and map=489");
+    		if (result1)
+    		{
+    			do
+    			{
+    				Field* fields = result1->Fetch();
+    				playersInBG1 = fields[0].GetUInt32();
+    			}
+    			while (result1->NextRow());
+    		}
+    		QueryResult result2 = CharacterDatabase.Query("select count(guid) from characters where level=80 and online=1 and (map=566 or map=529 or map=607)");
+    		if (result2)
+    		{
+    			do
+    			{
+    				Field* fields = result2->Fetch();
+    				playersInBG2 = fields[0].GetUInt32();
+    			}
+    			while (result2->NextRow());
+    		}
+    		QueryResult result3 = CharacterDatabase.Query("select count(guid) from characters where level=80 and online=1 and (map=30 or map=628)");
+    		if (result3)
+    		{
+    			do
+    			{
+    				Field* fields = result3->Fetch();
+    				playersInBG3 = fields[0].GetUInt32();
+    			}
+    			while (result3->NextRow());
+    		}
+    		if (playersInBG>25 && playersInBG3<20)
+    		{
+    			uint32 selectedWeight = 0;
+    			// Select a random value
+    			selectedWeight = urand(0, 0);
+    			// Select the correct bg (if we have in DB A(10), B(20), C(10), D(15) --> [0---A---9|10---B---29|30---C---39|40---D---54])
+    			weight = 0;
+    			for (auto it : selectionWeights)
+    			{
+    				weight += it.second;
+    				if (selectedWeight < weight && weight > 2 && weight < 4)
+    				{
+    					return it.first;
+    				}
+    			}
+    		}
+    		else if (playersInBG<16 || (((playersInBG2>30 && playersInBG2<46) || (playersInBG2>60 && playersInBG2<76) || (playersInBG2>90 && playersInBG2<106) || (playersInBG2>120 && playersInBG2<136) || (playersInBG2>150 && playersInBG2<166) || (playersInBG>80 && (playersInBG2<16 || (playersInBG2>30 && playersInBG2<46) || (playersInBG2>60 && playersInBG2<76) || (playersInBG2>90 && playersInBG2<106)))) && playersInBG1<6))
+    		{
+    			uint32 selectedWeight = 0;
+    			// Select a random value
+    			selectedWeight = urand(0, 0);
+    			// Select the correct bg (if we have in DB A(10), B(20), C(10), D(15) --> [0---A---9|10---B---29|30---C---39|40---D---54])
+    			weight = 0;
+    			for (auto it : selectionWeights)
+    			{
+    				weight += it.second;
+    				if (selectedWeight < weight && weight > 3 && weight < 5)
+    				{
+    					return it.first;
+    				}
+    			}
+    		}
+    		else
+    		{
+    			uint32 selectedWeight = 0;
+    			// Select a random value
+    			selectedWeight = urand(4, 9);
+    			// Select the correct bg (if we have in DB A(10), B(20), C(10), D(15) --> [0---A---9|10---B---29|30---C---39|40---D---54])
+    			weight = 0;
+    			for (auto it : selectionWeights)
+    			{
+    				weight += it.second;
+    				if (selectedWeight < weight && ((weight > 5 && weight < 7) || (weight > 7 && weight < 9) || (weight > 9 && weight < 11)))
+				    {
+			    		return it.first;
+	    			}
+    			}
+	    	}
+        }
+
+        else if (weight)
         {
             // Select a random value
             uint32 selectedWeight = urand(0, weight - 1);

commit 04c899900cc7b0efeca4b9fa10a9a14e780ff995

[TW354] - Core/Battleground: Solve some warnings added in https://github.com/TrueWoW/TrinityCore/commit/10bf6dbb0da76f086caafb18cf7d48cbdc3787a8 caused by uninitialized variables and clear some redundancy

src/server/game/Battlegrounds/BattlegroundMgr.cpp
@@ -977,10 +977,10 @@ BattlegroundTypeId BattlegroundMgr::GetRandomBG(BattlegroundTypeId bgTypeId)
 
         if (weight && bgTypeId == BATTLEGROUND_RB)
         {
-    		uint32 playersInBG;
-    		uint32 playersInBG1;
-    		uint32 playersInBG2;
-    		uint32 playersInBG3;
+    		uint32 playersInBG  = 0;
+    		uint32 playersInBG1 = 0;
+    		uint32 playersInBG2 = 0;
+    		uint32 playersInBG3 = 0;
     		QueryResult result = CharacterDatabase.Query("select count(guid) from characters where level=80 and online=1 and (map=489 or map=566 or map=529 or map=607 or map=30 or map=628)");
     		if (result)
     		{
@@ -1024,8 +1024,6 @@ BattlegroundTypeId BattlegroundMgr::GetRandomBG(BattlegroundTypeId bgTypeId)
     		if (playersInBG>25 && playersInBG3<20)
     		{
     			uint32 selectedWeight = 0;
-    			// Select a random value
-    			selectedWeight = urand(0, 0);
     			// Select the correct bg (if we have in DB A(10), B(20), C(10), D(15) --> [0---A---9|10---B---29|30---C---39|40---D---54])
     			weight = 0;
     			for (auto it : selectionWeights)
@@ -1040,8 +1038,6 @@ BattlegroundTypeId BattlegroundMgr::GetRandomBG(BattlegroundTypeId bgTypeId)
     		else if (playersInBG<16 || (((playersInBG2>30 && playersInBG2<46) || (playersInBG2>60 && playersInBG2<76) || (playersInBG2>90 && playersInBG2<106) || (playersInBG2>120 && playersInBG2<136) || (playersInBG2>150 && playersInBG2<166) || (playersInBG>80 && (playersInBG2<16 || (playersInBG2>30 && playersInBG2<46) || (playersInBG2>60 && playersInBG2<76) || (playersInBG2>90 && playersInBG2<106)))) && playersInBG1<6))
     		{
     			uint32 selectedWeight = 0;
-    			// Select a random value
-    			selectedWeight = urand(0, 0);
     			// Select the correct bg (if we have in DB A(10), B(20), C(10), D(15) --> [0---A---9|10---B---29|30---C---39|40---D---54])
     			weight = 0;
     			for (auto it : selectionWeights)