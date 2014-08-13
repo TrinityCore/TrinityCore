826e4168f138c7855660edc0b3239304547b19ae
roc13x <roc13x@gmail.com>
2014-08-13 19:37:14 +0100

    Core/RBAC: Simplify command permissions
    Removes ability to override a commands permission ID in the world DB. I
    see no benefit to this feature since every command has it's own
    permission ID now and permissions are controlled from auth

--- src/server/game/Chat/Chat.h
+++ src/server/game/Chat/Chat.h
@@ -128,7 +128,7 @@ class ChatHandler
         bool ShowHelpForCommand(ChatCommand* table, const char* cmd);
     protected:
         explicit ChatHandler() : m_session(NULL), sentErrorMessage(false) { }     // for CLI subclass
-        static bool SetDataForCommandInTable(ChatCommand* table, const char* text, uint32 permission, std::string const& help, std::string const& fullcommand);
+        static bool SetDataForCommandInTable(ChatCommand* table, const char* text, std::string const& help, std::string const& fullcommand);
         bool ExecuteCommandInTable(ChatCommand* table, const char* text, std::string const& fullcmd);
         bool ShowHelpForSubCommands(ChatCommand* table, char const* cmd, char const* subcmd);
 
