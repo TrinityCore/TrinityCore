c496979e1c4826ea89d6e06b35607c35de820d20
Aokromes <Aokromes@users.noreply.github.com>
2014-08-24 14:12:47 +0200

    Update 2014_07_10_00_world_creature_434.sql

--- sql/updates/world/2014_07_10_00_world_creature_434.sql
+++ sql/updates/world/2014_07_10_00_world_creature_434.sql
@@ -4,4 +4,4 @@ INSERT INTO `creature_template_addon` (`entry`, `mount`, `bytes1`, `bytes2`, `au
 
 DELETE FROM `creature` WHERE `id`=49044;
 INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `phaseId`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `MovementType`) VALUES
-(xxxxxxx, 49044, 0, 1, 1, 0, 1700.2, 1703.361, 137.1976, 1.675516, 120, 0, 0);
+(999999, 49044, 0, 1, 1, 0, 1700.2, 1703.361, 137.1976, 1.675516, 120, 0, 0); -- temp guid to "fix" travis.
