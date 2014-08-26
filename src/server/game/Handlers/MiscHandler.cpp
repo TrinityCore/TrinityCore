a3e36eae45b78ecef062e73299edd5a93ad6c142
DDuarte <dnpd.dd@gmail.com>
2014-08-26 21:40:17 +0100

    Revert "Server/PacketIO: Updated CMSG_TIME_SYNC_RESP for 434"
    This reverts commit c83f1af02e8182f59bf78445b33c05932922de4c.
    
    <Shauren> int __thiscall PlayerTimeSyncResponse::CliPut(PlayerTimeSyncResponse *this, CDataStore *a2)
    <Shauren> {
    <Shauren>   PlayerTimeSyncResponse *v2; // esi@1
    <Shauren>   v2 = this;
    <Shauren>   CDataStore::PutInt32(a2, 15116);
    <Shauren>   CDataStore::PutInt32(a2, v2->SyncCounter);
    <Shauren>   return CDataStore::PutInt32(a2, v2->MS_Time);
    <Shauren> }
    <Shauren> v2->SyncCounter is the value from SMSG_TIME_SYNC_REQ and ms_time = OsGetAsyncTimeMs()

--- src/server/game/Handlers/MiscHandler.cpp
+++ src/server/game/Handlers/MiscHandler.cpp
@@ -1500,7 +1500,7 @@ void WorldSession::HandleTimeSyncResp(WorldPacket& recvData)
     TC_LOG_DEBUG("network", "CMSG_TIME_SYNC_RESP");
 
     uint32 counter, clientTicks;
-    recvData >> clientTicks >> counter;
+    recvData >> counter >> clientTicks;
 
     if (counter != _player->m_timeSyncQueue.front())
         TC_LOG_ERROR("network", "Wrong time sync counter from player %s (cheater?)", _player->GetName().c_str());
