430b3adfe8ed23df453f906978731fcb83956335
DDuarte <dnpd.dd@gmail.com>
2014-08-26 13:36:50 +0100

    Scripts: Correct despawn time of multiple objects
    WorldObject::SummonGameObject respawn time is in seconds and not milliseconds

--- src/server/scripts/World/item_scripts.cpp
+++ src/server/scripts/World/item_scripts.cpp
@@ -241,7 +241,7 @@ public:
 
         float x, y, z;
         go->GetClosePoint(x, y, z, go->GetObjectSize() / 3, 7.0f);
-        go->SummonGameObject(GO_HIGH_QUALITY_FUR, go->GetPositionX(), go->GetPositionY(), go->GetPositionZ(), 0, 0, 0, 0, 0, 1000);
+        go->SummonGameObject(GO_HIGH_QUALITY_FUR, go->GetPositionX(), go->GetPositionY(), go->GetPositionZ(), 0, 0, 0, 0, 0, 1);
         if (TempSummon* summon = player->SummonCreature(NPC_NESINGWARY_TRAPPER, x, y, z, go->GetOrientation(), TEMPSUMMON_DEAD_DESPAWN, 1000))
         {
             summon->SetVisible(false);
