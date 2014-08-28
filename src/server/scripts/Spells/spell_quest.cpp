eb2f36febf7e5a9c893af967e6018d323fd2c2ed
Vincent-Michael <Vincent_Michael@gmx.de>
2014-08-28 20:16:16 +0200

    Scripts/Quest: Fix some movement stuff for Quest "Death Comes From On High"

--- src/server/scripts/Spells/spell_quest.cpp
+++ src/server/scripts/Spells/spell_quest.cpp
@@ -2026,7 +2026,6 @@ class spell_q12308_escape_from_silverbrook_summon_worgen : public SpellScriptLoa
         }
 };
 
-
 enum DeathComesFromOnHigh
 {
     SPELL_FORGE_CREDIT                  = 51974,
@@ -2094,6 +2093,37 @@ class spell_q12641_death_comes_from_on_high : public SpellScriptLoader
         SpellScript* GetSpellScript() const override
         {
             return new spell_q12641_death_comes_from_on_high_SpellScript();
+        }
+};
+
+// 52694 - Recall Eye of Acherus 
+class spell_q12641_recall_eye_of_acherus : public SpellScriptLoader
+{
+    public:
+        spell_q12641_recall_eye_of_acherus() : SpellScriptLoader("spell_q12641_recall_eye_of_acherus") { }
+
+        class spell_q12641_recall_eye_of_acherus_SpellScript : public SpellScript
+        {
+            PrepareSpellScript(spell_q12641_recall_eye_of_acherus_SpellScript);
+
+            void HandleDummy(SpellEffIndex /*effIndex*/)
+            {
+                if (Player* player = GetCaster()->GetCharmerOrOwner()->ToPlayer())
+                {
+                    player->StopCastingCharm();
+                    player->StopCastingBindSight();
+                }
+            }
+
+            void Register() override
+            {
+                OnEffectHitTarget += SpellEffectFn(spell_q12641_recall_eye_of_acherus_SpellScript::HandleDummy, EFFECT_0, SPELL_EFFECT_SCRIPT_EFFECT);
+            }
+        };
+
+        SpellScript* GetSpellScript() const override
+        {
+            return new spell_q12641_recall_eye_of_acherus_SpellScript();
         }
 };
 
@@ -2364,6 +2394,7 @@ void AddSC_quest_spell_scripts()
     new spell_q12308_escape_from_silverbrook_summon_worgen();
     new spell_q12308_escape_from_silverbrook();
     new spell_q12641_death_comes_from_on_high();
+    new spell_q12641_recall_eye_of_acherus();
     new spell_q12619_emblazon_runeblade();
     new spell_q12619_emblazon_runeblade_effect();
     new spell_q12919_gymers_grab();
