923fda5bc8e544f2ccbf96527e7e28a3f12a364e
pr0d1gy <pr0d1gy.bc@gmail.com>
2014-08-30 11:59:57 +0300

    Revert "Fixed typo in previous commit, apologize"
    This reverts commit 442dfb713d4ebc0a7a26c21bf929bf873129afd5.

--- sql/tw_sql/TW_2014_08_11_00_characters_pvpstats.sql
+++ sql/tw_sql/TW_2014_08_11_00_characters_pvpstats.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `pvpstats_faction` (
   `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `faction` BIT NOT NULL,
+  `faction` BIT UNSIGNED NOT NULL,
   `level` TINYINT UNSIGNED NOT NULL,
   `date` DATETIME NOT NULL,
   PRIMARY KEY (`id`)
