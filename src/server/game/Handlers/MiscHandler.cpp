4080ac27ab3509e4b24b9ad8142da349fbb84533
DDuarte <dnpd.dd@gmail.com>
2014-09-02 18:45:22 +0100

    Core/Handlers: Fix two warnings recently introduced

--- src/server/game/Handlers/MiscHandler.cpp
+++ src/server/game/Handlers/MiscHandler.cpp
@@ -533,8 +533,8 @@ void WorldSession::HandleRequestCemeteryList(WorldPacket& /*recvPacket*/)
     data.WriteBit(0); // Is MicroDungeon (WorldMapFrame.lua)
 
     data.WriteBits(graveyardIds.size(), 24);
-    for (int i = 0; i < graveyardIds.size(); ++i)
-        data << uint32(graveyardIds[i]);
+    for (auto id : graveyardIds)
+        data << uint32(graveyardIds);
 
     SendPacket(&data);
 }
