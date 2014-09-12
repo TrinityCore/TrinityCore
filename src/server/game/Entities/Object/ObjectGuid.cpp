2fc52b5af62e828e85e1da021b1d7ff0fe7ddd8e
Shauren <shauren.trinity@gmail.com>
2014-09-12 23:49:30 +0200

    Core/Entities: Added ObjectGuid class from MaNGOS

---
+++ src/server/game/Entities/Object/ObjectGuid.cpp
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2008-2014 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "ObjectGuid.h"
+#include "World.h"
+#include "ObjectMgr.h"
+#include <sstream>
+
+char const* ObjectGuid::GetTypeName(HighGuid high)
+{
+    switch (high)
+    {
+        case HIGHGUID_ITEM:         return "Item";
+        case HIGHGUID_PLAYER:       return "Player";
+        case HIGHGUID_GAMEOBJECT:   return "Gameobject";
+        case HIGHGUID_TRANSPORT:    return "Transport";
+        case HIGHGUID_UNIT:         return "Creature";
+        case HIGHGUID_PET:          return "Pet";
+        case HIGHGUID_VEHICLE:      return "Vehicle";
+        case HIGHGUID_DYNAMICOBJECT: return "DynObject";
+        case HIGHGUID_CORPSE:       return "Corpse";
+        case HIGHGUID_MO_TRANSPORT: return "MoTransport";
+        case HIGHGUID_INSTANCE:     return "InstanceID";
+        case HIGHGUID_GROUP:        return "Group";
+        default:
+            return "<unknown>";
+    }
+}
+
+std::string ObjectGuid::ToString() const
+{
+    std::ostringstream str;
+    str << GetTypeName();
+
+    if (IsPlayer())
+    {
+        std::string name;
+        if (sObjectMgr->GetPlayerNameByGUID(m_guid, name))
+            str << " " << name;
+    }
+
+    str << " (";
+    if (HasEntry())
+        str << (IsPet() ? "Petnumber: " : "Entry: ") << GetEntry() << " ";
+    str << "Guid: " << GetCounter() << ")";
+    return str.str();
+}
+
+template<HighGuid high>
+uint32 ObjectGuidGenerator<high>::Generate()
+{
+    if (m_nextGuid >= ObjectGuid::GetMaxCounter(high) - 1)
+    {
+        TC_LOG_ERROR("", "%s guid overflow!! Can't continue, shutting down server. ", ObjectGuid::GetTypeName(high));
+        World::StopNow(ERROR_EXIT_CODE);
+    }
+    return m_nextGuid++;
+}
+
+ByteBuffer& operator<<(ByteBuffer& buf, ObjectGuid const& guid)
+{
+    buf << uint64(guid.GetRawValue());
+    return buf;
+}
+
+ByteBuffer& operator>>(ByteBuffer& buf, ObjectGuid& guid)
+{
+    guid.Set(buf.read<uint64>());
+    return buf;
+}
+
+ByteBuffer& operator<<(ByteBuffer& buf, PackedGuid const& guid)
+{
+    buf.append(guid.m_packedGuid);
+    return buf;
+}
+
+ByteBuffer& operator>>(ByteBuffer& buf, PackedGuidReader const& guid)
+{
+    buf.readPackGUID(*reinterpret_cast<uint64*>(guid.m_guidPtr));
+    return buf;
+}
+
+template uint32 ObjectGuidGenerator<HIGHGUID_ITEM>::Generate();
+template uint32 ObjectGuidGenerator<HIGHGUID_PLAYER>::Generate();
+template uint32 ObjectGuidGenerator<HIGHGUID_GAMEOBJECT>::Generate();
+template uint32 ObjectGuidGenerator<HIGHGUID_TRANSPORT>::Generate();
+template uint32 ObjectGuidGenerator<HIGHGUID_UNIT>::Generate();
+template uint32 ObjectGuidGenerator<HIGHGUID_PET>::Generate();
+template uint32 ObjectGuidGenerator<HIGHGUID_VEHICLE>::Generate();
+template uint32 ObjectGuidGenerator<HIGHGUID_DYNAMICOBJECT>::Generate();
+template uint32 ObjectGuidGenerator<HIGHGUID_CORPSE>::Generate();
+template uint32 ObjectGuidGenerator<HIGHGUID_INSTANCE>::Generate();
+template uint32 ObjectGuidGenerator<HIGHGUID_GROUP>::Generate();
