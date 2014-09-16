f63338afe84ddf9c0b9584f29b81082e966dbffd
Nyeriah <sarah.trysan@live.com>
2014-09-16 11:49:53 -0300

    SQL: Fix typo at https://github.com/TrinityCore/TrinityCore/commit/81a5c2413d1c9932278fe0dd30cd3c22219d2226

--- sql/updates/world/2014_09_16_00_world_achievement_criteria_data.sql
+++ sql/updates/world/2014_09_16_00_world_achievement_criteria_data.sql
@@ -1,3 +1,3 @@
 DELETE FROM `achievement_criteria_data` WHERE `criteria_id` = 7361 AND `type` = 12;
 INSERT INTO `achievement_criteria_data`(`criteria_id`, `type`, `value1`, `value2`, `ScriptName`) VALUES
-(7361, 12, 1, 0, 11);
+(7361, 12, 1, 0, '');
