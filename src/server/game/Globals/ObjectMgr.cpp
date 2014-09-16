849f340f269c5adcbceb0363a8ef746f6a26154a
ShinDarth <borzifrancesco@gmail.com>
2014-09-17 01:57:36 +0200

    Core/ObjectMgr: check creature.orientation and gameobject.orientation range

--- src/server/game/Globals/ObjectMgr.cpp
+++ src/server/game/Globals/ObjectMgr.cpp
@@ -34,6 +34,7 @@
 #include "LFGMgr.h"
 #include "Log.h"
 #include "MapManager.h"
+#include "Object.h"
 #include "ObjectMgr.h"
 #include "Pet.h"
 #include "PoolMgr.h"
@@ -1727,6 +1728,12 @@ void ObjectMgr::LoadCreatures()
             data.phaseMask = 1;
         }
 
+        if (std::abs(data.orientation) > 2 * float(M_PI))
+        {
+            TC_LOG_ERROR("sql.sql", "Table `creature` has creature (GUID: %u Entry: %u) with abs(`orientation`) > 2*PI (orientation is expressed in radians), normalized.", guid, data.id);
+            data.orientation = Position::NormalizeOrientation(data.orientation);
+        }
+
         // Add to grid if not managed by the game event or pool system
         if (gameEvent == 0 && PoolId == 0)
             AddCreatureToGrid(guid, &data);
@@ -2008,6 +2015,12 @@ void ObjectMgr::LoadGameobjects()
         data.phaseMask      = fields[15].GetUInt32();
         int16 gameEvent     = fields[16].GetInt8();
         uint32 PoolId        = fields[17].GetUInt32();
+
+        if (std::abs(data.orientation) > 2 * float(M_PI))
+        {
+            TC_LOG_ERROR("sql.sql", "Table `gameobject` has gameobject (GUID: %u Entry: %u) with abs(`orientation`) > 2*PI (orientation is expressed in radians), normalized.", guid, data.id);
+            data.orientation = Position::NormalizeOrientation(data.orientation);
+        }
 
         if (data.rotation2 < -1.0f || data.rotation2 > 1.0f)
         {
