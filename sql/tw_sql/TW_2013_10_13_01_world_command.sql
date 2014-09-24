62daf58711a57a0f7ab496bfb2edca193a94fd8e
Sarah Alexander <sarah.trysan@live.com>
2014-09-24 02:10:08 -0300

    Delete TW_2013_10_13_01_world_command.sql

--- sql/tw_sql/TW_2013_10_13_01_world_command.sql
+++
@@ -1,4 +0,0 @@
--- DB/Commands Sets command data for .reset arenapoints
-INSERT INTO `command` (`name`, `permission`, `help`) VALUES
-('reset arena points', '1001', 'Syntax: .reset arenapoints [Playername] Resets all arena point data for targeted character.');
-
