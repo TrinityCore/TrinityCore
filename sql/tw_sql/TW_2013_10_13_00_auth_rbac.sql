7b8821c173f259a73c099df8b26393204937bd81
Sarah Alexander <sarah.trysan@live.com>
2014-09-24 02:10:18 -0300

    Delete TW_2013_10_13_00_auth_rbac.sql

--- sql/tw_sql/TW_2013_10_13_00_auth_rbac.sql
+++
@@ -1,4 +0,0 @@
--- Sets rbac permission for .reset arenapoints command
-INSERT INTO `rbac_permissions` (`id`, `name`) VALUES ('1001', 'Command: reset arenapoints');
-INSERT INTO `rbac_linked_permissions` (`id`, `linkedId`) VALUES ('196', '1001');
-
