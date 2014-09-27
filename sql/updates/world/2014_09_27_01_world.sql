3e6ad2603e44d77df95283ba17faf40fe2b467aa
Vincent-Michael <Vincent_Michael@gmx.de>
2014-09-27 02:17:06 +0200

    SQL: meh, save button

--- sql/updates/world/2014_09_27_01_world.sql
+++ sql/updates/world/2014_09_27_01_world.sql
@@ -1,3 +1,3 @@
 DELETE FROM `gameobject` WHERE `guid`=31688;
 INSERT INTO `gameobject` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES
-(31688, 186717, 1, 1, 1, 1349.707, -4357.939, 26.95916, -0.593412, 0, 0, 0, 1, 120, 255, 1), -- Brewfest Banner (Area: 0)
+(31688, 186717, 1, 1, 1, 1349.707, -4357.939, 26.95916, -0.593412, 0, 0, 0, 1, 120, 255, 1); -- Brewfest Banner (Area: 0)
