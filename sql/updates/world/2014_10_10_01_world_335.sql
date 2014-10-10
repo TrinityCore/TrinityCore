df94b7c6e78959b59b1a259dfdcab8a01f6f0475
Aokromes <Aokromes@users.noreply.github.com>
2014-10-10 17:33:14 +0200

    DB/Gameobject: Add missing gameobject

---
+++ sql/updates/world/2014_10_10_01_world_335.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `gameobject_template` WHERE `entry` = 180671;
+INSERT INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `data0`, `data1`, `VerifiedBuild`) VALUES
+(180671,3,644,'Xandivious\' Demon Bag',43,17516,-18019);
