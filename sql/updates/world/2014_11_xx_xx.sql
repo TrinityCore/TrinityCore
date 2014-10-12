d0e9db0f13907db51cc44e6f5f16bc900b27498d
Intra <zaronz@gmail.com>
2014-10-12 03:40:46 +0200

    Rework Shazzrah script and implement spell script

---
+++ sql/updates/world/2014_11_xx_xx.sql
@@ -0,0 +1,1 @@
+INSERT INTO spell_script_names VALUES (23138, "spell_shazzrah_gate_dummy");
--- src/server/scripts/EasternKingdoms/BlackrockMountain/MoltenCore/boss_shazzrah.cpp
+++ src/server/scripts/EasternKingdoms/BlackrockMountain/MoltenCore/boss_shazzrah.cpp
@@ -16,13 +16,6 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* ScriptData
-SDName: Boss_Shazzrah
-SD%Complete: 75
-SDComment: Teleport NYI
-SDCategory: Molten Core
-EndScriptData */
-
 #include "ObjectMgr.h"
 #include "ScriptMgr.h"
 #include "ScriptedCreature.h"
@@ -30,19 +23,22 @@ EndScriptData */
 
 enum Spells
 {
-    SPELL_ARCANE_EXPLOSION  = 19712,
-    SPELL_SHAZZRAH_CURSE    = 19713,
-    SPELL_MAGIC_GROUNDING   = 19714,
-    SPELL_COUNTERSPELL      = 19715,
+    SPELL_ARCANE_EXPLOSION      = 19712,
+    SPELL_SHAZZRAH_CURSE        = 19713,
+    SPELL_MAGIC_GROUNDING       = 19714,
+    SPELL_COUNTERSPELL          = 19715,
+    SPELL_SHAZZRAH_GATE_DUMMY   = 23138, // Teleports to and attacks a random target.
+    SPELL_SHAZZRAH_GATE         = 23139,
 };
 
 enum Events
 {
-    EVENT_ARCANE_EXPLOSION  = 1,
-    EVENT_SHAZZRAH_CURSE    = 2,
-    EVENT_MAGIC_GROUNDING   = 3,
-    EVENT_COUNTERSPELL      = 4,
-    EVENT_BLINK             = 5,
+    EVENT_ARCANE_EXPLOSION              = 1,
+    EVENT_ARCANE_EXPLOSION_TRIGGERED    = 2,
+    EVENT_SHAZZRAH_CURSE                = 3,
+    EVENT_MAGIC_GROUNDING               = 4,
+    EVENT_COUNTERSPELL                  = 5,
+    EVENT_SHAZZRAH_GATE                 = 6,
 };
 
 class boss_shazzrah : public CreatureScript
@@ -63,7 +59,7 @@ class boss_shazzrah : public CreatureScript
                 events.ScheduleEvent(EVENT_SHAZZRAH_CURSE, 10000);
                 events.ScheduleEvent(EVENT_MAGIC_GROUNDING, 24000);
                 events.ScheduleEvent(EVENT_COUNTERSPELL, 15000);
-                events.ScheduleEvent(EVENT_BLINK, 30000);
+                events.ScheduleEvent(EVENT_SHAZZRAH_GATE, 45000);
             }
 
             void UpdateAI(uint32 diff) override
@@ -82,10 +78,14 @@ class boss_shazzrah : public CreatureScript
                     {
                         case EVENT_ARCANE_EXPLOSION:
                             DoCastVictim(SPELL_ARCANE_EXPLOSION);
-                            events.ScheduleEvent(EVENT_ARCANE_EXPLOSION, urand(5000, 9000));
+                            events.ScheduleEvent(EVENT_ARCANE_EXPLOSION, urand(4000, 7000));
+                            break;
+                        // Triggered subsequent to using "Gate of Shazzrah".
+                        case EVENT_ARCANE_EXPLOSION_TRIGGERED:
+                            DoCastVictim(SPELL_ARCANE_EXPLOSION);
                             break;
                         case EVENT_SHAZZRAH_CURSE:
-                            if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0, 0.0f, true, -EVENT_SHAZZRAH_CURSE))
+                            if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0, 0.0f, true, -SPELL_SHAZZRAH_CURSE))
                                 DoCast(target, SPELL_SHAZZRAH_CURSE);
                             events.ScheduleEvent(EVENT_SHAZZRAH_CURSE, urand(25000, 30000));
                             break;
@@ -97,16 +97,16 @@ class boss_shazzrah : public CreatureScript
                             DoCastVictim(SPELL_COUNTERSPELL);
                             events.ScheduleEvent(EVENT_COUNTERSPELL, urand(16000, 20000));
                             break;
-                        case EVENT_BLINK:
-                            // Teleporting him to a random player and casting Arcane Explosion after that.
-                            // Blink is not working cause of LoS System we need to do this hardcoded.
-                            if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 1, 100.0f, true))
+                        case EVENT_SHAZZRAH_GATE:
+                            if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0, 100.0f, true))
                             {
-                                DoTeleportTo(target->GetPositionX(), target->GetPositionY(), target->GetPositionZ());
-                                DoCast(target, SPELL_ARCANE_EXPLOSION);
                                 DoResetThreat();
+                                AttackStart(target); // Attack the target which caster will teleport to.
+                                DoCast(me, SPELL_SHAZZRAH_GATE_DUMMY);
+                                events.ScheduleEvent(EVENT_ARCANE_EXPLOSION_TRIGGERED, 2000);
+                                events.RescheduleEvent(EVENT_ARCANE_EXPLOSION, urand(3000, 6000));
                             }
-                            events.ScheduleEvent(EVENT_BLINK, 45000);
+                            events.ScheduleEvent(EVENT_SHAZZRAH_GATE, 45000);
                             break;
                         default:
                             break;
@@ -123,7 +123,42 @@ class boss_shazzrah : public CreatureScript
         }
 };
 
+class spell_shazzrah_gate_dummy : public SpellScriptLoader
+{
+    public:
+        spell_shazzrah_gate_dummy() : SpellScriptLoader("spell_shazzrah_gate_dummy") { }
+
+        class spell_shazzrah_gate_dummy_SpellScript : public SpellScript
+        {
+            PrepareSpellScript(spell_shazzrah_gate_dummy_SpellScript);
+
+            bool Validate(SpellInfo const* /*spellInfo*/) override
+            {
+                if (!sSpellMgr->GetSpellInfo(SPELL_SHAZZRAH_GATE))
+                    return false;
+                return true;
+            }
+
+            void HandleScript(SpellEffIndex effIndex)
+            {
+                if (GetCaster() && GetHitUnit())
+                    GetHitUnit()->CastSpell(GetCaster(), SPELL_SHAZZRAH_GATE, true);
+            }
+
+            void Register() override
+            {
+                OnEffectHitTarget += SpellEffectFn(spell_shazzrah_gate_dummy_SpellScript::HandleScript, EFFECT_0, SPELL_EFFECT_DUMMY);
+            }
+        };
+
+        SpellScript* GetSpellScript() const override
+        {
+            return new spell_shazzrah_gate_dummy_SpellScript();
+        }
+};
+
 void AddSC_boss_shazzrah()
 {
     new boss_shazzrah();
+    new spell_shazzrah_gate_dummy();
 }
