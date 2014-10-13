19cb40648cd2a49a5b5fa7605f9c58d9d65311ce
jackpoz <giacomopoz@gmail.com>
2014-10-13 22:37:44 +0200

    Core/Movement: Fix evade issue
    Fix evade issue happening when an evading creature assists another one, causing it to stay in evading state forever.
    Fix #4943

--- src/server/game/Entities/Creature/Creature.cpp
+++ src/server/game/Entities/Creature/Creature.cpp
@@ -1876,6 +1876,9 @@ void Creature::CallForHelp(float radius)
 
 bool Creature::CanAssistTo(const Unit* u, const Unit* enemy, bool checkfaction /*= true*/) const
 {
+    if (IsInEvadeMode())
+        return false;
+
     // is it true?
     if (!HasReactState(REACT_AGGRESSIVE))
         return false;
