8301bd0042696fbc7cced3609b3b7384fe6f0f6a
Sarah Alexander <sarah.trysan@live.com>
2014-10-14 19:12:09 -0300

    Delete TW_2013_07_28_00_world.sql

--- sql/tw_sql/TW_2013_07_28_00_world.sql
+++
@@ -1,3 +0,0 @@
--- Adds Forbearance effect to Captain Brandon's Divine Shield
-DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = 71550 AND `spell_effect` = 25771;
-INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES (71550, 25771, 0, 'Captain Brandon - Divine Shield - Forbearance');
