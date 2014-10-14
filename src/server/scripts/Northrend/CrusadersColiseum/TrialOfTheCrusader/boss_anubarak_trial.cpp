TrinityCore/TrinityCore

commit b4e941869844dcabb386feb943082a096581f017

Core/Misc: dropped obsolete GameObject::GetGameObject method

src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/instance_trial_of_the_crusader.cpp
@@ -96,11 +96,10 @@ class instance_trial_of_the_crusader : public InstanceMapScript
                 // make sure Anub'arak isnt missing and floor is destroyed after a crash
                 if (GetBossState(BOSS_LICH_KING) == DONE && TrialCounter && GetBossState(BOSS_ANUBARAK) != DONE)
                 {
-                    Creature* anubArak = ObjectAccessor::GetCreature(*player, GetData64(NPC_ANUBARAK));
-                    if (!anubArak)
+                    if (Creature* anubArak = ObjectAccessor::GetCreature(*player, GetData64(NPC_ANUBARAK)))
                         anubArak = player->SummonCreature(NPC_ANUBARAK, AnubarakLoc[0].GetPositionX(), AnubarakLoc[0].GetPositionY(), AnubarakLoc[0].GetPositionZ(), 3, TEMPSUMMON_CORPSE_TIMED_DESPAWN, DESPAWN_TIME);
 
-                    if (GameObject* floor = GameObject::GetGameObject(*player, GetData64(GO_ARGENT_COLISEUM_FLOOR)))
+                    if (GameObject* floor = ObjectAccessor::GetGameObject(*player, GetData64(GO_ARGENT_COLISEUM_FLOOR)))
                         floor->SetDestructibleState(GO_DESTRUCTIBLE_DAMAGED);
                 }
             }

src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/trial_of_the_crusader.cpp
@@ -201,7 +201,7 @@ class npc_announcer_toc10 : public CreatureScript
             }
             else if (instance->GetBossState(BOSS_LICH_KING) != DONE)
             {
-                if (GameObject* floor = GameObject::GetGameObject(*player, instance->GetData64(GO_ARGENT_COLISEUM_FLOOR)))
+                if (GameObject* floor = ObjectAccessor::GetGameObject(*player, instance->GetData64(GO_ARGENT_COLISEUM_FLOOR)))
                     floor->SetDestructibleState(GO_DESTRUCTIBLE_DAMAGED);
 
                 creature->CastSpell(creature, SPELL_CORPSE_TELEPORT, false);
@@ -318,7 +318,7 @@ class boss_lich_king_toc : public CreatureScript
                             break;
                         case 5080:
                         {
-                            if (GameObject* go = GameObject::GetGameObject(*me, _instance->GetData64(GO_ARGENT_COLISEUM_FLOOR)))
+                            if (GameObject* go = ObjectAccessor::GetGameObject(*me, _instance->GetData64(GO_ARGENT_COLISEUM_FLOOR)))
                             {
                                 go->SetDisplayId(DISPLAYID_DESTROYED_FLOOR);
                                 go->SetFlag(GAMEOBJECT_FLAGS, GO_FLAG_DAMAGED | GO_FLAG_NODESPAWN);

src/server/game/Entities/GameObject/GameObject.cpp
@@ -892,11 +892,6 @@ void GameObject::DeleteFromDB()
     WorldDatabase.Execute(stmt);
 }
 
-GameObject* GameObject::GetGameObject(WorldObject& object, uint64 guid)
-{
-    return object.GetMap()->GetGameObject(guid);
-}
-
 /*********************************************************/
 /***                    QUEST SYSTEM                   ***/
 /*********************************************************/

src/server/game/Entities/GameObject/GameObject.h
@@ -643,7 +643,6 @@ class GameObject : public WorldObject, public GridObject<GameObject>, public Map
 
         bool Create(uint32 guidlow, uint32 name_id, Map* map, uint32 phaseMask, float x, float y, float z, float ang, float rotation0, float rotation1, float rotation2, float rotation3, uint32 animprogress, GOState go_state, uint32 artKit = 0);
         void Update(uint32 p_time) override;
-        static GameObject* GetGameObject(WorldObject& object, uint64 guid);
         GameObjectTemplate const* GetGOInfo() const { return m_goInfo; }
         GameObjectData const* GetGOData() const { return m_goData; }
         GameObjectValue const* GetGOValue() const { return &m_goValue; }

src/server/scripts/EasternKingdoms/Deadmines/boss_mr_smite.cpp
@@ -133,7 +133,7 @@ public:
                 ++uiHealth;
                 DoCastAOE(SPELL_SMITE_STOMP, false);
                 SetCombatMovement(false);
-                if (GameObject* go = GameObject::GetGameObject(*me, instance->GetData64(DATA_SMITE_CHEST)))
+                if (GameObject* go = ObjectAccessor::GetGameObject(*me, instance->GetData64(DATA_SMITE_CHEST)))
                 {
                     me->GetMotionMaster()->Clear();
                     me->GetMotionMaster()->MovePoint(1, go->GetPositionX() - 3.0f, go->GetPositionY(), go->GetPositionZ());

src/server/scripts/EasternKingdoms/Gnomeregan/gnomeregan.cpp
@@ -142,56 +142,48 @@ public:
                 uiPhase = uiPhaseStep;
         }
 
-        void CaveDestruction(bool bBool)
+        void CaveDestruction(bool isRight)
         {
             if (GoSummonList.empty())
                 return;
 
             for (std::list<uint64>::const_iterator itr = GoSummonList.begin(); itr != GoSummonList.end(); ++itr)
             {
-               if (GameObject* go = GameObject::GetGameObject(*me, *itr))
-               {
-                    if (go)
+                if (GameObject* go = ObjectAccessor::GetGameObject(*me, *itr))
+                {
+                    if (Creature* trigger = go->SummonTrigger(go->GetPositionX(), go->GetPositionY(), go->GetPositionZ(), 0, 1))
                     {
-                        if (Creature* trigger = go->SummonTrigger(go->GetPositionX(), go->GetPositionY(), go->GetPositionZ(), 0, 1))
-                        {
-                            //visual effects are not working!
-                            trigger->CastSpell(trigger, 11542, true);
-                            trigger->CastSpell(trigger, 35470, true);
-                        }
-                        go->RemoveFromWorld();
-                        //go->CastSpell(me, 12158); makes all die?!
+                        //visual effects are not working!
+                        trigger->CastSpell(trigger, 11542, true);
+                        trigger->CastSpell(trigger, 35470, true);
                     }
-               }
+                    go->RemoveFromWorld();
+                    //go->CastSpell(me, 12158); makes all die?!
+                }
             }
 
-           if (bBool)
-           {
-                if (GameObject* go = GameObject::GetGameObject(*me, instance->GetData64(DATA_GO_CAVE_IN_RIGHT)))
-                    instance->HandleGameObject(0, false, go);
-           }else
-                if (GameObject* go = GameObject::GetGameObject(*me, instance->GetData64(DATA_GO_CAVE_IN_LEFT)))
-                    instance->HandleGameObject(0, false, go);
+            if (GameObject* go = ObjectAccessor::GetGameObject(*me, instance->GetData64(isRight ? DATA_GO_CAVE_IN_RIGHT : DATA_GO_CAVE_IN_LEFT)))
+                instance->HandleGameObject(0, false, go);
         }
 
         void SetInFace(bool isRight)
         {
-            if (GameObject* go = GameObject::GetGameObject(*me, instance->GetData64(isRight ? DATA_GO_CAVE_IN_RIGHT : DATA_GO_CAVE_IN_LEFT)))
+            if (GameObject* go = ObjectAccessor::GetGameObject(*me, instance->GetData64(isRight ? DATA_GO_CAVE_IN_RIGHT : DATA_GO_CAVE_IN_LEFT)))
                 me->SetFacingToObject(go);
         }
 
         void RestoreAll()
         {
-            if (GameObject* go = GameObject::GetGameObject(*me, instance->GetData64(DATA_GO_CAVE_IN_RIGHT)))
+            if (GameObject* go = ObjectAccessor::GetGameObject(*me, instance->GetData64(DATA_GO_CAVE_IN_RIGHT)))
                 instance->HandleGameObject(0, false, go);
 
-            if (GameObject* go = GameObject::GetGameObject(*me, instance->GetData64(DATA_GO_CAVE_IN_LEFT)))
+            if (GameObject* go = ObjectAccessor::GetGameObject(*me, instance->GetData64(DATA_GO_CAVE_IN_LEFT)))
                 instance->HandleGameObject(0, false, go);
 
             if (!GoSummonList.empty())
                 for (std::list<uint64>::const_iterator itr = GoSummonList.begin(); itr != GoSummonList.end(); ++itr)
                 {
-                    if (GameObject* go = GameObject::GetGameObject(*me, *itr))
+                    if (GameObject* go = ObjectAccessor::GetGameObject(*me, *itr))
                         go->RemoveFromWorld();
                 }
 
@@ -406,7 +398,7 @@ public:
                             SetInFace(true);
                             Talk(SAY_BLASTMASTER_5);
                             Summon(1);
-                            if (GameObject* go = GameObject::GetGameObject(*me, instance->GetData64(DATA_GO_CAVE_IN_RIGHT)))
+                            if (GameObject* go = ObjectAccessor::GetGameObject(*me, instance->GetData64(DATA_GO_CAVE_IN_RIGHT)))
                                 instance->HandleGameObject(0, true, go);
                             NextStep(3000, true);
                             break;
@@ -452,7 +444,7 @@ public:
                         case 16:
                             Talk(SAY_BLASTMASTER_14);
                             SetInFace(false);
-                            if (GameObject* go = GameObject::GetGameObject(*me, instance->GetData64(DATA_GO_CAVE_IN_LEFT)))
+                            if (GameObject* go = ObjectAccessor::GetGameObject(*me, instance->GetData64(DATA_GO_CAVE_IN_LEFT)))
                                 instance->HandleGameObject(0, true, go);
                             NextStep(2000, true);
                             break;

src/server/scripts/EasternKingdoms/Scholomance/boss_darkmaster_gandling.cpp
@@ -162,52 +162,58 @@ class spell_shadow_portal : public SpellScriptLoader
         {
             PrepareSpellScript(spell_shadow_portal_SpellScript);
 
+            bool Load() override
+            {
+                _instance = GetCaster()->GetInstanceScript();
+                return _instance != nullptr;
+            }
+
             void HandleCast(SpellEffIndex /*effIndex*/)
             {
-                Creature* caster = GetCaster()->ToCreature();
-                int8 attempts  = 0;
-                int32 spell_to_cast =0;
+                Unit* caster = GetCaster();
+                uint8 attempts = 0;
+                uint32 spellId = 0;
 
-                while (!spell_to_cast)
+                while (!spellId)
                 {
                     if (attempts++ >= 6) break;
 
                     switch (urand(0, 5))
                     {
                         case ROOM_HALL_OF_SECRETS:
-                            if (InstanceScript* instance = GetCaster()->GetInstanceScript())
-                                if (GameObject::GetGameObject(*caster, instance->GetData64(GO_GATE_RAVENIAN))->GetGoState() == GO_STATE_ACTIVE)
-                                    spell_to_cast = SPELL_SHADOW_PORTAL_HALLOFSECRETS;
+                            if (GameObject* go = ObjectAccessor::GetGameObject(*caster, _instance->GetData64(GO_GATE_RAVENIAN)))
+                                if (go->GetGoState() == GO_STATE_ACTIVE)
+                                    spellId = SPELL_SHADOW_PORTAL_HALLOFSECRETS;
                             break;
                         case ROOM_HALL_OF_THE_DAMNED:
-                            if (InstanceScript* instance = GetCaster()->GetInstanceScript())
-                                if (GameObject::GetGameObject(*caster, instance->GetData64(GO_GATE_THEOLEN))->GetGoState() == GO_STATE_ACTIVE)
-                                    spell_to_cast = SPELL_SHADOW_PORTAL_HALLOFTHEDAMNED;
+                            if (GameObject* go = ObjectAccessor::GetGameObject(*caster, _instance->GetData64(GO_GATE_THEOLEN)))
+                                if (go->GetGoState() == GO_STATE_ACTIVE)
+                                    spellId = SPELL_SHADOW_PORTAL_HALLOFTHEDAMNED;
                             break;
                         case ROOM_THE_COVEN:
-                            if (InstanceScript* instance = GetCaster()->GetInstanceScript())
-                                if (GameObject::GetGameObject(*caster, instance->GetData64(GO_GATE_MALICIA))->GetGoState() == GO_STATE_ACTIVE)
-                                    spell_to_cast = SPELL_SHADOW_PORTAL_THECOVEN;
+                            if (GameObject* go = ObjectAccessor::GetGameObject(*caster, _instance->GetData64(GO_GATE_MALICIA)))
+                                if (go->GetGoState() == GO_STATE_ACTIVE)
+                                    spellId = SPELL_SHADOW_PORTAL_THECOVEN;
                             break;
                         case ROOM_THE_SHADOW_VAULT:
-                            if (InstanceScript* instance = GetCaster()->GetInstanceScript())
-                                if (GameObject::GetGameObject(*caster, instance->GetData64(GO_GATE_ILLUCIA))->GetGoState() == GO_STATE_ACTIVE)
-                                    spell_to_cast = SPELL_SHADOW_PORTAL_THESHADOWVAULT;
+                            if (GameObject* go = ObjectAccessor::GetGameObject(*caster, _instance->GetData64(GO_GATE_ILLUCIA)))
+                                if (go->GetGoState() == GO_STATE_ACTIVE)
+                                    spellId = SPELL_SHADOW_PORTAL_THESHADOWVAULT;
                             break;
                         case ROOM_BAROV_FAMILY_VAULT:
-                            if (InstanceScript* instance = GetCaster()->GetInstanceScript())
-                                if (GameObject::GetGameObject(*caster, instance->GetData64(GO_GATE_BAROV))->GetGoState() == GO_STATE_ACTIVE)
-                                    spell_to_cast = SPELL_SHADOW_PORTAL_BAROVFAMILYVAULT;
+                            if (GameObject* go = ObjectAccessor::GetGameObject(*caster, _instance->GetData64(GO_GATE_BAROV)))
+                                if (go->GetGoState() == GO_STATE_ACTIVE)
+                                    spellId = SPELL_SHADOW_PORTAL_BAROVFAMILYVAULT;
                             break;
                         case ROOM_VAULT_OF_THE_RAVENIAN:
-                            if (InstanceScript* instance = GetCaster()->GetInstanceScript())
-                                if (GameObject::GetGameObject(*caster, instance->GetData64(GO_GATE_POLKELT))->GetGoState() == GO_STATE_ACTIVE)
-                                    spell_to_cast = SPELL_SHADOW_PORTAL_VAULTOFTHERAVENIAN;
+                            if (GameObject* go = ObjectAccessor::GetGameObject(*caster, _instance->GetData64(GO_GATE_POLKELT)))
+                                if (go->GetGoState() == GO_STATE_ACTIVE)
+                                    spellId = SPELL_SHADOW_PORTAL_VAULTOFTHERAVENIAN;
                             break;
                     }
 
-                    if (spell_to_cast)
-                        GetHitUnit()->CastSpell(GetHitUnit(), spell_to_cast);
+                    if (spellId)
+                        GetHitUnit()->CastSpell(GetHitUnit(), spellId);
                 }
             }
 
@@ -215,6 +221,9 @@ class spell_shadow_portal : public SpellScriptLoader
             {
                 OnEffectHitTarget += SpellEffectFn(spell_shadow_portal_SpellScript::HandleCast, EFFECT_0, SPELL_EFFECT_DUMMY);
             }
+
+        private:
+            InstanceScript* _instance;
         };
 
         SpellScript* GetSpellScript() const override
@@ -276,12 +285,17 @@ class spell_shadow_portal_rooms : public SpellScriptLoader
         {
             PrepareSpellScript(spell_shadow_portal_rooms_SpellScript);
 
+            bool Load() override
+            {
+                _instance = GetCaster()->GetInstanceScript();
+                return _instance != nullptr;
+            }
+
             void HandleSendEvent(SpellEffIndex effIndex)
             {
                 // If only one player in threat list fail spell
 
-                Creature* Summoned = NULL;
-                Creature* caster = GetCaster()->ToCreature();
+                Unit* caster = GetCaster();
 
                 int8 pos_to_summon = 0;
                 int8 phase_to_set = 0;
@@ -323,21 +337,19 @@ class spell_shadow_portal_rooms : public SpellScriptLoader
                         break;
                 }
 
-                if (gate_to_close && (GetCaster()->GetMap()->GetId() == 289))
+                if (gate_to_close && (caster->GetMap()->GetId() == 289))
                 {
                     for (uint8 i = 0; i < 3; ++i)
                     {
-                        Summoned = GetCaster()->SummonCreature(NPC_RISEN_GUARDIAN, SummonPos[pos_to_summon++], TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 120000);
-                        if (Summoned)
+                        if (Creature* Summoned = caster->SummonCreature(NPC_RISEN_GUARDIAN, SummonPos[pos_to_summon++], TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 120000))
                         {
                             Summoned->GetMotionMaster()->MoveRandom(5);
                             Summoned->AI()->SetData(0, phase_to_set);
                         }
                     }
 
-                    if (InstanceScript* instance = GetCaster()->GetInstanceScript())
-                        if (GameObject* gate = GameObject::GetGameObject(*caster, instance->GetData64(gate_to_close)))
-                            gate->SetGoState(GO_STATE_READY);
+                    if (GameObject* gate = ObjectAccessor::GetGameObject(*caster, _instance->GetData64(gate_to_close)))
+                        gate->SetGoState(GO_STATE_READY);
                 }
             }
 
@@ -345,6 +357,9 @@ class spell_shadow_portal_rooms : public SpellScriptLoader
             {
                 OnEffectHit += SpellEffectFn(spell_shadow_portal_rooms_SpellScript::HandleSendEvent, EFFECT_1, SPELL_EFFECT_SEND_EVENT);
             }
+
+        private:
+            InstanceScript* _instance;
         };
 
         SpellScript* GetSpellScript() const override

src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheChampion/trial_of_the_champion.cpp
@@ -138,7 +138,7 @@ public:
                     break;
                 case DATA_IN_POSITION: //movement done.
                     me->GetMotionMaster()->MovePoint(1, 735.81f, 661.92f, 412.39f);
-                    if (GameObject* go = GameObject::GetGameObject(*me, instance->GetData64(DATA_MAIN_GATE)))
+                    if (GameObject* go = ObjectAccessor::GetGameObject(*me, instance->GetData64(DATA_MAIN_GATE)))
                         instance->HandleGameObject(go->GetGUID(), false);
                     NextStep(10000, false, 3);
                     break;

commit 8bb1d4d73b63c3666ead82c12a2bd9f2e25a5803

Scripts/ToC: fixed typo introduced in b4e941869844dcabb386feb943082a096581f017 * also update anub arak position to sniffed values
Closes #13323

src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/boss_anubarak_trial.cpp
@@ -212,7 +212,6 @@ class boss_anubarak_trial : public CreatureScript
             }
 
             void MoveInLineOfSight(Unit* /*who*/) override
-
             {
                 if (!_intro)
                 {

src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/instance_trial_of_the_crusader.cpp
@@ -68,15 +68,10 @@ class instance_trial_of_the_crusader : public InstanceMapScript
                 else
                     player->SendUpdateWorldState(UPDATE_STATE_UI_SHOW, 0);
 
-                // make sure Anub'arak isnt missing and floor is destroyed after a crash
+                // make sure Anub'arak isnt missing
                 if (GetBossState(BOSS_LICH_KING) == DONE && TrialCounter && GetBossState(BOSS_ANUBARAK) != DONE)
-                {
-                    if (Creature* anubArak = ObjectAccessor::GetCreature(*player, GetGuidData(NPC_ANUBARAK)))
-                        anubArak = player->SummonCreature(NPC_ANUBARAK, AnubarakLoc[0].GetPositionX(), AnubarakLoc[0].GetPositionY(), AnubarakLoc[0].GetPositionZ(), 3, TEMPSUMMON_CORPSE_TIMED_DESPAWN, DESPAWN_TIME);
-
-                    if (GameObject* floor = ObjectAccessor::GetGameObject(*player, GetGuidData(GO_ARGENT_COLISEUM_FLOOR)))
-                        floor->SetDestructibleState(GO_DESTRUCTIBLE_DAMAGED);
-                }
+                    if (!ObjectAccessor::GetCreature(*player, GetGuidData(NPC_ANUBARAK)))
+                        player->SummonCreature(NPC_ANUBARAK, AnubarakLoc[0], TEMPSUMMON_CORPSE_TIMED_DESPAWN, DESPAWN_TIME);
             }
 
             void OpenDoor(ObjectGuid guid)
@@ -160,23 +155,15 @@ class instance_trial_of_the_crusader : public InstanceMapScript
                 switch (go->GetEntry())
                 {
                     case GO_CRUSADERS_CACHE_10:
-                        if (instance->GetSpawnMode() == RAID_DIFFICULTY_10MAN_NORMAL)
-                            CrusadersCacheGUID = go->GetGUID();
-                        break;
                     case GO_CRUSADERS_CACHE_25:
-                        if (instance->GetSpawnMode() == RAID_DIFFICULTY_25MAN_NORMAL)
-                            CrusadersCacheGUID = go->GetGUID();
-                        break;
                     case GO_CRUSADERS_CACHE_10_H:
-                        if (instance->GetSpawnMode() == RAID_DIFFICULTY_10MAN_HEROIC)
-                            CrusadersCacheGUID = go->GetGUID();
-                        break;
                     case GO_CRUSADERS_CACHE_25_H:
-                        if (instance->GetSpawnMode() == RAID_DIFFICULTY_25MAN_HEROIC)
-                            CrusadersCacheGUID = go->GetGUID();
+                        CrusadersCacheGUID = go->GetGUID();
                         break;
                     case GO_ARGENT_COLISEUM_FLOOR:
                         FloorGUID = go->GetGUID();
+                        if (GetBossState(BOSS_LICH_KING) == DONE)
+                            go->SetDestructibleState(GO_DESTRUCTIBLE_DAMAGED);
                         break;
                     case GO_MAIN_GATE_DOOR:
                         MainGateDoorGUID = go->GetGUID();

src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/trial_of_the_crusader.cpp
@@ -207,11 +207,8 @@ class npc_announcer_toc10 : public CreatureScript
                 creature->CastSpell(creature, SPELL_CORPSE_TELEPORT, false);
                 creature->CastSpell(creature, SPELL_DESTROY_FLOOR_KNOCKUP, false);
 
-                Creature* anubArak = ObjectAccessor::GetCreature(*creature, instance->GetGuidData(NPC_ANUBARAK));
-                if (!anubArak || !anubArak->IsAlive())
-                    anubArak = creature->SummonCreature(NPC_ANUBARAK, AnubarakLoc[0].GetPositionX(), AnubarakLoc[0].GetPositionY(), AnubarakLoc[0].GetPositionZ(), 3, TEMPSUMMON_CORPSE_TIMED_DESPAWN, DESPAWN_TIME);
-
-                instance->SetBossState(BOSS_ANUBARAK, NOT_STARTED);
+                if (!ObjectAccessor::GetCreature(*creature, instance->GetGuidData(NPC_ANUBARAK)))
+                    creature->SummonCreature(NPC_ANUBARAK, AnubarakLoc[0], TEMPSUMMON_CORPSE_TIMED_DESPAWN, DESPAWN_TIME);
 
                 if (creature->IsVisible())
                     creature->SetVisible(false);
@@ -319,19 +316,15 @@ class boss_lich_king_toc : public CreatureScript
                         case 5080:
                         {
                             if (GameObject* go = ObjectAccessor::GetGameObject(*me, _instance->GetGuidData(GO_ARGENT_COLISEUM_FLOOR)))
-                            {
-                                go->SetDisplayId(DISPLAYID_DESTROYED_FLOOR);
-                                go->SetFlag(GAMEOBJECT_FLAGS, GO_FLAG_DAMAGED | GO_FLAG_NODESPAWN);
-                                go->SetGoState(GO_STATE_ACTIVE);
-                            }
+                                go->SetDestructibleState(GO_DESTRUCTIBLE_DAMAGED);
 
                             me->CastSpell(me, SPELL_CORPSE_TELEPORT, false);
                             me->CastSpell(me, SPELL_DESTROY_FLOOR_KNOCKUP, false);
 
                             _instance->SetBossState(BOSS_LICH_KING, DONE);
-                            Creature* temp = ObjectAccessor::GetCreature(*me, _instance->GetGuidData(NPC_ANUBARAK));
-                            if (!temp || !temp->IsAlive())
-                                temp = me->SummonCreature(NPC_ANUBARAK, AnubarakLoc[0].GetPositionX(), AnubarakLoc[0].GetPositionY(), AnubarakLoc[0].GetPositionZ(), 3, TEMPSUMMON_CORPSE_TIMED_DESPAWN, DESPAWN_TIME);
+
+                            if (!ObjectAccessor::GetCreature(*me, _instance->GetGuidData(NPC_ANUBARAK)))
+                                me->SummonCreature(NPC_ANUBARAK, AnubarakLoc[0], TEMPSUMMON_CORPSE_TIMED_DESPAWN, DESPAWN_TIME);
 
                             _instance->SetData(TYPE_EVENT, 0);
 

src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/trial_of_the_crusader.h
@@ -42,9 +42,7 @@ enum SpellIds
 
 enum MiscData
 {
-    DESPAWN_TIME                = 1200000,
-
-    DISPLAYID_DESTROYED_FLOOR   = 9060
+    DESPAWN_TIME                = 1200000
 };
 
 const Position ToCSpawnLoc[]=
@@ -133,7 +131,7 @@ const Position LichKingLoc[]=
 
 const Position AnubarakLoc[]=
 {
-    {787.932556f, 133.289780f, 142.612152f, 0},  // 0 - Anub'arak start location
+    {783.9305f, 132.9722f, 142.6711f, 3.141593f}, // 0 - Anub'arak Spawn Location (sniffed)
     {695.240051f, 137.834824f, 142.200000f, 0},  // 1 - Anub'arak move point location
     {694.886353f, 102.484665f, 142.119614f, 0},  // 3 - Nerub Spawn
     {694.500671f, 185.363968f, 142.117905f, 0},  // 5 - Nerub Spawn
@@ -232,13 +230,13 @@ enum CreatureIds
     NPC_HORDE_WARLOCK                   = 34450,
     NPC_HORDE_WARRIOR                   = 34453,
 
-    NPC_LIGHTBANE               = 34497,
-    NPC_DARKBANE                = 34496,
+    NPC_LIGHTBANE                       = 34497,
+    NPC_DARKBANE                        = 34496,
 
-    NPC_DARK_ESSENCE            = 34567,
-    NPC_LIGHT_ESSENCE           = 34568,
+    NPC_DARK_ESSENCE                    = 34567,
+    NPC_LIGHT_ESSENCE                   = 34568,
 
-    NPC_ANUBARAK                = 34564
+    NPC_ANUBARAK                        = 34564
 };
 
 enum GameObjectIds