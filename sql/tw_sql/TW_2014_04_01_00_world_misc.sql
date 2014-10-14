51539a044382b10c38e81bd2196580557c010b06
Sarah Alexander <sarah.trysan@live.com>
2014-10-14 19:08:15 -0300

    Delete TW_2014_04_01_00_world_misc.sql

--- sql/tw_sql/TW_2014_04_01_00_world_misc.sql
+++
@@ -1,6 +0,0 @@
-UPDATE `creature_template` SET `flags_extra` = '130' WHERE `entry` = '29397'; 
-DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = '54355';
-INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `comment`) VALUES ('54355', '54402', 'Landmine Knockback'); 
-DELETE FROM `disables` WHERE `sourceType` = '4' AND `entry` = '5258'; 
-DELETE FROM `spell_script_names` WHERE `spell_id` = '57099';
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('57099', 'spell_gen_landmine_knockback'); 
