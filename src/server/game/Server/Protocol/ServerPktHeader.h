681dc55798f6cfcb40f386561a6116c014fd477d
Shauren <shauren.trinity@gmail.com>
2014-10-20 01:27:00 +0200

    Core/PacketIO: Updated packet headers to 6.0

--- src/server/game/Server/Protocol/ServerPktHeader.h
+++ src/server/game/Server/Protocol/ServerPktHeader.h
@@ -19,43 +19,23 @@
 #ifndef __SERVERPKTHDR_H__
 #define __SERVERPKTHDR_H__
 
-#include "Log.h"
-
 #pragma pack(push, 1)
 
-struct ServerPktHeader
+union ServerPktHeader
 {
-    /**
-     * size is the length of the payload _plus_ the length of the opcode
-     */
-    ServerPktHeader(uint32 size, uint16 cmd) : size(size)
+    static uint32 const SizeOf[2];
+
+    struct
     {
-        uint8 headerIndex=0;
-        if (isLargePacket())
-        {
-            TC_LOG_DEBUG("network", "initializing large server to client packet. Size: %u, cmd: %u", size, cmd);
-            header[headerIndex++] = 0x80 | (0xFF & (size >> 16));
-        }
-        header[headerIndex++] = 0xFF &(size >> 8);
-        header[headerIndex++] = 0xFF & size;
+        uint16 Size;
+        uint32 Command;
+    } Setup;
 
-        header[headerIndex++] = 0xFF & cmd;
-        header[headerIndex++] = 0xFF & (cmd >> 8);
-    }
-
-    uint8 getHeaderLength()
+    struct
     {
-        // cmd = 2 bytes, size= 2||3bytes
-        return 2 + (isLargePacket() ? 3 : 2);
-    }
-
-    bool isLargePacket() const
-    {
-        return size > 0x7FFF;
-    }
-
-    const uint32 size;
-    uint8 header[5];
+        uint32 Command : 13;
+        uint32 Size : 19;
+    } Normal;
 };
 
 #pragma pack(pop)
--- src/server/game/Server/WorldSocket.cpp
+++ src/server/game/Server/WorldSocket.cpp
@@ -32,11 +32,18 @@ std::string const WorldSocket::ServerConnectionInitialize("WORLD OF WARCRAFT CON
 
 std::string const WorldSocket::ClientConnectionInitialize("WORLD OF WARCRAFT CONNECTION - CLIENT TO SERVER");
 
+uint32 const ClientPktHeader::SizeOf[2][2] =
+{
+    { 2, 0 },
+    { 6, 4 }
+};
+
+uint32 const ServerPktHeader::SizeOf[2] = { sizeof(uint16) + sizeof(uint32), sizeof(uint32) };
 
 WorldSocket::WorldSocket(tcp::socket&& socket)
     : Socket(std::move(socket)), _authSeed(rand32()), _OverSpeedPings(0), _worldSession(nullptr), _initialized(false)
 {
-    _headerBuffer.Resize(2);
+    _headerBuffer.Resize(ClientPktHeader::SizeOf[0][0]);
 }
 
 void WorldSocket::Start()
@@ -44,8 +51,9 @@ void WorldSocket::Start()
     AsyncRead();
 
     MessageBuffer initializer;
-    ServerPktHeader header(ServerConnectionInitialize.size(), 0);
-    initializer.Write(header.header, header.getHeaderLength() - 2);
+    ServerPktHeader header;
+    header.Setup.Size = ServerConnectionInitialize.size();
+    initializer.Write(&header, sizeof(header.Setup.Size));
     initializer.Write(ServerConnectionInitialize.c_str(), ServerConnectionInitialize.length());
 
     std::unique_lock<std::mutex> dummy(_writeLock, std::defer_lock);
@@ -121,38 +129,51 @@ void WorldSocket::ReadHandler()
     AsyncRead();
 }
 
+void WorldSocket::ExtractOpcodeAndSize(ClientPktHeader const* header, uint32& opcode, uint32& size) const
+{
+    if (_authCrypt.IsInitialized())
+    {
+        opcode = header->Normal.Command;
+        size = header->Normal.Size;
+    }
+    else
+    {
+        opcode = header->Setup.Command;
+        size = header->Setup.Size;
+        if (_initialized)
+            size -= 4;
+    }
+}
+
 bool WorldSocket::ReadHeaderHandler()
 {
-    ASSERT(_headerBuffer.GetActiveSize() == (_initialized ? sizeof(ClientPktHeader) : 2));
+    ASSERT(_headerBuffer.GetActiveSize() == ClientPktHeader::SizeOf[_initialized][_authCrypt.IsInitialized()], "Header size %u different than expected %u", _headerBuffer.GetActiveSize(), ClientPktHeader::SizeOf[_initialized][_authCrypt.IsInitialized()]);
 
     _authCrypt.DecryptRecv(_headerBuffer.GetReadPointer(), _headerBuffer.GetActiveSize());
 
     ClientPktHeader* header = reinterpret_cast<ClientPktHeader*>(_headerBuffer.GetReadPointer());
-    EndianConvertReverse(header->size);
+    uint32 opcode;
+    uint32 size;
 
-    if (_initialized)
-        EndianConvert(header->cmd);
+    ExtractOpcodeAndSize(header, opcode, size);
 
-    if (!header->IsValidSize() || (_initialized && !header->IsValidOpcode()))
+    if (!ClientPktHeader::IsValidSize(size) || (_initialized && !ClientPktHeader::IsValidOpcode(opcode)))
     {
         if (_worldSession)
         {
             Player* player = _worldSession->GetPlayer();
             TC_LOG_ERROR("network", "WorldSocket::ReadHeaderHandler(): client (account: %u, char [GUID: %u, name: %s]) sent malformed packet (size: %hu, cmd: %u)",
-                _worldSession->GetAccountId(), player ? player->GetGUIDLow() : 0, player ? player->GetName().c_str() : "<none>", header->size, header->cmd);
+                _worldSession->GetAccountId(), player ? player->GetGUIDLow() : 0, player ? player->GetName().c_str() : "<none>", size, opcode);
         }
         else
             TC_LOG_ERROR("network", "WorldSocket::ReadHeaderHandler(): client %s sent malformed packet (size: %hu, cmd: %u)",
-                GetRemoteIpAddress().to_string().c_str(), header->size, header->cmd);
+                GetRemoteIpAddress().to_string().c_str(), size, opcode);
 
         CloseSocket();
         return false;
     }
 
-    if (_initialized)
-        header->size -= sizeof(header->cmd);
-
-    _packetBuffer.Resize(header->size);
+    _packetBuffer.Resize(size);
     return true;
 }
 
@@ -161,8 +182,12 @@ bool WorldSocket::ReadDataHandler()
     if (_initialized)
     {
         ClientPktHeader* header = reinterpret_cast<ClientPktHeader*>(_headerBuffer.GetReadPointer());
+        uint32 cmd;
+        uint32 size;
 
-        Opcodes opcode = Opcodes(header->cmd);
+        ExtractOpcodeAndSize(header, cmd, size);
+
+        Opcodes opcode = Opcodes(cmd);
 
         std::string opcodeName = GetOpcodeNameForLogging(opcode);
 
@@ -247,7 +272,7 @@ bool WorldSocket::ReadDataHandler()
         }
 
         _initialized = true;
-        _headerBuffer.Resize(sizeof(ClientPktHeader));
+        _headerBuffer.Resize(ClientPktHeader::SizeOf[1][0]);
         _packetBuffer.Reset();
         HandleSendAuthSession();
     }
@@ -268,24 +293,34 @@ void WorldSocket::SendPacket(WorldPacket& packet)
 
     TC_LOG_TRACE("network.opcode", "S->C: %s %s", (_worldSession ? _worldSession->GetPlayerInfo() : GetRemoteIpAddress().to_string()).c_str(), GetOpcodeNameForLogging(packet.GetOpcode()).c_str());
 
-    ServerPktHeader header(packet.size() + 2, packet.GetOpcode());
-
     std::unique_lock<std::mutex> guard(_writeLock);
 
-    _authCrypt.EncryptSend(header.header, header.getHeaderLength());
+    ServerPktHeader header;
+    uint32 sizeOfHeader = ServerPktHeader::SizeOf[_authCrypt.IsInitialized()];
+    if (_authCrypt.IsInitialized())
+    {
+        header.Normal.Size = packet.size();
+        header.Normal.Command = packet.GetOpcode();
+        _authCrypt.EncryptSend((uint8*)&header, sizeOfHeader);
+    }
+    else
+    {
+        header.Setup.Size = packet.size() + 4;
+        header.Setup.Command = packet.GetOpcode();
+    }
 
 #ifndef TC_SOCKET_USE_IOCP
-    if (_writeQueue.empty() && _writeBuffer.GetRemainingSpace() >= header.getHeaderLength() + packet.size())
+    if (_writeQueue.empty() && _writeBuffer.GetRemainingSpace() >= sizeOfHeader + packet.size())
     {
-        _writeBuffer.Write(header.header, header.getHeaderLength());
+        _writeBuffer.Write((uint8*)&header, sizeOfHeader);
         if (!packet.empty())
             _writeBuffer.Write(packet.contents(), packet.size());
     }
     else
 #endif
     {
-        MessageBuffer buffer(header.getHeaderLength() + packet.size());
-        buffer.Write(header.header, header.getHeaderLength());
+        MessageBuffer buffer(sizeOfHeader + packet.size());
+        buffer.Write((uint8*)&header, sizeOfHeader);
         if (!packet.empty())
             buffer.Write(packet.contents(), packet.size());
 
@@ -396,6 +431,7 @@ void WorldSocket::HandleAuthSession(WorldPacket& recvPacket)
 
     // even if auth credentials are bad, try using the session key we have - client cannot read auth response error without it
     _authCrypt.Init(&k);
+    _headerBuffer.Resize(ClientPktHeader::SizeOf[1][1]);
 
     // First reject the connection if packet contains invalid data or realm state doesn't allow logging in
     if (sWorld->IsClosed())
