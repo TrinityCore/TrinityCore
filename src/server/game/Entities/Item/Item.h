a5be593f5419e287d47ffc99a637be2f29cbfb20
Shauren <shauren.trinity@gmail.com>
2014-10-22 22:18:21 +0200

    Core/Entities: Fifth part of removing GetGUIDLow() uses

--- src/server/game/Entities/Item/Item.h
+++ src/server/game/Entities/Item/Item.h
@@ -341,11 +341,11 @@ class Item : public Object
 
         // Item Refund system
         void SetNotRefundable(Player* owner, bool changestate = true, SQLTransaction* trans = NULL);
-        void SetRefundRecipient(uint32 pGuidLow) { m_refundRecipient = pGuidLow; }
+        void SetRefundRecipient(ObjectGuid const& guid) { m_refundRecipient = guid; }
         void SetPaidMoney(uint32 money) { m_paidMoney = money; }
         void SetPaidExtendedCost(uint32 iece) { m_paidExtendedCost = iece; }
 
-        uint32 GetRefundRecipient() const { return m_refundRecipient; }
+        ObjectGuid const& GetRefundRecipient() const { return m_refundRecipient; }
         uint32 GetPaidMoney() const { return m_paidMoney; }
         uint32 GetPaidExtendedCost() const { return m_paidExtendedCost; }
 
@@ -387,7 +387,7 @@ class Item : public Object
         int16 uQueuePos;
         bool mb_in_trade;                                   // true if item is currently in trade-window
         time_t m_lastPlayedTimeUpdate;
-        uint32 m_refundRecipient;
+        ObjectGuid m_refundRecipient;
         uint32 m_paidMoney;
         uint32 m_paidExtendedCost;
         GuidSet allowedGUIDs;
