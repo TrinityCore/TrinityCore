d67eaab87c9ad5d39e7da488efc857325881d246
DDuarte <dnpd.dd@gmail.com>
2014-10-23 01:43:51 +0100

    DB/World: Remove duplicated file added in 24bd4592fce97

--- sql/updates/world/2014_10_21_00_world_from_335.sql
+++
@@ -1,3 +0,0 @@
--- 
-UPDATE `achievement_criteria_data` SET `value1`=1 WHERE `criteria_id`=10291 AND `type`=12;
-UPDATE `achievement_criteria_data` SET `value1`=0 WHERE `criteria_id`=10292 AND `type`=12;
