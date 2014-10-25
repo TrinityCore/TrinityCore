02a107731ef1afbaedc991f174c1dbb8b89871ba
ShinDarth <borzifrancesco@gmail.com>
2014-10-25 10:00:47 +0200

    Fix #13351

--- src/server/game/Quests/QuestDef.h
+++ src/server/game/Quests/QuestDef.h
@@ -152,9 +152,9 @@ enum QuestFlags
     QUEST_FLAGS_OBJ_TEXT                = 0x00040000,   // use Objective text as Complete text
     QUEST_FLAGS_AUTO_ACCEPT             = 0x00080000,   // The client recognizes this flag as auto-accept. However, NONE of the current quests (3.3.5a) have this flag. Maybe blizz used to use it, or will use it in the future.
     QUEST_FLAGS_UNK1                    = 0x00100000,   //
-    QUEST_FLAGS_AUTO_TAKE               = 0x00200000,   // Automatically suggestion of accepting quest. Not from npc.
-    //QUEST_FLAGS_UNK2                    = 0x00400000,
-    //QUEST_FLAGS_UNK3                    = 0x00800000,   // Found in quest 14069
+    QUEST_FLAGS_PLAYER_CAST             = 0x00200000,   // Player cast reward spell on complete
+    QUEST_FLAGS_UNK2                    = 0x00400000,
+    QUEST_FLAGS_PORTRAIT                = 0x00800000,   // Portrait in quest log
     //QUEST_FLAGS_UNK4                    = 0x01000000,
     // ... 4.x added flags up to 0x80000000 - all unknown for now
 };
