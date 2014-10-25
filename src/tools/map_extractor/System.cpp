8f58e5b8ab7e4785eebaf87e2cd132cb27214c08
Shauren <shauren.trinity@gmail.com>
2014-10-25 12:32:14 +0200

    Tools/Map extractor: Restored default configuration to make maps smaller at the cost of height data precision

--- src/tools/map_extractor/System.cpp
+++ src/tools/map_extractor/System.cpp
@@ -89,7 +89,7 @@ bool  CONF_allow_height_limit = true;
 float CONF_use_minHeight = -500.0f;
 
 // This option allow use float to int conversion
-bool  CONF_allow_float_to_int   = false;
+bool  CONF_allow_float_to_int   = true;
 float CONF_float_to_int8_limit  = 2.0f;      // Max accuracy = val/256
 float CONF_float_to_int16_limit = 2048.0f;   // Max accuracy = val/65536
 float CONF_flat_height_delta_limit = 0.005f; // If max - min less this value - surface is flat
