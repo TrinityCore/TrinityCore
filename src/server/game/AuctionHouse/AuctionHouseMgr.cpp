5a02018836344610e2dbcb27d8fd47f714350f4e
jackpoz <giacomopoz@gmail.com>
2014-10-26 19:37:07 +0100

    Core/AH: Code cleanup
    Remove unused paramters from RemoveAuction(), kept till now for compatibility with ahbot patch.
    Reverts 28dffbee11024c1abc82a29a3fef655d9262904b
    
    (cherry picked from commit 8d39ad435a1f85d73e438c07e81003bfaa0c125f)

--- src/server/game/AuctionHouse/AuctionHouseMgr.cpp
+++ src/server/game/AuctionHouse/AuctionHouseMgr.cpp
@@ -435,7 +435,7 @@ void AuctionHouseObject::AddAuction(AuctionEntry* auction)
     sScriptMgr->OnAuctionAdd(this, auction);
 }
 
-bool AuctionHouseObject::RemoveAuction(AuctionEntry* auction, uint32 /*itemEntry*/)
+bool AuctionHouseObject::RemoveAuction(AuctionEntry* auction)
 {
     bool wasInMap = AuctionsMap.erase(auction->Id) ? true : false;
 
@@ -492,7 +492,7 @@ void AuctionHouseObject::Update()
         auction->DeleteFromDB(trans);
 
         sAuctionMgr->RemoveAItem(auction->itemGUIDLow);
-        RemoveAuction(auction, auction->itemEntry);
+        RemoveAuction(auction);
     }
 
     // Run DB changes
