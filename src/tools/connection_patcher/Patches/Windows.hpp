d554c20007fddd65db39418ae247f6b6feaeebad
Shauren <shauren.trinity@gmail.com>
2014-10-29 20:02:59 +0100

    Tools/Patcher: Added pattern & patch for portal to all build configurations

--- src/tools/connection_patcher/Patches/Windows.hpp
+++ src/tools/connection_patcher/Patches/Windows.hpp
@@ -30,7 +30,7 @@ namespace Connection_Patcher
             struct x86
             {
                 static const std::vector<unsigned char> BNet     () { return { 0xC7, 0x40, 0x0C, 0xD5, 0xF8, 0x7F, 0x82 }; }
-                static const std::vector<unsigned char> Portal   () { return { }; }
+                static const std::vector<unsigned char> Portal   () { return { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }; }
                 static const std::vector<unsigned char> Connect  () { return { 0xEB }; }
                 static const std::vector<unsigned char> Password () { return { 0x75 }; }
                 static const std::vector<unsigned char> Signature() { return { 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0xEB }; }
