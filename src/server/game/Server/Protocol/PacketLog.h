31730d52b6ba30d70ff14f8799e0d494e750c459
Shauren <shauren.trinity@gmail.com>
2014-11-09 20:48:13 +0100

    Core/Logging: Added connection index to packetlog

--- src/server/game/Server/Protocol/PacketLog.h
+++ src/server/game/Server/Protocol/PacketLog.h
@@ -48,7 +48,7 @@ class PacketLog
 
         void Initialize();
         bool CanLogPacket() const { return (_file != NULL); }
-        void LogPacket(WorldPacket const& packet, Direction direction, boost::asio::ip::address const& addr, uint16 port);
+        void LogPacket(WorldPacket const& packet, Direction direction, boost::asio::ip::address const& addr, uint16 port, ConnectionType connectionType);
 
     private:
         FILE* _file;
