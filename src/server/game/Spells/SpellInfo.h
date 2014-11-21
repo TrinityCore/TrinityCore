b1289244780f63df09797d597faac39289982e6a
Kinzcool <kinzzcool@hotmail.com>
2014-11-21 17:29:08 -0500

    Core/Spells: Made the loading of the orientation of SPELL_DEST_DB from SpellEffect.dbc

--- src/server/game/Spells/SpellInfo.h
+++ src/server/game/Spells/SpellInfo.h
@@ -245,6 +245,7 @@ public:
     int32     MiscValue;
     int32     MiscValueB;
     Mechanics Mechanic;
+    float     PositionFacing;
     SpellImplicitTargetInfo TargetA;
     SpellImplicitTargetInfo TargetB;
     SpellRadiusEntry const* RadiusEntry;
@@ -261,8 +262,8 @@ public:
 
     SpellEffectInfo() : _spellInfo(NULL), _effIndex(0), Effect(0), ApplyAuraName(0), ApplyAuraPeriod(0), DieSides(0),
                         RealPointsPerLevel(0), BasePoints(0), PointsPerResource(0), Amplitude(0), ChainAmplitude(0),
-                        BonusCoefficient(0), MiscValue(0), MiscValueB(0), Mechanic(MECHANIC_NONE), RadiusEntry(NULL), ChainTargets(0),
-                        ItemType(0), TriggerSpell(0), ImplicitTargetConditions(NULL) {}
+                        BonusCoefficient(0), MiscValue(0), MiscValueB(0), PositionFacing(0), Mechanic(MECHANIC_NONE),
+                        RadiusEntry(NULL), ChainTargets(0), ItemType(0), TriggerSpell(0), ImplicitTargetConditions(NULL) { }
     SpellEffectInfo(SpellEntry const* spellEntry, SpellInfo const* spellInfo, uint8 effIndex, SpellEffectEntry const* effect);
 
     bool IsEffect() const;
