7e20365acd14c78127386e2ed145fc5814baf228
Aokromes <Aokromes@users.noreply.github.com>
2014-11-24 01:41:04 +0100

    DB/Achievement: Now We're Cookin
    By @Vincent-Michael closes #3993

---
+++ sql/updates/world/2014_11_24_00_world.sql
@@ -0,0 +1,15 @@
+-- Achievement: Now Were Cookin
+DELETE FROM `achievement_criteria_data` WHERE `criteria_id` IN (11118,11119,11120,11121,11122,11123,11124,11125,11126,11127);
+INSERT INTO `achievement_criteria_data` (`criteria_id`, `type`, `value1`, `value2`) VALUES
+-- Alliance
+(11118, 16, 404, 0), -- Now Were Cookin / Cranberry Chutney
+(11119, 16, 404, 0), -- Now Were Cookin / Candied Sweet Potato
+(11120, 16, 404, 0), -- Now Were Cookin / Pumpkin Pie
+(11121, 16, 404, 0), -- Now Were Cookin / Slow-Roasted Turkey
+(11122, 16, 404, 0), -- Now Were Cookin / Spice Bread Stuffing
+-- Horde
+(11123, 16, 404, 0), -- Now Were Cookin / Candied Sweet Potato
+(11124, 16, 404, 0), -- Now Were Cookin / Cranberry Chutney
+(11125, 16, 404, 0), -- Now Were Cookin / Pumpkin Pie
+(11126, 16, 404, 0), -- Now Were Cookin / Slow-Roasted Turkey
+(11127, 16, 404, 0); -- Now Were Cookin / Spice Bread Stuffing
