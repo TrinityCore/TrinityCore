0fa85cd9070635056557443a4af77fd724608b43
Dr-J <daniel.jarrott0@gmail.com>
2014-11-27 22:27:36 +0000

    Fix type on delete clause

--- sql/updates/world/2014_11_27_01_world.sql
+++ sql/updates/world/2014_11_27_01_world.sql
@@ -2,7 +2,7 @@ UPDATE `vehicle_template_accessory` SET `minion`=0 WHERE  `entry` IN(32640,32633
 
 UPDATE `creature_template` SET `ainame`='SmartAI', `scriptname`='' WHERE `entry` IN(32641,32642,32638,32639);
 
-DELETE FROM `smart_scripts` WHERE `entryorguid` IN(32641,32642,62638,32639) AND `source_type`=0;
+DELETE FROM `smart_scripts` WHERE `entryorguid` IN(32641,32642,32638,32639) AND `source_type`=0;
 
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
 (32641, 0, 0, 0, 23, 0, 100, 1, 61424, 1, 0, 0, 22, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Drix Blackwrench - Mojodishu - On Has Aura - Set Phase 1'),
