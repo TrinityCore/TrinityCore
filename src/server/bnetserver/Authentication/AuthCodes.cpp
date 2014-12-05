7376c6b4721ae146464f0c55dcc67f58aa192e80
MistyWorld <kalicem76@hotmail.fr>
2014-12-05 23:46:30 +0100

    Ajout build WoW WoD

--- src/server/bnetserver/Authentication/AuthCodes.cpp
+++ src/server/bnetserver/Authentication/AuthCodes.cpp
@@ -22,6 +22,26 @@ namespace AuthHelper
 {
     static RealmBuildInfo const PostBcAcceptedClientBuilds[] =
     {
+        {19243, 6, 0, 3, ' '},        // Wow warlords of draenor public test et live 6.0.3 19243
+		{19227, 6, 0, 3, ' '},        // Wow warlords of draenor public test 6.0.3 19227
+		{19206, 6, 0, 3, ' '},        // Wow warlords of draenor public test 6.0.3 19206
+		{19103, 6, 0, 3, ' '},
+		{18443, 6, 0, 1, ' '},        // Wow warlords of draenor beta 6.0.1 18443
+		{18414, 5, 4, 8, ' '},        // Last version MoP
+		{18291, 5, 4, 8, ' '},
+		{18019, 5, 4, 7, ' '},
+		{17956, 5, 4, 7, ' '},
+		{17930, 5, 4, 7, ' '},
+		{17898, 5, 4, 7, ' '},
+		{17688, 5, 4, 2, 'a'},
+		{17658, 5, 4, 2, ' '},
+		{17538, 5, 4, 1, ' '},
+		{17399, 5, 4, 0, ' '},
+		{17128, 5, 3, 0, ' '},
+		{16769, 5, 2, 0, ' '},
+		{16357, 5, 1, 0, 'a'},
+		{16309, 5, 1, 0, ' '},
+		{16135, 5, 0, 5, 'b'},
         {15595, 4, 3, 4, ' '},
         {14545, 4, 2, 2, ' '},
         {13623, 4, 0, 6, 'a'},
