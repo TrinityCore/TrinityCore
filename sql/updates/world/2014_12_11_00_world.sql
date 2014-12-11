3e4d9582952ea4bab3cd536f567ba5f4b96ab406
Dr-J <daniel.jarrott0@gmail.com>
2014-12-11 20:19:11 +0000

    DB/Misc: Misc fixes
    1) Fix two black cages which are currently open by default
    2) Move Bash'ir Crystalforge http://www.wowhead.com/object=185921/bashir-crystalforge to its proper position

---
+++ sql/updates/world/2014_12_11_00_world.sql
@@ -0,0 +1,2 @@
+UPDATE `gameobject` SET `state`=1 WHERE `id`=195310 AND `guid` IN(61663,61683);
+UPDATE `gameobject` SET `position_x`=4020.56, `position_y`=5895.08, `position_z`=267.871 WHERE  `guid`=13355;
