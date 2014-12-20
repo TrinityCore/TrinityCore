5177d776d2114f8fe55772973e2cc0333d97d94f
Vincent-Michael <Vincent_Michael@gmx.de>
2014-12-20 11:31:18 +0100

    DB/BattleNet: Update wow build to 19342

--- sql/base/auth_database.sql
+++ sql/base/auth_database.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.15  Distrib 10.0.14-MariaDB, for Win64 (x86)
+-- MySQL dump 10.15  Distrib 10.0.15-MariaDB, for Win64 (x86)
 --
--- Host: localhost    Database: auth
+-- Host: localhost    Database: authdum
 -- ------------------------------------------------------
--- Server version   10.0.14-MariaDB
+-- Server version   10.0.15-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -252,7 +252,7 @@ CREATE TABLE `battlenet_components` (
 
 LOCK TABLES `battlenet_components` WRITE;
 /*!40000 ALTER TABLE `battlenet_components` DISABLE KEYS */;
-INSERT INTO `battlenet_components` VALUES ('Bnet','Mc64',37165),('Bnet','Win',37165),('Bnet','Wn64',37165),('WoW','base',19057),('WoW','deDE',0),('WoW','enGB',0),('WoW','enUS',0),('WoW','esES',0),('WoW','esMX',0),('WoW','frFR',0),('WoW','koKR',0),('WoW','Mc64',19243),('WoW','ruRU',0),('WoW','Win',19243),('WoW','Wn64',19243),('WoW','zhCN',0),('WoW','zhTW',0);
+INSERT INTO `battlenet_components` VALUES ('Bnet','Mc64',37165),('Bnet','Win',37165),('Bnet','Wn64',37165),('WoW','base',19057),('WoW','deDE',0),('WoW','enGB',0),('WoW','enUS',0),('WoW','esES',0),('WoW','esMX',0),('WoW','frFR',0),('WoW','koKR',0),('WoW','Mc64',19342),('WoW','ruRU',0),('WoW','Win',19342),('WoW','Wn64',19342),('WoW','zhCN',0),('WoW','zhTW',0);
 /*!40000 ALTER TABLE `battlenet_components` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -456,7 +456,7 @@ CREATE TABLE `rbac_default_permissions` (
   `secId` int(10) unsigned NOT NULL COMMENT 'Security Level id',
   `permissionId` int(10) unsigned NOT NULL COMMENT 'permission id',
   `realmId` int(11) NOT NULL DEFAULT '-1' COMMENT 'Realm Id, -1 means all',
-  PRIMARY KEY (`secId`,`permissionId`, `realmId`),
+  PRIMARY KEY (`secId`,`permissionId`,`realmId`),
   KEY `fk__rbac_default_permissions__rbac_permissions` (`permissionId`),
   CONSTRAINT `fk__rbac_default_permissions__rbac_permissions` FOREIGN KEY (`permissionId`) REFERENCES `rbac_permissions` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Default permission to assign to different account security levels';
@@ -468,7 +468,7 @@ CREATE TABLE `rbac_default_permissions` (
 
 LOCK TABLES `rbac_default_permissions` WRITE;
 /*!40000 ALTER TABLE `rbac_default_permissions` DISABLE KEYS */;
-INSERT INTO `rbac_default_permissions` VALUES (3,192,-1),(2,193,-1),(1,194,-1),(0,195,-1);
+INSERT INTO `rbac_default_permissions` VALUES (0,195,-1),(1,194,-1),(2,193,-1),(3,192,-1);
 /*!40000 ALTER TABLE `rbac_default_permissions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -621,4 +621,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-12-05 22:49:59
+-- Dump completed on 2014-12-20 11:26:27
