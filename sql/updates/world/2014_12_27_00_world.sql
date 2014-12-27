b0d7b697da06b6fbcf263d5cb2b80694b794c63a
Vincent-Michael <Vincent_Michael@gmx.de>
2014-12-27 01:46:44 +0100

    SQL: Meh (@Kinzcool)

--- sql/updates/world/2014_12_27_00_world.sql
+++ sql/updates/world/2014_12_27_00_world.sql
@@ -1,34 +1,34 @@
 DELETE FROM `quest_greeting` WHERE `Id` IN (28911, 29053, 76609, 82786, 50048, 75121, 74163, 80193, 233099, 79393, 82274, 73097, 28444, 79281, 78323, 18183, 82574, 75177, 82713, 74223, 80389, 79978, 80390, 77928, 85601, 31082, 81361, 19309, 80001, 82334, 80553, 79047);
 INSERT INTO `quest_greeting` (`Id`, `Type`, `GreetEmoteType`, `GreetEmoteDelay`, `Greeting`, `VerifiedBuild`) VALUES
-(28911, 3, 0, 0, '', 19243), -- 28911
-(29053, 3, 0, 0, '', 19243), -- 29053
-(76609, 3, 0, 0, 'Throm-Ka, $c. The ogres of Dreadmaul will pay with blood for their foolish actions.', 19243), -- 76609
-(82786, 3, 0, 0, 'In case you haven''t noticed, I''m busy here!', 19243), -- 82786
-(50048, 3, 0, 0, '', 19243), -- 50048
-(75121, 3, 0, 0, 'The Sunsworn will not let Auchindoun fall.', 19243), -- 75121
-(74163, 3, 0, 0, '', 19243), -- 74163
-(80193, 3, 2, 0, 'At your service, $n.', 19243), -- 80193
-(233099, 5, 0, 0, 'This poster is a list of bounties put up by citizens of Axefall.', 19243), -- 233099
-(79393, 3, 2, 0, 'At your service, $n.', 19243), -- 79393
-(82274, 3, 0, 0, 'The botani wanted to fatten me up! No one fattens Cutter up but Cutter. Nobody!$b$bExcept for maybe Kaz''s cooking.$b$bBut nobody else!', 19243), -- 82274
-(73097, 3, 0, 0, 'They will regret bringing me here alive.', 19243), -- 73097
-(28444, 3, 0, 0, '', 19243), -- 28444
-(79281, 3, 0, 0, '', 19243), -- 79281
-(78323, 3, 0, 0, 'You got my back, right?', 19243), -- 78323
-(18183, 3, 0, 0, '', 19243), -- 18183
-(82574, 3, 30, 0, 'Hey hey, commander! This dwarf I found is provin'' useful already.', 19243), -- 82574
-(75177, 3, 113, 0, 'There is much to do yet.', 19243), -- 75177
-(82713, 3, 0, 0, 'Yeah, yeah, I crashed. Big whoop.', 19243), -- 82713
-(74223, 3, 0, 0, 'Many orcs have fallen trying to prove themselves against the might of the gronn. Signs of their passing litter the stones below us.', 19243), -- 74223
-(80389, 3, 2, 0, 'At your service, $n.', 19243), -- 80389
-(79978, 3, 0, 0, '', 19243), -- 79978
-(80390, 3, 2, 0, 'At your service, $n.', 19243), -- 80390
-(77928, 3, 0, 0, 'Yo.', 19243), -- 77928
-(85601, 3, 0, 0, 'The botani wanted to fatten me up! No one fattens Cutter up but Cutter. Nobody!$b$bExcept for maybe Kaz''s cooking.$b$bBut nobody else!', 19243), -- 85601
-(31082, 3, 0, 0, '', 19243), -- 31082
-(81361, 3, 0, 0, 'It was quite a trial getting up here...', 19243), -- 81361
-(19309, 3, 0, 0, '', 19243), -- 19309
-(80001, 3, 0, 0, '', 19243), -- 80001
-(82334, 3, 0, 0, 'The botani wanted to fatten me up! No one fattens Cutter up but Cutter. Nobody!$b$bExcept for maybe Kaz''s cooking.$b$bBut nobody else!', 19243), -- 82334
-(80553, 3, 2, 0, 'At your service, $n.', 19243), -- 80553
-(79047, 3, 0, 0, 'I owe you a great debt for what you''ve done this day. I see my debts through. If you have need of me, you have only to call.', 19243); -- 79047
+(28911, 0, 0, 0, '', 19243), -- 28911
+(29053, 0, 0, 0, '', 19243), -- 29053
+(76609, 0, 0, 0, 'Throm-Ka, $c. The ogres of Dreadmaul will pay with blood for their foolish actions.', 19243), -- 76609
+(82786, 0, 0, 0, 'In case you haven''t noticed, I''m busy here!', 19243), -- 82786
+(50048, 0, 0, 0, '', 19243), -- 50048
+(75121, 0, 0, 0, 'The Sunsworn will not let Auchindoun fall.', 19243), -- 75121
+(74163, 0, 0, 0, '', 19243), -- 74163
+(80193, 0, 2, 0, 'At your service, $n.', 19243), -- 80193
+(233099, 1, 0, 0, 'This poster is a list of bounties put up by citizens of Axefall.', 19243), -- 233099
+(79393, 0, 2, 0, 'At your service, $n.', 19243), -- 79393
+(82274, 0, 0, 0, 'The botani wanted to fatten me up! No one fattens Cutter up but Cutter. Nobody!$b$bExcept for maybe Kaz''s cooking.$b$bBut nobody else!', 19243), -- 82274
+(73097, 0, 0, 0, 'They will regret bringing me here alive.', 19243), -- 73097
+(28444, 0, 0, 0, '', 19243), -- 28444
+(79281, 0, 0, 0, '', 19243), -- 79281
+(78323, 0, 0, 0, 'You got my back, right?', 19243), -- 78323
+(18183, 0, 0, 0, '', 19243), -- 18183
+(82574, 0, 30, 0, 'Hey hey, commander! This dwarf I found is provin'' useful already.', 19243), -- 82574
+(75177, 0, 113, 0, 'There is much to do yet.', 19243), -- 75177
+(82713, 0, 0, 0, 'Yeah, yeah, I crashed. Big whoop.', 19243), -- 82713
+(74223, 0, 0, 0, 'Many orcs have fallen trying to prove themselves against the might of the gronn. Signs of their passing litter the stones below us.', 19243), -- 74223
+(80389, 0, 2, 0, 'At your service, $n.', 19243), -- 80389
+(79978, 0, 0, 0, '', 19243), -- 79978
+(80390, 0, 2, 0, 'At your service, $n.', 19243), -- 80390
+(77928, 0, 0, 0, 'Yo.', 19243), -- 77928
+(85601, 0, 0, 0, 'The botani wanted to fatten me up! No one fattens Cutter up but Cutter. Nobody!$b$bExcept for maybe Kaz''s cooking.$b$bBut nobody else!', 19243), -- 85601
+(31082, 0, 0, 0, '', 19243), -- 31082
+(81361, 0, 0, 0, 'It was quite a trial getting up here...', 19243), -- 81361
+(19309, 0, 0, 0, '', 19243), -- 19309
+(80001, 0, 0, 0, '', 19243), -- 80001
+(82334, 0, 0, 0, 'The botani wanted to fatten me up! No one fattens Cutter up but Cutter. Nobody!$b$bExcept for maybe Kaz''s cooking.$b$bBut nobody else!', 19243), -- 82334
+(80553, 0, 2, 0, 'At your service, $n.', 19243), -- 80553
+(79047, 0, 0, 0, 'I owe you a great debt for what you''ve done this day. I see my debts through. If you have need of me, you have only to call.', 19243); -- 79047
