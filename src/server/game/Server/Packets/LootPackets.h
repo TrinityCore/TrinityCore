8523fbc63cad98b8afb1c191e86aee93bbd8f1ad
Intel <chemicstry@gmail.com>
2014-12-29 02:54:44 +0200

    Core/Loot: Implemented CMSG_LOOT packet Updated Object Dynamic Flags Cleaned up obsolete code Sorted packet struct alphabetically

---
+++ src/server/game/Server/Packets/LootPackets.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2008-2014 TrinityCore <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LootPackets_h__
+#define LootPackets_h__
+
+#include "Packet.h"
+
+namespace WorldPackets
+{
+    namespace Loot
+    {
+        class LootUnit final : public ClientPacket
+        {
+        public:
+            LootUnit(WorldPacket&& packet) : ClientPacket(CMSG_LOOT, std::move(packet)) { }
+
+            void Read() override;
+
+            ObjectGuid Unit;
+        };
+    }
+}
+
+#endif // LootPackets_h__
