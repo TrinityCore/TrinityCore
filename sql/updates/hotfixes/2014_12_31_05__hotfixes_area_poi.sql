7f3a6e9bff0475c458ae64762c575a834066cc37
Mogadischu <cgnad@live.de>
2014-12-31 20:47:16 +0100

    DB/Hotfixes: Updated full `area_poi` DB2 data to 6.0.3.19342

---
+++ sql/updates/hotfixes/2014_12_31_05__hotfixes_area_poi.sql
@@ -0,0 +1,12 @@
+UPDATE `area_poi` SET `AreaID`=4294967295 WHERE `ID` IN (1252 /*1252*/, 1254 /*1254*/, 1255 /*1255*/, 1352 /*1352*/, 1355 /*1355*/, 1363 /*1363*/, 1364 /*1364*/, 1369 /*1369*/, 1377 /*1377*/, 1378 /*1378*/, 1379 /*1379*/, 1380 /*1380*/, 1389 /*1389*/, 1405 /*1405*/, 1406 /*1406*/, 1407 /*1407*/, 1527 /*1527*/, 1528 /*1528*/, 1529 /*1529*/, 1530 /*1530*/, 1550 /*1550*/, 1612 /*1612*/, 1618 /*1618*/, 1623 /*1623*/, 1628 /*1628*/, 1632 /*1632*/, 1682 /*1682*/, 1696 /*1696*/, 1697 /*1697*/, 1700 /*1700*/, 1707 /*1707*/, 1718 /*1718*/, 2090 /*2090*/);
+UPDATE `area_poi` SET `PositionX`=-10994.64 WHERE `ID`=2560; -- 2560
+UPDATE `area_poi` SET `PositionX`=-10930.78 WHERE `ID`=2561; -- 2561
+UPDATE `area_poi` SET `PositionX`=-12706.02 WHERE `ID`=2562; -- 2562
+UPDATE `area_poi` SET `PositionX`=-12836.27 WHERE `ID`=2568; -- 2568
+UPDATE `area_poi` SET `PositionX`=-10110.46 WHERE `ID`=2572; -- 2572
+UPDATE `area_poi` SET `PositionX`=-13272.82 WHERE `ID`=2573; -- 2573
+UPDATE `area_poi` SET `PositionX`=-10528.03 WHERE `ID`=3004; -- 3004
+UPDATE `area_poi` SET `PositionX`=-14447.72 WHERE `ID`=3006; -- 3006
+UPDATE `area_poi` SET `PositionX`=-10229.63 WHERE `ID`=3022; -- 3022
+UPDATE `area_poi` SET `PositionX`=-11141.02 WHERE `ID`=3024; -- 3024
+UPDATE `area_poi` SET `PlayerConditionID`=21439 WHERE `ID`=4196; -- 4196
