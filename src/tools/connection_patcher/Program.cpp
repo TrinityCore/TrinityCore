e6625e2721c67d4e3afcdb9f9c3343fd23eb58c1
Vincent-Michael <Vincent_Michael@gmx.de>
2015-01-01 00:27:46 +0100

    Update copyright note for 2015 Happy new year

--- src/tools/connection_patcher/Program.cpp
+++ src/tools/connection_patcher/Program.cpp
@@ -1,6 +1,6 @@
 ﻿/*
  * Copyright (C) 2012-2014 Arctium Emulation <http://arctium.org>
- * Copyright (C) 2008-2014 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2008-2015 TrinityCore <http://www.trinitycore.org/>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
