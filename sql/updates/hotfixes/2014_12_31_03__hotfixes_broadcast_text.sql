aa6f3c22cc63e5b1447c65b2bb7c24aded979857
Mogadischu <cgnad@live.de>
2014-12-31 15:41:24 +0100

    DB/Hotfixes: Renamed two unknown fields in `broadcast_text` table

---
+++ sql/updates/hotfixes/2014_12_31_03__hotfixes_broadcast_text.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `broadcast_text` CHANGE COLUMN `UnkMoP1` `EndEmoteID` mediumint(8) UNSIGNED NOT NULL DEFAULT 0;
+ALTER TABLE `broadcast_text` CHANGE COLUMN `UnkMoP2` `Type` mediumint(8) UNSIGNED NOT NULL DEFAULT 0;
