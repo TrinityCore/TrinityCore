89bca8dd259a6fb2a10a097ec358827bfd6fd55f
Rochet2 <rochet2@post.com>
2015-01-09 12:35:47 +0200

    Update ExampleItemGossip.cpp

--- src/server/scripts/Custom/Player and Item Gossip/ExampleItemGossip.cpp
+++ src/server/scripts/Custom/Player and Item Gossip/ExampleItemGossip.cpp
@@ -3,16 +3,16 @@ class example_ItemGossip : public ItemScript
 public: 
     example_ItemGossip() : ItemScript("example_ItemGossip") {}
 
-    bool OnUse(Player* player, Item* item, SpellCastTargets const& targets) // Any hook here
+    bool OnUse(Player* player, Item* item, SpellCastTargets const& targets) override // Any hook here
     {
-        player->PlayerTalkClass->ClearMenus();                              // Clears old options
+        player->PlayerTalkClass->ClearMenus(); // Clears old options
         player->ADD_GOSSIP_ITEM(0, "Morph", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+1);
         player->ADD_GOSSIP_ITEM(0, "Demorph", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+2);
         player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE, item->GetGUID());
-        return false;
+        return false; // Cast the spell on use normally
     }
 
-    void OnGossipSelect(Player* player, Item* item, uint32 sender, uint32 action)
+    void OnGossipSelect(Player* player, Item* item, uint32 sender, uint32 action) override
     {
         player->PlayerTalkClass->ClearMenus();
 
