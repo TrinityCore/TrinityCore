a7b49225283aedd828b42cefb5e113b260003d27
Nayd <dnpd.dd@gmail.com>
2015-01-11 19:35:24 +0000

    DB/Schema: Boost stats in `player_levelstats` from tinyint to smallint
    Needed to accommodate base stats higher than 255
    
    Ref #13823

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -381,7 +381,7 @@ struct PlayerLevelInfo
 {
     PlayerLevelInfo() { for (uint8 i=0; i < MAX_STATS; ++i) stats[i] = 0; }
 
-    uint8 stats[MAX_STATS];
+    uint16 stats[MAX_STATS];
 };
 
 typedef std::list<uint32> PlayerCreateInfoSpells;
