142aa8bcf896a37d544e5704f15ced1bc17615b1
Rushor <PBienati@web.de>
2015-01-11 23:11:08 +0100

    Scripts/Stratholme: Remove unneeded SAI

--- sql/updates/world/2015_01_10_00_world.sql
+++ sql/updates/world/2015_01_10_00_world.sql
@@ -7,10 +7,3 @@ INSERT INTO `spell_target_position` (`id`, `effIndex`, `target_map`, `target_pos
 (17478, 0, 329, 4048.87, -3363.22, 115.05, 3.62, 0),
 (17479, 0, 329, 4051.77, -3350.89, 115.05, 3.06, 0),
 (17480, 0, 329, 4048.37, -3339.96, 115.05, 2.45, 0);
-
--- Mindless Skeleton SAI
-SET @ENTRY := 11197;
-UPDATE `creature_template` SET `AIName`="SmartAI", `ScriptName`='' WHERE `entry`=@ENTRY;
-DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=0;
-INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
-(@ENTRY,0,0,0,54,0,100,0,0,0,0,0,49,0,0,0,0,0,0,21,50,0,0,0,0,0,0,"Mindless Skeleton - On Just Summoned - Start Attacking");
