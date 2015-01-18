b911dc6ba1997751b36f5ccc586e005baad7ef57
Shauren <shauren.trinity@gmail.com>
2015-01-19 00:54:26 +0100

    Core/PacketIO: Updated IsInstanceOnlyOpcode

--- src/server/game/Server/Protocol/Opcodes.h
+++ src/server/game/Server/Protocol/Opcodes.h
@@ -1804,39 +1804,37 @@ inline bool IsInstanceOnlyOpcode(uint32 opcode)
     // TODO: Use names when known
     switch (opcode)
     {
-        case 0x000F: // Client
+        case SMSG_MOUNT_RESULT: // Client
         case 0x0111: // Client
         case 0x03E4: // Client
         case 0x0549: // Client
-        case 0x054C: // Client
         case 0x055A: // Client
-        case 0x056C: // Client
+        case 0x055C: // Client
         case 0x057A: // Client
-        case 0x057B: // Client
-        case 0x05CC: // Client
-        case 0x05EA: // Client
-        case 0x05EC: // Client
+        case 0x057C: // Client
+        case 0x05C9: // Client
+        case 0x05CA: // Client
+        case 0x05D9: // Client
+        case 0x05DA: // Client
+        case 0x05DC: // Client
+        case 0x05E9: // Client
         case 0x05F9: // Client
-        case 0x05FB: // Client
+        case 0x05FC: // Client
+        case 0x074B: // Client
         case 0x074C: // Client
         case 0x075B: // Client
+        case 0x075C: // Client
         case 0x076C: // Client
-        case 0x077B: // Client
-        case 0x077C: // Client
+        case 0x07CB: // Client
         case 0x07CC: // Client
         case 0x07DB: // Client
+        case 0x07DC: // Client
         case 0x07EC: // Client
-        case 0x07FB: // Client
-        case 0x07FC: // Client
-        case 0x0827: // Client
-        case 0x0935: // Client
-        case 0x0F0C: // ClientSpell
+        case SMSG_DUEL_REQUESTED: // Client
+        case SMSG_DUEL_WINNER: // Client
+        case 0x0F0F: // ClientSpell
         case 0x0F10: // ClientSpell
-        case 0x0F1B: // ClientSpell
-        case 0x0F1C: // ClientSpell
-        case 0x0F20: // ClientSpell
-        case 0x0F2C: // ClientSpell
-        case 0x0F2F: // ClientSpell
+        case 0x0F30: // ClientSpell
         case 0x0F3B: // ClientSpell
         case 0x0F8B: // ClientSpell
         case 0x0F8C: // ClientSpell
@@ -1844,35 +1842,43 @@ inline bool IsInstanceOnlyOpcode(uint32 opcode)
         case 0x0F9F: // ClientSpell
         case 0x0FA0: // ClientSpell
         case SMSG_ATTACKSTOP: // Client
-        case 0x14C9: // Client
-        case 0x154A: // Client
+        case SMSG_DUEL_COMPLETE: // Client
+        case 0x154B: // Client
+        case 0x154C: // Client
         case 0x155A: // Client
+        case 0x155B: // Client
         case 0x155C: // Client
         case SMSG_QUESTGIVER_STATUS: // ClientQuest
-        case 0x156A: // Client
         case 0x156B: // Client
-        case 0x157A: // Client
+        case 0x1579: // Client
         case 0x157B: // Client
+        case 0x15CB: // Client
+        case 0x15CC: // Client
+        case 0x15DB: // Client
         case 0x15DC: // Client
         case 0x15EB: // Client
         case 0x15FB: // Client
-        case 0x170C: // ClientSpell
-        case 0x171C: // ClientSpell
-        case 0x171F: // ClientSpell
+        case 0x1710: // ClientSpell
+        case 0x1720: // ClientSpell
+        case 0x172B: // ClientSpell
         case 0x172C: // ClientSpell
         case 0x172F: // ClientSpell
+        case 0x1730: // ClientSpell
         case 0x173C: // ClientSpell
-        case 0x173F: // ClientSpell
-        case 0x1740: // ClientSpell
+        case 0x178B: // ClientSpell
+        case 0x178C: // ClientSpell
+        case 0x178F: // ClientSpell
         case 0x1790: // ClientSpell
         case 0x179B: // ClientSpell
         case 0x179F: // ClientSpell
         case SMSG_ATTACKSTART: // Client
         case 0x1D82: // ClientQuest
         case 0x1D83: // ClientQuest
-        case 0x1D85: // ClientQuest
-        case 0x1D87: // ClientQuest
-        case 0x1D93: // ClientQuest
+        case 0x1D84: // ClientQuest
+        case 0x1D86: // ClientQuest
+        case 0x1D91: // ClientQuest
+        case 0x1D92: // ClientQuest
+        case 0x1D94: // ClientQuest
         case 0x1D96: // ClientQuest
         case 0x1D97: // ClientQuest
         case 0x1DA1: // ClientQuest
@@ -1882,39 +1888,47 @@ inline bool IsInstanceOnlyOpcode(uint32 opcode)
         case 0x1DA5: // ClientQuest
         case 0x1DA7: // ClientQuest
         case SMSG_QUERY_TIME_RESPONSE: // Client
-        case 0x1DC2: // ClientQuest
-        case 0x1DC6: // ClientQuest
-        case 0x1DC7: // ClientQuest
+        case 0x1DC4: // ClientQuest
+        case 0x1DC8: // ClientQuest
+        case 0x1DD1: // ClientQuest
         case 0x1DD2: // ClientQuest
         case 0x1DD3: // ClientQuest
+        case 0x1DD4: // ClientQuest
         case 0x1DD6: // ClientQuest
-        case 0x1DD7: // ClientQuest
-        case 0x1DD8: // ClientQuest
+        case 0x1DE1: // ClientQuest
+        case 0x1DE2: // ClientQuest
+        case 0x1DE3: // ClientQuest
         case 0x1DE4: // ClientQuest
         case 0x1DE5: // ClientQuest
         case 0x1DE7: // ClientQuest
-        case 0x1F02: // ClientQuest
-        case 0x1F06: // ClientQuest
-        case 0x1F07: // ClientQuest
-        case 0x1F0C: // ClientSpell
+        case 0x1F04: // ClientQuest
+        case 0x1F08: // ClientQuest
+        case 0x1F10: // ClientSpell
+        case 0x1F11: // ClientQuest
         case 0x1F12: // ClientQuest
         case 0x1F13: // ClientQuest
+        case 0x1F14: // ClientQuest
         case 0x1F16: // ClientQuest
-        case 0x1F17: // ClientQuest
-        case 0x1F18: // ClientQuest
-        case 0x1F1C: // ClientSpell
-        case 0x1F1F: // ClientSpell
+        case 0x1F20: // ClientSpell
+        case 0x1F21: // ClientQuest
+        case 0x1F22: // ClientQuest
+        case 0x1F23: // ClientQuest
         case 0x1F24: // ClientQuest
         case 0x1F25: // ClientQuest
         case 0x1F27: // ClientQuest
+        case 0x1F2B: // ClientSpell
         case 0x1F2C: // ClientSpell
         case 0x1F2F: // ClientSpell
+        case 0x1F30: // ClientSpell
         case 0x1F3C: // ClientSpell
-        case 0x1F3F: // ClientSpell
-        case 0x1F40: // ClientSpell
+        case 0x1F41: // ClientQuest
+        case 0x1F42: // ClientQuest
         case 0x1F44: // ClientQuest
+        case 0x1F46: // ClientQuest
+        case 0x1F47: // ClientQuest
         case 0x1F48: // ClientQuest
-        case 0x1F51: // ClientQuest
+        case 0x1F52: // ClientQuest
+        case 0x1F53: // ClientQuest
         case 0x1F55: // ClientQuest
         case 0x1F57: // ClientQuest
         case 0x1F61: // ClientQuest
@@ -1922,12 +1936,14 @@ inline bool IsInstanceOnlyOpcode(uint32 opcode)
         case 0x1F64: // ClientQuest
         case 0x1F65: // ClientQuest
         case 0x1F67: // ClientQuest
-        case 0x1F85: // ClientQuest
-        case 0x1F86: // ClientQuest
+        case 0x1F84: // ClientQuest
         case 0x1F87: // ClientQuest
+        case 0x1F8B: // ClientSpell
+        case 0x1F8C: // ClientSpell
+        case 0x1F8F: // ClientSpell
         case 0x1F90: // ClientSpell
-        case 0x1F92: // ClientQuest
-        case 0x1F94: // ClientQuest
+        case 0x1F91: // ClientQuest
+        case 0x1F95: // ClientQuest
         case 0x1F96: // ClientQuest
         case 0x1F97: // ClientQuest
         case 0x1F9B: // ClientSpell
@@ -1938,14 +1954,16 @@ inline bool IsInstanceOnlyOpcode(uint32 opcode)
         case 0x1FA4: // ClientQuest
         case 0x1FA5: // ClientQuest
         case 0x1FA7: // ClientQuest
-        case 0x1FC2: // ClientQuest
-        case 0x1FC6: // ClientQuest
-        case 0x1FC7: // ClientQuest
+        case 0x1FC4: // ClientQuest
+        case 0x1FC8: // ClientQuest
+        case 0x1FD1: // ClientQuest
         case 0x1FD2: // ClientQuest
         case 0x1FD3: // ClientQuest
+        case 0x1FD4: // ClientQuest
         case 0x1FD6: // ClientQuest
-        case 0x1FD7: // ClientQuest
-        case 0x1FD8: // ClientQuest
+        case 0x1FE1: // ClientQuest
+        case 0x1FE2: // ClientQuest
+        case 0x1FE3: // ClientQuest
         case 0x1FE4: // ClientQuest
         case 0x1FE5: // ClientQuest
         case 0x1FE7: // ClientQuest
