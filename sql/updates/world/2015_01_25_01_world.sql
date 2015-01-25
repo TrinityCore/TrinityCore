cb243575c494be37b76407da3b33d314eb50a7e3
Skyepup <john@skyebeyond.net>
2015-01-25 13:03:08 -1000

    DB/Item: Solace of the Fallen
    https://github.com/TrinityCore/TrinityCore/issues/1896

---
+++ sql/updates/world/2015_01_25_01_world.sql
@@ -0,0 +1,8 @@
+--
+DELETE FROM spell_proc_event WHERE entry=67698;
+INSERT INTO spell_proc_event (entry, SchoolMask, SpellFamilyName, SpellFamilyMask0, SpellFamilyMask1, SpellFamilyMask2, procFlags, procEx, ppmRate, CustomChance, Cooldown) VALUES
+(67698, 0, 0, 0, 0, 0, 0, 65536, 0, 0, 0);
+
+DELETE FROM spell_proc_event WHERE entry=67752;
+INSERT INTO spell_proc_event (entry, SchoolMask, SpellFamilyName, SpellFamilyMask0, SpellFamilyMask1, SpellFamilyMask2, procFlags, procEx, ppmRate, CustomChance, Cooldown) VALUES
+(67752, 0, 0, 0, 0, 0, 0, 65536, 0, 0, 0);
