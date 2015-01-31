95b1204798d3ab2a141f81b294b064255c335b8a
jackpoz <giacomopoz@gmail.com>
2015-01-31 22:59:21 +0100

    Core/Misc: Properly sanitize ReputationSpillover data
    Fix an issue added in 024b57bb74a664cd515cf0822d4b8e939a91fe03 that didn't properly skip ReputationSpillover rows with invalid data.
    Fix also a static analysis issue reported by Coverity.

--- src/server/shared/DataStores/DBCStore.h
+++ src/server/shared/DataStores/DBCStore.h
@@ -87,6 +87,13 @@ class DBCStorage
             return (id >= nCount) ? NULL : indexTable.asT[id];
         }
 
+        T const* AssertEntry(uint32 id) const
+        {
+            T const* entry = LookupEntry(id);
+            ASSERT(entry);
+            return entry;
+        }
+
         uint32  GetNumRows() const { return nCount; }
         char const* GetFormat() const { return fmt; }
         uint32 GetFieldCount() const { return fieldCount; }
