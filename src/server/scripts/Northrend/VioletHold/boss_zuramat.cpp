dbb95c9f40783cc9643f8dd6b86bad982206189b
Rushor <PBienati@web.de>
2015-02-12 12:22:03 +0100

    Scripts/Violet Hold: Zuramat the Obliterator - Improve Summonbehaviour of Void Sentry
    closes #12175
    
    (cherry picked from commit 02c145dc6ff4217b3b1a2bd86bc6378aef1d9f15)

--- src/server/scripts/Northrend/VioletHold/boss_zuramat.cpp
+++ src/server/scripts/Northrend/VioletHold/boss_zuramat.cpp
@@ -26,9 +26,6 @@ enum Spells
     SPELL_SUMMON_VOID_SENTRY                    = 54369,
     SPELL_VOID_SHIFT                            = 54361,
     H_SPELL_VOID_SHIFT                          = 59743,
-
-    SPELL_ZURAMAT_ADD_2                         = 54342,
-    H_SPELL_ZURAMAT_ADD_2                       = 59747
 };
 
 enum Creatures
@@ -190,14 +187,6 @@ public:
                 return;
 
             Talk(SAY_SLAY);
-        }
-
-        void JustSummoned(Creature* summon) override
-        {
-            summon->AI()->AttackStart(me->GetVictim());
-            summon->CastSpell((Unit*)NULL, SPELL_ZURAMAT_ADD_2);
-            summon->SetInPhase(169, true, true); // Normal phase
-            summon->SetInPhase(173, true, true); // Void phase
         }
     };
 
