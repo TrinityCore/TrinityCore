d136e806026fb4ec8a995dc08a5a31bb3379e59b
Aokromes <Aokromes@users.noreply.github.com>
2015-02-12 12:55:54 +0100

    DB/Quest: Tirisfal Questchains
    By Rushor, closes #13789

---
+++ sql/updates/world/2015_02_12_11_world.sql
@@ -0,0 +1,5 @@
+--
+UPDATE `quest_template` SET `PrevQuestId`=24971 WHERE  `Id`=24972;
+UPDATE `quest_template` SET `PrevQuestId`=25038 WHERE  `Id`=24990;
+UPDATE `quest_template` SET `PrevQuestId`=24996 WHERE  `Id`=24991;
+UPDATE `quest_template` SET `PrevQuestId`=24992 WHERE  `Id`=25039;
