84b4b2b4bab6c93623be9dc13c715d48c7cffcb5
Aokromes <Aokromes@users.noreply.github.com>
2015-02-14 13:27:19 +0100

    DB/Creature: Marie Allen, Gerard Walthorn
    By nkaren, closes #13345

---
+++ sql/updates/world/2015_02_14_01_world.sql
@@ -0,0 +1,4 @@
+-- Marie Allen (General Goods Vendor)
+UPDATE `gossip_menu_option` SET `option_id`=3,`npc_option_npcflag`=128 WHERE `menu_id`=10841;
+-- Gerard Walthorn
+UPDATE `gossip_menu_option` SET `option_id`=3,`npc_option_npcflag`=128 WHERE `menu_id`=10842;
