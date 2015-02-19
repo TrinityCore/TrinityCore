f8e60a6d9869fbfd0df5079eb04d342c397aa703
Duarte Duarte <dnpd.dd@gmail.com>
2015-02-19 00:09:19 +0000

    Contrib: Update merge_updates batch files with hotfixes db

--- contrib/merge_updates_unix.sh
+++ contrib/merge_updates_unix.sh
@@ -1,1 +1,2 @@
 cat ../sql/updates/world/*.sql > world_update.sql
+cat ../sql/updates/hotfixes/*.sql > hotfixes_update.sql
--- contrib/merge_updates_windows.bat
+++ contrib/merge_updates_windows.bat
@@ -1,1 +1,2 @@
 copy /a ..\sql\updates\world\*.sql /b world_updates.sql
+copy /a ..\sql\updates\hotfixes\*.sql /b hotfixes_updates.sql
