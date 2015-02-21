8aa7c1995b5af541260aed5fb48063d1bd0eaf4e
Aokromes <Aokromes@users.noreply.github.com>
2015-02-21 12:25:46 +0100

    Update 2015_02_12_07_world.sql

--- sql/updates/world/2015_02_12_07_world.sql
+++ sql/updates/world/2015_02_12_07_world.sql
@@ -10,6 +10,7 @@ UPDATE `gossip_menu_option` SET `OptionBroadcastTextID`=39130, `option_id`=1, `n
 UPDATE creature_template SET `AIName`='SmartAI' WHERE `entry`=37915;
 
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 37915 AND `source_type` = 0;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 37915*100 AND `source_type` = 9;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (37915, 0, 0, 0, 62, 0, 100, 0, 11152, 0, 0, 0, 80, 37915*100, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, '37915 - On Gossip Select - Actionlist'),
 (37915*100, 9, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 72, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, '37915 - Actionlist - Close gossip'),
