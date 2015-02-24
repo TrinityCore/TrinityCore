a7bd64a4a640d219afd205d841c7e93638cad240
Dr-J <daniel.jarrott0@gmail.com>
2015-02-24 00:29:19 +0000

    DB/Item: Bountiful Feast
    By @Killyana    Part solves #3991

---
+++ sql/updates/world/2015_02_23_09_world.sql
@@ -0,0 +1,5 @@
+DELETE FROM `spell_scripts` WHERE `id`= 66477;
+INSERT INTO `spell_scripts` (`id`, `effIndex`, `delay`, `command`, `datalong`, `datalong2`, `dataint`, `x`, `y`, `z`, `o`) VALUES 
+(66477, 0, 0, 15, 66622, 2, 1, 0, 0, 0, 0),
+(66477, 0, 0, 15, 66478, 2, 1, 0, 0, 0, 0),
+(66477, 0, 0, 15, 66041, 2, 1, 0, 0, 0, 0);
