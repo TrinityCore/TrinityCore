474e6951acdc5f81c571f476f3eed922410e1140
Vincent-Michael <Vincent_Michael@gmx.de>
2015-03-04 17:40:58 +0100

    Core/PacketIO: Sync with wpp and enable some opcodes

--- src/server/game/Server/Packets/QueryPackets.h
+++ src/server/game/Server/Packets/QueryPackets.h
@@ -87,7 +87,6 @@ namespace WorldPackets
             void Read() override;
 
             ObjectGuid Player;
-            PlayerGuidLookupHint Hint;
         };
 
         struct PlayerGuidLookupData
