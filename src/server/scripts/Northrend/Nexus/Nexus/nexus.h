5009d67f0c0dd90131ac6f81299e3719e2690ca1
MitchesD <majklprofik@seznam.cz>
2015-03-07 11:38:10 +0100

    Scripts/Nexus: commanders converted from SAI to C++

--- src/server/scripts/Northrend/Nexus/Nexus/nexus.h
+++ src/server/scripts/Northrend/Nexus/Nexus/nexus.h
@@ -21,32 +21,33 @@
 #define NexusScriptName "instance_nexus"
 #define DataHeader "NEX"
 
-uint32 const EncounterCount = 4; 
+uint32 const EncounterCount = 5;
 
 enum DataTypes
 {
-    DATA_MAGUS_TELESTRA               = 0,
-    DATA_ANOMALUS                     = 1,
-    DATA_ORMOROK                      = 2,
-    DATA_KERISTRASZA                  = 3,
+    DATA_COMMANDER                    = 0,
+    DATA_MAGUS_TELESTRA               = 1,
+    DATA_ANOMALUS                     = 2,
+    DATA_ORMOROK                      = 3,
+    DATA_KERISTRASZA                  = 4,
 
-    ANOMALUS_CONTAINMET_SPHERE        = 4,
-    ORMOROKS_CONTAINMET_SPHERE        = 5,
-    TELESTRAS_CONTAINMET_SPHERE       = 6
+    ANOMALUS_CONTAINMET_SPHERE        = 5,
+    ORMOROKS_CONTAINMET_SPHERE        = 6,
+    TELESTRAS_CONTAINMET_SPHERE       = 7
 };
 
 enum CreatureIds
 {
     NPC_ANOMALUS                      = 26763,
     NPC_KERISTRASZA                   = 26723,
-    
+
     // Alliance
     NPC_ALLIANCE_BERSERKER            = 26800,
     NPC_ALLIANCE_RANGER               = 26802,
     NPC_ALLIANCE_CLERIC               = 26805,
     NPC_ALLIANCE_COMMANDER            = 27949,
     NPC_COMMANDER_STOUTBEARD          = 26796,
-    
+
     // Horde
     NPC_HORDE_BERSERKER               = 26799,
     NPC_HORDE_RANGER                  = 26801,
@@ -59,7 +60,7 @@ enum GameObjectIds
 {
     GO_ANOMALUS_CONTAINMET_SPHERE     = 188527,
     GO_ORMOROKS_CONTAINMET_SPHERE     = 188528,
-    GO_TELESTRAS_CONTAINMET_SPHERE    = 188526 
+    GO_TELESTRAS_CONTAINMET_SPHERE    = 188526
 };
 
 #endif
