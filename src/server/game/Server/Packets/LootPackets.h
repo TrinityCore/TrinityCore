cdadcdc03c6db91058ce74d8c86d28c88eafa22f
Epicurus4 <maxmakki2@gmail.com>
2015-03-12 10:52:46 -0400

    Core/Formatting: White space clean-up.

--- src/server/game/Server/Packets/LootPackets.h
+++ src/server/game/Server/Packets/LootPackets.h
@@ -64,16 +64,16 @@ namespace WorldPackets
 
             ObjectGuid LootObj;
             ObjectGuid Owner;
-            uint8 Threshold      = 2; // Most common value
+            uint8 Threshold      = 2; // Most common value, 2 = Uncommon
             uint8 LootMethod     = 0;
             uint8 AcquireReason  = 0;
             uint8 FailureReason  = 17; // Most common value
-            uint32 Coins        = 0;
+            uint32 Coins         = 0;
             std::vector<LootItem> Items;
             std::vector<LootCurrency> Currencies;
             bool PersonalLooting = false;
             bool Acquired        = false;
-            bool AELooting      = false;
+            bool AELooting       = false;
         };
 
         struct LootRequest
