f6428df949225211958b9ad348b7119ba4253f12
MitchesD <majklprofik@seznam.cz>
2015-03-20 20:07:25 +0100

    Scripts/Misc: sync with 3cc305138c9b57da02d42b88cc0cae941016542a
    adfd8a6 - Scripts/Mulgore: The Plains Vision - Move Corescrript to SAI
    c21a600 - Delete zone_mulgore.cpp
    87f40a5 - Scripts/Kalimdor: Remove Mulgorescripts
    8904a94 - Scripts/The Barrens: Add missing Text for 'The Escape'
    cfc5fb2 - DB/Misc: Fix a few startup errors from recent Updates (Script part)

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -368,7 +368,6 @@ void AddSC_dustwallow_marsh();
 void AddSC_felwood();
 void AddSC_feralas();
 void AddSC_moonglade();
-void AddSC_mulgore();
 void AddSC_orgrimmar();
 void AddSC_silithus();
 void AddSC_stonetalon_mountains();
@@ -1090,7 +1089,6 @@ void AddKalimdorScripts()
     AddSC_felwood();
     AddSC_feralas();
     AddSC_moonglade();
-    AddSC_mulgore();
     AddSC_orgrimmar();
     AddSC_silithus();
     AddSC_stonetalon_mountains();
