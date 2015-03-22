bf480778c74e2144fb853b7eff04129a7c4eef0b
Shauren <shauren.trinity@gmail.com>
2015-03-22 13:13:18 +0100

    Core/PacketIO: Updated IsInstanceOnlyOpcode

--- src/server/game/Server/Protocol/Opcodes.h
+++ src/server/game/Server/Protocol/Opcodes.h
@@ -1075,12 +1075,12 @@ enum OpcodeServer : uint32
     SMSG_DISPLAY_TOAST                                = 0x17DA,
     SMSG_DONT_AUTO_PUSH_SPELLS_TO_ACTION_BAR          = 0xBADD,
     SMSG_DROP_NEW_CONNECTION                          = 0xBADD,
-    SMSG_DUEL_COMPLETE                                = 0xBADD,
+    SMSG_DUEL_COMPLETE                                = 0x1531,
     SMSG_DUEL_COUNTDOWN                               = 0x1B1A,
-    SMSG_DUEL_IN_BOUNDS                               = 0xBADD,
-    SMSG_DUEL_OUT_OF_BOUNDS                           = 0xBADD,
+    SMSG_DUEL_IN_BOUNDS                               = 0x13A3,
+    SMSG_DUEL_OUT_OF_BOUNDS                           = 0x15F4,
     SMSG_DUEL_REQUESTED                               = 0x0983,
-    SMSG_DUEL_WINNER                                  = 0xBADD,
+    SMSG_DUEL_WINNER                                  = 0x1519,
     SMSG_DUMP_RIDE_TICKETS_RESPONSE                   = 0xBADD,
     SMSG_DURABILITY_DAMAGE_DEATH                      = 0x1BCA,
     SMSG_DYNAMIC_DROP_ROLL_RESULT                     = 0xBADD,
@@ -1372,7 +1372,7 @@ enum OpcodeServer : uint32
     SMSG_MODIFY_COOLDOWN                              = 0x15EA,
     SMSG_MONEY_NOTIFY                                 = 0xBADD,
     SMSG_MOTD                                         = 0x12FB,
-    SMSG_MOUNT_RESULT                                 = 0xBADD,
+    SMSG_MOUNT_RESULT                                 = 0x1BAC,
     SMSG_MOVE_APPLY_MOVEMENT_FORCE                    = 0xBADD,
     SMSG_MOVE_CHARACTER_CHEAT_FAILURE                 = 0xBADD,
     SMSG_MOVE_CHARACTER_CHEAT_SUCCESS                 = 0xBADD,
@@ -1839,173 +1839,19 @@ enum OpcodeServer : uint32
 
 inline bool IsInstanceOnlyOpcode(uint32 opcode)
 {
-    // TODO: Use names when known
     switch (opcode)
     {
-        /*case SMSG_MOUNT_RESULT: // Client
-        case SMSG_DUEL_OUT_OF_BOUNDS: // Client
-        case SMSG_DUEL_IN_BOUNDS: // Client
-        case 0x0549: // Client
-        case 0x055A: // Client
-        case 0x055C: // Client
-        case 0x057A: // Client
-        case 0x057C: // Client
-        case 0x05C9: // Client
-        case 0x05CA: // Client
-        case 0x05D9: // Client
-        case 0x05DA: // Client
-        case 0x05DC: // Client
-        case 0x05E9: // Client
-        case 0x05F9: // Client
-        case 0x05FC: // Client
-        case 0x074B: // Client
-        case 0x074C: // Client
-        case 0x075B: // Client
-        case 0x075C: // Client
-        case 0x076C: // Client
-        case 0x07CB: // Client
-        case 0x07CC: // Client
-        case 0x07DB: // Client
-        case 0x07DC: // Client
-        case 0x07EC: // Client
-        case SMSG_DUEL_REQUESTED: // Client
-        case SMSG_DUEL_WINNER: // Client
-        case 0x0F0F: // ClientSpell
-        case 0x0F10: // ClientSpell
-        case 0x0F30: // ClientSpell
-        case 0x0F3B: // ClientSpell
-        case 0x0F8B: // ClientSpell
-        case 0x0F8C: // ClientSpell
-        case 0x0F90: // ClientSpell
-        case 0x0F9F: // ClientSpell
-        case 0x0FA0: // ClientSpell
-        case SMSG_ATTACKSTOP: // Client
-        case SMSG_DUEL_COMPLETE: // Client
-        case 0x154B: // Client
-        case 0x154C: // Client
-        case 0x155A: // Client
-        case 0x155B: // Client
-        case 0x155C: // Client
         case SMSG_QUESTGIVER_STATUS: // ClientQuest
-        case 0x156B: // Client
-        case 0x1579: // Client
-        case 0x157B: // Client
-        case 0x15CB: // Client
-        case 0x15CC: // Client
-        case 0x15DB: // Client
-        case 0x15DC: // Client
-        case 0x15EB: // Client
-        case 0x15FB: // Client
-        case 0x1710: // ClientSpell
-        case 0x1720: // ClientSpell
-        case 0x172B: // ClientSpell
-        case 0x172C: // ClientSpell
-        case 0x172F: // ClientSpell
-        case 0x1730: // ClientSpell
-        case 0x173C: // ClientSpell
-        case 0x178B: // ClientSpell
-        case 0x178C: // ClientSpell
-        case 0x178F: // ClientSpell
-        case 0x1790: // ClientSpell
-        case 0x179B: // ClientSpell
-        case 0x179F: // ClientSpell
-        case SMSG_ATTACKSTART: // Client
-        case 0x1D82: // ClientQuest
-        case 0x1D83: // ClientQuest
-        case 0x1D84: // ClientQuest
-        case 0x1D86: // ClientQuest
-        case 0x1D91: // ClientQuest
-        case 0x1D92: // ClientQuest
-        case 0x1D94: // ClientQuest
-        case 0x1D96: // ClientQuest
-        case 0x1D97: // ClientQuest
-        case 0x1DA1: // ClientQuest
-        case 0x1DA2: // ClientQuest
-        case 0x1DA3: // ClientQuest
-        case 0x1DA4: // ClientQuest
-        case 0x1DA5: // ClientQuest
-        case 0x1DA7: // ClientQuest
+        case SMSG_DUEL_REQUESTED: // Client
+        case SMSG_DUEL_IN_BOUNDS: // Client
         case SMSG_QUERY_TIME_RESPONSE: // Client
-        case 0x1DC4: // ClientQuest
-        case 0x1DC8: // ClientQuest
-        case 0x1DD1: // ClientQuest
-        case 0x1DD2: // ClientQuest
-        case 0x1DD3: // ClientQuest
-        case 0x1DD4: // ClientQuest
-        case 0x1DD6: // ClientQuest
-        case 0x1DE1: // ClientQuest
-        case 0x1DE2: // ClientQuest
-        case 0x1DE3: // ClientQuest
-        case 0x1DE4: // ClientQuest
-        case 0x1DE5: // ClientQuest
-        case 0x1DE7: // ClientQuest
-        case 0x1F04: // ClientQuest
-        case 0x1F08: // ClientQuest
-        case 0x1F10: // ClientSpell
-        case 0x1F11: // ClientQuest
-        case 0x1F12: // ClientQuest
-        case 0x1F13: // ClientQuest
-        case 0x1F14: // ClientQuest
-        case 0x1F16: // ClientQuest
-        case 0x1F20: // ClientSpell
-        case 0x1F21: // ClientQuest
-        case 0x1F22: // ClientQuest
-        case 0x1F23: // ClientQuest
-        case 0x1F24: // ClientQuest
-        case 0x1F25: // ClientQuest
-        case 0x1F27: // ClientQuest
-        case 0x1F2B: // ClientSpell
-        case 0x1F2C: // ClientSpell
-        case 0x1F2F: // ClientSpell
-        case 0x1F30: // ClientSpell
-        case 0x1F3C: // ClientSpell
-        case 0x1F41: // ClientQuest
-        case 0x1F42: // ClientQuest
-        case 0x1F44: // ClientQuest
-        case 0x1F46: // ClientQuest
-        case 0x1F47: // ClientQuest
-        case 0x1F48: // ClientQuest
-        case 0x1F52: // ClientQuest
-        case 0x1F53: // ClientQuest
-        case 0x1F55: // ClientQuest
-        case 0x1F57: // ClientQuest
-        case 0x1F61: // ClientQuest
-        case 0x1F63: // ClientQuest
-        case 0x1F64: // ClientQuest
-        case 0x1F65: // ClientQuest
-        case 0x1F67: // ClientQuest
-        case 0x1F84: // ClientQuest
-        case 0x1F87: // ClientQuest
-        case 0x1F8B: // ClientSpell
-        case 0x1F8C: // ClientSpell
-        case 0x1F8F: // ClientSpell
-        case 0x1F90: // ClientSpell
-        case 0x1F91: // ClientQuest
-        case 0x1F95: // ClientQuest
-        case 0x1F96: // ClientQuest
-        case 0x1F97: // ClientQuest
-        case 0x1F9B: // ClientSpell
-        case 0x1F9F: // ClientSpell
-        case 0x1FA1: // ClientQuest
-        case 0x1FA2: // ClientQuest
-        case 0x1FA3: // ClientQuest
-        case 0x1FA4: // ClientQuest
-        case 0x1FA5: // ClientQuest
-        case 0x1FA7: // ClientQuest
-        case 0x1FC4: // ClientQuest
-        case 0x1FC8: // ClientQuest
-        case 0x1FD1: // ClientQuest
-        case 0x1FD2: // ClientQuest
-        case 0x1FD3: // ClientQuest
-        case 0x1FD4: // ClientQuest
-        case 0x1FD6: // ClientQuest
-        case 0x1FE1: // ClientQuest
-        case 0x1FE2: // ClientQuest
-        case 0x1FE3: // ClientQuest
-        case 0x1FE4: // ClientQuest
-        case 0x1FE5: // ClientQuest
-        case 0x1FE7: // ClientQuest
-            return true;*/
+        case SMSG_DUEL_WINNER: // Client
+        case SMSG_DUEL_COMPLETE: // Client
+        case SMSG_DUEL_OUT_OF_BOUNDS: // Client
+        case SMSG_ATTACKSTOP: // Client
+        case SMSG_ATTACKSTART: // Client
+        case SMSG_MOUNT_RESULT: // Client
+            return true;
         default:
             return false;
     }
