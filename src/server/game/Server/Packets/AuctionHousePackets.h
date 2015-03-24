780898baa1b31466a69113e99efe8360e63dd2f6
Golrag <golrag.jeremy@gmail.com>
2015-03-24 15:17:06 +0100

    Core/Opcodes: CMSG_AUCTION_SELL_ITEM CMSG_AUCTION_PLACE_BID CMSG_AUCTION_LIST_BIDDER_ITEMS CMSG_AUCTION_REMOVE_ITEM CMSG_AUCTION_REPLICATE_ITEMS CMSG_AUCTION_LIST_PENDING_SALES SMSG_AUCTION_LIST_ITEMS_RESULT SMSG_AUCTION_LIST_OWNER_ITEMS_RESULT SMSG_AUCTION_LIST_BIDDER_ITEMS_RESULT CMSG_AUCTION_LIST_OWNER_ITEMS CMSG_AUCTION_LIST_ITEMS SMSG_AUCTION_LIST_PENDING_SALES_RESULT SMSG_AUCTION_CLOSED_NOTIFICATION SMSG_AUCTION_OWNER_BID_NOTIFICATION SMSG_AUCTION_WON_NOTIFICATION SMSG_AUCTION_OUTBID_NOTIFICATION SMSG_AUCTION_REPLICATE_RESPONSE SMSG_AUCTION_COMMAND_RESULT

--- src/server/game/Server/Packets/AuctionHousePackets.h
+++ src/server/game/Server/Packets/AuctionHousePackets.h
@@ -16,16 +16,67 @@
  */
 
 #ifndef AuctionHousePackets_h__
+#define AuctionHousePackets_h__
 
 #include "Packet.h"
 #include "ObjectGuid.h"
-
-struct AuctionEntry;
+#include "ItemPackets.h"
+#include "MailPackets.h"
 
 namespace WorldPackets
 {
     namespace AuctionHouse
     {
+        struct AuctionItem
+        {
+            struct AuctionItemEnchant
+            {
+                AuctionItemEnchant(int32 id, uint32 expiration, int32 charges, uint8 slot) : ID(id), Expiration(expiration), Charges(charges), Slot(slot) { }
+                int32 ID = 0;
+                uint32 Expiration = 0;
+                int32 Charges = 0;
+                uint8 Slot = 0;
+            };
+
+            Item::ItemInstance Item;
+            int32 Count = 0;
+            int32 Charges = 0;
+            std::vector<AuctionItemEnchant> Enchantments;
+            int32 Flags = 0;
+            int32 AuctionItemID = 0;
+            ObjectGuid Owner;
+            uint64 MinBid = 0;
+            uint64 MinIncrement = 0;
+            uint64 BuyoutPrice = 0;
+            int32 DurationLeft = 0;
+            uint8 DeleteReason = 0;
+            bool CensorServerSideInfo = false;
+            bool CensorBidInfo = false;
+            ObjectGuid ItemGuid;
+            ObjectGuid OwnerAccountID;
+            uint32 EndTime = 0;
+            ObjectGuid Bidder;
+            uint64 BidAmount = 0;
+        };
+
+        struct AuctionOwnerNotification
+        {
+            void Initialize(::AuctionEntry const* auction, ::Item const* item);
+
+            int32 AuctionItemID = 0;
+            uint64 BidAmount = 0;
+            Item::ItemInstance Item;
+        };
+
+        struct AuctionBidderNotification
+        {
+            void Initialize(::AuctionEntry const* auction, ::Item const* item);
+
+            int32 AuctionItemID = 0;
+            ObjectGuid Bidder;
+            Item::ItemInstance Item;
+        };
+
         class AuctionHelloRequest final : public ClientPacket
         {
         public:
@@ -58,17 +109,242 @@ namespace WorldPackets
                  *
                  * @param   auction         The relevant auction object
                  */
-                void InitializeAuction(AuctionEntry* auction);
+                void InitializeAuction(::AuctionEntry* auction);
 
                 WorldPacket const* Write() override;
 
-                uint32 AuctionId     = 0; ///< the id of the auction that triggered this notification
-                uint32 Action        = 0; ///< the type of action that triggered this notification. Possible values are @ref AuctionAction
-                uint32 ErrorCode     = 0; ///< the error code that was generated when trying to perform the action. Possible values are @ref AuctionError
-                uint64 Bid           = 0; ///< the amount of money that the player bid in copper
-                uint32 BidError      = 0; ///< the bid error. Possible values are @ref AuctionError
-                ObjectGuid Bidder;        ///< the GUID of the bidder for this auction.
-                uint64 AuctionOutBid = 0; ///< the sum of outbid is (1% of current bid) * 5, if the bid is too small, then this value is 1 copper.
+                uint32 AuctionItemID = 0; ///< the id of the auction that triggered this notification
+                uint32 Command       = 0; ///< the type of action that triggered this notification. Possible values are @ref AuctionAction
+                int32 ErrorCode      = 0; ///< the error code that was generated when trying to perform the action. Possible values are @ref AuctionError
+                uint64 Money         = 0; ///< the amount of money that the player bid in copper
+                int32 BagResult      = 0; ///< the bid error. Possible values are @ref AuctionError
+                ObjectGuid Guid;          ///< the GUID of the bidder for this auction.
+                uint64 MinIncrement  = 0; ///< the sum of outbid is (1% of current bid) * 5, if the bid is too small, then this value is 1 copper.
+        };
+
+        class AuctionSellItem final : public ClientPacket
+        {
+        public:
+            struct AuctionItemForSale
+            {
+                ObjectGuid Guid;
+                uint32 UseCount = 0;
+            };
+
+            AuctionSellItem(WorldPacket&& packet) : ClientPacket(CMSG_AUCTION_SELL_ITEM, std::move(packet)) { }
+
+            void Read() override;
+
+            uint64 BuyoutPrice = 0;
+            ObjectGuid Auctioneer;
+            uint64 MinBid = 0;
+            uint32 RunTime = 0;
+            std::vector<AuctionItemForSale> Items;
+        };
+
+        class AuctionPlaceBid final : public ClientPacket
+        {
+        public:
+            AuctionPlaceBid(WorldPacket&& packet) : ClientPacket(CMSG_AUCTION_PLACE_BID, std::move(packet)) { }
+
+            void Read() override;
+
+            ObjectGuid Auctioneer;
+            uint64 BidAmount = 0;
+            int32 AuctionItemID = 0;
+        };
+
+        class AuctionListBidderItems final : public ClientPacket
+        {
+        public:
+            AuctionListBidderItems(WorldPacket&& packet) : ClientPacket(CMSG_AUCTION_LIST_BIDDER_ITEMS, std::move(packet)) { }
+
+            void Read() override;
+
+            uint32 Offset = 0;
+            std::vector<uint32> AuctionItemIDs;
+            ObjectGuid Auctioneer;
+        };
+
+        class AuctionRemoveItem final : public ClientPacket
+        {
+        public:
+            AuctionRemoveItem(WorldPacket&& packet) : ClientPacket(CMSG_AUCTION_REMOVE_ITEM, std::move(packet)) { }
+
+            void Read() override;
+
+            ObjectGuid Auctioneer;
+            int32 AuctionItemID = 0;
+        };
+
+        class AuctionReplicateItems final : public ClientPacket
+        {
+        public:
+            AuctionReplicateItems(WorldPacket&& packet) : ClientPacket(CMSG_AUCTION_REPLICATE_ITEMS, std::move(packet)) { }
+
+            void Read() override;
+
+            ObjectGuid Auctioneer;
+            int32 Count = 0;
+            int32 ChangeNumberGlobal = 0;
+            int32 ChangeNumberCursor = 0;
+            int32 ChangeNumberTombstone = 0;
+        };
+
+        class AuctionListPendingSales final : public ClientPacket
+        {
+        public:
+            AuctionListPendingSales(WorldPacket&& packet) : ClientPacket(CMSG_AUCTION_LIST_PENDING_SALES, std::move(packet)) { }
+
+            void Read() override { }
+        };
+
+        class AuctionListItemsResult final : public ServerPacket
+        {
+        public:
+            AuctionListItemsResult() : ServerPacket(SMSG_AUCTION_LIST_ITEMS_RESULT, 150) { }
+
+            WorldPacket const* Write() override;
+
+            uint32 DesiredDelay = 0;
+            std::vector<AuctionItem> Items;
+            bool OnlyUsable = true;
+            uint32 TotalCount = 0;
+        };
+
+        class AuctionListOwnerItemsResult final : public ServerPacket
+        {
+        public:
+            AuctionListOwnerItemsResult() : ServerPacket(SMSG_AUCTION_LIST_OWNER_ITEMS_RESULT, 149) { }
+
+            WorldPacket const* Write() override;
+
+            uint32 DesiredDelay = 0;
+            uint32 TotalCount = 0;
+            std::vector<AuctionItem> Items;
+        };
+
+        class AuctionListBidderItemsResult final : public ServerPacket
+        {
+        public:
+            AuctionListBidderItemsResult() : ServerPacket(SMSG_AUCTION_LIST_BIDDER_ITEMS_RESULT, 149) { }
+
+            WorldPacket const* Write() override;
+
+            uint32 DesiredDelay = 0;
+            uint32 TotalCount = 0;
+            std::vector<AuctionItem> Items;
+        };
+
+        class AuctionListOwnerItems final : public ClientPacket
+        {
+        public:
+            AuctionListOwnerItems(WorldPacket&& packet) : ClientPacket(CMSG_AUCTION_LIST_OWNER_ITEMS, std::move(packet)) { }
+
+            void Read() override;
+
+            ObjectGuid Auctioneer;
+            uint32 Offset = 0;
+        };
+
+        class AuctionListItems final : public ClientPacket
+        {
+        public:
+            struct Sort
+            {
+                uint8 UnkByte1 = 0;
+                uint8 UnkByte2 = 0;
+            };
+
+            AuctionListItems(WorldPacket&& packet) : ClientPacket(CMSG_AUCTION_LIST_ITEMS, std::move(packet)) { }
+
+            void Read() override;
+
+            ObjectGuid Auctioneer;
+            uint8 SortCount = 0;
+            uint8 MaxLevel = 100;
+            uint32 Offset = 0;
+            int32 ItemClass = 0;
+            uint8 MinLevel = 1;
+            int32 InvType = 0;
+            int32 Quality = 0;
+            int32 ItemSubclass = 0;
+            bool ExactMatch = true;
+            std::string Name;
+            bool OnlyUsable = false;
+            std::vector<Sort> DataSort;
+        };
+
+        class AuctionListPendingSalesResult final : public ServerPacket
+        {
+        public:
+            AuctionListPendingSalesResult() : ServerPacket(SMSG_AUCTION_LIST_PENDING_SALES_RESULT, 140) { }
+
+            WorldPacket const* Write() override;
+
+            std::vector<Mail::MailListEntry> Mails;
+            int32 TotalNumRecords = 0;
+        };
+
+        class AuctionClosedNotification final : public ServerPacket
+        {
+        public:
+            AuctionClosedNotification() : ServerPacket(SMSG_AUCTION_CLOSED_NOTIFICATION, 45) { }
+
+            WorldPacket const* Write() override;
+
+            AuctionOwnerNotification Info;
+            float ProceedsMailDelay = 0.0f;
+            bool Sold = true;
+        };
+
+        class AuctionOwnerBidNotification final : public ServerPacket
+        {
+        public:
+            AuctionOwnerBidNotification() : ServerPacket(SMSG_AUCTION_OWNER_BID_NOTIFICATION, 62) { }
+
+            WorldPacket const* Write() override;
+
+            AuctionOwnerNotification Info;
+            ObjectGuid Bidder;
+            uint64 MinIncrement = 0;
+        };
+
+        class AuctionWonNotification final : public ServerPacket
+        {
+        public:
+            AuctionWonNotification() : ServerPacket(SMSG_AUCTION_WON_NOTIFICATION, 46) { }
+
+            WorldPacket const* Write() override;
+
+            AuctionBidderNotification Info;
+        };
+
+        class AuctionOutBidNotification final : public ServerPacket
+        {
+        public:
+            AuctionOutBidNotification() : ServerPacket(SMSG_AUCTION_OUTBID_NOTIFICATION, 62) { }
+
+            WorldPacket const* Write() override;
+
+            AuctionBidderNotification Info;
+            uint64 BidAmount = 0;
+            uint64 MinIncrement = 0;
+        };
+
+        class AuctionReplicateResponse final : public ServerPacket
+        {
+        public:
+            AuctionReplicateResponse() : ServerPacket(SMSG_AUCTION_REPLICATE_RESPONSE, 165) { }
+
+            WorldPacket const* Write() override;
+
+            uint32 ChangeNumberCursor = 0;
+            uint32 ChangeNumberGlobal = 0;
+            uint32 DesiredDelay = 0;
+            uint32 ChangeNumberTombstone = 0;
+            uint32 Result = 0;
+            std::vector<AuctionItem> Items;
         };
     }
 }
