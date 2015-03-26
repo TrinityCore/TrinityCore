7f08c0d0a04e6819276d18c3aaaec5bf900c023c
Dr-J <daniel.jarrott0@gmail.com>
2015-03-26 21:58:21 +0000

    DB/Misc: Slight update to commit
    This does not make any difference but fixes a error with a bad delete statement when trying to re-apply.

--- sql/updates/world/2015_03_23_08_world.sql
+++ sql/updates/world/2015_03_23_08_world.sql
@@ -8,7 +8,7 @@ INSERT INTO `creature_text` (`entry`, `groupid`, `id`, `text`, `type`, `language
 UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry` =21411;
 
 DELETE FROM `smart_scripts` WHERE `entryorguid` =21411 AND `source_type`=0;
-DELETE FROM `smart_scripts` WHERE `entryorguid` =2141100 AND `source_type`=0;
+DELETE FROM `smart_scripts` WHERE `entryorguid` =2141100 AND `source_type`=9;
 
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (21411, 0, 0, 1, 19, 0, 100, 0, 10550, 0, 0, 0, 64, 1, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Tobias the Filth Gorger - On Quest 10550 taken - Store Targetlist'),
