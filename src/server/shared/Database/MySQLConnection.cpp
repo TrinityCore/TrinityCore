990083ca5e117e706f30671cb1c10c9ca6f4f5d6
Naios <naios-dev@live.de>
2015-03-27 15:34:13 +0100

    Core/Database: Prepare statements at reconnect. * Closes #14430
    (cherry picked from commit 39bdd06446dc8ae6f19e017e4390bd2aa7834d03)

--- src/server/shared/Database/MySQLConnection.cpp
+++ src/server/shared/Database/MySQLConnection.cpp
@@ -137,7 +137,9 @@ uint32 MySQLConnection::Open()
         // set connection properties to UTF8 to properly handle locales for different
         // server configs - core sends data in UTF8, so MySQL must expect UTF8 too
         mysql_set_character_set(m_Mysql, "utf8");
-        return 0;
+
+        // Prepare statements only at reconnect
+        return m_reconnecting ? static_cast<uint32>(PrepareStatements()) : 0;
     }
     else
     {
