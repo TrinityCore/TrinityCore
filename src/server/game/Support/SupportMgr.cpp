63ba2ab6363b885a1a40b41a138923bed538249d
Carbenium <keresztesschmidt@gmail.com>
2015-03-28 11:54:03 +0100

    Core/Support: Added missing sql execute
    Core/PacketIO: Enabled support system related opcodes

--- src/server/game/Support/SupportMgr.cpp
+++ src/server/game/Support/SupportMgr.cpp
@@ -406,6 +406,7 @@ void ComplaintTicket::DeleteFromDB()
 
     stmt = CharacterDatabase.GetPreparedStatement(CHAR_DEL_GM_COMPLAINT_CHATLOG);
     stmt->setUInt32(0, _id);
+    CharacterDatabase.Execute(stmt);
 }
 
 std::string ComplaintTicket::FormatViewMessageString(ChatHandler& handler, bool detailed) const
