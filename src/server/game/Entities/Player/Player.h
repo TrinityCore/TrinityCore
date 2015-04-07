6a4159b5c917c288fb09074c06e40132822b9f0f
click <click@gonnamakeyou.com>
2015-04-07 23:51:19 +0200

    Core/Spells: Reinstate helperfunction removed in commit 0e3607df (with a new functionname to make more sense out of it), and adjust the remaining code slightly to accomodate that.

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -2136,6 +2136,8 @@ class Player : public Unit, public GridObject<Player>
         void SetFallInformation(uint32 time, float z);
         void HandleFall(MovementInfo const& movementInfo);
 
+        bool canFlyInZone(uint32 mapid, uint32 zone) const;
+
         void SetClientControl(Unit* target, bool allowMove);
 
         void SetMover(Unit* target);
