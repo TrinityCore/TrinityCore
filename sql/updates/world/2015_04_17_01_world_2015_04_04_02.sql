d16e8357354862774a16f0ca3b077480f2978716
DDuarte <dnpd.dd@gmail.com>
2015-04-18 00:36:05 +0100

    SQLs: Fix a typo in 2015_04_17_01_world_2015_04_04_02.sql

--- sql/updates/world/2015_04_17_01_world_2015_04_04_02.sql
+++ sql/updates/world/2015_04_17_01_world_2015_04_04_02.sql
@@ -1,5 +1,5 @@
 --
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=16 AND `SourceEntry`=4163;
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=16 AND `SourceEntry`=26813;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceGroup`,`SourceEntry`,`SourceId`,`ElseGroup`,`ConditionTypeOrReference`,`ConditionTarget`,`ConditionValue1`,`ConditionValue2`,`ConditionValue3`,`NegativeCondition`,`ErrorTextId`,`ScriptName`,`Comment`) VALUES
 (16,0,26813,0,0,23,0,4163,0,0,0,0,'','Dismount player from Kor''kron War Rider when not in intended zone(Icemist Village)');
 
