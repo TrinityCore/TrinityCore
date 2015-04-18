8aff0f4992468a93196f39e8323219d5e8a9054c
Kittnz <frederik156@hotmail.com>
2015-04-18 17:11:03 +0200

    Core/Condition: Add In Water Condition
    Updates #7019

--- src/server/game/Conditions/ConditionMgr.h
+++ src/server/game/Conditions/ConditionMgr.h
@@ -73,7 +73,8 @@ enum ConditionTypes
     CONDITION_HP_VAL                = 37,                   // hpVal            ComparisonType 0                  true if unit's hp matches given value
     CONDITION_HP_PCT                = 38,                   // hpPct            ComparisonType 0                  true if unit's hp matches given pct
     CONDITION_REALM_ACHIEVEMENT     = 39,                   // achievement_id   0              0                  true if realm achievement is complete
-    CONDITION_MAX                   = 40                    // MAX
+    CONDITION_IN_WATER              = 40,                   // 0                0              0                  true if unit in water
+    CONDITION_MAX                   = 41                    // MAX
 };
 
 /*! Documentation on implementing a new ConditionSourceType:
