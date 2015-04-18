d1f2e1f4b696922d4f10806e2f99bfa751ab64e2
Dr-J <daniel.jarrott0@gmail.com>
2015-04-18 20:16:39 +0100

    DB/Misc: Change guids from previous commit

--- sql/updates/world/2015_04_18_08_world.sql
+++ sql/updates/world/2015_04_18_08_world.sql
@@ -1,11 +1,13 @@
-    SET @GUID       := 143596; 
+	SET @GUID       := 143894; 
 
 	DELETE FROM `disables` WHERE `entry` = 7180 AND `sourceType` = 4;
 
 	UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry` IN(27737);
 
 	DELETE FROM `smart_scripts` WHERE `entryorguid` IN(27737) AND `source_type`=0;
-    DELETE FROM `smart_scripts` WHERE `entryorguid` BETWEEN -@GUID-89 AND -@GUID-0 AND `source_type`=0;
+	
+	DELETE FROM `smart_scripts` WHERE `entryorguid` BETWEEN -@GUID-89 AND -@GUID-0 AND `source_type`=0;
+	
 	INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 	(-@GUID-0, 0, 0, 1, 1, 0, 100, 1, 0, 0, 0, 0, 19, 768, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Risen Zombie - OOC - Remove Unit Flags (No repeat)'),
 	(-@GUID-0, 0, 1, 0, 61, 0, 100, 1, 0, 0, 0, 0, 47, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Risen Zombie - OOC - Set Visible (No repeat)'),
