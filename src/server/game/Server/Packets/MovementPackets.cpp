a1721211ca5711ac070fe8af2c2ca03d2a2221dd
Shauren <shauren.trinity@gmail.com>
2015-04-18 12:26:40 +0200

    Core/PacketIO: Fixed MoveUpdateCollisionHeight structure
    Closes #14546

--- src/server/game/Server/Packets/MovementPackets.cpp
+++ src/server/game/Server/Packets/MovementPackets.cpp
@@ -654,8 +654,8 @@ WorldPacket const* WorldPackets::Movement::MoveSetCollisionHeight::Write()
 WorldPacket const* WorldPackets::Movement::MoveUpdateCollisionHeight::Write()
 {
     _worldPacket << *movementInfo;
-    _worldPacket << float(Scale);
     _worldPacket << float(Height);
+    _worldPacket << float(Scale);
 
     return &_worldPacket;
 }
