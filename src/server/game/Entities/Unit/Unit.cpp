b04537df95cd6bafbf698ea4073af501c957412d
Vincent-Michael <Vincent_Michael@gmx.de>
2015-04-19 01:41:41 +0200

    Core Fix build

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -8112,7 +8112,7 @@ void Unit::SendHealSpellLog(Unit* victim, uint32 spellID, uint32 health, uint32 
 {
     WorldPackets::CombatLog::SpellHealLog spellHealLog;
 
-    TC_LOG_DEBUG("spells", "HealSpellLog -- SpellId: %u Caster: %s -- Target: %s Health: %u OverHeal: %u Absorbed: %u Crit: %u", spellID, GetGUID().ToString(), victim->GetGUID().ToString(),
+    TC_LOG_DEBUG("spells", "HealSpellLog -- SpellId: %u Caster: %s Target: %s (Health: %u OverHeal: %u Absorbed: %u Crit: %d)", spellID, GetGUID().ToString(), victim->GetGUID().ToString(),
         health, overHeal, absorbed, crit);
 
     spellHealLog.TargetGUID = victim->GetGUID();
