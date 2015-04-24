986665e2a723affd9a61cdb65ea2308c4c9a55f5
MitchesD <majklprofik@seznam.cz>
2015-04-24 22:26:18 +0200

    Scripts: finally (I hope) fixed nonpch build, damn you @ariel-

--- src/server/scripts/Outland/HellfireCitadel/ShatteredHalls/instance_shattered_halls.cpp
+++ src/server/scripts/Outland/HellfireCitadel/ShatteredHalls/instance_shattered_halls.cpp
@@ -24,6 +24,9 @@ SDCategory: Hellfire Citadel, Shattered Halls
 EndScriptData */
 
 #include "ScriptMgr.h"
+#include "ScriptedCreature.h"
+#include "SpellScript.h"
+#include "SpellAuraEffects.h"
 #include "InstanceScript.h"
 #include "Player.h"
 #include "SpellAuras.h"
