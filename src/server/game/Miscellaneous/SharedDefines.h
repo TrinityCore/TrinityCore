5772453762b128e43c644604a13920402f0a7f80
Shauren <shauren.trinity@gmail.com>
2015-05-20 00:04:48 +0200

    Core/Spells: Sync effect names with spellwork

--- src/server/game/Miscellaneous/SharedDefines.h
+++ src/server/game/Miscellaneous/SharedDefines.h
@@ -1174,7 +1174,7 @@ enum SpellEffectName
     SPELL_EFFECT_REMOVE_AURA                        = 164,
     SPELL_EFFECT_DAMAGE_FROM_MAX_HEALTH_PCT         = 165,
     SPELL_EFFECT_GIVE_CURRENCY                      = 166,
-    SPELL_EFFECT_167                                = 167,
+    SPELL_EFFECT_UPDATE_PLAYER_PHASE                = 167,
     SPELL_EFFECT_ALLOW_CONTROL_PET                  = 168, // NYI
     SPELL_EFFECT_DESTROY_ITEM                       = 169,
     SPELL_EFFECT_UPDATE_ZONE_AURAS_AND_PHASES       = 170, // NYI
