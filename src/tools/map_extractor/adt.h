0e99bb821dc55e892c7a7688c31ca7be8e364410
jackpoz <giacomopoz@gmail.com>
2015-06-28 22:01:47 +0200

    Tools/Misc: Fix build warnings

--- src/tools/map_extractor/adt.h
+++ src/tools/map_extractor/adt.h
@@ -67,8 +67,8 @@ class adt_MCLQ
         uint32 fcc;
         char   fcc_txt[4];
     };
-    uint32 size;
 public:
+    uint32 size;
     float height1;
     float height2;
     struct liquid_data{
@@ -96,8 +96,8 @@ class adt_MCNK
         uint32 fcc;
         char   fcc_txt[4];
     };
-    uint32 size;
 public:
+    uint32 size;
     uint32 flags;
     uint32 ix;
     uint32 iy;
@@ -155,8 +155,8 @@ class adt_MCIN
         uint32 fcc;
         char   fcc_txt[4];
     };
-    uint32 size;
 public:
+    uint32 size;
     struct adt_CELLS{
         uint32 offsMCNK;
         uint32 size;
@@ -271,6 +271,7 @@ class adt_MHDR
         uint32 fcc;
         char   fcc_txt[4];
     };
+public:
     uint32 size;
 
     uint32 pad;
@@ -289,7 +290,6 @@ class adt_MHDR
     uint32 data3;
     uint32 data4;
     uint32 data5;
-public:
     bool prepareLoadedData();
     adt_MCIN *getMCIN(){ return (adt_MCIN *)((uint8 *)&pad+offsMCIN);}
     adt_MH2O *getMH2O(){ return offsMH2O ? (adt_MH2O *)((uint8 *)&pad+offsMH2O) : 0;}
