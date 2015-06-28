a48f8f4de9c0fc8251fdbb90a09be9e64fc6a19e
jackpoz <giacomopoz@gmail.com>
2015-06-28 19:51:59 +0200

    Core/Dependencies: Update recast
    Update recast to https://github.com/memononen/recastnavigation/commit/1dd5cf1883d61e723fef3d4957cf758c50e7a52b
    Fix 2 warnings.
    
    These changes have no effect on MMAPs and it's not needed to re-extact them.
    
    (cherry picked from commit dfa556aaa2fd60f881845d381f71294b945396f6)
    
    Conflicts:
    	dep/PackageList.txt

--- src/server/game/Movement/PathGenerator.cpp
+++ src/server/game/Movement/PathGenerator.cpp
@@ -117,7 +117,7 @@ dtPolyRef PathGenerator::GetPathPolyByPosition(dtPolyRef const* polyPath, uint32
     }
 
     if (distance)
-        *distance = dtSqrt(minDist3d);
+        *distance = dtMathSqrtf(minDist3d);
 
     return (minDist2d < 3.0f) ? nearestPoly : INVALID_POLYREF;
 }
@@ -800,7 +800,7 @@ dtStatus PathGenerator::FindSmoothPath(float const* startPos, float const* endPo
         // Find movement delta.
         float delta[VERTEX_SIZE];
         dtVsub(delta, steerPos, iterPos);
-        float len = dtSqrt(dtVdot(delta, delta));
+        float len = dtMathSqrtf(dtVdot(delta, delta));
         // If the steer target is end of path or off-mesh link, do not move past the location.
         if ((endOfPath || offMeshConnection) && len < SMOOTH_PATH_STEP_SIZE)
             len = 1.0f;
