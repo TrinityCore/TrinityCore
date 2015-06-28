a4dffed865c64d739db1cb73fc682d3fc584c6b3
mik1893 <michele.roscelli@gmail.com>
2015-06-28 16:51:12 +0200

    DB/Oculus: typo in sql

--- sql/updates/world/2015_06_28_01_world.sql
+++ sql/updates/world/2015_06_28_01_world.sql
@@ -14,9 +14,9 @@ INSERT INTO `creature_template_addon` (`entry`,`auras`) VALUES
 (27641,'50044'),
 (30905,'59213');
 
-DELETE FROM `smart_scripts` WHERE `entryorguid` 27639 AND `source_type` = 0 AND `id` = 16;
-DELETE FROM `smart_scripts` WHERE `entryorguid` 27640 AND `source_type` = 0 AND `id` = 2;
-DELETE FROM `smart_scripts` WHERE `entryorguid` 27641 AND `source_type` = 0 AND `id` = 0;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 27639 AND `source_type` = 0 AND `id` = 16;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 27640 AND `source_type` = 0 AND `id` = 2;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 27641 AND `source_type` = 0 AND `id` = 0;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (27639, 0, 16, 0, 1, 0, 100, 0, 1000, 1000, 0, 0, 11, 51518, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Ring-Lord Sorceress - Out of Combat - Cast Beam Visual'),
 (27640, 0, 2, 0, 1, 0, 100, 0, 1000, 1000, 0, 0, 11, 51518, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Ring-Lord Conjurer - Out of combat - Cast Visual'),
