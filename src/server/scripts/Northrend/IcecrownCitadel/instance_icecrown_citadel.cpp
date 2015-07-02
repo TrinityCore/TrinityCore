8bf082770681311da7eff7935c956616da955de7
ManuFe <manuel.femia@gmail.com>
2015-07-02 15:18:09 +0200

    Scripts/ICC: Implement Putricide Pre-Event

--- src/server/scripts/Northrend/IcecrownCitadel/instance_icecrown_citadel.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/instance_icecrown_citadel.cpp
@@ -128,6 +128,7 @@ class instance_icecrown_citadel : public InstanceMapScript
                 UpperSpireTeleporterActiveState = NOT_STARTED;
                 BloodQuickeningState = NOT_STARTED;
                 BloodQuickeningMinutes = 0;
+                PutricideTrapEventState = NOT_STARTED;
             }
 
             // A function to help reduce the number of lines for teleporter management.
@@ -465,7 +466,6 @@ class instance_icecrown_citadel : public InstanceMapScript
                     case GO_ORATORY_OF_THE_DAMNED_ENTRANCE:
                     case GO_ORANGE_PLAGUE_MONSTER_ENTRANCE:
                     case GO_GREEN_PLAGUE_MONSTER_ENTRANCE:
-                    case GO_SCIENTIST_ENTRANCE:
                     case GO_CRIMSON_HALL_DOOR:
                     case GO_BLOOD_ELF_COUNCIL_DOOR:
                     case GO_BLOOD_ELF_COUNCIL_DOOR_RIGHT:
@@ -585,6 +585,10 @@ class instance_icecrown_citadel : public InstanceMapScript
                         else if (GetBossState(DATA_ROTFACE) == DONE)
                             HandleGameObject(PutricideGateGUIDs[1], false, go);
                         break;
+                    case GO_SCIENTIST_ENTRANCE:
+                        PutricideDoorGUID = go->GetGUID();
+                        if (GetData(DATA_PUTRICIDE_TRAP) == DONE)
+                            HandleGameObject(PutricideDoorGUID, false, go);
                     case GO_DOODAD_ICECROWN_ORANGETUBES02:
                         PutricidePipeGUIDs[0] = go->GetGUID();
                         if (GetBossState(DATA_FESTERGUT) == DONE)
@@ -687,6 +691,8 @@ class instance_icecrown_citadel : public InstanceMapScript
             {
                 switch (type)
                 {
+                    case DATA_PUTRICIDE_TRAP:
+                        return PutricideTrapEventState;
                     case DATA_SINDRAGOSA_FROSTWYRMS:
                         return FrostwyrmGUIDs.size();
                     case DATA_SPINESTALKER:
@@ -1072,6 +1078,25 @@ class instance_icecrown_citadel : public InstanceMapScript
                             if (GameObject* go = instance->GetGameObject(TeleporterUpperSpireGUID))
                                 SetTeleporterState(go, true);
                             SaveToDB();
+                        }
+                        break;
+                    case DATA_PUTRICIDE_TRAP:
+                        PutricideTrapEventState = data;
+                        if (data == DONE)
+                        {
+                            if (GameObject* go = instance->GetGameObject(PutricideDoorGUID))
+                                go->SetGoState(GO_STATE_ACTIVE_ALTERNATIVE);
+                            HandleGameObject(PutricideCollisionGUID, true);
+                            if (GameObject* go = instance->GetGameObject(PutricideGateGUIDs[0]))
+                                go->SetGoState(GO_STATE_ACTIVE_ALTERNATIVE);
+                            if (GameObject* go = instance->GetGameObject(PutricideGateGUIDs[1]))
+                                go->SetGoState(GO_STATE_ACTIVE_ALTERNATIVE);
+                        }
+                        else if (data == IN_PROGRESS)
+                        {
+                            HandleGameObject(PutricideCollisionGUID, false);
+                            HandleGameObject(PutricideGateGUIDs[0], false);
+                            HandleGameObject(PutricideGateGUIDs[1], false);
                         }
                         break;
                     default:
@@ -1476,6 +1501,7 @@ class instance_icecrown_citadel : public InstanceMapScript
             ObjectGuid FrozenBolvarGUID;
             ObjectGuid PillarsChainedGUID;
             ObjectGuid PillarsUnchainedGUID;
+            ObjectGuid PutricideDoorGUID;
             uint32 TeamInInstance;
             uint32 ColdflameJetsState;
             uint32 UpperSpireTeleporterActiveState;
@@ -1485,6 +1511,7 @@ class instance_icecrown_citadel : public InstanceMapScript
             uint32 BloodQuickeningState;
             uint32 HeroicAttempts;
             uint16 BloodQuickeningMinutes;
+            uint32 PutricideTrapEventState;
             bool IsBonedEligible;
             bool IsOozeDanceEligible;
             bool IsNauseaEligible;
