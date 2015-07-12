c8c3d78b78cbb83b50b1c636e7f3bc562a62c051
Mik1893 <michele.roscelli@gmail.com>
2015-07-12 18:30:04 +0100

    Scripts/Oculus: Implemented Drake GPS system
    (cherry picked from commit 684c71dc572a84ddcb49cfc6b1e6b7629bc117f6)

--- sql/updates/world/2015_07_12_30_world_2015_06_06_11.sql
+++ sql/updates/world/2015_07_12_30_world_2015_06_06_11.sql
@@ -1,2 +1,4 @@
 --
-UPDATE `quest_template` SET `OfferRewardText`="Thanks for the done work!" WHERE `id`=787;
+DELETE FROM `quest_offer_reward` WHERE `ID`=787 AND `VerifiedBuild`=0;
+INSERT INTO `quest_offer_reward` (`ID`, `RewardText`) VALUES
+(787, 'Thanks for the done work!');
