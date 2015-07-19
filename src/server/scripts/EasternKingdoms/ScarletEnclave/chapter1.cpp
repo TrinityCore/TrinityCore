22a0413327619fa18e1defaf3d2bd6d010bb4a6a
lafoniz <lafoniz@users.noreply.github.com>
2015-07-19 21:56:16 +0100

    Scripts/ScarletEnclave: Add weapons to Unworthy Initiate
    Closes #14954
    Closes #14955

--- src/server/scripts/EasternKingdoms/ScarletEnclave/chapter1.cpp
+++ src/server/scripts/EasternKingdoms/ScarletEnclave/chapter1.cpp
@@ -152,6 +152,7 @@ public:
             if (id == 1)
             {
                 wait_timer = 5000;
+                me->LoadEquipment(1);
                 me->CastSpell(me, SPELL_DK_INITIATE_VISUAL, true);
 
                 if (Player* starter = ObjectAccessor::GetPlayer(*me, playerGUID))
