a18dcd58882c84f70a9881b0052d2be852f7eac7
Aokromes <aokromes@gmail.com>
2015-07-19 17:20:15 +0200

    DB/Misc: More 3.3.5a Fixes

---
+++ sql/updates/world/2015_04_26_03_world_335.sql
@@ -0,0 +1,10 @@
+--
+/*
+DELETE FROM `gossip_menu` WHERE `Entry`=2944 AND `text_id` IN (3656, 3658);
+INSERT INTO `gossip_menu` (`entry`, `text_id`) VALUES (2944, 3656), (2944, 3658);
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=14 AND `SourceGroup`=2944;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceGroup`,`SourceEntry`,`ElseGroup`,`ConditionTypeOrReference`,`ConditionValue1`,`ConditionValue2`,`ConditionValue3`, `ErrorTextId`,`ScriptName`,`Comment`, `NegativeCondition`) VALUES
+(14,2944,3656,0,8,878,0,0,0,'','Show gossip menu text if Tribes at War is not rewarded', 1),
+(14,2944,3658,0,8,878,0,0,0,'','Show gossip menu text if Tribes at War rewarded', 0),
+(14,2944,3670,0,8,906,0,0,0,'','Show gossip menu text if Betrayal from Within is rewarded', 0);
+*/
