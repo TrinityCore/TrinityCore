a5fa52d54e3579f9035a163edb9ffde00176a259
Aokromes <aokromes@gmail.com>
2015-07-20 19:28:20 +0200

    DB/Misc: Fix startup errors

---
+++ sql/updates/world/2015_07_20_02_world.sql
@@ -0,0 +1,6 @@
+--
+UPDATE `smart_scripts` SET `entryorguid`=-260421 WHERE `entryorguid`=-37989 AND `source_type`=0 AND `id`=0 AND `link`=0;
+UPDATE `smart_scripts` SET `entryorguid`=-88704 WHERE `entryorguid`=-456 AND `source_type`=0 AND `id`=0 AND `link`=0;
+UPDATE `smart_scripts` SET `entryorguid`=-88703 WHERE `entryorguid`=-455 AND `source_type`=0 AND `id`=0 AND `link`=0;
+UPDATE `smart_scripts` SET `entryorguid`=-88702 WHERE `entryorguid`=-454 AND `source_type`=0 AND `id`=0 AND `link`=0;
+UPDATE `smart_scripts` SET `entryorguid`=-88701 WHERE `entryorguid`=-453 AND `source_type`=0 AND `id`=0 AND `link`=0;
