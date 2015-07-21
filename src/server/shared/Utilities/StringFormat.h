841409882dc0f7905dcc94a94946b0bfae4e2f01
Naios <naios-dev@live.de>
2015-07-22 01:03:40 +0200

    Core/Database: Enable perfect forwarding for database format calls.

--- src/server/shared/Utilities/StringFormat.h
+++ src/server/shared/Utilities/StringFormat.h
@@ -19,7 +19,7 @@
 #ifndef TRINITYCORE_STRING_FORMAT_H
 #define TRINITYCORE_STRING_FORMAT_H
 
-#include <format.h>
+#include "format.h"
 
 namespace Trinity
 {
@@ -28,6 +28,18 @@ namespace Trinity
     inline std::string StringFormat(Format&& fmt, Args&&... args)
     {
         return fmt::sprintf(std::forward<Format>(fmt), std::forward<Args>(args)...);
+    }
+
+    /// Returns true if the given char pointer is null.
+    inline bool IsFormatEmptyOrNull(const char* fmt)
+    {
+        return fmt == nullptr;
+    }
+
+    /// Returns true if the given std::string is empty.
+    inline bool IsFormatEmptyOrNull(std::string const& fmt)
+    {
+        return fmt.empty();
     }
 }
 
