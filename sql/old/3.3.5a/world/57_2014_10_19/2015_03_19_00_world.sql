b86f678a8be80baa6cbe0c3f8bed93cd0cb520e9
Aokromes <aokromes@gmail.com>
2015-07-25 03:03:29 +0200

    DB/Misc: Revert incorrect moves

--- sql/old/3.3.5a/world/57_2014_10_19/2015_03_19_00_world.sql
+++
@@ -1,4 +0,0 @@
-UPDATE `trinity_string` SET `content_default` = 'Map: %u (%s) Zone: %u (%s) Area: %u (%s) Phase: %u\nX: %f Y: %f Z: %f Orientation: %f' WHERE `entry` = 101;
-DELETE FROM `trinity_string` WHERE `entry` = 178;
-INSERT INTO `trinity_string` (`entry`, `content_default`) VALUES
-(178, 'grid[%u,%u]cell[%u,%u] InstanceID: %u\n ZoneX: %f ZoneY: %f\nGroundZ: %f FloorZ: %f Have height data (Map: %u VMap: %u MMap: %u)');
--- sql/old/3.3.5a/world/57_2014_10_19/2015_03_20_01_world.sql
+++
@@ -1,2 +0,0 @@
---
-UPDATE `smart_scripts` SET `link`=0, `event_flags`=0, `comment`='Syreian the Bonecarver - On Aggro CMC - Cast \'Frost Arrow\'' WHERE `entryorguid`=32438 AND `source_type`=0 AND `id`=0 AND `link`=3;
