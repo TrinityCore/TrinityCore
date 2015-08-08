deac934b808b2590c2ff242ddb6987b30bd5e95d
Aokromes <aokromes@gmail.com>
2015-08-08 16:45:38 +0200

    Core/Misc: Misc 3.3.5a fixes

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -2099,6 +2099,9 @@ uint32 Unit::CalculateDamage(WeaponAttackType attType, bool normalized, bool add
         }
     }
 
+    minDamage = std::max(0.f, minDamage);
+    maxDamage = std::max(0.f, maxDamage);
+
     if (minDamage > maxDamage)
         std::swap(minDamage, maxDamage);
 
