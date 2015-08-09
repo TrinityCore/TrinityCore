edcbe1bf473ba033a5ae3577a06740f788d0d6a7
Vincent-Michael <Vincent_Michael@gmx.de>
2015-08-09 17:27:48 +0200

    Core/Instances: Fix order (wrong build) in 57eae1dc13b5e5e4b841d826deed0d13cd6f2d1b

--- src/server/game/Miscellaneous/SharedDefines.h
+++ src/server/game/Miscellaneous/SharedDefines.h
@@ -4950,9 +4950,9 @@ enum RaidGroupReason
 
 enum ResetFailedReason
 {
-    INSTANCE_RESET_FAILED_OFFLINE   = 0, // "Cannot reset %s.  There are players offline in your party."
+    INSTANCE_RESET_FAILED           = 0, // "Cannot reset %s.  There are players still inside the instance."
     INSTANCE_RESET_FAILED_ZONING    = 1, // "Cannot reset %s.  There are players in your party attempting to zone into an instance."
-    INSTANCE_RESET_FAILED           = 2  // "Cannot reset %s.  There are players still inside the instance."
+    INSTANCE_RESET_FAILED_OFFLINE   = 2  // "Cannot reset %s.  There are players offline in your party."
 };
 
 #endif
