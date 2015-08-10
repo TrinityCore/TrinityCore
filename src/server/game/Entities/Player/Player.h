be7eac90ed35b231ee07357a5fcffcf08fa41ee3
Rochet2 <tqbattlenet@gmail.com>
2015-08-10 18:12:22 +0200

    Core/RestState: Check area trigger radius instead of 1.0f distance
    Closes #15016
    Closes #15052
    
    Signed-off-by: jackpoz <giacomopoz@gmail.com>
    (cherry picked from commit 88b25b6236e0fad56014c5fa58011e9b4e8cddd8)
    
    Conflicts:
    	src/server/game/Entities/Player/Player.h
    	src/server/game/Handlers/MiscHandler.cpp

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -1306,6 +1306,7 @@ class Player : public Unit, public GridObject<Player>
 
         bool IsInWater() const override { return m_isInWater; }
         bool IsUnderWater() const override;
+        bool IsInAreaTriggerRadius(const AreaTriggerEntry* trigger) const;
 
         void SendInitialPacketsBeforeAddToMap();
         void SendInitialPacketsAfterAddToMap();
@@ -1367,7 +1368,7 @@ class Player : public Unit, public GridObject<Player>
 
         void setDeathState(DeathState s) override;                   // overwrite Unit::setDeathState
 
-        void InnEnter(time_t time, uint32 mapid, float x, float y, float z);
+        void InnEnter(time_t time, uint32 triggerId);
 
         float GetRestBonus() const { return m_rest_bonus; }
         void SetRestBonus(float rest_bonus_new);
@@ -1375,10 +1376,7 @@ class Player : public Unit, public GridObject<Player>
         RestType GetRestType() const { return rest_type; }
         void SetRestType(RestType n_r_type) { rest_type = n_r_type; }
 
-        uint32 GetInnPosMapId() const { return inn_pos_mapid; }
-        float GetInnPosX() const { return inn_pos_x; }
-        float GetInnPosY() const { return inn_pos_y; }
-        float GetInnPosZ() const { return inn_pos_z; }
+        uint32 GetInnTriggerId() const { return inn_triggerId; }
 
         time_t GetTimeInnEnter() const { return time_inn_enter; }
         void UpdateInnerTime (time_t time) { time_inn_enter = time; }
@@ -2823,10 +2821,7 @@ class Player : public Unit, public GridObject<Player>
 
         ////////////////////Rest System/////////////////////
         time_t time_inn_enter;
-        uint32 inn_pos_mapid;
-        float  inn_pos_x;
-        float  inn_pos_y;
-        float  inn_pos_z;
+        uint32 inn_triggerId;
         float m_rest_bonus;
         RestType rest_type;
         ////////////////////Rest System/////////////////////
