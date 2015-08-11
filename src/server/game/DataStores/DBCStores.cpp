eb1e6ba3ad30189397073b71c7114fc7e21eb771
David Carlos Manuelda <StormByte@gmail.com>
2015-08-11 15:47:10 +0200

    Fix compile error + updated comment to be explained better

--- src/server/game/DataStores/DBCStores.cpp
+++ src/server/game/DataStores/DBCStores.cpp
@@ -927,9 +927,9 @@ std::string GetDBCLocaleFolder(std::string const& dataPath)
 
     const int locale = sConfigMgr->GetIntDefault("DBC.Locale", LOCALE_enUS);
     for(; dir_iter != end_iter; ++dir_iter)
-        if (boost::filesystem::is_directory(*dir_iter) && GetLocaleByName((*dir_iter).path().filename()) == locale)
+        if (boost::filesystem::is_directory(*dir_iter) && GetLocaleByName((*dir_iter).path().filename().string()) == locale)
             return (boost::filesystem::path(dataPath) / (*dir_iter).path()).string(); //Return the full path appending detected locale folder
     
-    //No subfolder detected but files were present, so we return it unmodified and let the caller throw error if necesary
+    //Empty folder, or it has files but not subfolders (probably the DBC files). Return it unmodified and let caller detect its correctness
     return dataPath;
 }
