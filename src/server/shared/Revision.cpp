21cf8bd158d5694e6874fb87aef89de59981e779
Shauren <shauren.trinity@gmail.com>
2015-08-16 18:08:03 +0200

    Build fix for WheatyExceptionReport, c++ exceptions must not be thrown in SEH handlers

--- src/server/shared/Revision.cpp
+++ src/server/shared/Revision.cpp
@@ -2,70 +2,67 @@
 #include "CompilerDefs.h"
 #include "revision_data.h"
 
-std::string Revision::GetPackageName()
-{
-    return "TrinityCore";
-}
-
-std::string Revision::GetHash()
+char const* Revision::GetHash()
 {
     return _HASH;
 }
 
-std::string Revision::GetDate()
+char const* Revision::GetDate()
 {
     return _DATE;
 }
 
-std::string Revision::GetBranch()
+char const* Revision::GetBranch()
 {
     return _BRANCH;
 }
 
-std::string Revision::GetSourceDirectory()
+char const* Revision::GetSourceDirectory()
 {
     return _SOURCE_DIRECTORY;
 }
 
-std::string Revision::GetMySQLExecutable()
+char const* Revision::GetMySQLExecutable()
 {
     return _MYSQL_EXECUTABLE;
 }
 
-std::string Revision::GetFullDatabase()
+char const* Revision::GetFullDatabase()
 {
     return _FULL_DATABASE;
 }
 
-std::string Revision::GetFullVersion()
+#define _PACKAGENAME "TrinityCore"
+
+char const* Revision::GetFullVersion()
 {
 #if PLATFORM == PLATFORM_WINDOWS
 # ifdef _WIN64
-    return GetPackageName() + " rev. " + VER_PRODUCTVERSION_STR + " (Win64, " + _BUILD_DIRECTIVE + ")";
+    return _PACKAGENAME " rev. " VER_PRODUCTVERSION_STR " (Win64, " _BUILD_DIRECTIVE ")";
 # else
-    return GetPackageName() + " rev. " + VER_PRODUCTVERSION_STR + " (Win32, " + _BUILD_DIRECTIVE + ")";
+    return _PACKAGENAME " rev. " VER_PRODUCTVERSION_STR " (Win32, " _BUILD_DIRECTIVE ")";
 # endif
 #else
-    return GetPackageName() + " rev. " + VER_PRODUCTVERSION_STR + " (Unix, " + _BUILD_DIRECTIVE + ")";
+    return _PACKAGENAME " rev. " VER_PRODUCTVERSION_STR " (Unix, " _BUILD_DIRECTIVE ")";
 #endif
 }
 
-std::string GetCompanyNameStr()
+char const* GetCompanyNameStr()
 {
     return VER_COMPANYNAME_STR;
 }
 
-std::string GetLegalCopyrightStr()
+char const* GetLegalCopyrightStr()
 {
     return VER_LEGALCOPYRIGHT_STR;
 }
 
-std::string GetFileVersionStr()
+char const* GetFileVersionStr()
 {
     return VER_FILEVERSION_STR;
 }
 
-std::string GetProductVersionStr()
+char const* GetProductVersionStr()
 {
     return VER_PRODUCTVERSION_STR;
 }
