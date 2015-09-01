adce3440678ec0e0664e21e972aa2d9f451ade7b
Aokromes <aokromes@gmail.com>
2015-09-01 10:51:13 +0200

    DB/Misc: Fix some startup errors

---
+++ sql/updates/world/2015_09_01_01_world.sql
@@ -0,0 +1,3 @@
+--
+INSERT INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `IconName`, `castBarCaption`, `unk1`, `faction`, `flags`, `size`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`, `Data6`, `Data7`, `Data8`, `Data9`, `Data10`, `Data11`, `Data12`, `Data13`, `Data14`, `Data15`, `Data16`, `Data17`, `Data18`, `Data19`, `Data20`, `Data21`, `Data22`, `Data23`, `Data24`, `Data25`, `Data26`, `Data27`, `Data28`, `Data29`, `Data30`, `Data31`, `unkInt32`, `AIName`, `ScriptName`, `VerifiedBuild`) VALUES
+(214611,31,11469,'Instance Portal (Raid 4 Difficulties)','','','',35,8192,5,2,216,217,216,217,11471,0,6,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'','',20182);
---
+++ sql/updates/world/2015_09_01_02_world.sql
@@ -0,0 +1,6 @@
+--
+UPDATE `creature_template` SET `lootid`=215, `pickpocketloot`=215 WHERE `entry`=215;
+UPDATE `creature_template` SET `lootid`=909, `pickpocketloot`=909 WHERE `entry`=909;
+UPDATE `creature_template` SET `lootid`=4096, `pickpocketloot`=4096 WHERE `entry`=4096;
+UPDATE `creature_template` SET `lootid`=5615, `pickpocketloot`=5615 WHERE `entry`=5615;
+UPDATE `creature_template` SET `lootid`=5618, `pickpocketloot`=5618 WHERE `entry`=5618;
