2c457d65c6a38c72720cccedb6e62496a66fb019
Aokromes <Aokromes@users.noreply.github.com>
2015-09-03 09:56:27 +0200

    DB/Misc: Delete more duplicated spawns

---
+++ sql/updates/world/2015_09_03_02_world.sql
@@ -0,0 +1,7 @@
+--
+DELETE FROM `creature` WHERE `guid`=201508 AND `id`=37824;
+DELETE FROM `creature` WHERE `guid`=201460 AND `id`=38228;
+DELETE FROM `creature` WHERE `guid`=201661 AND `id`=38228;
+DELETE FROM `creature` WHERE `guid`=201442 AND `id`=38231;
+DELETE FROM `creature` WHERE `guid`=201490 AND `id`=38232;
+DELETE FROM `creature` WHERE `guid`=201539 AND `id`=38232;
