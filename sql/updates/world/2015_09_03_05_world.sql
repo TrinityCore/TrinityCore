288a46a347873b7f625d9e394d5e988bde786d8c
Aokromes <aokromes@gmail.com>
2015-09-03 11:43:53 +0200

    DB/Spawns: Sync some 3.3.5a spawns with 3.3.5a db

---
+++ sql/updates/world/2015_09_03_05_world.sql
@@ -0,0 +1,8 @@
+--
+DELETE FROM `creature` WHERE `guid`=984 and `id`=25039;
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `VerifiedBuild`) VALUES
+(984, 25039, 530, 0, 0, 1, 1, 0, 1, 12845.9, -7005.7, 18.5936, 3.59695, 25, 0, 0, 4890, 7196, 0, 0, 0, 0, 0);
+
+DELETE FROM `creature` WHERE `guid`=1007 and `id`=18694;
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `VerifiedBuild`) VALUES
+(1007, 18694, 530, 0, 0, 1, 1, 0, 0, -4058.48, 730.852, 0.618181, 5.42156, 86400, 0, 0, 13084, 0, 0, 0, 0, 0, 0);
