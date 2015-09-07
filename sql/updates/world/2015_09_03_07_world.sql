cec7f60d73d1a35f8a573e5acb88e12f9675ff97
Aokromes <aokromes@gmail.com>
2015-09-07 05:08:33 +0200

    DB/Misc: Fix value

--- sql/updates/world/2015_09_03_07_world.sql
+++ sql/updates/world/2015_09_03_07_world.sql
@@ -4,4 +4,4 @@ SET @Guid :=     43467;
 
 DELETE FROM `creature` WHERE `id`=@Thane;
 INSERT INTO `creature` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`MovementType`,`npcflag`,`unit_flags`,`dynamicflags`)  VALUES
-(@Guid,@Thane ,571,1,1,0,1419,3317.76,-5112.08,300.462,1.78269,300,0,0,12762,3561,0,0,0,0); -- spawn NPC if there isn't, GUID is random
+(@Guid,@Thane ,571,1,1,0,1,3317.76,-5112.08,300.462,1.78269,300,0,0,12762,3561,0,0,0,0); -- spawn NPC if there isn't, GUID is random
