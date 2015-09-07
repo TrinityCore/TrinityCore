9d451007f92284eb5369d69df86f5a1fc05fb44e
Aokromes <aokromes@gmail.com>
2015-09-07 05:10:36 +0200

    DB/Misc: Fix late night typo

--- sql/updates/world/2015_09_07_00_world.sql
+++ sql/updates/world/2015_09_07_00_world.sql
@@ -1,3 +1,3 @@
 --
 DELETE FROM `creature_addon` WHERE `guid` IN (660);
-DELETE FROM `game_object_creature` WHERE `guid` IN (289,290,291);
+DELETE FROM `game_event_creature` WHERE `guid` IN (289,290,291);
