0b6aaa28b12b1061e3433c936f8bb7dc2250cfc3
Aokromes <aokromes@gmail.com>
2015-09-07 05:00:17 +0200

    DB/Misc: Fix some startup errors

---
+++ sql/updates/world/2015_09_04_11_world.sql
@@ -0,0 +1,3 @@
+--
+UPDATE `smart_scripts` SET `entryorguid`=-142407 WHERE `entryorguid`=-3620 AND `source_type`=0 AND `id`=0 AND `link`=0;
+UPDATE `smart_scripts` SET `entryorguid`=-142408 WHERE `entryorguid`=-3621 AND `source_type`=0 AND `id`=0 AND `link`=0;
---
+++ sql/updates/world/2015_09_07_00_world.sql
@@ -0,0 +1,3 @@
+--
+DELETE FROM `creature_addon` WHERE `guid` IN (660);
+DELETE FROM `game_object_creature` WHERE `guid` IN (289,290,291);
