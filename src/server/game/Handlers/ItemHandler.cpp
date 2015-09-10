0a488b7e41c5415565096d68d672d8d804e9e330
Rochet2 <rochet2@post.com>
2015-09-10 20:59:31 +0300

    Fix logic in Item::CanTransmogrifyItemWithItem
    Allows transmogrifying axe, sword, mace (1h and 2h respectively) with
    eachother and fixes possible misuse of thrown and chest
    http://www.wowhead.com/guide=1564/how-to-transmogrify-and-what-you-can-and-cannot-do
    Closes https://github.com/TrinityCore/TrinityCore/issues/15475

--- src/server/game/Handlers/ItemHandler.cpp
+++ src/server/game/Handlers/ItemHandler.cpp
@@ -1141,6 +1141,11 @@ void WorldSession::HandleTransmogrifyItems(WorldPackets::Item::TransmogrifyItems
             TC_LOG_DEBUG("network", "WORLD: HandleTransmogrifyItems - Player (%s, name: %s) tried to transmogrify an invalid item in a valid slot (slot: %u).", player->GetGUID().ToString().c_str(), player->GetName().c_str(), transmogItem.Slot);
             return;
         }
+        if (player->CanUseItem(itemTransmogrified->GetTemplate()) != EQUIP_ERR_OK)
+        {
+            TC_LOG_DEBUG("network", "WORLD: HandleTransmogrifyItems - Player (%s, name: %s) tried to transmogrify an unequippable item in a valid slot (slot: %u).", player->GetGUID().ToString().c_str(), player->GetName().c_str(), transmogItem.Slot);
+            return;
+        }
 
         WorldPackets::Item::ItemInstance itemInstance;
         BonusData const* bonus = nullptr;
@@ -1151,6 +1156,11 @@ void WorldSession::HandleTransmogrifyItems(WorldPackets::Item::TransmogrifyItems
             if (!itemTransmogrifier)
             {
                 TC_LOG_DEBUG("network", "WORLD: HandleTransmogrifyItems - Player (%s, name: %s) tried to transmogrify with an invalid item (%s).", player->GetGUID().ToString().c_str(), player->GetName().c_str(), transmogItem.SrcItemGUID->ToString().c_str());
+                return;
+            }
+            if (player->CanUseItem(itemTransmogrifier->GetTemplate()) != EQUIP_ERR_OK)
+            {
+                TC_LOG_DEBUG("network", "WORLD: HandleTransmogrifyItems - Player (%s, name: %s) tried to transmogrify with an unequippable item (%s).", player->GetGUID().ToString().c_str(), player->GetName().c_str(), transmogItem.SrcItemGUID->ToString().c_str());
                 return;
             }
 
@@ -1166,6 +1176,12 @@ void WorldSession::HandleTransmogrifyItems(WorldPackets::Item::TransmogrifyItems
             if (!itemTransmogrifier)
             {
                 TC_LOG_DEBUG("network", "WORLD: HandleTransmogrifyItems - Player (%s, name: %s) tried to transmogrify with an invalid void storage item (%s).", player->GetGUID().ToString().c_str(), player->GetName().c_str(), transmogItem.SrcVoidItemGUID->ToString().c_str());
+                return;
+            }
+            ItemTemplate const * transmogrifierTemplate = sObjectMgr->GetItemTemplate(itemTransmogrifier->ItemEntry);
+            if (player->CanUseItem(transmogrifierTemplate) != EQUIP_ERR_OK)
+            {
+                TC_LOG_DEBUG("network", "WORLD: HandleTransmogrifyItems - Player (%s, name: %s) tried to transmogrify with an unequippable void storage item (%s).", player->GetGUID().ToString().c_str(), player->GetName().c_str(), transmogItem.SrcVoidItemGUID->ToString().c_str());
                 return;
             }
 
