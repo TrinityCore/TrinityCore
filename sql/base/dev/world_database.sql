5beae594af1f9f695c2f3abd4294917e11e0408a
Aokromes <aokromes@gmail.com>
2015-09-11 06:13:01 +0200

    DB/Misc: Fix travis

--- sql/base/dev/world_database.sql
+++ sql/base/dev/world_database.sql
@@ -3608,14 +3608,14 @@ DROP TABLE IF EXISTS `spell_target_position`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `spell_target_position` (
   `id` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT 'Identifier',
-  `effIndex` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `EffectIndex` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `target_map` smallint(5) unsigned NOT NULL DEFAULT '0',
   `target_position_x` float NOT NULL DEFAULT '0',
   `target_position_y` float NOT NULL DEFAULT '0',
   `target_position_z` float NOT NULL DEFAULT '0',
   `target_orientation` float NOT NULL DEFAULT '0',
   `VerifiedBuild` smallint(5) DEFAULT '0',
-  PRIMARY KEY (`id`,`effIndex`)
+  PRIMARY KEY (`id`,`EffectIndex`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Spell System';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
