1f0e7038852ea7b08a7d99df8c6e9b096f695bcd
Killyana <morphone1@gmail.com>
2015-09-11 21:21:34 +0200

    DB: Fix Some DB errors

---
+++ sql/updates/world/2015_09_11_55_world.sql
@@ -0,0 +1,5 @@
+-- 
+UPDATE `smart_scripts` SET `action_param1`= 371 WHERE `entryorguid`=30134 AND `source_type`=0 AND `id` =16;
+UPDATE `smart_scripts` SET `action_param1`= 371 WHERE `entryorguid`=3038200 AND `source_type`=9 AND `id` =2;
+UPDATE `creature_addon` SET `mount`=14332 WHERE `guid`=370861;
+UPDATE `creature_addon` SET `mount`=0 WHERE `guid`=370857;
