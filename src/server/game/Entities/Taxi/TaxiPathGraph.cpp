54e5147e91ea586cbe1c04b405effd15debd9f5a
StormBytePP <stormbyte@gmail.com>
2015-09-12 03:05:25 +0200

    Updated node path algorithm and added an explanation about its research

--- src/server/game/Entities/Taxi/TaxiPathGraph.cpp
+++ src/server/game/Entities/Taxi/TaxiPathGraph.cpp
@@ -102,42 +102,65 @@ void TaxiPathGraph::_addVerticeAndEdgeFromNodeInfo(const TaxiNodeInfo& from, con
 }
 
 size_t TaxiPathGraph::GetCompleteNodeRoute(uint32_t sourceNodeID, uint32_t destinationNodeID, std::vector<uint32>& shortestPath)
-{    
-    std::vector<Graph::vertex_descriptor> p(boost::num_vertices(m_graph));
-    std::vector<cost> d(boost::num_vertices(m_graph));
+{
+    /*
+        Information about node algorithm from client
+    Since client does not give information about *ALL* nodes you have to pass by when going from sourceNodeID to destinationNodeID, algorithms like A* on directed graph of taxi
+    nodes are needed. But since, 6.x implemented early landing request feature, client seems not to be picking the least expensive path in terms in neither distance neither money cost.
+    Examining several paths I discovered the following algorithm:
+        * If destinationNodeID has is the next destination, connected directly to sourceNodeID, then, client just pick up this route regardless of distance
+        * else, client will avoid to pick a node which distance is equal or greater than TaxiPathGraph::MaxFlightDistanceThreshold to avoid long node to node distances for requesting
+        early landings, so we use A* algorithm with a custom weight property map to reflect this condition.
 
-    vertex start = _getVertexIDFromNodeID(sourceNodeID);
-    vertex goal = _getVertexIDFromNodeID(destinationNodeID);
-    //WeightMap weightmap = boost::get(boost::edge_weight, m_graph);
+        * When early landing is requested, according to behavior on retail, you can never end in a node you did not discovered before
+    */
+    bool hasDirectPath = false;
+    shortestPath.clear();
     
-    try
+    // Find if we have a direct path from sourceNodeID to destinationNodeID in graph
+    typename boost::graph_traits<Graph>::out_edge_iterator ei, ei_end;
+    for (boost::tie(ei, ei_end) = boost::out_edges(_getVertexIDFromNodeID(sourceNodeID), m_graph); ei != ei_end && !hasDirectPath; ++ei)
+        if (boost::target(*ei, m_graph) == _getVertexIDFromNodeID(destinationNodeID))
+            hasDirectPath = true;
+    
+    if (hasDirectPath)
+        shortestPath = { sourceNodeID, destinationNodeID };
+    else
     {
-        auto wow_custom_weight_map = boost::make_transform_value_property_map(
-            [](float w) { return w>MaxFlightDistanceThreshold ? std::numeric_limits<cost>::infinity() : w; },
-            boost::get(boost::edge_weight, m_graph)
-        );
-        boost::astar_search(m_graph, start,
-            boost::astar_heuristic<Graph, cost>(),
-            boost::weight_map(wow_custom_weight_map)
-            .predecessor_map(boost::make_iterator_property_map(p.begin(), get(boost::vertex_index, m_graph)))
-            .distance_map(boost::make_iterator_property_map(d.begin(), get(boost::vertex_index, m_graph)))
-            .visitor(astar_goal_visitor<vertex>(goal)));
-    }
-    catch(found_goal fg)
-    { // found a path to the goal
-        std::list<vertex> shortest_path;
-        for(vertex v = goal;; v = p[v]) {
-            shortest_path.push_front(v);
-            if(p[v] == v)
-                break;
+        std::vector<Graph::vertex_descriptor> p(boost::num_vertices(m_graph));
+        std::vector<cost> d(boost::num_vertices(m_graph));
+
+        vertex start = _getVertexIDFromNodeID(sourceNodeID);
+        vertex goal = _getVertexIDFromNodeID(destinationNodeID);
+        //WeightMap weightmap = boost::get(boost::edge_weight, m_graph);
+
+        try
+        {
+            auto wow_custom_weight_map = boost::make_transform_value_property_map(
+                [](float w) { return w>MaxFlightDistanceThreshold ? std::numeric_limits<cost>::infinity() : w; },
+                boost::get(boost::edge_weight, m_graph)
+            );
+            boost::astar_search(m_graph, start,
+                boost::astar_heuristic<Graph, cost>(),
+                boost::weight_map(wow_custom_weight_map)
+                .predecessor_map(boost::make_iterator_property_map(p.begin(), get(boost::vertex_index, m_graph)))
+                .distance_map(boost::make_iterator_property_map(d.begin(), get(boost::vertex_index, m_graph)))
+                .visitor(astar_goal_visitor<vertex>(goal)));
         }
-        
-        //shortestPath.push_back(_getVertexIDFromNodeID(start));
-        for(std::list<vertex>::const_iterator spi = shortest_path.begin(); spi != shortest_path.end(); spi++)
-            shortestPath.push_back(_getNodeIDFromVertexID(*spi));
-        return shortestPath.size();
+        catch(found_goal fg)
+        { // found a path to the goal
+            std::list<vertex> shortest_path;
+            for(vertex v = goal;; v = p[v]) {
+                shortest_path.push_front(v);
+                if(p[v] == v)
+                    break;
+            }
+
+            for(std::list<vertex>::const_iterator spi = shortest_path.begin(); spi != shortest_path.end(); spi++)
+                shortestPath.push_back(_getNodeIDFromVertexID(*spi));
+        }
     }
-    return 0;
+    return shortestPath.size();
 }
 
 TaxiPathGraph::TaxiNodeInfo const* TaxiPathGraph::GetTaxiNodeInfoByID(uint32_t nodeID)
