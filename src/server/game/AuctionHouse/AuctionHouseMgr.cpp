4c5422dc1ee7cdf69363ee29dcdcb98fc95938c9
Tomatoes <diredarknil@gmail.com>
2015-09-17 13:12:37 -0500

    Server/Game/AuctionHouse
    Fixes world config value of less than 1 being ignored by deposit formula.
    
    Had problems with the last commits and pr, sorry
    
    Update AuctionHouseMgr.cpp
    
    Adds another check to config to handle values over 1 and properly adjust the minimum higher. Thanks jackpoz!
    
    (cherry picked from commit 038af33a2787a7b7de44a4f083c200bbd3a1beb2)

--- src/server/game/AuctionHouse/AuctionHouseMgr.cpp
+++ src/server/game/AuctionHouse/AuctionHouseMgr.cpp
@@ -67,7 +67,7 @@ uint32 AuctionHouseMgr::GetAuctionDeposit(AuctionHouseEntry const* entry, uint32
     uint32 MSV = pItem->GetTemplate()->GetSellPrice();
 
     if (MSV <= 0)
-        return AH_MINIMUM_DEPOSIT;
+        return AH_MINIMUM_DEPOSIT * sWorld->getRate(RATE_AUCTION_DEPOSIT);
 
     float multiplier = CalculatePct(float(entry->DepositRate), 3);
     uint32 timeHr = (((time / 60) / 60) / 12);
@@ -78,8 +78,8 @@ uint32 AuctionHouseMgr::GetAuctionDeposit(AuctionHouseEntry const* entry, uint32
     TC_LOG_DEBUG("auctionHouse", "Multiplier: %f", multiplier);
     TC_LOG_DEBUG("auctionHouse", "Deposit:    %u", deposit);
 
-    if (deposit < AH_MINIMUM_DEPOSIT)
-        return AH_MINIMUM_DEPOSIT;
+    if (deposit < AH_MINIMUM_DEPOSIT * sWorld->getRate(RATE_AUCTION_DEPOSIT))
+        return AH_MINIMUM_DEPOSIT * sWorld->getRate(RATE_AUCTION_DEPOSIT);
     else
         return deposit;
 }
