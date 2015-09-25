e68ff4186e685de00362b12bc0b5084a4d6065dd
Shauren <shauren.trinity@gmail.com>
2015-09-25 21:26:31 +0200

    Core/Commands: Fixed .gobject move and turn
    (cherry picked from commit 7eb25f1af6b74d3559b541d45da6cce50e657ba4)
    
    Closes #3802
    Closes #15598

--- src/server/game/Entities/Transport/Transport.cpp
+++ src/server/game/Entities/Transport/Transport.cpp
@@ -352,6 +352,7 @@ GameObject* Transport::CreateGOPassenger(uint32 guid, GameObjectData const* data
     go->m_movementInfo.transport.pos.Relocate(x, y, z, o);
     CalculatePassengerPosition(x, y, z, &o);
     go->Relocate(x, y, z, o);
+    go->RelocateStationaryPosition(x, y, z, o);
 
     if (!go->IsPositionValid())
     {
@@ -704,6 +705,7 @@ void Transport::UpdatePassengerPositions(PassengerSet& passengers)
                 break;
             case TYPEID_GAMEOBJECT:
                 GetMap()->GameObjectRelocation(passenger->ToGameObject(), x, y, z, o, false);
+                passenger->ToGameObject()->RelocateStationaryPosition(x, y, z, o);
                 break;
             case TYPEID_DYNAMICOBJECT:
                 GetMap()->DynamicObjectRelocation(passenger->ToDynObject(), x, y, z, o);
