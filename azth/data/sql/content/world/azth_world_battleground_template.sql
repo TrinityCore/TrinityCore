51fce895a1b99a6157b063809689a1259665f42c
ShinDarth <borzifrancesco@gmail.com>
2015-09-27 15:54:43 +0200

    [AZTH] Fix RBG values

--- azth/data/sql/content/world/azth_world_battleground_template.sql
+++ azth/data/sql/content/world/azth_world_battleground_template.sql
@@ -1,3 +1,26 @@
-UPDATE `battleground_template` SET `MinPlayersPerTeam` = 2 WHERE `ID` = 2; -- Warsong Gulch
-UPDATE `battleground_template` SET `MinPlayersPerTeam` = 5 WHERE `ID` IN (3, 7, 9); -- Arathi Basin, Eye of The Storm, Strand of the Ancients
+--  1 = Alterac Valley
+--  2 = Warsong Gulch
+--  3 = Arathi Basin
+--  7 = Eye of The Storm
+--  4 = Nagrand Arena
+--  5 = Blades's Edge Arena
+--  6 = All Arena
+--  8 = Ruins of Lordaeron
+--  9 = Strand of the Ancients
+-- 10 = Dalaran Sewers
+-- 11 = The Ring of Valor
+-- 30 = Isle of Conquest
+-- 32 = Random battleground
+
+-- MinPlayersPerTeam
+UPDATE `battleground_template` SET `MinPlayersPerTeam` = 2  WHERE `ID` = 2; -- Warsong Gulch
+UPDATE `battleground_template` SET `MinPlayersPerTeam` = 5  WHERE `ID` IN (3, 7, 9); -- Arathi Basin, Eye of The Storm, Strand of the Ancients
 UPDATE `battleground_template` SET `MinPlayersPerTeam` = 10 WHERE `ID` = 1; -- Alterac Valley
+UPDATE `battleground_template` SET `MinPlayersPerTeam` = 5  WHERE `ID` = 32; -- RBG
+
+-- Weights
+UPDATE `battleground_template` SET `Weight` = 0  WHERE `ID` = 1;       -- Alterac Valley
+UPDATE `battleground_template` SET `Weight` = 0  WHERE `ID` = 30;      -- Isle of Conquest
+UPDATE `battleground_template` SET `Weight` = 2  WHERE `ID` = 2;       -- Warsong Gulch
+UPDATE `battleground_template` SET `Weight` = 2  WHERE `ID` = 9;       -- Strand of the Ancients
+UPDATE `battleground_template` SET `Weight` = 3  WHERE `ID` IN (3, 7); -- Arathi Basin, Eye of The Storm
