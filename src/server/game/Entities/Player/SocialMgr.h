e84a94d1e6f3db132603c899798f4f4af9e12fa4
Shauren <shauren.trinity@gmail.com>
2015-10-03 17:59:21 +0200

    Core/Players: Added missed changes to previous commit

--- src/server/game/Entities/Player/SocialMgr.h
+++ src/server/game/Entities/Player/SocialMgr.h
@@ -58,7 +58,8 @@ struct FriendInfo
     FriendInfo() : Status(FRIEND_STATUS_OFFLINE), Flags(0), Area(0), Level(0), Class(0), Note()
     { }
 
-    FriendInfo(uint8 flags, std::string const& note) : Status(FRIEND_STATUS_OFFLINE), Flags(flags), Area(0), Level(0), Class(0), Note(note)
+    FriendInfo(ObjectGuid const& accountGuid, uint8 flags, std::string const& note) : WowAccountGuid(accountGuid), Status(FRIEND_STATUS_OFFLINE),
+        Flags(flags), Area(0), Level(0), Class(0), Note(note)
     { }
 };
 
