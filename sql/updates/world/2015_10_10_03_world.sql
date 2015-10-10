1f02e39c3ebd36f07ac85f508fe1cfd66dbc0028
Killyana <morphone1@gmail.com>
2015-10-10 13:55:07 +0200

    DB/Locales: Kobold Camp Cleanup
    By @DanielMo1603
    Closes #15690

---
+++ sql/updates/world/2015_10_10_03_world.sql
@@ -0,0 +1,2 @@
+--
+UPDATE `locales_quest` SET `Title_loc2` = "Le nettoyage du camp des kobolds", `Title_loc3` = "Säuberung im Koboldlager", `Title_loc7` = "La Limpieza del Campamento Kóbold", `Title_loc8` = "Нападение на лагерь кобольдов" WHERE `Id` = 7;
