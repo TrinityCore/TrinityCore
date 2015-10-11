5736deb8549da301cec076fba4d3c1aeedb4227a
Naios <naios-dev@live.de>
2015-10-12 00:58:11 +0200

    Core/Build: Never overwrite the msvc cxx flags in the cache * Setting all compiler flags in the cache led to circular bloating of   msvc cxx parameters (was visible in console) which caused MSVC to   rebuild the whole solution even on a small CMake change.   It's not neccessary anyway to set the cxx parameters to the cache   to take effect. * I'm not sure if the workarround is still needed for MSVC 12+. * Cleaning the CMake cache is recommended! * ref #15671
    (cherry picked from commit 0946fcf2aa35ebe0b8db07813315e3d6546b9d31)

--- cmake/compiler/msvc/settings.cmake
+++ cmake/compiler/msvc/settings.cmake
@@ -78,7 +78,7 @@ endif()
 # Fixes a compiler-problem when using PCH - the /Ym flag is adjusted by the compiler in MSVC2012, hence we need to set an upper limit with /Zm to avoid discrepancies)
 # (And yes, this is a verified , unresolved bug with MSVC... *sigh*)
 string(REGEX REPLACE "/Zm[0-9]+ *" "" CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /Zm500" CACHE STRING "" FORCE)
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /Zm500")
 
 # Enable and treat as errors the following warnings to easily detect virtual function signature failures:
 # 'function' : member function does not override any base class virtual member function
