TrinityCore/TrinityCore

commit b1032ed6203a5839906fab7137484b0ef8f0e58f

Change many uint32 to ObjectGuid::LowType in line with 6.x changes.

src/server/game/Entities/Player/SocialMgr.h
@@ -60,8 +60,8 @@ struct FriendInfo
     { }
 };
 
-typedef std::map<uint32, FriendInfo> PlayerSocialMap;
-typedef std::map<uint32, PlayerSocial> SocialMap;
+typedef std::map<ObjectGuid::LowType, FriendInfo> PlayerSocialMap;
+typedef std::map<ObjectGuid::LowType, PlayerSocial> SocialMap;
 
 /// Results of friend related commands
 enum FriendsResult
@@ -104,20 +104,20 @@ class PlayerSocial
     public:
         PlayerSocial();
         // adding/removing
-        bool AddToSocialList(uint32 friend_guid, bool ignore);
-        void RemoveFromSocialList(uint32 friend_guid, bool ignore);
-        void SetFriendNote(uint32 friendGuid, std::string note);
+        bool AddToSocialList(ObjectGuid::LowType friendGuid, bool ignore);
+        void RemoveFromSocialList(ObjectGuid::LowType friend_guid, bool ignore);
+        void SetFriendNote(ObjectGuid::LowType friendGuid, std::string note);
         // Packet send's
         void SendSocialList(Player* player);
         // Misc
-        bool HasFriend(uint32 friend_guid);
-        bool HasIgnore(uint32 ignore_guid);
-        uint32 GetPlayerGUID() const { return m_playerGUID; }
-        void SetPlayerGUID(uint32 guid) { m_playerGUID = guid; }
+        bool HasFriend(ObjectGuid::LowType friend_guid);
+        bool HasIgnore(ObjectGuid::LowType ignore_guid);
+        ObjectGuid::LowType GetPlayerGUID() const { return m_playerGUID; }
+        void SetPlayerGUID(ObjectGuid::LowType guid) { m_playerGUID = guid; }
         uint32 GetNumberOfSocialsWithFlag(SocialFlag flag);
     private:
         PlayerSocialMap m_playerSocialMap;
-        uint32 m_playerGUID;
+        ObjectGuid::LowType m_playerGUID;
 };
 
 class SocialMgr
@@ -134,15 +134,15 @@ class SocialMgr
         }
 
         // Misc
-        void RemovePlayerSocial(uint32 guid) { m_socialMap.erase(guid); }
+        void RemovePlayerSocial(ObjectGuid::LowType guid) { m_socialMap.erase(guid); }
 
-        void GetFriendInfo(Player* player, uint32 friendGUID, FriendInfo &friendInfo);
+        void GetFriendInfo(Player* player, ObjectGuid::LowType friendGUID, FriendInfo &friendInfo);
         // Packet management
-        void MakeFriendStatusPacket(FriendsResult result, uint32 friend_guid, WorldPacket* data);
-        void SendFriendStatus(Player* player, FriendsResult result, uint32 friend_guid, bool broadcast);
+        void MakeFriendStatusPacket(FriendsResult result, ObjectGuid::LowType friend_guid, WorldPacket* data);
+        void SendFriendStatus(Player* player, FriendsResult result, ObjectGuid::LowType friend_guid, bool broadcast);
         void BroadcastToFriendListers(Player* player, WorldPacket* packet);
         // Loading
-        PlayerSocial *LoadFromDB(PreparedQueryResult result, uint32 guid);
+        PlayerSocial *LoadFromDB(PreparedQueryResult result, ObjectGuid::LowType guid);
     private:
         SocialMap m_socialMap;
 };

src/server/game/Events/GameEventMgr.h
@@ -144,14 +144,14 @@ class GameEventMgr
         void SaveWorldEventStateToDB(uint16 event_id);
         bool hasCreatureQuestActiveEventExcept(uint32 quest_id, uint16 event_id);
         bool hasGameObjectQuestActiveEventExcept(uint32 quest_id, uint16 event_id);
-        bool hasCreatureActiveEventExcept(uint32 creature_guid, uint16 event_id);
-        bool hasGameObjectActiveEventExcept(uint32 go_guid, uint16 event_id);
+        bool hasCreatureActiveEventExcept(ObjectGuid::LowType creature_guid, uint16 event_id);
+        bool hasGameObjectActiveEventExcept(ObjectGuid::LowType go_guid, uint16 event_id);
 
-        typedef std::list<uint32> GuidList;
+        typedef std::list<ObjectGuid::LowType> GuidList;
         typedef std::list<uint32> IdList;
         typedef std::vector<GuidList> GameEventGuidMap;
         typedef std::vector<IdList> GameEventIdMap;
-        typedef std::pair<uint32, ModelEquip> ModelEquipPair;
+        typedef std::pair<ObjectGuid::LowType, ModelEquip> ModelEquipPair;
         typedef std::list<ModelEquipPair> ModelEquipList;
         typedef std::vector<ModelEquipList> GameEventModelEquipMap;
         typedef std::pair<uint32, uint32> QuestRelation;
@@ -160,7 +160,7 @@ class GameEventMgr
         typedef std::list<NPCVendorEntry> NPCVendorList;
         typedef std::vector<NPCVendorList> GameEventNPCVendorMap;
         typedef std::map<uint32 /*quest id*/, GameEventQuestToEventConditionNum> QuestIdToEventConditionMap;
-        typedef std::pair<uint32 /*guid*/, uint32 /*npcflag*/> GuidNPCFlagPair;
+        typedef std::pair<ObjectGuid::LowType /*guid*/, uint32 /*npcflag*/> GuidNPCFlagPair;
         typedef std::list<GuidNPCFlagPair> NPCFlagList;
         typedef std::vector<NPCFlagList> GameEventNPCFlagMap;
         typedef std::vector<uint32> GameEventBitmask;

src/server/game/Mails/Mail.h
@@ -81,7 +81,7 @@ enum MailShowFlags
 class MailSender
 {
     public:                                                 // Constructors
-        MailSender(MailMessageType messageType, uint32 sender_guidlow_or_entry, MailStationery stationery = MAIL_STATIONERY_DEFAULT)
+        MailSender(MailMessageType messageType, ObjectGuid::LowType sender_guidlow_or_entry, MailStationery stationery = MAIL_STATIONERY_DEFAULT)
             : m_messageType(messageType), m_senderId(sender_guidlow_or_entry), m_stationery(stationery)
         {
         }
@@ -91,31 +91,31 @@ class MailSender
         MailSender(Player* sender);
     public:                                                 // Accessors
         MailMessageType GetMailMessageType() const { return m_messageType; }
-        uint32 GetSenderId() const { return m_senderId; }
+        ObjectGuid::LowType GetSenderId() const { return m_senderId; }
         MailStationery GetStationery() const { return m_stationery; }
     private:
         MailMessageType m_messageType;
-        uint32 m_senderId;                                  // player low guid or other object entry
+        ObjectGuid::LowType m_senderId;                                  // player low guid or other object entry
         MailStationery m_stationery;
 };
 
 class MailReceiver
 {
     public:                                                 // Constructors
-        explicit MailReceiver(uint32 receiver_lowguid) : m_receiver(NULL), m_receiver_lowguid(receiver_lowguid) { }
+        explicit MailReceiver(ObjectGuid::LowType receiver_lowguid) : m_receiver(NULL), m_receiver_lowguid(receiver_lowguid) { }
         MailReceiver(Player* receiver);
-        MailReceiver(Player* receiver, uint32 receiver_lowguid);
+        MailReceiver(Player* receiver, ObjectGuid::LowType receiver_lowguid);
     public:                                                 // Accessors
         Player* GetPlayer() const { return m_receiver; }
-        uint32  GetPlayerGUIDLow() const { return m_receiver_lowguid; }
+        ObjectGuid::LowType  GetPlayerGUIDLow() const { return m_receiver_lowguid; }
     private:
         Player* m_receiver;
-        uint32  m_receiver_lowguid;
+        ObjectGuid::LowType  m_receiver_lowguid;
 };
 
 class MailDraft
 {
-    typedef std::map<uint32, Item*> MailItemMap;
+    typedef std::map<ObjectGuid::LowType, Item*> MailItemMap;
 
     public:                                                 // Constructors
         explicit MailDraft(uint16 mailTemplateId, bool need_items = true)
@@ -136,7 +136,7 @@ class MailDraft
         MailDraft& AddCOD(uint32 COD) { m_COD = COD; return *this; }
 
     public:                                                 // finishers
-        void SendReturnToSender(uint32 sender_acc, uint32 sender_guid, uint32 receiver_guid, SQLTransaction& trans);
+        void SendReturnToSender(uint32 sender_acc, ObjectGuid::LowType sender_guid, ObjectGuid::LowType receiver_guid, SQLTransaction& trans);
         void SendMailTo(SQLTransaction& trans, MailReceiver const& receiver, MailSender const& sender, MailCheckMask checked = MAIL_CHECK_MASK_NONE, uint32 deliver_delay = 0);
 
     private:
@@ -156,7 +156,7 @@ class MailDraft
 
 struct MailItemInfo
 {
-    uint32 item_guid;
+    ObjectGuid::LowType item_guid;
     uint32 item_template;
 };
 typedef std::vector<MailItemInfo> MailItemInfoVec;
@@ -167,12 +167,12 @@ struct Mail
     uint8 messageType;
     uint8 stationery;
     uint16 mailTemplateId;
-    uint32 sender;
-    uint32 receiver;
+    ObjectGuid::LowType sender;
+    ObjectGuid::LowType receiver;
     std::string subject;
     std::string body;
     std::vector<MailItemInfo> items;
-    std::vector<uint32> removedItems;
+    std::vector<ObjectGuid::LowType> removedItems;
     time_t expire_time;
     time_t deliver_time;
     uint32 money;
@@ -180,7 +180,7 @@ struct Mail
     uint32 checked;
     MailState state;
 
-    void AddItem(uint32 itemGuidLow, uint32 item_template)
+    void AddItem(ObjectGuid::LowType itemGuidLow, uint32 item_template)
     {
         MailItemInfo mii;
         mii.item_guid = itemGuidLow;
@@ -188,7 +188,7 @@ struct Mail
         items.push_back(mii);
     }
 
-    bool RemoveItem(uint32 item_guid)
+    bool RemoveItem(ObjectGuid::LowType item_guid)
     {
         for (MailItemInfoVec::iterator itr = items.begin(); itr != items.end(); ++itr)
         {

src/server/game/Maps/TransportMgr.h
@@ -111,7 +111,7 @@ class TransportMgr
         void LoadTransportTemplates();
 
         // Creates a transport using given GameObject template entry
-        Transport* CreateTransport(uint32 entry, uint32 guid = 0, Map* map = NULL);
+        Transport* CreateTransport(uint32 entry, ObjectGuid::LowType guid = 0, Map* map = NULL);
 
         // Spawns all continent transports, used at core startup
         void SpawnContinentTransports();

src/server/game/Tools/PlayerDump.h
@@ -72,14 +72,14 @@ class PlayerDumpWriter : public PlayerDump
     public:
         PlayerDumpWriter() { }
 
-        bool GetDump(uint32 guid, std::string& dump);
-        DumpReturn WriteDump(std::string const& file, uint32 guid);
+        bool GetDump(ObjectGuid::LowType guid, std::string& dump);
+        DumpReturn WriteDump(std::string const& file, ObjectGuid::LowType guid);
     private:
-        typedef std::set<uint32> GUIDs;
+        typedef std::set<ObjectGuid::LowType> GUIDs;
 
-        bool DumpTable(std::string& dump, uint32 guid, char const*tableFrom, char const*tableTo, DumpTableType type);
+        bool DumpTable(std::string& dump, ObjectGuid::LowType guid, char const*tableFrom, char const*tableTo, DumpTableType type);
         std::string GenerateWhereStr(char const* field, GUIDs const& guids, GUIDs::const_iterator& itr);
-        std::string GenerateWhereStr(char const* field, uint32 guid);
+        std::string GenerateWhereStr(char const* field, ObjectGuid::LowType guid);
 
         GUIDs pets;
         GUIDs mails;
@@ -91,7 +91,7 @@ class PlayerDumpReader : public PlayerDump
     public:
         PlayerDumpReader() { }
 
-        DumpReturn LoadDump(std::string const& file, uint32 account, std::string name, uint32 guid);
+        DumpReturn LoadDump(std::string const& file, uint32 account, std::string name, ObjectGuid::LowType guid);
 };
 
 #endif

commit 3f29ce9e8f7c9c57eacf38493737b5a7bce39925

Core/Pch - Fix non PCH build.
Issue introduced in https://github.com/TrinityCore/TrinityCore/commit/b1032ed6203a5839906fab7137484b0ef8f0e58f

src/server/game/Entities/Player/SocialMgr.h
@@ -21,6 +21,7 @@
 
 #include "DatabaseEnv.h"
 #include "Common.h"
+#include "ObjectGuid.h"
 
 class SocialMgr;
 class PlayerSocial;

src/server/game/Events/GameEventMgr.h
@@ -20,6 +20,7 @@
 #define TRINITY_GAMEEVENT_MGR_H
 
 #include "Common.h"
+#include "ObjectGuid.h"
 #include "SharedDefines.h"
 #include "Define.h"
 

src/server/game/Mails/Mail.h
@@ -20,6 +20,7 @@
 #define TRINITY_MAIL_H
 
 #include "Common.h"
+#include "ObjectGuid.h"
 #include <map>
 
 struct AuctionEntry;

src/server/game/Maps/TransportMgr.h
@@ -21,6 +21,7 @@
 #include <G3D/Quat.h>
 #include "Spline.h"
 #include "DBCStores.h"
+#include "ObjectGuid.h"
 
 struct KeyFrame;
 struct GameObjectTemplate;

src/server/game/Tools/PlayerDump.h
@@ -22,6 +22,7 @@
 #include <string>
 #include <map>
 #include <set>
+#include "ObjectGuid.h"
 
 enum DumpTableType
 {