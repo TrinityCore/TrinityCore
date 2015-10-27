709c8d622cc0e0dfd50e5ddd6e2f6da8532cdbc4
ShinDarth <borzifrancesco@gmail.com>
2015-10-27 16:26:33 +0100

    [AZTH] Hackfix Sons of Hodir Vendor

--- azth/data/sql/content/world/azth_world_creature.sql
+++ azth/data/sql/content/world/azth_world_creature.sql
@@ -1,2 +1,7 @@
 -- Flame Revenant (30873)
 UPDATE `creature` SET `phaseMask` = 1 WHERE `id` = 30873;
+
+-- Hackfix Sons of Hodir Vendor
+DELETE FROM `creature` WHERE `guid` = 900014;
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `VerifiedBuild`) VALUES
+(900014, 32540, 571, 0, 0, 1, 1, 0, 1, 7294.19, -2874.07, 822.378, 1.52255, 300, 0, 0, 50400, 0, 0, 0, 0, 0, 0);
