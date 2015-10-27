d56f685fb117a393040efc904a232160739fb4df
Yehonal <hw.2@hotmail.it>
2015-10-27 01:13:40 +0100

    [AZTH] updated compiling scripts to support compilation type

--- azth/bin/1-configure.sh
+++ azth/bin/1-configure.sh
@@ -7,13 +7,14 @@ CWD=$(pwd)
 cd $BUILDPATH
 
 echo "Build path: $BUILDPATH"
-echo "DEBUG: $CDEBUG"
-# -DCMAKE_BUILD_TYPE=$DEBUG disable optimization "slow and huge amount of ram"
-# -DWITH_COREDEBUG=$DEBUG compiled with debug information
+echo "DEBUG info: $CDEBUG"
+echo "Compilation type: $CCTYPE"
+# -DCMAKE_BUILD_TYPE=$CCTYPE disable optimization "slow and huge amount of ram"
+# -DWITH_COREDEBUG=$CDEBUG compiled with debug information
 
 make clean
 find -iname '*cmake*' -not -name CMakeLists.txt -exec rm -rf {} \+
 cmake $SRCPATH -DCMAKE_INSTALL_PREFIX=$BINPATH -DCONF_DIR=$CONFDIR -DSERVERS=$CSERVERS -DSCRIPTS=$CSCRIPTS \
--DTOOLS=$CTOOLS -DUSE_SCRIPTPCH=$CSCRIPTPCH -DUSE_COREPCH=$CCOREPCH -DWITH_COREDEBUG=$CDEBUG -DWITH_WARNINGS=$CWARNINGS
+-DTOOLS=$CTOOLS -DUSE_SCRIPTPCH=$CSCRIPTPCH -DUSE_COREPCH=$CCOREPCH -DWITH_COREDEBUG=$CDEBUG  -DCMAKE_BUILD_TYPE=$CCTYPE -DWITH_WARNINGS=$CWARNINGS
 
 cd $CWD
--- azth/bin/defines.sh
+++ azth/bin/defines.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-source ./config.sh
+source ./config.sh.dist # "hack" to avoid missing conf variables
+
+source ./config.sh # should overwrite previous
+
+# you can choose build type from cmd argument
+if [ ! -z $1 ] 
+then 
+    CCTYPE=$1
+    CCTYPE=${CCTYPE^} # capitalize first letter if it's not yet
+fi
 
 BUILDPATH=$BINPATH
 
-(( $CDEBUG == 1 )) && BUILDPATH="$BUILDPATH/debug/build/" ||  BUILDPATH="$BUILDPATH/release/build/" 
+[ $CCTYPE == "Debug" ] && BUILDPATH="$BUILDPATH/debug/build/" ||  BUILDPATH="$BUILDPATH/release/build/" 
 
-(( $CDEBUG == 1  )) && BINPATH="$BINPATH/debug" || BINPATH="$BINPATH/release" 
+[ $CCTYPE == "Debug" ] && BINPATH="$BINPATH/debug" || BINPATH="$BINPATH/release" 
 
 mkdir -p $BUILDPATH
 mkdir -p $BINPATH
