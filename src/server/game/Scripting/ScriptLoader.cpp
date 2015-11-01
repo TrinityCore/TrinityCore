b02272445807cce8aee16bc7fcbe712f7689ca65
Incorrect <ovitnez@gmail.com>
2015-11-01 23:53:54 +0200

    Core/Misc: Remove unneeded files
    These 3 BlackRock Mountain scripts can be removed from source:
    
    - boss_anubshiah.cpp
    - boss_gorosh_the_dervish.cpp
    - boss_grizzle.cpp
    
    They already removed from the creatures in DB, and are currently causing
    DBErrors.
    
    (cherry picked from commit d98037a75dd8a3edc2d95051b68fb4db83fb9d7b)

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -113,11 +113,8 @@ void AddSC_blackrock_caverns();
 void AddSC_instance_blackrock_caverns();
 void AddSC_blackrock_depths();               //Blackrock Depths
 void AddSC_boss_ambassador_flamelash();
-void AddSC_boss_anubshiah();
 void AddSC_boss_draganthaurissan();
 void AddSC_boss_general_angerforge();
-void AddSC_boss_gorosh_the_dervish();
-void AddSC_boss_grizzle();
 void AddSC_boss_high_interrogator_gerstahn();
 void AddSC_boss_magmus();
 void AddSC_boss_moira_bronzebeard();
@@ -836,11 +833,8 @@ void AddEasternKingdomsScripts()
     AddSC_instance_blackrock_caverns();
     AddSC_blackrock_depths();               //Blackrock Depths
     AddSC_boss_ambassador_flamelash();
-    AddSC_boss_anubshiah();
     AddSC_boss_draganthaurissan();
     AddSC_boss_general_angerforge();
-    AddSC_boss_gorosh_the_dervish();
-    AddSC_boss_grizzle();
     AddSC_boss_high_interrogator_gerstahn();
     AddSC_boss_magmus();
     AddSC_boss_moira_bronzebeard();
