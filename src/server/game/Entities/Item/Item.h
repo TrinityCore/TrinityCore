c024fb364739669a00acb5c4489113903992e823
Carbenium <carbenium@outlook.com>
2015-11-03 17:48:01 +0100

    Merge pull request #15720 from Tonghost/3.3.5
    Core/Items: Fix a trading spoof exploit.
    (cherry picked from commit 4ae95154cfde75cc2b499aa0b776d5095a0789b7)
    
    Conflicts:
    	src/server/game/Entities/Item/Item.cpp

--- src/server/game/Entities/Item/Item.h
+++ src/server/game/Entities/Item/Item.h
@@ -318,7 +318,7 @@ class Item : public Object
         bool GemsFitSockets() const;
 
         uint32 GetCount() const { return GetUInt32Value(ITEM_FIELD_STACK_COUNT); }
-        void SetCount(uint32 value) { SetUInt32Value(ITEM_FIELD_STACK_COUNT, value); }
+        void SetCount(uint32 value);
         uint32 GetMaxStackCount() const { return GetTemplate()->GetMaxStackSize(); }
         uint8 GetGemCountWithID(uint32 GemID) const;
         uint8 GetGemCountWithLimitCategory(uint32 limitCategory) const;
