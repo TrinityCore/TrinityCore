271390753d63ea34e498e45f5e00a758f6997fbc
Carbenium <carbenium@outlook.com>
2015-11-08 01:36:49 +0100

    TDB 6.03 - 2015/11/08

--- revision_data.h.in.cmake
+++ revision_data.h.in.cmake
@@ -5,8 +5,8 @@
  #define _BRANCH                    "@rev_branch@"
  #define _SOURCE_DIRECTORY          "@CMAKE_SOURCE_DIR@"
  #define _MYSQL_EXECUTABLE          "@MYSQL_EXECUTABLE@"
- #define _FULL_DATABASE             "TDB_full_world_6.02_2015_07_14.sql"
- #define _HOTFIXES_DATABASE         "TDB_full_hotfixes_6.02_2015_07_14.sql"
+ #define _FULL_DATABASE             "TDB_full_world_6.03_2015_11_08.sql"
+ #define _HOTFIXES_DATABASE         "TDB_full_hotfixes_6.03_2015_11_08.sql"
  #define VER_COMPANYNAME_STR        "TrinityCore Developers"
  #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2015 TrinityCore"
  #define VER_FILEVERSION            0,0,0
