645e1d9671cae39e4e20b84974477f9cd6133fe4
ShinDarth <borzifrancesco@gmail.com>
2015-11-10 22:10:46 +0100

    [AZTH] Remove duplicate character SQL updates (we already had those changes, so they shouldn't be applied again)

--- sql/old/3.3.5a/characters/59_2015_11_07/2015_10_28_00_characters.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS `battleground_deserters`;
-CREATE TABLE `battleground_deserters` (
-	`guid` INT(10) UNSIGNED NOT NULL COMMENT 'characters.guid',
-	`type` TINYINT(3) UNSIGNED NOT NULL COMMENT 'type of the desertion',
-	`datetime` DATETIME NOT NULL COMMENT 'datetime of the desertion'
-);
