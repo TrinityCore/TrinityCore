27bb569368363546e3690474320e54266b6d8071
Gacko <gacko28@gmx.de>
2015-11-15 18:28:25 +0100

    Build/Clang: Suppress unused-local-typedefs warning.

--- cmake/compiler/clang/settings.cmake
+++ cmake/compiler/clang/settings.cmake
@@ -2,7 +2,7 @@
 add_definitions(-D_BUILD_DIRECTIVE='"${CMAKE_BUILD_TYPE}"')
 
 if(WITH_WARNINGS)
-  set(WARNING_FLAGS "-W -Wall -Wextra -Winit-self -Wfatal-errors -Wno-mismatched-tags")
+  set(WARNING_FLAGS "-W -Wall -Wextra -Winit-self -Wfatal-errors -Wno-mismatched-tags -Wno-unused-local-typedef")
   set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${WARNING_FLAGS}")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${WARNING_FLAGS} -Woverloaded-virtual")
   message(STATUS "Clang: All warnings enabled")
