59eee66d221026cff84c733c4d4e5e74e39be783
Helias <stefanoborzi32@gmail.com>
2015-11-23 00:05:09 +0100

    Revert "[AZTH]  Modified Sons of Hodir Quartermaster (Lillehoff) Phasemask to 1"
    This reverts commit ceb1a015be8f26f3e0d174e4f9493b930a7942b2.

--- azth/data/sql/content/world/azth_world_creature.sql
+++ azth/data/sql/content/world/azth_world_creature.sql
@@ -12,5 +12,7 @@ INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `p
 -- Flame Revenant (30873)
 UPDATE `creature` SET `phaseMask` = 1 WHERE `id` = 30873;
 
--- Lillehoff (32540)
-UPDATE `creature` SET `phaseMask` = 1 WHERE id = 32540;
+-- Hackfix Sons of Hodir Vendor
+DELETE FROM `creature` WHERE `guid` = 900014;
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `VerifiedBuild`) VALUES
+(900014, 32540, 571, 0, 0, 1, 1, 0, 1, 7294.19, -2874.07, 822.378, 1.52255, 300, 0, 0, 50400, 0, 0, 0, 0, 0, 0);
