f7afcac83e06b7f58197a23f719bff1be917e5bb
Helias <stefanoborzi32@gmail.com>
2015-11-22 23:16:24 +0100

    Modified Sons of Hodir Quartermaster (Lillehoff) Phasemask to 1

--- azth/data/sql/content/world/azth_world_creature.sql
+++ azth/data/sql/content/world/azth_world_creature.sql
@@ -11,3 +11,6 @@ INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `p
 
 -- Flame Revenant (30873)
 UPDATE `creature` SET `phaseMask` = 1 WHERE `id` = 30873;
+
+-- Lillehoff (32540)
+UPDATE `creature` SET `phaseMask` = 1 WHERE id = 32540;
