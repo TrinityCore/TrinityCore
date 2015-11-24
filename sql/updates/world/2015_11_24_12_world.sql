139682a526f4bf44aaa7d9b7b952e3544b08bb9e
Killyana <morphone1@gmail.com>
2015-11-24 17:25:31 +0100

    DB/quest: Fix credit quest for The Wakening

---
+++ sql/updates/world/2015_11_24_12_world.sql
@@ -0,0 +1,4 @@
+-- 
+UPDATE `smart_scripts` SET `action_type`=33, `action_param1`=38895 WHERE `entryorguid`=3889500 AND `source_type`=9 AND `id`=1;
+UPDATE `smart_scripts` SET `action_type`=33, `action_param1`=49230 WHERE `entryorguid`=4923000 AND `source_type`=9 AND `id`=1;
+UPDATE `smart_scripts` SET `action_type`=33, `action_param1`=49231 WHERE `entryorguid`=4923100 AND `source_type`=9 AND `id`=1;
