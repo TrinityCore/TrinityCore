0ff77b55b764311222a6800f5fceed7c5018c47f
F03SD <maratkoo@yandex.ru>
2015-11-24 10:33:36 +0300

    DB/Misc: Fix wrong npcflag's

---
+++ sql/updates/world/2015_11_24_00_world.sql
@@ -0,0 +1,18 @@
+-- Human Start Location 
+UPDATE `creature_template` SET `npcflag`=0 WHERE `entry`=49871; -- Blackrock Battle Worg
+UPDATE `creature_template` SET `npcflag`=0 WHERE entry=49874; -- Blackrock Spy
+UPDATE `creature_template` SET `npcflag`=0 WHERE entry=50039; -- Goblin Assassin
+-- Dwarf Start Location
+UPDATE `creature_template` SET `npcflag`=0 WHERE entry=37070; -- Rockjaw Invader
+UPDATE `creature_template` SET `npcflag`=0 WHERE entry=706; -- Frostmane Troll Whelp
+-- Gnome Start Location
+UPDATE `creature_template` SET `npcflag`=0 WHERE entry=46363; -- Crazed Leper Gnome
+-- Draenei Start Location
+UPDATE `creature_template` SET `npcflag`=0 WHERE entry=16516; -- Volatile Mutation
+-- Undead Start Location
+UPDATE `creature_template` SET `npcflag`=0 WHERE entry=1501; -- Mindless Zombie
+-- Troll Start Location
+UPDATE `creature_template` SET `npcflag`=0 WHERE entry=38038; -- Tiki Target
+-- Blood Elf Start Location
+UPDATE `creature_template` SET `npcflag`=0 WHERE entry=15274; -- Mana Wyrm
+UPDATE `creature_template` SET `npcflag`=0 WHERE entry=15271; -- Tender
