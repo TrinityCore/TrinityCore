3cd2301128b325d2ad98e3378a751171db58910d
Killyana <morphone1@gmail.com>
2015-11-24 17:46:44 +0100

    Fix a typo

--- sql/updates/world/2015_11_24_13_world.sql
+++ sql/updates/world/2015_11_24_13_world.sql
@@ -1,4 +1,4 @@
 -- 
 DELETE FROM `smart_scripts` WHERE `entryorguid`=2287 AND `source_type`=0 AND `id`=3;
-UPDATE `smart_scripts SET `link`=0 WHERE `entryorguid`=2287 AND `source_type`=0 AND `id`=2;
+UPDATE `smart_scripts` SET `link`=0 WHERE `entryorguid`=2287 AND `source_type`=0 AND `id`=2;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=15491 AND `source_type`=0;
