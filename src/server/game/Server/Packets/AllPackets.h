04058052d354933418b121a3c56983ba73db16a7
Shauren <shauren.trinity@gmail.com>
2015-12-12 00:25:27 +0100

    Core/Misc: Build time improvement

---
+++ src/server/game/Server/Packets/AllPackets.h
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2008-2015 TrinityCore <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef AllPackets_h__
+#define AllPackets_h__
+
+#include "AchievementPackets.h"
+#include "AuctionHousePackets.h"
+#include "AuthenticationPackets.h"
+#include "BankPackets.h"
+#include "BattlefieldPackets.h"
+#include "BattlegroundPackets.h"
+#include "BattlePetPackets.h"
+#include "BlackMarketPackets.h"
+#include "CalendarPackets.h"
+#include "ChannelPackets.h"
+#include "CharacterPackets.h"
+#include "ChatPackets.h"
+#include "ClientConfigPackets.h"
+#include "CombatLogPackets.h"
+#include "CombatPackets.h"
+#include "DuelPackets.h"
+#include "EquipmentSetPackets.h"
+#include "GameObjectPackets.h"
+#include "GarrisonPackets.h"
+#include "GuildFinderPackets.h"
+#include "GuildPackets.h"
+#include "InspectPackets.h"
+#include "InstancePackets.h"
+#include "ItemPackets.h"
+#include "LFGPackets.h"
+#include "LootPackets.h"
+#include "MailPackets.h"
+#include "MiscPackets.h"
+#include "MovementPackets.h"
+#include "NPCPackets.h"
+#include "PacketUtilities.h"
+#include "PartyPackets.h"
+#include "PetitionPackets.h"
+#include "QueryPackets.h"
+#include "QuestPackets.h"
+#include "ReferAFriendPackets.h"
+#include "ReputationPackets.h"
+#include "ScenePackets.h"
+#include "SocialPackets.h"
+#include "SpellPackets.h"
+#include "SystemPackets.h"
+#include "TalentPackets.h"
+#include "TaxiPackets.h"
+#include "TicketPackets.h"
+#include "TokenPackets.h"
+#include "TotemPackets.h"
+#include "ToyPackets.h"
+#include "TradePackets.h"
+#include "VehiclePackets.h"
+#include "VoidStoragePackets.h"
+#include "WhoPackets.h"
+#include "WorldStatePackets.h"
+
+#endif // AllPackets_h__
