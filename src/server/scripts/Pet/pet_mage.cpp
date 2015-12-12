54424aff3051bdc2a05d44cbd56adf6f47b98229
Vincent-Michael <vincent_michael@gmx.de>
2015-12-12 18:00:22 +0100

    Scripts/Pets: Fixed non pch build part 3 ...

--- src/server/scripts/Pet/pet_mage.cpp
+++ src/server/scripts/Pet/pet_mage.cpp
@@ -26,7 +26,9 @@
 #include "Pet.h"
 #include "PetAI.h"
 #include "Cell.h"
+#include "CellImpl.h"
 #include "GridNotifiers.h"
+#include "GridNotifiersImpl.h"
 
 enum MageSpells
 {
@@ -108,10 +110,10 @@ class npc_pet_mage_mirror_image : public CreatureScript
                 }
                 // Prioritize units with threat referenced to owner
                 if (highestThreat > 0.0f && highestThreatUnit)
-					me->Attack(highestThreatUnit, false);
+                    me->Attack(highestThreatUnit, false);
                 // If there is no such target, try to attack nearest hostile unit if such exists
                 else if (nearestPlayer)
-					me->Attack(nearestPlayer, false);
+                    me->Attack(nearestPlayer, false);
             }
 
             bool IsInThreatList(Unit* target)
