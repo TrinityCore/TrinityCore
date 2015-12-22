6a4d5ae22e159ea1c50ae490f69a9e01314896ea
ike3 <ike3@aloe.no-ip.biz>
2015-12-22 16:09:20 +0300

    [ai-0.36-130] move random more often

--- src/plugins/playerbot/strategy/generic/MoveRandomStrategy.cpp
+++ src/plugins/playerbot/strategy/generic/MoveRandomStrategy.cpp
@@ -7,7 +7,7 @@ using namespace ai;
 void MoveRandomStrategy::InitTriggers(std::list<TriggerNode*> &triggers)
 {
     triggers.push_back(new TriggerNode(
-        "random",
+        "often",
         NextAction::array(0, new NextAction("move random", 1.5f), NULL)));
 }
 
