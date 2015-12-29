5ab58d68f281edd696457151097ddf2d1cc951f4
Bernd Lörwald <bloerwald@googlemail.com>
2015-12-29 17:57:38 +0100

    fix WorldPackets::Misc::LoginSetTimeSpeed

--- src/server/game/Server/Packets/MiscPackets.cpp
+++ src/server/game/Server/Packets/MiscPackets.cpp
@@ -38,10 +38,10 @@ WorldPacket const* WorldPackets::Misc::InvalidatePlayer::Write()
 
 WorldPacket const* WorldPackets::Misc::LoginSetTimeSpeed::Write()
 {
-    _worldPacket.AppendPackedTime(ServerTime);
-    _worldPacket.AppendPackedTime(GameTime);
     _worldPacket << float(NewSpeed);
     _worldPacket << uint32(ServerTimeHolidayOffset);
+    _worldPacket.AppendPackedTime(GameTime);
+    _worldPacket.AppendPackedTime(ServerTime);
     _worldPacket << uint32(GameTimeHolidayOffset);
 
     return &_worldPacket;
