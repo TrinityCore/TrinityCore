5f4e0ac45e294f633a99095473283ee05929820e
ShinDarth <borzifrancesco@gmail.com>
2015-12-30 15:12:40 +0100

    [AZTH] Auctioneer Beardo friendly and immune

--- azth/data/sql/content/world/azth_world_creature_template.sql
+++ azth/data/sql/content/world/azth_world_creature_template.sql
@@ -1,2 +1,5 @@
 -- Strand of The Ancients demolisher fix
 UPDATE `creature_template` SET `mechanic_immune_mask` = 344407930 WHERE `entry` IN (28781, 32796);
+
+-- Auctioneer Beardo friendly and immune
+UPDATE `creature_template` SET `unit_flags` = 898, `faction` = 1774 WHERE `entry` = 8661;
