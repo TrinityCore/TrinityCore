37685efa3872464cdc8eed49e31a345b6cb59c72
Aokromes <aokromes@gmail.com>
2016-01-12 00:40:42 +0100

    DB/Misc: Fix some gameobject spawnmasks

---
+++ sql/updates/world/2016_01_12_00_world_434.sql
@@ -0,0 +1,13 @@
+--
+UPDATE `gameobject` SET `PhaseId`=169 WHERE `phaseMask`=1;
+UPDATE `gameobject` SET `PhaseId`=170 WHERE `phaseMask`=2;
+UPDATE `gameobject` SET `PhaseId`=171 WHERE `phaseMask`=4;
+UPDATE `gameobject` SET `PhaseId`=172 WHERE `phaseMask`=8;
+UPDATE `gameobject` SET `PhaseId`=173 WHERE `phaseMask`=16;
+UPDATE `gameobject` SET `PhaseId`=174 WHERE `phaseMask`=32;
+UPDATE `gameobject` SET `PhaseId`=175 WHERE `phaseMask`=64;
+UPDATE `gameobject` SET `PhaseId`=176 WHERE `phaseMask`=128;
+UPDATE `gameobject` SET `PhaseGroup`=371 WHERE `phaseMask`=3;
+UPDATE `gameobject` SET `PhaseGroup`=396 WHERE `phaseMask`=5;
+UPDATE `gameobject` SET `PhaseGroup`=368 WHERE `phaseMask`=7;
+UPDATE `gameobject` SET `PhaseGroup`=392 WHERE `phaseMask`=10;
