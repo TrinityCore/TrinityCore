faac17657d543978e2f2863ac298221fe3af9d09
ariel- <ariel.silva305@gmail.com>
2016-01-11 02:22:08 -0300

    Fix typo

--- src/server/game/Maps/Map.cpp
+++ src/server/game/Maps/Map.cpp
@@ -2649,7 +2649,7 @@ void Map::SendObjectUpdates()
     }
 
     WorldPacket packet;                                     // here we allocate a std::vector with a size of 0x10000
-    for (UpdateDataMapType::iterator iter = update_players.begin(); iter != update_players.end(); )
+    for (UpdateDataMapType::iterator iter = update_players.begin(); iter != update_players.end(); ++iter)
     {
         iter->second.BuildPacket(&packet);
         iter->first->SendDirectMessage(&packet);
