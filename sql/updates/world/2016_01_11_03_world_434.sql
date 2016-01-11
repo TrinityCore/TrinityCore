c8667700ceeba16dc3fb2f2e2c86bac627881021
ariel- <ariel.silva305@gmail.com>
2016-01-11 19:32:04 -0300

    Fix Cleave AP contribution

---
+++ sql/updates/world/2016_01_11_03_world_434.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_bonus_data` WHERE `entry`=845;
+INSERT INTO `spell_bonus_data` (`entry`,`direct_bonus`,`dot_bonus`,`ap_bonus`,`ap_dot_bonus`,`comments`) VALUES
+(845,0,0,0.45,0,'Warrior - Cleave');
