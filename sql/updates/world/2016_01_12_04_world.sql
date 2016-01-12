60e3127714f81748c8f2bc1d9774a71ecca786f2
Kittnz <frederik156@hotmail.com>
2016-01-12 22:21:17 +0100

    Quick little SQL update

--- sql/updates/world/2016_01_12_04_world.sql
+++ sql/updates/world/2016_01_12_04_world.sql
@@ -11,4 +11,4 @@ INSERT INTO `game_event_gameobject` (`eventEntry`, `guid`) VALUES
 (@Event, @OGUID+0);
 
 DELETE FROM `game_event_creature` WHERE `guid` BETWEEN @CGUID+0 AND @CGUID+4 AND `eventEntry`=@Event;
-INSERT INTO game_event_creature SELECT @Event, creature.guid FROM `creature` WHERE creature.guid BETWEEN @CGUID+0 AND @CGUID+4;
+INSERT INTO `game_event_creature` SELECT @Event, creature.guid FROM `creature` WHERE creature.guid BETWEEN @CGUID+0 AND @CGUID+4;
