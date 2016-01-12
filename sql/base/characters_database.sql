4316a967f862cf85962b6964495913b702410ab5
Aokromes <aokromes@gmail.com>
2016-01-12 01:13:39 +0100

    DB/Misc: typo fix

--- sql/base/characters_database.sql
+++ sql/base/characters_database.sql
@@ -1204,7 +1204,7 @@ CREATE TABLE `character_spell_cooldown` (
   `item` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Item Identifier',
   `time` int(10) unsigned NOT NULL DEFAULT '0',
   `categoryId` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Spell category Id',
-  `categoryEnd` int(10) unsigned NOT NULL DEFAULT '0';
+  `categoryEnd` int(10) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`guid`,`spell`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
