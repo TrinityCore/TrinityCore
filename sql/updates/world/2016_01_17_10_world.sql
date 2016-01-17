024114491af4b310f61faf2440eaa2638d739fe9
Aokromes <aokromes@gmail.com>
2016-01-17 11:09:56 +0100

    DB/misc: fix some startup errors

---
+++ sql/updates/world/2016_01_17_10_world.sql
@@ -0,0 +1,6 @@
+--
+DELETE FROM `game_event_gameobject` WHERE `guid` = 224988; 
+DELETE FROM `game_event_creature` WHERE `guid` BETWEEN 724 AND 727; 
+DELETE FROM `game_event_creature` WHERE `guid` BETWEEN 134972 AND 134975; 
+DELETE FROM `game_event_creature` WHERE `guid` BETWEEN 134978 AND 134987; 
+DELETE FROM `game_event_creature` WHERE `guid` BETWEEN 134990 AND 134993; 
