10b7490a330ceb7835614d3f43b56bd0feb3bb91
Killyana <morphone1@gmail.com>
2016-01-17 19:58:46 +0100

    DB/Object: Add missing object

---
+++ sql/updates/world/2016_01_17_14_world.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `gameobject_template` WHERE `entry`=208077;
+INSERT INTO gameobject_template (`entry`, `type`, `displayId`, `name`, `size`, `Data6`, `VerifiedBuild`) VALUES
+(208077, 5, 10418, "Web Wide", 1, -1, -18019);
