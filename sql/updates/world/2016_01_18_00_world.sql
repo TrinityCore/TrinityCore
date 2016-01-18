be57c9686310e6a444ec0240ffff9aea5f565ca5
ariel- <ariel.silva305@gmail.com>
2016-01-18 00:29:30 -0300

    DB/Achievements: fixed arena season achievements rewarded on login.
    Closes #20

---
+++ sql/updates/world/2016_01_18_00_world.sql
@@ -0,0 +1,17 @@
+DELETE FROM `achievement_criteria_data` WHERE `criteria_id` IN (18114,18115,18116,18117,18842,18843,18844,18845,18846,18847,18848,18849,18144,18488);
+INSERT INTO `achievement_criteria_data` (`criteria_id`,`type`,`value1`,`value2`,`ScriptName`) VALUES
+(18114,23,279,0,''), -- Gladiator - Vicious Gladiator
+(18115,23,279,0,''), -- Duelist - Vicious Gladiator
+(18116,23,279,0,''), -- Rival - Vicious Gladiator
+(18117,23,279,0,''), -- Challenger - Vicious Gladiator
+(18842,23,280,0,''), -- Challenger - Ruthless Gladiator
+(18843,23,280,0,''), -- Gladiator - Ruthless Gladiator
+(18844,23,281,0,''), -- Gladiator - Cataclysmic Gladiator
+(18845,23,280,0,''), -- Duelist - Ruthless Gladiator
+(18846,23,281,0,''), -- Duelist - Cataclysmic Gladiator
+(18847,23,280,0,''), -- Rival - Ruthless Gladiator
+(18848,23,281,0,''), -- Rival - Cataclysmic Gladiator
+(18849,23,281,0,''), -- Challenger - Cataclysmic Gladiator
+
+(18144,23,279,0,''), -- Vicious Gladiator Season 9
+(18488,23,280,0,''); -- Ruthless Gladiator Season 10
