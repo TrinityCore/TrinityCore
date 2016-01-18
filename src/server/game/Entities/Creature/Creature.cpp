6b13e3aa2551120f1a41c0a1ad47dac9f21232cd
treeston <treeston.mmoc@gmail.com>
2016-01-18 02:10:17 +0100

    Spell facing follow-up: Fix an issue with creatures' channeled spells not cancelling properly in certain conditions
    (cherry picked from commit ab9567e41a8aeed673d4e4998b05e90e107da512)

--- src/server/game/Entities/Creature/Creature.cpp
+++ src/server/game/Entities/Creature/Creature.cpp
@@ -2664,7 +2664,7 @@ bool Creature::FocusTarget(Spell const* focusSpell, WorldObject const* target)
                     }
                 }
             if (shouldDelay)
-                shouldDelay = (!focusSpell->IsTriggered() && !focusSpell->GetCastTime());
+                shouldDelay = !(focusSpell->IsTriggered() || focusSpell->GetCastTime() || focusSpell->GetSpellInfo()->IsChanneled());
 
         }
     }
