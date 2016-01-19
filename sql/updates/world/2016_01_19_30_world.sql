8838540556e3be7d9ec2bc7a9112ce09b4665eb7
Aokromes <aokromes@gmail.com>
2016-01-19 23:41:22 +0100

    DB/Misc: Declare heroic modes for ShadowFang Keep creatures
    By dragosgirip

---
+++ sql/updates/world/2016_01_19_30_world.sql
@@ -0,0 +1,79 @@
+--
+-- Corpse Eater
+UPDATE creature_template SET difficulty_entry_1=48851 WHERE entry=47134;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=16,speed_walk=0.8,speed_run=0.992063,unit_flags=32832 WHERE entry=48851;
+
+-- Frantic Geist
+UPDATE creature_template SET difficulty_entry_1=48842 WHERE entry=47131;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=16,speed_run=1.42857,BaseAttackTime=2000,unit_flags=32768 WHERE entry=48842;
+
+-- Shadowy Attendant
+UPDATE creature_template SET difficulty_entry_1=48973 WHERE entry=47231;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=16,unit_class=8,unit_flags=32832 WHERE entry=48973;
+
+-- Haunted Servitor
+UPDATE creature_template SET difficulty_entry_1=48942 WHERE entry=3875;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=24,unit_flags=32832,MovementType=1,DamageModifier=1.7,mechanic_immune_mask=8388624 WHERE entry=48942;
+
+-- Mindless Horror
+UPDATE creature_template SET difficulty_entry_1=48919 WHERE entry=47137;
+UPDATE creature_template SET minlevel=86,maxlevel=86,exp=3,faction=16,speed_run=1.42857,unit_flags=32832 WHERE entry=47137;
+
+UPDATE creature_template SET difficulty_entry_1=48981 WHERE entry=47232;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=16,unit_flags=32832 WHERE entry=48981;
+
+-- Wailing Guardsman
+UPDATE creature_template SET difficulty_entry_1=48985 WHERE entry=3877;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=24,unit_flags=32832,MovementType=1,DamageModifier=1.7,mechanic_immune_mask=8388624 WHERE entry=48985;
+
+-- Spitebone Skeleton
+UPDATE creature_template SET difficulty_entry_1=48988 WHERE entry=47143;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=16,unit_flags=32832 WHERE entry=48988;
+
+-- Tormented Officer
+UPDATE creature_template SET difficulty_entry_1=49059 WHERE entry=3873;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=16,unit_flags=32832,MovementType=1,DamageModifier=1.7,mechanic_immune_mask=8388624 WHERE entry=49059;
+
+-- Sorcerous Skeleton
+UPDATE creature_template SET difficulty_entry_1=49130 WHERE entry=47140;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=16,speed_run=1,unit_class=8,unit_flags=32832 WHERE entry=49130;
+
+-- Fetid Ghoul
+UPDATE creature_template SET difficulty_entry_1=49126 WHERE entry=47135;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=16,speed_walk=0.8,speed_run=0.992063,unit_flags=32832 WHERE entry=49126;
+
+-- Sleeper Stone
+UPDATE creature_template SET difficulty_entry_1=49160 WHERE entry=3870;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=16,unit_flags=33587264,DamageModifier=7.5,mechanic_immune_mask=8388624,flags_extra=2 WHERE entry=49160;
+
+-- Lesser Gargoyle
+UPDATE creature_template SET difficulty_entry_1=49155,exp=0 WHERE entry=3869;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=16,unit_flags=33587264,DamageModifier=7.5,mechanic_immune_mask=8388624,flags_extra=2 WHERE entry=49155;
+
+-- Fratic Geist
+UPDATE creature_template SET difficulty_entry_1=48842 WHERE entry=47131;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=16,speed_run=1.42857 WHERE entry=48842;
+
+-- Dark Creeper
+UPDATE creature_template SET difficulty_entry_1=49125 WHERE entry=47132;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=16,speed_run=1,unit_flags=32768 WHERE entry=49125;
+
+-- Unstable Ravager
+UPDATE creature_template SET difficulty_entry_1=49184 WHERE entry=47136;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=16,speed_walk=0.8,speed_run=0.992063,unit_flags=32768 WHERE entry=49125;
+
+-- Spitebone Guardian
+UPDATE creature_template SET difficulty_entry_1=49174 WHERE entry=47145;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=16,unit_flags=32768 WHERE entry=49174;
+
+-- Spitebone Flayer
+UPDATE creature_template SET difficulty_entry_1=49186 WHERE entry=47146;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=16,unit_flags=32832 WHERE entry=49186;
+
+-- Dread Scryer
+UPDATE creature_template SET difficulty_entry_1=49164 WHERE entry=47141;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=16,speed_run=1,unit_class=8,unit_flags=32832 WHERE entry=49164;
+
+-- Pustulant Monstrosity
+UPDATE creature_template SET difficulty_entry_1=49187 WHERE entry=47138;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=16,speed_run=1.42857,unit_flags=32832,movementId=144 WHERE entry=49187;
