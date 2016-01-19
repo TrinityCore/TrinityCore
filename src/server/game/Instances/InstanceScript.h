954323d9cb13d4ab3d478bcb51eeb157009d8853
DDuarte <dnpd.dd@gmail.com>
2016-01-19 13:01:43 +0000

    Core/Instances: Fix build in older VS versions
    Relax const uses in BossBoundaryEntry so the compiler can generate proper
    copy assignment operator (error C2582: 'operator =' function is unavailable in 'BossBoundaryEntry')
    
    Also renamed its members to match our coding standards
    (public members should be UpperCamelCase)

--- src/server/game/Instances/InstanceScript.h
+++ src/server/game/Instances/InstanceScript.h
@@ -75,9 +75,10 @@ struct DoorData
 
 struct BossBoundaryEntry
 {
-    uint32 const bossId;
-    AreaBoundary const* const boundary;
+    uint32 BossId;
+    AreaBoundary const* Boundary;
 };
+
 struct BossBoundaryData
 {
     typedef std::vector<BossBoundaryEntry> StorageType;
