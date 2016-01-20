afb8c16989e7c9f92c9e4a0dfe51821ea7b6102a
ariel- <ariel.silva305@gmail.com>
2016-01-20 02:03:01 -0300

    DB/Spells: Added spell proc entry for Meat Cleaver

---
+++ sql/updates/world/2016_01_20_06_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_proc_event` WHERE `entry`=-12329;
+INSERT INTO `spell_proc_event` (`entry`,`SchoolMask`,`SpellFamilyName`,`SpellFamilyMask0`,`SpellFamilyMask1`,`SpellFamilyMask2`,`procFlags`,`procEx`,`ppmRate`,`CustomChance`,`Cooldown`) VALUES
+(-12329,0,4,0x00400000,0x00000004,0x00000000,0,0,0,0,0);
