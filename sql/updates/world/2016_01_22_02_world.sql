c40c33e96cdced092d649511b82368767338085a
Aokromes <aokromes@gmail.com>
2016-01-22 16:27:03 +0100

    DB/Misc: Fix 3 startup errors

---
+++ sql/updates/world/2016_01_22_02_world.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_template` SET `npcflag`=`npcflag`|128 WHERE `entry`= 9988;
+UPDATE `npc_vendor` SET `incrtime`=7200 WHERE `entry`=50458 AND `item`=10938;
+UPDATE `npc_vendor` SET `incrtime`=7200 WHERE `entry`=50458 AND `item`=10940;
