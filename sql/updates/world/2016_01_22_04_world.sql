3dcb62e382d3d93b88b85ae23791a8dbb91124d6
Aokromes <aokromes@gmail.com>
2016-01-22 17:16:55 +0100

    DB/Misc: missed part

--- sql/updates/world/2016_01_22_04_world.sql
+++ sql/updates/world/2016_01_22_04_world.sql
@@ -1,3 +1,4 @@
 -- Tomb of the Lightbringer
 -- This quest was marked obsolete by Blizzard and cannot be obtained or completed.
 UPDATE `creature_template` SET `ScriptName`='' WHERE `entry`=17238;
+DELETE FROM `script_waypoint` WHERE `entry`=17238;
