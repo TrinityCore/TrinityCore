89fc81fa15a11aab36438238e043334eacda16d3
Aokromes <aokromes@gmail.com>
2016-01-22 17:01:04 +0100

    DB/Misc: remove one startup error

---
+++ sql/updates/world/2016_01_22_03_world.sql
@@ -0,0 +1,4 @@
+-- ScriptName 'spell_dk_bloodworms' exists in database, but no core script found!
+-- http://wowwiki.wikia.com/wiki/Bloodworms
+-- The subject of this article was removed from World of Warcraft in patch 4.0.1.
+DELETE FROM spell_script_names WHERE spell_id=-49027;
