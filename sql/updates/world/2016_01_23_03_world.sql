4313e31048954a0b1bab2b60dcf9b8f5f05551aa
Aokromes <aokromes@gmail.com>
2016-01-23 11:21:33 +0100

    DB/Misc: Fix some startup errors

---
+++ sql/updates/world/2016_01_23_03_world.sql
@@ -0,0 +1,6 @@
+--
+INSERT INTO `spell_loot_template` (`Entry`, `Item`, `MinCount`, `MaxCount`) VALUES
+(75183, 54464, 8, 11);
+
+INSERT INTO `spell_loot_template` (`Entry`, `Item`) VALUES
+(86615, 61987);
