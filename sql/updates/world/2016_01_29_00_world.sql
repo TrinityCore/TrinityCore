cc6f1f500e47b5e1b207e323af858a4579394e32
Aokromes <aokromes@gmail.com>
2016-01-29 17:34:56 +0100

    DB/Misc: forgot save

--- sql/updates/world/2016_01_29_00_world.sql
+++ sql/updates/world/2016_01_29_00_world.sql
@@ -1,7 +1,7 @@
 --
 SET @guid:=235190;
 DELETE FROM `gameobject` WHERE `guid` BETWEEN @guid+0 AND @guid+2;
-INSERT INTO `gameobject` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`position_x`,`position_y`,`position_z`,`orientation`,`rotation0`,`rotation1`,`rotation2`,`rotation3`,`spawntimesecs`,`animprogress`,`state`) VALUES
-(@guid+0, 207389, 329, 3, 1, '', 3457.544, -3106.003, 137.8965, 1.902409, 0, 0, 0, 1, 7200, 255, 1), -- 207389 (Area: 0)
-(@guid+1, 182187, 0, 41, 2837, 1, 1, -11369.6, -2163.95, 24.3, 0, 0, 0, 0, 0, 180, 0, 1, 0),
-(@guid+2, 182187, 0, 41, 2837, 1, 1, -11170.3, -1900.21, -17.78, 0, 0, 0, 0, 0, 180, 0, 1, 0);
+INSERT INTO `gameobject` (`guid`,`id`,`map`,`spawnMask`,`phaseId`,`position_x`,`position_y`,`position_z`,`orientation`,`rotation0`,`rotation1`,`rotation2`,`rotation3`,`spawntimesecs`,`animprogress`,`state`) VALUES
+(@guid+0, 207389, 329, 3, 169, 3457.544, -3106.003, 137.8965, 1.902409, 0, 0, 0, 1, 7200, 255, 1), -- 207389 (Area: 0)
+(@guid+1, 182187, 0, 1, 169, -11369.6, -2163.95, 24.3, 0, 0, 0, 0, 180, 0, 1, 0),
+(@guid+2, 182187, 0, 1, 169, -11170.3, -1900.21, -17.78, 0, 0, 0, 0, 180, 0, 1, 0);
