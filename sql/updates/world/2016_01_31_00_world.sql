404a5f639f678eab6af34eb7bda405742bd8cc31
Aokromes <aokromes@gmail.com>
2016-01-31 18:21:18 +0100

    DB/Misc: Fix some startup errors

---
+++ sql/updates/world/2016_01_31_00_world.sql
@@ -0,0 +1,4 @@
+--
+UPDATE `gameobject` SET `spawnMask`=1 WHERE `guid`=235190;  
+UPDATE `gameobject` SET `rotation3`=1 WHERE `guid`=235191;  
+UPDATE `gameobject` SET `rotation3`=1 WHERE `guid`=235192;  
