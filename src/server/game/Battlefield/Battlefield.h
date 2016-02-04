0fc728efb9491bdbbc7c964402d566ade47bce0c
Shauren <shauren.trinity@gmail.com>
2016-02-04 21:00:00 +0100

    Core/PacketIO: Updated and enabled SMSG_AREA_SPIRIT_HEALER_TIME

--- src/server/game/Battlefield/Battlefield.h
+++ src/server/game/Battlefield/Battlefield.h
@@ -334,7 +334,7 @@ class Battlefield : public ZoneScript
         /// Return if we can use mount in battlefield
         bool CanFlyIn() { return !m_isActive; }
 
-        void SendAreaSpiritHealerQueryOpcode(Player* player, ObjectGuid guid);
+        void SendAreaSpiritHealerQueryOpcode(Player* player, ObjectGuid const& guid);
 
         void StartBattle();
         void EndBattle(bool endByTimer);
