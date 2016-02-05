d1a185e3d5af9db2c85e04df846167ce9c4a4cf9
Vincent-Michael <vincent_michael@gmx.de>
2016-02-05 21:57:05 +0100

    Scripts: Added missing override

--- src/server/scripts/World/npcs_special.cpp
+++ src/server/scripts/World/npcs_special.cpp
@@ -2551,7 +2551,7 @@ class npc_train_wrecker : public CreatureScript
                 }
             }
 
-            void MovementInform(uint32 /*type*/, uint32 id)
+            void MovementInform(uint32 /*type*/, uint32 id) override
             {
                 if (id == MOVEID_CHASE)
                     _nextAction = EVENT_DO_JUMP;
