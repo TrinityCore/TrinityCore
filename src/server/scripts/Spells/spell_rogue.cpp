7672ca194552704eacf0fa49c7c28e91ade64399
ccrs <crs_92_19@hotmail.com>
2016-02-09 00:05:41 +0100

    Core/Spells: Rogue Honor Among Thieves
    (cherry picked from commit 983bbe4a4c28ee0eda19daa3ab8b713a99ffa00d)

--- src/server/scripts/Spells/spell_rogue.cpp
+++ src/server/scripts/Spells/spell_rogue.cpp
@@ -52,6 +52,8 @@ enum RogueSpells
     SPELL_ROGUE_TRICKS_OF_THE_TRADE_PROC            = 59628,
     SPELL_ROGUE_SERRATED_BLADES_R1                  = 14171,
     SPELL_ROGUE_RUPTURE                             = 1943,
+    SPELL_ROGUE_HONOR_AMONG_THIEVES                 = 51698,
+    SPELL_ROGUE_HONOR_AMONG_THIEVES_PROC            = 51699
 };
 
 enum RogueSpellIcons
@@ -912,6 +914,36 @@ public:
     }
 };
 
+// 51701 - Honor Among Thieves
+class spell_rog_honor_among_thieves : public SpellScriptLoader
+{
+public:
+    spell_rog_honor_among_thieves() : SpellScriptLoader("spell_rog_honor_among_thieves") { }
+
+    class spell_rog_honor_among_thieves_AuraScript : public AuraScript
+    {
+        PrepareAuraScript(spell_rog_honor_among_thieves_AuraScript);
+
+        void HandleProc(AuraEffect const* aurEff, ProcEventInfo& /*eventInfo*/)
+        {
+            PreventDefaultAction();
+
+            Unit* target = GetUnitOwner();
+            target->CastSpell(target, SPELL_ROGUE_HONOR_AMONG_THIEVES_PROC, TRIGGERED_FULL_MASK, nullptr, aurEff);
+        }
+
+        void Register() override
+        {
+            OnEffectProc += AuraEffectProcFn(spell_rog_honor_among_thieves_AuraScript::HandleProc, EFFECT_0, SPELL_AURA_DUMMY);
+        }
+    };
+
+    AuraScript* GetAuraScript() const override
+    {
+        return new spell_rog_honor_among_thieves_AuraScript();
+    }
+};
+
 void AddSC_rogue_spell_scripts()
 {
     new spell_rog_blade_flurry();
@@ -930,4 +962,5 @@ void AddSC_rogue_spell_scripts()
     new spell_rog_tricks_of_the_trade();
     new spell_rog_tricks_of_the_trade_proc();
     new spell_rog_serrated_blades();
+    new spell_rog_honor_among_thieves();
 }
