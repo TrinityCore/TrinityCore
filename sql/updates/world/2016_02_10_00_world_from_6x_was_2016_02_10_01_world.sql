a4192502415b633d473493257c8c34a70b0bc136
Aokromes <aokromes@gmail.com>
2016-02-10 21:26:43 +0100

    DB/Misc: 6.x branch fixes

---
+++ sql/updates/world/2016_02_10_00_world_from_6x_was_2016_02_10_01_world.sql
@@ -0,0 +1,5 @@
+--
+-- DB/Creature: Ironforge Gryphon Riders Hovering
+DELETE FROM `creature_template_addon` WHERE `entry`=51383;
+INSERT INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES
+(51383, 0, 24688, 50331648, 257, 0, ''); -- 51383
