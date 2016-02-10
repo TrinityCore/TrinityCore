7224dafad4b534f244a9c96b99d68df70a81b168
Rushor <PBienati@web.de>
2016-02-10 21:16:38 +0100

    DB/Quest: Have They No Shame - AllowableRaces
    * by Alastyr
    * closes #16510

---
+++ sql/updates/world/2016_02_10_04_world.sql
@@ -0,0 +1,3 @@
+-- 
+UPDATE `quest_template` SET `AllowableRaces` = 1 + 4 + 8 + 64 + 1024 + 2097152 + 16777216 WHERE id=13094;
+UPDATE `quest_template` SET `AllowableRaces` = 2 + 16 + 32 + 128 + 256 + 512 + 33554432 WHERE id=13095;
