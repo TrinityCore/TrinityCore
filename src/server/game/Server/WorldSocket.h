1ea80eb3f2cf576f6c6a54f8a3f744b533d999f2
Shauren <shauren.trinity@gmail.com>
2016-02-19 19:23:04 +0100

    Core/Networking: Rewrite networking threading model
    Each network thread has its own io_service - this means that all operations on a given socket except queueing packets run from a single thread, removing the need for locking
    Sending packets now writes to a lockfree intermediate queue directly, encryption is applied in network thread if it was required at the time of sending the packet

--- src/server/game/Server/WorldSocket.h
+++ src/server/game/Server/WorldSocket.h
@@ -26,11 +26,13 @@
 #include "Util.h"
 #include "WorldPacket.h"
 #include "WorldSession.h"
+#include "MPSCQueue.h"
 #include <chrono>
 #include <boost/asio/ip/tcp.hpp>
 
 using boost::asio::ip::tcp;
 struct z_stream_s;
+class EncryptablePacket;
 
 namespace WorldPackets
 {
@@ -111,7 +113,7 @@ private:
     void LogOpcodeText(OpcodeClient opcode, std::unique_lock<std::mutex> const& guard) const;
     /// sends and logs network.opcode without accessing WorldSession
     void SendPacketAndLogOpcode(WorldPacket const& packet);
-    void WritePacketToBuffer(WorldPacket const& packet, MessageBuffer& buffer);
+    void WritePacketToBuffer(EncryptablePacket const& packet, MessageBuffer& buffer);
     uint32 CompressPacket(uint8* buffer, WorldPacket const& packet);
 
     void HandleSendAuthSession();
@@ -142,12 +144,12 @@ private:
 
     MessageBuffer _headerBuffer;
     MessageBuffer _packetBuffer;
+    MPSCQueue<EncryptablePacket> _bufferQueue;
 
     z_stream_s* _compressionStream;
 
     bool _initialized;
 
-    std::mutex _queryLock;
     PreparedQueryResultFuture _queryFuture;
     std::function<void(PreparedQueryResult&&)> _queryCallback;
     std::string _ipCountry;
