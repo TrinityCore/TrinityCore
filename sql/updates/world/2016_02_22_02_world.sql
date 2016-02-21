c1dc9affcf5bf308f7e0440b711d754c8f5c5596
Aokromes <aokromes@gmail.com>
2016-02-21 22:33:57 +0100

    DB/Misc: Fix some startup errors

---
+++ sql/updates/world/2016_02_22_02_world.sql
@@ -0,0 +1,6 @@
+-- 
+UPDATE `spell_script_names` SET `spell_id`=33943 WHERE `spell_id`=-33943;
+DELETE FROM `smart_scripts` WHERE `entryorguid`=4427;
+DELETE FROM `smart_scripts` WHERE `entryorguid`=4523 AND `source_type`=0 AND `id`=1 AND `link`=0;
+DELETE FROM `smart_scripts` WHERE `entryorguid`=4525 AND `source_type`=0 AND `id`=1 AND `link`=0;
+DELETE FROM `smart_scripts` WHERE `entryorguid`=6168 AND `source_type`=0 AND `id`=1 AND `link`=0;
