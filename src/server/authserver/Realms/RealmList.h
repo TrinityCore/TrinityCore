e7b0092b11fc4aeaff1096272c667854e5e969e2
DDuarte <dnpd.dd@gmail.com>
2016-02-28 03:48:28 +0000

    Core: Backport 6.x realm changes
    Make acessible all the info about current realm (e.g name) anywhere, not only realm id
    Reduce the number of differences between the two branches
    
    Original changes by Shauren
    
    Partial port of bacc90b6baa34e6a194c93e5a7860d4041f08af7 and 63def8aa3291d0a6e5f83b289ad12c4c8a3cebd9

--- src/server/authserver/Realms/RealmList.h
+++ src/server/authserver/Realms/RealmList.h
@@ -19,48 +19,19 @@
 #ifndef _REALMLIST_H
 #define _REALMLIST_H
 
+#include "Common.h"
+#include "Realm/Realm.h"
 #include <boost/asio/ip/address.hpp>
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/io_service.hpp>
-#include "Common.h"
 
 using namespace boost::asio;
-
-enum RealmFlags
-{
-    REALM_FLAG_NONE = 0x00,
-    REALM_FLAG_INVALID = 0x01,
-    REALM_FLAG_OFFLINE = 0x02,
-    REALM_FLAG_SPECIFYBUILD = 0x04,
-    REALM_FLAG_UNK1 = 0x08,
-    REALM_FLAG_UNK2 = 0x10,
-    REALM_FLAG_RECOMMENDED = 0x20,
-    REALM_FLAG_NEW = 0x40,
-    REALM_FLAG_FULL = 0x80
-};
-
-// Storage object for a realm
-struct Realm
-{
-    ip::address ExternalAddress;
-    ip::address LocalAddress;
-    ip::address LocalSubnetMask;
-    uint16 port;
-    std::string name;
-    uint8 icon;
-    RealmFlags flag;
-    uint8 timezone;
-    uint32 m_ID;
-    AccountTypes allowedSecurityLevel;
-    float populationLevel;
-    uint32 gamebuild;
-};
 
 /// Storage object for the list of realms on the server
 class RealmList
 {
 public:
-    typedef std::map<std::string, Realm> RealmMap;
+    typedef std::map<RealmHandle, Realm> RealmMap;
 
     static RealmList* instance()
     {
@@ -74,22 +45,21 @@ public:
 
     void UpdateIfNeed();
 
-    void AddRealm(const Realm& NewRealm) { m_realms[NewRealm.name] = NewRealm; }
+    void AddRealm(const Realm& NewRealm) { _realms[NewRealm.Id] = NewRealm; }
 
-    RealmMap::const_iterator begin() const { return m_realms.begin(); }
-    RealmMap::const_iterator end() const { return m_realms.end(); }
-    uint32 size() const { return m_realms.size(); }
+    RealmMap const& GetRealms() const { return _realms; }
+    Realm const* GetRealm(RealmHandle const& id) const;
 
 private:
     RealmList();
 
     void UpdateRealms(bool init = false);
-    void UpdateRealm(uint32 id, const std::string& name, ip::address const& address, ip::address const& localAddr,
-        ip::address const& localSubmask, uint16 port, uint8 icon, RealmFlags flag, uint8 timezone, AccountTypes allowedSecurityLevel, float population, uint32 build);
+    void UpdateRealm(RealmHandle const& id, uint32 build, const std::string& name, ip::address const& address, ip::address const& localAddr,
+        ip::address const& localSubmask, uint16 port, uint8 icon, RealmFlags flag, uint8 timezone, AccountTypes allowedSecurityLevel, float population);
 
-    RealmMap m_realms;
-    uint32   m_UpdateInterval;
-    time_t   m_NextUpdateTime;
+    RealmMap _realms;
+    uint32   _updateInterval;
+    time_t   _nextUpdateTime;
     boost::asio::ip::tcp::resolver* _resolver;
 };
 
