dfe49d7d2404d754856c14524e24ca18e4223c33
MitchesD <majklprofik@seznam.cz>
2016-03-03 23:16:59 +0100

    Merge pull request #16523 from tkrokli/smart_action_set_corpse_delay
    [3.3.5][6.x]Core/SmartScript: create SMART_ACTION_SET_CORPSE_DELAY
    (cherry picked from commit 68b53895cc563726a58ae74c06cb7be609319c83)

--- src/server/game/AI/SmartScripts/SmartScriptMgr.cpp
+++ src/server/game/AI/SmartScripts/SmartScriptMgr.cpp
@@ -1263,6 +1263,7 @@ bool SmartAIMgr::IsEventValid(SmartScriptHolder& e)
         case SMART_ACTION_REMOVE_GO_FLAG:
         case SMART_ACTION_SUMMON_CREATURE_GROUP:
         case SMART_ACTION_RISE_UP:
+        case SMART_ACTION_SET_CORPSE_DELAY:
             break;
         default:
             TC_LOG_ERROR("sql.sql", "SmartAIMgr: Not handled action_type(%u), event_type(%u), Entry " SI64FMTD " SourceType %u Event %u, skipped.", e.GetActionType(), e.GetEventType(), e.entryOrGuid, e.GetScriptType(), e.event_id);
