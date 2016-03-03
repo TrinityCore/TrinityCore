ba3f439bcb7523776c8c064333d1c3aada46d59c
Naios <naios-dev@live.de>
2016-03-03 01:27:02 +0100

    Core/Database: Close the databases correctly when the DBUpdater fails * Also fixes a memory leak spotted by Aokromes
    (cherry picked from commit 7d3cffc297b6a1e24faf64a19b5167609ad8abbf)

--- src/server/database/Database/DatabaseLoader.h
+++ src/server/database/Database/DatabaseLoader.h
@@ -21,8 +21,9 @@
 #include "DatabaseWorkerPool.h"
 #include "DatabaseEnv.h"
 
-#include <stack>
 #include <functional>
+#include <stack>
+#include <queue>
 
 // A helper class to initiate all database worker pools,
 // handles updating, delays preparing of statements and cleans up on failure.
@@ -57,16 +58,18 @@ private:
     bool PrepareStatements();
 
     using Predicate = std::function<bool()>;
+    using Closer = std::function<void()>;
 
-    static bool Process(std::stack<Predicate>& stack);
+    // Invokes all functions in the given queue and closes the databases on errors.
+    // Returns false when there was an error.
+    bool Process(std::queue<Predicate>& queue);
 
     std::string const _logger;
     bool const _autoSetup;
     uint32 const _updateFlags;
 
-    std::stack<std::pair<Predicate, std::function<void()>>> _open;
-    std::stack<std::function<void()>> _close;
-    std::stack<Predicate> _populate, _update, _prepare;
+    std::queue<Predicate> _open, _populate, _update, _prepare;
+    std::stack<Closer> _close;
 };
 
 #endif // DatabaseLoader_h__
