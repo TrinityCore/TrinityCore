4f7948af31a3b58b7d3ab4b2640ce6a9fc1646c9
Aokromes <aokromes@gmail.com>
2016-03-04 08:13:25 +0100

    DB/Misc: Fix startup errors

---
+++ sql/updates/world/2016_03_04_05_world.sql
@@ -0,0 +1,4 @@
+--
+UPDATE `conditions` SET `ConditionTypeOrReference`=42 WHERE `SourceTypeOrReferenceId`=15 AND `SourceGroup`=3502 AND `SourceEntry`=0 AND `SourceId`=0 AND `ElseGroup`=1 AND `ConditionTypeOrReference`=4 AND `ConditionTarget`=0 AND `ConditionValue1`=1 AND `ConditionValue2`=1 AND `ConditionValue3`=0;
+UPDATE `smart_scripts` SET `action_param1`=175 WHERE `entryorguid`=31428 AND `source_type`=0 AND `id`=6 AND `link`=7;
+UPDATE `smart_scripts` SET `action_param1`=175 WHERE `entryorguid`=31432 AND `source_type`=0 AND `id`=0 AND `link`=1;
