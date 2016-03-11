7ed12cfcce8964f2e1145287cc83f99fcbeb3694
DevQuad <mr.quadcore@mail.ru>
2016-03-11 19:38:06 +0400

    Update

--- sql/updates/world/2016_03_01_NPC_THALORIEN_DAWNSEEKER_world.sql
+++ sql/updates/world/2016_03_01_NPC_THALORIEN_DAWNSEEKER_world.sql
@@ -11,6 +11,12 @@ INSERT INTO `gossip_menu_option` (`menu_id`,`id`,`option_icon`,`option_text`,`Op
 
 UPDATE `creature_template` SET `ScriptName`="npc_thalorien_dawnseeker" WHERE `entry`=@ENTRY;
 
+-- Phase
+DELETE FROM `spell_area` WHERE `spell`=70193;
+INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_end`, `aura_spell`, `racemask`, `gender`, `autocast`, `quest_start_status`, `quest_end_status`) VALUES 
+('70193', '4075', '24535', '0', '0', '0', '2', '1', '8', '11'),
+('70193', '4075', '24563', '0', '0', '0', '2', '1', '8', '11');
+
 -- Thalorien Dawnseeker SAI
 SET @ENTRY := 37205;
 UPDATE `creature_template` SET `faction`=250 WHERE `entry`=@ENTRY;
