205556b22670f136ff21aa5edbddef4d705e5709
Naios <naios-dev@live.de>
2016-03-11 15:57:26 +0100

    Core/Database: Converted the database library to a shared library. * There is still the possibility to static link against database.
    (cherry picked from commit 6cfea2d8f7de3de8206a1f4cf23a6d653814afe1)

--- src/server/database/Database/PreparedStatement.h
+++ src/server/database/Database/PreparedStatement.h
@@ -70,7 +70,7 @@ struct PreparedStatementData
 class MySQLPreparedStatement;
 
 //- Upper-level class that is used in code
-class PreparedStatement
+class TC_DATABASE_API PreparedStatement
 {
     friend class PreparedStatementTask;
     friend class MySQLPreparedStatement;
@@ -109,7 +109,7 @@ class PreparedStatement
 //- Class of which the instances are unique per MySQLConnection
 //- access to these class objects is only done when a prepared statement task
 //- is executed.
-class MySQLPreparedStatement
+class TC_DATABASE_API MySQLPreparedStatement
 {
     friend class MySQLConnection;
     friend class PreparedStatement;
@@ -157,7 +157,7 @@ typedef std::future<PreparedQueryResult> PreparedQueryResultFuture;
 typedef std::promise<PreparedQueryResult> PreparedQueryResultPromise;
 
 //- Lower-level class, enqueuable operation
-class PreparedStatementTask : public SQLOperation
+class TC_DATABASE_API PreparedStatementTask : public SQLOperation
 {
     public:
         PreparedStatementTask(PreparedStatement* stmt, bool async = false);
