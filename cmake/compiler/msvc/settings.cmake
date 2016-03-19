f4e0945b13a70225684e7421d9542efae6a47c89
Naios <naios-dev@live.de>
2016-03-20 00:50:21 +0100

    Core/Build: Add the possibility to link libraries dynamically. * makes it possible to access exported singletons from other shared lib's. * reduces binary size

--- cmake/compiler/msvc/settings.cmake
+++ cmake/compiler/msvc/settings.cmake
@@ -75,6 +75,13 @@ if(NOT WITH_WARNINGS)
   message(STATUS "MSVC: Disabled generic compiletime warnings")
 endif()
 
+if (WITH_DYNAMIC_LINKING)
+  # C4251: needs to have dll-interface to be used by clients of class '...'
+  # C4275: non dll-interface class ...' used as base for dll-interface class '...'
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4251 /wd4275")
+  message(STATUS "MSVC: Enabled shared linking")
+endif()
+
 # Specify the maximum PreCompiled Header memory allocation limit
 # Fixes a compiler-problem when using PCH - the /Ym flag is adjusted by the compiler in MSVC2012, hence we need to set an upper limit with /Zm to avoid discrepancies)
 # (And yes, this is a verified , unresolved bug with MSVC... *sigh*)
