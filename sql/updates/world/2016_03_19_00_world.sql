a910718d845adc2063d1ee705ba7ceb9b0f75533
DevQuad <mr.quadcore@mail.ru>
2016-03-20 14:00:12 +0400

    Update 2016_03_19_00_world.sql

--- sql/updates/world/2016_03_19_00_world.sql
+++ sql/updates/world/2016_03_19_00_world.sql
@@ -1,3 +1,3 @@
 -- Blinding Light
-DELETE FROM `spell_script_names` WHERE `spell_id` = 115750;
-INSERT INTO spell_script_names VALUE (115750, 'spell_pal_blinding_light');
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_pal_blinding_light';
+INSERT INTO `spell_script_names` VALUE (115750, 'spell_pal_blinding_light');
