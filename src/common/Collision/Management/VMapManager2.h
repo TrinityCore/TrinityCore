1a1c0d1dc4b5476bd438585e8b16466101ad509a
Naios <naios-dev@live.de>
2016-03-22 19:58:32 +0100

    Core/Common: Converted the common library to a shared library. * There is still the possibility to static link against common.
    (cherry picked from commit 501fd9b50232ca3ae3623877c3e521c9c1f894be)

--- src/common/Collision/Management/VMapManager2.h
+++ src/common/Collision/Management/VMapManager2.h
@@ -51,7 +51,7 @@ namespace VMAP
     class StaticMapTree;
     class WorldModel;
 
-    class ManagedModel
+    class TC_COMMON_API ManagedModel
     {
         public:
             ManagedModel() : iModel(nullptr), iRefCount(0) { }
@@ -75,7 +75,7 @@ namespace VMAP
         VMAP_DISABLE_LIQUIDSTATUS   = 0x8
     };
 
-    class VMapManager2 : public IVMapManager
+    class TC_COMMON_API VMapManager2 : public IVMapManager
     {
         protected:
             // Tree to check collision
