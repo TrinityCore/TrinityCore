09da8c2cbd1e212c6ab9d13533f75d7b4d450976
DDuarte <dnpd.dd@gmail.com>
2016-03-28 02:02:29 +0100

    Core/Network: Change SocketMgr.StartNetwork to take the thread count by arg instead of config
    AuthSocketMgr doesn't need "Network.Threads" config
    
    Fixes "Missing name Network.Threads in config file authserver.conf"
    
    Ref #16859
    
    (cherry picked from commit 4307f533be5782fe79c59e5365a1a8506ef1dde0)
    
    # Conflicts:
    #	src/server/authserver/Server/AuthSocketMgr.h
    #	src/server/game/Server/WorldSocketMgr.cpp

--- src/server/game/Server/WorldSocketMgr.cpp
+++ src/server/game/Server/WorldSocketMgr.cpp
@@ -58,7 +58,7 @@ WorldSocketMgr& WorldSocketMgr::Instance()
     return instance;
 }
 
-bool WorldSocketMgr::StartNetwork(boost::asio::io_service& service, std::string const& bindIp, uint16 port)
+bool WorldSocketMgr::StartNetwork(boost::asio::io_service& service, std::string const& bindIp, uint16 port, int threadCount)
 {
     _tcpNoDelay = sConfigMgr->GetBoolDefault("Network.TcpNodelay", true);
 
@@ -76,7 +76,7 @@ bool WorldSocketMgr::StartNetwork(boost::asio::io_service& service, std::string 
         return false;
     }
 
-    BaseSocketMgr::StartNetwork(service, bindIp, port);
+    BaseSocketMgr::StartNetwork(service, bindIp, port, threadCount);
     _instanceAcceptor = new AsyncAcceptor(service, bindIp, uint16(sWorld->getIntConfig(CONFIG_PORT_INSTANCE)));
 
     _acceptor->SetSocketFactory(std::bind(&BaseSocketMgr::GetSocketForAccept, this));
