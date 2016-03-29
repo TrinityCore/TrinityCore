77ad11e09378e0015daa0330860148eca0a990e3
Aokromes <aokromes@gmail.com>
2016-03-29 06:06:59 +0200

    DB/Misc: 3.3.5a fixes

---
+++ sql/updates/world/2016_03_29_01_world_from_335_was_2016_03_29_00_world_335.sql
@@ -0,0 +1,11 @@
+--
+UPDATE `game_event` SET `start_time`='2016-06-21 00:01:00' WHERE `eventEntry`=1; -- midsummer
+UPDATE `game_event` SET `start_time`='2016-12-15 06:00:00' WHERE `eventEntry`=2; -- winter
+UPDATE `game_event` SET `start_time`='2016-03-27 00:01:00' WHERE `eventEntry`=9; -- noblegarden
+UPDATE `game_event` SET `start_time`='2016-05-02 00:01:00' WHERE `eventEntry`=10; -- childen
+UPDATE `game_event` SET `start_time`='2016-09-09 00:01:00' WHERE `eventEntry`=11; -- harvest
+UPDATE `game_event` SET `start_time`='2016-10-18 01:00:00' WHERE `eventEntry`=12; -- hallow
+UPDATE `game_event` SET `start_time`='2016-09-20 00:01:00' WHERE `eventEntry`=24; -- brewfest
+UPDATE `game_event` SET `start_time`='2016-11-21 01:00:00' WHERE `eventEntry`=26; -- pilgrim
+UPDATE `game_event` SET `start_time`='2016-09-19 00:01:00' WHERE `eventEntry`=50; -- pirate
+UPDATE `game_event` SET `start_time`='2016-11-01 01:00:00' WHERE `eventEntry`=51; -- day
