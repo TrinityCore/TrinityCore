ddd022217bfedba11620de9b405423b157b27b6e
edea <efectokoleos@gmail.com>
2016-04-01 22:47:11 +0200

    Rename sql file and use the scriptname on delete query.

--- sql/updates/world/9999_99_99_00_world.sql
+++ sql/updates/world/9999_99_99_00_world.sql
@@ -1,3 +1,4 @@
 DELETE FROM `spell_script_names` WHERE `ScriptName`= 'spell_gen_pvp_trinket';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (42292, 'spell_gen_pvp_trinket');
+
