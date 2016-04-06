1af5ab6294f99135dea9b70598d2f86597b7cde6
Killyana <morphone1@gmail.com>
2016-04-06 04:01:18 +0200

    DB/Object: Fix more gameobjects

---
+++ sql/updates/world/2016_04_06_03_world.sql
@@ -0,0 +1,4 @@
+-- 
+UPDATE `gameobject_template` SET `data0`=0 WHERE `entry` IN (202357) AND `data0`=17334;
+UPDATE `gameobject_template` SET `data0`=1691 WHERE `entry` IN (209276) AND `data0`=2112;
+UPDATE `gameobject_template` SET `data0`=43 WHERE `entry` IN (73) AND `data0`=7;
