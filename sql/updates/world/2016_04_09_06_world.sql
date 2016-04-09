7f9d668f141d6c37a20211944991531f98a24c96
Aokromes <aokromes@gmail.com>
2016-04-09 21:56:53 +0200

    DB/Misc: Typo fix

---
+++ sql/updates/world/2016_04_09_06_world.sql
@@ -0,0 +1,5 @@
+--
+UPDATE `creature` SET `spawnMask`=1 WHERE `map`=732;
+UPDATE `gameobject` SET `spawnMask`=1 WHERE `map`=732;
+UPDATE `creature` SET `spawnMask`=1 WHERE `map`=757;
+UPDATE `gameobject` SET `spawnMask`=1 WHERE `map`=757;
