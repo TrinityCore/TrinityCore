5529e3e4b9ca97c38cbe743286aea2348d79beb8
joschiwald <joschiwald.trinity@gmail.com>
2016-04-13 20:47:57 +0200

    Core/Movement: enable item use and spell cast when dismounting
    Remove error message and allow spell cast or item use from
    mounted state on ground, especially when sitting on
    a landed flying mount. Current behavior is that the player
    will get the error message "You can't do that while moving"
    when trying to cast a spell or pick up an item
    while sitting on a flying mount after having landed.
    
    By joschiwald, closes #6236

--- src/server/game/Movement/MotionMaster.cpp
+++ src/server/game/Movement/MotionMaster.cpp
@@ -479,11 +479,12 @@ void MotionMaster::MoveFall(uint32 id /*=0*/)
     if (std::fabs(_owner->GetPositionZ() - tz) < 0.1f)
         return;
 
+    _owner->AddUnitMovementFlag(MOVEMENTFLAG_FALLING);
+    _owner->m_movementInfo.SetFallTime(0);
+
+    // don't run spline movement for players
     if (_owner->GetTypeId() == TYPEID_PLAYER)
-    {
-        _owner->AddUnitMovementFlag(MOVEMENTFLAG_FALLING);
-        _owner->m_movementInfo.SetFallTime(0);
-    }
+        return;
 
     Movement::MoveSplineInit init(_owner);
     init.MoveTo(_owner->GetPositionX(), _owner->GetPositionY(), tz, false);
