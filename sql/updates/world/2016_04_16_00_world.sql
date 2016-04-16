4418020fcedd8b833ea89412a89f47c9d4f7440b
Lopfest <lopfest@gmail.com>
2016-04-16 15:06:43 +0200

    DB/Spell: forgot to add spell_script_name

--- sql/updates/world/2016_04_16_00_world.sql
+++ sql/updates/world/2016_04_16_00_world.sql
@@ -2,3 +2,7 @@
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=13 AND `SourceEntry`=114710;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (13, 3, 114710, 0, 0, 31, 0, 4, 0, 0, 0, 0, 0, '', 'Forcecast Summon Amberleaf Troublemaker target Player');
+
+DELETE FROM `spell_script_names` WHERE `ScriptName`="spell_summon_troublemaker";
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(114698, 'spell_summon_troublemaker');
