f938c506f0d3977e5807f6ab75eee7785a2d64a8
Infamous-devel <infamous.devel@gmail.com>
2016-04-21 14:10:22 +0200

    Added Script Loader

---
+++ src/server/scripts/Pandaria/pandaria_script_loader.cpp
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Gate of the Setting Sun */
+void AddSC_instance_gate_setting_sun();
+void AddSC_gate_setting_sun();
+void AddSC_boss_commander_rimok();
+void AddSC_boss_raigonn();
+void AddSC_boss_saboteur_kiptilak();
+void AddSC_boss_striker_gadok();
+
+
+void AddPandariaScripts()
+{
+    AddSC_instance_gate_setting_sun();
+    AddSC_gate_setting_sun();
+    AddSC_boss_commander_rimok();
+    AddSC_boss_raigonn();
+    AddSC_boss_saboteur_kiptilak();
+    AddSC_boss_striker_gadok();
+}
