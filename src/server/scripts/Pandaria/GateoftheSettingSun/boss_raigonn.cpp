b04e0581e22ce7b105eb242bd3e0503e3337eb85
Infamous-devel <infamous.devel@gmail.com>
2016-04-21 14:01:28 +0200

    Added Copyright

--- src/server/scripts/Pandaria/GateoftheSettingSun/boss_raigonn.cpp
+++ src/server/scripts/Pandaria/GateoftheSettingSun/boss_raigonn.cpp
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2008-2012 TrinityCore <http://www.trinitycore.org/>
- * Copyright (C) 2006-2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ * Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -15,7 +14,6 @@
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 #include "ScriptMgr.h"
 #include "ScriptedCreature.h"
 #include "gate_setting_sun.h"
--- src/server/scripts/Pandaria/GateoftheSettingSun/gate_setting_sun.h
+++ src/server/scripts/Pandaria/GateoftheSettingSun/gate_setting_sun.h
@@ -1,7 +1,19 @@
 /*
-    Dungeon : Gate of the Setting Sun 90-90
-    Instance General Script
-*/
+ * Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef GATE_SETTING_SUN_H_
 #define GATE_SETTING_SUN_H_
