93bd30d9e4ad214735c0bbe5bdd8429755a05e73
Girip Dragos <dragosgirip@yahoo.com>
2016-04-23 22:37:08 +0000

    Diff

--- src/server/scripts/EasternKingdoms/ZulAman/boss_nalorakk.cpp
+++ src/server/scripts/EasternKingdoms/ZulAman/boss_nalorakk.cpp
@@ -140,6 +140,7 @@ class boss_nalorakk : public CreatureScript
                 {
                     me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE);
                     me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
+                    me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_IMMUNE_TO_PC);
                     inMove = false;
                     waitTimer = 0;
                     me->SetSpeed(MOVE_RUN, 2);
@@ -253,7 +254,7 @@ class boss_nalorakk : public CreatureScript
 
                                         me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE);
                                         me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
-
+                                        me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_IMMUNE_TO_PC);
                                         MoveEvent = false;
                                     }
                                     break;
