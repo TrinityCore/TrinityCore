827259ad42cfc1a896986e0156b3219d82923872
Aokromes <aokromes@gmail.com>
2016-04-25 07:00:20 +0200

    DB/Misc: 3.3.5a fixes

---
+++ sql/updates/world/2016_04_25_00_world_from_335_was_2016_04_24_00_world_335.sql
@@ -0,0 +1,3 @@
+--
+-- spell aura effect 53055 from Hand of Salvation should not break invisibility
+-- INSERT INTO `spell_custom_attr` (`entry`,`attributes`) VALUES (53055, 64);
---
+++ sql/updates/world/2016_04_25_02_world.sql
@@ -0,0 +1,4 @@
+--
+UPDATE `smart_scripts` SET `action_param1`=169, `comment`='Gymer - On Passenger Removed - Set Phase 169' WHERE `entryorguid`=29884 AND `source_type`=0 AND `id`=5 AND `link`=6;
+UPDATE `smart_scripts` SET `action_param1`=177, `comment`='Gymer - On Passenger Boarded - Set Phase 177' WHERE `entryorguid`=29884 AND `source_type`=0 AND `id`=2 AND `link`=3;
+
