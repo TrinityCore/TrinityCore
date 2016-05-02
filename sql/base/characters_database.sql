66bd7b23743397cb9917f7cc09e9c79aeb0a7888
myran2 <henrytgordon@gmail.com>
2016-05-02 12:10:58 -0400

    Add specialization field to base character sql

--- sql/base/characters_database.sql
+++ sql/base/characters_database.sql
@@ -1071,6 +1071,7 @@ CREATE TABLE `character_pet` (
   `curmana` int(10) unsigned NOT NULL DEFAULT '0',
   `savetime` int(10) unsigned NOT NULL DEFAULT '0',
   `abdata` text,
+  `specialization` smallint(5) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `owner` (`owner`),
   KEY `idx_slot` (`slot`)
