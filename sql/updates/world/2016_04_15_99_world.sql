44a161bad58c301f49716a32f49af438d44e69fe
Aokromes <aokromes@gmail.com>
2016-05-12 08:36:05 +0200

    DB\misc: delete duplicate file

--- sql/updates/world/2016_04_15_99_world.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_baron_geddon_inferno';
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
-(19695, 'spell_baron_geddon_inferno');
