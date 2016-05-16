6cf1446d39db994cdca2db8f7f61da8423672bb3
Shauren <shauren.trinity@gmail.com>
2016-05-17 00:02:55 +0200

    Core/Auras: Implemented auras SPELL_AURA_PREVENT_DURABILITY_LOSS and SPELL_AURA_PREVENT_DURABILITY_LOSS_FROM_COMBAT
    (cherry picked from commit 17118f4832d527f160dc300a52db7ba6c255bce2)

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -4941,6 +4941,9 @@ void Player::DurabilityPointsLossAll(int32 points, bool inventory)
 
 void Player::DurabilityPointsLoss(Item* item, int32 points)
 {
+    if (HasAuraType(SPELL_AURA_PREVENT_DURABILITY_LOSS))
+        return;
+
     int32 pMaxDurability = item->GetUInt32Value(ITEM_FIELD_MAXDURABILITY);
     int32 pOldDurability = item->GetUInt32Value(ITEM_FIELD_DURABILITY);
     int32 pNewDurability = pOldDurability - points;
