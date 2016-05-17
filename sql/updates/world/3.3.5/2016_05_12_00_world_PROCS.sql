23563b66f396f0a889dad203a62d9055ac335b8e
mik1893 <michele.roscelli@gmail.com>
2016-05-17 08:05:58 +0100

    Core/Scripts: converted Battle Experience (ICC - Gunship) check to spellscript

--- sql/updates/world/3.3.5/2016_05_12_00_world_PROCS.sql
+++ sql/updates/world/3.3.5/2016_05_12_00_world_PROCS.sql
@@ -58,13 +58,19 @@ INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
 DELETE FROM `spell_script_names` WHERE `ScriptName`= 'spell_item_soul_preserver';
 INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
 (60510, 'spell_item_soul_preserver');
+-- Death Choice Trinket
 DELETE FROM `spell_script_names` WHERE `ScriptName`= 'spell_item_death_choice`';
 INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
 (67702, 'spell_item_death_choice'),
 (67771, 'spell_item_death_choice');
+-- ToC 25 Caster Trinket
 DELETE FROM `spell_script_names` WHERE `ScriptName`= 'spell_item_trinket_stack`';
 INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
 (37657, 'spell_item_trinket_stack'),
-(54841, 'spell_item_trinket_stack');
+(54841, 'spell_item_trinket_stack'),
 (67712, 'spell_item_trinket_stack'),
 (67758, 'spell_item_trinket_stack');
+-- Battle Experience (Gunship - ICC)
+DELETE FROM `spell_script_names` WHERE `ScriptName`= 'spell_igb_battle_experience_check`';
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(71201, 'spell_igb_battle_experience_check');
--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -7841,10 +7841,6 @@ bool Unit::HandleProcTriggerSpell(Unit* victim, uint32 damage, AuraEffect* trigg
                             RemoveAurasDueToSpell(50240);
                         break;
                     }
-                    // Battle Experience
-                    // already handled in gunship battle script
-                    case 71201:
-                        return false;
                 }
                 break;
             case SPELLFAMILY_WARLOCK:
