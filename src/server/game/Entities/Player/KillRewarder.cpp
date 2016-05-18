7cf22eab81ae012099c2ed9e50bb36b146175a76
Shauren <shauren.trinity@gmail.com>
2016-05-18 23:28:40 +0200

    Core/Auras: Implemented SPELL_AURA_MOD_XP_FROM_CREATURE_TYPE

--- src/server/game/Entities/Player/KillRewarder.cpp
+++ src/server/game/Entities/Player/KillRewarder.cpp
@@ -151,9 +151,9 @@ inline void KillRewarder::_RewardXP(Player* player, float rate)
     }
     if (xp)
     {
-        // 4.2.2. Apply auras modifying rewarded XP (SPELL_AURA_MOD_XP_PCT).
-        for (auto const& aura : player->GetAuraEffectsByType(SPELL_AURA_MOD_XP_PCT))
-            AddPct(xp, aura->GetAmount());
+        // 4.2.2. Apply auras modifying rewarded XP (SPELL_AURA_MOD_XP_PCT and SPELL_AURA_MOD_XP_FROM_CREATURE_TYPE).
+        xp *= player->GetTotalAuraMultiplier(SPELL_AURA_MOD_XP_PCT);
+        xp *= player->GetTotalAuraMultiplierByMiscValue(SPELL_AURA_MOD_XP_FROM_CREATURE_TYPE, int32(_victim->GetCreatureType()));
 
         // 4.2.3. Give XP to player.
         player->GiveXP(xp, _victim, _groupRate);
