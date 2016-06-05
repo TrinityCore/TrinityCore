bd38768f9bb588eff86e3e27108e00ae8907bac9
jackpoz <giacomopoz@gmail.com>
2016-06-05 21:53:38 +0200

    Core/Player: Fix duel edge case crash
    Fix a crash that could be triggered if an aura removed after duel finished would have caused damage to one of the players.
    
    (cherry picked from commit 681fb6962477ab0fcd6f42d3e525405b8e5c7abf)

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -429,7 +429,7 @@ struct PvPInfo
 
 struct DuelInfo
 {
-    DuelInfo() : initiator(nullptr), opponent(nullptr), startTimer(0), startTime(0), outOfBound(0), isMounted(false) { }
+    DuelInfo() : initiator(nullptr), opponent(nullptr), startTimer(0), startTime(0), outOfBound(0), isMounted(false), isCompleted(false) { }
 
     Player* initiator;
     Player* opponent;
@@ -437,6 +437,7 @@ struct DuelInfo
     time_t startTime;
     time_t outOfBound;
     bool isMounted;
+    bool isCompleted;
 };
 
 struct Areas
