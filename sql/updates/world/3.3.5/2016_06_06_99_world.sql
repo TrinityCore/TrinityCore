6ce47d0a5df7744659b48ce62329508c732b36cd
treeston <treeston.mmoc@gmail.com>
2016-06-09 12:54:42 +0200

    sql fix

--- sql/updates/world/3.3.5/2016_06_06_99_world.sql
+++ sql/updates/world/3.3.5/2016_06_06_99_world.sql
@@ -9,6 +9,6 @@ INSERT INTO gossip_menu (`entry`,`text_id`) VALUES
 
 DELETE FROM `gossip_menu_option` WHERE (`menu_id`=4108 AND `id`=0) OR (`menu_id`=4109 AND `id`=0) OR (`menu_id`=4093 AND `id`=0);
 INSERT INTO `gossip_menu_option` (`menu_id`,`id`,`option_icon`,`option_text`,`OptionBroadcastTextID`,`option_id`,`npc_option_npcflag`,`action_menu_id`,`action_poi_id`,`box_coded`,`box_money`,`box_text`,`BoxBroadcastTextID`) VALUES
-(4093,0,0, 'Tell me more.',                                                            7675,1,1,4109,0,0,0,'',0); -- menu_id + text in TDB 6.04
+(4093,0,0, 'Tell me more.',                                                            7675,1,1,4109,0,0,0,'',0), -- menu_id + text in TDB 6.04
 (4109,0,0, 'What else do you have to say?',                                            7673,1,1,4108,0,0,0,'',0), -- menu_id + text in TDB 6.04
-(4108,0,0, 'You challenged us and we have come. Where is this master you speak of?\n', 7646,1,1,   0,0,0,0,'',0), -- menu_id + text in TDB 6.04
+(4108,0,0, 'You challenged us and we have come. Where is this master you speak of?\n', 7646,1,1,   0,0,0,0,'',0); -- menu_id + text in TDB 6.04
