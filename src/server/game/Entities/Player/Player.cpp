2df6a9126ac0173f0d89bbc22e735726206af536
Alan Deutscher <adeutscher@gmail.com>
2016-06-14 00:40:14 -0700

    Adjustment to Player::SetViewpoint to allow for Mind Vision "hops". (#17041)
    * Adjustment to Player::SetViewpoint to allow for Mind Vision "hops".
    
    * Dynamic objects should not be a valid viewpoint for spells like Mind Vision.
    
    * Dynamic objects should not be a valid viewpoint for spells like Mind Vision.
    
    * Removed redundant TYPEMASK_PLAYER check.
    
    * Moved SetSeer outside of if statement.

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -24069,6 +24069,7 @@ void Player::SetViewpoint(WorldObject* target, bool apply)
 
         if (target->isType(TYPEMASK_UNIT) && target != GetVehicleBase())
             static_cast<Unit*>(target)->AddPlayerToVision(this);
+        SetSeer(target);
     }
     else
     {
@@ -24084,7 +24085,7 @@ void Player::SetViewpoint(WorldObject* target, bool apply)
             static_cast<Unit*>(target)->RemovePlayerFromVision(this);
 
         //must immediately set seer back otherwise may crash
-        m_seer = this;
+        SetSeer(this);
 
         //WorldPacket data(SMSG_CLEAR_FAR_SIGHT_IMMEDIATE, 0);
         //GetSession()->SendPacket(&data);
