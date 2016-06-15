f944c789c4bdc79bf80ceabac1204926c129b7c8
Andrew Letson <aletson@mainstreethost.com>
2016-06-14 20:54:11 -0400

    I love Kiera2 but I also find my comments more informative.

--- sql/updates/world/3.3.5/2016_06_14_99_world.sql
+++ sql/updates/world/3.3.5/2016_06_14_99_world.sql
@@ -41,7 +41,7 @@ INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceGroup`,`SourceEntry`,
 -- Migrate NPC off of cpp script
 UPDATE `creature_template` SET `AIName` = 'SmartAI', `ScriptName` = '' WHERE `entry` = 22112;
 
--- Table smart_scripts
+-- Create SmartAI for Karynaku
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = 22112);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (22112, 0, 0, 0, 19, 0, 100, 0, 10866, 0, 0, 0, 12, 11980, 1, 300000, 0, 0, 0, 8, 0, 0, 0, -4204.94, 316.397, 122.508, 1.309, 'Karynaku - On Quest Accepted - Summon Creature \'Zuluhed the Whacked\''),
