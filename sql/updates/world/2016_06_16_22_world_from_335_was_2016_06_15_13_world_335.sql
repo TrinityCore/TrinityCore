a40f6903d003b9361904c768198d3742982b4af8
Aokromes <aokromes@gmail.com>
2016-06-16 07:45:13 +0200

    DB/Misc: 3.3.5a fixes

---
+++ sql/updates/world/2016_06_16_22_world_from_335_was_2016_06_15_13_world_335.sql
@@ -0,0 +1,6 @@
+/*
+--
+DELETE FROM `gameobject_template` WHERE `entry` IN (181290);
+INSERT INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `IconName`, `castBarCaption`, `unk1`, `faction`, `flags`, `size`, `data0`, `data1`, `data2`, `data3`, `data4`, `data5`, `data6`, `data7`, `data8`, `data9`, `data10`, `data11`, `data12`, `AIName`, `ScriptName`, `VerifiedBuild`) VALUES
+(181290,6,0,'Midsummer Bonfire Spawn Trap','','','',0,0,1,0,0,0,28784,0,0,-1,0,0,0,0,0,0,'','',0);
+*/
---
+++ sql/updates/world/2016_06_16_24_world_from_335_was_2016_06_15_15_world_335.sql
@@ -0,0 +1,4 @@
+/*
+-- 
+UPDATE  `creature_template` SET `type_flags`=0 WHERE `entry` IN (33557,33555,33553,33556,33554,33657,33653,33307,33310,33650);
+*/
