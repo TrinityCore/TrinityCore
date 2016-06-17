63bc7d01ecd80b55fc6c91421b24cea5b019e1af
ike3 <ike3@aloe.no-ip.biz>
2016-06-17 10:46:56 +0300

    [ai-0.36-143] fix say action probability table

--- src/plugins/playerbot/strategy/actions/SayAction.cpp
+++ src/plugins/playerbot/strategy/actions/SayAction.cpp
@@ -62,7 +62,7 @@ bool SayAction::Execute(Event event)
     time_t lastSaid = AI_VALUE2(time_t, "last said", qualifier);
     ai->GetAiObjectContext()->GetValue<time_t>("last said", qualifier)->Set(time(0) + urand(1, 60));
 
-    uint32 probability = probabilityTable[name];
+    uint32 probability = probabilityTable[qualifier];
     if (!probability) probability = 100;
     if (urand(0, 100) >= probability) return false;
 
