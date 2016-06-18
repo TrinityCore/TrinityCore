1899be4fd174720cf969e5c1b430f7a7c25cda04
ConanHUN <conan_513@hotmail.com>
2016-06-18 12:16:52 +0200

    Limit spawn area for players/bots (Goldshire)

---
+++ scripts/restricted zone.lua
@@ -0,0 +1,23 @@
+local ZoneCheck = {
+        AllowedZones = {
+				[12] = true,
+        },
+        TeleportLocations = {
+                -- [TeamId] = {Map, X, Y, Z, Orientation}
+                [0] = {0, -9468, 108, 58, 0},
+                [1] = {0, -9410, 16, 60, 0},
+        },
+        RestrictedMessage = "You have entered a restricted zone. You have been removed from the area."
+}
+ 
+function ZoneCheck.OnChange(event, player, newZone)
+        if not player:IsGM() then
+                if not(ZoneCheck.AllowedZones[newZone]) then
+                        local map, x, y, z, o = table.unpack(ZoneCheck.TeleportLocations[player:GetTeam()])
+                        player:Teleport(map, x, y, z, o)
+                        player:SendAreaTriggerMessage(ZoneCheck.RestrictedMessage)
+                end
+        end
+end
+ 
+RegisterPlayerEvent(27, ZoneCheck.OnChange)
