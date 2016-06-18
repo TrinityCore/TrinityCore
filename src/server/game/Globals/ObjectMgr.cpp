c8465ad18827b0e37ff8cac6fedb5fefbec3e53b
Shauren <shauren.trinity@gmail.com>
2016-06-18 16:23:55 +0200

    Core/Items: Updated max durability calculation

--- src/server/game/Globals/ObjectMgr.cpp
+++ src/server/game/Globals/ObjectMgr.cpp
@@ -2325,26 +2325,26 @@ uint32 FillMaxDurability(uint32 itemClass, uint32 itemSubClass, uint32 inventory
 
     static float const qualityMultipliers[MAX_ITEM_QUALITY] =
     {
-        1.0f, 1.0f, 1.0f, 1.17f, 1.37f, 1.68f, 0.0f, 0.0f
+        0.92f, 0.92f, 0.92f, 1.11f, 1.32f, 1.61f, 0.0f, 0.0f
     };
 
     static float const armorMultipliers[MAX_INVTYPE] =
     {
         0.00f, // INVTYPE_NON_EQUIP
-        0.59f, // INVTYPE_HEAD
+        0.60f, // INVTYPE_HEAD
         0.00f, // INVTYPE_NECK
-        0.59f, // INVTYPE_SHOULDERS
+        0.60f, // INVTYPE_SHOULDERS
         0.00f, // INVTYPE_BODY
         1.00f, // INVTYPE_CHEST
-        0.35f, // INVTYPE_WAIST
-        0.75f, // INVTYPE_LEGS
-        0.49f, // INVTYPE_FEET
-        0.35f, // INVTYPE_WRISTS
-        0.35f, // INVTYPE_HANDS
+        0.33f, // INVTYPE_WAIST
+        0.72f, // INVTYPE_LEGS
+        0.48f, // INVTYPE_FEET
+        0.33f, // INVTYPE_WRISTS
+        0.33f, // INVTYPE_HANDS
         0.00f, // INVTYPE_FINGER
         0.00f, // INVTYPE_TRINKET
         0.00f, // INVTYPE_WEAPON
-        1.00f, // INVTYPE_SHIELD
+        0.72f, // INVTYPE_SHIELD
         0.00f, // INVTYPE_RANGED
         0.00f, // INVTYPE_CLOAK
         0.00f, // INVTYPE_2HWEAPON
@@ -2363,27 +2363,27 @@ uint32 FillMaxDurability(uint32 itemClass, uint32 itemSubClass, uint32 inventory
 
     static float const weaponMultipliers[MAX_ITEM_SUBCLASS_WEAPON] =
     {
-        0.89f, // ITEM_SUBCLASS_WEAPON_AXE
-        1.03f, // ITEM_SUBCLASS_WEAPON_AXE2
-        0.77f, // ITEM_SUBCLASS_WEAPON_BOW
-        0.77f, // ITEM_SUBCLASS_WEAPON_GUN
-        0.89f, // ITEM_SUBCLASS_WEAPON_MACE
-        1.03f, // ITEM_SUBCLASS_WEAPON_MACE2
-        1.03f, // ITEM_SUBCLASS_WEAPON_POLEARM
-        0.89f, // ITEM_SUBCLASS_WEAPON_SWORD
-        1.03f, // ITEM_SUBCLASS_WEAPON_SWORD2
+        0.91f, // ITEM_SUBCLASS_WEAPON_AXE
+        1.00f, // ITEM_SUBCLASS_WEAPON_AXE2
+        1.00f, // ITEM_SUBCLASS_WEAPON_BOW
+        1.00f, // ITEM_SUBCLASS_WEAPON_GUN
+        0.91f, // ITEM_SUBCLASS_WEAPON_MACE
+        1.00f, // ITEM_SUBCLASS_WEAPON_MACE2
+        1.00f, // ITEM_SUBCLASS_WEAPON_POLEARM
+        0.91f, // ITEM_SUBCLASS_WEAPON_SWORD
+        1.00f, // ITEM_SUBCLASS_WEAPON_SWORD2
         0.00f, // ITEM_SUBCLASS_WEAPON_Obsolete
-        1.03f, // ITEM_SUBCLASS_WEAPON_STAFF
+        1.00f, // ITEM_SUBCLASS_WEAPON_STAFF
         0.00f, // ITEM_SUBCLASS_WEAPON_EXOTIC
         0.00f, // ITEM_SUBCLASS_WEAPON_EXOTIC2
-        0.64f, // ITEM_SUBCLASS_WEAPON_FIST_WEAPON
+        0.66f, // ITEM_SUBCLASS_WEAPON_FIST_WEAPON
         0.00f, // ITEM_SUBCLASS_WEAPON_MISCELLANEOUS
-        0.64f, // ITEM_SUBCLASS_WEAPON_DAGGER
-        0.64f, // ITEM_SUBCLASS_WEAPON_THROWN
+        0.66f, // ITEM_SUBCLASS_WEAPON_DAGGER
+        0.00f, // ITEM_SUBCLASS_WEAPON_THROWN
         0.00f, // ITEM_SUBCLASS_WEAPON_SPEAR
-        0.77f, // ITEM_SUBCLASS_WEAPON_CROSSBOW
-        0.64f, // ITEM_SUBCLASS_WEAPON_WAND
-        0.64f, // ITEM_SUBCLASS_WEAPON_FISHING_POLE
+        1.00f, // ITEM_SUBCLASS_WEAPON_CROSSBOW
+        0.66f, // ITEM_SUBCLASS_WEAPON_WAND
+        0.66f, // ITEM_SUBCLASS_WEAPON_FISHING_POLE
     };
 
     float levelPenalty = 1.0f;
@@ -2395,10 +2395,10 @@ uint32 FillMaxDurability(uint32 itemClass, uint32 itemSubClass, uint32 inventory
         if (inventoryType > INVTYPE_ROBE)
             return 0;
 
-        return 5 * uint32(23.0f * qualityMultipliers[quality] * armorMultipliers[inventoryType] * levelPenalty + 0.5f);
+        return 5 * uint32(round(25.0f * qualityMultipliers[quality] * armorMultipliers[inventoryType] * levelPenalty));
     }
 
-    return 5 * uint32(17.0f * qualityMultipliers[quality] * weaponMultipliers[itemSubClass] * levelPenalty + 0.5f);
+    return 5 * uint32(round(18.0f * qualityMultipliers[quality] * weaponMultipliers[itemSubClass] * levelPenalty));
 };
 
 void FillDisenchantFields(uint32* disenchantID, uint32* requiredDisenchantSkill, ItemTemplate const& itemTemplate)
