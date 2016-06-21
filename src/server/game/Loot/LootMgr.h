b294acae4b4b4f3c9a9ee0e3da8a48d48eb44c94
Shauren <shauren.trinity@gmail.com>
2016-06-21 20:41:30 +0200

    Core/Items: Item upgrade followup
    * Fixed Item::SetModifier to broadcast value updates
    * Generate base item upgrade when looting
    * Generate base item upgrade when loading from db and db2 data changed

--- src/server/game/Loot/LootMgr.h
+++ src/server/game/Loot/LootMgr.h
@@ -159,6 +159,7 @@ struct TC_GAME_API LootItem
     uint32  itemid;
     uint32  randomSuffix;
     int32   randomPropertyId;
+    int32   upgradeId;
     std::vector<int32> BonusListIDs;
     ConditionContainer conditions;                               // additional loot condition
     GuidSet allowedGUIDs;
@@ -177,7 +178,7 @@ struct TC_GAME_API LootItem
     explicit LootItem(LootStoreItem const& li);
 
     // Empty constructor for creating an empty LootItem to be filled in with DB data
-    LootItem() : itemid(0), randomSuffix(0), randomPropertyId(0), count(0), is_looted(false), is_blocked(false),
+    LootItem() : itemid(0), randomSuffix(0), randomPropertyId(0), upgradeId(0), count(0), is_looted(false), is_blocked(false),
                  freeforall(false), is_underthreshold(false), is_counted(false), needs_quest(false), follow_loot_rules(false),
                  canSave(true){ };
 
