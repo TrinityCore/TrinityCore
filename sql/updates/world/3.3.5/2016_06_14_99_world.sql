8fdf4cf230f47b1d1a11a6e2c17552bbee8eed69
Andrew Letson <velinath@cloudyday.net>
2016-06-21 17:18:14 -0400

    syntax fail

--- sql/updates/world/3.3.5/2016_06_14_99_world.sql
+++ sql/updates/world/3.3.5/2016_06_14_99_world.sql
@@ -9,7 +9,7 @@ UPDATE `creature_template` SET `AIName` = 'SmartAI', `ScriptName` = '' WHERE `en
 UPDATE `creature_template` SET `ScriptName` = '' WHERE `entry` = 23489;
 
 -- Create SmartAI
-DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid IN (21727, 21725, 22112);
+DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid IN (21727, 21725, 22112));
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (21727, 0, 0, 1, 62, 0, 100, 0, 8356, 0, 0, 0, 56, 30658, 1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Flanis Swiftwing - On Gossip Option 0 Selected - Add Item \'Flanis\'s Pack\'  1 Time'),
 (21727, 0, 1, 0, 61, 0, 100, 0, 0, 0, 0, 0, 72, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Flanis Swiftwing - On Linked Action - Close Gossip'),
