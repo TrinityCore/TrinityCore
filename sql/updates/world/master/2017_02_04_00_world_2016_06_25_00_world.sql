b95c1520ab5d4464a793a862ed71ccee2d3163b1
Aokromes <Aokromes@users.noreply.github.com>
2016-06-25 00:40:07 +0200

    DB/Creature: add missing aura and flags to Scryers' bookshelves in Shattrath
    Closes #17453 by SnapperRy
    (cherry picked from commit 73663887c5bfd112c22469ce1b13d232961766d7)

---
+++ sql/updates/world/master/2017_02_04_00_world_2016_06_25_00_world.sql
@@ -0,0 +1,3 @@
+--
+UPDATE `creature_template` SET `unit_flags`=2147746560 WHERE `entry` BETWEEN 33608 AND 33615;
+UPDATE `creature_template_addon` SET `auras`="28782" WHERE `entry` BETWEEN 33608 AND 33615;
