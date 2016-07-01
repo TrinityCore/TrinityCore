8c9f2b48fa10656a02923f301be881a06dedc448
Aokromes <aokromes@gmail.com>
2016-07-01 02:10:04 +0200

    DB/Misc: Fix more startup errors

---
+++ sql/updates/world/2016_07_01_00_world.sql
@@ -0,0 +1,6 @@
+--
+DELETE FROM `creature_queststarter` WHERE `quest` IN (24629,24635,24636);
+INSERT INTO `creature_queststarter` (`id`, `quest`) VALUES
+(37675, 24629),
+(37675, 24635),
+(37675, 24636);
