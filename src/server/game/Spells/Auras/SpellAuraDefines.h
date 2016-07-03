35dc374faaa30b83903a2129792938953891ab2f
Shauren <shauren.trinity@gmail.com>
2016-07-03 22:35:14 +0200

    Core/Auras: Defined more aura types

--- src/server/game/Spells/Auras/SpellAuraDefines.h
+++ src/server/game/Spells/Auras/SpellAuraDefines.h
@@ -519,10 +519,10 @@ enum AuraType
     SPELL_AURA_IGNORE_MOVEMENT_FORCES                       = 459,  // NYI
     SPELL_AURA_RESET_COOLDOWNS_ON_DUEL_START                = 460,  // NYI
     SPELL_AURA_461                                          = 461,
-    SPELL_AURA_462                                          = 462,
-    SPELL_AURA_CONVER_CRIT_RATING_PCT_TO_PARRY_RATING       = 463, // NYI
-    SPELL_AURA_464                                          = 464,
-    SPELL_AURA_465                                          = 465,
+    SPELL_AURA_MOD_HEALING_AND_ABSORB_FROM_CASTER           = 462,  // NYI
+    SPELL_AURA_CONVERT_CRIT_RATING_PCT_TO_PARRY_RATING      = 463,  // NYI
+    SPELL_AURA_MOD_ATTACK_POWER_OF_BONUS_ARMOR              = 464,  // NYI
+    SPELL_AURA_MOD_BONUS_ARMOR                              = 465,  // NYI
     SPELL_AURA_MOD_BONUS_ARMOR_PCT                          = 466,  // Affects bonus armor gain from all sources except base stats
     SPELL_AURA_MOD_STAT_BONUS_PCT                           = 467,  // Affects stat gain from all sources except base stats
     SPELL_AURA_TRIGGER_SPELL_ON_HEALTH_BELOW_PCT            = 468,  // Triggers spell when health falls below specified percent value (once, not every time damage is taken below threshold)
