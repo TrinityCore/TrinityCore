80258dda1a076e7f70308c7ed7941ede6ab22c1d
Aokromes <aokromes@gmail.com>
2016-07-05 19:08:27 +0200

    DB/Misc: Fix import

--- sql/updates/world/2016_07_05_15_world_from_335_was_2016_07_05_00_world.sql
+++ sql/updates/world/2016_07_05_15_world_from_335_was_2016_07_05_00_world.sql
@@ -1,6 +1,6 @@
 SET @CGUID := 11003;
 
-UPDATE `spell_dbc` SET `EffectBasePoints1`=20 WHERE `Id`=70507;
+UPDATE `spelleffect_dbc` SET `EffectBasePoints`=20 WHERE `Id`=70507;
 UPDATE `creature_template` SET `unit_flags`=32832 WHERE `entry` IN(40142,40143,40144,40145);
 UPDATE `creature_template` SET `flags_extra`=128 WHERE `entry` IN (40081,40470,40471,40472);
 
