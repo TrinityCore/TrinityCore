5e5071c53d8395ca051166d08d69d148c568b963
Krudor <erikstrandberg93@hotmail.com>
2016-07-10 13:03:55 +0200

    Adds taunt immunity to generic berserk spell

---
+++ sql/updates/world/6.x/2016_07_xx_00_world.sql
@@ -0,0 +1,4 @@
+﻿-- Added missing spellscript (generic berserk spell)
+DELETE FROM `spell_script_names` WHERE `spell_id` = 26662;
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(26662, 'spell_gen_berserk');
