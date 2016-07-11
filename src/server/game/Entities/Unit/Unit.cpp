d315f9efb2b6e2c46d3db86ff8527d813ce21e4d
Meruemu <Meruemu@users.noreply.github.com>
2016-07-11 10:15:05 +0200

    Core/Entities: Fix flag CREATURE_FLAG_EXTRA_NO_PARRY_HASTEN implementation. (#17548)
    (cherry picked from commit 10ef8fcee5f3d01077d4e2f60f003f8d34405951)

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -1315,7 +1315,8 @@ void Unit::DealMeleeDamage(CalcDamageInfo* damageInfo, bool durabilityLoss)
     if (damageInfo->blocked_amount && damageInfo->TargetState != VICTIMSTATE_BLOCKS)
         victim->HandleEmoteCommand(EMOTE_ONESHOT_PARRY_SHIELD);
 
-    if (damageInfo->TargetState == VICTIMSTATE_PARRY)
+    if (damageInfo->TargetState == VICTIMSTATE_PARRY &&
+        (GetTypeId() != TYPEID_UNIT || (ToCreature()->GetCreatureTemplate()->flags_extra & CREATURE_FLAG_EXTRA_NO_PARRY_HASTEN) == 0))
     {
         // Get attack timers
         float offtime  = float(victim->getAttackTimer(OFF_ATTACK));
