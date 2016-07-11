5c9a442061095fa4f1cc34346ddf7572a6fc11e4
Aokromes <aokromes@gmail.com>
2016-07-11 05:28:02 +0200

    DB/Creature: Fix runtime error

---
+++ sql/updates/world/2016_07_11_07_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `creature_equip_template` WHERE `CreatureID`=25239;
+INSERT INTO `creature_equip_template` (`CreatureID`, `ID`, `ItemID1`, `ItemID2`, `ItemID3`, `VerifiedBuild`) VALUES
+(25239, 1, 6829, 0, 0, 0);
