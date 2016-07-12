9e5e52735bd3b1a975353a912961a32ece23e772
codie72 <codie198>
2016-07-12 14:45:09 +1000

    This Database Update will return Northshire to Classic Wow. +4 Deathweave bags
    item=38145/deathweave-bag  deathknight Bound bags.

---
+++ sql/updates/world/3.3.5/2016_04_11_03_world_335.sql
@@ -0,0 +1,13 @@
+UPDATE `world`.`creature_template` SET `flags_extra`='0' WHERE `entry`='257';
+
+UPDATE `world`.`creature_template` SET `flags_extra`='0' WHERE `entry`='80';
+
+UPDATE `world`.`creature_template` SET `flags_extra`='0' WHERE `entry`='6';
+
+UPDATE `world`.`creature_template` SET `faction`='17' WHERE `entry`='38';
+UPDATE `world`.`creature_template` SET `faction`='17' WHERE `entry`='103';
+
+
+
+UPDATE `world`.`quest_template` SET `RewardItem1`='38145', `RewardAmount1`='4'  WHERE `ID`='783';
+
