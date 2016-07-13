3b87278c21c280534d45f97e40bba6363076b065
codie72 <codie198>
2016-07-13 14:37:20 +1000

    Kobolds now attack after faction change to faction 26

--- sql/updates/world/3.3.5/2016_04_11_03_world_335.sql
+++ sql/updates/world/3.3.5/2016_04_11_03_world_335.sql
@@ -3,6 +3,12 @@ UPDATE `world`.`creature_template` SET `flags_extra`='0' WHERE `entry`='80';
 UPDATE `world`.`creature_template` SET `flags_extra`='0' WHERE `entry`='6';
 UPDATE `world`.`creature_template` SET `faction`='17' WHERE `entry`='38';
 UPDATE `world`.`creature_template` SET `faction`='17' WHERE `entry`='103';
+UPDATE `world`.`creature_template` SET `faction`='26' WHERE `entry`='6';
+
+UPDATE `world`.`creature_template` SET `faction`='26' WHERE `entry`='80';
+
+UPDATE `world`.`creature_template` SET `faction`='26' WHERE `entry`='257';
+
 
 UPDATE `world`.`creature_template` SET `flags_extra`='0' WHERE `entry`='1989';
 UPDATE `world`.`creature_template` SET `flags_extra`='0' WHERE `entry`='1985';
