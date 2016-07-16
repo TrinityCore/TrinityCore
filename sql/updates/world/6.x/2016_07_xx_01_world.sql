4ea9f4aa12b17d855b0f5d34af3202d62902554e
Krudor <erikstrandberg93@hotmail.com>
2016-07-17 01:13:35 +0200

    Fixed some aura procs affected by changes

---
+++ sql/updates/world/6.x/2016_07_xx_01_world.sql
@@ -0,0 +1,15 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (28200,29602,45040,90900,99947,118089,127271,127272,127274,127278,144671,145738,187213);
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(28200, 'spell_item_talisman_of_ascendance'),
+(29602, 'spell_item_jom_gabbar'),
+(45040, 'spell_item_battle_trance'),
+(90900, 'spell_item_world_queller_focus'),
+(99947, 'spell_gen_face_rage'),
+(118089, 'spell_item_water_strider'),
+(127271, 'spell_item_water_strider'),
+(127272, 'spell_item_water_strider'),
+(127274, 'spell_item_water_strider'),
+(127278, 'spell_item_water_strider'),
+(144671, 'spell_item_brutal_kinship'),
+(145738, 'spell_item_brutal_kinship'),
+(187213, 'spell_gen_impatient_mind');
