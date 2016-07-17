2056ce4d66cdaf12fe4e35c110508360026f9f7b
Shauren <shauren.trinity@gmail.com>
2016-07-17 15:20:56 +0200

    Scripts: Build fix

--- src/server/scripts/Northrend/IcecrownCitadel/icecrown_citadel_teleport.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/icecrown_citadel_teleport.cpp
@@ -60,7 +60,8 @@ class icecrown_citadel_teleport : public GameObjectScript
 
                 if (player->IsInCombat())
                 {
-                    Spell::SendCastResult(player, spell, 0, SPELL_FAILED_AFFECTING_COMBAT);
+                    ObjectGuid castId = ObjectGuid::Create<HighGuid::Cast>(SPELL_CAST_SOURCE_NORMAL, player->GetMapId(), spell->Id, player->GetMap()->GenerateLowGuid<HighGuid::Cast>());
+                    Spell::SendCastResult(player, spell, 0, castId, SPELL_FAILED_AFFECTING_COMBAT);
                     return true;
                 }
 
@@ -85,7 +86,10 @@ class at_frozen_throne_teleport : public AreaTriggerScript
             if (player->IsInCombat())
             {
                 if (SpellInfo const* spell = sSpellMgr->GetSpellInfo(FROZEN_THRONE_TELEPORT))
-                    Spell::SendCastResult(player, spell, 0, SPELL_FAILED_AFFECTING_COMBAT);
+                {
+                    ObjectGuid castId = ObjectGuid::Create<HighGuid::Cast>(SPELL_CAST_SOURCE_NORMAL, player->GetMapId(), spell->Id, player->GetMap()->GenerateLowGuid<HighGuid::Cast>());
+                    Spell::SendCastResult(player, spell, 0, castId, SPELL_FAILED_AFFECTING_COMBAT);
+                }
                 return true;
             }
 
