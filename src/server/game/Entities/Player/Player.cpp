76118d790893ae677b03ab21f4d1e3ce24c2267c
ariel- <ariel-@users.noreply.github.com>
2016-07-16 21:38:24 -0300

    Core/Player: fix crash
    Closes  #17073
    
    (cherry picked from commit 957f7ea9e6340ec6c055768682eabcbc84634130)

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -19297,7 +19297,7 @@ bool Player::CheckInstanceValidity(bool /*isLogin*/)
         return true;
 
     // non-instances are always valid
-    Map* map = GetMap();
+    Map* map = FindMap();
     if (!map || !map->IsDungeon())
         return true;
 
