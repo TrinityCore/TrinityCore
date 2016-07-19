716b2fe167e2bf45e0fe5d21634f7ec5336715ae
ariel- <ariel-@users.noreply.github.com>
2016-07-19 12:14:00 -0300

    Core/Entities: rename ItemPrototype to ItemTemplate

--- src/server/game/Entities/Item/ItemTemplate.cpp
+++ src/server/game/Entities/Item/ItemTemplate.cpp
@@ -15,7 +15,7 @@
 * with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "ItemPrototype.h"
+#include "ItemTemplate.h"
 
 bool ItemTemplate::CanChangeEquipStateInCombat() const
 {
