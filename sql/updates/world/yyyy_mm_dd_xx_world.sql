11c028f4bb142eaf16b4b8ddfdee10ac3f96522b
Aokromes <aokromes@gmail.com>
2016-07-19 11:08:51 +0200

    Core/Tools: Fix pdump write

--- sql/updates/world/yyyy_mm_dd_xx_world.sql
+++
@@ -1,1 +0,0 @@
-UPDATE `creature_model_info` SET `BoundingRadius`=7.5, `CombatReach`=6 WHERE `DisplayID`=30890;
--- src/server/game/Tools/PlayerDump.cpp
+++ src/server/game/Tools/PlayerDump.cpp
@@ -317,13 +317,13 @@ bool PlayerDumpWriter::DumpTable(std::string& dump, ObjectGuid::LowType guid, ch
                     break;
                 case DTT_CHARACTER:
                 {
-                    if (result->GetFieldCount() <= 64)          // avoid crashes on next check
+                    if (result->GetFieldCount() <= 69)          // avoid crashes on next check
                     {
                         TC_LOG_FATAL("misc", "PlayerDumpWriter::DumpTable - Trying to access non-existing or wrong positioned field (`deleteInfos_Account`) in `characters` table.");
                         return false;
                     }
 
-                    if (result->Fetch()[64].GetUInt32())        // characters.deleteInfos_Account - if filled error
+                    if (result->Fetch()[69].GetUInt32())        // characters.deleteInfos_Account - if filled error
                         return false;
                     break;
                 }
