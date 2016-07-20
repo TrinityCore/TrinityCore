bee4c5518f8165c8facc4589ea695e3144740eae
Aokromes <aokromes@gmail.com>
2016-07-20 22:58:26 +0200

    DB/Misc: 3.3.5a fixes

---
+++ sql/updates/world/2016_07_20_17_world_from_335_was_2016_07_16_05_world_335.sql
@@ -0,0 +1,5 @@
+/*
+DELETE FROM `creature` WHERE `guid`=52668;
+INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `VerifiedBuild`) VALUES
+(52668, 10979, 0, 1, 1, 2709, 0, 2109.13, -1829.79, 58.1995, 3.9185, 315, 3, 0, 2964, 0, 1, 0, 0, 0, 0);
+*/
