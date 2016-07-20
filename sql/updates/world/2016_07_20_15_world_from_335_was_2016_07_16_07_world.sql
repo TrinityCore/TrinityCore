eacbddd471de978dd01776b89a50d949c74fe6ce
Aokromes <aokromes@gmail.com>
2016-07-20 22:53:29 +0200

    Restore file deleted by mistake

---
+++ sql/updates/world/2016_07_20_15_world_from_335_was_2016_07_16_07_world.sql
@@ -0,0 +1,5 @@
+ -- enable 2 new spell scripts: spell_hallow_end_candy & spell_hallow_end_candy_pirate
+ DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_hallow_end_candy','spell_hallow_end_candy_pirate');
+ INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+ (24930,'spell_hallow_end_candy'),
+ (24926,'spell_hallow_end_candy_pirate');
