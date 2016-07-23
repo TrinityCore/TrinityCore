a8b9edefbcd45194c3be5a46ce09fc43b180dddf
ariel- <ariel.silva305@gmail.com>
2016-07-23 06:50:43 -0300

    Core/Spell: whitespace fix

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -2050,13 +2050,15 @@ void AuraEffect::HandleAuraTransform(AuraApplication const* aurApp, uint8 mode, 
 
                     // Polymorph (sheep)
                     if (GetSpellInfo()->SpellFamilyName == SPELLFAMILY_MAGE && GetSpellInfo()->SpellIconID == 82 && GetSpellInfo()->SpellVisual[0] == 12978)
+                    {
                         if (Unit* caster = GetCaster())
                         {
                             if (caster->HasAura(52648))         // Glyph of the Penguin
                                 model_id = 26452;
                             else if (caster->HasAura(57927))	// Glyph of the Monkey 
-                                model_id = 21362; 
+                                model_id = 21362;
                         }
+                    }
 
                     target->SetDisplayId(model_id);
 
