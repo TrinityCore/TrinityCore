9dac2dbca99904845c9a62cb2edb367ffc5a66f3
Aokromes <aokromes@gmail.com>
2016-07-25 15:29:38 +0200

    Script/Stonecore: Fix coverity 82008

--- src/server/scripts/Maelstrom/Stonecore/instance_stonecore.cpp
+++ src/server/scripts/Maelstrom/Stonecore/instance_stonecore.cpp
@@ -1,20 +1,19 @@
 /*
-* Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
-* Copyright (C) 2006-2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
-*
-* This program is free software; you can redistribute it and/or modify it
-* under the terms of the GNU General Public License as published by the
-* Free Software Foundation; either version 2 of the License, or (at your
-* option) any later version.
-*
-* This program is distributed in the hope that it will be useful, but WITHOUT
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
-* more details.
-*
-* You should have received a copy of the GNU General Public License along
-* with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "ScriptMgr.h"
 #include "Player.h"
@@ -220,7 +219,7 @@ class instance_stonecore : public InstanceMapScript
             ObjectGuid corborusRockDoorGUID;
             GuidVector slabhideRockWallGUIDs;
             
-            EncounterState slabhideIntro;
+            EncounterState slabhideIntro = NOT_STARTED;
         };
 
         InstanceScript* GetInstanceScript(InstanceMap* map) const override
