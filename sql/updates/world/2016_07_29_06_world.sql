f3c560f87ee80b284610b052d32c090d94108ad5
Aokromes <aokromes@gmail.com>
2016-07-29 23:30:46 +0200

    DB/Misc: Fix gold of some creatures By Malcrom

---
+++ sql/updates/world/2016_07_29_06_world.sql
@@ -0,0 +1,4 @@
+
+UPDATE `creature_template` SET `mingold`=1, `maxgold`=7 WHERE `entry` IN (40940);
+UPDATE `creature_template` SET `mingold`=2, `maxgold`=12 WHERE `entry` IN (40941);
+UPDATE `creature_template` SET `mingold`=25, `maxgold`=50 WHERE `entry` IN (1137);
