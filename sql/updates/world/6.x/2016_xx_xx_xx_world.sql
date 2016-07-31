be2f88040b5fbedc5a225b862549c6980d7fcff0
Kito <kito@vortexirc.com>
2016-08-01 00:39:56 +0200

    Scripts/Spells: Fix Shaman Talent Path of Flames

---
+++ sql/updates/world/6.x/2016_xx_xx_xx_world.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (51505, 210621); 
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(51505, 'spell_sha_lava_burst'),
+(210621, 'spell_sha_path_of_flames_spread');
