a7aae1f1784d3d149890f8f81aff5add65756864
Aokromes <Aokromes@users.noreply.github.com>
2016-07-31 05:25:41 +0200

    Core/PVP: Improve log (#17698)
    (cherry picked from commit ea4e7fec0181cfe59400cba069bf3e27aa4860df)

--- src/server/game/Handlers/BattleGroundHandler.cpp
+++ src/server/game/Handlers/BattleGroundHandler.cpp
@@ -586,7 +586,7 @@ void WorldSession::HandleReportPvPAFK(WorldPackets::Battleground::ReportPvPPlaye
     Player* reportedPlayer = ObjectAccessor::FindPlayer(reportPvPPlayerAFK.Offender);
     if (!reportedPlayer)
     {
-        TC_LOG_DEBUG("bg.battleground", "WorldSession::HandleReportPvPAFK: player not found");
+        TC_LOG_INFO("bg.reportpvpafk", "WorldSession::HandleReportPvPAFK: %s [IP: %s] reported %s [IP: %s]", _player->GetName().c_str(), _player->GetSession()->GetRemoteAddress().c_str(), reportedPlayer->GetName().c_str(),reportedPlayer->GetSession()->GetRemoteAddress().c_str());
         return;
     }
 
