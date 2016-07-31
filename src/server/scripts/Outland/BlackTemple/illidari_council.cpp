9c09a0bd75271c01882ed2673a0abaefb2bd9dbd
tkrokli <tkrokli@gmail.com>
2016-07-31 06:08:43 +0200

    Core/Scripts: Remove useless SD2 error log (#17691)
    * Core/Scripts:  Remove useless log message (ref. joschiwald)
    
    (cherry picked from commit 8fdb9b4579793dbd8fa68cd5ad331d7a92ce8fea)

--- src/server/scripts/Outland/BlackTemple/illidari_council.cpp
+++ src/server/scripts/Outland/BlackTemple/illidari_council.cpp
@@ -90,8 +90,6 @@ enum IllidariCouncil
     SPELL_BERSERK               = 45078
 };
 
-#define ERROR_INST_DATA           "SD2 ERROR: Instance Data for Black Temple not set properly; Illidari Council event will not function properly."
-
 struct CouncilYells
 {
     int32 entry;
@@ -165,7 +163,7 @@ public:
                 Council[1] = instance->GetGuidData(DATA_VERAS_DARKSHADOW);
                 Council[2] = instance->GetGuidData(DATA_LADY_MALANDE);
                 Council[3] = instance->GetGuidData(DATA_HIGH_NETHERMANCER_ZEREVOR);
-            } else TC_LOG_ERROR("scripts", ERROR_INST_DATA);
+            }
         }
 
         void EnterCombat(Unit* /*who*/) override { }
