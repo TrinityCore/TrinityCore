7b99b6e74b291ebd58f2981102b501d028b66254
Aokromes <aokromes@gmail.com>
2016-07-31 19:58:33 +0200

    DB/Item: Add some missing items to remove startup errors

---
+++ sql/updates/world/2016_07_31_03_world.sql
@@ -0,0 +1,11 @@
+--
+DELETE FROM `item_template` WHERE `entry` IN (5397,35480,46819,52498,35482,37089,37090,40843);
+INSERT INTO `item_template` (`entry`, `name`) VALUES
+(5397, 'Defias Gunpowder'),
+(35480, 'Harvest Collector Maintenance Guide'),
+(46819, 'Black Mandrake'),
+(52498, 'Oxygenated Seaweed'),
+(35482, 'Agricultural Engineering for Dummies'),
+(37089, 'Honeycone'),
+(37090, 'Quickpoppy'),
+(40843, 'Iceberries');
