65f48246314f3662dba6dc76968890810c4004ae
Aokromes <aokromes@gmail.com>
2016-08-01 09:15:36 +0200

    Better error message

--- src/server/game/Handlers/GroupHandler.cpp
+++ src/server/game/Handlers/GroupHandler.cpp
@@ -115,7 +115,7 @@ void WorldSession::HandleGroupInviteOpcode(WorldPacket& recvData)
 
     if (!player->GetSocial()->HasFriend(GetPlayer()->GetGUID().GetCounter()) && player->getLevel() < sWorld->getIntConfig(CONFIG_PARTY_LEVEL_REQ))
     {
-        SendPartyResult(PARTY_OP_INVITE, membername, ERR_IGNORING_YOU_S);
+        SendPartyResult(PARTY_OP_INVITE, membername, ERR_INVITE_RESTRICTED);
         return;
     }
 
