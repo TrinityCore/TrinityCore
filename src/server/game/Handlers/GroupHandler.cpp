5264374fd657b5e42811631ab9a6d94e539aea46
Aokromes <Aokromes@users.noreply.github.com>
2016-08-01 10:43:49 +0200

    Core/Player: New PartyLevelReq setting (#17707)
    (cherry picked from commit bfda0ba91d2b686177f471052637f837f8a6c1c4)
    
    Typo fix
    (cherry picked from commit 35f06ccb5568becadc6f4becdb6760cf0485d9aa)

--- src/server/game/Handlers/GroupHandler.cpp
+++ src/server/game/Handlers/GroupHandler.cpp
@@ -100,6 +100,12 @@ void WorldSession::HandlePartyInviteOpcode(WorldPackets::Party::PartyInviteClien
         return;
     }
 
+    if (!player->GetSocial()->HasFriend(GetPlayer()->GetGUID()) && GetPlayer()->getLevel() < sWorld->getIntConfig(CONFIG_PARTY_LEVEL_REQ))
+    {
+        SendPartyResult(PARTY_OP_INVITE, player->GetName(), ERR_INVITE_RESTRICTED);
+        return;
+    }
+
     Group* group = GetPlayer()->GetGroup();
     if (group && group->isBGGroup())
         group = GetPlayer()->GetOriginalGroup();
