6147ba61568fc389e105bd8a92a0051744542716
ForesterDev <forester.manv@gmail.com>
2016-08-04 17:24:41 +0400

    Core\Command: remove account_muted data on account deletion
    (cherry picked from commit 6a87524204161234576810f770d8ad2a0b9bb9c8)
    
    # Conflicts:
    #	src/server/database/Database/Implementation/LoginDatabase.cpp
    #	src/server/database/Database/Implementation/LoginDatabase.h

--- src/server/database/Database/Implementation/LoginDatabase.cpp
+++ src/server/database/Database/Implementation/LoginDatabase.cpp
@@ -113,6 +113,7 @@ void LoginDatabaseConnection::DoPrepareStatements()
 
     PrepareStatement(LOGIN_INS_ACCOUNT_MUTE, "INSERT INTO account_muted VALUES (?, UNIX_TIMESTAMP(), ?, ?, ?)", CONNECTION_ASYNC);
     PrepareStatement(LOGIN_SEL_ACCOUNT_MUTE_INFO, "SELECT mutedate, mutetime, mutereason, mutedby FROM account_muted WHERE guid = ? ORDER BY mutedate ASC", CONNECTION_SYNCH);
+    PrepareStatement(LOGIN_DEL_ACCOUNT_MUTED, "DELETE FROM account_muted WHERE guid = ?", CONNECTION_ASYNC);
 
 #define BnetAccountInfo "ba.id, UPPER(ba.email), ba.locked, ba.lock_country, ba.last_ip, ba.failed_logins, bab.unbandate > UNIX_TIMESTAMP() OR bab.unbandate = bab.bandate, bab.unbandate = bab.bandate"
 #define BnetGameAccountInfo "a.id, a.username, ab.unbandate > UNIX_TIMESTAMP() OR ab.unbandate = ab.bandate, ab.unbandate = ab.bandate, aa.gmlevel"
