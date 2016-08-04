2bb136c1bd17b4cc0fdcd97a4702e784aeb89d5e
Aokromes <aokromes@gmail.com>
2016-08-04 03:00:46 +0200

    remove startup error

--- sql/updates/world/2016_08_04_00_world__from_335_was_2016_08_04_00_world_335.sql
+++ sql/updates/world/2016_08_04_00_world__from_335_was_2016_08_04_00_world_335.sql
@@ -1,9 +1,8 @@
-/*
 -- NPC ID 9021 Kharan Mighthammer, Quest ID 4001 'What Is Going On?' and 4342 'Kharan's Tale'
 SET @Kharan := 9021;
 
-UPDATE `creature_template` SET `AIName`= 'SmartAI', `ScriptName`= '' WHERE `entry` = @Kharan;
-
+UPDATE `creature_template` SET `AIName`= '', `ScriptName`= '' WHERE `entry` = @Kharan;
+/*
 DELETE FROM `smart_scripts` WHERE (source_type = 0 AND entryorguid = @Kharan);
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (@Kharan,0,0,0, 10,0,100,0,   0,20,0,0,  1,   0,3000,0,0,0,0,7,0,0,0,0,0,0,0,'Kharan Mighthammer - Within 0-20 Range Out of Combat LoS - Say Line 0'),
