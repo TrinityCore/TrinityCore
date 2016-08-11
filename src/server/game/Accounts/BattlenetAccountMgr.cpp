9d5304fd4f9824c1beb640ae96bf0513d26926ff
ForesterDev <forester.manv@gmail.com>
2016-08-11 13:32:52 +0200

    Core/Command: Check account password max length
    (cherry picked from commit fdcea5c7efd58c929e5d653af955f524dc543034)
    
    Rename 2016_08_04_05_world.sql to 2016_08_11_00_world.sql
    (cherry picked from commit d4f2f5968aadc5145e9a8c58dc7a4e0bd48c87e8)

--- src/server/game/Accounts/BattlenetAccountMgr.cpp
+++ src/server/game/Accounts/BattlenetAccountMgr.cpp
@@ -35,7 +35,7 @@ AccountOpResult Battlenet::AccountMgr::CreateBattlenetAccount(std::string email,
     Utf8ToUpperOnlyLatin(password);
 
     if (GetId(email))
-         return AccountOpResult::AOR_NAME_ALREADY_EXIST;
+        return AccountOpResult::AOR_NAME_ALREADY_EXIST;
 
     PreparedStatement* stmt = LoginDatabase.GetPreparedStatement(LOGIN_INS_BNET_ACCOUNT);
     stmt->setString(0, email);
