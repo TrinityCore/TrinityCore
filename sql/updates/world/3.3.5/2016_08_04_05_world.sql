e1d34cabc934389f2df5133e5cf02a63474950f7
Aokromes <aokromes@gmail.com>
2016-08-12 12:21:55 +0200

    Core/Dynamic: Reduce differences between 3.3.5 and 6.x branches Code functionality shouldn't have been modified.

--- sql/updates/world/3.3.5/2016_08_04_05_world.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM `trinity_string` WHERE `entry` = 1031;
-INSERT INTO `trinity_string` (`entry`, `content_default`) VALUES
-(1031, 'An account password can NOT be longer than 16 characters (client limit). The account was NOT created.');
