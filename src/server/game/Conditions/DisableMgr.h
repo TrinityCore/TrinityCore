8b62ceceaea10af6c3e423abfd976ea52eae1523
Aokromes <aokromes@gmail.com>
2016-08-15 10:28:28 +0200

    Core/DungeonFinder: disable map only from looking for dungeon

--- src/server/game/Conditions/DisableMgr.h
+++ src/server/game/Conditions/DisableMgr.h
@@ -33,7 +33,8 @@ enum DisableType
     DISABLE_TYPE_ACHIEVEMENT_CRITERIA   = 4,
     DISABLE_TYPE_OUTDOORPVP             = 5,
     DISABLE_TYPE_VMAP                   = 6,
-    DISABLE_TYPE_MMAP                   = 7
+    DISABLE_TYPE_MMAP                   = 7,
+    DISABLE_TYPE_LFG_MAP                = 8
 };
 
 enum SpellDisableTypes
