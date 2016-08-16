dad23103e32a52c992c67ca82e3266a22197a177
Conan <conanhun@gmail.com>
2016-08-16 17:04:28 +0200

    Rename some command

--- update.sh
+++ update.sh
@@ -8,15 +8,17 @@ echo "######################################"
 echo
 read -n1 -r -p "Press any key to continue..." key
 
-sudo rm /usr/local/bin/update
-sudo cp ./update /usr/local/bin
+sudo rm /usr/local/bin/server-update
+sudo cp ./update /usr/local/bin/server-update
 
-sudo rm /usr/local/bin/rebuild
-sudo cp ./rebuild /usr/local/bin
+sudo rm /usr/local/bin/server-rebuild
+sudo cp ./rebuild /usr/local/bin/server-rebuild
 
-sudo rm /usr/local/bin/update-config
-sudo cp ./update-config /usr/local/bin
+sudo rm /usr/local/bin/server-update-config
+sudo cp ./update-config /usr/local/bin/server-update-config
 
+
+clear
 echo Restoring modified files to default.
 git reset --hard
 echo
