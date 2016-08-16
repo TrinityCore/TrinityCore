c69c5a0d0d64d64819d61b2d458ea9fbe1e2f6b5
Conan <conanhun@gmail.com>
2016-08-16 16:58:50 +0200

    update-config command

--- update.sh
+++ update.sh
@@ -8,8 +8,14 @@ echo "######################################"
 echo
 read -n1 -r -p "Press any key to continue..." key
 
+sudo rm /usr/local/bin/update
+sudo cp ./update /usr/local/bin
+
 sudo rm /usr/local/bin/rebuild
 sudo cp ./rebuild /usr/local/bin
+
+sudo rm /usr/local/bin/update-config
+sudo cp ./update-config /usr/local/bin
 
 echo Restoring modified files to default.
 git reset --hard
