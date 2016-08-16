753b5889ba9b06f7e358b0ac0ee1bee683463ee2
Conan <conanhun@gmail.com>
2016-08-16 17:16:04 +0200

    Update config files

--- update.sh
+++ update.sh
@@ -8,16 +8,6 @@ echo "######################################"
 echo
 read -n1 -r -p "Press any key to continue..." key
 
-sudo rm /usr/local/bin/server-update
-sudo cp ./update /usr/local/bin/server-update
-
-sudo rm /usr/local/bin/server-rebuild
-sudo cp ./rebuild /usr/local/bin/server-rebuild
-
-sudo rm /usr/local/bin/server-update-config
-sudo cp ./update-config /usr/local/bin/server-update-config
-
-
 clear
 echo Restoring modified files to default.
 git reset --hard
@@ -26,5 +16,14 @@ echo Pull the latest updates from repo
 echo
 git pull
 echo
+
+sudo rm /usr/local/bin/server-update
+sudo cp ./update /usr/local/bin/server-update
+
+sudo rm /usr/local/bin/server-rebuild
+sudo cp ./rebuild /usr/local/bin/server-rebuild
+
+sudo rm /usr/local/bin/server-update-config
+sudo cp ./update-config /usr/local/bin/server-update-config
 
 ./compile.sh
