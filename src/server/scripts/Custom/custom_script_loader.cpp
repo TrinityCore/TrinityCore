a0ce9080bc32b9d6362b796c035eccba28e35b2e
ConanHUN <conan_513@hotmail.com>
2016-08-18 22:13:36 +0200

    Solocraft mode from 3.3.5

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -16,32 +16,32 @@
  */
 
 // This is where scripts' loading functions should be declared:
-
+void AddSC_solocraft();
 
 void AddSC_GOMove_commandscript();  //Rocher2-gomove_master
-void AddSC_XpWeekend();             //周末双倍经验
-void AddSC_solocraft();             //单人随机地下城查找器脚本
-void AddSC_SpellCraft();            //单人随机地下城查找器脚本
-void AddSC_LeechCraft();            //单人随机地下城查找器脚本
-void AddSC_CombatRatingCraft();     //单人随机地下城查找器脚本
-void AddSC_Hastecraft();            //单人随机地下城查找器脚本
-void AddLfgSoloScripts();           //单人随机地下城查找器脚本
-void AddSC_custom_npcs();           //自定义NPC-好像是暴风城的龙王加冕事件
-void AddSC_custom_player_script();  //单人随机地下城查找器脚本
-void AddSC_GuardianAngel();         //守护神
-void AddSC_onlogin_announcer();      //玩家欢迎信息
-void AddSC_Boss_Announcer();        //boss击杀公告
-void AddSC_PvP_System();            //pvp击杀公告
+void AddSC_XpWeekend();             //��ĩ˫������
+void AddSC_solocraft();             //����������³ǲ������ű�
+void AddSC_SpellCraft();            //����������³ǲ������ű�
+void AddSC_LeechCraft();            //����������³ǲ������ű�
+void AddSC_CombatRatingCraft();     //����������³ǲ������ű�
+void AddSC_Hastecraft();            //����������³ǲ������ű�
+void AddLfgSoloScripts();           //����������³ǲ������ű�
+void AddSC_custom_npcs();           //�Զ���NPC-�����Ǳ���ǵ����������¼�
+void AddSC_custom_player_script();  //����������³ǲ������ű�
+void AddSC_GuardianAngel();         //�ػ���
+void AddSC_onlogin_announcer();      //��һ�ӭ��Ϣ
+void AddSC_Boss_Announcer();        //boss��ɱ����
+void AddSC_PvP_System();            //pvp��ɱ����
 
 
-//void AddSC_debug_lfg();             //单人进地下城尝试, OpenLCore,已经自己通过配置实现,停用
-//void AddSC_double_xp();             //周六、周天双倍经验, OpenLCore,和现有XpWeekend重复,故而停用
-//void AddSC_start_equipment();       //OpenLCore,出生送装备
-//void AddSC_quest_conversation();     //任务对话?           ////这货启用,会使服务端启动不了,千万别启用,谨记!
+//void AddSC_debug_lfg();             //���˽����³ǳ���, OpenLCore,�Ѿ��Լ�ͨ������ʵ��,ͣ��
+//void AddSC_double_xp();             //����������˫������, OpenLCore,������XpWeekend�ظ�,�ʶ�ͣ��
+//void AddSC_start_equipment();       //OpenLCore,������װ��
+//void AddSC_quest_conversation();     //����Ի�?           ////�������,��ʹ������������,ǧ�������,����!
 
 
 
-//void AddSC_gossiptp();              //闲聊
+//void AddSC_gossiptp();              //����
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
 
