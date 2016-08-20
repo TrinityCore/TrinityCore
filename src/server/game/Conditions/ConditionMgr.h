3a62ecb5160e9d564204f5070270227b10be5e35
treeston <treeston.mmoc@gmail.com>
2016-08-20 14:36:26 +0200

    Core/Conditions: Add new CONDITION_TAXI (46). Does what it says on the tin. Tagging issue #15560.
    (cherry picked from commit 08b383305146962f686af716c90f10776a12bc12)

--- src/server/game/Conditions/ConditionMgr.h
+++ src/server/game/Conditions/ConditionMgr.h
@@ -94,7 +94,8 @@ enum ConditionTypes
     CONDITION_DAILY_QUEST_DONE      = 43,                   // quest id         0              0                  true if daily quest has been completed for the day
     CONDITION_CHARMED               = 44,                   // 0                0              0                  true if unit is currently charmed
     CONDITION_PET_TYPE              = 45,                   // mask             0              0                  true if player has a pet of given type(s)
-    CONDITION_MAX                   = 46                    // MAX
+    CONDITION_TAXI                  = 46,                   // 0                0              0                  true if player is on taxi
+    CONDITION_MAX                   = 47                    // MAX
 };
 
 /*! Documentation on implementing a new ConditionSourceType:
