fa87649f13400bb41f6609b661938478f662cb82
joschiwald <joschiwald.trinity@gmail.com>
2016-08-27 12:10:51 +0200

    Core/Logging: delete old logs of the own realm only
    (cherry picked from commit beedf737c815c1f946381904417471c42a21de76)

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -2242,6 +2242,7 @@ void World::Update(uint32 diff)
 
             stmt->setUInt32(0, sWorld->getIntConfig(CONFIG_LOGDB_CLEARTIME));
             stmt->setUInt32(1, uint32(time(0)));
+            stmt->setUInt32(2, realm.Id.Realm);
 
             LoginDatabase.Execute(stmt);
         }
