2cf4f6820a8bd57d1f9311baca903ae4815c0fdf
Killyana <morphone1@gmail.com>
2016-08-28 13:04:42 +0200

    DB: Fix some DB errors

---
+++ sql/updates/world/2016_08_28_00_world.sql
@@ -0,0 +1,6 @@
+-- 
+UPDATE  `conditions` SET `SourceGroup`=1 WHERE `SourceTypeOrReferenceId` = 13 AND `SourceEntry` IN (85123);
+UPDATE `smart_scripts` SET `action_param1`=71985 WHERE `entryorguid`=26428 AND `source_type` = 0 AND `id` =11 AND `action_type` = 11;
+UPDATE `smart_scripts` SET `link`=0 WHERE `entryorguid`=47599 AND `source_type` = 0 AND `id` =0 AND `action_type` = 11;
+UPDATE `smart_scripts` SET `link`=0 WHERE `entryorguid`=41641 AND `source_type` = 0 AND `id` =0;
+UPDATE `smart_scripts` SET `link`=0 WHERE `entryorguid`=47600 AND `source_type` = 0 AND `id` =0;
