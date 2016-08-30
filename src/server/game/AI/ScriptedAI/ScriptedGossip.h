f94dc007aedadd0e7f1d9f72702892ca5ec4b78d
treeston <treeston.mmoc@gmail.com>
2016-08-31 00:11:57 +0200

    Fix dynamic build.
    (cherry picked from commit 54f923fdee9cf8a17e5eaee8a8f0290dadc9905b)

--- src/server/game/AI/ScriptedAI/ScriptedGossip.h
+++ src/server/game/AI/ScriptedAI/ScriptedGossip.h
@@ -83,16 +83,16 @@ enum eTradeskill
 };
 
 class Creature;
-void ClearGossipMenuFor(Player* player);
+void TC_GAME_API ClearGossipMenuFor(Player* player);
 // Using provided text, not from DB
-void AddGossipItemFor(Player* player, uint32 icon, const char* text, uint32 sender, uint32 action);
+void TC_GAME_API AddGossipItemFor(Player* player, uint32 icon, const char* text, uint32 sender, uint32 action);
 // Using provided texts, not from DB
-void AddGossipItemFor(Player* player, uint32 icon, const char* text, uint32 sender, uint32 action, const char* popupText, uint32 popupMoney, bool coded);
+void TC_GAME_API AddGossipItemFor(Player* player, uint32 icon, const char* text, uint32 sender, uint32 action, const char* popupText, uint32 popupMoney, bool coded);
 // Uses gossip item info from DB
-void AddGossipItemFor(Player* player, uint32 gossipMenuID, uint32 gossipMenuItemID, uint32 sender, uint32 action);
-void SendGossipMenuFor(Player* player, uint32 npcTextID, ObjectGuid const& guid);
-void SendGossipMenuFor(Player* player, uint32 npcTextID, Creature const* creature);
-void CloseGossipMenuFor(Player* player);
+void TC_GAME_API AddGossipItemFor(Player* player, uint32 gossipMenuID, uint32 gossipMenuItemID, uint32 sender, uint32 action);
+void TC_GAME_API SendGossipMenuFor(Player* player, uint32 npcTextID, ObjectGuid const& guid);
+void TC_GAME_API SendGossipMenuFor(Player* player, uint32 npcTextID, Creature const* creature);
+void TC_GAME_API CloseGossipMenuFor(Player* player);
 
 // Defined fuctions to use with player.
 // This fuction add's a menu item,
