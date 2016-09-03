1a6dcb528d4758162d36cd1fa8a29bfb17538d98
Aokromes <aokromes@gmail.com>
2016-09-03 02:11:16 +0200

    DB/Misc: More startup errors

---
+++ sql/updates/world/4.3.4/2016_09_03_04_world.sql
@@ -0,0 +1,6 @@
+--
+DELETE FROM `npc_spellclick_spells` WHERE `npc_entry` IN (51101,51102,51103);
+INSERT INTO `npc_spellclick_spells` (`npc_entry`, `spell_id`, `cast_flags`) VALUES
+(51101, 41020, 1),
+(51102, 41020, 1,
+(51103, 41020, 1);
