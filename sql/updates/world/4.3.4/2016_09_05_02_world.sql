f216285fac9dea262c801379c4dca0a96b879ce7
Aokromes <aokromes@gmail.com>
2016-09-05 12:55:30 +0200

    typo fix

--- sql/updates/world/4.3.4/2016_09_05_02_world.sql
+++ sql/updates/world/4.3.4/2016_09_05_02_world.sql
@@ -1,7 +1,7 @@
 --
 UPDATE `creature_template` SET `minlevel`=86, `maxlevel`=86, `exp`=3, `faction`=14, `speed_walk`=20, `speed_run`=7.14286, `unit_flags`=33554752, `unit_flags2`=34816 WHERE `entry`=49101;
 UPDATE `creature_template` SET `minlevel`=86, `maxlevel`=86, `exp`=3, `faction`=14, `speed_walk`=20, `speed_run`=7.14286, `unit_flags`=33554752, `unit_flags2`=34816 WHERE `entry`=49100;
-UPDATE `creature_template` SET `minlevel`=86, `maxlevel`=86, `exp`=3, `faction`=14, `speed_walk`=20, `speed_run`=7.14286, `unit_flags`=64, `unit_flags2`=134250496' WHERE `entry`=49097;
+UPDATE `creature_template` SET `minlevel`=86, `maxlevel`=86, `exp`=3, `faction`=14, `speed_walk`=20, `speed_run`=7.14286, `unit_flags`=64, `unit_flags2`=134250496 WHERE `entry`=49097;
 UPDATE `creature_template` SET `minlevel`=86, `maxlevel`=86, `exp`=3, `faction`=14, `speed_walk`=20, `speed_run`=7.14286, `unit_flags`=64, `unit_flags2`=34816 WHERE `entry`=49096;
 UPDATE `creature_template` SET `minlevel`=86, `maxlevel`=86, `exp`=3, `faction`=14, `unit_flags`=33554432 WHERE `entry`=49099;
 UPDATE `creature_template` SET `minlevel`=87, `maxlevel`=87, `exp`=3, `faction`=14, `unit_flags`=32832 WHERE `entry`=49080;
