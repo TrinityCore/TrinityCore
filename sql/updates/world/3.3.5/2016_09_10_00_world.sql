993f800d7548ff64b79a99b7d20a6485628a3808
Aokromes <aokromes@gmail.com>
2016-09-10 14:34:52 +0200

    DB/Misc: 3.3.5a fixes

--- sql/updates/world/3.3.5/2016_09_10_00_world.sql
+++
@@ -1,2 +0,0 @@
---
-UPDATE `spell_dbc` SET `Effect1`=28, `EffectMiscValueB1`=64 WHERE `Id`=29869;
---
+++ sql/updates/world/4.3.4/2016_09_10_00_world_from_335_was_2016_09_10_00_world.sql
@@ -0,0 +1,2 @@
+--
+UPDATE `spelleffect_dbc` SET `Effect`=28, `EffectMiscValueB`=64 WHERE `Id`=29869;
