6f1e823cac8050780afb11f2befb3fb829200418
SnapperRy <snapperryen@gmail.com>
2016-09-13 04:16:05 +0200

    Core/Fishing: implement retail-like fishing skill-up functionality (#17528)

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -2524,6 +2524,8 @@ class TC_GAME_API Player : public Unit, public GridObject<Player>
 
         uint8 m_grantableLevels;
 
+        uint8 m_fishingSteps;
+
         bool m_needsZoneUpdate;
 
     private:
