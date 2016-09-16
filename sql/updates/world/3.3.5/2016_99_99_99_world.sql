0d3d9f5e22b45ac22a42b45a655e4fef2f0329f7
ccrs <ccrsdev1992@gmail.com>
2016-09-16 22:35:27 +0200

    some corrections

--- sql/updates/world/3.3.5/2016_99_99_99_world.sql
+++ sql/updates/world/3.3.5/2016_99_99_99_world.sql
@@ -3,7 +3,7 @@ INSERT INTO `spell_proc` (`SpellId`, `SchoolMask`, `SpellFamilyName`, `SpellFami
 (-18213,  32,  5, 0x00004000, 0x00000000, 0x00000000,     2, 0x0, 0x0,    0, 0, 0, 0,      0, 0), -- Improved Drain Soul
 (-12834,   0,  0, 0x00000000, 0x00000000, 0x00000000,     0, 0x1, 0x2,    2, 0, 0, 0,      0, 0), -- Deep Wounds Aura
 (26467,   0,  0, 0x00000000, 0x00000000, 0x00000000,     0, 0x2, 0x2,    0, 0, 0, 0,      0, 0), -- Persistent Shield
-(38164,   0,  0, 0x00000000, 0x00000000, 0x00000000,     0, 0x0, 0x0,    2, 0, 0, 0,   5000, 0); -- Unyielding Knights
+(38164,   0,  0, 0x00000000, 0x00000000, 0x00000000,     0, 0x0, 0x2,    0, 0, 0, 0,      0, 0); -- Unyielding Knights
 
 DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_warl_improved_drain_soul','spell_warr_deep_wounds_aura','spell_gen_dummy_trigger','spell_item_persistent_shield');
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
@@ -11,6 +11,6 @@ INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (-12834, 'spell_warr_deep_wounds_aura'),
 (26467, 'spell_item_persistent_shield');
 
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=17 AND `SourceGroup`=0 AND `SourceEntry`=38164;
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=24 AND `SourceGroup`=0 AND `SourceEntry`=38164;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceGroup`,`SourceEntry`,`SourceId`,`ElseGroup`,`ConditionTypeOrReference`,`ConditionTarget`,`ConditionValue1`,`ConditionValue2`,`ConditionValue3`,`NegativeCondition`,`ErrorType`,`ErrorTextId`,`ScriptName`,`Comment`) VALUES
-(17,0,38164,0,0,31,1,3,19457,0,0,0,0,"","Spell Unyielding Knights can only hit selected target on condition");
+(24,0,38164,0,0,31,1,3,19457,0,0,0,0,"","Aura Unyielding Knights can only proc on condition target");
