e19e95d480c04729675fd8e9e480a6720b643347
alexandre433 <alexandreluisbarreto@gmail.com>
2016-09-16 15:35:35 +0100

    Created

---
+++ contrib/merge_old_windows.bat
@@ -0,0 +1,157 @@
+::				TDB_31_to_TDB33
+copy /a ..\sql\old\3.3.5a\TDB31_to_TDB32_updates\auth\*.sql /b auth_TDB31_to_TDB32_updates.sql
+
+
+::				TDB_32_to_TDB33
+copy /a ..\sql\old\3.3.5a\TDB32_to_TDB33_updates\characters\*.sql /b characters_TDB32_to_TDB33_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB32_to_TDB33_updates\world\*.sql /b world_TDB32_to_TDB33_updates.sql
+
+
+::				TDB_33_to_TDB34
+copy /a ..\sql\old\3.3.5a\TDB33_to_TDB34_updates\auth\*.sql /b auth_TDB33_to_TDB34_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB33_to_TDB34_updates\characters\*.sql /b characters_TDB33_to_TDB34_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB33_to_TDB34_updates\world\*.sql /b world_TDB33_to_TDB34_updates.sql
+
+
+::				TDB_34_to_TDB35
+copy /a ..\sql\old\3.3.5a\TDB34_to_TDB35_updates\characters\*.sql /b characters_TDB34_to_TDB35_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB34_to_TDB35_updates\world\*.sql /b world_TDB34_to_TDB35_updates.sql
+
+
+::				TDB_35_to_TDB36
+copy /a ..\sql\old\3.3.5a\TDB35_to_TDB36_updates\world\*.sql /b world_TDB35_to_TDB36_updates.sql
+
+
+::				TDB_36_to_TDB37
+copy /a ..\sql\old\3.3.5a\TDB36_to_TDB37_updates\characters\*.sql /b characters_TDB36_to_TDB37_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB36_to_TDB37_updates\world\*.sql /b world_TDB36_to_TDB37_updates.sql
+
+
+::				TDB_37_to_TDB38
+copy /a ..\sql\old\3.3.5a\TDB37_to_TDB38_updates\characters\*.sql /b characters_TDB37_to_TDB38_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB37_to_TDB38_updates\world\*.sql /b world_TDB37_to_TDB38_updates.sql
+
+
+::				TDB_38_to_TDB39
+copy /a ..\sql\old\3.3.5a\TDB38_to_TDB39_updates\characters\*.sql /b characters_TDB38_to_TDB39_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB38_to_TDB39_updates\world\*.sql /b world_TDB38_to_TDB39_updates.sql
+
+
+::				TDB_39_to_TDB40
+copy /a ..\sql\old\3.3.5a\TDB39_to_TDB40_updates\characters\*.sql /b characters_TDB39_to_TDB40_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB39_to_TDB40_updates\world\*.sql /b world_TDB39_to_TDB40_updates.sql
+
+
+::				TDB_40_to_TDB41
+copy /a ..\sql\old\3.3.5a\TDB40_to_TDB41_updates\characters\*.sql /b characters_TDB40_to_TDB41_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB40_to_TDB41_updates\world\*.sql /b world_TDB40_to_TDB41_updates.sql
+
+
+::				TDB_41_to_TDB42
+copy /a ..\sql\old\3.3.5a\TDB41_to_TDB42_updates\auth\*.sql /b auth_TDB41_to_TDB42_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB41_to_TDB42_updates\characters\*.sql /b characters_TDB41_to_TDB42_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB41_to_TDB42_updates\world\*.sql /b world_TDB41_to_TDB42_updates.sql
+
+
+::				TDB_42_to_TDB43
+copy /a ..\sql\old\3.3.5a\TDB42_to_TDB43_updates\characters\*.sql /b characters_TDB42_to_TDB43_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB42_to_TDB43_updates\world\*.sql /b world_TDB42_to_TDB43_updates.sql
+
+
+::				TDB_43_to_TDB44
+copy /a ..\sql\old\3.3.5a\TDB43_to_TDB44_updates\world\*.sql /b world_TDB43_to_TDB44_updates.sql
+
+
+::				TDB_44_to_TDB45
+copy /a ..\sql\old\3.3.5a\TDB44_to_TDB45_updates\characters\*.sql /b characters_TDB44_to_TDB45_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB44_to_TDB45_updates\world\*.sql /b world_TDB44_to_TDB45_updates.sql
+
+
+::				TDB_45_to_TDB46
+copy /a ..\sql\old\3.3.5a\TDB45_to_TDB46_updates\characters\*.sql /b characters_TDB45_to_TDB46_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB45_to_TDB46_updates\world\*.sql /b world_TDB45_to_TDB46_updates.sql
+
+
+::				TDB_46_to_TDB47
+copy /a ..\sql\old\3.3.5a\TDB46_to_TDB47_updates\auth\*.sql /b auth_TDB46_to_TDB47_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB46_to_TDB47_updates\characters\*.sql /b characters_TDB46_to_TDB47_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB46_to_TDB47_updates\world\*.sql /b world_TDB46_to_TDB47_updates.sql
+
+
+::				TDB_47_to_TDB48
+copy /a ..\sql\old\3.3.5a\TDB47_to_TDB48_updates\characters\*.sql /b characters_TDB47_to_TDB48_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB47_to_TDB48_updates\world\*.sql /b world_TDB47_to_TDB48_updates.sql
+
+
+::				TDB_48_to_TDB49
+copy /a ..\sql\old\3.3.5a\TDB48_to_TDB49_updates\auth\*.sql /b auth_TDB48_to_TDB49_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB48_to_TDB49_updates\characters\*.sql /b characters_TDB48_to_TDB49_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB48_to_TDB49_updates\world\*.sql /b world_TDB48_to_TDB49_updates.sql
+
+
+::				TDB_49_to_TDB50
+copy /a ..\sql\old\3.3.5a\TDB49_to_TDB50_updates\characters\*.sql /b characters_TDB49_to_TDB50_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB49_to_TDB50_updates\world\*.sql /b world_TDB49_to_TDB50_updates.sql
+
+
+::				TDB_50_to_TDB51
+copy /a ..\sql\old\3.3.5a\TDB50_to_TDB51_updates\auth\*.sql /b auth_TDB50_to_TDB51_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB50_to_TDB51_updates\world\*.sql /b world_TDB50_to_TDB51_updates.sql
+
+
+::				TDB_51_to_TDB52
+copy /a ..\sql\old\3.3.5a\TDB51_to_TDB52_updates\auth\*.sql /b auth_TDB51_to_TDB52_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB51_to_TDB52_updates\characters\*.sql /b characters_TDB51_to_TDB52_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB51_to_TDB52_updates\world\*.sql /b world_TDB51_to_TDB52_updates.sql
+
+
+::				TDB_52_to_TDB53
+copy /a ..\sql\old\3.3.5a\TDB52_to_TDB53_updates\auth\*.sql /b auth_TDB52_to_TDB53_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB52_to_TDB53_updates\characters\*.sql /b charactersTDB52_to_TDB53_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB52_to_TDB53_updates\world\*.sql /b world_TDB52_to_TDB53_updates.sql
+
+
+::				TDB_53_to_TDB54
+copy /a ..\sql\old\3.3.5a\TDB53_to_TDB54_updates\auth\*.sql /b auth_TDB53_to_TDB54_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB53_to_TDB54_updates\characters\*.sql /b characters_TDB53_to_TDB54_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB53_to_TDB54_updates\world\*.sql /b world_TDB53_to_TDB54_updates.sql
+
+
+::				TDB_54_to_TDB55
+copy /a ..\sql\old\3.3.5a\TDB54_to_TDB55_updates\auth\*.sql /b auth_TDB54_to_TDB55_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB54_to_TDB55_updates\world\*.sql /b world_TDB54_to_TDB55_updates.sql
+
+
+::				TDB_55_to_TDB56
+copy /a ..\sql\old\3.3.5a\TDB55_to_TDB56_updates\auth\*.sql /b auth_TDB55_to_TDB56_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB55_to_TDB56_updates\characters\*.sql /b characters_TDB55_to_TDB56_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB55_to_TDB56_updates\world\*.sql /b world_TDB55_to_TDB56_updates.sql
+
+
+::				TDB_56_to_TDB57
+copy /a ..\sql\old\3.3.5a\TDB56_to_TDB57_updates\auth\*.sql /b auth_TDB56_to_TDB57_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB56_to_TDB57_updates\characters\*.sql /b characters_TDB56_to_TDB57_updates.sql
+copy /a ..\sql\old\3.3.5a\TDB56_to_TDB57_updates\world\*.sql /b world_TDB56_to_TDB57_updates.sql
+
+
+
+::				auth_TDB_57_to_auth_TDB_60
+copy /a ..\sql\old\3.3.5a\auth\57_2014_10_19\*.sql /b auth_57.sql
+copy /a ..\sql\old\3.3.5a\auth\60_2016_04_11\*.sql /b auth_59.sql
+copy /a ..\sql\old\3.3.5a\auth\59_2015_11_07\*.sql /b auth_60.sql
+
+
+::				characters_TDB_57_to_characters_TDB_60
+copy /a ..\sql\old\3.3.5a\characters\57_2014_10_19\*.sql /b characters_57.sql
+copy /a ..\sql\old\3.3.5a\characters\58_2015_03_21\*.sql /b characters_58.sql
+copy /a ..\sql\old\3.3.5a\characters\59_2015_11_07\*.sql /b characters_59.sql
+copy /a ..\sql\old\3.3.5a\characters\60_2016_04_11\*.sql /b characters_60.sql
+
+
+::				world_TDB_57_to_world_TDB_60
+copy /a ..\sql\old\3.3.5a\world\57_2014_10_19\*.sql /b world_57.sql
+copy /a ..\sql\old\3.3.5a\world\58_2015_03_21\*.sql /b world_58.sql
+copy /a ..\sql\old\3.3.5a\world\59_2015_11_07\*.sql /b world_59.sql
+copy /a ..\sql\old\3.3.5a\world\60_2016_04_11\*.sql /b world_60.sql
+
+
