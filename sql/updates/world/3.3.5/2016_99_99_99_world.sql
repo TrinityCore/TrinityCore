e39903907619732e930c69146b5d4d0bb84d4e65
ccrs <ccrsdev1992@gmail.com>
2016-09-16 12:58:13 +0200

    some corrections

--- sql/updates/world/3.3.5/2016_99_99_99_world.sql
+++ sql/updates/world/3.3.5/2016_99_99_99_world.sql
@@ -1,8 +1,8 @@
 DELETE FROM `spell_proc` WHERE `SpellId` IN (49222, 51209, 70844);
 INSERT INTO `spell_proc` (`SpellId`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `ProcFlags`, `SpellTypeMask`, `SpellPhaseMask`, `HitMask`, `AttributesMask`, `ProcsPerMinute`, `Chance`, `Cooldown`, `Charges`) VALUES
-(49222,   0,  0, 0x00000000, 0x00000000, 0x00000000,     0, 0x0, 0x0,    0, 0, 0, 0,      0, 0), -- Bone Shield
+(49222,   0,  0, 0x00000000, 0x00000000, 0x00000000,139944, 0x0, 0x0,    0, 0, 0, 0,   2000, 0), -- Bone Shield
 (51209,   0,  0, 0x00000000, 0x00000000, 0x00000000,     0, 0x0, 0x0,    0, 0, 0, 0,      0, 0), -- Hungering Cold
-(70844,   0,  0, 0x00000000, 0x00000000, 0x00000000,     0, 0x0, 0x0,    0, 0, 0, 0,      0, 0); -- Item - Warrior T10 Protection 4P Bonus
+(70844,   0,  4, 0x00000100, 0x00000000, 0x00000000,     0, 0x0, 0x2,    0, 0, 0, 0,      0, 0); -- Item - Warrior T10 Protection 4P Bonus
 
 DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_dk_hungering_cold','spell_warr_item_T10_prot_4P_bonus');
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
