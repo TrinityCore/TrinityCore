83a2470a802046e30dc05e17136e3063fe027249
ariel- <ariel-@users.noreply.github.com>
2016-09-17 17:37:37 -0300

    DB/Spell: remove SP scaling for Shaman T8 Elemental 4p bonus

---
+++ sql/updates/world/3.3.5/2016_09_17_03_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_bonus_data` WHERE `entry`=64930;
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES
+(64930, 0, 0, 0, 0, 'Electrified');
