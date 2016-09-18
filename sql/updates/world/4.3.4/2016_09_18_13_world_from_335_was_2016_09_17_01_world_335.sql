56e3e69dbaca4847aebd4c7c9b253c9171e8b8e8
Aokromes <aokromes@gmail.com>
2016-09-18 07:36:51 +0200

    DB/Misc: 3.3.5a fixes

---
+++ sql/updates/world/4.3.4/2016_09_18_13_world_from_335_was_2016_09_17_01_world_335.sql
@@ -0,0 +1,2 @@
+-- Update location of Tua'kea's Fishing Hook
+-- UPDATE `gameobject` SET `position_x`=2651.38, `position_y`=1007.243, `position_z`=-24.03463, `orientation`=-2.33874 WHERE `guid`=150122;
