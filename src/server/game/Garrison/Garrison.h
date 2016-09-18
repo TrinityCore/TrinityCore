93789adf6b0c2d972ce793b1bd613bcff212c84a
Shauren <shauren.trinity@gmail.com>
2016-09-18 22:18:45 +0200

    Core/Garrison: Defined new follower status flag

--- src/server/game/Garrison/Garrison.h
+++ src/server/game/Garrison/Garrison.h
@@ -162,7 +162,8 @@ enum GarrisonFollowerStatus
     FOLLOWER_STATUS_FAVORITE    = 0x01,
     FOLLOWER_STATUS_EXHAUSTED   = 0x02,
     FOLLOWER_STATUS_INACTIVE    = 0x04,
-    FOLLOWER_STATUS_TROOP       = 0x08
+    FOLLOWER_STATUS_TROOP       = 0x08,
+    FOLLOWER_STATUS_NO_XP_GAIN  = 0x10
 };
 
 class GameObject;
