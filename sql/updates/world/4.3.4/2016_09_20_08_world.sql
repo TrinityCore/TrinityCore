48691056232e1e90f3fe3ebeb0e1d4c3c6c0e76c
Aokromes <aokromes@gmail.com>
2016-09-20 11:17:28 +0200

    DB/Misc: Link difficulties

---
+++ sql/updates/world/4.3.4/2016_09_20_08_world.sql
@@ -0,0 +1,51 @@
+--
+UPDATE `creature_template` SET `difficulty_entry_1`=57409, `difficulty_entry_2`=57771, `difficulty_entry_3`=57772 WHERE `entry`=55265;
+UPDATE `creature_template` SET `difficulty_entry_1`=56576, `difficulty_entry_2`=56577, `difficulty_entry_3`=56578 WHERE `entry`=55294;
+UPDATE `creature_template` SET `difficulty_entry_1`=57371, `difficulty_entry_2`=57372, `difficulty_entry_3`=57373 WHERE `entry`=55864;
+UPDATE `creature_template` SET `difficulty_entry_1`=56516, `difficulty_entry_2`=56517, `difficulty_entry_3`=56518 WHERE `entry`=53890;
+UPDATE `creature_template` SET `difficulty_entry_1`=52559, `difficulty_entry_2`=52560, `difficulty_entry_3`=52561 WHERE `entry`=52558;
+UPDATE `creature_template` SET `difficulty_entry_1`=54340, `difficulty_entry_2`=54341, `difficulty_entry_3`=54342 WHERE `entry`=54093;
+UPDATE `creature_template` SET `difficulty_entry_1`=53773, `difficulty_entry_2`=53774, `difficulty_entry_3`=53775 WHERE `entry`=53772;
+UPDATE `creature_template` SET `difficulty_entry_1`=54193, `difficulty_entry_2`=54194, `difficulty_entry_3`=54195 WHERE `entry`=54192;
+UPDATE `creature_template` SET `difficulty_entry_1`=54200, `difficulty_entry_2`=54201, `difficulty_entry_3`=54202 WHERE `entry`=54199;
+UPDATE `creature_template` SET `difficulty_entry_1`=53979, `difficulty_entry_2`=54079, `difficulty_entry_3`=54080 WHERE `entry`=53691;
+
+UPDATE `creature_template` SET `minlevel`=88, `maxlevel`=88, `exp`=3, `faction`=14, `unit_class`=2, `unit_flags`=32832 WHERE `entry`=57409;
+UPDATE `creature_template` SET `minlevel`=88, `maxlevel`=88, `exp`=3, `faction`=14, `unit_class`=2, `unit_flags`=32832 WHERE `entry`=57771;
+UPDATE `creature_template` SET `minlevel`=88, `maxlevel`=88, `exp`=3, `faction`=14, `unit_class`=2, `unit_flags`=32832 WHERE `entry`=57772;
+
+UPDATE `creature_template` SET `minlevel`=88, `maxlevel`=88, `exp`=3, `faction`=14, `unit_flags`=32832, `unit_flags2`=134219776 WHERE `entry`=56576;
+UPDATE `creature_template` SET `minlevel`=88, `maxlevel`=88, `exp`=3, `faction`=14, `unit_flags`=32832, `unit_flags2`=134219776 WHERE `entry`=56577;
+UPDATE `creature_template` SET `minlevel`=88, `maxlevel`=88, `exp`=3, `faction`=14, `unit_flags`=32832, `unit_flags2`=134219776 WHERE `entry`=56578;
+
+UPDATE `creature_template` SET `minlevel`=87, `maxlevel`=87, `exp`=3, `faction`=16, `unit_flags`=33587200, `unit_flags2`=524288 WHERE `entry`=57371;
+UPDATE `creature_template` SET `minlevel`=87, `maxlevel`=87, `exp`=3, `faction`=16, `unit_flags`=33587200, `unit_flags2`=524288 WHERE `entry`=57372;
+UPDATE `creature_template` SET `minlevel`=87, `maxlevel`=87, `exp`=3, `faction`=16, `unit_flags`=33587200, `unit_flags2`=524288 WHERE `entry`=57373;
+
+UPDATE `creature_template` SET `minlevel`=88, `maxlevel`=88, `exp`=3, `faction`=14 WHERE `entry`=56516;
+UPDATE `creature_template` SET `minlevel`=88, `maxlevel`=88, `exp`=3, `faction`=14 WHERE `entry`=56517;
+UPDATE `creature_template` SET `minlevel`=88, `maxlevel`=88, `exp`=3, `faction`=14 WHERE `entry`=56518;
+
+UPDATE `creature_template` SET `minlevel`=88, `maxlevel`=88, `exp`=3, `faction`=2146, `unit_flags`=32832, `unit_flags2`=83888128 WHERE `entry`=52559;
+UPDATE `creature_template` SET `minlevel`=88, `maxlevel`=88, `exp`=3, `faction`=2146, `unit_flags`=32832, `unit_flags2`=83888128 WHERE `entry`=52560;
+UPDATE `creature_template` SET `minlevel`=88, `maxlevel`=88, `exp`=3, `faction`=2146, `unit_flags`=32832, `unit_flags2`=83888128 WHERE `entry`=52561;
+
+UPDATE `creature_template` SET `minlevel`=88, `maxlevel`=88, `exp`=3, `faction`=2146 WHERE `entry`=54340;
+UPDATE `creature_template` SET `minlevel`=88, `maxlevel`=88, `exp`=3, `faction`=2146 WHERE `entry`=54341;
+UPDATE `creature_template` SET `minlevel`=88, `maxlevel`=88, `exp`=3, `faction`=2146 WHERE `entry`=54342;
+
+UPDATE `creature_template` SET `minlevel`=88, `maxlevel`=88, `exp`=3, `faction`=2146 WHERE `entry`=53773;
+UPDATE `creature_template` SET `minlevel`=88, `maxlevel`=88, `exp`=3, `faction`=2146 WHERE `entry`=53774;
+UPDATE `creature_template` SET `minlevel`=88, `maxlevel`=88, `exp`=3, `faction`=2146 WHERE `entry`=53775;
+
+UPDATE `creature_template` SET `minlevel`=88, `maxlevel`=88, `exp`=3, `faction`=2146 WHERE `entry`=54193;
+UPDATE `creature_template` SET `minlevel`=88, `maxlevel`=88, `exp`=3, `faction`=2146 WHERE `entry`=54194;
+UPDATE `creature_template` SET `minlevel`=88, `maxlevel`=88, `exp`=3, `faction`=2146 WHERE `entry`=54195;
+
+UPDATE `creature_template` SET `minlevel`=88, `maxlevel`=88, `exp`=3, `faction`=2146 WHERE `entry`=54200;
+UPDATE `creature_template` SET `minlevel`=88, `maxlevel`=88, `exp`=3, `faction`=2146 WHERE `entry`=54201;
+UPDATE `creature_template` SET `minlevel`=88, `maxlevel`=88, `exp`=3, `faction`=2146 WHERE `entry`=54202;
+
+UPDATE `creature_template` SET `minlevel`=88, `maxlevel`=88, `exp`=3, `faction`=2109, `unit_flags`=32768 WHERE `entry`=53979;
+UPDATE `creature_template` SET `minlevel`=88, `maxlevel`=88, `exp`=3, `faction`=2109, `unit_flags`=32768 WHERE `entry`=54079;
+UPDATE `creature_template` SET `minlevel`=88, `maxlevel`=88, `exp`=3, `faction`=2109, `unit_flags`=32768 WHERE `entry`=54080;
