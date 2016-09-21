4deeee66bda390d2b35b64aaeb5924dac4170e10
treeston <treeston.mmoc@gmail.com>
2016-09-21 16:49:19 +0200

    d3214a0 follow-up for code style.

--- src/server/game/Movement/MovementGenerators/SplineChainMovementGenerator.cpp
+++ src/server/game/Movement/MovementGenerators/SplineChainMovementGenerator.cpp
@@ -142,10 +142,12 @@ SplineChainResumeInfo SplineChainMovementGenerator::GetResumeInfo(Unit const* me
 /* static */ void SplineChainMovementGenerator::GetResumeInfo(Unit const* me, SplineChainResumeInfo& info)
 {
     if (MovementGenerator const* activeGen = me->GetMotionMaster()->GetMotionSlot(MOTION_SLOT_ACTIVE))
+    {
         if (activeGen->GetMovementGeneratorType() == SPLINE_CHAIN_MOTION_TYPE)
         {
             info = reinterpret_cast<SplineChainMovementGenerator const*>(activeGen)->GetResumeInfo(me);
             return;
         }
+    }
     info.Chain = nullptr;
 }
