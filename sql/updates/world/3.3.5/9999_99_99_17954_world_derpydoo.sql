d6d5b756095d939b20db10697aedb256b66ca253
Aokromes <Aokromes@users.noreply.github.com>
2016-09-26 08:08:50 +0200

    Update and rename derpydoo.sql to 9999_99_99_17954_world_derpydoo.sql

--- sql/updates/world/3.3.5/9999_99_99_17954_world_derpydoo.sql
+++ sql/updates/world/3.3.5/9999_99_99_17954_world_derpydoo.sql
@@ -1,4 +1,10 @@
 -- Battle for the Undercity - Horde
+DROP TABLE IF EXISTS `location_scripts`;
+CREATE TABLE `location_scripts`
+(
+`zone_entry` INT(11) NOT NULL DEFAULT '0',
+`ScriptName` CHAR(64) NOT NULL DEFAULT '',
+UNIQUE KEY `zone_entry` (`zone_entry`, `ScriptName`)) ENGINE=INNODB DEFAULT CHARSET=utf8;
 
 DELETE FROM `location_scripts` WHERE `zone_entry` = 85;
 INSERT INTO `location_scripts` VALUES
