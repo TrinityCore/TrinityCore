ca4daeb70ebae2589d6db0c0fdc61d0a1eb1759e
Vincent-Michael <vincent_michael@gmx.de>
2016-09-27 23:24:39 +0200

    DB/Spells: Updated spell_target_position data from 7.0.3

---
+++ sql/updates/world/6.x/2016_09_27_01_world.sql
@@ -0,0 +1,69 @@
+DELETE FROM `spell_target_position` WHERE (`ID`=212490 AND `EffectIndex`=0) OR (`ID`=212737 AND `EffectIndex`=0) OR (`ID`=211783 AND `EffectIndex`=0) OR (`ID`=217272 AND `EffectIndex`=0) OR (`ID`=132624 AND `EffectIndex`=0) OR (`ID`=225435 AND `EffectIndex`=0) OR (`ID`=204146 AND `EffectIndex`=0) OR (`ID`=47673 AND `EffectIndex`=0) OR (`ID`=206720 AND `EffectIndex`=0) OR (`ID`=204900 AND `EffectIndex`=0) OR (`ID`=204706 AND `EffectIndex`=0) OR (`ID`=220907 AND `EffectIndex`=0) OR (`ID`=212313 AND `EffectIndex`=0) OR (`ID`=210431 AND `EffectIndex`=0) OR (`ID`=211784 AND `EffectIndex`=0) OR (`ID`=219501 AND `EffectIndex`=1) OR (`ID`=193671 AND `EffectIndex`=0) OR (`ID`=218880 AND `EffectIndex`=0) OR (`ID`=220767 AND `EffectIndex`=0) OR (`ID`=212257 AND `EffectIndex`=0) OR (`ID`=212058 AND `EffectIndex`=0) OR (`ID`=207065 AND `EffectIndex`=0) OR (`ID`=211780 AND `EffectIndex`=0) OR (`ID`=211302 AND `EffectIndex`=0) OR (`ID`=210272 AND `EffectIndex`=0) OR (`ID`=206486 AND `EffectIndex`=0) OR (`ID`=193749 AND `EffectIndex`=1) OR (`ID`=196083 AND `EffectIndex`=0) OR (`ID`=218394 AND `EffectIndex`=0) OR (`ID`=223171 AND `EffectIndex`=0) OR (`ID`=210252 AND `EffectIndex`=1) OR (`ID`=209155 AND `EffectIndex`=0) OR (`ID`=209156 AND `EffectIndex`=0) OR (`ID`=209133 AND `EffectIndex`=0) OR (`ID`=208565 AND `EffectIndex`=0) OR (`ID`=208560 AND `EffectIndex`=0) OR (`ID`=221694 AND `EffectIndex`=0) OR (`ID`=223369 AND `EffectIndex`=0) OR (`ID`=223348 AND `EffectIndex`=0) OR (`ID`=223144 AND `EffectIndex`=0) OR (`ID`=228594 AND `EffectIndex`=0) OR (`ID`=197104 AND `EffectIndex`=0) OR (`ID`=207368 AND `EffectIndex`=0) OR (`ID`=192241 AND `EffectIndex`=1) OR (`ID`=199859 AND `EffectIndex`=2) OR (`ID`=220623 AND `EffectIndex`=0) OR (`ID`=193094 AND `EffectIndex`=0) OR (`ID`=210079 AND `EffectIndex`=0) OR (`ID`=207683 AND `EffectIndex`=0) OR (`ID`=227951 AND `EffectIndex`=0) OR (`ID`=224940 AND `EffectIndex`=1) OR (`ID`=224940 AND `EffectIndex`=0) OR (`ID`=224191 AND `EffectIndex`=0) OR (`ID`=216138 AND `EffectIndex`=0) OR (`ID`=201960 AND `EffectIndex`=0) OR (`ID`=201652 AND `EffectIndex`=0) OR (`ID`=201643 AND `EffectIndex`=0) OR (`ID`=227249 AND `EffectIndex`=0);
+INSERT INTO `spell_target_position` (`ID`, `EffectIndex`, `MapID`, `PositionX`, `PositionY`, `PositionZ`, `VerifiedBuild`) VALUES
+(212490, 0, 1469, 948.15, 1087.68, 17.16, 22624), -- Spell: Teleport Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(212737, 0, 1605, 100, -33.54, 64.6, 22624), -- Spell: Inferno Hawk Efffect: 28 (SPELL_EFFECT_SUMMON)
+(211783, 0, 1605, -519.58, 309.37, 112.81, 22624), -- Spell: Teleport Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(217272, 0, 1220, -828.72, 4371.78, 738.64, 22624), -- Spell: Teleport Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(132624, 0, 870, 917.64, 294.879, 506.095, 22624), -- Spell: Portal: Vale of Eternal Blossoms Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(225435, 0, 1220, 1758.49, 7160.07, 75.32, 22624), -- Spell: Town Portal: Kal'delar Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(204146, 0, 1469, 1007.14, 1143.47, 45.5, 22624), -- Spell: Summon Reward Chest FX Efffect: 28 (SPELL_EFFECT_SUMMON)
+(47673, 0, 230, 824.809, -176.166, -49.7551, 22624), -- Spell: Move Bind Sight Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(206720, 0, 1220, 4058.26, 4341.55, 674.34, 22624), -- Spell: Teleport from War of the Ancients (Highmountain) Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(204900, 0, 1515, 4175.39, -5344.18, 112.86, 22624), -- Spell: Champions: Summon Vehicle (Miner) Efffect: 28 (SPELL_EFFECT_SUMMON)
+(204706, 0, 1515, 4175.39, -5344.18, 112.86, 22624), -- Spell: Champion Vehicle: Teleport Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(220907, 0, 870, 3139.03, 657.78, 501.74, 22624), -- Spell: Travel to Pandaria Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(212313, 0, 1469, 1008.93, 1121.29, 33.43, 22624), -- Spell: Teleport Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(210431, 0, 646, 307.78, -177.17, 33.62, 22624), -- Spell: Teleport: The Twilight Throne Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(211784, 0, 646, 2336.96, 176.58, 180.06, 22624), -- Spell: Teleport Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(219501, 1, 646, 1011.39, 658.65, 156.67, 22624), -- Spell: Deepholm Fly-In - Cancel Scene Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(193671, 0, 1220, -440, 5343.16, 67.09, 22624), -- Spell: Basic Dimensional Rifting Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(218880, 0, 1220, 3137.77, 7312.89, 53.27, 22624), -- Spell: Black Rook Rumble - Teleport Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(220767, 0, 1604, 306.63, 1505.63, -33.59, 22624), -- Spell: Teleport: Niskara Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(212257, 0, 1469, 1076.26, 1127.02, 19.64, 22624), -- Spell: Teleport Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(212058, 0, 1602, -889.27, -188.05, 664.51, 22624), -- Spell: Slipstream Efffect: 28 (SPELL_EFFECT_SUMMON)
+(207065, 0, 1602, -774.15, -65.64, 639.93, 22624), -- Spell: Slipstream Efffect: 28 (SPELL_EFFECT_SUMMON)
+(211780, 0, 1602, -338.05, 14.8, 626.98, 22624), -- Spell: Teleport Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(211302, 0, 1515, 3614.52, -4666.6, 116.65, 22624), -- Spell: Champions: Summon Vehicle (Night Elf) Efffect: 28 (SPELL_EFFECT_SUMMON)
+(210272, 0, 1469, 823.76, 1087.22, 57.69, 22624), -- Spell: Ancient Safeguards Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(206486, 0, 1469, 836.16, 1035.4, 48.61, 22624), -- Spell: Astral Portal Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(196083, 0, 1503, 2458.65, 65.83, -155.58, 22624), -- Spell: Descend to Deepholm Efffect: 227 (SPELL_EFFECT_TELEPORT_TO_LFG_DUNGEON)
+(218394, 0, 646, 875.7, 533.34, 245.8, 22624), -- Spell: Travel to the Deepholm with Mylra Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(223171, 0, 1469, 834.07, 1046.17, 48.52, 22624), -- Spell: Summon Stormbeak Efffect: 28 (SPELL_EFFECT_SUMMON)
+(210252, 1, 1469, 836.16, 1035.4, 48.61, 22624), -- Spell: Astral Portal Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(209155, 0, 1600, -47.92, 794.45, 797.37, 22624), -- Spell: Summon Wave (Left) Efffect: 28 (SPELL_EFFECT_SUMMON)
+(209156, 0, 1600, -45.91, 811.65, 797.54, 22624), -- Spell: Summon Wave (Right) Efffect: 28 (SPELL_EFFECT_SUMMON)
+(209133, 0, 1600, -44.02, 802.24, 797.19, 22624), -- Spell: Summon Wave (Center) Efffect: 28 (SPELL_EFFECT_SUMMON)
+(208560, 0, 1600, -214.32, 808.81, 262.09, 22624), -- Spell: Summon Elevator Vehicle Efffect: 28 (SPELL_EFFECT_SUMMON)
+(221694, 0, 0, -6798.39, 4563.18, -604.92, 22624), -- Spell: Teleport to Vashj'ir Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(223369, 0, 1469, 812.33, 1053.75, 48.63, 22624), -- Spell: Summon Erunak's Bubble to Vashj'ir Efffect: 28 (SPELL_EFFECT_SUMMON)
+(223348, 0, 1469, 816.13, 1054.63, 50.96, 22624), -- Spell: Summon Bubble to Vashj'ir Efffect: 28 (SPELL_EFFECT_SUMMON)
+(223144, 0, 1469, 822.25, 1049.65, 48.54, 22624), -- Spell: Summon Graddoc Efffect: 28 (SPELL_EFFECT_SUMMON)
+(228594, 0, 1220, -862.16, 4623.23, 749.64, 22624), -- Spell: Teleport: Dalaran Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(197104, 0, 1, 1776.5, -4338.8, -7.48, 22624), -- Spell: Teleport: Orgrimmar Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(207368, 0, 1220, -884.08, 4690.21, 952.8, 22624), -- Spell: Teleport Violet Citadel Spire Up - Broken Isles Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(192241, 1, 1220, -838.31, 4317.46, 744.8, 22624), -- Spell: Teleport: Dalaran Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(220623, 0, 870, 3045.68, 49.42, 652.07, 22624), -- Spell: Travel to Pandaria Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(193094, 0, 1572, 824.32, 1094.25, 57.72, 22624), -- Spell: Summon Finale Vehicle Efffect: 28 (SPELL_EFFECT_SUMMON)
+(210079, 0, 1572, 383.42, 1403.67, 193, 22624), -- Spell: Travel to the Maelstrom with Mylra Efffect: 227 (SPELL_EFFECT_TELEPORT_TO_LFG_DUNGEON)
+(207683, 0, 1600, -629.42, 808.68, 247.67, 22624), -- Spell: Descend to the Throne of Tides Efffect: 227 (SPELL_EFFECT_TELEPORT_TO_LFG_DUNGEON)
+(227951, 0, 1220, -828.72, 4371.78, 738.64, 22624), -- Spell: Skip Broken Shore Introduction Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(224940, 1, 1220, 3288.65, 7322.53, 90.52, 22624), -- Spell: CHANGE FINAL WORLD TAB LOC TO 1220 Teleport from Ravencrest's Legacy Efffect: 219 (SPELL_EFFECT_219)
+(224940, 0, 1220, 3288.65, 7322.53, 90.52, 22624), -- Spell: CHANGE FINAL WORLD TAB LOC TO 1220 Teleport from Ravencrest's Legacy Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+(224191, 0, 1646, 3264.82, 7287.42, 90.77, 22624), -- Spell: Ravencrest's Legacy Launcher Efffect: 227 (SPELL_EFFECT_TELEPORT_TO_LFG_DUNGEON)
+(216138, 0, 1220, -525.93, 4683.16, 654.72, 22624); -- Spell: Teleport Efffect: 252 (SPELL_EFFECT_TELEPORT_UNITS)
+
+UPDATE `spell_target_position` SET `VerifiedBuild`=22624 WHERE (`ID`=54406 AND `EffectIndex`=0);
+UPDATE `spell_target_position` SET `PositionX`=901.068, `PositionY`=-143.939, `PositionZ`=-49.755, `VerifiedBuild`=22624 WHERE (`ID`=47523 AND `EffectIndex`=0);
+UPDATE `spell_target_position` SET `VerifiedBuild`=22624 WHERE (`ID`=93089 AND `EffectIndex`=0);
+UPDATE `spell_target_position` SET `VerifiedBuild`=22624 WHERE (`ID`=93088 AND `EffectIndex`=0);
+UPDATE `spell_target_position` SET `VerifiedBuild`=22624 WHERE (`ID`=93087 AND `EffectIndex`=0);
+UPDATE `spell_target_position` SET `VerifiedBuild`=22624 WHERE (`ID`=121856 AND `EffectIndex`=0);
+UPDATE `spell_target_position` SET `VerifiedBuild`=22624 WHERE (`ID`=53822 AND `EffectIndex`=0);
+UPDATE `spell_target_position` SET `VerifiedBuild`=22624 WHERE (`ID`=82903 AND `EffectIndex`=0);
+UPDATE `spell_target_position` SET `VerifiedBuild`=22624 WHERE (`ID`=82891 AND `EffectIndex`=0);
+UPDATE `spell_target_position` SET `PositionX`=9998.46, `PositionY`=-7106.55, `VerifiedBuild`=22624 WHERE (`ID`=121855 AND `EffectIndex`=0);
+UPDATE `spell_target_position` SET `VerifiedBuild`=22624 WHERE (`ID`=121861 AND `EffectIndex`=0);
+UPDATE `spell_target_position` SET `VerifiedBuild`=22624 WHERE (`ID`=81237 AND `EffectIndex`=0);
+UPDATE `spell_target_position` SET `VerifiedBuild`=22624 WHERE (`ID`=84513 AND `EffectIndex`=0);
+UPDATE `spell_target_position` SET `VerifiedBuild`=22624 WHERE (`ID`=49359 AND `EffectIndex`=0);
+
