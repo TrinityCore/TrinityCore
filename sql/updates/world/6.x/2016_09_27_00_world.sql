6499a6d9230f73c2fd98a7d79b649e71c71b6696
JaredNinja <github@exclusive-it.nl>
2016-09-27 08:38:47 +0200

    Core/Player: Allow graveyards to be specified for areas, not only zones
    Closes #14089
    Closes #18006

---
+++ sql/updates/world/6.x/2016_09_27_00_world.sql
@@ -0,0 +1,3 @@
+-- Changing subzone 6170 (Northshire, Elwynn). Was zone 12 before, not right.
+-- Part of my total fix for graveyard problems (https://github.com/TrinityCore/TrinityCore/issues/14089)
+UPDATE `graveyard_zone` SET `GhostZone`= 6170 WHERE `ID`= 105 AND `Faction`= 469;
