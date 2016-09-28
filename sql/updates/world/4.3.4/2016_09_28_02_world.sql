7c236c9fa280a820d6e03316fc9c2622c6b722aa
Aokromes <aokromes@gmail.com>
2016-09-28 03:56:16 +0200

    DB/Misc: Fix some startup errors

---
+++ sql/updates/world/4.3.4/2016_09_28_02_world.sql
@@ -0,0 +1,19 @@
+UPDATE `creature_template` SET `unit_flags2`=`unit_flags2`|524288 WHERE `entry`=57365;
+UPDATE `creature_template` SET `unit_flags2`=`unit_flags2`|524288 WHERE `entry`=57366;
+UPDATE `creature_template` SET `unit_flags2`=`unit_flags2`|524288 WHERE `entry`=57367;
+
+UPDATE `creature_template` SET `unit_flags2`=`unit_flags2`|524288 WHERE `entry`=57368;
+UPDATE `creature_template` SET `unit_flags2`=`unit_flags2`|524288 WHERE `entry`=57369;
+UPDATE `creature_template` SET `unit_flags2`=`unit_flags2`|524288 WHERE `entry`=57370;
+
+UPDATE `creature_template` SET `unit_flags2`=`unit_flags2`|524288 WHERE `entry`=57374;
+UPDATE `creature_template` SET `unit_flags2`=`unit_flags2`|524288 WHERE `entry`=57375;
+UPDATE `creature_template` SET `unit_flags2`=`unit_flags2`|524288 WHERE `entry`=57376;
+
+UPDATE `creature_template` SET `unit_flags2`=`unit_flags2`|524288 WHERE `entry`=57362;
+UPDATE `creature_template` SET `unit_flags2`=`unit_flags2`|524288 WHERE `entry`=57363;
+UPDATE `creature_template` SET `unit_flags2`=`unit_flags2`|524288 WHERE `entry`=57364;
+
+UPDATE `creature_template` SET `unit_flags2`=`unit_flags2`|526336 WHERE `entry`=55862;
+
+UPDATE `creature_template` SET `mechanic_immune_mask`=`mechanic_immune_mask`|536941137 WHERE `entry`=54229;
