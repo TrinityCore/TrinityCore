04ef5ef57176607eafb7cfb509f05f6b28a4ed29
Krudor <erikstrandberg93@hotmail.com>
2016-10-02 20:13:22 +0200

    Removed unnecessary table column from testing

--- sql/updates/characters/6.x/2016_07_xx_00_characters.sql
+++ sql/updates/characters/6.x/2016_07_xx_00_characters.sql
@@ -6,7 +6,6 @@
 DROP TABLE IF EXISTS `instance_scenario_progress`;
 CREATE TABLE IF NOT EXISTS `instance_scenario_progress` (
   `id` int(10) unsigned NOT NULL,
-  `scenarioStep` tinyint(3) unsigned NOT NULL,
   `criteria` int(10) unsigned NOT NULL,
   `counter` bigint(20) unsigned NOT NULL,
   `date` int(10) unsigned NOT NULL DEFAULT '0',
