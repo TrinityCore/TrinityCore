405167dafd4e08d2f4ae92f73ce55dcd7e7d97a9
ariel- <ariel-@users.noreply.github.com>
2016-10-02 15:49:29 -0300

    Core/Spell: Add Dual Wield skill dependency to Dual Wield effect, should remove the skill properly when unlearning spell.
    Closes #17196

--- src/server/game/Spells/SpellMgr.h
+++ src/server/game/Spells/SpellMgr.h
@@ -545,7 +545,7 @@ struct SpellLearnSkillNode
     uint16 maxvalue;                                        // 0  - max skill value for player level
 };
 
-typedef std::map<uint32, SpellLearnSkillNode> SpellLearnSkillMap;
+typedef std::unordered_map<uint32, SpellLearnSkillNode> SpellLearnSkillMap;
 
 struct SpellLearnSpellNode
 {
