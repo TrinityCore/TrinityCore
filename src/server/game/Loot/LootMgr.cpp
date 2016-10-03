fbbe1032454338c4daabe35e47427dc7c5fc3eb0
SnapperRy <snapperryen@gmail.com>
2016-10-03 23:44:51 +0200

    Core/Loot: limit unique items check in LootItem::AllowedForPlayer() to bind-when-picked-up items only.

--- src/server/game/Loot/LootMgr.cpp
+++ src/server/game/Loot/LootMgr.cpp
@@ -399,8 +399,8 @@ bool LootItem::AllowedForPlayer(Player const* player) const
     if (!(pProto->FlagsCu & ITEM_FLAGS_CU_IGNORE_QUEST_STATUS) && ((needs_quest || (pProto->StartQuest && player->GetQuestStatus(pProto->StartQuest) != QUEST_STATUS_NONE)) && !player->HasQuestForItem(itemid)))
         return false;
 
-    // Don't show unique items if player already has the maximum allowed quantity.
-    if (pProto->MaxCount && int32(player->GetItemCount(itemid, true)) >= pProto->MaxCount)
+    // Don't show bind-when-picked-up unique items if player already has the maximum allowed quantity.
+    if (pProto->Bonding == BIND_WHEN_PICKED_UP && pProto->MaxCount && int32(player->GetItemCount(itemid, true)) >= pProto->MaxCount)
         return false;
 
     return true;
