fbfd912d57afcfe27917c21a281345631483f17b
SnapperRy <snapperryen@gmail.com>
2016-10-03 23:33:44 +0200

    Core/Loot: do not show unique items if player already has the maximum allowed quantity.
    (cherry picked from commit 3a8e599adf355ebaa90d76d2e6eb632eda8f2997)
    
    Core/Loot: limit unique items check in LootItem::AllowedForPlayer() to bind-when-picked-up items only.
    
    (cherry picked from commit ab0fabeb10544f1ffeee7f12f169201625cf7bd6)

--- src/server/game/Loot/LootMgr.cpp
+++ src/server/game/Loot/LootMgr.cpp
@@ -401,6 +401,10 @@ bool LootItem::AllowedForPlayer(Player const* player) const
     if (!(pProto->FlagsCu & ITEM_FLAGS_CU_IGNORE_QUEST_STATUS) && ((needs_quest || (pProto->GetStartQuest() && player->GetQuestStatus(pProto->GetStartQuest()) != QUEST_STATUS_NONE)) && !player->HasQuestForItem(itemid)))
         return false;
 
+    // Don't show bind-when-picked-up unique items if player already has the maximum allowed quantity.
+    if (pProto->GetBonding() == BIND_ON_ACQUIRE && pProto->GetMaxCount() && int32(player->GetItemCount(itemid, true)) >= pProto->GetMaxCount())
+        return false;
+
     return true;
 }
 
