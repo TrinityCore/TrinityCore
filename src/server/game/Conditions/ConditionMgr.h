01c08af405f7d6c62229a0cd099fb63a1f485ea1
SnapperRy <snapperryen@gmail.com>
2016-10-04 18:47:58 +0200

    Core/Conditions: partially revert 723c253, replacing CONDITION_WINTERGRASP_HORDE with a ConditionScript.

--- src/server/game/Conditions/ConditionMgr.h
+++ src/server/game/Conditions/ConditionMgr.h
@@ -78,8 +78,7 @@ enum ConditionTypes
     CONDITION_PET_TYPE              = 45,                   // mask             0              0                  true if player has a pet of given type(s)
     CONDITION_TAXI                  = 46,                   // 0                0              0                  true if player is on taxi
     CONDITION_QUESTSTATE            = 47,                   // quest_id         state_mask     0                  true if player is in any of the provided quest states for the quest (1 = not taken, 2 = completed, 8 = in progress, 32 = failed, 64 = rewarded)
-    CONDITION_WINTERGRASP_HORDE     = 48,                   // 0                0              0                  true if Wintergrasp is enabled and held by horde
-    CONDITION_MAX                   = 49                    // MAX
+    CONDITION_MAX                   = 48                    // MAX
 };
 
 /*! Documentation on implementing a new ConditionSourceType:
