0f5f87add00ac884693a8a5dac79512318b1ef50
SnapperRy <snapperryen@gmail.com>
2016-10-04 13:29:12 +0200

    Core/Wintergrasp: fix victory quests.
    By Retriman, updates #7953.

--- src/server/game/Battlefield/Zones/BattlefieldWG.h
+++ src/server/game/Battlefield/Zones/BattlefieldWG.h
@@ -150,6 +150,12 @@ enum WintergraspAreaIds
     AREA_THE_CHILLED_QUAGMIRE       = 4589
 };
 
+enum WintergraspQuests
+{
+    QUEST_VICTORY_WINTERGRASP_A = 13181,
+    QUEST_VICTORY_WINTERGRASP_H = 13183
+};
+
 /*#########################
  *####### Graveyards ######
  *#########################*/
