a87d3398519da7366ad2af9fb4d3153e8cae11ad
Aokromes <Aokromes@users.noreply.github.com>
2016-10-05 23:23:50 +0200

    Update 2016_10_05_11_world_335.sql

--- sql/updates/world/3.3.5/2016_10_05_11_world_335.sql
+++ sql/updates/world/3.3.5/2016_10_05_11_world_335.sql
@@ -2,6 +2,7 @@
 UPDATE `gameobject_template` SET `ScriptName`='go_brewfest_music' WHERE `entry`=186221;
 
 -- Add missing Brewfest Music Doodad spawns
+SET @OGUID:=6037;
 DELETE FROM `gameobject` WHERE `guid` BETWEEN @OGUID+0 AND @OGUID+1;
 INSERT INTO `gameobject` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES
 (@OGUID+0 , 186221, 530, 1, 1, -1897.498, 5560.614, -12.34483, 4.363323, 0, 0, 0, 0, 120, 255, 1),
