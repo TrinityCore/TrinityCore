ad44223e9c30f8e61ff1a26e28d79b1482b9475a
Killyana <morphone1@gmail.com>
2016-10-15 19:03:08 +0200

    DB: Update remaining phases

---
+++ sql/updates/world/4.3.4/2016_10_15_01_world.sql
@@ -0,0 +1,6 @@
+-- 
+UPDATE `gameobject` SET `phaseMask`=1, `PhaseId`=169 WHERE `map` IN (649);
+UPDATE `gameobject` SET `phaseMask`=1, `PhaseId`=169 WHERE `guid` BETWEEN 76251 AND 76263;
+UPDATE `gameobject` SET `phaseMask`=1, `PhaseId`=169 WHERE `guid` BETWEEN 76329 AND 76343;
+UPDATE `gameobject` SET `PhaseGroup`=376 WHERE `PhaseId`=0 AND `PhaseGroup`=0 AND `phaseMask`=511;
+UPDATE `gameobject` SET `PhaseGroup`=393 WHERE `PhaseId`=0 AND `PhaseGroup`=0 AND `phaseMask`=510;
