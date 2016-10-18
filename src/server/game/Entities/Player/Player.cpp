142bf9d5f6f75b98d1c8075e4c5580052614571b
Shauren <shauren.trinity@gmail.com>
2016-10-19 00:25:07 +0200

    Core/Spells: Fixed a crash happening when casting a spell with effects that only exist on some difficulties but not on others (for example, effect 2 of 98175)

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -1781,9 +1781,9 @@ bool Player::IsImmunedToSpellEffect(SpellInfo const* spellInfo, uint32 index) co
         return false;
 
     // players are immune to taunt (the aura and the spell effect).
-    if (spellInfo->GetEffect(index)->IsAura(SPELL_AURA_MOD_TAUNT))
+    if (effect->IsAura(SPELL_AURA_MOD_TAUNT))
         return true;
-    if (spellInfo->GetEffect(index)->IsEffect(SPELL_EFFECT_ATTACK_ME))
+    if (effect->IsEffect(SPELL_EFFECT_ATTACK_ME))
         return true;
 
     return Unit::IsImmunedToSpellEffect(spellInfo, index);
