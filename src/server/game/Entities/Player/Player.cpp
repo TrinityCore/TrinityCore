3dce7a02ffaea099cd886b2f6dcea2979d69f545
Palabola <Palabola@users.noreply.github.com>
2016-10-21 17:51:51 +0200

    Core/Player: Fix Quest + Phase issue
    Solve problem when sometimes Phase doesn't update at Quest complete/reward.

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -15790,6 +15790,7 @@ void Player::SendQuestUpdate(uint32 questId)
 
     UpdateForQuestWorldObjects();
     SendUpdatePhasing();
+    UpdateAreaAndZonePhase();
 }
 
 QuestGiverStatus Player::GetQuestDialogStatus(Object* questgiver)
