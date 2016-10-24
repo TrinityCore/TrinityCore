6edf08c7647c68860b72e51cbab968e7774de823
Aokromes <aokromes@gmail.com>
2016-10-24 19:21:49 +0200

    revert this

--- sql/updates/world/4.3.4/2016_10_24_26_world_from_335_was_2016_10_08_07_world_335.sql
+++ sql/updates/world/4.3.4/2016_10_24_26_world_from_335_was_2016_10_08_07_world_335.sql
@@ -1,3 +1,5 @@
+/*
 DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_hun_lock_and_load_dummy';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (56342, 'spell_hun_lock_and_load_dummy');
+*/
--- sql/updates/world/4.3.4/2016_10_24_46_world_from_335_was_2016_10_14_05_world.sql
+++ sql/updates/world/4.3.4/2016_10_24_46_world_from_335_was_2016_10_14_05_world.sql
@@ -1,6 +1,6 @@
 UPDATE `creature_template` SET `unit_flags`=33554432 WHERE `entry`=38222;
 UPDATE `creature_template` SET `speed_run`=0.571429 WHERE `entry`=38222;
-UPDATE `spell_dbc` SET `DurationIndex`=29, `Effect1`=6, `EffectImplicitTargetA1`=1, `EffectApplyAuraName1`=61, `EffectBasePoints1`=100 WHERE `Id`=71290;
+-- UPDATE `spell_dbc` SET `DurationIndex`=29, `Effect1`=6, `EffectImplicitTargetA1`=1, `EffectApplyAuraName1`=61, `EffectBasePoints1`=100 WHERE `Id`=71290;
 
 DELETE FROM `linked_respawn`WHERE `guid`=201129 AND `linkedGuid`=201129;
 DELETE FROM `creature_template_addon` WHERE `entry` IN (38009,38010,38136);
