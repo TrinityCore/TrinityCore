2da34a4dbb3e6c71f03b0a1463d8fdec80771b93
SnapperRy <snapperryen@gmail.com>
2016-10-27 16:43:27 +0200

    Forgot a wild comment, sorry.

--- sql/updates/world/3.3.5/2016_10_27_04_world.sql
+++ sql/updates/world/3.3.5/2016_10_27_04_world.sql
@@ -1,7 +1,7 @@
 --
 DELETE FROM `smart_scripts` WHERE `entryorguid` IN (27056, 2705600, 2705601, 2705602);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(27056, 0, 0, 0, 1, 0, 100, 0, 10000, 20000/*60000*/, 3600000, 3600000, 87, 2705600, 2705601, 2705602, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Sentinel Sweetspring - Out of Combat - Call random script"),
+(27056, 0, 0, 0, 1, 0, 100, 0, 10000, 20000, 3600000, 3600000, 87, 2705600, 2705601, 2705602, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Sentinel Sweetspring - Out of Combat - Call random script"),
 (2705600, 9, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Sentinel Sweetspring - On Script - Talk 0"),
 (2705600, 9, 1, 0, 0, 0, 100, 0, 5000, 5000, 0, 0, 1, 0, 0, 0, 0, 0, 0, 19, 27055, 0, 0, 0, 0, 0, 0, "Sentinel Sweetspring - On Script - Talk 0 (Sentinel Amberline)"),
 (2705601, 9, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "Sentinel Sweetspring - On Script - Talk 1"),
