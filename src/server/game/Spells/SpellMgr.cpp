0c59f2f0f79b9965242b1baefe16e451d13a3af9
ariel- <ariel-@users.noreply.github.com>
2016-10-28 01:17:38 -0300

    Core/Scripts: fix Cobra Strikes

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -2755,10 +2755,6 @@ void SpellMgr::LoadSpellInfoCorrections()
             case 44544: // Fingers of Frost
                 spellInfo->Effects[EFFECT_0].SpellClassMask = flag96(685904631, 1151048, 0);
                 break;
-            case 53257: // Cobra Strikes
-                spellInfo->ProcCharges = 2;
-                spellInfo->StackAmount = 0;
-                break;
             case 49224: // Magic Suppression - DK
             case 49610: // Magic Suppression - DK
             case 49611: // Magic Suppression - DK
