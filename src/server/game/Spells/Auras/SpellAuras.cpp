697147a596515c82817a161e7a9a5f07cdf9a8f3
ariel- <ariel-@users.noreply.github.com>
2016-10-30 15:20:54 -0300

    Core/Auras: fix elixir aura saving

--- src/server/game/Spells/Auras/SpellAuras.cpp
+++ src/server/game/Spells/Auras/SpellAuras.cpp
@@ -987,8 +987,8 @@ bool Aura::CanBeSaved() const
     if (IsUsingCharges() && !GetCharges())
         return false;
 
-    // don't save auras triggered by items, they'll be recasted on login if necessary
-    if (GetCastItemGUID())
+    // don't save permanent auras triggered by items, they'll be recasted on login if necessary
+    if (GetCastItemGUID() && IsPermanent())
         return false;
 
     return true;
