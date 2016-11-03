9439f45117607dd0d2a1208af952ad6109976683
Aokromes <aokromes@gmail.com>
2016-11-03 16:16:00 +0100

    DB/Misc: Rename files

---
+++ sql/updates/world/4.3.4/2016_11_03_04_world_from_335_2016_11_03_02_world_335.sql
@@ -0,0 +1,6 @@
+-- Maelstrom Weapon
+/*
+DELETE FROM `spell_proc` WHERE `SpellId`=53817;
+INSERT INTO `spell_proc` (`SpellId`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `ProcFlags`, `SpellTypeMask`, `SpellPhaseMask`, `HitMask`, `AttributesMask`, `ProcsPerMinute`, `Chance`, `Cooldown`, `Charges`) VALUES
+(53817, 0, 11, 0x000001C3, 0x00008000, 0x00000000, 0, 0x0, 0x1, 0x0, 0x8, 0, 0, 0, 0);
+*/
