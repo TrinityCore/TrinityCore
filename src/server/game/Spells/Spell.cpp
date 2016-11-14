425f6c3c34b72190e481ba89fd4757df238475d8
ariel- <ariel-@users.noreply.github.com>
2016-11-14 15:22:41 -0300

    Core/Spell: perform charmer check against player controlling unit. Fixes spellcasts while charmed

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -5768,8 +5768,12 @@ SpellCastResult Spell::CheckCasterAuras(uint32* param1) const
 
     // Get unit state
     uint32 const unitflag = m_caster->GetUInt32Value(UNIT_FIELD_FLAGS);
-    if (m_caster->GetCharmerGUID() && CheckCasterNotImmunedCharmAuras(param1))
-        result = SPELL_FAILED_CHARMED;
+    if (m_caster->GetCharmerGUID())
+    {
+        if (Unit* charmer = m_caster->GetCharmer())
+            if (charmer->GetUnitBeingMoved() != m_caster && CheckCasterNotImmunedCharmAuras(param1))
+                result = SPELL_FAILED_CHARMED;
+    }
     else if (unitflag & UNIT_FLAG_STUNNED && !usableWhileStunned && CheckCasterNotImmunedStunAuras(param1))
         result = SPELL_FAILED_STUNNED;
     else if (unitflag & UNIT_FLAG_SILENCED && m_spellInfo->PreventionType == SPELL_PREVENTION_TYPE_SILENCE && CheckCasterNotImmunedSilenceAuras(param1))
