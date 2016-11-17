8b8fe3aa429394ccf93f94b577fd0ca6d810ab0d
HelloKitty <andrew.blakely@ymail.com>
2016-11-17 07:49:51 -0600

    Remove DB name from sql query

--- sql/updates/world/3.3.5/2016_11_16_00_world.sql
+++ sql/updates/world/3.3.5/2016_11_16_00_world.sql
@@ -1,2 +1,2 @@
 -- Adds a reference to the spell_vael_burning_adrenaline script for Burning Adrenaline (Vael Fight)
-INSERT INTO `world`.`spell_script_names` (`spell_id`, `ScriptName`) VALUES ('18173', 'spell_vael_burning_adrenaline');
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('18173', 'spell_vael_burning_adrenaline');
