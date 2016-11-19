379cd2756649190e4fd8859e2f6b8692b1488cc3
HelloKitty <andrew.blakely@ymail.com>
2016-11-19 11:19:10 -0600

    Change SQL delete to be by ScriptName

--- sql/updates/world/3.3.5/2016_11_19_01_world_335.sql
+++ sql/updates/world/3.3.5/2016_11_19_01_world_335.sql
@@ -1,5 +1,5 @@
 -- Adds a script for Naxxramas Anub corpse scarab summoning spell 29105 28864
-DELETE FROM 'spell_script_names' WHERE 'spell_id' IN (29105, 28864);
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+DELETE FROM 'spell_script_names' WHERE 'ScriptName' = 'spell_summon_corpse_scarabs';
+INSERT INTO 'spell_script_names' ('spell_id', 'ScriptName') VALUES 
 (29105, 'spell_summon_corpse_scarabs'),
-(28864, 'spell_summon_corpse_scarabs ');
+(28864, 'spell_summon_corpse_scarabs');
