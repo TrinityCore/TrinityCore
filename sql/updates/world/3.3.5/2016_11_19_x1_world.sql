ad916c8c56b77d6a6f7feec79312682e99f215dc
Rochet2 <tqbattlenet@gmail.com>
2016-11-19 17:19:55 +0200

    Fix documentation and trinity_string for some existing commands

---
+++ sql/updates/world/3.3.5/2016_11_19_x1_world.sql
@@ -0,0 +1,2 @@
+UPDATE `command` SET `help`='Syntax: .gobject turn [guid|link] [oz [oy [ox]]]\r\n\r\nSet the orientation of the gameobject to player\'s orientation or the given orientation.' WHERE  `name`='gobject turn';
+UPDATE `trinity_string` SET `content_default`='>> Add Game Object \'%u\' (%s) (GUID: %u) added at \'%f %f %f\'.' WHERE  `entry`=525;
