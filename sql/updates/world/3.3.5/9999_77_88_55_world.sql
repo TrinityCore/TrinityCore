d53691ce93d443a31c31028899e58726169e3f48
Keader <keader.android@gmail.com>
2016-11-20 23:28:07 -0200

    fixed a text (ty again offl)

--- sql/updates/world/3.3.5/9999_77_88_55_world.sql
+++ sql/updates/world/3.3.5/9999_77_88_55_world.sql
@@ -11,3 +11,5 @@ DELETE FROM `creature_text` WHERE `entry`=22887 AND (`groupid` IN(5,6) OR (`grou
 INSERT INTO `creature_text` (`entry`,`groupid`,`id`,`text`,`type`,`language`,`probability`,`emote`,`duration`,`sound`,`BroadcastTextId`,`TextRange`,`comment`) VALUES
 (22887,4,1,'My patience has run out! Die! Die!',14,0,100,0,0,11458,21096,0,'najentus SAY_ENRAGE2'),
 (22887,5,0,'Lord Illidan will... crush you!',14,0,100,0,0,11459,21093,0,'najentus SAY_DEATH');
+
+UPDATE `creature_text` SET `text`='Stick around...',`BroadcastTextId`=21089 WHERE `entry`=22887 AND `groupid`=1 AND `id`=0;
