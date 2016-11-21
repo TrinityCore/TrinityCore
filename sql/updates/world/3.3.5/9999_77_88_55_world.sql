264a979b637da239f34bd6d076dc316e84950797
tkrokli <tkrokli@users.noreply.github.com>
2016-11-21 22:22:18 +0100

    Update 9999_77_88_55_world.sql
    Updated creature_text comments with full NPC name.

--- sql/updates/world/3.3.5/9999_77_88_55_world.sql
+++ sql/updates/world/3.3.5/9999_77_88_55_world.sql
@@ -9,7 +9,7 @@ INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 
 DELETE FROM `creature_text` WHERE `entry`=22887 AND (`groupid` IN(5,6) OR (`groupid`=4 AND `id`=1));
 INSERT INTO `creature_text` (`entry`,`groupid`,`id`,`text`,`type`,`language`,`probability`,`emote`,`duration`,`sound`,`BroadcastTextId`,`TextRange`,`comment`) VALUES
-(22887,4,1,'My patience has run out! Die! Die!',14,0,100,0,0,11458,21096,0,'najentus SAY_ENRAGE2'),
-(22887,5,0,'Lord Illidan will... crush you!',14,0,100,0,0,11459,21093,0,'najentus SAY_DEATH');
+(22887,4,1,'My patience has run out! Die! Die!',14,0,100,0,0,11458,21096,0,"High Warlord Naj'entus SAY_ENRAGE2"),
+(22887,5,0,'Lord Illidan will... crush you!',14,0,100,0,0,11459,21093,0,"High Warlord Naj'entus SAY_DEATH");
 
 UPDATE `creature_text` SET `text`='Stick around...',`BroadcastTextId`=21089 WHERE `entry`=22887 AND `groupid`=1 AND `id`=0;
