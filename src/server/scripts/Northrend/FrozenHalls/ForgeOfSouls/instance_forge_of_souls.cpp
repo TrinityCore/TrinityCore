acb680d4534404d7695d3a130dce14915395993a
Keader <keader.android@gmail.com>
2016-11-23 12:45:21 -0200

    Core/Scripts: Added Boundary in Forge of Souls (#18226)
    Core/Scripts: Added Boundary in Forge of Souls
    
    (cherry picked from commit 0fa4d8a43ff74107acb042715a8febf55fec78be)

--- src/server/scripts/Northrend/FrozenHalls/ForgeOfSouls/instance_forge_of_souls.cpp
+++ src/server/scripts/Northrend/FrozenHalls/ForgeOfSouls/instance_forge_of_souls.cpp
@@ -22,6 +22,13 @@
 #include "Map.h"
 #include "Player.h"
 
+
+BossBoundaryData const boundaries =
+{
+    { DATA_BRONJAHM,  new CircleBoundary(Position(5297.3f, 2506.45f), 100.96) },
+    { DATA_DEVOURER_OF_SOULS, new ParallelogramBoundary(Position(5663.56f, 2570.53f), Position(5724.39f, 2520.45f), Position(5570.36f, 2461.42f)) }
+};
+
 class instance_forge_of_souls : public InstanceMapScript
 {
     public:
@@ -33,6 +40,7 @@ class instance_forge_of_souls : public InstanceMapScript
             {
                 SetHeaders(DataHeader);
                 SetBossNumber(EncounterCount);
+                LoadBossBoundaries(boundaries);
 
                 teamInInstance = 0;
             }
