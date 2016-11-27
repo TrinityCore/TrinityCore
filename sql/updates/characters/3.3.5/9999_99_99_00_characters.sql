8505bf1da8fd64e17c62b2a419fdac28509b7be9
iridinite <codingcuddlewolf@gmail.com>
2016-11-27 12:26:43 +0100

    Remove collate from sql update

--- sql/updates/characters/3.3.5/9999_99_99_00_characters.sql
+++ sql/updates/characters/3.3.5/9999_99_99_00_characters.sql
@@ -3,6 +3,4 @@ CREATE TABLE `auctionbidders` (
 	`id` INT(10) UNSIGNED NOT NULL DEFAULT '0',
 	`bidder` INT(10) UNSIGNED NOT NULL DEFAULT '0',
 	PRIMARY KEY (`id`, `bidder`)
-)
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
