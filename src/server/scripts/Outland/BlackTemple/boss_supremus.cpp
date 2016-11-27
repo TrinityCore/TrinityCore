6d3d63d47c7990f4bac3899e2966b84fa0f325f6
Keader <keader.android@gmail.com>
2016-11-27 21:25:34 -0200

    Core/Scripts: Partial Revert in 87977b00d3f040c5a52b5bba6fc5e04d6a4d7385 misunderstand concept. (#18334)
    Sorry for the mess

--- src/server/scripts/Outland/BlackTemple/boss_supremus.cpp
+++ src/server/scripts/Outland/BlackTemple/boss_supremus.cpp
@@ -178,9 +178,6 @@ public:
                 default:
                     break;
             }
-
-            if (me->HasUnitState(UNIT_STATE_CASTING))
-                return;
         }
 
     };
