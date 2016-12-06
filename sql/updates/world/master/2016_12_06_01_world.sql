6b82e24831a5dfde5580065b6a0be8f021aa12c7
Unknown-1000 <Unknown-1000@users.noreply.github.com>
2016-12-06 22:12:07 +0600

    Create 2016_12_06_01_world.sql

---
+++ sql/updates/world/master/2016_12_06_01_world.sql
@@ -0,0 +1,4 @@
+-- spell_dru_might_of_ursoc
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_dru_might_of_ursoc';
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(105737,'spell_dru_might_of_ursoc');
