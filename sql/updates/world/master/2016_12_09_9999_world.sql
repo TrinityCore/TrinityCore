df200e7fcb0d1409440f962cc2ee51ae5b070eac
Unknown-1000 <Unknown-1000@users.noreply.github.com>
2016-12-09 19:42:17 +0600

    Create 2016_12_09_9999_world.sql
    Sql part.

---
+++ sql/updates/world/master/2016_12_09_9999_world.sql
@@ -0,0 +1,4 @@
+-- spell_gen_rasthe_unholy_resurrection
+ DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_gen_rasthe_unholy_resurrection';
+ INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+ (180457,'spell_gen_rasthe_unholy_resurrection');
