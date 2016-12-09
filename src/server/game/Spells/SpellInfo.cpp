343c2fbf526a279964ca7d1927a2e245821607c3
ariel- <ariel-@users.noreply.github.com>
2016-12-09 14:08:36 -0300

    Revert "Core/Spell: Magic Absorption Calculation"
    This reverts commit fb52e69169a5881c67011f38c4d0448946856779.

--- src/server/game/Spells/SpellInfo.cpp
+++ src/server/game/Spells/SpellInfo.cpp
@@ -419,8 +419,7 @@ int32 SpellEffectInfo::CalcValue(Unit const* caster, int32 const* bp, Unit const
             level = int32(_spellInfo->MaxLevel);
         else if (level < int32(_spellInfo->BaseLevel))
             level = int32(_spellInfo->BaseLevel);
-        if (!_spellInfo->IsPassive())
-           level -= int32(_spellInfo->SpellLevel);
+        level -= int32(_spellInfo->SpellLevel);
         basePoints += int32(level * basePointsPerLevel);
     }
 
