452ef146f41a9bf06ca2bfb8dc9f26ca20b4fb83
ariel- <ariel-@users.noreply.github.com>
2016-12-15 10:01:34 -0300

    Core/Spell: add an exception for Cyclone ignoring it's own immunity using dispel type
    - Unsure of what is the correct check, most generic thing I could make
    
    Closes #18479

--- src/server/game/Spells/SpellInfo.cpp
+++ src/server/game/Spells/SpellInfo.cpp
@@ -1293,7 +1293,7 @@ bool SpellInfo::CanPierceImmuneAura(SpellInfo const* auraSpellInfo) const
         if (!auraSpellInfo ||
             (auraSpellInfo->Mechanic != MECHANIC_IMMUNE_SHIELD &&
                 auraSpellInfo->Mechanic != MECHANIC_INVULNERABILITY &&
-                (auraSpellInfo->Mechanic != MECHANIC_BANISH || IsRankOf(auraSpellInfo)))) // Banish shouldn't be immune to itself
+                (auraSpellInfo->Mechanic != MECHANIC_BANISH || (IsRankOf(auraSpellInfo) && auraSpellInfo->Dispel != DISPEL_NONE)))) // Banish shouldn't be immune to itself, but Cyclone should
             return true;
     }
 
