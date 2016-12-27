192e20074968e1c1cce3174ede3f2e75eefad4be
Keader <keader.android@gmail.com>
2016-12-27 10:12:41 -0300

    Core/Scripts: Blood Prince Council refactoring and fixed reported issues

---
+++ sql/updates/world/3.3.5/2020_22_48_30_world.sql
@@ -0,0 +1,11 @@
+DELETE FROM `linked_respawn` WHERE `guid`IN(201369,201493,201577,201286);
+DELETE FROM `creature` WHERE `id` IN(37973,37972,37970);
+DELETE FROM `creature_summon_groups` WHERE `summonerId`=38008; -- Blood orb controller
+INSERT INTO `creature_summon_groups` (`summonerId`,`summonerType`,`groupId`,`entry`,`position_x`,`position_y`,`position_z`,`orientation`,`summonType`,`summonTime`) VALUES
+(38008,0,1,37973,4682.89,2755.11,364.17,3.14159 ,6,3600000), -- Taldaram
+(38008,0,1,37972,4682.73,2783.42,364.17,3.14159,6,3600000), -- Keleseth
+(38008,0,1,37970,4680.29,2769.24,364.17,3.14159,6,3600000); -- Valanar
+
+DELETE FROM `spell_proc` WHERE `SpellId`=72059;
+INSERT INTO `spell_proc` (`SpellId`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `ProcFlags`, `SpellTypeMask`, `SpellPhaseMask`, `HitMask`, `AttributesMask`, `ProcsPerMinute`, `Chance`, `Cooldown`, `Charges`) VALUES
+(72059, 0, 0, 0x00000000, 0x00000000, 0x00000000, 0, 0x1, 0x2, 0x00000400, 0x2, 0, 0, 0, 0);
