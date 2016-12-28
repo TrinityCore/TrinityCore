ffb8b463d1062c9f59e255c227e5e35970c45dee
Aokromes <aokromes@gmail.com>
2016-12-28 16:02:55 +0100

    Core/Misc: Mark some rbac/trinity_string entries as used

--- src/server/game/Miscellaneous/Language.h
+++ src/server/game/Miscellaneous/Language.h
@@ -892,7 +892,11 @@ enum TrinityStrings
     LANG_ACCOUNT_INVALID_BNET_NAME      = 1029, // 6.x ONLY
     LANG_ACCOUNT_USE_BNET_COMMANDS      = 1030, // 6.x enum value name but different text in DB
     LANG_ACCOUNT_PASS_TOO_LONG          = 1031,
-    // Room for more level 4              1032-1099 not used
+    LANG_ACCOUNT_CREATED_BNET_WITH_GAME = 1032, // master branch ONLY
+    LANG_ACCOUNT_CREATED_BNET           = 1033, // master branch ONLY
+    LANG_ACCOUNT_BNET_LIST_HEADER       = 1034, // master branch ONLY
+    LANG_ACCOUNT_BNET_LIST_NO_ACCOUNTS  = 1035, // master branch ONLY
+    // Room for more level 4              1036-1099 not used
 
     // Level 3 (continue)
     LANG_ACCOUNT_SETADDON               = 1100,
