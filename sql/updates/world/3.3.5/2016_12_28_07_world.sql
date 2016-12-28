8887e017a2d105f305966bd5f6ae36aa3fa38977
Killyana <morphone1@gmail.com>
2016-12-28 13:58:10 +0100

    DB: Fix a DB error
    By Rushor

---
+++ sql/updates/world/3.3.5/2016_12_28_07_world.sql
@@ -0,0 +1,2 @@
+-- 
+UPDATE `smart_scripts` SET `action_type`=11, `action_param1`=30444, `action_param2`=0, `action_param3`=0, `target_type`=7, `target_x`=0, `target_y`=0, `target_z`=0, `target_o`=0, `comment`='Totem of Yor - On Quest \'Totem of Vark\' Taken - Cast \'Stillpine Ancestor Yor TRIGGER\'' WHERE  `entryorguid`=17363 AND `source_type`=0 AND `id`=0 AND `link`=0;
