2b02cff5a291fb8cf49e63a8881a2a5c7df1ce8d
Aokromes <aokromes@gmail.com>
2016-12-28 17:04:56 +0100

    DB/Misc: Fix some startup errors

---
+++ sql/updates/world/4.3.4/2016_12_28_52_world.sql
@@ -0,0 +1,5 @@
+-- 
+UPDATE `smart_scripts` SET `entryorguid`=-303316 WHERE `entryorguid`=-13579;
+UPDATE `smart_scripts` SET `entryorguid`=-303311 WHERE `entryorguid`=-19361;
+UPDATE `smart_scripts` SET `entryorguid`=-303312 WHERE `entryorguid`=-19402;
+UPDATE `smart_scripts` SET `entryorguid`=-303315 WHERE `entryorguid`=-19412;
