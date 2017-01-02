e3700df8fa8b92fbd70a32aa0c63d138d02c569f
tkrokli <tkrokli@users.noreply.github.com>
2017-01-02 21:38:05 +0100

    Update Copyright notice for 2017 in 4 more files
    4 files missed in the global search & replace performed
    in commit 6a51a88767247ce9fc85fb9d1bcbb5febf1598a2
    
    Also some corrections & whitespace changes in /doc/UnixInstall.txt

--- revision_data.h.in.cmake
+++ revision_data.h.in.cmake
@@ -9,7 +9,7 @@
  #define _MYSQL_EXECUTABLE          R"(@MYSQL_EXECUTABLE@)"
  #define _FULL_DATABASE             "TDB_full_world_335.62_2016_10_17.sql"
  #define VER_COMPANYNAME_STR        "TrinityCore Developers"
- #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2016 TrinityCore"
+ #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2017 TrinityCore"
  #define VER_FILEVERSION            0,0,0
  #define VER_FILEVERSION_STR        "@rev_hash@ @rev_date@ (@rev_branch@ branch)"
  #define VER_PRODUCTVERSION         VER_FILEVERSION
