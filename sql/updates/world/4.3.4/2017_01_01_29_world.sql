2d1b7fb1233bfe9a613ab9e1ed76fc608e034a7b
Aokromes <aokromes@gmail.com>
2017-01-02 08:30:42 +0100

    DB/Misc: Fix some startup errors

---
+++ sql/updates/world/4.3.4/2017_01_01_29_world.sql
@@ -0,0 +1,5 @@
+--
+UPDATE `creature_formations` SET `leaderGUID`=303307, `memberGUID`=303307 WHERE `memberGUID`=13166;
+UPDATE `creature_formations` SET `leaderGUID`=303307, `memberGUID`=303306 WHERE `memberGUID`=13578;
+DELETE FROM `creature_formations` WHERE `leaderGUID`=13178;
+--
