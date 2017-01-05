9531f370f4d38179263ef8eb78dec2c2d6a14e65
Staleness89 <staleness665@live.com>
2017-01-05 04:03:48 -0800

    Core/Player: Add another config option for IsGroupVisibleFor() (#18723)
    (cherry-picked from 7c379129e90f61ee51acfd613755c93a9aaca739)

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -2268,6 +2268,7 @@ bool Player::IsGroupVisibleFor(Player const* p) const
         default: return IsInSameGroupWith(p);
         case 1:  return IsInSameRaidWith(p);
         case 2:  return GetTeam() == p->GetTeam();
+        case 3:  return false;
     }
 }
 
