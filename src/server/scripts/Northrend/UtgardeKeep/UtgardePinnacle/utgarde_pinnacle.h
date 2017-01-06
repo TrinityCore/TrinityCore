0fc16d847376cea15457a3502fbeed8a480ea70c
Keader <keader.android@gmail.com>
2017-01-06 20:47:36 -0200

    Core/Scripts: Palehoof rewritten & Utgarde Pinnacle updated (#18712)
    Closes #15405
    
    (cherrypicked from ba1df45d36285e283a295ea93560c2215ec21e37)

--- src/server/scripts/Northrend/UtgardeKeep/UtgardePinnacle/utgarde_pinnacle.h
+++ src/server/scripts/Northrend/UtgardeKeep/UtgardePinnacle/utgarde_pinnacle.h
@@ -49,6 +49,7 @@ enum UPDataTypes
 
 enum UPCreatureIds
 {
+    // Bosses
     NPC_SVALA_SORROWGRAVE           = 26668,
     NPC_GORTOK_PALEHOOF             = 26687,
     NPC_SKADI_THE_RUTHLESS          = 26693,
@@ -62,7 +63,8 @@ enum UPCreatureIds
     NPC_RAVENOUS_FURBOLG            = 26684,
     NPC_MASSIVE_JORMUNGAR           = 26685,
     NPC_FEROCIOUS_RHINO             = 26686,
-    NPC_PALEHOOF_ORB                = 26688,
+    NPC_PALEHOOF_ORB                = 22515, // World Trigger
+    NPC_JORMUNGAR_WORM              = 27228,
 
     // Skadi
     NPC_GRAUF                       = 26893,
