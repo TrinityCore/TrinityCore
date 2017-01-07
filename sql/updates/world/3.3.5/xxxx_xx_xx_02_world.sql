f4a3cfa9a540ad7daf9058f12364f3a5653ea6f9
Rochet2 <tqbattlenet@gmail.com>
2017-01-07 22:57:17 +0200

    Rename and merge sql files, fix typo in 'slected_waypoint'

--- sql/updates/world/3.3.5/xxxx_xx_xx_02_world.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `waypoint_data`
-	CHANGE COLUMN `wpguid` `wpguid` INT(11) UNSIGNED NOT NULL DEFAULT '0' AFTER `action_chance`;
-
-UPDATE `command` SET `help`='Syntax: .wp show $option\nOptions:\non $pathid (or selected creature with loaded path) - Show path\nfirst $pathid (or selected creature with loaded path) - Show first waypoint in path\nlast $pathid (or selected creature with loaded path) - Show last waypoint in path\noff - Hide all paths\ninfo $slected_waypoint - Show info for selected waypoint.' WHERE  `name`='wp show';
