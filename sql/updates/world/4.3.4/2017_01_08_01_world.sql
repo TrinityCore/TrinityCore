f4e686a812cbb58ec1214a607d45d328e0583d8a
Aokromes <aokromes@gmail.com>
2017-01-08 06:41:08 +0100

    DB/Creature: Stonecore Entry Updates

---
+++ sql/updates/world/4.3.4/2017_01_08_01_world.sql
@@ -0,0 +1,57 @@
+--
+-- Millhouse Manastorm
+UPDATE creature_template SET difficulty_entry_1=49653 WHERE entry=43391;
+UPDATE creature_template SET minlevel=86,maxlevel=86,exp=3,faction=16,unit_class=8,unit_flags=64,unit_flags2=0,mechanic_immune_mask=617299839 WHERE entry=49653;
+
+-- Unbound Earth Rager
+UPDATE creature_template SET difficulty_entry_1=49669 WHERE entry=43662;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=16,speed_walk=1.2,speed_run=0.714286 WHERE entry=49669;
+
+-- Stonecore Earthshaper
+UPDATE creature_template SET difficulty_entry_1=49662 WHERE entry=43537;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=16,speed_walk=1.2,speed_run=1,unit_class=8,unit_flags=64 WHERE entry=49662;
+
+-- Stonecore Warbringer
+UPDATE creature_template SET difficulty_entry_1=49667 WHERE entry=42696;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=16,speed_walk=1.2,speed_run=1,unit_flags=64 WHERE entry=49667;
+
+-- Stonecore Berserker
+UPDATE creature_template SET difficulty_entry_1=49660 WHERE entry=43430;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=16,speed_walk=1.2,speed_run=1,unit_flags=64 WHERE entry=49660;
+
+-- Stonecore Flayer
+UPDATE creature_template SET difficulty_entry_1=49663 WHERE entry=42808;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=16,speed_walk=1.2,speed_run=1,unit_flags=64 WHERE entry=49963;
+
+-- Rock Borer Trash
+UPDATE creature_template SET difficulty_entry_1=49655 WHERE entry=42845;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=16,speed_walk=4.8,speed_run=1.71429 WHERE entry=49655;
+
+-- Rock Borer - Corborus
+UPDATE creature_template SET difficulty_entry_1=49656 WHERE entry=43917;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=14 WHERE entry=49656;
+
+-- Crystalspawn Giant
+UPDATE creature_template SET difficulty_entry_1=49647 WHERE entry=42810;
+UPDATE creature_template SET minlevel=86,maxlevel=86,exp=3,faction=16,speed_walk=1.2,speed_run=1,unit_flags=64 WHERE entry=49647;
+
+-- Stonecore Sentry
+UPDATE creature_template SET difficulty_entry_1=49666 WHERE entry=42695;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=16,speed_walk=1.2,speed_run=1,unit_flags=64 WHERE entry=49666;
+
+-- Stonecore Rift Conjurer
+UPDATE creature_template SET difficulty_entry_1=49665 WHERE entry=42691;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=16,speed_walk=1.2,speed_run=1,unit_class=2,unit_flags=64 WHERE entry=49665;
+
+-- Stonecore Magmalord
+UPDATE creature_template SET difficulty_entry_1=49664 WHERE entry=42789;
+UPDATE creature_template SET minlevel=85,maxlevel=85,exp=3,faction=16,speed_walk=1.2,speed_run=1,unit_class=2,unit_flags=64 WHERE entry=49664;
+
+-- Stonecore Bruiser
+UPDATE creature_template SET difficulty_entry_1=49661 WHERE entry=42692;
+UPDATE creature_template SET minlevel=86,maxlevel=86,exp=3,faction=16,speed_walk=1.2,speed_run=1,unit_flags=64 WHERE entry=49661;
+
+-- Devout Follower
+UPDATE creature_template SET difficulty_entry_1=49648 WHERE entry=42428;
+UPDATE creature_template SET minlevel=86,maxlevel=86,exp=3,faction=1771,npcflag=2,speed_walk=1.2,speed_run=1 WHERE entry=49648;
+--
