9ac702e60bf0f25df3f7c7ee015a35cc1dbbf840
Aokromes <aokromes@gmail.com>
2017-01-11 01:57:47 +0100

    Script/Taxi Sharpbeak Hinterlands

---
+++ sql/updates/world/4.3.4/2017_01_11_00_world.sql
@@ -0,0 +1,4 @@
+--
+-- Update some values for Sharpbeak entry 43161 and add script
+UPDATE `creature_template` SET `speed_run`=1.38571, `unit_flags`=33554440, `ScriptName`="npc_sharpbeak" WHERE `entry`=43161;
+--
