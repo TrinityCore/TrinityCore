742c5f1e0b4690ef7ab3923e406b2599ea01b985
Shauren <shauren.trinity@gmail.com>
2017-01-17 20:14:44 +0100

    Fix static analysis issues
    CID 1291964
    CID 1292775
    
    (cherry picked from commit 6767bbc95f46df4b62c8131a39a132d3f9bb8a26)
    
    Conflicts:
    	src/server/game/Entities/Player/Player.cpp
    	src/server/game/Spells/SpellHistory.h
    	src/server/game/Spells/SpellInfo.cpp

--- src/common/Collision/Management/MMapManager.cpp
+++ src/common/Collision/Management/MMapManager.cpp
@@ -154,6 +154,17 @@ namespace MMAP
             return false;
         }
 
+        long pos = ftell(file);
+        fseek(file, 0, SEEK_END);
+        if (static_cast<int32>(fileHeader.size) > ftell(file) - pos)
+        {
+            TC_LOG_ERROR("maps", "MMAP:loadMap: %03u%02i%02i.mmtile has corrupted data size", mapId, x, y);
+            fclose(file);
+            return false;
+        }
+
+        fseek(file, pos, SEEK_SET);
+
         unsigned char* data = (unsigned char*)dtAlloc(fileHeader.size, DT_ALLOC_PERM);
         ASSERT(data);
 
