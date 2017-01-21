3d16c390fb5dc298b4773e8a36d21b48103c87ca
sirikfoll <sirikfoll@hotmail.com>
2017-01-20 23:56:01 -0200

    Clean-up

--- src/server/scripts/EasternKingdoms/Karazhan/boss_nightbane.cpp
+++ src/server/scripts/EasternKingdoms/Karazhan/boss_nightbane.cpp
@@ -27,13 +27,12 @@ enum NightbaneSpells
     SPELL_CHARRED_EARTH         = 30129,
     SPELL_CLEAVE                = 30131,
     SPELL_DISTRACTING_ASH       = 30130,
-    SPELL_SMOLDERING_BREATH     = 30210,
-    SPELL_TAIL_SWEEP            = 25653,
     SPELL_RAIN_OF_BONES         = 37098,
     SPELL_SMOKING_BLAST         = 30128,
     SPELL_SMOKING_BLAST_T       = 37057,
-    SPELL_FIREBALL_BARRAGE      = 30282,
-    SPELL_SUMMON_SKELETON       = 30170
+    SPELL_SMOLDERING_BREATH     = 30210,
+    SPELL_SUMMON_SKELETON       = 30170,
+    SPELL_TAIL_SWEEP            = 25653
 };
 
 enum Says
@@ -45,26 +44,36 @@ enum Says
     EMOTE_BREATH                = 4
 };
 
+enum NightbanePoints
+{
+    POINT_INTRO_START           = 0,
+    POINT_INTRO_END             = 1,
+    POINT_INTRO_LANDING         = 2,
+    POINT_PHASE_TWO_FLY         = 3,
+    POINT_PHASE_TWO_LANDING     = 5,
+    POINT_PHASE_TWO_END         = 6
+};
+
 enum NightbaneEvents
 {
     EVENT_BELLOWING_ROAR = 1,
     EVENT_CHARRED_EARTH,
-    EVENT_DISTRACTING_ASH,
-    EVENT_SMOLDERING_BREATH,
-    EVENT_TAIL_SWEEP,
     EVENT_CLEAVE,
+    EVENT_DISTRACTING_ASH,
     EVENT_EMOTE_BREATH,
-    EVENT_RAIN_OF_BONES,
-    EVENT_SMOKING_BLAST,
-    EVENT_SMOKING_BLAST_T,
-    EVENT_PRE_LAND,
+    EVENT_END_INTRO,
+    EVENT_END_PHASE_TWO,
+    EVENT_FACE_TARGET,
+    EVENT_INTRO_LANDING,
     EVENT_LAND,
     EVENT_LANDED,
+    EVENT_PRE_LAND,
+    EVENT_RAIN_OF_BONES,
+    EVENT_SMOLDERING_BREATH,
+    EVENT_SMOKING_BLAST,
+    EVENT_SMOKING_BLAST_T,
     EVENT_START_INTRO_PATH,
-    EVENT_END_INTRO,
-    EVENT_INTRO_LANDING,
-    EVENT_END_PHASE_TWO,
-    EVENT_FACE_TARGET
+    EVENT_TAIL_SWEEP
 };
 
 enum NightbanePhases
@@ -78,16 +87,6 @@ enum NightbaneGroups
 {
     GROUP_GROUND = 1,
     GROUP_FLY
-};
-
-enum NightbanePoints
-{
-    POINT_INTRO_START       = 0,
-    POINT_INTRO_END         = 1,
-    POINT_INTRO_LANDING     = 2,
-    POINT_PHASE_TWO_FLY     = 3,
-    POINT_PHASE_TWO_END     = 6,
-    POINT_PHASE_TWO_LANDING = 5,
 };
 
 enum NightbaneMisc
@@ -180,7 +179,7 @@ public:
                 {
                     case POINT_INTRO_START:
                         me->RemoveByteFlag(UNIT_FIELD_BYTES_1, UNIT_BYTES_1_OFFSET_STAND_STATE, UNIT_BYTE1_FLAG_ALWAYS_STAND | UNIT_BYTE1_FLAG_HOVER);
-                        events.ScheduleEvent(EVENT_START_INTRO_PATH, Seconds(0));
+                        events.ScheduleEvent(EVENT_START_INTRO_PATH, Milliseconds(1));
                         break;
                     case POINT_INTRO_END:
                         events.ScheduleEvent(EVENT_END_INTRO, Seconds(2));
@@ -211,7 +210,7 @@ public:
                 events.ScheduleEvent(EVENT_SMOKING_BLAST, Seconds(18), GROUP_FLY);
             }
             else if (type == WAYPOINT_MOTION_TYPE && pointId == POINT_PHASE_TWO_END)
-                events.ScheduleEvent(EVENT_END_PHASE_TWO, Seconds(0));
+                events.ScheduleEvent(EVENT_END_PHASE_TWO, Milliseconds(1));
         }
 
         void StartPhaseFly()
@@ -232,8 +231,26 @@ public:
         {
             switch (eventId)
             {
-                case EVENT_START_INTRO_PATH:
-                    me->GetMotionMaster()->MoveAlongSplineChain(POINT_INTRO_END, 2, false);
+                case EVENT_BELLOWING_ROAR:
+                    DoCastAOE(SPELL_BELLOWING_ROAR);
+                    events.Repeat(Seconds(46), Minutes(1));
+                    break;
+                case EVENT_CHARRED_EARTH:
+                    if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 1, 0.0f, true))
+                        DoCast(target, SPELL_CHARRED_EARTH);
+                    events.Repeat(Seconds(18), Seconds(47));
+                    break;
+                case EVENT_CLEAVE:
+                    DoCastVictim(SPELL_CLEAVE);
+                    events.Repeat(Seconds(6), Seconds(20));
+                    break;
+                case EVENT_DISTRACTING_ASH:
+                    if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0, 0.0f, true))
+                        DoCast(target, SPELL_DISTRACTING_ASH);
+                    break;
+                case EVENT_EMOTE_BREATH:
+                    Talk(EMOTE_BREATH);
+                    events.ScheduleEvent(EVENT_RAIN_OF_BONES, Seconds(3), GROUP_FLY);
                     break;
                 case EVENT_END_INTRO:
                     me->GetMotionMaster()->MoveAlongSplineChain(POINT_INTRO_LANDING, 3, false);
@@ -241,14 +258,14 @@ public:
                 case EVENT_END_PHASE_TWO:
                     me->GetMotionMaster()->MoveAlongSplineChain(POINT_PHASE_TWO_LANDING, 5, false);
                     break;
+                case EVENT_FACE_TARGET:
+                    me->SetFacingToObject(me->GetVictim(), true);
+                    me->SetControlled(true, UNIT_STATE_ROOT);
+                    break;
                 case EVENT_INTRO_LANDING:
                     me->SetHomePosition(me->GetPosition());
                     me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_IMMUNE_TO_PC);
                     me->SetInCombatWithZone();
-                    break;
-                case EVENT_PRE_LAND:
-                    events.CancelEventGroup(GROUP_FLY);
-                    events.ScheduleEvent(EVENT_LAND, Seconds(2), GROUP_GROUND);
                     break;
                 case EVENT_LAND:
                     Talk(YELL_LAND_PHASE);
@@ -261,55 +278,37 @@ public:
                     SetupGroundPhase();
                     me->SetReactState(REACT_AGGRESSIVE);
                     break;
-                case EVENT_FACE_TARGET:
-                    me->SetFacingToObject(me->GetVictim(), true);
-                    me->SetControlled(true, UNIT_STATE_ROOT);
+                case EVENT_PRE_LAND:
+                    events.CancelEventGroup(GROUP_FLY);
+                    events.ScheduleEvent(EVENT_LAND, Seconds(2), GROUP_GROUND);
                     break;
-                case EVENT_CLEAVE:
-                    DoCastVictim(SPELL_CLEAVE);
-                    events.Repeat(Seconds(6), Seconds(20));
-                    break;
-                case EVENT_BELLOWING_ROAR:
-                    DoCastAOE(SPELL_BELLOWING_ROAR);
-                    events.Repeat(Seconds(46), Minutes(1));
-                    break;
-                case EVENT_CHARRED_EARTH:
-                    if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 1, 0.0f, true))
-                        DoCast(target, SPELL_CHARRED_EARTH);
-                    events.Repeat(Seconds(18), Seconds(47));
-                    break;
-                case EVENT_DISTRACTING_ASH:
-                    if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0, 0.0f, true))
-                        DoCast(target, SPELL_DISTRACTING_ASH);
-                    break;
-                case EVENT_SMOLDERING_BREATH:
-                    DoCastVictim(SPELL_SMOLDERING_BREATH);
-                    events.Repeat(Seconds(28), Seconds(35));
-                    break;
-                case EVENT_TAIL_SWEEP:
-                    if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0, 0.0f, true))
-                        if (!me->HasInArc(float(M_PI), target))
-                            DoCast(target, SPELL_TAIL_SWEEP);
-                    events.Repeat(Seconds(18), Seconds(30));
-                    break;
-                case EVENT_EMOTE_BREATH:
-                    Talk(EMOTE_BREATH);
-                    events.ScheduleEvent(EVENT_RAIN_OF_BONES, Seconds(3), GROUP_FLY);
+                case EVENT_START_INTRO_PATH:
+                    me->GetMotionMaster()->MoveAlongSplineChain(POINT_INTRO_END, 2, false);
                     break;
                 case EVENT_RAIN_OF_BONES:
                     DoResetThreat();
                     if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0, 0.0f, true))
                         DoCast(target, SPELL_RAIN_OF_BONES);
                     break;
-                case EVENT_SMOKING_BLAST_T:
-                    if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0, 0.0f, true))
-                        DoCast(target, SPELL_SMOKING_BLAST_T);
-                    events.Repeat(Seconds(6));
+                case EVENT_SMOLDERING_BREATH:
+                    DoCastVictim(SPELL_SMOLDERING_BREATH);
+                    events.Repeat(Seconds(28), Seconds(35));
                     break;
                 case EVENT_SMOKING_BLAST:
                     if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0, 0.0f, true))
                         DoCast(target, SPELL_SMOKING_BLAST);
                     events.Repeat(Milliseconds(1200));
+                    break;
+                case EVENT_SMOKING_BLAST_T:
+                    if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0, 0.0f, true))
+                        DoCast(target, SPELL_SMOKING_BLAST_T);
+                    events.Repeat(Seconds(6));
+                    break;
+                case EVENT_TAIL_SWEEP:
+                    if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0, 0.0f, true))
+                        if (!me->HasInArc(float(M_PI), target))
+                            DoCast(target, SPELL_TAIL_SWEEP);
+                    events.Repeat(Seconds(18), Seconds(30));
                     break;
                 default:
                     break;
@@ -386,7 +385,7 @@ class go_blackened_urn : public GameObjectScript
 public:
     go_blackened_urn() : GameObjectScript("go_blackened_urn") { }
 
-    bool OnGossipHello(Player* player, GameObject* go) override
+    bool OnGossipHello(Player* /*player*/, GameObject* go) override
     {
         if (go->HasFlag(GAMEOBJECT_FLAGS, GO_FLAG_IN_USE))
             return true;
