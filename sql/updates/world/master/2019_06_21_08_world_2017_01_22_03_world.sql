95091afed33d5a946ec2e5add7e52c113617498b
Rushor <Rushor@users.noreply.github.com>
2017-01-22 16:42:43 +0100

    DB/Creature:  Ironwool Mammoth, Part 1
    Closes #18895
    
    (cherrypicked from 41530b5ac9a3e56f005d04fbc6d94d90c5e2afe8)

---
+++ sql/updates/world/master/2019_06_21_08_world_2017_01_22_03_world.sql
@@ -0,0 +1,7 @@
+-- 
+UPDATE `creature` SET `spawndist`=0, `MovementType`=0, `unit_flags`=`unit_flags`|536870912|32770, `dynamicflags`=32 WHERE  `guid`=116778;
+
+DELETE FROM `creature_addon` WHERE `guid` IN (113320, 116778);
+INSERT INTO `creature_addon` (`guid`,`path_id`,`mount`,`bytes1`,`bytes2`,`emote`,`auras`) VALUES 
+(113320,0,0,0,1,233, ''),
+(116778,0,0,7,1,0, '29266');
