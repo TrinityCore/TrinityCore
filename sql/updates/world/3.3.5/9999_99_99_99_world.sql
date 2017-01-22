18c8786fc98f9d8bcfa87eda8dd9daf93535ae94
Xinef <none@empty.com>
2017-01-22 15:08:30 +0100

    Sql file

---
+++ sql/updates/world/3.3.5/9999_99_99_99_world.sql
@@ -0,0 +1,4 @@
+
+UPDATE `gameobject_template` SET `data2`=32783, `data3`=0 WHERE `entry` IN(36727, 179944, 194108); -- Summoning stones and Ritual of Summoning animation spell
+UPDATE `gameobject_template` SET `data2`=51733, `data3`=0 WHERE `entry` IN(181622, 193168); -- Ritual of Souls animation spell
+UPDATE `gameobject_template` SET `data2`=51733, `data3`=0 WHERE `entry` IN(177193); -- Ritual of Doom animation spell
