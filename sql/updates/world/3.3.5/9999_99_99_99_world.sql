9f2e49f38b8386f9b44d6993166e98acdbb2909d
Xinef <none@empty.com>
2017-01-23 20:13:53 +0100

    Missing sql file

---
+++ sql/updates/world/3.3.5/9999_99_99_99_world.sql
@@ -0,0 +1,26 @@
+
+REPLACE INTO `spell_custom_attr` VALUES (63124, 131072); -- quest There's Something About the Squire (13654)
+
+REPLACE INTO `spell_custom_attr` VALUES (63293, 4); -- Mimiron - spinning damage
+REPLACE INTO `spell_custom_attr` VALUES (68873, 4); -- Wailing Souls
+REPLACE INTO `spell_custom_attr` VALUES (70324, 4); -- Wailing Souls
+REPLACE INTO `spell_custom_attr` VALUES (64619, 4); -- Ulduar, Mimiron, Emergency Fire Bot, Water Spray
+
+REPLACE INTO `spell_custom_attr` VALUES (66809, 8); -- Meteor Fists
+REPLACE INTO `spell_custom_attr` VALUES (67331, 8); -- Meteor Fists
+REPLACE INTO `spell_custom_attr` VALUES (66765, 8); -- Meteor Fists
+REPLACE INTO `spell_custom_attr` VALUES (67333, 8); -- Meteor Fists
+
+REPLACE INTO `spell_custom_attr` VALUES (66378, 32768); -- Trial of the Crusader, Jaraxxus, Shivan Slash
+REPLACE INTO `spell_custom_attr` VALUES (67097, 32768); -- Trial of the Crusader, Jaraxxus, Shivan Slash
+REPLACE INTO `spell_custom_attr` VALUES (67098, 32768); -- Trial of the Crusader, Jaraxxus, Shivan Slash
+REPLACE INTO `spell_custom_attr` VALUES (67099, 32768); -- Trial of the Crusader, Jaraxxus, Shivan Slash
+REPLACE INTO `spell_custom_attr` VALUES (64125, 32768); -- Ulduar, Yogg-Saron, Squeeze
+REPLACE INTO `spell_custom_attr` VALUES (64126, 32768); -- Ulduar, Yogg-Saron, Squeeze
+REPLACE INTO `spell_custom_attr` VALUES (72409, 32768); -- Rune of Blood (Deathbringer Saurfang)
+REPLACE INTO `spell_custom_attr` VALUES (72447, 32768); -- Rune of Blood (Deathbringer Saurfang)
+REPLACE INTO `spell_custom_attr` VALUES (72448, 32768); -- Rune of Blood (Deathbringer Saurfang)
+REPLACE INTO `spell_custom_attr` VALUES (72449, 32768); -- Rune of Blood (Deathbringer Saurfang)
+REPLACE INTO `spell_custom_attr` VALUES (62775, 32768); -- Ulduar: XT-002 Tympanic Tamparum
+
+REPLACE INTO `spell_custom_attr` VALUES (45145, 1048576); -- Snake Trap Effect
