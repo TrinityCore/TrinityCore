870693d94ff2f775c8ad2ac103eb2a3eed8ca994
Macabra <kurigv@gmail.com>
2017-01-28 01:15:26 +0400

    ArthasCore rev

--- src/common/GitRevision.cpp
+++ src/common/GitRevision.cpp
@@ -63,7 +63,7 @@ char const* GitRevision::GetFullDatabase()
 
 char const* GitRevision::GetFullVersion()
 {
-  return "TrinityCore rev. " VER_PRODUCTVERSION_STR
+  return "ArthasCore rev. " VER_PRODUCTVERSION_STR
     " (" TRINITY_PLATFORM_STR ", " _BUILD_DIRECTIVE ", " TRINITY_LINKAGE_TYPE_STR ")";
 }
 
