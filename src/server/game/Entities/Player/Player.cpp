d941b8495f4802dfeedda94e3d524ac7f0fe96a0
Gerhood <mrambrius@gmail.com>
2017-01-29 14:06:40 +0100

    Core/Spells: implement Mad/Crazy Alchemist's potions
    (cherrypicked frombab59b28faad262dc5d9873ca7c0856477ec4d66)

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -22901,7 +22901,12 @@ void Player::UpdatePotionCooldown(Spell* spell)
     }
     // from spell cases (m_lastPotionId set in Spell::SendSpellCooldown)
     else
-        GetSpellHistory()->SendCooldownEvent(spell->m_spellInfo, m_lastPotionId, spell);
+    {
+        if (spell->IsIgnoringCooldowns())
+            return;
+        else
+            GetSpellHistory()->SendCooldownEvent(spell->m_spellInfo, m_lastPotionId, spell);
+    }
 
     m_lastPotionId = 0;
 }
