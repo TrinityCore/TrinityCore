e769592e1160940d4c9c15051cc9916ac715f3a0
Aokromes <aokromes@gmail.com>
2017-02-01 17:24:06 +0100

    Core/Server: fix duplicated inserts into tutorials table for newly cr… …eated characters

--- src/server/game/Server/WorldSession.cpp
+++ src/server/game/Server/WorldSession.cpp
@@ -824,6 +824,10 @@ void WorldSession::SaveTutorialsData(SQLTransaction &trans)
     stmt->setUInt32(MAX_ACCOUNT_TUTORIAL_VALUES, GetAccountId());
     trans->Append(stmt);
 
+    // now has, set flag so next save uses update query
+    if (!hasTutorialsInDB)
+        m_TutorialsChanged |= TUTORIALS_FLAG_LOADED_FROM_DB;
+
     m_TutorialsChanged &= ~TUTORIALS_FLAG_CHANGED;
 }
 
