bc95fce93f5f3d0057f6d766567cbd6c341737b8
Keader <keader.android@gmail.com>
2017-02-04 12:14:28 -0300

    Core/Movement: Added support to change orientation in MovePoint function (#18893)

--- src/server/game/Movement/MotionMaster.h
+++ src/server/game/Movement/MotionMaster.h
@@ -126,9 +126,16 @@ class TC_GAME_API MotionMaster
         void MoveFleeing(Unit* enemy, uint32 time = 0);
         void MovePoint(uint32 id, Position const& pos, bool generatePath = true)
         {
-            MovePoint(id, pos.m_positionX, pos.m_positionY, pos.m_positionZ, generatePath);
+            if (pos.GetOrientation())
+                MovePoint(id, pos.m_positionX, pos.m_positionY, pos.m_positionZ, pos.GetOrientation(), generatePath);
+            else
+                MovePoint(id, pos.m_positionX, pos.m_positionY, pos.m_positionZ, generatePath);
         }
-        void MovePoint(uint32 id, float x, float y, float z, bool generatePath = true);
+        void MovePoint(uint32 id, float x, float y, float z, bool generatePath = true)
+        {
+            MovePoint(id, x, y, z, 0.0f, generatePath);
+        }
+        void MovePoint(uint32 id, float x, float y, float z, float o, bool generatePath = true);
 
         /*  Makes the unit move toward the target until it is at a certain distance from it. The unit then stops.
             Only works in 2D.
