caef7a9f605602cd8e40c523f8bcffdc1596bf18
Xinef <none@empty.com>
2017-02-12 13:26:58 +0100

    Fixed setting and unsetting of m_spellModTakingSpell, a problem when a different spell tries to overwrite existing ModTakingSpell should no longer happen

--- src/server/game/Spells/SpellInfo.h
+++ src/server/game/Spells/SpellInfo.h
@@ -494,17 +494,17 @@ class TC_GAME_API SpellInfo
         SpellSpecificType GetSpellSpecific() const;
 
         float GetMinRange(bool positive = false) const;
-        float GetMaxRange(bool positive = false, Unit* caster = NULL, Spell* spell = NULL) const;
+        float GetMaxRange(bool positive = false, Unit* caster = nullptr, Spell* spell = nullptr) const;
 
         int32 GetDuration() const;
         int32 GetMaxDuration() const;
 
         uint32 GetMaxTicks() const;
 
-        uint32 CalcCastTime(Spell* spell = NULL) const;
+        uint32 CalcCastTime(Spell* spell = nullptr) const;
         uint32 GetRecoveryTime() const;
 
-        int32 CalcPowerCost(Unit const* caster, SpellSchoolMask schoolMask) const;
+        int32 CalcPowerCost(Unit const* caster, SpellSchoolMask schoolMask, Spell* spell = nullptr) const;
 
         bool IsRanked() const;
         uint8 GetRank() const;
