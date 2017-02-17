daeab3a10c1df199d4392e4fb1c96a2c8ec29222
Aokromes <aokromes@gmail.com>
2017-02-17 19:53:19 +0000

    Update 2017_02_17_06_world.sql

--- sql/updates/world/4.3.4/2017_02_17_06_world.sql
+++ sql/updates/world/4.3.4/2017_02_17_06_world.sql
@@ -8,7 +8,7 @@ INSERT INTO `creature_text` (`entry`, `groupid`, `id`, `text`, `type`, `language
 
 -- Mortimer Moloch
 UPDATE `creature_template` set `ScriptName` = 'npc_mortimer_moloch' WHERE `entry` = 46482;
-DELETE FROM `creature_text` WHERE entry = 46382;
+DELETE FROM `creature_text` WHERE entry = 46482;
 INSERT INTO `creature_text` (`entry`, `groupid`, `id`, `text`, `type`, `language`, `probability`, `emote`, `duration`, `sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES
 (46482, 0, 0, '%s collapses from a heart attack!', 16, 0, 100, 0, 0, 0, 0, 3, 'MORTIMER MOLOCH - Stockades - MORTIMER_MOLOCH_EMOTE'),
 (46482, 1, 0, 'Egad! My sophisticated heart!', 14, 0, 100, 0, 0, 0, 0, 3, 'MORTIMER MOLOCH - Stockades - MORTIMER_MOLOCH_EMOTE');
