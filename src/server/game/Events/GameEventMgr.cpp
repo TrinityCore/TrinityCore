9d74252e8d728a4e7ab6359a85d982b29672c5cd
Kittnz <frederik156@hotmail.com>
2017-02-17 20:30:52 +0100

    Reverting: Game/Scripting: Add OnGameEvent hook for game objects.

--- src/server/game/Events/GameEventMgr.cpp
+++ src/server/game/Events/GameEventMgr.cpp
@@ -28,7 +28,6 @@
 #include "BattlegroundMgr.h"
 #include "UnitAI.h"
 #include "GameObjectAI.h"
-#include "ScriptMgr.h"
 
 GameEventMgr* GameEventMgr::instance()
 {
@@ -1642,13 +1641,8 @@ public:
     void Visit(std::unordered_map<ObjectGuid, GameObject*>& gameObjectMap)
     {
         for (auto const& p : gameObjectMap)
-        {
             if (p.second->IsInWorld())
-            {
                 p.second->AI()->OnGameEvent(_activate, _eventId);
-                sScriptMgr->OnGameEvent(_activate, _eventId);
-            }
-        }
     }
 
     template<class T>
