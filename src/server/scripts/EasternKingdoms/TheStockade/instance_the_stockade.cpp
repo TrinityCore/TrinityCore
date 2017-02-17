499f7d3d5be2a6aadbf6a3115d77bf950e20a23b
Flameshot <dragosgirip@yahoo.com>
2017-02-17 16:46:52 +0200

    Removed unedeed things

--- src/server/scripts/EasternKingdoms/TheStockade/instance_the_stockade.cpp
+++ src/server/scripts/EasternKingdoms/TheStockade/instance_the_stockade.cpp
@@ -1,6 +1,5 @@
 /*
 * Copyright (C) 2008-2017 TrinityCore <http://www.trinitycore.org/>
-* Copyright (C) 2017-2017 SharpCore <http://emusharpers.tk>
 *
 * This program is free software; you can redistribute it and/or modify it
 * under the terms of the GNU General Public License as published by the
