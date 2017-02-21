f96f1cebd4eadb2f8c5ee6a7f588c98b48b639ed
ariel- <ariel-@users.noreply.github.com>
2017-02-21 17:14:11 -0300

    Core/Packets: readd incorrectly removed line
    Closes #19180

--- src/server/game/Handlers/QueryHandler.cpp
+++ src/server/game/Handlers/QueryHandler.cpp
@@ -340,6 +340,8 @@ void WorldSession::HandleQuestPOIQuery(WorldPackets::Query::QuestPOIQuery& query
         questIds.insert(query.MissingQuestPOIs[i]); // quest id
 
     WorldPacket data(SMSG_QUEST_POI_QUERY_RESPONSE, 4 + (4 + 4 + 40) * questIds.size());
+    data << uint32(questIds.size()); // count
+    
     for (uint32 questId : questIds)
     {
         uint16 const questSlot = _player->FindQuestSlot(questId);
