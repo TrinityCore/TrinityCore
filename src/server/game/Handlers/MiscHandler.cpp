dbc59d7bc605923afaaba87996355b7db4fa301b
TheHighlord <ladiszlaizeno@gmail.com>
2017-02-21 20:45:51 +0100

    Pandaren faction choose opcode added

--- src/server/game/Handlers/MiscHandler.cpp
+++ src/server/game/Handlers/MiscHandler.cpp
@@ -1173,3 +1173,43 @@ void WorldSession::HandleMountSetFavorite(WorldPackets::Misc::MountSetFavorite& 
 {
     _collectionMgr->MountSetFavorite(mountSetFavorite.MountSpellID, mountSetFavorite.IsFavorite);
 }
+
+#define JOIN_THE_ALLIANCE 1
+#define JOIN_THE_HORDE    0
+
+void WorldSession::HandleSelectFactionOpcode(WorldPacket& recvPacket)
+{
+	uint32 choice = recvPacket.read<uint32>();
+
+	if (_player->getRace() != RACE_PANDAREN_NEUTRAL)
+		return;
+
+	if (choice == JOIN_THE_HORDE)
+	{
+		_player->SetByteValue(UNIT_FIELD_BYTES_0, 0, RACE_PANDAREN_HORDE);
+		_player->setFactionForRace(RACE_PANDAREN_HORDE);
+		_player->SaveToDB();
+		WorldLocation location(1, 1357.62f, -4373.55f, 26.13f, 0.13f);
+		_player->TeleportTo(location);
+		_player->SetHomebind(location, 363);
+		_player->LearnSpell(669, false); // Language Orcish
+		_player->LearnSpell(108127, false); // Language Pandaren
+	}
+	else if (choice == JOIN_THE_ALLIANCE)
+	{
+		_player->SetByteValue(UNIT_FIELD_BYTES_0, 0, RACE_PANDAREN_ALLIANCE);
+		_player->setFactionForRace(RACE_PANDAREN_ALLIANCE);
+		_player->SaveToDB();
+		WorldLocation location(0, -8960.02f, 516.10f, 96.36f, 0.67f);
+		_player->TeleportTo(location);
+		_player->SetHomebind(location, 9);
+		_player->LearnSpell(668, false); // Language Common
+		_player->LearnSpell(108127, false); // Language Pandaren
+	}
+
+	if (_player->GetQuestStatus(31450) == QUEST_STATUS_INCOMPLETE)
+		_player->KilledMonsterCredit(64594);
+	
+
+	_player->SendMovieStart(116);
+}
