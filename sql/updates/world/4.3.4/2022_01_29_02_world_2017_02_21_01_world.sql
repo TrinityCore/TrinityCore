5e57ad6fa8b40956532eb0f91d7348ac2ae37d4e
ariel- <ariel-@users.noreply.github.com>
2017-02-21 22:37:53 -0300

    Core/Scripts: fix Nightfall proc chance and reduce it for victims with level above 60
    Closes #15734
    
    missing code
    (cherry picked from commit eac9c1f0b992fd3622fd7dd864b2acc2781ec778)

---
+++ sql/updates/world/4.3.4/2022_01_29_02_world_2017_02_21_01_world.sql
@@ -0,0 +1,2 @@
+-- UPDATE `item_template` SET `spellppmRate_1`=2.5, `ScriptName`='item_generic_limit_chance_above_60' WHERE `entry`=19169;
+UPDATE `item_template_addon` SET `SpellPPMChance`=2.5 WHERE `Id`=19169;
--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -7983,7 +7983,7 @@ void Player::CastItemCombatSpell(DamageInfo const& damageInfo, Item* item, ItemT
             else if (chance > 100.0f)
                 chance = GetWeaponProcChance();
 
-            if (roll_chance_f(chance))
+            if (roll_chance_f(chance) && sScriptMgr->OnCastItemCombatSpell(this, damageInfo.GetVictim(), spellInfo, item))
                 CastSpell(damageInfo.GetVictim(), spellInfo->Id, item);
         }
     }
