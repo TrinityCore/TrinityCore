b91eac6a1c377c14ebe2b66104ac02e2f9a01ad4
ariel- <ariel-@users.noreply.github.com>
2017-02-21 21:07:45 -0300

    Core/Spells: fix Hand of Justice proc chance and reduce it past level 60.
    Ref #7789
    
    (cherrypicked from f4ae639961542112dddcf627fc2e1f11967048a9)

--- src/server/game/Spells/Auras/SpellAuras.cpp
+++ src/server/game/Spells/Auras/SpellAuras.cpp
@@ -1811,6 +1811,11 @@ float Aura::CalcProcChance(SpellProcEntry const& procEntry, ProcEventInfo& event
         if (Player* modOwner = caster->GetSpellModOwner())
             modOwner->ApplySpellMod(GetId(), SPELLMOD_CHANCE_OF_SUCCESS, chance);
     }
+
+    // proc chance is reduced by an additional 3.333% per level past 60
+    if ((procEntry.AttributesMask & PROC_ATTR_REDUCE_PROC_60) && eventInfo.GetActor()->getLevel() > 60)
+        chance = std::max(0.f, (1.f - ((eventInfo.GetActor()->getLevel() - 60) * 1.f / 30.f)) * chance);
+
     return chance;
 }
 
