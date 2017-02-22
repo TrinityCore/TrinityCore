6ed7cb477d33ac6229c2b2637e283eb4c25b2cfe
ariel- <ariel-@users.noreply.github.com>
2017-02-21 21:07:45 -0300

    Core/Spells: fix Hand of Justice proc chance and reduce it past level 60.
    Ref #7789
    
    (cherrypicked from f4ae639961542112dddcf627fc2e1f11967048a9)

--- src/server/game/Spells/SpellMgr.h
+++ src/server/game/Spells/SpellMgr.h
@@ -257,7 +257,10 @@ enum ProcAttributes
     PROC_ATTR_REQ_EXP_OR_HONOR   = 0x0000001, // requires proc target to give exp or honor for aura proc
     PROC_ATTR_TRIGGERED_CAN_PROC = 0x0000002, // aura can proc even with triggered spells
     PROC_ATTR_REQ_POWER_COST     = 0x0000004, // requires triggering spell to have a power cost for aura proc
-    PROC_ATTR_REQ_SPELLMOD       = 0x0000008  // requires triggering spell to be affected by proccing aura to drop charges
+    PROC_ATTR_REQ_SPELLMOD       = 0x0000008, // requires triggering spell to be affected by proccing aura to drop charges
+
+
+    PROC_ATTR_REDUCE_PROC_60     = 0x0000080  // aura should have a reduced chance to proc if level of proc Actor > 60
 };
 
 struct SpellProcEntry
