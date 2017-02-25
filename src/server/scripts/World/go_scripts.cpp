69400719948fc32b3802a0ff6834b6261e4a0754
jackpoz <giacomopoz@gmail.com>
2017-02-25 15:54:11 +0100

    Core/Misc: Fix static analysis issues

--- src/server/scripts/World/go_scripts.cpp
+++ src/server/scripts/World/go_scripts.cpp
@@ -1614,7 +1614,7 @@ public:
 
     struct go_bellsAI : public GameObjectAI
     {
-        go_bellsAI(GameObject* go) : GameObjectAI(go) { }
+        go_bellsAI(GameObject* go) : GameObjectAI(go), _soundId(0) { }
 
         void InitializeAI() override
         {
