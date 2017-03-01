dd59d9a3b397bfbc76ad059eab7e5c9551fcf11e
Keader <keader.android@gmail.com>
2017-03-01 18:58:47 -0300

    Core/Scripts: Fixed crash in Sindragosa
    Closes #19227
    
    (cherrypicked from ecfb346d1af39305923db1f19a4568355c4d94e5)

--- src/server/scripts/Northrend/IcecrownCitadel/boss_sindragosa.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_sindragosa.cpp
@@ -1098,8 +1098,10 @@ class spell_sindragosa_s_fury : public SpellScriptLoader
             {
                 targets.remove_if([](WorldObject* obj) -> bool
                 {
-                    // SPELL_ATTR3_ONLY_TARGET_PLAYERS present on the spell, we can safely cast to Player
-                    return obj->ToPlayer()->IsGameMaster();
+                    if (Player* target = obj->ToPlayer())
+                        if (target->IsGameMaster())
+                            return true;
+                    return false;
                 });
 
                 _targetCount = targets.size();
