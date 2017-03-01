125d8ccac958ecf12d17f12f8e028e37be9a056b
ariel- <ariel-@users.noreply.github.com>
2017-03-01 19:35:39 -0300

    Core/Scripts: Change null-check to assert
    - ToPlayer() should never be null in this context
    
    (cherrypicked from 12c680f9b1f1068099c3b19443d93d456690ac56)

--- src/server/scripts/Northrend/IcecrownCitadel/boss_sindragosa.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_sindragosa.cpp
@@ -1098,10 +1098,8 @@ class spell_sindragosa_s_fury : public SpellScriptLoader
             {
                 targets.remove_if([](WorldObject* obj) -> bool
                 {
-                    if (Player* target = obj->ToPlayer())
-                        if (target->IsGameMaster())
-                            return true;
-                    return false;
+                    // SPELL_ATTR3_ONLY_TARGET_PLAYERS present on the spell, we can safely cast to Player
+                    return ASSERT_NOTNULL(obj->ToPlayer())->IsGameMaster();
                 });
 
                 _targetCount = targets.size();
