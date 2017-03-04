6abf20d97ff4822bd2ea188bc92d23ead19efdbd
kaytotes <sbobshillito@gmail.com>
2017-03-04 20:56:49 +0000

    Moved NozdormuWoW specific SQL changes to the sql/custom/world directory

--- sql/updates/world/3.3.5/2017_03_04_03_world_335.sql
+++
@@ -1,3 +0,0 @@
--- Revert Diseased Wolf Hack Fix
-UPDATE creature_template SET flags_extra=0 WHERE entry=299; -- Diseased Young Wolf
-UPDATE creature_template SET flags_extra=0 WHERE entry=69; -- Diseased Timber Wolf
