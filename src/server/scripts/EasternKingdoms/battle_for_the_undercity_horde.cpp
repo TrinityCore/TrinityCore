a31e73fb6ce739fc24e40ba774abd3bc167d404a
tkrokli <tkrokli@users.noreply.github.com>
2017-03-04 22:17:33 +0100

    Various file updates (minor changes)
    - remove redundant world DB SQL file
    - keep TC copyright header code style
    - update copyright year range to 2017

--- src/server/scripts/EasternKingdoms/battle_for_the_undercity_horde.cpp
+++ src/server/scripts/EasternKingdoms/battle_for_the_undercity_horde.cpp
@@ -1,19 +1,19 @@
 /*
-* Copyright (C) 2008-2016 TrinityCore <http://www.trinitycore.org/>
-*
-* This program is free software; you can redistribute it and/or modify it
-* under the terms of the GNU General Public License as published by the
-* Free Software Foundation; either version 2 of the License, or (at your
-* option) any later version.
-*
-* This program is distributed in the hope that it will be useful, but WITHOUT
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
-* more details.
-*
-* You should have received a copy of the GNU General Public License along
-* with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright (C) 2008-2017 TrinityCore <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "CreatureGroups.h"
 #include "Group.h"
@@ -59,7 +59,7 @@ enum BattleForUndercityHorde
     SPELL_GREATER_MASS_TELEPORT         = 60516,
     SPELL_WATER_REVENANT_ENTRANCE       = 55760,
 
-    // NPC's
+    // NPCs
     NPC_WARSONG_BATTLEGUARD             = 31739,
     NPC_VORTEX                          = 31782,
     NPC_TIDAL_WAVE                      = 31765,
@@ -69,7 +69,7 @@ enum BattleForUndercityHorde
     NPC_OVERLORD_SAURFANG               = 32315,
     NPC_DISTANT_VOICE                   = 31814,
 
-    // GO'S
+    // GOs
     GO_BLOCKED_PASSAGE                  = 194935,
     GO_HORDE_BANNER                     = 194004,
     GO_PORTAL_ORGRIMMAR                 = 193427,
