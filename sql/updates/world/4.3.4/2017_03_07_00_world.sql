5bfc7e25f95989e6b64590e0d28acab28a8c5add
Aokromes <aokromes@gmail.com>
2017-03-07 15:01:04 +0100

    DB/Gameobject: Add missing Darkmoon Carousel template

---
+++ sql/updates/world/4.3.4/2017_03_07_00_world.sql
@@ -0,0 +1,5 @@
+--
+DELETE FROM `gameobject_template`  WHERE `entry` =216666;
+INSERT INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `IconName`, `castBarCaption`, `unk1`, `size`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`, `Data6`, `Data7`, `Data8`, `Data9`, `Data10`, `Data11`, `Data12`, `Data13`, `Data14`, `Data15`, `Data16`, `Data17`, `Data18`, `Data19`, `Data20`, `Data21`, `Data22`, `Data23`, `Data24`, `Data25`, `Data26`, `Data27`, `Data28`, `Data29`, `Data30`, `Data31`, `VerifiedBuild`) VALUES
+(216666, 11, 13729, 'Doodad_MerrygoRound_transport001', '', '', '', 1, 0, 0, 0, 5778672, 5778672, 1113, 0, 5778672, 0, 5778672, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 23420); -- 216666
+--
