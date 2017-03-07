c3aaf501ab6d6b741b036566d2e350ac1434dffc
ariel- <ariel-@users.noreply.github.com>
2017-03-07 20:15:30 -0300

    Core/Creature: don't override display id if addon tables load a morph aura
    Ref #7337

--- src/server/game/Entities/Creature/Creature.cpp
+++ src/server/game/Entities/Creature/Creature.cpp
@@ -936,9 +936,12 @@ bool Creature::Create(ObjectGuid::LowType guidlow, Map* map, uint32 /*phaseMask*
     CreatureModelInfo const* minfo = sObjectMgr->GetCreatureModelRandomGender(&displayID);
     if (minfo && !IsTotem())                               // Cancel load if no model defined or if totem
     {
-        SetDisplayId(displayID);
         SetNativeDisplayId(displayID);
-        SetByteValue(UNIT_FIELD_BYTES_0, UNIT_BYTES_0_OFFSET_GENDER, minfo->gender);
+
+        Unit::AuraEffectList const& transformAuras = GetAuraEffectsByType(SPELL_AURA_TRANSFORM);
+        Unit::AuraEffectList const& shapeshiftAuras = GetAuraEffectsByType(SPELL_AURA_MOD_SHAPESHIFT);
+        if (transformAuras.empty() && shapeshiftAuras.empty())
+            SetDisplayId(displayID);
     }
 
     LastUsedScriptID = GetCreatureTemplate()->ScriptID;
