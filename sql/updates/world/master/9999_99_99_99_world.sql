177e5b464c8d954853bb8c5aaab38cac0f47eb3c
Alexander <alexrussian@bk.ru>
2017-03-14 00:31:42 +0300

    Remove obsolete spellscript linking.

--- sql/updates/world/master/9999_99_99_99_world.sql
+++ sql/updates/world/master/9999_99_99_99_world.sql
@@ -1,3 +1,4 @@
+DELETE FROM `spell_script_names` WHERE `ScriptName` LIKE '%spell_dru_eclipse%' OR `ScriptName` LIKE '%spell_dru_glyph%' OR `ScriptName` = 'spell_dru_insect_swarm';
 DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_dru_moonfire' OR `ScriptName` = 'spell_dru_sunfire';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
 (8921, 'spell_dru_moonfire'),
