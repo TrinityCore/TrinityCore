c54b0cb3db705a5d1f7c68db77871e290f82e8c1
ConanHUN <conan_513@hotmail.com>
2017-03-13 07:44:11 +0100

    SingleCore personality

--- revision_data.h.in.cmake
+++ revision_data.h.in.cmake
@@ -8,8 +8,8 @@
  #define _BUILD_DIRECTORY           R"(@BUILDDIR@)"
  #define _MYSQL_EXECUTABLE          R"(@MYSQL_EXECUTABLE@)"
  #define _FULL_DATABASE             "TDB_full_world_335.61_2016_04_11.sql"
- #define VER_COMPANYNAME_STR        "TrinityCore Developers"
- #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2016 TrinityCore"
+ #define VER_COMPANYNAME_STR        "SingleCore Developers"
+ #define VER_LEGALCOPYRIGHT_STR     "(c)2012-2017 SingleCore"
  #define VER_FILEVERSION            0,0,0
  #define VER_FILEVERSION_STR        "@rev_hash@ @rev_date@ (@rev_branch@ branch)"
  #define VER_PRODUCTVERSION         VER_FILEVERSION
