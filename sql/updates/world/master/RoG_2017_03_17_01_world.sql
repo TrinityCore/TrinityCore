4c368fe46c98a53852e560ac81a464e04f16331b
HannibalRoG <edreisantafe@gmail.com>
2017-03-17 01:08:10 -0400

    Update Warrior Bloodthirst to 7.1.5

---
+++ sql/updates/world/master/RoG_2017_03_17_01_world.sql
@@ -0,0 +1,5 @@
+-- Class: Warrior
+-- Spell: Bloodthirst
+
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_warr_bloodthirst';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('23881', 'spell_warr_bloodthirst');
