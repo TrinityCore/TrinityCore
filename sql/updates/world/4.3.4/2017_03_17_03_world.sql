eae8edbbb979e0ea1ec48a616ec5664966c280b2
Aokromes <aokromes@gmail.com>
2017-03-17 11:11:20 +0100

    DB/Player: Convert Gnome - Irradiated Aura to mask

---
+++ sql/updates/world/4.3.4/2017_03_17_03_world.sql
@@ -0,0 +1,5 @@
+--
+DELETE FROM `playercreateinfo_cast_spell` WHERE `spell` =80653;
+INSERT INTO `playercreateinfo_cast_spell` (`raceMask`, `classMask`, `spell`, `note`) VALUES
+(64, 410, 80653, 'Gnome - Irradiated Aura');
+--
