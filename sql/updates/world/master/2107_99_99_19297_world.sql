0dfcfc6738459a5ecba80bea0c0339477975e015
tkrokli <tkrokli@users.noreply.github.com>
2017-03-19 23:32:53 +0100

    Update 2107_99_99_19297_world.sql
    - add 32215, 'spell_warr_victorious_state' to spell_script_names

--- sql/updates/world/master/2107_99_99_19297_world.sql
+++ sql/updates/world/master/2107_99_99_19297_world.sql
@@ -1,5 +1,6 @@
--- Class: Warrior, Spec: Arms, Protection, Spell: Impending Victory
+-- Class: Warrior, Spec: Arms, Protection, Spells: Impending Victory & Victorious State
 
-DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_warr_impending_victory';
+DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_warr_impending_victory','spell_warr_victorious_state');
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
-('202168', 'spell_warr_impending_victory');
+(202168, 'spell_warr_impending_victory'),
+( 32215, 'spell_warr_victorious_state');
