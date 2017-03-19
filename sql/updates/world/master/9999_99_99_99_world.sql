68f9d7977c22d5ecf4f1dcfeadc40d96ff7e9691
Hadifaren <uther110@gmail.com>
2017-03-19 08:28:12 -0700

    sql file

---
+++ sql/updates/world/master/9999_99_99_99_world.sql
@@ -0,0 +1,2 @@
+DELETE FROM `spell_script_names` WHERE `spell_id`=12294 AND `ScriptName`='spell_warr_mortalstrike';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (12294, 'spell_warr_mortalstrike');
