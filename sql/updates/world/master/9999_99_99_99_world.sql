e74c42c5c495bb81a25109a894428b6767469a64
Hadifaren <uther110@gmail.com>
2017-03-19 08:54:19 -0700

    merge sql

--- sql/updates/world/master/9999_99_99_99_world.sql
+++ sql/updates/world/master/9999_99_99_99_world.sql
@@ -1,2 +1,5 @@
 DELETE FROM `spell_script_names` WHERE `spell_id`=12294 AND `ScriptName`='spell_warr_mortalstrike';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (12294, 'spell_warr_mortalstrike');
+
+DELETE FROM `spell_script_names` WHERE `spell_id`=167105 AND `ScriptName`='spell_warr_colossus_smash';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (167105, 'spell_warr_colossus_smash');
