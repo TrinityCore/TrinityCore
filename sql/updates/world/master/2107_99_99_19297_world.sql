99986468250c2e6c5c5bd0252b1dfe663beb82cf
tkrokli <tkrokli@users.noreply.github.com>
2017-03-19 21:55:13 +0100

    Update and rename RoG_2107_03_14_01_world.sql to 2107_99_99_19297_world.sql
    - simplified and spellchecked SQL comments
    - added 1 Line Break after VALUES

---
+++ sql/updates/world/master/2107_99_99_19297_world.sql
@@ -0,0 +1,5 @@
+-- Class: Warrior, Spec: Arms, Protection, Spell: Impending Victory
+
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_warr_impending_victory';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+('202168', 'spell_warr_impending_victory');
