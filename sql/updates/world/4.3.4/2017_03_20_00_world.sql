580dbe946a65849e3a3cb2ce0335195a44956281
Aokromes <aokromes@gmail.com>
2017-03-20 23:55:38 +0100

    DB/Misc: Make windows mysql appy

--- sql/updates/world/4.3.4/2017_03_20_00_world.sql
+++ sql/updates/world/4.3.4/2017_03_20_00_world.sql
@@ -2,14 +2,14 @@
 ALTER TABLE `locales_page_text` CHANGE `entry` `ID` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '0';
 
 ALTER TABLE `locales_points_of_interest` CHANGE `entry` `ID` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '0';
-ALTER TABLE `locales_points_of_interest` CHANGE `icon_name_loc1` `Name_loc1` TEXT NOT NULL DEFAULT '';
-ALTER TABLE `locales_points_of_interest` CHANGE `icon_name_loc2` `Name_loc2` TEXT NOT NULL DEFAULT '';
-ALTER TABLE `locales_points_of_interest` CHANGE `icon_name_loc3` `Name_loc3` TEXT NOT NULL DEFAULT '';
-ALTER TABLE `locales_points_of_interest` CHANGE `icon_name_loc4` `Name_loc4` TEXT NOT NULL DEFAULT '';
-ALTER TABLE `locales_points_of_interest` CHANGE `icon_name_loc5` `Name_loc5` TEXT NOT NULL DEFAULT '';
-ALTER TABLE `locales_points_of_interest` CHANGE `icon_name_loc6` `Name_loc6` TEXT NOT NULL DEFAULT '';
-ALTER TABLE `locales_points_of_interest` CHANGE `icon_name_loc7` `Name_loc7` TEXT NOT NULL DEFAULT '';
-ALTER TABLE `locales_points_of_interest` CHANGE `icon_name_loc8` `Name_loc8` TEXT NOT NULL DEFAULT '';
+ALTER TABLE `locales_points_of_interest` CHANGE `icon_name_loc1` `Name_loc1` TEXT;
+ALTER TABLE `locales_points_of_interest` CHANGE `icon_name_loc2` `Name_loc2` TEXT;
+ALTER TABLE `locales_points_of_interest` CHANGE `icon_name_loc3` `Name_loc3` TEXT;
+ALTER TABLE `locales_points_of_interest` CHANGE `icon_name_loc4` `Name_loc4` TEXT;
+ALTER TABLE `locales_points_of_interest` CHANGE `icon_name_loc5` `Name_loc5` TEXT;
+ALTER TABLE `locales_points_of_interest` CHANGE `icon_name_loc6` `Name_loc6` TEXT;
+ALTER TABLE `locales_points_of_interest` CHANGE `icon_name_loc7` `Name_loc7` TEXT;
+ALTER TABLE `locales_points_of_interest` CHANGE `icon_name_loc8` `Name_loc8` TEXT;
 
 ALTER TABLE `npc_trainer` CHANGE `reqLevel` `ReqLevel` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0';
 
