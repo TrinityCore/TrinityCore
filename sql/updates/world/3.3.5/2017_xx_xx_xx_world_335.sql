bc93cf65fddce592655b9e5f6126703727dcce6d
Chazy Chaz <ChazyTheBest@hotmail.es>
2017-03-23 18:21:06 +0100

    fix typo and update sql file

--- sql/updates/world/3.3.5/2017_xx_xx_xx_world_335.sql
+++ sql/updates/world/3.3.5/2017_xx_xx_xx_world_335.sql
@@ -11,35 +11,35 @@ ADD PRIMARY KEY (`ID`,`locale`);
 
 -- koKR
 INSERT INTO `points_of_interest_locale` (`ID`, `locale`, `Name`)
-	(SELECT `entry`, "koKR", `icon_name_loc1` FROM `locales_points_of_interest`);
+	(SELECT `entry`, "koKR", `icon_name_loc1` FROM `locales_points_of_interest` WHERE LENGTH(icon_name_loc1) > 0);
 
 -- frFR
 INSERT INTO `points_of_interest_locale` (`ID`, `locale`, `Name`)
-	(SELECT `entry`, "frFR", `icon_name_loc2` FROM `locales_points_of_interest`);
+	(SELECT `entry`, "frFR", `icon_name_loc2` FROM `locales_points_of_interest` WHERE LENGTH(icon_name_loc2) > 0);
 
 -- deDE
 INSERT INTO `points_of_interest_locale` (`ID`, `locale`, `Name`)
-	(SELECT `entry`, "deDE", `icon_name_loc3` FROM `locales_points_of_interest`);
+	(SELECT `entry`, "deDE", `icon_name_loc3` FROM `locales_points_of_interest` WHERE LENGTH(icon_name_loc3) > 0);
 
 -- zhCN
 INSERT INTO `points_of_interest_locale` (`ID`, `locale`, `Name`)
-	(SELECT `entry`, "zhCN", `icon_name_loc4` FROM `locales_points_of_interest`);
+	(SELECT `entry`, "zhCN", `icon_name_loc4` FROM `locales_points_of_interest` WHERE LENGTH(icon_name_loc4) > 0);
 
 -- zhTW
 INSERT INTO `points_of_interest_locale` (`ID`, `locale`, `Name`)
-	(SELECT `entry`, "zhTW", `icon_name_loc6` FROM `locales_points_of_interest`);
+	(SELECT `entry`, "zhTW", `icon_name_loc5` FROM `locales_points_of_interest` WHERE LENGTH(icon_name_loc5) > 0);
 
 -- esES
 INSERT INTO `points_of_interest_locale` (`ID`, `locale`, `Name`)
-	(SELECT `entry`, "esES", `icon_name_loc6` FROM `locales_points_of_interest`);
+	(SELECT `entry`, "esES", `icon_name_loc6` FROM `locales_points_of_interest` WHERE LENGTH(icon_name_loc6) > 0);
 
 -- esMX
 INSERT INTO `points_of_interest_locale` (`ID`, `locale`, `Name`)
-	(SELECT `entry`, "esMX", `icon_name_loc7` FROM `locales_points_of_interest`);
+	(SELECT `entry`, "esMX", `icon_name_loc7` FROM `locales_points_of_interest` WHERE LENGTH(icon_name_loc7) > 0);
 
 -- ruRU
 INSERT INTO `points_of_interest_locale` (`ID`, `locale`, `Name`)
-	(SELECT `entry`, "ruRU", `icon_name_loc8` FROM `locales_points_of_interest`);
+	(SELECT `entry`, "ruRU", `icon_name_loc8` FROM `locales_points_of_interest` WHERE LENGTH(icon_name_loc8) > 0);
 
 DROP TABLE IF EXISTS `locales_points_of_interest`;
 
