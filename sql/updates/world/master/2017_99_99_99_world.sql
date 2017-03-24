73725c29f55b2a7b145367a38f55c17fc9155ac7
tkrokli <tkrokli@users.noreply.github.com>
2017-03-24 23:26:25 +0100

    Update and rename RoG_22_03_2017_00_world.sql to 2017_99_99_99_world.sql
    - replaced comma with semicolon at end
    - removed unwanted apostrophes around integers

--- sql/updates/world/master/2017_99_99_99_world.sql
+++ sql/updates/world/master/2017_99_99_99_world.sql
@@ -1,4 +1,4 @@
 DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_warr_whirlwind','spell_warr_trauma');
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
-('1680', 'spell_warr_whirlwind'),
-('215538', 'spell_warr_trauma'),
+(1680, 'spell_warr_whirlwind'),
+(215538, 'spell_warr_trauma');
