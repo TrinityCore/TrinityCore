fce974c3b33b38b71166960d37ca8cff67fa8f14
ccrs <ccrs@users.noreply.github.com>
2017-03-25 12:02:25 +0100

    Core/Movement: cleanup MovementGeneratorType

--- src/server/game/Movement/MotionMaster.cpp
+++ src/server/game/Movement/MotionMaster.cpp
@@ -158,7 +158,7 @@ MovementGeneratorType MotionMaster::GetCurrentMovementGeneratorType() const
 MovementGeneratorType MotionMaster::GetMotionSlotType(int slot) const
 {
     if (!_slot[slot])
-        return NULL_MOTION_TYPE;
+        return MAX_MOTION_TYPE;
     else
         return _slot[slot]->GetMovementGeneratorType();
 }
