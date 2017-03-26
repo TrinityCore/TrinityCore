9c5e580c173ac4845bd258662cbdaf75efa1241e
Aokromes <aokromes@gmail.com>
2017-03-26 06:13:21 +0200

    Core/Scripts: minor fixes in boss_the_lich_king

--- src/server/scripts/Northrend/IcecrownCitadel/icecrown_citadel.h
+++ src/server/scripts/Northrend/IcecrownCitadel/icecrown_citadel.h
@@ -317,6 +317,7 @@ enum ICCreaturesIds
     NPC_WORLD_TRIGGER_INFINITE_AOI              = 36171,
     NPC_SPIRIT_BOMB                             = 39189,
     NPC_FROSTMOURNE_TRIGGER                     = 38584,
+    NPC_SHADOW_TRAP                             = 39137,
 
     // Generic
     NPC_INVISIBLE_STALKER                       = 30298
@@ -520,8 +521,7 @@ enum ICWorldStatesICC
 
 enum ICAreaIds
 {
-    AREA_ICECROWN_CITADEL   = 4812,
-    AREA_THE_FROZEN_THRONE  = 4859
+    AREA_ICECROWN_CITADEL   = 4812
 };
 
 class spell_trigger_spell_from_caster : public SpellScriptLoader
