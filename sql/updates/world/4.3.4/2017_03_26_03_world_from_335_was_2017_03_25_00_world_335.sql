c87742e190d7b30d2ebede7ae3d6a6cebfc8a538
Aokromes <aokromes@gmail.com>
2017-03-26 06:19:56 +0200

    rename files

---
+++ sql/updates/world/4.3.4/2017_03_26_03_world_from_335_was_2017_03_25_00_world_335.sql
@@ -0,0 +1,2 @@
+-- 4 gameobject entries already removed from Zul'Aman in commit 226a69f5dc
+-- DELETE FROM `gameobject_addon` WHERE `guid` IN (12647, 18539, 20584, 47284);
--- sql/updates/world/4.3.4/2017_03_26_06_world_from_335_was_2017_03_26_01_world_335.sql
+++ sql/updates/world/4.3.4/2017_03_26_06_world_from_335_was_2017_03_26_01_world_335.sql
@@ -1,5 +1,5 @@
 -- Verifonix <The Surveyor>
-UPDATE `creature_template` SET `faction`=47 WHERE `entry`=14492;
+-- UPDATE `creature_template` SET `faction`=47 WHERE `entry`=14492;
 
 DELETE FROM `creature_onkill_reputation` WHERE `creature_id`=14492;
 INSERT INTO `creature_onkill_reputation` (`creature_id`,`RewOnKillRepFaction1`,`RewOnKillRepFaction2`,`MaxStanding1`,`IsTeamAward1`,`RewOnKillRepValue1`,`MaxStanding2`,`IsTeamAward2`,`RewOnKillRepValue2`,`TeamDependent`) VALUES
