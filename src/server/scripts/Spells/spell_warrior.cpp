6a6c6c42a8bf4116aa8dcdb6495e24f0aabdecef
HannibalRoG <edreisantafe@gmail.com>
2017-03-26 22:24:23 -0400

    Fixes.

--- src/server/scripts/Spells/spell_warrior.cpp
+++ src/server/scripts/Spells/spell_warrior.cpp
@@ -1113,13 +1113,17 @@ public:
         void HandleProc(AuraEffect const* /*aurEff*/, ProcEventInfo& eventInfo)
         {
             PreventDefaultAction();
-            Unit* target = eventInfo.GetActionTarget();
-            
-            //Get the Remaining Damage from the aura (if exist)
-            int32 remainingDamage = target->GetRemainingPeriodicAmount(eventInfo.GetActor()->GetGUID(), SPELL_WARRIOR_TRAUMA_EFFECT, SPELL_AURA_PERIODIC_DAMAGE);
-            //Get 25% of damage from the spell casted (Slam & Whirlwind) plus Remaining Damage from Aura
-            int32 damage = (eventInfo.GetDamageInfo()->GetDamage() * 25.0 / 100.0) + remainingDamage;
-            GetCaster()->CastCustomSpell(target, SPELL_WARRIOR_TRAUMA_EFFECT, &damage, nullptr, nullptr, true);
+            if (Unit* caster = GetCaster())
+            {
+                if (Unit* target = eventInfo.GetActionTarget())
+                {
+                    //Get the Remaining Damage from the aura (if exist)
+                    int32 remainingDamage = target->GetRemainingPeriodicAmount(target->GetGUID(), SPELL_WARRIOR_TRAUMA_EFFECT, SPELL_AURA_PERIODIC_DAMAGE);
+                    //Get 25% of damage from the spell casted (Slam & Whirlwind) plus Remaining Damage from Aura
+                    int32 damage = (eventInfo.GetDamageInfo()->GetDamage() * 25.0 / 100.0) + remainingDamage;
+                    caster->CastCustomSpell(target, SPELL_WARRIOR_TRAUMA_EFFECT, &damage, nullptr, nullptr, true);
+                }
+            }
         }
 
         void Register() override
