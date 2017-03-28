d3151fed8cb555e71d87d3354357a2abb594f7e5
ariel- <ariel-@users.noreply.github.com>
2017-03-28 01:52:49 -0300

    Core/AI: AreaBoundary refactor
    - Added an auxiliary function IsInBounds to base CreatureAI
    - Changed container to vector. Set had no sense because we're storing new pointers, they have different addresses even if the boundary is the same

--- src/server/game/Instances/InstanceScript.cpp
+++ src/server/game/Instances/InstanceScript.cpp
@@ -120,7 +120,7 @@ void InstanceScript::LoadBossBoundaries(const BossBoundaryData& data)
 {
     for (BossBoundaryEntry const& entry : data)
         if (entry.BossId < bosses.size())
-            bosses[entry.BossId].boundary.insert(entry.Boundary);
+            bosses[entry.BossId].boundary.push_back(entry.Boundary);
 }
 
 void InstanceScript::LoadMinionData(const MinionData* data)
