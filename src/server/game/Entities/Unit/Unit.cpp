e478434146fd479fab955aaa80eb925c15e29809
ariel- <ariel-@users.noreply.github.com>
2017-04-09 19:18:22 -0300

    Core/Misc: fix static analysis issues
    CID 1373466
    CID 1373481
    CID 1373482

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -1712,7 +1712,7 @@ uint32 Unit::CalcSpellResistedDamage(Unit* victim, uint32 damage, SpellSchoolMas
     while (r >= probabilitySum && resistance < 10)
         probabilitySum += discreteResistProbability[++resistance];
 
-    float damageResisted = float(damage * resistance / 10);
+    float damageResisted = damage * resistance / 10.f;
     if (damageResisted > 0.0f) // if any damage was resisted
     {
         int32 ignoredResistance = 0;
