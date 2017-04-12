a723deb52ededb1bec0542b3d80d705add80819c
Aokromes <aokromes@gmail.com>
2017-04-12 23:26:15 +0200

    missing part

--- src/server/game/Movement/MotionMaster.h
+++ src/server/game/Movement/MotionMaster.h
@@ -146,8 +146,8 @@ class TC_GAME_API MotionMaster
         }
         void MoveJump(float x, float y, float z, float o, float speedXY, float speedZ, uint32 id = EVENT_JUMP, bool hasOrientation = false);
         void MoveCirclePath(float x, float y, float z, float radius, bool clockwise, uint8 stepCount);
-        void MoveSmoothPath(uint32 pointId, G3D::Vector3 const* pathPoints, size_t pathSize, bool walk, bool fly);
-        void MoveSmoothPath(uint32 pointId, Movement::PointsArray const& points, bool walk, bool fly);
+        void MoveSmoothPath(uint32 pointId, G3D::Vector3 const* pathPoints, size_t pathSize, bool walk = false, bool fly = false);
+        void MoveSmoothPath(uint32 pointId, Movement::PointsArray const& points, bool walk = false, bool fly = false);
         // Walk along spline chain stored in DB (script_spline_chain_meta and script_spline_chain_waypoints)
         void MoveAlongSplineChain(uint32 pointId, uint16 dbChainId, bool walk);
         void MoveAlongSplineChain(uint32 pointId, SplineChain const& chain, bool walk);
