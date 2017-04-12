296abf3e1229ea477fa5e266a6375c4a970f6300
ariel- <ariel-@users.noreply.github.com>
2017-04-12 13:31:11 -0300

    Core/Player: fix use after free crash
    (cherry picked from commit d914c55c0585fe31a9a1b9fe4e9d4a9b498e8e8f)

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -13875,10 +13875,11 @@ void Player::RemoveItemFromBuyBackSlot(uint32 slot, bool del)
             pItem->RemoveFromWorld();
             if (del)
             {
-                pItem->SetState(ITEM_REMOVED, this);
                 if (ItemTemplate const* itemTemplate = pItem->GetTemplate())
                     if (itemTemplate->GetFlags() & ITEM_FLAG_HAS_LOOT)
                         sLootItemStorage->RemoveStoredLootForContainer(pItem->GetGUID().GetCounter());
+
+                pItem->SetState(ITEM_REMOVED, this);
             }
         }
 
