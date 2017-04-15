eb0a14bba24a150942d9db3b6c9109336f743c72
ConanHUN <conan_513@hotmail.com>
2017-04-15 12:38:07 +0200

    Keep TC ahbot for testing
    This reverts commit aff2e978b193da1f47b56e0ca6c63538acb593c0.

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -2331,8 +2331,8 @@ void World::Update(uint32 diff)
         ///- Handle expired auctions
         sAuctionMgr->Update();
 
-        // ahbot mod
-        auctionbot.Update();
+        // ahbot mod (DMANBOB DISABLE ike3 AHbot) 
+		//auctionbot.Update();
     }
 
     // playerbot mod
@@ -2345,13 +2345,13 @@ void World::Update(uint32 diff)
 
         sAuctionMgr->UpdatePendingAuctions();
     }
-
+	// DMANBOB Restore Trinity AHBOT use worldserver.conf to enable or diasble AHbot
     /// <li> Handle AHBot operations
-    // if (m_timers[WUPDATE_AHBOT].Passed())
-    //{
-    //    sAuctionBot->Update();
-    //    m_timers[WUPDATE_AHBOT].Reset();
-    //}
+	if (m_timers[WUPDATE_AHBOT].Passed())
+	{
+		sAuctionBot->Update();
+		m_timers[WUPDATE_AHBOT].Reset();
+	}
     // end of playerbot mod
 
     /// <li> Handle file changes
