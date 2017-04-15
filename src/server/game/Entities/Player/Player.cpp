f20b35ff3d7c172ecd7c3fbcac24f5bfcd0b1710
ConanHUN <conan_513@hotmail.com>
2017-04-15 14:17:54 +0200

    Typo for Dungeon checkoint fix

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -5302,13 +5302,15 @@ void Player::RepopAtGraveyard()
 				sDynRes->DynamicResurrection(this);
 			else
 			{
-				TeleportTo(ClosestGrave->MapID, ClosestGrave->Loc.X, ClosestGrave->Loc.Y, ClosestGrave->Loc.Z, (ClosestGrave->Facing * M_PI) / 180); // Orientation is initially in degrees
+				TeleportTo(ClosestGrave->map_id, ClosestGrave->x, ClosestGrave->y, ClosestGrave->z, GetOrientation());
 				if (isDead())                                        // not send if alive, because it used in TeleportTo()
 				{
-					WorldPackets::Misc::DeathReleaseLoc packet;
-					packet.MapID = ClosestGrave->MapID;
-					packet.Loc = G3D::Vector3(ClosestGrave->Loc.X, ClosestGrave->Loc.Y, ClosestGrave->Loc.Z);
-					GetSession()->SendPacket(packet.Write());
+					WorldPacket data(SMSG_DEATH_RELEASE_LOC, 4 * 4);  // show spirit healer position on minimap
+					data << ClosestGrave->map_id;
+					data << ClosestGrave->x;
+					data << ClosestGrave->y;
+					data << ClosestGrave->z;
+					GetSession()->SendPacket(&data);
 				}
 			}
 		}
