6871bb1729c18d3c629b42fd2969c824f0fde316
Aokromes <aokromes@gmail.com>
2017-04-20 22:25:46 +0200

    Build: Added 1.64 Boost version to FindBoost

---
+++ sql/updates/world/4.3.4/2017_04_20_05_world.sql
@@ -0,0 +1,2 @@
+--
+UPDATE `command` SET `name`='reload gameobject_template_locale', `help`='Syntax: .reload gameobject_template_localeReload gameobject_template_locale table.' WHERE `name`='reload locales_gameobject';
