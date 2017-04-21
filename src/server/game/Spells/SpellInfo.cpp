8b8bf5399d9aa4eec48a384639db0f49e4b93b83
ariel- <ariel-@users.noreply.github.com>
2017-04-21 01:35:42 -0300

    Revert "Core/Spell: don't set scaleAura for some special spells"
    This reverts commit 1ca8434b365687dbd12ca232a615d126234eba5e.
    
    Closes #19472

--- src/server/game/Spells/SpellInfo.cpp
+++ src/server/game/Spells/SpellInfo.cpp
@@ -3094,10 +3094,6 @@ bool SpellInfo::_IsPositiveEffect(uint8 effIndex, bool deep) const
     if (HasAttribute(SPELL_ATTR0_NEGATIVE_1))
         return false;
 
-    // these spells must not be downscaled, thus marking them negative (see GetAuraRankForLevel)
-    if (HasAttribute(SPELL_ATTR2_UNK3))
-        return false;
-
     switch (SpellFamilyName)
     {
         case SPELLFAMILY_GENERIC:
@@ -3169,6 +3165,7 @@ bool SpellInfo::_IsPositiveEffect(uint8 effIndex, bool deep) const
                 case SPELL_AURA_MOD_STEALTH:
                     return true;
                 case SPELL_AURA_CHANNEL_DEATH_ITEM:
+                case SPELL_AURA_EMPATHY:
                     return false;
             }
         }
