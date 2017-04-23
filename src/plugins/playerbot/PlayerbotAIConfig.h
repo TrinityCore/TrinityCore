c4fdabdc1478dfb85a08c727b3edb88aba4fc931
ConanHUN <conan_513@hotmail.com>
2017-04-23 20:29:30 +0200

    Revert "Remove randomBotBracketPlayer"
    This reverts commit 4f5f32125bb178f3b29a2f16ae4a7c3fa4d3b3c1.

--- src/plugins/playerbot/PlayerbotAIConfig.h
+++ src/plugins/playerbot/PlayerbotAIConfig.h
@@ -53,6 +53,7 @@ public:
     bool randomBotJoinLfg;
     bool randomBotLoginAtStartup;
 	bool randomBotJoinBG;
+	bool randomBotBracketPlayer;
     uint32 randomBotTeleLevel;
     bool logInGroupOnly, logValuesPerTick;
     bool fleeingEnabled;
