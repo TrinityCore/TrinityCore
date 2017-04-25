d01f1d31bd0972bd30a4ad50b80c2004a329e859
jackpoz <giacomopoz@gmail.com>
2017-04-25 11:52:50 +0200

    Core/Misc: Fix static analysis issues

--- src/server/database/Database/DatabaseEnv.h
+++ src/server/database/Database/DatabaseEnv.h
@@ -30,7 +30,7 @@
 #include "Transaction.h"
 
 #define _LIKE_           "LIKE"
-#define _TABLE_SIM_      "`"
+#define _TABLE_SIM_      '`'
 #define _CONCAT3_(A, B, C) "CONCAT( " A ", " B ", " C " )"
 #define _OFFSET_         "LIMIT %d, 1"
 
