92fa559cb2a69b7fbb7ff721e93da745ed32d3fe
Aokromes <aokromes@gmail.com>
2017-04-28 13:01:33 +0200

    rename files

--- sql/updates/world/3.3.5/2017_04_27_00_world_335.sql
+++
@@ -1,2 +0,0 @@
--- Fix for "Guarded Thunderbrew Barrel" and "Distracting Jarven"
-UPDATE `quest_template_addon` SET `PrevQuestID`=0 WHERE `id` in (308,403);
---
+++ sql/updates/world/4.3.4/2017_04_28_00_world_from_335_2017_04_27_00_world_335.sql
@@ -0,0 +1,2 @@
+-- Fix for "Guarded Thunderbrew Barrel" and "Distracting Jarven"
+-- UPDATE `quest_template_addon` SET `PrevQuestID`=0 WHERE `id` in (308,403);
