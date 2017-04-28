9ab7f25e5101c68bdb64bd1f298d6ebddba9f57f
TecDian <tec.dian@akilet.de>
2017-04-28 18:03:10 +0200

    adds a missing line for "Love is in the air" boss fight, closes #19520

---
+++ sql/updates/world/3.3.5/2017_xx_xx_xx_world_335.sql
@@ -0,0 +1,3 @@
+-- add text for use in boss_apothecary_hummel.cpp
+INSERT INTO `creature_text` (`entry`,`groupid`,`id`,`text`,`type`,`language`,`probability`,`emote`,`duration`,`sound`,`BroadcastTextId`,`TextRange`,`comment`) VALUES
+(36296,6,0,"Apothecaries! Give your life for the Crown!",12,0,100,0,0,0,38594,3,'SAY_SUMMON_ADDS');
