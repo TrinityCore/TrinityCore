7a26c1a2e9ac42c5b2dde0c0f238f3f511145362
Chazy Chaz <ChazyTheBest@hotmail.es>
2017-04-30 18:14:44 +0200

    sql: fix copy paste error

--- sql/updates/world/3.3.5/2017_xx_xx_xx_world.sql
+++ sql/updates/world/3.3.5/2017_xx_xx_xx_world.sql
@@ -21,34 +21,34 @@ CREATE TABLE IF NOT EXISTS `achievement_reward_locale` (
 
 -- koKR
 INSERT INTO `achievement_reward_locale` (`ID`, `Locale`, `Subject`, `Text`)
-  (SELECT `entry`, "koKR", `subject_loc1`, `text_loc1` FROM `locales_item` WHERE LENGTH(subject_loc1) > 0 OR LENGTH(text_loc1) > 0);
+  (SELECT `entry`, "koKR", `subject_loc1`, `text_loc1` FROM `locales_achievement_reward` WHERE LENGTH(subject_loc1) > 0 OR LENGTH(text_loc1) > 0);
 
 -- frFR
 INSERT INTO `achievement_reward_locale` (`ID`, `locale`, `Subject`, `Text`)
-  (SELECT `entry`, "frFR", `subject_loc2`, `text_loc2` FROM `locales_item` WHERE LENGTH(subject_loc2) > 0 OR LENGTH(text_loc2) > 0);
+  (SELECT `entry`, "frFR", `subject_loc2`, `text_loc2` FROM `locales_achievement_reward` WHERE LENGTH(subject_loc2) > 0 OR LENGTH(text_loc2) > 0);
 
 -- deDE
 INSERT INTO `achievement_reward_locale` (`ID`, `locale`, `Subject`, `Text`)
-  (SELECT `entry`, "deDE", `subject_loc3`, `text_loc3` FROM `locales_item` WHERE LENGTH(subject_loc3) > 0 OR LENGTH(text_loc3) > 0);
+  (SELECT `entry`, "deDE", `subject_loc3`, `text_loc3` FROM `locales_achievement_reward` WHERE LENGTH(subject_loc3) > 0 OR LENGTH(text_loc3) > 0);
 
 -- zhCN
 INSERT INTO `achievement_reward_locale` (`ID`, `locale`, `Subject`, `Text`)
-  (SELECT `entry`, "zhCN", `subject_loc4`, `text_loc4` FROM `locales_item` WHERE LENGTH(subject_loc4) > 0 OR LENGTH(text_loc4) > 0);
+  (SELECT `entry`, "zhCN", `subject_loc4`, `text_loc4` FROM `locales_achievement_reward` WHERE LENGTH(subject_loc4) > 0 OR LENGTH(text_loc4) > 0);
 
 -- zhTW
 INSERT INTO `achievement_reward_locale` (`ID`, `locale`, `Subject`, `Text`)
-  (SELECT `entry`, "zhTW", `subject_loc5`, `text_loc5` FROM `locales_item` WHERE LENGTH(subject_loc5) > 0 OR LENGTH(text_loc5) > 0);
+  (SELECT `entry`, "zhTW", `subject_loc5`, `text_loc5` FROM `locales_achievement_reward` WHERE LENGTH(subject_loc5) > 0 OR LENGTH(text_loc5) > 0);
 
 -- esES
 INSERT INTO `achievement_reward_locale` (`ID`, `locale`, `Subject`, `Text`)
-  (SELECT `entry`, "esES", `subject_loc6`, `text_loc6` FROM `locales_item` WHERE LENGTH(subject_loc6) > 0 OR LENGTH(text_loc6) > 0);
+  (SELECT `entry`, "esES", `subject_loc6`, `text_loc6` FROM `locales_achievement_reward` WHERE LENGTH(subject_loc6) > 0 OR LENGTH(text_loc6) > 0);
 
 -- esMX
 INSERT INTO `achievement_reward_locale` (`ID`, `locale`, `Subject`, `Text`)
-  (SELECT `entry`, "esMX", `subject_loc7`, `text_loc7` FROM `locales_item` WHERE LENGTH(subject_loc7) > 0 OR LENGTH(text_loc7) > 0);
+  (SELECT `entry`, "esMX", `subject_loc7`, `text_loc7` FROM `locales_achievement_reward` WHERE LENGTH(subject_loc7) > 0 OR LENGTH(text_loc7) > 0);
 
 -- ruRU
 INSERT INTO `achievement_reward_locale` (`ID`, `locale`, `Subject`, `Text`)
-  (SELECT `entry`, "ruRU", `subject_loc8`, `text_loc8` FROM `locales_item` WHERE LENGTH(subject_loc8) > 0 OR LENGTH(text_loc8) > 0);
+  (SELECT `entry`, "ruRU", `subject_loc8`, `text_loc8` FROM `locales_achievement_reward` WHERE LENGTH(subject_loc8) > 0 OR LENGTH(text_loc8) > 0);
 
 DROP TABLE IF EXISTS `locales_achievement_reward`;
