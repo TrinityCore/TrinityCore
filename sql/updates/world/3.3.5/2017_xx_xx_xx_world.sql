51c59d7f7a9d3ca87004a4ecc822515aceb12b19
Chazy Chaz <ChazyTheBest@hotmail.es>
2017-04-30 19:52:53 +0200

    sql: fix incomplete queries...

--- sql/updates/world/3.3.5/2017_xx_xx_xx_world.sql
+++ sql/updates/world/3.3.5/2017_xx_xx_xx_world.sql
@@ -28,34 +28,34 @@ CREATE TABLE IF NOT EXISTS `gossip_menu_option_locale` (
 
 -- koKR
 INSERT INTO `gossip_menu_option_locale` (`MenuID`, `OptionID`, `Locale`, `OptionText`, `BoxText`)
-  (SELECT `menu_id`, `id`, "koKR", `text_loc1` FROM `locales_gossip_menu_option` WHERE LENGTH(text_loc1) > 0);
+  (SELECT `menu_id`, `id`, "koKR", `option_text_loc1`, `box_text_loc1` FROM `locales_gossip_menu_option` WHERE LENGTH(option_text_loc1) > 0 && LENGTH(box_text_loc1) > 0);
 
 -- frFR
 INSERT INTO `gossip_menu_option_locale` (`MenuID`, `OptionID`, `Locale`, `OptionText`, `BoxText`)
-  (SELECT `menu_id`, `id`, "frFR", `text_loc2` FROM `locales_gossip_menu_option` WHERE LENGTH(text_loc2) > 0);
+  (SELECT `menu_id`, `id`, "frFR", `option_text_loc2`, `box_text_loc2` FROM `locales_gossip_menu_option` WHERE LENGTH(option_text_loc2) > 0 && LENGTH(box_text_loc2) > 0);
 
 -- deDE
 INSERT INTO `gossip_menu_option_locale` (`MenuID`, `OptionID`, `Locale`, `OptionText`, `BoxText`)
-  (SELECT `menu_id`, `id`, "deDE", `text_loc3` FROM `locales_gossip_menu_option` WHERE LENGTH(text_loc3) > 0);
+  (SELECT `menu_id`, `id`, "deDE", `option_text_loc3`, `box_text_loc3` FROM `locales_gossip_menu_option` WHERE LENGTH(option_text_loc3) > 0 && LENGTH(box_text_loc3) > 0);
 
 -- zhCN
 INSERT INTO `gossip_menu_option_locale` (`MenuID`, `OptionID`, `Locale`, `OptionText`, `BoxText`)
-  (SELECT `menu_id`, `id`, "zhCN", `text_loc4` FROM `locales_gossip_menu_option` WHERE LENGTH(text_loc4) > 0);
+  (SELECT `menu_id`, `id`, "zhCN", `option_text_loc4`, `box_text_loc4` FROM `locales_gossip_menu_option` WHERE LENGTH(option_text_loc4) > 0 && LENGTH(box_text_loc4) > 0);
 
 -- zhTW
 INSERT INTO `gossip_menu_option_locale` (`MenuID`, `OptionID`, `Locale`, `OptionText`, `BoxText`)
-  (SELECT `menu_id`, `id`, "zhTW", `text_loc5` FROM `locales_gossip_menu_option` WHERE LENGTH(text_loc5) > 0);
+  (SELECT `menu_id`, `id`, "zhTW", `option_text_loc5`, `box_text_loc5` FROM `locales_gossip_menu_option` WHERE LENGTH(option_text_loc5) > 0 && LENGTH(box_text_loc5) > 0);
 
 -- esES
 INSERT INTO `gossip_menu_option_locale` (`MenuID`, `OptionID`, `Locale`, `OptionText`, `BoxText`)
-  (SELECT `menu_id`, `id`, "esES", `text_loc6` FROM `locales_gossip_menu_option` WHERE LENGTH(text_loc6) > 0);
+  (SELECT `menu_id`, `id`, "esES", `option_text_loc6`, `box_text_loc6` FROM `locales_gossip_menu_option` WHERE LENGTH(option_text_loc6) > 0 && LENGTH(box_text_loc6) > 0);
 
 -- esMX
 INSERT INTO `gossip_menu_option_locale` (`MenuID`, `OptionID`, `Locale`, `OptionText`, `BoxText`)
-  (SELECT `menu_id`, `id`, "esMX", `text_loc7` FROM `locales_gossip_menu_option` WHERE LENGTH(text_loc7) > 0);
+  (SELECT `menu_id`, `id`, "esMX", `option_text_loc7`, `box_text_loc7` FROM `locales_gossip_menu_option` WHERE LENGTH(option_text_loc7) > 0 && LENGTH(box_text_loc7) > 0);
 
 -- ruRU
 INSERT INTO `gossip_menu_option_locale` (`MenuID`, `OptionID`, `Locale`, `OptionText`, `BoxText`)
-  (SELECT `menu_id`, `id`, "ruRU", `text_loc8` FROM `locales_gossip_menu_option` WHERE LENGTH(text_loc8) > 0);
+  (SELECT `menu_id`, `id`, "ruRU", `option_text_loc8`, `box_text_loc8` FROM `locales_gossip_menu_option` WHERE LENGTH(option_text_loc8) > 0 && LENGTH(box_text_loc8) > 0);
 
 DROP TABLE IF EXISTS `locales_gossip_menu_option`;
