31cdd7f9f03fbce49ba55aedf99a66ce28834fbe
Aokromes <aokromes@gmail.com>
2017-05-02 04:58:13 +0200

    meh

--- sql/updates/world/4.3.4/2017_05_02_06_world.sql
+++ sql/updates/world/4.3.4/2017_05_02_06_world.sql
@@ -1,2 +1,2 @@
 --
-UPDATE `item_template` SET `ScriptName`='item_generic_limit_chance_above_60' WHERE `entry`=19169;
+-- UPDATE `item_template` SET `ScriptName`='item_generic_limit_chance_above_60' WHERE `entry`=19169;
