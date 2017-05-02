6a6632767dfe29909ae64b2c1786de9a81399417
Aokromes <aokromes@gmail.com>
2017-05-02 05:18:41 +0200

    DB/Creature: Restore incorrectly deleted creature

---
+++ sql/updates/world/4.3.4/2017_05_02_07_world.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `creature` WHERE `guid` IN (250023);
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `PhaseId`, `PhaseGroup`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `VerifiedBuild`) VALUES
+(250023, 40970, 1, 0, 0, 1, 169, 0, 0, 1, 318.4895, -4740.3544, 9.57535, 0.086387, 300, 0, 0, 0, 0, 2, 0, 0, 0, 0);
