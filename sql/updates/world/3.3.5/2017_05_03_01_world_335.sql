5e9f1c00797bc852f69d5720d19775aaf959c949
Aokromes <aokromes@gmail.com>
2017-05-04 17:05:24 +0200

    rename files

--- sql/updates/world/3.3.5/2017_05_03_01_world_335.sql
+++
@@ -1,3 +0,0 @@
--- Marin Noggenfogger should mix the elixir before quest 2662 completes
-UPDATE `smart_scripts` SET `event_type`=19, `comment`= "Marin Noggenfogger - On Quest 'Noggenfogger Elixir' Taken - Run Script"
- WHERE `entryorguid`= 7564 AND `source_type`= 0;
