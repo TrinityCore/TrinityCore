ba591e8f2930b1a692f1fddb0f4b5ca19bb4f83b
tkrokli <tkrokli@users.noreply.github.com>
2017-05-05 19:00:36 +0200

    Update 2107_99_99_19297_world.sql
    - removing unneeded blank line

--- sql/updates/world/master/2107_99_99_19297_world.sql
+++ sql/updates/world/master/2107_99_99_19297_world.sql
@@ -1,5 +1,4 @@
 -- Class: Warrior, Spec: Arms, Protection, Spells: Impending Victory & Victorious State
-
 DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_warr_impending_victory','spell_warr_victorious_state');
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (202168, 'spell_warr_impending_victory'),
