48fb35b9d7a6e010e33a878b620fa7bd4c3ac9e5
Defu <estafeitor@gmail.com>
2017-05-12 22:55:21 +0200

    SQL security fix

--- sql/updates/world/master/9999_99_99_99_world.sql
+++ sql/updates/world/master/9999_99_99_99_world.sql
@@ -1,2 +1,2 @@
-DELETE FROM spell_script_names WHERE spell_id=186257;
+DELETE FROM spell_script_names WHERE spell_id=186257 AND ScriptName="spell_hun_aspect_cheetah";
 INSERT INTO spell_script_names(spell_id, ScriptName) VALUES(186257,"spell_hun_aspect_cheetah");
