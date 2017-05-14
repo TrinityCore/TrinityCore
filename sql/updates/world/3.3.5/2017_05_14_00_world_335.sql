4b6273438191dd5fceca5b61fe35d2bcbcf9aa65
Shauren <shauren.trinity@gmail.com>
2017-05-14 12:31:49 +0200

    Fix sql import

--- sql/updates/world/3.3.5/2017_05_14_00_world_335.sql
+++ sql/updates/world/3.3.5/2017_05_14_00_world_335.sql
@@ -1,4 +1,4 @@
 -- add text for use in boss_apothecary_hummel.cpp
-DELETE FROM `creature_text` WHERE `entry`=36296 AND `groupid`=6 AND `id`=0;
-INSERT INTO `creature_text` (`entry`,`groupid`,`id`,`text`,`type`,`language`,`probability`,`emote`,`duration`,`sound`,`BroadcastTextId`,`TextRange`,`comment`) VALUES
+DELETE FROM `creature_text` WHERE `CreatureID`=36296 AND `GroupID`=6 AND `ID`=0;
+INSERT INTO `creature_text` (`CreatureID`,`GroupID`,`ID`,`Text`,`Type`,`Language`,`Probability`,`Emote`,`Duration`,`sound`,`BroadcastTextId`,`TextRange`,`comment`) VALUES
 (36296,6,0,"Apothecaries! Give your life for the Crown!",12,0,100,0,0,0,38594,3,'SAY_SUMMON_ADDS');
