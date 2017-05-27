6f73960480368521fbc1d4cb58e940c8916d162e
treeston <treeston.mmoc@gmail.com>
2017-05-27 20:49:24 +0200

    Instances/Ulduar: Fix a super edge case bug where Algalon would evade if his tank successfully tanked a Cosmic Smash and got knocked up. Shadow priests everywhere rejoiced.
    (cherry picked from commit b6a4e9f3f9aca8e82e4b8cc1868e4b24dd2bba6f)

--- src/server/scripts/Northrend/Ulduar/Ulduar/instance_ulduar.cpp
+++ src/server/scripts/Northrend/Ulduar/Ulduar/instance_ulduar.cpp
@@ -38,9 +38,9 @@ static BossBoundaryData const boundaries =
     { BOSS_XT002, new RectangleBoundary(755.0f, 940.0f, -125.0f, 95.0f) },
     { BOSS_ASSEMBLY_OF_IRON, new CircleBoundary(Position(1587.2f, 121.0f), 90.0) },
     { BOSS_ALGALON, new CircleBoundary(Position(1632.668f, -307.7656f), 45.0) },
-    { BOSS_ALGALON, new ZRangeBoundary(410.0f, 440.0f) },
+    { BOSS_ALGALON, new ZRangeBoundary(410.0f, 470.0f) },
     { BOSS_HODIR, new EllipseBoundary(Position(2001.5f, -240.0f), 50.0, 75.0) },
-    // Thorim sets boundaries dinamically
+    // Thorim sets boundaries dynamically
     { BOSS_FREYA, new RectangleBoundary(2094.6f, 2520.0f, -250.0f, 200.0f) },
     { BOSS_MIMIRON, new CircleBoundary(Position(2744.0f, 2569.0f), 70.0) },
     { BOSS_VEZAX, new RectangleBoundary(1740.0f, 1930.0f, 31.0f, 228.0f) },
