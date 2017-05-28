9016c3738978983995714aff4dcb33ef53370ba6
Aokromes <aokromes@gmail.com>
2017-05-29 00:04:53 +0200

    DB/Misc: Fix some missing texts

---
+++ sql/updates/world/4.3.4/2017_05_28_06_world.sql
@@ -0,0 +1,5 @@
+--
+UPDATE `npc_text` SET `text0_0`='I have no time for a sermon now, $c. Seek your knowledge elsewhere.', `text0_1`='I have no time for a sermon now, $c. Seek your knowledge elsewhere.' WHERE `ID`=9186;
+UPDATE `npc_text` SET `text0_0`='What assistance can I offer in your studies?' WHERE `ID`=9192;
+UPDATE `npc_text` SET `text0_0`='It appears you took a wrong turn, $c.', `text0_1`='It appears you took a wrong turn, $c.' WHERE `ID`=9187;
+UPDATE `npc_text` SET `text0_0`='I train only warriors, $c. You\'ll have to look elsewhere.', `text0_1`='I train only warriors, $c. You\'ll have to look elsewhere.' WHERE `ID`=16540;
