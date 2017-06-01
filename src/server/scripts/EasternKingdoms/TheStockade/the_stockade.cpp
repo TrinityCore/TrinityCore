b4a8fa676c45c5875e16da617cc47c972b8477c6
Artamedes <ownedned123@gmail.com>
2017-06-01 17:15:31 -0400

    Scripts/Stockades - Add scripts to instance (#19522)

--- src/server/scripts/EasternKingdoms/TheStockade/the_stockade.cpp
+++ src/server/scripts/EasternKingdoms/TheStockade/the_stockade.cpp
@@ -16,31 +16,5 @@
  */
 
 #include "ScriptMgr.h"
-#include "InstanceScript.h"
-#include "Map.h"
+#include "ScriptedCreature.h"
 #include "the_stockade.h"
-
-class instance_the_stockade : public InstanceMapScript
-{
-public:
-    instance_the_stockade() : InstanceMapScript("instance_the_stockade", 34) { }
-
-    struct instance_the_stockade_InstanceMapScript : public InstanceScript
-    {
-        instance_the_stockade_InstanceMapScript(Map* map) : InstanceScript(map)
-        {
-            SetHeaders(DataHeader);
-            SetBossNumber(EncounterCount);
-        }
-    };
-
-    InstanceScript* GetInstanceScript(InstanceMap* map) const override
-    {
-        return new instance_the_stockade_InstanceMapScript(map);
-    }
-};
-
-void AddSC_instance_the_stockade()
-{
-    new instance_the_stockade();
-}
