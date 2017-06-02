214b5a65e34b87bc76868bd97c3a2764afbeccac
Aokromes <aokromes@gmail.com>
2017-06-02 16:07:06 +0200

    DB/Misc: Misc fixes for Dun Morogh By Malcrom

--- src/server/scripts/EasternKingdoms/eastern_kingdoms_script_loader.cpp
+++ src/server/scripts/EasternKingdoms/eastern_kingdoms_script_loader.cpp
@@ -185,6 +185,7 @@ void AddSC_instance_zulgurub();
 //void AddSC_alterac_mountains();
 void AddSC_arathi_highlands();
 void AddSC_blasted_lands();
+void AddSC_dun_morogh();
 void AddSC_dun_morogh_area_coldridge_valley();
 void AddSC_duskwood();
 //void AddSC_eastern_plaguelands();
@@ -375,6 +376,7 @@ void AddEasternKingdomsScripts()
     //AddSC_alterac_mountains();
     AddSC_arathi_highlands();
     AddSC_blasted_lands();
+    AddSC_dun_morogh();
     AddSC_dun_morogh_area_coldridge_valley();
     AddSC_duskwood();
     //AddSC_eastern_plaguelands();
