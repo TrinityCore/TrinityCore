866074b6899d87df6c835b936524d9f76ba78949
ariel- <ariel-@users.noreply.github.com>
2017-06-04 16:43:36 -0300

    Core/SAI: properly validate stored targets when using SmartTrigger and fix a related crash

--- src/server/game/AI/SmartScripts/SmartScript.h
+++ src/server/game/AI/SmartScripts/SmartScript.h
@@ -106,7 +106,7 @@ class TC_GAME_API SmartScript
         {
             // insert or replace
             _storedTargets.erase(id);
-            _storedTargets.emplace(id, ObjectGuidVector(GetBaseObject(), targets));
+            _storedTargets.emplace(id, ObjectGuidVector(targets));
         }
 
         bool IsSmart(Creature* c = nullptr)
@@ -138,11 +138,11 @@ class TC_GAME_API SmartScript
             return smart;
         }
 
-        ObjectVector const* GetStoredTargetVector(uint32 id) const
+        ObjectVector const* GetStoredTargetVector(uint32 id, WorldObject const& ref) const
         {
             auto itr = _storedTargets.find(id);
             if (itr != _storedTargets.end())
-                return itr->second.GetObjectVector();
+                return itr->second.GetObjectVector(ref);
             return nullptr;
         }
 
