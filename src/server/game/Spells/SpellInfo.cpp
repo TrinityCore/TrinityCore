bc1887c5df84c1cec24dc031f4834d7300db0d11
Keader <keader.android@gmail.com>
2017-06-05 12:02:05 -0300

    Core/Spells: Allows Dispersion under Freeze (stun by hunter trap)

--- src/server/game/Spells/SpellInfo.cpp
+++ src/server/game/Spells/SpellInfo.cpp
@@ -2949,6 +2949,7 @@ void SpellInfo::_LoadImmunityInfo()
         switch (Id)
         {
             case 22812: // Barkskin
+            case 47585: // Dispersion
                 _allowedMechanicMask |=
                     (1 << MECHANIC_STUN) |
                     (1 << MECHANIC_FREEZE) |
@@ -2970,8 +2971,8 @@ void SpellInfo::_LoadImmunityInfo()
     {
         switch (Id)
         {
-            case 47585: // Dispersion
             case 22812: // Barkskin
+            case 47585: // Dispersion
                 _allowedMechanicMask |= (1 << MECHANIC_FEAR) | (1 << MECHANIC_HORROR);
                 break;
             default:
