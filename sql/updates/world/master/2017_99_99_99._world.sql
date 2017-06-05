60e259f9aa9793a36c2bedce7d19449c73016fe8
Defu <estafeitor@gmail.com>
2017-06-05 23:55:50 +0200

    New Script

---
+++ sql/updates/world/master/2017_99_99_99._world.sql
@@ -0,0 +1,5 @@
+
+-- Exhilaration
+DELETE FROM `spell_script_names` where `ScriptName`='spell_hun_exhilaration';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) 
+VALUES(109304,'spell_hun_exhilaration');
