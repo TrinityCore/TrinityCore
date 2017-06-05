14e221a4db2dad79290639f0e30f58fb01223350
ConanHUN <conan_513@hotmail.com>
2017-06-05 16:29:21 +0200

    Revert "Update GenericRogueNonCombatStrategy.cpp"
    This reverts commit 174b0107637199ebfd2a260f6883731a7ab3676d.

--- src/plugins/playerbot/strategy/rogue/GenericRogueNonCombatStrategy.cpp
+++ src/plugins/playerbot/strategy/rogue/GenericRogueNonCombatStrategy.cpp
@@ -7,10 +7,34 @@
 
 using namespace ai;
 
-void GenericRogueNonCombatStrategy::InitTriggers(std::list<TriggerNode*> &triggers)
+class GenericRogueNonCombatStrategyActionNodeFactory : public NamedObjectFactory<ActionNode>
+{
+public:
+	GenericRogueNonCombatStrategyActionNodeFactory()
 	{
-    	NonCombatStrategy::InitTriggers(triggers);
-    		triggers.push_back(new TriggerNode(
-		"in battleground without flag",
-			NextAction::array(0, new NextAction("mount", 1.0f), NULL)));    
+		
+		creators["stealth"] = &stealth;
 	}
+private:
+	static ActionNode* stealth(PlayerbotAI* ai)
+	{
+		return new ActionNode("stealth",
+			/*P*/ NULL,
+			/*A*/ NULL,
+			/*C*/ NULL);
+	}
+	
+};
+
+
+GenericRogueNonCombatStrategy::GenericRogueNonCombatStrategy(PlayerbotAI* ai) : NonCombatStrategy(ai)
+{
+	actionNodeFactories.Add(new GenericRogueNonCombatStrategyActionNodeFactory());
+}
+/*
+void GenericRogueNonCombatStrategy::InitTriggers(std::list<TriggerNode*> &triggers)
+{
+    NonCombatStrategy::InitTriggers(triggers);
+        
+}
+*/
