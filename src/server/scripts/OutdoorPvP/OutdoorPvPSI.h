3e7b64b2f765bebc722c1194b7fc2f749fae77f7
ariel- <ariel-@users.noreply.github.com>
2017-06-11 16:41:11 -0300

    Core/OutdoorPvP: refactor using Position and Quat to pack parameters
    - Moved statics to cpp
    - Save scriptids into an array

--- src/server/scripts/OutdoorPvP/OutdoorPvPSI.h
+++ src/server/scripts/OutdoorPvP/OutdoorPvPSI.h
@@ -28,22 +28,6 @@ enum OutdoorPvPSISpells
     SI_CENARION_FAVOR = 30754
 };
 
-const uint32 SI_MAX_RESOURCES = 200;
-
-const uint8 OutdoorPvPSIBuffZonesNum = 3;
-
-const uint32 OutdoorPvPSIBuffZones[OutdoorPvPSIBuffZonesNum] = { 1377, 3428, 3429 };
-
-const uint32 SI_AREATRIGGER_H = 4168;
-
-const uint32 SI_AREATRIGGER_A = 4162;
-
-const uint32 SI_TURNIN_QUEST_CM_A = 17090;
-
-const uint32 SI_TURNIN_QUEST_CM_H = 18199;
-
-const uint32 SI_SILITHYST_MOUND = 181597;
-
 enum SI_WorldStates
 {
     SI_GATHERED_A = 2313,
