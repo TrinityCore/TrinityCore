d33f462200ef9f348358e454b23c8eca61fe8439
treeston <treeston.mmoc@gmail.com>
2017-06-12 13:17:48 +0200

    Removed a now-unused local (ca02629 follow-up)

--- src/server/scripts/Commands/cs_group.cpp
+++ src/server/scripts/Commands/cs_group.cpp
@@ -69,7 +69,6 @@ public:
         }
 
         Player* gmPlayer = handler->GetSession()->GetPlayer();
-        Group* gmGroup = gmPlayer->GetGroup();
         Map* gmMap = gmPlayer->GetMap();
         bool toInstance = gmMap->Instanceable();
         bool onlyLocalSummon = false;
