364631d67eaaa10cf4d6fc7268e9c547616d65d7
ConanHUN <conan_513@hotmail.com>
2017-06-13 15:36:33 +0200

    Revert "Pull DK from Lidocain"
    This reverts commit 459f1170d0697ce20fc610091493fe15a3fa79f2, reversing
    changes made to 1df5eb1200700a5ab10284cac363991299392336.

--- src/plugins/playerbot/strategy/rogue/GenericRogueNonCombatStrategy.cpp
+++ src/plugins/playerbot/strategy/rogue/GenericRogueNonCombatStrategy.cpp
@@ -7,10 +7,25 @@
 
 using namespace ai;
 
-void GenericRogueNonCombatStrategy::InitTriggers(std::list<TriggerNode*> &triggers)
+class GenericRogueNonCombatStrategyActionNodeFactory : public NamedObjectFactory<ActionNode>
+{
+public:
+	GenericRogueNonCombatStrategyActionNodeFactory()
 	{
-    	NonCombatStrategy::InitTriggers(triggers);
-    		triggers.push_back(new TriggerNode(
-		"in battleground without flag",
-			NextAction::array(0, new NextAction("mount", 1.0f), NULL)));    
+	
 	}
+	
+};
+
+
+GenericRogueNonCombatStrategy::GenericRogueNonCombatStrategy(PlayerbotAI* ai) : NonCombatStrategy(ai)
+{
+	actionNodeFactories.Add(new GenericRogueNonCombatStrategyActionNodeFactory());
+}
+/*
+void GenericRogueNonCombatStrategy::InitTriggers(std::list<TriggerNode*> &triggers)
+{
+    NonCombatStrategy::InitTriggers(triggers);
+        
+}
+*/
