50c70bc23f2edbbd7a2508bbe463d57e1bccd29a
tkrokli <tkrokli@users.noreply.github.com>
2017-06-15 17:05:29 +0200

    Update SQL
    - remove redundant blank lines before and after comment
    - add required space between comment marker and text

--- sql/updates/world/master/RoG_2017_06_13_00_world.sql
+++ sql/updates/world/master/RoG_2017_06_13_00_world.sql
@@ -1,5 +1,3 @@
-
---Spell ID:81749    Spell Name:	Atonement
-
+-- spell Atonement (ID 81749) SPELLFAMILY_PRIEST
 DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_pri_atonement';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (81749, 'spell_pri_atonement');
