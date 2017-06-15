9375d02a42eb3909bd0a6e3338a702b273771e7c
Markus Theil <thevanoq@gmail.com>
2017-06-15 09:24:56 +0200

    Notepad++ being dumb and not adding newline?

--- sql/updates/world/3.3.5/2017_99_99_99_world.sql
+++ sql/updates/world/3.3.5/2017_99_99_99_world.sql
@@ -4,3 +4,4 @@ DELETE FROM conditions WHERE SourceEntry='43559';
 INSERT INTO conditions
 	(SourceTypeOrReferenceId, SourceGroup, SourceEntry, SourceId, ElseGroup, ConditionTypeOrReference, ConditionTarget, ConditionValue1, ConditionValue2, ConditionValue3, NegativeCondition, ErrorType, ErrorTextId, ScriptName, Comment)
 	VALUES (13, 1, 43559, 0, 0, 36, 0, 0, 0, 0, 1, 0, 0, '', '');
+
