4dce811a8a56984026ef72a999995d24052dc66a
tkrokli <tkrokli@users.noreply.github.com>
2017-06-15 12:24:14 +0200

    SQL code style
    - never apostrophes around integer number values
    - always back ticks around names of tables and columns
    - add comment line on top for TDB content separation

--- sql/updates/world/3.3.5/2017_99_99_99_world.sql
+++ sql/updates/world/3.3.5/2017_99_99_99_world.sql
@@ -1,7 +1,6 @@
-UPDATE creature_template SET AIName='', ScriptName='npc_necro_overlord_mezhen' WHERE entry='24018';
+-- Necro Lord Mezhen (24018)
+UPDATE `creature_template` SET `AIName` = '', `ScriptName` = 'npc_necro_overlord_mezhen' WHERE `entry` = 24018;
 
-DELETE FROM conditions WHERE SourceEntry='43559';
-INSERT INTO conditions
-	(SourceTypeOrReferenceId, SourceGroup, SourceEntry, SourceId, ElseGroup, ConditionTypeOrReference, ConditionTarget, ConditionValue1, ConditionValue2, ConditionValue3, NegativeCondition, ErrorType, ErrorTextId, ScriptName, Comment)
-	VALUES (13, 1, 43559, 0, 0, 36, 0, 0, 0, 0, 1, 0, 0, '', '');
-
+DELETE FROM `conditions` WHERE `SourceEntry` = 43559;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceGroup`,`SourceEntry`,`SourceId`,`ElseGroup`,`ConditionTypeOrReference`,`ConditionTarget`,`ConditionValue1`,`ConditionValue2`,`ConditionValue3`,`NegativeCondition`,`ErrorType`,`ErrorTextId`,`ScriptName`,`Comment`) VALUES
+(13, 1, 43559, 0, 0, 36, 0, 0, 0, 0, 1, 0, 0, '', '');
