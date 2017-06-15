410bc499768ce2b5d1e5247815efebd0ba9b65a3
Shauren <shauren.trinity@gmail.com>
2017-06-15 15:23:08 +0200

    Core: Updated to 7.2.5.24330

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -13851,7 +13851,7 @@ void Unit::SendThreatListUpdate()
         {
             WorldPackets::Combat::ThreatInfo info;
             info.UnitGUID = (*itr)->getUnitGuid();
-            info.Threat = int32((*itr)->getThreat() * 100);
+            info.Threat = int64((*itr)->getThreat() * 100);
             packet.ThreatList.push_back(info);
         }
         SendMessageToSet(packet.Write(), false);
@@ -13871,7 +13871,7 @@ void Unit::SendChangeCurrentVictimOpcode(HostileReference* pHostileReference)
         {
             WorldPackets::Combat::ThreatInfo info;
             info.UnitGUID = (*itr)->getUnitGuid();
-            info.Threat = int32((*itr)->getThreat() * 100);
+            info.Threat = int64((*itr)->getThreat() * 100);
             packet.ThreatList.push_back(info);
         }
         SendMessageToSet(packet.Write(), false);
