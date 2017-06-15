461ba1cb566ed0eeebf89c1e60ac4b23d1133d17
Aokromes <aokromes@gmail.com>
2017-06-15 08:46:45 +0200

    Fix (C) for multiple files

--- src/server/scripts/Maelstrom/kezan.cpp
+++ src/server/scripts/Maelstrom/kezan.cpp
@@ -1,7 +1,9 @@
 /*
+ * Copyright (C) 2008-2017 TrinityCore <http://www.trinitycore.org/>
+ *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 3 of the License, or (at your
+ * Free Software Foundation; either version 2 of the License, or (at your
  * option) any later version.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
