9327d2dc513fc89199a9b9fd1f50f5221e5364c6
HannibalRoG <edreisantafe@gmail.com>
2017-06-14 21:48:54 -0400

    Fix .sql with code standard, thanks tkrokli.

--- sql/updates/world/master/RoG_2017_06_13_00_world.sql
+++ sql/updates/world/master/RoG_2017_06_13_00_world.sql
@@ -1,8 +1,5 @@
-/* 
-	Spell ID:		81749
-	Spell Name:		Atonement
-	Spell Family: 	SPELLFAMILY_PRIEST
-*/
+
+--Spell ID:81749    Spell Name:	Atonement
 
 DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_pri_atonement';
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES ('81749', 'spell_pri_atonement');
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (81749, 'spell_pri_atonement');
