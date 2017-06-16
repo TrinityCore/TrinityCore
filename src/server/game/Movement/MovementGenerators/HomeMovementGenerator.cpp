ec9fbaceb057e5beb8dda066b2c4773abe3dba7e
treeston <treeston.mmoc@gmail.com>
2017-06-16 22:28:41 +0200

    Fix an issue where temp summons were ignoring m_regenHealth. (Hi Keader.)
    (cherry picked from commit 30ac4f537774c7d36b75fa260acea884f307b8f9)

--- src/server/game/Movement/MovementGenerators/HomeMovementGenerator.cpp
+++ src/server/game/Movement/MovementGenerators/HomeMovementGenerator.cpp
@@ -82,7 +82,11 @@ void HomeMovementGenerator<Creature>::DoFinalize(Creature* owner)
         owner->SetWalk(true);
         owner->LoadCreaturesAddon();
         owner->AI()->JustReachedHome();
-        owner->SetSpawnHealth();
+        if (owner->isRegeneratingHealth() && (owner->IsDungeonBoss() || owner->isWorldBoss()))
+        {
+            owner->SetFullHealth();
+            owner->SetPower(POWER_MANA, owner->GetMaxPower(POWER_MANA));
+        }
     }
 }
 
