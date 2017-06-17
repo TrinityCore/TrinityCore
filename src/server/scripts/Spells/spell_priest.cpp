0b2353ac70d0a211aba8534d5d91ffd374afa7ad
vincent-michael <vincent_michael@gmx.de>
2017-06-18 00:19:57 +0200

    Core: Fixed non pch build

--- src/server/scripts/Spells/spell_priest.cpp
+++ src/server/scripts/Spells/spell_priest.cpp
@@ -24,6 +24,7 @@
 #include "ScriptMgr.h"
 #include "AreaTriggerAI.h"
 #include "GridNotifiers.h"
+#include "ObjectAccessor.h"
 #include "Player.h"
 #include "SpellAuraEffects.h"
 #include "SpellMgr.h"
