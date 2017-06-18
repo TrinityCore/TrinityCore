aa1219335b145f05567b158df139c6e23716d302
Aokromes <aokromes@gmail.com>
2017-06-18 20:31:49 +0200

    TDB 4.3.4.20 2017/06/18

---
+++ sql/updates/characters/4.3.4/2017_06_18_00_characters.sql
@@ -0,0 +1,2 @@
+-- TDB 4.3.4-20 characters database
+UPDATE `updates` SET `state`='ARCHIVED';
---
+++ sql/updates/world/4.3.4/2017_06_18_05_world.sql
@@ -0,0 +1,3 @@
+UPDATE `version` SET `db_version`='TDB 434.20', `cache_id`=20 LIMIT 1;
+
+UPDATE `updates` SET `state`='ARCHIVED';
