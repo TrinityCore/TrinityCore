b301aabe509b0d417a7bbddc5cfeb14952c9e4b4
JayWolf95 <jaywolf95@gmail.com>
2017-06-24 05:05:10 -0500

    Update 9999_99_99_99_world.sql
    Fix Syntax

--- sql/updates/world/master/9999_99_99_99_world.sql
+++ sql/updates/world/master/9999_99_99_99_world.sql
@@ -1,5 +1,5 @@
-DELETE FROM 'spell_script_names' WHERE 'ScriptName' IN ('spell_dru_travel_form');
-INSERT INTO 'spell_script_names' ('spell_id', 'ScriptNames') VALUES
+DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_dru_travel_form');
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptNames`) VALUES
 (783, 'spell_dru_travel_form'),
 (1066, 'spell_dru_travel_form'),
 (783, 'spell_dru_travel_form'),
