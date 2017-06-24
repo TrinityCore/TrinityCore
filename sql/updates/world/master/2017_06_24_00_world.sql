dd104e91f0dbf6b65121d79aa613f2c2864a8c4b
JayWolf95 <jaywolf95@gmail.com>
2017-06-24 04:34:29 -0500

    Fix for Druid Travel Form

---
+++ sql/updates/world/master/2017_06_24_00_world.sql
@@ -0,0 +1,7 @@
+DELETE FROM 'spell_script_names' WHERE 'ScriptName' IN ('spell_dru_travel_form');
+INSERT INTO 'spell_script_names' ('spell_id', 'ScriptNames') VALUES
+(783, 'spell_dru_travel_form'),
+(1066, 'spell_dru_travel_form'),
+(783, 'spell_dru_travel_form'),
+(40120, 'spell_dru_travel_form'),
+(165961, 'spell_dru_travel_form');
