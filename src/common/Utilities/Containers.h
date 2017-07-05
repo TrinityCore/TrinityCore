c37940c33fc423ff8718c74b212ddf786bb35bf1
treeston <treeston.mmoc@gmail.com>
2017-07-06 01:13:52 +0200

    remove unneeded decltype

--- src/common/Utilities/Containers.h
+++ src/common/Utilities/Containers.h
@@ -59,7 +59,7 @@ namespace Trinity
         template<class C>
         void RandomResize(C& container, std::size_t requestedSize)
         {
-            static_assert(std::is_base_of<std::forward_iterator_tag, typename std::iterator_traits<decltype(std::begin(container))>::iterator_category>::value, "Invalid container passed to Trinity::Containers::RandomResize");
+            static_assert(std::is_base_of<std::forward_iterator_tag, typename std::iterator_traits<typename C::iterator>::iterator_category>::value, "Invalid container passed to Trinity::Containers::RandomResize");
             if (Size(container) <= requestedSize)
                 return;
             auto keepIt = std::begin(container), curIt = std::begin(container);
