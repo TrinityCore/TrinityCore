5efc110f397b3b5c0931475f2a24f26e28cbaf3f
Aokromes <aokromes@gmail.com>
2017-07-09 20:44:25 +0200

    Formation idle: Remove more shitty code. Fixes #19995 for real this time.
    cherry pick from 2a6fe60258546f033be4c425d0bf58995747453b

--- src/server/game/Entities/Creature/CreatureGroups.cpp
+++ src/server/game/Entities/Creature/CreatureGroups.cpp
@@ -237,8 +237,7 @@ void CreatureGroup::LeaderMoveTo(float x, float y, float z)
     for (CreatureGroupMemberType::iterator itr = m_members.begin(); itr != m_members.end(); ++itr)
     {
         Creature* member = itr->first;
-        uint8 groupAI = sFormationMgr->CreatureGroupMap[member->GetSpawnId()]->groupAI;
-        if (member == m_leader || !member->IsAlive() || member->GetVictim() || !(groupAI & FLAG_IDLE_IN_FORMATION))
+        if (member == m_leader || !member->IsAlive() || member->GetVictim() || !(itr->second->groupAI & FLAG_IDLE_IN_FORMATION))
             continue;
 
         if (itr->second->point_1)
