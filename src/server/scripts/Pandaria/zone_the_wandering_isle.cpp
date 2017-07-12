8c7e646b30c7f84d83cb062559384e73d4e7bfcf
Lopfest <lopfest@gmail.com>
2017-07-12 21:30:15 +0200

    hopefully fixed last merge errors

--- src/server/scripts/Pandaria/zone_the_wandering_isle.cpp
+++ src/server/scripts/Pandaria/zone_the_wandering_isle.cpp
@@ -15,6 +15,8 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "DB2Stores.h"
+#include "GameObject.h"
 #include "ScriptMgr.h"
 #include "ScriptedCreature.h"
 #include "SpellScript.h"
