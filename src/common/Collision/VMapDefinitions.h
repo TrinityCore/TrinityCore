5b38e9532e553471962468adcab23a34fe86e278
Golrag <golrag.jeremy@gmail.com>
2017-07-14 18:00:51 +0200

    Core/VMaps: Fix no collision triangles
    (cherry picked from commit 89d641f1e68d11359a2398ff51d3d6eee1470ae8)

--- src/common/Collision/VMapDefinitions.h
+++ src/common/Collision/VMapDefinitions.h
@@ -24,8 +24,8 @@
 
 namespace VMAP
 {
-    const char VMAP_MAGIC[] = "VMAP_4.8";
-    const char RAW_VMAP_MAGIC[] = "VMAP048";                // used in extracted vmap files with raw data
+    const char VMAP_MAGIC[] = "VMAP_4.9";
+    const char RAW_VMAP_MAGIC[] = "VMAP049";                // used in extracted vmap files with raw data
     const char GAMEOBJECT_MODELS[] = "GameObjectModels.dtree";
 
     // defined in VMapManager2.cpp currently...
