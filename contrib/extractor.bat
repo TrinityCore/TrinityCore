f144c718f83db9cd10321aace1f1cd63f5856e25
cribolouf <cribolouf@yahoo.com>
2017-07-14 02:11:23 -0400

    Update extractor.bat

--- contrib/extractor.bat
+++ contrib/extractor.bat
@@ -47,5 +47,6 @@ start /b /w mapextractor.exe
 start /b /w vmap4extractor.exe
 start /b /w vmap4assembler.exe
 rmdir Buildings /s /q
+ECHO The next step may take a few hours to complete. Please be patient.
 start /b /w mmaps_generator.exe
 GOTO MENU
