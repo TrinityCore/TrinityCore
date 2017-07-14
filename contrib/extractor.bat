e4f0722dd1fe05366680bbabc97ca3b982a1af89
cribolouf <cribolouf@yahoo.com>
2017-07-14 01:24:51 -0400

    Update extractor.bat

--- contrib/extractor.bat
+++ contrib/extractor.bat
@@ -1,5 +1,6 @@
 @ECHO OFF
 CLS
+
 :MENU
 ECHO.
 ECHO ...............................................
@@ -24,40 +25,25 @@ IF %M%==2 GOTO VMAPS
 IF %M%==3 GOTO MMAPS
 IF %M%==4 GOTO ALL
 IF %M%==5 GOTO EOF
+
 :MAPS
-start mapextractor.exe
-ECHO wait before mapextractor.exe closes before continue
-pause
+start /b /w mapextractor.exe
 GOTO MENU
+
 :VMAPS
-md vmaps
-start vmap4extractor.exe
-ECHO wait before vmap4extractor.exe closes before continue
-pause
-start vmap4assembler.exe Buildings vmaps
-ECHO wait before vmap4assembler.exe closes before continue
-pause
+start /b /w vmap4extractor.exe
+start /b /w vmap4assembler.exe Buildings vmaps
 rmdir Buildings /s /q
 GOTO MENU
+
 :MMAPS
-md mmaps
-start mmaps_generator.exe
-pause
+start /b /w mmaps_generator.exe
 GOTO MENU
+
 :ALL
-start mapextractor.exe
-ECHO wait before mapextractor.exe closes before continue
-pause
-md vmaps
-start vmap4extractor.exe
-ECHO wait before vmap4extractor.exe closes before continue
-pause
-start vmap4assembler.exe Buildings vmaps
-ECHO wait before vmap4assembler.exe closes before continue
-pause
+start /b /w mapextractor.exe
+start /b /w vmap4extractor.exe
+start /b /w vmap4assembler.exe
 rmdir Buildings /s /q
-md mmaps
-start mmaps_generator.exe --threads %NUMBER_OF_PROCESSORS%
-ECHO wait before mmaps_generator.exe closes before continue (may take hours)
-pause
+start /b /w mmaps_generator.exe
 GOTO MENU
