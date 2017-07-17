e0194e4a2c659597da18e220000055484ff504ad
treeston <treeston.mmoc@gmail.com>
2017-07-17 23:23:54 +0200

    Revert "Scripts/Commands: Fix a potential crash when summoning players that are just logging on." - the check was unnecessary (already done in extractPlayerTarget).
    This reverts commit 05cdcdb230a9d69f8ad8f2af696f452151a1c364.

--- src/server/scripts/Commands/cs_group.cpp
+++ src/server/scripts/Commands/cs_group.cpp
@@ -115,7 +115,7 @@ public:
 
             std::string plNameLink = handler->GetNameLink(player);
 
-            if (!player->IsInWorld() || player->IsBeingTeleported())
+            if (player->IsBeingTeleported())
             {
                 handler->PSendSysMessage(LANG_IS_TELEPORTED, plNameLink.c_str());
                 continue;
