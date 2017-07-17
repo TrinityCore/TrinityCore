c6a1ea7c324624f1406824c6e4dd859ea43db35c
Aokromes <aokromes@gmail.com>
2017-07-17 10:26:41 +0200

    Tools: Automatically create directories for tools

--- src/tools/vmap4_assembler/VMapAssembler.cpp
+++ src/tools/vmap4_assembler/VMapAssembler.cpp
@@ -23,14 +23,21 @@
 
 int main(int argc, char* argv[])
 {
-    if (argc != 3)
+    std::string src = "Buildings";
+    std::string dest = "vmaps";       
+
+    if (argc > 3)
     {
         std::cout << "usage: " << argv[0] << " <raw data dir> <vmap dest dir>" << std::endl;
         return 1;
     }
-
-    std::string src = argv[1];
-    std::string dest = argv[2];
+    else
+    {
+        if (argc > 1)
+            src = argv[1];
+        if (argc > 2)
+            dest = argv[2];
+    }
 
     std::cout << "using " << src << " as source directory and writing output to " << dest << std::endl;
 
