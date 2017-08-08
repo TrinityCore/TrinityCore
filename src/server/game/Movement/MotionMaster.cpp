ee2d7c1d53986d32779379caf0591d6cde530ead
ccrs <ccrs@users.noreply.github.com>
2017-08-08 19:35:13 +0200

    Core/Movement: add new helper that cleans the referenced MovementSlot

--- src/server/game/Movement/MotionMaster.cpp
+++ src/server/game/Movement/MotionMaster.cpp
@@ -119,23 +119,6 @@ void MotionMaster::Clear(bool reset /*= true*/)
         DirectClean(reset);
 }
 
-void MotionMaster::ClearExpireList()
-{
-    for (auto itr : _expireList)
-        DirectDelete(itr);
-
-    _expireList.clear();
-
-    if (empty())
-        Initialize();
-    else if (NeedInitTop())
-        InitTop();
-    else if (_cleanFlag & MMCF_RESET)
-        top()->Reset(_owner);
-
-    _cleanFlag &= ~MMCF_RESET;
-}
-
 void MotionMaster::MovementExpired(bool reset /*= true*/)
 {
     if (_cleanFlag & MMCF_UPDATE)
@@ -148,6 +131,29 @@ void MotionMaster::MovementExpired(bool reset /*= true*/)
     }
     else
         DirectExpire(reset);
+}
+
+void MotionMaster::ExpireMovement(MovementSlot slot)
+{
+    if (empty() || slot >= MAX_MOTION_SLOT)
+        return;
+
+    if (MovementGenerator* motion = GetMotionSlot(slot))
+    {
+        DirectDelete(motion);
+        _slot[slot] = nullptr;
+    }
+
+    if (!top())
+    {
+        while (!empty() && !top())
+            --_top;
+
+        if (empty())
+            Initialize();
+        else if (NeedInitTop())
+            InitTop();
+    }
 }
 
 MovementGeneratorType MotionMaster::GetCurrentMovementGeneratorType() const
@@ -831,4 +837,21 @@ void MotionMaster::DelayedDelete(MovementGenerator* curr)
         return;
 
     _expireList.push_back(curr);
+}
+
+void MotionMaster::ClearExpireList()
+{
+    for (auto itr : _expireList)
+        DirectDelete(itr);
+
+    _expireList.clear();
+
+    if (empty())
+        Initialize();
+    else if (NeedInitTop())
+        InitTop();
+    else if (_cleanFlag & MMCF_RESET)
+        top()->Reset(_owner);
+
+    _cleanFlag &= ~MMCF_RESET;
 }
