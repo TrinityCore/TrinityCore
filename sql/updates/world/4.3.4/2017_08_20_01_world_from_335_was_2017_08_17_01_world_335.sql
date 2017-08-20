333078ae38c3345c5abcfdf50af7dca80447b74a
Aokromes <aokromes@gmail.com>
2017-08-20 07:12:03 +0200

    rename files

--- sql/updates/world/4.3.4/2017_08_20_01_world_from_335_was_2017_08_17_01_world_335.sql
+++ sql/updates/world/4.3.4/2017_08_20_01_world_from_335_was_2017_08_17_01_world_335.sql
@@ -1,3 +1,4 @@
+/*
 -- 
 UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=233;
 DELETE FROM `creature_text` WHERE `CreatureID`=233;
@@ -9,3 +10,4 @@ INSERT INTO `creature_text` (`CreatureID`,`GroupID`,`ID`,`Text`,`Type`,`Language
 DELETE FROM `smart_scripts` WHERE `entryorguid`=233;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (233,0,0,0,1,0,100,0,1000,15000,150000,180000,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,'Farmer Saldean - Out of Combat - Say Line 0');
+*/
