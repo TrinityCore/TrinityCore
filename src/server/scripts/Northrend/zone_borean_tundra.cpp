148605948b5602f2c3dcc3457c5fd8508daacd30
hiMalleus <so.solid.snake@hotmail.com>
2017-09-07 00:28:31 +0100

    Winterfin quest and wrong space
    changed his state from Aggressive to Defensive as shown on video https://www.youtube.com/watch?v=apkZvnlkLX8 on minute 1:57, His cage door now opens for 20 seconds calculated from video and fixed a misstyped space on Broggok script

--- src/server/scripts/Northrend/zone_borean_tundra.cpp
+++ src/server/scripts/Northrend/zone_borean_tundra.cpp
@@ -628,7 +628,7 @@ public:
                             IntroTimer = 7500;
                             break;
                         case 3:
-                            me->SetReactState(REACT_AGGRESSIVE);
+                            me->SetReactState(REACT_DEFENSIVE);
                             IntroPhase = 0;
                             IntroTimer = 0;
                             break;
@@ -672,8 +672,7 @@ public:
                 if (GameObject* go = me->FindNearestGameObject(GO_CAGE, 5.0f))
                 {
                     go->SetRespawnTime(0);
-                    go->SetGoType(GAMEOBJECT_TYPE_BUTTON);
-                    go->UseDoorOrButton(20);
+                    go->UseDoorOrButton(20000);
                 }
 
                 Start(true, false, player->GetGUID());
