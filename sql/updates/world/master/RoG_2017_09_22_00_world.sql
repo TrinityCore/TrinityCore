4188f44389134167853c69b1b74080cd870999d2
HannibalRoG <edreisantafe@gmail.com>
2017-09-22 20:06:28 -0400

    Fix Warrior Revenge proc

---
+++ sql/updates/world/master/RoG_2017_09_22_00_world.sql
@@ -0,0 +1,5 @@
+-- Fix for: 	Revenge - (6572, 5302, 5301)
+-- Class:		Warrior
+-- Spec:		Protection
+DELETE FROM `spell_proc` WHERE `SpellId` = 5301;
+INSERT INTO `spell_proc` (`SpellId`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `SpellFamilyMask3`, `ProcFlags`, `SpellTypeMask`, `SpellPhaseMask`, `HitMask`, `AttributesMask`, `ProcsPerMinute`, `Chance`, `Cooldown`, `Charges`) VALUES ('5301', '0', '0', '0', '0', '0', '0', '0', '0', '0', '48', '0', '0', '0', '0', '0');
--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -2917,6 +2917,13 @@ void SpellMgr::LoadSpellInfoCorrections()
         spellInfo->AttributesEx3 |= SPELL_ATTR3_CANT_TRIGGER_PROC;
     });
 
+    // Revenge (Warrior) - DBC have SpellLevel & BaseLevel = 0 
+    ApplySpellFix({ 5301, 5302 }, [](SpellInfo* spellInfo)
+    {
+        spellInfo->SpellLevel = 22;
+        spellInfo->BaseLevel = 22;
+    });
+
     ApplySpellFix({
         31347, // Doom
         36327, // Shoot Arcane Explosion Arrow
