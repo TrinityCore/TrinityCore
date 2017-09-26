2e40b25d9d6a665b308b4c614adc686549ce8dfc
ZenoX92 <ZenoX92@users.noreply.github.com>
2017-09-26 11:57:58 +0200

    DB/SAI: Eridan Bluewind event related to the quest "Purified!"
    Closes #20425

---
+++ sql/updates/world/3.3.5/2017_09_26_05_world_335.sql
@@ -0,0 +1,2 @@
+-- 
+UPDATE `smart_scripts` SET `event_type`=19, `event_param1`=4442, `comment`="Eridan Bluewind - On Quest \'Purified!'\ Taken - Run Script" WHERE `entryorguid`=9116 AND `source_type`=0;
