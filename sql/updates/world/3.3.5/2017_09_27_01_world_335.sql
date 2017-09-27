617881c9cb55d04a1c57af084eeb3ecdedc75a49
Wyreth <Wyreth@users.noreply.github.com>
2017-09-27 17:42:23 +0200

    DB/Quest: Fix chaining for quests Martek the Exiled and Encrusted Tail Fin
    Closes #20453

---
+++ sql/updates/world/3.3.5/2017_09_27_01_world_335.sql
@@ -0,0 +1,4 @@
+-- 
+UPDATE `quest_template_addon` SET `NextQuestID`=0, `PrevQuestID`=0 WHERE `id` IN (1106, 1107);
+UPDATE `quest_template_addon` SET `NextQuestID`=1106 WHERE `id` IN (1104);
+UPDATE `quest_template_addon` SET `NextQuestID`=1107 WHERE `id` IN (1105);
