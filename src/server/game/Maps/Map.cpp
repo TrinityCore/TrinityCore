07c7940f88cd52fefa29aa606017394ac51267b0
r00ty-tc <r00ty-tc@users.noreply.github.com>
2017-10-05 00:39:43 +0100

    Core/Spawns: Resolve crash in BattlegroundAV  - Correctly set spawngroup on spawned creatures in Alterac Valley BG  - Sanity check on application of dynamic spawn rates (not for BG/Arena    maps)  - Check that spawndata exists before using it.
    Closes #20535
    
    (cherry picked from commit 55b9a3f65faeefb1c361b74b7a424d53e2d4c72e)

--- src/server/game/Maps/Map.cpp
+++ src/server/game/Maps/Map.cpp
@@ -3574,6 +3574,10 @@ void Map::ApplyDynamicModeRespawnScaling(WorldObject const* obj, ObjectGuid::Low
 {
     ASSERT(mode == 1);
     ASSERT(obj->GetMap() == this);
+
+    if (IsBattlegroundOrArena())
+        return;
+
     SpawnObjectType type;
     switch (obj->GetTypeId())
     {
@@ -3588,7 +3592,7 @@ void Map::ApplyDynamicModeRespawnScaling(WorldObject const* obj, ObjectGuid::Low
     }
 
     SpawnData const* data = sObjectMgr->GetSpawnData(type, spawnId);
-    if (!data || !(data->spawnGroupData->flags & SPAWNGROUP_FLAG_DYNAMIC_SPAWN_RATE))
+    if (!data || !data->spawnGroupData || !(data->spawnGroupData->flags & SPAWNGROUP_FLAG_DYNAMIC_SPAWN_RATE))
         return;
 
     auto it = _zonePlayerCountMap.find(obj->GetZoneId());
