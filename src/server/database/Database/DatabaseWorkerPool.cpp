da2506cfde55e04a1ef39f180f345908b7ad08d4
freekode <level.is03@gmail.com>
2017-10-10 20:03:03 +0200

    fix version message

--- src/server/database/Database/DatabaseWorkerPool.cpp
+++ src/server/database/Database/DatabaseWorkerPool.cpp
@@ -61,8 +61,8 @@ DatabaseWorkerPool<T>::DatabaseWorkerPool()
     WPFatal(mysql_thread_safe(), "Used MySQL library isn't thread-safe.");
     WPFatal(mysql_get_client_version() >= MIN_MYSQL_CLIENT_VERSION, "TrinityCore does not support MySQL versions below 5.1");
     WPFatal(usedMysqlMajorMinorVersion == compileMysqlMajorMinorVersion,
-        "Used MySQL library version (%s) does not match the version used to compile TrinityCore (%s). Your version should be >= %s. Search on forum for TCE00011.",
-        mysql_get_client_info(), MYSQL_SERVER_VERSION, compileMysqlMajorMinorVersion * 100);
+        "Used MySQL library version (%s) does not match the version used to compile TrinityCore (%s). Your version should match minor and major compiled version. Search on forum for TCE00011.",
+        mysql_get_client_info(), MYSQL_SERVER_VERSION);
 }
 
 template <class T>
