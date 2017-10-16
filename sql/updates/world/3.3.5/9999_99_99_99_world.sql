8d32f9f4fddf47b917927a65b23b9aff772d8984
velinath <velinath@cloudyday.net>
2017-10-16 15:26:24 -0400

    Migrate Rivern Frostwind to conditions only

---
+++ sql/updates/world/3.3.5/9999_99_99_99_world.sql
@@ -0,0 +1,9 @@
+--
+DELETE FROM `gossip_menu_option` WHERE `MenuID` = 3130;
+-- TODO check against live for what the actual gossip menu is. Didn't find anything helpful searching the broadcast_text table.
+INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionText`, `OptionBroadcastTextID`, `OptionType`, `OptionNpcFlag`, `ActionMenuID`, `ActionPoiID`, `BoxCoded`, `BoxMoney`, `BoxText`, `BoxBroadcastTextID`, `VerifiedBuild`) VALUES
+(3130, 0, 1, 'I want to browse your goods.', 3370, 3, 128, 0, 0, 0, 0, '', 0, 0),
+
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=15 AND `SourceGroup`=3130 AND `SourceEntry`=0 AND `SourceId`=0;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
+(15, 3130, 0, 0, 0, 5, 0, 589, 128, 0, 0, 0, 0, '', 'Show gossip menu 3130 option id 0 if player is Exalted with faction Wintersaber Trainers.');
