e0085ec484e540cb10b47c7f5e143cdb742fac0a
PolarCookie <squigig@gmail.com>
2017-10-17 19:28:42 +0200

    sql update

--- sql/updates/world/3.3.5/9999_99_99_99_world_2_335.sql
+++ sql/updates/world/3.3.5/9999_99_99_99_world_2_335.sql
@@ -1,13 +1,8 @@
--- Infernal SAI
-SET @ENTRY := 89;
-UPDATE `creature_template` SET `AIName`="SmartAI" WHERE `entry`=@ENTRY;
-DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=0;
-INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
-(@ENTRY,0,0,0,54,0,100,0,0,0,0,0,11,19483,0,0,0,0,0,1,0,0,0,0,0,0,0,"Infernal - On Just Summon - Cast 'Immolation'"),
-(@ENTRY,0,1,0,54,0,100,0,0,0,0,0,11,32233,0,0,0,0,0,1,0,0,0,0,0,0,0,"Infernal - On Just Summon - Cast 'Avoidance'");
-
 -- This change is needed otherwise Infernal gains hp, armor and damage from creature_classlevelstats where class = 1 and level = 68, as he is a guardian and not pet proper.
-UPDATE `creature_template` SET `HealthModifier`='0', `ManaModifier`='0', `ArmorModifier`='0', `DamageModifier`='0', `ExperienceModifier`='0' WHERE `entry`='89';
+UPDATE `creature_template` SET `speed_walk`='1.0', `speed_run`='1.14286', `HealthModifier`='0', `ManaModifier`='0', `ArmorModifier`='0', `DamageModifier`='0', `ExperienceModifier`='0' WHERE `entry`='89';
+
+-- Immolation and Avoidance
+UPDATE `creature_template_addon` SET `auras`='19483 32233' WHERE `entry`='89';
 
 -- Infernal stats
 DELETE FROM `pet_levelstats` WHERE `creature_entry`=89;
