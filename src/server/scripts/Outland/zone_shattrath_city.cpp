6d4fe1e5c59ca81ac7fbbfb277b57c79e10a7c39
velinath <velinath@cloudyday.net>
2017-10-16 22:29:10 -0400

    remove ScriptedGossip include

--- src/server/scripts/Outland/zone_shattrath_city.cpp
+++ src/server/scripts/Outland/zone_shattrath_city.cpp
@@ -29,7 +29,6 @@ EndContentData */
 
 #include "ScriptMgr.h"
 #include "ScriptedCreature.h"
-#include "ScriptedGossip.h"
 #include "ScriptedEscortAI.h"
 #include "Player.h"
 #include "WorldSession.h"
