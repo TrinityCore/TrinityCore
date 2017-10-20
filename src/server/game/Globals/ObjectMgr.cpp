b8dc4df22098e62395528ae0860df8ed94d3a20b
Shauren <shauren.trinity@gmail.com>
2017-10-21 00:28:57 +0200

    Really fix b5b03a7be03e85722f5738842e6f7fef246253d7

--- src/server/game/Globals/ObjectMgr.cpp
+++ src/server/game/Globals/ObjectMgr.cpp
@@ -871,7 +871,7 @@ void ObjectMgr::CheckCreatureTemplate(CreatureTemplate const* cInfo)
     if (!factionTemplate)
     {
         TC_LOG_ERROR("sql.sql", "Creature (Entry: %u) has non-existing faction template (%u). This can lead to crashes, set to faction 35.", cInfo->Entry, cInfo->faction);
-        factionTemplate = sFactionTemplateStore.AssertEntry(35);
+        const_cast<CreatureTemplate*>(cInfo)->faction = sFactionTemplateStore.AssertEntry(35)->ID; // this might seem stupid but all shit will would break if faction 35 did not exist
     }
 
     // used later for scale
