e50ed4054065f00bf1f1f55eef4f9de3becb2b59
Andrew Letson <velinath@users.noreply.github.com>
2017-10-21 06:03:02 -0400

    DB/Scripts: Yenniku (move npc_yenniku to SAI)
    (cherry picked from commit a7f3d5e59fd05d1f015ea0ad7426292d36b3dee8)

--- src/server/scripts/EasternKingdoms/eastern_kingdoms_script_loader.cpp
+++ src/server/scripts/EasternKingdoms/eastern_kingdoms_script_loader.cpp
@@ -196,7 +196,6 @@ void AddSC_isle_of_queldanas();
 void AddSC_redridge_mountains();
 void AddSC_silverpine_forest();
 void AddSC_stormwind_city();
-void AddSC_stranglethorn_vale();
 void AddSC_swamp_of_sorrows();
 void AddSC_tirisfal_glades();
 void AddSC_tol_barad();
@@ -389,7 +388,6 @@ void AddEasternKingdomsScripts()
     AddSC_redridge_mountains();
     AddSC_silverpine_forest();
     AddSC_stormwind_city();
-    AddSC_stranglethorn_vale();
     AddSC_swamp_of_sorrows();
     AddSC_tirisfal_glades();
     AddSC_tol_barad();
