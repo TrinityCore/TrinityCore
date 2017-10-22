56dca10f1b7aa02e2869176ae93b5483406dea09
Wyreth <32145860+Wyreth@users.noreply.github.com>
2017-10-22 21:37:38 +0200

    Core/SAI: allow to pause/resume waypoint for non-escorting NPCs (#20684)
    (cherry picked from commit 21bd52cb995bb7ba6e0bee33317857a285ffd14c)

--- src/server/game/AI/SmartScripts/SmartAI.cpp
+++ src/server/game/AI/SmartScripts/SmartAI.cpp
@@ -128,9 +128,6 @@ bool SmartAI::LoadPath(uint32 entry)
 
 void SmartAI::PausePath(uint32 delay, bool forced)
 {
-    if (!HasEscortState(SMART_ESCORT_ESCORTING))
-        return;
-
     if (HasEscortState(SMART_ESCORT_PAUSED))
     {
         TC_LOG_ERROR("misc", "SmartAI::PausePath: Creature entry %u wanted to pause waypoint (current waypoint: %u) movement while already paused, ignoring.", me->GetEntry(), _currentWaypointNode);
@@ -155,9 +152,6 @@ void SmartAI::PausePath(uint32 delay, bool forced)
 
 void SmartAI::StopPath(uint32 DespawnTime, uint32 quest, bool fail)
 {
-    if (!HasEscortState(SMART_ESCORT_ESCORTING))
-        return;
-
     if (quest)
         mEscortQuestID = quest;
 
@@ -269,9 +263,6 @@ void SmartAI::ReturnToLastOOCPos()
 
 void SmartAI::UpdatePath(const uint32 diff)
 {
-    if (!HasEscortState(SMART_ESCORT_ESCORTING))
-        return;
-
     if (_escortInvokerCheckTimer < diff)
     {
         if (!IsEscortInvokerInRange())
