22cacced2d94e39f3a3a16fadc458722c50df92c
Aokromes <aokromes@gmail.com>
2017-10-24 19:30:50 +0200

    DB/Misc: Fix some startup errors

---
+++ sql/updates/world/4.3.4/2017_10_24_12_world.sql
@@ -0,0 +1,3 @@
+--
+UPDATE `smart_scripts` SET `action_param1` = 371 WHERE `entryorguid` = 23671 AND `source_type` = 0 AND `id` = 1 AND `link` = 2;
+UPDATE `smart_scripts` SET `action_param1` = 169 WHERE `entryorguid` = 23671 AND `source_type` = 0 AND `id` = 3 AND `link` = 4;
