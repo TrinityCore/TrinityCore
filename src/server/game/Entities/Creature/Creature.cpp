5b05043d90da4fae65d5e8e92f4768ff294fd53d
Teppic1 <Teppic1@users.noreply.github.com>
2017-10-26 04:25:49 +0100

    Core/SAI: Remove duplicate check for evade

--- src/server/game/Entities/Creature/Creature.cpp
+++ src/server/game/Entities/Creature/Creature.cpp
@@ -2276,9 +2276,6 @@ void Creature::CallForHelp(float radius)
 
 bool Creature::CanAssistTo(Unit const* u, Unit const* enemy, bool checkfaction /*= true*/) const
 {
-    if (IsInEvadeMode())
-        return false;
-
     // is it true?
     if (!HasReactState(REACT_AGGRESSIVE))
         return false;
