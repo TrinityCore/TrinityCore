7d89339cea08e80146823d4e8606c42060c43c4d
Killyana <morphone1@gmail.com>
2017-10-28 21:05:13 +0200

    Creature/Scripts: Add Aggro sound to Cairne Bloodhoo, Sylvanas Windrunner and Thrall
    (cherry picked from commit 48ecd81d614559c6f023aa8910a99901d9d794b5)

--- src/server/scripts/EasternKingdoms/zone_undercity.cpp
+++ src/server/scripts/EasternKingdoms/zone_undercity.cpp
@@ -80,6 +80,11 @@ enum Sylvanas
     GUID_EVENT_INVOKER              = 1,
 };
 
+enum Sounds
+{
+    SOUND_AGGRO                     = 5886
+};
+
 float HighborneLoc[4][3]=
 {
     {1285.41f, 312.47f, 0.51f},
@@ -118,6 +123,7 @@ public:
 
         void EnterCombat(Unit* /*who*/) override
         {
+            DoPlaySoundToSet(me, SOUND_AGGRO);
             _events.ScheduleEvent(EVENT_FADE, 30000);
             _events.ScheduleEvent(EVENT_SUMMON_SKELETON, 20000);
             _events.ScheduleEvent(EVENT_BLACK_ARROW, 15000);
