46da5fdb19d883b9673cefbd61b97911c1c67b8a
Wyreth <32145860+Wyreth@users.noreply.github.com>
2017-10-30 22:14:55 +0100

    Update zone_borean_tundra.cpp

--- src/server/scripts/Northrend/zone_borean_tundra.cpp
+++ src/server/scripts/Northrend/zone_borean_tundra.cpp
@@ -1764,7 +1764,7 @@ public:
                     Talk(SAY_BONKER_5);
         }
 
-        void UpdateAI(uint32 diff) override
+        void UpdateEscortAI(uint32 diff) override
         {
             _events.Update(diff);
 
@@ -1789,8 +1789,6 @@ public:
                         break;
                 }
             }
-
-            EscortAI::UpdateAI(diff);
 
             if (!UpdateVictim())
                 return;
