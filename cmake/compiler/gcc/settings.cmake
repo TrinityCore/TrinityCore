af7352118b6e7306a8d8a28934ee62f27886808e
Naios <naios-dev@live.de>
2017-10-31 01:27:13 +0100

    Build: Set the minimum required compiler version to GCC 6.3 * This is the current GCC version of debian 9
    (cherry picked from commit 5a9ccae3e1115d6fb1dcc287b1cb2d45779fd39b)

--- cmake/compiler/gcc/settings.cmake
+++ cmake/compiler/gcc/settings.cmake
@@ -1,7 +1,7 @@
 # Set build-directive (used in core to tell which buildtype we used)
 add_definitions(-D_BUILD_DIRECTIVE='"${CMAKE_BUILD_TYPE}"')
 
-set(GCC_EXPECTED_VERSION 4.9.0)
+set(GCC_EXPECTED_VERSION 6.3.0)
 
 if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS GCC_EXPECTED_VERSION)
   message(FATAL_ERROR "GCC: TrinityCore requires version ${GCC_EXPECTED_VERSION} to build but found ${CMAKE_CXX_COMPILER_VERSION}")
