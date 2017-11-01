e8daee3b28c0d6f2962cf6de1235a202cbbde66b
Riztazz <felianther15@gmail.com>
2017-11-01 20:44:28 +0100

    add temp guids to sql so travis can build properly

--- sql/updates/world/3.3.5/9999_99_99_99_world.sql
+++ sql/updates/world/3.3.5/9999_99_99_99_world.sql
@@ -1,5 +1,5 @@
-SET @CGUID:= XXX; -- 1 guid
-SET @GGUID:= XXX; -- 2 guids
+SET @CGUID:= 999999; -- 1 guid
+SET @GGUID:= 999999; -- 2 guids
 
 DELETE FROM `creature` WHERE `guid` = @CGUID;
 INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES 
