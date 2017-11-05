7fb021ef5b069bad8ade971ed778da2f2533eb9f
Aokromes <aokromes@gmail.com>
2017-11-05 15:11:20 +0100

    DB/Spells: Fix attack power bonus for hunter's arcane shot
    By Ovahlord

---
+++ sql/updates/world/4.3.4/2017_11_05_01_world.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_bonus_data` WHERE `entry`= 3044;
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES
+(3044, 0, 0, 0.0483, 0, 'Hunter - Arcane Shot');
+
