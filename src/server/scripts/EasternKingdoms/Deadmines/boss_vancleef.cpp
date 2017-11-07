6d7ab3a15f6a7da3a35dce2b8aa6b93c1245c084
Teppic1 <32468801+Teppic1@users.noreply.github.com>
2017-11-07 07:35:00 +0000

    Replace TDB guard positions with sniffed data

--- src/server/scripts/EasternKingdoms/Deadmines/boss_vancleef.cpp
+++ src/server/scripts/EasternKingdoms/Deadmines/boss_vancleef.cpp
@@ -42,11 +42,11 @@ enum Speech
     SAY_THREE  = 5     // The Brotherhood shall prevail!
 };
 
-// From TDB 335.63
+// Sniffed - 3.3.0 11519
 Position const BlackguardPositions[] =
 {
-        { -78.2791f, -824.784f, 40.0007f, 2.93215f },
-        { -77.8071f, -815.097f, 40.0188f, 3.26377f }
+        { -69.30876f, -819.8949f, 40.67694f, 6.055284f },
+        { -68.65159f, -822.0574f, 40.76994f, 6.265795f }
 };
 
 struct boss_vancleef : public BossAI
