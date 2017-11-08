0257681324f3d4a39549a812afc31dd1816bfe01
Aokromes <aokromes@gmail.com>
2017-11-08 21:15:35 +0100

    DB/Archaeology: Add default loot

---
+++ sql/updates/world/4.3.4/2017_11_08_08_world.sql
@@ -0,0 +1,11 @@
+DELETE FROM `gameobject_loot_template` WHERE `Entry` IN (36037, 28434, 36055, 35827, 36019, 36056, 35546, 36054);
+INSERT INTO `gameobject_loot_template` (`Entry`, `Item`, `Chance`, `MinCount`, `MaxCount`) VALUES
+(36037, 64394, 4, 1, 3), -- Draenei Tomes
+(28434, 52843, 4, 1, 3), -- Dwarf Rune Stone
+(36055, 64396, 4, 1, 3), -- Nerubian Obelisk
+(35827, 63127, 4, 1, 3), -- Highborne Scroll
+(36019, 64392, 4, 1, 3), -- Orc Blood Text
+(36056, 64397, 4, 1, 3), -- Tolvir Hieroglyphic
+(35546, 63128, 4, 1, 3), -- Troll Tablet
+(36054, 36054, 4, 1, 3); -- Vrykul Rune Stick
+
