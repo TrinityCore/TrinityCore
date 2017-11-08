2931d5c1056091865e55fca5269642a741eb5967
Andrew Letson <velinath@users.noreply.github.com>
2017-11-08 07:40:04 -0500

    Fixes wrong smart script id
    The cast still isn't going off, maybe it needs castflags? Further testing required zzz

--- sql/updates/world/3.3.5/9999_99_99_99_world.sql
+++ sql/updates/world/3.3.5/9999_99_99_99_world.sql
@@ -16,7 +16,7 @@ DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryorguid` = 9563;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (9563, 0, 0, 1, 62, 0, 100, 0, 2062, 0, 0, 0, 72, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Ragged John - On Gossip Option 0 Selected - Close Gossip'),
 (9563, 0, 1, 2, 61, 0, 100, 0, 0, 0, 0, 0, 11, 16472, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Ragged John - On Gossip Option 0 Selected - Cast \'Wicked Milking\''),
-(9563, 0, 1, 0, 61, 0, 100, 0, 0, 0, 0, 0, 15, 4866, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Ragged John - On Gossip Option 0 Selected - Quest Credit \'Mother\'s Milk\'');
+(9563, 0, 2, 0, 61, 0, 100, 0, 0, 0, 0, 0, 15, 4866, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'Ragged John - On Gossip Option 0 Selected - Quest Credit \'Mother\'s Milk\'');
 
 -- Conditions
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 15 AND `SourceGroup` = 2061 AND `SourceEntry` = 0 AND `SourceId` = 0;
