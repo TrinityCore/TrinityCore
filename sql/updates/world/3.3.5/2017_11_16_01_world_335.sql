682f1fe25d1deed32bea47eac975c85b4179a072
Killyana <morphone1@gmail.com>
2017-11-16 11:37:01 +0100

    DB/GameObject: Fix some Nodes positions
    Closes #15607

---
+++ sql/updates/world/3.3.5/2017_11_16_01_world_335.sql
@@ -0,0 +1,5 @@
+-- 
+UPDATE `gameobject` SET `position_x`=-9202.341, `position_y`=-2442.0495, `position_z`=58.6061, `orientation`=0.431121 WHERE `guid`=5236;
+UPDATE `gameobject` SET `position_x`=-9143.816, `position_y`=-2506.8930, `position_z`=118.587, `orientation`=4.210773 WHERE `guid`=1537;
+UPDATE `gameobject` SET `position_x`=-9603.038, `position_y`=-1909.8031, `position_z`=61.5558, `orientation`=4.150383 WHERE `guid`=1740;
+UPDATE `gameobject` SET `position_x`=-9752.696, `position_y`=-1765.9846, `position_z`=52.6211, `orientation`=0.900624 WHERE `guid`=1374;
