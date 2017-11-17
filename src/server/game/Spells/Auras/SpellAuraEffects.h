332a8b30bffbcaea2b5bb124aab4ad50cd0b7b05
Hadifaren <uther110@gmail.com>
2017-11-17 17:05:00 +0100

    Core/Spells: Implemented SPELL_AURA_MOD_MAX_POWER

--- src/server/game/Spells/Auras/SpellAuraEffects.h
+++ src/server/game/Spells/Auras/SpellAuraEffects.h
@@ -236,6 +236,7 @@ class TC_GAME_API AuraEffect
         void HandleModStatBonusPercent(AuraApplication const* aurApp, uint8 mode, bool apply) const;
         void HandleOverrideSpellPowerByAttackPower(AuraApplication const* aurApp, uint8 mode, bool apply) const;
         void HandleOverrideAttackPowerBySpellPower(AuraApplication const* aurApp, uint8 mode, bool apply) const;
+        void HandleAuraModMaxPower(AuraApplication const* aurApp, uint8 mode, bool apply) const;
         //   heal and energize
         void HandleModPowerRegen(AuraApplication const* aurApp, uint8 mode, bool apply) const;
         void HandleModPowerRegenPCT(AuraApplication const* aurApp, uint8 mode, bool apply) const;
