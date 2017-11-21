02bfe0eb8f3d18b56694ad4879113030018e119c
Aokromes <aokromes@gmail.com>
2017-11-21 05:02:20 +0100

    DB/Quest: Add The Arts of a 'Class' quest_offer_reward
    By Malcrom

---
+++ sql/updates/world/4.3.4/2017_11_21_00_world.sql
@@ -0,0 +1,11 @@
+-- The Arts of a 'Class' Quest
+DELETE FROM `quest_offer_reward` WHERE `ID` IN (26204,24760,24772,24778,26274,24752,24766,24784);
+INSERT INTO `quest_offer_reward` (`ID`,`RewardText`,`VerifiedBuild`) VALUES
+(26204, 'Good work, mon! Dis stuff come easy to ya.',14545),
+(24760, 'Good work, mon! Dis stuff come easy to ya.',14545),
+(24772, 'Good work, mon! Dis stuff come easy to ya.',14545),
+(24778, 'Good work, mon! Dis stuff come easy to ya.',14545),
+(26274, 'Good work, mon! Dis stuff come easy to ya.',14545),
+(24752, 'Good work, mon! Dis stuff come easy to ya.',14545),
+(24766, 'Good work, mon! Dis stuff come easy to ya.',14545),
+(24784, 'Good work, mon! Dis stuff come easy to ya.',14545);
