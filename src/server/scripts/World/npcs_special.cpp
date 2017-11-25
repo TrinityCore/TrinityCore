95ea4b50904a26710eead36e48a6ab29780d464b
Shauren <shauren.trinity@gmail.com>
2017-11-25 22:33:15 +0100

    Scripts/Events: Fixed memory leak in npc_bountiful_table added in 75df296bb6f8d30a65f669dab6af3d1b2d6046f7

--- src/server/scripts/World/npcs_special.cpp
+++ src/server/scripts/World/npcs_special.cpp
@@ -2878,7 +2878,7 @@ class CastFoodSpell : public BasicEvent
         bool Execute(uint64 /*execTime*/, uint32 /*diff*/) override
         {
             _owner->CastSpell(_owner, _spellId, true);
-            return false;
+            return true;
         }
 
     private:
