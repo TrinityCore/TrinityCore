edafb45b79868c1bec9aa2a7005a08ef48eeca59
Naios <naios-dev@live.de>
2017-11-28 22:05:25 +0100

    Build: Experimentally disable the /Zm workaround for MSVC
    (cherry picked from commit 697b4dbef20b5ceacd4c66f365b5ff71c62cb80c)

--- cmake/compiler/msvc/settings.cmake
+++ cmake/compiler/msvc/settings.cmake
@@ -139,12 +139,6 @@ if (BUILD_SHARED_LIBS)
   message(STATUS "MSVC: Enabled shared linking")
 endif()
 
-# Specify the maximum PreCompiled Header memory allocation limit
-# Fixes a compiler-problem when using PCH - the /Ym flag is adjusted by the compiler in MSVC2012, hence we need to set an upper limit with /Zm to avoid discrepancies)
-# (And yes, this is a verified , unresolved bug with MSVC... *sigh*)
-string(REGEX REPLACE "/Zm[0-9]+ *" "" CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /Zm500")
-
 # Enable and treat as errors the following warnings to easily detect virtual function signature failures:
 # 'function' : member function does not override any base class virtual member function
 # 'virtual_function' : no override available for virtual member function from base 'class'; function is hidden
