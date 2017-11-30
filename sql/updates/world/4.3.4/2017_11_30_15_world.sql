e89937441371ab8e1c70a685caf9603c8e1a545a
Aokromes <aokromes@gmail.com>
2017-11-30 15:07:07 +0100

    DB/Misc: Fix few startup errors

---
+++ sql/updates/world/4.3.4/2017_11_30_15_world.sql
@@ -0,0 +1,4 @@
+--
+UPDATE smart_scripts SET link=0 WHERE entryorguid=46274 AND source_Type=0 AND id=0;
+UPDATE smart_scripts SET link=2 WHERE entryorguid=47250 AND source_Type=0 AND id=0;
+UPDATE smart_scripts SET event_flags=1 WHERE entryorguid=46942 AND source_Type=0 AND id IN (10,11);
