96d5fa62d61695f582792f7a68fb04ca277bcc2c
Aokromes <aokromes@gmail.com>
2017-12-01 19:09:07 +0100

    DB/Misc: Fix few startup errors
    By Killyana

---
+++ sql/updates/world/4.3.4/2017_12_01_06_world.sql
@@ -0,0 +1,4 @@
+--
+UPDATE `smart_scripts` SET `action_param1`=12039 WHERE `entryorguid`=4517 AND `source_type`=0 AND `id`=2;
+UPDATE `smart_scripts` SET `action_type`=85, `action_param1`=79606 WHERE `entryorguid`=204019 AND `source_type`=1 AND `id`=1;
+DELETE FROM `spell_target_position` WHERE `ID`=71241;
