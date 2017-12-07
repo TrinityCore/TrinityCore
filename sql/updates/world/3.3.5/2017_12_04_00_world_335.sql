313c6a0677cbcf5b0b9c9e6311893515ca7d4019
Aokromes <aokromes@gmail.com>
2017-12-07 18:16:46 +0100

    Rename files

--- sql/updates/world/3.3.5/2017_12_04_00_world_335.sql
+++
@@ -1,2 +0,0 @@
--- Replace two Riverpaw Scouts with Riverpaw Shamans
-UPDATE `creature` SET `id`=1065, `modelid`=204, `curhealth`=230, `curmana`=249 WHERE `guid` IN (86657, 86662);
--- sql/updates/world/3.3.5/2017_12_04_03_world_335.sql
+++
@@ -1,2 +0,0 @@
--- Parqual Fintallas
-UPDATE `smart_scripts` SET `event_param1`=4764 WHERE `entryorguid`=4488 AND `source_type`=0 AND `id`=10;
