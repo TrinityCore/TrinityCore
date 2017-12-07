869affa89300c24b85896a050b58241714786454
Ovah <dreadkiller@gmx.de>
2017-12-07 17:41:11 +0100

    Core/Creatures: Implemented extra flag to use offhand attacks (#21008)
    * Fixed initial offhand attack timer so the main and offhand attacks will now act blizzlike instead of getting executed at the same time
    
    Note: since some creature equipment items cannot be identified as weapon since they are only visual items there is no way to detect this automaticly

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -5889,9 +5889,9 @@ bool Unit::Attack(Unit* victim, bool meleeAttack)
         SetUInt32Value(UNIT_NPC_EMOTESTATE, EMOTE_ONESHOT_NONE);
     }
 
-    // delay offhand weapon attack to next attack time
+    // delay offhand weapon attack by 50% of the base attack time
     if (haveOffhandWeapon() && GetTypeId() != TYPEID_PLAYER)
-        resetAttackTimer(OFF_ATTACK);
+        setAttackTimer(OFF_ATTACK, std::max(getAttackTimer(OFF_ATTACK), getAttackTimer(BASE_ATTACK) + uint32(CalculatePct(GetFloatValue(UNIT_FIELD_BASEATTACKTIME), 50))));
 
     if (meleeAttack)
         SendMeleeAttackStart(victim);
