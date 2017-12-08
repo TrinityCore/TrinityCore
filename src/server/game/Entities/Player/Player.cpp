925557d1c097cd45311b38dc3a9c715579456980
jackpoz <giacomopoz@gmail.com>
2017-12-08 15:14:51 +0100

    Core/Misc: Fix static analysis issues
    Fix various coverity reported issues

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -15824,6 +15824,7 @@ void Player::RemoveRewardedQuest(uint32 questId, bool update /*= true*/)
 
     // Remove seasonal quest also
     Quest const* qInfo = sObjectMgr->GetQuestTemplate(questId);
+    ASSERT(qInfo);
     if (qInfo->IsSeasonal())
     {
         uint16 eventId = qInfo->GetEventIdForQuest();
