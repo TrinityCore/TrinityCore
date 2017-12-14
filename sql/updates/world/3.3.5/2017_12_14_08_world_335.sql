61113d7a5f86c43f37b2f37ade6c94dbe844e0aa
Aokromes <aokromes@gmail.com>
2017-12-14 17:18:05 +0100

    Fix startup error

--- sql/updates/world/3.3.5/2017_12_14_08_world_335.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM `spawn_group` WHERE `groupId`=2 AND `spawnType`=0 AND `spawnId` IN (16668,16669,16806,17553);
-UPDATE `conditions` SET `SourceTypeOrReferenceId`=17, `SourceGroup`=0, `ConditionTarget`=1 WHERE `SourceEntry`=29528 AND `SourceTypeOrReferenceId`=13;
-UPDATE `conditions` SET `SourceTypeOrReferenceId`=17, `SourceGroup`=0, `ConditionTarget`=1 WHERE `SourceEntry`=57908 AND `SourceTypeOrReferenceId`=13;
