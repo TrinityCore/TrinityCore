90d37a1f5c509c0174d012d8085367076ff68ee7
ccrs <ccrs@users.noreply.github.com>
2017-12-15 21:35:14 +0100

    and so... the nightmare begins

--- src/server/game/Battlefield/Battlefield.h
+++ src/server/game/Battlefield/Battlefield.h
@@ -31,7 +31,6 @@ class Map;
 class Player;
 class Unit;
 class WorldPacket;
-struct QuaternionData;
 struct WorldSafeLocsEntry;
 
 class BattlefieldGraveyard;
@@ -133,10 +132,6 @@ class TC_GAME_API Battlefield : public ZoneScript
         bool AddOrSetPlayerToCorrectBfGroup(Player* player);
         void RemovePlayerFromResurrectQueue(ObjectGuid playerGUID);
         void SendAreaSpiritHealerQueryOpcode(Player* player, ObjectGuid guid);
-        Creature* SpawnCreature(uint32 entry, Position const& pos);
-        GameObject* SpawnGameObject(uint32 entry, Position const& pos, QuaternionData const& rot);
-        void HideCreature(Creature* creature);
-        void ShowCreature(Creature* creature, bool aggressive);
         void DoPlaySoundToAll(uint32 soundId);
         // packet senders
         void BroadcastPacketToZone(WorldPacket& data) const;
@@ -144,6 +139,10 @@ class TC_GAME_API Battlefield : public ZoneScript
         void BroadcastPacketToWar(WorldPacket& data) const;
         void AddCapturePoint(BattlefieldCapturePoint* capturePoint) { _capturePoints.push_back(capturePoint); }
         void TeamCastSpell(TeamId team, int32 spellId);
+
+        // SpawnGroups
+        void SpawnGroupSpawn(uint32 groupId);
+        void SpawnGroupDespawn(uint32 groupId);
 
         bool IsEnabled() const { return _enabled; }
         bool IsWarTime() const { return _active; }
