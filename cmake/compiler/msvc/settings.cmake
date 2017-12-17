891960edbe3768284a3892ae3edbe4afb6395e8f
Naios <naios-dev@live.de>
2017-12-17 20:54:41 +0100

    Build: Require Visual Studio 2017 or greater * See the deprecation notice at   https://community.trinitycore.org/topic/13294-gcc-49-visual-studio-2015-end-of-life/
    (cherry picked from commit b9b667f9253cb5c247cf3dd195710bf764f549d6)

--- cmake/compiler/msvc/settings.cmake
+++ cmake/compiler/msvc/settings.cmake
@@ -1,10 +1,11 @@
 # set up output paths for executable binaries (.exe-files, and .dll-files on DLL-capable platforms)
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
 
-set(MSVC_EXPECTED_VERSION 19.0.24210) # MSVC 2015 Update 3
+set(MSVC_EXPECTED_VERSION 19.10)
+set(MSVC_EXPECTED_VERSION_STRING "MSVC 2017")
 
 if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS MSVC_EXPECTED_VERSION)
-  message(FATAL_ERROR "MSVC: TrinityCore requires version ${MSVC_EXPECTED_VERSION} (MSVC 2015 Update 3) to build but found ${CMAKE_CXX_COMPILER_VERSION}")
+  message(FATAL_ERROR "MSVC: TrinityCore requires version ${MSVC_EXPECTED_VERSION} (${MSVC_EXPECTED_VERSION_STRING}) to build but found ${CMAKE_CXX_COMPILER_VERSION}")
 endif()
 
 # CMake sets warning flags by default, however we manage it manually
