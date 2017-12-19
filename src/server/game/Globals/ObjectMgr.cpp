18f2d51a0afbaca5b2370eb12d9ed991e86928a7
ariel- <ariel-@users.noreply.github.com>
2017-12-19 19:59:36 -0300

    Core/Globals: validate creature_template mingold and maxgold fields
    (cherry picked from commit 5798fda0a027c1199ea4ddd50f5567cf24c74c5b)

--- src/server/game/Globals/ObjectMgr.cpp
+++ src/server/game/Globals/ObjectMgr.cpp
@@ -938,6 +938,13 @@ void ObjectMgr::CheckCreatureTemplate(CreatureTemplate const* cInfo)
         ok = true;
     }
 
+    if (cInfo->mingold > cInfo->maxgold)
+    {
+        TC_LOG_ERROR("sql.sql", "Creature (Entry: %u) has `mingold` %u which is greater than `maxgold` %u, setting `maxgold` to %u.",
+            cInfo->Entry, cInfo->mingold, cInfo->maxgold, cInfo->mingold);
+        const_cast<CreatureTemplate*>(cInfo)->maxgold = cInfo->mingold;
+    }
+
     if (cInfo->AIName == "TotemAI")
     {
         TC_LOG_ERROR("sql.sql", "Creature (Entry: %u) has not-allowed `AIName` '%s' set, removing", cInfo->Entry, cInfo->AIName.c_str());
