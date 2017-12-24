e7104ab56d01d376c5c275436cf847aeee27de12
tkrokli <tkrokli@users.noreply.github.com>
2017-12-24 14:47:59 +0100

    Update 9999_99_99_99_world.sql
    - only perform necessary changes, not redo all the spell IDs

--- sql/updates/world/3.3.5/9999_99_99_99_world.sql
+++ sql/updates/world/3.3.5/9999_99_99_99_world.sql
@@ -1,11 +1,5 @@
--- repeal/replace some wrong spells
-DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_gen_despawn_self', 'spell_deliver_stolen_horse');
+-- unlink & link the necessary spells
+DELETE FROM `spell_script_names` WHERE `spell_id`= 52267;
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_deliver_stolen_horse';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
-(15998, 'spell_gen_despawn_self'),
-(25952, 'spell_gen_despawn_self'),
-(29435, 'spell_gen_despawn_self'),
-(45980, 'spell_gen_despawn_self'),
-(51592, 'spell_gen_despawn_self'),
-(51910, 'spell_gen_despawn_self'),
-(52264, 'spell_deliver_stolen_horse'),
-(54420, 'spell_gen_despawn_self');
+(52264, 'spell_deliver_stolen_horse');
