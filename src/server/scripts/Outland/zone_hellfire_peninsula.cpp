b8e4863cd92b7a5dac1dab05f3185a0ea1f70073
Kittnz <frederik156@hotmail.com>
2017-12-25 14:35:33 +0100

    some fixes

--- src/server/scripts/Outland/zone_hellfire_peninsula.cpp
+++ src/server/scripts/Outland/zone_hellfire_peninsula.cpp
@@ -1177,9 +1177,10 @@ struct npc_infernal_rain_hellfire : public ScriptedAI
                     Trinity::CreatureListSearcher<Trinity::AllCreaturesOfEntryInRange> searcher(me, infernalrainList, checker);
                     Cell::VisitAllObjects(me, searcher, 200.0f);
 
-                    if (Creature* random = Trinity::Containers::SelectRandomContainerElement(infernalrainList))
-                        if (random->isMoving() && random->GetPositionZ() < 118.0f)
-                            me->CastCustomSpell(SPELL_INFERNAL_RAIN, SPELLVALUE_MAX_TARGETS, 1, random, true);
+                    Creature* random = Trinity::Containers::SelectRandomContainerElement(infernalrainList);
+
+                    if (random->isMoving() && random->GetPositionZ() < 118.0f)
+                        me->CastCustomSpell(SPELL_INFERNAL_RAIN, SPELLVALUE_MAX_TARGETS, 1, random, true);
 
                     _events.ScheduleEvent(EVENT_INFERNAL_RAIN_CAST, 1s, 2s);
                     break;
