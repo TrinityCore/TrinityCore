97c7e32a42d4041b4d74e1d6bf951ed2f6161661
joschiwald <joschiwald.trinity@gmail.com>
2017-12-31 00:10:25 +0100

    SQL: Fixed typo

--- sql/updates/auth/master/2017_12_30_01_auth.sql
+++ sql/updates/auth/master/2017_12_30_01_auth.sql
@@ -2,9 +2,10 @@
 DELETE FROM `rbac_linked_permissions` WHERE `id`=198 AND `linkedId`=867;
 DELETE FROM `rbac_permissions` WHERE `id`=867;
 --
+DELETE FROM `rbac_linked_permissions` WHERE `id`=198 AND `linkedId`=868;
 DELETE FROM `rbac_permissions` WHERE `id`=868;
 INSERT INTO `rbac_permissions` (`id`, `name`) VALUES
-(867, 'Command: modify power');
+(868, 'Command: modify power');
 
-DELETE FROM `rbac_linked_permissions` WHERE `id`=198 AND `linkedId`=868;
-INSERT INTO `rbac_linked_permissions` (`id`, `linkedId`) VALUES (198, 867);
+INSERT INTO `rbac_linked_permissions` (`id`, `linkedId`) VALUES
+(198, 868);
