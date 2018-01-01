e39f4575cb8bee8e592cc7d4d878308166972e77
funjoker <torti-esser@web.de>
2018-01-02 00:39:50 +0100

    Update to 7.3.2

--- src/server/game/Entities/Pet/Pet.h
+++ src/server/game/Entities/Pet/Pet.h
@@ -27,13 +27,13 @@
 
 enum StableResultCode
 {
-    STABLE_ERR_MONEY = 0x01,                         // "you don't have enough money"
-    STABLE_ERR_INVALID_SLOT = 0x03,                         // "That slot is locked"
-    STABLE_SUCCESS_STABLE = 0x08,                         // stable success
+    STABLE_ERR_MONEY        = 0x01,                         // "you don't have enough money"
+    STABLE_ERR_INVALID_SLOT = 0x02,                         // "That slot is locked"
+    STABLE_SUCCESS_STABLE   = 0x08,                         // stable success
     STABLE_SUCCESS_UNSTABLE = 0x09,                         // unstable/swap success
     STABLE_SUCCESS_BUY_SLOT = 0x0A,                         // buy slot success
-    STABLE_ERR_EXOTIC = 0x0B,                         // "you are unable to control exotic creatures"
-    STABLE_ERR_STABLE = 0x0C,                         // "Internal pet error"
+    STABLE_ERR_EXOTIC       = 0x0B,                         // "you are unable to control exotic creatures"
+    STABLE_ERR_STABLE       = 0x0C                          // "Internal pet error"
 };
 
 struct PetSpell
