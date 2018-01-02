9bf5a03bc42682f695f7381bd1eb83ec9bc9df96
ariel- <ariel-@users.noreply.github.com>
2018-01-02 11:45:09 -0300

    Core/Scripts: fix Millhouse Manastorm resetting dialogs after Skyriss encounter
    Closes #21159
    
    (cherry picked from commit fd7fff80c9ce9954c58fa3fa35eadcdfe5648e3d)

--- src/server/scripts/Outland/TempestKeep/arcatraz/arcatraz.cpp
+++ src/server/scripts/Outland/TempestKeep/arcatraz/arcatraz.cpp
@@ -74,7 +74,7 @@ class npc_millhouse_manastorm : public CreatureScript
 
         struct npc_millhouse_manastormAI : public ScriptedAI
         {
-            npc_millhouse_manastormAI(Creature* creature) : ScriptedAI(creature)
+            npc_millhouse_manastormAI(Creature* creature) : ScriptedAI(creature), Init(false)
             {
                 Initialize();
                 instance = creature->GetInstanceScript();
@@ -84,7 +84,6 @@ class npc_millhouse_manastorm : public CreatureScript
             {
                 EventProgress_Timer = 2000;
                 LowHp = false;
-                Init = false;
                 Phase = 1;
 
                 Pyroblast_Timer = 1000;
