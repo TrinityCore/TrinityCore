9f9507e6a1fd50a5ce643a4096c1712700244a61
Treeston <treeston.mmoc@gmail.com>
2018-01-04 04:46:22 +0100

    Core/Misc: A variety of clean-up changes, mostly following up on 532ab1c to fix legacy bugs exposed by it:
    - Triggers can no longer have a threat list (this may expose some ugliness in old legacy scripts)
    - Threat entries are forced to OFFLINE if the AI refuses to attack the target
    - Clean up passive creature evade behavior to be more consistent
    - Fix a months old issue in spawn group management that would cause "Inactive" to incorrectly show in .list respawns for system groups outside of map 0
    - Valithria script cleanups, remove old hacks and make it work with the new system. Closes #21174.
    - Some strings cleanup

--- src/server/scripts/Commands/cs_debug.cpp
+++ src/server/scripts/Commands/cs_debug.cpp
@@ -852,8 +852,6 @@ public:
             handler->PSendSysMessage("%s (guid %u) is not alive.", target->GetName().c_str(), target->GetGUID().GetCounter());
             return true;
         }
-        if (!target->CanHaveThreatList())
-            handler->PSendSysMessage("%s (guid %u) cannot have a threat list.", target->GetName().c_str(), target->GetGUID().GetCounter());
 
         uint32 count = 0;
         auto const& threatenedByMe = target->GetThreatManager().GetThreatenedByMeList();
@@ -871,11 +869,11 @@ public:
         }
 
         if (!mgr.CanHaveThreatList())
-            return true;
-        if (mgr.IsEngaged())
+            handler->PSendSysMessage("%s (guid %u) cannot have a threat list.", target->GetName().c_str(), target->GetGUID().GetCounter());
+        else if (mgr.IsEngaged())
         {
             count = 0;
-            handler->PSendSysMessage("Threat list of %s (guid %u, DB GUID %u)", target->GetName().c_str(), target->GetGUID().GetCounter(), target->GetTypeId() == TYPEID_UNIT ? target->ToCreature()->GetSpawnId() : 0);
+            handler->PSendSysMessage("Threat list of %s (guid %u, spawnID %u)", target->GetName().c_str(), target->GetGUID().GetCounter(), target->GetTypeId() == TYPEID_UNIT ? target->ToCreature()->GetSpawnId() : 0);
             for (ThreatReference const* ref : mgr.GetSortedThreatList())
             {
                 Unit* unit = ref->GetVictim();
@@ -908,7 +906,7 @@ public:
             handler->SendSysMessage("End of threat list.");
         }
         else
-            handler->PSendSysMessage("%s (guid %u, DB GUID %u) is not currently engaged.", target->GetName().c_str(), target->GetGUID().GetCounter(), target->GetTypeId() == TYPEID_UNIT ? target->ToCreature()->GetSpawnId() : 0);
+            handler->PSendSysMessage("%s (guid %u, spawnID %u) is not currently engaged.", target->GetName().c_str(), target->GetGUID().GetCounter(), target->GetTypeId() == TYPEID_UNIT ? target->ToCreature()->GetSpawnId() : 0);
         return true;
     }
 
