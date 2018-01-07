b792911cfb8688d5eb6a4ea97c6c89e98ea5720e
zamalaev <zgn88@mail.ru>
2018-01-07 05:29:40 +0300

    Core/Battleground: fix visual display of the flag Twin Peaks

--- src/server/game/Entities/GameObject/GameObject.cpp
+++ src/server/game/Entities/GameObject/GameObject.cpp
@@ -1856,7 +1856,7 @@ void GameObject::Use(Unit* user)
                     {
                         case 179785:                        // Silverwing Flag
                         case 179786:                        // Warsong Flag
-                            if (bg->GetTypeID(true) == BATTLEGROUND_WS)
+                            if (bg->GetTypeID(true) == BATTLEGROUND_WS || bg->GetTypeID(true) == BATTLEGROUND_TP)
                                 bg->EventPlayerClickedOnFlag(player, this);
                             break;
                         case 184142:                        // Netherstorm Flag
