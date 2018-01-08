be5c403d789ecac1e4ca45f1d574aabcd4b6e7e8
zamalaev <zgn88@mail.ru>
2018-01-08 23:09:14 +0300

    Core/GnomeRegan: fix quest Decontamination and some fix intro gnome
    call issues #278

---
+++ sql/ashamane/world/2018_01_08_00_world_gnome_zone.sql
@@ -0,0 +1,7 @@
+UPDATE `creature_template` SET `npcflag`=0 WHERE `entry`= 46363;
+UPDATE `creature_template` SET `ScriptName`='npc_sanitron_5000', `InhabitType` = 4, `HoverHeight` = 0 WHERE `entry`= 46185;
+INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `comment`, `BroadcastTextID`) VALUES
+(46185, 0, 0, 'Commencing decontamination sequence...', 12, 0, 100, 0, 0, 0, NULL, 46323),
+(46185, 1, 0, 'Decontamination complete. Standby for delivery.', 12, 0, 100, 0, 0, 0, NULL, 46324),
+(46185, 2, 0, 'Warning, system overload. Malfunction imminent!', 12, 0, 100, 0, 0, 0, NULL, 46325),
+(46230, 0, 0, 'Ugh! Not this again! I''m asking for a new station next expedition...', 12, 0, 100, 0, 0, 0, NULL, 46342);
