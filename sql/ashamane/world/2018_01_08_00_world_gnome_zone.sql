1d98aa8f9d66596390e197d10fa64062c08b01b4
zamalaev <zgn88@mail.ru>
2018-01-08 23:22:25 +0300

    DB/Misc: add missing data prev commit

--- sql/ashamane/world/2018_01_08_00_world_gnome_zone.sql
+++ sql/ashamane/world/2018_01_08_00_world_gnome_zone.sql
@@ -1,5 +1,6 @@
 UPDATE `creature_template` SET `npcflag`=0 WHERE `entry`= 46363;
 UPDATE `creature_template` SET `ScriptName`='npc_sanitron_5000', `InhabitType` = 4, `HoverHeight` = 0 WHERE `entry`= 46185;
+DELETE FROM `creature_text` WHERE `CreatureID` IN (46185, 46230);
 INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `comment`, `BroadcastTextID`) VALUES
 (46185, 0, 0, 'Commencing decontamination sequence...', 12, 0, 100, 0, 0, 0, NULL, 46323),
 (46185, 1, 0, 'Decontamination complete. Standby for delivery.', 12, 0, 100, 0, 0, 0, NULL, 46324),
