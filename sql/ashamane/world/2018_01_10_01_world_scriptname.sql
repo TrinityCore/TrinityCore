f4635b6988bbf51b0c7115ff05c6bbda253fd06e
zamalaev <zgn88@mail.ru>
2018-01-10 15:21:07 +0300

    DB/Misc: fix error sql

--- sql/ashamane/world/2018_01_10_01_world_scriptname.sql
+++ sql/ashamane/world/2018_01_10_01_world_scriptname.sql
@@ -1,2 +1,2 @@
-DELETE FROM spell_script_names WHERE scriptname = "spell_hun_barrage");
-INSERT INTO spell_script_names VALUES (120361, "spell_hun_barrage");
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_hun_barrage';
+INSERT INTO spell_script_names VALUES (120361, 'spell_hun_barrage');
