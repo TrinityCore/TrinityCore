770367a514ec6af2bcae687418b95d01681189ae
ariel- <ariel-@users.noreply.github.com>
2018-01-13 21:59:20 -0300

    Core/Unit: fix Sanctified Wrath (again)
    - Actually only rank 1 was affected because of 50% reduction on rank 2
    
    (cherry picked from commit 0e9b1635e0a2351342e8ed0ae4a1495479e09ffc)

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -7013,7 +7013,7 @@ uint32 Unit::SpellDamageBonusTaken(Unit* caster, SpellInfo const* spellProto, ui
             if (!(aurEff->GetMiscValue() & spellProto->GetSchoolMask()))
                 continue;
 
-            ApplyPct(damageReduction, aurEff->GetAmount());
+            AddPct(damageReduction, -aurEff->GetAmount());
         }
 
         TakenTotalMod = 1.0f - damageReduction;
@@ -7931,7 +7931,7 @@ uint32 Unit::MeleeDamageBonusTaken(Unit* attacker, uint32 pdamage, WeaponAttackT
             if (!(aurEff->GetMiscValue() & attackSchoolMask))
                 continue;
 
-            ApplyPct(damageReduction, aurEff->GetAmount());
+            AddPct(damageReduction, -aurEff->GetAmount());
         }
 
         TakenTotalMod = 1.0f - damageReduction;
