8b8b40971692652d003dead615423bb31e7b8684
Sean Rhone <Espionage724@users.noreply.github.com>
2018-01-15 15:33:05 -0500

    MYSQL_SERVER_VERSION to MIN_MYSQL_SERVER_VERSION
    Fixes the following during compiling:
    
    ‘‘‘
    In file included from /srv/trinity/src/src/server/database/PrecompiledHeaders/databasePCH.h:19:0,
                     from /srv/trinity/build/src/server/database/cotire/database_CXX_prefix.cxx:4,
                     from /srv/trinity/build/src/server/database/cotire/database_CXX_prefix.hxx:4:
    /srv/trinity/src/src/server/database/Database/DatabaseWorkerPool.cpp: In constructor ‘DatabaseWorkerPool<T>::DatabaseWorkerPool()’:
    /srv/trinity/src/src/server/database/Database/DatabaseWorkerPool.cpp:60:34: error: ‘MYSQL_SERVER_VERSION’ was not declared in this scope
             mysql_get_client_info(), MYSQL_SERVER_VERSION);
    ‘‘‘

--- src/server/database/Database/DatabaseWorkerPool.cpp
+++ src/server/database/Database/DatabaseWorkerPool.cpp
@@ -57,7 +57,7 @@ DatabaseWorkerPool<T>::DatabaseWorkerPool()
     WPFatal(mysql_thread_safe(), "Used MySQL library isn't thread-safe.");
     WPFatal(mysql_get_client_version() >= MIN_MYSQL_CLIENT_VERSION, "TrinityCore does not support MySQL versions below 5.1");
     WPFatal(mysql_get_client_version() == MYSQL_VERSION_ID, "Used MySQL library version (%s) does not match the version used to compile TrinityCore (%s). Search on forum for TCE00011.",
-        mysql_get_client_info(), MYSQL_SERVER_VERSION);
+        mysql_get_client_info(), MIN_MYSQL_SERVER_VERSION);
 }
 
 template <class T>
