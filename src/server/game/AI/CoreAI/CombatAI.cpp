3ff61beafa9f028e582c4d810b93352aa85b18a7
sirikfoll <sirikfoll@hotmail.com>
2018-01-17 19:01:09 -0200

    Core/CombatAI: Npcs with TurretAI will be able to attack again
    Needed after the new Threat system
    
    (cherry picked from commit e12e0ad5827d7e794baa630724069680d7e07cfb)

--- src/server/game/AI/CoreAI/CombatAI.cpp
+++ src/server/game/AI/CoreAI/CombatAI.cpp
@@ -248,11 +248,11 @@ TurretAI::TurretAI(Creature* c) : CreatureAI(c)
     me->m_SightDistance = me->m_CombatDistance;
 }
 
-bool TurretAI::CanAIAttack(Unit const* /*who*/) const
+bool TurretAI::CanAIAttack(Unit const* who) const
 {
     /// @todo use one function to replace it
-    if (!me->IsWithinCombatRange(me->GetVictim(), me->m_CombatDistance)
-        || (m_minRange && me->IsWithinCombatRange(me->GetVictim(), m_minRange)))
+    if (!me->IsWithinCombatRange(who, me->m_CombatDistance)
+        || (m_minRange && me->IsWithinCombatRange(who, m_minRange)))
         return false;
     return true;
 }
