71178863836b12b2275bc475b3acf10a5da0f2a9
Moein <tje3d@yahoo.com>
2018-01-17 20:44:53 +0330

    Should Delete Before Insert ScriptName

--- sql/updates/world/3.3.5/2018_01_17_xx_world.sql
+++ sql/updates/world/3.3.5/2018_01_17_xx_world.sql
@@ -1,6 +1,6 @@
 -- Reanimated Abomination Abilities
 UPDATE `creature_template` SET `spell1` = 59564, `spell2` = 59576 WHERE `entry` = 31692;
 
+DELETE FROM `spell_script_names` WHERE `spell_id` = 59576;
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 	(59576, 'spell_q13264_thats_abominable');
-
