df585571f108b3c44d3db1ab82683151f798cbf7
ariel- <ariel-@users.noreply.github.com>
2018-01-22 03:55:04 -0300

    Core/Unit: fixed Global cooldown cancelation on spell interrupt
    Thanks to Nyeriah for the heads up
    
    (cherry picked from commit 514c847881a9fff504e90f1186a1209c0b8a3674)

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -3078,12 +3078,13 @@ void Unit::InterruptSpell(CurrentSpellTypes spellType, bool withDelayed, bool wi
             if (GetTypeId() == TYPEID_PLAYER)
                 ToPlayer()->SendAutoRepeatCancel(this);
 
-        m_currentSpells[spellType] = nullptr;
-
         if (spell->getState() != SPELL_STATE_FINISHED)
             spell->cancel();
         else
+        {
+            m_currentSpells[spellType] = nullptr;
             spell->SetReferencedFromCurrent(false);
+        }
 
         if (GetTypeId() == TYPEID_UNIT && IsAIEnabled)
             ToCreature()->AI()->OnSpellCastInterrupt(spell->GetSpellInfo());
