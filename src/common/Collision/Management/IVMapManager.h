e0397433e05f36dc8c765e8f1746017d1caf926f
Golrag <golrag.jeremy@gmail.com>
2018-01-28 20:35:08 +0100

    make travis happy ?

--- src/common/Collision/Management/IVMapManager.h
+++ src/common/Collision/Management/IVMapManager.h
@@ -94,7 +94,7 @@ namespace VMAP
 
             virtual bool isInLineOfSight(unsigned int pMapId, float x1, float y1, float z1, float x2, float y2, float z2, ModelIgnoreFlags ignoreFlags) = 0;
             virtual float getHeight(unsigned int pMapId, float x, float y, float z, float maxSearchDist) = 0;
-            virtual float getCeil(unsigned int pMapId, float x, float y, float z, float maxSearchDist) { return VMAP_INVALID_HEIGHT_VALUE; }
+            virtual float getCeil(unsigned int /*pMapId*/, float /*x*/, float /*y*/, float /*z*/, float /*maxSearchDist*/) { return VMAP_INVALID_CEIL_VALUE; }
             /**
             test if we hit an object. return true if we hit one. rx, ry, rz will hold the hit position or the dest position, if no intersection was found
             return a position, that is pReduceDist closer to the origin
