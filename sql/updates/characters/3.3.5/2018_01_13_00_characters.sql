51c167afcbfd1f985cdebf1c059103e963914825
Ivan <fwef@wefwef.com>
2018-02-02 20:23:30 -0800

    SQL FIx

--- sql/updates/characters/3.3.5/2018_01_13_00_characters.sql
+++ sql/updates/characters/3.3.5/2018_01_13_00_characters.sql
@@ -1,7 +1,7 @@
-ALTER TABLE `character_aura`
+/*ALTER TABLE `character_aura`
 ADD COLUMN `critChance` FLOAT NOT NULL DEFAULT '0' AFTER `remainCharges`,
 ADD COLUMN `applyResilience` TINYINT(3) NOT NULL DEFAULT '0' AFTER `critChance`;
 
 ALTER TABLE `pet_aura`
 ADD COLUMN `critChance` FLOAT NOT NULL DEFAULT '0' AFTER `remainCharges`,
-ADD COLUMN `applyResilience` TINYINT(3) NOT NULL DEFAULT '0' AFTER `critChance`;
+ADD COLUMN `applyResilience` TINYINT(3) NOT NULL DEFAULT '0' AFTER `critChance`;*/
