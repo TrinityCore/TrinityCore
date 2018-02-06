9fdb4c0a4d7368e011773cbb3862dfa76d362a15
ariel- <ariel-@users.noreply.github.com>
2018-02-06 13:59:08 -0300

    Core/Scripts: fix non-functional Celestial Planetarium Access GO
    - Client only sends CMSG_GAMEOBJ_REPORT_USE for this one
    
    Closes #19982

--- src/server/scripts/Northrend/Ulduar/Ulduar/boss_algalon_the_observer.cpp
+++ src/server/scripts/Northrend/Ulduar/Ulduar/boss_algalon_the_observer.cpp
@@ -979,7 +979,7 @@ class go_celestial_planetarium_access : public GameObjectScript
             {
             }
 
-            bool GossipHello(Player* player) override
+            bool OnReportUse(Player* player) override
             {
                 if (me->HasFlag(GAMEOBJECT_FLAGS, GO_FLAG_IN_USE))
                     return true;
