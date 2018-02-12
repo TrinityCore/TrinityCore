e6888f72e067631876362fa6be7728fe471deeb5
ariel- <ariel-@users.noreply.github.com>
2018-02-11 21:10:56 -0300

    Core/Scripts: implemented Combined Toxins proc only from poisons

--- src/server/scripts/Northrend/AzjolNerub/Ahnkahet/instance_ahnkahet.cpp
+++ src/server/scripts/Northrend/AzjolNerub/Ahnkahet/instance_ahnkahet.cpp
@@ -280,7 +280,26 @@ class instance_ahnkahet : public InstanceMapScript
         }
 };
 
+// 56584 - Combined Toxins
+class spell_combined_toxins : public AuraScript
+{
+    PrepareAuraScript(spell_combined_toxins);
+
+    bool CheckProc(AuraEffect const* /*aurEff*/, ProcEventInfo& eventInfo)
+    {
+        // only procs on poisons (damage class check to exclude stuff like Envenom)
+        SpellInfo const* spellInfo = eventInfo.GetSpellInfo();
+        return (spellInfo && spellInfo->Dispel == DISPEL_POISON && spellInfo->DmgClass != SPELL_DAMAGE_CLASS_MELEE);
+    }
+
+    void Register() override
+    {
+        DoCheckEffectProc += AuraCheckEffectProcFn(spell_combined_toxins::CheckProc, EFFECT_0, SPELL_AURA_PROC_TRIGGER_DAMAGE);
+    }
+};
+
 void AddSC_instance_ahnkahet()
 {
     new instance_ahnkahet();
+    RegisterAuraScript(spell_combined_toxins);
 }
