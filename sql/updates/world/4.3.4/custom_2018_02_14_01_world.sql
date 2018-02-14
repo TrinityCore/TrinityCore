cf2f63b7756358dbcedc2d91849a3c5f611d7faf
Ovalord <1Don7H4v3@m41L.com>
2018-02-14 21:06:09 +0100

    DB/GB: fixed a syntax error

--- sql/updates/world/4.3.4/custom_2018_02_14_01_world.sql
+++ sql/updates/world/4.3.4/custom_2018_02_14_01_world.sql
@@ -1,4 +1,4 @@
-DELETE FROM `spell_script_names` WHERE `ScriptName`= 'spell_umbris_blitz';
+DELETE FROM `spell_script_names` WHERE `ScriptName`= 'spell_umbriss_blitz';
 
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (74670, 'spell_umbriss_blitz'),
