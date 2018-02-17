263617f81fb2e05c2e9bec05f739ee37ee524a94
sirikfoll <sirikfoll@hotmail.com>
2018-02-17 20:11:37 -0200

    Scripts/Icecrown Citadel: Prevent Lady Deathwhisper movement during first phase.

--- src/server/scripts/Northrend/IcecrownCitadel/boss_lady_deathwhisper.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_lady_deathwhisper.cpp
@@ -286,10 +286,10 @@ class boss_lady_deathwhisper : public CreatureScript
                     return;
                 }
 
+                _phase = PHASE_ONE;
                 me->SetCombatPulseDelay(5);
                 me->setActive(true);
                 DoZoneInCombat();
-                _phase = PHASE_ONE;
                 scheduler.CancelGroup(GROUP_INTRO);
                 // phase-independent events
                 scheduler
@@ -363,10 +363,10 @@ class boss_lady_deathwhisper : public CreatureScript
                         darnavan->SetReactState(REACT_PASSIVE);
                         darnavan->m_Events.AddEvent(new DaranavanMoveEvent(*darnavan), darnavan->m_Events.CalculateTime(10000));
                         darnavan->AI()->Talk(SAY_DARNAVAN_RESCUED);
-						
+
 						if (!killer)
 							return;
-						
+
                         if (Player* owner = killer->GetCharmerOrOwnerPlayerOrPlayerItself())
                         {
                             if (Group* group = owner->GetGroup())
@@ -887,10 +887,10 @@ class npc_darnavan : public CreatureScript
             void JustDied(Unit* killer) override
             {
                 _events.Reset();
-				
+
 				if (!killer)
 					return;
-				
+
                 if (Player* owner = killer->GetCharmerOrOwnerPlayerOrPlayerItself())
                 {
                     if (Group* group = owner->GetGroup())
