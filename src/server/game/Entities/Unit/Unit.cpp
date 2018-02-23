fa5c01daf242306b49bfcc5df6a99a523d6ea422
ariel- <ariel-@users.noreply.github.com>
2018-02-23 13:15:50 -0300

    Core/Scripts: remove hack that caused aura to not be applied and saved twice on owned auras, causing an error on save
    - This is handled by group stack rules actually
    - Also reset removed aura counter when cleaning removed auras
    
    Closes #21486
    
    (cherry picked from commit 1074a9b053c7d1fbff1321bf95b15d7be1ee669f)

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -2883,6 +2883,8 @@ void Unit::_DeleteRemovedAuras()
         delete m_removedAuras.front();
         m_removedAuras.pop_front();
     }
+
+    m_removedAurasCount = 0;
 }
 
 void Unit::_UpdateSpells(uint32 time)
