e9ab687288aadbd376e4f2f2657aaabd7a7a4b47
aeroashamane <36714383+aeroashamane@users.noreply.github.com>
2018-02-24 16:03:31 +0100

    Update 2018_02_23_04_quest.sql

--- sql/ashamane/world/2018_02_23_04_quest.sql
+++ sql/ashamane/world/2018_02_23_04_quest.sql
@@ -1,2 +1,26 @@
 -- Table `creature_template`
 UPDATE `creature_template` SET `InhabitType` = 4 WHERE (entry = 41768);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `InhabitType` = 3 WHERE (entry = 105362);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `InhabitType` = 3 WHERE (entry = 105360);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `InhabitType` = 3 WHERE (entry = 97919);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `InhabitType` = 3 WHERE (entry = 97920);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `InhabitType` = 3 WHERE (entry = 104582);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `InhabitType` = 3 WHERE (entry = 104589);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `InhabitType` = 3 WHERE (entry = 88099);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `InhabitType` = 3 WHERE (entry = 88089);
