2ae023cb21cc279862cb5e0b5e10e06afcc9a756
aeroashamane <36714383+aeroashamane@users.noreply.github.com>
2018-02-24 22:07:46 +0100

    DB/Quest #334

---
+++ sql/ashamane/world/2018_02_24_07_quest.sql
@@ -0,0 +1,5 @@
+-- Table `creature_template`
+UPDATE `creature_template` SET `InhabitType` = 3 WHERE (entry = 108314);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `InhabitType` = 3 WHERE (entry = 94282);
