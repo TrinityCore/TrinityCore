6661cbc533b68a8541c09a5c309c66614df50519
aeroashamane <36714383+aeroashamane@users.noreply.github.com>
2018-02-27 23:21:39 +0100

    DB/Fix ADD

---
+++ sql/ashamane/world/2018_02_27_04_world.sql
@@ -0,0 +1,74 @@
+-- Table `creature_template`
+UPDATE `creature_template` SET `InhabitType` = 4 WHERE (entry = 14272);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `unit_flags` = 32768 WHERE (entry = 48432);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `InhabitType` = 6 WHERE (entry = 8976);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `unit_class` = 1, `unit_flags` = 32784 WHERE (entry = 48119);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `unit_flags` = 32784 WHERE (entry = 48432);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `InhabitType` = 4 WHERE (entry = 2831);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `InhabitType` = 6 WHERE (entry = 47103);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `InhabitType` = 6 WHERE (entry = 2830);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `unit_flags` = 80, `InhabitType` = 4 WHERE (entry = 2931);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `unit_flags` = 80, `InhabitType` = 6 WHERE (entry = 2726);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `InhabitType` = 4 WHERE (entry = 14268);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `unit_flags` = 72, `InhabitType` = 6 WHERE (entry = 44628);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `InhabitType` = 4 WHERE (entry = 44184);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `unit_flags` = 512 WHERE (entry = 42155);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `InhabitType` = 4 WHERE (entry = 2579);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `unit_flags` = 33280 WHERE (entry = 45882);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `unit_flags` = 33280 WHERE (entry = 45604);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `unit_flags` = 32776 WHERE (entry = 45636);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `unit_flags` = 576 WHERE (entry = 46560);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `unit_flags` = 33280 WHERE (entry = 44732);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `unit_flags` = 33280 WHERE (entry = 44733);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `unit_flags` = 33280 WHERE (entry = 46559);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `unit_flags` = 512 WHERE (entry = 45251);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `unit_flags` = 33280 WHERE (entry = 44636);
+
+-- Table `creature_template`
+UPDATE `creature_template` SET `InhabitType` = 4 WHERE (entry = 1554);
