868b7f182ce7e4904a8f1a3724e03676f3eb5555
aeroashamane <36714383+aeroashamane@users.noreply.github.com>
2018-03-03 00:39:00 +0100

    DB/Fix add argus

--- sql/ashamane/world/2018_03_02_world.sql
+++ sql/ashamane/world/2018_03_02_world.sql
@@ -5,172 +5,172 @@ UPDATE `creature_template` SET  `minlevel` = 112, `maxlevel` = 112, `ManaModifie
 UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `faction` = 14, `rank` = 2, `BaseAttackTime` = 2800, `unit_class` = 2, `unit_flags` = 64, `unit_flags2` = 2048, `type` = 3, `type_flags` = 2097260, `HealthModifier` = 119661438, `ManaModifier` = 285263 WHERE (entry = 123464);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 205897311, `ManaModifier` = 285263 WHERE (entry = 127705);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `ManaModifier` = 285263 WHERE (entry = 127705);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 101873927, `ManaModifier` = 285263 WHERE (entry = 127376);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `ManaModifier` = 285263 WHERE (entry = 127376);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 108342113, `ManaModifier` = 285263 WHERE (entry = 123689);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `ManaModifier` = 285263 WHERE (entry = 123689);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 97022788 WHERE (entry = 122958);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 122958);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 117833405, `ManaModifier` = 285263 WHERE (entry = 120393);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `ManaModifier` = 285263 WHERE (entry = 120393);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 214476366 WHERE (entry = 127700);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 127700);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 202974423, `ManaModifier` = 285263 WHERE (entry = 127706);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `ManaModifier` = 285263 WHERE (entry = 127706);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 117833405, `ManaModifier` = 285263 WHERE (entry = 125388);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `ManaModifier` = 285263 WHERE (entry = 125388);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 101487212, `ManaModifier` = 285263 WHERE (entry = 126115);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `ManaModifier` = 285263 WHERE (entry = 126115);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 121278485, `ManaModifier` = 285263 WHERE (entry = 125820);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `ManaModifier` = 285263 WHERE (entry = 125820);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 137007736, `ManaModifier` = 285263 WHERE (entry = 125824);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `ManaModifier` = 285263 WHERE (entry = 125824);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 129363717 WHERE (entry = 122912);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 122912);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 132597810 WHERE (entry = 124775);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 124775);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 125172006, `ManaModifier` = 285263 WHERE (entry = 126815);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `ManaModifier` = 285263 WHERE (entry = 126815);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 98639834 WHERE (entry = 126860);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 126860);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 97022788 WHERE (entry = 126864);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 126864);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 145465004, `ManaModifier` = 285263 WHERE (entry = 126866);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `ManaModifier` = 285263 WHERE (entry = 126866);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 99725913 WHERE (entry = 126868);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 126868);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 101849794 WHERE (entry = 126885);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 126885);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 86602479, `ManaModifier` = 570526 WHERE (entry = 126889);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `ManaModifier` = 570526 WHERE (entry = 126889);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 100207055 WHERE (entry = 126898);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 126898);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 109959160 WHERE (entry = 124440);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 124440);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 113327387 WHERE (entry = 125498);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 125498);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 106050064 WHERE (entry = 126908);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 126908);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 97022788 WHERE (entry = 126912);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 126912);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 118044392 WHERE (entry = 126338);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 126338);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 111635933 WHERE (entry = 127300);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 127300);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 134666748 WHERE (entry = 126254);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 126254);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 129776350 WHERE (entry = 127084);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 127084);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 106725067, `ManaModifier` = 285263 WHERE (entry = 126946);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `ManaModifier` = 285263 WHERE (entry = 126946);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 103492532 WHERE (entry = 122838);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 122838);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 147075714 WHERE (entry = 122999);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 122999);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 114392222, `ManaModifier` = 285263 WHERE (entry = 122947);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112,  `ManaModifier` = 285263 WHERE (entry = 122947);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 113903905 WHERE (entry = 127581);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 127581);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 202000122, `ManaModifier` = 285263 WHERE (entry = 127703);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `ManaModifier` = 285263 WHERE (entry = 127703);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 106725067, `ManaModifier` = 285263 WHERE (entry = 124804);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `ManaModifier` = 285263 WHERE (entry = 124804);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 119516739, `ManaModifier` = 285263 WHERE (entry = 125479);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `ManaModifier` = 285263 WHERE (entry = 125479);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 104366730 WHERE (entry = 126199);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 126199);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 98639834 WHERE (entry = 126419);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 126419);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 105108020, `ManaModifier` = 285263 WHERE (entry = 122911);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `ManaModifier` = 285263 WHERE (entry = 122911);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 119661438, `ManaModifier` = 285263 WHERE (entry = 123464);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `ManaModifier` = 285263 WHERE (entry = 123464);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 92099842 WHERE (entry = 126852);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 126852);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 100787849 WHERE (entry = 126862);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 126862);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 108420748, `ManaModifier` = 285263 WHERE (entry = 126865);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `ManaModifier` = 285263 WHERE (entry = 126865);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 101334395 WHERE (entry = 126867);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 126867);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 110985290 WHERE (entry = 126869);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 126869);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 143053721 WHERE (entry = 126887);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 126887);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 121200074 WHERE (entry = 126896);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 126896);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 107629742 WHERE (entry = 126899);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 126899);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 108342113 WHERE (entry = 125497);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 125497);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 84977137, `ManaModifier` = 570526 WHERE (entry = 126900);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `ManaModifier` = 570526 WHERE (entry = 126900);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 99162239 WHERE (entry = 126910);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 126910);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 126129624 WHERE (entry = 126913);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 126913);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 116427345 WHERE (entry = 127288);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 127288);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 131419088 WHERE (entry = 127090);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 127090);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 105108020, `ManaModifier` = 285263 WHERE (entry = 127096);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `ManaModifier` = 285263 WHERE (entry = 127096);
 
 -- Table `creature_template`
-UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112, `HealthModifier` = 99162239 WHERE (entry = 126208);
+UPDATE `creature_template` SET `minlevel` = 112, `maxlevel` = 112 WHERE (entry = 126208);
