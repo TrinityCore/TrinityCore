e18ce5e33c8a6d22cb40d6b8527dae3483ca0c02
Golrag <golrag.jeremy@gmail.com>
2018-03-03 19:02:44 +0100

    Core/map: small fix to check if outdoors

--- src/server/game/Maps/Map.cpp
+++ src/server/game/Maps/Map.cpp
@@ -2696,7 +2696,6 @@ void Map::GetFullTerrainStatusForPosition(float x, float y, float z, PositionFul
     {
         data.areaId = areaEntry->ID;
         data.floorZ = vmapData.floorZ;
-        data.outdoors = IsOutdoorWMO(vmapData.areaInfo->mogpFlags, wmoEntry, areaEntry);
     }
     else
     {
@@ -2710,8 +2709,12 @@ void Map::GetFullTerrainStatusForPosition(float x, float y, float z, PositionFul
             areaEntry = sAreaTableStore.LookupEntry(data.areaId);
 
         data.floorZ = mapHeight;
-        data.outdoors = true; // @todo default true taken from old GetAreaId check, maybe review
     }
+
+    if (vmapData.areaInfo)
+        data.outdoors = IsOutdoorWMO(vmapData.areaInfo->mogpFlags, wmoEntry, areaEntry);
+    else
+        data.outdoors = true; // @todo default true taken from old GetAreaId check, maybe review
 
     // liquid processing
     data.liquidStatus = LIQUID_MAP_NO_WATER;
