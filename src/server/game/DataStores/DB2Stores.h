2ad4b86d28728226b61b23b5f06b166c50251970
Mihapro <Mihapro@users.noreply.github.com>
2018-03-04 12:48:22 +0100

    Core/Creatures: Updated power type handling (#20981)
    Closes #17476
    Closes #12487

--- src/server/game/DataStores/DB2Stores.h
+++ src/server/game/DataStores/DB2Stores.h
@@ -265,7 +265,7 @@ public:
     CharSectionsEntry const* GetCharSectionEntry(uint8 race, uint8 gender, CharBaseSectionVariation variation, uint8 variationIndex, uint8 color) const;
     CharStartOutfitEntry const* GetCharStartOutfitEntry(uint8 race, uint8 class_, uint8 gender) const;
     static char const* GetClassName(uint8 class_, LocaleConstant locale = DEFAULT_LOCALE);
-    uint32 GetPowerIndexByClass(uint32 powerType, uint32 classId) const;
+    uint32 GetPowerIndexByClass(Powers power, uint32 classId) const;
     static char const* GetChrRaceName(uint8 race, LocaleConstant locale = DEFAULT_LOCALE);
     ChrSpecializationEntry const* GetChrSpecializationByIndex(uint32 class_, uint32 index) const;
     ChrSpecializationEntry const* GetDefaultChrSpecializationForClass(uint32 class_) const;
