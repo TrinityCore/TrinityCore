68dde9f8c5467c5983078b190e58105f99dad75c
ariel- <ariel-@users.noreply.github.com>
2018-03-07 00:54:59 -0300

    Scripts/Command: build fix for horrible outdated boost version used by travis

--- src/server/scripts/Commands/cs_server.cpp
+++ src/server/scripts/Commands/cs_server.cpp
@@ -200,7 +200,7 @@ public:
         for (std::string const& subDir : subDirs)
         {
             boost::filesystem::path mapPath(dataDir);
-            mapPath.append(subDir);
+            mapPath /= subDir;
 
             if (!boost::filesystem::exists(mapPath))
             {
