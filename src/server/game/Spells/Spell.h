0c2a6dee0781be56bdee81e4d155431d7e7634f1
ariel- <ariel-@users.noreply.github.com>
2018-03-10 03:55:14 -0300

    Core/Spell: fix spell visual for other players
    - Ported SMSG_SPELL_GO and SMSG_SPELL_START to new packet system

--- src/server/game/Spells/Spell.h
+++ src/server/game/Spells/Spell.h
@@ -26,6 +26,16 @@
 #include "SharedDefines.h"
 #include <memory>
 
+namespace WorldPackets
+{
+    namespace Spells
+    {
+        struct SpellTargetData;
+        struct SpellAmmo;
+        struct SpellCastData;
+    }
+}
+
 class Aura;
 class AuraEffect;
 class Corpse;
@@ -125,7 +135,7 @@ class TC_GAME_API SpellCastTargets
         ~SpellCastTargets();
 
         void Read(ByteBuffer& data, Unit* caster);
-        void Write(ByteBuffer& data);
+        void Write(WorldPackets::Spells::SpellTargetData& data);
 
         uint32 GetTargetMask() const { return m_targetMask; }
         void SetTargetMask(uint32 newMask) { m_targetMask = newMask; }
@@ -449,8 +459,8 @@ class TC_GAME_API Spell
         void setState(uint32 state) { m_spellState = state; }
 
         void DoCreateItem(uint32 i, uint32 itemtype);
-        void WriteSpellGoTargets(WorldPacket* data);
-        void WriteAmmoToPacket(WorldPacket* data);
+        void UpdateSpellCastDataTargets(WorldPackets::Spells::SpellCastData& data);
+        void UpdateSpellCastDataAmmo(WorldPackets::Spells::SpellAmmo& data);
 
         bool CheckEffectTarget(Unit const* target, uint32 eff, Position const* losPosition) const;
         bool CanAutoCast(Unit* target);
