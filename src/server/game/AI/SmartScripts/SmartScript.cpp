19ce97eae4d08340f324babcc6464b2404868665
jackpoz <giacomopoz@gmail.com>
2018-03-11 21:01:53 +0100

    Core/SmartScripts: Code cleanup

--- src/server/game/AI/SmartScripts/SmartScript.cpp
+++ src/server/game/AI/SmartScripts/SmartScript.cpp
@@ -3431,14 +3431,12 @@ void SmartScript::ProcessEvent(SmartScriptHolder& e, Unit* unit, uint32 var0, ui
                 //if friendly event&&who is not hostile OR hostile event&&who is hostile
                 if ((e.event.los.noHostile && !me->IsHostileTo(unit)) ||
                     (!e.event.los.noHostile && me->IsHostileTo(unit)))
- 			    if (!me || !unit)
-                    return;
-                if (e.event.los.playerOnly && unit->GetTypeId() != TYPEID_PLAYER)
-				    return;
-                if (e.event.los.playerOnly == TYPEID_PLAYER)
-                    return;
-            RecalcTimer(e, e.event.los.cooldownMin, e.event.los.cooldownMax);
-            ProcessAction(e, unit);
+                {
+                    if (e.event.los.playerOnly && unit->GetTypeId() != TYPEID_PLAYER)
+                        return;
+                    RecalcTimer(e, e.event.los.cooldownMin, e.event.los.cooldownMax);
+                    ProcessAction(e, unit);
+                }
             }
             break;
         }
@@ -3455,14 +3453,12 @@ void SmartScript::ProcessEvent(SmartScriptHolder& e, Unit* unit, uint32 var0, ui
                 //if friendly event&&who is not hostile OR hostile event&&who is hostile
                 if ((e.event.los.noHostile && !me->IsHostileTo(unit)) ||
                     (!e.event.los.noHostile && me->IsHostileTo(unit)))
- 			    if (!me || !unit)
-                    return;
-                if (e.event.los.playerOnly && unit->GetTypeId() != TYPEID_PLAYER)
-				    return;
-                if (e.event.los.playerOnly == TYPEID_PLAYER)
-                    return;
-            RecalcTimer(e, e.event.los.cooldownMin, e.event.los.cooldownMax);
-            ProcessAction(e, unit);
+                {
+                    if (e.event.los.playerOnly && unit->GetTypeId() != TYPEID_PLAYER)
+                        return;
+                    RecalcTimer(e, e.event.los.cooldownMin, e.event.los.cooldownMax);
+                    ProcessAction(e, unit);
+                }
             }
             break;
         }
