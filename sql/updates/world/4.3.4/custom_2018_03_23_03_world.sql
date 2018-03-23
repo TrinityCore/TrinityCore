88956fe39e0cdcad0608c65a9c0b1a2a2f7e3650
Ovahlord <dreadkiller@gmx.de>
2018-03-23 16:36:34 +0100

    DB/Items: corrected cooldown timer

--- sql/updates/world/4.3.4/custom_2018_03_23_03_world.sql
+++ sql/updates/world/4.3.4/custom_2018_03_23_03_world.sql
@@ -1,5 +1,5 @@
 INSERT INTO `spell_proc` (`SpellId`, `ProcFlags`, `SpellTypeMask`, `SpellPhaseMask`, `HitMask`, `Cooldown`) VALUES
-(94746, 0x00054000, 0x00000001 | 0x00000002, 0x00000002, 0x00000000, 450000), -- Enchant Weapon - Power Torrent
-(94747, 0x00054400, 0x00000001 | 0x00000002, 0x00000002, 0x00000000, 450000), -- Enchant Weapon - Hurricane
-(95472, 0x00050000, 0x00000001, 0x00000002, 0x00000000, 450000), -- Enchant Weapon - Avalanche
-(95709, 0x00050000, 0x00000001, 0x00000002, 0x00000000, 450000); -- Enchant Weapon - Mending
+(94746, 0x00054000, 0x00000001 | 0x00000002, 0x00000002, 0x00000000, 45000), -- Enchant Weapon - Power Torrent
+(94747, 0x00054400, 0x00000001 | 0x00000002, 0x00000002, 0x00000000, 45000), -- Enchant Weapon - Hurricane
+(95472, 0x00050000, 0x00000001, 0x00000002, 0x00000000, 45000), -- Enchant Weapon - Avalanche
+(95709, 0x00050000, 0x00000001, 0x00000002, 0x00000000, 45000); -- Enchant Weapon - Mending
