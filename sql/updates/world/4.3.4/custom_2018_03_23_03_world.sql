b15c07102eeda2d39d410f38aa75074bdc100daf
Ovahlord <dreadkiller@gmx.de>
2018-03-23 16:40:59 +0100

    DB/Items: fixed Landslide and Windwalk enchantment procs

--- sql/updates/world/4.3.4/custom_2018_03_23_03_world.sql
+++ sql/updates/world/4.3.4/custom_2018_03_23_03_world.sql
@@ -3,3 +3,8 @@ INSERT INTO `spell_proc` (`SpellId`, `ProcFlags`, `SpellTypeMask`, `SpellPhaseMa
 (94747, 0x00054400, 0x00000001 | 0x00000002, 0x00000002, 0x00000000, 45000), -- Enchant Weapon - Hurricane
 (95472, 0x00050000, 0x00000001, 0x00000002, 0x00000000, 45000), -- Enchant Weapon - Avalanche
 (95709, 0x00050000, 0x00000001, 0x00000002, 0x00000000, 45000); -- Enchant Weapon - Mending
+
+DELETE FROM `spell_enchant_proc_data` WHERE `entry` IN (4098, 4099);
+INSERT INTO `spell_enchant_proc_data` (`entry`, `customChance`, `PPMChance`, `procEx`) VALUES
+(4098, 0, 1.25, 0), -- Enchant Weapon - Windwalk
+(4099, 0, 1.25, 0); -- Enchant Weapon - Landslide
