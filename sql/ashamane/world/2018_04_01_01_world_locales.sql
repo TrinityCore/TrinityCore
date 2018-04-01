a0d9daa10779b78dfc945128e48d2d7e35cf529c
Oldarorn <36448848+Oldarorn@users.noreply.github.com>
2018-04-01 17:24:22 +0200

    Add some FR translations for Northshire
    Quest reward text and some creature text

---
+++ sql/ashamane/world/2018_04_01_01_world_locales.sql
@@ -0,0 +1,58 @@
+-- ----------------------------
+-- Records of quest_offer_reward_locale
+-- ----------------------------
+INSERT INTO `quest_offer_reward_locale` VALUES ('28757', 'frFR', 'Vous nous avez fait gagner un peu de temps, $N, mais des problèmes encore plus importants nous attendent.', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28762', 'frFR', 'Vous nous avez fait gagner un peu de temps, $N, mais des problèmes encore plus importants nous attendent.', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28763', 'frFR', 'Vous nous avez fait gagner un peu de temps, $N, mais des problèmes encore plus importants nous attendent.', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28764', 'frFR', 'Vous nous avez fait gagner un peu de temps, $N, mais des problèmes encore plus importants nous attendent.', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28765', 'frFR', 'Vous nous avez fait gagner un peu de temps, $N, mais des problèmes encore plus importants nous attendent.', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28766', 'frFR', 'Vous nous avez fait gagner un peu de temps, $N, mais des problèmes encore plus importants nous attendent.', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28767', 'frFR', 'Vous nous avez fait gagner un peu de temps, $N, mais des problèmes encore plus importants nous attendent.', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28780', 'frFR', 'C’est vrai, on nous a tendu une embuscade. Je n’ose plus envoyer de soldats là-bas et risquer de les perdre aussi. Il me faut un volontaire qui serait prêt à risquer sa vie !', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28784', 'frFR', 'C’est vrai, on nous a tendu une embuscade. Je n’ose plus envoyer de soldats là-bas et risquer de les perdre aussi. Il me faut un volontaire qui serait prêt à risquer sa vie !', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28785', 'frFR', 'C’est vrai, on nous a tendu une embuscade. Je n’ose plus envoyer de soldats là-bas et risquer de les perdre aussi. Il me faut un volontaire qui serait prêt à risquer sa vie !', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28786', 'frFR', 'C’est vrai, on nous a tendu une embuscade. Je n’ose plus envoyer de soldats là-bas et risquer de les perdre aussi. Il me faut un volontaire qui serait prêt à risquer sa vie !', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28787', 'frFR', 'C’est vrai, on nous a tendu une embuscade. Je n’ose plus envoyer de soldats là-bas et risquer de les perdre aussi. Il me faut un volontaire qui serait prêt à risquer sa vie !', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28788', 'frFR', 'C’est vrai, on nous a tendu une embuscade. Je n’ose plus envoyer de soldats là-bas et risquer de les perdre aussi. Il me faut un volontaire qui serait prêt à risquer sa vie !', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28789', 'frFR', 'C’est vrai, on nous a tendu une embuscade. Je n’ose plus envoyer de soldats là-bas et risquer de les perdre aussi. Il me faut un volontaire qui serait prêt à risquer sa vie !', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28791', 'frFR', 'Ça leur apprendra, à ces monstres ! Ils y réfléchiront à deux fois avant d’accepter une autre mission de mercenaire pour les orcs.', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28792', 'frFR', 'Ça leur apprendra, à ces monstres ! Ils y réfléchiront à deux fois avant d’accepter une autre mission de mercenaire pour les orcs.', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28793', 'frFR', 'Ça leur apprendra, à ces monstres ! Ils y réfléchiront à deux fois avant d’accepter une autre mission de mercenaire pour les orcs.', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28794', 'frFR', 'Ça leur apprendra, à ces monstres ! Ils y réfléchiront à deux fois avant d’accepter une autre mission de mercenaire pour les orcs.', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28795', 'frFR', 'Ça leur apprendra, à ces monstres ! Ils y réfléchiront à deux fois avant d’accepter une autre mission de mercenaire pour les orcs.', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28796', 'frFR', 'Ça leur apprendra, à ces monstres ! Ils y réfléchiront à deux fois avant d’accepter une autre mission de mercenaire pour les orcs.', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28797', 'frFR', 'Ça leur apprendra, à ces monstres ! Ils y réfléchiront à deux fois avant d’accepter une autre mission de mercenaire pour les orcs.', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('29078', 'frFR', 'Vous nous avez fait gagner un peu de temps, $N, mais des problèmes encore plus importants nous attendent.', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('29080', 'frFR', 'C’est vrai, on nous a tendu une embuscade. Je n’ose plus envoyer de soldats là-bas et risquer de les perdre aussi. Il me faut un volontaire qui serait prêt à risquer sa vie !', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('29081', 'frFR', 'Ça leur apprendra, à ces monstres ! Ils y réfléchiront à deux fois avant d’accepter une autre mission de mercenaire pour les orcs.', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('31139', 'frFR', 'Vous nous avez fait gagner un peu de temps, $N, mais des problèmes encore plus importants nous attendent.', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('31143', 'frFR', 'C’est vrai, on nous a tendu une embuscade. Je n’ose plus envoyer de soldats là-bas et risquer de les perdre aussi. Il me faut un volontaire qui serait prêt à risquer sa vie !', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('31144', 'frFR', 'Ça leur apprendra, à ces monstres ! Ils y réfléchiront à deux fois avant d’accepter une autre mission de mercenaire pour les orcs.', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28813', 'frFR', 'Je pense que désormais, vous comprenez le pouvoir de la Lumière. La Lumière donne l’espoir, mon frère, la Lumière combat les ténèbres ! LOUÉE SOIT LA LUMIÈRE !', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28812', 'frFR', 'Je pense que désormais, vous comprenez le pouvoir de la Lumière. La Lumière donne l’espoir, mon frère, la Lumière combat les ténèbres ! LOUÉE SOIT LA LUMIÈRE !', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28806', 'frFR', 'Je pense que désormais, vous comprenez le pouvoir de la Lumière. La Lumière donne l’espoir, mon frère, la Lumière combat les ténèbres ! LOUÉE SOIT LA LUMIÈRE !', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('29082', 'frFR', 'Je pense que désormais, vous comprenez le pouvoir de la Lumière. La Lumière donne l’espoir, mon frère, la Lumière combat les ténèbres ! LOUÉE SOIT LA LUMIÈRE !', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28811', 'frFR', 'Je pense que désormais, vous comprenez le pouvoir de la Lumière. La Lumière donne l’espoir, mon frère, la Lumière combat les ténèbres ! LOUÉE SOIT LA LUMIÈRE !', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28808', 'frFR', 'Je pense que désormais, vous comprenez le pouvoir de la Lumière. La Lumière donne l’espoir, mon frère, la Lumière combat les ténèbres ! LOUÉE SOIT LA LUMIÈRE !', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28810', 'frFR', 'Je pense que désormais, vous comprenez le pouvoir de la Lumière. La Lumière donne l’espoir, mon frère, la Lumière combat les ténèbres ! LOUÉE SOIT LA LUMIÈRE !', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28819', 'frFR', 'Je pense que désormais, vous comprenez le pouvoir de la Lumière. La Lumière donne l’espoir, mon frère, la Lumière combat les ténèbres ! LOUÉE SOIT LA LUMIÈRE !', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28822', 'frFR', 'Grâce à vous, nous avons réussi à sécuriser les secteurs nord et ouest de Comté-du-Nord. Il reste encore un grand nombre d’orcs rochenoires à l’est et ils ont commencé à incendier la forêt !', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28817', 'frFR', 'Grâce à vous, nous avons réussi à sécuriser les secteurs nord et ouest de Comté-du-Nord. Il reste encore un grand nombre d’orcs rochenoires à l’est et ils ont commencé à incendier la forêt !', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28818', 'frFR', 'Grâce à vous, nous avons réussi à sécuriser les secteurs nord et ouest de Comté-du-Nord. Il reste encore un grand nombre d’orcs rochenoires à l’est et ils ont commencé à incendier la forêt !', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28820', 'frFR', 'Grâce à vous, nous avons réussi à sécuriser les secteurs nord et ouest de Comté-du-Nord. Il reste encore un grand nombre d’orcs rochenoires à l’est et ils ont commencé à incendier la forêt !', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28823', 'frFR', 'Grâce à vous, nous avons réussi à sécuriser les secteurs nord et ouest de Comté-du-Nord. Il reste encore un grand nombre d’orcs rochenoires à l’est et ils ont commencé à incendier la forêt !', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('29083', 'frFR', 'Grâce à vous, nous avons réussi à sécuriser les secteurs nord et ouest de Comté-du-Nord. Il reste encore un grand nombre d’orcs rochenoires à l’est et ils ont commencé à incendier la forêt !', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('28821', 'frFR', 'Grâce à vous, nous avons réussi à sécuriser les secteurs nord et ouest de Comté-du-Nord. Il reste encore un grand nombre d’orcs rochenoires à l’est et ils ont commencé à incendier la forêt !', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('31145', 'frFR', 'Grâce à vous, nous avons réussi à sécuriser les secteurs nord et ouest de Comté-du-Nord. Il reste encore un grand nombre d’orcs rochenoires à l’est et ils ont commencé à incendier la forêt !', '0');
+INSERT INTO `quest_offer_reward_locale` VALUES ('26391', 'frFR', 'Soyez béni $N. Vous êtes un vrai héros de l’Alliance ! Bon, tout a brûlé et il ne reste rien de mes vignes, à part des cendres et des débris, mais je m’en remettrai. Pas vrai ? Après tout, qu’est-ce qui pourrait m’arriver de pire ?', '0');
+
+-- ----------------------------
+-- Records of creature_text_locale
+-- ----------------------------
+INSERT INTO `creature_text_locale` VALUES ('50039', '0', '0', 'frFR', 'On peut tuer n\'importe qui si le prix est correct !');
+INSERT INTO `creature_text_locale` VALUES ('50039', '0', '1', 'frFR', 'On va tout brûler ici !');
+INSERT INTO `creature_text_locale` VALUES ('42937', '0', '0', 'frFR', 'Orc TUER $r !');
+INSERT INTO `creature_text_locale` VALUES ('42937', '0', '1', 'frFR', 'Rochenoire prend forêt !');
+INSERT INTO `creature_text_locale` VALUES ('42937', '0', '2', 'frFR', 'Les raisins étaient DÉLICIEUX !');
+INSERT INTO `creature_text_locale` VALUES ('42937', '0', '3', 'frFR', 'Manger toi !');
+INSERT INTO `creature_text_locale` VALUES ('42937', '0', '4', 'frFR', 'Suppliez pour rester en vie !');
