74defba5a7e3e0619ca05808936ebd74741e1472
Treeston <treeston.mmoc@gmail.com>
2018-04-07 17:23:26 +0200

    Core/Movement: Fix a potential crash on newly created pets. Closes #21791.

--- src/server/scripts/Spells/spell_paladin.cpp
+++ src/server/scripts/Spells/spell_paladin.cpp
@@ -1830,7 +1830,7 @@ class spell_pal_righteous_defense : public SpellScript
         return SPELL_CAST_OK;
     }
 
-    void HandleDummy(SpellEffIndex effIndex)
+    void HandleDummy(SpellEffIndex /*effIndex*/)
     {
         if (Unit* target = GetHitUnit())
         {
