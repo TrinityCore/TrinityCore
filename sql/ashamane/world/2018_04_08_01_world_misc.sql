a880c748c0371d3eb38cb75d5ee1b10414879462
Oldarorn <36448848+Oldarorn@users.noreply.github.com>
2018-04-09 21:10:24 +0200

    I add DELETE because Neredun is asking for it

--- sql/ashamane/world/2018_04_08_01_world_misc.sql
+++ sql/ashamane/world/2018_04_08_01_world_misc.sql
@@ -1,5 +1,6 @@
 -- Table `creature`
-
+DELETE FROM `creature` WHERE guid = '210112048';
+DELETE FROM `creature` WHERE guid = '210112049';
 INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseUseFlags`, `PhaseId`, `PhaseGroup`, `terrainSwapMap`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `unit_flags2`, `unit_flags3`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
 (210112048, 87501, 0, 0, 0, 1, 0, 0, 0, -1, 0, 1, -8685.39, 454.683, 100.619, 5.82418, 300, 0, 0, 3117801, 0, 0, 0, 0, 0, 0, 0, '', 0);
 INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseUseFlags`, `PhaseId`, `PhaseGroup`, `terrainSwapMap`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `unit_flags2`, `unit_flags3`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
