b0a1cfb38492bff186e576c298949f4b8551139c
Shauren <shauren.trinity@gmail.com>
2018-04-17 17:10:58 +0200

    Core/Gossip: Implemented new gossip option type GOSSIP_OPTION_TRANSMOGRIFIER
    Ref #21845

--- src/server/game/Entities/Creature/GossipDef.h
+++ src/server/game/Entities/Creature/GossipDef.h
@@ -52,6 +52,7 @@ enum Gossip_Option
     GOSSIP_OPTION_UNLEARNPETTALENTS_OLD = 17,               // deprecated
     GOSSIP_OPTION_LEARNDUALSPEC     = 18,                   //UNIT_NPC_FLAG_TRAINER             (16) (bonus option for GOSSIP_OPTION_TRAINER)
     GOSSIP_OPTION_OUTDOORPVP        = 19,                   //added by code (option for outdoor pvp creatures)
+    GOSSIP_OPTION_TRANSMOGRIFIER    = 20,                   //UNIT_NPC_FLAG_TRANSMOGRIFIER
     GOSSIP_OPTION_MAX
 };
 
