43547e858da3499886908140894a6d698167b598
Nitair <nitair@users.noreply.github.com>
2018-04-22 21:57:10 +0200

    Added missing emotes, added unholy aura (but it does not apply), added known text for baron run

--- sql/updates/world/3.3.5/2018_99_99_99_world_335.sql
+++ sql/updates/world/3.3.5/2018_99_99_99_world_335.sql
@@ -1,17 +1,22 @@
 -- Stratholme - Baron Rivendare - Set position for each Raise Dead spell (1-6)
 DELETE FROM `spell_target_position` WHERE `ID` BETWEEN 17475 AND 17480;
 INSERT INTO `spell_target_position` (`ID`, `EffectIndex`, `MapID`, `PositionX`, `PositionY`, `PositionZ`, `Orientation`, `VerifiedBuild`) VALUES 
-(17475, 0, 0, 4017.403809, -3339.703369, 115.057655, 5.487860, 0),
-(17476, 0, 0, 4013.189209, -3351.808350, 115.052254, 0.134280, 0),
-(17477, 0, 0, 4017.738037, -3363.478016, 115.057274, 0.723313, 0),
-(17478, 0, 0, 4048.877197, -3363.223633, 115.054253, 3.627735, 0),
-(17479, 0, 0, 4051.777588, -3350.893311, 115.055351, 3.066176, 0),
-(17480, 0, 0, 4048.375977, -3339.966309, 115.055222, 2.457497, 0);
+(17475, 0, 329, 4017.403809, -3339.703369, 115.057655, 5.487860, 0),
+(17476, 0, 329, 4013.189209, -3351.808350, 115.052254, 0.134280, 0),
+(17477, 0, 329, 4017.738037, -3363.478016, 115.057274, 0.723313, 0),
+(17478, 0, 329, 4048.877197, -3363.223633, 115.054253, 3.627735, 0),
+(17479, 0, 329, 4051.777588, -3350.893311, 115.055351, 3.066176, 0),
+(17480, 0, 329, 4048.375977, -3339.966309, 115.055222, 2.457497, 0);
 
 -- Stratholme - Baron Rivendare - Kill Skeleton (if alive) and Heal himself
 DELETE FROM `conditions` WHERE `SourceEntry` = 17471;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `Comment`) VALUES 
-(13, 0, 17471, 0, 0, 29, 1, 11197, 0, 1, 0, 0, 0, 'Stratholme - Baron Rivendare - Kill Skeleton (if alive) and Heal himself');
+(13, 1, 17471, 0, 0, 29, 1, 11197, 50, 1, 0, 0, 0, 'Stratholme - Baron Rivendare - Kill Skeleton (if alive) and Heal himself');
+
+-- Stratholme - Baron Rivendare - Adding Unholy Aura to Baron Rivendare
+DELETE FROM `creature_template_addon` WHERE `entry` = 10440;
+INSERT INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES
+(10440, 0, 0, 0, 0, 0, 17467);
 
 -- Stratholme - Baron Rivendare - Unholy Aura does not trigger combat (Aura and Trigger of Aura)
 SET @SPELL_ATTR0_CU_NO_INITIAL_THREAT = 16;
@@ -19,3 +24,9 @@ DELETE FROM `spell_custom_attr` WHERE `entry` IN (17466, 17467);
 INSERT INTO `spell_custom_attr` (`entry`, `attributes`) VALUES
 (17466, @SPELL_ATTR0_CU_NO_INITIAL_THREAT),
 (17467, @SPELL_ATTR0_CU_NO_INITIAL_THREAT);
+
+-- Stratholme - Baron Rivendare - Add missing emotes
+DELETE FROM `creature_text` WHERE `CreatureID` IN (10440);
+INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES 
+(10440, 0, 0, '%s raises an undead servant back to life!', 14, 0, 0, 0, 0, 0, 0, 0, 'Baron Rivendare - EMOTE_RAISE_DEAD'),
+(10440, 1, 0, '%s attempts to cast Death Pact on his servants!', 14, 0, 0, 0, 0, 0, 0, 0, 'Baron Rivendare - EMOTE_DEATH_PACT');
--- src/server/scripts/EasternKingdoms/Stratholme/boss_baron_rivendare.cpp
+++ src/server/scripts/EasternKingdoms/Stratholme/boss_baron_rivendare.cpp
@@ -1,32 +1,38 @@
 /*
- * Copyright (C) 2008-2018 TrinityCore <https://www.trinitycore.org/>
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
- */
+* Copyright (C) 2008-2018 TrinityCore <https://www.trinitycore.org/>
+*
+* This program is free software; you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the
+* Free Software Foundation; either version 2 of the License, or (at your
+* option) any later version.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+* more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "InstanceScript.h"
 #include "ScriptedCreature.h"
 #include "ScriptMgr.h"
 #include "stratholme.h"
 
-enum Says
+enum Texts
 {
-    SAY_BARON_RUN_START         = 0,
-    SAY_BARON_RUN_BOSS_KILL     = 1,
-    SAY_BARON_RUN_FAIL          = 2,
-    SAY_EVENT_RAMSTEIN          = 3,
-    SAY_EVENT_BARON             = 4
+    EMOTE_RAISE_DEAD            = 0,    // %s raises an undead servant back to life!
+    EMOTE_DEATH_PACT            = 1     // %s attempts to cast Death Pact on his servants!     
+};
+
+enum BaronRun
+{
+    YELL_BARON_RUN_START        = 0,    // Intruders! More pawns of the Argent Dawn, no doubt. I already count one of their number among my prisoners. Withdraw from my domain before she is executed!
+    YELL_BARON_RUN_BOSS_KILL    = 1,    // The Ash'ari Crystals have been destroyed! The Slaughterhouse is vulnerable!
+    YELL_BARON_RUN_FAIL         = 2,    // Unknown
+    YELL_EVENT_RAMSTEIN         = 3,    // So you see fit to toy with the Lich King's creations? Ramstein, be sure to give the intruders a proper greeting.
+    YELL_EVENT_BARON            = 4,    // Time to take matters into my own hands. Come. Enter my domain and challenge the might of the Scourge!
 };
 
 enum Spells
@@ -34,8 +40,9 @@ enum Spells
     SPELL_SHADOWBOLT            = 17393,
     SPELL_CLEAVE                = 15284,
     SPELL_MORTALSTRIKE          = 15708,
-    SPELL_UNHOLY_AURA           = 17467,    // Unholy Aura is handled via DB
-    SPELL_RAISEDEAD             = 17473,    // Triggers death pact (17471)
+    SPELL_DEATH_PACT            = 17471,
+    SPELL_RAISE_DEAD            = 17473,
+    SPELL_UNHOLY_AURA           = 17467,
     SPELL_RAISE_DEAD1           = 17475,
     SPELL_RAISE_DEAD2           = 17476,
     SPELL_RAISE_DEAD3           = 17477,
@@ -49,36 +56,38 @@ enum BaronRivendareEvents
     EVENT_SHADOWBOLT            = 1,
     EVENT_CLEAVE                = 2,
     EVENT_MORTALSTRIKE          = 3,
-    EVENT_RAISEDEAD             = 4
+    EVENT_RAISE_DEAD            = 4
 };
 
 struct boss_baron_rivendare : public BossAI
 {
-    boss_baron_rivendare(Creature* creature) : BossAI(creature, TYPE_BARON), _raiseDead(false) { }
+    boss_baron_rivendare(Creature* creature) : BossAI(creature, TYPE_BARON), RaiseDead(false) { }
 
     void Reset() override
     {
         if (instance->GetData(TYPE_RAMSTEIN) == DONE)
             instance->SetData(TYPE_BARON, NOT_STARTED);
 
-        _raiseDead = false;
-
-        events.Reset();
-        scheduler.CancelAll();
+        BossAI::Reset();
     }
 
     void JustEngagedWith(Unit* who) override
     {
+        if (instance->GetData(TYPE_BARON) == NOT_STARTED)
+            instance->SetData(TYPE_BARON, IN_PROGRESS);
+
         events.ScheduleEvent(EVENT_SHADOWBOLT, 5s);
         events.ScheduleEvent(EVENT_CLEAVE, 8s);
         events.ScheduleEvent(EVENT_MORTALSTRIKE, 12s);
-        events.ScheduleEvent(EVENT_RAISEDEAD, 15s);
+        events.ScheduleEvent(EVENT_RAISE_DEAD, 15s);
 
         BossAI::JustEngagedWith(who);
     }
 
     void JustDied(Unit* killer) override
     {
+        instance->SetData(TYPE_BARON, DONE);
+
         BossAI::JustDied(killer);
     }
 
@@ -96,35 +105,43 @@ struct boss_baron_rivendare : public BossAI
         {
             switch (eventId)
             {
-                case EVENT_SHADOWBOLT:
-                    if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0))
-                        DoCast(target, SPELL_SHADOWBOLT);
+            case EVENT_SHADOWBOLT:
+                if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0))
+                    DoCast(target, SPELL_SHADOWBOLT);
 
-                    events.Repeat(10s);
-                    break;
-                case EVENT_CLEAVE:
-                    DoCastVictim(SPELL_CLEAVE);
-                    events.Repeat(7s, 17s);
-                    break;
-                case EVENT_MORTALSTRIKE:
-                    DoCastVictim(SPELL_MORTALSTRIKE);
-                    events.Repeat(10s, 25s);
-                    break;
-                case EVENT_RAISEDEAD:
-                    // After 12 seconds, if the servant still lives,
-                    // it will sacrifice itself to heal the caster
-                    if (!_raiseDead)
-                    {
-                        DoCast(SPELL_RAISEDEAD);
-                        _raiseDead = true;
-                        events.Repeat(1s);
-                    }
-                    else
-                    {
-                        _raiseDead = false;
-                        events.Repeat(12s);
-                    }
-                    break;
+                events.Repeat(10s);
+                break;
+            case EVENT_CLEAVE:
+                DoCastVictim(SPELL_CLEAVE);
+                events.Repeat(7s, 17s);
+                break;
+            case EVENT_MORTALSTRIKE:
+                DoCastVictim(SPELL_MORTALSTRIKE);
+                events.Repeat(10s, 25s);
+                break;
+            case EVENT_RAISE_DEAD:
+                if (!RaiseDead)
+                {
+                    DoCast(SPELL_RAISE_DEAD);
+
+                    // Just a test, looks like it should - but not the best way to handle like this
+                    DoCastSelf(SPELL_RAISE_DEAD1, true);
+                    DoCastSelf(SPELL_RAISE_DEAD2, true);
+                    DoCastSelf(SPELL_RAISE_DEAD3, true);
+                    DoCastSelf(SPELL_RAISE_DEAD4, true);
+                    DoCastSelf(SPELL_RAISE_DEAD5, true);
+                    DoCastSelf(SPELL_RAISE_DEAD6, true);
+                    RaiseDead = true;
+                    Talk(EMOTE_RAISE_DEAD);
+                }
+                else
+                {
+                    DoCast(SPELL_DEATH_PACT);
+                    RaiseDead = false;
+                    Talk(EMOTE_DEATH_PACT);
+                }
+                events.Repeat(12s, 15s);
+                break;
             }
 
             if (me->HasUnitState(UNIT_STATE_CASTING))
@@ -135,7 +152,7 @@ struct boss_baron_rivendare : public BossAI
     }
 
 private:
-    bool _raiseDead;
+    bool RaiseDead;
 };
 
 void AddSC_boss_baron_rivendare()
