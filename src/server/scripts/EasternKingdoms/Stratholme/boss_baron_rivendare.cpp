6c9fe1c549d987a6b3226c4d86138e6cf41ab0bf
Nitair <nitair@users.noreply.github.com>
2018-04-22 22:06:52 +0200

    Readd changes probably

--- src/server/scripts/EasternKingdoms/Stratholme/boss_baron_rivendare.cpp
+++ src/server/scripts/EasternKingdoms/Stratholme/boss_baron_rivendare.cpp
@@ -20,65 +20,74 @@
 #include "ScriptMgr.h"
 #include "stratholme.h"
 
-enum Says
+enum Texts
 {
-    SAY_BARON_RUN_START         = 0,
-    SAY_BARON_RUN_BOSS_KILL     = 1,
-    SAY_BARON_RUN_FAIL          = 2,
-    SAY_EVENT_RAMSTEIN          = 3,
-    SAY_EVENT_BARON             = 4
+    EMOTE_RAISE_DEAD = 0,    // %s raises an undead servant back to life!
+    EMOTE_DEATH_PACT = 1     // %s attempts to cast Death Pact on his servants!     
+};
+
+enum BaronRun
+{
+    YELL_BARON_RUN_START = 0,    // Intruders! More pawns of the Argent Dawn, no doubt. I already count one of their number among my prisoners. Withdraw from my domain before she is executed!
+    YELL_BARON_RUN_BOSS_KILL = 1,    // The Ash'ari Crystals have been destroyed! The Slaughterhouse is vulnerable!
+    YELL_BARON_RUN_FAIL = 2,    // Unknown
+    YELL_EVENT_RAMSTEIN = 3,    // So you see fit to toy with the Lich King's creations? Ramstein, be sure to give the intruders a proper greeting.
+    YELL_EVENT_BARON = 4,    // Time to take matters into my own hands. Come. Enter my domain and challenge the might of the Scourge!
 };
 
 enum Spells
 {
-    SPELL_SHADOWBOLT            = 17393,
-    SPELL_CLEAVE                = 15284,
-    SPELL_MORTALSTRIKE          = 15708,
-    SPELL_UNHOLY_AURA           = 17467,    // Unholy Aura is handled via DB
-    SPELL_RAISEDEAD             = 17473,    // Triggers death pact (17471)
-    SPELL_RAISE_DEAD1           = 17475,
-    SPELL_RAISE_DEAD2           = 17476,
-    SPELL_RAISE_DEAD3           = 17477,
-    SPELL_RAISE_DEAD4           = 17478,
-    SPELL_RAISE_DEAD5           = 17479,
-    SPELL_RAISE_DEAD6           = 17480
+    SPELL_SHADOWBOLT = 17393,
+    SPELL_CLEAVE = 15284,
+    SPELL_MORTALSTRIKE = 15708,
+    SPELL_DEATH_PACT = 17471,
+    SPELL_RAISE_DEAD = 17473,
+    SPELL_UNHOLY_AURA = 17467,
+    SPELL_RAISE_DEAD1 = 17475,
+    SPELL_RAISE_DEAD2 = 17476,
+    SPELL_RAISE_DEAD3 = 17477,
+    SPELL_RAISE_DEAD4 = 17478,
+    SPELL_RAISE_DEAD5 = 17479,
+    SPELL_RAISE_DEAD6 = 17480
 };
 
 enum BaronRivendareEvents
 {
-    EVENT_SHADOWBOLT            = 1,
-    EVENT_CLEAVE                = 2,
-    EVENT_MORTALSTRIKE          = 3,
-    EVENT_RAISEDEAD             = 4
+    EVENT_SHADOWBOLT = 1,
+    EVENT_CLEAVE = 2,
+    EVENT_MORTALSTRIKE = 3,
+    EVENT_RAISE_DEAD = 4
 };
 
 struct boss_baron_rivendare : public BossAI
 {
-    boss_baron_rivendare(Creature* creature) : BossAI(creature, TYPE_BARON), _raiseDead(false) { }
+    boss_baron_rivendare(Creature* creature) : BossAI(creature, TYPE_BARON), RaiseDead(false) { }
 
     void Reset() override
     {
         if (instance->GetData(TYPE_RAMSTEIN) == DONE)
             instance->SetData(TYPE_BARON, NOT_STARTED);
 
-        _raiseDead = false;
-
-        events.Reset();
-        scheduler.CancelAll();
+        BossAI::Reset();
     }
 
     void JustEngagedWith(Unit* who) override
     {
+        if (instance->GetData(TYPE_BARON) == NOT_STARTED)
+            instance->SetData(TYPE_BARON, IN_PROGRESS);
+
         events.ScheduleEvent(EVENT_SHADOWBOLT, 5s);
         events.ScheduleEvent(EVENT_CLEAVE, 8s);
         events.ScheduleEvent(EVENT_MORTALSTRIKE, 12s);
-        events.ScheduleEvent(EVENT_RAISEDEAD, 15s);
+        events.ScheduleEvent(EVENT_RAISE_DEAD, 15s);
 
         BossAI::JustEngagedWith(who);
     }
 
     void JustDied(Unit* killer) override
     {
+        instance->SetData(TYPE_BARON, DONE);
+
         BossAI::JustDied(killer);
     }
 
@@ -96,35 +105,41 @@ struct boss_baron_rivendare : public BossAI
         {
             switch (eventId)
             {
-                case EVENT_SHADOWBOLT:
-                    if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0))
-                        DoCast(target, SPELL_SHADOWBOLT);
+            case EVENT_SHADOWBOLT:
+                if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0))
+                    DoCast(target, SPELL_SHADOWBOLT);
 
-                    events.Repeat(10s);
-                    break;
-                case EVENT_CLEAVE:
-                    DoCastVictim(SPELL_CLEAVE);
-                    events.Repeat(7s, 17s);
-                    break;
-                case EVENT_MORTALSTRIKE:
-                    DoCastVictim(SPELL_MORTALSTRIKE);
-                    events.Repeat(10s, 25s);
-                    break;
-                case EVENT_RAISEDEAD:
-                    // After 12 seconds, if the servant still lives,
-                    // it will sacrifice itself to heal the caster
-                    if (!_raiseDead)
-                    {
-                        DoCast(SPELL_RAISEDEAD);
-                        _raiseDead = true;
-                        events.Repeat(1s);
-                    }
-                    else
-                    {
-                        _raiseDead = false;
-                        events.Repeat(12s);
-                    }
-                    break;
+                events.Repeat(10s);
+                break;
+            case EVENT_CLEAVE:
+                DoCastVictim(SPELL_CLEAVE);
+                events.Repeat(7s, 17s);
+                break;
+            case EVENT_MORTALSTRIKE:
+                DoCastVictim(SPELL_MORTALSTRIKE);
+                events.Repeat(10s, 25s);
+                break;
+            case EVENT_RAISE_DEAD:
+                if (!RaiseDead)
+                {
+                    DoCast(SPELL_RAISE_DEAD);
+                    DoCastSelf(SPELL_RAISE_DEAD1, true);
+                    DoCastSelf(SPELL_RAISE_DEAD2, true);
+                    DoCastSelf(SPELL_RAISE_DEAD3, true);
+                    DoCastSelf(SPELL_RAISE_DEAD4, true);
+                    DoCastSelf(SPELL_RAISE_DEAD5, true);
+                    DoCastSelf(SPELL_RAISE_DEAD6, true);
+                    RaiseDead = true;
+                    Talk(EMOTE_RAISE_DEAD);
+                }
+                else
+                {
+                    DoCast(SPELL_DEATH_PACT);
+                    RaiseDead = false;
+                    Talk(EMOTE_DEATH_PACT);
+                }
+                events.Repeat(12s, 15s);
+                break;
             }
 
             if (me->HasUnitState(UNIT_STATE_CASTING))
@@ -135,7 +150,7 @@ struct boss_baron_rivendare : public BossAI
     }
 
 private:
-    bool _raiseDead;
+    bool RaiseDead;
 };
 
 void AddSC_boss_baron_rivendare()
