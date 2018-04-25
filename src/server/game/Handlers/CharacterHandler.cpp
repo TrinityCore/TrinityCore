1c19aa7b71ffc6782c94a64ce4aa3d178994e6a7
Keader <keader.android@gmail.com>
2018-04-24 21:18:07 -0300

    Core/Handlers: Fixed equipament manager with empty slots. Yes kids, dont use drugs !
    Followup: 42be04898c6fd001160ee4fdf00fc2ba5c5c1ee3

--- src/server/game/Handlers/CharacterHandler.cpp
+++ src/server/game/Handlers/CharacterHandler.cpp
@@ -1707,7 +1707,7 @@ void WorldSession::HandleEquipmentSetUse(WorldPacket& recvData)
             InventoryResult msg = _player->CanStoreItem(NULL_BAG, NULL_SLOT, sDest, uItem, false);
             if (msg == EQUIP_ERR_OK)
             {
-                if (_player->CanEquipItem(NULL_SLOT, dstpos, uItem, false) != EQUIP_ERR_OK)
+                if (_player->CanUnequipItem(dstpos, true) != EQUIP_ERR_OK)
                     continue;
 
                 _player->RemoveItem(INVENTORY_SLOT_BAG_0, i, true);
@@ -1722,7 +1722,7 @@ void WorldSession::HandleEquipmentSetUse(WorldPacket& recvData)
         if (item->GetPos() == dstpos)
             continue;
 
-        if (_player->CanUnequipItem(dstpos, true) != EQUIP_ERR_OK)
+        if (_player->CanEquipItem(NULL_SLOT, dstpos, item, false) != EQUIP_ERR_OK)
             continue;
 
         _player->SwapItem(item->GetPos(), dstpos);
