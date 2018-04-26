af63bda147431c1f7b9076fad770329b30d29096
Ovahlord <dreadkiller@gmx.de>
2018-04-26 16:17:00 +0200

    DB/Creatures: fixed some startup errors

---
+++ sql/updates/world/custom/custom_2018_04_26_00_world.sql
@@ -0,0 +1,5 @@
+﻿UPDATE `creature_template` SET `mechanic_immune_mask`= 8388624 WHERE `entry`=48844;
+UPDATE `creature_template` SET `unit_flags2`= 34816 WHERE `entry` IN (46227, 46228, 46229);
+UPDATE `creature_template` SET `unit_class`= 1 WHERE `entry`=48902;
+UPDATE `creature_template` SET `unit_flags2`= 0 WHERE `entry` IN (46224, 46225, 46226);
+UPDATE `creature_template` SET `faction`=17 WHERE `entry` IN (49851, 49855, 49853);
