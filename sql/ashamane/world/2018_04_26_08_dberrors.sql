a573fe7eeb72ad5997df69090d67e33155b1f2d5
HannibalRoG <edreisantafe@gmail.com>
2018-04-26 20:32:30 -0400

    Fix sql

--- sql/ashamane/world/2018_04_26_08_dberrors.sql
+++ sql/ashamane/world/2018_04_26_08_dberrors.sql
@@ -2,7 +2,7 @@
 -- Table `creature_template`
 UPDATE `creature_template` SET `npcflag` = 4305 WHERE (entry = 4888);
 UPDATE `creature_template` SET `npcflag` = 4307 WHERE (entry = 16583);
-UPDATE `creature_template` SET `npcflag` = 4306 WHERE (entry = 16823);
+UPDATE `creature_template` SET `npcflag` = 4307 WHERE (entry = 16823);
 UPDATE `creature_template` SET `npcflag` = 82 WHERE (entry = 56227);
 UPDATE `creature_template` SET `npcflag` = 81 WHERE (entry = 19341);
 UPDATE `creature_template` SET `npcflag` = 4305 WHERE (entry = 57620);
@@ -25,3 +25,5 @@ UPDATE `creature_template` SET `npcflag` = 18 WHERE (entry = 48513);
 UPDATE `creature_template` SET `npcflag` = 17 WHERE (entry = 96786);
 UPDATE `creature_template` SET `npcflag` = 17 WHERE (entry = 97529);
 DELETE FROM npc_trainer WHERE ID IN (20214,19341,98118);
+INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`, `Index`) VALUES ('19341', '-201004', '0', '0', '0', '0', '0');
+
