4bfe4b6163522a5fe2c14d89917953a8f3a2696d
Sefrenia <36268900+Sefrenia-Caecias@users.noreply.github.com>
2018-05-07 20:51:21 -0300

    DBErros Log Fixes - spell_script_names
    Fix of the error produced in https://github.com/AshamaneProject/AshamaneCore/commit/2430354343389001028d7a4bf89dc1ce922714c1.

---
+++ sql/ashamane/world/2018_05_07_14_world_spell_script_names.sql
@@ -0,0 +1,7 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (198103,8042,198067,188070);
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(198103, 'spell_sha_earth_elemental'),
+(8042, 'spell_sha_earth_shock'),
+(198067, 'spell_sha_fire_elemental'),
+(188070, 'spell_sha_healing_surge');
+
