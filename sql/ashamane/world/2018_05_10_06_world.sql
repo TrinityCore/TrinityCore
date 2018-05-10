738a0255177ed7ce7553f26635e2087a3e359128
HannibalRoG <edreisantafe@gmail.com>
2018-05-10 18:36:30 -0400

    Add spell 88163 (Attack) to warriors action bar Closed #1055

---
+++ sql/ashamane/world/2018_05_10_06_world.sql
@@ -0,0 +1,8 @@
+DELETE FROM playercreateinfo_action WHERE class=1 AND action=88163;
+INSERT INTO playercreateinfo_action (race, class, button, action, type) VALUES
+(8,  1, 0, 88163, 0),
+(9,  1, 0, 88163, 0),
+(10, 1, 0, 88163, 0),
+(11, 1, 0, 88163, 0),
+(22, 1, 0, 88163, 0),
+(24, 1, 0, 88163, 0);
