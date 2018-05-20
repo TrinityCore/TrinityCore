ba5f972a4997cd208722139d0495f47302685de0
Rochet2 <rochet2@post.com>
2018-05-20 03:56:55 +0300

    Update 2018_05_20_00_characters_transmog.sql

--- sql/custom/characters/2018_05_20_00_characters_transmog.sql
+++ sql/custom/characters/2018_05_20_00_characters_transmog.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `item_instance` ADD COLUMN `transmog` INT UNSIGNED NOT NULL DEFAULT '0' AFTER `text`;
 UPDATE `item_instance` AS `ii` INNER JOIN `custom_transmogrification` AS `ct` ON  `ii`.`guid` = `ct`.`GUID` SET `transmog` = `FakeEntry`;
-DROP TABLE `custom_transmogrification`;
+DROP TABLE IF EXISTS `custom_transmogrification`;
