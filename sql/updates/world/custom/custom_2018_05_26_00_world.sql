e8e1e0ac0947eccd76a3e3f5a666e18cade2bf3e
Ovahlord <dreadkiller@gmx.de>
2018-05-26 14:38:10 +0200

    DB: fixed a ton of startup errors

---
+++ sql/updates/world/custom/custom_2018_05_26_00_world.sql
@@ -0,0 +1,100 @@
+﻿-- Difficulty spell entries
+DELETE FROM `spell_dbc` WHERE `Id` IN (42636, 45433, 90120, 92943, 90059, 92948, 89289, 92951, 94991, 95858, 92666, 95884, 98192, 98846, 98900, 99288, 98899, 99551, 99877, 99878, 107352, 108859, 110615, 110614, 110035, 110034, 110033, 110032, 103179, 110515);
+INSERT INTO `spell_dbc`(`Id`, `Attributes`, `AttributesEx`, `AttributesEx2`, `AttributesEx3`, `AttributesEx4`, `AttributesEx5`, `AttributesEx6`, `AttributesEx7`, `AttributesEx8`, `AttributesEx9`, `AttributesEx10`, `CastingTimeIndex`, `DurationIndex`, `RangeIndex`, `SchoolMask`, `SpellAuraOptionsId`, `SpellCastingRequirementsId`, `SpellCategoriesId`, `SpellClassOptionsId`, `SpellEquippedItemsId`, `SpellInterruptsId`, `SpellLevelsId`, `SpellTargetRestrictionsId`, `Comment`) VALUES
+(107352, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Twilight Barrage'),
+(108859, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, 'Spelldifficulty_dbc id:4343 spellid1 serverside spell'),
+(110615, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, 'Spelldifficulty_dbc id:4343 spellid2 serverside spell'),
+(110614, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, 'Spelldifficulty_dbc id:4343 spellid3 serverside spell'),
+(110035, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Countdown'),
+(110034, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, 'Spelldifficulty_dbc id:4444 spellid1 serverside spell'),
+(110033, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, 'Spelldifficulty_dbc id:4444 spellid2 serverside spell'),
+(110032, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, 'Spelldifficulty_dbc id:4444 spellid3 serverside spell'),
+(103179, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Black Blood of the Earth'),
+(110515, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, 'Spelldifficulty_dbc id:4469 spellid1 serverside spell'),
+(42636, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, 'Birmingham Tools Test 3'),
+(45433, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, 'Birmingham Tools Test 5'),
+(90120, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Summon Drake'),
+(92943, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, 'Inspired'),
+(90059, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Summon Spitecaller'),
+(92948, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, 'Spelldifficulty_dbc id:3255 spellid1 serverside spell'),
+(89289, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Twilight Essence'),
+(92951, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, 'Spelldifficulty_dbc id:3258 spellid1 serverside spell'),
+(94991, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, 'Spelldifficulty_dbc id:3523 spellid1 serverside spell'),
+(95858, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, 'Spelldifficulty_dbc id:3579 spellid1 serverside spell'),
+(92666, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Summon Imp Trigger'),
+(95884, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, 'Spelldifficulty_dbc id:3582 spellid1 serverside spell'),
+(98192, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Foot Damage Tracker'),
+(98846, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, 'Spelldifficulty_dbc id:3741 spellid1 serverside spell'),
+(98900, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Drone Trigger'),
+(99288, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, 'Spelldifficulty_dbc id:3749 spellid1 serverside spell'),
+(98899, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Spiderling Trigger'),
+(99551, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, 'Spelldifficulty_dbc id:3752 spellid1 serverside spell'),
+(99877, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Summon Armor Elementals'),
+(99878, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, 'Spelldifficulty_dbc id:3764 spellid1 serverside spell');
+
+-- GameObject spells
+DELETE FROM `spell_dbc` WHERE `Id` IN (84506, 84505, 90245, 90244, 86761, 89691, 89685, 81358, 96364, 85892, 85891, 48084, 48085, 43981, 82094);
+INSERT INTO `spell_dbc`(`Id`, `Attributes`, `AttributesEx`, `AttributesEx2`, `AttributesEx3`, `AttributesEx4`, `AttributesEx5`, `AttributesEx6`, `AttributesEx7`, `AttributesEx8`, `AttributesEx9`, `AttributesEx10`, `CastingTimeIndex`, `DurationIndex`, `RangeIndex`, `SchoolMask`, `SpellAuraOptionsId`, `SpellCastingRequirementsId`, `SpellCategoriesId`, `SpellClassOptionsId`, `SpellEquippedItemsId`, `SpellInterruptsId`, `SpellLevelsId`, `SpellTargetRestrictionsId`, `Comment`) VALUES
+(84506, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Teleport to Orgrimmar'),
+(84505, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Teleport to Stormwind'),
+(90245, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) <INTERNAL>Stormwind Portal to Vashj''ir (from Portal)'),
+(90244, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) <INTERNAL>Orgrimmar Portal to Vashj''ir (from Portal)'),
+(86761, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Keg Cover Spell'),
+(89691, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) The Mad Magus: Ajamon''s Portal - Teleport to Southsea Holdfast - Cover'),
+(89685, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) The Mad Magus: Ajamon''s Portal - Teleport to Tirth''s Haunt - Cover'),
+(81358, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Swabbing Duty: Force Summon Controller'),
+(96364, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Create S.A.F.E. ""Parachute""'),
+(85892, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Victory From Within: Library Wing Quest Credit'),
+(85891, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Victory From Within: Hall of Arms Quest Credit'),
+(82094, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Force Periscope');
+
+-- Quest template, spellclick, encounter credit related spells
+DELETE FROM `spell_dbc` WHERE `Id` IN (94641, 98867, 89249, 89848, 89781, 94998, 89550, 88469, 90404, 89669, 90372, 89762, 95203, 82359, 82456, 110158, 111533);
+INSERT INTO `spell_dbc`(`Id`, `Attributes`, `AttributesEx`, `AttributesEx2`, `AttributesEx3`, `AttributesEx4`, `AttributesEx5`, `AttributesEx6`, `AttributesEx7`, `AttributesEx8`, `AttributesEx9`, `AttributesEx10`, `CastingTimeIndex`, `DurationIndex`, `RangeIndex`, `SchoolMask`, `SpellAuraOptionsId`, `SpellCastingRequirementsId`, `SpellCategoriesId`, `SpellClassOptionsId`, `SpellEquippedItemsId`, `SpellInterruptsId`, `SpellLevelsId`, `SpellTargetRestrictionsId`, `Comment`) VALUES
+(94641, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Cancel Tonga''s Eyes'),
+(98867, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Destroy Cudgel of Discipline - Delay'),
+(89249, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Perfect Costume Credit'),
+(89848, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Launch Quest: Placing the Pawns'),
+(89781, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Protector Growth Validate'),
+(94998, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Achievement Check - Full Caravan'),
+(89550, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Sapling Growth Validate'),
+(88469, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Rendan''s Weakness: Despawn Dancers'),
+(90404, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Launch Quest: Placing the Pawns'),
+(89669, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Fluorishing Protector Root Trigger'),
+(90372, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Summon Whelplings'),
+(89762, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) The Elder Crone: Quest Complete'),
+(95203, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Summon Main Cutscene Vehicle [INTERNAL ONLY]'),
+(82359, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Reactivate the Constructs: Dummy to Deactivated Construct On Interact'),
+(82456, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Sprout No More: Dummy to Giant Mushroom On Interact'),
+(110158, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Achievement Credit'),
+(111533, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 38, 0, 0, 0, 0, 0, 0, 0, '(Serverside/Non-DB2) Deathwing Kill Credit <Do Not Translate>');
+
+
+-- Delete deprecated GameObjects
+DELETE FROM `gameobject_template` WHERE `entry` IN (181105, 181106, 181165, 183510, 183511, 193171, 193169, 193170, 188597, 188598, 186810);
+DELETE FROM `gameobject_template_addon` WHERE `entry` IN (181105, 181106, 181165, 183510, 183511, 193171, 193169, 193170, 188597, 188598, 186810);
+
+-- Delete deprecated spell ranks
+DELETE FROM `spell_ranks` WHERE `first_spell_id` IN (66188, 66196, 66198, 66215, 66216);
+
+-- spell_proc fixups
+UPDATE `spell_proc` SET `AttributesMask`= 0 WHERE `SpellId`= 48108;
+DELETE FROM `spell_proc` WHERE `SpellId` IN (-20234, 30675, 31244, 31876, -44404, -49182, 53290, -53380, 54747, -64127, 1719, 11129, 24932, 53646, 54754, 54937, 54939, 55677, 62259, 63280, 53551, 48516, 44449, -34753, 20925, -18119, 57870, -30299, 17364, 43819, 55689, 58620, 63611, 1120, -53583);
+UPDATE `spell_proc` SET `ProcFlags`= 0x00010000 WHERE `SpellId` IN (47383, 71162, 71165);
+UPDATE `spell_proc` SET `ProcFlags`= 0x00000004 | 0x00000010 WHERE `SpellId`= 62606;
+UPDATE `spell_proc` SET `SpellPhaseMask`= 2 WHERE `SpellId` IN (70664, 31656, -48539, 28716, 40438, 56218, 62606, -47245, -47195, -19572, 37603, 38394, 40478, 45054, 60061, 60170, 60176, 60487, 61188, 64752, 64824, 64914, 67209, 67356, 67361, 67365, 70730, 70841, 70854, 71174, 71176, 71178, 71198, 71606, 71637, -12317);
+UPDATE `spell_proc` SET `SpellPhaseMask`= 0, `SpellTypeMask`= 0 WHERE `SpellId` IN (-49018, -53486, -33191, 46832, 74981, 90738, 90759, 87900, 87897);
+UPDATE `spell_proc` SET `ProcFlags`= 0x000A22A8 WHERE `SpellId`= -30299;
+UPDATE `spell_proc` SET `ProcFlags`= 0x00014000, `HitMask`= 0 WHERE `SpellId`= -16880;
+UPDATE `spell_proc` SET `SpellPhaseMask`= 1, `SpellId`= -12322 WHERE `SpellId`= 12322;
+UPDATE `spell_proc` SET `HitMask`= 0 WHERE `SpellId` IN (84722, 84723);
+UPDATE `spell_proc` SET `ProcFlags`= 0x00015000, `HitMask`= 0 WHERE `SpellId`= -48965;
+
+-- quest_template_addon fixups
+UPDATE `quest_template_addon` SET `ProvidedItemCount`= 1 WHERE `ID`= 14386;
+
+-- script_waypoint cleanup
+DELETE FROM `script_waypoint` WHERE `entry` IN (3849, 3850);
+
+-- SAI cleanup
+DELETE FROM `smart_scripts` WHERE `entryorguid`= -143921;
