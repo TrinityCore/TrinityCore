4376098be4c473461f1d4debb7269bf03ba77769
Ovahlord <dreadkiller@gmx.de>
2018-05-28 21:33:52 +0200

    Core/Spells: fixed broken riding trainers when Druids learned Flight Form

---
+++ sql/updates/world/custom/custom_2018_05_28_01_world.sql
@@ -0,0 +1,3 @@
+﻿DELETE FROM `spell_learn_spell` WHERE `entry`= 33943;
+UPDATE `npc_trainer` SET `SpellID`= 33950 WHERE `SpellID`= 33943;
+UPDATE `npc_trainer` SET `SpellID`= 40123 WHERE `SpellID`= 40120;
