aab2b3b295167881f9bb256c3366bbe4c3c2709e
cooler-SAI <cooler19860212@yahoo.com>
2018-06-08 19:53:34 +0300

    typo

--- sql/ashamane/world/2018_06_07_00_world_sai.sql
+++ sql/ashamane/world/2018_06_07_00_world_sai.sql
@@ -32,7 +32,7 @@ SET @ENTRY := 96608;
 UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
 DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
-(@ENTRY,0,0,0,0,0,100,0,5000,8000,12000,15000,11,199179,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Leap for the Throat'),
+(@ENTRY,0,0,0,0,0,100,0,5000,8000,12000,15000,11,199179,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Leap for the Throat');
 
 -- Gildedfur Stag
 SET @ENTRY := 96609;
@@ -83,7 +83,7 @@ SET @ENTRY := 101637;
 UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
 DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
-(@ENTRY,0,0,0,0,0,100,0,5000,8000,12000,15000,11,199034,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Valkyra\'s Advance'),
+(@ENTRY,0,0,0,0,0,100,0,5000,8000,12000,15000,11,199034,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Valkyra\'s Advance');
 
 -- Valarjar Champion
 SET @ENTRY := 97087;
