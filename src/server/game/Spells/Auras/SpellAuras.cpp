a60381901b5ca86d4b689705a78643a3332b55f6
Muru <lorder222@Rambler.ru>
2018-06-13 16:40:06 +0700

    Update SpellAuras.cpp

--- src/server/game/Spells/Auras/SpellAuras.cpp
+++ src/server/game/Spells/Auras/SpellAuras.cpp
@@ -420,9 +420,9 @@ Aura* Aura::Create(AuraCreateInfo& createInfo)
 Aura::Aura(AuraCreateInfo const& createInfo) :
 m_spellInfo(createInfo._spellInfo), m_casterGuid(createInfo.CasterGUID),
 m_castItemGuid(createInfo.CastItem ? createInfo.CastItem->GetGUID() : ObjectGuid::Empty), m_applyTime(GameTime::GetGameTime()),
-m_owner(createInfo._owner), m_timeCla(0), m_updateTargetMapInterval(0),
+m_owner(createInfo._owner), m_timeCla(0), m_heartBeatTimer(0), m_updateTargetMapInterval(0),
 _casterInfo(), m_procCharges(0), m_stackAmount(1),
-m_isRemoved(false), m_isSingleTarget(false), m_isUsingCharges(false), m_dropEvent(nullptr), m_heartBeatTimer(0),
+m_isRemoved(false), m_isSingleTarget(false), m_isUsingCharges(false), m_dropEvent(nullptr),
 m_procCooldown(std::chrono::steady_clock::time_point::min())
 {
     if (m_spellInfo->ManaPerSecond || m_spellInfo->ManaPerSecondPerLevel)
