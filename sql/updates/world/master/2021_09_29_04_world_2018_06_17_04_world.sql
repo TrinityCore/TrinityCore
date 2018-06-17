a3e74a4871c13151246c2b966b8880c8908e0470
Killyana <morphone1@gmail.com>
2018-06-17 15:41:33 +0200

    DB: Typo
    (cherry picked from commit b06f62649b62320d8fef47d990bbbe2f54ba82e2)

--- sql/updates/world/master/2021_09_29_04_world_2018_06_17_04_world.sql
+++ sql/updates/world/master/2021_09_29_04_world_2018_06_17_04_world.sql
@@ -199,7 +199,7 @@ DELETE FROM `smart_scripts` WHERE `entryorguid`=19681 AND `source_type`=0;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`event_param5`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (19681,0,0,0,1,0,100,0,1000,120000,120000,240000,0,11,34303,0,0,0,0,0,1,0,0,0,0,0,0,0,"Void Spawner L - Out of Combat - Cast 'Nether Charge'");
 
-UPDATE `creature_addon` SET `auras`="" WHERE `auras`=34303;
+UPDATE `creature_addon` SET `auras`="" WHERE `auras`="34303";
 
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=13 AND `SourceEntry`=34303;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
