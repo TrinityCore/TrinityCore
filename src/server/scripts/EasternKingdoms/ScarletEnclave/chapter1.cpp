80990079fc9949d352b03ed0f662c446065d3312
DanVS <33371360+DanVS@users.noreply.github.com>
2018-06-19 12:51:21 +0300

    schedule event on init

--- src/server/scripts/EasternKingdoms/ScarletEnclave/chapter1.cpp
+++ src/server/scripts/EasternKingdoms/ScarletEnclave/chapter1.cpp
@@ -347,8 +347,8 @@ class go_acherus_soul_prison : public GameObjectScript
 };
 
  /*######
-## npc_eye_of_acherus
-######*/
+ ## npc_eye_of_acherus
+ ######*/
 
 enum EyeOfAcherus
 {
@@ -376,16 +376,17 @@ class npc_eye_of_acherus : public CreatureScript
         {
             npc_eye_of_acherusAI(Creature* creature) : ScriptedAI(creature)
             {
-                me->SetDisplayId(me->GetCreatureTemplate()->Modelid1);
-                if (Player* owner = me->GetCharmerOrOwner()->ToPlayer())
-                    owner->SendAutoRepeatCancel(me);
-
+                me->CastSpell(me, SPELL_EYE_VISUAL, true);
                 me->SetReactState(REACT_PASSIVE);
+                me->SetDisableGravity(true);
+                me->SetControlled(true, UNIT_STATE_ROOT);
 
                 Movement::MoveSplineInit init(me);
                 init.MoveTo(EyeOFAcherusFallPoint.GetPositionX(), EyeOFAcherusFallPoint.GetPositionY(), EyeOFAcherusFallPoint.GetPositionZ(), false);
                 init.SetFall();
                 me->GetMotionMaster()->LaunchMoveSpline(std::move(init), POINT_EYE_FALL, MOTION_PRIORITY_NORMAL, POINT_MOTION_TYPE);
+
+                _events.ScheduleEvent(EVENT_MOVE_START, 7000);
             }
 
             void OnCharmed(bool /*apply*/) override { }
@@ -400,9 +401,9 @@ class npc_eye_of_acherus : public CreatureScript
                     {
                         case EVENT_MOVE_START:
                         {
-                            DoCast(me, SPELL_EYE_FLIGHT_BOOST);
-
+                            me->CastSpell(me, SPELL_EYE_FLIGHT_BOOST);
                             me->SetControlled(false, UNIT_STATE_ROOT);
+
                             if (Player* owner = me->GetCharmerOrOwner()->ToPlayer())
                             {
                                 for (uint8 i = 0; i < MAX_MOVE_TYPE; ++i)
@@ -422,7 +423,6 @@ class npc_eye_of_acherus : public CreatureScript
             {
                 if (movementType == WAYPOINT_MOTION_TYPE && pointId == POINT_EYE_MOVE_END - 1)
                 {
-                    me->SetSheath(SHEATH_STATE_MELEE);
                     me->RemoveAllAuras();
 
                     if (Player* owner = me->GetCharmerOrOwner()->ToPlayer())
@@ -434,14 +434,7 @@ class npc_eye_of_acherus : public CreatureScript
                         Talk(TALK_CONTROL, owner);
                     }
                     me->SetDisableGravity(false);
-                    DoCast(me, SPELL_EYE_FLIGHT);
-                }
-
-                if (movementType == POINT_MOTION_TYPE && pointId == POINT_EYE_FALL)
-                {
-                    me->SetDisableGravity(true);
-                    me->SetControlled(true, UNIT_STATE_ROOT);
-                    _events.ScheduleEvent(EVENT_MOVE_START, 5000);
+                    me->CastSpell(me, SPELL_EYE_FLIGHT);
                 }
             }
 
@@ -454,6 +447,7 @@ class npc_eye_of_acherus : public CreatureScript
             return new npc_eye_of_acherusAI(creature);
         }
 };
+
 
 /*######
 ## npc_death_knight_initiate
