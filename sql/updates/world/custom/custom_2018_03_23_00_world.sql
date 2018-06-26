244e8398bb79cfd7185601d2975447d1c130a4d4
Ovahlord <dreadkiller@gmx.de>
2018-06-26 17:13:56 +0200

    DB/Spells: fixed Healing Rain triggered effect scriptname

--- sql/updates/world/custom/custom_2018_03_23_00_world.sql
+++ sql/updates/world/custom/custom_2018_03_23_00_world.sql
@@ -4,4 +4,4 @@ DELETE FROM `spell_script_names` where `ScriptName` IN
 
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (73920, 'spell_sha_healing_rain'),
-(73921, 'spell_sha_healing_rain_trigger');
+(73921, 'spell_sha_healing_rain_triggered');
