c08496d3f51dc6fcd7ba84bda585b915bd5ad7e5
Ovahlord <dreadkiller@gmx.de>
2018-06-27 02:05:19 +0200

    DB/Spells: fixed a multibatching error

--- sql/updates/world/custom/custom_2018_06_26_00_world.sql
+++ sql/updates/world/custom/custom_2018_06_26_00_world.sql
@@ -2,5 +2,6 @@ DELETE FROM `spell_proc` WHERE `SpellId`= 76806;
 INSERT INTO `spell_proc` (`SpellId`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `ProcFlags`, `SpellTypeMask`, `SpellPhaseMask`, `HitMask`, `AttributesMask`, `ProcsPerMinute`, `Chance`, `Cooldown`, `Charges`) VALUES
 (76806, 0, 0, 0x0, 0x0, 0x0, 0x0, 1, 2, 0, 32, 0, 0,  0, 0);
 
+DELETE FROM `spell_script_names` WHERE `ScriptName`= 'spell_rog_main_gauche';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
 (76806,'spell_rog_main_gauche');
