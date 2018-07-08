11dc8799242fb218fd31373e5d2f24854c4d653b
Ovahlord <dreadkiller@gmx.de>
2018-07-09 01:16:01 +0200

    Core/Skills: declare Archaeology as profession skill

--- src/server/game/Spells/SpellMgr.h
+++ src/server/game/Spells/SpellMgr.h
@@ -558,7 +558,7 @@ bool IsWeaponSkill(uint32 skill);
 
 inline bool IsProfessionSkill(uint32 skill)
 {
-    return  IsPrimaryProfessionSkill(skill) || skill == SKILL_FISHING || skill == SKILL_COOKING || skill == SKILL_FIRST_AID;
+    return  IsPrimaryProfessionSkill(skill) || skill == SKILL_FISHING || skill == SKILL_COOKING || skill == SKILL_FIRST_AID || skill == SKILL_ARCHAEOLOGY;
 }
 
 inline bool IsProfessionOrRidingSkill(uint32 skill)
