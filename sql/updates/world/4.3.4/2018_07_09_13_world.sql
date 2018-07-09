f820232f217c98fa0322012a4a15fae3b8bcb549
Aokromes <aokromes@gmail.com>
2018-07-09 11:54:37 +0200

    DB/Misc: Halls of Origination Heroic Template Updates By giripdragosandrei closes #176

---
+++ sql/updates/world/4.3.4/2018_07_09_13_world.sql
@@ -0,0 +1,91 @@
+-- Void Wyrm 
+UPDATE `creature_template` SET `difficulty_entry_1` = 49305 WHERE `entry` = 41374;
+UPDATE `creature_template` SET `minlevel` = 85, `maxlevel` = 85, `exp` = 3, `faction` = 16, `unit_flags` = 64 WHERE `entry` = 49305;
+
+-- Void Seeker 
+UPDATE `creature_template` SET `difficulty_entry_1` = 49304 WHERE `entry` = 41371;
+UPDATE `creature_template` SET `minlevel` = 85, `maxlevel` = 85, `exp` = 3, `faction` = 16, `speed_run` = 1, `unit_flags` = 64 WHERE `entry` = 49304;
+
+-- Void Lord
+UPDATE `creature_template` SET `difficulty_entry_1` = 49303 WHERE `entry` = 41364;
+UPDATE `creature_template` SET `minlevel` = 85, `maxlevel` = 85, `exp` = 3, `faction` = 16, `speed_run` = 1, `unit_flags` = 262208 WHERE `entry` = 49303;
+
+-- Setesh's Void Seeker 
+UPDATE `creature_template` SET `difficulty_entry_1` = 48889 WHERE `entry` = 41148;
+UPDATE `creature_template` SET `minlevel` = 85, `maxlevel` = 85, `exp` = 3, `faction` = 16 WHERE `entry` = 48889;
+
+-- Setesh's Void Sentinel
+UPDATE `creature_template` SET `difficulty_entry_1` = 48888 WHERE `entry` = 41208;
+UPDATE `creature_template` SET `minlevel` = 85, `maxlevel` = 85, `exp` = 3, `faction` = 16 WHERE `entry` = 48888;
+
+-- Bloodpetal Sprout
+UPDATE `creature_template` SET `difficulty_entry_1` = 48717 WHERE `entry` = 40630;
+UPDATE `creature_template` SET `minlevel` = 85, `maxlevel` = 85, `exp` = 3, `faction` = 16 WHERE `entry` = 48717;
+
+-- Lifewarden Nymph
+UPDATE `creature_template` SET `difficulty_entry_1` = 49300 WHERE `entry` = 40715;
+UPDATE `creature_template` SET `minlevel` = 85, `maxlevel` = 85, `exp` = 3, `faction` = 16, `speed_run` = 1, `unit_class` = 2, `unit_flags` = 64, `unit_flags2` = 33556480 WHERE `entry` = 49300;
+
+-- Living Vine 
+UPDATE `creature_template` SET `difficulty_entry_1` = 49299 WHERE `entry` = 40668;
+UPDATE `creature_template` SET `unit_flags` = 64, `InhabitType` = 12 WHERE `entry` = 49299;
+
+-- Ammunae's Bloodpetal Blossom 
+UPDATE `creature_template` SET `difficulty_entry_1` = 48716 WHERE `entry` = 40620;
+UPDATE `creature_template` SET `minlevel` = 85, `maxlevel` = 85, `exp` = 3, `faction` = 16  WHERE `entry` = 48716;
+
+-- Star Shard
+UPDATE `creature_template` SET `difficulty_entry_1` = 49292 WHERE `entry` = 40106;
+UPDATE `creature_template` SET `minlevel` = 85, `maxlevel` = 85, `exp` = 3, `faction` = 16, `speed_run` = 1, `unit_class` = 2, `unit_flags` = 64  WHERE `entry` = 49292;
+
+-- Flux Animator
+UPDATE `creature_template` SET  `difficulty_entry_1` = 49291 WHERE `entry` = 40033;
+UPDATE `creature_template` SET `minlevel` = 85, `maxlevel` = 85, `exp` = 3, `faction` = 91, `speed_walk` = 1.55556, `unit_flags` = 64  WHERE `entry` = 49291;
+
+-- Stone Trogg Rock Flinger
+UPDATE `creature_template` SET `difficulty_entry_1` = 48897 WHERE `entry` = 40252;
+UPDATE `creature_template` SET `minlevel` = 85, `maxlevel` = 85, `exp` = 3, `faction` = 16 WHERE `entry` = 48897;
+
+-- Stone Trogg Brute 
+UPDATE `creature_template` SET `difficulty_entry_1` = 48899 WHERE `entry` = 40251;
+UPDATE `creature_template` SET `minlevel` = 85, `maxlevel` = 85, `exp` = 3, `faction` = 16 WHERE `entry` = 48899;
+
+-- Dustbone Horror
+UPDATE `creature_template` SET `difficulty_entry_1` = 49302 WHERE `entry` = 40808;
+UPDATE `creature_template` SET `minlevel` = 85, `maxlevel` = 85, `exp` = 3, `faction` = 16, `unit_flags` = 64 WHERE `entry` = 49302;
+
+-- Dustbone Tormentor
+UPDATE `creature_template` SET `difficulty_entry_1` = 49295, `unit_flags` = 64 WHERE `entry` = 40311;
+UPDATE `creature_template` SET `minlevel` = 85, `maxlevel` = 85, `exp` = 3, `faction` = 16, `unit_class` = 2, `unit_flags` = 64 WHERE `entry` = 49295;
+
+-- Dustbone Horror
+UPDATE `creature_template` SET `difficulty_entry_1` = 49301, `unit_flags` = 64 WHERE `entry` = 40787;
+UPDATE `creature_template` SET `minlevel` = 85, `maxlevel` = 85, `exp` = 3, `faction` = 16, `unit_flags` = 64 WHERE `entry` = 49301;
+
+-- Venomous Skitterer 
+UPDATE `creature_template` SET `difficulty_entry_1` = 49286 WHERE `entry` = 39440;
+UPDATE `creature_template` SET `minlevel` = 85, `maxlevel` = 85, `exp` = 3, `faction` = 16, `unit_flags` = 33816640 WHERE `entry` = 49286;
+
+-- Jeweled Scarab
+UPDATE `creature_template` SET `difficulty_entry_1` = 49306 WHERE `entry` = 42556;
+UPDATE `creature_template` SET `minlevel` = 85, `maxlevel` = 85, `exp` = 3, `faction` = 16, `unit_flags` = 33816576 WHERE `entry` = 49306;
+
+-- Blistering Scarab 
+UPDATE `creature_template` SET `difficulty_entry_1` = 49294 WHERE `entry` = 40310;
+UPDATE `creature_template` SET `minlevel` = 85, `maxlevel` = 85, `exp` = 3, `faction` = 16, `unit_flags` = 33816640 WHERE `entry` = 49294;
+
+-- Temple Runecaster
+UPDATE `creature_template` SET `difficulty_entry_1` = 49308 WHERE `entry` = 48140;
+UPDATE `creature_template` SET `minlevel` = 85, `maxlevel` = 85, `exp` = 3, `faction` = 16, `speed_walk`= 0.666668, `unit_flags` = 32832 WHERE `entry` = 49308;
+
+-- Temple Shadowlancer
+UPDATE `creature_template` SET `difficulty_entry_1` = 49309 WHERE `entry` = 48141;
+UPDATE `creature_template` SET `minlevel` = 85, `maxlevel` = 85, `exp` = 3, `faction` = 16, `speed_walk`= 0.666668, `unit_flags` = 32832 WHERE `entry` = 49309;
+
+-- Temple Swiftstalker
+UPDATE `creature_template` SET `difficulty_entry_1` = 49307 WHERE `entry` = 48139;
+UPDATE `creature_template` SET `minlevel` = 85, `maxlevel` = 85, `exp` = 3, `faction` = 16, `speed_walk`= 0.666668, `unit_flags` = 32832 WHERE `entry` = 49307;
+
+-- Temple Fireshaper
+UPDATE `creature_template` SET `difficulty_entry_1` = 49310 WHERE `entry` = 48143;
+UPDATE `creature_template` SET `minlevel` = 85, `maxlevel` = 85, `exp` = 3, `faction` = 16, `speed_walk`= 0.666668, `unit_flags` = 32832 WHERE `entry` = 49310;
