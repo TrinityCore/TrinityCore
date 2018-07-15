0f46bd62b6b62f1021b7141a9ba8fbef5869eb26
leviathonbeast <leviathonbeast@googlemail.com>
2018-07-15 14:56:01 +0200

    try to fix the timing of the gossip

--- sql/ashamane/world/2018_07_13_99_world_nightbourne_spawn.sql
+++ sql/ashamane/world/2018_07_13_99_world_nightbourne_spawn.sql
@@ -382,7 +382,7 @@ SET @FIRST_ARCANIST_THALYSSRA := 131326;
 UPDATE `creature_template` SET `AIName`="SmartAI" WHERE `entry`=@FIRST_ARCANIST_THALYSSRA;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=@FIRST_ARCANIST_THALYSSRA AND `source_type`=0;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
-(@FIRST_ARCANIST_THALYSSRA,0,0,0,19,0,100,0,49933,0,0,0,1,0,3000,0,0,0,0,1,0,0,0,0,0,0,0,"First Arcanist Thalyssra - On Quest 'For the Horde' Taken - Say Line 0"),
+(@FIRST_ARCANIST_THALYSSRA,0,0,0,19,0,100,0,49933,0,0,0,1,0,5000,0,0,0,0,1,0,0,0,0,0,0,0,"First Arcanist Thalyssra - On Quest 'For the Horde' Taken - Say Line 0"),
 (@FIRST_ARCANIST_THALYSSRA,0,1,0,52,0,100,0,0,@FIRST_ARCANIST_THALYSSRA,0,0,1,1,11000,0,0,0,0,1,0,0,0,0,0,0,0,"First Arcanist Thalyssra - On Text 0 Over - Say Line 1"),
 (@FIRST_ARCANIST_THALYSSRA,0,2,0,52,0,100,0,1,@FIRST_ARCANIST_THALYSSRA,0,0,1,2,11000,0,0,0,0,1,0,0,0,0,0,0,0,"First Arcanist Thalyssra - On Text 1 Over - Say Line 2"),
 (@FIRST_ARCANIST_THALYSSRA,0,3,0,52,0,100,0,2,@FIRST_ARCANIST_THALYSSRA,0,0,1,3,11000,0,0,0,0,1,0,0,0,0,0,0,0,"First Arcanist Thalyssra - On Text 2 Over - Say Line 3"),
