5a41d66450cbc8279175eb6e0be28ea332a54cbc
Aokromes <aokromes@gmail.com>
2018-07-16 10:29:39 +0200

    Fix some startup errors

---
+++ sql/updates/world/4.3.4/2018_07_16_04.sql
@@ -0,0 +1,4 @@
+--
+UPDATE `creature_template` SET `unit_class`=2 WHERE `entry`=49310;
+UPDATE `creature_template` SET `unit_class`=2 WHERE `entry`=49308;
+UPDATE `creature_template` SET `minlevel` = 85, `maxlevel` = 85, `exp` = 3, `faction` = 16, `unit_flags2` = 33589280 WHERE `entry` = 49299;
