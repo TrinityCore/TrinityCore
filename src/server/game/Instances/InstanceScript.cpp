ce7757639cacdc7b2000a3035d36494838ccb9a4
Aokromes <aokromes@gmail.com>
2018-07-22 21:37:25 +0200

    Fix linux build

--- src/server/game/Instances/InstanceScript.cpp
+++ src/server/game/Instances/InstanceScript.cpp
@@ -344,7 +344,7 @@ bool InstanceScript::SetBossState(uint32 id, EncounterState state)
         if (bossInfo->state == TO_BE_DECIDED) // loading
         {
             bossInfo->state = state;
-            TC_LOG_DEBUG("scripts", "InstanceScript: Initialize boss %u state as %s (map %u, %u).", id, GetBossStateName(state), instance->GetId(), instance->GetInstanceId());
+            TC_LOG_DEBUG("scripts", "InstanceScript: Initialize boss %u state as %s (map %u, %u).", id, GetBossStateName(state).c_str(), instance->GetId(), instance->GetInstanceId());
             return false;
         }
 
@@ -355,7 +355,7 @@ bool InstanceScript::SetBossState(uint32 id, EncounterState state)
 
             if (bossInfo->state == DONE)
             {
-                TC_LOG_ERROR("map", "InstanceScript: Tried to set instance state from %s back to %s for map %u, instance id %u. Blocked!", GetBossStateName(bossInfo->state), GetBossStateName(state), instance->GetId(), instance->GetInstanceId());
+                TC_LOG_ERROR("map", "InstanceScript: Tried to set instance state from %s back to %s for map %u, instance id %u. Blocked!", GetBossStateName(bossInfo->state).c_str(), GetBossStateName(state).c_str(), instance->GetId(), instance->GetInstanceId());
                 return false;
             }
 
