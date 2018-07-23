31670056506c6b32e0fe54c29385f7fdc6e8affd
Aokromes <aokromes@gmail.com>
2018-07-23 11:34:08 +0200

    fix warnings

--- src/server/scripts/Commands/cs_list.cpp
+++ src/server/scripts/Commands/cs_list.cpp
@@ -681,7 +681,7 @@ public:
     static char const* GetZoneName(uint32 zoneId, LocaleConstant locale)
     {
         AreaTableEntry const* zoneEntry = sAreaTableStore.LookupEntry(zoneId);
-        return zoneEntry ? (const char*)zoneEntry->area_name[locale] : "<unknown zone>";
+        return zoneEntry ? zoneEntry->area_name->Str[locale] : "<unknown zone>";
     }
     static bool HandleListRespawnsCommand(ChatHandler* handler, char const* args)
     {
