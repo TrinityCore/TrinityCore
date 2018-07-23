b689dfc0bfd8e30f046f610c9c272462b2520eb9
Ovahlord <dreadkiller@gmx.de>
2018-07-23 12:23:05 +0200

    DB: fixed format in last commit

--- sql/updates/world/custom/custom_2018_07_23_00_world.sql
+++ sql/updates/world/custom/custom_2018_07_23_00_world.sql
@@ -8,8 +8,7 @@ INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 
 UPDATE `creature_template` SET `unit_class`= 8, `ScriptName`= 'npc_pet_mage_mirror_image' WHERE `entry` IN (47244, 47243, 31216);
 
-
-DELETE FROM `conditions` WHERE `SourceEntry` IN (58836) AND `SourceTypeOrReferenceId`= 13;
+DELETE FROM `conditions` WHERE `SourceEntry`= 58836 AND `SourceTypeOrReferenceId`= 13;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ScriptName`, `Comment`) VALUES
 (13, 1, 58836, 0, 0, 31, 0, 3, 47244, 0, 0, 0, '', 'Initialize Images - Target Mirror Image'),
 (13, 1, 58836, 0, 1, 31, 0, 3, 47243, 0, 0, 0, '', 'Initialize Images - Target Mirror Image'),
