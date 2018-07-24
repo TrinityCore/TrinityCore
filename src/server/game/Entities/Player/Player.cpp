aaebc1687e64d1a21c2428eaf7b45e1a8cbfd8ce
miorey <clem.balea@gmail.com>
2018-07-25 00:40:10 +0200

    Talent based on quest

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -24611,15 +24611,15 @@ void Player::StoreLootItem(uint8 lootSlot, Loot* loot)
         SendEquipError(msg, nullptr, nullptr, item->itemid);
 }
 
+/**
+ *
+ * @return
+ */
 uint32 Player::CalculateTalentsPoints() const
 {
-    uint32 base_talent = getLevel() < 10 ? 0 : getLevel()-9;
+    uint32 base_talent = static_cast<uint32>(getLevel() < 10 ? 0 : getLevel() - 9);
 
-    if (getClass() != CLASS_DEATH_KNIGHT || GetMapId() != 609)
-        return uint32(base_talent * sWorld->getRate(RATE_TALENT));
-
-    uint32 talentPointsForLevel = getLevel() < 56 ? 0 : getLevel() - 55;
-    talentPointsForLevel += m_questRewardTalentCount;
+    uint32 talentPointsForLevel = m_questRewardTalentCount;
 
     if (talentPointsForLevel > base_talent)
         talentPointsForLevel = base_talent;
