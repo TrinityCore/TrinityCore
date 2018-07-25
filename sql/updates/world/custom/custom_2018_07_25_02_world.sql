96a45e6b00ff0ea3a966950b8dac4f170a673990
Ovahlord <dreadkiller@gmx.de>
2018-07-25 18:36:47 +0200

    DB/Stormwind: added a missing entry to construction workers

--- sql/updates/world/custom/custom_2018_07_25_02_world.sql
+++ sql/updates/world/custom/custom_2018_07_25_02_world.sql
@@ -1,5 +1,5 @@
 DELETE FROM `creature_template_addon` WHERE `entry` IN (28596, 28569);
-DELETE FROM `creature_addon` WHERE `guid` IN (313934, 313933, 313930, 313935, 313931, 313938, 313939, 314108, 314106, 314109, 314107, 314113, 314110, 314111, 314112);
+DELETE FROM `creature_addon` WHERE `guid` IN (313934, 313933, 313930, 313935, 313931, 313938, 313939, 314108, 314106, 314109, 314107, 314113, 314110, 314111, 314112, 313932);
 INSERT INTO `creature_addon` (`guid`, `bytes2`, `emote`) VALUES
 (313934, 257, 234),
 (313933, 257, 234),
@@ -15,14 +15,15 @@ INSERT INTO `creature_addon` (`guid`, `bytes2`, `emote`) VALUES
 (314113, 257, 234),
 (314110, 257, 234),
 (314111, 257, 234),
-(314112, 257, 234);
+(314112, 257, 234),
+(313932, 257, 234);
 
 DELETE FROM `creature_equip_template` WHERE `CreatureID` IN (28596, 28569);
 INSERT INTO `creature_equip_template` (`CreatureID`, `ID`, `ItemID1`, `ItemID2`, `ItemID3`) VALUES
 (28596, 1, 5292, 0, 0), -- Dwarven Construction Worker
 (28569, 1, 5292, 0, 0); -- Construction Worker
 
-UPDATE `creature` SET `equipment_id`= 1 WHERE `guid` IN (313934, 313933, 313930, 313935, 313931, 313938, 313939, 314108, 314106, 314109, 314107, 314113, 314110, 314111, 314112);
+UPDATE `creature` SET `equipment_id`= 1 WHERE `guid` IN (313934, 313933, 313930, 313935, 313931, 313938, 313939, 314108, 314106, 314109, 314107, 314113, 314110, 314111, 314112, 313932);
 
 DELETE FROM `creature` WHERE `guid`= 314226;
 DELETE FROM `creature_addon` WHERE `guid`= 314226;
