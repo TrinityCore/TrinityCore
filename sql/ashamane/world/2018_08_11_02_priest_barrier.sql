ce7172e5c34e4acf8d9e286beaaade2c02f03f4d
leviathonbeast <leviathonbeast@googlemail.com>
2018-08-11 17:27:57 +0200

    Spells/Priest Barrier Fix

---
+++ sql/ashamane/world/2018_08_11_02_priest_barrier.sql
@@ -0,0 +1,9 @@
+
+-- Fix Priest Barrier Spell
+UPDATE areatrigger_scripts SET entry = 1489 WHERE ScriptName = "at_pri_power_word_barrier";
+UPDATE areatrigger_template SET ScriptName = at_pri_power_world_barrier WHERE Id = 5802;
+
+
+
+
+
