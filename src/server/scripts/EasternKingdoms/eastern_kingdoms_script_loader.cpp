5cafc211e623c2c6ad730302ea3313d931eb8bb5
illfated <38162891+illfated@users.noreply.github.com>
2018-08-11 19:12:48 +0200

    Scripts/Eastern Kingdoms: zone_wetlands converted to SAI cleanup (#22252)
    (cherry picked from commit 59edf6b100a9092cd530af6cab2b68a84ecd11f0)

--- src/server/scripts/EasternKingdoms/eastern_kingdoms_script_loader.cpp
+++ src/server/scripts/EasternKingdoms/eastern_kingdoms_script_loader.cpp
@@ -201,7 +201,6 @@ void AddSC_tirisfal_glades();
 void AddSC_tol_barad();
 void AddSC_undercity();
 void AddSC_western_plaguelands();
-void AddSC_wetlands();
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
@@ -393,5 +392,4 @@ void AddEasternKingdomsScripts()
     AddSC_tol_barad();
     AddSC_undercity();
     AddSC_western_plaguelands();
-    AddSC_wetlands();
 }
