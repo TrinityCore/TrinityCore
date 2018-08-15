b90ea7a1efaf8b59cfe74dedce327fdaab6231df
Aokromes <aokromes@gmail.com>
2018-08-15 14:31:12 +0200

    TDB 4.3.4.33 2018/08/15
    missing part

---
+++ sql/updates/auth/4.3.4/2018_08_15_00_auth.sql
@@ -0,0 +1,2 @@
+-- TDB 4.3.4-33 auth database
+UPDATE `updates` SET `state`='ARCHIVED';
---
+++ sql/updates/world/4.3.4/2018_08_15_00.sql
@@ -0,0 +1,3 @@
+UPDATE `version` SET `db_version`='TDB 434.33', `cache_id`=33 LIMIT 1;
+
+UPDATE `updates` SET `state`='ARCHIVED';
