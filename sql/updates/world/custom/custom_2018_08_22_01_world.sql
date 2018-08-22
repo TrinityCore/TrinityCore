101eb6d142b73fd4b86bee5641d30a7297060b3d
Ovahlord <dreadkiller@gmx.de>
2018-08-23 00:19:14 +0200

    DB/Spells: fixed a startup error caused by Fulmation proc entry

--- sql/updates/world/custom/custom_2018_08_22_01_world.sql
+++ sql/updates/world/custom/custom_2018_08_22_01_world.sql
@@ -7,4 +7,4 @@ INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 
 DELETE FROM `spell_proc` WHERE `SpellId`= 95774;
 INSERT INTO `spell_proc` (`SpellId`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `ProcFlags`, `SpellTypeMask`, `SpellPhaseMask`, `HitMask`, `AttributesMask`, `Cooldown`, `Charges`, `Chance`) VALUES
-(95774, 11, 0x00100000, 0, 0, 0x00010000, 1, 1, 1 | 2 | 4 | 8 | 512 | 1024 | 2048, 0, 0, 1, 100);
+(95774, 11, 0x00100000, 0, 0, 0x00010000, 1, 1, 0, 0, 0, 1, 100);
