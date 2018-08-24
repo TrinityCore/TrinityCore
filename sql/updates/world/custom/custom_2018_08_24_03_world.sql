0617a3ef97338e510579b00aa1eea534aa28ffa4
Ovahlord <dreadkiller@gmx.de>
2018-08-24 21:17:09 +0200

    DB/Spells: added missing AP bonus to Explosive Shot

---
+++ sql/updates/world/custom/custom_2018_08_24_03_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_bonus_data` WHERE `entry`= 53301;
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES
+(53301, 0, 0, 0, 0.273, 'Hunter - Explosive Shot');
