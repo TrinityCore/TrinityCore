6420e847734c67a42ab27d084f77a2421c6fa0f0
Treeston <treeston.mmoc@gmail.com>
2018-08-29 18:07:31 +0200

    Core/Maps: Fix a crash when spawning a spawn group

--- src/server/game/Maps/Map.cpp
+++ src/server/game/Maps/Map.cpp
@@ -3316,6 +3316,7 @@ bool Map::SpawnGroupSpawn(uint32 groupId, bool ignoreRespawn, bool force, std::v
         return false;
     }
 
+    SetSpawnGroupActive(groupId, true); // start processing respawns for the group
     for (auto& pair : sObjectMgr->GetSpawnDataForGroup(groupId))
     {
         SpawnData const* data = pair.second;
@@ -3366,7 +3367,6 @@ bool Map::SpawnGroupSpawn(uint32 groupId, bool ignoreRespawn, bool force, std::v
             return false;
         }
     }
-    SetSpawnGroupActive(groupId, true); // start processing respawns for the group
     return true;
 }
 
