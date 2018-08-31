7aefa275ab5d5125257b57a91136ebed752a8d3e
Ovahlord <dreadkiller@gmx.de>
2018-08-31 12:44:34 +0200

    Core/Movement: fixed flight path travel routes broken by last flight path merge

--- src/server/game/Movement/MovementGenerators/FlightPathMovementGenerator.cpp
+++ src/server/game/Movement/MovementGenerators/FlightPathMovementGenerator.cpp
@@ -140,6 +140,9 @@ void FlightPathMovementGenerator::DoReset(Player* player)
     }
     init.SetFirstPointId(GetCurrentNode());
     init.SetFly();
+    init.SetSmooth();
+    init.SetUncompressed();
+    init.SetWalk(true);
     init.SetVelocity(PLAYER_FLIGHT_SPEED);
     init.Launch();
 }
