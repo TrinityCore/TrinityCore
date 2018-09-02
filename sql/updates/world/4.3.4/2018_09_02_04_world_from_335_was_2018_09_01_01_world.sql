5bd62371956416073bade407ea04668402738dae
Aokromes <aokromes@gmail.com>
2018-09-02 22:43:47 +0200

    disable update, it needs hover for 4.3.4

--- sql/updates/world/4.3.4/2018_09_02_04_world_from_335_was_2018_09_01_01_world.sql
+++ sql/updates/world/4.3.4/2018_09_02_04_world_from_335_was_2018_09_01_01_world.sql
@@ -1,3 +1,4 @@
+/*
 -- 
 DELETE FROM `creature_template_movement` WHERE `CreatureId` IN (36853, 38265, 38266, 38267);
 INSERT INTO `creature_template_movement` (`CreatureId`, `Ground`, `Swim`, `Flight`, `Rooted`) VALUES
@@ -5,3 +6,4 @@ INSERT INTO `creature_template_movement` (`CreatureId`, `Ground`, `Swim`, `Fligh
 (38265, 1, 0, 2, 0),
 (38266, 1, 0, 2, 0),
 (38267, 1, 0, 2, 0);
+*/
