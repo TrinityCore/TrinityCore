3e024d1eb9b335ea434ec7ef5fc74427e76ed24b
Killyana <morphone1@gmail.com>
2018-09-03 22:06:51 +0200

    DB/GameObject: Fix some wrong spawns

---
+++ sql/updates/world/3.3.5/2018_09_03_03_world_335.sql
@@ -0,0 +1,4 @@
+-- 
+UPDATE `gameobject` SET `id`=171942, `spawntimesecs`=180, `animprogress`=100 WHERE `guid`=18199;
+DELETE FROM `gameobject` WHERE `guid`=48892;
+DELETE FROM `gameobject_addon` WHERE `guid`=48892;
