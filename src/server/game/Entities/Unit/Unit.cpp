c8b11f7e3db8b40a0c02a759d722dff0f7c55fe8
Treeston <treeston.mmoc@gmail.com>
2018-09-12 14:28:49 +0200

    Core/Unit: Successfully (?) prevented the zombie apocalypse. Corpses can no longer gain HP. Closes #22409.
    (cherry picked from commit f50a8e5926372400e87337fdcdcd03fd68a8a4c6)

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -8852,7 +8852,7 @@ void Unit::SetLevel(uint8 lvl)
 
 void Unit::SetHealth(uint64 val)
 {
-    if (getDeathState() == JUST_DIED)
+    if (getDeathState() == JUST_DIED || getDeathState() == CORPSE)
         val = 0;
     else if (GetTypeId() == TYPEID_PLAYER && getDeathState() == DEAD)
         val = 1;
