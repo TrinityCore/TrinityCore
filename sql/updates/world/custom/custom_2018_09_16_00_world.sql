8666fe27b5afceeb151f71d51873a230a6aaf61c
Ovahlord <dreadkiller@gmx.de>
2018-09-16 00:30:43 +0200

    DB: added missing delete statement from last commit

--- sql/updates/world/custom/custom_2018_09_16_00_world.sql
+++ sql/updates/world/custom/custom_2018_09_16_00_world.sql
@@ -1,4 +1,5 @@
-UPDATE `creature_template_addon` SET `auras`= '' WHERE `entry` IN (44687, 44600, 46227, 46228, 46229);
+UPDATE `creature_template_addon` SET `auras`= '' WHERE `entry` IN (44687, 44600);
+DELETE FROM `creature_template_addon` WHERE `entry` IN (46227, 46228, 46229);
 INSERT INTO `creature_template_addon` (`entry`, `bytes1`, `bytes2`) VALUES
 (46227, 50331648, 1),
 (46228, 50331648, 1),
