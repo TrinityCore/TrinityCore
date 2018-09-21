7634a57f64a38e1151d648eeacae8480c83d19df
jackpoz <giacomopoz@gmail.com>
2018-09-21 17:56:20 +0200

    Core/SAI: Add a 4th target parameter to SAI

--- src/server/game/AI/SmartScripts/SmartScriptMgr.h
+++ src/server/game/AI/SmartScripts/SmartScriptMgr.h
@@ -1205,12 +1205,13 @@ enum SMARTAI_TARGETS
 
 struct SmartTarget
 {
-    SmartTarget (SMARTAI_TARGETS t = SMART_TARGET_NONE, uint32 p1 = 0, uint32 p2 = 0, uint32 p3 = 0)
+    SmartTarget (SMARTAI_TARGETS t = SMART_TARGET_NONE, uint32 p1 = 0, uint32 p2 = 0, uint32 p3 = 0, uint32 p4 = 0)
     {
         type = t;
         raw.param1 = p1;
         raw.param2 = p2;
         raw.param3 = p3;
+        raw.param4 = p4;
         x = 0.0f;
         y = 0.0f;
         z = 0.0f;
@@ -1322,6 +1323,7 @@ struct SmartTarget
             uint32 param1;
             uint32 param2;
             uint32 param3;
+            uint32 param4;
         } raw;
 
         struct
