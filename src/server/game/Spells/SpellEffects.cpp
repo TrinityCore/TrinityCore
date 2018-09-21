868f810e11524401175b45190c2e4d947fc28cff
Killyana <morphone1@gmail.com>
2018-09-21 15:29:34 +0200

    DB/Quest: Hah...You're Not So Big Now!
    Closes #22478

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -3924,9 +3924,6 @@ void Spell::EffectScriptEffect(SpellEffIndex effIndex)
                     if (!unitTarget || unitTarget->GetTypeId() != TYPEID_UNIT)
                         return;
 
-                    if (roll_chance_i(50))                  // chance unknown, using 50
-                        return;
-
                     static uint32 const spellPlayer[5] =
                     {
                         45674,                            // Bigger!
