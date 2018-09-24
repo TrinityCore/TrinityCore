7c694dcf635c74cab77ed49163c41776bba72b11
Ovahlord <dreadkiller@gmx.de>
2018-09-24 20:54:16 +0200

    DB/MountHyjal: added some default phasing arround the Shrine of Aessina

---
+++ sql/updates/world/custom/custom_2018_09_24_03_world.sql
@@ -0,0 +1,9 @@
+DELETE FROM `phase_area` WHERE `AreaId` IN (4989, 5022, 5013, 5014, 5016, 5018, 5019);
+INSERT INTO `phase_area` (`AreaId`, `PhaseId`, `Comment`) VALUES
+(4989, 194, 'Mount Hyjal - Grove of Aessina - Phase 194'),
+(5022, 194, 'Mount Hyjal - Rim of the World - Phase 194'),
+(5013, 194, 'Mount Hyjal - Blackhorn''s Penance - Phase 194'),
+(4861, 194, 'Mount Hyjal - The Regrowth - Phase 194'),
+(5016, 194, 'Mount Hyjal - Whistling Grove - Phase 194'),
+(5018, 194, 'Mount Hyjal - Ashen Lake - Phase 194'),
+(5019, 194, 'Mount Hyjal - Sanctuary of Malorne - Phase 194');
