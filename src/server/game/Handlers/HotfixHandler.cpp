c29c450baa7d6e02eb9280f7f64bbece7b263b84
Palabola <Palabola@users.noreply.github.com>
2018-10-03 14:38:43 +0200

    Revert "Revert "smsg_db_reply removed""
    This reverts commit b657ba88de68eaaf7cb8128e383fda945c1fd0c7.

--- src/server/game/Handlers/HotfixHandler.cpp
+++ src/server/game/Handlers/HotfixHandler.cpp
@@ -25,6 +25,7 @@
 
 void WorldSession::HandleDBQueryBulk(WorldPackets::Hotfix::DBQueryBulk& dbQuery)
 {
+    return; 
     DB2StorageBase const* store = sDB2Manager.GetStorage(dbQuery.TableHash);
     if (!store)
     {
