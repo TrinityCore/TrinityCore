4789b633221e4d9ca82df26c3202f57a341275a5
AlcDenat <alcdenat@hotmail.com>
2018-10-08 23:34:31 +0200

    `

--- sql/updates/world/3.3.5/9999_99_99_99_world_335.sql
+++ sql/updates/world/3.3.5/9999_99_99_99_world_335.sql
@@ -16,9 +16,9 @@ CREATE TABLE `quest_request_items_locale` (
   PRIMARY KEY (`ID`,`locale`)
 );
 
-INSERT INTO quest_request_items_locale (ID, locale, CompletionText, VerifiedBuild) SELECT ID, locale, RequestItemsText, VerifiedBuild from quest_template_locale WHERE RequestItemsText != ""; -- Migrate data to new table
-INSERT INTO quest_offer_reward_locale (ID, locale, RewardText, VerifiedBuild) SELECT ID, locale, OfferRewardText, VerifiedBuild from quest_template_locale WHERE OfferRewardText != ""; -- Migrate data to new table
+INSERT INTO `quest_request_items_locale` (`ID`, `locale`, `CompletionText`, `VerifiedBuild`) SELECT `ID`, `locale`, `RequestItemsText`, `VerifiedBuild` from `quest_template_locale` WHERE `RequestItemsText` != ""; -- Migrate data to new table
+INSERT INTO `quest_offer_reward_locale` (`ID`, `locale`, `RewardText`, `VerifiedBuild`) SELECT `ID`, `locale`, `OfferRewardText`, `VerifiedBuild` from `quest_template_locale` WHERE `OfferRewardText` != ""; -- Migrate data to new table
 
-ALTER TABLE quest_template_locale
-  DROP COLUMN RequestItemsText,
-  DROP COLUMN OfferRewardText;
+ALTER TABLE `quest_template_locale`
+  DROP COLUMN `RequestItemsText`,
+  DROP COLUMN `OfferRewardText`;
