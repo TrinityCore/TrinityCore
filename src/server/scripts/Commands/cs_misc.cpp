e58d737a937cab75d892f825da4b01f3a5a1108b
ForesterDev <11771800+ForesterDev@users.noreply.github.com>
2018-10-11 13:36:35 +0400

    Core/Commands: fix .appear command provide wrong undermap position (#22624)
    (cherry picked from commit 1e99f98327189c00e9e5e552d332120f12d38f03)

--- src/server/scripts/Commands/cs_misc.cpp
+++ src/server/scripts/Commands/cs_misc.cpp
@@ -462,7 +462,7 @@ public:
 
             // to point to see at target with same orientation
             float x, y, z;
-            target->GetContactPoint(_player, x, y, z);
+            target->GetClosePoint(x, y, z, _player->GetCombatReach(), 1.0f);
 
             _player->TeleportTo(target->GetMapId(), x, y, z, _player->GetAbsoluteAngle(target), TELE_TO_GM_MODE);
             PhasingHandler::InheritPhaseShift(_player, target);
