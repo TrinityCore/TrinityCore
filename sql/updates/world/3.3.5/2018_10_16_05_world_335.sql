f271caee3a3b42dfba5290acf329274ae9f49317
Killyana <morphone1@gmail.com>
2018-10-17 00:44:14 +0200

    DB/Creature: Fix a type in a previous commit
    Water Elemental immunity
    Ref #22642

---
+++ sql/updates/world/3.3.5/2018_10_16_05_world_335.sql
@@ -0,0 +1,3 @@
+-- 
+UPDATE `creature_template` SET `spell_school_immune_mask`=0 WHERE `entry`=501;
+UPDATE `creature_template` SET `spell_school_immune_mask`=`spell_school_immune_mask`|16 WHERE `entry`=510;
