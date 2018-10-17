3f7608c496128024af3e11102361d0609eafb0a3
Aokromes <Aokromes@users.noreply.github.com>
2018-10-17 19:33:01 +0200

    fix typo

--- sql/updates/world/3.3.5/2018_10_17_00_world_335.sql
+++ sql/updates/world/3.3.5/2018_10_17_00_world_335.sql
@@ -8,4 +8,4 @@ DELETE FROM `gossip_menu_option` WHERE `MenuId` IN (6045,6021,6101,21334,21333,2
 INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionText`, `OptionBroadcastTextID`, `OptionType`, `OptionNpcFlag`, `ActionMenuID`, `ActionPoiID`, `BoxCoded`, `BoxMoney`, `BoxText`, `BoxBroadcastTextID`, `VerifiedBuild`) VALUES
 (6021, 0, 0, "I cannot Vaelastraz! Surely something can be done to heal you!", 9847, 1, 1, 6101, 0, 0, 0, "", 0, 0),
 (6101, 0, 0, "Vaelastraz,, no!!", 10011, 1, 1, 0, 0, 0, 0, "", 0, 0);
-UPDATE `creature_template` SET `gossip_menu_id`=6021 WHERE `entry`=13020
+UPDATE `creature_template` SET `gossip_menu_id`=6021 WHERE `entry`=13020;
