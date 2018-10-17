9530ce4a2e140efe878a00e0ac1b904e9daa3882
Killyana <morphone1@gmail.com>
2018-10-17 17:04:12 +0200

    DB/Creature: Update Private Weeks with the correct gossip menu ID

---
+++ sql/updates/world/3.3.5/2018_10_17_03_world_335.sql
@@ -0,0 +1,8 @@
+-- 
+DELETE FROM `gossip_menu` WHERE `MenuId` IN (21253);
+DELETE FROM `gossip_menu_option` WHERE `MenuId` IN (7771,21253);
+INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionText`, `OptionBroadcastTextID`, `OptionType`, `OptionNpcFlag`, `ActionMenuID`, `ActionPoiID`, `BoxCoded`, `BoxMoney`, `BoxText`, `BoxBroadcastTextID`, `VerifiedBuild`) VALUES
+(7771, 0, 0, "Private Weeks, I need another disguise.", 15788, 1, 1, 0, 0, 0, 0, "", 0, 0);
+
+UPDATE `creature_template` SET `gossip_menu_id`=7771 WHERE `entry`=18715;
+UPDATE `smart_scripts` SET `event_param1`=7771 WHERE `entryorguid` IN (18715) AND `source_type`=0 AND `id`=2;
