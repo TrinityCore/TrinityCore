d744c075e6221b27439b8a4286841ac43fe38ee4
Killyana <morphone1@gmail.com>
2018-10-17 21:16:26 +0200

    DB: Typo

--- sql/updates/world/3.3.5/2018_10_17_04_world_335.sql
+++ sql/updates/world/3.3.5/2018_10_17_04_world_335.sql
@@ -8,11 +8,11 @@ INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionTex
 (5713, 0, 0, "Prepare to meet your maker.", 9362, 1, 1, 0, 0, 0, 0, "", 0, 0);
 
 DELETE FROM `gossip_menu` WHERE `MenuID`=5709 AND `TextID` IN (6878,6879,6880,6881);
-UPDATE ``smart_scripts` SET `action_param1`=5709, `action_param2`=6877 WHERE `entryorguid`=14354 AND `source_type`=0 AND `id`=1;
-UPDATE ``smart_scripts` SET `action_param1`=5710, `action_param2`=6878 WHERE `entryorguid`=14354 AND `source_type`=0 AND `id`=2;
-UPDATE ``smart_scripts` SET `action_param1`=5711, `action_param2`=6879 WHERE `entryorguid`=14354 AND `source_type`=0 AND `id`=3;
-UPDATE ``smart_scripts` SET `action_param1`=5712, `action_param2`=6880 WHERE `entryorguid`=14354 AND `source_type`=0 AND `id`=4;
-UPDATE ``smart_scripts` SET `action_param1`=5713, `action_param2`=6881 WHERE `entryorguid`=14354 AND `source_type`=0 AND `id`=5;
+UPDATE `smart_scripts` SET `action_param1`=5709, `action_param2`=6877 WHERE `entryorguid`=14354 AND `source_type`=0 AND `id`=1;
+UPDATE `smart_scripts` SET `action_param1`=5710, `action_param2`=6878 WHERE `entryorguid`=14354 AND `source_type`=0 AND `id`=2;
+UPDATE `smart_scripts` SET `action_param1`=5711, `action_param2`=6879 WHERE `entryorguid`=14354 AND `source_type`=0 AND `id`=3;
+UPDATE `smart_scripts` SET `action_param1`=5712, `action_param2`=6880 WHERE `entryorguid`=14354 AND `source_type`=0 AND `id`=4;
+UPDATE `smart_scripts` SET `action_param1`=5713, `action_param2`=6881 WHERE `entryorguid`=14354 AND `source_type`=0 AND `id`=5;
 
 UPDATE `smart_scripts` SET `event_param1`=5709 WHERE `entryorguid`=14354 AND `source_type`=0 AND `id`=6;
 UPDATE `smart_scripts` SET `event_param1`=5710 WHERE `entryorguid`=14354 AND `source_type`=0 AND `id`=7;
