60847bb535f7af416e404e824dc87ae6f85ca4f8
Ovahlord <dreadkiller@gmx.de>
2018-10-27 19:55:44 +0200

    DB: corrected a wrong entry from last commit

--- sql/updates/world/custom/custom_2018_10_27_02_world.sql
+++ sql/updates/world/custom/custom_2018_10_27_02_world.sql
@@ -1,5 +1,5 @@
 -- light trash npc's (level 85, normal rank)
-UPDATE `creature_template` SET `DamageModifier`= 8, `BaseVariance`= 0.5, `mingold`= 5000, `maxgold`= 9000 WHERE `entry` IN (48842, 47132);
+UPDATE `creature_template` SET `DamageModifier`= 8, `BaseVariance`= 0.5, `mingold`= 5000, `maxgold`= 9000 WHERE `entry` IN (48842, 49125);
 
 -- generic trash npc's (level 85, elite rank)
 UPDATE `creature_template` SET `DamageModifier`= 30, `BaseVariance`= 0.5, `mingold`= 5000, `maxgold`= 9000 WHERE `entry` IN (48851, 48942, 48967, 48968, 48973, 48981, 48985, 49059, 49130, 49184, 49186, 49164);
