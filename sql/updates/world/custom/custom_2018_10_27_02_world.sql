e420773a302a4b9352467296bb972e9e0a6308f3
Ovahlord <dreadkiller@gmx.de>
2018-10-27 19:52:42 +0200

    DB/SFK: updated damage and gold drop values for all heroic trash entries

---
+++ sql/updates/world/custom/custom_2018_10_27_02_world.sql
@@ -0,0 +1,8 @@
+-- light trash npc's (level 85, normal rank)
+UPDATE `creature_template` SET `DamageModifier`= 8, `BaseVariance`= 0.5, `mingold`= 5000, `maxgold`= 9000 WHERE `entry` IN (48842, 47132);
+
+-- generic trash npc's (level 85, elite rank)
+UPDATE `creature_template` SET `DamageModifier`= 30, `BaseVariance`= 0.5, `mingold`= 5000, `maxgold`= 9000 WHERE `entry` IN (48851, 48942, 48967, 48968, 48973, 48981, 48985, 49059, 49130, 49184, 49186, 49164);
+UPDATE `creature_template` SET `DamageModifier`= 30, `BaseVariance`= 0.5, `mingold`= 3000, `maxgold`= 6000 WHERE `entry` IN (49126, 49174);
+UPDATE `creature_template` SET `DamageModifier`= 30, `BaseVariance`= 0.5, `mingold`= 10000, `maxgold`= 13000 WHERE `entry` IN (49155, 49160);
+UPDATE `creature_template` SET `minlevel`= 86, `maxlevel`= 86, `DamageModifier`= 60, `BaseVariance`= 0.5, `mingold`= 3000, `maxgold`= 6000 WHERE `entry` IN (49187, 48988, 48919);
