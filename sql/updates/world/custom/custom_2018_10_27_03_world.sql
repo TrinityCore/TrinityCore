3413063b6e9a6087732a4c0112002c887f389b05
Ovahlord <dreadkiller@gmx.de>
2018-10-27 23:54:07 +0200

    DB/VP: added difficulty entries, damage and gold values for all Vortex Pinnacle trash npc's

---
+++ sql/updates/world/custom/custom_2018_10_27_03_world.sql
@@ -0,0 +1,39 @@
+-- Gust Soldier
+UPDATE `creature_template` SET `mingold`= 7000, `maxgold`= 13000, `DamageModifier`= 15, `BaseVariance`= 0.5, `difficulty_entry_1`= 45478 WHERE `entry`= 45477;
+UPDATE `creature_template` SET `mingold`= 7000, `maxgold`= 13000, `DamageModifier`= 30, `BaseVariance`= 0.5, `minlevel`= 85, `maxlevel`= 85,`exp`= 3, `faction`= 16, `speed_walk`= 1.55556, `unit_flags`= 32832 WHERE `entry`= 45478;
+-- Wild Vortex
+UPDATE `creature_template` SET `mingold`= 7000, `maxgold`= 13000, `DamageModifier`= 15, `BaseVariance`= 0.5, `difficulty_entry_1`= 45913 WHERE `entry`= 45912;
+UPDATE `creature_template` SET `mingold`= 7000, `maxgold`= 13000, `DamageModifier`= 30, `BaseVariance`= 0.5, `minlevel`= 85, `maxlevel`= 85,`exp`= 3, `faction`= 16, `speed_walk`= 1.55556, `unit_flags`= 32832, `unit_class`= 2 WHERE `entry`= 45913;
+-- Armored Mistral
+UPDATE `creature_template` SET `mingold`= 7000, `maxgold`= 13000, `DamageModifier`= 15, `BaseVariance`= 0.5, `difficulty_entry_1`= 45916 WHERE `entry`= 45915;
+UPDATE `creature_template` SET `mingold`= 7000, `maxgold`= 13000, `DamageModifier`= 30, `BaseVariance`= 0.5, `minlevel`= 85, `maxlevel`= 85,`exp`= 3, `faction`= 16, `speed_run`= 0.595239, `unit_flags`= 32832, `modelid1`= 33828 WHERE `entry`= 45916;
+-- Lurking Tempest
+UPDATE `creature_template` SET `DamageModifier`= 15, `BaseVariance`= 0.5, `difficulty_entry_1`= 46450 WHERE `entry`= 45704;
+UPDATE `creature_template` SET `DamageModifier`= 30, `BaseVariance`= 0.5, `minlevel`= 85, `maxlevel`= 85,`exp`= 3, `faction`= 14, `speed_walk`= 1.6, `speed_run`= 0.571429, `unit_flags`= 32832, `unit_flags2`= 0 WHERE `entry`= 46450;
+-- Cloud Prince
+UPDATE `creature_template` SET `mingold`= 7000, `maxgold`= 13000, `DamageModifier`= 15, `BaseVariance`= 0.5, `difficulty_entry_1`= 45918 WHERE `entry`= 45917;
+UPDATE `creature_template` SET `mingold`= 7000, `maxgold`= 13000, `DamageModifier`= 30, `BaseVariance`= 0.5, `minlevel`= 85, `maxlevel`= 85,`exp`= 3, `faction`= 16, `speed_walk`= 1.55556, `unit_flags`= 32832, `unit_class`= 2 WHERE `entry`= 45918;
+-- Turbolent Squall
+UPDATE `creature_template` SET `mingold`= 7000, `maxgold`= 13000, `DamageModifier`= 15, `BaseVariance`= 0.5, `difficulty_entry_1`= 45925 WHERE `entry`= 45924;
+UPDATE `creature_template` SET `mingold`= 7000, `maxgold`= 13000, `DamageModifier`= 30, `BaseVariance`= 0.5, `minlevel`= 85, `maxlevel`= 85,`exp`= 3, `faction`= 16, `speed_walk`= 1.55556, `unit_flags`= 32832, `unit_class`= 2 WHERE `entry`= 45925;
+-- Empyrian Assassin
+UPDATE `creature_template` SET `mingold`= 7000, `maxgold`= 13000, `DamageModifier`= 15, `BaseVariance`= 0.5, `difficulty_entry_1`= 45923 WHERE `entry`= 45922;
+UPDATE `creature_template` SET `mingold`= 7000, `maxgold`= 13000, `DamageModifier`= 30, `BaseVariance`= 0.5, `minlevel`= 85, `maxlevel`= 85,`exp`= 3, `faction`= 16, `unit_flags`= 32832 WHERE `entry`= 45923;
+-- Young Storm Dragon
+UPDATE `creature_template` SET `mingold`= 7000, `maxgold`= 13000, `DamageModifier`= 15, `BaseVariance`= 0.5, `difficulty_entry_1`= 45920 WHERE `entry`= 45919;
+UPDATE `creature_template` SET `mingold`= 7000, `maxgold`= 13000, `DamageModifier`= 30, `BaseVariance`= 0.5, `minlevel`= 85, `maxlevel`= 85,`exp`= 3, `faction`= 16, `speed_walk`= 1.6, `unit_flags`= 32832, `InhabitType`= 7 WHERE `entry`= 45920;
+-- Executor of the Caliph
+UPDATE `creature_template` SET `mingold`= 7000, `maxgold`= 13000, `DamageModifier`= 15, `BaseVariance`= 0.5, `difficulty_entry_1`= 45929 WHERE `entry`= 45928;
+UPDATE `creature_template` SET `mingold`= 7000, `maxgold`= 13000, `DamageModifier`= 30, `BaseVariance`= 0.5, `minlevel`= 85, `maxlevel`= 85,`exp`= 3, `faction`= 16, `speed_walk`= 0.666668, `unit_flags`= 32832 WHERE `entry`= 45929;
+-- Minister of Air
+UPDATE `creature_template` SET `mingold`= 7000, `maxgold`= 13000, `DamageModifier`= 15, `BaseVariance`= 0.5, `difficulty_entry_1`= 45931 WHERE `entry`= 45930;
+UPDATE `creature_template` SET `mingold`= 7000, `maxgold`= 13000, `DamageModifier`= 30, `BaseVariance`= 0.5, `minlevel`= 85, `maxlevel`= 85,`exp`= 3, `faction`= 16, `speed_walk`= 0.666668, `unit_flags`= 32832, `unit_class`= 8 WHERE `entry`= 45931;
+-- Temple Adept
+UPDATE `creature_template` SET `mingold`= 7000, `maxgold`= 13000, `DamageModifier`= 15, `BaseVariance`= 0.5, `difficulty_entry_1`= 45936 WHERE `entry`= 45935;
+UPDATE `creature_template` SET `mingold`= 7000, `maxgold`= 13000, `DamageModifier`= 30, `BaseVariance`= 0.5, `minlevel`= 85, `maxlevel`= 85,`exp`= 3, `faction`= 16, `speed_walk`= 0.666668, `unit_flags`= 32832, `unit_class`= 2 WHERE `entry`= 45936;
+-- Servant of Asaad
+UPDATE `creature_template` SET `mingold`= 7000, `maxgold`= 13000, `DamageModifier`= 15, `BaseVariance`= 0.5, `difficulty_entry_1`= 45927 WHERE `entry`= 45926;
+UPDATE `creature_template` SET `mingold`= 7000, `maxgold`= 13000, `DamageModifier`= 30, `BaseVariance`= 0.5, `minlevel`= 85, `maxlevel`= 85,`exp`= 3, `faction`= 16, `speed_walk`= 0.666668, `unit_flags`= 32832, `unit_class`= 2 WHERE `entry`= 45927;
+-- Skyfall Star
+UPDATE `creature_template` SET `difficulty_entry_1`= 45933 WHERE `entry`= 45932;
+UPDATE `creature_template` SET `minlevel`= 85, `maxlevel`= 85,`exp`= 3, `faction`= 16, `speed_walk`= 0.8, `speed_run`= 0.285714, `unit_flags`= 32832, `unit_flags2`= 0, `InhabitType`= 7 WHERE `entry`= 45933;
