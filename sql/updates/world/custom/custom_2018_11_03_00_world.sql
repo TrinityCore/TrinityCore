4e955af00b7f870fc84f2dee1ef6e2468ebe1653
Ovahlord <dreadkiller@gmx.de>
2018-11-03 19:39:20 +0100

    DB/Players: since 4.3 the required experience for level 71 up to level 80 has been reduced by roughly 33%

---
+++ sql/updates/world/custom/custom_2018_11_03_00_world.sql
@@ -0,0 +1,10 @@
+UPDATE `player_xp_for_level` SET `Experience`= 813000 WHERE `Level`= 70;
+UPDATE `player_xp_for_level` SET `Experience`= 821000 WHERE `Level`= 71;
+UPDATE `player_xp_for_level` SET `Experience`= 830000 WHERE `Level`= 72;
+UPDATE `player_xp_for_level` SET `Experience`= 838000 WHERE `Level`= 73;
+UPDATE `player_xp_for_level` SET `Experience`= 847000 WHERE `Level`= 74;
+UPDATE `player_xp_for_level` SET `Experience`= 855000 WHERE `Level`= 75;
+UPDATE `player_xp_for_level` SET `Experience`= 865000 WHERE `Level`= 76;
+UPDATE `player_xp_for_level` SET `Experience`= 873000 WHERE `Level`= 77;
+UPDATE `player_xp_for_level` SET `Experience`= 882000 WHERE `Level`= 78;
+UPDATE `player_xp_for_level` SET `Experience`= 891000 WHERE `Level`= 79;
