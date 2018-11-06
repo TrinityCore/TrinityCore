cad325a311cfd85abe07bad327e63b82a695df55
Palabola <Palabola@users.noreply.github.com>
2018-11-06 15:14:21 +0100

    Update sql/updates/world/master/2018_11_06_00_world.sql

---
+++ sql/updates/world/master/2018_11_06_00_world.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `conditions` CHANGE `ConditionValue1` `ConditionValue1` BIGINT(20) UNSIGNED NOT NULL DEFAULT '0';
+ALTER TABLE `conditions` CHANGE `ConditionValue2` `ConditionValue2` BIGINT(20) UNSIGNED NOT NULL DEFAULT '0';
+ALTER TABLE `conditions` CHANGE `ConditionValue3` `ConditionValue3` BIGINT(20) UNSIGNED NOT NULL DEFAULT '0';
