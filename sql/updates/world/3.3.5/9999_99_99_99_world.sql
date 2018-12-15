bbec9032443d09589c2416763ce05644f3ce86c2
Rynem Storm <45861232+Rynem@users.noreply.github.com>
2018-12-15 16:35:56 +0100

    Fix mistake, thanks to Keader

--- sql/updates/world/3.3.5/9999_99_99_99_world.sql
+++ sql/updates/world/3.3.5/9999_99_99_99_world.sql
@@ -1,5 +1,1 @@
-DELETE FROM `spell_proc` WHERE `SpellId`=64568;
-INSERT INTO `spell_proc` (`SpellId`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `ProcFlags`, `SpellTypeMask`, `SpellPhaseMask`, `HitMask`, `AttributesMask`, `ProcsPerMinute`, `Chance`, `Cooldown`, `Charges`) VALUES
-(64568, 0, 0, 0, 0, 0, 0x100000, 0, 0, 0, 0, 0, 0, 0, 0);
-
 UPDATE `spell_bonus_data` SET `ap_bonus`=0, `ap_dot_bonus`=0 WHERE `entry`=64569;
