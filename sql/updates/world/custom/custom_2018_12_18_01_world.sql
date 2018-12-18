89c1d2e2d1ad607ebe72a66a04ffe3c1196b0a2f
Ovahlord <dreadkiller@gmx.de>
2018-12-18 12:38:55 +0100

    DB/TotFW: added interrupt immunity to the Conclave of Wind bosses

---
+++ sql/updates/world/custom/custom_2018_12_18_01_world.sql
@@ -0,0 +1,1 @@
+UPDATE `creature_template` SET `mechanic_immune_mask`= `mechanic_immune_mask` | 33554432 WHERE `entry` IN (45870, 50103, 50113, 50123, 45871, 50098, 50108, 50118, 45872, 50095, 50105, 50115);
