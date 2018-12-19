a9c4137e91dfab5a9f5352c12b648054024eaea9
Rynem <Rynem@users.noreply.github.com>
2018-12-19 11:59:04 +0100

    DB/Spell: Necrocution
    Closes #22865
    
    (cherry picked from commit aefc96f2d8fe7d0cc6dd0bf579525f206e1a1bf7)

---
+++ sql/updates/world/master/2021_11_16_45_world_2018_12_19_02_world.sql
@@ -0,0 +1,4 @@
+-- 
+DELETE FROM `spell_script_names` WHERE `spell_id`=63233 AND `ScriptName`="spell_gen_break_shield";
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(63233, "spell_gen_break_shield");
