4be60643beb3200efb339d2d1842108e775a8e70
Killyana <morphone1@gmail.com>
2018-12-26 19:32:44 +0100

    DB/Spell: Mounting a Stabled Campaign Warhorse
    Closes #22866

---
+++ sql/updates/world/3.3.5/2018_12_26_00_world.sql
@@ -0,0 +1,4 @@
+-- 
+DELETE FROM `spell_script_names` WHERE `spell_id`=63215 AND `ScriptName`="spell_gen_summon_tournament_mount";
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(63215, "spell_gen_summon_tournament_mount");
