1c07e946664437cdd21d593331596d6834b6b0c0
Ovahlord <dreadkiller@gmx.de>
2019-01-02 15:49:15 +0100

    DB/Creatures: corrected some inhabit types for creatures in Uldum and Dragon Soul

---
+++ sql/updates/world/custom/custom_2019_01_02_00_world.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_template` SET `InhabitType`= 4, `flags_extra`= 2 WHERE `entry`= 47546;
+UPDATE `creature_template` SET `InhabitType`= 4 WHERE `entry` IN (45715, 55971, 55913, 56142, 55911);
