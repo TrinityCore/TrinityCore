53fb3d29bf71ed28944c90135e4aa1c2f915a2a2
Ovahlord <dreadkiller@gmx.de>
2019-01-03 19:15:22 +0100

    DB/BoT: correct damage values for all bosses

---
+++ sql/updates/world/custom/custom_2019_01_03_00_world.sql
@@ -0,0 +1,22 @@
+-- Halfus Wyrmbreaker
+UPDATE `creature_template` SET `DamageModifier`= 90 WHERE `entry` IN (44600, 46209, 46210, 46211); -- Halfus Wyrmbreaker
+UPDATE `creature_template` SET `DamageModifier`= 120 WHERE `entry` IN (44826, 46218, 46219, 46220); -- Storm Rider
+UPDATE `creature_template` SET `DamageModifier`= 120 WHERE `entry` IN (44828, 46212, 46213, 46214); -- Nether Scion
+UPDATE `creature_template` SET `DamageModifier`= 120 WHERE `entry` IN (44829, 46215, 46216, 46217); -- Slate Dragon
+UPDATE `creature_template` SET `DamageModifier`= 120 WHERE `entry` IN (44653, 46221, 46222, 46223); -- Time Warden
+UPDATE `creature_template` SET `DamageModifier`= 15 WHERE `entry` IN (44641, 46224, 46225, 46226); -- Orphaned Emerald Whelp
+
+-- Theralion and Valiona
+UPDATE `creature_template` SET `DamageModifier`= 120 WHERE `entry` IN (45992, 49897, 49898, 49899, 45993, 49903, 49904, 49905);
+
+-- Ascendant Council
+UPDATE `creature_template` SET `DamageModifier`= 120 WHERE `entry` IN (43686, 49615, 49616, 49617); -- Ignacious
+UPDATE `creature_template` SET `DamageModifier`= 120 WHERE `entry` IN (43687, 49612, 49613, 49614); -- Feludius
+UPDATE `creature_template` SET `DamageModifier`= 120 WHERE `entry` IN (43688, 49606, 49607, 49608); -- Arion
+UPDATE `creature_template` SET `DamageModifier`= 120 WHERE `entry` IN (43689, 49609, 49610, 49611); -- Terrastra
+UPDATE `creature_template` SET `DamageModifier`= 120 WHERE `entry` IN (43735, 49619, 49620, 49621); -- Eelementium Monstrosity
+
+-- Cho'Gall
+UPDATE `creature_template` SET `DamageModifier`= 120 WHERE `entry` IN (43324, 50131, 50132, 50133); -- Cho'Gall
+UPDATE `creature_template` SET `DamageModifier`= 120 WHERE `entry` IN (43622, 50184, 50185, 50186); -- Corrupting Adherent
+UPDATE `creature_template` SET `DamageModifier`= 15 WHERE `entry` IN (43707, 50168, 50169, 50170); -- Blood of the Old God
