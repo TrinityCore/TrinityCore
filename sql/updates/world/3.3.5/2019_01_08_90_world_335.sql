206f30f2009069077b1991f2a7a888c8ccc4a754
Miorey <clem.balea@gmail.com>
2019-01-09 00:37:24 +0100

    - group quest reward - class quest reward - no xp npc to 0

--- sql/updates/world/3.3.5/2019_01_08_90_world_335.sql
+++ sql/updates/world/3.3.5/2019_01_08_90_world_335.sql
@@ -1,3 +1,4 @@
+-- Add lvl 60 reward
 INSERT INTO `achievement_reward`
 (`ID`,
 `TitleA`,
@@ -17,7 +18,7 @@ VALUES
 'I am so proud of you!$B$B It is so amazing to get you among my friends. This little gift is for our next party.$B$Bkissy kissy!',
 0);
 
-
+-- add lvl 60 reward translate
 INSERT into `world`.`achievement_reward_locale`
 (`ID`,
 `Locale`,
@@ -28,3 +29,22 @@ VALUES(
 'frFR',
 'Coucou',
 'Je suis si fier de toi! $B$B C''est incroyable de t''avoir parmi mes amis. Voici un petit présent pour notre prochaine fête.$B$BBisous bisou.');
+
+
+-- talent reward for group and dungeon quest without daily
+UPDATE quest_template
+SET RewardTalents = 1
+WHERE QuestInfoID IN (1, 81) AND RewardTalents = 0 AND QuestType != 1 AND Flags != 4096
+AND ID NOT IN (4606,4605,4604, 4603, 4602, 4601, 2952, 2951);
+
+
+-- talent reward for class quest without daily
+UPDATE world.quest_template
+SET RewardTalents = 1
+WHERE QuestSortID IN (-61, -81, -82, -141, -161, -162, -261, -262, -263, -372)
+AND RewardTalents = 0 AND MinLevel > 9 AND Flags != 4096;
+
+-- remove price for no experience npcs
+UPDATE world.gossip_menu_option
+SET BoxMoney = 0
+WHERE MenuID = 10638
