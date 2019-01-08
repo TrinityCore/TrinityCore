f94e23ef903b5af3666d76eae4ce76c65b02ccb1
Miorey <clem.balea@gmail.com>
2019-01-08 23:45:08 +0100

    fix db

---
+++ sql/updates/world/3.3.5/2019_01_08_90_world_335.sql
@@ -0,0 +1,30 @@
+INSERT INTO `achievement_reward`
+(`ID`,
+`TitleA`,
+`TitleH`,
+`ItemID`,
+`Sender`,
+`Subject`,
+`Body`,
+`MailTemplateID`)
+VALUES
+(11,
+0,
+0,
+38301,
+18756,
+'My new friend',
+'I am so proud of you!$B$B It is so amazing to get you among my friends. This little gift is for our next party.$B$Bkissy kissy!',
+0);
+
+
+INSERT into `world`.`achievement_reward_locale`
+(`ID`,
+`Locale`,
+`Subject`,
+`Body`)
+VALUES(
+11,
+'frFR',
+'Coucou',
+'Je suis si fier de toi! $B$B C''est incroyable de t''avoir parmi mes amis. Voici un petit présent pour notre prochaine fête.$B$BBisous bisou.');
