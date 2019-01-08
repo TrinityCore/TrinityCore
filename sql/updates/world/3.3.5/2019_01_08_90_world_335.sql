bd86186f3ad268ed04e1779672d25328f4b31960
Miorey <clem.balea@gmail.com>
2019-01-09 00:43:48 +0100

    - fix - add auth

--- sql/updates/world/3.3.5/2019_01_08_90_world_335.sql
+++ sql/updates/world/3.3.5/2019_01_08_90_world_335.sql
@@ -19,7 +19,7 @@ VALUES
 0);
 
 -- add lvl 60 reward translate
-INSERT into `world`.`achievement_reward_locale`
+INSERT into `achievement_reward_locale`
 (`ID`,
 `Locale`,
 `Subject`,
@@ -32,19 +32,19 @@ VALUES(
 
 
 -- talent reward for group and dungeon quest without daily
-UPDATE quest_template
-SET RewardTalents = 1
-WHERE QuestInfoID IN (1, 81) AND RewardTalents = 0 AND QuestType != 1 AND Flags != 4096
+UPDATE `quest_template`
+SET `RewardTalents` = 1
+WHERE `QuestInfoID` IN (1, 81) AND `RewardTalents` = 0 AND `QuestType` != 1 AND `Flags` != 4096
 AND ID NOT IN (4606,4605,4604, 4603, 4602, 4601, 2952, 2951);
 
 
 -- talent reward for class quest without daily
-UPDATE world.quest_template
-SET RewardTalents = 1
-WHERE QuestSortID IN (-61, -81, -82, -141, -161, -162, -261, -262, -263, -372)
-AND RewardTalents = 0 AND MinLevel > 9 AND Flags != 4096;
+UPDATE `quest_template`
+SET `RewardTalents` = 1
+WHERE `QuestSortID` IN (-61, -81, -82, -141, -161, -162, -261, -262, -263, -372)
+AND `RewardTalents` = 0 AND `MinLevel` > 9 AND `Flags` != 4096;
 
 -- remove price for no experience npcs
-UPDATE world.gossip_menu_option
-SET BoxMoney = 0
-WHERE MenuID = 10638
+UPDATE `gossip_menu_option`
+SET `BoxMoney` = 0
+WHERE `MenuID` = 10638
