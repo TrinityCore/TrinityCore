c375208e5b15d20b22bd21892483589c9bd3e1bb
Killyana <morphone1@gmail.com>
2019-01-10 21:58:00 +0100

    Rename spell correctly
    (cherry picked from commit 4c53dd930eb13d9e2df93061ec5fe4a6139e744c)

--- src/server/scripts/EasternKingdoms/Stratholme/stratholme.cpp
+++ src/server/scripts/EasternKingdoms/Stratholme/stratholme.cpp
@@ -200,9 +200,9 @@ public:
 
 enum GhostlyCitizenSpells
 {
-    SPELL_HAUNTING_PHANTOM  = 16336,
-    DEBILITATING_TOUCH      = 16333,
-    SPELL_SLAP              = 6754
+    SPELL_HAUNTING_PHANTOM        = 16336,
+    SPELL_DEBILITATING_TOUCH      = 16333,
+    SPELL_SLAP                    = 6754
 };
 
 class npc_spectral_ghostly_citizen : public CreatureScript
@@ -286,7 +286,7 @@ public:
             if (TouchTimer <= diff)
             {
                 if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0))
-                    DoCast(target, DEBILITATING_TOUCH);
+                    DoCast(target, SPELL_DEBILITATING_TOUCH);
                 TouchTimer = 7000;
             }
             else TouchTimer -= diff;
