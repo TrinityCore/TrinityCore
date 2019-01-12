d00bac924b4eadcc9653425fd8ec41c056a78319
Killyana <morphone1@gmail.com>
2019-01-12 19:27:52 +0100

    DB/Creature: Abyssal Flamebringer
    (cherry picked from commit 7cf7a3fd108d3d36da356c7cac73ea9a8af6adf9)

---
+++ sql/updates/world/master/2021_11_21_22_world_2019_01_12_13_world.sql
@@ -0,0 +1,3 @@
+-- 
+UPDATE `creature` SET `position_x`=1558.8469, `position_y`=7195.6274, `position_z`=363.863 WHERE `guid`=71197; 
+UPDATE `creature` SET `position_x`=1406.1982, `position_y`=7179.0869, `position_z`=363.817 WHERE `guid`=71205; 
