32bbec9e2383b1f8d9135f9c957480f4b21fb74d
Jonas Kervin Hansen <jonas.kerwin.hansen@gmail.com>
2019-01-16 16:30:11 +0100

    Increased stat value cap

---
+++ sql/custom/world/increase_stats_values.sql
@@ -0,0 +1,11 @@
+ALTER TABLE `item_template` 
+CHANGE COLUMN `stat_value1` `stat_value1` INT(10) NOT NULL DEFAULT '0' ,
+CHANGE COLUMN `stat_value2` `stat_value2` INT(10) NOT NULL DEFAULT '0' ,
+CHANGE COLUMN `stat_value3` `stat_value3` INT(10) NOT NULL DEFAULT '0' ,
+CHANGE COLUMN `stat_value4` `stat_value4` INT(10) NOT NULL DEFAULT '0' ,
+CHANGE COLUMN `stat_value5` `stat_value5` INT(10) NOT NULL DEFAULT '0' ,
+CHANGE COLUMN `stat_value6` `stat_value6` INT(10) NOT NULL DEFAULT '0' ,
+CHANGE COLUMN `stat_value7` `stat_value7` INT(10) NOT NULL DEFAULT '0' ,
+CHANGE COLUMN `stat_value8` `stat_value8` INT(10) NOT NULL DEFAULT '0' ,
+CHANGE COLUMN `stat_value9` `stat_value9` INT(10) NOT NULL DEFAULT '0' ,
+CHANGE COLUMN `stat_value10` `stat_value10` INT(10) NOT NULL DEFAULT '0' ;
