da9a3770b29c1affc5f1506e6d31575d348da5bb
Ovahlord <dreadkiller@gmx.de>
2019-01-28 20:00:33 +0100

    Scripts/MGT: added a missing override to instance script

--- src/server/scripts/EasternKingdoms/MagistersTerrace/instance_magisters_terrace.cpp
+++ src/server/scripts/EasternKingdoms/MagistersTerrace/instance_magisters_terrace.cpp
@@ -79,7 +79,7 @@ class instance_magisters_terrace : public InstanceMapScript
                 Initialize();
             }
 
-            void Initialize()
+            void Initialize() override
             {
                 _delrissaDeathCount = 0;
                 _kaelthasIntroState = 0;
