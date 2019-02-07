d997bb6d28d8a3ea7f9735333af9abb44fe94636
Ovahlord <dreadkiller@gmx.de>
2019-02-07 04:21:14 +0100

    DB/ZG: updated more difficulty entries and damage values for Zul'Gurub npc's

---
+++ sql/updates/world/custom/custom_2019_02_07_01_world.sql
@@ -0,0 +1,83 @@
+-- Kaulema the Mover
+UPDATE `creature_template` SET `difficulty_entry_1`= 52515, `flags_extra`= 0x40000000, `mechanic_immune_mask`= 617299803 WHERE `entry`= 52422;
+UPDATE `creature_template` SET `minlevel`= 86, `maxlevel`= 86, `exp`= 3, `faction`= 16, `speed_walk`= 0.666668, `unit_flags`= 64, `lootid`= 52422, `DamageModifier`= 120, `BaseVariance`= 0.5, `mingold`= 10000, `maxgold`= 15000, `flags_extra`= 0x40000000, `mechanic_immune_mask`= 617299803 WHERE `entry`= 52515;
+-- Mr'Lek the Dismantler
+UPDATE `creature_template` SET `difficulty_entry_1`= 52514, `flags_extra`= 0x40000000, `mechanic_immune_mask`= 617299803 WHERE `entry`= 52405;
+UPDATE `creature_template` SET `minlevel`= 86, `maxlevel`= 86, `exp`= 3, `faction`= 16, `speed_walk`= 0.666668, `unit_flags`= 64, `lootid`= 52405, `DamageModifier`= 120, `BaseVariance`= 0.5, `mingold`= 10000, `maxgold`= 15000, `flags_extra`= 0x40000000, `mechanic_immune_mask`= 617299803 WHERE `entry`= 52514;
+-- Chosen of Hethiss
+UPDATE `creature_template` SET `difficulty_entry_1`= 52513, `flags_extra`= 0x40000000, `mechanic_immune_mask`= 617299803 WHERE `entry`= 52323;
+UPDATE `creature_template` SET `minlevel`= 85, `maxlevel`= 85, `exp`= 3, `faction`= 16, `unit_flags`= 64, `lootid`= 52323, `DamageModifier`= 25, `BaseVariance`= 0.5, `mingold`= 3000, `maxgold`= 5000, `flags_extra`= 0x40000000, `mechanic_immune_mask`= 617299803 WHERE `entry`= 52513;
+-- Witch Doctor Qu'in
+UPDATE `creature_template` SET `difficulty_entry_1`= 52512, `flags_extra`= 0x40000000, `mechanic_immune_mask`= 617299803 WHERE `entry`= 52322;
+UPDATE `creature_template` SET `minlevel`= 86, `maxlevel`= 86, `exp`= 3, `faction`= 16, `unit_flags`= 64, `lootid`= 52322, `DamageModifier`= 120, `BaseVariance`= 0.5, `mingold`= 10000, `maxgold`= 15000, `flags_extra`= 0x40000000, `mechanic_immune_mask`= 617299803 WHERE `entry`= 52512;
+-- Mortaxx
+UPDATE `creature_template` SET `difficulty_entry_1`= 52506, `flags_extra`= 0x40000000, `mechanic_immune_mask`= 617299803 WHERE `entry`= 52438;
+UPDATE `creature_template` SET `minlevel`= 86, `maxlevel`= 86, `exp`= 3, `faction`= 14, `speed_run`= 0.992063, `unit_flags`= 64, `lootid`= 52438, `DamageModifier`= 120, `BaseVariance`= 0.5, `mingold`= 10000, `maxgold`= 15000, `flags_extra`= 0x40000000, `mechanic_immune_mask`= 617299803 WHERE `entry`= 52506;
+-- Gurubashi Berserker
+UPDATE `creature_template` SET `difficulty_entry_1`= 52144, `flags_extra`= 0x40000000, `mechanic_immune_mask`= 617299803 WHERE `entry`= 52077;
+UPDATE `creature_template` SET `minlevel`= 85, `maxlevel`= 85, `exp`= 3, `faction`= 28, `speed_walk`= 1.6, `speed_run`= 0.571429, `unit_flags`= 64, `lootid`= 52077, `DamageModifier`= 120, `BaseVariance`= 0.5, `mingold`= 10000, `maxgold`= 15000, `flags_extra`= 0x40000000, `mechanic_immune_mask`= 617299803 WHERE `entry`= 52144;
+-- Pride of Bethekk
+UPDATE `creature_template` SET `difficulty_entry_1`= 52398 WHERE `entry`= 52345;
+UPDATE `creature_template` SET `minlevel`= 85, `maxlevel`= 85, `exp`= 3, `faction`= 16, `speed_walk`= 2, `speed_run`= 1, `unit_flags`= 32768, `lootid`= 52345, `DamageModifier`= 30, `BaseVariance`= 0.5 WHERE `entry`= 52398;
+-- Pride of Bethekk
+UPDATE `creature_template` SET `difficulty_entry_1`= 52397 WHERE `entry`= 52061;
+UPDATE `creature_template` SET `minlevel`= 85, `maxlevel`= 85, `exp`= 3, `faction`= 14, `speed_walk`= 4.4, `speed_run`= 1.57143, `unit_flags`= 32768, `DamageModifier`= 30, `BaseVariance`= 0.5 WHERE `entry`= 52397;
+-- Gurubashi Shadow Hunter
+UPDATE `creature_template` SET `difficulty_entry_1`= 52394 WHERE `entry`= 52327;
+UPDATE `creature_template` SET `minlevel`= 85, `maxlevel`= 85, `exp`= 3, `faction`= 16, `unit_flags`= 32832, `lootid`= 52327, `DamageModifier`= 60, `BaseVariance`= 0.5, `mingold`= 10000, `maxgold`= 15000 WHERE `entry`= 52394;
+-- Gurubashi Blood Drinker
+UPDATE `creature_template` SET `difficulty_entry_1`= 52396 WHERE `entry`= 52325;
+UPDATE `creature_template` SET `minlevel`= 85, `maxlevel`= 85, `exp`= 3, `faction`= 16, `unit_flags`= 32832, `lootid`= 52325, `DamageModifier`= 60, `BaseVariance`= 0.5, `mingold`= 10000, `maxgold`= 15000 WHERE `entry`= 52396;
+-- Lesser Priest of Bethekk
+UPDATE `creature_template` SET `difficulty_entry_1`= 52395 WHERE `entry`= 52339;
+UPDATE `creature_template` SET `minlevel`= 85, `maxlevel`= 85, `exp`= 3, `faction`= 16, `unit_class`= 2, `unit_flags`= 32832, `lootid`= 52339, `DamageModifier`= 60, `BaseVariance`= 0.5, `mingold`= 10000, `maxgold`= 15000 WHERE `entry`= 52395;
+-- Rezzashi Adder
+UPDATE `creature_template` SET `difficulty_entry_1`= 52814 WHERE `entry`= 52085;
+UPDATE `creature_template` SET `minlevel`= 85, `maxlevel`= 85, `exp`= 3, `faction`= 16, `unit_flags`= 32832, `lootid`= 52085, `DamageModifier`= 30, `BaseVariance`= 0.5 WHERE `entry`= 52814;
+-- Hakkari Witch Doctor
+UPDATE `creature_template` SET `difficulty_entry_1`= 52691 WHERE `entry`= 52086;
+UPDATE `creature_template` SET `minlevel`= 85, `maxlevel`= 85, `exp`= 3, `faction`= 28, `unit_class`= 2, `unit_flags`= 32832, `lootid`= 52086, `DamageModifier`= 60, `BaseVariance`= 0.5, `mingold`= 10000, `maxgold`= 15000 WHERE `entry`= 52691;
+-- Zanzil Mindslave
+UPDATE `creature_template` SET `difficulty_entry_1`= 52140 WHERE `entry`= 52087;
+UPDATE `creature_template` SET `minlevel`= 85, `maxlevel`= 85, `exp`= 3, `faction`= 37, `unit_flags`= 32832, `DamageModifier`= 15, `BaseVariance`= 0.5 WHERE `entry`= 52140;
+-- Zanzili Berserker
+UPDATE `creature_template` SET `difficulty_entry_1`= 52142, `flags_extra`= 1073742080 WHERE `entry`= 52054;
+UPDATE `creature_template` SET `minlevel`= 85, `maxlevel`= 85, `exp`= 3, `faction`= 107, `speed_walk`= 0.666668, `unit_flags`= 33555264, `DamageModifier`= 120, `BaseVariance`= 0.5, `flags_extra`= 1073742080, `mechanic_immune_mask`= 42154367, `InhabitType`= 4 WHERE `entry`= 52142;
+-- Zanzili Zombie
+UPDATE `creature_template` SET `DamageModifier`= 15 WHERE `entry`= 52055;
+-- Twisted Spirit
+UPDATE `creature_template` SET `difficulty_entry_1`= 52706 WHERE `entry`= 52624;
+UPDATE `creature_template` SET `minlevel`= 85, `maxlevel`= 85, `exp`= 3, `faction`= 14, `speed_walk`= 0.8, `speed_run`= 0.571429, `unit_flags`= 32768, `DamageModifier`= 50, `BaseVariance`= 0.5 WHERE `entry`= 52706;
+-- Gurubashi Spirit Warrior
+UPDATE `creature_template` SET `difficulty_entry_1`= 52677, `mechanic_immune_mask`= 42154367, `flags_extra`= 1073742080 WHERE `entry`= 52167;
+UPDATE `creature_template` SET `minlevel`= 86, `maxlevel`= 86, `exp`= 3, `faction`= 16, `speed_walk`= 0.666668, `speed_run`= 1.42857, `unit_flags`= 32832, `lootid`= 52167, `DamageModifier`= 90, `BaseVariance`= 0.5, `mechanic_immune_mask`= 42154367, `flags_extra`= 1073742080 WHERE `entry`= 52677;
+-- Gurubashi Spirit
+UPDATE `creature_template` SET `mechanic_immune_mask`= 42154367 WHERE `entry`= 52730;
+-- Gub
+UPDATE `creature_template` SET `difficulty_entry_1`= 52517, `mechanic_immune_mask`= 42154367, `flags_extra`= 1073742080 WHERE `entry`= 52440;
+UPDATE `creature_template` SET `minlevel`= 86, `maxlevel`= 86, `exp`= 3, `faction`= 16, `speed_walk`= 0.666668, `unit_flags`= 64, `lootid`= 52440, `DamageModifier`= 120, `BaseVariance`= 0.5, `flags_extra`= 1073742080, `mechanic_immune_mask`= 42154367 WHERE `entry`= 52517;
+-- Tor-Tun
+UPDATE `creature_template` SET `difficulty_entry_1`= 52509, `mechanic_immune_mask`= 42154367, `flags_extra`= 1073742080 WHERE `entry`= 52414;
+UPDATE `creature_template` SET `minlevel`= 86, `maxlevel`= 86, `exp`= 3, `faction`= 16, `speed_run`= 0.992063, `unit_flags`= 64, `lootid`= 52414, `DamageModifier`= 120, `BaseVariance`= 0.5, `flags_extra`= 1073742080, `mechanic_immune_mask`= 42154367 WHERE `entry`= 52509;
+-- Florawing Hive Queen
+UPDATE `creature_template` SET `difficulty_entry_1`= 52516, `mechanic_immune_mask`= 42154367, `flags_extra`= 1073742080 WHERE `entry`= 52442;
+UPDATE `creature_template` SET `minlevel`= 86, `maxlevel`= 86, `exp`= 3, `faction`= 16, `speed_run`= 0.992063, `unit_flags`= 64, `lootid`= 52442, `DamageModifier`= 120, `BaseVariance`= 0.5, `flags_extra`= 1073742080, `mechanic_immune_mask`= 42154367, `InhabitType`= 4 WHERE `entry`= 52516;
+-- Lost Offspring of Gahz'ranka
+UPDATE `creature_template` SET `difficulty_entry_1`= 52508, `mechanic_immune_mask`= 42154367, `flags_extra`= 1073742080 WHERE `entry`= 52418;
+UPDATE `creature_template` SET `minlevel`= 86, `maxlevel`= 86, `exp`= 3, `faction`= 14, `speed_walk`= 1.6, `speed_run`= 1.42857, `unit_flags`= 32832, `lootid`= 52418, `DamageModifier`= 120, `BaseVariance`= 0.5, `flags_extra`= 1073742080, `mechanic_immune_mask`= 42154367 WHERE `entry`= 52508;
+
+DELETE FROM `creature_template_addon` WHERE `entry` IN (52515, 52514, 52513, 52397, 52677, 52706, 52142, 52517, 52509);
+INSERT INTO `creature_template_addon` (`entry`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES
+(52515, 0, 1, 438, ''),
+(52514, 0, 1, 438, ''),
+(52513, 8, 0, 0, ''),
+(52397, 0, 1, 0, '96446'),
+(52677, 0, 1, 0, '28126 18950'),
+(52706, 0, 1, 0, '97285'),
+(52517, 0, 1, 0, '97014'),
+(52509, 0, 1, 0, '84186');
+
+INSERT INTO `creature_template_addon` (`entry`, `bytes1`, `bytes2`, `emote`, `aiAnimKit`, `auras`) VALUES
+(52142, 0, 1, 0, 1293, '');
+
+UPDATE `creature`SET `MovementType`= 1, `spawndist`= 5 WHERE `guid` IN (314543, 314544, 314548, 314551, 314562, 314563, 314564, 314565, 314566, 314567, 314568, 314577, 314578, 314579, 314580, 314581, 314582, 314583, 314584, 314588, 314602, 334968, 334969, 334987, 334998, 335016, 336429, 336430, 336449, 336450, 336527, 336680);
