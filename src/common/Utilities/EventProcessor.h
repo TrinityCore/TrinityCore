7515553c292c5b6e02cdf69bdebaafb0254327c4
Shauren <shauren.trinity@gmail.com>
2019-02-15 21:37:31 +0100

    Fix build for older compilers

--- src/common/Utilities/EventProcessor.h
+++ src/common/Utilities/EventProcessor.h
@@ -89,7 +89,7 @@ private:
 };
 
 template<typename T>
-using is_lambda_event = std::enable_if_t<!std::is_base_of_v<BasicEvent, std::remove_pointer_t<T>>>;
+using is_lambda_event = std::enable_if_t<!advstd::is_base_of_v<BasicEvent, std::remove_pointer_t<T>>>;
 
 class TC_COMMON_API EventProcessor
 {
