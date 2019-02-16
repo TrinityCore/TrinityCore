a3833d201a7453d3533e6435513d21bf096ca345
Ovahlord <dreadkiller@gmx.de>
2019-02-16 01:59:13 +0100

    DB/SFK: added formation inversion points for the Geist formations

---
+++ sql/updates/world/custom/custom_2019_02_16_00_world.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_formations` SET `point_1`= 5, `point_2`= 1 WHERE `leaderGUID`= 370964 AND `memberGUID` != 370964;
+UPDATE `creature_formations` SET `point_1`= 3, `point_2`= 9 WHERE `leaderGUID`= 371072 AND `memberGUID` != 371072;
+UPDATE `creature_formations` SET `point_1`= 5, `point_2`= 13 WHERE `leaderGUID`= 371060 AND `memberGUID` != 371060;
