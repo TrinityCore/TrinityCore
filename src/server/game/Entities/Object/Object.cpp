7a413ae5431fb078e14fe91196246bb67d9cbb30
Shauren <shauren.trinity@gmail.com>
2019-02-20 18:35:35 +0100

    Core/Spells: Fixed spells using MovePositionToFirstCollision for selecting target position getting Z coord offset by caster or target collision height (depending on spell target type)
    (cherry picked from commit d63d15c505708d175c52d87afc76cbe4ac59497b)

--- src/server/game/Entities/Object/Object.cpp
+++ src/server/game/Entities/Object/Object.cpp
@@ -3056,6 +3056,8 @@ void WorldObject::MovePositionToFirstCollision(Position &pos, float dist, float 
         destx, desty, destz + halfHeight,
         destx, desty, destz, -0.5f);
 
+    destz -= halfHeight;
+
     // collision occured
     if (col)
     {
@@ -3070,6 +3072,8 @@ void WorldObject::MovePositionToFirstCollision(Position &pos, float dist, float 
         pos.m_positionX, pos.m_positionY, pos.m_positionZ + halfHeight,
         destx, desty, destz + halfHeight,
         destx, desty, destz, -0.5f);
+
+    destz -= halfHeight;
 
     // Collided with a gameobject
     if (col)
