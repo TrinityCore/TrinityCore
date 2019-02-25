b3f75852c284e730756a7f007b96bbae0ac3c158
Sorikoff <46191832+Sorikoff@users.noreply.github.com>
2019-02-25 08:36:36 +0000

    Allow walk when chasing (#23069)
    (cherry picked from commit c2f2db08aa4f729fbd5b21528a215f6df0c131b6)

--- src/server/game/Movement/MovementGenerators/ChaseMovementGenerator.h
+++ src/server/game/Movement/MovementGenerators/ChaseMovementGenerator.h
@@ -30,7 +30,7 @@ class Unit;
 class ChaseMovementGenerator : public MovementGenerator, public AbstractFollower
 {
     public:
-        explicit ChaseMovementGenerator(Unit* target, Optional<ChaseRange> range = {}, Optional<ChaseAngle> angle = {});
+        explicit ChaseMovementGenerator(Unit* target, Optional<ChaseRange> range = {}, Optional<ChaseAngle> angle = {}, bool walk = false);
         ~ChaseMovementGenerator();
 
         void Initialize(Unit*) override;
@@ -47,6 +47,7 @@ class ChaseMovementGenerator : public MovementGenerator, public AbstractFollower
 
         Optional<ChaseRange> const _range;
         Optional<ChaseAngle> const _angle;
+        bool _walk;
 
         std::unique_ptr<PathGenerator> _path;
         Optional<Position> _lastTargetPosition;
