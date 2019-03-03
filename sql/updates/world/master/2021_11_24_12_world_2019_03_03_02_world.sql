b5c4ae633f51627f6289e741f28ee1c127088364
Killyana <morphone1@gmail.com>
2019-03-03 15:40:24 +0100

    DB/Quest: Improvements for Valduran the Stormborn
    (cherry picked from commit 25db347b09c5b9f8492d3553bdf5e5a266ff710b)

---
+++ sql/updates/world/master/2021_11_24_12_world_2019_03_03_02_world.sql
@@ -0,0 +1,4 @@
+-- 
+UPDATE `smart_scripts` SET `action_param1`=0, `target_type`=19, `target_param1`=29368 WHERE `entryorguid` IN (29801,30152) AND `source_type`=0 AND `action_type`=66;
+UPDATE `smart_scripts` SET `action_param1`=1 WHERE `entryorguid` IN (29801,30152) AND `source_type`=0 AND `action_type`=53;
+UPDATE `smart_scripts` SET `action_param1`=10000 WHERE `entryorguid` IN (29801,30152) AND `source_type`=0 AND `action_type`=41;
