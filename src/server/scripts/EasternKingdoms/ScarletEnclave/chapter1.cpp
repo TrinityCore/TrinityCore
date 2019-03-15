fd64cb28547c998a6e8700c8590614b1b5cb6a4f
Sorikoff <46191832+Sorikoff@users.noreply.github.com>
2019-03-15 07:19:22 +0200

    RAND

--- src/server/scripts/EasternKingdoms/ScarletEnclave/chapter1.cpp
+++ src/server/scripts/EasternKingdoms/ScarletEnclave/chapter1.cpp
@@ -927,7 +927,7 @@ struct spell_gift_of_the_harvester : public SpellScript
 
     void HandleScriptEffect(SpellEffIndex /*effIndex*/)
     {
-        uint32 spellId = urand(0, 1) ? SPELL_MINER_GHOUL_TRANSFORM : SPELL_MINER_GHOST_TRANSFORM;
+        uint32 spellId = RAND(SPELL_MINER_GHOUL_TRANSFORM, SPELL_MINER_GHOST_TRANSFORM);
         GetCaster()->CastSpell(GetHitUnit(), spellId, true);
     }
 
