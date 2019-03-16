c77719c8ede875a91dcbcbd834942d054b8bd3f1
ccrs <ccrs@users.noreply.github.com>
2019-03-16 03:10:45 +0100

    battlefieldmgr missing parameters on battlefieldwintergrasp creation

--- src/server/game/Battlefield/BattlefieldMgr.cpp
+++ src/server/game/Battlefield/BattlefieldMgr.cpp
@@ -40,7 +40,7 @@ BattlefieldMgr* BattlefieldMgr::instance()
 
 void BattlefieldMgr::Initialize()
 {
-    BattlefieldPointer wintergrasp = std::make_unique<BattlefieldWintergrasp>();
+    BattlefieldPointer wintergrasp = std::make_unique<BattlefieldWintergrasp>(BATTLEFIELD_BATTLEID_WINTERGRASP, BATTLEFIELD_ZONEID_WINTERGRASP);
     if (!wintergrasp->Initialize(/*sWorld->getBoolConfig(CONFIG_WINTERGRASP_ENABLE)*/ false))
     {
         TC_LOG_ERROR("server.loading", ">> Wintergrasp initialization failed!");
