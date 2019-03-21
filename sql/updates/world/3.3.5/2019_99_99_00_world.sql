33249a8425d36c7c60a5e387b3cddea1e1c27d32
ccrs <ccrs@users.noreply.github.com>
2019-03-21 12:27:10 +0100

    cleanup sql

--- sql/updates/world/3.3.5/2019_99_99_00_world.sql
+++ sql/updates/world/3.3.5/2019_99_99_00_world.sql
@@ -1,20 +1,6 @@
 -- 'npc_wg_demolisher_engineer' Gossip stuff
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 14 AND `SourceGroup` = 9923 AND `SourceEntry` = 14172 AND `SourceId` = 0 AND `ElseGroup` = 0 AND `ConditionTypeOrReference` = 1 AND `ConditionTarget` = 0 AND `ConditionValue1` = 55629 AND `ConditionValue2` = 0 AND `ConditionValue3` = 0;
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 14 AND `SourceGroup` = 9923 AND `SourceEntry` = 14172 AND `SourceId` = 0 AND `ElseGroup` = 0 AND `ConditionTypeOrReference` = 1 AND `ConditionTarget` = 0 AND `ConditionValue1` = 33280 AND `ConditionValue2` = 0 AND `ConditionValue3` = 0;
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 14 AND `SourceGroup` = 9923 AND `SourceEntry` = 13798 AND `SourceId` = 0 AND `ElseGroup` = 1 AND `ConditionTypeOrReference` = 1 AND `ConditionTarget` = 0 AND `ConditionValue1` = 55629 AND `ConditionValue2` = 0 AND `ConditionValue3` = 0;
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 14 AND `SourceGroup` = 9923 AND `SourceEntry` = 13798 AND `SourceId` = 0 AND `ElseGroup` = 0 AND `ConditionTypeOrReference` = 1 AND `ConditionTarget` = 0 AND `ConditionValue1` = 33280 AND `ConditionValue2` = 0 AND `ConditionValue3` = 0;
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 14 AND `SourceGroup` = 9904 AND `SourceEntry` = 13761 AND `SourceId` = 0 AND `ElseGroup` = 0 AND `ConditionTypeOrReference` = 1 AND `ConditionTarget` = 0 AND `ConditionValue1` = 55629 AND `ConditionValue2` = 0 AND `ConditionValue3` = 0;
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 14 AND `SourceGroup` = 9904 AND `SourceEntry` = 13761 AND `SourceId` = 0 AND `ElseGroup` = 0 AND `ConditionTypeOrReference` = 1 AND `ConditionTarget` = 0 AND `ConditionValue1` = 33280 AND `ConditionValue2` = 0 AND `ConditionValue3` = 0;
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 14 AND `SourceGroup` = 9904 AND `SourceEntry` = 13759 AND `SourceId` = 0 AND `ElseGroup` = 1 AND `ConditionTypeOrReference` = 1 AND `ConditionTarget` = 0 AND `ConditionValue1` = 55629 AND `ConditionValue2` = 0 AND `ConditionValue3` = 0;
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 14 AND `SourceGroup` = 9904 AND `SourceEntry` = 13759 AND `SourceId` = 0 AND `ElseGroup` = 0 AND `ConditionTypeOrReference` = 1 AND `ConditionTarget` = 0 AND `ConditionValue1` = 33280 AND `ConditionValue2` = 0 AND `ConditionValue3` = 0;
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 15 AND `SourceGroup` = 9923 AND `SourceEntry` = 0 AND `SourceId` = 0 AND `ElseGroup` = 0 AND `ConditionTypeOrReference` = 1 AND `ConditionTarget` = 0 AND `ConditionValue1` = 33280 AND `ConditionValue2` = 0 AND `ConditionValue3` = 0;
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 15 AND `SourceGroup` = 9904 AND `SourceEntry` = 2 AND `SourceId` = 0 AND `ElseGroup` = 0 AND `ConditionTypeOrReference` = 1 AND `ConditionTarget` = 0 AND `ConditionValue1` = 55629 AND `ConditionValue2` = 0 AND `ConditionValue3` = 0;
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 15 AND `SourceGroup` = 9904 AND `SourceEntry` = 1 AND `SourceId` = 0 AND `ElseGroup` = 0 AND `ConditionTypeOrReference` = 1 AND `ConditionTarget` = 0 AND `ConditionValue1` = 55629 AND `ConditionValue2` = 0 AND `ConditionValue3` = 0;
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 15 AND `SourceGroup` = 9904 AND `SourceEntry` = 0 AND `SourceId` = 0 AND `ElseGroup` = 1 AND `ConditionTypeOrReference` = 1 AND `ConditionTarget` = 0 AND `ConditionValue1` = 55629 AND `ConditionValue2` = 0 AND `ConditionValue3` = 0;
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 15 AND `SourceGroup` = 9904 AND `SourceEntry` = 0 AND `SourceId` = 0 AND `ElseGroup` = 0 AND `ConditionTypeOrReference` = 1 AND `ConditionTarget` = 0 AND `ConditionValue1` = 33280 AND `ConditionValue2` = 0 AND `ConditionValue3` = 0;
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 15 AND `SourceGroup` = 9923 AND `SourceEntry` = 0 AND `SourceId` = 0 AND `ElseGroup` = 1 AND `ConditionTypeOrReference` = 1 AND `ConditionTarget` = 0 AND `ConditionValue1` = 55629 AND `ConditionValue2` = 0 AND `ConditionValue3` = 0;
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 15 AND `SourceGroup` = 9923 AND `SourceEntry` = 1 AND `SourceId` = 0 AND `ElseGroup` = 0 AND `ConditionTypeOrReference` = 1 AND `ConditionTarget` = 0 AND `ConditionValue1` = 55629 AND `ConditionValue2` = 0 AND `ConditionValue3` = 0;
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 15 AND `SourceGroup` = 9923 AND `SourceEntry` = 2 AND `SourceId` = 0 AND `ElseGroup` = 0 AND `ConditionTypeOrReference` = 1 AND `ConditionTarget` = 0 AND `ConditionValue1` = 55629 AND `ConditionValue2` = 0 AND `ConditionValue3` = 0;
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 14 AND `SourceGroup` IN (9923, 9904);
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 15 AND `SourceGroup` IN (9923, 9904);
 
 DELETE FROM `gossip_menu` WHERE `MenuID` = 9904 AND `TextID` IN (13786, 13761);
 DELETE FROM `gossip_menu` WHERE `MenuID` = 9923 AND `TextID` IN (13786, 14172);
@@ -28,15 +14,8 @@ UPDATE `creature_template` SET `ScriptName` = '' WHERE `ScriptName` = 'npc_wg_gi
 DELETE FROM `spell_script_names` WHERE `spell_id` = 49899 AND `ScriptName` = 'spell_wintergrasp_force_building';
 
 -- Remove spell_area from 56618 - Horde Controls Factory Phase Shift
-DELETE FROM `spell_area` WHERE `spell` = 56618 AND `area` = 4538 AND `quest_start` = 0 AND `aura_spell` = 0 AND `racemask` = 0 AND `gender` = 2;
-DELETE FROM `spell_area` WHERE `spell` = 56618 AND `area` = 4539 AND `quest_start` = 0 AND `aura_spell` = 0 AND `racemask` = 0 AND `gender` = 2;
-DELETE FROM `spell_area` WHERE `spell` = 56618 AND `area` = 4611 AND `quest_start` = 0 AND `aura_spell` = 0 AND `racemask` = 0 AND `gender` = 2;
-DELETE FROM `spell_area` WHERE `spell` = 56618 AND `area` = 4612 AND `quest_start` = 0 AND `aura_spell` = 0 AND `racemask` = 0 AND `gender` = 2;
 -- Remove spell_area from 56617 - Alliance Controls Factory Phase Shift
-DELETE FROM `spell_area` WHERE `spell` = 56617 AND `area` = 4538 AND `quest_start` = 0 AND `aura_spell` = 0 AND `racemask` = 0 AND `gender` = 2;
-DELETE FROM `spell_area` WHERE `spell` = 56617 AND `area` = 4539 AND `quest_start` = 0 AND `aura_spell` = 0 AND `racemask` = 0 AND `gender` = 2;
-DELETE FROM `spell_area` WHERE `spell` = 56617 AND `area` = 4611 AND `quest_start` = 0 AND `aura_spell` = 0 AND `racemask` = 0 AND `gender` = 2;
-DELETE FROM `spell_area` WHERE `spell` = 56617 AND `area` = 4612 AND `quest_start` = 0 AND `aura_spell` = 0 AND `racemask` = 0 AND `gender` = 2;
+DELETE FROM `spell_area` WHERE `spell` IN (56618, 56617);
 
 -- Add 'spell_wintergrasp_waiting_to_resurrect'
 DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_wintergrasp_waiting_to_resurrect';
