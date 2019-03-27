3ef0a0cc63f9a36d6e43a2cccd7f90c6f859b947
Killyana <morphone1@gmail.com>
2019-03-27 16:46:21 +0100

    DB/Quest: Valduran the Stormborn
    Remove the npc flag before the wp start, otherwise they will be restored at the end of the path
    
    (cherry picked from commit 11f6aa651884e5d024319fc4bb0ee38ddd4e4aac)

---
+++ sql/updates/world/master/2021_11_24_45_world_2019_03_27_01_world.sql
@@ -0,0 +1,9 @@
+-- 
+UPDATE `smart_scripts` SET  `id`=30, `link`=2, `event_type`=54  WHERE `entryorguid` IN (29801) AND `source_type`=0 AND `action_type`=83; 
+UPDATE `smart_scripts` SET  `id`=31, `link`=0, `event_type`=61  WHERE `entryorguid` IN (29801) AND `source_type`=0 AND `action_type`=53; 
+UPDATE `smart_scripts` SET  `id`=1, `link`=2, `event_type`=54  WHERE `entryorguid` IN (29801) AND `source_type`=0 AND `action_type`=83; 
+UPDATE `smart_scripts` SET  `id`=2, `link`=0, `event_type`=61  WHERE `entryorguid` IN (29801) AND `source_type`=0 AND `action_type`=53; 
+UPDATE `smart_scripts` SET  `id`=30, `link`=1, `event_type`=54  WHERE `entryorguid` IN (30152) AND `source_type`=0 AND `action_type`=83; 
+UPDATE `smart_scripts` SET  `id`=31, `link`=0, `event_type`=61  WHERE `entryorguid` IN (30152) AND `source_type`=0 AND `action_type`=53;
+UPDATE `smart_scripts` SET  `id`=0, `link`=1, `event_type`=54  WHERE `entryorguid` IN (30152) AND `source_type`=0 AND `action_type`=83; 
+UPDATE `smart_scripts` SET  `id`=1, `link`=0, `event_type`=61  WHERE `entryorguid` IN (30152) AND `source_type`=0 AND `action_type`=53; 
