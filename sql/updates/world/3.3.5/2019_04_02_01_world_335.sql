770a14c0035f846d3b7af5c28ea78af2b4f26591
Killyana <morphone1@gmail.com>
2019-04-03 00:45:08 +0200

    DB: Typo

--- sql/updates/world/3.3.5/2019_04_02_01_world_335.sql
+++ sql/updates/world/3.3.5/2019_04_02_01_world_335.sql
@@ -5,8 +5,8 @@ UPDATE `creature_template` SET `unit_flags`=`unit_flags`|33554432, `AIName`='Sma
 DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=1;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=12120 AND `source_type`=0;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
-(@ENTRY,1,0,2,19,0,100,0,6390,0,0,0,12,12120,3,60,0,0,0,1,0,0,0,0,0,0,0, "Termite Barrel - On quest accepted - summon creature"),
-(@ENTRY,1,1,2,19,0,100,0,6389,0,0,0,12,12120,3,60,0,0,0,1,0,0,0,0,0,0,0, "Termite Barrel - On quest accepted - summon creature"),
+(@ENTRY,1,0,2,19,0,100,0,6390,0,0,0,12,12120,3,60000,0,0,0,1,0,0,0,0,0,0,0, "Termite Barrel - On quest accepted - summon creature"),
+(@ENTRY,1,1,2,19,0,100,0,6389,0,0,0,12,12120,3,60000,0,0,0,1,0,0,0,0,0,0,0, "Termite Barrel - On quest accepted - summon creature"),
 (@ENTRY,1,2,3,61,0,100,0,0,0,0,0,12,12120,3,60000,0,0,0,1,0,0,0,0,0,0,0, "Termite Barrel - On quest accepted - summon creature"),
 (@ENTRY,1,3,4,61,0,100,0,0,0,0,0,12,12120,3,60000,0,0,0,1,0,0,0,0,0,0,0, "Termite Barrel - On quest accepted - summon creature"),
 (@ENTRY,1,4,5,61,0,100,0,0,0,0,0,12,12120,3,60000,0,0,0,1,0,0,0,0,0,0,0, "Termite Barrel - On quest accepted - summon creature"),
