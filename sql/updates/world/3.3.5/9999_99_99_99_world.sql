b1c5c6c829ee86ae326e1b290408787877dd2404
PolarCookie <squigig@gmail.com>
2019-04-03 18:23:35 +0200

    To Winterspring! & Starfall

--- sql/updates/world/3.3.5/9999_99_99_99_world.sql
+++ sql/updates/world/3.3.5/9999_99_99_99_world.sql
@@ -1,5 +1,10 @@
 ALTER TABLE `quest_template_addon`
 ADD COLUMN `BreadcrumbForQuestId` MEDIUMINT(8) NOT NULL DEFAULT '0' AFTER `ExclusiveGroup`;
 
+-- Rejold's New Brew
 UPDATE `quest_template_addon` SET `BreadcrumbForQuestId` = 413 WHERE `Id` = 415;
 UPDATE `quest_template_addon` SET `PrevQuestID` = 315 WHERE `ID` = 413;
+
+-- To Winterspring! & Starfall
+UPDATE `quest_template_addon` SET `BreadcrumbForQuestId` = 5244 WHERE `ID` = 5249;
+UPDATE `quest_template_addon` SET `BreadcrumbForQuestId` = 5244 WHERE `ID` = 5250;
