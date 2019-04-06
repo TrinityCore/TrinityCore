8c80ea70fb4b661ea65ee2c5161f682109ff7e80
Ovahlord <dreadkiller@gmx.de>
2019-04-06 11:16:31 +0200

    Core/Maps: Ensure all child terrain maps are created before doing any thing on parent map such as loading respawn times and corpses (ported commit: f7290509005d3b93bb62149faf3add0bb7f54f51)

--- src/server/game/Maps/MapManager.cpp
+++ src/server/game/Maps/MapManager.cpp
@@ -103,16 +103,18 @@ Map* MapManager::CreateBaseMap_i(MapEntry const* mapEntry)
     if (mapEntry->Instanceable())
         map = new MapInstanced(mapEntry->ID, i_gridCleanUpDelay);
     else
-    {
         map = new Map(mapEntry->ID, i_gridCleanUpDelay, 0, REGULAR_DIFFICULTY);
-        map->LoadRespawnTimes();
-        map->LoadCorpseData();
-    }
 
     i_maps[mapEntry->ID] = map;
 
     for (uint32 childMapId : _parentMapData[mapEntry->ID])
         map->AddChildTerrainMap(CreateBaseMap_i(sMapStore.AssertEntry(childMapId)));
+
+    if (!mapEntry->Instanceable())
+    {
+        map->LoadRespawnTimes();
+        map->LoadCorpseData();
+    }
 
     return map;
 }
