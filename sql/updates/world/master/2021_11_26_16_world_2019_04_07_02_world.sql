7ea8331fa1a2f0c98afb3854bb9710534d19e6df
Killyana <morphone1@gmail.com>
2019-04-07 19:06:55 +0200

    DB/Creature: Blackwing Orb Trigger
    (cherry picked from commit 36f5114a87c12765100bc971f70df0b267e2f795)

---
+++ sql/updates/world/master/2021_11_26_16_world_2019_04_07_02_world.sql
@@ -0,0 +1,4 @@
+-- 
+DELETE FROM `creature` WHERE `guid` IN (61706);
+INSERT INTO `creature` (`guid`, `id`, `map`, `spawnDifficulties`, `phaseId`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`,`spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`) VALUES
+(61706, 14449, 469, '9,18', 0, 0, 0, -7615.51, -1025.59, 413.465, 5.23599, 10, 0, 0, 42, 0, 0);
