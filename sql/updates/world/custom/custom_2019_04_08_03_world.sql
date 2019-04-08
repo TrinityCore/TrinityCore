fe38ec35f44027f8e358f22c33b957b9e7d97674
Ovahlord <dreadkiller@gmx.de>
2019-04-08 20:25:27 +0200

    DB/Gameobjects: added missing 25player difficulty version for Cache of the Firelord

---
+++ sql/updates/world/custom/custom_2019_04_08_03_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `gameobject_template` WHERE `entry`=209261;
+INSERT INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `IconName`, `castBarCaption`, `unk1`, `size`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`, `Data6`, `Data7`, `Data8`, `Data9`, `Data10`, `Data11`, `Data12`, `Data13`, `Data14`, `Data15`, `Data16`, `Data17`, `Data18`, `Data19`, `Data20`, `Data21`, `Data22`, `Data23`, `Data24`, `Data25`, `Data26`, `Data27`, `Data28`, `Data29`, `Data30`, `Data31`, `RequiredLevel`, `AIName`, `ScriptName`, `VerifiedBuild`) VALUES
+(209261, 3, 10684, "Cache of the Fire Lord", "", "", "", 3.5, 1634, 40107, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "", "", 15211);
