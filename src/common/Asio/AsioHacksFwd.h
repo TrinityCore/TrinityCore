bcda8dd7421cfa1950a3e47081f2f28d032d38ac
Shauren <shauren.trinity@gmail.com>
2019-04-14 00:13:23 +0200

    Core/Dep: Boost 1.70 compatibility

--- src/common/Asio/AsioHacksFwd.h
+++ src/common/Asio/AsioHacksFwd.h
@@ -46,29 +46,25 @@ namespace boost
 
             typedef basic_endpoint<tcp> tcp_endpoint;
         }
+#if BOOST_VERSION >= 107000
+        class executor;
 
-#if BOOST_VERSION >= 106600
-        template <typename Time, typename TimeTraits>
-        class basic_deadline_timer;
+        namespace ip
+        {
+            template <typename InternetProtocol, typename Executor>
+            class basic_resolver;
 
-        typedef basic_deadline_timer<posix_time::ptime, time_traits<posix_time::ptime>> deadline_timer;
-
+            typedef basic_resolver<tcp, executor> tcp_resolver;
+        }
+#elif BOOST_VERSION >= 106600
         namespace ip
         {
             template <typename InternetProtocol>
             class basic_resolver;
 
             typedef basic_resolver<tcp> tcp_resolver;
-    }
+        }
 #else
-        template <typename TimeType, typename TimeTraits>
-        class deadline_timer_service;
-
-        template <typename Time, typename TimeTraits, typename TimerService>
-        class basic_deadline_timer;
-
-        typedef basic_deadline_timer<posix_time::ptime, time_traits<posix_time::ptime>, deadline_timer_service<posix_time::ptime, time_traits<posix_time::ptime>>> deadline_timer;
-
         namespace ip
         {
             template <typename InternetProtocol>
