56176bdbdd1ee7aac916cadeea67e93765154225
Aokromes <Aokromes@users.noreply.github.com>
2019-04-17 22:09:02 +0200

    Core/SmartScripts: Reserve SMART_ACTION_PLAY_CINEMATIC

--- src/server/game/AI/SmartScripts/SmartScriptMgr.h
+++ src/server/game/AI/SmartScripts/SmartScriptMgr.h
@@ -567,8 +567,12 @@ enum SMART_ACTION
     SMART_ACTION_PLAY_ANIMKIT                       = 128,    // id, type (0 = oneShot, 1 = aiAnim, 2 = meleeAnim, 3 = movementAnim)
     SMART_ACTION_SCENE_PLAY                         = 129,    // sceneId
     SMART_ACTION_SCENE_CANCEL                       = 130,    // sceneId
-
-    SMART_ACTION_END                                = 131
+    SMART_ACTION_SPAWN_SPAWNGROUP                   = 131,    // Group ID, min secs, max secs, spawnflags
+    SMART_ACTION_DESPAWN_SPAWNGROUP                 = 132,    // Group ID, min secs, max secs, spawnflags
+    SMART_ACTION_RESPAWN_BY_SPAWNID                 = 133,    // spawnType, spawnId
+    SMART_ACTION_INVOKER_CAST                       = 134,    // spellID, castFlags
+    SMART_ACTION_PLAY_CINEMATIC                     = 135,    // reserved for future uses
+    SMART_ACTION_END                                = 136
 };
 
 struct SmartAction
