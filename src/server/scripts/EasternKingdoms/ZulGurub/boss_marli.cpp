73207d653d2766c7517fde71e80a765f43569bff
Yanislav Vasilev <ianislav_vasilev@abv.bg>
2019-04-22 15:12:08 +0300

    Issue 22974: Initial bugfix suggestion.

--- src/server/scripts/EasternKingdoms/ZulGurub/boss_marli.cpp
+++ src/server/scripts/EasternKingdoms/ZulGurub/boss_marli.cpp
@@ -27,6 +27,7 @@ EndScriptData */
 #include "ScriptedCreature.h"
 #include "TemporarySummon.h"
 #include "zulgurub.h"
+#include "GameObject.h"
 
 enum Says
 {
@@ -43,6 +44,7 @@ enum Spells
     SPELL_ENVOLWINGWEB        = 24110,
     SPELL_POISON_VOLLEY       = 24099,
     SPELL_SPIDER_FORM         = 24084,
+    SPELL_HATCH_SPIDER_EGG    = 24082,
     // The Spider Spell
     SPELL_LEVELUP             = 24312  // Not right Spell.
 };
@@ -51,7 +53,7 @@ enum Events
 {
     EVENT_SPAWN_START_SPIDERS = 1, // Phase 1
     EVENT_POISON_VOLLEY       = 2, // Phase All
-    EVENT_SPAWN_SPIDER        = 3, // Phase All
+    EVENT_HATCH_SPIDER_EGG        = 3, // Phase All
     EVENT_CHARGE_PLAYER       = 4, // Phase 3
     EVENT_ASPECT_OF_MARLI     = 5, // Phase 2
     EVENT_TRANSFORM           = 6, // Phase 2
@@ -67,7 +69,8 @@ enum Phases
 
 enum Misc
 {
-    NPC_SPIDER                = 15041
+    NPC_SPIDER                = 15041,
+    GOB_SPIDER_EGG            = 179985,
 };
 
 // AWFUL HACK WARNING
@@ -77,142 +80,180 @@ enum Misc
 float const DamageIncrease = 35.0f;
 float const DamageDecrease = 100.f / (1.f + DamageIncrease / 100.f) - 100.f;
 
+float const MaxRangeHatch = 100.f;
+
 class boss_marli : public CreatureScript
 {
     public: boss_marli() : CreatureScript("boss_marli") { }
 
         struct boss_marliAI : public BossAI
         {
-            boss_marliAI(Creature* creature) : BossAI(creature, DATA_MARLI) { }
+            public:
+                boss_marliAI(Creature* creature) : BossAI(creature, DATA_MARLI) { }
 
-            void Reset() override
-            {
-                if (events.IsInPhase(PHASE_THREE))
-                    me->ApplyStatPctModifier(UNIT_MOD_DAMAGE_MAINHAND, TOTAL_PCT, DamageDecrease); // hack
-                _Reset();
-            }
-
-            void JustDied(Unit* /*killer*/) override
-            {
-                _JustDied();
-                Talk(SAY_DEATH);
-            }
-
-            void JustEngagedWith(Unit* /*who*/) override
-            {
-                _JustEngagedWith();
-                events.ScheduleEvent(EVENT_SPAWN_START_SPIDERS, 1s, 0, PHASE_ONE);
-                Talk(SAY_AGGRO);
-            }
-
-            void UpdateAI(uint32 diff) override
-            {
-                if (!UpdateVictim())
-                    return;
-
-                events.Update(diff);
-
-                if (me->HasUnitState(UNIT_STATE_CASTING))
-                    return;
-
-                while (uint32 eventId = events.ExecuteEvent())
+                void Initialize()
                 {
-                    switch (eventId)
-                    {
-                        case EVENT_SPAWN_START_SPIDERS:
-                            if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0))
-                            {
-                                Talk(SAY_SPIDER_SPAWN);
-                                for (uint8 i = 0; i < 4; ++i)
-                                    if (Creature* spider = me->SummonCreature(NPC_SPIDER, target->GetPositionX(), target->GetPositionY(), target->GetPositionZ(), 0, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 15000))
-                                        spider->AI()->AttackStart(target);
-                            }
-                            events.ScheduleEvent(EVENT_ASPECT_OF_MARLI, 12s, 0, PHASE_TWO);
-                            events.ScheduleEvent(EVENT_TRANSFORM, 45s, 0, PHASE_TWO);
-                            events.ScheduleEvent(EVENT_POISON_VOLLEY, 15s);
-                            events.ScheduleEvent(EVENT_SPAWN_SPIDER, 30s);
-                            events.ScheduleEvent(EVENT_TRANSFORM, 45s, 0, PHASE_TWO);
-                            events.SetPhase(PHASE_TWO);
-                            break;
-                        case EVENT_POISON_VOLLEY:
-                            DoCastVictim(SPELL_POISON_VOLLEY, true);
-                            events.ScheduleEvent(EVENT_POISON_VOLLEY, 10s, 20s);
-                            break;
-                        case EVENT_ASPECT_OF_MARLI:
-                            DoCastVictim(SPELL_ASPECT_OF_MARLI, true);
-                            events.ScheduleEvent(EVENT_ASPECT_OF_MARLI, urand(13000, 18000), 0, PHASE_TWO);
-                            break;
-                        case EVENT_SPAWN_SPIDER:
-                            if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0))
-                                if (Creature* spider = me->SummonCreature(NPC_SPIDER, target->GetPositionX(), target->GetPositionY(), target->GetPositionZ(), 0, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 15000))
-                                    spider->AI()->AttackStart(target);
-                            events.ScheduleEvent(EVENT_SPAWN_SPIDER, 12s, 17s);
-                            break;
-                        case EVENT_TRANSFORM:
+                    _hatchedEggs = std::set<GameObject*>();
+                }
+
+                void Reset() override
+                {
+                    if (events.IsInPhase(PHASE_THREE))
+                        me->ApplyStatPctModifier(UNIT_MOD_DAMAGE_MAINHAND, TOTAL_PCT, DamageDecrease); // hack
+                    std::for_each(_hatchedEggs.begin(), _hatchedEggs.end(), [](GameObject* go)
                         {
-                            Talk(SAY_TRANSFORM);
-                            DoCast(me, SPELL_SPIDER_FORM); // SPELL_AURA_TRANSFORM
-                            /*
-                            CreatureTemplate const* cinfo = me->GetCreatureTemplate();
-                            me->SetBaseWeaponDamage(BASE_ATTACK, MINDAMAGE, (cinfo->mindmg +((cinfo->mindmg/100) * 35)));
-                            me->SetBaseWeaponDamage(BASE_ATTACK, MAXDAMAGE, (cinfo->maxdmg +((cinfo->maxdmg/100) * 35)));
-                            me->UpdateDamagePhysical(BASE_ATTACK);
-                            */
-                            me->ApplyStatPctModifier(UNIT_MOD_DAMAGE_MAINHAND, TOTAL_PCT, DamageIncrease); // hack
-                            DoCastVictim(SPELL_ENVOLWINGWEB);
-                            if (GetThreat(me->GetVictim()))
-                                ModifyThreatByPercent(me->GetVictim(), -100);
-                            events.ScheduleEvent(EVENT_CHARGE_PLAYER, 1500ms, 0, PHASE_THREE);
-                            events.ScheduleEvent(EVENT_TRANSFORM_BACK, 25s, 0, PHASE_THREE);
-                            events.SetPhase(PHASE_THREE);
-                            break;
-                        }
-                        case EVENT_CHARGE_PLAYER:
-                        {
-                            Unit* target = nullptr;
-                            int i = 0;
-                            while (i++ < 3) // max 3 tries to get a random target with power_mana
-                            {
-                                target = SelectTarget(SELECT_TARGET_RANDOM, 1, 100, true);  // not aggro leader
-                                if (target && target->GetPowerType() == POWER_MANA)
-                                    break;
-                            }
-                            if (target)
-                            {
-                                DoCast(target, SPELL_CHARGE);
-                                AttackStart(target);
-                            }
-                            events.ScheduleEvent(EVENT_CHARGE_PLAYER, 8s, 0, PHASE_THREE);
-                            break;
-                        }
-                        case EVENT_TRANSFORM_BACK:
-                        {
-                            me->RemoveAura(SPELL_SPIDER_FORM);
-                            /*
-                            CreatureTemplate const* cinfo = me->GetCreatureTemplate();
-                            me->SetBaseWeaponDamage(BASE_ATTACK, MINDAMAGE, (cinfo->mindmg +((cinfo->mindmg/100) * 1)));
-                            me->SetBaseWeaponDamage(BASE_ATTACK, MAXDAMAGE, (cinfo->maxdmg +((cinfo->maxdmg/100) * 1)));
-                            me->UpdateDamagePhysical(BASE_ATTACK);
-                            */
-                            me->ApplyStatPctModifier(UNIT_MOD_DAMAGE_MAINHAND, TOTAL_PCT, DamageDecrease); // hack
-                            events.ScheduleEvent(EVENT_ASPECT_OF_MARLI, 12s, 0, PHASE_TWO);
-                            events.ScheduleEvent(EVENT_TRANSFORM, 45s, 0, PHASE_TWO);
-                            events.ScheduleEvent(EVENT_POISON_VOLLEY, 15s);
-                            events.ScheduleEvent(EVENT_SPAWN_SPIDER, 30s);
-                            events.ScheduleEvent(EVENT_TRANSFORM, urand(35000, 60000), 0, PHASE_TWO);
-                            events.SetPhase(PHASE_TWO);
-                            break;
-                        }
-                        default:
-                            break;
-                    }
+                            go->Respawn();
+                        });
+                    
+                    _Reset();
+                    Initialize();
+                }
+
+                void SpellHitTarget(GameObject* target, SpellInfo const* /*spellInfo*/) override
+                {
+                    if(target->GetEntry() == GOB_SPIDER_EGG)
+                        _hatchedEggs.insert(target);
+                }
+
+                void JustDied(Unit* /*killer*/) override
+                {
+                    _JustDied();
+                    Talk(SAY_DEATH);
+                }
+
+                void JustEngagedWith(Unit* /*who*/) override
+                {
+                    _JustEngagedWith();
+                    events.ScheduleEvent(EVENT_SPAWN_START_SPIDERS, 1s, 0, PHASE_ONE);
+                    Talk(SAY_AGGRO);
+                }
+
+                void UpdateAI(uint32 diff) override
+                {
+                    if (!UpdateVictim())
+                        return;
+
+                    events.Update(diff);
 
                     if (me->HasUnitState(UNIT_STATE_CASTING))
                         return;
+
+                    while (uint32 eventId = events.ExecuteEvent())
+                    {
+                        switch (eventId)
+                        {
+                            case EVENT_SPAWN_START_SPIDERS:
+                                Talk(SAY_SPIDER_SPAWN);
+
+                                for (int i = 0; i < 4; ++i)
+                                {
+                                    if (GameObject * egg = me->FindNearestGameObject(GOB_SPIDER_EGG, MaxRangeHatch))
+                                    {
+                                        me->CastSpell(egg, SPELL_HATCH_SPIDER_EGG);
+                                        if (Creature * spider = me->FindNearestCreature(NPC_SPIDER, MaxRangeHatch))
+                                        {
+                                            spider->Attack(SelectTarget(SELECT_TARGET_RANDOM, 0), true);
+                                        }
+
+                                        egg->DespawnOrUnsummon();
+                                    }
+                                }
+                                events.ScheduleEvent(EVENT_ASPECT_OF_MARLI, 12s, 0, PHASE_TWO);
+                                events.ScheduleEvent(EVENT_TRANSFORM, 45s, 0, PHASE_TWO);
+                                events.ScheduleEvent(EVENT_POISON_VOLLEY, 15s);
+                                events.ScheduleEvent(EVENT_HATCH_SPIDER_EGG, 30s);
+                                events.ScheduleEvent(EVENT_TRANSFORM, 45s, 0, PHASE_TWO);
+                                events.SetPhase(PHASE_TWO);
+                                break;
+                            case EVENT_POISON_VOLLEY:
+                                DoCastVictim(SPELL_POISON_VOLLEY, true);
+                                events.ScheduleEvent(EVENT_POISON_VOLLEY, 10s, 20s);
+                                break;
+                            case EVENT_ASPECT_OF_MARLI:
+                                DoCastVictim(SPELL_ASPECT_OF_MARLI, true);
+                                events.ScheduleEvent(EVENT_ASPECT_OF_MARLI, urand(13000, 18000), 0, PHASE_TWO);
+                                break;
+                            case EVENT_HATCH_SPIDER_EGG:
+                                if (GameObject * egg = me->FindNearestGameObject(GOB_SPIDER_EGG, MaxRangeHatch))
+                                {
+                                    me->CastSpell(egg, SPELL_HATCH_SPIDER_EGG);
+                                    egg->DespawnOrUnsummon();
+                                    if (Creature * spider = me->FindNearestCreature(NPC_SPIDER, MaxRangeHatch))
+                                    {
+                                        spider->Attack(SelectTarget(SELECT_TARGET_RANDOM, 0), true);
+                                    }
+                                }
+                                events.ScheduleEvent(EVENT_HATCH_SPIDER_EGG, 12s, 17s);
+                                break;
+                            case EVENT_TRANSFORM:
+                            {
+                                Talk(SAY_TRANSFORM);
+                                DoCast(me, SPELL_SPIDER_FORM); // SPELL_AURA_TRANSFORM
+                                /*
+                                CreatureTemplate const* cinfo = me->GetCreatureTemplate();
+                                me->SetBaseWeaponDamage(BASE_ATTACK, MINDAMAGE, (cinfo->mindmg +((cinfo->mindmg/100) * 35)));
+                                me->SetBaseWeaponDamage(BASE_ATTACK, MAXDAMAGE, (cinfo->maxdmg +((cinfo->maxdmg/100) * 35)));
+                                me->UpdateDamagePhysical(BASE_ATTACK);
+                                */
+                                me->ApplyStatPctModifier(UNIT_MOD_DAMAGE_MAINHAND, TOTAL_PCT, DamageIncrease); // hack
+                                DoCastVictim(SPELL_ENVOLWINGWEB);
+                                if (GetThreat(me->GetVictim()))
+                                    ModifyThreatByPercent(me->GetVictim(), -100);
+                                events.ScheduleEvent(EVENT_CHARGE_PLAYER, 1500ms, 0, PHASE_THREE);
+                                events.ScheduleEvent(EVENT_TRANSFORM_BACK, 25s, 0, PHASE_THREE);
+                                events.CancelEvent(EVENT_HATCH_SPIDER_EGG);
+                                events.SetPhase(PHASE_THREE);
+                                break;
+                            }
+                            case EVENT_CHARGE_PLAYER:
+                            {
+                                Unit* target = nullptr;
+                                int i = 0;
+                                while (i++ < 3) // max 3 tries to get a random target with power_mana
+                                {
+                                    target = SelectTarget(SELECT_TARGET_RANDOM, 1, 100, true);  // not aggro leader
+                                    if (target && target->GetPowerType() == POWER_MANA)
+                                        break;
+                                }
+                                if (target)
+                                {
+                                    DoCast(target, SPELL_CHARGE);
+                                    AttackStart(target);
+                                }
+                                events.ScheduleEvent(EVENT_CHARGE_PLAYER, 8s, 0, PHASE_THREE);
+                                break;
+                            }
+                            case EVENT_TRANSFORM_BACK:
+                            {
+                                me->RemoveAura(SPELL_SPIDER_FORM);
+                                /*
+                                CreatureTemplate const* cinfo = me->GetCreatureTemplate();
+                                me->SetBaseWeaponDamage(BASE_ATTACK, MINDAMAGE, (cinfo->mindmg +((cinfo->mindmg/100) * 1)));
+                                me->SetBaseWeaponDamage(BASE_ATTACK, MAXDAMAGE, (cinfo->maxdmg +((cinfo->maxdmg/100) * 1)));
+                                me->UpdateDamagePhysical(BASE_ATTACK);
+                                */
+                                me->ApplyStatPctModifier(UNIT_MOD_DAMAGE_MAINHAND, TOTAL_PCT, DamageDecrease); // hack
+                                events.ScheduleEvent(EVENT_ASPECT_OF_MARLI, 12s, 0, PHASE_TWO);
+                                events.ScheduleEvent(EVENT_TRANSFORM, 45s, 0, PHASE_TWO);
+                                events.ScheduleEvent(EVENT_POISON_VOLLEY, 15s);
+                                events.ScheduleEvent(EVENT_HATCH_SPIDER_EGG, 12s, 17s);
+                                events.ScheduleEvent(EVENT_TRANSFORM, urand(35000, 60000), 0, PHASE_TWO);
+                                events.SetPhase(PHASE_TWO);
+                                break;
+                            }
+                            default:
+                                break;
+                        }
+
+                        if (me->HasUnitState(UNIT_STATE_CASTING))
+                            return;
+                    }
+
+                    DoMeleeAttackIfReady();
                 }
 
-                DoMeleeAttackIfReady();
-            }
+            private:
+                std::set<GameObject*> _hatchedEggs;
         };
 
         CreatureAI* GetAI(Creature* creature) const override
