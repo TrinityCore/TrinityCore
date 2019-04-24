a2c0f6d32113c8ff42760c389826bad80ce77815
ForesterDev <11771800+ForesterDev@users.noreply.github.com>
2019-04-24 18:58:56 +0400

    Scripts/Commands: add some missing checks for .guild create command (#23201)
    * Core/Conditions: implement CONDITION_GAMEMASTER
    
    * Scripts/Commands: add some missing checks for .guild create command
    
    (cherry picked from commit ae8750716f63ddc41f21bb38ceafe949d7260f0e)

--- src/server/scripts/Commands/cs_guild.cpp
+++ src/server/scripts/Commands/cs_guild.cpp
@@ -90,7 +90,22 @@ public:
         if (target->GetGuildId())
         {
             handler->SendSysMessage(LANG_PLAYER_IN_GUILD);
-            return true;
+            handler->SetSentErrorMessage(true);
+            return false;
+        }
+
+        if (sGuildMgr->GetGuildByName(guildName))
+        {
+            handler->SendSysMessage(LANG_GUILD_RENAME_ALREADY_EXISTS);
+            handler->SetSentErrorMessage(true);
+            return false;
+        }
+
+        if (sObjectMgr->IsReservedName(guildName) || !sObjectMgr->IsValidCharterName(guildName))
+        {
+            handler->SendSysMessage(LANG_BAD_VALUE);
+            handler->SetSentErrorMessage(true);
+            return false;
         }
 
         Guild* guild = new Guild;
