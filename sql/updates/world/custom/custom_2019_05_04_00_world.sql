efdf540372c26ae758fc9b2e7825a61e9b5d9659
Ovahlord <dreadkiller@gmx.de>
2019-05-04 23:35:37 +0200

    DB: fixed a startup error caused by last commit

--- sql/updates/world/custom/custom_2019_05_04_00_world.sql
+++ sql/updates/world/custom/custom_2019_05_04_00_world.sql
@@ -1,6 +1,6 @@
 DELETE FROM `spell_proc` WHERE `SpellId`= 84617;
 INSERT INTO `spell_proc` (`SpellId`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `ProcFlags`, `SpellTypeMask`, `SpellPhaseMask`, `HitMask`, `AttributesMask`, `Cooldown`, `Charges`, `Chance`) VALUES
-(84617, 0, 0, 0, 0, 0, 1, 4, 0, 64, 0, 0, 0);
+(84617, 0, 0, 0, 0, 0, 1, 0, 0, 64, 0, 0, 0);
 
 DELETE FROM `spell_script_names` WHERE `ScriptName`= 'spell_rog_envenom';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
