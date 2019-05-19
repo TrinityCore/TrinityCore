afa767dcc55410d49892a6dbc97acd1e82f4ef65
jackpoz <giacomopoz@gmail.com>
2019-05-19 16:53:49 +0200

    Core/TempSummons: Add support to SAI for GameObject owner of TempSummon

--- src/server/game/AI/SmartScripts/SmartAI.cpp
+++ src/server/game/AI/SmartScripts/SmartAI.cpp
@@ -636,14 +636,9 @@ void SmartAI::ReceiveEmote(Player* player, uint32 textEmote)
     GetScript()->ProcessEventsFor(SMART_EVENT_RECEIVE_EMOTE, player, textEmote);
 }
 
-void SmartAI::IsSummonedBy(WorldObject* summonerWO)
+void SmartAI::IsSummonedBy(WorldObject* summoner)
 {
-    // ToDo: Add support to WorldObject summoner in SAI too
-    Unit* summoner = summonerWO->ToUnit();
-    if (!summoner)
-        return;
-
-    GetScript()->ProcessEventsFor(SMART_EVENT_JUST_SUMMONED, summoner);
+    GetScript()->ProcessEventsFor(SMART_EVENT_JUST_SUMMONED, summoner->ToUnit(), 0, 0, false, nullptr, summoner->ToGameObject());
 }
 
 void SmartAI::DamageDealt(Unit* doneTo, uint32& damage, DamageEffectType /*damagetype*/)
