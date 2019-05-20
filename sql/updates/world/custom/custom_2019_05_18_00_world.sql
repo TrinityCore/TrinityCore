af52225c6af50394bb18300f5232147f71a47caf
Ovahlord <dreadkiller@gmx.de>
2019-05-20 12:10:57 +0200

    DB: fixed another startup error

--- sql/updates/world/custom/custom_2019_05_18_00_world.sql
+++ sql/updates/world/custom/custom_2019_05_18_00_world.sql
@@ -1,6 +1,6 @@
 UPDATE `creature_template` SET `flags_extra`= 0x00000100 WHERE `entry` IN (42897, 49121, 49122, 49123);
 UPDATE `creature_template` SET `flags_extra` = `flags_extra` | 1 WHERE `entry` IN (42180, 49050, 49051, 49052);
-UPDATE `creature_template` SET `flags_extra` = `flags_extra` | 0x00200000 WHERE `entry` IN (42180, 49050, 49051, 49052, 42178, 49053, 49054, 49055, 42179, 49047, 49048, 49040, 42166, 49056, 49057, 49058);
+UPDATE `creature_template` SET `flags_extra` = `flags_extra` | 0x00200000 WHERE `entry` IN (42180, 49050, 49051, 49052, 42178, 49053, 49054, 49055, 42179, 49047, 49048, 49049, 42166, 49056, 49057, 49058);
 
 -- Currency Loot
 DELETE FROM `creature_onkill_reward` WHERE `creature_id` IN (42180, 49050, 49051, 49052);
