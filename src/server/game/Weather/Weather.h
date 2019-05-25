9c2ce24c8a622e1fd15d308a99a98e6bbe500432
Kittnz <frederik156@hotmail.com>
2019-05-25 18:17:53 +0200

    Core/Weather: Define WEATHER_STATE_DRIZZLE state
    (cherry picked from commit cf05c3dd2c8fd27e0cb7a7e76913ba46cbee0a2c)

--- src/server/game/Weather/Weather.h
+++ src/server/game/Weather/Weather.h
@@ -46,6 +46,7 @@ enum WeatherState : uint32
 {
     WEATHER_STATE_FINE              = 0,
     WEATHER_STATE_FOG               = 1, // Used in some instance encounters.
+    WEATHER_STATE_DRIZZLE           = 2,
     WEATHER_STATE_LIGHT_RAIN        = 3,
     WEATHER_STATE_MEDIUM_RAIN       = 4,
     WEATHER_STATE_HEAVY_RAIN        = 5,
