efbb6f5b7d66c31671aed47881825b6baa6f5019
jackpoz <giacomopoz@gmail.com>
2019-06-01 15:59:52 +0200

    Core/Map: Don't log errors about Area ID 0
    Restore a check about Area ID 0 that would silently ignore this case instead of spamming log errors. Check removed in e79c595b695ea5f35b8324efbb7741ccb102bce9
    Fix #23085
    
    (cherry picked from commit bda351d7f1cb3ff663a9d231afa85864ea0f8078)

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -6206,6 +6206,9 @@ void Player::CheckAreaExploreAndOutdoor()
         RemoveAurasWithAttribute(SPELL_ATTR0_OUTDOORS_ONLY);
 
     uint32 const areaId = GetAreaId();
+    if (!areaId)
+        return;
+
     AreaTableEntry const* areaEntry = sAreaTableStore.LookupEntry(areaId);
     if (!areaEntry)
     {
