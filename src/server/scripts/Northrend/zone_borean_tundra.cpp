b52dcc47c61d0db44c84d7533b8b2d448237f146
Wyrserth <wyrserth@protonmail.com>
2019-06-02 23:05:37 +0200

    Script/Quest: implement event for the quest Orabus the Helmsman.
    Thanks Wyreth for the original script.

--- src/server/scripts/Northrend/zone_borean_tundra.cpp
+++ src/server/scripts/Northrend/zone_borean_tundra.cpp
@@ -2503,6 +2503,46 @@ public:
     }
 };
 
+enum OrabusTheHelmsman
+{
+    NPC_ORABUS            = 32576,
+    NPC_KVALDIR_CREWMAN_1 = 32577,
+    NPC_KVALDIR_CREWMAN_2 = 32578,
+    NPC_KVALDIR_CREWMAN_3 = 32579,
+    NPC_KVALDIR_CREWMAN_4 = 32580
+};
+
+class npc_orabus_the_helmsman_ship_exit_pos : public UnitScript
+{
+public:
+    npc_orabus_the_helmsman_ship_exit_pos() : UnitScript("npc_orabus_the_helmsman_ship_exit_pos") { }
+
+    void ModifyVehiclePassengerExitPos(Unit* passenger, Vehicle* /*vehicle*/, Position& pos)
+    {
+        if (passenger->GetTypeId() == TYPEID_UNIT)
+        {
+            switch (passenger->GetEntry())
+            {
+            case NPC_ORABUS:
+                pos.Relocate(2807.858f, 7038.571f, 7.075813f, 4.73481f);
+                break;
+            case NPC_KVALDIR_CREWMAN_1:
+                pos.Relocate(2803.729f, 7046.516f, 5.362013f, 4.73481f);
+                break;
+            case NPC_KVALDIR_CREWMAN_2:
+                pos.Relocate(2801.168f, 7046.468f, 5.362013f, 4.73481f);
+                break;
+            case NPC_KVALDIR_CREWMAN_3:
+                pos.Relocate(2803.319f, 7051.411f, 5.362913f, 4.73481f);
+                break;
+            case NPC_KVALDIR_CREWMAN_4:
+                pos.Relocate(2801.48f, 7051.379f, 5.362913f, 4.73481f);
+                break;
+            }
+        }
+    }
+};
+
 void AddSC_borean_tundra()
 {
     new npc_sinkhole_kill_credit();
@@ -2529,4 +2569,5 @@ void AddSC_borean_tundra()
     new spell_windsoul_totem_aura();
     new spell_q11719_bloodspore_ruination_45997();
     new npc_bloodmage_laurith();
+    new npc_orabus_the_helmsman_ship_exit_pos();
 }
