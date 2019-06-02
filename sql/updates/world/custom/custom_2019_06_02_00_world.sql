92d44837ca4622bb3370e38b08c3abfe0414d8a7
Ovahlord <dreadkiller@gmx.de>
2019-06-02 12:29:29 +0200

    DB: fixed a typo in last commit

--- sql/updates/world/custom/custom_2019_06_02_00_world.sql
+++ sql/updates/world/custom/custom_2019_06_02_00_world.sql
@@ -1,3 +1,3 @@
 DELETE FROM `spell_bonus_data` WHERE `entry`= 974;
 INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `comments`) VALUES
-(974, 0.538, 'Shaman - Earth Shield');
+(974, 1, 'Shaman - Earth Shield');
