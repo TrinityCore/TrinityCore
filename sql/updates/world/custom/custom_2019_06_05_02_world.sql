95df9221708170ed6492ae474ffbb09105148321
Ovahlord <dreadkiller@gmx.de>
2019-06-05 19:51:10 +0200

    DB: fixed a typo

--- sql/updates/world/custom/custom_2019_06_05_02_world.sql
+++ sql/updates/world/custom/custom_2019_06_05_02_world.sql
@@ -1,4 +1,4 @@
-UPDATE `spell_area` SET `quest_start`= 14320 WHERE `spell`= 14375 AND `area`= 4714;
+UPDATE `spell_area` SET `quest_start`= 14321 WHERE `spell`= 68482 AND `area`= 4714;
 
 DELETE FROM `spell_script_names` WHERE `ScriptName`= 'spell_gilneas_worgen_intro_completion';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
