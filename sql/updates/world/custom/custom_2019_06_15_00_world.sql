d1417ce516fe8ecaecd4313752f279bfef09543b
Ovahlord <dreadkiller@gmx.de>
2019-06-17 09:07:05 +0200

    DB/Gilneas: added missing scriptname for spell: Gilneas - Quest - Save James

--- sql/updates/world/custom/custom_2019_06_15_00_world.sql
+++ sql/updates/world/custom/custom_2019_06_15_00_world.sql
@@ -1,1 +1,3 @@
 DELETE FROM `spell_script_names` WHERE `ScriptName`= 'spell_gilneas_quest_save_james';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(68596, 'spell_gilneas_quest_save_the_children');
