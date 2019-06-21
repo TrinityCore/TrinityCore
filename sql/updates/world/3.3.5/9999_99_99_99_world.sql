a5be792b924d7c0c11fb83ca991729ad64c7d3da
Sorikoff <46191832+Sorikoff@users.noreply.github.com>
2019-06-21 10:57:27 +0300

    Revert "Scripts/Spells: Portal to Orgrimmar & Portal to Stormwind"
    This reverts commit a8055960

--- sql/updates/world/3.3.5/9999_99_99_99_world.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_teleport_leaders_blessing');
-INSERT INTO `spell_script_names` VALUES
-(58418,'spell_teleport_leaders_blessing'),
-(58420,'spell_teleport_leaders_blessing');
