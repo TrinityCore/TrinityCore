4ae5a855e9db871f3ecbc84d3ce3846bd81235f9
Sorikoff <46191832+Sorikoff@users.noreply.github.com>
2019-06-21 10:50:21 +0300

    Correct header

--- src/server/scripts/Northrend/zone_borean_tundra.cpp
+++ src/server/scripts/Northrend/zone_borean_tundra.cpp
@@ -39,7 +39,7 @@ EndContentData */
 #include "ObjectMgr.h"
 #include "Player.h"
 #include "QuestDef.h"
-#include "ScriptedCreature.h"
+#include "CreatureAIImpl.h"
 #include "ScriptedEscortAI.h"
 #include "ScriptedFollowerAI.h"
 #include "ScriptedGossip.h"
