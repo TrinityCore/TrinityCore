819f8b1a2dc2093bc0c6cd87369d7c64b8e81bae
Wyrserth <wyrserth@protonmail.com>
2019-07-01 14:39:06 +0200

    Script/DarkmoonFaire: fix logical error in go_darkmoon_faire_music.
    Closes #23548.
    
    (cherry picked from commit 028d16a812bd3ff56f19da919ca1a35e7523fe8c)

--- src/server/scripts/World/go_scripts.cpp
+++ src/server/scripts/World/go_scripts.cpp
@@ -1740,14 +1740,14 @@ public:
             {
                 switch (eventId)
                 {
-                case EVENT_DFM_START_MUSIC:
-                    if (!IsHolidayActive(HOLIDAY_DARKMOON_FAIRE))
+                    case EVENT_DFM_START_MUSIC:
+                        if (!IsHolidayActive(HOLIDAY_DARKMOON_FAIRE))
+                            break;
+                        me->PlayDirectMusic(MUSIC_DARKMOON_FAIRE_MUSIC);
+                        _events.ScheduleEvent(EVENT_DFM_START_MUSIC, 5s);  // Every 5 second's SMSG_PLAY_MUSIC packet (PlayDirectMusic) is pushed to the client (sniffed value)
                         break;
-                    me->PlayDirectMusic(MUSIC_DARKMOON_FAIRE_MUSIC);
-                    _events.ScheduleEvent(EVENT_DFM_START_MUSIC, 5s);  // Every 5 second's SMSG_PLAY_MUSIC packet (PlayDirectMusic) is pushed to the client (sniffed value)
-                    break;
-                default:
-                    break;
+                    default:
+                        break;
                 }
             }
         }
