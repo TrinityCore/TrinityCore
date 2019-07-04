a0e7958f855e328fffd8c9bc8563e3f0f7477663
SolarisSDK <52369286+SolarisSDK@users.noreply.github.com>
2019-07-04 20:18:44 +0300

    Local-only mode fix (#23546)
    * Local-only mode fix
    
    In response to issue #21309
    
    * Full patch also for boost 1.66 and newer

--- src/common/Asio/Resolver.h
+++ src/common/Asio/Resolver.h
@@ -31,13 +31,15 @@ namespace Trinity
         {
             boost::system::error_code ec;
 #if BOOST_VERSION >= 106600
-            boost::asio::ip::tcp::resolver::results_type results = resolver.resolve(protocol, host, service, ec);
+            boost::asio::ip::resolver_base::flags flagsResolver = boost::asio::ip::resolver_base::all_matching;
+            boost::asio::ip::tcp::resolver::results_type results = resolver.resolve(protocol, host, service, flagsResolver, ec);
             if (results.begin() == results.end() || ec)
                 return {};
 
             return results.begin()->endpoint();
 #else
-            boost::asio::ip::tcp::resolver::query query(std::move(protocol), std::move(host), std::move(service));
+            boost::asio::ip::resolver_query_base::flags flagsQuery = boost::asio::ip::tcp::resolver::query::all_matching;
+            boost::asio::ip::tcp::resolver::query query(std::move(protocol), std::move(host), std::move(service), flagsQuery);
             boost::asio::ip::tcp::resolver::iterator itr = resolver.resolve(query, ec);
             boost::asio::ip::tcp::resolver::iterator end;
             if (itr == end || ec)
