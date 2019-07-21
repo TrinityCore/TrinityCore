22326500532da0946c59c4569bcfa30edc213539
Wyrserth <wyrserth@protonmail.com>
2019-07-21 19:05:12 +0200

    Script/ScarletEnclave: remove Eye of Acherus control and speed auras from player when the Eye of Acherus is dismissed.
    # Conflicts:
    #	src/server/scripts/EasternKingdoms/ScarletEnclave/chapter1.cpp

--- src/server/scripts/EasternKingdoms/ScarletEnclave/chapter1.cpp
+++ src/server/scripts/EasternKingdoms/ScarletEnclave/chapter1.cpp
@@ -346,6 +346,7 @@ public:
 
 enum EyeOfAcherus
 {
+    SPELL_THE_EYE_OF_ACHERUS = 51852,
     SPELL_EYE_VISUAL            = 51892,
     SPELL_EYE_FLIGHT_BOOST      = 51923,
     SPELL_EYE_FLIGHT            = 51890,
@@ -387,7 +388,14 @@ class npc_eye_of_acherus : public CreatureScript
                 init.Launch();
             }
 
-            void OnCharmed(bool /*apply*/) override { }
+            void OnCharmed(bool /*isNew*/) override
+            {
+                if (!me->IsCharmed())
+                {
+                    me->GetCharmerOrOwner()->RemoveAurasDueToSpell(SPELL_THE_EYE_OF_ACHERUS);
+                    me->GetCharmerOrOwner()->RemoveAurasDueToSpell(SPELL_EYE_FLIGHT_BOOST);
+                }
+            }
 
             void UpdateAI(uint32 diff) override
             {
