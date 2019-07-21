c9da7b9f525dea155532a9ebf11020a10edcd4fd
Gildor <gildor55@gmail.com>
2019-07-21 19:27:04 +0200

    delete unnecessary comments

--- sql/updates/world/3.3.5/9999_99_99_99_world_335.sql
+++ sql/updates/world/3.3.5/9999_99_99_99_world_335.sql
@@ -1,12 +1,3 @@
--- Patch 1.10.2 (2006-05-02): This ability will no longer stack with [Arcane Power].
--- If you gain both at the same time, Arcane Power will take precedence.
-
--- If you have Power Infusion Aura and cast Arcane Power, Power Infusion should be replaced by Arcane Power.
--- But if you have Arcane Power and try to cast Power Infusion, you should get msg "A more powerful spell is active"
--- Arcane Power and Power Infusion don't have the same effect, so stack_rule 4 (SPELL_GROUP_STACK_RULE_EXCLUSIVE_HIGHEST) isn't valid.
--- So, I put stack_rule 1 (SPELL_GROUP_STACK_RULE_EXCLUSIVE) for prevent stack the two auras.
--- And SpellScript for get error msg when you cast Power Infusion when target have Arcane Power Aura.
-
 SET @GROUP_ID := 1122; -- Change with the proper ID if this is in use
 DELETE FROM `spell_group` WHERE `id`=@GROUP_ID AND `spell_id` IN (10060,12042);
 INSERT INTO `spell_group` (`id`,`spell_id`) VALUES
