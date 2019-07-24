ca71fc7b6cc43d4883ad0a2a4c649b36a841b022
Keader <keader.android@gmail.com>
2019-07-24 08:31:34 -0300

    Core/Spells: Added spellgroup support to HandleModCastingSpeed (#23592)
    (cherry picked from commit 4c2f7df4f39cd98eae21bc64ebac59e3d2eea577)

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -4048,6 +4048,13 @@ void AuraEffect::HandleModCastingSpeed(AuraApplication const* aurApp, uint8 mode
         return;
     }
 
+    int32 spellGroupVal = target->GetHighestExclusiveSameEffectSpellGroupValue(this, GetAuraType());
+    if (abs(spellGroupVal) >= abs(GetAmount()))
+        return;
+
+    if (spellGroupVal)
+        target->ApplyCastTimePercentMod(float(spellGroupVal), !apply);
+    
     target->ApplyCastTimePercentMod((float)GetAmount(), apply);
 }
 
