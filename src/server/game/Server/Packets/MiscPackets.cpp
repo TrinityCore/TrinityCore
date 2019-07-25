The-Legion-Preservation-Project/AshamaneCore

Core/Packets: added SMSG_PLAY_OBJECT_SOUND packet class

src/server/game/Server/Packets/MiscPackets.cpp
@@ -434,6 +434,16 @@ void WorldPackets::Misc::ObjectUpdateRescued::Read()
     _worldPacket >> ObjectGUID;
 }
 
+WorldPacket const* WorldPackets::Misc::PlayObjectSound::Write()
+{
+    _worldPacket << int32(SoundKitID);
+    _worldPacket << SourceObjectGUID;
+    _worldPacket << TargetObjectGUID;
+    _worldPacket << Position;
+
+    return &_worldPacket;
+}
+
 WorldPacket const* WorldPackets::Misc::PlaySound::Write()
 {
     _worldPacket << int32(SoundKitID);

src/server/game/Server/Packets/MiscPackets.h
@@ -599,6 +599,19 @@ namespace WorldPackets
             ObjectGuid ObjectGUID;
         };
 
+        class PlayObjectSound final : public ServerPacket
+        {
+        public:
+            PlayObjectSound() : ServerPacket(SMSG_PLAY_OBJECT_SOUND, 16 + 16 + 4 + 4 * 4) { }
+
+            WorldPacket const* Write() override;
+
+            ObjectGuid TargetObjectGUID;
+            ObjectGuid SourceObjectGUID;
+            int32 SoundKitID = 0;
+            G3D::Vector3 Position;
+        };
+
         class TC_GAME_API PlaySound final : public ServerPacket
         {
         public:

TrinityCore/TrinityCore

(cherry picked from commit 1272e9eea684d1f155709baee44a301ff938ade5)
# Conflicts:
#	src/server/game/Server/Packets/MiscPackets.cpp
#	src/server/game/Server/Packets/MiscPackets.h
#	src/server/game/Server/Protocol/Opcodes.cpp

src/server/game/Server/Packets/MiscPackets.cpp
@@ -33,14 +33,22 @@ WorldPacket const* WorldPackets::Misc::Weather::Write()
 
 WorldPacket const* WorldPackets::Misc::PlayMusic::Write()
 {
-    _worldPacket << uint32(SoundKitID);
+    _worldPacket << SoundKitID;
+
+    return &_worldPacket;
+}
+
+WorldPacket const* WorldPackets::Misc::PlayObjectSound::Write()
+{
+    _worldPacket << SoundKitID;
+    _worldPacket << SourceObjectGUID;
 
     return &_worldPacket;
 }
 
 WorldPacket const* WorldPackets::Misc::PlaySound::Write()
 {
-    _worldPacket << uint32(SoundKitID);
+    _worldPacket << SoundKitID;
 
     return &_worldPacket;
 }

src/server/game/Server/Packets/MiscPackets.h
@@ -40,7 +40,7 @@ namespace WorldPackets
             WeatherState WeatherID = WeatherState(0);
         };
 
-        class PlayMusic final : public ServerPacket
+        class TC_GAME_API PlayMusic final : public ServerPacket
         {
         public:
             PlayMusic() : ServerPacket(SMSG_PLAY_MUSIC, 4) { }
@@ -49,6 +49,18 @@ namespace WorldPackets
             WorldPacket const* Write() override;
 
             uint32 SoundKitID = 0;
+        };
+
+        class TC_GAME_API PlayObjectSound final : public ServerPacket
+        {
+        public:
+            PlayObjectSound() : ServerPacket(SMSG_PLAY_OBJECT_SOUND, 4 + 8) { }
+
+            WorldPacket const* Write() override;
+
+            uint32 SoundKitID = 0;
+            ObjectGuid SourceObjectGUID;
+            
         };
 
         class TC_GAME_API PlaySound final : public ServerPacket