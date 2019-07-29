dddb88cf41c9b4798700a0761bb25e99a9f3d45e
Ovahlord <dreadkiller@gmx.de>
2019-07-29 14:54:28 +0200

    DB/VP: added formation data for most Vortex Pinnacle trash npcs

---
+++ sql/updates/world/custom/custom_2019_07_29_04_world.sql
@@ -0,0 +1,120 @@
+SET @LEADER := 368197;
+DELETE FROM `creature_formations` WHERE `leaderGUID`= @LEADER;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES
+(@LEADER, 368197, 0, 0, 3, 0, 0),
+(@LEADER, 368180, 0, 0, 3, 0, 0),
+(@LEADER, 368186, 0, 0, 3, 0, 0),
+(@LEADER, 368187, 0, 0, 3, 0, 0),
+(@LEADER, 368179, 0, 0, 3, 0, 0);
+
+SET @LEADER := 368190;
+DELETE FROM `creature_formations` WHERE `leaderGUID`= @LEADER;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES
+(@LEADER, 368190, 0, 0, 3, 0, 0),
+(@LEADER, 368189, 0, 0, 3, 0, 0),
+(@LEADER, 368181, 0, 0, 3, 0, 0),
+(@LEADER, 368188, 0, 0, 3, 0, 0);
+
+SET @LEADER := 368192;
+DELETE FROM `creature_formations` WHERE `leaderGUID`= @LEADER;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES
+(@LEADER, 368192, 0, 0, 3, 0, 0),
+(@LEADER, 368182, 0, 0, 3, 0, 0),
+(@LEADER, 368185, 0, 0, 3, 0, 0),
+(@LEADER, 368183, 0, 0, 3, 0, 0);
+
+SET @LEADER := 368194;
+DELETE FROM `creature_formations` WHERE `leaderGUID`= @LEADER;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES
+(@LEADER, 368194, 0, 0, 3, 0, 0),
+(@LEADER, 368191, 0, 0, 3, 0, 0),
+(@LEADER, 368184, 0, 0, 3, 0, 0),
+(@LEADER, 368193, 0, 0, 3, 0, 0);
+
+SET @LEADER := 368203;
+DELETE FROM `creature_formations` WHERE `leaderGUID`= @LEADER;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES
+(@LEADER, 368203, 0, 0, 3, 0, 0),
+(@LEADER, 368195, 0, 0, 3, 0, 0),
+(@LEADER, 368196, 0, 0, 3, 0, 0);
+SET @LEADER := 368204;
+DELETE FROM `creature_formations` WHERE `leaderGUID`= @LEADER;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES
+(@LEADER, 368204, 0, 0, 3, 0, 0),
+(@LEADER, 368205, 0, 0, 3, 0, 0);
+
+SET @LEADER := 368224;
+DELETE FROM `creature_formations` WHERE `leaderGUID`= @LEADER;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES
+(@LEADER, 368224, 0, 0, 3, 0, 0),
+(@LEADER, 368225, 0, 0, 3, 0, 0),
+(@LEADER, 368226, 0, 0, 3, 0, 0),
+(@LEADER, 368227, 0, 0, 3, 0, 0);
+
+SET @LEADER := 368220;
+DELETE FROM `creature_formations` WHERE `leaderGUID`= @LEADER;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES
+(@LEADER, 368220, 0, 0, 3, 0, 0),
+(@LEADER, 368221, 0, 0, 3, 0, 0),
+(@LEADER, 368222, 0, 0, 3, 0, 0),
+(@LEADER, 368223, 0, 0, 3, 0, 0);
+
+SET @LEADER := 368229;
+DELETE FROM `creature_formations` WHERE `leaderGUID`= @LEADER;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES
+(@LEADER, 368229, 0, 0, 3, 0, 0),
+(@LEADER, 368231, 0, 0, 3, 0, 0),
+(@LEADER, 368241, 0, 0, 3, 0, 0),
+(@LEADER, 368230, 0, 0, 3, 0, 0),
+(@LEADER, 368228, 0, 0, 3, 0, 0),
+(@LEADER, 368240, 0, 0, 3, 0, 0);
+
+SET @LEADER := 368263;
+DELETE FROM `creature_formations` WHERE `leaderGUID`= @LEADER;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES
+(@LEADER, 368263, 0, 0, 3, 0, 0),
+(@LEADER, 368269, 0, 0, 3, 0, 0),
+(@LEADER, 368260, 0, 0, 3, 0, 0),
+(@LEADER, 368277, 0, 0, 3, 0, 0);
+
+SET @LEADER := 368279;
+DELETE FROM `creature_formations` WHERE `leaderGUID`= @LEADER;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES
+(@LEADER, 368279, 0, 0, 3, 0, 0),
+(@LEADER, 368281, 0, 0, 3, 0, 0),
+(@LEADER, 368284, 0, 0, 3, 0, 0),
+(@LEADER, 368286, 0, 0, 3, 0, 0),
+(@LEADER, 368282, 0, 0, 3, 0, 0),
+(@LEADER, 368283, 0, 0, 3, 0, 0),
+(@LEADER, 368285, 0, 0, 3, 0, 0),
+(@LEADER, 368280, 0, 0, 3, 0, 0);
+
+SET @LEADER := 368261;
+DELETE FROM `creature_formations` WHERE `leaderGUID`= @LEADER;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES
+(@LEADER, 368261, 0, 0, 3, 0, 0),
+(@LEADER, 368270, 0, 0, 3, 0, 0),
+(@LEADER, 368267, 0, 0, 3, 0, 0),
+(@LEADER, 368266, 0, 0, 3, 0, 0),
+(@LEADER, 368273, 0, 0, 3, 0, 0);
+
+SET @LEADER := 368268;
+DELETE FROM `creature_formations` WHERE `leaderGUID`= @LEADER;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES
+(@LEADER, 368268, 0, 0, 3, 0, 0),
+(@LEADER, 368275, 0, 0, 3, 0, 0),
+(@LEADER, 368274, 0, 0, 3, 0, 0),
+(@LEADER, 368262, 0, 0, 3, 0, 0),
+(@LEADER, 368276, 0, 0, 3, 0, 0);
+
+SET @LEADER := 368292;
+DELETE FROM `creature_formations` WHERE `leaderGUID`= @LEADER;
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`, `point_1`, `point_2`) VALUES
+(@LEADER, 368292, 0, 0, 3, 0, 0),
+(@LEADER, 368288, 0, 0, 3, 0, 0),
+(@LEADER, 368289, 0, 0, 3, 0, 0),
+(@LEADER, 368287, 0, 0, 3, 0, 0),
+(@LEADER, 368290, 0, 0, 3, 0, 0),
+(@LEADER, 368294, 0, 0, 3, 0, 0),
+(@LEADER, 368293, 0, 0, 3, 0, 0),
+(@LEADER, 368291, 0, 0, 3, 0, 0);
