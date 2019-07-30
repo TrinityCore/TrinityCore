2db7d810e3b730164e8e136597d14c7cd2458f72
Ovahlord <dreadkiller@gmx.de>
2019-07-30 17:43:47 +0200

    Core/Spells: allow PROC_HIT_INTERRUPT also getting triggered by melee spells *fixed Rude Interruption talent (fix by @Keader)

---
+++ sql/updates/world/custom/custom_2019_07_30_00_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_proc` WHERE `SpellId`=-61216;
+INSERT INTO `spell_proc` (`SpellId`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `ProcFlags`, `SpellTypeMask`, `SpellPhaseMask`, `HitMask`, `AttributesMask`, `ProcsPerMinute`, `Chance`, `Cooldown`, `Charges`) VALUES
+(-61216, 0, 4, 8, 0, 0, 0, 4, 2, 4096, 0, 0, 0, 0, 0);
