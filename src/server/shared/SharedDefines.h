b2b7ae8898bf6b0547970a8d01e0b6eeda73e840
Treeston <treeston.mmoc@gmail.com>
2019-07-30 12:04:26 +0200

    Contrib/Misc: There, build fix. 19d71fb follow-up.

--- src/server/shared/SharedDefines.h
+++ src/server/shared/SharedDefines.h
@@ -93,7 +93,7 @@ enum Races
     RACE_TROLL              = 8,  // TITLE Troll
     //RACE_GOBLIN             = 9,
     RACE_BLOODELF           = 10, // TITLE Blood Elf
-    RACE_DRAENEI            = 11  // TITLE Draenei
+    RACE_DRAENEI            = 11 //, TITLE Draenei
     //RACE_FEL_ORC            = 12,
     //RACE_NAGA               = 13,
     //RACE_BROKEN             = 14,
