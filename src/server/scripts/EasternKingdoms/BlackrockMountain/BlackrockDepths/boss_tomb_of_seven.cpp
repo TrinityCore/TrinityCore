cd0fb6edbf54f34c01269b92fe0cc2363e89204e
Sorikoff <46191832+Sorikoff@users.noreply.github.com>
2019-08-05 12:09:09 +0000

    Scripts/Blackrock Depths: Use DB Texts for Doom'rel (#23681)
    * Scripts/Blackrock Depths: Use DB Texts for Doom'rel
    
    * Quick Fix
    
    (cherry picked from commit 33807a065a24410cb3daf17cea6eb31af408c4e8)

--- src/server/scripts/EasternKingdoms/BlackrockMountain/BlackrockDepths/boss_tomb_of_seven.cpp
+++ src/server/scripts/EasternKingdoms/BlackrockMountain/BlackrockDepths/boss_tomb_of_seven.cpp
@@ -114,7 +114,14 @@ enum DoomrelSpells
     SPELL_DEMONARMOR                                       = 13787,
     SPELL_SUMMON_VOIDWALKERS                               = 15092
 };
+enum DoomrelText
+{
+    GOSSIP_SELECT_DOOMREL                                  = 1828,
+    GOSSIP_MENU_ID_CONTINUE                                = 1,
 
+    GOSSIP_MENU_CHALLENGE                                  = 1947,
+    GOSSIP_MENU_ID_CHALLENGE                               = 0
+};
 enum DoomrelEvents
 {
     EVENT_SHADOW_BOLT_VOLLEY                               = 1,
@@ -123,9 +130,6 @@ enum DoomrelEvents
     EVENT_DEMONARMOR                                       = 4,
     EVENT_SUMMON_VOIDWALKERS                               = 5
 };
-
-#define GOSSIP_ITEM_CHALLENGE   "Your bondage is at an end, Doom'rel. I challenge you!"
-#define GOSSIP_SELECT_DOOMREL   "[PH] Continue..."
 
 class boss_doomrel : public CreatureScript
 {
@@ -233,7 +237,7 @@ class boss_doomrel : public CreatureScript
                 switch (action)
                 {
                     case GOSSIP_ACTION_INFO_DEF + 1:
-                        AddGossipItemFor(player, GossipOptionIcon::None, GOSSIP_SELECT_DOOMREL, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
+                        AddGossipItemFor(player, GOSSIP_SELECT_DOOMREL, GOSSIP_MENU_ID_CONTINUE, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
                         SendGossipMenuFor(player, 2605, me->GetGUID());
                         break;
                     case GOSSIP_ACTION_INFO_DEF + 2:
@@ -251,7 +255,7 @@ class boss_doomrel : public CreatureScript
 
             bool GossipHello(Player* player) override
             {
-                AddGossipItemFor(player, GossipOptionIcon::None, GOSSIP_ITEM_CHALLENGE, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
+                AddGossipItemFor(player, GOSSIP_MENU_CHALLENGE, GOSSIP_MENU_ID_CHALLENGE, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
                 SendGossipMenuFor(player, 2601, me->GetGUID());
 
                 return true;
