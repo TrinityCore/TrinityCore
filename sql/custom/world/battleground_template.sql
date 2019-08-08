e775b08092fe0491b28c035917c687be57c612a1
JinnBlue <jinnblue@126.com>
2019-08-08 22:07:42 +0800

    [3.3.5] DB/Locale: add some zhCN locales

---
+++ sql/custom/world/battleground_template.sql
@@ -0,0 +1,13 @@
+update battleground_template set Comment='奥特兰克山谷' where id=1;
+update battleground_template set Comment='战歌峡谷' where id=2;
+update battleground_template set Comment='阿拉斯高地' where id=3;
+update battleground_template set Comment='风暴之眼' where id=7;
+update battleground_template set Comment='纳格兰竞技场' where id=4;
+update battleground_template set Comment='刀锋山竞技场' where id=5;
+update battleground_template set Comment='竞技场' where id=6;
+update battleground_template set Comment='洛丹伦废墟' where id=8;
+update battleground_template set Comment='远古海滩' where id=9;
+update battleground_template set Comment='达拉然下水道' where id=10;
+update battleground_template set Comment='勇士之环' where id=11;
+update battleground_template set Comment='征服之岛' where id=30;
+update battleground_template set Comment='随机战场' where id=32;
---
+++ sql/custom/world/outdoorpvp_template.sql
@@ -0,0 +1,6 @@
+update outdoorpvp_template set comment='地狱火半岛' where TypeId=1;
+update outdoorpvp_template set comment='纳格兰' where TypeId=2;
+update outdoorpvp_template set comment='特罗卡森林' where TypeId=3;
+update outdoorpvp_template set comment='赞加沼泽' where TypeId=4;
+update outdoorpvp_template set comment='希利苏斯' where TypeId=5;
+update outdoorpvp_template set comment='东瘟疫之地' where TypeId=6;
