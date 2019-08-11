e0e1978b08f7bae5a24bf52944873ad0e01a7fe7
Treeston <treeston.mmoc@gmail.com>
2019-08-11 13:56:42 +0200

    Entities/Player: ReacquireSpellFocusTarget is now private. It should've been private all along.
    (cherry picked from commit 1c2b1cbba82fcd523ccd0cceafe532f35d71dcc1)

--- src/server/game/Entities/Creature/Creature.h
+++ src/server/game/Entities/Creature/Creature.h
@@ -348,7 +348,6 @@ class TC_GAME_API Creature : public Unit, public GridObject<Creature>, public Ma
 
         // Handling caster facing during spellcast
         void SetTarget(ObjectGuid const& guid) override;
-        void ReacquireSpellFocusTarget();
         void DoNotReacquireSpellFocusTarget() { _spellFocusInfo.Delay = 0; }
         void SetSpellFocus(Spell const* focusSpell, WorldObject const* target);
         bool HasSpellFocus(Spell const* focusSpell = nullptr) const override;
@@ -439,6 +438,7 @@ class TC_GAME_API Creature : public Unit, public GridObject<Creature>, public Ma
         bool m_respawnCompatibilityMode;
 
         /* Spell focus system */
+        void ReacquireSpellFocusTarget();
         struct
         {
             ::Spell const* Spell = nullptr;
