4df96f81260b7ce8f6db4819464fdfba223711a6
Ovahlord <dreadkiller@gmx.de>
2019-08-13 18:16:15 +0200

    Core/Scripts: removed an unintended change from last commit

--- src/server/scripts/Commands/cs_script_loader.cpp
+++ src/server/scripts/Commands/cs_script_loader.cpp
@@ -57,7 +57,6 @@ void AddSC_tele_commandscript();
 void AddSC_ticket_commandscript();
 void AddSC_titles_commandscript();
 void AddSC_wp_commandscript();
-void AddSC_dev_commandscript();
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
@@ -104,5 +103,4 @@ void AddCommandsScripts()
     AddSC_ticket_commandscript();
     AddSC_titles_commandscript();
     AddSC_wp_commandscript();
-    AddSC_dev_commandscript();
 }
