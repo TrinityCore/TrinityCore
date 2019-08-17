7d60b9a289ee0942cf57b49626c0f3f9caeed331
KobeSagami <44218804+KobeSagami@users.noreply.github.com>
2019-08-17 09:07:16 -0600

    Core/Misc: Minor change to racial wisp handling to make custom easier (or so i heard) (#22954)

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -4235,7 +4235,8 @@ void Player::BuildPlayerRepop()
     packet.PlayerGUID = GetGUID();
     GetSession()->SendPacket(packet.Write());
 
-    if (getRace() == RACE_NIGHTELF)
+    // If the player has the Wisp racial then cast the Wisp aura on them
+    if (HasSpell(20585))
         CastSpell(this, 20584, true);
     CastSpell(this, 8326, true);
 
