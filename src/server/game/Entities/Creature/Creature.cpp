9665bbf66522e607207a0ce129e8683ba7b23019
Treeston <treeston.mmoc@gmail.com>
2019-08-18 12:30:21 +0200

    Entities/Creature: Fix a bug that caused map respawn timers to be scheduled for compatibility mode creatures.

--- src/server/game/Entities/Creature/Creature.cpp
+++ src/server/game/Entities/Creature/Creature.cpp
@@ -2405,6 +2405,7 @@ void Creature::SaveRespawnTime(uint32 forceDelay)
         ri.spawnId = m_spawnId;
         ri.respawnTime = m_respawnTime;
         GetMap()->SaveRespawnInfoDB(ri);
+        return;
     }
 
     time_t thisRespawnTime = forceDelay ? GameTime::GetGameTime() + forceDelay : m_respawnTime;
