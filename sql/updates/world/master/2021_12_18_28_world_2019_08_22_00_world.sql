f2e03dc9e57469ce2cb78f4ef36c6c7bd8dd22e3
Keader <keader.android@gmail.com>
2019-08-21 23:03:25 -0300

    Core/Scripts: Proper fix Gal'Darah Puncture remove
    (cherry picked from commit cbc01f35b6b80c1069b2274a619c8f7a3ff730c7)

---
+++ sql/updates/world/master/2021_12_18_28_world_2019_08_22_00_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_gal_darah_clear_puncture';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(60022,'spell_gal_darah_clear_puncture');
