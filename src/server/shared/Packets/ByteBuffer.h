c7c6fad06112e56bdd2627b4111c425cc967cd49
Shauren <shauren.trinity@gmail.com>
2019-08-24 15:53:12 +0200

    Core/ByteBuffer: Replace throw() with noexcept

--- src/server/shared/Packets/ByteBuffer.h
+++ src/server/shared/Packets/ByteBuffer.h
@@ -31,12 +31,12 @@ class MessageBuffer;
 class TC_SHARED_API ByteBufferException : public std::exception
 {
 public:
-    ~ByteBufferException() throw() { }
+    ~ByteBufferException() noexcept { }
 
-    char const* what() const throw() override { return msg_.c_str(); }
+    char const* what() const noexcept override { return msg_.c_str(); }
 
 protected:
-    std::string & message() throw() { return msg_; }
+    std::string& message() { return msg_; }
 
 private:
     std::string msg_;
@@ -47,7 +47,7 @@ class TC_SHARED_API ByteBufferPositionException : public ByteBufferException
 public:
     ByteBufferPositionException(size_t pos, size_t size, size_t valueSize);
 
-    ~ByteBufferPositionException() throw() { }
+    ~ByteBufferPositionException() noexcept { }
 };
 
 class TC_SHARED_API ByteBuffer
@@ -98,7 +98,7 @@ class TC_SHARED_API ByteBuffer
             return *this;
         }
 
-        ByteBuffer& operator=(ByteBuffer&& right)
+        ByteBuffer& operator=(ByteBuffer&& right) noexcept
         {
             if (this != &right)
             {
