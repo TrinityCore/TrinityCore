1d178426e028c30b8fe3a132103499a688f49435
Ovahlord <dreadkiller@gmx.de>
2019-08-25 19:18:02 +0200

    DB/Spells: added missing attack power coefficient bonuses for Druid's Thrash ability

---
+++ sql/updates/world/custom/custom_2019_08_25_02_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_bonus_data` WHERE `entry`= 77758;
+INSERT INTO `spell_bonus_data` (`entry`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES
+(77758, 0.0982, 0.0167, 'Druid - Trash (Bear Form)');
