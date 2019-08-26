6200b10c8cf5bcdedc6852cdb3d6d31db6dbd358
Ovahlord <dreadkiller@gmx.de>
2019-08-26 22:28:27 +0200

    DB: fixed a typo in Death Knight presence rework sql file

--- sql/updates/world/custom/custom_2019_08_25_01_world.sql
+++ sql/updates/world/custom/custom_2019_08_25_01_world.sql
@@ -1,7 +1,7 @@
 DELETE FROM `spell_script_names` WHERE `ScriptName` IN
 ('spell_dk_improved_blood_presence',
 'spell_dk_improved_frost_presence',
-'spell_dk_improved_holy_presence',
+'spell_dk_improved_unholy_presence',
 'spell_dk_improved_presence');
 
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
