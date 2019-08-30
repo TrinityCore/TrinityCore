5fd124c2795f39df6829d70f0e1d5bac32c4255c
Ovahlord <dreadkiller@gmx.de>
2019-08-30 12:30:17 +0200

    DB: fixed a typo

--- sql/updates/world/custom/custom_2019_08_30_00_world.sql
+++ sql/updates/world/custom/custom_2019_08_30_00_world.sql
@@ -1,3 +1,3 @@
-DELETE FROM `spell_proc` WHERE `SpellId` -48532;
+DELETE FROM `spell_proc` WHERE `SpellId`= -48532;
 INSERT INTO `spell_proc` (`SpellId`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `ProcFlags`, `SpellTypeMask`, `SpellPhaseMask`, `HitMask`, `AttributesMask`, `Cooldown`) VALUES
 (-48532, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3000);
