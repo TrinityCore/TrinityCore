8da40c3605f56d837deae590833f0ffe5279893d
Ovahlord <dreadkiller@gmx.de>
2019-08-31 02:12:53 +0200

    DB/Spells: fixed Warrior T11 4p DPS proc

---
+++ sql/updates/world/custom/custom_2019_08_31_00_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_proc` WHERE `SpellId`= 90295;
+INSERT INTO `spell_proc` (`SpellId`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `ProcFlags`, `SpellTypeMask`, `SpellPhaseMask`, `HitMask`, `AttributesMask`, `Cooldown`) VALUES
+(90295, 4, 0x00000004, 0x04000000, 0, 0, 1, 1, 0, 0, 0);
