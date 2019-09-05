8418c1759094cbfa0415c1fa710537ddbcf3652d
Ovahlord <dreadkiller@gmx.de>
2019-09-05 18:29:19 +0200

    DB: fixed a typo

--- sql/updates/world/custom/custom_2019_09_05_00_world.sql
+++ sql/updates/world/custom/custom_2019_09_05_00_world.sql
@@ -1,5 +1,5 @@
 DELETE FROM `spell_proc` WHERE `SpellId` IN (-53486, -84652);
 INSERT INTO `spell_proc` (`SpellId`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellTypeMask`, `SpellPhaseMask`) VALUES
-(84652, 8, 0x2, 0x00800000, 1, 2);
+(-84652, 8, 0x2, 0x00800000, 1, 2);
 
 UPDATE `spell_proc` SET `SpellFamilyName`= 11, `SpellFamilyMask0`= 0x00000001 WHERE `SpellId`= -82984;
