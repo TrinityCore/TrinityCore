28eda522b1bef6d354d5a6b54fc0864ea503121a
Shauren <shauren.trinity@gmail.com>
2019-10-25 19:29:52 +0200

    Core/Entities: Add support for future optional updatefields

--- src/server/game/Entities/Object/Object.h
+++ src/server/game/Entities/Object/Object.h
@@ -115,6 +115,12 @@ namespace UF
     {
         setter.Clear();
     }
+
+    template<typename T>
+    inline void RemoveOptionalUpdateFieldValue(OptionalUpdateFieldSetter<T>& setter)
+    {
+        setter.RemoveValue();
+    }
 }
 
 class TC_GAME_API Object
@@ -252,6 +258,13 @@ class TC_GAME_API Object
         {
             AddToObjectUpdateIfNeeded();
             UF::ClearDynamicUpdateFieldValues(setter);
+        }
+
+        template<typename T>
+        void RemoveOptionalUpdateFieldValue(UF::OptionalUpdateFieldSetter<T> setter)
+        {
+            AddToObjectUpdateIfNeeded();
+            UF::RemoveOptionalUpdateFieldValue(setter);
         }
 
         // stat system helpers
