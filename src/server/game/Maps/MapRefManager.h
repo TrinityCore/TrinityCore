06c6b30ed484850228c57313dc2c024c38e2055f
ccrs <ccrs@users.noreply.github.com>
2019-10-26 23:33:30 +0200

    Scripts/ScarletMonastery: instance script cleanup (#23889)
    Adapt InstanceScript into newer structure and style
    Adapt existing boss scripts into newer structure and style
    Improve existing boss related definitions
    
    Should make each boss more functionally reliable, but nothing new has been added

--- src/server/game/Maps/MapRefManager.h
+++ src/server/game/Maps/MapRefManager.h
@@ -29,13 +29,13 @@ class MapRefManager : public RefManager<Map, Player>
         typedef LinkedListHead::Iterator<MapReference> iterator;
         typedef LinkedListHead::Iterator<MapReference const> const_iterator;
 
-        MapReference* getFirst()             { return (MapReference*)RefManager<Map, Player>::getFirst(); }
+        MapReference* getFirst() { return (MapReference*)RefManager<Map, Player>::getFirst(); }
         MapReference const* getFirst() const { return (MapReference const*)RefManager<Map, Player>::getFirst(); }
 
         iterator begin() { return iterator(getFirst()); }
-        iterator end()   { return iterator(nullptr); }
+        iterator end() { return iterator(nullptr); }
 
         const_iterator begin() const { return const_iterator(getFirst()); }
-        const_iterator end() const   { return const_iterator(nullptr); }
+        const_iterator end() const { return const_iterator(nullptr); }
 };
 #endif
