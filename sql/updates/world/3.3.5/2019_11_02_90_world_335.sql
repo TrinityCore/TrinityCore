7919224ec282616baed478e70266dcbebaecc559
Miorey <clem.balea@gmail.com>
2019-11-02 12:39:22 +0100

    fix: team members

--- sql/updates/world/3.3.5/2019_11_02_90_world_335.sql
+++ sql/updates/world/3.3.5/2019_11_02_90_world_335.sql
@@ -6,10 +6,10 @@ WHERE ID = 1;
 
 -- Warsong Gulch
 UPDATE `battleground_template`
-SET `MinPlayersPerTeam` = 5, `MaxPlayersPerTeam` = 10
+SET `MinPlayersPerTeam` = 3, `MaxPlayersPerTeam` = 6
 WHERE ID = 2;
 
 -- Arathi Basin
 UPDATE `battleground_template`
-SET `MinPlayersPerTeam` = 10, `MaxPlayersPerTeam` = 20
+SET `MinPlayersPerTeam` = 5, `MaxPlayersPerTeam` = 10
 WHERE ID = 3;
