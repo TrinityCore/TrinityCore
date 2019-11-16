a753668e5cce1c78d159b79073d741629fa1126e
Ovahlord <dreadkiller@gmx.de>
2019-11-16 16:13:26 +0100

    DB/Spells: allow Rolling Thunder to proc from triggered mastery spells as well

---
+++ sql/updates/world/custom/custom_2019_11_16_01_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_proc` WHERE `SpellId`= -88756;
+INSERT INTO `spell_proc` (`SpellId`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `ProcFlags`, `SpellTypeMask`, `SpellPhaseMask`, `HitMask`, `AttributesMask`, `Cooldown`, `Charges`) VALUES
+(-88756, 8, 11, 0x3, 0, 0x8000, 0, 1, 2, 0, 0x2, 0, 0);
