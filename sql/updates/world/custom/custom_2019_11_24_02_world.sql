39f021a800c6ed5ba8b4362557bd7eb3a87b3588
Ovahlord <dreadkiller@gmx.de>
2019-11-24 12:06:23 +0100

    DB/Spells: corrected procs per minute value for Killing Machine

---
+++ sql/updates/world/custom/custom_2019_11_24_02_world.sql
@@ -0,0 +1,3 @@
+UPDATE `spell_proc` SET `ProcsPerMinute`= 4.5 WHERE `SpellId`= 51128;
+UPDATE `spell_proc` SET `ProcsPerMinute`= 3 WHERE `SpellId`= 51127;
+UPDATE `spell_proc` SET `ProcsPerMinute`= 1.5 WHERE `SpellId`= 51123;
