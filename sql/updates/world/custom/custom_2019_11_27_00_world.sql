679af9e3e8e904a050546ea46060cf3808f399c5
Ovahlord <dreadkiller@gmx.de>
2019-11-27 18:09:30 +0100

    DB: fixed applying last sql

--- sql/updates/world/custom/custom_2019_11_27_00_world.sql
+++ sql/updates/world/custom/custom_2019_11_27_00_world.sql
@@ -1,4 +1,4 @@
 -- Lightning Shield
-UPDATE `spell_proc` SET `Cooldown`= 1500 WHERE `SpellId`= 324
+UPDATE `spell_proc` SET `Cooldown`= 1500 WHERE `SpellId`= 324;
 -- Earth Shield
 UPDATE `spell_proc` SET `Cooldown`= 3500 WHERE `SpellId`= 974;
