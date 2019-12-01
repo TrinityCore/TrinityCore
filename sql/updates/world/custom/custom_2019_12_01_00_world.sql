be32801d4ab39a7842dffd65e77c6a591b44ca61
Ovahlord <dreadkiller@gmx.de>
2019-12-01 20:16:10 +0100

    DB/Spells: added missing proc cooldown for Pit Fighter

---
+++ sql/updates/world/custom/custom_2019_12_01_00_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_proc` WHERE `SpellId`= 109995;
+INSERT INTO `spell_proc` (`SpellId`, `SpellFamilyName`, `SpellFamilyMask0`, `ProcFlags`, `SpellTypeMask`, `SpellPhaseMask`, `HitMask`, `Cooldown`) VALUES
+(109995, 0, 0, 0, 1, 2, 0, 50000);
