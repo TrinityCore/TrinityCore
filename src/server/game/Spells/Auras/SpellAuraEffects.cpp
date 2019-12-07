d4b00df78191bb0e6b79bc99e6ca7a2bf8091eba
Treeston <Treeston@users.noreply.github.com>
2019-12-07 19:13:06 +0100

    Core/Spell: Reset threat to zero when the hunter use Feign Death
    Closes #23874

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -2190,6 +2190,9 @@ void AuraEffect::HandleFeignDeath(AuraApplication const* aurApp, uint8 mode, boo
                 }
             }
         }
+		
+        for (auto& pair : target->GetThreatManager().GetThreatenedByMeList())
+          pair.second->ScaleThreat(0.0f);
 
         if (target->GetMap()->IsDungeon()) // feign death does not remove combat in dungeons
         {
