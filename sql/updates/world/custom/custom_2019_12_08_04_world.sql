12842e5786abf7f0a43bc8cf53e1d797058d9693
Ovahlord <dreadkiller@gmx.de>
2019-12-08 21:08:15 +0100

    DB: fixed spacing in 68af0d49903ca190b4abb3a0e382aa98bce8dd69

--- sql/updates/world/custom/custom_2019_12_08_04_world.sql
+++ sql/updates/world/custom/custom_2019_12_08_04_world.sql
@@ -4,4 +4,4 @@ UPDATE `spell_proc` SET `SpellFamilyMask0`= 0x00000080, `SpellFamilyMask1`= 0x00
 -- Demonic Pact
 DELETE FROM `spell_pet_auras` WHERE `spell`= 47236;
 INSERT INTO `spell_pet_auras` (`spell`, `effectId`, `pet`, `aura`) VALUES
-(47236 , 0, 0, 53646);
+(47236, 0, 0, 53646);
