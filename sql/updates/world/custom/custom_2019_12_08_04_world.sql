68af0d49903ca190b4abb3a0e382aa98bce8dd69
Ovahlord <dreadkiller@gmx.de>
2019-12-08 21:07:09 +0100

    DB/Spells: * updated Soul Leech proc entry to Cataclysm * fixed Demonic Pact

---
+++ sql/updates/world/custom/custom_2019_12_08_04_world.sql
@@ -0,0 +1,7 @@
+-- Soul Leech
+UPDATE `spell_proc` SET `SpellFamilyMask0`= 0x00000080, `SpellFamilyMask1`= 0x00000080 | 0x00020000 WHERE `SpellId`= -30293;
+
+-- Demonic Pact
+DELETE FROM `spell_pet_auras` WHERE `spell`= 47236;
+INSERT INTO `spell_pet_auras` (`spell`, `effectId`, `pet`, `aura`) VALUES
+(47236 , 0, 0, 53646);
