06acf137a0da94f5e33139e454f27f39458a3dba
Ovahlord <dreadkiller@gmx.de>
2019-12-12 19:48:21 +0100

    DB: fixed applying custom_2019_12_09_02_world.sql

--- sql/updates/world/custom/custom_2019_12_09_02_world.sql
+++ sql/updates/world/custom/custom_2019_12_09_02_world.sql
@@ -4,4 +4,4 @@ DELETE FROM `spell_script_names` WHERE `ScriptName` IN
 
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (82692, 'spell_hun_focus_fire'),
-(19615, 'spell_hun_frenzy_effect')
+(19615, 'spell_hun_frenzy_effect');
