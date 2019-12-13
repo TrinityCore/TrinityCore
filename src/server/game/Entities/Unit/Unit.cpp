10a7b29b4de3bc75180b2fa90654740ed9122268
Ovahlord <dreadkiller@gmx.de>
2019-12-13 23:35:40 +0100

    Core/Units: removed a unneeded player type check

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -14504,7 +14504,7 @@ void Unit::BuildValuesUpdate(uint8 updateType, ByteBuffer* data, Player* target)
                     if (!target->CanSeeSpellClickOn(creature))
                         appendValue &= ~UNIT_NPC_FLAG_SPELLCLICK;
 
-                    if (target->IsPlayer() && !creature->IsClassTrainerOf(target->ToPlayer()))
+                    if (!creature->IsClassTrainerOf(target->ToPlayer()))
                         appendValue &= ~UNIT_NPC_FLAG_TRAINER_CLASS;
                 }
 
