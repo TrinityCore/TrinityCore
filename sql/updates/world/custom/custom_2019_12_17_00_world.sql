ee1fdbb938a631b16932111567a2405a4d528015
Ovahlord <dreadkiller@gmx.de>
2019-12-17 03:14:23 +0100

    DB/Quests: added some missing completion texts and emotes for Mount Hyjal quests

---
+++ sql/updates/world/custom/custom_2019_12_17_00_world.sql
@@ -0,0 +1,18 @@
+UPDATE `quest_request_items` SET `CompletionText`='Did you succeed in your task, $n?', `VerifiedBuild`=27326 WHERE `ID`=25554; -- Secrets of the Flame
+UPDATE `quest_request_items` SET `CompletionText`='Did you get the documents, $n?', `VerifiedBuild`=27326 WHERE `ID`=25296; -- Gather the Intelligence
+UPDATE `quest_request_items` SET `CompletionText`='Yes, $ndamus?', `VerifiedBuild`=27326 WHERE `ID`=25224; -- In Bloom
+UPDATE `quest_request_items` SET `EmoteOnComplete`=6, `CompletionText`='I am at my wit\'s end, $n! Aessina is silent.$B$BHave you found something? What is this?', `VerifiedBuild`=27326 WHERE `ID`=25372; -- Aessina's Miracle
+UPDATE `quest_request_items` SET `EmoteOnComplete`=274, `VerifiedBuild`=27326 WHERE `ID`=25776; -- Sethria's Demise
+UPDATE `quest_request_items` SET `EmoteOnComplete`=1, `VerifiedBuild`=27326 WHERE `ID`=25761; -- Disassembly
+UPDATE `quest_request_items` SET `EmoteOnComplete`=6, `VerifiedBuild`=27326 WHERE `ID`=25758; -- A Gap in Their Armor
+UPDATE `quest_request_items` SET `EmoteOnComplete`=1, `VerifiedBuild`=27326 WHERE `ID`=25507; -- Hell's Shells
+UPDATE `quest_request_items` SET `EmoteOnComplete`=6, `VerifiedBuild`=27326 WHERE `ID`=25493; -- Fresh Bait
+UPDATE `quest_request_items` SET `EmoteOnComplete`=6, `VerifiedBuild`=27326 WHERE `ID`=25428; -- Black Heart of Flame
+UPDATE `quest_request_items` SET `EmoteOnComplete`=6, `VerifiedBuild`=27326 WHERE `ID`=25385; -- Save the Wee Animals
+UPDATE `quest_request_items` SET `EmoteOnComplete`=1, `VerifiedBuild`=27326 WHERE `ID`=25408; -- Seeds of Their Demise
+UPDATE `quest_request_items` SET `EmoteOnComplete`=6, `VerifiedBuild`=27326 WHERE `ID`=25385; -- Save the Wee Animals
+UPDATE `quest_request_items` SET `EmoteOnComplete`=6, `VerifiedBuild`=27326 WHERE `ID`=25599; -- Cindermaul, the Portal Master
+UPDATE `quest_request_items` SET `EmoteOnComplete`=6, `VerifiedBuild`=27326 WHERE `ID`=25298; -- Free Your Mind, the Rest Follows
+UPDATE `quest_request_items` SET `EmoteOnComplete`=6, `CompletionText`='Have you created some havoc down there, $n?', `VerifiedBuild`=27326 WHERE `ID`=25234; -- In the Rear With the Gear
+UPDATE `quest_request_items` SET `CompletionText`='The key, $n.  Did you get it?', `VerifiedBuild`=27326 WHERE `ID`=25321; -- Twilight Captivity
+UPDATE `quest_request_items` SET `CompletionText`='Yes, $n?', `VerifiedBuild`=27326 WHERE `ID`=25472; -- The Flameseer's Staff
