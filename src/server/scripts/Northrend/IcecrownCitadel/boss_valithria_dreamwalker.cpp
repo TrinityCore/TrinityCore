41b7b39e3c72cb396c6789ed6432312fbf466106
Killyana <morphone1@gmail.com>
2019-12-18 21:41:40 +0100

    Spell: Move Column of Frost & Mana Void to the cpp
    (cherry picked from commit 74f92d776b2c85c8795bae1a216abd8e7ea204fa)

--- src/server/scripts/Northrend/IcecrownCitadel/boss_valithria_dreamwalker.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_valithria_dreamwalker.cpp
@@ -60,6 +60,8 @@ enum Spells
     SPELL_CLEAR_ALL                     = 71721,
     SPELL_AWARD_REPUTATION_BOSS_KILL    = 73843,
     SPELL_CORRUPTION_VALITHRIA          = 70904,
+    SPELL_MANA_VOID_AURA                = 71085,
+    SPELL_COLUMN_OF_FROST_AURA          = 70715,
     SPELL_WEAKENED_SOUL                 = 72232,
 
     // The Lich King
@@ -744,9 +746,15 @@ class npc_risen_archmage : public CreatureScript
             void JustSummoned(Creature* summon) override
             {
                 if (summon->GetEntry() == NPC_COLUMN_OF_FROST)
+                {
+                    summon->CastSpell(summon, SPELL_COLUMN_OF_FROST_AURA, true);
                     summon->m_Events.AddEvent(new DelayedCastEvent(summon, SPELL_COLUMN_OF_FROST_DAMAGE, ObjectGuid::Empty, 8000), summon->m_Events.CalculateTime(2000));
+                }
                 else if (summon->GetEntry() == NPC_MANA_VOID)
+                {
+                    summon->CastSpell(summon, SPELL_MANA_VOID_AURA, true);
                     summon->DespawnOrUnsummon(36000);
+                }
             }
 
             void UpdateAI(uint32 diff) override
