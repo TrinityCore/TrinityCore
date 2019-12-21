f9a6289b2d988e08b850c9807213402907feb991
jackpoz <giacomopoz@gmail.com>
2019-12-21 23:27:46 +0100

    Core/Movement: Fix wrong orientation set when pausing movement

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -10171,7 +10171,8 @@ void Unit::StopMoving()
         return;
 
     // Update position now since Stop does not start a new movement that can be updated later
-    UpdateSplinePosition();
+    if (movespline->Started())
+        UpdateSplinePosition();
     Movement::MoveSplineInit init(this);
     init.Stop();
 }
