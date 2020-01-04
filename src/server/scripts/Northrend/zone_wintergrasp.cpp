d6d3f06f06f498fde74425e3517426f50edc4ddd
Aokromes <aokromes@gmail.com>
2020-01-04 18:31:06 +0100

    New year

--- src/server/scripts/Northrend/zone_wintergrasp.cpp
+++ src/server/scripts/Northrend/zone_wintergrasp.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2008-2020 TrinityCore <https://www.trinitycore.org/>
+/* This file is part of the TrinityCore Project. See AUTHORS file for Copyright information
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
