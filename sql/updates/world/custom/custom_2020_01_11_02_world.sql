a404624fa214c6dd2eab619c873be03329f18102
Ovahlord <dreadkiller@gmx.de>
2020-01-11 15:31:26 +0100

    DB/Creatures: corrected gold loot values for heroic Shadowfang Keep and Stonecore trash npcs

---
+++ sql/updates/world/custom/custom_2020_01_11_02_world.sql
@@ -0,0 +1,5 @@
+-- Shadowfang Keep trash gold
+UPDATE `creature_template` SET `mingold`= 4000, `maxgold`= 21000 WHERE `entry` IN (48842, 47132, 48851, 48942, 48967, 48968, 48973, 48981, 48985, 49059, 49130, 49184, 49186, 49164, 49126, 49174, 49155, 49160, 49187, 48988, 48919);
+
+-- Stonecore trash gold
+UPDATE `creature_template` SET `mingold`= 4000, `maxgold`= 21000 WHERE `entry` IN (49647, 49661, 49662, 49667, 49663, 49665, 49667, 49664, 49669, 49655, 49666, 49648, 49660);
