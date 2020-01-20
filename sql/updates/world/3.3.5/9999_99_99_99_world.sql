a8c6c4c58d242ee4904437f5d41d4117eb3ba25c
Sorikoff <46191832+Sorikoff@users.noreply.github.com>
2020-01-20 21:59:45 +0200

    Scripts/Borean Tundra: Remove item script

--- sql/updates/world/3.3.5/9999_99_99_99_world.sql
+++ sql/updates/world/3.3.5/9999_99_99_99_world.sql
@@ -1,6 +1,12 @@
 UPDATE `creature_template` SET `ScriptName`='',`AIName`='SmartAI' WHERE `entry` IN (26248,26249);
 UPDATE `creature_template` SET `unit_flags`=768 WHERE `entry`=26250;
 
+UPDATE `item_template` SET `ScriptName`='' WHERE `entry`=35704;
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=17 AND `SourceEntry`=46797;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
+(17,0,46797,0,0,29,0,26248,15,0,0,0,0,'','Spell \'Set Explosives Cart\' can be used within 15 yards of Northern Sinkhole'),
+(17,0,46797,0,1,29,0,26249,15,0,0,0,0,'','Spell \'Set Explosives Cart\' can be used within 15 yards of Southern Sinkhole');
+
 DELETE FROM `smart_scripts` WHERE `entryorguid` IN (26248,26249) AND `source_type`=0;
 DELETE FROM `smart_scripts` WHERE `entryorguid` IN (2624800,2624900) AND `source_type`=9;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_param4`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
