e410c3498b1a4a771b861601179ba54170095669
Wyrserth <wyrserth@protonmail.com>
2020-01-27 03:23:38 +0100

    Core/SAI: add param3 to SMART_ACTION_CALL_TIMED_ACTIONLIST to allow it to override a currently running actionlist.

--- src/server/game/AI/SmartScripts/SmartScript.cpp
+++ src/server/game/AI/SmartScripts/SmartScript.cpp
@@ -3860,8 +3860,8 @@ void SmartScript::SetScript9(SmartScriptHolder& e, uint32 entry)
         return;
     }
 
-    // Do NOT allow to start a new actionlist if a previous one is already running. We need to always finish the current actionlist
-    if (!mTimedActionList.empty())
+    // Do NOT allow to start a new actionlist if a previous one is already running, unless explicitly allowed. We need to always finish the current actionlist
+    if (!e.action.timedActionList.allowOverride && !mTimedActionList.empty())
         return;
 
     mTimedActionList.clear();
--- src/server/game/AI/SmartScripts/SmartScriptMgr.h
+++ src/server/game/AI/SmartScripts/SmartScriptMgr.h
@@ -972,6 +972,7 @@ struct SmartAction
         {
             uint32 id;
             uint32 timerType;
+            uint32 allowOverride;
         } timedActionList;
 
         struct
