4965d188994b277eb15617965065424340142ba2
ratkosrb <ratkomladic2@abv.bg>
2020-02-04 22:29:37 +0200

    Fix query.

--- sql/updates/world/3.3.5/2020_99_99_99_world_335.sql
+++ sql/updates/world/3.3.5/2020_99_99_99_world_335.sql
@@ -1,6 +1,6 @@
 -- Use more appropriate name for distance used by random movement generator.
 ALTER TABLE `creature`
-	CHANGE COLUMN `spawndist` `wander_distance` FLOAT NOT NULL DEFAULT '0' AFTER `spawntimesecsmax`;
+	CHANGE COLUMN `spawndist` `wander_distance` FLOAT NOT NULL DEFAULT '0' AFTER `spawntimesecs`;
 
 -- Update name used by chat command.
 UPDATE `trinity_string` SET `content_default`='Wander distance changed to: %f' WHERE `entry`=297;
