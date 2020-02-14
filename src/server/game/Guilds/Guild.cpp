7e818771c409efc5e3fa117b468eba06de84e5b7
LilleCarl <git@lillecarl.com>
2020-02-14 11:53:55 +0100

    Optimize one lookup away in Guild::DeleteMember
    This is not a performance critical region.

--- src/server/game/Guilds/Guild.cpp
+++ src/server/game/Guilds/Guild.cpp
@@ -2400,9 +2400,12 @@ void Guild::DeleteMember(SQLTransaction& trans, ObjectGuid guid, bool isDisbandi
     // Call script on remove before member is actually removed from guild (and database)
     sScriptMgr->OnGuildRemoveMember(this, player, isDisbanding, isKicked);
 
-    delete (GetMember(guid));
-
-    m_members.erase(lowguid);
+    auto memberItr = m_members.find(guid);
+    if (memberItr != m_members.end())
+    {
+        delete memberItr->second;
+        m_members.erase(memberItr);
+    }
 
     // If player not online data in data field will be loaded from guild tabs no need to update it !!
     if (player)
