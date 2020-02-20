d0f946f30a7fb9b6a2b5d15560a0c5d9272bd9ef
Ovahlord <dreadkiller@gmx.de>
2020-02-20 18:33:13 +0100

    DB/Trainers: added gossip trainer assignment for Brother Sammuel
    closes #37

---
+++ sql/updates/world/custom/custom_2020_02_20_03_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `gossip_menu_option_trainer` WHERE `MenuId`= 4663 AND `TrainerId`= 168;
+INSERT INTO `gossip_menu_option_trainer` (`MenuId`, `OptionIndex`, `TrainerId`) VALUES
+(4663, 0, 168);
