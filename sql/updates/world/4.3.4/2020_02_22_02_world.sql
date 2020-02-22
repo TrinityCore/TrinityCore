396cfcc71ea5591c594a9bbd1e8d3cf42a9a1f32
Ovahlord <dreadkiller@gmx.de>
2020-02-22 09:18:10 +0100

    DB: fixed a typo
    *big oof

--- sql/updates/world/4.3.4/2020_02_22_02_world.sql
+++ sql/updates/world/4.3.4/2020_02_22_02_world.sql
@@ -1,6 +1,3 @@
 -- Interrogator Vishas
-UPDATE `creature_template` SET `flags_extra`= `flags_extra` | 0x800;
+UPDATE `creature_template` SET `flags_extra`= `flags_extra` | 0x800 WHERE `entry`= 3983;
 UPDATE `creature_template_addon` SET `auras`= '8981' WHERE `entry`= 3983;
-
--- Bloodmage Thalnos
-UPDATE `creature_template` SET `flags_extra`= 0 WHERE `entry`= 4543;
