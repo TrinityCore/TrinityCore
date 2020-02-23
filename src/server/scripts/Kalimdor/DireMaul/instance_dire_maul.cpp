f3c1e71a53b2bad026c228cd66eb1a3157b2f908
jokerlfm <jokerlfm@hotmail.com>
2020-02-23 12:28:34 +0800

    dire maul scripts ms1

--- src/server/scripts/Kalimdor/DireMaul/instance_dire_maul.cpp
+++ src/server/scripts/Kalimdor/DireMaul/instance_dire_maul.cpp
@@ -15,41 +15,89 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
-This placeholder for the instance is needed for dungeon finding to be able
-to give credit after the boss defined in lastEncounterDungeon is killed.
-Without it, the party doing random dungeon won't get satchel of spoils and
-gets instead the deserter debuff.
-*/
+ /*
+ This placeholder for the instance is needed for dungeon finding to be able
+ to give credit after the boss defined in lastEncounterDungeon is killed.
+ Without it, the party doing random dungeon won't get satchel of spoils and
+ gets instead the deserter debuff.
+ */
 
 #include "ScriptMgr.h"
 #include "InstanceScript.h"
 #include "Map.h"
+#include "GameObject.h"
 
-// Bosses (East)
-// 0 - Pusillin
-// 1 - Lethtendris
-// 2 - Hydrospawn
-// 3 - Zevrim Thornhoof
-// 4 - Alzzin the Wildshaper
+ // Bosses (East)
+ // 0 - Pusillin
+ // 1 - Lethtendris
+ // 2 - Hydrospawn
+ // 3 - Zevrim Thornhoof
+ // 4 - Alzzin the Wildshaper
 
-// West
-// 5 - Tendris Warpwood
-// 6 - Magister Kalendris
-// 7 - Tsu'zee
-// 8 - Illyanna Ravenoak
-// 9 - Immol'thar
-// 10 - Prince Tortheldrin
+ // West
+ // 5 - Tendris Warpwood
+ // 6 - Magister Kalendris
+ // 7 - Tsu'zee
+ // 8 - Illyanna Ravenoak
+ // 9 - Immol'thar
+ // 10 - Prince Tortheldrin
 
-// North
-// 11 - Guard Mol'dar
-// 12 - Stomper Kreeg
-// 13 - Guard Fengus
-// 14 - Guard Slip'kik
-// 15 - Captain Kromcrush
-// 16 - King Gordok
+ // North
+ // 11 - Guard Mol'dar
+ // 12 - Stomper Kreeg
+ // 13 - Guard Fengus
+ // 14 - Guard Slip'kik
+ // 15 - Captain Kromcrush
+ // 16 - King Gordok
 
 uint8 const EncounterCount = 17;
+
+// EJ scripts
+enum DIRE_MAUL_NPC
+{
+    NPC_ZEVRIM_THORNHOOF = 11490,
+    NPC_OLD_IRONBARK = 11491,
+    NPC_IRONBARK_THE_REDEEMED = 14241,
+};
+
+enum DIRE_MAUL_GAMEOBJECT
+{
+    GAMEOBJECT_CONSERVATORY_DOOR = 176907,
+};
+
+enum DIRE_MAUL_BOSS
+{
+    BOSS_ZEVRIM_THORNHOOF = 0,
+};
+
+enum DIRE_MAUL_WAYPOINT
+{
+    WAYPOINT_IRONBARK_THE_REDEEMED = 142410,
+};
+
+enum DIRE_MAUL_LINE_OLD_IRONBARK
+{
+    LINE_OLD_IRONBARK_0 = 0,
+};
+
+enum DIRE_MAUL_LINE_IRONBARK_THE_REDEEMED
+{
+    LINE_IRONBARK_THE_REDEEMED_0 = 0,
+};
+
+enum DIRE_MAUL_DATA
+{
+    DATA_OLD_IRONBARK_REDEEM = 0,
+    DATA_CONSERVATORY_DOOR_SPAWN_ID = 1,
+};
+
+enum DIRE_MAUL_EVENT_IRONBARK_THE_REDEEMED
+{
+    EVENT_OPEN_DOOR_EMOTE = 0,
+    EVENT_DOOR_OPEN = 1,
+    EVENT_FAKE_DEATH = 2,
+    EVENT_DESPAWN = 3,
+};
 
 class instance_dire_maul : public InstanceMapScript
 {
@@ -61,6 +109,101 @@ public:
         instance_dire_maul_InstanceMapScript(Map* map) : InstanceScript(map)
         {
             SetBossNumber(EncounterCount);
+            SetBossState(DIRE_MAUL_BOSS::BOSS_ZEVRIM_THORNHOOF, EncounterState::NOT_STARTED);
+        }
+
+        ObjectGuid OGBossZevrimThornhoof;
+        ObjectGuid OGOldIronbark;
+        uint32 siConservatoryDoor;
+
+        void OnCreatureCreate(Creature* creature) override
+        {
+            switch (creature->GetEntry())
+            {
+            case DIRE_MAUL_NPC::NPC_OLD_IRONBARK:
+            {
+                OGOldIronbark = creature->GetGUID();
+                creature->RemoveFlag(EUnitFields::UNIT_NPC_FLAGS, NPCFlags::UNIT_NPC_FLAG_GOSSIP);
+                break;
+            }
+            case DIRE_MAUL_NPC::NPC_ZEVRIM_THORNHOOF:
+            {
+                OGBossZevrimThornhoof = creature->GetGUID();
+                break;
+            }
+            default:
+            {
+                break;
+            }
+            }
+        }
+
+        void OnGameObjectCreate(GameObject* go) override
+        {
+            switch (go->GetEntry())
+            {
+            case DIRE_MAUL_GAMEOBJECT::GAMEOBJECT_CONSERVATORY_DOOR:
+            {
+                siConservatoryDoor = go->GetSpawnId();
+                go->SetGoState(GOState::GO_STATE_READY);
+                break;
+            }
+            default:
+            {
+                break;
+            }
+            }
+        }
+
+        void SetData(uint32 type, uint32 data) override
+        {
+            if (type == DIRE_MAUL_DATA::DATA_OLD_IRONBARK_REDEEM)
+            {
+                if (Creature* checkC = instance->GetCreature(OGOldIronbark))
+                {
+                    checkC->AI()->Talk(DIRE_MAUL_LINE_OLD_IRONBARK::LINE_OLD_IRONBARK_0);
+                    checkC->UpdateEntry(DIRE_MAUL_NPC::NPC_IRONBARK_THE_REDEEMED);
+                    checkC->SetFlag(EUnitFields::UNIT_NPC_FLAGS, NPCFlags::UNIT_NPC_FLAG_GOSSIP);
+                }
+            }
+        }
+
+        uint32 GetData(uint32 type) const override
+        {
+            if (type == DIRE_MAUL_DATA::DATA_CONSERVATORY_DOOR_SPAWN_ID)
+            {
+                return siConservatoryDoor;
+            }
+            return 0;
+        }
+
+        virtual void Update(uint32 diff) override
+        {
+            if (GetBossState(DIRE_MAUL_BOSS::BOSS_ZEVRIM_THORNHOOF) == EncounterState::NOT_STARTED)
+            {
+                if (Creature* checkBoss = instance->GetCreature(OGBossZevrimThornhoof))
+                {
+                    if (checkBoss->IsInCombat())
+                    {
+                        SetBossState(DIRE_MAUL_BOSS::BOSS_ZEVRIM_THORNHOOF, EncounterState::IN_PROGRESS);
+                    }
+                }
+            }
+            else if (GetBossState(DIRE_MAUL_BOSS::BOSS_ZEVRIM_THORNHOOF) == EncounterState::IN_PROGRESS)
+            {
+                if (Creature* checkBoss = instance->GetCreature(OGBossZevrimThornhoof))
+                {
+                    if (!checkBoss->IsAlive())
+                    {
+                        SetData(DIRE_MAUL_DATA::DATA_OLD_IRONBARK_REDEEM, 0);
+                        SetBossState(DIRE_MAUL_BOSS::BOSS_ZEVRIM_THORNHOOF, EncounterState::DONE);
+                    }
+                    else if (!checkBoss->IsInCombat())
+                    {
+                        SetBossState(DIRE_MAUL_BOSS::BOSS_ZEVRIM_THORNHOOF, EncounterState::NOT_STARTED);
+                    }
+                }
+            }
         }
     };
 
@@ -68,6 +211,109 @@ public:
     {
         return new instance_dire_maul_InstanceMapScript(map);
     }
+};
+
+class npc_ironbark_the_redeemed : public CreatureScript
+{
+public:
+    npc_ironbark_the_redeemed() : CreatureScript("npc_ironbark_the_redeemed") { }
+
+    struct npc_ironbark_the_redeemedAI : public ScriptedAI
+    {
+        npc_ironbark_the_redeemedAI(Creature* creature) : ScriptedAI(creature)
+        {
+            Reset();
+        }
+
+        void Reset() override
+        {
+            events.Reset();
+        }
+
+        bool GossipSelect(Player* player, uint32 menuId, uint32 gossipListId) override
+        {
+            if (menuId == 0)
+            {
+                me->RemoveFlag(EUnitFields::UNIT_NPC_FLAGS, NPCFlags::UNIT_NPC_FLAG_GOSSIP);
+                me->Say(DIRE_MAUL_LINE_IRONBARK_THE_REDEEMED::LINE_IRONBARK_THE_REDEEMED_0);
+                me->StopMoving();
+                me->GetMotionMaster()->Clear();
+                me->GetMotionMaster()->MovePath(DIRE_MAUL_WAYPOINT::WAYPOINT_IRONBARK_THE_REDEEMED, false);
+                return true;
+            }
+            return false;
+        }
+
+        void MovementInform(uint32 type, uint32 id) override
+        {
+            if (type == WAYPOINT_MOTION_TYPE)
+            {
+                switch (id)
+                {
+                case 5:
+                {
+                    me->StopMoving();
+                    me->GetMotionMaster()->Clear();
+                    events.ScheduleEvent(DIRE_MAUL_EVENT_IRONBARK_THE_REDEEMED::EVENT_OPEN_DOOR_EMOTE, 2000);
+                    events.ScheduleEvent(DIRE_MAUL_EVENT_IRONBARK_THE_REDEEMED::EVENT_OPEN_DOOR_EMOTE, 6000);
+                    events.ScheduleEvent(DIRE_MAUL_EVENT_IRONBARK_THE_REDEEMED::EVENT_DOOR_OPEN, 9000);
+                    events.ScheduleEvent(DIRE_MAUL_EVENT_IRONBARK_THE_REDEEMED::EVENT_FAKE_DEATH, 14000);
+                    events.ScheduleEvent(DIRE_MAUL_EVENT_IRONBARK_THE_REDEEMED::EVENT_DESPAWN, 20000);
+                    break;
+                }
+                default:
+                {
+                    break;
+                }
+                }
+            }
+        }
+
+        void UpdateAI(uint32 diff) override
+        {
+            events.Update(diff);
+
+            switch (events.ExecuteEvent())
+            {
+            case DIRE_MAUL_EVENT_IRONBARK_THE_REDEEMED::EVENT_OPEN_DOOR_EMOTE:
+            {
+                me->HandleEmoteCommand(Emote::EMOTE_ONESHOT_ATTACK_UNARMED);
+                break;
+            }
+            case DIRE_MAUL_EVENT_IRONBARK_THE_REDEEMED::EVENT_DOOR_OPEN:
+            {
+                if (Map* myMap = me->GetMap())
+                {
+                    if (InstanceScript* is = me->GetInstanceScript())
+                    {
+                        if (GameObject* goDoor = myMap->GetGameObjectBySpawnId(is->GetData(DIRE_MAUL_DATA::DATA_CONSERVATORY_DOOR_SPAWN_ID)))
+                        {
+                            goDoor->SetGoState(GOState::GO_STATE_ACTIVE);
+                        }
+                    }
+                }
+                break;
+            }
+            case DIRE_MAUL_EVENT_IRONBARK_THE_REDEEMED::EVENT_FAKE_DEATH:
+            {
+                me->HandleEmoteCommand(Emote::EMOTE_STATE_DEAD);
+                break;
+            }
+            case DIRE_MAUL_EVENT_IRONBARK_THE_REDEEMED::EVENT_DESPAWN:
+            {
+                me->DespawnOrUnsummon(100, 24h * 7);
+                break;
+            }
+            default:
+            {
+                break;
+            }
+            }
+        }
+
+    private:
+        EventMap events;
+    };
 };
 
 void AddSC_instance_dire_maul()
