5996f1289597db7002639830af48c16c184c03e9
Rushor <PBienati@web.de>
2020-02-23 17:52:58 +0100

    Scripts/Gameobject: Move Orb of Command script to SAI.

--- sql/updates/world/3.3.5/2020_12_23_00_world.sql
+++ sql/updates/world/3.3.5/2020_12_23_00_world.sql
@@ -1,12 +1,14 @@
 -- Orb of Command (179879)
-DELETE FROM `conditions` WHERE SourceTypeOrReferenceId=15 AND SourceGroup=6001;
-INSERT INTO `conditions` VALUES(15, 6001, 0, 0, 0, 8, 0, 7761, 0, 0, 0, 0, 0, '', 'Gossip Requires Quest 7761 rewarded');
-DELETE FROM `gossip_menu` WHERE `MenuID` = 6001;
-INSERT INTO `gossip_menu` (`MenuID`, `TextID`, `VerifiedBuild`) VALUES (6001, 7155, 0);
-
-DELETE FROM `gossip_menu_option` WHERE `MenuID` = 6001;
-INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionText`, `OptionBroadcastTextID`, `OptionType`, `OptionNpcFlag`, `ActionMenuID`, `ActionPoiID`, `BoxCoded`, `BoxMoney`, `BoxText`, `BoxBroadcastTextID`, `VerifiedBuild`) VALUES 
-(6001, 0, 0, '<Place my hand on the orb.>', 9859, 1, 0, 0, 0, 0, 0, '', 0, 0);
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=15 AND `SourceGroup`=6001;
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=14 AND `SourceGroup`=6001;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
+(15,6001,0,0,0,8,0,7761,0,0,0,0,0,"","Show gossip option if quest '7761' is rewarded"),
+(14,6001,7155,0,0,8,0,7761,0,0,0,0,0,"","Show gossip text 7155 if quest '7761' rewarded");
+DELETE FROM `gossip_menu` WHERE `MenuID`=6001 AND `TextID`=7155;
+INSERT INTO `gossip_menu` (`MenuID`, `TextID`) VALUES (6001,7155);
+DELETE FROM `gossip_menu_option` WHERE `MenuID`=6001;
+INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionText`, `OptionBroadcastTextID`, `OptionType`, `OptionNpcFlag`, `ActionMenuID`, `ActionPoiID`, `BoxCoded`, `BoxMoney`, `BoxText`, `BoxBroadcastTextID`, `VerifiedBuild`) VALUES
+(6001,0,0,"<Place my hand on the orb.>" ,9859,1,1,0,0,0,0,"",0,0);
 
 -- Orb of Command SAI
 SET @ENTRY := 179879;
