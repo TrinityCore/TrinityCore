469e3aed7ef8db4da2d7db5c32fefb26f1f967cd
Keader <keader.android@gmail.com>
2020-03-14 18:10:49 -0300

    update sql

--- sql/updates/world/3.3.5/2020_03_14_04_world.sql
+++ sql/updates/world/3.3.5/2020_03_14_04_world.sql
@@ -1,4 +1,4 @@
 -- 
-DELETE FROM `spell_script_names` WHERE `spell_id`=55095 AND `ScriptName`='spell_dk_frost_fever';
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_dk_frost_fever';
 INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
 (55095,'spell_dk_frost_fever');
