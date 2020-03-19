96155a49147f6119e8bd9bc08936ab706173b67e
Shauren <shauren.trinity@gmail.com>
2020-03-19 20:42:44 +0100

    Core/PacketIO: Fixed SMSG_UPDATE_OBJECT packets going missing for new items
    Closes #16906

--- src/server/game/Entities/Item/Item.cpp
+++ src/server/game/Entities/Item/Item.cpp
@@ -1051,10 +1051,15 @@ void Item::BuildUpdate(UpdateDataMapType& data_map)
     ClearUpdateMask(false);
 }
 
-void Item::AddToObjectUpdate()
+bool Item::AddToObjectUpdate()
 {
     if (Player* owner = GetOwner())
+    {
         owner->GetMap()->AddUpdateObject(this);
+        return true;
+    }
+
+    return false;
 }
 
 void Item::RemoveFromObjectUpdate()
