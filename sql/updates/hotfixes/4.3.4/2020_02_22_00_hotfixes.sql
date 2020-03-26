e5d760435f61d9acad12504f284ff7c3a409b055
funjoker <funjoker109@gmail.com>
2020-03-26 05:05:46 +0100

    DB/Hotixes: Fix a typo

--- sql/updates/hotfixes/4.3.4/2020_02_22_00_hotfixes.sql
+++ sql/updates/hotfixes/4.3.4/2020_02_22_00_hotfixes.sql
@@ -3,4 +3,4 @@ ALTER TABLE `hotfix_data`
   DROP PRIMARY KEY,
   ADD PRIMARY KEY (`TableHash`, `RecordID`);
 
-ALTER TABLE `hotfixes`.`item` ADD COLUMN `DisplayInfoID` INT(10) UNSIGNED DEFAULT 0 NOT NULL AFTER `Material`;
+ALTER TABLE `item` ADD COLUMN `DisplayInfoID` INT(10) UNSIGNED DEFAULT 0 NOT NULL AFTER `Material`;
