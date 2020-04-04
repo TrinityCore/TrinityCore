9badac5fa0a3a126d8428c16095cc0ea31e59867
Eridium <6587064+Killyana@users.noreply.github.com>
2020-04-04 15:27:10 +0200

    Update 9999_99_99_99_world.sql
    SQL fix

--- sql/updates/world/3.3.5/9999_99_99_99_world.sql
+++ sql/updates/world/3.3.5/9999_99_99_99_world.sql
@@ -1,6 +1,7 @@
 -- Baby Blizzard Bear
 UPDATE `creature_template` SET `AIName`='SmartAI',`ScriptName`='' WHERE `entry`=32841;
-REPLACE INTO `creature_template_addon` (`entry`, `auras`) VALUES (32841,'61854');
+DELETE FROM `creature_template_addon` WHERE `entry` IN (32841);
+INSERT INTO `creature_template_addon` (`entry`, `auras`) VALUES (32841,'61854');
 DELETE FROM `smart_scripts` WHERE `entryorguid`=32841 AND `source_type`=0;
 DELETE FROM `smart_scripts` WHERE `entryorguid`=3284100 AND `source_type`=9;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
