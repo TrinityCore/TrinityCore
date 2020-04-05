2de72f78d81a1badb8046a34c1017af80346fe0c
jackpoz <giacomopoz@gmail.com>
2020-04-05 20:23:46 +0200

    Scripts/Guards: Fix spell cast spam after killing npc_air_force_bots guards

--- src/server/scripts/World/npcs_special.cpp
+++ src/server/scripts/World/npcs_special.cpp
@@ -133,6 +133,10 @@ public:
             if (!guard)
                 return;
 
+            // Keep the list of targets for later on when the guards will be alive
+            if (!guard->IsAlive())
+                return;
+
             for (ObjectGuid guid : _toAttack)
             {
                 Unit* target = ObjectAccessor::GetUnit(*me, guid);
