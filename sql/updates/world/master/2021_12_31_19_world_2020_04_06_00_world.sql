14ca12931d596c63833c3e1f2cac20797da40b09
Killyana <morphone1@gmail.com>
2020-04-06 18:38:06 +0200

    DB/Quest: Conversing With the Depths
    Fix the reset of the gob and allow the second action list to override the first one and fix the cosmetic npc trigger position
    
    (cherry picked from commit 46d38526a975b656b03f4068a82714fabcb883d4)

---
+++ sql/updates/world/master/2021_12_31_19_world_2020_04_06_00_world.sql
@@ -0,0 +1,4 @@
+-- 
+UPDATE `smart_scripts` SET `action_param1`=2 WHERE `entryorguid`=18842200 AND `source_type`=9 AND `id`=6;
+UPDATE `smart_scripts` SET `action_param3`=1 WHERE `entryorguid`=26648 AND `source_type`=0 AND `id`=9;
+UPDATE `creature` SET `position_x`=2452.6499, `position_y`=1721.3299, `position_z`=63.055, `Orientation`=3.285 WHERE `guid`=62851;
