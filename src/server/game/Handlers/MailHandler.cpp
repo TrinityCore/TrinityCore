d7524928e57d2555c5351a6e3d9c61e098b5c976
Shauren <shauren.trinity@gmail.com>
2020-04-17 17:36:24 +0200

    Core/Mail: Updated mailbox limit to 8.3 value

--- src/server/game/Handlers/MailHandler.cpp
+++ src/server/game/Handlers/MailHandler.cpp
@@ -581,8 +581,8 @@ void WorldSession::HandleGetMailList(WorldPackets::Mail::MailGetList& packet)
         if (m->state == MAIL_STATE_DELETED || curTime < m->deliver_time)
             continue;
 
-        // max. 50 mails can be sent
-        if (response.Mails.size() < 50)
+        // max. 100 mails can be sent
+        if (response.Mails.size() < 100)
             response.Mails.emplace_back(m, player);
 
         ++response.TotalNumRecords;
