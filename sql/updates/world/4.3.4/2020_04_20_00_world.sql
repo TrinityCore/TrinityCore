04b4a63313953709ae28be053295caffeaeff71c
Ovahlord <dreadkiller@gmx.de>
2020-04-20 15:09:33 +0200

    DB/Achievements: added reward titles to heroic Cataclysm raid boss achievements and the Avengers of Hyjal reputation achievement

---
+++ sql/updates/world/4.3.4/2020_04_20_00_world.sql
@@ -0,0 +1,9 @@
+DELETE FROM `achievement_reward` WHERE `ID` IN (5116, 5121, 5123, 5803, 6177, 6116, 5827);
+INSERT INTO `achievement_reward` (`ID`, `TitleA`, `TitleH`) VALUES
+(5116, 229, 229), -- Heroic: Nefarian
+(5121, 228, 228), -- Heroic: Sinestra
+(5123, 226, 226), -- Heroic: Al'Akir
+(5803, 278, 278), -- Heroic: Ragnaros
+(6177, 287, 287), -- Destroyer's End
+(6116, 285, 285), -- Heroic: Madness of Deathwing
+(5827, 267, 267); -- Avengers of Hyjal
