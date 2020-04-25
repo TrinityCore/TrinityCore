6c558bb37679a7a500fc0b17d0e1cd5072271d57
Ovahlord <dreadkiller@gmx.de>
2020-04-25 05:32:03 +0200

    Updated headers

--- src/server/scripts/Argus/AntorusTheBurningThrone/instance_antorus_the_burning_throne.cpp
+++ src/server/scripts/Argus/AntorusTheBurningThrone/instance_antorus_the_burning_throne.cpp
@@ -1,9 +1,9 @@
 /*
- * Copyright (C) 2008-2019 TrinityCore <https://www.trinitycore.org/>
+ * This file is part of the TrinityCore Project. See AUTHORS file for Copyright information
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 3 of the License, or (at your
+ * Free Software Foundation; either version 2 of the License, or (at your
  * option) any later version.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
