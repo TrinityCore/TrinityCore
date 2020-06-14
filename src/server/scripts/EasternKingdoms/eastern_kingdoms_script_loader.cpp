c088588701b5741b13118551da5eda20803e8710
offl <offl@users.noreply.github.com>
2020-06-14 17:52:12 +0200

    DB/Quest: Convert & update support for 'Escape from the Catacombs' to SAI
    Closes #24728
    
    (cherry picked from commit e47f302fc95d4e3f4d16b19950ce6172b549b166)

--- src/server/scripts/EasternKingdoms/eastern_kingdoms_script_loader.cpp
+++ src/server/scripts/EasternKingdoms/eastern_kingdoms_script_loader.cpp
@@ -190,7 +190,7 @@ void AddSC_dun_morogh();
 void AddSC_dun_morogh_area_coldridge_valley();
 void AddSC_duskwood();
 //void AddSC_eastern_plaguelands();
-void AddSC_ghostlands();
+//void AddSC_ghostlands();
 void AddSC_goldshire();
 void AddSC_hinterlands();
 void AddSC_isle_of_queldanas();
@@ -382,7 +382,7 @@ void AddEasternKingdomsScripts()
     AddSC_dun_morogh_area_coldridge_valley();
     AddSC_duskwood();
     //AddSC_eastern_plaguelands();
-    AddSC_ghostlands();
+    //AddSC_ghostlands();
     AddSC_goldshire();
     AddSC_hinterlands();
     AddSC_isle_of_queldanas();
