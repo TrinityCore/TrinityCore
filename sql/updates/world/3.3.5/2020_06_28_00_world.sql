3d3621d5294a96cd6065869eccadca38185e9a64
Keader <keader.android@gmail.com>
2020-06-28 13:08:22 -0300

    typo in last commit

--- sql/updates/world/3.3.5/2020_06_28_00_world.sql
+++ sql/updates/world/3.3.5/2020_06_28_00_world.sql
@@ -1,2 +1,2 @@
 -- 
-UPDATE `creature_template_movement` SET `Flight`=0, `Rooted`=1 WHERE `CreatureId`=37690;
+UPDATE `creature_template_movement` SET `Flight`=0, `Rooted`=0 WHERE `CreatureId`=37690;
