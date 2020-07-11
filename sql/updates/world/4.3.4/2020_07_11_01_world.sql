fa8a3e903ae266f4d3f9957914db68e984f53318
Ovahlord <dreadkiller@gmx.de>
2020-07-11 14:05:44 +0200

    DB/BoT: increased the melee damage for heroic bosses and increased Cho'Gall's overall melee damage for all difficulties

---
+++ sql/updates/world/4.3.4/2020_07_11_01_world.sql
@@ -0,0 +1,5 @@
+UPDATE `creature_template` SET `DamageModifier`= 120 WHERE `entry` IN (46210, 46211);
+UPDATE `creature_template` SET `DamageModifier`= 180 WHERE `entry` IN (49898, 49899, 49904, 49905);
+UPDATE `creature_template` SET `DamageModifier`= 180 WHERE `entry` IN (49607, 49608, 49610, 49611, 49613, 49614, 49616, 49617, 49620, 49621);
+UPDATE `creature_template` SET `DamageModifier`= 180 WHERE `entry` IN (43324, 50131, 43622, 50184);
+UPDATE `creature_template` SET `DamageModifier`= 240 WHERE `entry` IN (50132, 50133, 50185, 50186);
