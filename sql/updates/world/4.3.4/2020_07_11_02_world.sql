cc031b0316c03113e25a11cc01814cebd73b67e3
Ovahlord <dreadkiller@gmx.de>
2020-07-11 21:25:09 +0200

    DB/Creatures: updated damage values for all 4.0 raid creatures

---
+++ sql/updates/world/4.3.4/2020_07_11_02_world.sql
@@ -0,0 +1,121 @@
+/*Blackwing Descent*/
+-- Drakonid Chainwielder
+UPDATE `creature_template` SET `BaseVariance`=0.85, `DamageModifier`=160 WHERE `entry` IN (42649, 49490);
+-- Drakonid Drudge
+UPDATE `creature_template` SET `BaseVariance`=0.43, `DamageModifier`=140 WHERE `entry` IN (42362, 49489);
+-- Golem Sentry
+UPDATE `creature_template` SET `BaseVariance`=0.87, `DamageModifier`=200 WHERE `entry` IN (42800, 49501);
+-- Vile Swill
+UPDATE `creature_template` SET `BaseVariance`=0.75, `DamageModifier`=45 WHERE `entry` IN (49811, 49976, 49982, 49988);
+-- Aberration
+UPDATE `creature_template` SET `BaseVariance`=0.33, `DamageModifier`=25 WHERE `entry` IN (41440, 49971);
+UPDATE `creature_template` SET `BaseVariance`=0.33, `DamageModifier`=36 WHERE `entry` IN (49977, 49983);
+-- Prime Subject
+UPDATE `creature_template` SET `BaseVariance`=0.4, `DamageModifier`=75 WHERE `entry` IN (41841, 49975);
+UPDATE `creature_template` SET `BaseVariance`=0.4, `DamageModifier`=100 WHERE `entry` IN (49981, 49987);
+-- Drakeadon Mongel
+UPDATE `creature_template` SET `BaseVariance`=0.75, `DamageModifier`=120 WHERE `entry` IN (42803, 49503);
+-- Drakonid Slayer
+UPDATE `creature_template` SET `BaseVariance`=0.87, `DamageModifier`=200 WHERE `entry` IN (42802, 49502);
+-- Ivoroc
+UPDATE `creature_template` SET `BaseVariance`=0.85, `DamageModifier`=160 WHERE `entry` IN (42767, 49492);
+-- Maimgor
+UPDATE `creature_template` SET `BaseVariance`=0.85, `DamageModifier`=160 WHERE `entry` IN (42768, 49500);
+-- Pyreclaw
+UPDATE `creature_template` SET `BaseVariance`=0.85, `DamageModifier`=160 WHERE `entry` IN (42764, 49491);
+-- Spirits
+UPDATE `creature_template` SET `BaseVariance`=0.75, `DamageModifier`=130 WHERE `entry` IN (43119, 43122, 43125, 43126, 43127, 43128, 43129, 43130, 49504, 49505, 49507, 49508, 49509, 49510, 49511, 49512);
+-- Chimaeron
+UPDATE `creature_template` SET `BaseVariance`=1, `DamageModifier`=265 WHERE `entry` IN (43296, 47774);
+UPDATE `creature_template` SET `BaseVariance`=1, `DamageModifier`=350 WHERE `entry` IN (47775, 47776);
+-- Nefarian and Oyxia
+UPDATE `creature_template` SET `BaseVariance`=1, `DamageModifier`=90 WHERE `entry` IN (41376, 51104, 41270, 51116);
+UPDATE `creature_template` SET `BaseVariance`=1, `DamageModifier`=120 WHERE `entry` IN (51105, 51106, 51117, 51118);
+-- Magmaw
+UPDATE `creature_template` SET `BaseVariance`=1, `DamageModifier`=190 WHERE `entry` IN (41570, 51101);
+UPDATE `creature_template` SET `BaseVariance`=1, `DamageModifier`=250 WHERE `entry` IN (51102, 51103);
+-- Maloriak
+UPDATE `creature_template` SET `BaseVariance`=1, `DamageModifier`=90 WHERE `entry` IN (41378, 49974);
+UPDATE `creature_template` SET `BaseVariance`=1, `DamageModifier`=120 WHERE `entry` IN (49980, 49986);
+-- Atramedes
+UPDATE `creature_template` SET `BaseVariance`=1, `DamageModifier`=150 WHERE `entry` IN (41442, 49583);
+UPDATE `creature_template` SET `BaseVariance`=1, `DamageModifier`=200 WHERE `entry` IN (49584, 49585);
+-- Omnotron Defense System
+UPDATE `creature_template` SET `BaseVariance`=1, `DamageModifier`=150 WHERE `entry` IN (42166, 49056, 42178, 49053, 42179, 49047, 42180, 49050);
+UPDATE `creature_template` SET `BaseVariance`=1, `DamageModifier`=200 WHERE `entry` IN (49057, 49058, 49054, 49055, 49048, 49049, 49051, 49052);
+
+/*Bastion of Twilight*/
+-- Halfus Wyrmbreaker
+UPDATE `creature_template` SET `BaseVariance`=0.9, `DamageModifier`=80 WHERE `entry` IN (44600, 46209);
+UPDATE `creature_template` SET `BaseVariance`=0.9, `DamageModifier`=100 WHERE `entry` IN (46210, 46211);
+-- Dragons
+UPDATE `creature_template` SET `BaseVariance`=1, `DamageModifier`=90 WHERE `entry` IN (44826, 46218, 44828, 46212, 44829, 46215, 46221, 44653);
+UPDATE `creature_template` SET `BaseVariance`=1, `DamageModifier`=120 WHERE `entry` IN (46219, 46220, 46213, 46214, 46216, 46217, 46222, 46223);
+-- Orphaned Emerald Whelps
+UPDATE `creature_template` SET `BaseVariance`=0.33, `DamageModifier`=25 WHERE `entry` IN (44641, 46224);
+UPDATE `creature_template` SET `BaseVariance`=0.33, `DamageModifier`=36 WHERE `entry` IN (46225, 46226);
+-- Twilight Soul Blade
+UPDATE `creature_template` SET `BaseVariance`=0.75, `DamageModifier`=85 WHERE `entry` IN (45265, 48593);
+-- Twilight Shadow Knight
+UPDATE `creature_template` SET `BaseVariance`=0.75, `DamageModifier`=70 WHERE `entry` IN (45261, 48592);
+-- Twilight Dark Mender
+UPDATE `creature_template` SET `BaseVariance`=0.75, `DamageModifier`=70 WHERE `entry` IN (45266, 48589);
+-- Twilight Crossfire
+UPDATE `creature_template` SET `BaseVariance`=0.75, `DamageModifier`=70 WHERE `entry` IN (45264, 48588);
+-- Twilight Phase-Twister
+UPDATE `creature_template` SET `BaseVariance`=0.75, `DamageModifier`=30 WHERE `entry` IN (45267, 48590);
+-- Chosen Warrior
+UPDATE `creature_template` SET `BaseVariance`=0.75, `DamageModifier`=70 WHERE `entry` IN (46951, 50289);
+-- Chosen Seer
+UPDATE `creature_template` SET `BaseVariance`=0.9, `DamageModifier`=30 WHERE `entry` IN (46952, 50288);
+-- Azureborne Destroyer
+UPDATE `creature_template` SET `BaseVariance`=0.9, `DamageModifier`=35 WHERE `entry` IN (47087, 50287);
+-- Earth Ravager
+UPDATE `creature_template` SET `BaseVariance`=0.9, `DamageModifier`=60 WHERE `entry` IN (47150, 50292);
+-- Elemental Firelord
+UPDATE `creature_template` SET `BaseVariance`=0.95, `DamageModifier`=50 WHERE `entry` IN (47081, 50293);
+-- Twilight Elementalist
+UPDATE `creature_template` SET `BaseVariance`=1, `DamageModifier`=60 WHERE `entry` IN (47152, 50295);
+-- Wind Breaker
+UPDATE `creature_template` SET `BaseVariance`=1, `DamageModifier`=65 WHERE `entry` IN (47151, 50296);
+-- Theralion and Valiona
+UPDATE `creature_template` SET `BaseVariance`=1, `DamageModifier`=120 WHERE `entry` IN (45992, 49897, 45993, 49903);
+UPDATE `creature_template` SET `BaseVariance`=1, `DamageModifier`=160 WHERE `entry` IN (49898, 49899, 49904, 49905);
+-- Twilight Fiend
+UPDATE `creature_template` SET `BaseVariance`=1, `DamageModifier`=15 WHERE `entry` IN (49864, 51133);
+-- Evolved Drakonaar
+UPDATE `creature_template` SET `BaseVariance`=0.87, `DamageModifier`=85 WHERE `entry` IN (49813, 49814);
+-- Bound Inferno
+UPDATE `creature_template` SET `BaseVariance`=0.70, `DamageModifier`=75 WHERE `entry` IN (49817, 49827);
+-- Bound Zephyr
+UPDATE `creature_template` SET `BaseVariance`=0.60, `DamageModifier`=20 WHERE `entry` IN (49821, 49828);
+-- Bound Rumbler
+UPDATE `creature_template` SET `BaseVariance`=0.65, `DamageModifier`=150 WHERE `entry` IN (49826, 49830);
+-- Bound Deluge
+UPDATE `creature_template` SET `BaseVariance`=0.83, `DamageModifier`=100 WHERE `entry` IN (49825, 49829);
+-- Ascendant Council
+UPDATE `creature_template` SET `BaseVariance`=0.71, `DamageModifier`=45 WHERE `entry` IN (43687, 49612, 43689, 49609, 43688, 49606, 43686, 49615);
+UPDATE `creature_template` SET `BaseVariance`=0.71, `DamageModifier`=60 WHERE `entry` IN (49613, 49614, 49610, 49611, 49607, 49608, 49616, 49617);
+UPDATE `creature_template` SET `BaseVariance`=0.82, `DamageModifier`=70. WHERE `entry` IN (43735, 49619);
+UPDATE `creature_template` SET `BaseVariance`=0.82, `DamageModifier`=90. WHERE `entry` IN (49620, 49621);
+-- Twilight Shifter
+UPDATE `creature_template` SET `BaseVariance`=0.75, `DamageModifier`=110 WHERE `entry` IN (45687, 50200);
+-- Twilight Shadow Mender
+UPDATE `creature_template` SET `BaseVariance`=0.94, `DamageModifier`=160 WHERE `entry` IN (45699, 50202);
+-- Faceless Minion
+UPDATE `creature_template` SET `BaseVariance`=0.75, `DamageModifier`=15 WHERE `entry`= 45703;
+-- Twilight Portal Shaper
+UPDATE `creature_template` SET `BaseVariance`=0.9, `DamageModifier`=180.2247 WHERE `entry` IN (45700, 50201);
+-- Corrupting Adherent
+UPDATE `creature_template` SET `BaseVariance`=0.8, `DamageModifier`=135 WHERE `entry` IN (43622, 50184);
+UPDATE `creature_template` SET `BaseVariance`=0.8, `DamageModifier`=180 WHERE `entry` IN (50185, 50186);
+-- Cho'Gall
+UPDATE `creature_template` SET `BaseVariance`=0.8, `DamageModifier`=75 WHERE `entry` IN (43324, 50131);
+UPDATE `creature_template` SET `BaseVariance`=0.8, `DamageModifier`=100 WHERE `entry` IN (50132, 50133);
+-- Faceless Guardian
+UPDATE `creature_template` SET `BaseVariance`=0.8, `DamageModifier`=85 WHERE `entry` IN (45676, 50193);
+
+/*Throne of the Four Winds*/
+-- Conclave of Wind
+UPDATE `creature_template` SET `BaseVariance`=1, `DamageModifier`=50 WHERE `entry` IN (45870, 50103, 45871, 50098, 45872, 50095);
+UPDATE `creature_template` SET `BaseVariance`=1, `DamageModifier`=75 WHERE `entry` IN (50113, 50123, 50108, 50118, 50105, 50115);
