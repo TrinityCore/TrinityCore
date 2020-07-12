fae8bb6fbde169df3f9ed7b75fb3818f5ce41300
Carbenium <carbenium@outlook.com>
2020-07-12 13:54:19 +0200

    Scritps/Naxx: Fix a -Wimplicit-fallthrough warning reported by GCC

--- src/server/scripts/Northrend/Naxxramas/boss_thaddius.cpp
+++ src/server/scripts/Northrend/Naxxramas/boss_thaddius.cpp
@@ -402,6 +402,7 @@ struct boss_thaddius : public BossAI
                         break;
                     case EVENT_ENABLE_BALL_LIGHTNING:
                         ballLightningUnlocked = true;
+                        [[fallthrough]];
                     case EVENT_ENGAGE:
                         me->SetReactState(REACT_AGGRESSIVE);
                         break;
