56c3d0fa11992dc812591cdf3bdab82f5bca536a
Ovahlord <dreadkiller@gmx.de>
2020-07-13 14:29:37 +0200

    DB/DS: added missing movement template data for Deathwing's cosmetic arm passengers

---
+++ sql/updates/world/4.3.4/2020_07_13_01_world.sql
@@ -0,0 +1,6 @@
+DELETE FROM `creature_template_movement` WHERE `CreatureId` IN (57686, 57694, 57696, 57695);
+INSERT INTO `creature_template_movement` (`CreatureId`, `Flight`, `Rooted`) VALUES
+(57686, 1, 1),
+(57694, 1, 1),
+(57696, 1, 1),
+(57695, 1, 1);
