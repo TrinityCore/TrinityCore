07645562b1933a425f8cc10e446480c723a73c44
Carbenium <carbenium@outlook.com>
2020-07-13 21:46:35 +0200

    Scripts/Ulduar: Use std::chrono::duration overloads of EventMap
    Contributes to https://github.com/TrinityCore/TrinityCore/issues/25012

--- src/server/scripts/Northrend/Ulduar/HallsOfLightning/boss_loken.cpp
+++ src/server/scripts/Northrend/Ulduar/HallsOfLightning/boss_loken.cpp
@@ -106,7 +106,7 @@ public:
             BossAI::JustEngagedWith(who);
             Talk(SAY_AGGRO);
             events.SetPhase(PHASE_NORMAL);
-            events.ScheduleEvent(EVENT_ARC_LIGHTNING, 15000);
+            events.ScheduleEvent(EVENT_ARC_LIGHTNING, 15s);
             events.ScheduleEvent(EVENT_LIGHTNING_NOVA, 20s);
             events.ScheduleEvent(EVENT_RESUME_PULSING_SHOCKWAVE, 1s);
             instance->DoStartTimedAchievement(ACHIEVEMENT_TIMED_TYPE_EVENT, ACHIEV_TIMELY_DEATH_START_EVENT);
@@ -157,7 +157,7 @@ public:
                         Talk(EMOTE_NOVA);
                         DoCastAOE(SPELL_LIGHTNING_NOVA);
                         me->RemoveAurasDueToSpell(sSpellMgr->GetSpellIdForDifficulty(SPELL_PULSING_SHOCKWAVE, me));
-                        events.ScheduleEvent(EVENT_RESUME_PULSING_SHOCKWAVE, DUNGEON_MODE(5000, 4000)); // Pause Pulsing Shockwave aura
+                        events.ScheduleEvent(EVENT_RESUME_PULSING_SHOCKWAVE, DUNGEON_MODE(5s, 4s)); // Pause Pulsing Shockwave aura
                         events.ScheduleEvent(EVENT_LIGHTNING_NOVA, 20s, 21s);
                         break;
                     case EVENT_RESUME_PULSING_SHOCKWAVE:
