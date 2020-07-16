allankrdec/TrinityCore-10-2024

Core/Scripts: fix some /W4 warnings

src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/boss_twin_valkyr.cpp
@@ -793,17 +793,16 @@ class spell_valkyr_essences : public SpellScriptLoader
                             {
                                 if (dmgInfo.GetSpellInfo()->Id == darkVortex || dmgInfo.GetSpellInfo()->Id == lightVortex)
                                 {
-                                    Aura* pAura = owner->GetAura(poweringUp);
-                                    if (pAura)
+                                    if (Aura* aura = owner->GetAura(poweringUp))
                                     {
-                                        pAura->ModStackAmount(stacksCount);
+                                        aura->ModStackAmount(stacksCount);
                                         owner->CastSpell(owner, poweringUp, true);
                                     }
                                     else
                                     {
                                         owner->CastSpell(owner, poweringUp, true);
-                                        if ((pAura = owner->GetAura(poweringUp)))
-                                            pAura->ModStackAmount(stacksCount);
+                                        if (Aura* newAura = owner->GetAura(poweringUp))
+                                            newAura->ModStackAmount(stacksCount);
                                     }
                                 }
                             }
@@ -817,18 +816,17 @@ class spell_valkyr_essences : public SpellScriptLoader
                                 if (dmgInfo.GetSpellInfo()->Id == unleashedDark || dmgInfo.GetSpellInfo()->Id == unleashedLight)
                                 {
                                     // need to do the things in this order, else players might have 100 charges of Powering Up without anything happening
-                                    Aura* pAura = owner->GetAura(poweringUp);
-                                    if (pAura)
+                                    if (Aura* aura = owner->GetAura(poweringUp))
                                     {
                                         // 2 lines together add the correct amount of buff stacks
-                                        pAura->ModStackAmount(stacksCount);
+                                        aura->ModStackAmount(stacksCount);
                                         owner->CastSpell(owner, poweringUp, true);
                                     }
                                     else
                                     {
                                         owner->CastSpell(owner, poweringUp, true);
-                                        if ((pAura = owner->GetAura(poweringUp)))
-                                            pAura->ModStackAmount(stacksCount);
+                                        if (Aura* newAura = owner->GetAura(poweringUp))
+                                            newAura->ModStackAmount(stacksCount);
                                     }
                                 }
                             }

TrinityCore/TrinityCore

(cherry picked from commit d6cae113c64f1024b11493ee68e06d9b3052bfab)

src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/boss_twin_valkyr.cpp
@@ -759,17 +759,16 @@ class spell_valkyr_essences : public SpellScriptLoader
                         {
                             if (dmgInfo.GetSpellInfo()->Id == SPELL_DARK_VORTEX_DAMAGE || dmgInfo.GetSpellInfo()->Id == SPELL_LIGHT_VORTEX_DAMAGE)
                             {
-                                Aura* pAura = owner->GetAura(SPELL_POWERING_UP);
-                                if (pAura)
+                                if (Aura* aura = owner->GetAura(SPELL_POWERING_UP))
                                 {
-                                    pAura->ModStackAmount(stacksCount);
+                                    aura->ModStackAmount(stacksCount);
                                     owner->CastSpell(owner, SPELL_POWERING_UP, true);
                                 }
                                 else
                                 {
                                     owner->CastSpell(owner, SPELL_POWERING_UP, true);
-                                    if (Aura* pTemp = owner->GetAura(SPELL_POWERING_UP))
-                                        pTemp->ModStackAmount(stacksCount);
+                                    if (Aura* newAura = owner->GetAura(SPELL_POWERING_UP))
+                                        newAura->ModStackAmount(stacksCount);
                                 }
                             }
                         }
@@ -778,18 +777,17 @@ class spell_valkyr_essences : public SpellScriptLoader
                         if (dmgInfo.GetSpellInfo()->Id == SPELL_UNLEASHED_DARK || dmgInfo.GetSpellInfo()->Id == SPELL_UNLEASHED_LIGHT)
                         {
                             // need to do the things in this order, else players might have 100 charges of Powering Up without anything happening
-                            Aura* pAura = owner->GetAura(SPELL_POWERING_UP);
-                            if (pAura)
+                            if (Aura* aura = owner->GetAura(SPELL_POWERING_UP))
                             {
                                 // 2 lines together add the correct amount of buff stacks
-                                pAura->ModStackAmount(stacksCount);
+                                aura->ModStackAmount(stacksCount);
                                 owner->CastSpell(owner, SPELL_POWERING_UP, true);
                             }
                             else
                             {
                                 owner->CastSpell(owner, SPELL_POWERING_UP, true);
-                                if (Aura* pTemp = owner->GetAura(SPELL_POWERING_UP))
-                                    pTemp->ModStackAmount(stacksCount);
+                                if (Aura* newAura = owner->GetAura(SPELL_POWERING_UP))
+                                    newAura->ModStackAmount(stacksCount);
                             }
                         }
                     }