831c45b1da8ccf564e344b390c453b73e3c1f71d
Carbenium <carbenium@outlook.com>
2020-07-22 20:27:54 +0200

    dep: Add catch2 unit test framework and wire it up
    To enable the test suite, make sure to configure CMake with -DBUILD_TESTING=1 , since it is disabled by default. The catch2 dependency will be downloaded during configure time.
    
    Also add a new target "tests-common", which includes unit tests for the "common" project. To finally run the tests use the "test" target.
    
    CircleCI: Run unit tests
    (cherry picked from commit 6a28ee7b2a4269aa7e43265d1cd0067537e3e883)

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -50,4 +50,5 @@ option(WITH_COREDEBUG   "Include additional debug-code in core"                 
 set(WITH_SOURCE_TREE    "hierarchical" CACHE STRING "Build the source tree for IDE's.")
 set_property(CACHE WITH_SOURCE_TREE PROPERTY STRINGS no flat hierarchical hierarchical-folders)
 option(WITHOUT_GIT      "Disable the GIT testing routines"                            0)
+option(BUILD_TESTING    "Build test suite" 0)
 
