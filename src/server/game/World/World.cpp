c63fdd66b426cc878f1b93d2d3b99f519b55378b
Ovahlord <dreadkiller@gmx.de>
2020-07-23 15:08:36 +0200

    Core/World: fixed selecting default locales

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -511,7 +511,7 @@ void World::LoadConfigSettings(bool reload)
 
     m_defaultDbcLocale = LocaleConstant(sConfigMgr->GetIntDefault("DBC.Locale", 0));
 
-    if (m_defaultDbcLocale >= TOTAL_LOCALES || m_defaultDbcLocale < LOCALE_NONE)
+    if (m_defaultDbcLocale >= TOTAL_LOCALES || m_defaultDbcLocale < LOCALE_enUS || m_defaultDbcLocale == LOCALE_NONE)
     {
         TC_LOG_ERROR("server.loading", "Incorrect DBC.Locale! Must be >= 0 and < %d and not %d (set to 0)", TOTAL_LOCALES, LOCALE_NONE);
         m_defaultDbcLocale = LOCALE_enUS;
