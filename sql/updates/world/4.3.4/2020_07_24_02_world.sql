bbc09fd80b286b960392c04bff5659d9bbea4d98
Ovahlord <dreadkiller@gmx.de>
2020-07-24 13:14:17 +0200

    DB/ICC: removed some outdated linked respawns from Valithria Dreamwalker's encounter

---
+++ sql/updates/world/4.3.4/2020_07_24_02_world.sql
@@ -0,0 +1,5 @@
+DELETE FROM `linked_respawn` WHERE `linkedGuid`= 137752;
+INSERT INTO `linked_respawn` (`guid`, `linkedGuid`, `linkType`) VALUES
+(137746, 137752, 0),
+(137747, 137752, 0),
+(137789, 137752, 0);
