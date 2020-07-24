b336ed89961c25ff39317b1f57e8aa4a70a0994c
Carbenium <carbenium@outlook.com>
2020-07-24 19:00:44 +0200

    Build: Add WITH_LD_GOLD option to use gold linker on unix
    gold is an ELF-only linker which has a better performance
    than the default ld.
    
    (cherry picked from commit 796e2b32e03e7b1a56e44bd4b8988f9156bbb026)

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -55,3 +55,7 @@ set(WITH_SOURCE_TREE    "hierarchical" CACHE STRING "Build the source tree for I
 set_property(CACHE WITH_SOURCE_TREE PROPERTY STRINGS no flat hierarchical hierarchical-folders)
 option(WITHOUT_GIT      "Disable the GIT testing routines"                            0)
 option(BUILD_TESTING    "Build test suite" 0)
+
+if(UNIX)
+  option(USE_LD_GOLD    "Use GNU gold linker"                                        0)
+endif()
