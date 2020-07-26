ac37ca040be8f5beaad4e94a613534fa643c04c9
Treeston <treeston.mmoc@gmail.com>
2020-07-26 05:33:47 +0200

    how does VC++ never catch these? 5e36bf7 follow-up
    (No more witty one-liners. Let me go to bed please.)
    
    (cherry picked from commit 35411e5ed99584c7061a5feede8cb9b8d71c9b68)

--- src/common/Cryptography/BigNumber.h
+++ src/common/Cryptography/BigNumber.h
@@ -42,7 +42,7 @@ class TC_COMMON_API BigNumber
         void SetQword(uint64);
         void SetBinary(uint8 const* bytes, int32 len, bool littleEndian = true);
         template <typename Container>
-        auto SetBinary(Container const& c, bool littleEndian = true) -> std::enable_if_t<!std::is_pointer_v<Container>> { SetBinary(advstd::data(c), advstd::size(c), littleEndian); }
+        auto SetBinary(Container const& c, bool littleEndian = true) -> std::enable_if_t<!std::is_pointer_v<std::decay_t<Container>>> { SetBinary(advstd::data(c), advstd::size(c), littleEndian); }
         bool SetHexStr(char const* str);
         bool SetHexStr(std::string const& str) { return SetHexStr(str.c_str()); }
 
