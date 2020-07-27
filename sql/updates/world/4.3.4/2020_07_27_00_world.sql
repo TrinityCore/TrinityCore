ae5a3ca7e2e47e6c29221069c8bc8fed27209565
Ovahlord <dreadkiller@gmx.de>
2020-07-27 21:27:09 +0200

    DB/Spells: Glyph of Silencing Shot will now only trigger when hitting with Silencing Shot

---
+++ sql/updates/world/4.3.4/2020_07_27_00_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_proc` WHERE `SpellId`= 56836;
+INSERT INTO `spell_proc` (`SpellId`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellTypeMask`, `SpellPhaseMask`) VALUES
+(56836, 9, 0x40000, 4, 2);
