3378383aff74b7116d0bc98d1a0e57ee0ae79d46
Jinnaix <37972361+Jinnaix@users.noreply.github.com>
2020-08-03 11:58:16 +0200

    Blackwing Lair - Suppression Device

--- src/server/scripts/EasternKingdoms/BlackrockMountain/BlackwingLair/boss_broodlord_lashlayer.cpp
+++ src/server/scripts/EasternKingdoms/BlackrockMountain/BlackwingLair/boss_broodlord_lashlayer.cpp
@@ -17,6 +17,9 @@
 
 #include "ScriptMgr.h"
 #include "blackwing_lair.h"
+#include "InstanceScript.h"
+#include "GameObject.h"
+#include "GameObjectAI.h"
 #include "ScriptedCreature.h"
 
 enum Say
@@ -30,7 +33,9 @@ enum Spells
     SPELL_CLEAVE            = 26350,
     SPELL_BLASTWAVE         = 23331,
     SPELL_MORTALSTRIKE      = 24573,
-    SPELL_KNOCKBACK         = 25778
+    SPELL_KNOCKBACK         = 25778,
+    //Suppression Device Spell
+    SPELL_SUPPRESSION_AURA  = 22247
 };
 
 enum Events
@@ -39,7 +44,11 @@ enum Events
     EVENT_BLASTWAVE         = 2,
     EVENT_MORTALSTRIKE      = 3,
     EVENT_KNOCKBACK         = 4,
-    EVENT_CHECK             = 5
+    EVENT_CHECK             = 5,
+    //Suppression Device Events
+    EVENT_SUPPRESSION_AURA  = 6,
+    EVENT_SUPPRESSION_ANIM  = 7,
+    EVENT_SUPPRESSION_RESET = 8
 };
 
 class boss_broodlord : public CreatureScript
@@ -113,7 +122,86 @@ public:
     }
 };
 
+class go_suppression_device : public GameObjectScript
+{
+    public:
+        go_suppression_device() : GameObjectScript("go_suppression_device") { }
+
+        struct go_suppression_deviceAI : public GameObjectAI
+        {
+            go_suppression_deviceAI(GameObject* go) : GameObjectAI(go), _instance(go->GetInstanceScript()) 
+            {
+                _events.ScheduleEvent(EVENT_SUPPRESSION_AURA, Seconds(6));
+                _events.ScheduleEvent(EVENT_SUPPRESSION_ANIM, Seconds(2));
+            }
+
+            void UpdateAI(uint32 diff) override
+            {
+                if (_instance->GetBossState(DATA_VAELASTRAZ_THE_CORRUPT) != DONE)
+                    return;
+
+                if (_instance->GetBossState(DATA_VAELASTRAZ_THE_CORRUPT) == DONE)
+                    {
+                        if (me->GetGoState() == GO_STATE_DESTROYED)
+                        me->SetLootState(GO_READY); // NOT doing this will despawn the gameobject after using Disarm Trap
+                    }
+
+                if (_instance->GetBossState(DATA_BROODLORD_LASHLAYER) == DONE)
+                    {
+                        me->SetGoState(GO_STATE_DESTROYED);
+                        me->SetFlag(GAMEOBJECT_FLAGS, GO_FLAG_NOT_SELECTABLE);
+                        return;
+                    };
+
+                _events.Update(diff);
+
+                while (uint32 eventId = _events.ExecuteEvent())
+                {
+                    switch (eventId)
+                    {
+                        case EVENT_SUPPRESSION_AURA:
+                            if (me->GetGoState() == GO_READY)
+                            me->CastSpell(nullptr,SPELL_SUPPRESSION_AURA,true);
+                            _events.ScheduleEvent(EVENT_SUPPRESSION_AURA, Seconds(6)); //I couldnt find out if there is a time between the aura effect and the recast - this results in constantly being under effect of the aura
+                            break;
+                        case EVENT_SUPPRESSION_ANIM:
+                            if (me->GetGoState() == GO_READY)
+                            me->SendCustomAnim(0);
+                            _events.ScheduleEvent(EVENT_SUPPRESSION_ANIM, Seconds(1), Seconds(2)); //in current retail Videos,there is a ~7 Seconds timer for the animation loop but they arent fully synched.
+                            break;
+                        case EVENT_SUPPRESSION_RESET:
+                            if (me->GetGoState() == GO_STATE_DESTROYED)
+                            me->SetGoState(GO_STATE_READY);
+                            me->SetLootState(GO_READY);
+                            me->RemoveFlag(GAMEOBJECT_FLAGS, GO_FLAG_NOT_SELECTABLE);
+                            break;
+                    }
+                }
+            }
+
+            void OnLootStateChanged(uint32 state, Unit* who) override
+            {
+                if (state == GO_ACTIVATED)
+                {
+                    me->SetGoState(GO_STATE_DESTROYED);
+                    me->SetFlag(GAMEOBJECT_FLAGS, GO_FLAG_NOT_SELECTABLE);
+                    _events.ScheduleEvent(EVENT_SUPPRESSION_RESET, Seconds(30), Seconds(120));
+                }
+            }
+
+        private:
+            InstanceScript* _instance;
+            EventMap _events;
+        };
+
+        GameObjectAI* GetAI(GameObject* go) const override
+        {
+            return GetBlackwingLairAI<go_suppression_deviceAI>(go);
+        }
+};
+
 void AddSC_boss_broodlord()
 {
     new boss_broodlord();
+    new go_suppression_device();
 }
