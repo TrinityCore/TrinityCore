32fd740ad37d8bb56d126598c3089337420cea60
Varjgard <61460712+Varjgard@users.noreply.github.com>
2020-08-06 13:06:08 +0300

    Core/CharacterHandler: Allow alliedraces.
    Allow developers to create alliedrace characters if "achievementId" is set to 0 in "world.race_unlock_requirement" table.

--- src/server/game/Handlers/CharacterHandler.cpp
+++ src/server/game/Handlers/CharacterHandler.cpp
@@ -381,6 +381,7 @@ void WorldSession::HandleCharEnum(PreparedQueryResult result)
         WorldPackets::Character::EnumCharactersResult::RaceUnlock raceUnlock;
         raceUnlock.RaceID = requirement.first;
         raceUnlock.HasExpansion = GetAccountExpansion() >= requirement.second.Expansion;
+        raceUnlock.HasAchievement = requirement.second.AchievementId == 0;
         charEnum.RaceUnlockData.push_back(raceUnlock);
     }
 
