52fa88536d6b8e20456a713097549f04a2360252
Gildor <gildor55@gmail.com>
2020-08-06 18:07:07 +0200

    applying Ovahlord's suggestions :)

--- sql/updates/world/3.3.5/9999_99_99_99_world.sql
+++ sql/updates/world/3.3.5/9999_99_99_99_world.sql
@@ -1,2 +1,6 @@
--- 
-UPDATE `creature_summon_groups` SET `summonTime`=19000 WHERE `summonerId`=16082 AND `summonerType`=0;
+-- Remove speed hack from Living Poison and disable movement flag updates for it
+UPDATE `creature_template` SET `flags_extra`=`flags_extra`|512,`speed_run`= 1.42857 WHERE `entry`= 16027;
+-- Delete unneeded spawn groups
+DELETE FROM `creature_summon_groups` WHERE `summonerId`=16082 AND `summonerType`=0;
+-- Delete unneeded waypoints
+DELETE FROM `waypoint_data` WHERE `id` IN (160270,160271,160272);
