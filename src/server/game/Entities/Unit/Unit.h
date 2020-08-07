2819951dce545438724719836e3a13d3039b8690
Ovah <dreadkiller@gmx.de>
2020-08-07 14:51:40 +0200

    Core/Units: add new functionality for units to block or enable combat interactions (#25168)
    * Core/Units: add new functionality for units to block or enable combat entirely via helper to reflect what CREATURE_DIFFICULTYFLAGS_IGNORE_COMBAT and client AI functions imply
    
    * yeah...
    
    * Nuke Creature::IsCombatDisallowed helper as its unused by now
    
    * no combat extra flag may now be changed on transforms if the transformed entry does not have the flag
    
    (cherry picked from commit 764f22fc9157ad184baa38995ee4e2d821299935)

--- src/server/game/Entities/Unit/Unit.h
+++ src/server/game/Entities/Unit/Unit.h
@@ -1849,6 +1849,11 @@ class TC_GAME_API Unit : public WorldObject
         uint16 GetVirtualItemAppearanceMod(uint32 slot) const;
         void SetVirtualItem(uint32 slot, uint32 itemId, uint16 appearanceModId = 0, uint16 itemVisual = 0);
 
+        // returns if the unit is ignoring any combat interaction
+        bool IsIgnoringCombat() const { return _isIgnoringCombat; }
+        // enables/disables combat interaction of this unit.
+        void SetIgnoringCombat(bool apply) { _isIgnoringCombat = apply; }
+
         std::string GetDebugInfo() const override;
 
         UF::UpdateField<UF::UnitData, 0, TYPEID_UNIT> m_unitData;
@@ -2017,6 +2022,8 @@ class TC_GAME_API Unit : public WorldObject
 
         std::unique_ptr<MovementForces> _movementForces;
         PositionUpdateInfo _positionUpdateInfo;
+
+        bool _isIgnoringCombat;
 };
 
 namespace Trinity
