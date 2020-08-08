c04f53b886ec27dd8cf100524fe93a71dc4e1aa1
jackpoz <giacomopoz@gmail.com>
2020-08-08 17:24:18 +0200

    Core/Warden: Fix Warden reporting false positive checks
    Issue introduced in 210176fd915cf4ba16f428d3c1a249a71f4aa7a7

--- src/server/game/Warden/WardenWin.cpp
+++ src/server/game/Warden/WardenWin.cpp
@@ -391,7 +391,7 @@ void WardenWin::HandleData(ByteBuffer &buff)
                     continue;
                 }
 
-                std::vector<uint8> result = rs->Result.ToByteVector();
+                std::vector<uint8> result = rs->Result.ToByteVector(0, false);
                 if (memcmp(buff.contents() + buff.rpos(), result.data(), rd->Length) != 0)
                 {
                     TC_LOG_DEBUG("warden", "RESULT MEM_CHECK fail CheckId %u account Id %u", *itr, _session->GetAccountId());
