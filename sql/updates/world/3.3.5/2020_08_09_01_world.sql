39f2d2d1cf4d79ec9f34f92ac999332d10b04e90
offl <offl@users.noreply.github.com>
2020-08-09 03:19:11 +0300

    DB/Texts: Add correct BroadcastTextId and comments to some creature texts

---
+++ sql/updates/world/3.3.5/2020_08_09_01_world.sql
@@ -0,0 +1,8 @@
+--
+UPDATE `creature_text` SET `BroadcastTextId` = 34299 WHERE `CreatureID` = 33350 AND `GroupID` = 3 AND `ID` = 0;
+UPDATE `creature_text` SET `Text` = "There is no escape.", `BroadcastTextId` = 34355 WHERE `CreatureID` = 33515 AND `GroupID` = 1 AND `ID` = 2;
+UPDATE `creature_text` SET `comment` = "Squire Cavin" WHERE `CreatureID` = 33522;
+UPDATE `creature_text` SET `comment` = "Wolvar Orphan" WHERE `CreatureID` = 33532;
+UPDATE `creature_text` SET `comment` = "Oracle Orphan" WHERE `CreatureID` = 33533;
+UPDATE `creature_text` SET `comment` = "The Black Knight" WHERE `CreatureID` = 33785;
+UPDATE `creature_text` SET `comment` = "Broxel Goldgrasp" WHERE `CreatureID` = 33853;
