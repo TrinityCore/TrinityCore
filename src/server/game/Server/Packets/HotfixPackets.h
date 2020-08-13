2c99678118798279372f17d4bb5f5a88ac95c413
Shauren <shauren.trinity@gmail.com>
2020-08-13 14:15:26 +0200

    Core/PacketIO: Send the expected value in available hotfixes packet - realm address, not arbitrary number (it no longer has any effect for clearing client cache)

--- src/server/game/Server/Packets/HotfixPackets.h
+++ src/server/game/Server/Packets/HotfixPackets.h
@@ -61,12 +61,12 @@ namespace WorldPackets
         class AvailableHotfixes final : public ServerPacket
         {
         public:
-            AvailableHotfixes(int32 hotfixCacheVersion, uint32 hotfixCount, DB2Manager::HotfixContainer const& hotfixes)
-                : ServerPacket(SMSG_AVAILABLE_HOTFIXES), HotfixCacheVersion(hotfixCacheVersion), HotfixCount(hotfixCount), Hotfixes(hotfixes) { }
+            AvailableHotfixes(int32 virtualRealmAddress, uint32 hotfixCount, DB2Manager::HotfixContainer const& hotfixes)
+                : ServerPacket(SMSG_AVAILABLE_HOTFIXES), VirtualRealmAddress(virtualRealmAddress), HotfixCount(hotfixCount), Hotfixes(hotfixes) { }
 
             WorldPacket const* Write() override;
 
-            int32 HotfixCacheVersion;
+            int32 VirtualRealmAddress;
             uint32 HotfixCount;
             DB2Manager::HotfixContainer const& Hotfixes;
         };
