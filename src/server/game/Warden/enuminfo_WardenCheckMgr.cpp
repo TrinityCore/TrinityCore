0531463a3c02d98ed77e9c7689ad7237ecaddc09
Treeston <treeston.mmoc@gmail.com>
2020-08-21 01:19:28 +0200

    Core/Warden: Add Lua checks to Warden (PR #25286)

--- src/server/game/Warden/enuminfo_WardenCheckMgr.cpp
+++ src/server/game/Warden/enuminfo_WardenCheckMgr.cpp
@@ -115,10 +115,10 @@ TC_API_EXPORT EnumText EnumUtils<WardenCheckType>::ToString(WardenCheckType valu
 {
     switch (value)
     {
-        case LUA_STR_CHECK: return { "LUA_STR_CHECK", "LUA_STR_CHECK", "139: byte luaNameIndex (check to ensure LUA string isn't used)" };
         case TIMING_CHECK: return { "TIMING_CHECK", "TIMING_CHECK", "nyi" };
         case DRIVER_CHECK: return { "DRIVER_CHECK", "DRIVER_CHECK", "uint Seed + byte[20] SHA1 + byte driverNameIndex (check to ensure driver isn't loaded)" };
         case PROC_CHECK: return { "PROC_CHECK", "PROC_CHECK", "nyi" };
+        case LUA_EVAL_CHECK: return { "LUA_EVAL_CHECK", "LUA_EVAL_CHECK", "evaluate arbitrary Lua check" };
         case MPQ_CHECK: return { "MPQ_CHECK", "MPQ_CHECK", "get hash of MPQ file (to check it is not modified)" };
         case PAGE_CHECK_A: return { "PAGE_CHECK_A", "PAGE_CHECK_A", "scans all pages for specified SHA1 hash" };
         case PAGE_CHECK_B: return { "PAGE_CHECK_B", "PAGE_CHECK_B", "scans only pages starts with MZ+PE headers for specified hash" };
@@ -136,10 +136,10 @@ TC_API_EXPORT WardenCheckType EnumUtils<WardenCheckType>::FromIndex(size_t index
 {
     switch (index)
     {
-        case 0: return LUA_STR_CHECK;
-        case 1: return TIMING_CHECK;
-        case 2: return DRIVER_CHECK;
-        case 3: return PROC_CHECK;
+        case 0: return TIMING_CHECK;
+        case 1: return DRIVER_CHECK;
+        case 2: return PROC_CHECK;
+        case 3: return LUA_EVAL_CHECK;
         case 4: return MPQ_CHECK;
         case 5: return PAGE_CHECK_A;
         case 6: return PAGE_CHECK_B;
@@ -154,10 +154,10 @@ TC_API_EXPORT size_t EnumUtils<WardenCheckType>::ToIndex(WardenCheckType value)
 {
     switch (value)
     {
-        case LUA_STR_CHECK: return 0;
-        case TIMING_CHECK: return 1;
-        case DRIVER_CHECK: return 2;
-        case PROC_CHECK: return 3;
+        case TIMING_CHECK: return 0;
+        case DRIVER_CHECK: return 1;
+        case PROC_CHECK: return 2;
+        case LUA_EVAL_CHECK: return 3;
         case MPQ_CHECK: return 4;
         case PAGE_CHECK_A: return 5;
         case PAGE_CHECK_B: return 6;
