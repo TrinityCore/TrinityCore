1e27afe3156a1ed8ac7b922e2e2f7fbc04edf848
funjoker <funjoker109@gmail.com>
2020-08-24 21:32:57 +0200

    DB: Update some gossip_menu

---
+++ sql/updates/world/4.3.4/2020_08_24_08_world.sql
@@ -0,0 +1,111 @@
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 13412) AND (`TextID` = 18851);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 13210) AND (`TextID` = 18618);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 13168) AND (`TextID` = 18562);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 13142) AND (`TextID` = 18508);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 13085) AND (`TextID` = 18376);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 12990) AND (`TextID` = 18266);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 12989) AND (`TextID` = 18265);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 12986) AND (`TextID` = 18262);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 12829) AND (`TextID` = 18032);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 12779) AND (`TextID` = 17968);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 12746) AND (`TextID` = 17891);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 12678) AND (`TextID` = 17821);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 12482) AND (`TextID` = 17559);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 12450) AND (`TextID` = 17512);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 12446) AND (`TextID` = 17503);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 12441) AND (`TextID` = 17496);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 12335) AND (`TextID` = 17332);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 12326) AND (`TextID` = 17321);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 12157) AND (`TextID` = 17096);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 12134) AND (`TextID` = 17045);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 12110) AND (`TextID` = 17005);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 12087) AND (`TextID` = 16967);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 12073) AND (`TextID` = 17007);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 12073) AND (`TextID` = 16942);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 12072) AND (`TextID` = 16941);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 12020) AND (`TextID` = 16846);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 11921) AND (`TextID` = 16737);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 11914) AND (`TextID` = 16728);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 11872) AND (`TextID` = 16635);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 11729) AND (`TextID` = 16422);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 11672) AND (`TextID` = 16330);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 11597) AND (`TextID` = 16192);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 11471) AND (`TextID` = 16001);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 11430) AND (`TextID` = 15920);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 11430) AND (`TextID` = 15919);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 11427) AND (`TextID` = 15914);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 11419) AND (`TextID` = 15899);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 11407) AND (`TextID` = 15892);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 11406) AND (`TextID` = 15890);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 11395) AND (`TextID` = 15874);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 11154) AND (`TextID` = 15461);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 10940) AND (`TextID` = 15295);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 10735) AND (`TextID` = 14897);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 10715) AND (`TextID` = 14873);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 10674) AND (`TextID` = 14795);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 10648) AND (`TextID` = 14751);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 10528) AND (`TextID` = 14565);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 10519) AND (`TextID` = 14553);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 10514) AND (`TextID` = 14546);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 10217) AND (`TextID` = 14204);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 10200) AND (`TextID` = 14160);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 10035) AND (`TextID` = 13919);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 9863) AND (`TextID` = 13680);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 9774) AND (`TextID` = 13451);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 9729) AND (`TextID` = 13321);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 9160) AND (`TextID` = 12403);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 9156) AND (`TextID` = 12389);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 9155) AND (`TextID` = 12388);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 9127) AND (`TextID` = 12340);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 8950) AND (`TextID` = 12052);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 8737) AND (`TextID` = 11059);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 8719) AND (`TextID` = 10980);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 8710) AND (`TextID` = 10957);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 8680) AND (`TextID` = 11020);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 8679) AND (`TextID` = 10919);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 8666) AND (`TextID` = 10901);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 8652) AND (`TextID` = 10863);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 8652) AND (`TextID` = 10862);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 8649) AND (`TextID` = 10849);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 8560) AND (`TextID` = 7778);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 8545) AND (`TextID` = 10691);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 8493) AND (`TextID` = 10854);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 8492) AND (`TextID` = 10605);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 8460) AND (`TextID` = 10568);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 8104) AND (`TextID` = 10024);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 8103) AND (`TextID` = 10022);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 8082) AND (`TextID` = 9989);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 7992) AND (`TextID` = 9844);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 7933) AND (`TextID` = 9718);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 7531) AND (`TextID` = 9134);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 6934) AND (`TextID` = 8223);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 6932) AND (`TextID` = 8221);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 6232) AND (`TextID` = 7406);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 6231) AND (`TextID` = 7390);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 5709) AND (`TextID` = 6877);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 5542) AND (`TextID` = 6595);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 5181) AND (`TextID` = 6194);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 4608) AND (`TextID` = 5720);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 4516) AND (`TextID` = 5005);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 4356) AND (`TextID` = 5575);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 4267) AND (`TextID` = 5431);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 4261) AND (`TextID` = 5413);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 4166) AND (`TextID` = 5228);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 4001) AND (`TextID` = 5856);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 3647) AND (`TextID` = 4445);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 3501) AND (`TextID` = 4253);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 2883) AND (`TextID` = 3559);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 2861) AND (`TextID` = 3554);
+UPDATE `gossip_menu` SET `VerifiedBuild` = 15050 WHERE (`MenuID` = 2741) AND (`TextID` = 3395);
+
+DELETE FROM `gossip_menu` WHERE (`MenuId`=13167 AND `TextId`=18561) OR (`MenuId`=11363 AND `TextId`=15836) OR (`MenuId`=10131 AND `TextId`=14066) OR (`MenuId`=10200 AND `TextId`=14159) OR (`MenuId`=9248 AND `TextId`=12558) OR (`MenuId`=10674 AND `TextId`=14968) OR (`MenuId`=10685 AND `TextId`=14602) OR (`MenuId`=11878 AND `TextId`=5005) OR (`MenuId`=10680 AND `TextId`=14834);
+INSERT INTO `gossip_menu` (`MenuId`, `TextId`, `VerifiedBuild`) VALUES
+(13167, 18561, 15050), -- 55125 (Elder Daftusk)
+(11363, 15836, 15050), -- 40131 (Sensiria)
+(10131, 14066, 15050), -- 31848 (Zidormi)
+(10200, 14159, 15050), -- 32239 (Highlord Tirion Fordring)
+(9248, 12558, 15050), -- 26158 (Mother Tauranook)
+(10674, 14968, 15050), -- 35757 (Bizby)
+(10685, 14602, 15050), -- 34692 (Sister Goldskimmer)
+(11878, 5005, 15050), -- 44740 (Sahi Cloudsinger)
+(10680, 14834, 15050); -- 35773 (Cenarion Researcher Korrah)
