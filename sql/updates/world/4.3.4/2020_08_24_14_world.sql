25e7a52e99670711ebe6b02fb0ae97d29abf1d36
funjoker <funjoker109@gmail.com>
2020-08-24 22:02:54 +0200

    DB: Verify some gossip_menu_options (7)

---
+++ sql/updates/world/4.3.4/2020_08_24_14_world.sql
@@ -0,0 +1,3 @@
+UPDATE `gossip_menu_option` SET `VerifiedBuild`=14333 WHERE (`MenuId`=646 AND `OptionIndex`=0);
+UPDATE `gossip_menu_option` SET `VerifiedBuild`=14333 WHERE (`MenuId`=4009 AND `OptionIndex`=1);
+UPDATE `gossip_menu_option` SET `VerifiedBuild`=14333 WHERE (`MenuId`=1482 AND `OptionIndex`=0);
