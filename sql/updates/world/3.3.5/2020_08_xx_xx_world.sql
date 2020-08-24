6f16514e310d049c17635fa55e3c113dd029ca9c
FAQ <ainarsh@gmail.com>
2020-08-24 23:06:44 +0300

    DB/Creature: Correcting movement flags for Blood-Queen Lana'thel
    from 7c5c4bd9fa867d9e2fdf50712f596222e7666ca9

--- sql/updates/world/3.3.5/2020_08_xx_xx_world.sql
+++ sql/updates/world/3.3.5/2020_08_xx_xx_world.sql
@@ -1,2 +1,5 @@
 -- Disable movement flag updates for Grauf and let script handle them
 UPDATE `creature_template` SET `flags_extra` = `flags_extra`|512 WHERE `entry`=26893;
+-- Disable movement flag updates for Blood-Queen Lana'thel
+UPDATE `creature_template_movement` SET `Ground`= 1, `Flight`= 0 WHERE `CreatureId` IN (37955, 38434, 38435, 38436);
+UPDATE `creature_template` SET `flags_extra`= `flags_extra` | 513 WHERE `entry` IN (37955, 38434, 38435, 38436);
