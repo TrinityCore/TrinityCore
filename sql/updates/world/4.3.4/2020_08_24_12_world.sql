061756d2a001dfd74d52f4e7f636f0d22c4ddee5
funjoker <funjoker109@gmail.com>
2020-08-24 21:47:58 +0200

    DB/Trainer: Add missing spell

---
+++ sql/updates/world/4.3.4/2020_08_24_12_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `trainer_spell` WHERE (`TrainerId`=407 AND `SpellId`=3926);
+INSERT INTO `trainer_spell` (`TrainerId`, `SpellId`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqAbility1`, `ReqAbility2`, `ReqAbility3`, `ReqLevel`, `VerifiedBuild`) VALUES
+(407, 3926, 225, 202, 65, 0, 0, 0, 0, 14545);
