ac234a30a42fe4dd8ab6a604ba3467301b13ab0e
FAQ <ainarsh@gmail.com>
2020-08-25 15:01:11 +0300

    DB/Creature: Correcting movement flags for Malygos

--- sql/updates/world/3.3.5/2020_08_xx_xx_world.sql
+++ sql/updates/world/3.3.5/2020_08_xx_xx_world.sql
@@ -3,3 +3,5 @@ UPDATE `creature_template` SET `flags_extra` = `flags_extra`|512 WHERE `entry`=2
 -- Disable movement flag updates for Blood-Queen Lana'thel
 UPDATE `creature_template_movement` SET `Ground`= 1, `Flight`= 0 WHERE `CreatureId` IN (37955, 38434, 38435, 38436);
 UPDATE `creature_template` SET `flags_extra`= `flags_extra` | 513 WHERE `entry` IN (37955, 38434, 38435, 38436);
+-- Disable movement flag updates for Malygos
+UPDATE `creature_template` SET `flags_extra`= `flags_extra` | 513 WHERE `entry` IN (28859, 31734);
