d99c4520c15bf814f1d4d4d2b9625907841b1478
funjoker <funjoker109@gmail.com>
2020-08-25 13:14:19 +0200

    DB/Gossip: Add and update some gossip_menu_options (6)

---
+++ sql/updates/world/4.3.4/2020_08_25_05_world.sql
@@ -0,0 +1,128 @@
+DELETE FROM `gossip_menu_option` WHERE (`MenuId`=11473 AND `OptionIndex`=1) OR (`MenuId`=8304 AND `OptionIndex`=5) OR (`MenuId`=8455 AND `OptionIndex`=0) OR (`MenuId`=10466 AND `OptionIndex`=1) OR (`MenuId`=10467 AND `OptionIndex`=1) OR (`MenuId`=10471 AND `OptionIndex`=1) OR (`MenuId`=7440 AND `OptionIndex`=1) OR (`MenuId`=7440 AND `OptionIndex`=0) OR (`MenuId`=12197 AND `OptionIndex`=8) OR (`MenuId`=12189 AND `OptionIndex`=8) OR (`MenuId`=12196 AND `OptionIndex`=8) OR (`MenuId`=10490 AND `OptionIndex`=0) OR (`MenuId`=12722 AND `OptionIndex`=1) OR (`MenuId`=11942 AND `OptionIndex`=0) OR (`MenuId`=12722 AND `OptionIndex`=0) OR (`MenuId`=11785 AND `OptionIndex`=1) OR (`MenuId`=11785 AND `OptionIndex`=0) OR (`MenuId`=9838 AND `OptionIndex`=1) OR (`MenuId`=9838 AND `OptionIndex`=0) OR (`MenuId`=4326 AND `OptionIndex`=1) OR (`MenuId`=11744 AND `OptionIndex`=0) OR (`MenuId`=12189 AND `OptionIndex`=9) OR (`MenuId`=12189 AND `OptionIndex`=7) OR (`MenuId`=12185 AND `OptionIndex`=9) OR (`MenuId`=12185 AND `OptionIndex`=7) OR (`MenuId`=12196 AND `OptionIndex`=9) OR (`MenuId`=12196 AND `OptionIndex`=7) OR (`MenuId`=12188 AND `OptionIndex`=9) OR (`MenuId`=12188 AND `OptionIndex`=7) OR (`MenuId`=12198 AND `OptionIndex`=7) OR (`MenuId`=12190 AND `OptionIndex`=7) OR (`MenuId`=12186 AND `OptionIndex`=7) OR (`MenuId`=12191 AND `OptionIndex`=9) OR (`MenuId`=12191 AND `OptionIndex`=7) OR (`MenuId`=12192 AND `OptionIndex`=9) OR (`MenuId`=12192 AND `OptionIndex`=7) OR (`MenuId`=12195 AND `OptionIndex`=9) OR (`MenuId`=12195 AND `OptionIndex`=7) OR (`MenuId`=12199 AND `OptionIndex`=9) OR (`MenuId`=12199 AND `OptionIndex`=7) OR (`MenuId`=12197 AND `OptionIndex`=9) OR (`MenuId`=12197 AND `OptionIndex`=7) OR (`MenuId`=12035 AND `OptionIndex`=1) OR (`MenuId`=3864 AND `OptionIndex`=1) OR (`MenuId`=12131 AND `OptionIndex`=0) OR (`MenuId`=10431 AND `OptionIndex`=0) OR (`MenuId`=12103 AND `OptionIndex`=1) OR (`MenuId`=12103 AND `OptionIndex`=0) OR (`MenuId`=10024 AND `OptionIndex`=1) OR (`MenuId`=11613 AND `OptionIndex`=1);
+INSERT INTO `gossip_menu_option` (`MenuId`, `OptionIndex`, `OptionIcon`, `OptionText`, `OptionBroadcastTextId`, `VerifiedBuild`) VALUES
+(11473, 1, 0, 'I would like to start the Amulet Ritual, Lynnore.', 41390, 14545),
+(8304, 5, 0, 'I want to fly to an old location!', 19735, 14545),
+(8455, 0, 0, 'Send me back to the Jagged Ridge.', 19729, 14545),
+(10466, 1, 0, 'I am ready to fight!', 33430, 14545),
+(10467, 1, 0, 'I am ready to fight!', 33430, 14545),
+(10471, 1, 0, 'I am ready to fight!', 33430, 14545),
+(7440, 1, 8, 'I want to create a guild crest.', 3415, 14545),
+(7440, 0, 7, 'How do I form a guild?', 3413, 14545),
+(12197, 8, 3, 'Train me in Mining.', 47116, 14545),
+(12189, 8, 3, 'Train me in Mining.', 47116, 14545),
+(12196, 8, 3, 'Train me in Mining.', 47116, 14545),
+(10490, 0, 0, 'Ancient Onu, where may I find a Seed of the Earth?', 34486, 14545),
+(12722, 1, 0, 'Is Zen\'Kiki available for some more combat practice?', 50822, 14545),
+(11942, 0, 0, 'I was just wondering what you knew about the gnolls to the south.  Someone has been giving them supplies to practice necromancy.', 45300, 14545),
+(12722, 0, 0, 'Is Zen\'Kiki available for some more combat practice?', 50822, 14545),
+(11785, 1, 0, 'I am ready for the Mind Control ritual, Thaalia.', 50739, 14545),
+(11785, 0, 0, 'I am ready for the Mind Vision ritual, Thaalia.', 50738, 14545),
+(9838, 1, 7, 'How do I form a guild?', 3413, 14545),
+(9838, 0, 8, 'I want to create a guild crest.', 3415, 14545),
+(4326, 1, 0, 'Send me to Orgrimmar.', 42892, 14545),
+(11744, 0, 0, 'Here\'s the head and the hat, Fleet Master.', 43589, 14545),
+(12189, 9, 3, 'Train me in Skinning.', 47117, 14545),
+(12189, 7, 3, 'Train me in Leatherworking.', 47115, 14545),
+(12185, 9, 3, 'Train me in Skinning.', 47117, 14545),
+(12185, 7, 3, 'Train me in Leatherworking.', 47115, 14545),
+(12196, 9, 3, 'Train me in Skinning.', 47117, 14545),
+(12196, 7, 3, 'Train me in Leatherworking.', 47115, 14545),
+(12188, 9, 3, 'Train me in Skinning.', 47117, 14545),
+(12188, 7, 3, 'Train me in Leatherworking.', 47115, 14545),
+(12198, 7, 3, 'Train me in Leatherworking.', 47115, 14545),
+(12190, 7, 3, 'Train me in Leatherworking.', 47115, 14545),
+(12186, 7, 3, 'Train me in Leatherworking.', 47115, 14545),
+(12191, 9, 3, 'Train me in Skinning.', 47117, 14545),
+(12191, 7, 3, 'Train me in Leatherworking.', 47115, 14545),
+(12192, 9, 3, 'Train me in Skinning.', 47117, 14545),
+(12192, 7, 3, 'Train me in Leatherworking.', 47115, 14545),
+(12195, 9, 3, 'Train me in Skinning.', 47117, 14545),
+(12195, 7, 3, 'Train me in Leatherworking.', 47115, 14545),
+(12199, 9, 3, 'Train me in Skinning.', 47117, 14545),
+(12199, 7, 3, 'Train me in Leatherworking.', 47115, 14545),
+(12197, 9, 3, 'Train me in Skinning.', 47117, 14545),
+(12197, 7, 3, 'Train me in Leatherworking.', 47115, 14545),
+(12035, 1, 0, 'Ride Fiona\'s Caravan to its next destination.', 45675, 14545),
+(3864, 1, 0, 'Carlin, do you have the extended Annals of Darrowshire?', 45603, 14545),
+(12131, 0, 0, 'Tell me that story again, Theldurin.', 46593, 14545),
+(10431, 0, 0, 'I am ready. Begin the attack.', 34222, 14545),
+(12103, 1, 0, 'Gimme some more of that swim tonic!', 47576, 14545),
+(12103, 0, 0, 'Gimme some more of that swim tonic!', 47576, 14545),
+(10024, 1, 0, 'I\'m ready to fly to Sholazar Basin.', 31594, 14545),
+(11613, 1, 0, 'I would like to start the Amulet Ritual, Kasim.', 42216, 14545);
+
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 52824, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12952) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 52636, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12941) AND  (`OptionIndex` = 1);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 44663, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12865) AND  (`OptionIndex` = 13);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 44649, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12865) AND  (`OptionIndex` = 1);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 25084, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12785) AND  (`OptionIndex` = 3);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 51426, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12785) AND  (`OptionIndex` = 2);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 51421, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12785) AND  (`OptionIndex` = 1);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 51424, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12785) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 25084, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12784) AND  (`OptionIndex` = 3);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 51423, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12784) AND  (`OptionIndex` = 2);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 51425, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12784) AND  (`OptionIndex` = 1);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 51420, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12784) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 50828, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12723) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 50794, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12717) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 50712, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12677) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 48372, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12397) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 48299, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12385) AND  (`OptionIndex` = 1);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 48298, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12385) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 48300, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12384) AND  (`OptionIndex` = 1);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 48297, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12384) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 48299, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12383) AND  (`OptionIndex` = 1);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 48297, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12383) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 48298, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12382) AND  (`OptionIndex` = 1);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 48297, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12382) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 48291, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12381) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 48243, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12376) AND  (`OptionIndex` = 2);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 48243, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12376) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 48213, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12374) AND  (`OptionIndex` = 3);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 48211, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12374) AND  (`OptionIndex` = 1);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 48209, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12373) AND  (`OptionIndex` = 3);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 48207, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12373) AND  (`OptionIndex` = 1);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 48206, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12373) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 48205, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12372) AND  (`OptionIndex` = 3);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 48203, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12372) AND  (`OptionIndex` = 1);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 48214, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12371) AND  (`OptionIndex` = 2);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 48214, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12371) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 47776, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12292) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 47188, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12205) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 30181, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12106) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 46336, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12099) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 46265, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12094) AND  (`OptionIndex` = 1);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 46264, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12094) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 45662, `VerifiedBuild` = 14545 WHERE (`MenuId` = 12034) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 45306, `VerifiedBuild` = 14545 WHERE (`MenuId` = 11943) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 45297, `VerifiedBuild` = 14545 WHERE (`MenuId` = 11940) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 45294, `VerifiedBuild` = 14545 WHERE (`MenuId` = 11938) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 45291, `VerifiedBuild` = 14545 WHERE (`MenuId` = 11936) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 45288, `VerifiedBuild` = 14545 WHERE (`MenuId` = 11934) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 44384, `VerifiedBuild` = 14545 WHERE (`MenuId` = 11791) AND  (`OptionIndex` = 1);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 43573, `VerifiedBuild` = 14545 WHERE (`MenuId` = 11742) AND  (`OptionIndex` = 1);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 43572, `VerifiedBuild` = 14545 WHERE (`MenuId` = 11742) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 43571, `VerifiedBuild` = 14545 WHERE (`MenuId` = 11741) AND  (`OptionIndex` = 1);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 43278, `VerifiedBuild` = 14545 WHERE (`MenuId` = 11711) AND  (`OptionIndex` = 1);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 43246, `VerifiedBuild` = 14545 WHERE (`MenuId` = 11706) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 41200, `VerifiedBuild` = 14545 WHERE (`MenuId` = 11479) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 38946, `VerifiedBuild` = 14545 WHERE (`MenuId` = 11127) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 35835, `VerifiedBuild` = 14545 WHERE (`MenuId` = 10649) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 35108, `VerifiedBuild` = 14545 WHERE (`MenuId` = 10576) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 34585, `VerifiedBuild` = 14545 WHERE (`MenuId` = 10509) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 34514, `VerifiedBuild` = 14545 WHERE (`MenuId` = 10486) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 34110, `VerifiedBuild` = 14545 WHERE (`MenuId` = 10429) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 34044, `VerifiedBuild` = 14545 WHERE (`MenuId` = 10410) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 2822, `VerifiedBuild` = 14545 WHERE (`MenuId` = 10201) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 25923, `VerifiedBuild` = 14545 WHERE (`MenuId` = 9429) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 51429, `VerifiedBuild` = 14545 WHERE (`MenuId` = 9197) AND  (`OptionIndex` = 14);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 51428, `VerifiedBuild` = 14545 WHERE (`MenuId` = 9197) AND  (`OptionIndex` = 13);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 50664, `VerifiedBuild` = 14545 WHERE (`MenuId` = 9197) AND  (`OptionIndex` = 12);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 25112, `VerifiedBuild` = 14545 WHERE (`MenuId` = 9190) AND  (`OptionIndex` = 1);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 22620, `VerifiedBuild` = 14545 WHERE (`MenuId` = 8763) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 20764, `VerifiedBuild` = 14545 WHERE (`MenuId` = 8629) AND  (`OptionIndex` = 0);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 14741, `VerifiedBuild` = 14545 WHERE (`MenuId` = 7560) AND  (`OptionIndex` = 2);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 14739, `VerifiedBuild` = 14545 WHERE (`MenuId` = 7559) AND  (`OptionIndex` = 2);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 42071, `VerifiedBuild` = 14545 WHERE (`MenuId` = 4360) AND  (`OptionIndex` = 1);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 45662, `VerifiedBuild` = 14545 WHERE (`MenuId` = 3864) AND  (`OptionIndex` = 2);
+UPDATE `gossip_menu_option` SET `OptionBroadcastTextId` = 11869, `VerifiedBuild` = 14545 WHERE (`MenuId` = 1541) AND  (`OptionIndex` = 1);
