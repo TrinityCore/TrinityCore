2118381ef594e009f0b679e937efbc0b5635e529
Treeston <treeston.mmoc@gmail.com>
2020-08-30 00:28:49 +0200

    oops

--- src/server/game/Chat/ChatCommands/ChatCommand.h
+++ src/server/game/Chat/ChatCommands/ChatCommand.h
@@ -128,7 +128,7 @@ namespace Trinity::Impl::ChatCommands
             return args;
     }
 
-    template <typename T> struct HandlerToTuple { static_assert(Trinity::dependant_false<T>, "Invalid command handler signature"); };
+    template <typename T> struct HandlerToTuple { static_assert(Trinity::dependant_false_v<T>, "Invalid command handler signature"); };
     template <typename... Ts> struct HandlerToTuple<bool(*)(ChatHandler*, Ts...)> { using type = std::tuple<ChatHandler*, advstd::remove_cvref_t<Ts>...>; };
     template <typename T> using TupleType = typename HandlerToTuple<T>::type;
 }
