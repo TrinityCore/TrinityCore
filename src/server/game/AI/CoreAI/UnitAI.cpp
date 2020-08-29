c08180d9bc7cb203746650a9b2fc775efbeb2540
Ovahlord <dreadkiller@gmx.de>
2020-08-29 22:48:22 +0200

    Core/AI: SortByDistance will now sort the referenced list instead of a copy of it. This fixes MinDistance and MaxDistance target selection

--- src/server/game/AI/CoreAI/UnitAI.cpp
+++ src/server/game/AI/CoreAI/UnitAI.cpp
@@ -238,7 +238,7 @@ ThreatManager& UnitAI::GetThreatManager()
     return me->GetThreatManager();
 }
 
-void UnitAI::SortByDistance(std::list<Unit*> list, bool ascending)
+void UnitAI::SortByDistance(std::list<Unit*>& list, bool ascending)
 {
     list.sort(Trinity::ObjectDistanceOrderPred(me, ascending));
 }
