c02a9ab923c08ab2b377f5919873010b632f5233
Ovahlord <dreadkiller@gmx.de>
2020-08-30 15:23:01 +0200

    DB/Spells: fixed glyph of Berserker Rage

---
+++ sql/updates/world/4.3.4/2020_08_30_09_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_proc` WHERE `SpellId`= 58096;
+INSERT INTO `spell_proc` (`SpellId`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellTypeMask`, `SpellPhaseMask`) VALUES
+(58096, 4, 0x10000000, 4, 2);
