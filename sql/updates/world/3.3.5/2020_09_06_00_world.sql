3b1e911da3551cf9c69798bbb8b5f52d34be8522
Treeston <treeston.mmoc@gmail.com>
2020-09-06 01:40:00 +0200

    Scripts/Commands: trinity_string updates

---
+++ sql/updates/world/3.3.5/2020_09_06_00_world.sql
@@ -0,0 +1,2 @@
+UPDATE `trinity_string` SET `content_default`='You can only summon a player to your instance if that player, or their group leader, is in your instance.' WHERE `entry`=103;
+UPDATE `trinity_string` SET `content_default`='You can only teleport to a player\'s instance if you are in their group, or you have GM mode enabled.' WHERE `entry`=105;
