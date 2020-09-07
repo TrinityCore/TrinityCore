de4bcc886059acdd1a4d00677bf8e3cf5857cbcb
Keader <keader.android@gmail.com>
2020-09-07 18:32:20 -0300

    Fixed issues

--- sql/updates/world/3.3.5/2020_xx_xx_xx_world.sql
+++
@@ -1,5 +0,0 @@
--- Nerub'ar broodkeep start event
-DELETE FROM `areatrigger_scripts` WHERE `entry` IN (5611,5612);
-INSERT INTO `areatrigger_scripts` (`entry`, `ScriptName`) VALUES
-(5611, 'at_icc_nerubar_broodkeeper'),
-(5612, 'at_icc_nerubar_broodkeeper');
--- src/server/scripts/Northrend/IcecrownCitadel/icecrown_citadel.h
+++ src/server/scripts/Northrend/IcecrownCitadel/icecrown_citadel.h
@@ -118,7 +118,8 @@ enum ICDataTypes
     DATA_BLOOD_QUEEN_LANA_THEL_COUNCIL = 42,
     DATA_BLOOD_PRINCE_COUNCIL_INTRO    = 43,
     DATA_SINDRAGOSA_INTRO              = 44,
-    DATA_FACTION_BUFF                  = 45 // used by conditions
+    DATA_FACTION_BUFF                  = 45, // used by conditions
+    DATA_NERUBAR_BROODKEEPER_EVENT     = 46
 };
 
 enum ICCreaturesIds
@@ -478,6 +479,9 @@ enum ICAchievementCriteriaIds
 
 enum ICSharedActions
 {
+    // Nerub'ar Broodkeeper event
+    ACTION_NERUBAR_FALL          = 1,
+
     // Icecrown Gunship Battle
     ACTION_ENEMY_GUNSHIP_TALK   = -369390,
     ACTION_EXIT_SHIP            = -369391,
@@ -531,9 +535,10 @@ enum ICWorldStatesICC
     WORLDSTATE_ATTEMPTS_MAX         = 4942
 };
 
-enum ICAreaIds
+enum ICMisc
 {
-    AREA_ICECROWN_CITADEL   = 4812
+    AREA_ICECROWN_CITADEL   = 4812,
+    AT_NERUBAR_BROODKEEPER  = 5611
 };
 
 struct Position;
