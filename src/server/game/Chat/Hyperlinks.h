5189f134cae4ee9bcb9e04d5c87550b130f36474
Treeston <treeston.mmoc@gmail.com>
2020-09-10 14:26:50 +0200

    Core/Chat: Permissible quest level for hyperlinks, [-1, 127] -> [-1, 32767]. 08064f0 follow-up.

--- src/server/game/Chat/Hyperlinks.h
+++ src/server/game/Chat/Hyperlinks.h
@@ -70,7 +70,7 @@ namespace Trinity::Hyperlinks
     struct QuestLinkData
     {
         ::Quest const* Quest;
-        int8 QuestLevel;
+        int16 QuestLevel;
     };
 
     struct TalentLinkData
