3ce1a5b3ac84fd039052dd2352e8ac001af2c1a7
offl <offl@users.noreply.github.com>
2020-09-18 18:44:08 +0300

    DB/Gameobject: Add correct door to Sethekk Halls

---
+++ sql/updates/world/3.3.5/2020_09_18_07_world.sql
@@ -0,0 +1,2 @@
+--
+UPDATE `gameobject` SET `id` = 183398, `position_x` = 44.7715, `position_y` = 150.721, `position_z` = -0.0341, `orientation` = 1.57079, `rotation2` = 0.707106, `rotation3` = 0.707106, `VerifiedBuild` = 14545 WHERE `guid` = 28507 AND `id` = 177203;
