2e6d05434c8030d916bc1e1bb404dd36a12ea058
funjoker <funjoker109@gmail.com>
2020-09-22 01:26:57 +0200

    DB/Misc: Set default charset for auth and characters to utf8mb4 in create_mysql.sql

--- sql/create/create_mysql.sql
+++ sql/create/create_mysql.sql
@@ -4,9 +4,9 @@ GRANT USAGE ON * . * TO 'trinity'@'localhost';
 
 CREATE DATABASE `world` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-CREATE DATABASE `characters` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE `characters` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-CREATE DATABASE `auth` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE `auth` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE DATABASE `hotfixes` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
