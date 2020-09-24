ba78de9354ed3e15a271ce69288f338eaa90fb68
Ovahlord <dreadkiller@gmx.de>
2020-09-25 00:10:53 +0200

    DB/Creatures: added velocity values for the cyclic splines of Ban'thalos, Xariona, Tank and Blazewing

---
+++ sql/updates/world/4.3.4/2020_09_25_00_world.sql
@@ -0,0 +1,8 @@
+-- Ban'thalos
+UPDATE `waypoint_data` SET `velocity`= 4.6 WHERE `id`= 3878490;
+-- Blazewing
+UPDATE `waypoint_data` SET `velocity`= 16 WHERE `id`= 3860060;
+-- Tank
+UPDATE `waypoint_data` SET `velocity`= 9 WHERE `id`= 2916760;
+-- Xariona
+UPDATE `waypoint_data` SET `velocity`= 14.6 WHERE `id`= 3964070;
