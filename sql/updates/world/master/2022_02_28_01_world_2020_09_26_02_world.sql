4deaec0b4ff854bc5c90c81c1a858ed7485b03b0
dr-j <dr-j@users.noreply.github.com>
2020-09-26 03:58:21 +0300

    DB/Creature: Make Guard Slip'kik's gossip option available
    Closes #25496
    
    (cherry picked from commit 86d2544d11b4c4ccb648e20c2f20e1916d15609e)

---
+++ sql/updates/world/master/2022_02_28_01_world_2020_09_26_02_world.sql
@@ -0,0 +1,2 @@
+--
+UPDATE `smart_scripts` SET `event_phase_mask` = 0, `action_type` = 11, `action_param2` = 0, `comment` = "Guard Slip'kik - On Gossip Option 0 Selected - Cast 'Slip'kik's Savvy'" WHERE  `entryorguid` = 14323 AND `source_type` = 0 AND `id` = 13;
