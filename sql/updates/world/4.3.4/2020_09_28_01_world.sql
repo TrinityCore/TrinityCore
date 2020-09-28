f3696e6cff9d6036d493174bd21ee1d5f628a9da
Ovahlord <dreadkiller@gmx.de>
2020-09-28 20:38:17 +0200

    DB/Loot: reverted Magmaw's token drop removal

--- sql/updates/world/4.3.4/2020_09_28_01_world.sql
+++ sql/updates/world/4.3.4/2020_09_28_01_world.sql
@@ -1,3 +1,1 @@
 UPDATE `creature_loot_template` SET `MinCount`= 2, `MaxCount`= 2 WHERE `Entry`= 47775 AND `Reference`= 477750;
-DELETE FROM `reference_loot_template` WHERE `Entry`= 511021;
-DELETE FROM `creature_loot_template` WHERE `Reference`= 511021;
