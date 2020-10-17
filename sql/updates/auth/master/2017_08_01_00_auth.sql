568a9d1e445d5ee37f72f7f1147af92ca062f9c2
Aadhyaan <72241281+Aadhyaan@users.noreply.github.com>
2020-10-17 10:41:39 +0530

    Update 2017_08_01_00_auth.sql

--- sql/updates/auth/master/2017_08_01_00_auth.sql
+++ sql/updates/auth/master/2017_08_01_00_auth.sql
@@ -1,8 +1,8 @@
 -- 
-DELETE FROM `rbac_permissions` WHERE `id`=866;
-INSERT INTO `rbac_permissions` (`id`,`name`) VALUES
-(866, 'Command: list spawnpoints');
+DELETE FROM `rbac_permissions_into` WHERE `id`=864;
+INSERT INTO `rbac_permissions_into` (`id`,`name`) VALUES
+(864, 'Command: list spawnpoints null');
 
-DELETE FROM `rbac_linked_permissions` WHERE `linkedId`=866;
-INSERT INTO `rbac_linked_permissions` (`id`,`linkedId`) VALUES
-(196,866);
+DELETE FROM `rbac_linked_permissions_into` WHERE `linkedId`=864;
+INSERT INTO `rbac_linked_permissions_into` (`id`,`linkedId`) VALUES
+(196,864);
