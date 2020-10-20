44b24fa12203b4ddbe34e40a5e6a7b925358781a
funjoker <funjoker109@gmail.com>
2020-10-20 22:48:52 +0200

    DB/Hotfixes: Update zhTW hotfixes to final 35662

---
+++ sql/updates/hotfixes/master/2020_10_20_10_hotfixes.sql
@@ -0,0 +1,121 @@
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 3776013982 AND `RecordId` = 298096 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28721 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28720 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28719 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28718 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28717 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28716 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28715 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28714 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28713 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28712 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28711 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28710 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28709 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28708 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28707 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28706 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28705 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28704 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28703 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28702 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28701 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28700 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28699 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28698 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28697 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28696 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28695 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28694 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28693 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28692 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28691 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28690 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28689 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28688 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28687 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28686 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28685 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28684 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28683 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28682 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28681 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28680 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28679 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28678 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28677 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28676 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28675 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28674 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28673 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28672 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28671 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28670 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28669 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28668 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28667 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28666 AND `locale` = 'zhTW';
+DELETE FROM `hotfix_blob` WHERE `VerifiedBuild`>0 AND `TableHash` = 603412935 AND `RecordId` = 28665 AND `locale` = 'zhTW';
+INSERT INTO `hotfix_blob` (`TableHash`, `RecordId`, `locale`, `Blob`, `VerifiedBuild`) VALUES 
+(3776013982, 298096, 'zhTW', 0x00E59CA8E4BDA0E79BA4E5AD90E7A9BAE4BA86E79A84E69982E58099E68FAEE6898BE696B0E8A681E4B880E79BA4EFBC810000, 35662),
+(603412935, 28721, 'zhTW', 0xE7BF92E7ABB9E5B8AD0004, 35662),
+(603412935, 28720, 'zhTW', 0xE7BF92E5A4AAE5AD900004, 35662),
+(603412935, 28719, 'zhTW', 0xE7BF92E69292E5B9A30004, 35662),
+(603412935, 28718, 'zhTW', 0xE7BF92E685B6E8B1900004, 35662),
+(603412935, 28717, 'zhTW', 0xE7BF92E5BCB7E6AC8A0004, 35662),
+(603412935, 28716, 'zhTW', 0xE7BF92E6BAA5E584800004, 35662),
+(603412935, 28715, 'zhTW', 0xE7BF92E69292420004, 35662),
+(603412935, 28714, 'zhTW', 0xE7BF92E5B18FE7A6810004, 35662),
+(603412935, 28713, 'zhTW', 0xE7BF92E8B3A3E59C8B0004, 35662),
+(603412935, 28712, 'zhTW', 0xE7BF92E58D96E59BBD0004, 35662),
+(603412935, 28711, 'zhTW', 0xE7BF92E6B091E4B8BB0004, 35662),
+(603412935, 28710, 'zhTW', 0xE7BF92E6AF9BE980890004, 35662),
+(603412935, 28709, 'zhTW', 0xE7BF92E9A286E5AFBC0004, 35662),
+(603412935, 28708, 'zhTW', 0xE78EB0E4BBA3E99A8BE78280E5B89D0004, 35662),
+(603412935, 28707, 'zhTW', 0xE78EB0E4BBBBE79A87E5B89D0004, 35662),
+(603412935, 28706, 'zhTW', 0xE7BB86E98791E5B9B30004, 35662),
+(603412935, 28705, 'zhTW', 0xE7BB86E7A681E8AF840004, 35662),
+(603412935, 28704, 'zhTW', 0xE6888FE7A681E8AF840004, 35662),
+(603412935, 28703, 'zhTW', 0xE5BDB1E5B084E7BF920004, 35662),
+(603412935, 28702, 'zhTW', 0xE4B8ADE59BBDE7A681E997BB0004, 35662),
+(603412935, 28701, 'zhTW', 0xE680BBE4B9A6E7B18D0004, 35662),
+(603412935, 28700, 'zhTW', 0xE680BBE4B9A6E8848A0004, 35662),
+(603412935, 28699, 'zhTW', 0xE680BBE58F94E8AEA10004, 35662),
+(603412935, 28698, 'zhTW', 0xE680BBE6B791E8AEB00004, 35662),
+(603412935, 28697, 'zhTW', 0xE680BBE8BE93E69CBA0004, 35662),
+(603412935, 28696, 'zhTW', 0xE680BBE8B58EE5A6930004, 35662),
+(603412935, 28695, 'zhTW', 0xE680BBE5B19EE9B8A10004, 35662),
+(603412935, 28694, 'zhTW', 0xE680BBE7B4A0E9B8A10004, 35662),
+(603412935, 28693, 'zhTW', 0xE4B89CE58C97E78BACE7AB8B0004, 35662),
+(603412935, 28692, 'zhTW', 0xE585ABE4B99DE694BFE6B2BB0004, 35662),
+(603412935, 28691, 'zhTW', 0xE694B9E69C9DE68DA2E4BBA30004, 35662),
+(603412935, 28690, 'zhTW', 0xE696B0E79686E69AB4E5BE920004, 35662),
+(603412935, 28689, 'zhTW', 0xE4B8ADE59BBDE5BC8FE6B885E7AE970004, 35662),
+(603412935, 28688, 'zhTW', 0xE59CA3E59F8EE68B89E890A8E79A84E593ADE6B3A30004, 35662),
+(603412935, 28687, 'zhTW', 0xE982AAE681B6E79A84E5859A0004, 35662),
+(603412935, 28686, 'zhTW', 0xE8978FE4BABAE8A2ABE68D950004, 35662),
+(603412935, 28685, 'zhTW', 0xE8978FE4BABAE99C80E8A681E887AAE794B10004, 35662),
+(603412935, 28684, 'zhTW', 0xE69D80E696B0E79686E4BABA0004, 35662),
+(603412935, 28683, 'zhTW', 0xE5A4ABE4BABAE694BFE6B2BB0004, 35662),
+(603412935, 28682, 'zhTW', 0xE7BAA2E69C9DE8B08EE8A880E5BD950004, 35662),
+(603412935, 28681, 'zhTW', 0xE68B89E890A8E4BA8BE4BBB60004, 35662),
+(603412935, 28680, 'zhTW', 0xE9BB84E6B5B7E69A97E69D800004, 35662),
+(603412935, 28679, 'zhTW', 0xE4BEB5E78AAFE59BBDE5A496E4B893E588A90004, 35662),
+(603412935, 28678, 'zhTW', 0xE7ACACE4BA94E4BBA3E7BAA2E4BABA0004, 35662),
+(603412935, 28677, 'zhTW', 0xE9BB91E79E8EE5AD90E5B29B0004, 35662),
+(603412935, 28676, 'zhTW', 0xE9AB98E5B9B2E696B0E5AD90E5BC9F0004, 35662),
+(603412935, 28675, 'zhTW', 0xE6B19FE5B7B2E79785E58DB10004, 35662),
+(603412935, 28674, 'zhTW', 0xE5A4A7E9A5A5E88D92E68FADE7A7980004, 35662),
+(603412935, 28673, 'zhTW', 0xE6B19FE6AF920004, 35662),
+(603412935, 28672, 'zhTW', 0xE6B19FE6B389E99B86E59BA20004, 35662),
+(603412935, 28671, 'zhTW', 0xE6B19FE6B4BEE4BABAE9A9AC0004, 35662),
+(603412935, 28670, 'zhTW', 0xE6B19FE4BABAE9A9AC0004, 35662),
+(603412935, 28669, 'zhTW', 0xE6B19FE5ABA1E7B3BB0004, 35662),
+(603412935, 28668, 'zhTW', 0xE6B19FE4B891E997BB0004, 35662),
+(603412935, 28667, 'zhTW', 0xE6B19FE6B08FE5AEB6E6978F0004, 35662),
+(603412935, 28666, 'zhTW', 0xE883A1E6B19FE9A38EE4BA910004, 35662),
+(603412935, 28665, 'zhTW', 0xE58FBCE8BF91E793B60004, 35662);
+
+DELETE FROM `spell_name_locale` WHERE `VerifiedBuild`>0 AND `locale` = 'zhTW' AND `ID` = 298096;
+INSERT INTO `spell_name_locale` (`ID`, `locale`, `Name_lang`, `VerifiedBuild`) VALUES
+(298096, 'zhTW', '新的一盤', 35662);
