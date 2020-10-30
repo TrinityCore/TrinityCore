0bb1afbf316b7b15c05290284aa4cbe66d95547b
Ovahlord <dreadkiller@gmx.de>
2020-10-30 21:23:25 +0100

    DB/Creatures: corrected some creature data for Cruicible of Carnage creatures

---
+++ sql/updates/world/4.3.4/2020_10_30_02_world.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_template` SET `flags_extra`= `flags_extra` | 0x2000 WHERE `entry`= 48591;
+UPDATE `creature_template` SET `flags_extra`= `flags_extra` | 0x40000000 WHERE `entry`= 46947;
