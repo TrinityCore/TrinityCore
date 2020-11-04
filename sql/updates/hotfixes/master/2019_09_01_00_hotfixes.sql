5e88cd1557f035384d3b07d46b6ce40d759ee399
ReyDonovan <gennady.zamalaev@gmail.com>
2020-11-04 17:09:16 +0300

    DB/Misc: missing prev commit

--- sql/updates/hotfixes/master/2019_09_01_00_hotfixes.sql
+++ sql/updates/hotfixes/master/2019_09_01_00_hotfixes.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS `spell_visual_kit`;
 CREATE TABLE `spell_visual_kit` (
   `ID` int(10) unsigned NOT NULL DEFAULT '0',
   `Flags` int(11) NOT NULL DEFAULT '0',
-  `FallbackPriority` tinyint(3) NOT NULL DEFAULT '0',
+  `FallbackPriority` float NOT NULL DEFAULT '0',
   `FallbackSpellVisualKitId` int(11) unsigned NOT NULL DEFAULT '0',
   `DelayMin` smallint(5) unsigned NOT NULL DEFAULT '0',
   `DelayMax` smallint(5) unsigned NOT NULL DEFAULT '0',
