05fa1a46b36f81c177a503f7f5de14eb34830208
offl <offl@users.noreply.github.com>
2020-11-10 00:37:35 +0200

    DB/Quest: Update support for 'Merciful Freedom' (11676)

--- src/server/scripts/World/go_scripts.cpp
+++ src/server/scripts/World/go_scripts.cpp
@@ -23,7 +23,6 @@ go_resonite_cask
 go_tablet_of_the_seven
 go_tele_to_dalaran_crystal
 go_tele_to_violet_stand
-go_scourge_cage
 go_soulwell
 go_amberpine_outhouse
 go_veil_skith_cage
@@ -435,43 +434,6 @@ public:
     GameObjectAI* GetAI(GameObject* go) const override
     {
         return new go_matrix_punchographAI(go);
-    }
-};
-
-/*######
-## go_scourge_cage
-######*/
-
-enum ScourgeCage
-{
-    NPC_SCOURGE_PRISONER = 25610
-};
-
-class go_scourge_cage : public GameObjectScript
-{
-public:
-    go_scourge_cage() : GameObjectScript("go_scourge_cage") { }
-
-    struct go_scourge_cageAI : public GameObjectAI
-    {
-        go_scourge_cageAI(GameObject* go) : GameObjectAI(go) { }
-
-        bool OnGossipHello(Player* player) override
-        {
-            me->UseDoorOrButton();
-            if (Creature* pNearestPrisoner = me->FindNearestCreature(NPC_SCOURGE_PRISONER, 5.0f, true))
-            {
-                player->KilledMonsterCredit(NPC_SCOURGE_PRISONER, pNearestPrisoner->GetGUID());
-                pNearestPrisoner->DisappearAndDie();
-            }
-
-            return true;
-        }
-    };
-
-    GameObjectAI* GetAI(GameObject* go) const override
-    {
-        return new go_scourge_cageAI(go);
     }
 };
 
@@ -1421,7 +1383,6 @@ void AddSC_go_scripts()
     new go_tele_to_dalaran_crystal();
     new go_tele_to_violet_stand();
     new go_matrix_punchograph();
-    new go_scourge_cage();
     new go_blood_filled_orb();
     new go_soulwell();
     new go_amberpine_outhouse();
