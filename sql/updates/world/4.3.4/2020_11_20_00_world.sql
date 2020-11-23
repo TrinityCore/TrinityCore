52aada0c5e1985de29a1dda36cb02846c1fae28e
iThorgrim <raindash@outlook.fr>
2020-11-23 02:23:49 +0100

    Core/Misc: added trainer data to Myriam Spellwaker and added missing config settings to bnetserver.conf (#193)

---
+++ sql/updates/world/4.3.4/2020_11_20_00_world.sql
@@ -0,0 +1,4 @@
+DELETE FROM `creature_trainer` WHERE `CreatureId` IN (35872, 44465);
+INSERT INTO `creature_trainer` (`CreatureId`, `TrainerId`, `MenuId`, `OptionIndex`) VALUES
+(35872, 44, 10698, 0),
+(44465, 135, 10698, 0);
