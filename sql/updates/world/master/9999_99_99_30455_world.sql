4d99efb6c32ebfd7c7101ae5023c5ad8b4e3c3b1
Ryzen <972481+Ryzen-0@users.noreply.github.com>
2020-11-29 09:58:12 +0100

    Scripts/Spells: Mage Ice Lance (draft)
    Missing/todo the following:
    
    - x3 damage on frozen target
    - lower damage of second lance triggered by Splitting Ice to 80% (now it deals 100%)
    - Chain Reaction.

---
+++ sql/updates/world/master/9999_99_99_30455_world.sql
@@ -0,0 +1,3 @@
+-- mage ice lance
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_mage_ice_lance';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (30455, 'spell_mage_ice_lance');
