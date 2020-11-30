ad19a2f8c4e4c9d89365301b98916e28cc4407b5
tswow-admin <tswow@protonmail.com>
2020-11-30 09:50:39 +0100

    Add missing #pragma

--- src/server/game/Tswow/Scripting/Public/TSSpellInfo.h
+++ src/server/game/Tswow/Scripting/Public/TSSpellInfo.h
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License 
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
+#pragma once
 #include "TSClasses.h"
 
 class TC_GAME_API TSSpellInfo {
