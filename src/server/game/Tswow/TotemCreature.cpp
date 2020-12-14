3bdd016dbb96a4386a5f2430f660ddc754299e47
tswow-admin <tswow@protonmail.com>
2020-12-14 17:55:21 +0100

    Max TotemCreature hp when level has changed

--- src/server/game/Tswow/TotemCreature.cpp
+++ src/server/game/Tswow/TotemCreature.cpp
@@ -26,28 +26,34 @@ void TotemCreature::SetupTotemStats()
     }
 
     uint32 petlevel = owner->GetLevel();
-    SetLevel(petlevel);
-    CreatureTemplate const* cinfo = GetCreatureTemplate();
+    bool isNewLevel = this->GetLevel() != petlevel;
+    if (isNewLevel)
+    {
+        SetLevel(petlevel);
+        CreatureTemplate const* cinfo = GetCreatureTemplate();
 
-    // remove elite bonuses included in DB values
-    CreatureBaseStats const* stats = sObjectMgr->GetCreatureBaseStats(petlevel, cinfo->unit_class);
-    float healthmod = _GetHealthMod(cinfo->rank);
-    uint32 basehp = stats->GenerateHealth(cinfo);
-    uint32 health = uint32(basehp * healthmod);
-    uint32 mana = stats->GenerateMana(cinfo);
+        // remove elite bonuses included in DB values
+        CreatureBaseStats const* stats = sObjectMgr->GetCreatureBaseStats(petlevel, cinfo->unit_class);
+        float healthmod = _GetHealthMod(cinfo->rank);
+        uint32 basehp = stats->GenerateHealth(cinfo);
+        uint32 health = uint32(basehp * healthmod);
+        uint32 mana = stats->GenerateMana(cinfo);
 
-    SetCreateHealth(health);
-    SetCreateMana(mana);
-    SetCreateStat(STAT_STRENGTH, 1);
-    SetCreateStat(STAT_AGILITY, 1);
-    SetCreateStat(STAT_STAMINA, 1);
-    SetCreateStat(STAT_INTELLECT, 1);
-    SetCreateStat(STAT_SPIRIT, 1);
-    SetBaseWeaponDamage(BASE_ATTACK, MINDAMAGE, float(petlevel - (petlevel / 4)));
-    SetBaseWeaponDamage(BASE_ATTACK, MAXDAMAGE, float(petlevel + (petlevel / 4)));
-    UpdateAllStats();
-    SetArmor(petlevel*4);
-    SetMaxHealth(health);
+        SetCreateHealth(health);
+        SetCreateMana(mana);
+        SetCreateStat(STAT_STRENGTH, 1);
+        SetCreateStat(STAT_AGILITY, 1);
+        SetCreateStat(STAT_STAMINA, 1);
+        SetCreateStat(STAT_INTELLECT, 1);
+        SetCreateStat(STAT_SPIRIT, 1);
+        SetBaseWeaponDamage(BASE_ATTACK, MINDAMAGE, float(petlevel - (petlevel / 4)));
+        SetBaseWeaponDamage(BASE_ATTACK, MAXDAMAGE, float(petlevel + (petlevel / 4)));
+        UpdateAllStats();
+        SetArmor(petlevel*4);
+
+        SetMaxHealth(health);
+        this->SetHealth(health);
+    }
 }
 
 void TotemCreature::Update(uint32 time)
