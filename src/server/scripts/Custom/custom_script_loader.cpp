c40b28cb7393d6173af1a32f32cf64b8fb7c4155
Stitch11 <stitchounet@orange.fr>
2020-12-27 16:27:37 +0100

    PNJ 15000386 : Choix au level 1 : Lieux de depart ou instant level 10

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -16,10 +16,13 @@
  */
 
 // This is where scripts' loading functions should be declared:
+void AddSC_npc_levels();
 
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
 void AddCustomScripts()
 {
+    AddSC_npc_levels();
+
 }
