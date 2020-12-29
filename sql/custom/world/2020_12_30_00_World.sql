8ea9ace809dc05f67bdc0bca2646ecdd268e1c8d
TheGhost <theghostgroup@gmail.com>
2020-12-30 12:00:17 +1300

    DB/Creature: fix invalid model ID's

---
+++ sql/custom/world/2020_12_30_00_World.sql
@@ -0,0 +1,16 @@
+-- creature model info errors fixes
+
+UPDATE `creature_template` SET `modelid1` = 14732 WHERE `entry` IN (112590, 93773, 91498, 93791, 25256, 25749, 32315, 91195, 111106, 73716, 89753, 111115,
+111105, 110519, 109071, 103663, 100636, 93795);
+UPDATE `creature_template` SET `modelid1` = 229 WHERE `entry` = 2052;
+UPDATE `creature_template` SET `modelid1` = 30272 WHERE `entry` = 2993;
+UPDATE `creature_template` SET `modelid1` = 66644 WHERE `entry` = 11878;
+UPDATE `creature_template` SET `modelid1` = 16217 WHERE `entry` IN (25544, 24692);
+UPDATE `creature_template` SET `modelid1` = 71757 WHERE `entry` = 20267;
+UPDATE `creature_template` SET `modelid1` = 60753 WHERE `entry` = 88133;
+UPDATE `creature_template` SEt `modelid1` = 18342 WHERE `entry` = 18604;
+UPDATE `creature_template` SET `modelid1` = 66086 WHERE `entry` IN (90710, 117121);
+UPDATE `creature_template` SET `modelid1` = 28213 WHERE `entry` IN (30426, 30427, 30428);
+UPDATE `creature_template` SET `modelid1` = 67156 WHERE `entry` = 38258;
+UPDATE `creature_template` SET `modelid1` = 67146 WHERE `entry` IN (39120, 39121, 39122);
+UPDaTE `creature_template` SET `modelid1` = 37526 WHERE `entry` IN (51467);
