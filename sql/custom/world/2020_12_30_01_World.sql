0ae1d786fe248a809618c6d31c15fee6d15cc538
TheGhost <theghostgroup@gmail.com>
2020-12-30 20:38:26 +1300

    DB/Gameobject: Add missing templates & fixes.

---
+++ sql/custom/world/2020_12_30_01_World.sql
@@ -0,0 +1,107 @@
+-- fix gameobject template errors
+
+UPDATE `gameobject_template` SET `Data1` = 0 WHERE `entry` = 215364;
+UPDATE `gameobject_template` SET `Data6`=0, `VerifiedBuild`=26972 WHERE `entry`=205577; -- Fire
+UPDATE `gameobject_template` SET `Data6`=0, `VerifiedBuild`=26972 WHERE `entry`=205578; -- Fire
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=4667, `VerifiedBuild`=26972 WHERE `entry`=103815; -- Ambermill Strongbox
+UPDATE `gameobject_template` SET `Data1`=0, `Data30`=34700, `VerifiedBuild`=26972 WHERE `entry`=205534; -- Ambermill Codex
+UPDATE `gameobject_template` SET `Data1`=0, `Data30`=34375, `VerifiedBuild`=26972 WHERE `entry`=205363; -- Forsaken Communication Device
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=26878, `VerifiedBuild`=26972 WHERE `entry`=194341; -- Dusty Journal
+UPDATE `gameobject_template` SET `Data1`=0, `Data30`=13605, `VerifiedBuild`=26972 WHERE `entry`=176145; -- Joseph Redpath's Monument
+UPDATE `gameobject_template` SET `Data6`=0, `VerifiedBuild`=26972 WHERE `entry`=22620; -- Wooden Chair
+UPDATE `gameobject_template` SET `Data8`=0, `VerifiedBuild`=26972 WHERE `entry`=207430; -- Cultist Cage
+UPDATE `gameobject_template` SET `Data8`=0, `VerifiedBuild`=26972 WHERE `entry`=207428; -- Cultist Cage
+UPDATE `gameobject_template` SET `Data8`=0, `VerifiedBuild`=26972 WHERE `entry`=205160; -- Cultist Cage
+UPDATE `gameobject_template` SET `Data8`=0, `VerifiedBuild`=26972 WHERE `entry`=205155; -- Cultist Cage
+UPDATE `gameobject_template` SET `Data8`=0, `VerifiedBuild`=26972 WHERE `entry`=207427; -- Cultist Cage
+UPDATE `gameobject_template` SET `Data8`=0, `VerifiedBuild`=26972 WHERE `entry`=205156; -- Cultist Cage
+UPDATE `gameobject_template` SET `Data8`=0, `VerifiedBuild`=26972 WHERE `entry`=207429; -- Cultist Cage
+UPDATE `gameobject_template` SET `Data8`=0, `VerifiedBuild`=26972 WHERE `entry`=205159; -- Cultist Cage
+UPDATE `gameobject_template` SET `Data8`=0, `VerifiedBuild`=26972 WHERE `entry`=205157; -- Cultist Cage
+UPDATE `gameobject_template` SET `Data8`=0, `VerifiedBuild`=26972 WHERE `entry`=205158; -- Cultist Cage
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=32199, `VerifiedBuild`=26972 WHERE `entry`=205154; -- Child's Painting
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=32198, `VerifiedBuild`=26972 WHERE `entry`=205153; -- Prayer Book
+UPDATE `gameobject_template` SET `Data6`=0, `VerifiedBuild`=26972 WHERE `entry`=22636; -- Wooden Chair
+UPDATE `gameobject_template` SET `Data6`=0, `VerifiedBuild`=26972 WHERE `entry`=22634; -- Wooden Chair
+UPDATE `gameobject_template` SET `Data1`=0, `Data30`=1746, `VerifiedBuild`=26972 WHERE `entry`=2893; -- Zul'Mamwe Trophy Skulls
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=30083, `VerifiedBuild`=26972 WHERE `entry`=204087; -- Mosh'Ogg Bounty
+UPDATE `gameobject_template` SET `Data1`=0, `Data30`=1744, `VerifiedBuild`=26972 WHERE `entry`=2891; -- Balia'mah Trophy Skulls
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=3309, `VerifiedBuild`=26972 WHERE `entry`=20691; -- Cozzle's Footlocker
+UPDATE `gameobject_template` SET `Data1`=0, `Data30`=1690, `VerifiedBuild`=26972 WHERE `entry`=179426; -- Gri'lek the Wanderer
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=1690, `VerifiedBuild`=26972 WHERE `entry`=58; -- Gri'lek the Wanderer
+UPDATE `gameobject_template` SET `Data26`=1, `VerifiedBuild`=26972 WHERE `entry`=2576; -- Altar of the Tides
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=30069, `VerifiedBuild`=26972 WHERE `entry`=204000; -- Grom'gol Rations Crate
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=31220, `VerifiedBuild`=26972 WHERE `entry`=57; -- Bloodscalp Lore Tablet
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=1570, `VerifiedBuild`=26972 WHERE `entry`=1765; -- Worn Wooden Chest
+UPDATE `gameobject_template` SET `type`=10, `Data0`=2824, `Data10`=261655, `Data17`=1, `Data20`=1, `Data23`=1, `VerifiedBuild`=26972 WHERE `entry`=207279; -- Warchief's Command Board
+UPDATE `gameobject_template` SET `Data1`=0, `Data30`=36026, `VerifiedBuild`=26972 WHERE `entry`=207183; -- Humbert's Pants
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=4305, `VerifiedBuild`=26972 WHERE `entry`=92013; -- Tome of the Cabal
+UPDATE `gameobject_template` SET `Data1`=0, `Data30`=2143, `VerifiedBuild`=26972 WHERE `entry`=2710; -- Spirit of Silverpine Log
+UPDATE `gameobject_template` SET `Data1`=0, `Data30`=2142, `VerifiedBuild`=26972 WHERE `entry`=2708; -- Spirit of Silverpine Charts
+UPDATE `gameobject_template` SET `Data1`=0, `Data30`=2140, `VerifiedBuild`=26972 WHERE `entry`=2707; -- Maiden's Folly Charts
+UPDATE `gameobject_template` SET `Data1`=0, `Data30`=2141, `VerifiedBuild`=26972 WHERE `entry`=2709; -- Maiden's Folly Log
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=2177, `VerifiedBuild`=26972 WHERE `entry`=2718; -- Trelane's Lockbox
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=2178, `VerifiedBuild`=26972 WHERE `entry`=2717; -- Trelane's Footlocker
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=2179, `VerifiedBuild`=26972 WHERE `entry`=2716; -- Trelane's Chest
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=2138, `VerifiedBuild`=26972 WHERE `entry`=2690; -- Stone of Outer Binding
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=14625, `VerifiedBuild`=26972 WHERE `entry`=177789; -- Augustus' Receipt Book
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=34718, `VerifiedBuild`=26972 WHERE `entry`=205558; -- Flesh Giant Foot
+UPDATE `gameobject_template` SET `Data1`=0, `Data30`=14287, `VerifiedBuild`=26972 WHERE `entry`=177264; -- Symbol of Lost Honor
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=34714, `VerifiedBuild`=26972 WHERE `entry`=205537; -- Open Prayer Book
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=34750, `VerifiedBuild`=26972 WHERE `entry`=205880; -- Battered Chest
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=34751, `VerifiedBuild`=26972 WHERE `entry`=205881; -- Battered Chest
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=34749, `VerifiedBuild`=26972 WHERE `entry`=205879; -- Battered Chest
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=34748, `VerifiedBuild`=26972 WHERE `entry`=205878; -- Battered Chest
+UPDATE `gameobject_template` SET `Data18`=683, `VerifiedBuild`=26972 WHERE `entry`=207488; -- Sturdy Treasure Chest
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=29583, `VerifiedBuild`=26972 WHERE `entry`=203206; -- Dreadmaul Cache
+UPDATE `gameobject_template` SET `Data18`=676, `VerifiedBuild`=26972 WHERE `entry`=207478; -- Silverbound Treasure Chest
+UPDATE `gameobject_template` SET `Data18`=683, `VerifiedBuild`=26972 WHERE `entry`=207513; -- Silken Treasure Chest
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=34741, `VerifiedBuild`=26972 WHERE `entry`=205828; -- Stack of Questionable Publications
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=34740, `VerifiedBuild`=26972 WHERE `entry`=205827; -- Extra-Pure Blasting Powder
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=34739, `VerifiedBuild`=26972 WHERE `entry`=205826; -- Thousand-Thread-Count Fuse
+UPDATE `gameobject_template` SET `Data8`=0, `VerifiedBuild`=26972 WHERE `entry`=202831; -- Portcullis
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=8920, `VerifiedBuild`=26972 WHERE `entry`=148506; -- Twilight Artifact
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=35545, `VerifiedBuild`=26972 WHERE `entry`=206597; -- Twilight's Hammer Crate
+UPDATE `gameobject_template` SET `Data18`=683, `VerifiedBuild`=26972 WHERE `entry`=207500; -- Dark Iron Treasure Chest
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=28239, `VerifiedBuild`=26972 WHERE `entry`=1586; -- Crate of Candles
+UPDATE `gameobject_template` SET `Data1`=0, `Data30`=1684, `VerifiedBuild`=26972 WHERE `entry`=1165; -- Gunther's Books
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=4765, `VerifiedBuild`=26972 WHERE `entry`=105170; -- Agamand Weapon Rack
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=4764, `VerifiedBuild`=26972 WHERE `entry`=105169; -- Agamand Weapon Rack
+UPDATE `gameobject_template` SET `Data1`=0, `Data30`=36135, `VerifiedBuild`=26972 WHERE `entry`=105171; -- Agamand Weapon Rack
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=4767, `VerifiedBuild`=26972 WHERE `entry`=105172; -- Agamand Weapon Rack
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=3625, `VerifiedBuild`=26972 WHERE `entry`=37098; -- Perrine's Chest
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=1853, `VerifiedBuild`=26972 WHERE `entry`=2087; -- Bloodsail Orders
+UPDATE `gameobject_template` SET `Data1`=0, `Data30`=30796, `VerifiedBuild`=26972 WHERE `entry`=759; -- The Holy Spring
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=29599, `VerifiedBuild`=26972 WHERE `entry`=203229; -- Allistarjian Vault
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=29582, `VerifiedBuild`=26972 WHERE `entry`=203205; -- Dreadmaul Cache
+UPDATE `gameobject_template` SET `type`=50, `Data0`=2863, `Data1`=51298, `Data3`=0, `Data4`=225, `Data5`=275, `Data6`=30, `Data12`=683, `Data13`=1, `Data18`=10, `Data19`=0, `VerifiedBuild`=26972 WHERE `entry`=150082; -- Small Thorium Vein
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=29581, `VerifiedBuild`=26972 WHERE `entry`=203204; -- Dreadmaul Cache
+UPDATE `gameobject_template` SET `Data1`=0, `Data30`=30439, `VerifiedBuild`=26972 WHERE `entry`=204133; -- Cache of Shadra
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=13342, `VerifiedBuild`=26972 WHERE `entry`=175889; -- Ancient Egg
+UPDATE `gameobject_template` SET `Data1`=0, `Data30`=16925, `VerifiedBuild`=26972 WHERE `entry`=179914; -- Pile of Bones
+UPDATE `gameobject_template` SET `Data1`=0, `Data30`=30296, `VerifiedBuild`=26972 WHERE `entry`=204120; -- Cache of Shadra
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=16919, `VerifiedBuild`=26972 WHERE `entry`=179908; -- Slagtree's Lost Tools
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=3625, `VerifiedBuild`=26972 WHERE `entry`=37098; -- Perrine's Chest
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data6`=0, `Data30`=30943, `VerifiedBuild`=26972 WHERE `entry`=204587; -- Narkk's Handbombs
+UPDATE `gameobject_template` SET `Data6`=0, `VerifiedBuild`=26972 WHERE `entry`=204586; -- Bloodsail Rope
+UPDATE `gameobject_template` SET `Data6`=0, `VerifiedBuild`=26972 WHERE `entry`=204585; -- Blackwater Rope
+UPDATE `gameobject_template` SET `Data4`=1, `Data6`=0, `Data18`=81, `VerifiedBuild`=26972 WHERE `entry`=204572; -- Pirate Ship
+UPDATE `gameobject_template` SET `Data4`=1, `Data6`=0, `Data18`=81, `VerifiedBuild`=26972 WHERE `entry`=204571; -- Pirate Ship
+UPDATE `gameobject_template` SET `Data4`=1, `Data6`=0, `Data18`=81, `VerifiedBuild`=26972 WHERE `entry`=204570; -- Pirate Ship
+UPDATE `gameobject_template` SET `Data4`=1, `Data6`=0, `Data18`=81, `VerifiedBuild`=26972 WHERE `entry`=204569; -- Pirate Ship
+UPDATE `gameobject_template` SET `Data4`=1, `Data6`=0, `Data18`=81, `VerifiedBuild`=26972 WHERE `entry`=204568; -- Pirate Ship
+UPDATE `gameobject_template` SET `Data4`=1, `Data6`=0, `Data18`=93, `VerifiedBuild`=26972 WHERE `entry`=204567; -- Pirate Ship
+UPDATE `gameobject_template` SET `Data4`=1, `Data6`=0, `Data18`=92, `VerifiedBuild`=26972 WHERE `entry`=204566; -- Pirate Ship (Riptide)
+UPDATE `gameobject_template` SET `Data4`=1, `Data6`=0, `Data18`=91, `VerifiedBuild`=26972 WHERE `entry`=204565; -- Pirate Ship (Damsel's Luck)
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=13642, `VerifiedBuild`=26972 WHERE `entry`=176208; -- Horgus' Skull
+UPDATE `gameobject_template` SET `type`=50, `Data3`=0, `Data6`=60, `Data7`=0, `Data18`=10, `Data20`=176117, `VerifiedBuild`=26972 WHERE `entry`=176143; -- Pamela's Doll's Right Side
+UPDATE `gameobject_template` SET `type`=50, `Data3`=0, `Data6`=60, `Data7`=0, `Data18`=10, `Data20`=176117, `VerifiedBuild`=26972 WHERE `entry`=176142; -- Pamela's Doll's Left Side
+UPDATE `gameobject_template` SET `type`=50, `Data3`=0, `Data6`=60, `Data7`=0, `Data18`=10, `Data20`=176117, `VerifiedBuild`=26972 WHERE `entry`=176116; -- Pamela's Doll's Head
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=13644, `VerifiedBuild`=26972 WHERE `entry`=176209; -- Shattered Sword of Marduk
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=14625, `VerifiedBuild`=26972 WHERE `entry`=177789; -- Augustus' Receipt Book
+UPDATE `gameobject_template` SET `Data8`=0, `VerifiedBuild`=26972 WHERE `entry`=207429; -- Cultist Cage
+UPDATE `gameobject_template` SET `Data18`=683, `VerifiedBuild`=26972 WHERE `entry`=207513; -- Silken Treasure Chest
+UPDATE `gameobject_template` SET `Data1`=0, `Data30`=35739, `VerifiedBuild`=26972 WHERE `entry`=206882; -- Fel Slider Cider
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=35738, `VerifiedBuild`=26972 WHERE `entry`=206881; -- Slumber Sand
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=35357, `VerifiedBuild`=26972 WHERE `entry`=206388; -- Angor's Coffer
+UPDATE `gameobject_template` SET `Data18`=683, `VerifiedBuild`=26972 WHERE `entry`=207476; -- Silverbound Treasure Chest
