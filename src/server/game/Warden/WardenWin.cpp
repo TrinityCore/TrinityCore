de33d50c1940512985887f916118014c98b53ecb
Treeston <treeston.mmoc@gmail.com>
2021-01-04 23:52:50 +0100

    Core/Warden: Max packet size 500 -> 450

--- src/server/game/Warden/WardenWin.cpp
+++ src/server/game/Warden/WardenWin.cpp
@@ -251,7 +251,7 @@ void WardenWin::RequestChecks()
         [&expectedSize](uint16 id)
         {
             uint8 const thisSize = GetCheckPacketSize(sWardenCheckMgr->GetCheckData(id));
-            if ((expectedSize + thisSize) > 500) // warden packets are truncated to 512 bytes clientside
+            if ((expectedSize + thisSize) > 450) // warden packets are truncated to 512 bytes clientside
                 return true;
             expectedSize += thisSize;
             return false;
