db5b7f6db38f8d7155594196fce2724b9d1e8a5a
TheGhost <theghostgroup@gmail.com>
2021-01-04 19:25:58 +1300

    DB/Gameobject: Fixes for gameobject templates.

---
+++ sql/custom/world/2021_01_04_02_World.sql
@@ -0,0 +1,88 @@
+-- gameobject template errors fix
+
+UPDATE `gameobject_template` SET `Data26`=1, `VerifiedBuild`=26972 WHERE `entry`=186455; -- Ashtree Smoke (x2.0)
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=18449, `VerifiedBuild`=26972 WHERE `entry`=181681; -- Chalice of Elune
+UPDATE `gameobject_template` SET `Data26`=1, `VerifiedBuild`=26972 WHERE `entry`=194612; -- Ritual Gem
+UPDATE `gameobject_template` SET `Data3`=1, `VerifiedBuild`=26972 WHERE `entry`=180718; -- The Scarab Gong
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=17349, `VerifiedBuild`=26972 WHERE `entry`=180435; -- Noggle's Satchel
+UPDATE `gameobject_template` SET `type`=5, `Data2`=0, `Data3`=1, `VerifiedBuild`=26972 WHERE `entry`=181617; -- Sillithus Flag Stand, Horde
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=16593, `VerifiedBuild`=26972 WHERE `entry`=179565; -- Dusty Reliquary
+UPDATE `gameobject_template` SET `displayId`=8948, `VerifiedBuild`=26972 WHERE `entry`=207565; -- Portal to Blasted Lands
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=27288, `VerifiedBuild`=26972 WHERE `entry`=195205; -- Hanging Pirate Head
+UPDATE `gameobject_template` SET `Data6`=0, `VerifiedBuild`=26972 WHERE `entry`=208161; -- Candy Bucket
+UPDATE `gameobject_template` SET `Data26`=1, `VerifiedBuild`=26972 WHERE `entry`=3192; -- Attack Plan: Orgrimmar
+UPDATE `gameobject_template` SET `Data26`=1, `VerifiedBuild`=26972 WHERE `entry`=3190; -- Attack Plan: Sen'jin Village
+UPDATE `gameobject_template` SET `Data26`=1, `VerifiedBuild`=26972 WHERE `entry`=3189; -- Attack Plan: Valley of Trials
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=27522, `VerifiedBuild`=26972 WHERE `entry`=195531; -- Demonic Chest
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=11121, `VerifiedBuild`=26972 WHERE `entry`=164910; -- Ornate Chest
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=11460, `VerifiedBuild`=26972 WHERE `entry`=166863; -- Fresh Threshadon Carcass
+UPDATE `gameobject_template` SET `Data6`=0, `VerifiedBuild`=26972 WHERE `entry`=208167; -- Candy Bucket
+UPDATE `gameobject_template` SET `Data26`=1, `VerifiedBuild`=26972 WHERE `entry`=202133; -- Naga Banner
+UPDATE `gameobject_template` SET `Data6`=0, `Data12`=202361, `VerifiedBuild`=26972 WHERE `entry`=202353; -- Rockin' Powder - Inert
+UPDATE `gameobject_template` SET `Data6`=0, `VerifiedBuild`=26972 WHERE `entry`=202248; -- Naga Banner
+UPDATE `gameobject_template` SET `Data18`=683, `VerifiedBuild`=26972 WHERE `entry`=207484; -- Sturdy Treasure Chest
+UPDATE `gameobject_template` SET `Data13`=0, `Data26`=1, `VerifiedBuild`=26972 WHERE `entry`=195188; -- Goblin Escape Pod
+UPDATE `gameobject_template` SET `Data6`=0, `VerifiedBuild`=26972 WHERE `entry`=185016; -- Shadow Council Magic Device 01
+UPDATE `gameobject_template` SET `type`=50, `Data0`=2850, `Data3`=0, `Data4`=235, `Data5`=285, `Data6`=30, `Data12`=683, `Data13`=1, `Data18`=10, `Data19`=0, `Data23`=0, `Data24`=0, `VerifiedBuild`=26972 WHERE `entry`=142140; -- Purple Lotus
+UPDATE `gameobject_template` SET `size`=1.739999890327453613, `VerifiedBuild`=26972 WHERE `entry`=206585; -- Totem of Ruumbo
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=27525, `VerifiedBuild`=26972 WHERE `entry`=195584; -- Stolen Manual
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=27645, `VerifiedBuild`=26972 WHERE `entry`=196835; -- Important Documents
+UPDATE `gameobject_template` SET `Data26`=1, `VerifiedBuild`=26972 WHERE `entry`=195582; -- Stolen Manual
+UPDATE `gameobject_template` SET `Data26`=1, `VerifiedBuild`=26972 WHERE `entry`=195576; -- Stolen Manual
+UPDATE `gameobject_template` SET `Data26`=1, `VerifiedBuild`=26972 WHERE `entry`=195575; -- Stolen Manual
+UPDATE `gameobject_template` SET `Data26`=1, `VerifiedBuild`=26972 WHERE `entry`=195577; -- Stolen Manual
+UPDATE `gameobject_template` SET `Data18`=683, `VerifiedBuild`=26972 WHERE `entry`=207485; -- Sturdy Treasure Chest
+UPDATE `gameobject_template` SET `Data6`=0, `Data8`=1, `VerifiedBuild`=26972 WHERE `entry`=201578; -- Wrenchmen Recruitment Poster
+UPDATE `gameobject_template` SET `Data6`=0, `Data8`=1, `VerifiedBuild`=26972 WHERE `entry`=203755; -- Wrenchmen Recruitment Poster
+UPDATE `gameobject_template` SET `Data5`=1, `Data6`=0, `Data10`=69465, `VerifiedBuild`=26972 WHERE `entry`=200301; -- Impaling Spine
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=27288, `VerifiedBuild`=26972 WHERE `entry`=195205; -- Hanging Pirate Head
+UPDATE `gameobject_template` SET `Data26`=1, `VerifiedBuild`=26972 WHERE `entry`=201743; -- Field Banner
+UPDATE `gameobject_template` SET `Data26`=1, `VerifiedBuild`=26972 WHERE `entry`=201781; -- Field Banner
+UPDATE `gameobject_template` SET `Data26`=1, `VerifiedBuild`=26972 WHERE `entry`=201810; -- Field Banner
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=22301, `VerifiedBuild`=26972 WHERE `entry`=186266; -- Deserter Propaganda
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=11626, `VerifiedBuild`=26972 WHERE `entry`=174728; -- Damaged Crate
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=3314, `VerifiedBuild`=26972 WHERE `entry`=20727; -- Gizmorium Shipping Crate
+UPDATE `gameobject_template` SET `Data20`=1, `Data26`=1, `VerifiedBuild`=26972 WHERE `entry`=22234; -- Signal Torch
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=3285, `VerifiedBuild`=26972 WHERE `entry`=19905; -- Mok'Morokk's Grog
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=3284, `VerifiedBuild`=26972 WHERE `entry`=19904; -- Mok'Morokk's Snuff
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=3286, `VerifiedBuild`=26972 WHERE `entry`=19906; -- Mok'Morokk's Strongbox
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=22328, `VerifiedBuild`=26972 WHERE `entry`=186329; -- Stonemaul Clan Banner
+UPDATE `gameobject_template` SET `Data18`=683, `VerifiedBuild`=26972 WHERE `entry`=207475; -- Silverbound Treasure Chest
+UPDATE `gameobject_template` SET `Data5`=1, `Data7`=0, `Data12`=176592, `VerifiedBuild`=26972 WHERE `entry`=176635; -- Shellfish Trap
+UPDATE `gameobject_template` SET `Data8`=5421, `VerifiedBuild`=26972 WHERE `entry`=176582; -- Shellfish Trap
+UPDATE `gameobject_template` SET `Data0`=259, `VerifiedBuild`=26972 WHERE `entry`=195674; -- Aloe Thistle
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=27522, `VerifiedBuild`=26972 WHERE `entry`=195531; -- Demonic Chest
+UPDATE `gameobject_template` SET `Data6`=0, `Data8`=1, `VerifiedBuild`=26972 WHERE `entry`=201578; -- Wrenchmen Recruitment Poster
+UPDATE `gameobject_template` SET `Data6`=0, `Data8`=1, `VerifiedBuild`=26972 WHERE `entry`=203755; -- Wrenchmen Recruitment Poster
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=27554, `VerifiedBuild`=26972 WHERE `entry`=201579; -- Keystone Shard
+UPDATE `gameobject_template` SET `Data1`=0, `Data30`=27589, `VerifiedBuild`=26972 WHERE `entry`=196462; -- Giant-Sized Laxative
+UPDATE `gameobject_template` SET `Data1`=0, `Data30`=27506, `VerifiedBuild`=26972 WHERE `entry`=195513; -- Ancient Stone Cask
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=27729, `VerifiedBuild`=26972 WHERE `entry`=201572; -- SFG
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=35731, `VerifiedBuild`=26972 WHERE `entry`=206834; -- Twilight Element of Fire
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=35729, `VerifiedBuild`=26972 WHERE `entry`=206832; -- Twilight Element of Air
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=35730, `VerifiedBuild`=26972 WHERE `entry`=206833; -- Twilight Element of Earth
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=35732, `VerifiedBuild`=26972 WHERE `entry`=206835; -- Twilight Element of Water
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=35715, `VerifiedBuild`=26972 WHERE `entry`=206758; -- Codemaster's Code Device
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=34654, `VerifiedBuild`=26972 WHERE `entry`=205421; -- The Drums of War
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=34576, `VerifiedBuild`=26972 WHERE `entry`=205416; -- The Writ of History
+UPDATE `gameobject_template` SET `Data26`=1, `VerifiedBuild`=26972 WHERE `entry`=203009; -- Pirate Accuracy Increasing Fire
+UPDATE `gameobject_template` SET `Data26`=1, `VerifiedBuild`=26972 WHERE `entry`=201743; -- Field Banner
+UPDATE `gameobject_template` SET `Data18`=683, `VerifiedBuild`=26972 WHERE `entry`=207474; -- Silverbound Treasure Chest
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=29568, `VerifiedBuild`=26972 WHERE `entry`=203088; -- BD-816 War Apparatus
+UPDATE `gameobject_template` SET `Data1`=0, `Data30`=29567, `VerifiedBuild`=26972 WHERE `entry`=203176; -- Mechanized Ice
+UPDATE `gameobject_template` SET `Data1`=0, `Data30`=3091, `VerifiedBuild`=26972 WHERE `entry`=19602; -- Venture Co. Engineering Plans
+UPDATE `gameobject_template` SET `Data1`=0, `Data30`=29565, `VerifiedBuild`=26972 WHERE `entry`=203174; -- Mechanized Fire
+UPDATE `gameobject_template` SET `Data6`=0, `VerifiedBuild`=26972 WHERE `entry`=194504; -- Thunder Bluff Banner
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=39470, `VerifiedBuild`=26972 WHERE `entry`=3524; -- The Demon Seed
+UPDATE `gameobject_template` SET `castBarCaption`='Recovering', `Data1`=0, `Data2`=1, `Data14`=55091, `Data30`=27293, `VerifiedBuild`=26972 WHERE `entry`=195224; -- Stolen Silver Lockbox
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data30`=3878, `VerifiedBuild`=26972 WHERE `entry`=58369; -- Stolen Iron Chest
+UPDATE `gameobject_template` SET `Data18`=683, `VerifiedBuild`=26972 WHERE `entry`=207484; -- Sturdy Treasure Chest
+UPDATE `gameobject_template` SET `Data6`=1, `VerifiedBuild`=26972 WHERE `entry`=201736; -- Gasbot Control Panel
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data4`=1, `Data5`=1, `Data13`=1, `Data30`=27509, `Data31`=1, `VerifiedBuild`=26972 WHERE `entry`=195516; -- The Goblin Lisa
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data4`=1, `Data5`=1, `Data13`=1, `Data30`=27510, `Data31`=1, `VerifiedBuild`=26972 WHERE `entry`=195518; -- The Ultimate Bomb
+UPDATE `gameobject_template` SET `Data1`=0, `Data2`=1, `Data4`=1, `Data5`=1, `Data13`=1, `Data30`=27508, `Data31`=1, `VerifiedBuild`=26972 WHERE `entry`=195515; -- Maldy's Falcon
+UPDATE `gameobject_template` SET `Data26`=1, `VerifiedBuild`=26972 WHERE `entry`=195488; -- Kaja'mite Deposit
+UPDATE `gameobject_template` SET `Data26`=1, `VerifiedBuild`=26972 WHERE `entry`=201735; -- Defective Generator
+UPDATE `gameobject_template` SET `Data26`=1, `VerifiedBuild`=26972 WHERE `entry`=201734; -- Flammable Bed
+UPDATE `gameobject_template` SET `Data26`=1, `VerifiedBuild`=26972 WHERE `entry`=201733; -- Leaky Stove
+
