aea90dc20609e1849c3ce3d8e25a7d04913c28a6
offl <offl@users.noreply.github.com>
2021-01-08 23:26:40 +0200

    DB/Creature: Remove difference between difficulties in case of HoverHeight & gossip menu

---
+++ sql/updates/world/3.3.5/2021_01_08_04_world.sql
@@ -0,0 +1,24 @@
+--
+UPDATE `creature_template` SET `HoverHeight` = 1.2 WHERE `entry` = 31598;
+UPDATE `creature_template` SET `HoverHeight` = 5.85 WHERE `entry` = 30903;
+UPDATE `creature_template` SET `HoverHeight` = 5.85 WHERE `entry` = 30518;
+UPDATE `creature_template` SET `HoverHeight` = 2 WHERE `entry` = 30521;
+UPDATE `creature_template` SET `HoverHeight` = 5 WHERE `entry` = 34227;
+UPDATE `creature_template` SET `HoverHeight` = 3 WHERE `entry` = 31669;
+UPDATE `creature_template` SET `HoverHeight` = 8 WHERE `entry` = 33724;
+UPDATE `creature_template` SET `HoverHeight` = 9 WHERE `entry` = 38220;
+UPDATE `creature_template` SET `HoverHeight` = 12 WHERE `entry` = 38258;
+UPDATE `creature_template` SET `HoverHeight` = 9 WHERE `entry` = 38219;
+UPDATE `creature_template` SET `HoverHeight` = 9 WHERE `entry` = 38444;
+UPDATE `creature_template` SET `HoverHeight` = 10 WHERE `entry` = 33694;
+UPDATE `creature_template` SET `HoverHeight` = 3 WHERE `entry` = 30810;
+UPDATE `creature_template` SET `HoverHeight` = 4 WHERE `entry` = 38418;
+
+UPDATE `creature_template` SET `gossip_menu_id` = 10012 WHERE `entry` = 31366;
+UPDATE `creature_template` SET `gossip_menu_id` = 7525 WHERE `entry` = 20165;
+UPDATE `creature_template` SET `gossip_menu_id` = 8724 WHERE `entry` = 30792;
+UPDATE `creature_template` SET `gossip_menu_id` = 8721 WHERE `entry` = 30796;
+UPDATE `creature_template` SET `gossip_menu_id` = 8716 WHERE `entry` = 30797;
+UPDATE `creature_template` SET `gossip_menu_id` = 8723 WHERE `entry` = 30799;
+UPDATE `creature_template` SET `gossip_menu_id` = 8722 WHERE `entry` = 30801;
+UPDATE `creature_template` SET `gossip_menu_id` = 5441 WHERE `entry` = 22640;
