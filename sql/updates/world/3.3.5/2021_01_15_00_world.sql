793e6ae4ae3d4806df9976b30eb64c87c6adda04
offl <offl@users.noreply.github.com>
2021-01-15 01:49:26 +0200

    DB/Creature: Update stats of some creatures

---
+++ sql/updates/world/3.3.5/2021_01_15_00_world.sql
@@ -0,0 +1,55 @@
+--
+UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80 WHERE `entry` = 35062;
+UPDATE `creature_template` SET `minlevel` = 79 WHERE `entry` = 35079;
+UPDATE `creature_template` SET `minlevel` = 78 WHERE `entry` = 35080;
+UPDATE `creature_template` SET `minlevel` = 78 WHERE `entry` = 35084;
+UPDATE `creature_template` SET `minlevel` = 78 WHERE `entry` = 35092;
+UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80 WHERE `entry` = 35226;
+UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80 WHERE `entry` = 35227;
+UPDATE `creature_template` SET `difficulty_entry_1` = 36135, `minlevel` = 80, `unit_flags2` = 32 WHERE `entry` = 35311;
+UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80, `speed_run` = 0.992063, `unit_flags2` = 32, `flags_extra` = `flags_extra` |64 WHERE `entry` = 36135;
+UPDATE `creature_template` SET `minlevel` = 60, `maxlevel` = 60 WHERE `entry` = 35339;
+UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80 WHERE `entry` = 35376;
+UPDATE `creature_template` SET `minlevel` = 83, `maxlevel` = 83, `unit_flags` = 33555200 WHERE `entry` = 35473;
+UPDATE `creature_template` SET `minlevel` = 1, `maxlevel` = 1 WHERE `entry` = 35651;
+
+UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80, `unit_flags` = 33587968 WHERE `entry` = 36173;
+UPDATE `creature_template` SET `minlevel` = 82, `maxlevel` = 82 WHERE `entry` = 36503;
+UPDATE `creature_template` SET `minlevel` = 82, `maxlevel` = 82 WHERE `entry` = 36504;
+UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80 WHERE `entry` = 36549;
+
+UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80 WHERE `entry` = 37183;
+UPDATE `creature_template` SET `minlevel` = 81, `maxlevel` = 81 WHERE `entry` = 37547;
+UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80 WHERE `entry` = 37852;
+UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80 WHERE `entry` = 37948;
+
+UPDATE `creature_template` SET `minlevel` = 60, `maxlevel` = 60 WHERE `entry` = 38153;
+UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80, `unit_flags` = 33555200 WHERE `entry` = 38340;
+UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80, `unit_flags` = 33555200 WHERE `entry` = 38341;
+UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80, `unit_flags` = 33555200 WHERE `entry` = 38342;
+UPDATE `creature_template` SET `minlevel` = 83, `maxlevel` = 83 WHERE `entry` = 38569;
+UPDATE `creature_template` SET `minlevel` = 60, `maxlevel` = 60 WHERE `entry` = 38763;
+UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80, `unit_flags` = 33554432 WHERE `entry` = 38879;
+
+UPDATE `creature_template` SET `minlevel` = 65, `maxlevel` = 65, `unit_flags` = 32768 WHERE `entry` = 34038;
+UPDATE `creature_template` SET `minlevel` = 70, `maxlevel` = 70, `unit_flags` = 32768 WHERE `entry` = 34060;
+UPDATE `creature_template` SET `minlevel` = 65, `maxlevel` = 65, `unit_flags` = 32768 WHERE `entry` = 34063;
+UPDATE `creature_template` SET `minlevel` = 80, `unit_flags` = 32768 WHERE `entry` = 34734;
+
+UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80, `BaseAttackTime` = 10000 WHERE `entry` = 33108;
+UPDATE `creature_template_addon` SET `bytes2` = 1, `auras` = "62898" WHERE `entry` = 33108;
+UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80 WHERE `entry` = 33212;
+UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80, `BaseAttackTime` = 10000 WHERE `entry` = 33364;
+-- UPDATE `creature_template_addon` SET `bytes2` = 1, `auras` = "62897" WHERE `entry` = 33364;
+UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80 WHERE `entry` = 33365;
+UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80, `BaseAttackTime` = 10000 WHERE `entry` = 33366;
+-- UPDATE `creature_template_addon` SET `bytes2` = 1, `auras` = "62895" WHERE `entry` = 33366;
+UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80 WHERE `entry` = 33367;
+UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80, `BaseAttackTime` = 10000 WHERE `entry` = 33369;
+UPDATE `creature_template_addon` SET `bytes2` = 1, `auras` = "62898" WHERE `entry` = 33369;
+UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80 WHERE `entry` = 33370;
+UPDATE `creature_template` SET `minlevel` = 60, `maxlevel` = 60, `unit_flags` = 33554688 WHERE `entry` = 33874;
+
+UPDATE `creature_template` SET `minlevel` = 1, `maxlevel` = 1 WHERE `entry` = 32261;
+
+UPDATE `creature_template` SET `minlevel` = 79 WHERE `entry` = 31033;
