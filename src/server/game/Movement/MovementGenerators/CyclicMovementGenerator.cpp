dfab0416484575084e635eea04ddfd2a6bd9233b
Ovahlord <dreadkiller@gmx.de>
2021-01-15 16:01:18 +0100

    Core/Movement: initialize stalled member in CyclicMovementGenerator's 2nd ctor overload

--- src/server/game/Movement/MovementGenerators/CyclicMovementGenerator.cpp
+++ src/server/game/Movement/MovementGenerators/CyclicMovementGenerator.cpp
@@ -44,7 +44,7 @@ CyclicMovementGenerator<Creature>::CyclicMovementGenerator(WaypointPath const* p
         _velocity = *velocity;
 }
 
-CyclicMovementGenerator<Creature>::CyclicMovementGenerator() : _path(nullptr), _moveTimer(0)
+CyclicMovementGenerator<Creature>::CyclicMovementGenerator() : _path(nullptr), _moveTimer(0), _stalled(false)
 {
 }
 
