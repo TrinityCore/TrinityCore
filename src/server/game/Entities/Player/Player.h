cdfa85957e2726472f8afa70c66a7dfe57cf8ca6
Matan Shukry <matanshukry@gmail.com>
2021-01-17 18:08:22 +0200

    Core/logs: added xp info on login & logout (#25867)
    * Core/logs: added xp info on login & logout
    
    * - Moved getters to header
    - Changed %s to %u
    
    (cherry picked from commit bb24c6a11b2410f6dd4ef34f2b7fb87e4b01ba37)
    
    # Conflicts:
    #	src/server/game/Entities/Player/Player.h
    #	src/server/game/Handlers/CharacterHandler.cpp
    #	src/server/game/Server/WorldSession.cpp

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -966,6 +966,7 @@ class TC_GAME_API Player : public Unit, public GridObject<Player>
         void SetPvPDeath(bool on) { if (on) m_ExtraFlags |= PLAYER_EXTRA_PVP_DEATH; else m_ExtraFlags &= ~PLAYER_EXTRA_PVP_DEATH; }
 
         uint32 GetXP() const { return GetUInt32Value(PLAYER_XP); }
+        uint32 GetXPForNextLevel() const { return GetUInt32Value(PLAYER_NEXT_LEVEL_XP); }
         void SetXP(uint32 xp) { SetUInt32Value(PLAYER_XP, xp); }
         void GiveXP(uint32 xp, Unit* victim, float group_rate = 1.0f);
         void GiveLevel(uint8 level);
