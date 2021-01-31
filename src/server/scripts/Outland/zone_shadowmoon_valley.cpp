a9c83d556635a51c13cea5e231e879a67d2e77ff
offl <offl@users.noreply.github.com>
2021-01-31 18:39:25 +0200

    DB/Creature: Remove UNIT_FLAG_PACIFIED
    Ref #25764

--- src/server/scripts/Outland/zone_shadowmoon_valley.cpp
+++ src/server/scripts/Outland/zone_shadowmoon_valley.cpp
@@ -59,6 +59,8 @@ enum InvisInfernalCaster
     MODEL_INVISIBLE            = 20577,
     MODEL_INFERNAL             = 17312,
     SPELL_SUMMON_INFERNAL      = 37277,
+    SPELL_SPAWN_AND_PACIFY     = 37791,
+    SPELL_TRANSFORM_INFERNAL   = 37794,
     TYPE_INFERNAL              = 1,
     DATA_DIED                  = 1
 };
@@ -153,7 +155,8 @@ public:
         void IsSummonedBy(WorldObject* summoner) override
         {
             if (summoner->ToCreature())
-                casterGUID = summoner->ToCreature()->GetGUID();;
+                casterGUID = summoner->ToCreature()->GetGUID();
+            DoCastSelf(SPELL_SPAWN_AND_PACIFY);
         }
 
         void JustDied(Unit* /*killer*/) override
@@ -166,9 +169,12 @@ public:
         {
             if (spellInfo->Id == SPELL_SUMMON_INFERNAL)
             {
-                me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_PACIFIED | UNIT_FLAG_NOT_SELECTABLE);
+                me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE);
                 me->SetImmuneToPC(false);
+                me->RemoveAurasDueToSpell(SPELL_SPAWN_AND_PACIFY);
+                // handle by the spell below when such auras will be not removed after evade
                 me->SetDisplayId(MODEL_INFERNAL);
+                // DoCastSelf(SPELL_TRANSFORM_INFERNAL);
             }
         }
 
