730a8da9e99fdb50459e9eb57d79ac995430125e
jackpoz <giacomopoz@gmail.com>
2021-02-06 11:20:32 +0100

    Core/Unit: Set stand state on damage only to players
    (cherry picked from commit 4cc1f850670b868a1d16833a2cf5c60715f749ef)

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -1040,8 +1040,8 @@ bool Unit::HasBreakableByDamageCrowdControlAura(Unit* excludeCasterChannel) cons
         }
     }
 
-    // check to see if victim is sitting
-    if (victim->GetStandState())
+    // make player victims stand up automatically
+    if (victim->GetStandState() && victim->IsPlayer())
         victim->SetStandState(UNIT_STAND_STATE_STAND);
 
     return damage;
