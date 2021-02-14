fe43899c8365b3bfd037a836ee010e5566c1e660
jackpoz <giacomopoz@gmail.com>
2021-02-14 16:12:45 +0100

    Core/Maps: Use a fixed offset instead of full collision height when retrieving floor Z
    Use a fixed offset 0.5f instead of full collision height when retrieving floor Z as a full collision height ended up on the floor above a few times. It makes more sense to cast the ray just a bit higher up than using a full collision height (which by default is 2.0f)

--- src/server/shared/SharedDefines.h
+++ src/server/shared/SharedDefines.h
@@ -24,6 +24,7 @@
 #include "SmartEnum.h"
 
 float const GROUND_HEIGHT_TOLERANCE = 0.05f; // Extra tolerance to z position to check if it is in air or on ground.
+constexpr float Z_OFFSET_FIND_HEIGHT = 0.5f;
 
 enum SpellEffIndex : uint8
 {
