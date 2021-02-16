116a9864d83b739f950fdc649331d3450f7a637f
Ovahlord <dreadkiller@gmx.de>
2021-02-16 17:09:46 +0100

    DB/Misc: removed all query field hacks from creature_template based on bruteforce data

---
+++ sql/updates/world/4.3.4/2021_02_16_00_world.sql
@@ -0,0 +1,86 @@
+UPDATE `creature_template` SET `type_flags`=134217728, `VerifiedBuild`=15595 WHERE `entry`=233; -- Farmer Saldean
+UPDATE `creature_template` SET `modelid2`=38373, `VerifiedBuild`=15595 WHERE `entry`=510; -- Water Elemental
+UPDATE `creature_template` SET `type_flags`=0, `VerifiedBuild`=15595 WHERE `entry`=739; -- Brother Nimetz
+UPDATE `creature_template` SET `type_flags`=0, `VerifiedBuild`=15595 WHERE `entry`=927; -- Brother Wilhelm
+UPDATE `creature_template` SET `type_flags`=0, `VerifiedBuild`=15595 WHERE `entry`=1182; -- Brother Anton
+UPDATE `creature_template` SET `type_flags`=0, `VerifiedBuild`=15595 WHERE `entry`=1351; -- Brother Cassius
+UPDATE `creature_template` SET `type_flags`=0, `VerifiedBuild`=15595 WHERE `entry`=1444; -- Brother Kristoff
+UPDATE `creature_template` SET `HealthModifier`=2, `VerifiedBuild`=15595 WHERE `entry`=2611; -- Fozruk
+UPDATE `creature_template` SET `HealthModifier`=0.150000005960464477, `VerifiedBuild`=15595 WHERE `entry`=2764; -- Sleeby
+UPDATE `creature_template` SET `HealthModifier`=0.150000005960464477, `VerifiedBuild`=15595 WHERE `entry`=2765; -- Znort
+UPDATE `creature_template` SET `type_flags`=0, `VerifiedBuild`=15595 WHERE `entry`=3963; -- Danlaar Nightstride
+UPDATE `creature_template` SET `HealthModifier`=0.150000005960464477, `VerifiedBuild`=15595 WHERE `entry`=4063; -- Feeboz
+UPDATE `creature_template` SET `type_flags`=0, `VerifiedBuild`=15595 WHERE `entry`=5484; -- Brother Benjamin
+UPDATE `creature_template` SET `type_flags`=0, `VerifiedBuild`=15595 WHERE `entry`=5489; -- Brother Joshua
+UPDATE `creature_template` SET `type_flags`=0, `VerifiedBuild`=15595 WHERE `entry`=5661; -- Brother Malach
+UPDATE `creature_template` SET `type_flags`=134217728, `VerifiedBuild`=15595 WHERE `entry`=6091; -- Dellylah
+UPDATE `creature_template` SET `type_flags`=134217728, `VerifiedBuild`=15595 WHERE `entry`=7952; -- Zjolnir
+UPDATE `creature_template` SET `type_flags`=0, `VerifiedBuild`=15595 WHERE `entry`=8140; -- Brother Karman
+UPDATE `creature_template` SET `type_flags`=0, `VerifiedBuild`=15595 WHERE `entry`=12336; -- Brother Crowley
+UPDATE `creature_template` SET `ManaModifier`=2, `VerifiedBuild`=15595 WHERE `entry`=15371; -- Sunstrider Guardian
+UPDATE `creature_template` SET `type_flags`=12, `VerifiedBuild`=15595 WHERE `entry`=15491; -- Eranikus, Tyrant of the Dream
+UPDATE `creature_template` SET `modelid2`=2001, `modelid3`=6802, `modelid4`=8589, `VerifiedBuild`=15595 WHERE `entry`=17432; -- Stillpine Defender
+UPDATE `creature_template` SET `modelid2`=2001, `modelid3`=6802, `modelid4`=8589, `VerifiedBuild`=15595 WHERE `entry`=17437; -- Stillpine Defender Corpse
+UPDATE `creature_template` SET `modelid2`=2001, `modelid3`=6802, `modelid4`=8589, `VerifiedBuild`=15595 WHERE `entry`=17439; -- Stillpine Hunter
+UPDATE `creature_template` SET `modelid1`=16480, `modelid2`=16925, `VerifiedBuild`=15595 WHERE `entry`=18955; -- Camera Shaker - 30-90 seconds
+UPDATE `creature_template` SET `modelid1`=1126, `modelid2`=16925, `VerifiedBuild`=15595 WHERE `entry`=21393; -- Cannon Channeler
+UPDATE `creature_template` SET `type`=10, `VerifiedBuild`=15595 WHERE `entry`=21861; -- Vision of the Raven God
+UPDATE `creature_template` SET `modelid1`=1126, `modelid2`=11686, `VerifiedBuild`=15595 WHERE `entry`=23398; -- Angered Soul Fragment
+UPDATE `creature_template` SET `modelid1`=1126, `modelid2`=11686, `type_flags`=0, `VerifiedBuild`=15595 WHERE `entry`=24438; -- Steel Gate - Grapple Target
+UPDATE `creature_template` SET `HealthModifier`=6, `ManaModifier`=4.754357814788818359, `VerifiedBuild`=15595 WHERE `entry`=25334; -- Horde Siege Tank
+UPDATE `creature_template` SET `HealthModifier`=1, `VerifiedBuild`=15595 WHERE `entry`=25743; -- Wooly Mammoth Bull
+UPDATE `creature_template` SET `HealthModifier`=5, `VerifiedBuild`=15595 WHERE `entry`=25755; -- Ahunite Hailstone
+UPDATE `creature_template` SET `HealthModifier`=0.75, `VerifiedBuild`=15595 WHERE `entry`=25756; -- Ahunite Coldwave
+UPDATE `creature_template` SET `HealthModifier`=0.75, `VerifiedBuild`=15595 WHERE `entry`=25757; -- Ahunite Frostwind
+UPDATE `creature_template` SET `HealthModifier`=25, `VerifiedBuild`=15595 WHERE `entry`=25865; -- Frozen Core
+UPDATE `creature_template` SET `type_flags`=0, `VerifiedBuild`=15595 WHERE `entry`=26044; -- Durkot Wolfbrother
+UPDATE `creature_template` SET `rank`=0, `VerifiedBuild`=15595 WHERE `entry`=28182; -- Dusk
+UPDATE `creature_template` SET `rank`=3, `VerifiedBuild`=15595 WHERE `entry`=28952; -- Akali
+UPDATE `creature_template` SET `IconName`='', `VerifiedBuild`=15595 WHERE `entry`=29884; -- Gymer
+UPDATE `creature_template` SET `type_flags`=8, `VerifiedBuild`=15595 WHERE `entry`=30134; -- Brann's Flying Machine
+UPDATE `creature_template` SET `type_flags`=4, `VerifiedBuild`=15595 WHERE `entry`=30399; -- Thorim
+UPDATE `creature_template` SET `modelid1`=1126, `modelid2`=11686, `VerifiedBuild`=15595 WHERE `entry`=30959; -- Lady Nightswood's Moveto Target Bunny
+UPDATE `creature_template` SET `HealthModifier`=2, `VerifiedBuild`=15595 WHERE `entry`=32178; -- Rampaging Ghoul
+UPDATE `creature_template` SET `HealthModifier`=1, `VerifiedBuild`=15595 WHERE `entry`=32181; -- Living Plague
+UPDATE `creature_template` SET `modelid1`=16480, `modelid2`=21342, `VerifiedBuild`=15595 WHERE `entry`=32445; -- Summoned Plague Cauldron Bunny 02
+UPDATE `creature_template` SET `HealthModifier`=0.75, `VerifiedBuild`=15595 WHERE `entry`=32484; -- Bone Spider
+UPDATE `creature_template` SET `type_flags`=4, `VerifiedBuild`=15595 WHERE `entry`=32588; -- Illidan Stormrage
+UPDATE `creature_template` SET `type_flags`=134221824, `VerifiedBuild`=15595 WHERE `entry`=32979; -- Gorbold Steelhand
+UPDATE `creature_template` SET `type_flags`=134479872, `VerifiedBuild`=15595 WHERE `entry`=36186; -- Gobber
+UPDATE `creature_template` SET `type_flags`=134479872, `VerifiedBuild`=15595 WHERE `entry`=36463; -- Gobber
+UPDATE `creature_template` SET `IconName`='', `VerifiedBuild`=15595 WHERE `entry`=36812; -- Stormwind Love Boat
+UPDATE `creature_template` SET `type_flags`=134217728, `VerifiedBuild`=15595 WHERE `entry`=37500; -- Vinny Slapchop
+UPDATE `creature_template` SET `IconName`='', `VerifiedBuild`=15595 WHERE `entry`=37966; -- Undercity Love Boat
+UPDATE `creature_template` SET `IconName`='', `VerifiedBuild`=15595 WHERE `entry`=37980; -- Darnassus Love Boat
+UPDATE `creature_template` SET `type_flags`=134217728, `VerifiedBuild`=15595 WHERE `entry`=38381; -- Brett "Coins" McQuid
+UPDATE `creature_template` SET `type_flags`=134479872, `VerifiedBuild`=15595 WHERE `entry`=38746; -- Gobber
+UPDATE `creature_template` SET `type_flags`=134217728, `VerifiedBuild`=15595 WHERE `entry`=39063; -- Chawg
+UPDATE `creature_template` SET `type_flags`=134217728, `VerifiedBuild`=15595 WHERE `entry`=39144; -- Alto Stonespire
+UPDATE `creature_template` SET `IconName`='', `VerifiedBuild`=15595 WHERE `entry`=39294; -- Battered Red Drake
+UPDATE `creature_template` SET `modelid1`=169, `modelid2`=11686, `VerifiedBuild`=15595 WHERE `entry`=40081; -- Orb Carrier
+UPDATE `creature_template` SET `modelid1`=169, `modelid2`=11686, `VerifiedBuild`=15595 WHERE `entry`=40091; -- Orb Rotation Focus
+UPDATE `creature_template` SET `modelid1`=169, `modelid2`=11686, `VerifiedBuild`=15595 WHERE `entry`=40470; -- Orb Carrier (1)
+UPDATE `creature_template` SET `modelid1`=169, `modelid2`=11686, `VerifiedBuild`=15595 WHERE `entry`=40471; -- Orb Carrier (2)
+UPDATE `creature_template` SET `modelid1`=169, `modelid2`=11686, `VerifiedBuild`=15595 WHERE `entry`=40472; -- Orb Carrier (3)
+UPDATE `creature_template` SET `modelid1`=31701, `VerifiedBuild`=15595 WHERE `entry`=40818; -- Shadow of Obsidius (1)
+UPDATE `creature_template` SET `type_flags`=134217728, `VerifiedBuild`=15595 WHERE `entry`=41135; -- "Plucky" Johnson
+UPDATE `creature_template` SET `modelid1`=169, `modelid2`=11686, `VerifiedBuild`=15595 WHERE `entry`=43280; -- Orb Rotation Focus (1)
+UPDATE `creature_template` SET `modelid1`=169, `modelid2`=11686, `VerifiedBuild`=15595 WHERE `entry`=43281; -- Orb Rotation Focus (2)
+UPDATE `creature_template` SET `modelid1`=169, `modelid2`=11686, `VerifiedBuild`=15595 WHERE `entry`=43282; -- Orb Rotation Focus (3)
+UPDATE `creature_template` SET `type_flags`=134217728, `VerifiedBuild`=15595 WHERE `entry`=43418; -- Sprinkle Noggenfogger
+UPDATE `creature_template` SET `ManaModifier`=1, `VerifiedBuild`=15595 WHERE `entry`=43876; -- Asaad (1)
+UPDATE `creature_template` SET `ManaModifier`=1, `VerifiedBuild`=15595 WHERE `entry`=43879; -- Grand Vizier Ertan (1)
+UPDATE `creature_template` SET `type_flags`=134217728, `VerifiedBuild`=15595 WHERE `entry`=45286; -- KTC Train-a-Tron Deluxe
+UPDATE `creature_template` SET `type_flags`=134217728, `VerifiedBuild`=15595 WHERE `entry`=45417; -- Fiona
+UPDATE `creature_template` SET `modelid1`=8714, `VerifiedBuild`=15595 WHERE `entry`=45916; -- Armored Mistral (1)
+UPDATE `creature_template` SET `modelid1`=8714, `VerifiedBuild`=15595 WHERE `entry`=45920; -- Young Storm Dragon (1)
+UPDATE `creature_template` SET `modelid1`=8714, `VerifiedBuild`=15595 WHERE `entry`=45923; -- Empyrean Assassin (1)
+UPDATE `creature_template` SET `type_flags`=134217728, `VerifiedBuild`=15595 WHERE `entry`=47383; -- Brivelthwerp
+UPDATE `creature_template` SET `movementId`=0, `VerifiedBuild`=15595 WHERE `entry`=49187; -- Pustulant Monstrosity (1)
+UPDATE `creature_template` SET `type_flags`=134217728, `VerifiedBuild`=15595 WHERE `entry`=49885; -- KTC Train-a-Tron Deluxe
+UPDATE `creature_template` SET `type_flags`=134217728, `VerifiedBuild`=15595 WHERE `entry`=49934; -- The Great Pisani
+UPDATE `creature_template` SET `movementId`=118, `VerifiedBuild`=15595 WHERE `entry`=50169; -- Blood of the Old God (2)
+UPDATE `creature_template` SET `movementId`=118, `VerifiedBuild`=15595 WHERE `entry`=50170; -- Blood of the Old God (3)
+UPDATE `creature_template` SET `modelid1`=23767, `modelid2`=1126, `VerifiedBuild`=15595 WHERE `entry`=50522; -- Mystery Toxin
+UPDATE `creature_template` SET `HealthModifier`=0.144589006900787353, `VerifiedBuild`=15595 WHERE `entry`=50562; -- Bloodthirsty Ghoul (1)
+UPDATE `creature_template` SET `HealthModifier`=3, `VerifiedBuild`=15595 WHERE `entry`=50616; -- Tormented Officer (1)
