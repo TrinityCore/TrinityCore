c7a538d43c0017ae58cee3cdc0c6815b258ea411
jackpoz <giacomopoz@gmail.com>
2021-02-21 22:12:13 +0100

    Move message about libbacktrace being linked in NOT WIN32 check

--- cmake/showoptions.cmake
+++ cmake/showoptions.cmake
@@ -172,9 +172,6 @@ if(WITH_STRICT_DATABASE_TYPE_CHECKS)
 endif()
 
 if(WITH_BOOST_STACKTRACE)
-  message("")
-  message("*** libbacktrace will be linked")
-  
   if (BOOST_STACKTRACE_BACKTRACE_INCLUDE_FILE)
     add_definitions(-DBOOST_STACKTRACE_BACKTRACE_INCLUDE_FILE="${BOOST_STACKTRACE_BACKTRACE_INCLUDE_FILE}")
   endif()
