08f8237dc528a3f8e7eebed2d42f89fcddf468da
Jakub C <jakub.cieplinski@outlook.com>
2021-02-22 18:17:37 +0100

    First version of command handler

--- src/server/game/Accounts/RBAC.h
+++ src/server/game/Accounts/RBAC.h
@@ -748,6 +748,7 @@ enum RBACPermissions
     // IF YOU ADD NEW PERMISSIONS, ADD THEM IN MASTER BRANCH AS WELL!
     //
     // custom permissions 1000+
+    RBAC_PERM_COMMAND_REVIVEALL                                 = 882,
     RBAC_PERM_MAX
 };
 
