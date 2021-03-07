d3f142c17c9982dc1c5f8bfbfd94df120156e08b
MistiX <mistix20@gmail.com>
2021-03-07 16:35:30 +0100

    Adding files to get started

---
+++ src/server/scripts/Shadowlands/ExilesReach/zone_exiles_reach.cpp
@@ -0,0 +1,18 @@
+#include "ScriptMgr.h"
+#include "Player.h"
+#include "Creature.h"
+#include "CreatureAI.h"
+#include "GameObject.h"
+#include "GameObjectAI.h"
+#include "AreaTriggerAI.h"
+#include "AreaTrigger.h"
+#include "InstanceScript.h"
+#include "CombatAI.h"
+#include "PhasingHandler.h"
+#include "Log.h"
+#include "Chat.h"
+
+void AddSC_zone_exiles_reach()
+{    
+   
+}
