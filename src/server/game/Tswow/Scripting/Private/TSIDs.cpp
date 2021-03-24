ea153350aece6dff7fcf563af00d57390dddf773
ihm-ts <ihm-ts@protonmail.com>
2021-03-24 12:53:20 +0100

    tswow/trinitycore: add license headers

--- src/server/game/Tswow/Scripting/Private/TSIDs.cpp
+++ src/server/game/Tswow/Scripting/Private/TSIDs.cpp
@@ -1,3 +1,19 @@
+/*
+ * This file is part of tswow (https://github.com/tswow/).
+ * Copyright (C) 2021 tswow <https://github.com/tswow/>
+ * 
+ * This program is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation, version 3.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 #include "TSString.h"
 #include "TSIDs.h"
 #include "Config.h"
