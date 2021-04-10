bc5579362db41ebed9b2d04ffe3245ef16084e65
Gildor <gildor55@gmail.com>
2021-04-10 12:28:05 +0200

    apply @Ovahlord feedback

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -870,7 +870,7 @@ void AuraEffect::PeriodicTick(AuraApplication* aurApp, Unit* caster) const
 
     // Update serverside orientation of tracking channeled auras on periodic update ticks
     // exclude players because can turn during channeling and shouldn't desync orientation client/server
-    if (caster && caster->GetTypeId() != TYPEID_PLAYER && m_spellInfo->IsChanneled() && m_spellInfo->HasAttribute(SPELL_ATTR1_CHANNEL_TRACK_TARGET))
+    if (caster && !caster->IsPlayer() && m_spellInfo->IsChanneled() && m_spellInfo->HasAttribute(SPELL_ATTR1_CHANNEL_TRACK_TARGET))
     {
         ObjectGuid const channelGuid = caster->GetChannelObjectGuid();
         if (!channelGuid.IsEmpty() && channelGuid != caster->GetGUID())
