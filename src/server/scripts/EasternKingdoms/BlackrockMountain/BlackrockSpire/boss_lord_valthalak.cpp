6f7dae94f479137047cd043765bbbcc172c5c253
offl <offl@users.noreply.github.com>
2021-04-15 00:18:03 +0300

    Scripts/BRS: Update scripts to new model (2/3)

--- src/server/scripts/EasternKingdoms/BlackrockMountain/BlackrockSpire/boss_lord_valthalak.cpp
+++ src/server/scripts/EasternKingdoms/BlackrockMountain/BlackrockSpire/boss_lord_valthalak.cpp
@@ -40,110 +40,99 @@ enum Events
     EVENT_SHADOW_WRATH              = 3
 };
 
-class boss_lord_valthalak : public CreatureScript
+struct boss_lord_valthalak : public BossAI
 {
-public:
-    boss_lord_valthalak() : CreatureScript("boss_lord_valthalak") { }
-
-    struct boss_lord_valthalakAI : public BossAI
+    boss_lord_valthalak(Creature* creature) : BossAI(creature, DATA_LORD_VALTHALAK)
     {
-        boss_lord_valthalakAI(Creature* creature) : BossAI(creature, DATA_LORD_VALTHALAK)
-        {
-            Initialize();
-        }
+        Initialize();
+    }
 
-        void Initialize()
-        {
-            frenzy40 = false;
-            frenzy15 = false;
-        }
+    void Initialize()
+    {
+        frenzy40 = false;
+        frenzy15 = false;
+    }
 
-        void Reset() override
-        {
-            _Reset();
-            Initialize();
-        }
+    void Reset() override
+    {
+        _Reset();
+        Initialize();
+    }
 
-        void JustEngagedWith(Unit* who) override
-        {
-            BossAI::JustEngagedWith(who);
-            events.ScheduleEvent(EVENT_SUMMON_SPECTRAL_ASSASSIN, 6s, 8s);
-            events.ScheduleEvent(EVENT_SHADOW_WRATH, 9s, 18s);
-        }
+    void JustEngagedWith(Unit* who) override
+    {
+        BossAI::JustEngagedWith(who);
+        events.ScheduleEvent(EVENT_SUMMON_SPECTRAL_ASSASSIN, 6s, 8s);
+        events.ScheduleEvent(EVENT_SHADOW_WRATH, 9s, 18s);
+    }
 
-        void JustDied(Unit* /*killer*/) override
-        {
-            instance->SetData(DATA_LORD_VALTHALAK, DONE);
-        }
+    void JustDied(Unit* /*killer*/) override
+    {
+        instance->SetData(DATA_LORD_VALTHALAK, DONE);
+    }
 
-        void UpdateAI(uint32 diff) override
-        {
-            if (!UpdateVictim())
-                return;
+    void UpdateAI(uint32 diff) override
+    {
+        if (!UpdateVictim())
+            return;
 
-            events.Update(diff);
+        events.Update(diff);
+
+        if (me->HasUnitState(UNIT_STATE_CASTING))
+            return;
+
+        while (uint32 eventId = events.ExecuteEvent())
+        {
+            switch (eventId)
+            {
+                case EVENT_SUMMON_SPECTRAL_ASSASSIN:
+                    DoCast(me, SPELL_SUMMON_SPECTRAL_ASSASSIN);
+                    events.ScheduleEvent(EVENT_SUMMON_SPECTRAL_ASSASSIN, 30s, 35s);
+                    break;
+                case EVENT_SHADOW_BOLT_VOLLEY:
+                    DoCastVictim(SPELL_SHADOW_BOLT_VOLLEY);
+                    events.ScheduleEvent(EVENT_SHADOW_BOLT_VOLLEY, 4s, 6s);
+                    break;
+                case EVENT_SHADOW_WRATH:
+                    DoCastVictim(SPELL_SHADOW_WRATH);
+                    events.ScheduleEvent(EVENT_SHADOW_WRATH, 19s, 24s);
+                    break;
+                default:
+                    break;
+            }
 
             if (me->HasUnitState(UNIT_STATE_CASTING))
                 return;
-
-            while (uint32 eventId = events.ExecuteEvent())
-            {
-                switch (eventId)
-                {
-                    case EVENT_SUMMON_SPECTRAL_ASSASSIN:
-                        DoCast(me, SPELL_SUMMON_SPECTRAL_ASSASSIN);
-                        events.ScheduleEvent(EVENT_SUMMON_SPECTRAL_ASSASSIN, 30s, 35s);
-                        break;
-                    case EVENT_SHADOW_BOLT_VOLLEY:
-                        DoCastVictim(SPELL_SHADOW_BOLT_VOLLEY);
-                        events.ScheduleEvent(EVENT_SHADOW_BOLT_VOLLEY, 4s, 6s);
-                        break;
-                    case EVENT_SHADOW_WRATH:
-                        DoCastVictim(SPELL_SHADOW_WRATH);
-                        events.ScheduleEvent(EVENT_SHADOW_WRATH, 19s, 24s);
-                        break;
-                    default:
-                        break;
-                }
-
-                if (me->HasUnitState(UNIT_STATE_CASTING))
-                    return;
-            }
-
-            if (!frenzy40)
-            {
-                if (HealthBelowPct(40))
-                {
-                    DoCast(me, SPELL_FRENZY);
-                    events.CancelEvent(EVENT_SUMMON_SPECTRAL_ASSASSIN);
-                    frenzy40 = true;
-                }
-            }
-
-            if (!frenzy15)
-            {
-                if (HealthBelowPct(15))
-                {
-                    DoCast(me, SPELL_FRENZY);
-                    events.ScheduleEvent(EVENT_SHADOW_BOLT_VOLLEY, 7s, 14s);
-                    frenzy15 = true;
-                }
-            }
-
-            DoMeleeAttackIfReady();
         }
-        private:
-            bool frenzy40;
-            bool frenzy15;
-    };
 
-    CreatureAI* GetAI(Creature* creature) const override
-    {
-        return GetBlackrockSpireAI<boss_lord_valthalakAI>(creature);
+        if (!frenzy40)
+        {
+            if (HealthBelowPct(40))
+            {
+                DoCast(me, SPELL_FRENZY);
+                events.CancelEvent(EVENT_SUMMON_SPECTRAL_ASSASSIN);
+                frenzy40 = true;
+            }
+        }
+
+        if (!frenzy15)
+        {
+            if (HealthBelowPct(15))
+            {
+                DoCast(me, SPELL_FRENZY);
+                events.ScheduleEvent(EVENT_SHADOW_BOLT_VOLLEY, 7s, 14s);
+                frenzy15 = true;
+            }
+        }
+
+        DoMeleeAttackIfReady();
     }
+    private:
+        bool frenzy40;
+        bool frenzy15;
 };
 
 void AddSC_boss_lord_valthalak()
 {
-    new boss_lord_valthalak();
+    RegisterBlackrockSpireCreatureAI(boss_lord_valthalak);
 }
