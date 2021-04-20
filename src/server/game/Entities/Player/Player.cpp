abb5164f551a114b5a8602e799f480736cab11e0
Roc13x <roc13x@gmail.com>
2021-04-20 15:06:10 +0100

    Core/Channel: Implement use of CHANNEL_DBC_FLAG_INITIAL (#256)
    Reduces differences with TC

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -4650,6 +4650,9 @@ void Player::UpdateLocalChannels(uint32 newZone)
         if (!channelEntry)
             continue;
 
+        if (!(channelEntry->Flags & CHANNEL_DBC_FLAG_INITIAL))
+            continue;
+
         Channel* usedChannel = nullptr;
         for (Channel* channel : m_channels)
         {
