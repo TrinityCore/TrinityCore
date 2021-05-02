985cff72d54d2cc411b2934dc0a69c743bd36716
jackpoz <giacomopoz@gmail.com>
2021-05-02 20:25:57 +0200

    Core/Pet: Attempt to fix an assertion triggered when re-summoning the current pet

--- src/server/scripts/Spells/spell_generic.cpp
+++ src/server/scripts/Spells/spell_generic.cpp
@@ -2841,6 +2841,9 @@ class spell_gen_pet_summoned : public SpellScript
         Player* player = GetCaster()->ToPlayer();
         if (player->GetLastPetNumber())
         {
+            if (HandlePetAlreadySummonedCase())
+                return;
+
             PetType newPetType = (player->GetClass() == CLASS_HUNTER) ? HUNTER_PET : SUMMON_PET;
             Pet* newPet = new Pet(player, newPetType);
             if (newPet->LoadPetFromDB(player, 0, player->GetLastPetNumber(), true))
@@ -2865,6 +2868,54 @@ class spell_gen_pet_summoned : public SpellScript
             else
                 delete newPet;
         }
+    }
+
+    bool HandlePetAlreadySummonedCase()
+    {
+        // The code below has been copied from Spell::EffectSummonPet() to handle the case of summoning the current pet again
+
+        Player* owner = GetCaster()->ToPlayer();
+        Pet* OldSummon = owner->GetPet();
+
+        // if pet requested type already exist
+        if (OldSummon)
+        {
+            uint32 lastPetNumber = owner->GetLastPetNumber();
+            if (OldSummon->GetCharmInfo()->GetPetNumber() == lastPetNumber)
+            {
+                // revive the pet if it is dead
+                if (OldSummon->getDeathState() != ALIVE && OldSummon->getDeathState() != JUST_RESPAWNED)
+                    OldSummon->setDeathState(JUST_RESPAWNED);
+
+                OldSummon->SetFullHealth();
+                OldSummon->SetPower(OldSummon->GetPowerType(), OldSummon->GetMaxPower(OldSummon->GetPowerType()));
+
+                ASSERT(OldSummon->GetMap() == owner->GetMap());
+
+                //OldSummon->GetMap()->Remove(OldSummon->ToCreature(), false);
+
+                float px, py, pz;
+                owner->GetClosePoint(px, py, pz, OldSummon->GetCombatReach());
+
+                OldSummon->NearTeleportTo(px, py, pz, OldSummon->GetOrientation());
+                //OldSummon->Relocate(px, py, pz, OldSummon->GetOrientation());
+                //OldSummon->SetMap(owner->GetMap());
+                //owner->GetMap()->Add(OldSummon->ToCreature());
+                if (OldSummon->getPetType() == SUMMON_PET)
+                {
+                    OldSummon->SetHealth(OldSummon->GetMaxHealth());
+                    OldSummon->SetPower(OldSummon->GetPowerType(), OldSummon->GetMaxPower(OldSummon->GetPowerType()));
+                    OldSummon->GetSpellHistory()->ResetAllCooldowns();
+                }
+
+                if (owner->GetTypeId() == TYPEID_PLAYER && OldSummon->isControlled())
+                    owner->ToPlayer()->PetSpellInitialize();
+
+                return true;
+            }
+        }
+
+        return false;
     }
 
     void Register() override
