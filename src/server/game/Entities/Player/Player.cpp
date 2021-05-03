af5202b52891c36d231af8214156b6be6411caf3
Shauren <shauren.trinity@gmail.com>
2021-05-03 20:10:56 +0200

    Core/Auras: Add missing SPELL_AURA_COMPREHEND_LANGUAGE check for serverside chat translations

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -22092,6 +22092,9 @@ bool Player::CanUnderstandLanguage(Language language) const
         if (languageDesc.second.SkillId && HasSkill(languageDesc.second.SkillId))
             return true;
 
+    if (HasAuraTypeWithMiscvalue(SPELL_AURA_COMPREHEND_LANGUAGE, language))
+        return true;
+
     return false;
 }
 
