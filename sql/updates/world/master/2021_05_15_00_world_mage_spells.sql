f484b63d0b4cc157d84584a84b1e87385f9ae5c4
Shauren <shauren.trinity@gmail.com>
2021-05-15 13:14:49 +0200

    Scripts/Spells: Remove manual damage adjustment from Arcane Barrage for scaling with arcane charges and use spell cast for mana energize

--- sql/updates/world/master/2021_05_15_00_world_mage_spells.sql
+++ sql/updates/world/master/2021_05_15_00_world_mage_spells.sql
@@ -3,3 +3,7 @@ DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_mage_arcane_barra
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (44425, 'spell_mage_arcane_barrage'),
 (1449, 'spell_mage_arcane_explosion');
+
+DELETE FROM `spell_proc` WHERE `SpellId` IN (36032);
+INSERT INTO `spell_proc` (`SpellId`,`SchoolMask`,`SpellFamilyName`,`SpellFamilyMask0`,`SpellFamilyMask1`,`SpellFamilyMask2`,`SpellFamilyMask3`,`ProcFlags`,`SpellTypeMask`,`SpellPhaseMask`,`HitMask`,`AttributesMask`,`DisableEffectsMask`,`ProcsPerMinute`,`Chance`,`Cooldown`,`Charges`) VALUES
+(36032,0,3,0x0,0x8000,0x0,0x0,0,1,1,0x2FFF,0x8,0,0,0,0,0); -- Arcane Charge, drop charges from Arcane Barrage only
