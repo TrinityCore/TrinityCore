68a490923d183c438fd852dde73f3054554d023a
Shauren <shauren.trinity@gmail.com>
2021-05-16 22:32:42 +0200

    Build: Set correct year in VER_LEGALCOPYRIGHT_STR

--- revision_data.h.in.cmake
+++ revision_data.h.in.cmake
@@ -11,7 +11,7 @@
  #define _MYSQL_EXECUTABLE          R"(@MYSQL_EXECUTABLE@)"
  #define _FULL_DATABASE             "TDB_full_world_335.21051_2021_05_14.sql"
  #define VER_COMPANYNAME_STR        "TrinityCore Developers"
- #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2019 TrinityCore"
+ #define VER_LEGALCOPYRIGHT_STR     "(c)2008-@rev_year@ TrinityCore"
  #define VER_FILEVERSION            0,0,0
  #define VER_FILEVERSION_STR        "@rev_hash@ @rev_date@ (@rev_branch@ branch)"
  #define VER_PRODUCTVERSION         VER_FILEVERSION
