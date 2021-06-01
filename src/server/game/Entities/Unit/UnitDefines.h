6258e059c93c740edc1211ccbf3c95a24326d5be
Chaouki Dhib <chaodhib@gmail.com>
2021-06-02 00:25:43 +0200

    fix formatting

--- src/server/game/Entities/Unit/UnitDefines.h
+++ src/server/game/Entities/Unit/UnitDefines.h
@@ -290,15 +290,15 @@ enum MovementFlags2 : uint32
 
 enum UnitMoveType
 {
-    MOVE_WALK = 0,
-    MOVE_RUN = 1,
-    MOVE_RUN_BACK = 2,
-    MOVE_SWIM = 3,
-    MOVE_SWIM_BACK = 4,
-    MOVE_TURN_RATE = 5,
-    MOVE_FLIGHT = 6,
-    MOVE_FLIGHT_BACK = 7,
-    MOVE_PITCH_RATE = 8
+    MOVE_WALK           = 0,
+    MOVE_RUN            = 1,
+    MOVE_RUN_BACK       = 2,
+    MOVE_SWIM           = 3,
+    MOVE_SWIM_BACK      = 4,
+    MOVE_TURN_RATE      = 5,
+    MOVE_FLIGHT         = 6,
+    MOVE_FLIGHT_BACK    = 7,
+    MOVE_PITCH_RATE     = 8
 };
 
 #define MAX_MOVE_TYPE     9
