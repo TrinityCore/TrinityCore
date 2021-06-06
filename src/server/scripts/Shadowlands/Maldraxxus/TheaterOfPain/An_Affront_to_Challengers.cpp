66e8df980e406f9599f9a7bc37f7b24dba48374a
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2021-06-06 19:01:23 +0300

    Update An_Affront_to_Challengers.cpp

--- src/server/scripts/Shadowlands/Maldraxxus/TheaterOfPain/An_Affront_to_Challengers.cpp
+++ src/server/scripts/Shadowlands/Maldraxxus/TheaterOfPain/An_Affront_to_Challengers.cpp
@@ -1,1 +1,12 @@
 
+
+#include "ScriptMgr.h"
+#include "Player.h"
+#include "Creature.h"
+#include "CreatureAI.h"
+#include "GameObject.h"
+#include "GameObjectAI.h"
+#include "AreaTrigger.h"
+#include "AreaTriggerAI.h"
+#include "InstanceScript.h"
+#include "theater_of_pain.h"
