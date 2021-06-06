091d38c67c3f76f7b0f70b5e3e1ce9aaa68d098f
offl <11556157+offl@users.noreply.github.com>
2021-06-07 00:26:02 +0300

    Scripts/Instances: Update Mechanar scripts to new register model (#26569)
    (cherry picked from commit 7b9476ea4b8dd43dcb041ced158bcdd46baeff85)

--- src/server/scripts/Outland/TempestKeep/Mechanar/boss_nethermancer_sepethrea.cpp
+++ src/server/scripts/Outland/TempestKeep/Mechanar/boss_nethermancer_sepethrea.cpp
@@ -60,178 +60,157 @@ enum Events
     EVENT_SOLARBURN                = 5
 };
 
-class boss_nethermancer_sepethrea : public CreatureScript
+struct boss_nethermancer_sepethrea : public BossAI
 {
-    public: boss_nethermancer_sepethrea(): CreatureScript("boss_nethermancer_sepethrea") { }
+    boss_nethermancer_sepethrea(Creature* creature) : BossAI(creature, DATA_NETHERMANCER_SEPRETHREA) { }
 
-        struct boss_nethermancer_sepethreaAI : public BossAI
+    void JustEngagedWith(Unit* who) override
+    {
+        BossAI::JustEngagedWith(who);
+        events.ScheduleEvent(EVENT_FROST_ATTACK, 7s, 10s);
+        events.ScheduleEvent(EVENT_ARCANE_BLAST, 12s, 18s);
+        events.ScheduleEvent(EVENT_DRAGONS_BREATH, 18s, 22s);
+        events.ScheduleEvent(EVENT_KNOCKBACK, 22s, 28s);
+        events.ScheduleEvent(EVENT_SOLARBURN, 30s);
+        Talk(SAY_AGGRO);
+        DoCast(who, SPELL_SUMMON_RAGIN_FLAMES);
+        Talk(SAY_SUMMON);
+    }
+
+    void KilledUnit(Unit* /*victim*/) override
+    {
+        Talk(SAY_SLAY);
+    }
+
+    void JustDied(Unit* /*killer*/) override
+    {
+        _JustDied();
+        Talk(SAY_DEATH);
+    }
+
+    void UpdateAI(uint32 diff) override
+    {
+        if (!UpdateVictim())
+            return;
+
+        events.Update(diff);
+
+        if (me->HasUnitState(UNIT_STATE_CASTING))
+            return;
+
+        while (uint32 eventId = events.ExecuteEvent())
         {
-            boss_nethermancer_sepethreaAI(Creature* creature) : BossAI(creature, DATA_NETHERMANCER_SEPRETHREA) { }
-
-            void JustEngagedWith(Unit* who) override
+            switch (eventId)
             {
-                BossAI::JustEngagedWith(who);
-                events.ScheduleEvent(EVENT_FROST_ATTACK, 7s, 10s);
-                events.ScheduleEvent(EVENT_ARCANE_BLAST, 12s, 18s);
-                events.ScheduleEvent(EVENT_DRAGONS_BREATH, 18s, 22s);
-                events.ScheduleEvent(EVENT_KNOCKBACK, 22s, 28s);
-                events.ScheduleEvent(EVENT_SOLARBURN, 30s);
-                Talk(SAY_AGGRO);
-                DoCast(who, SPELL_SUMMON_RAGIN_FLAMES);
-                Talk(SAY_SUMMON);
+                case EVENT_FROST_ATTACK:
+                    DoCastVictim(SPELL_FROST_ATTACK, true);
+                    events.ScheduleEvent(EVENT_FROST_ATTACK, 7s, 10s);
+                    break;
+                case EVENT_ARCANE_BLAST:
+                    DoCastVictim(SPELL_ARCANE_BLAST, true);
+                    events.ScheduleEvent(EVENT_ARCANE_BLAST, 15s);
+                    break;
+                case EVENT_DRAGONS_BREATH:
+                    DoCastVictim(SPELL_DRAGONS_BREATH, true);
+                    events.ScheduleEvent(EVENT_DRAGONS_BREATH, 12s, 22s);
+                    if (roll_chance_i(50))
+                        Talk(SAY_DRAGONS_BREATH);
+                    break;
+                case EVENT_KNOCKBACK:
+                    DoCastVictim(SPELL_KNOCKBACK, true);
+                    events.ScheduleEvent(EVENT_KNOCKBACK, 15s, 25s);
+                    break;
+                case EVENT_SOLARBURN:
+                    DoCastVictim(SPELL_SOLARBURN, true);
+                    events.ScheduleEvent(EVENT_SOLARBURN, 30s);
+                    break;
+                default:
+                    break;
             }
 
-            void KilledUnit(Unit* /*victim*/) override
-            {
-                Talk(SAY_SLAY);
-            }
-
-            void JustDied(Unit* /*killer*/) override
-            {
-                _JustDied();
-                Talk(SAY_DEATH);
-            }
-
-            void UpdateAI(uint32 diff) override
-            {
-                if (!UpdateVictim())
-                    return;
-
-                events.Update(diff);
-
-                if (me->HasUnitState(UNIT_STATE_CASTING))
-                    return;
-
-                while (uint32 eventId = events.ExecuteEvent())
-                {
-                    switch (eventId)
-                    {
-                        case EVENT_FROST_ATTACK:
-                            DoCastVictim(SPELL_FROST_ATTACK, true);
-                            events.ScheduleEvent(EVENT_FROST_ATTACK, 7s, 10s);
-                            break;
-                        case EVENT_ARCANE_BLAST:
-                            DoCastVictim(SPELL_ARCANE_BLAST, true);
-                            events.ScheduleEvent(EVENT_ARCANE_BLAST, 15s);
-                            break;
-                        case EVENT_DRAGONS_BREATH:
-                            DoCastVictim(SPELL_DRAGONS_BREATH, true);
-                            events.ScheduleEvent(EVENT_DRAGONS_BREATH, 12s, 22s);
-                            if (roll_chance_i(50))
-                                Talk(SAY_DRAGONS_BREATH);
-                            break;
-                        case EVENT_KNOCKBACK:
-                            DoCastVictim(SPELL_KNOCKBACK, true);
-                            events.ScheduleEvent(EVENT_KNOCKBACK, 15s, 25s);
-                            break;
-                        case EVENT_SOLARBURN:
-                            DoCastVictim(SPELL_SOLARBURN, true);
-                            events.ScheduleEvent(EVENT_SOLARBURN, 30s);
-                            break;
-                        default:
-                            break;
-                    }
-
-                    if (me->HasUnitState(UNIT_STATE_CASTING))
-                        return;
-                }
-
-                DoMeleeAttackIfReady();
-            }
-        };
-
-        CreatureAI* GetAI(Creature* creature) const override
-        {
-            return GetMechanarAI<boss_nethermancer_sepethreaAI>(creature);
+            if (me->HasUnitState(UNIT_STATE_CASTING))
+                return;
         }
+
+        DoMeleeAttackIfReady();
+    }
 };
 
-class npc_ragin_flames : public CreatureScript
+struct npc_ragin_flames : public ScriptedAI
 {
-    public:
-        npc_ragin_flames() : CreatureScript("npc_ragin_flames") { }
+    npc_ragin_flames(Creature* creature) : ScriptedAI(creature)
+    {
+        Initialize();
+        instance = creature->GetInstanceScript();
+    }
 
-            struct npc_ragin_flamesAI : public ScriptedAI
+    void Initialize()
+    {
+        inferno_Timer = 10000;
+        flame_timer = 500;
+        Check_Timer = 2000;
+        onlyonce = false;
+    }
+
+    InstanceScript* instance;
+
+    uint32 inferno_Timer;
+    uint32 flame_timer;
+    uint32 Check_Timer;
+
+    bool onlyonce;
+
+    void Reset() override
+    {
+        Initialize();
+    }
+
+    void JustEngagedWith(Unit* /*who*/) override
+    {
+    }
+
+    void UpdateAI(uint32 diff) override
+    {
+        //Check_Timer
+        if (Check_Timer <= diff)
+        {
+            if (instance->GetBossState(DATA_NETHERMANCER_SEPRETHREA) != IN_PROGRESS)
             {
-                npc_ragin_flamesAI(Creature* creature) : ScriptedAI(creature)
-                {
-                    Initialize();
-                    instance = creature->GetInstanceScript();
-                }
-
-                void Initialize()
-                {
-                    inferno_Timer = 10000;
-                    flame_timer = 500;
-                    Check_Timer = 2000;
-                    onlyonce = false;
-                }
-
-                InstanceScript* instance;
-
-                uint32 inferno_Timer;
-                uint32 flame_timer;
-                uint32 Check_Timer;
-
-                bool onlyonce;
-
-                void Reset() override
-                {
-                    Initialize();
-                }
-
-                void JustEngagedWith(Unit* /*who*/) override
-                {
-                }
-
-                void UpdateAI(uint32 diff) override
-                {
-                    //Check_Timer
-                    if (Check_Timer <= diff)
-                    {
-                        if (instance->GetBossState(DATA_NETHERMANCER_SEPRETHREA) != IN_PROGRESS)
-                        {
-                            //remove
-                            me->DespawnOrUnsummon();
-                            return;
-                        }
-                        Check_Timer = 1000;
-                    } else Check_Timer -= diff;
-
-                    if (!UpdateVictim())
-                        return;
-
-                    if (!onlyonce)
-                    {
-                        if (Unit* target = SelectTarget(SelectTargetMethod::Random, 0))
-                            me->GetMotionMaster()->MoveChase(target);
-                        onlyonce = true;
-                    }
-
-                    if (inferno_Timer <= diff)
-                    {
-                        DoCastVictim(SPELL_INFERNO);
-                        inferno_Timer = 10000;
-                    } else inferno_Timer -= diff;
-
-                    if (flame_timer <= diff)
-                    {
-                        DoCast(me, SPELL_FIRE_TAIL);
-                        flame_timer = 500;
-                    } else flame_timer -=diff;
-
-                    DoMeleeAttackIfReady();
-                }
-
-            };
-            CreatureAI* GetAI(Creature* creature) const override
-            {
-                return GetMechanarAI<npc_ragin_flamesAI>(creature);
+                //remove
+                me->DespawnOrUnsummon();
+                return;
             }
+            Check_Timer = 1000;
+        } else Check_Timer -= diff;
+
+        if (!UpdateVictim())
+            return;
+
+        if (!onlyonce)
+        {
+            if (Unit* target = SelectTarget(SelectTargetMethod::Random, 0))
+                me->GetMotionMaster()->MoveChase(target);
+            onlyonce = true;
+        }
+
+        if (inferno_Timer <= diff)
+        {
+            DoCastVictim(SPELL_INFERNO);
+            inferno_Timer = 10000;
+        } else inferno_Timer -= diff;
+
+        if (flame_timer <= diff)
+        {
+            DoCast(me, SPELL_FIRE_TAIL);
+            flame_timer = 500;
+        } else flame_timer -=diff;
+
+        DoMeleeAttackIfReady();
+    }
 };
 
 void AddSC_boss_nethermancer_sepethrea()
 {
-    new boss_nethermancer_sepethrea();
-    new npc_ragin_flames();
+    RegisterMechanarCreatureAI(boss_nethermancer_sepethrea);
+    RegisterMechanarCreatureAI(npc_ragin_flames);
 }
