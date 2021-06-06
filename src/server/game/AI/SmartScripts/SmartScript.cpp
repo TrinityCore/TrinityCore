8fb099e9aee4700694467fd199725cee67b870e4
jackpoz <giacomopoz@gmail.com>
2021-06-06 18:31:14 +0200

    Core/SAI: Add support to friendly+hostile to SMART_EVENT_OOC_LOS and SMART_EVENT_IC_LOS
    Change SMART_EVENT_OOC_LOS and SMART_EVENT_IC_LOS first parameter to have 3 values:
    - 0 : Hostile
    - 1 : Not hostile (not that Friendly and Not hostile have different meanings)
    - 2 : Any, as in both Hostile and Not hostile
    
    Close #24789

--- src/server/game/AI/SmartScripts/SmartScript.cpp
+++ src/server/game/AI/SmartScripts/SmartScript.cpp
@@ -3180,8 +3180,9 @@ void SmartScript::ProcessEvent(SmartScriptHolder& e, Unit* unit, uint32 var0, ui
             if (me->IsWithinDistInMap(unit, range) && me->IsWithinLOSInMap(unit))
             {
                 //if friendly event&&who is not hostile OR hostile event&&who is hostile
-                if ((e.event.los.noHostile && !me->IsHostileTo(unit)) ||
-                    (!e.event.los.noHostile && me->IsHostileTo(unit)))
+                if ((e.event.los.hostilityMode == static_cast<uint32>(SmartEvent::LOS::Any)) ||
+                    (e.event.los.hostilityMode == static_cast<uint32>(SmartEvent::LOS::NotHostile) && !me->IsHostileTo(unit)) ||
+                    (e.event.los.hostilityMode == static_cast<uint32>(SmartEvent::LOS::Hostile) && me->IsHostileTo(unit)))
                 {
                     if (e.event.los.playerOnly && unit->GetTypeId() != TYPEID_PLAYER)
                         return;
@@ -3202,8 +3203,9 @@ void SmartScript::ProcessEvent(SmartScriptHolder& e, Unit* unit, uint32 var0, ui
             if (me->IsWithinDistInMap(unit, range) && me->IsWithinLOSInMap(unit))
             {
                 //if friendly event&&who is not hostile OR hostile event&&who is hostile
-                if ((e.event.los.noHostile && !me->IsHostileTo(unit)) ||
-                    (!e.event.los.noHostile && me->IsHostileTo(unit)))
+                if ((e.event.los.hostilityMode == static_cast<uint32>(SmartEvent::LOS::Any)) ||
+                    (e.event.los.hostilityMode == static_cast<uint32>(SmartEvent::LOS::NotHostile) && !me->IsHostileTo(unit)) ||
+                    (e.event.los.hostilityMode == static_cast<uint32>(SmartEvent::LOS::Hostile) && me->IsHostileTo(unit)))
                 {
                     if (e.event.los.playerOnly && unit->GetTypeId() != TYPEID_PLAYER)
                         return;
