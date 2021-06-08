76c470fb3207c195804e789812a330394e81ecc3
Giacomo Pozzoni <giacomopoz@gmail.com>
2021-06-08 17:59:46 +0200

    Core/SAI: Add support to friendly+hostile to SMART_EVENT_OOC_LOS and SMART_EVENT_IC_LOS (#26579)
    Change SMART_EVENT_OOC_LOS and SMART_EVENT_IC_LOS first parameter to have 3 values:
    - 0 : Hostile
    - 1 : Not hostile (not that Friendly and Not hostile have different meanings)
    - 2 : Any, as in both Hostile and Not hostile
    
    Close #24789
    
    * Add validation on startup for allowed values.
    Code cleanup.
    
    * Fix errors
    
    (cherry picked from commit 9e7cdb02d290e85f27e3e463922cd4cfac46129a)

---
+++ sql/updates/world/master/2022_03_10_28_world_2021_06_08_00_world.sql
@@ -0,0 +1,4 @@
+--
+UPDATE `smart_scripts` SET `event_param1` = 2 WHERE `entryorguid` = 18428 AND `source_type` = 0 AND `id` = 0;
+UPDATE `smart_scripts` SET `event_param1` = 1, `event_param2` = 20 WHERE `entryorguid` = 19283 AND `source_type` = 0 AND `id` = 3;
+UPDATE `smart_scripts` SET `event_type` = 1, `event_param1` = 6000, `event_param2` = 11000, `target_type` = 19, `target_param1` = 24042, `target_param2` = 40 WHERE `entryorguid` = 23760 AND `source_type` = 0 AND `id` = 3;
