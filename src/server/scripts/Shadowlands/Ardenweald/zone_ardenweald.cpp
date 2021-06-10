dd1c0d11cb51384846014462b686653f5d7d01b3
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2021-06-10 13:40:46 +0300

    Update zone_ardenweald.cpp

--- src/server/scripts/Shadowlands/Ardenweald/zone_ardenweald.cpp
+++ src/server/scripts/Shadowlands/Ardenweald/zone_ardenweald.cpp
@@ -14,11 +14,36 @@
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
+#include "ScriptMgr.h"
+#include "Player.h"
+#include "InstanceScript.h"
+#include "instance_mists_of_tirna_scithe.cpp"
+#include "zone_mists_of_tirna_scithe.cpp"
+#include "zone_mists_of_tirna_scithe.h"
+#include "Objects_Mist_Of_Tirna_Scithe.h"
+#include "Boss_Ingra _Maloch.cpp"
+#include "Boss_Mistcaller.cpp"
+#include "Boss_Tred_ova.cpp"
 #include "zone_ardenweald.cpp"
 #include "Objects_Ardenweald.h"
 #include "npc_Ardenweald.h"
+#include "objects_De_Other_Side.h"
+#include "npc_De_Other_Side.h"
+#include "boss_dealer_xyexa.cpp"
+#include "boss_hakkar_the_soulflayer_os.cpp"
+#include "boss_muehzala.cpp"
+#include "boss_the_manastorms.cpp"
 
 void AddSC_zone_ardenweald()
 {
 }
+
+void AddSC_Zone_Mist_Of_Tirna_Scithe()
+{
+}
+
+void AddSC_Zone_De_Other_Side()
+{
+}
+
+   
