5dfe780a2a2ebf90016d1f98a54105e0030a680d
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2021-06-12 18:26:45 +0300

    Update Objects_De_Other_Side.h

--- src/server/scripts/Shadowlands/Ardenweald/DeOtherSide/Objects_De_Other_Side.h
+++ src/server/scripts/Shadowlands/Ardenweald/DeOtherSide/Objects_De_Other_Side.h
@@ -18,10 +18,10 @@
 #include "ScriptMgr.h"
 #include "Player.h"
 #include "InstanceScript.h"
-#include "instance_Heart_Of_The_Forest.cpp"
-#include "zone_Heart_Of_The_Forest.h"
-#include "Objects_Heart_Of_The_Forest.h"
-#include "npc_Heart_Of_The_Forest.h"
+#include "instance_de_other_side.cpp"
+#include "Zone_De_Other_Side.h"
+#include "Objects_De_Other_Side.h"
+#include "npc_De_Other_Side.h"
 #include "AreaTrigger.h"
 #include "AreaTriggerAI.h"
 #include "GameObject.h"
@@ -29,3 +29,109 @@
 #include "NpcObject.h"
 #include "NpcObjectAI.h"
 #include "SpellAuraEffects.h"
+#include "boss_dealer_xyexa.cpp"
+#include "boss_hakkar_the_soulflayer_os.cpp"
+#include "boss_muehzala.cpp"
+
+
+
+
+enum Objects
+{
+    Objects_Anvil                           = 192628,
+    Objects_Challengers_Cache               = 354985,
+    Objects_Crate_of_Gnomish_Machinations   = 355436,
+    Objects_Dusty_Cache_of_Ancient_Treasure = 355295,
+    Objects_Haunted_Urn                     = 357744,
+    Objects_Snow_Patch                      = 358375,
+}   
+
+void Objects_Anvil 
+ObjectGuid Anvil;
+
+void Objects_Challengers_Cache
+ObjectGuid Challengers_Cache;
+
+void Objects_Crate_of_Gnomish_Machinations
+ObjectGuid Crate_of_Gnomish_Machinations;
+
+void Objects_Dusty_Cache_of_Ancient_Treasure
+ObjectGuid Dusty_Cache_of_Ancient_Treasure;
+
+void Objects_Haunted_Urn
+ObjectGuid Haunted_Urn;
+
+void Objects_Snow_Patch
+ObjectGuid Snow_Patch;
+
+void OnGameObjectCreate(GameObject* go) override
+{
+    {
+        switch (go->GetEntry())
+        {
+        case GO_Objects_Anvil:
+            Anvil = go->GetGUID();
+            break;
+        }
+    }
+};
+
+void OnGameObjectCreate(GameObject* go) override
+{
+    {
+        switch (go->GetEntry())
+        {
+        case GO_Objects_Challengers_Cache:
+            Challengers_Cache = go->GetGUID();
+            break;
+        }
+    }
+};
+
+void OnGameObjectCreate(GameObject* go) override
+{
+    {
+        switch (go->GetEntry())
+        {
+        case GO_Objects_Crate_of_Gnomish_Machinations:
+            Crate_of_Gnomish_Machinations = go->GetGUID();
+            break;
+        }
+    }
+};
+
+void OnGameObjectCreate(GameObject* go) override
+{
+    {
+        switch (go->GetEntry())
+        {
+        case GO_Objects_Dusty_Cache_of_Ancient_Treasure:
+            Dusty_Cache_of_Ancient_Treasure = go->GetGUID();
+            break;
+        }
+    }
+};
+
+void OnGameObjectCreate(GameObject* go) override
+{
+    {
+        switch (go->GetEntry())
+        {
+        case GO_Objects_Haunted_Urn:
+            Haunted_Urn = go->GetGUID();
+            break;
+        }
+    }
+};
+
+void OnGameObjectCreate(GameObject* go) override
+{ 
+    {
+       swich (go->GetEntry())
+       {
+        case GO_Objects_Snow_Patch:
+            Snow_Patch = go->GetGUID();
+            break;
+        }
+    }
+};
