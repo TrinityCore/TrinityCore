41dbc498706c3ee6a74c3d04ee42fd33fd617b7d
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2021-06-14 11:26:56 +0300

    update

---
+++ src/server/scripts/Shadowlands/Bastion/NecroticWake/zone_necrotic_wake.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2021 
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "ScriptMgr.h"
+#include "Player.h"
+#include "InstanceScript.h"
+#include "necrotic_wake.cpp"
+#include "necrotic_wake.h"
+#include "instance_necrotic_wake.cpp"
+#include "Objects_necrotic_wake.h"
+#include "npc_necrotic_wake.h"
+#include "boss_amarth.cpp"
+#include "boss_blightbone.cpp"
+#include "boss_nalthor.cpp"
+#include "boss_surgeon_stitchflesh.cpp"
+
+void AddSC_zone_necrotic_wake()
+{
+}
+
+void AddSC_necrotic_wake()
+{
+}	
