1fdd62726729931fac5fec126b90ebb548e92491
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2021-06-14 11:33:24 +0300

    Create zone_spires_of_ascensions.h

---
+++ src/server/scripts/Shadowlands/Bastion/SpiresOfAscension/zone_spires_of_ascensions.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2021
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "ScriptMgr.h"
+#include "Player.h"
+#include "InstanceScript.h"
+#include "instance_spires_of_ascension.cpp"
+#include "spires_of_ascension.h"
+#include "boss_devos.cpp"
+#include "boss_kintara.cpp"
+#include "boss_oryphrion.cpp"
+#include "boss_ventunax.cpp"
+#include "Objects_spires_of_ascension.h"
+#include "npc_spires_of_ascension.h"
