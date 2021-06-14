7e1cff3e1e749ec06970309cc1d6b04018548eee
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2021-06-14 11:44:23 +0300

    Create zone_darkmaul_citadel.h

---
+++ src/server/scripts/Shadowlands/ExilesReach/DarkmaulCitadel/zone_darkmaul_citadel.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2021
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "ScriptMgr.h"
+#include "Player.h"
+#include "InstanceScript.h"
+#include "instance_darkmaul_citadel.cpp"
+#include "darkmaul_citadel.h"
+#include "darkmaul_citadel.cpp"
+#include "boss_gorgroth.cpp"
+#include "boss_ravnyr.cpp"
+#include "boss_tunk.cpp"
+#include "Objects_darkmaul_citadel.h"
+#include "npc_darkmaul_citadel.h"
+
+void AddSC_Zone_darkmaul_citadel();
+{
+}
+
+void AddSC_darkmaul_citadel();
+{
+}
+    
