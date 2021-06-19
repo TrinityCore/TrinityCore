f34cba3092b2f426c7d6ae78c7490f018867eea6
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2021-06-19 16:41:52 +0300

    Update Boss_The_Eye_of_the_Jailer.cpp

--- src/server/scripts/Shadowlands/TheMaw/Sanctum of Domination/Boss_The_Eye_of_the_Jailer.cpp
+++ src/server/scripts/Shadowlands/TheMaw/Sanctum of Domination/Boss_The_Eye_of_the_Jailer.cpp
@@ -7,12 +7,24 @@
 #include "zone_Sanctum_of_Domination.cpp"
 #include "zone_Sanctum_of_Domination.h"
 #include "instance_Sanctum_of_Domination.cpp"
-#include "MON_Crucible_Soulseeker_Attack_01_174592.OGG"
-#include "MON_Crucible_Soulseeker_Attack_01_174592"
+#include "src/server/scripts/Shadowlands/TheMaw/Sanctum of Domination/Sanctum_of_Domination_Data/MON_Crucible_Soulseeker_Attack_01_174592.ogg"
+#include "src/server/scripts/Shadowlands/TheMaw/Sanctum of Domination/Sanctum_of_Domination_Data/MON_Crucible_Soulseeker_Attack_02_174592.ogg"
+#include "src/server/scripts/Shadowlands/TheMaw/Sanctum of Domination/Sanctum_of_Domination_Data/MON_Crucible_Soulseeker_Attack_03_174592.ogg"
+#include "src/server/scripts/Shadowlands/TheMaw/Sanctum of Domination/Sanctum_of_Domination_Data/MON_Crucible_Soulseeker_Attack_04_174592.ogg"
+#include "src/server/scripts/Shadowlands/TheMaw/Sanctum of Domination/Sanctum_of_Domination_Data/MON_Crucible_Soulseeker_Attack_05_174592.ogg"
+#include "src/server/scripts/Shadowlands/TheMaw/Sanctum of Domination/Sanctum_of_Domination_Data/MON_Crucible_Soulseeker_Attack_06_174592.ogg"
+#include "src/server/scripts/Shadowlands/TheMaw/Sanctum of Domination/Sanctum_of_Domination_Data/MON_Crucible_Soulseeker_Attack_07_174592.ogg"
+#include "src/server/scripts/Shadowlands/TheMaw/Sanctum of Domination/Sanctum_of_Domination_Data/MON_Crucible_Soulseeker_Attack_08_174592.ogg"
+
 enum Creatures
 {
 
  NPC_Eye_of_the_Jailer                 = 180018,
+};
+
+enum achivements
+{
+  achivements_Mythic: The Eye of the Jailer = 15113,
 };
 
 enum Sounds
@@ -28,4 +40,46 @@ enum Sounds
   TempMono	// NPC Combat	Fidget	
   TempMono	// NPC Combat	Fidget	
   MON_MawSworn_loop_01_171773
-}
+};
+
+enum Loot_mode
+{
+  void NPC_Eye_of_the_Jailer() 
+  mode_LFR()
+   {
+     item_Artifice_of_the_Archmage = 181738,
+     item_Blood_Bond = 181963,
+     item_Corrupting_Leer =182478,
+     item_Expurgation = 182462,
+     item_Eye_of_Allseeing = 186554,
+     item_Fade_to_Nothing = 183500,
+     item_Lights_Inspiration = 181840,
+     item_Marksmans_Advantage = 182441,
+     item_Safeguard = 180896,
+     item_Tumbling_Technique = 181640,
+     item_Well_Honed_Instincts = 183486,
+     item_Airborne_Abductors_Vambraces = 186316,
+     item_Coiled_Stygian Grapnel = 186301,
+     item_Gazepiercer = 186383,
+     item_Grasps_of_the_Clairvoyant_Sage = 186288,
+     item_Greaves_of_Extermination = 186306,
+     item_Guarms_Lost_Chew_Toy = 186418,
+     item_Loyal_Kvaldirs_Handwraps = 186295,
+     item_Mawsworn_Eviscerators_Cuirass = 186296,
+     item_Stygian_Ember = 186599,
+     item_Stygian_Lance_of_Passage = 186403,
+     item_Titanic_Ocular_Gland = 186423,
+   };
+  mode_N()
+    {
+      item_1;
+    };
+  mode_HC()
+    {
+      item_1;
+    };
+  mode_Mythic()
+    {
+      item_1;
+    };
+};
