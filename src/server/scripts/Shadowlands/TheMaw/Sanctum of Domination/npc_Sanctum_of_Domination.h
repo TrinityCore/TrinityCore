03565b25cc53be483993f3caf77b6d90a3b99486
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2021-06-19 16:59:55 +0300

    Update npc_Sanctum_of_Domination.h

--- src/server/scripts/Shadowlands/TheMaw/Sanctum of Domination/npc_Sanctum_of_Domination.h
+++ src/server/scripts/Shadowlands/TheMaw/Sanctum of Domination/npc_Sanctum_of_Domination.h
@@ -23,7 +23,7 @@
 #include "zone_Sanctum_of_Domination.cpp"
 #include "zone_Sanctum_of_Domination.h"
 #include "instance_Sanctum_of_Domination.cpp"
-#include "AddSC_Sanctum_of_Domination.h"
+#include "Sanctum_of_Domination.h"
 #include "Boss_The_Tarragrue.cpp"
 #include "Boss_The_Eye_of_the_Jailer.cpp"
 #include "Boss_The_Nine"
@@ -38,115 +38,116 @@
 enum Creatures
 {
 
- NPC_Hakkar_the_Soulflayer             = 166473,
- NPC_Millificent_Manastorm             = 101976,
- NPC_Muehzala                          = 169769,
- NPC_4.RF_4.RF                         = 167964,
- NPC_Ancient_Kodo                      = 151843,
- NPC_Ancient_Swoop                     = 151846,
- NPC_Atalai_Deathwalker                = 170480,
- NPC_Atalai_Deathwalkers_Spirit        = 170483,
- NPC_Atalai_Devoted                    = 170486,171333,
- NPC_Atalai_High_Priest                = 170490,
- NPC_Atalai_Hoodoo_Hexxer              = 170572,
- NPC_Bladebeak_Hatchling               = 171341,
- NPC_Bladebeak_Matriarch               = 171343,
- NPC_Bwonsamdi                         = 166281,168141,171472,
- NPC_Dealer_Xyexa                      = 164450,
- NPC_Death_Speaker                     = 168942,
- NPC_Defunct_Dental_Drill              = 167962,
- NPC_Doomguard                         = 168932,
- NPC_Enraged_Mask                      = 169912,
- NPC_Enraged_Spirit                    = 168934,
- NPC_Ethereal_Transmorpher             = 149596,
- NPC_Experimental_Sludge               = 167966,
- NPC_Felguard                          = 169421,
- NPC_Felhound                          = 169425,
- NPC_Fleeting_Memory                   = 175581,
- NPC_Gormling                          = 175665,
- NPC_Gravid_Devourer_Mite              = 175692,
- NPC_Hakkar_the_Soulflayer             = 164558,
- NPC_Headless_Client                   = 167963,
- NPC_Infernal                          = 169426,
- NPC_Juvenile_Runestag                 = 171342,
- NPC_Lubricator                        = 167965,
- NPC_Manifestation_of_Pride            = 173729,
- NPC_Mask_of_Bwonsamdi                 = 174327,
- NPC_Meikothe_Clutch_Guardian          = 173458,
- NPC_Merchant_Maku                     = 142668,
- NPC_Millhouse Manastorm               = 164556,
- NPC_Millificent_Manastorm             = 164555,
- NPC_Muehzala                          = 166608,
- NPC_Mythresh_Skys_Talons              = 171184,
- NPC_Nathrian_Inquisitor               = 175513,
- NPC_Risen_Bonesoldier                 = 168949,
- NPC_Risen_Cultist                     = 168992,
- NPC_Risen_Warlord                     = 169905,
- NPC_Runestag_Elderhorn                = 164873,
- NPC_Sentient_Oil                      = 167967,
- NPC_Shadowlands_Wormhole              = 169501,
- NPC_Shattered_Visage                  = 168326,
- NPC_Shivarra                          = 169429,
- NPC_Skeletal_Raptor                   = 168986,
- NPC_Son_of_Hakkar                     = 165905,170488,
- NPC_Spiteful_Shade                    = 174773,
- NPC_Spriggan_Barkbinder               = 164861,
- NPC_Spriggan_Mendbender               = 164857,
- NPC_Territorial_Bladebeak             = 171181,
- NPC_Turnip_Punching_Bag               = 65310,
- NPC_Urzul                             = 169430,
- NPC_Volatile_Memory                   = 170147,
- NPC_Weald_Shimmermoth                 = 164862,
- NPC_Wrathguard                        = 169428,
+ NPC_Eye_of_the_Jailer                 = 180018,
+ NPC_  = 101976,
+ NPC_  = 169769,
+ NPC_  = 167964,
+ NPC_  = 151843,
+ NPC_  = 151846,
+ NPC_  = 170480,
+ NPC_  = 170483,
+ NPC_  = 170486,171333,
+ NPC_  = 170490,
+ NPC_  = 170572,
+ NPC_  = 171341,
+ NPC_  = 171343,
+ NPC_  = 166281,168141,171472,
+ NPC_  = 164450,
+ NPC_  = 168942,
+ NPC_  = 167962,
+ NPC_  = 168932,
+ NPC_  = 169912,
+ NPC_  = 168934,
+ NPC_  = 149596,
+ NPC_  = 167966,
+ NPC_  = 169421,
+ NPC_  = 169425,
+ NPC_  = 175581,
+ NPC_  = 175665,
+ NPC_  = 175692,
+ NPC_  = 164558,
+ NPC_  = 167963,
+ NPC_  = 169426,
+ NPC_  = 171342,
+ NPC_  = 167965,
+ NPC_  = 173729,
+ NPC_  = 174327,
+ NPC_  = 173458,
+ NPC_  = 142668,
+ NPC_  = 164556,
+ NPC_  = 164555,
+ NPC_  = 166608,
+ NPC_  = 171184,
+ NPC_  = 175513,
+ NPC_  = 168949,
+ NPC_  = 168992,
+ NPC_  = 169905,
+ NPC_  = 164873,
+ NPC_  = 167967,
+ NPC_  = 169501,
+ NPC_  = 168326,
+ NPC_  = 169429,
+ NPC_  = 168986,
+ NPC_  = 165905,170488,
+ NPC_  = 174773,
+ NPC_  = 164861,
+ NPC_  = 164857,
+ NPC_  = 171181,
+ NPC_  = 65310,
+ NPC_  = 169430,
+ NPC_  = 170147,
+ NPC_  = 164862,
+ NPC_  = 169428,
 };
 
 void OnCreatureCreate(Creature* creature) override
 {
-        switch (creature->GetEntry())
-        {
-        case NPC_Hakkar_the_Soulflayer:
-            Hakkar_the_Soulflayer = creature->GetGUID();
+    switch (creature->GetEntry())
+    {
+    case NPC_Eye_of_the_Jailer:
+        Eye_of_the_Jailer = creature->GetGUID();
             break;
         }
-        case NPC_Millificent_Manastorm:
-            Millificent_Manastorm = creature->GetGUID();
+        case NPC_:
+             = creature->GetGUID();
             break;
         }
-        case NPC_Muehzala:
-            Muehzala = creature->GetGUID();
+        case NPC_:
+             = creature->GetGUID();
             break;
         }
-        case NPC_4.RF_4.RF:
-            4.RF_4.RF = creature->GetGUID();
+        case NPC_:
+             = creature->GetGUID();
             break;
         }
-        case NPC_Ancient_Kodo:
-            Ancient_Kodo = creature->GetGUID();
+        case NPC_:
+             = creature->GetGUID();
             break;
         }
-        case NPC_Ancient_Swoop:
-            Ancient_Swoop = creature->GetGUID();
+        case NPC_:
+             = creature->GetGUID();
             break;
         }
-        case NPC_Atalai_Deathwalker:
-            Atalai_Deathwalker = creature->GetGUID();
+        case NPC_:
+             = creature->GetGUID();
             break;
         }
-        case NPC_Atalai_Deathwalkers_Spirit:
-            Atalai_Deathwalkers_Spirit = creature->GetGUID();
+        case NPC_:
+             = creature->GetGUID();
             break;
         }
-        case NPC_Atalai_Devoted:
-            Atalai_Devoted = creature->GetGUID();
+        case :
+             = creature->GetGUID();
             break;
         }
-        case NPC_Atalai_High_Priest:
-            Atalai_High_Priest = creature->GetGUID();
+        case NPC_:
+             = creature->GetGUID();
             break;
         }
-        case NPC_Atalai_Hoodoo_Hexxer:
-            Atalai_Hoodoo_Hexxer = creature->GetGUID();
+        case NPC_:
+             = creature->GetGUID();
             break;
         }
-                                                   
+    };    
+};                                               
 
