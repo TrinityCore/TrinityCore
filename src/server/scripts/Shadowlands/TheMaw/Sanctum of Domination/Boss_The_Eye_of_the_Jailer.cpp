58bd1dba11034d3f0728afe5581b0777f232be1f
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2021-06-19 15:52:30 +0300

    Update Boss_The_Eye_of_the_Jailer.cpp

--- src/server/scripts/Shadowlands/TheMaw/Sanctum of Domination/Boss_The_Eye_of_the_Jailer.cpp
+++ src/server/scripts/Shadowlands/TheMaw/Sanctum of Domination/Boss_The_Eye_of_the_Jailer.cpp
@@ -7,3 +7,25 @@
 #include "zone_Sanctum_of_Domination.cpp"
 #include "zone_Sanctum_of_Domination.h"
 #include "instance_Sanctum_of_Domination.cpp"
+#include "MON_Crucible_Soulseeker_Attack_01_174592.OGG"
+#include "MON_Crucible_Soulseeker_Attack_01_174592"
+enum Creatures
+{
+
+ NPC_Eye_of_the_Jailer                 = 180018,
+};
+
+enum Sounds
+{
+  MON_Crucible_Soulseeker_Attack_01_174592	// Spells	Attack	
+  MON_Crucible_Soulseeker_Attack_01_174592	// Spells	Critical strike	
+  MON_Crucible_Soulseeker_Wound_01_174593	// Spells	Injury	
+  MON_Crucible_Soulseeker_Wound_01_174593	// Spells	Critical injury	
+  MON_Crucible_Soulseeker_Attack_01_174592	// Spells	Death	
+  MON_Crucible_Soulseeker_Wound_01_174593	// NPC Combat	Aggro	
+  FX_ChainGauntlet_Movement01	 //NPC Combat	Fidget	
+  MON_Talethis_Target_Fidget01_01_168902	// NPC Combat	Fidget	
+  TempMono	// NPC Combat	Fidget	
+  TempMono	// NPC Combat	Fidget	
+  MON_MawSworn_loop_01_171773
+}
