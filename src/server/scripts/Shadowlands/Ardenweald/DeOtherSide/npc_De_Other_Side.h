979446356efda1134b4c693f2898c0b5edab885a
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2021-06-20 13:23:12 +0300

    Update npc_De_Other_Side.h

--- src/server/scripts/Shadowlands/Ardenweald/DeOtherSide/npc_De_Other_Side.h
+++ src/server/scripts/Shadowlands/Ardenweald/DeOtherSide/npc_De_Other_Side.h
@@ -1,6 +1,6 @@
 
 /*
- * Copyright 2021 Thordekk
+ * Copyright 2021 FuzionCore
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
