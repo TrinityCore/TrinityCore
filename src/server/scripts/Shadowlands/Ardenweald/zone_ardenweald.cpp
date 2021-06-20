829b7ca9a91b36574be429a118c0d826b78e5b1c
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2021-06-20 19:19:30 +0300

    Update zone_ardenweald.cpp

--- src/server/scripts/Shadowlands/Ardenweald/zone_ardenweald.cpp
+++ src/server/scripts/Shadowlands/Ardenweald/zone_ardenweald.cpp
@@ -1,5 +1,23 @@
+
 /*
  * Copyright 2023 AzgathCore
+ * Copyright 2021 
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+ * Copyright 2021 
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -17,33 +35,47 @@
 #include "ScriptMgr.h"
 #include "Player.h"
 #include "InstanceScript.h"
-#include "instance_mists_of_tirna_scithe.cpp"
-#include "zone_mists_of_tirna_scithe.cpp"
-#include "zone_mists_of_tirna_scithe.h"
-#include "Objects_Mist_Of_Tirna_Scithe.h"
-#include "Boss_Ingra _Maloch.cpp"
-#include "Boss_Mistcaller.cpp"
-#include "Boss_Tred_ova.cpp"
-#include "zone_ardenweald.cpp"
-#include "Objects_Ardenweald.h"
-#include "npc_Ardenweald.h"
-#include "objects_De_Other_Side.h"
+#include "instance_de_other_side.cpp"
+#include "zone_De_Other_Side.cpp"
+#include "zone_De_Other_Side.h"
 #include "npc_De_Other_Side.h"
+#include "Objects_De_Other_Side.h"
+#include "De_Other_Side.h"
 #include "boss_dealer_xyexa.cpp"
 #include "boss_hakkar_the_soulflayer_os.cpp"
 #include "boss_muehzala.cpp"
 #include "boss_the_manastorms.cpp"
+#include "Heart_Of_The_Forest.cpp"
+#include "zone_Heart_Of_The_Forest.cpp"
+#include "zone_Heart_Of_The_Forest.h"
+#include "npc_Heart_Of_The_Forest.h"
+#include "Objects_Heart_Of_The_Forest.h"
+#include "instance_mists_of_tirna_scithe.cpp"
+#include "zone_mists_of_tirna_scithe.cpp"
+#include "zone_mists_of_tirna_scithe.h"
+#include "Objects_Mist_Of_Tirna_Scithe.h"
+#include "npc_Mist_Of_Tirna_Scthithe.h"
+#include "Boss_Ingra _Maloch.cpp"
+#include "Boss_Mistcaller.cpp"
+#include "Boss_Tred_ova.cpp"
+#include "Ardenweald.cpp"
+#include "zone_Ardenweald.cpp"
+#include "zone_Ardenweald.h" 
+#include "Objects_Ardenweald.h"
 
 void AddSC_zone_ardenweald()
 {
+  void zone_ardenwenweald();
 }
 
 void AddSC_Zone_Mist_Of_Tirna_Scithe()
 {
+  void Zone_Mist_Of_Tirna_Scithe();
 }
 
 void AddSC_Zone_De_Other_Side()
 {
+  void Zone_De_Other_Side();
 }
 
    
