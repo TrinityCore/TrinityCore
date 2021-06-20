2605f0144ead3eb98ae1de06f0ce3ccf6ed5835f
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2021-06-20 20:00:47 +0300

    Update npc_Ardenweald.h

--- src/server/scripts/Shadowlands/Ardenweald/npc_Ardenweald.h
+++ src/server/scripts/Shadowlands/Ardenweald/npc_Ardenweald.h
@@ -14,22 +14,6 @@
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-/*
- * Copyright 2021 
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
- */
 #include "ScriptMgr.h"
 #include "Player.h"
 #include "InstanceScript.h"
@@ -200,6 +184,14 @@ enum NPC
   NPC_Blighted_Conqueror                              = 174729,
   NPC_Blighted_Creeper                                = 169629,176179,
   NPC_Blighted_Darkcaster                             = 169631, 
+};
+
+Enum mode
+{
+  void mode_LFR()
+  void mode_N()
+  void mode_HC()
+  void mode_Mythic()
 };
 
 enum NpcLoot
