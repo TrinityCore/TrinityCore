7b34a91f3b24844acdb593f1ba6f6e54ca7c2633
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2021-06-20 14:07:48 +0300

    Update npc_Ardenweald.h

--- src/server/scripts/Shadowlands/Ardenweald/npc_Ardenweald.h
+++ src/server/scripts/Shadowlands/Ardenweald/npc_Ardenweald.h
@@ -64,32 +64,40 @@
 
 enum NPC
 {
-  NPC_Oranomonos the Everbranching                   
-  NPC_Granny                                          
-  NPC_A_Naughty_Story                                 
-  NPC_Amun                                            
-  NPC_Abandoned_Whelp                                 
-  NPC_Adopted_Son                                     
-  NPC_Aerto                                           
-  NPC_Agitated_Mistfeather                            
-  NPC_Aithlyn                                         
-  NPC_Alascene                                        
-  NPC_Aldas                                           
-  NPC_Alderyn                                       
-  NPC_Alice                                           
-  NPC_Amberfoot                                       
-  NPC_Ammi                                            
-  NPC_Amphitheater_Musician                           
-  NPC_Anaris_Windwood                                 
-  NPC_Anaya_Dawnrunner                                
-  NPC_Ancient_Gazelle                                 
-  NPC_Angelia                                         
-  NPC_Angry_Egg_Tender                                
-  NPC_Angwyrdmn                                       
-  NPC_Anima_Starved_Matriarch                         
-  NPC_Anima_Starved_Patriarch                         
-  NPC_Anima_Starved_Shrieker                          
-  NPC_Anima_Starved_Stalker                           
+  NPC_Oranomonos the Everbranching = "Oranomonos the Everbranching"                  
+  NPC_Granny = ""Granny""                                         
+  NPC_A_Naughty_Story = "A Naughty Story"                                
+  NPC_Amun = "A'mun"                                           
+  NPC_Abandoned_Whelp = "Abandoned Whelp"                                
+  NPC_Adopted_Son = "Adopted Son"                                    
+  NPC_Aerto = "Aerto <Grove Ranger>"                                          
+  NPC_Agitated_Mistfeather = "Agitated Mistfeather"                           
+  NPC_Aithlyn = "Aithlyn <Wild Hunt Quartermaster>"                                        
+  NPC_Alascene = "Alascene"                                       
+  NPC_Aldas = "Aldas <General Goods>"                                          
+  NPC_Alderyn = "Alderyn <The Wild Hunt>"                                      
+  NPC_Alice = "Alice"                                          
+  NPC_Amberfoot = "Amberfoot"                                      
+  NPC_Ammi = "Ammi"                                           
+  NPC_Amphitheater_Musician = "Amphitheater Musician"                          
+  NPC_Anaris_Windwood = "Anaris Windwood"                                
+  NPC_Anaya_Dawnrunner = "Anaya Dawnrunner"                               
+  NPC_Ancient_Gazelle = "Ancient Gazelle"                                
+  NPC_Angelia = "Angelia <Food & Drink>"                                        
+  NPC_Angry_Egg_Tender = "Angry Egg Tender"                               
+  NPC_Angwyrdmn = "Angwyrdmn <Innkeeper>"                                      
+  NPC_Anima_Starved_Matriarch = "Anima Starved Matriarch"                        
+  NPC_Anima_Starved_Patriarch = "Anima Starved Patriarch"                        
+  NPC_Anima_Starved_Shrieker = "Anima Starved Shrieker"                         
+  NPC_Anima_Starved_Stalker = "Anima Starved Stalker"
+  NPC_Anir = "Anir <Trade Goods & Repairs>"                           
+  NPC_Aralon = "Ara'lon <The Wild Hunt>"
+  NPC_Archivist Dreyden = "Archivist Dreyden"
+  NPC_Ardenmoth = "Ardenmoth"
+  NPC_Ardent_Pacifier = "Ardent Pacifier"
+  NPC_Arkelios = "Arkelios <Wandering Spirit>"
+  NPC_
+  NPC_
   NPC_
 }
 
@@ -122,6 +130,10 @@ enum NPC
   NPC_Anima_Starved_Shrieker                          = 169050,
   NPC_Anima_Starved_Stalker                           = 169049,169228,
   NPC_Anir                                            = 158494,169152,
-  NPC_  
-
+  NPC_Aralon                                          = 160963,161378,161492,169935,170290,170485,170705,
+                                                        161603,162979,165250,169649,170463,170522,171641,172133, 
+  NPC_Archivist_Dreyden                               = 165867,
+  NPC_Ardenmoth                                       = 174201,174248,
+  NPC_Ardent_Pacifier                                 = 172493,
+  NPC_Arkelios                                        = 173939,
 };
