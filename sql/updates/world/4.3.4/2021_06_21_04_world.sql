41e7c65dcb3cc63f75d194706c3ec33fa238bebc
funjoker <funjoker109@gmail.com>
2021-06-21 23:05:21 +0200

    DB/Quest: Update some timers

---
+++ sql/updates/world/4.3.4/2021_06_21_04_world.sql
@@ -0,0 +1,8 @@
+UPDATE `quest_template_addon` SET `TimeAllowed` = 40 WHERE (`ID` = 11922);
+UPDATE `quest_template_addon` SET `TimeAllowed` = 40 WHERE (`ID` = 11731);
+UPDATE `quest_template_addon` SET `TimeAllowed` = 600 WHERE (`ID` = 11690);
+UPDATE `quest_template_addon` SET `TimeAllowed` = 6000 WHERE (`ID` = 11579);
+UPDATE `quest_template_addon` SET `TimeAllowed` = 6000 WHERE (`ID` = 11578);
+UPDATE `quest_template_addon` SET `TimeAllowed` = 120 WHERE (`ID` = 11320);
+UPDATE `quest_template_addon` SET `TimeAllowed` = 6000 WHERE (`ID` = 9065);
+UPDATE `quest_template_addon` SET `TimeAllowed` = 300 WHERE (`ID` = 8274);
