0428209246a45f9769cb8abe90948ceb1328bc2d
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2021-06-23 12:51:27 +0300

    Chromie

---
+++ src/server/scripts/Chromie/Chromie.cpp
@@ -0,0 +1,34 @@
+#include "zone_stormwind_city.cpp"
+#include "zone_orgrimmar.cpp"
+
+enum Quest 
+{
+   Adventurers_Wanted_Chromies_Call_A = 62567,    //Adventurers Wanted: Chromie's Call
+   Adventurers_Wanted_Chromies_Call_H = 62568,    //Adventurers Wanted: Chromie's Call
+};
+
+enum Npc
+{
+	Chromie = 167032,
+};
+
+enum NpcList
+{
+	Chromie = "Chromie <Emissary of the Bronze Dragonflight>"
+};
+
+enum Factions
+{
+	Alliance();
+	Horde();
+};
+
+enum NpcData
+{
+	Chromie
+}
+
+enum playerchoise
+{
+	player 
+}
