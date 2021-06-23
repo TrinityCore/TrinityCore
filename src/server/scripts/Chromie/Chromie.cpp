29e61bc56fdd4767404f49a811c12b307ed5a7aa
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2021-06-23 13:18:58 +0300

    Update Chromie.cpp

--- src/server/scripts/Chromie/Chromie.cpp
+++ src/server/scripts/Chromie/Chromie.cpp
@@ -7,6 +7,11 @@ enum Quest
    Adventurers_Wanted_Chromies_Call_H = 62568,    //Adventurers Wanted: Chromie's Call
 };
 
+enum Spell
+{
+	Timewalking = 150683,
+};
+
 enum Npc
 {
 	Chromie = 167032,
@@ -26,9 +31,42 @@ enum Factions
 enum NpcData
 {
 	Chromie
-}
+};
+
+enum Menu
+{
+	Pandaria;
+	Cataclysm;
+
+};
 
 enum playerchoise
 {
 	player 
+};
+
+enum  DungeonFinder
+{
+    Dungeon_Gate_of_the_Setting_Sun = 5976,
+	Dungeon_Temple_of_the_Jade_Serpent =  5956,
+	Dungeon_Stormstout_Brewery = 5963,
+	Dungeon_Siege_of_Niuzao_Temple = 6214,
+	Dungeon_Shado_Pan_Monastery = 5918,
+	Dungeon_Mogu_shan_Palace = 6182,
+};
+
+enum TimewalkingDungeonEvent
+
+enum Event
+{
+	Event = 643,
 }
+//Pandaria
+{
+	Event_Gate_of_the_Setting_Sun = 5976,
+	Event_Temple_of_the_Jade_Serpent =  5956,
+	Event_Stormstout_Brewery = 5963,
+	Event_Siege_of_Niuzao_Temple = 6214,
+	Event_Shado_Pan_Monastery = 5918,
+	Event_Mogu_shan_Palace = 6182,
+};
