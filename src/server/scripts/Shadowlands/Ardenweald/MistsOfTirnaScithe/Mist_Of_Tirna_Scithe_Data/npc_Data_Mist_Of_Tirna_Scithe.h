e6a120e3e52d55791984ab468d60840240cb27cd
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2021-07-02 14:49:31 +0300

    update

---
+++ src/server/scripts/Shadowlands/Ardenweald/MistsOfTirnaScithe/Mist_Of_Tirna_Scithe_Data/npc_Data_Mist_Of_Tirna_Scithe.h
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2021 
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "ScriptMgr.h"
+#include "Player.h"
+#include "InstanceScript.h"
+#include "instance_de_other_side.cpp"
+#include "zone_De_Other_Side.cpp"
+#include "zone_De_Other_Side.h"
+#include "npc_De_Other_Side.h"
+#include "Objects_De_Other_Side.h"
+#include "De_Other_Side.h"
+#include "boss_dealer_xyexa.cpp"
+#include "boss_hakkar_the_soulflayer_os.cpp"
+#include "boss_muehzala.cpp"
+#include "boss_the_manastorms.cpp"
+#include "Heart_Of_The_Forest.cpp"
+#include "zone_Heart_Of_The_Forest.cpp"
+#include "zone_Heart_Of_The_Forest.h"
+#include "npc_Heart_Of_The_Forest.h"
+#include "Objects_Heart_Of_The_Forest.h"
+#include "instance_mists_of_tirna_scithe.cpp"
+#include "zone_mists_of_tirna_scithe.cpp"
+#include "zone_mists_of_tirna_scithe.h"
+#include "Objects_Mist_Of_Tirna_Scithe.h"
+#include "npc_Mist_Of_Tirna_Scthithe.h"
+#include "Boss_Ingra _Maloch.cpp"
+#include "Boss_Mistcaller.cpp"
+#include "Boss_Tred_ova.cpp"
+#include "Ardenweald.cpp"
+#include "zone_Ardenweald.cpp"
+#include "zone_Ardenweald.h" 
+#include "Objects_Ardenweald.h"
+ 
+enum Mode
+{
+  void Mode_LFR();
+  void Mode_N();
+  void Mode_HC();
+  void Mode_Mythic();
+};
+
+enum AuraScript
+{
+  void aura();
+};
+ 
+enum SpellScript
+{
+  void Spell();
+};
+
+enum EffectScript
+{
+  void Effect();
+};
+
+enum ModeScript
+{
+  void Mode();
+};
+
+enum SpawnScript
+{
+  void Spawn();
+};
+
+enum NpcLootScript
+{
+  void NpcLoot();
+};
+
+enum NpcDataScript
+{
+  void NpcData();
+};
+
+enum QuestDataScript
+{
+  void QuestData();
+};
+
+enum QuestRewardsScript
+{
+  void QuestRewards();
+};
+
+enum FishingScripts
+{
+  void Fishing();
+};
+
+enum StartsQuestScript
+{
+  void StartsQuest();
+};
+
+enum QuestScript
+{
+  void Quest();
+}; 
+
+void AddSC_npc_Data_Ardenweald()
+{
+  RegisterAuraScript(aura);
+  RegisterSpellScript(spell);
+  RegisterEffectScript(efect);
+  RegisterModeScript(mode);
+  RegisterSpawnScript(Spawn);
+  RegisterNpcLootScript(NpcLoot);
+  RegisterNpcDataScript(NpcData);
+  RegisterQuestDataScript(QuestData);
+  RegisterQuestRewardsScript(QuestRewards);
+  RegisterFishingScripts(Fishing);
+  RegisterQuestScript(Quest);
+  RegisterStartsQuestScript(StartsQuest);
+  RegisterFishingScripts(Fishing);
+};
