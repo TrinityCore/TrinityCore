b5246a5a7877a1212f9943b68dfc550627f99870
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2021-07-08 13:37:40 +0300

    Update torghast_tower_of_the_damned.h

--- src/server/scripts/Shadowlands/TheMaw/torghast_tower_of_the_damned.h
+++ src/server/scripts/Shadowlands/TheMaw/torghast_tower_of_the_damned.h
@@ -14,3 +14,26 @@
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+#include "ScriptMgr.h"
+#include "Player.h"
+#include "Creature.h"
+#include "CreatureAI.h"
+#include "GameObject.h"
+#include "GameObjectAI.h"
+#include "AreaTrigger.h"
+#include "AreaTriggerAI.h"
+#include "SpellAuraEffects.h"
+#include "Map.h"
+#include "InstanceScript.h"
+#include "Transport.h"
+#include "TransportMgr.h"
+#include "AreaTrigger.h"
+#include "AreaTriggerAI.h"
+#include "torghast_tower_of_the_damned.h"
+#include "zone_the_maw.cpp"
+#include "instance_torgast_tower_of_the_damned.cpp"
+
+class torgast_tower_of_the_damned
+{
+    void torgast_tower_of_the_damned();
+};
