037296ae0f7b770e9c0ef2495099c8ae3bb9de6f
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2021-07-08 13:39:53 +0300

    Update instance_torgast_tower_of_the_damned.cpp

--- src/server/scripts/Shadowlands/TheMaw/instance_torgast_tower_of_the_damned.cpp
+++ src/server/scripts/Shadowlands/TheMaw/instance_torgast_tower_of_the_damned.cpp
@@ -17,11 +17,20 @@
 
 #include "ScriptMgr.h"
 #include "Player.h"
-#include "AreaTrigger.h"
-#include "AreaTriggerAI.h"
-#include "InstanceScript.h"
+#include "Creature.h"
+#include "CreatureAI.h"
 #include "GameObject.h"
 #include "GameObjectAI.h"
+#include "AreaTrigger.h"
+#include "AreaTriggerAI.h"
+#include "SpellAuraEffects.h"
+#include "Map.h"
+#include "InstanceScript.h"
+#include "Transport.h"
+#include "TransportMgr.h"
+#include "AreaTrigger.h"
+#include "AreaTriggerAI.h"
+#include "zone_the_maw.cpp"
 #include "torghast_tower_of_the_damned.h"
 
 //2162
