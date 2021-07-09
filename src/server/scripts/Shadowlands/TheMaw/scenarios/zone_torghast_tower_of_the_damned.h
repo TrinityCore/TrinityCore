dffac32fa9a0c8c0f45f1997b13648f102370619
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2021-07-09 11:35:41 +0300

    Create zone_torghast_tower_of_the_damned.h

---
+++ src/server/scripts/Shadowlands/TheMaw/scenarios/zone_torghast_tower_of_the_damned.h
@@ -0,0 +1,23 @@
+#include "ScriptMgr.h"
+#include "Player.h"
+#include "Creature.h"
+#include "CreatureAI.h"
+#include "GameObject.h"
+#include "GameObjectAI.h"
+#include "AreaTrigger.h"
+#include "AreaTriggerAI.h"
+#include "SpellAuraEffects.h"
+#include "Map.h"
+#include "InstanceScript.h"
+#include "Transport.h"
+#include "TransportMgr.h"
+#include "AreaTrigger.h"
+#include "AreaTriggerAI.h"
+#include "zone_the_maw.cpp"
+#include "torghast_tower_of_the_damned.h" 
+#include "instance_torgast_tower_of_the_damned.cpp"
+
+class torgast_tower_of_the_damned
+{
+    void torgast_tower_of_the_damned();
+};
