80de98123a51259a7d4ced1d9331464ba72de498
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2021-07-09 13:28:36 +0300

    Update zone_Sanctum_of_Domination.cpp

--- src/server/scripts/Shadowlands/TheMaw/Sanctum of Domination/zone_Sanctum_of_Domination.cpp
+++ src/server/scripts/Shadowlands/TheMaw/Sanctum of Domination/zone_Sanctum_of_Domination.cpp
@@ -94,4 +94,40 @@ enum ObjectSpawn
     ObjectSpawn_1;
 }
 
-
+enum Achievements
+{
+    AHEAD_OF_THE_CURVE_SYLVANAS_WINDRUNNER                                          = 15134,
+    CUTTING_EDGE_SYLVANAS_WINDRUNNER                                                = 15135,
+    EYE_WISH_YOU_WERE_HERE                                                          = 15065,
+    FLAWLESS_FATE                                                                   = 15040,
+    HALL_OF_FAME_SYLVANAS_ALLIANCE                                                  = 15197,
+    HALL_OF_FAME_SYLVANAS_HORDE                                                     = 15196,
+    HEROIC_SANCTUM_OF_DOMINAIION                                                    = 15127,
+    HEROIC_SANCTUM_OF_DOMINAIION_GUILD_RUN                                          = 15183,
+    I_USED_TO_BULLSEYE_DEEPRUN_RATS_BACK_HOME                                       = 15058,
+    KNOWLEDGE_IS_POWER                                                              = 15132,
+    MYTHIC_FATESCRIBE_ROH_KALO                                                      = 15119,
+    MYTHIC_GUARDIAN_OF_THE_FIRST_ONES                                               = 15118,
+    MYTHIC_KELTHUZAD                                                                = 15120,
+    MYTHIC_PAINSMITH_RAZNAL                                                         = 15117,
+    MYTHIC_REMNANT_OF_NERZHUL                                                       = 15115,
+    MYTHIC_SANCTUM_OF_DOMINATION                                                    = 15128,
+    MYTHIC_SOULRENDER_DORMAZAIN                                                     = 15116,
+    MYTHIC_SYLVANAS_WINDRUNNER                                                      = 15121,
+    MYTHIC_SYLVANAS_WINDRUNNER_GUILD_RUN                                            = 15184,
+    MYTHIC_THE_EYE_OF_THE_JAILER                                                    = 15113,
+    MYTHIC_THE_NINE                                                                 = 15114,
+    MYTHIC_THE_TARRAGRUE                                                            = 15112,
+    NAME_A_BETTER_DUO_ILL_WAIT                                                      = 14998,
+    SANCTUM_OF_DOMINAIION                                                           = 15126,
+    SANCTUM_OF_DOMINAIION_GUILD_RUN                                                 = 15182,
+    SHACKLES_OF_FATE                                                                = 15124,
+    THE_DARK_BASTILLE                                                               = 15123,
+    THE_JAILERS_VANGUARD                                                            = 15122,
+    THE_RECKONING                                                                   = 15125,
+    THIS_WORLD_IS_A_PRISM                                                           = 15133,
+    TO_THE_NINES                                                                    = 15003,
+    TOGETHER_FOREVER                                                                = 15108,
+    TORMENTORS_TANGO                                                                = 15105,
+    WHACK_A_SOUL                                                                    = 15131,
+}
