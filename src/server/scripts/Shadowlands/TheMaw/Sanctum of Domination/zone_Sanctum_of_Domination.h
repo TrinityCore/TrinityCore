4706cc4f4d085d931554788ad9899dc47a1a98a4
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2021-07-09 11:33:06 +0300

    update

--- src/server/scripts/Shadowlands/TheMaw/Sanctum of Domination/zone_Sanctum_of_Domination.h
+++ src/server/scripts/Shadowlands/TheMaw/Sanctum of Domination/zone_Sanctum_of_Domination.h
@@ -116,7 +116,7 @@ void AddSC_Boss_Sylvanas_Windrunner()
 
 {
     AddSC_instance_Sanctum_of_Domination();
-        AddSC_zone_The_Maw()
+         AddSC_zone_The_Maw()
          AddSC_zone_Sanctum_of_Domination();
          AddSC_npc_Sanctum_of_Domination();
          AddSC_Objects_Sanctum_of_Domination();
