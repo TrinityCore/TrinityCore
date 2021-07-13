67bd11f5ffe17cb2daeb5074d92ca410eb269b09
offl <11556157+offl@users.noreply.github.com>
2021-07-13 14:16:00 +0300

    Scripts/BWL: Update scripts to new register model (1/3) (#26686)

--- src/server/scripts/EasternKingdoms/BlackrockMountain/BlackwingLair/boss_broodlord_lashlayer.cpp
+++ src/server/scripts/EasternKingdoms/BlackrockMountain/BlackwingLair/boss_broodlord_lashlayer.cpp
@@ -54,190 +54,168 @@ enum Actions
     ACTION_DEACTIVATE = 0
 };
 
-class boss_broodlord : public CreatureScript
+struct boss_broodlord : public BossAI
 {
-public:
-    boss_broodlord() : CreatureScript("boss_broodlord") { }
+    boss_broodlord(Creature* creature) : BossAI(creature, DATA_BROODLORD_LASHLAYER) { }
 
-    struct boss_broodlordAI : public BossAI
+    void JustEngagedWith(Unit* who) override
     {
-        boss_broodlordAI(Creature* creature) : BossAI(creature, DATA_BROODLORD_LASHLAYER) { }
+        BossAI::JustEngagedWith(who);
+        Talk(SAY_AGGRO);
 
-        void JustEngagedWith(Unit* who) override
+        events.ScheduleEvent(EVENT_CLEAVE, 8s);
+        events.ScheduleEvent(EVENT_BLASTWAVE, 12s);
+        events.ScheduleEvent(EVENT_MORTALSTRIKE, 20s);
+        events.ScheduleEvent(EVENT_KNOCKBACK, 30s);
+        events.ScheduleEvent(EVENT_CHECK, 1s);
+    }
+
+    void JustDied(Unit* /*killer*/) override
+    {
+        _JustDied();
+
+        std::list<GameObject*> _goList;
+        GetGameObjectListWithEntryInGrid(_goList, me, GO_SUPPRESSION_DEVICE, 200.0f);
+        for (std::list<GameObject*>::const_iterator itr = _goList.begin(); itr != _goList.end(); itr++)
+            ((*itr)->AI()->DoAction(ACTION_DEACTIVATE));
+    }
+
+    void UpdateAI(uint32 diff) override
+    {
+        if (!UpdateVictim())
+            return;
+
+        events.Update(diff);
+
+        while (uint32 eventId = events.ExecuteEvent())
         {
-            BossAI::JustEngagedWith(who);
-            Talk(SAY_AGGRO);
-
-            events.ScheduleEvent(EVENT_CLEAVE, 8s);
-            events.ScheduleEvent(EVENT_BLASTWAVE, 12s);
-            events.ScheduleEvent(EVENT_MORTALSTRIKE, 20s);
-            events.ScheduleEvent(EVENT_KNOCKBACK, 30s);
-            events.ScheduleEvent(EVENT_CHECK, 1s);
-        }
-
-        void JustDied(Unit* /*killer*/) override
-        {
-            _JustDied();
-
-            std::list<GameObject*> _goList;
-            GetGameObjectListWithEntryInGrid(_goList, me, GO_SUPPRESSION_DEVICE, 200.0f);
-            for (std::list<GameObject*>::const_iterator itr = _goList.begin(); itr != _goList.end(); itr++)
-                ((*itr)->AI()->DoAction(ACTION_DEACTIVATE));
-        }
-
-        void UpdateAI(uint32 diff) override
-        {
-            if (!UpdateVictim())
-                return;
-
-            events.Update(diff);
-
-            while (uint32 eventId = events.ExecuteEvent())
+            switch (eventId)
             {
-                switch (eventId)
-                {
-                    case EVENT_CLEAVE:
-                        DoCastVictim(SPELL_CLEAVE);
-                        events.ScheduleEvent(EVENT_CLEAVE, 7s);
-                        break;
-                    case EVENT_BLASTWAVE:
-                        DoCastVictim(SPELL_BLASTWAVE);
-                        events.ScheduleEvent(EVENT_BLASTWAVE, 8s, 16s);
-                        break;
-                    case EVENT_MORTALSTRIKE:
-                        DoCastVictim(SPELL_MORTALSTRIKE);
-                        events.ScheduleEvent(EVENT_MORTALSTRIKE, 25s, 35s);
-                        break;
-                    case EVENT_KNOCKBACK:
-                        DoCastVictim(SPELL_KNOCKBACK);
-                        if (GetThreat(me->GetVictim()))
-                            ModifyThreatByPercent(me->GetVictim(), -50);
-                        events.ScheduleEvent(EVENT_KNOCKBACK, 15s, 30s);
-                        break;
-                    case EVENT_CHECK:
-                        if (me->GetDistance(me->GetHomePosition()) > 150.0f)
-                        {
-                            Talk(SAY_LEASH);
-                            EnterEvadeMode(EVADE_REASON_BOUNDARY);
-                        }
-                        events.ScheduleEvent(EVENT_CHECK, 1s);
-                        break;
-                }
+                case EVENT_CLEAVE:
+                    DoCastVictim(SPELL_CLEAVE);
+                    events.ScheduleEvent(EVENT_CLEAVE, 7s);
+                    break;
+                case EVENT_BLASTWAVE:
+                    DoCastVictim(SPELL_BLASTWAVE);
+                    events.ScheduleEvent(EVENT_BLASTWAVE, 8s, 16s);
+                    break;
+                case EVENT_MORTALSTRIKE:
+                    DoCastVictim(SPELL_MORTALSTRIKE);
+                    events.ScheduleEvent(EVENT_MORTALSTRIKE, 25s, 35s);
+                    break;
+                case EVENT_KNOCKBACK:
+                    DoCastVictim(SPELL_KNOCKBACK);
+                    if (GetThreat(me->GetVictim()))
+                        ModifyThreatByPercent(me->GetVictim(), -50);
+                    events.ScheduleEvent(EVENT_KNOCKBACK, 15s, 30s);
+                    break;
+                case EVENT_CHECK:
+                    if (me->GetDistance(me->GetHomePosition()) > 150.0f)
+                    {
+                        Talk(SAY_LEASH);
+                        EnterEvadeMode(EVADE_REASON_BOUNDARY);
+                    }
+                    events.ScheduleEvent(EVENT_CHECK, 1s);
+                    break;
             }
-
-            DoMeleeAttackIfReady();
         }
-    };
 
-    CreatureAI* GetAI(Creature* creature) const override
-    {
-        return GetBlackwingLairAI<boss_broodlordAI>(creature);
+        DoMeleeAttackIfReady();
     }
 };
 
-class go_suppression_device : public GameObjectScript
+struct go_suppression_device : public GameObjectAI
 {
-    public:
-        go_suppression_device() : GameObjectScript("go_suppression_device") { }
+    go_suppression_device(GameObject* go) : GameObjectAI(go), _instance(go->GetInstanceScript()), _active(true) { }
 
-        struct go_suppression_deviceAI : public GameObjectAI
+    void InitializeAI() override
+    {
+        if (_instance->GetBossState(DATA_BROODLORD_LASHLAYER) == DONE)
         {
-            go_suppression_deviceAI(GameObject* go) : GameObjectAI(go), _instance(go->GetInstanceScript()), _active(true) { }
-
-            void InitializeAI() override
-            {
-                if (_instance->GetBossState(DATA_BROODLORD_LASHLAYER) == DONE)
-                {
-                    Deactivate();
-                    return;
-                }
-
-                _events.ScheduleEvent(EVENT_SUPPRESSION_CAST, 0s, 5s);
-            }
-
-            void UpdateAI(uint32 diff) override
-            {
-                _events.Update(diff);
-
-                while (uint32 eventId = _events.ExecuteEvent())
-                {
-                    switch (eventId)
-                    {
-                        case EVENT_SUPPRESSION_CAST:
-                            if (me->GetGoState() == GO_STATE_READY)
-                            {
-                                me->CastSpell(nullptr, SPELL_SUPPRESSION_AURA, true);
-                                me->SendCustomAnim(0);
-                            }
-                            _events.ScheduleEvent(EVENT_SUPPRESSION_CAST, 5s);
-                            break;
-                        case EVENT_SUPPRESSION_RESET:
-                            Activate();
-                            break;
-                    }
-                }
-            }
-
-            void OnLootStateChanged(uint32 state, Unit* /*unit*/) override
-            {
-                switch (state)
-                {
-                    case GO_ACTIVATED:
-                        Deactivate();
-                        _events.CancelEvent(EVENT_SUPPRESSION_CAST);
-                        _events.ScheduleEvent(EVENT_SUPPRESSION_RESET, 30s, 120s);
-                        break;
-                    case GO_JUST_DEACTIVATED: // This case prevents the Gameobject despawn by Disarm Trap
-                        me->SetLootState(GO_READY);
-                        break;
-                }
-            }
-
-            void DoAction(int32 action) override
-            {
-                if (action == ACTION_DEACTIVATE)
-                {
-                    Deactivate();
-                    _events.CancelEvent(EVENT_SUPPRESSION_RESET);
-                }
-            }
-
-            void Activate()
-            {
-                if (_active)
-                    return;
-                _active = true;
-                if (me->GetGoState() == GO_STATE_ACTIVE)
-                    me->SetGoState(GO_STATE_READY);
-                me->SetLootState(GO_READY);
-                me->RemoveFlag(GAMEOBJECT_FLAGS, GO_FLAG_NOT_SELECTABLE);
-                _events.ScheduleEvent(EVENT_SUPPRESSION_CAST, 0s);
-            }
-
-            void Deactivate()
-            {
-                if (!_active)
-                    return;
-                _active = false;
-                me->SetGoState(GO_STATE_ACTIVE);
-                me->SetFlag(GAMEOBJECT_FLAGS, GO_FLAG_NOT_SELECTABLE);
-                _events.CancelEvent(EVENT_SUPPRESSION_CAST);
-            }
-
-        private:
-            InstanceScript* _instance;
-            EventMap _events;
-            bool _active;
-        };
-
-        GameObjectAI* GetAI(GameObject* go) const override
-        {
-            return GetBlackwingLairAI<go_suppression_deviceAI>(go);
+            Deactivate();
+            return;
         }
+
+        _events.ScheduleEvent(EVENT_SUPPRESSION_CAST, 0s, 5s);
+    }
+
+    void UpdateAI(uint32 diff) override
+    {
+        _events.Update(diff);
+
+        while (uint32 eventId = _events.ExecuteEvent())
+        {
+            switch (eventId)
+            {
+                case EVENT_SUPPRESSION_CAST:
+                    if (me->GetGoState() == GO_STATE_READY)
+                    {
+                        me->CastSpell(nullptr, SPELL_SUPPRESSION_AURA, true);
+                        me->SendCustomAnim(0);
+                    }
+                    _events.ScheduleEvent(EVENT_SUPPRESSION_CAST, 5s);
+                    break;
+                case EVENT_SUPPRESSION_RESET:
+                    Activate();
+                    break;
+            }
+        }
+    }
+
+    void OnLootStateChanged(uint32 state, Unit* /*unit*/) override
+    {
+        switch (state)
+        {
+            case GO_ACTIVATED:
+                Deactivate();
+                _events.CancelEvent(EVENT_SUPPRESSION_CAST);
+                _events.ScheduleEvent(EVENT_SUPPRESSION_RESET, 30s, 120s);
+                break;
+            case GO_JUST_DEACTIVATED: // This case prevents the Gameobject despawn by Disarm Trap
+                me->SetLootState(GO_READY);
+                break;
+        }
+    }
+
+    void DoAction(int32 action) override
+    {
+        if (action == ACTION_DEACTIVATE)
+        {
+            Deactivate();
+            _events.CancelEvent(EVENT_SUPPRESSION_RESET);
+        }
+    }
+
+    void Activate()
+    {
+        if (_active)
+            return;
+        _active = true;
+        if (me->GetGoState() == GO_STATE_ACTIVE)
+            me->SetGoState(GO_STATE_READY);
+        me->SetLootState(GO_READY);
+        me->RemoveFlag(GAMEOBJECT_FLAGS, GO_FLAG_NOT_SELECTABLE);
+        _events.ScheduleEvent(EVENT_SUPPRESSION_CAST, 0s);
+    }
+
+    void Deactivate()
+    {
+        if (!_active)
+            return;
+        _active = false;
+        me->SetGoState(GO_STATE_ACTIVE);
+        me->SetFlag(GAMEOBJECT_FLAGS, GO_FLAG_NOT_SELECTABLE);
+        _events.CancelEvent(EVENT_SUPPRESSION_CAST);
+    }
+
+private:
+    InstanceScript* _instance;
+    EventMap _events;
+    bool _active;
 };
 
 void AddSC_boss_broodlord()
 {
-    new boss_broodlord();
-    new go_suppression_device();
+    RegisterBlackwingLairCreatureAI(boss_broodlord);
+    RegisterBlackwingLairGameObjectAI(go_suppression_device);
 }
