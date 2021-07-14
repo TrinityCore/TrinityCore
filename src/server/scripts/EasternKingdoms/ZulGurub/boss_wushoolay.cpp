94ff33894d1ab0bcccbe56b8ae3bba1cbd4b902e
offl <11556157+offl@users.noreply.github.com>
2021-07-14 22:24:36 +0300

    Scripts/ZG: Update scripts to new register model (5/5) (#26695)
    (cherry picked from commit 7f853ea735bdb8ee27cd4cc002da67f1fbc81d71)

--- src/server/scripts/EasternKingdoms/ZulGurub/boss_wushoolay.cpp
+++ src/server/scripts/EasternKingdoms/ZulGurub/boss_wushoolay.cpp
@@ -31,56 +31,45 @@ enum Events
 {
 };
 
-class boss_wushoolay : public CreatureScript
+struct boss_wushoolay : public BossAI
 {
-    public:
-        boss_wushoolay() : CreatureScript("boss_wushoolay") { }
+    boss_wushoolay(Creature* creature) : BossAI(creature, DATA_HAZZARAH)
+    {
+    }
 
-        struct boss_wushoolayAI : public BossAI
+    void JustEngagedWith(Unit* who) override
+    {
+        BossAI::JustEngagedWith(who);
+    }
+
+    void UpdateAI(uint32 diff) override
+    {
+        if (!UpdateVictim())
+            return;
+
+        events.Update(diff);
+
+        if (me->HasUnitState(UNIT_STATE_CASTING))
+            return;
+        /*
+        while (uint32 eventId = events.ExecuteEvent())
         {
-            boss_wushoolayAI(Creature* creature) : BossAI(creature, DATA_HAZZARAH)
+            switch (eventId)
             {
+                default:
+                    break;
             }
 
-            void JustEngagedWith(Unit* who) override
-            {
-                BossAI::JustEngagedWith(who);
-            }
-
-            void UpdateAI(uint32 diff) override
-            {
-                if (!UpdateVictim())
-                    return;
-
-                events.Update(diff);
-
-                if (me->HasUnitState(UNIT_STATE_CASTING))
-                    return;
-                /*
-                while (uint32 eventId = events.ExecuteEvent())
-                {
-                    switch (eventId)
-                    {
-                        default:
-                            break;
-                    }
-
-                    if (me->HasUnitState(UNIT_STATE_CASTING))
-                        return;
-                }
-                */
-
-                DoMeleeAttackIfReady();
-            }
-        };
-
-        CreatureAI* GetAI(Creature* creature) const override
-        {
-            return GetZulGurubAI<boss_wushoolayAI>(creature);
+            if (me->HasUnitState(UNIT_STATE_CASTING))
+                return;
         }
+        */
+
+        DoMeleeAttackIfReady();
+    }
 };
 
 void AddSC_boss_wushoolay()
 {
-    new boss_wushoolay();
+    RegisterZulGurubCreatureAI(boss_wushoolay);
 }
