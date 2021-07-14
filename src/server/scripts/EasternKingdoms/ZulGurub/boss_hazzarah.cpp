7a40a62b60d1b6c634a8b3cb0c9f746354c43f6f
offl <offl@users.noreply.github.com>
2021-07-14 16:52:24 +0300

    Scripts/ZG: Update scripts to new register model (2/5)

--- src/server/scripts/EasternKingdoms/ZulGurub/boss_hazzarah.cpp
+++ src/server/scripts/EasternKingdoms/ZulGurub/boss_hazzarah.cpp
@@ -33,85 +33,74 @@ enum Events
     EVENT_ILLUSIONS = 3
 };
 
-class boss_hazzarah : public CreatureScript
+struct boss_hazzarah : public BossAI
 {
-    public:
-        boss_hazzarah() : CreatureScript("boss_hazzarah") { }
+    boss_hazzarah(Creature* creature) : BossAI(creature, DATA_EDGE_OF_MADNESS) { }
 
-        struct boss_hazzarahAI : public BossAI
+    void Reset() override
+    {
+        _Reset();
+    }
+
+    void JustDied(Unit* /*killer*/) override
+    {
+        _JustDied();
+    }
+
+    void JustEngagedWith(Unit* who) override
+    {
+        BossAI::JustEngagedWith(who);
+        events.ScheduleEvent(EVENT_MANABURN, 4s, 10s);
+        events.ScheduleEvent(EVENT_SLEEP, 10s, 18s);
+        events.ScheduleEvent(EVENT_ILLUSIONS, 10s, 18s);
+    }
+
+    void UpdateAI(uint32 diff) override
+    {
+        if (!UpdateVictim())
+            return;
+
+        events.Update(diff);
+
+        if (me->HasUnitState(UNIT_STATE_CASTING))
+            return;
+
+        while (uint32 eventId = events.ExecuteEvent())
         {
-            boss_hazzarahAI(Creature* creature) : BossAI(creature, DATA_EDGE_OF_MADNESS) { }
-
-            void Reset() override
+            switch (eventId)
             {
-                _Reset();
-            }
-
-            void JustDied(Unit* /*killer*/) override
-            {
-                _JustDied();
-            }
-
-            void JustEngagedWith(Unit* who) override
-            {
-                BossAI::JustEngagedWith(who);
-                events.ScheduleEvent(EVENT_MANABURN, 4s, 10s);
-                events.ScheduleEvent(EVENT_SLEEP, 10s, 18s);
-                events.ScheduleEvent(EVENT_ILLUSIONS, 10s, 18s);
-            }
-
-            void UpdateAI(uint32 diff) override
-            {
-                if (!UpdateVictim())
-                    return;
-
-                events.Update(diff);
-
-                if (me->HasUnitState(UNIT_STATE_CASTING))
-                    return;
-
-                while (uint32 eventId = events.ExecuteEvent())
-                {
-                    switch (eventId)
+                case EVENT_MANABURN:
+                    DoCastVictim(SPELL_MANABURN, true);
+                    events.ScheduleEvent(EVENT_MANABURN, 8s, 16s);
+                    break;
+                case EVENT_SLEEP:
+                    DoCastVictim(SPELL_SLEEP, true);
+                    events.ScheduleEvent(EVENT_SLEEP, 12s, 20s);
+                    break;
+                case EVENT_ILLUSIONS:
+                    // We will summon 3 illusions that will spawn on a random gamer and attack this gamer
+                    // We will just use one model for the beginning
+                    for (uint8 i = 0; i < 3; ++i)
                     {
-                        case EVENT_MANABURN:
-                            DoCastVictim(SPELL_MANABURN, true);
-                            events.ScheduleEvent(EVENT_MANABURN, 8s, 16s);
-                            break;
-                        case EVENT_SLEEP:
-                            DoCastVictim(SPELL_SLEEP, true);
-                            events.ScheduleEvent(EVENT_SLEEP, 12s, 20s);
-                            break;
-                        case EVENT_ILLUSIONS:
-                            // We will summon 3 illusions that will spawn on a random gamer and attack this gamer
-                            // We will just use one model for the beginning
-                            for (uint8 i = 0; i < 3; ++i)
-                            {
-                                if (Unit* target = SelectTarget(SelectTargetMethod::Random, 0, 0.f, true))
-                                    if (TempSummon* illusion = me->SummonCreature(NPC_NIGHTMARE_ILLUSION, target->GetPosition(), TEMPSUMMON_TIMED_OR_CORPSE_DESPAWN, 30s))
-                                        illusion->AI()->AttackStart(target);
-                            }
-                            events.ScheduleEvent(EVENT_ILLUSIONS, 15s, 25s);
-                            break;
-                        default:
-                            break;
+                        if (Unit* target = SelectTarget(SelectTargetMethod::Random, 0, 0.f, true))
+                            if (TempSummon* illusion = me->SummonCreature(NPC_NIGHTMARE_ILLUSION, target->GetPosition(), TEMPSUMMON_TIMED_OR_CORPSE_DESPAWN, 30s))
+                                illusion->AI()->AttackStart(target);
                     }
-
-                    if (me->HasUnitState(UNIT_STATE_CASTING))
-                        return;
-                }
-
-                DoMeleeAttackIfReady();
+                    events.ScheduleEvent(EVENT_ILLUSIONS, 15s, 25s);
+                    break;
+                default:
+                    break;
             }
-        };
 
-        CreatureAI* GetAI(Creature* creature) const override
-        {
-            return GetZulGurubAI<boss_hazzarahAI>(creature);
+            if (me->HasUnitState(UNIT_STATE_CASTING))
+                return;
         }
+
+        DoMeleeAttackIfReady();
+    }
 };
 
 void AddSC_boss_hazzarah()
 {
-    new boss_hazzarah();
+    RegisterZulGurubCreatureAI(boss_hazzarah);
 }
