cc588d119a77c18c1c5baed61c7b85d6e589de5e
dr-j <dr-j@users.noreply.github.com>
2021-07-21 20:04:33 +0300

    DB/Achievement: Add missing criteria to 'Disturbing the Peace'
    Closes #26729

---
+++ sql/updates/world/3.3.5/2021_07_21_01_world.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `achievement_criteria_data` WHERE `criteria_id`=4112 AND `type`=15;
+INSERT INTO `achievement_criteria_data` (`criteria_id`, `type`, `value1`, `value2`, `ScriptName`) VALUES
+(4112, 15, 3, 0, '');
