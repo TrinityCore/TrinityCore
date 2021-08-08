ad4bc7cc1f3d3f541219e9077238743c73130108
Meji <alvaromegias_46@hotmail.com>
2021-08-08 13:22:04 +0200

    Core/Objects: Units possessing another ones must use the detection of the latter (#26702)
    * Core/Objects: Units with invisibility auras can see units that cannot detect them
    
    * Core/Objects: A unit that possess another one, uses the detection of the latter
    
    * GCC: Avoid ambiguous 'else'
    
    Closes #23528

--- src/server/game/Entities/Object/Object.cpp
+++ src/server/game/Entities/Object/Object.cpp
@@ -1645,10 +1645,18 @@ bool WorldObject::CanDetect(WorldObject const* obj, bool ignoreStealth, bool che
 {
     WorldObject const* seer = this;
 
+    // If a unit is possessing another one, it uses the detection of the latter
     // Pets don't have detection, they use the detection of their masters
     if (Unit const* thisUnit = ToUnit())
-        if (Unit* controller = thisUnit->GetCharmerOrOwner())
+    {
+        if (thisUnit->isPossessing())
+        {
+            if (Unit* charmed = thisUnit->GetCharmed())
+                seer = charmed;
+        }
+        else if (Unit* controller = thisUnit->GetCharmerOrOwner())
             seer = controller;
+    }
 
     if (obj->IsAlwaysDetectableFor(seer))
         return true;
