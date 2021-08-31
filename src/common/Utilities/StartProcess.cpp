6ec73a765937e79e525d31e6f21c11b659844853
Shauren <shauren.trinity@gmail.com>
2021-08-31 13:46:17 +0200

    Core/Misc: Fix windows nopch build
    Closes #25363
    
    (cherry picked from commit 6ad58d604a63bacc13ee92c525189931d41d1e91)

--- src/common/Utilities/StartProcess.cpp
+++ src/common/Utilities/StartProcess.cpp
@@ -20,6 +20,8 @@
 #include "Log.h"
 #include "Optional.h"
 
+#include "Hacks/boost_1_73_process_windows_nopch.h"
+
 #include <boost/algorithm/string/join.hpp>
 #include <boost/iostreams/copy.hpp>
 #include <boost/process.hpp>
