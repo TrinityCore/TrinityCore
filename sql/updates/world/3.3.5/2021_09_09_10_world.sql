ff8ddbfd53271a144075b49d01a75a1da33ccb6e
offl <11556157+offl@users.noreply.github.com>
2021-09-09 23:54:05 +0300

    Scripts/Spells: Script for 'Injured' (22269) (#26882)

---
+++ sql/updates/world/3.3.5/2021_09_09_10_world.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `spell_script_names` WHERE `spell_id` = 22269 AND `ScriptName` = 'spell_gen_injured';
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(22269,'spell_gen_injured');
