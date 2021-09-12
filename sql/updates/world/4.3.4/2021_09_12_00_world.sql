e2f388fc397422223053e0b84af2d14278c4b28a
Ovahlord <dreadkiller@gmx.de>
2021-09-12 14:52:45 +0200

    DB/Gilneas: respawned Bloodfang Worgen and tied some creatures to the correct terrain swap map for correct pathfinding and line of sight

---
+++ sql/updates/world/4.3.4/2021_09_12_00_world.sql
@@ -0,0 +1,86 @@
+DELETE FROM `creature` WHERE `guid` IN (376651, 376630, 376607, 376629, 376606, 376631, 376604, 376633, 376601, 376634, 376648, 376635, 376650, 376637, 376649, 376657, 376658, 376659, 376661, 376660, 376656, 376655, 376654, 376653, 376646, 376645, 376591, 376592, 376593, 376587, 376585, 376584, 376580, 376577, 376574, 376564, 376560, 376528, 376526, 376518, 376519, 376517, 376515, 376514, 376510, 376504, 376502, 376500, 376499, 376496, 376488, 376487, 376486);
+DELETE FROM `creature_addon` WHERE `guid` IN (376651, 376630, 376607, 376629, 376606, 376631, 376604, 376633, 376601, 376634, 376648, 376635, 376650, 376637, 376649, 376657, 376658, 376659, 376661, 376660, 376656, 376655, 376654, 376653, 376646, 376645, 376591, 376592, 376593, 376587, 376585, 376584, 376580, 376577, 376574, 376564, 376560, 376528, 376526, 376518, 376519, 376517, 376515, 376514, 376510, 376504, 376502, 376500, 376499, 376496, 376488, 376487, 376486);
+
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `PhaseId`, `PhaseGroup`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `VerifiedBuild`) VALUES
+(376657, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1653.6458740234375, 1379.4913330078125, 15.60750007629394531, 3.996803998947143554, 120, 0, 0, 45, 0, 0, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer)
+(376658, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1654.607666015625, 1385.7379150390625, 16.42245864868164062, 0, 120, 0, 0, 38, 0, 0, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer)
+(376659, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1658.25, 1382.9478759765625, 15.21820068359375, 3.996803998947143554, 120, 0, 0, 38, 0, 0, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer)
+(376661, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1659.5555419921875, 1387.951416015625, 15.21820068359375, 3.996803998947143554, 120, 0, 0, 38, 0, 0, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer)
+(376660, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1662.30908203125, 1343.8992919921875, 15.21820259094238281, 0, 120, 10, 0, 38, 0, 1, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer) (possible waypoints or random movement)
+(376656, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1672.0972900390625, 1372.9114990234375, 15.43614006042480468, 0, 120, 10, 0, 38, 0, 1, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer) (possible waypoints or random movement)
+(376655, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1679.9791259765625, 1336.9896240234375, 15.21820354461669921, 0, 120, 0, 0, 38, 0, 0, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer)
+(376654, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1682.0416259765625, 1414.9617919921875, 21.75196075439453125, 0, 120, 10, 0, 38, 0, 1, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer) (possible waypoints or random movement)
+(376653, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1682.28125, 1349.592041015625, 15.21820259094238281, 0, 120, 0, 0, 38, 0, 0, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer)
+(376646, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1683.6510009765625, 1401.7379150390625, 21.75196075439453125, 0, 120, 0, 0, 38, 0, 0, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer)
+(376645, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1693.439208984375, 1387.392333984375, 21.75196075439453125, 2.600540637969970703, 120, 0, 0, 38, 0, 0, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer)
+(376591, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1696.24658203125, 1370.3941650390625, 23.10253334045410156, 0, 120, 0, 0, 38, 0, 0, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer)
+(376592, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1700.71533203125, 1377.5225830078125, 23.0797576904296875, 0, 120, 0, 0, 38, 0, 0, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer)
+(376593, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1704.798583984375, 1425.265625, 21.75196075439453125, 0, 120, 0, 0, 38, 0, 0, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer)
+(376587, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1711.1353759765625, 1439.43408203125, 21.75196075439453125, 0, 120, 0, 0, 45, 0, 0, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer)
+(376585, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1715.032958984375, 1413.7396240234375, 21.75195884704589843, 0, 120, 0, 0, 38, 0, 0, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer)
+(376584, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1721.2725830078125, 1391.6788330078125, 21.75195693969726562, 0, 120, 0, 0, 45, 0, 0, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer)
+(376580, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1723.076416015625, 1354.2396240234375, 19.91219329833984375, 0, 120, 0, 0, 45, 0, 0, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer)
+(376577, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1723.5347900390625, 1395.2117919921875, 21.75195693969726562, 0, 120, 0, 0, 38, 0, 0, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer)
+(376574, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1729.30908203125, 1399.4791259765625, 21.75195693969726562, 0, 120, 0, 0, 38, 0, 0, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer)
+(376564, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1733.3021240234375, 1417.5521240234375, 21.75196266174316406, 0, 120, 0, 0, 38, 0, 0, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer)
+(376560, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1739.8316650390625, 1378.421875, 20.045440673828125, 0, 120, 0, 0, 38, 0, 0, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer)
+(376528, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1742.90625, 1420.7552490234375, 23.06669235229492187, 5.864306449890136718, 120, 0, 0, 38, 0, 0, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer)
+(376526, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1742.90625, 1420.7552490234375, 23.06669235229492187, 5.864306449890136718, 120, 0, 0, 38, 0, 0, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer)
+(376518, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1742.90625, 1420.7552490234375, 23.06669235229492187, 5.864306449890136718, 120, 0, 0, 45, 0, 0, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer)
+(376519, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1742.90625, 1420.7552490234375, 23.06669235229492187, 5.864306449890136718, 120, 0, 0, 38, 0, 0, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer)
+(376517, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1742.90625, 1420.7552490234375, 23.06669235229492187, 5.864306449890136718, 120, 0, 0, 45, 0, 0, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer)
+(376515, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1745.3031005859375, 1429.6448974609375, 52.75605010986328125, 4.820647716522216796, 120, 10, 0, 45, 0, 1, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer) (possible waypoints or random movement)
+(376514, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1763.907958984375, 1398.4913330078125, 20.0507965087890625, 0, 120, 0, 0, 38, 0, 0, 0, 0, 0, 15595), -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer)
+(376510, 35118, 654, 4755, 4757, 1, 170, 0, 0, 0, -1768.831787109375, 1433.2220458984375, 20.02196884155273437, 3.835277318954467773, 120, 0, 0, 38, 0, 0, 0, 0, 0, 15595); -- Bloodfang Worgen (Area: Military District - Difficulty: 0) (Auras: 83470 - Arcane Missiles Trainer)
+
+ -- Bloodfang Worgen
+SET @ENTRY := 35118;
+DELETE FROM `smart_scripts` WHERE `entryOrGuid` = @ENTRY AND `source_type` = 0;
+UPDATE `creature_template` SET `AIName` = "SmartAI", `ScriptName` = "" WHERE `entry` = @ENTRY;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(@ENTRY, 0, 0, 0, 2, 0, 100, 1, 0, 30, 0, 0, 11, 8599, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "When health between 0%-30%% (check once) - Self: Cast spell Enrage (8599) on Self // Bloodfang Worgen - Between 0-30% Health - Cast 'Enrage' (No Repeat)"),
+(@ENTRY, 0, 1, 0, 8, 0, 100, 0, 5143, 0, 0, 0, 33, 44175, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "On spell Arcane Missiles (5143) hit - Caster: Give kill credit Spell Practice Credit (44175)"),
+(@ENTRY, 0, 2, 0, 8, 0, 100, 0, 2098, 0, 0, 0, 33, 44175, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "On spell Eviscerate (2098) hit - Caster: Give kill credit Spell Practice Credit (44175)"),
+(@ENTRY, 0, 3, 0, 8, 0, 100, 0, 56641, 0, 0, 0, 33, 44175, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "On spell Steady Shot (56641) hit - Caster: Give kill credit Spell Practice Credit (44175)"),
+(@ENTRY, 0, 4, 0, 8, 0, 100, 0, 348, 0, 0, 0, 33, 44175, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "On spell Immolate (348) hit - Caster: Give kill credit Spell Practice Credit (44175)");
+
+-- Bloodfang Worgen
+SET @ENTRY := -376661;
+DELETE FROM `smart_scripts` WHERE `entryOrGuid` = @ENTRY AND `source_type` = 0;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(@ENTRY, 0, 0, 0, 2, 0, 100, 1, 0, 30, 0, 0, 11, 8599, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "When health between 0%-30%% (check once) - Self: Cast spell Enrage (8599) on Self // Bloodfang Worgen - Between 0-30% Health - Cast 'Enrage' (No Repeat)"),
+(@ENTRY, 0, 1, 0, 63, 0, 100, 0, 0, 0, 0, 0, 49, 0, 0, 0, 0, 0, 0, 10, 376501, 0, 0, 0, 0, 0, 0, "When just created - Self: Attack Creature with guid 376501"),
+(@ENTRY, 0, 2, 0, 8, 0, 100, 0, 5143, 0, 0, 0, 33, 44175, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "On spell Arcane Missiles (5143) hit - Caster: Give kill credit Spell Practice Credit (44175)"),
+(@ENTRY, 0, 3, 0, 8, 0, 100, 0, 2098, 0, 0, 0, 33, 44175, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "On spell Eviscerate (2098) hit - Caster: Give kill credit Spell Practice Credit (44175)"),
+(@ENTRY, 0, 4, 0, 8, 0, 100, 0, 56641, 0, 0, 0, 33, 44175, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "On spell Steady Shot (56641) hit - Caster: Give kill credit Spell Practice Credit (44175)"),
+(@ENTRY, 0, 5, 0, 8, 0, 100, 0, 348, 0, 0, 0, 33, 44175, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "On spell Immolate (348) hit - Caster: Give kill credit Spell Practice Credit (44175)");
+
+-- Bloodfang Worgen
+SET @ENTRY := -376657;
+DELETE FROM `smart_scripts` WHERE `entryOrGuid` = @ENTRY AND `source_type` = 0;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(@ENTRY, 0, 0, 0, 2, 0, 100, 1, 0, 30, 0, 0, 11, 8599, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "When health between 0%-30%% (check once) - Self: Cast spell Enrage (8599) on Self // Bloodfang Worgen - Between 0-30% Health - Cast 'Enrage' (No Repeat)"),
+(@ENTRY, 0, 1, 0, 63, 0, 100, 0, 0, 0, 0, 0, 49, 0, 0, 0, 0, 0, 0, 10, 376501, 0, 0, 0, 0, 0, 0, "When just created - Self: Attack Creature with guid 376501"),
+(@ENTRY, 0, 2, 0, 8, 0, 100, 0, 5143, 0, 0, 0, 33, 44175, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "On spell Arcane Missiles (5143) hit - Caster: Give kill credit Spell Practice Credit (44175)"),
+(@ENTRY, 0, 3, 0, 8, 0, 100, 0, 2098, 0, 0, 0, 33, 44175, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "On spell Eviscerate (2098) hit - Caster: Give kill credit Spell Practice Credit (44175)"),
+(@ENTRY, 0, 4, 0, 8, 0, 100, 0, 56641, 0, 0, 0, 33, 44175, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "On spell Steady Shot (56641) hit - Caster: Give kill credit Spell Practice Credit (44175)"),
+(@ENTRY, 0, 5, 0, 8, 0, 100, 0, 348, 0, 0, 0, 33, 44175, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "On spell Immolate (348) hit - Caster: Give kill credit Spell Practice Credit (44175)");
+
+-- Bloodfang Worgen
+SET @ENTRY := -376661;
+DELETE FROM `smart_scripts` WHERE `entryOrGuid` = @ENTRY AND `source_type` = 0;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(@ENTRY, 0, 0, 0, 2, 0, 100, 1, 0, 30, 0, 0, 11, 8599, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, "When health between 0%-30%% (check once) - Self: Cast spell Enrage (8599) on Self // Bloodfang Worgen - Between 0-30% Health - Cast 'Enrage' (No Repeat)"),
+(@ENTRY, 0, 1, 0, 63, 0, 100, 0, 0, 0, 0, 0, 49, 0, 0, 0, 0, 0, 0, 10, 376501, 0, 0, 0, 0, 0, 0, "When just created - Self: Attack Creature with guid 376501"),
+(@ENTRY, 0, 2, 0, 8, 0, 100, 0, 5143, 0, 0, 0, 33, 44175, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "On spell Arcane Missiles (5143) hit - Caster: Give kill credit Spell Practice Credit (44175)"),
+(@ENTRY, 0, 3, 0, 8, 0, 100, 0, 2098, 0, 0, 0, 33, 44175, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "On spell Eviscerate (2098) hit - Caster: Give kill credit Spell Practice Credit (44175)"),
+(@ENTRY, 0, 4, 0, 8, 0, 100, 0, 56641, 0, 0, 0, 33, 44175, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "On spell Steady Shot (56641) hit - Caster: Give kill credit Spell Practice Credit (44175)"),
+(@ENTRY, 0, 5, 0, 8, 0, 100, 0, 348, 0, 0, 0, 33, 44175, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, "On spell Immolate (348) hit - Caster: Give kill credit Spell Practice Credit (44175)");
+
+-- Correct terrain swap map for creatures to use correct pathfinding and line of sight
+-- Bloodfang Worgen
+UPDATE `creature` SET `terrainSwapMap`= 638 WHERE `guid` IN (376657, 376658, 376659, 376661, 376660, 376656, 376655, 376654, 376653, 376646, 376645, 376591, 376592, 376593, 376587, 376585, 376584, 376580, 376577, 376574, 376564, 376560, 376528, 376526, 376518, 376519, 376517, 376515, 376514, 376510);
+-- Gilnean Royal Guard
+UPDATE `creature` SET `terrainSwapMap`= 638 WHERE `guid` IN (376571, 376567, 376566, 376558, 376553, 376549, 376548, 376546, 376539, 376541, 376538, 376533, 376531, 376530, 376529, 376524, 376522, 376520, 376516, 376512);
+
+UPDATE `creature_template` SET `DamageModifier`= 4 WHERE `entry`= 35839;
