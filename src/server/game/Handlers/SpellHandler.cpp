febe029f617153fcb3e8b624630b865e331d1bab
stoneharry <stoneharryjames@gmail.com>
2021-09-12 14:35:54 +0100

    Refactor const SpellInfo declaration
    Co-authored-by: Shauren <shauren.trinity@gmail.com>

--- src/server/game/Handlers/SpellHandler.cpp
+++ src/server/game/Handlers/SpellHandler.cpp
@@ -591,7 +591,7 @@ void WorldSession::HandleSelfResOpcode(WorldPacket & /*recvData*/)
 {
     TC_LOG_DEBUG("network", "WORLD: CMSG_SELF_RES");                  // empty opcode
 
-    if (const SpellInfo* spell = sSpellMgr->GetSpellInfo(_player->GetUInt32Value(PLAYER_SELF_RES_SPELL)))
+    if (SpellInfo const* spell = sSpellMgr->GetSpellInfo(_player->GetUInt32Value(PLAYER_SELF_RES_SPELL)))
     {
         if (_player->HasAuraType(SPELL_AURA_PREVENT_RESURRECTION) && !spell->HasAttribute(SPELL_ATTR7_BYPASS_NO_RESURRECT_AURA))
             return; // silent return, client should display error by itself and not send this opcode
