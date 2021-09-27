9aeac0b1a3ef93714d3ddea49dc3f98483275c64
xuehyc <49224813+xuehyc@users.noreply.github.com>
2021-09-27 20:58:29 +0800

    Update Main.cpp

--- src/server/worldserver/Main.cpp
+++ src/server/worldserver/Main.cpp
@@ -72,6 +72,7 @@ namespace fs = boost::filesystem;
 #ifdef _WIN32
 #include "ServiceWin32.h"
 char serviceName[] = "worldserver";
+//服务器名称设置
 char serviceLongName[] = "TrinityCore world service";
 char serviceDescription[] = "TrinityCore World of Warcraft emulator world service";
 /*
