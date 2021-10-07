d2d734da3afcd2b6dc3778a5d2276cee3426a540
Legends of Azeroth <81566364+NemoPRM@users.noreply.github.com>
2021-10-07 16:39:58 +0500

    update
    update

---
+++ src/server/scripts/DarkmoonIsland/DarkmoonIsland.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2017-2019 AshamaneProject <https://github.com/AshamaneProject>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DARKMOON_ISLAND_H
+#define DARKMOON_ISLAND_H
+
+enum darkmoonIslandQuests
+{
+    QUEST_WHACK_A_GNOLL         = 29463,
+    QUEST_HUMANOID_CANNONBALL   = 29436
+};
+
+enum darkmoonIslandItems
+{
+    ITEM_DARKMOON_TOKEN         = 71083
+};
+
+enum darkmoonIslandSpells
+{
+    SPELL_WHEE                  = 46668
+};
+
+enum darkmoonIslandGameObjects
+{
+    GOB_DARKMOON_CAROUSEL       = 216666
+};
+
+#endif
