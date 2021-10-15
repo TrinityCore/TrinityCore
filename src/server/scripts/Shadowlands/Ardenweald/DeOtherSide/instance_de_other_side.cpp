af8e9b621c449d991567934ddc91a7152a352a00
devovh <devovh@gmail.com>
2021-10-15 23:00:22 +0200

    Core/Misc - 409 files changed.

--- src/server/scripts/Shadowlands/Ardenweald/DeOtherSide/instance_de_other_side.cpp
+++ src/server/scripts/Shadowlands/Ardenweald/DeOtherSide/instance_de_other_side.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Thordekk
+ * This file is part of the TrinityCore Project. See AUTHORS file for Copyright information
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+ 
 #include "ScriptMgr.h"
 #include "Player.h"
 #include "InstanceScript.h"
--- src/server/scripts/Shadowlands/TheMaw/instance_torgast_tower_of_the_damned.cpp
+++ src/server/scripts/Shadowlands/TheMaw/instance_torgast_tower_of_the_damned.cpp
@@ -1,3 +1,20 @@
+/*
+ * This file is part of the TrinityCore Project. See AUTHORS file for Copyright information
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
 #include "ScriptMgr.h"
 #include "Player.h"
 #include "AreaTrigger.h"
@@ -5,7 +22,6 @@
 #include "InstanceScript.h"
 #include "GameObject.h"
 #include "GameObjectAI.h"
-#include "torghast_tower_of_the_damned.h"
 
 //2162
 struct instance_torgast_tower_of_the_damned : public InstanceScript
