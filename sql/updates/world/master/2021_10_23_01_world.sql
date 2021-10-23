dc14f019835f50fcf31d0d0eac0e8379d084c3f1
Shauren <shauren.trinity@gmail.com>
2021-10-23 13:09:55 +0200

    Update and rename 9999_99_99_99_world.sql to 2021_10_23_01_world.sql

--- sql/updates/world/master/2021_10_23_01_world.sql
+++ sql/updates/world/master/2021_10_23_01_world.sql
@@ -1,6 +1,5 @@
 -- 
-ALTER TABLE `creature_text`
-    ADD COLUMN `SoundPlayType` TINYINT UNSIGNED NOT NULL DEFAULT '0' AFTER `Sound`;
+ALTER TABLE `creature_text` ADD `SoundPlayType` tinyint(3) UNSIGNED NOT NULL DEFAULT '0' AFTER `Sound`;
     
 UPDATE `creature_text` SET `SoundPlayType`=1 WHERE `CreatureID`=130986;
 
