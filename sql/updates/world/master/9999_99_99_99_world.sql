283e4b22ff0371b89b1e916aa862dc8a6f8606e3
ModoX <moardox@gmail.com>
2021-10-28 00:43:16 +0200

    fix overridden data of existing creatures

--- sql/updates/world/master/9999_99_99_99_world.sql
+++ sql/updates/world/master/9999_99_99_99_world.sql
@@ -2,6 +2,7 @@
 ALTER TABLE `creature_addon` ADD COLUMN `noNPCDamageBelowHealthPct` TINYINT UNSIGNED NOT NULL DEFAULT '0' AFTER `auras`;
 ALTER TABLE `creature_template_addon` ADD COLUMN `noNPCDamageBelowHealthPct` TINYINT UNSIGNED NOT NULL DEFAULT '0' AFTER `auras`;
 
+-- transition from old sparring
 DELETE FROM `creature_template_addon` WHERE `entry` IN(32882,32883,32885,32886,32907,32908);
 INSERT INTO `creature_template_addon` (`entry`, `bytes2`, `noNPCDamageBelowHealthPct`) VALUES
 (32882, 1, 100),
@@ -9,3 +10,6 @@ INSERT INTO `creature_template_addon` (`entry`, `bytes2`, `noNPCDamageBelowHealt
 (32885, 1, 100),
 (32907, 1, 100),
 (32908, 1, 100);
+
+-- fix overridden data of existing creatures
+UPDATE `creature_addon` SET `noNPCDamageBelowHealthPct`=85 WHERE `guid`=454966;
