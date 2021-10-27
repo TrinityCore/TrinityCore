allankrdec/TrinityCore-10-2024

Core/Auras: Convert most rules from Aura::CanBeSaved to a custom spell attribute

src/server/game/Spells/SpellMgr.cpp
@@ -2676,6 +2676,23 @@ void SpellMgr::LoadSpellInfoCustomAttributes()
                     break;
             }
 
+            switch (effect->ApplyAuraName)
+            {
+                case SPELL_AURA_OPEN_STABLE:    // No point in saving this, since the stable dialog can't be open on aura load anyway.
+                // Auras that require both caster & target to be in world cannot be saved
+                case SPELL_AURA_CONTROL_VEHICLE:
+                case SPELL_AURA_BIND_SIGHT:
+                case SPELL_AURA_MOD_POSSESS:
+                case SPELL_AURA_MOD_POSSESS_PET:
+                case SPELL_AURA_MOD_CHARM:
+                case SPELL_AURA_AOE_CHARM:
+                // Controlled by Battleground
+                case SPELL_AURA_BATTLEGROUND_PLAYER_POSITION:
+                case SPELL_AURA_BATTLEGROUND_PLAYER_POSITION_FACTIONAL:
+                    spellInfoMutable->AttributesCu |= SPELL_ATTR0_CU_AURA_CANNOT_BE_SAVED;
+                    break;
+            }
+
             switch (effect->Effect)
             {
                 case SPELL_EFFECT_SCHOOL_DAMAGE:

TrinityCore/TrinityCore

(cherry picked from commit b4aa698acb88e1a74bc22d97279148a567b88cad)
(cherry picked from commit 2bea816bbec6ba9d50f7bd6725806e81c6cafaae)

src/server/game/Spells/SpellMgr.cpp
@@ -2673,6 +2673,23 @@ void SpellMgr::LoadSpellInfoCustomAttributes()
                     break;
             }
 
+            switch (spellEffectInfo.ApplyAuraName)
+            {
+                case SPELL_AURA_CONVERT_RUNE:   // Can't be saved - aura handler relies on calculated amount and changes it
+                case SPELL_AURA_OPEN_STABLE:    // No point in saving this, since the stable dialog can't be open on aura load anyway.
+                // Auras that require both caster & target to be in world cannot be saved
+                case SPELL_AURA_CONTROL_VEHICLE:
+                case SPELL_AURA_BIND_SIGHT:
+                case SPELL_AURA_MOD_POSSESS:
+                case SPELL_AURA_MOD_POSSESS_PET:
+                case SPELL_AURA_MOD_CHARM:
+                case SPELL_AURA_AOE_CHARM:
+                    spellInfo->AttributesCu |= SPELL_ATTR0_CU_AURA_CANNOT_BE_SAVED;
+                    break;
+                default:
+                    break;
+            }
+
             switch (spellEffectInfo.Effect)
             {
                 case SPELL_EFFECT_SCHOOL_DAMAGE:
@@ -2949,7 +2966,7 @@ void SpellMgr::LoadSpellInfoCustomAttributes()
     {
         if (uint32 spellId = liquid->SpellID)
             if (SpellInfo* spellInfo = _GetSpellInfo(spellId))
-                spellInfo->AttributesCu |= SPELL_ATTR0_CU_LIQUID_AURA;
+                spellInfo->AttributesCu |= SPELL_ATTR0_CU_AURA_CANNOT_BE_SAVED;
     }
 
     TC_LOG_INFO("server.loading", ">> Loaded SpellInfo custom attributes in %u ms", GetMSTimeDiffToNow(oldMSTime));