5a3c9c46f654c1cecf4aebb2c53e58e220c7aead
offl <offl@users.noreply.github.com>
2021-10-30 19:41:24 +0300

    DB/Creature: Add pre-cata equipment to 2 creatures
    Closes #27209

---
+++ sql/updates/world/3.3.5/2021_10_30_00_world_335.sql
@@ -0,0 +1,4 @@
+-- Ancient creature_equip_template with item models, first found item with that model
+UPDATE `creature_equip_template` SET `ItemID1` = 1907, `ItemID2` = 0, `VerifiedBuild` = -1 WHERE `CreatureID` = 3203;
+-- Vmangos
+UPDATE `creature_equip_template` SET `ItemID1` = 2177, `VerifiedBuild` = 31727 WHERE `CreatureID` = 4667;
