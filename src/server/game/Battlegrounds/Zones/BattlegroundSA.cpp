fefcec4d35207f92832ba98ffa2c061299a57d8a
Gildor <gildor55@gmail.com>
2021-11-16 13:23:08 +0100

    Core/Battlegrounds: Avoid teleport players when relogin inside Strand of the Ancients

--- src/server/game/Battlegrounds/Zones/BattlegroundSA.cpp
+++ src/server/game/Battlegrounds/Zones/BattlegroundSA.cpp
@@ -489,7 +489,8 @@ void BattlegroundSA::AddPlayer(Player* player)
 
     SendTransportInit(player);
 
-    TeleportToEntrancePosition(player);
+    if (!isInBattleground)
+        TeleportToEntrancePosition(player);
 }
 
 void BattlegroundSA::RemovePlayer(Player* /*player*/, ObjectGuid /*guid*/, uint32 /*team*/) { }
