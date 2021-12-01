c9cde3bbf64c23d8521905ee8ba0b1433f40b073
Grindelwald <137000133@qq.com>
2021-12-01 10:45:31 +0800

    Update 9999_99_99_99_world.sql

--- sql/custom/world/9999_99_99_99_world.sql
+++ sql/custom/world/9999_99_99_99_world.sql
@@ -1,2 +1,1 @@
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (66041, 'spell_gen_bountiful_feast');
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (66478, 'spell_gen_bountiful_feast');
