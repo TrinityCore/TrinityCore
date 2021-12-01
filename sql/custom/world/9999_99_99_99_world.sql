42f0048187377525f2e0894a0be2556fd7b5571a
Grindelwald <137000133@qq.com>
2021-12-01 11:10:23 +0800

    Update 9999_99_99_99_world.sql

--- sql/custom/world/9999_99_99_99_world.sql
+++ sql/custom/world/9999_99_99_99_world.sql
@@ -1,1 +1,4 @@
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (66041, 'spell_gen_bountiful_feast');
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (66041,66478);
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(66041,'spell_pilgrims_bounty_dink'),
+(66478,'spell_pilgrims_bounty_feast');
