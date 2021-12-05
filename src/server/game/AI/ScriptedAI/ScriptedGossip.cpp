allankrdec/TrinityCore-10-2024

Core/AI: variable naming standarization
plus minimum codestyle changes

src/server/game/AI/ScriptedAI/ScriptedGossip.cpp
@@ -19,14 +19,46 @@
 #include "Player.h"
 #include "Creature.h"
 
-uint32 GetGossipActionFor(Player* player, uint32 gossipListId) { return player->PlayerTalkClass->GetGossipOptionAction(gossipListId); }
-void ClearGossipMenuFor(Player* player) { player->PlayerTalkClass->ClearMenus(); }
+uint32 GetGossipActionFor(Player* player, uint32 gossipListId)
+{
+    return player->PlayerTalkClass->GetGossipOptionAction(gossipListId);
+}
+
+void ClearGossipMenuFor(Player* player)
+{
+    player->PlayerTalkClass->ClearMenus();
+}
+
 // Using provided text, not from DB
-void AddGossipItemFor(Player* player, uint32 icon, std::string const& text, uint32 sender, uint32 action) { player->PlayerTalkClass->GetGossipMenu().AddMenuItem(-1, icon, text, sender, action, "", 0); }
+void AddGossipItemFor(Player* player, uint32 icon, std::string const& text, uint32 sender, uint32 action)
+{
+    player->PlayerTalkClass->GetGossipMenu().AddMenuItem(-1, icon, text, sender, action, "", 0);
+}
+
 // Using provided texts, not from DB
-void AddGossipItemFor(Player* player, uint32 icon, std::string const& text, uint32 sender, uint32 action, std::string const& popupText, uint32 popupMoney, bool coded) { player->PlayerTalkClass->GetGossipMenu().AddMenuItem(-1, icon, text, sender, action, popupText, popupMoney, coded); }
+void AddGossipItemFor(Player* player, uint32 icon, std::string const& text, uint32 sender, uint32 action, std::string const& popupText, uint32 popupMoney, bool coded)
+{
+    player->PlayerTalkClass->GetGossipMenu().AddMenuItem(-1, icon, text, sender, action, popupText, popupMoney, coded);
+}
+
 // Uses gossip item info from DB
-void AddGossipItemFor(Player* player, uint32 gossipMenuID, uint32 gossipMenuItemID, uint32 sender, uint32 action) { player->PlayerTalkClass->GetGossipMenu().AddMenuItem(gossipMenuID, gossipMenuItemID, sender, action); }
-void SendGossipMenuFor(Player* player, uint32 npcTextID, ObjectGuid const& guid) { player->PlayerTalkClass->SendGossipMenu(npcTextID, guid); }
-void SendGossipMenuFor(Player* player, uint32 npcTextID, Creature const* creature) { if (creature) SendGossipMenuFor(player, npcTextID, creature->GetGUID()); }
-void CloseGossipMenuFor(Player* player) { player->PlayerTalkClass->SendCloseGossip(); }
+void AddGossipItemFor(Player* player, uint32 gossipMenuID, uint32 gossipMenuItemID, uint32 sender, uint32 action)
+{
+    player->PlayerTalkClass->GetGossipMenu().AddMenuItem(gossipMenuID, gossipMenuItemID, sender, action);
+}
+
+void SendGossipMenuFor(Player* player, uint32 npcTextID, ObjectGuid const& guid)
+{
+    player->PlayerTalkClass->SendGossipMenu(npcTextID, guid);
+}
+
+void SendGossipMenuFor(Player* player, uint32 npcTextID, Creature const* creature)
+{
+    if (creature)
+        SendGossipMenuFor(player, npcTextID, creature->GetGUID());
+}
+
+void CloseGossipMenuFor(Player* player)
+{
+    player->PlayerTalkClass->SendCloseGossip();
+}

TrinityCore/TrinityCore

(cherry picked from commit 179c7da1fc264b1444aa50632317e16dca02f78b)

src/server/game/AI/ScriptedAI/ScriptedGossip.cpp
@@ -19,14 +19,46 @@
 #include "Creature.h"
 #include "Player.h"
 
-uint32 GetGossipActionFor(Player* player, uint32 gossipListId) { return player->PlayerTalkClass->GetGossipOptionAction(gossipListId); }
-void ClearGossipMenuFor(Player* player) { player->PlayerTalkClass->ClearMenus(); }
+uint32 GetGossipActionFor(Player* player, uint32 gossipListId)
+{
+    return player->PlayerTalkClass->GetGossipOptionAction(gossipListId);
+}
+
+void ClearGossipMenuFor(Player* player)
+{
+    player->PlayerTalkClass->ClearMenus();
+}
+
 // Using provided text, not from DB
-void AddGossipItemFor(Player* player, GossipOptionIcon icon, std::string const& text, uint32 sender, uint32 action) { player->PlayerTalkClass->GetGossipMenu().AddMenuItem(-1, icon, text, sender, action, "", 0); }
+void AddGossipItemFor(Player* player, GossipOptionIcon icon, std::string const& text, uint32 sender, uint32 action)
+{
+    player->PlayerTalkClass->GetGossipMenu().AddMenuItem(-1, icon, text, sender, action, "", 0);
+}
+
 // Using provided texts, not from DB
-void AddGossipItemFor(Player* player, GossipOptionIcon icon, std::string const& text, uint32 sender, uint32 action, std::string const& popupText, uint32 popupMoney, bool coded) { player->PlayerTalkClass->GetGossipMenu().AddMenuItem(-1, icon, text, sender, action, popupText, popupMoney, coded); }
+void AddGossipItemFor(Player* player, GossipOptionIcon icon, std::string const& text, uint32 sender, uint32 action, std::string const& popupText, uint32 popupMoney, bool coded)
+{
+    player->PlayerTalkClass->GetGossipMenu().AddMenuItem(-1, icon, text, sender, action, popupText, popupMoney, coded);
+}
+
 // Uses gossip item info from DB
-void AddGossipItemFor(Player* player, uint32 gossipMenuID, uint32 gossipMenuItemID, uint32 sender, uint32 action) { player->PlayerTalkClass->GetGossipMenu().AddMenuItem(gossipMenuID, gossipMenuItemID, sender, action); }
-void SendGossipMenuFor(Player* player, uint32 npcTextID, ObjectGuid const& guid) { player->PlayerTalkClass->SendGossipMenu(npcTextID, guid); }
-void SendGossipMenuFor(Player* player, uint32 npcTextID, Creature const* creature) { if (creature) SendGossipMenuFor(player, npcTextID, creature->GetGUID()); }
-void CloseGossipMenuFor(Player* player) { player->PlayerTalkClass->SendCloseGossip(); }
+void AddGossipItemFor(Player* player, uint32 gossipMenuID, uint32 gossipMenuItemID, uint32 sender, uint32 action)
+{
+    player->PlayerTalkClass->GetGossipMenu().AddMenuItem(gossipMenuID, gossipMenuItemID, sender, action);
+}
+
+void SendGossipMenuFor(Player* player, uint32 npcTextID, ObjectGuid const& guid)
+{
+    player->PlayerTalkClass->SendGossipMenu(npcTextID, guid);
+}
+
+void SendGossipMenuFor(Player* player, uint32 npcTextID, Creature const* creature)
+{
+    if (creature)
+        SendGossipMenuFor(player, npcTextID, creature->GetGUID());
+}
+
+void CloseGossipMenuFor(Player* player)
+{
+    player->PlayerTalkClass->SendCloseGossip();
+}