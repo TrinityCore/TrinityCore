allankrdec/TrinityCore-10-2024

Core/Chat: Custom channel preservation rewrite. Channel data is now loaded at startup, and written to the DB periodically, instead of both things happening in real time.

src/server/game/Chat/Channels/ChannelMgr.cpp
@@ -34,7 +34,78 @@ ChannelMgr::~ChannelMgr()
         delete itr->second;
 }
 
-ChannelMgr* ChannelMgr::forTeam(uint32 team)
+/*static*/ void ChannelMgr::LoadFromDB()
+{
+    if (!sWorld->getBoolConfig(CONFIG_PRESERVE_CUSTOM_CHANNELS))
+    {
+        TC_LOG_INFO("server.loading", ">> Loaded 0 custom chat channels. Custom channel saving is disabled.");
+        return;
+    }
+
+    uint32 oldMSTime = getMSTime();
+    if (uint32 days = sWorld->getIntConfig(CONFIG_PRESERVE_CUSTOM_CHANNEL_DURATION))
+    {
+        PreparedStatement* stmt = CharacterDatabase.GetPreparedStatement(CHAR_DEL_OLD_CHANNELS);
+        stmt->setUInt32(0, days * DAY);
+        CharacterDatabase.Execute(stmt);
+    }
+
+    QueryResult result = CharacterDatabase.Query("SELECT name, team, announce, ownership, password, bannedList FROM channels");
+    if (!result)
+    {
+        TC_LOG_INFO("server.loading", ">> Loaded 0 custom chat channels. DB table `channels` is empty.");
+        return;
+    }
+
+    std::vector<std::pair<std::string, uint32>> toDelete;
+    uint32 count = 0;
+    do
+    {
+        Field* fields = result->Fetch();
+        std::string dbName = fields[0].GetString(); // may be different - channel names are case insensitive
+        uint32 team = fields[1].GetUInt32();
+        bool dbAnnounce = fields[2].GetBool();
+        bool dbOwnership = fields[3].GetBool();
+        std::string dbPass = fields[4].GetString();
+        std::string dbBanned = fields[5].GetString();
+
+        std::wstring channelName;
+        if (!Utf8toWStr(dbName, channelName))
+        {
+            TC_LOG_ERROR("server.loading", "Failed to load custom chat channel '%s' from database - invalid utf8 sequence? Deleted.", dbName.c_str());
+            toDelete.push_back({ dbName, team });
+            continue;
+        }
+
+        ChannelMgr* mgr = forTeam(team);
+        if (!mgr)
+        {
+            TC_LOG_ERROR("server.loading", "Failed to load custom chat channel '%s' from database - invalid team %u. Deleted.", dbName.c_str());
+            toDelete.push_back({ dbName, team });
+            continue;
+        }
+
+        Channel* channel = new Channel(dbName, team, dbBanned);
+        channel->SetAnnounce(dbAnnounce);
+        channel->SetOwnership(dbOwnership);
+        channel->SetPassword(dbPass);
+        ASSERT_NOTNULL(forTeam(team))->_customChannels.emplace(channelName, channel);
+
+        ++count;
+    } while (result->NextRow());
+
+    for (auto pair : toDelete)
+    {
+        PreparedStatement* stmt = CharacterDatabase.GetPreparedStatement(CHAR_DEL_CHANNEL);
+        stmt->setString(0, pair.first);
+        stmt->setUInt32(1, pair.second);
+        CharacterDatabase.Execute(stmt);
+    }
+
+    TC_LOG_INFO("server.loading", ">> Loaded %u custom chat channels in %u ms", count, GetMSTimeDiffToNow(oldMSTime));
+}
+
+/*static*/ ChannelMgr* ChannelMgr::forTeam(uint32 team)
 {
     static ChannelMgr allianceChannelMgr(ALLIANCE);
     static ChannelMgr hordeChannelMgr(HORDE);
@@ -74,6 +145,12 @@ Channel* ChannelMgr::GetChannelForPlayerByNamePart(std::string const& namePart, 
     return nullptr;
 }
 
+void ChannelMgr::SaveToDB()
+{
+    for (auto pair : _customChannels)
+        pair.second->UpdateChannelInDB();
+}
+
 Channel* ChannelMgr::GetSystemChannel(uint32 channelId, AreaTableEntry const* zoneEntry)
 {
     ChatChannelsEntry const* channelEntry = sChatChannelsStore.AssertEntry(channelId);
@@ -105,21 +182,13 @@ Channel* ChannelMgr::CreateCustomChannel(std::string const& name)
         return nullptr;
 
     Channel* newChannel = new Channel(name, _team);
-
-    if (sWorld->getBoolConfig(CONFIG_PRESERVE_CUSTOM_CHANNELS))
-    {
-        PreparedStatement* stmt = CharacterDatabase.GetPreparedStatement(CHAR_INS_CHANNEL);
-        stmt->setString(0, name);
-        stmt->setUInt32(1, _team);
-        CharacterDatabase.Execute(stmt);
-        TC_LOG_DEBUG("chat.system", "Channel(%s) saved in database", name.c_str());
-    }
+    newChannel->SetDirty();
 
     c = newChannel;
     return newChannel;
 }
 
-Channel* ChannelMgr::GetCustomChannel(std::string const& name)
+Channel* ChannelMgr::GetCustomChannel(std::string const& name) const
 {
     std::wstring channelName;
     if (!Utf8toWStr(name, channelName))
@@ -129,28 +198,6 @@ Channel* ChannelMgr::GetCustomChannel(std::string const& name)
     auto itr = _customChannels.find(channelName);
     if (itr != _customChannels.end())
         return itr->second;
-    else if (sWorld->getBoolConfig(CONFIG_PRESERVE_CUSTOM_CHANNELS))
-    {
-        PreparedStatement* stmt = CharacterDatabase.GetPreparedStatement(CHAR_SEL_CHANNEL);
-        stmt->setString(0, name);
-        stmt->setUInt32(1, _team);
-        if (PreparedQueryResult result = CharacterDatabase.Query(stmt))
-        {
-            Field* fields = result->Fetch();
-            std::string dbName = fields[0].GetString(); // may be different - channel names are case insensitive
-            bool dbAnnounce = fields[1].GetBool();
-            bool dbOwnership = fields[2].GetBool();
-            std::string dbPass = fields[3].GetString();
-            std::string dbBanned = fields[4].GetString();
-
-            Channel* channel = new Channel(dbName, _team, dbBanned);
-            channel->SetAnnounce(dbAnnounce);
-            channel->SetOwnership(dbOwnership);
-            channel->SetPassword(dbPass);
-            _customChannels.emplace(channelName, channel);
-            return channel;
-        }
-    }
 
     return nullptr;
 }

src/server/game/World/World.cpp
@@ -30,7 +30,7 @@
 #include "BattlefieldMgr.h"
 #include "BattlegroundMgr.h"
 #include "CalendarMgr.h"
-#include "Channel.h"
+#include "ChannelMgr.h"
 #include "CharacterCache.h"
 #include "CharacterDatabaseCleaner.h"
 #include "Chat.h"
@@ -679,6 +679,7 @@ void World::LoadConfigSettings(bool reload)
     m_int_configs[CONFIG_MAIL_LEVEL_REQ] = sConfigMgr->GetIntDefault("LevelReq.Mail", 1);
     m_bool_configs[CONFIG_PRESERVE_CUSTOM_CHANNELS] = sConfigMgr->GetBoolDefault("PreserveCustomChannels", false);
     m_int_configs[CONFIG_PRESERVE_CUSTOM_CHANNEL_DURATION] = sConfigMgr->GetIntDefault("PreserveCustomChannelDuration", 14);
+    m_int_configs[CONFIG_PRESERVE_CUSTOM_CHANNEL_INTERVAL] = sConfigMgr->GetIntDefault("PreserveCustomChannelInterval", 5);
     m_bool_configs[CONFIG_GRID_UNLOAD] = sConfigMgr->GetBoolDefault("GridUnload", true);
     m_bool_configs[CONFIG_BASEMAP_LOAD_GRIDS] = sConfigMgr->GetBoolDefault("BaseMapLoadAllGrids", false);
     if (m_bool_configs[CONFIG_BASEMAP_LOAD_GRIDS] && m_bool_configs[CONFIG_GRID_UNLOAD])
@@ -2076,6 +2077,8 @@ void World::SetInitialWorldSettings()
 
     m_timers[WUPDATE_WHO_LIST].SetInterval(5 * IN_MILLISECONDS); // update who list cache every 5 seconds
 
+    m_timers[WUPDATE_CHANNEL_SAVE].SetInterval(getIntConfig(CONFIG_PRESERVE_CUSTOM_CHANNEL_INTERVAL) * MINUTE * IN_MILLISECONDS);
+
     //to set mailtimer to return mails every day between 4 and 5 am
     //mailtimer is increased when updating auctions
     //one second is 1000 -(tested on win system)
@@ -2103,8 +2106,8 @@ void World::SetInitialWorldSettings()
     TC_LOG_INFO("server.loading", "Initialize AuctionHouseBot...");
     sAuctionBot->Initialize();
 
-    // Delete all custom channels which haven't been used for PreserveCustomChannelDuration days.
-    Channel::CleanOldChannelsInDB();
+    TC_LOG_INFO("server.loading", "Initializing chat channels...");
+    ChannelMgr::LoadFromDB();
 
     TC_LOG_INFO("server.loading", "Initializing Opcodes...");
     opcodeTable.Initialize();
@@ -2278,6 +2281,20 @@ void World::Update(uint32 diff)
     {
         m_timers[WUPDATE_WHO_LIST].Reset();
         sWhoListStorageMgr->Update();
+    }
+
+    if (m_timers[WUPDATE_CHANNEL_SAVE].Passed())
+    {
+        m_timers[WUPDATE_CHANNEL_SAVE].Reset();
+
+        if (sWorld->getBoolConfig(CONFIG_PRESERVE_CUSTOM_CHANNELS))
+        {
+            ChannelMgr* mgr1 = ASSERT_NOTNULL(ChannelMgr::forTeam(ALLIANCE));
+            mgr1->SaveToDB();
+            ChannelMgr* mgr2 = ASSERT_NOTNULL(ChannelMgr::forTeam(HORDE));
+            if (mgr1 != mgr2)
+                mgr2->SaveToDB();
+        }
     }
 
     /// Handle daily quests reset time

TrinityCore/TrinityCore

(cherry picked from commit fedf1f557b2aa1cbb0f4bc722b24afb59cc30855)

src/server/game/Chat/Channels/ChannelMgr.cpp
@@ -35,7 +35,78 @@ ChannelMgr::~ChannelMgr()
         delete itr->second;
 }
 
-ChannelMgr* ChannelMgr::ForTeam(uint32 team)
+/*static*/ void ChannelMgr::LoadFromDB()
+{
+    if (!sWorld->getBoolConfig(CONFIG_PRESERVE_CUSTOM_CHANNELS))
+    {
+        TC_LOG_INFO("server.loading", ">> Loaded 0 custom chat channels. Custom channel saving is disabled.");
+        return;
+    }
+
+    uint32 oldMSTime = getMSTime();
+    if (uint32 days = sWorld->getIntConfig(CONFIG_PRESERVE_CUSTOM_CHANNEL_DURATION))
+    {
+        CharacterDatabasePreparedStatement* stmt = CharacterDatabase.GetPreparedStatement(CHAR_DEL_OLD_CHANNELS);
+        stmt->setUInt32(0, days * DAY);
+        CharacterDatabase.Execute(stmt);
+    }
+
+    QueryResult result = CharacterDatabase.Query("SELECT name, team, announce, ownership, password, bannedList FROM channels");
+    if (!result)
+    {
+        TC_LOG_INFO("server.loading", ">> Loaded 0 custom chat channels. DB table `channels` is empty.");
+        return;
+    }
+
+    std::vector<std::pair<std::string, uint32>> toDelete;
+    uint32 count = 0;
+    do
+    {
+        Field* fields = result->Fetch();
+        std::string dbName = fields[0].GetString(); // may be different - channel names are case insensitive
+        uint32 team = fields[1].GetUInt32();
+        bool dbAnnounce = fields[2].GetBool();
+        bool dbOwnership = fields[3].GetBool();
+        std::string dbPass = fields[4].GetString();
+        std::string dbBanned = fields[5].GetString();
+
+        std::wstring channelName;
+        if (!Utf8toWStr(dbName, channelName))
+        {
+            TC_LOG_ERROR("server.loading", "Failed to load custom chat channel '%s' from database - invalid utf8 sequence? Deleted.", dbName.c_str());
+            toDelete.push_back({ dbName, team });
+            continue;
+        }
+
+        ChannelMgr* mgr = ForTeam(team);
+        if (!mgr)
+        {
+            TC_LOG_ERROR("server.loading", "Failed to load custom chat channel '%s' from database - invalid team %u. Deleted.", dbName.c_str());
+            toDelete.push_back({ dbName, team });
+            continue;
+        }
+
+        Channel* channel = new Channel(mgr->CreateCustomChannelGuid(), dbName, team, dbBanned);
+        channel->SetAnnounce(dbAnnounce);
+        channel->SetOwnership(dbOwnership);
+        channel->SetPassword(dbPass);
+        mgr->_customChannels.emplace(channelName, channel);
+
+        ++count;
+    } while (result->NextRow());
+
+    for (std::pair<std::string, uint32> const& pair : toDelete)
+    {
+        CharacterDatabasePreparedStatement* stmt = CharacterDatabase.GetPreparedStatement(CHAR_DEL_CHANNEL);
+        stmt->setString(0, pair.first);
+        stmt->setUInt32(1, pair.second);
+        CharacterDatabase.Execute(stmt);
+    }
+
+    TC_LOG_INFO("server.loading", ">> Loaded %u custom chat channels in %u ms", count, GetMSTimeDiffToNow(oldMSTime));
+}
+
+/*static*/ ChannelMgr* ChannelMgr::ForTeam(uint32 team)
 {
     static ChannelMgr allianceChannelMgr(ALLIANCE);
     static ChannelMgr hordeChannelMgr(HORDE);
@@ -75,6 +146,12 @@ Channel* ChannelMgr::GetChannelForPlayerByNamePart(std::string const& namePart, 
     return nullptr;
 }
 
+void ChannelMgr::SaveToDB()
+{
+    for (auto pair : _customChannels)
+        pair.second->UpdateChannelInDB();
+}
+
 Channel* ChannelMgr::GetChannelForPlayerByGuid(ObjectGuid channelGuid, Player* playerSearcher)
 {
     for (Channel* channel : playerSearcher->GetJoinedChannels())
@@ -109,21 +186,13 @@ Channel* ChannelMgr::CreateCustomChannel(std::string const& name)
         return nullptr;
 
     Channel* newChannel = new Channel(CreateCustomChannelGuid(), name, _team);
-
-    if (sWorld->getBoolConfig(CONFIG_PRESERVE_CUSTOM_CHANNELS))
-    {
-        CharacterDatabasePreparedStatement* stmt = CharacterDatabase.GetPreparedStatement(CHAR_INS_CHANNEL);
-        stmt->setString(0, name);
-        stmt->setUInt32(1, _team);
-        CharacterDatabase.Execute(stmt);
-        TC_LOG_DEBUG("chat.system", "Channel(%s) saved in database", name.c_str());
-    }
+    newChannel->SetDirty();
 
     c = newChannel;
     return newChannel;
 }
 
-Channel* ChannelMgr::GetCustomChannel(std::string const& name)
+Channel* ChannelMgr::GetCustomChannel(std::string const& name) const
 {
     std::wstring channelName;
     if (!Utf8toWStr(name, channelName))
@@ -133,28 +202,6 @@ Channel* ChannelMgr::GetCustomChannel(std::string const& name)
     auto itr = _customChannels.find(channelName);
     if (itr != _customChannels.end())
         return itr->second;
-    else if (sWorld->getBoolConfig(CONFIG_PRESERVE_CUSTOM_CHANNELS))
-    {
-        CharacterDatabasePreparedStatement* stmt = CharacterDatabase.GetPreparedStatement(CHAR_SEL_CHANNEL);
-        stmt->setString(0, name);
-        stmt->setUInt32(1, _team);
-        if (PreparedQueryResult result = CharacterDatabase.Query(stmt))
-        {
-            Field* fields = result->Fetch();
-            std::string dbName = fields[0].GetString(); // may be different - channel names are case insensitive
-            bool dbAnnounce = fields[1].GetBool();
-            bool dbOwnership = fields[2].GetBool();
-            std::string dbPass = fields[3].GetString();
-            std::string dbBanned = fields[4].GetString();
-
-            Channel* channel = new Channel(CreateCustomChannelGuid(), dbName, _team, dbBanned);
-            channel->SetAnnounce(dbAnnounce);
-            channel->SetOwnership(dbOwnership);
-            channel->SetPassword(dbPass);
-            _customChannels.emplace(channelName, channel);
-            return channel;
-        }
-    }
 
     return nullptr;
 }

src/server/game/World/World.cpp
@@ -33,7 +33,7 @@
 #include "BattlePetMgr.h"
 #include "BlackMarketMgr.h"
 #include "CalendarMgr.h"
-#include "Channel.h"
+#include "ChannelMgr.h"
 #include "CharacterCache.h"
 #include "CharacterDatabaseCleaner.h"
 #include "CharacterTemplateDataStore.h"
@@ -763,6 +763,7 @@ void World::LoadConfigSettings(bool reload)
     m_int_configs[CONFIG_MAIL_LEVEL_REQ] = sConfigMgr->GetIntDefault("LevelReq.Mail", 1);
     m_bool_configs[CONFIG_PRESERVE_CUSTOM_CHANNELS] = sConfigMgr->GetBoolDefault("PreserveCustomChannels", false);
     m_int_configs[CONFIG_PRESERVE_CUSTOM_CHANNEL_DURATION] = sConfigMgr->GetIntDefault("PreserveCustomChannelDuration", 14);
+    m_int_configs[CONFIG_PRESERVE_CUSTOM_CHANNEL_INTERVAL] = sConfigMgr->GetIntDefault("PreserveCustomChannelInterval", 5);
     m_bool_configs[CONFIG_GRID_UNLOAD] = sConfigMgr->GetBoolDefault("GridUnload", true);
     m_bool_configs[CONFIG_BASEMAP_LOAD_GRIDS] = sConfigMgr->GetBoolDefault("BaseMapLoadAllGrids", false);
     if (m_bool_configs[CONFIG_BASEMAP_LOAD_GRIDS] && m_bool_configs[CONFIG_GRID_UNLOAD])
@@ -2310,6 +2311,8 @@ void World::SetInitialWorldSettings()
 
     m_timers[WUPDATE_WHO_LIST].SetInterval(5 * IN_MILLISECONDS); // update who list cache every 5 seconds
 
+    m_timers[WUPDATE_CHANNEL_SAVE].SetInterval(getIntConfig(CONFIG_PRESERVE_CUSTOM_CHANNEL_INTERVAL) * MINUTE * IN_MILLISECONDS);
+
     //to set mailtimer to return mails every day between 4 and 5 am
     //mailtimer is increased when updating auctions
     //one second is 1000 -(tested on win system)
@@ -2337,8 +2340,8 @@ void World::SetInitialWorldSettings()
     TC_LOG_INFO("server.loading", "Initialize AuctionHouseBot...");
     sAuctionBot->Initialize();
 
-    // Delete all custom channels which haven't been used for PreserveCustomChannelDuration days.
-    Channel::CleanOldChannelsInDB();
+    TC_LOG_INFO("server.loading", "Initializing chat channels...");
+    ChannelMgr::LoadFromDB();
 
     TC_LOG_INFO("server.loading", "Initializing Opcodes...");
     opcodeTable.Initialize();
@@ -2488,6 +2491,20 @@ void World::Update(uint32 diff)
     {
         m_timers[WUPDATE_WHO_LIST].Reset();
         sWhoListStorageMgr->Update();
+    }
+
+    if (m_timers[WUPDATE_CHANNEL_SAVE].Passed())
+    {
+        m_timers[WUPDATE_CHANNEL_SAVE].Reset();
+
+        if (sWorld->getBoolConfig(CONFIG_PRESERVE_CUSTOM_CHANNELS))
+        {
+            ChannelMgr* mgr1 = ASSERT_NOTNULL(ChannelMgr::ForTeam(ALLIANCE));
+            mgr1->SaveToDB();
+            ChannelMgr* mgr2 = ASSERT_NOTNULL(ChannelMgr::ForTeam(HORDE));
+            if (mgr1 != mgr2)
+                mgr2->SaveToDB();
+        }
     }
 
     /// Handle daily quests reset time