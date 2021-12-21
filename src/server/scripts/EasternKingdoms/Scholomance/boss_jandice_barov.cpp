allankrdec/TrinityCore-10-2024

Scripts/Gameobject: Journal of Jandice Barov- Convert script to SAI. (#24157)
* Scripts/Gameobject: Journal of Jandice Barov- Convert script to SAI. (Thanks to Sunwell)

* Scripts/Gameobject: Journal of Jandice Barov - Update script and move go spawn to bossscript

* Update boss_jandice_barov.cpp

Co-authored-by: Eridium <6587064+Killyana@users.noreply.github.com>

src/server/scripts/EasternKingdoms/Scholomance/boss_jandice_barov.cpp
@@ -22,7 +22,8 @@
 enum Spells
 {
     SPELL_CURSE_OF_BLOOD        = 24673,
-    SPELL_ILLUSION              = 17773
+    SPELL_ILLUSION              = 17773,
+    SPELL_DROP_JOURNAL          = 26096
 };
 
 enum Events
@@ -67,6 +68,7 @@ public:
         void JustDied(Unit* /*killer*/) override
         {
             Summons.DespawnAll();
+            DoCast(SPELL_DROP_JOURNAL, true);
         }
 
         void UpdateAI(uint32 diff) override

TrinityCore/TrinityCore

(cherry picked from commit f0bf447cd14315f1fe0bdab7d051691113525762)

src/server/scripts/EasternKingdoms/Scholomance/boss_jandice_barov.cpp
@@ -22,7 +22,8 @@
 enum Spells
 {
     SPELL_CURSE_OF_BLOOD        = 24673,
-    SPELL_ILLUSION              = 17773
+    SPELL_ILLUSION              = 17773,
+    SPELL_DROP_JOURNAL          = 26096
 };
 
 enum Events
@@ -67,6 +68,7 @@ public:
         void JustDied(Unit* /*killer*/) override
         {
             Summons.DespawnAll();
+            DoCastSelf(SPELL_DROP_JOURNAL, true);
         }
 
         void UpdateAI(uint32 diff) override