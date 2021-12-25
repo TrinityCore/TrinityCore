a3d06f2f329a6d21b9a79aed8b39f1d56fa933e6
Matan Shukry <matanshukry@gmail.com>
2021-12-25 22:25:54 +0200

    Scripts/Spells: Implemented various rogue spells (#26319)
    * Ruthlessness
    * Mastery: Main Gauche
    * Venomous Wounds
    * Symbols of Death
    * Backstab
    * Premeditation
    * Grand Melee
    * True Bearing
    * Slice and Dice
    * Roll the Bones

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -7338,10 +7338,20 @@ void Spell::DelayedChannel()
 
 bool Spell::HasPowerTypeCost(Powers power) const
 {
-    return std::find_if(m_powerCost.cbegin(), m_powerCost.cend(), [power](SpellPowerCost const& cost)
+    return GetPowerTypeCostAmount(power).has_value();
+}
+
+Optional<int32> Spell::GetPowerTypeCostAmount(Powers power) const
+{
+    auto itr = std::find_if(m_powerCost.cbegin(), m_powerCost.cend(), [power](SpellPowerCost const& cost)
     {
         return cost.Power == power;
-    }) != m_powerCost.cend();
+    });
+
+    if (itr == m_powerCost.cend())
+        return { };
+
+    return itr->Amount;
 }
 
 bool Spell::UpdatePointers()
