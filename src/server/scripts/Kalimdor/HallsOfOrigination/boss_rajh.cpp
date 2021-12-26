76a77b5a6d72c16f0a81607f480ca439dec68a03
Ovahlord <dreadkiller@gmx.de>
2021-12-26 10:43:24 +0100

    Scripts/HoO: some minor refactors for Rajh

--- src/server/scripts/Kalimdor/HallsOfOrigination/boss_rajh.cpp
+++ src/server/scripts/Kalimdor/HallsOfOrigination/boss_rajh.cpp
@@ -50,10 +50,10 @@ enum Spells
 
 enum Texts
 {
-    SAY_AGGRO               = 0,
-    SAY_BLESSING_OF_THE_SUN = 1,
-    SAY_SLAY                = 2,
-    SAY_DEATH               = 3
+    SAY_AGGRO                   = 0,
+    SAY_BLESSING_OF_THE_SUN     = 1,
+    SAY_SLAY                    = 2,
+    SAY_DEATH                   = 3
 };
 
 enum Events
@@ -83,7 +83,7 @@ enum Events
 
 enum MovePoints
 {
-    POINT_NONE,
+    POINT_NONE = 0,
     POINT_RAJH_CENTER
 };
 
@@ -91,21 +91,10 @@ Position const RajhMiddlePos = { -318.5936f, 192.8621f, 343.9443f };
 
 struct boss_rajh : public BossAI
 {
-    boss_rajh(Creature* creature) : BossAI(creature, DATA_RAJH)
-    {
-        Initialize();
-    }
+    boss_rajh(Creature* creature) : BossAI(creature, DATA_RAJH), _energized(true), _randomTimerCase(0) { }
 
-    void Initialize()
+    void JustAppeared() override
     {
-        _energized = true;
-        _randomTimerCase = 0;
-    }
-
-    void Reset() override
-    {
-        _Reset();
-        Initialize();
         me->MakeInterruptable(false);
     }
 
@@ -274,7 +263,7 @@ struct npc_rajh_solar_winds : public ScriptedAI
     void IsSummonedBy(Unit* /*summoner*/) override
     {
         me->SetWalk(true);
-        _events.ScheduleEvent(EVENT_GROW, Seconds(1));
+        _events.ScheduleEvent(EVENT_GROW, 1s);
     }
 
     void JustSummoned(Creature* summon) override
@@ -293,11 +282,11 @@ struct npc_rajh_solar_winds : public ScriptedAI
             {
                 case EVENT_GROW:
                     me->SetObjectScale(1.0f);
-                    _events.ScheduleEvent(EVENT_START_MOVING, Seconds(2));
+                    _events.ScheduleEvent(EVENT_START_MOVING, 2s);
                     break;
                 case EVENT_START_MOVING:
                     DoCastSelf(SPELL_SOLAR_WINDS_PERIODIC, true);
-                    _events.ScheduleEvent(EVENT_MOVE_ARROUND, 1);
+                    _events.ScheduleEvent(EVENT_MOVE_ARROUND, 1ms);
                     break;
                 case EVENT_MOVE_ARROUND:
                 {
@@ -306,7 +295,7 @@ struct npc_rajh_solar_winds : public ScriptedAI
                     pos.m_positionZ += 5.0f;
                     me->MovePositionToFirstCollision(pos, 60.0f, o);
                     me->GetMotionMaster()->MovePoint(POINT_NONE, pos, true);
-                    _events.Repeat(Seconds(7));
+                    _events.Repeat(7s);
                     break;
                 }
                 default:
@@ -325,7 +314,7 @@ struct npc_rajh_orb_of_the_sun : public ScriptedAI
 
     void IsSummonedBy(Unit* /*summoner*/) override
     {
-        _events.ScheduleEvent(EVENT_GROW, Seconds(1));
+        _events.ScheduleEvent(EVENT_GROW, 1s);
     }
 
     void SpellHitTarget(Unit* /*target*/, SpellInfo const* spell) override
@@ -344,11 +333,11 @@ struct npc_rajh_orb_of_the_sun : public ScriptedAI
             {
                 case EVENT_GROW:
                     me->SetObjectScale(1.0f);
-                    _events.ScheduleEvent(EVENT_SUMMON_BLAZING_INFERNO, Seconds(1));
+                    _events.ScheduleEvent(EVENT_SUMMON_BLAZING_INFERNO, 1s);
                     break;
                 case EVENT_SUMMON_BLAZING_INFERNO:
                     DoCastSelf(SPELL_SUMMON_METEOR);
-                    _events.ScheduleEvent(EVENT_RIDE_VEHICLE, Seconds(2));
+                    _events.ScheduleEvent(EVENT_RIDE_VEHICLE, 2s);
                     break;
                 case EVENT_RIDE_VEHICLE:
                     if (Creature* inferno = me->FindNearestCreature(NPC_BLAZING_INFERNO, 200.0f, true))
