b1acc6353d71769a20b0d04092f7ad3c57b1b241
Ovahlord <dreadkiller@gmx.de>
2021-12-28 21:28:09 +0100

    DB/Spells: Seals of Command will no longer proc on explicit unit targets when hitting enemies with Divine Storm

---
+++ sql/updates/world/4.3.4/2021_12_28_00_world.sql
@@ -0,0 +1,1 @@
+UPDATE `spell_proc` SET `SchoolMask`= 2, `SpellFamilyMask0`= 0x0, `SpellFamilyMask1`= 0x20000000 | 0x00000800, `SpellFamilyMask2`= 0x00010000, `SpellPhaseMask`= 2 WHERE `SpellId`= 85126;
