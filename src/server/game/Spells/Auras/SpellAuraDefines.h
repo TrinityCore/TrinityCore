9083df9efadf04b7298134ffcb524166b665bf2e
aquadeus <efectokoleos13@gmail.com>
2021-12-29 17:32:34 +0100

    Implement SPELL_AURA_MOD_MELEE_DAMAGE_FROM_CASTER

--- src/server/game/Spells/Auras/SpellAuraDefines.h
+++ src/server/game/Spells/Auras/SpellAuraDefines.h
@@ -434,7 +434,7 @@ enum AuraType : uint32
     SPELL_AURA_MOD_RESURRECTED_HEALTH_BY_GUILD_MEMBER       = 340,  // Increases health gained when resurrected by a guild member by X
     SPELL_AURA_MOD_SPELL_CATEGORY_COOLDOWN                  = 341,  // Modifies cooldown of all spells using affected category
     SPELL_AURA_MOD_MELEE_RANGED_HASTE_2                     = 342,
-    SPELL_AURA_MOD_MELEE_DAMAGE_FROM_CASTER                 = 343,  // NYI
+    SPELL_AURA_MOD_MELEE_DAMAGE_FROM_CASTER                 = 343,
     SPELL_AURA_MOD_AUTOATTACK_DAMAGE                        = 344,
     SPELL_AURA_BYPASS_ARMOR_FOR_CASTER                      = 345,
     SPELL_AURA_ENABLE_ALT_POWER                             = 346,
