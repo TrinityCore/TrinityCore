allankrdec/TrinityCore-10-2024

Scripts/OnyxiasLair: Fix Onyxia Eruption spell (#24472)

src/server/scripts/Kalimdor/OnyxiasLair/instance_onyxias_lair.cpp
@@ -111,7 +111,9 @@ public:
                 //THIS GOB IS A TRAP - What shall i do? =(
                 //Cast it spell? Copyed Heigan method
                 floorEruption->SendCustomAnim(floorEruption->GetGoAnimProgress());
-                floorEruption->CastSpell(nullptr, Difficulty(instance->GetSpawnMode()) == RAID_DIFFICULTY_10MAN_NORMAL ? 17731 : 69294); //pFloorEruption->GetGOInfo()->trap.spellId
+                CastSpellExtraArgs args;
+                args.OriginalCaster = onyxiaGUID;
+                floorEruption->CastSpell(floorEruption, floorEruption->GetGOInfo()->trap.spellId, args);
 
                 //Get all immediatly nearby floors
                 std::list<GameObject*> nearFloorList;

TrinityCore/TrinityCore

(cherry picked from commit e4476fab6eb8e9b712a9fa25c8db7b78ab22ddb7)

src/server/scripts/Kalimdor/OnyxiasLair/instance_onyxias_lair.cpp
@@ -112,7 +112,9 @@ public:
                 //THIS GOB IS A TRAP - What shall i do? =(
                 //Cast it spell? Copyed Heigan method
                 floorEruption->SendCustomAnim(floorEruption->GetGoAnimProgress());
-                floorEruption->CastSpell(nullptr, instance->GetDifficultyID() == DIFFICULTY_10_N ? 17731 : 69294); //pFloorEruption->GetGOInfo()->trap.spellId
+                CastSpellExtraArgs args;
+                args.OriginalCaster = onyxiaGUID;
+                floorEruption->CastSpell(floorEruption, floorEruption->GetGOInfo()->trap.spell, args);
 
                 //Get all immediatly nearby floors
                 std::list<GameObject*> nearFloorList;