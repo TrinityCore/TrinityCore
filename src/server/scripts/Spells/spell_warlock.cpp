aa0efa4dc7f848cfda494f0fa0f0687babdb5bf6
aquadeus <efectokoleos13@gmail.com>
2022-01-15 02:55:42 +0100

    Core/Spells: Fix warlock chaos bolt damage

--- src/server/scripts/Spells/spell_warlock.cpp
+++ src/server/scripts/Spells/spell_warlock.cpp
@@ -109,6 +109,27 @@ class spell_warl_banish : public SpellScriptLoader
         }
 };
 
+// 116858 - Chaos Bolt
+class spell_warl_chaos_bolt : public SpellScript
+{
+    PrepareSpellScript(spell_warl_chaos_bolt);
+
+    bool Load() override
+    {
+        return GetCaster()->GetTypeId() == TYPEID_PLAYER;
+    }
+
+    void HandleDummy(SpellEffIndex /*effIndex*/)
+    {
+        SetHitDamage((GetHitDamage() * 2) + CalculatePct(GetHitDamage(), GetCaster()->ToPlayer()->m_activePlayerData->SpellCritPercentage));
+    }
+
+    void Register() override
+    {
+        OnEffectHitTarget += SpellEffectFn(spell_warl_chaos_bolt::HandleDummy, EFFECT_0, SPELL_EFFECT_SCHOOL_DAMAGE);
+    }
+};
+
 // 77220 - Mastery: Chaotic Energies
 class spell_warl_chaotic_energies : public AuraScript
 {
@@ -1020,6 +1041,7 @@ public:
 void AddSC_warlock_spell_scripts()
 {
     new spell_warl_banish();
+    RegisterSpellScript(spell_warl_chaos_bolt);
     RegisterAuraScript(spell_warl_chaotic_energies);
     new spell_warl_create_healthstone();
     new spell_warl_demonic_circle_summon();
