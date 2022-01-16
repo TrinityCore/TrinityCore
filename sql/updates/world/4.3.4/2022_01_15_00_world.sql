f621152df6729c21cce637d5a3e6aacc62b581b9
Ovahlord <dreadkiller@gmx.de>
2022-01-16 03:33:46 +0100

    typo fix²

--- sql/updates/world/4.3.4/2022_01_15_00_world.sql
+++ sql/updates/world/4.3.4/2022_01_15_00_world.sql
@@ -1,2 +1,2 @@
-UPDATE `pet_levelstats` SET `hp`= 40529 `armor`= 11092, `str`= 476, `agi`= 3343, `sta`= 389, `inte`= 69, `spi`= 116 WHERE `creature_entry`= 26125 AND `level`= 85;
+UPDATE `pet_levelstats` SET `hp`= 40529, `armor`= 11092, `str`= 476, `agi`= 3343, `sta`= 389, `inte`= 69, `spi`= 116 WHERE `creature_entry`= 26125 AND `level`= 85;
 UPDATE `creature_template` SET `unit_class`= 4 WHERE `entry`= 26125;
