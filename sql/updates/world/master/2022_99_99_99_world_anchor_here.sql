e666accf622a72eadde534737abacbb0934ecc4d
aquadeus <efectokoleos13@gmail.com>
2022-01-19 21:10:48 +0100

    Core/Spells: Move spell anchor here to generic because is used by more bosses

---
+++ sql/updates/world/master/2022_99_99_99_world_anchor_here.sql
@@ -0,0 +1,2 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` = 45313 AND `ScriptName` = 'spell_sc_anchor_here';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (45313, 'spell_anchor_here');
