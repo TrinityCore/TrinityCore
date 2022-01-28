12d1dd6d794411793679f20066a55cfffd25a08e
Nyr <nyrdeveloper@gmail.com>
2022-01-28 23:51:28 +0100

    fixed typo2

--- sql/updates/world/master/2022_01_28_98_world.sql
+++ sql/updates/world/master/2022_01_28_98_world.sql
@@ -1,6 +1,6 @@
-UPDATE `spell_script_names` SET `ScriptName`='spell_pal_judgement' WHERE `spell_id`=20271;
-UPDATE `spell_script_names` SET `ScriptName`='spell_pal_judgement' WHERE `spell_id`=275779;
+UPDATE `spell_script_names` SET `ScriptName`='spell_pal_judgment' WHERE `spell_id`=20271;
+UPDATE `spell_script_names` SET `ScriptName`='spell_pal_judgment' WHERE `spell_id`=275779;
 
 DELETE FROM `spell_script_names` WHERE `spell_id`=275773 AND `ScriptName`='spell_pal_judgement';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
-(275773, 'spell_pal_judgement');
+(275773, 'spell_pal_judgment');
