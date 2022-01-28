29eca68952578c0283538a59296c5c51d0bf0e31
Nyr <nyrdeveloper@gmail.com>
2022-01-28 23:50:49 +0100

    fixed typo

--- sql/updates/world/master/2022_01_28_98_world.sql
+++ sql/updates/world/master/2022_01_28_98_world.sql
@@ -1,5 +1,5 @@
-UPDATE `spell_script_names` SET `ScriptName`='spell_pal_judgement' WHERE `Id`=20271;
-UPDATE `spell_script_names` SET `ScriptName`='spell_pal_judgement' WHERE `Id`=275779;
+UPDATE `spell_script_names` SET `ScriptName`='spell_pal_judgement' WHERE `spell_id`=20271;
+UPDATE `spell_script_names` SET `ScriptName`='spell_pal_judgement' WHERE `spell_id`=275779;
 
 DELETE FROM `spell_script_names` WHERE `spell_id`=275773 AND `ScriptName`='spell_pal_judgement';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
