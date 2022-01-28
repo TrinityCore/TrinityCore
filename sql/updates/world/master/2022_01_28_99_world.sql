546e0307a60a4ccc739afb8a917ed6ddaba7d783
Nyr <nyrdeveloper@gmail.com>
2022-01-28 06:17:10 +0100

    Spells/Paladin: Ashen Hallow has been scripted.
    * TODO: on 9.1.5. Blizzard decided to soft cap (rather than hard cap) several spells falling under the category of AoE class abilities. This one uses a 5-player cap, which should decrease its dmg/heal dealt depending on the given amount of units if greater than 5 are inside of the areatrigger when the periodic tick happens.

---
+++ sql/updates/world/master/2022_01_28_99_world.sql
@@ -0,0 +1,12 @@
+DELETE FROM `areatrigger_template` WHERE (`IsServerSide`=0 AND `Id`=23072);
+INSERT INTO `areatrigger_template` (`Id`, `IsServerSide`, `Type`, `Flags`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`, `VerifiedBuild`) VALUES
+(23072, 0, 4, 0, 20, 20, 16, 16, 8, 8, 41488);
+
+DELETE FROM `areatrigger_create_properties` WHERE `Id`=19042;
+INSERT INTO `areatrigger_create_properties` (`Id`, `AreaTriggerId`, `MoveCurveId`, `ScaleCurveId`, `MorphCurveId`, `FacingCurveId`, `AnimId`, `AnimKitId`, `DecalPropertiesId`, `TimeToTarget`, `TimeToTargetScale`, `Shape`, `ShapeData0`, `ShapeData1`, `ShapeData2`, `ShapeData3`, `ShapeData4`, `ShapeData5`, `ScriptName`, `VerifiedBuild`) VALUES 
+(19042, 23072, 0, 0, 0, 0, -1, 0, 0, 0, 30000, 4, 20, 20, 16, 16, 8, 8, 'areatrigger_pal_ashen_hallow', 41488);
+
+ -- Ashen Hallow
+DELETE FROM `spell_script_names` WHERE `spell_id`=316958 AND `ScriptName`='spell_pal_ashen_hallow';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(316958, 'spell_pal_ashen_hallow');
