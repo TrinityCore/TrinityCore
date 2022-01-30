b366cc3f9d6cbeb52ad03058f7ec4a83aafe3b3e
Nyr <nyrdeveloper@gmail.com>
2022-01-30 11:35:55 +0100

    fixed typo in SQL

--- sql/updates/world/master/2022_01_29_02_world.sql
+++ sql/updates/world/master/2022_01_29_02_world.sql
@@ -3,3 +3,4 @@ INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (114918,'spell_pal_light_hammer_periodic');
 
 UPDATE `creature_template` SET `unit_flags`=33554440, `unit_flags2`=67143680, `unit_flags3`=1, `ScriptName`='npc_pet_pal_light_hammer' WHERE `entry`=59738;
+
