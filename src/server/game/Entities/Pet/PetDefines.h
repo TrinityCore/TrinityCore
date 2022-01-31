22e823b5f9693e461ee158913c1caf3484eeb039
Shauren <shauren.trinity@gmail.com>
2022-01-31 21:50:32 +0100

    Core/Pets: Change PET_SAVE_NOT_IN_SLOT constant from 100 to -1 to truly avoid conflicts when growing stable size and change PET_SAVE_AS_DELETED from -1 to -2

--- src/server/game/Entities/Pet/PetDefines.h
+++ src/server/game/Entities/Pet/PetDefines.h
@@ -36,13 +36,13 @@ enum PetType : uint8
 #define MAX_PET_STABLES         4
 
 // stored in character_pet.slot
-enum PetSaveMode : int8
+enum PetSaveMode : int16
 {
-    PET_SAVE_AS_DELETED        = -1,                        // not saved in fact
+    PET_SAVE_AS_DELETED        = -2,                        // not saved in fact
     PET_SAVE_AS_CURRENT        =  0,                        // in current slot (with player)
     PET_SAVE_FIRST_STABLE_SLOT =  1,
     PET_SAVE_LAST_STABLE_SLOT  =  MAX_PET_STABLES,          // last in DB stable slot index (including), all higher have same meaning as PET_SAVE_NOT_IN_SLOT
-    PET_SAVE_NOT_IN_SLOT       =  100                       // for avoid conflict with stable size grow will use 100
+    PET_SAVE_NOT_IN_SLOT       = -1                         // for avoid conflict with stable size grow will use negative value
 };
 
 enum PetSpellState
