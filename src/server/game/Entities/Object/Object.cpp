9cb9dd8a0b6a88d54531898bdc442d289a1e16a0
Seyden <saiifii@live.de>
2022-02-01 22:29:09 +0100

    [Transport/TempSummon]: Fixed if a TempSummon spawns that it gets added to the transport

--- src/server/game/Entities/Object/Object.cpp
+++ src/server/game/Entities/Object/Object.cpp
@@ -1809,6 +1809,18 @@ TempSummon* Map::SummonCreature(uint32 entry, Position const& pos, SummonPropert
         return nullptr;
     }
 
+    Transport* transport = summon->m_movementInfo.transport.guid.IsEmpty() ? summonerUnit->GetTransport() : nullptr;
+    if (transport)
+    {
+        float x, y, z, o;
+        pos.GetPosition(x, y, z, o);
+        transport->CalculatePassengerOffset(x, y, z, &o);
+        summon->m_movementInfo.transport.pos.Relocate(x, y, z, o);
+
+        // This object must be added to transport before adding to map for the client to properly display it
+        transport->AddPassenger(summon);
+    }
+
     if (summoner && !(properties && properties->GetFlags().HasFlag(SummonPropertiesFlags::IgnoreSummonerPhase)))
         PhasingHandler::InheritPhaseShift(summon, summoner);
 
@@ -1836,6 +1848,10 @@ TempSummon* Map::SummonCreature(uint32 entry, Position const& pos, SummonPropert
 
     if (!AddToMap(summon->ToCreature()))
     {
+        // Returning false will cause the object to be deleted - remove from transport
+        if (transport)
+            transport->RemovePassenger(summon);
+
         delete summon;
         return nullptr;
     }
