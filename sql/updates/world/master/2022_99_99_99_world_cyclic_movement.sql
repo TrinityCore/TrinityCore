7eccdf0e54a1e60a69d17618c62c6d324c8b101b
Aqua Deus <95978183+aquadeus@users.noreply.github.com>
2022-02-04 19:56:43 +0100

    Update 2022_99_99_99_world_cyclic_movement.sql

--- sql/updates/world/master/2022_99_99_99_world_cyclic_movement.sql
+++ sql/updates/world/master/2022_99_99_99_world_cyclic_movement.sql
@@ -5,4 +5,3 @@ ALTER TABLE `creature_addon`
 ALTER TABLE `creature_template_addon`   
 	CHANGE `path_id` `waypointPathId` INT(10) UNSIGNED DEFAULT 0 NOT NULL,
 	ADD COLUMN `cyclicSplinePathId` INT(10) UNSIGNED DEFAULT 0 NOT NULL AFTER `waypointPathId`;
-	
