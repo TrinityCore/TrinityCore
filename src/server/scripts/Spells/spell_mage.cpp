be0d2edcfb8640a1edb56a2fdfbd510c6a2e0a67
Nyr <nyrdeveloper@gmail.com>
2022-02-04 12:05:02 +0100

    renamed _refreshTimer into _tickTimer.

--- src/server/scripts/Spells/spell_mage.cpp
+++ src/server/scripts/Spells/spell_mage.cpp
@@ -326,25 +326,25 @@ class spell_mage_blazing_barrier : public AuraScript
 // 4658 - AreaTrigger Create Properties
 struct areatrigger_mage_blizzard : AreaTriggerAI
 {
-    areatrigger_mage_blizzard(AreaTrigger* areatrigger) : AreaTriggerAI(areatrigger), _refreshTimer(TICK_PERIOD) { }
+    areatrigger_mage_blizzard(AreaTrigger* areatrigger) : AreaTriggerAI(areatrigger), _tickTimer(TICK_PERIOD) { }
 
     static constexpr Milliseconds TICK_PERIOD = Milliseconds(1000);
 
     void OnUpdate(uint32 diff) override
     {
-        _refreshTimer -= Milliseconds(diff);
+        _tickTimer -= Milliseconds(diff);
 
-        while (_refreshTimer <= 0s)
+        while (_tickTimer <= 0s)
         {
             if (Unit* caster = at->GetCaster())
                 caster->CastSpell(at->GetPosition(), SPELL_MAGE_BLIZZARD_DAMAGE);
 
-            _refreshTimer += TICK_PERIOD;
+            _tickTimer += TICK_PERIOD;
         }
     }
 
 private:
-    Milliseconds _refreshTimer;
+    Milliseconds _tickTimer;
 };
 
 // 190357 - Blizzard (Damage)
