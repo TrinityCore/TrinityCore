735ab9911beede4747fb5cfb60de87a534834f1a
offl <offl@users.noreply.github.com>
2022-02-12 03:03:28 +0200

    Scripts/Spells: Script one more player talk

---
+++ sql/updates/world/3.3.5/2022_02_12_20_world.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `spell_script_names` WHERE `spell_id` = 49550 AND `ScriptName` = 'spell_call_out_injured_soldier';
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(49550,'spell_call_out_injured_soldier');
