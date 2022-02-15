f12f3a972ff12168f07cee655077cc819b6dcaa2
Aqua Deus <95978183+aquadeus@users.noreply.github.com>
2022-02-16 00:00:55 +0100

    Core/Auras: Implement SPELL_AURA_MOD_ENVIRONMENTAL_DAMAGE_TAKEN (#27781)

--- src/server/game/Spells/Auras/SpellAuraDefines.h
+++ src/server/game/Spells/Auras/SpellAuraDefines.h
@@ -527,7 +527,7 @@ enum AuraType : uint32
     SPELL_AURA_433                                          = 433,
     SPELL_AURA_434                                          = 434,
     SPELL_AURA_435                                          = 435,
-    SPELL_AURA_MOD_ENVIRONMENTAL_DAMAGE_TAKEN               = 436,  // NYI
+    SPELL_AURA_MOD_ENVIRONMENTAL_DAMAGE_TAKEN               = 436,
     SPELL_AURA_MOD_MINIMUM_SPEED_RATE                       = 437,
     SPELL_AURA_PRELOAD_PHASE                                = 438,  // NYI
     SPELL_AURA_439                                          = 439,
