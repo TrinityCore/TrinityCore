9679447f6e979941bcc2104b0a391102dfde7b63
aquadeus <efectokoleos13@gmail.com>
2022-02-17 18:05:40 +0100

    Only remove misschance from dual wield

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -11578,15 +11578,8 @@ float Unit::MeleeSpellMissChance(Unit const* victim, WeaponAttackType attType, S
     float missChance = victim->GetUnitMissChance();
 
     // melee attacks while dual wielding have +19% chance to miss
-    if (!spellInfo && haveOffhandWeapon())
-    {
-        // Melee auto attack can't miss if SPELL_AURA_IGNORE_DUAL_WIELD_HIT_PENALTY is active
-        if (HasAuraType(SPELL_AURA_IGNORE_DUAL_WIELD_HIT_PENALTY))
-            return 0.f;
-
-        if (!IsInFeralForm())
-            missChance += 19.0f;
-    }
+    if (!spellInfo && haveOffhandWeapon() && !IsInFeralForm() && !HasAuraType(SPELL_AURA_IGNORE_DUAL_WIELD_HIT_PENALTY))
+        missChance += 19;
 
     // Spellmod from SpellModOp::HitChance
     float resistMissChance = 100.0f;
