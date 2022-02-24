15d09300379a798cd9b784a89244110f18e8ba2e
Seyden <saiifii@live.de>
2022-02-24 03:02:24 +0100

    Tab

--- sql/updates/characters/master/2022_02_23_00_characters.sql
+++ sql/updates/characters/master/2022_02_23_00_characters.sql
@@ -1,7 +1,7 @@
 -- 
 ALTER TABLE `respawn`
 	ADD COLUMN `teamId` TINYINT(3) UNSIGNED NOT NULL COMMENT 'Team Identifier for Faction splitted Maps' AFTER `instanceId`,
-    DROP PRIMARY KEY,
+	DROP PRIMARY KEY,
 	ADD PRIMARY KEY (`type`, `spawnId`, `instanceId`, `teamId`);
 
 ALTER TABLE `corpse`
