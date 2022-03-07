c19863745addec42cf8e1cd77f3daf10fa25ae16
funjoker <funjoker109@gmail.com>
2022-03-07 20:23:27 +0100

    Misc: Update copyright year in revision_data

--- revision_data.h.in.cmake
+++ revision_data.h.in.cmake
@@ -12,7 +12,7 @@
  #define _FULL_DATABASE             "TDB_full_world_920.22031_2022_03_06.sql"
  #define _HOTFIXES_DATABASE         "TDB_full_hotfixes_920.22031_2022_03_06.sql"
  #define VER_COMPANYNAME_STR        "TrinityCore Developers"
- #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2019 TrinityCore"
+ #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2022 TrinityCore"
  #define VER_FILEVERSION            0,0,0
  #define VER_FILEVERSION_STR        "@rev_hash@ @rev_date@ (@rev_branch@ branch)"
  #define VER_PRODUCTVERSION         VER_FILEVERSION
