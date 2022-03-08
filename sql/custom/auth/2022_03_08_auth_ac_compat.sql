ed9b7cdb0e2638d787c907c1c49ab8a187fd287e
ihm-tswow@protonmail.com <ihm-ts@protonmail.com>
2022-03-08 23:33:18 +0100

    trinitycore: add azerothcore auth compatibility columns/tables

---
+++ sql/custom/auth/2022_03_08_auth_ac_compat.sql
@@ -0,0 +1,24 @@
+ALTER TABLE `account` ADD COLUMN `totaltime` INT UNSIGNED AFTER `recruiter`;
+
+DROP TABLE IF EXISTS `version_db_auth`;
+CREATE TABLE IF NOT EXISTS `version_db_auth` (
+  `sql_rev` varchar(100) NOT NULL,
+  `required_rev` varchar(100) DEFAULT NULL,
+  `date` varchar(50) DEFAULT NULL,
+  `2021_11_06_00` bit(1) DEFAULT NULL,
+  PRIMARY KEY (`sql_rev`),
+  KEY `required` (`required_rev`),
+  CONSTRAINT `required` FOREIGN KEY (`required_rev`) REFERENCES `version_db_auth` (`sql_rev`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='Last applied sql update to DB';
+
+INSERT INTO `version_db_auth` (`sql_rev`, `required_rev`, `date`, `2021_11_06_00`) VALUES
+	('1554142988374631100', NULL, NULL, NULL),
+	('1579213352894781043', NULL, NULL, NULL),
+	('1609867708436603000', NULL, NULL, NULL),
+	('1615629613255169700', NULL, NULL, NULL),
+	('1620079951672711500', NULL, NULL, NULL),
+	('1620114805872279900', NULL, NULL, NULL),
+	('1620146306002634000', NULL, '2021_06_17_00', NULL),
+	('1621715473238990700', NULL, '2021_05_30_00', NULL),
+	('1634163668021762900', NULL, '2021_10_14_00', NULL),
+	('1635587640506554000', NULL, '2021_11_06_00', NULL);
