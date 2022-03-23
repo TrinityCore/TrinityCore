09e39635a74962b60574dc5b46773a258e1e0f30
leelf00 <leelf_00@126.com>
2022-03-23 19:42:37 +0800

    remove whitespace

--- sql/updates/world/3.3.5/9999_99_99_world.sql
+++ sql/updates/world/3.3.5/9999_99_99_world.sql
@@ -2,7 +2,7 @@
 DELETE FROM gossip_menu_option WHERE MenuID=7552 AND OptionID IN (0,1,2);
 DELETE FROM gossip_menu_option WHERE MenuID=7581 AND OptionID IN (0,1,2);
 DELETE FROM gossip_menu_option WHERE MenuID=7706 AND OptionID=0;
-INSERT INTO `gossip_menu_option`(`MenuID`, `OptionID`, `OptionIcon`, `OptionText`, `OptionBroadcastTextID`, `OptionType`, `OptionNpcFlag`, `ActionMenuID`, `ActionPoiID`, `BoxCoded`, `BoxMoney`, `BoxText`, `BoxBroadcastTextID`, `VerifiedBuild`) VALUES 
+INSERT INTO `gossip_menu_option`(`MenuID`, `OptionID`, `OptionIcon`, `OptionText`, `OptionBroadcastTextID`, `OptionType`, `OptionNpcFlag`, `ActionMenuID`, `ActionPoiID`, `BoxCoded`, `BoxMoney`, `BoxText`, `BoxBroadcastTextID`, `VerifiedBuild`) VALUES
 (7552, 0, 0, 'My companions and I are with you, Lady Proudmoore.', 14698, 0, 0, 7556, 0, 0, 0, NULL, 0, 26365),
 (7552, 1, 0, 'We are ready for whatever Archimonde might send our way, Lady Proudmoore.', 15393, 0, 0, 0, 0, 0, 0, NULL, 0, 0),
 (7552, 2, 0, 'Until we meet again, Lady Proudmoore.', 15416, 0, 0, 0, 0, 0, 0, NULL, 0, 0),
@@ -14,11 +14,11 @@ INSERT INTO `gossip_menu_option`(`MenuID`, `OptionID`, `OptionIcon`, `OptionText
 -- Karazhan missing optionid
 DELETE FROM gossip_menu_option WHERE MenuID=7421 AND OptionID=0;
 DELETE FROM gossip_menu_option WHERE MenuID=7422 AND OptionID=0;
-INSERT INTO `gossip_menu_option`(`MenuID`, `OptionID`, `OptionIcon`, `OptionText`, `OptionBroadcastTextID`, `OptionType`, `OptionNpcFlag`, `ActionMenuID`, `ActionPoiID`, `BoxCoded`, `BoxMoney`, `BoxText`, `BoxBroadcastTextID`, `VerifiedBuild`) VALUES 
+INSERT INTO `gossip_menu_option`(`MenuID`, `OptionID`, `OptionIcon`, `OptionText`, `OptionBroadcastTextID`, `OptionType`, `OptionNpcFlag`, `ActionMenuID`, `ActionPoiID`, `BoxCoded`, `BoxMoney`, `BoxText`, `BoxBroadcastTextID`, `VerifiedBuild`) VALUES
 (7421, 0, 0, 'I\'m not an actor.', 14052, 0, 0, 0, 0, 0, 0, NULL, 0, 0),
 (7422, 0, 0, 'Ok, I\'ll give it a try, then.', 14054, 0, 0, 0, 0, 0, 0, NULL, 0, 0);
 
 -- HallsOfStone missing optionid
 DELETE FROM gossip_menu_option WHERE MenuID=9670 AND OptionID=0;
-INSERT INTO `gossip_menu_option`(`MenuID`, `OptionID`, `OptionIcon`, `OptionText`, `OptionBroadcastTextID`, `OptionType`, `OptionNpcFlag`, `ActionMenuID`, `ActionPoiID`, `BoxCoded`, `BoxMoney`, `BoxText`, `BoxBroadcastTextID`, `VerifiedBuild`) VALUES 
+INSERT INTO `gossip_menu_option`(`MenuID`, `OptionID`, `OptionIcon`, `OptionText`, `OptionBroadcastTextID`, `OptionType`, `OptionNpcFlag`, `ActionMenuID`, `ActionPoiID`, `BoxCoded`, `BoxMoney`, `BoxText`, `BoxBroadcastTextID`, `VerifiedBuild`) VALUES
 (9670, 0, 0, 'Let\'s move Brann, enough of the history lesson!', 27616, 0, 0, 0, 0, 0, 0, NULL, 0, 0);
