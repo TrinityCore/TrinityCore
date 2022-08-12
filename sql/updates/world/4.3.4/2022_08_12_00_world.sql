e4ea987bab70ba7fa3bf2f2045d8fdf1f3a3965e
Ovahlord <dreadkiller@gmx.de>
2022-08-12 23:32:40 +0200

    DB/Weather: added missing weather data for Uldum

---
+++ sql/updates/world/4.3.4/2022_08_12_00_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `game_weather` WHERE `zone`= 5034;
+INSERT INTO `game_weather` (`zone`, `spring_storm_chance`, `summer_storm_chance`, `fall_storm_chance`, `winter_storm_chance`, `ScriptName`) VALUES
+(5034, 15, 15, 15, 15, ''); 
