5ff6fc9de1eff839d79bb71934bb840d591461ba
aquadeus <efectokoleos13@gmail.com>
2022-08-31 00:23:29 +0200

    Fix feedback

--- sql/updates/world/master/2022_99_99_99_world_maghtar_orc_racial_spell.sql
+++ sql/updates/world/master/2022_99_99_99_world_maghtar_orc_racial_spell.sql
@@ -1,2 +1,2 @@
-DELETE FROM `spell_script_names` WHERE `spell_id` = 274738;
+DELETE FROM `spell_script_names` WHERE `ScriptName` = `spell_gen_ancestral_call`;
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (274738, 'spell_gen_ancestral_call');
