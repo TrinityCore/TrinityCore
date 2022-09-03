1f1e1b3a666c6b6137e1dbe360f86968bfa0f55c
Gildor <gildor55@gmail.com>
2022-09-03 13:01:09 +0200

    Core/Spells: Improve check for non damage/heal spells in Spell::TargetInfo::DoDamageAndTriggers (#24467)
    * this prevent cases like full absorb damage that override proper hitMask

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -2592,10 +2592,12 @@ void Spell::TargetInfo::DoDamageAndTriggers(Spell* spell)
 
     // All calculated do it!
     // Do healing
+    bool hasHealing = false;
     std::unique_ptr<DamageInfo> spellDamageInfo;
     std::unique_ptr<HealInfo> healInfo;
     if (spell->m_healing > 0)
     {
+        hasHealing = true;
         uint32 addhealth = spell->m_healing;
         if (IsCrit)
         {
@@ -2614,8 +2616,10 @@ void Spell::TargetInfo::DoDamageAndTriggers(Spell* spell)
     }
 
     // Do damage
+    bool hasDamage = false;
     if (spell->m_damage > 0)
     {
+        hasDamage = true;
         // Fill base damage struct (unitTarget - is real spell target)
         SpellNonMeleeDamage damageInfo(caster, spell->unitTarget, spell->m_spellInfo->Id, spell->m_spellSchoolMask);
         // Check damage immunity
@@ -2659,7 +2663,7 @@ void Spell::TargetInfo::DoDamageAndTriggers(Spell* spell)
     }
 
     // Passive spell hits/misses or active spells only misses (only triggers)
-    if (spell->m_damage <= 0 && spell->m_healing <= 0)
+    if (!hasHealing && !hasDamage)
     {
         // Fill base damage struct (unitTarget - is real spell target)
         SpellNonMeleeDamage damageInfo(caster, spell->unitTarget, spell->m_spellInfo->Id, spell->m_spellSchoolMask);
