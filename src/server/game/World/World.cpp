6ff3d888688fd382d317de0c2dbf26af6a6e7da5
Shauren <shauren.trinity@gmail.com>
2022-09-23 19:27:56 +0200

    Core/Spawns: Added startup error for incorrect map assignments in instance_spawn_groups
    (cherry picked from commit 881950c1d5b4a98c89240a5598b8484108608a85)

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -1996,9 +1996,6 @@ void World::SetInitialWorldSettings()
     TC_LOG_INFO("server.loading", "Loading Spawn Group Templates...");
     sObjectMgr->LoadSpawnGroupTemplates();
 
-    TC_LOG_INFO("server.loading", "Loading instance spawn groups...");
-    sObjectMgr->LoadInstanceSpawnGroups();
-
     TC_LOG_INFO("server.loading", "Loading Creature Data...");
     sObjectMgr->LoadCreatures();
 
@@ -2022,6 +2019,9 @@ void World::SetInitialWorldSettings()
 
     TC_LOG_INFO("server.loading", "Loading Spawn Group Data...");
     sObjectMgr->LoadSpawnGroups();
+
+    TC_LOG_INFO("server.loading", "Loading instance spawn groups...");
+    sObjectMgr->LoadInstanceSpawnGroups();
 
     TC_LOG_INFO("server.loading", "Loading GameObject Addon Data...");
     sObjectMgr->LoadGameObjectAddons();                          // must be after LoadGameObjects()
