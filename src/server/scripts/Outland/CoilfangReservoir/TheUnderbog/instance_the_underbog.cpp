e3ac84b929163d4253c15e3c9c430440c2cd15ac
Shauren <shauren.trinity@gmail.com>
2022-09-25 23:10:09 +0200

    Scripts/The Underbog: Added spawn groups for bosses and implemented boss state tracking

--- src/server/scripts/Outland/CoilfangReservoir/TheUnderbog/instance_the_underbog.cpp
+++ src/server/scripts/Outland/CoilfangReservoir/TheUnderbog/instance_the_underbog.cpp
@@ -31,15 +31,34 @@ class instance_the_underbog : public InstanceMapScript
 public:
     instance_the_underbog() : InstanceMapScript(TheUndebogScriptName, 546) { }
 
+    struct instance_the_underbog_InstanceMapScript : public InstanceScript
+    {
+        instance_the_underbog_InstanceMapScript(InstanceMap* map) : InstanceScript(map)
+        {
+            SetHeaders(TheUndebogDataHeader);
+            SetBossNumber(TheUnderbogBossCount);
+        }
+
+        void OnUnitDeath(Unit* unit) override
+        {
+            switch (unit->GetEntry())
+            {
+                case NPC_GHAZAN:
+                    SetBossState(DATA_GHAZAN, DONE);
+                    break;
+                case NPC_SWAMPLORD_MUSELEK:
+                    SetBossState(DATA_SWAMPLORD_MUSELEK, DONE);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
     InstanceScript* GetInstanceScript(InstanceMap* map) const override
     {
         return new instance_the_underbog_InstanceMapScript(map);
     }
-
-    struct instance_the_underbog_InstanceMapScript : public InstanceScript
-    {
-        instance_the_underbog_InstanceMapScript(InstanceMap* map) : InstanceScript(map) { }
-    };
 };
 
 void AddSC_instance_the_underbog()
