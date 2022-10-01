eddc848b752129e5cc37de07d98f93be77c8c2c5
Malcrom <malcromdev@gmail.com>
2022-10-01 12:42:00 -0300

    Remove set Visible

--- src/server/scripts/Northrend/zone_borean_tundra.cpp
+++ src/server/scripts/Northrend/zone_borean_tundra.cpp
@@ -171,7 +171,7 @@ struct go_caribou_trap : public GameObjectAI
             case EVENT_SPAWN_TRAPPER:
                 if (TempSummon* trapper = me->SummonCreature(NPC_NESINGWARY_TRAPPER, me->GetPositionX() + (std::cos(me->GetOrientation()) * 21), me->GetPositionY() + (std::sin(me->GetOrientation()) * 21), me->GetPositionZ(), me->GetOrientation(), TEMPSUMMON_DEAD_DESPAWN, 6s))
                 {
-                    trapper->SetVisible(false);
+                    //trapper->SetVisible(false);
                     trapper->SetFacingToObject(me);
                     _trapperGUID = trapper->GetGUID();
                 }
@@ -181,7 +181,7 @@ struct go_caribou_trap : public GameObjectAI
                 if (Creature* trapper = ObjectAccessor::GetCreature(*me, _trapperGUID))
                 {
                     trapper->GetMotionMaster()->MovePoint(0, trapper->GetPositionX() + (std::cos(trapper->GetOrientation()) * 20), trapper->GetPositionY() + (std::sin(trapper->GetOrientation()) * 20), me->GetPositionZ());
-                    trapper->SetVisible(true);
+                    //trapper->SetVisible(true);
                 }
                 _events.ScheduleEvent(EVENT_TRAPPER_TEXT, 5s);
                 break;
