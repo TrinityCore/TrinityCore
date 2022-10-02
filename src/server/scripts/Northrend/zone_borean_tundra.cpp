2ab323efab65d83f24a77ffe46e6c324b7fdce5b
Malcrom <malcromdev@gmail.com>
2022-10-01 22:08:08 -0300

    Update zone_borean_tundra.cpp

--- src/server/scripts/Northrend/zone_borean_tundra.cpp
+++ src/server/scripts/Northrend/zone_borean_tundra.cpp
@@ -1402,7 +1402,7 @@ enum BloodsporeRuination
 {
     NPC_BLOODMAGE_LAURITH   = 25381,
     SAY_BLOODMAGE_LAURITH   = 0,
-    EVENT_TALK              = 9,
+    EVENT_TALK              = 1,
     EVENT_RESET_ORIENTATION
 };
 
