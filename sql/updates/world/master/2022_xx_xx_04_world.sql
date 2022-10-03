d59a074f36ec362344f2e9deb05985266392e6bb
Nyr <nyrdeveloper@gmail.com>
2022-10-03 02:53:45 +0200

    added missing spells for spiders.
    * added spell for Lurker to vanish correctly into the shadows.
    * removed unnused spell trigger since it's handled by the aura trigger.

--- sql/updates/world/master/2022_xx_xx_04_world.sql
+++ sql/updates/world/master/2022_xx_xx_04_world.sql
@@ -534,6 +534,20 @@ INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Lan
 -- Skitterweb Lurker
 UPDATE `creature` SET `wander_distance`= 10, `MovementType`= 1 WHERE `id` = 1781;
 
+SET @ENTRY := 1781;
+DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryOrGuid` = @ENTRY;
+UPDATE `creature_template` SET `AIName` = 'SmartAI', `ScriptName` = '' WHERE `entry` = @ENTRY;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(@ENTRY, 0, 0, 1, 0, 0, 100, 0, 6000, 9000, 18000, 24000, 85, 82715, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Every 18 - 24 seconds (6 - 9s initially) (IC) - Self: Cast spell 82715 on self'),
+(@ENTRY, 0, 1, 0, 61, 0, 100, 0, 0, 0, 0, 0, 85, 82717, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Every 18 - 24 seconds (6 - 9s initially) (IC) - Self: Cast spell 82717 on self');
+
+-- Skitterweb Striker
+SET @ENTRY := 1780;
+DELETE FROM `smart_scripts` WHERE `source_type` = 0 AND `entryOrGuid` = @ENTRY;
+UPDATE `creature_template` SET `AIName` = 'SmartAI', `ScriptName` = '' WHERE `entry` = @ENTRY;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(@ENTRY, 0, 0, 0, 9, 0, 100, 0, 0, 5, 12000, 14000, 11, 87081, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'When victim in range 0 - 5 yards (cooldown 12000 - 14000 ms) - Self: Cast spell 87081 on Victim');
+
 -- Skitterweb Matriarch
 UPDATE `creature_template` SET `ScriptName` = 'npc_silverpine_skitterweb_matriarch', `AIName` = '', `flags_extra` = 512 WHERE `entry` = 44906;
 
