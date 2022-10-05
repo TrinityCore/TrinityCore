93bf00b114aea99ea50ece44ad9df83053ecbee3
Nyr <nyrdeveloper@gmail.com>
2022-10-05 10:29:22 +0200

    fixed non-triggered spell

--- sql/updates/world/master/2022_xx_xx_04_world.sql
+++ sql/updates/world/master/2022_xx_xx_04_world.sql
@@ -540,7 +540,7 @@ UPDATE `creature_template` SET `AIName` = 'SmartAI', `ScriptName` = '' WHERE `en
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (@ENTRY, 0, 0, 1, 0, 0, 100, 0, 6000, 9000, 18000, 24000, 85, 82715, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Every 18 - 24 seconds (6 - 9s initially) (IC) - Self: Cast spell 82715 on self'),
 (@ENTRY, 0, 1, 0, 61, 0, 100, 0, 0, 0, 0, 0, 67, 1, 1000, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Every 18 - 24 seconds (6 - 9s initially) (IC) - Trigger timed event timedEvent[1] in 1000 - 1000 ms // -meta_wait'),
-(@ENTRY, 0, 2, 0, 59, 0, 100, 0, 1, 0, 0, 0, 85, 82717, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'On timed event timedEvent[1] triggered - Self: Cast spell 82717 on self');
+(@ENTRY, 0, 2, 0, 59, 0, 100, 0, 1, 0, 0, 0, 85, 82717, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'On timed event timedEvent[1] triggered - Self: Cast spell 82717 with flags triggered on self');
 
 -- Skitterweb Striker
 SET @ENTRY := 1780;
