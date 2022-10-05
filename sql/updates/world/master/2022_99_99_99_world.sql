5b1e2fe84cbaa64a4038d3238f81f5c9d7787892
Ovahlord <dreadkiller@gmx.de>
2022-10-05 17:13:22 +0200

    Core/Spells: fixed Howling Blast

---
+++ sql/updates/world/master/2022_99_99_99_world.sql
@@ -0,0 +1,5 @@
+-- Howling Blast
+DELETE FROM `spell_script_names` WHERE `ScriptName`= 'spell_dk_howling_blast';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(49184, 'spell_dk_howling_blast'),
+(237680, 'spell_dk_howling_blast');
