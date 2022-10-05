2278fdbab430a2c73bc74b2cf202b94559742354
Ovah <dreadkiller@gmx.de>
2022-10-05 10:26:15 +0200

    Update 2022_99_00_00_world.sql

--- sql/updates/world/master/2022_99_00_00_world.sql
+++ sql/updates/world/master/2022_99_00_00_world.sql
@@ -5,5 +5,5 @@ INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 
 DELETE FROM `spell_proc` WHERE `SpellId` IN (59057, 59052);
 INSERT INTO `spell_proc` (`SpellId`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellTypeMask`, `SpellPhaseMask`, `DisableEffectsMask`, `Charges`) VALUES
-(59057, 15, 0x8000, 0x20000, 1, 1, 0x2, 0),
+(59057, 15, 0x8000 | 0x80, 0, 1, 1, 0x2, 0),
 (59052, 15, 0x0, 0x2, 1, 1, 0, 1);
