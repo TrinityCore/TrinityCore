b3c27656bcaf389ca901cfef680edb301b5eefb5
Aqua Deus <95978183+aquadeus@users.noreply.github.com>
2022-10-11 14:12:00 +0200

    Core/Spells: Move draenor spell to generic spell (#28356)

--- src/server/scripts/Spells/spell_generic.cpp
+++ src/server/scripts/Spells/spell_generic.cpp
@@ -5160,6 +5160,7 @@ class spell_gen_reverse_cast_target_to_caster_triggered: public SpellScript
 // Note: this spell unsummons any creature owned by the caster. Set appropriate target conditions on the DB.
 // 84065 - Despawn All Summons
 // 83935 - Despawn All Summons
+// 160938 - Despawn All Summons (Garrison Intro Only)
 class spell_gen_despawn_all_summons_owned_by_caster : public SpellScript
 {
     PrepareSpellScript(spell_gen_despawn_all_summons_owned_by_caster);
