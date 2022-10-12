ce1341d43b15dcd9c65b1ae62b578e8e25bc5f79
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2022-10-12 14:31:59 +0300

    Update torghast_tower_of_the_damned.h

---
+++ src/server/scripts/Shadowlands/TheMaw/torghast_tower_of_the_damned/torghast_tower_of_the_damned.h
@@ -0,0 +1,341 @@
+/*
+ * Copyright 2021 FuzionCore Project
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Script Data Start
+SDName: 
+SDAuthor: Frozen
+SD%Complete: %
+SDComment: still working on structure.
+Script Data End */
+
+
+enum Objects
+{
+    Anvil = 192628,
+    Domination-Etched Treasure Cache = 369898,
+    Gold_Coins = 235985,
+    Snow_Patch = 358375,
+    Xyraths_Booby-Trapped Cache = 375536,
+};
+
+enum Quests
+{
+    Blingtron_7000 = 56042,
+    Owlcat Soul = 62432,
+    Sanctum of Domination_N - Damned If You Dont = 64597,
+    Sanctum of Domination - Damned If You Dont_H = 64598,
+    Sanctum of Domination - Damned If You Dont_M = 64599,
+    Storming the Sanctum = 63903,
+};
+
+enum Loot
+{
+    Soothsayers_Runes = 71716,
+    Owlcat_Soul = 182177,
+    Runekeepers_Ingot = 187175,
+};
+
+enum Achievements 
+{
+    // Guild achievements
+    Hall_of_Fame: Sylvanas_A = 15197,
+    Hall_of_Fame: Sylvanas_H = 15196,
+    Heroic: Sanctum_of_Domination_Guild_Run = 15183,
+    Mythic: Sanctum_of_Domination_Guild_Run = 15184,
+    Sanctum_of_Domination_Guild_Run = 15182,
+    // Character Achievements
+    Ahead_of_the_Curve: Sylvanas_Windrunner = 15134,
+    Coming_to_Terms = 15399,
+    Cutting_Edge: Sylvanas_Windrunner = 15135,
+    Eye_Wish_You_Were_Here = 15065,
+    Fate_of_Domination = 15667,
+    Flawless_Fate = 15040,
+    Heroic: Fate_of_Domination = 15668,
+    Heroic: Sanctum_of_Domination = 15127,
+    I_Used_to_Bullseye_Deeprun_Rats_Back_Home = 15058,
+    Knowledge_is_Power = 15132,
+    Mythic: Fate_of_Domination = 15669,
+    Mythic: Fatescribe_Roh-Kalo = 15119,
+    Mythic: Guardian of the First Ones = 15118,
+    Mythic: KelThuzad = 15120,
+    Mythic: Painsmith Raznal = 15117,
+    Mythic: Remnant of Nerzhul = 15115,
+    Mythic: Sanctum of Domination = 15128,
+    Mythic: Soulrender Dormazain = 15116,
+    Mythic: Sylvanas Windrunner = 15121,
+    Mythic: The Eye of the Jailer = 15113,
+    Mythic: The Nine = 15114,
+    Mythic: The Tarragrue = 15112,
+    Name A Better Duo, I_ll Wait = 14998,
+    Sanctum of Domination = 15126,
+    Shackles of Fate = 15124,
+    The Dark Bastille = 15123,
+    The Jailers Vanguard = 15122,
+    The Reckoning = 15125,
+    This World is a Prism = 15133,
+    To the Nines = 15003,
+    Together Forever = 15108,
+    Tormentors Tango = 15105,
+    Whack-A-Soul = 15131,
+};
+
+enum Creatures
+{
+    Adjutant_Felipos           = 185103,
+    Adjutant_Felipos_1         = 156158,
+    Amethia                    = 166157,
+    Anduin                     = 178372,
+    Animated_Prowler           = 156242,
+    Animated_Stygia            = 170417,
+    Animated_Stygia_1          = 170417,
+    Animimic                   = 165060,
+    Arch-Suppressor_Laguas     = 185019,
+    Arch-Suppressor_Laguas_1   = 171422,
+    Arch-Tyrant_Zerios         = 175238,
+    Armed_Prisoner             = 154011,
+    Ashen_Phylactery           = 164698,
+    Ashen_Phylactery_1         = 165533,
+    Ashen_Phylactery_2         = 167986,
+    Ashen_Phylactery_3         = 165523,
+    Ashraka                    = 173395,
+    Ashraka_1                  = 173750,
+    Aspirants_Amulet           = 179704,
+    Assembler_Xertora          = 173401,
+    Assembler_Xertora_1        = 173749,
+    Ayeleth                    = 171918,
+    Ayeleth_1                  = 171920,
+    Ayeleths_Torturer          = 171919,
+    Baine_Bloodhoof            = 175294,
+    Binder_Baritas             = 185015,
+    Binder_Baritas_1           = 153011,
+    Blazing_Elemental          = 154128,
+    Blightsmasher              = 173136,
+    Blightsmasher_1            = 185111,
+    Blisswing                  = 172244,
+    Blisswing_1                = 172275,
+    Bloated_Mawroach           = 185665,
+    Bloodletter_Phantoriax     = 157432,
+    Boiling_Soul_Remnant       = 167985,
+    Bone_Husk                  = 170228,
+    Bone_Husk_1                = 185098,
+    Bonepile                   = 179706,
+    Bonesmith_Heirmir          = 178370,
+    Bound_Soul                 = 153015,
+    Bound_Soul_Remnant         = 157561,
+    Bound_Soul_Remnant_1       = 169782,
+    Bound_Soul_Remnant_2       = 169783,
+    Bound_Soul_Remnant_3       = 169902,
+    Bound_Soul_Remnant_4       = 180672,
+    Bound_Soul_Remnant_5       = 169785,
+    Broker_Ve_ken              = 152594,
+    Broker_Ve_nott             = 170257,
+    Bubbling_Soulmass          = 156022,
+    Burning_Emberguard         = 154129,
+    Buttons                    = 166246,
+    Calix                      = 166147,
+    Callie_Carrington          = 118451,
+    Captive_Dredger            = 175542,
+    Cellblock_Sentinel         = 151331,
+    Cellblock_Sentinel_1       = 185010,
+    Chalkyth                   = 172240,
+    Chalkyth_1                 = 172261,
+    Chalkyths_Saber            = 172257,
+    Coagulated_Blood           = 179281,
+    Coldheart_Agent            = 185219,
+    Coldheart_Agent_1          = 156212,
+    Coldheart_Ambusher         = 165594,
+    Coldheart_Ascendant        = 185220,
+    Coldheart_Ascendant_1      = 156157,
+    Coldheart_Automaton        = 172021,
+    Coldheart_Binder           = 185201,
+    Coldheart_Binder_1         = 156226,
+    Coldheart_Guardian         = 185202,
+    Coldheart_Guardian_1       = 156213,
+    Coldheart_Javelineer       = 185221,
+    Coldheart_Javelineer_1     = 156159,
+    Coldheart_Scout            = 185203,
+    Coldheart_Scout_1          = 156219,
+    Commander_Jarod_Shadowsong = 118447,
+    Corrupted_Risen_Arbalest   = 118492,
+    Corrupted_Risen_Arbalest_1 = 122397,
+    Corrupted_Risen_Mage       = 118491,
+    Corrupted_Risen_Soldier    = 118489,
+    Croman                     = 175648,
+    Croman_1                   = 175649,
+    Curious_Squirrel           = 93997,
+    Custodian_Thonar           = 153165,
+    Custodian_Thonar_1         = 185011,
+    Damaged_Archer             = 119480,
+    Damaged_Mage               = 119477,
+    Damaged_Soldier            = 119476,
+    Damaged_Soul               = 119470,
+    Dark_Ascended_Corrus       = 185020,
+    Dark_Ascended_Corrus_1     = 156239,
+    Darksworn_Goliath          = 185105,
+    Darksworn_Goliath_1        = 173134,
+    Deadsoul_Amalgam           = 152500,
+    Deadsoul_Amalgam_1         = 185119,
+    Deadsoul_Chorus            = 185252,  
+    Deadsoul_Chorus_1          = 152898,
+    Deadsoul_Devil             = 151817,
+    Deadsoul_Drifter           = 152644,
+    Deadsoul_Echo              = 151815,
+    Deadsoul_Echo_1            = 185237,
+
+
+    torghast_tower_of_the_damned  =  10472,
+};
+
+class torghast_tower_of_the_damned : public PlayerScript
+{
+public:
+    torghast_tower_of_the_damned() : PlayerScript(torghast_tower_of_the_damned) { }
+
+    void OnLogin(Player* player,bool /*fristlogin*/) override
+    {
+        // Set zone as Raid
+        if (player->GetMapId() ==  10472);
+            player->GoMapId()  == GetMapId;
+    };
+        
+        void OnUpdateArea(Player* player, uint32 /*newArea*/,) override
+    {
+        // Set zone as Raid
+        if (player->GetMapId() == MAP_NPE)
+            player->GoMapId()  == GetMapId;
+    };
+
+//Sanctum of Domination
+
+struct torghast_tower_of_the_damned : public InstanceScript
+{
+    torghast_tower_of_the_damned(InstanceMap*) : InstanceScript(torghast_tower_of_the_damned()) { }
+};  
+
+struct torghast_tower_of_the_damned : public CreatureScript
+{
+    torghast_tower_of_the_damned(Creature* Creature) : RegisterCreatureAI(torghast_tower_of_the_damned()) { }
+};  
+void AddSC_instance_torgast_tower_of_the_damned();
+
+
+void AddSC_torghast_tower_of_the_damned()
+{
+    AddSC_torghast_tower_of_the_damned();
+    RegisterInstanceScript(torghast_tower_of_the_damned);
+    RegisterCreatureAI(Remnant_of_ner_zhul);
+    RegisterCreatureAI(Adjutant_Felipos);
+    RegisterCreatureAI(Adjutant_Felipos_1);
+    RegisterCreatureAI(Amethia);
+    RegisterCreatureAI(Anduin);
+    RegisterCreatureAI(Animated_Prowler);
+    RegisterCreatureAI(Animated_Stygia);
+    RegisterCreatureAI(Animated_Stygia_1);
+    RegisterCreatureAI(Animimic);
+    RegisterCreatureAI(Arch-Suppressor_Laguas);
+    RegisterCreatureAI(Arch-Suppressor_Laguas_1);
+    RegisterCreatureAI(Arch-Tyrant_Zerios);
+    RegisterCreatureAI(Armed_Prisoner);
+    RegisterCreatureAI(Ashen_Phylactery);
+    RegisterCreatureAI(Ashen_Phylactery_1);
+    RegisterCreatureAI(Ashen_Phylactery_2);
+    RegisterCreatureAI(Ashen_Phylactery_3);
+    RegisterCreatureAI(Ashraka);
+    RegisterCreatureAI(Ashraka_1);
+    RegisterCreatureAI(Aspirants_Amulet);
+    RegisterCreatureAI(Assembler_Xertora);
+    RegisterCreatureAI(Assembler_Xertora_1);
+    RegisterCreatureAI(Ayeleth);
+    RegisterCreatureAI(Ayeleth_1);
+    RegisterCreatureAI(Ayeleths_Torturer);
+    RegisterCreatureAI(Baine_Bloodhoof);
+    RegisterCreatureAI(Binder_Baritas);
+    RegisterCreatureAI(Binder_Baritas_1);
+    RegisterCreatureAI(Blazing_Elemental);
+    RegisterCreatureAI(Blightsmasher);
+    RegisterCreatureAI(Blightsmasher_1);
+    RegisterCreatureAI(Blisswing);
+    RegisterCreatureAI(Blisswing_1);
+    RegisterCreatureAI(Bloated_Mawroach);
+    RegisterCreatureAI(Bloodletter_Phantoriax);
+    RegisterCreatureAI(Boiling_Soul_Remnant);
+    RegisterCreatureAI(Bone_Husk);
+    RegisterCreatureAI(Bone_Husk_1);
+    RegisterCreatureAI(Bonepile);
+    RegisterCreatureAI(Bonesmith_Heirmir);
+    RegisterCreatureAI(Bound_Soul);
+    RegisterCreatureAI(Bound_Soul_Remnant);
+    RegisterCreatureAI(Bound_Soul_Remnant_1);
+    RegisterCreatureAI(Bound_Soul_Remnant_2);
+    RegisterCreatureAI(Bound_Soul_Remnant_3);
+    RegisterCreatureAI(Bound_Soul_Remnant_4);
+    RegisterCreatureAI(Bound_Soul_Remnant_5);
+    RegisterCreatureAI(Broker_Ve_ken);
+    RegisterCreatureAI(Broker_Ve_nott);
+    RegisterCreatureAI(Bubbling_Soulmass);
+    RegisterCreatureAI(Burning_Emberguard);
+    RegisterCreatureAI(Buttons);
+    RegisterCreatureAI(Calix);
+    RegisterCreatureAI(Callie_Carrington);
+    RegisterCreatureAI(Captive_Dredger);
+    RegisterCreatureAI(Cellblock_Sentinel);
+    RegisterCreatureAI(Cellblock_Sentinel_1);
+    RegisterCreatureAI(Chalkyth);
+    RegisterCreatureAI(Chalkyth_1);
+    RegisterCreatureAI(Chalkyths_Saber);
+    RegisterCreatureAI(Coagulated_Blood);
+    RegisterCreatureAI(Coldheart_Agent);
+    RegisterCreatureAI(Coldheart_Agent_1);
+    RegisterCreatureAI(Coldheart_Ambusher);
+    RegisterCreatureAI(Coldheart_Ascendant);
+    RegisterCreatureAI(Coldheart_Ascendant_1);
+    RegisterCreatureAI(Coldheart_Automaton);
+    RegisterCreatureAI(Coldheart_Binder);
+    RegisterCreatureAI(Coldheart_Binder_1);
+    RegisterCreatureAI(Coldheart_Guardian);
+    RegisterCreatureAI(Coldheart_Guardian_1);
+    RegisterCreatureAI(Coldheart_Javelineer);
+    RegisterCreatureAI(Coldheart_Javelineer_1);
+    RegisterCreatureAI(Coldheart_Scout);
+    RegisterCreatureAI(Coldheart_Scout_1);
+    RegisterCreatureAI(Commander_Jarod_Shadowsong);
+    RegisterCreatureAI(Corrupted_Risen_Arbalest);
+    RegisterCreatureAI(Corrupted_Risen_Arbalest_1);
+    RegisterCreatureAI(Corrupted_Risen_Mage);
+    RegisterCreatureAI(Corrupted_Risen_Soldier);
+    RegisterCreatureAI(Croman);
+    RegisterCreatureAI(Croman_1);
+    RegisterCreatureAI(Curious_Squirrel);
+    RegisterCreatureAI(Custodian_Thonar);
+    RegisterCreatureAI(Custodian_Thonar_1);
+    RegisterCreatureAI(Damaged_Archer);
+    RegisterCreatureAI(Damaged_Mage);
+    RegisterCreatureAI(Damaged_Soldier);
+    RegisterCreatureAI(Damaged_Soul);
+    RegisterCreatureAI(Dark_Ascended_Corrus);
+    RegisterCreatureAI(Dark_Ascended_Corrus_1);
+    RegisterCreatureAI(Darksworn_Goliath);
+    RegisterCreatureAI(Darksworn_Goliath_1);
+    RegisterCreatureAI(Deadsoul_Amalgam);
+    RegisterCreatureAI(Deadsoul_Amalgam_1);
+    RegisterCreatureAI(Deadsoul_Chorus);  
+    RegisterCreatureAI(Deadsoul_Chorus_1);
+    RegisterCreatureAI(Deadsoul_Devil);
+    RegisterCreatureAI(Deadsoul_Drifter);
+    RegisterCreatureAI(Deadsoul_Echo);
+    RegisterCreatureAI(Deadsoul_Echo_1);
+};
