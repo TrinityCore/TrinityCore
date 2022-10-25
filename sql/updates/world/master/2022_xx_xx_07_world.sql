5347d40e6c93b6e3082a6a16846349c6a7186c5e
Nyr <nyrdeveloper@gmail.com>
2022-10-26 01:52:44 +0200

    few changes

--- sql/updates/world/master/2022_xx_xx_07_world.sql
+++ sql/updates/world/master/2022_xx_xx_07_world.sql
@@ -650,7 +650,7 @@ UPDATE `creature` SET `unit_flags` = 768, `MovementType`= 0, `wander_distance`= 
 
 DELETE FROM `creature_addon` WHERE `guid` = 321101;
 INSERT INTO `creature_addon` (`guid`, `path_id`, `mount`, `MountCreatureID`, `bytes1`, `bytes2`, `emote`, `aiAnimKit`, `movementAnimKit`, `meleeAnimKit`, `visibilityDistanceType`, `auras`) VALUES 
-(321101, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, '80636');
+(321101, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, '29266');
 
 -- Caretaker Smithers
 UPDATE `creature_template` SET `ScriptName`= 'npc_silverpine_caretaker_smithers' WHERE `entry` = 45219;
@@ -662,19 +662,21 @@ INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Lan
 (45219, 2, 0, '%s goes into a frenzy!', 16, 0, 100, 0, 0, 0, 0, 2384, 0, '');
 
 -- Forsaken Catapult
+-- Note: unit_flag3 = 1 seems related to being a PassiveAI, only the catapults that are using Permanent Feign Death have this flag, but it's not set by the spell or the REACT_STATE on TC, so we set it manually for now.
 UPDATE `creature_template` SET `unit_flags3`=1 WHERE `entry` IN (321185, 321177, 321159, 321154, 321143, 321142, 321119, 321103);
 
 DELETE FROM `creature_addon` WHERE `guid` IN (321185, 321177, 321159, 321154, 321143, 321142, 321119, 321103);
 INSERT INTO `creature_addon` (`guid`, `path_id`, `mount`, `MountCreatureID`, `bytes1`, `bytes2`, `emote`, `aiAnimKit`, `movementAnimKit`, `meleeAnimKit`, `visibilityDistanceType`, `auras`) VALUES 
-(321185, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '29266 84386'),
-(321177, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '29266 84386'),
-(321159, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '29266 84386'),
-(321154, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '29266 84386'),
-(321143, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '29266 84386'),
-(321142, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '29266 84386'),
-(321119, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '29266 84386'),
-(321103, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '29266 84386');
+(321185, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, '84386'),
+(321177, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, '84386'),
+(321159, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, '84386'),
+(321154, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, '84386'),
+(321143, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, '84386'),
+(321142, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, '84386'),
+(321119, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, '84386'),
+(321103, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, '84386');
 
+-- Permanent Feign Death
 DELETE FROM `spell_script_names` WHERE `spell_id`=84386 AND `ScriptName`='spell_gen_feign_death_all_flags';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
 (84386, 'spell_gen_feign_death_all_flags');
