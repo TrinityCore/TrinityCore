b5f1ac0aa98eeaa4cbdfde745db2e5f6aeb6a350
Jildor <Jildor@users.noreply.github.com>
2022-10-31 18:30:00 +0100

    DB/Locales: Wintersbite
    Closes #28346

---
+++ sql/updates/world/3.3.5/2022_10_31_00_world.sql
@@ -0,0 +1,12 @@
+-- 
+UPDATE `item_template_locale` SET `Name`='Invernalia' WHERE `ID`=3819 AND `locale`='esES';
+UPDATE `item_template_locale` SET `Name`='Invernalia' WHERE `ID`=3819 AND `locale`='esMX';
+UPDATE `item_template_locale` SET `Name`='겨울서리풀' WHERE `ID`=3819 AND `locale`='koKR';
+UPDATE `item_template_locale` SET `Name`='冬刺草' WHERE `ID`=3819 AND `locale`='zhCN';
+UPDATE `item_template_locale` SET `Name`='冬刺草' WHERE `ID`=3819 AND `locale`='zhTW';
+
+UPDATE `gameobject_template_locale` SET `Name`='Invernalia' WHERE `entry`=2044 AND `locale`='esES';
+UPDATE `gameobject_template_locale` SET `Name`='Invernalia' WHERE `entry`=2044 AND `locale`='esMX';
+UPDATE `gameobject_template_locale` SET `Name`='겨울서리풀' WHERE `entry`=2044 AND `locale`='koKR';
+UPDATE `gameobject_template_locale` SET `Name`='冬刺草' WHERE `entry`=2044 AND `locale`='zhCN';
+UPDATE `gameobject_template_locale` SET `Name`='冬刺草' WHERE `entry`=2044 AND `locale`='zhTW';
