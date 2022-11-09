c0cef0110ca85090b57ac96f03179c6b49f0fa4b
Ovahlord <dreadkiller@gmx.de>
2022-11-09 23:05:05 +0100

    DB/Spells:
    * added internal proc cooldown for Master of Elements to avoid gaining mana from hitting multiple critical strikes by a single spell
    * added internal proc cooldown for Flametongue Weapon's proc
    * added internal proc cooldown for Reaping
    * added internal proc coolodwn for Elemental Focus
    * added internal proc cooldown for Glyph of Sinister Strike
    * corrected proc data for Arcane Concentration. This will increase the chance to proc while the cooldown remains the same to reflect patch 4.0.3a patch notes behavior

---
+++ sql/updates/world/4.3.4/2022_11_09_00_world.sql
@@ -0,0 +1,10 @@
+UPDATE `spell_proc` SET `Cooldown`= 8 WHERE `SpellId`= 29074; -- Master of Elements
+UPDATE `spell_proc` SET `Cooldown`= 8 WHERE `SpellId`= 10400; -- Flametongue (Passive)
+UPDATE `spell_proc` SET `Cooldown`= 100 WHERE `SpellId`= 56835; -- Reaping (Passive)
+UPDATE `spell_proc` SET `Cooldown`= 500 WHERE `SpellId`= 16164; -- Elemental Focus
+UPDATE `spell_proc` SET `Cooldown`= 500 WHERE `SpellId`= 56821; -- Glyph of Sinister Strike
+
+-- Arcane Concentration
+DELETE FROM `spell_proc` WHERE `SpellId` IN (11213, 12574, 12575, -11213);
+INSERT INTO `spell_proc` (`SpellId`, `SpellFamilyName`, `SpellTypeMask`, `SpellPhaseMask`, `Cooldown`) VALUES
+(-11213, 3, 1, 2, 15000);
