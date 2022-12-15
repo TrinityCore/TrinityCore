ff8695eb129b8e451660ceada3280e7604d7132f
Ovahlord <dreadkiller@gmx.de>
2022-12-15 21:23:35 +0100

    DB/Loot: added item loot to Strange Bloated Stomach

---
+++ sql/updates/world/4.3.4/2022_12_15_01_world.sql
@@ -0,0 +1,7 @@
+DELETE FROM `item_loot_template` WHERE `Entry`= 67495;
+INSERT INTO `item_loot_template` (`Entry`, `Item`, `Chance`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES
+(67495, 52328, 0, 1, 1, 1, 2),
+(67495, 52327, 0, 1, 1, 1, 2),
+(67495, 52329, 0, 1, 1, 1, 2),
+(67495, 52326, 0, 1, 1, 1, 2),
+(67495, 52325, 0, 1, 1, 1, 2);
