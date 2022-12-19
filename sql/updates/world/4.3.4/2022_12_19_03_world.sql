56ba5175e1530a705f73b71c8dbc78c605be4be7
Ovahlord <dreadkiller@gmx.de>
2022-12-19 18:18:18 +0100

    DB/Vendors: updated Gotri's vendor data to Cataclysm

---
+++ sql/updates/world/4.3.4/2022_12_19_03_world.sql
@@ -0,0 +1,11 @@
+DELETE FROM `npc_vendor` WHERE `entry`= 3369;
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `ExtendedCost`, `type`, `PlayerConditionID`, `VerifiedBuild`) VALUES
+(3369, 9, 30747, 0, 0, 1, 0, 15595), -- Gem Pouch
+(3369, 8, 30748, 0, 0, 1, 0, 15595), -- Enchanter's Satchel
+(3369, 7, 30745, 0, 0, 1, 0, 15595), -- Heavy Toolbox
+(3369, 6, 30746, 0, 0, 1, 0, 15595), -- Mining Sack
+(3369, 5, 60335, 0, 0, 1, 0, 15595), -- Thick Hide Pack
+(3369, 4, 4499, 0, 0, 1, 0, 15595), -- Huge Brown Sack
+(3369, 3, 4497, 0, 0, 1, 0, 15595), -- Heavy Brown Bag
+(3369, 2, 4498, 0, 0, 1, 0, 15595), -- Brown Leather Satchel
+(3369, 1, 4496, 0, 0, 1, 0, 15595); -- Small Brown Pouch
