6f44f49a9fc209bfe3fffec8b8315169b0501fe7
洪义成 <xuehyc@163.com>
2023-01-04 23:14:00 +0800

    修正文件名与内容

--- 登录器/登录器文件更新-从客户端到此处.bat
+++ 登录器/登录器文件更新-从客户端到此处.bat
@@ -1,1 +1,1 @@
-copy /Y D:\Program Files (x86)\World of Warcraft\_retail_\WTF\Config.wtf "D:\Program Files (x86)\WOWDIY\TrinityCore\登录器\配置文件\World of Warcraft\_retail_\WTF\" 
+copy /Y D:\Program Files (x86)\World of Warcraft\Arctium WoW Launcher.exe "D:\Program Files (x86)\WOWDIY\TrinityCore\登录器\" 
