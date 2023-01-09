e2ed3e23f8241933b4f931e9def7eee69f1abb79
Meji <alvaro.megias@outlook.com>
2023-01-09 23:08:22 +0100

    Core/Creatures: Fix IsMovementPreventedByCasting logic (#28551)

--- src/server/game/Entities/Creature/Creature.cpp
+++ src/server/game/Entities/Creature/Creature.cpp
@@ -3389,18 +3389,19 @@ void Creature::DoNotReacquireSpellFocusTarget()
 
 bool Creature::IsMovementPreventedByCasting() const
 {
+    // Can always move when not casting
+    if (!HasUnitState(UNIT_STATE_CASTING))
+        return false;
+
     // first check if currently a movement allowed channel is active and we're not casting
     if (Spell* spell = m_currentSpells[CURRENT_CHANNELED_SPELL])
     {
         if (spell->getState() != SPELL_STATE_FINISHED && spell->IsChannelActive())
             if (spell->CheckMovement() != SPELL_CAST_OK)
-                return false;
+                return true;
     }
 
     if (HasSpellFocus())
-        return true;
-
-    if (HasUnitState(UNIT_STATE_CASTING))
         return true;
 
     return false;
