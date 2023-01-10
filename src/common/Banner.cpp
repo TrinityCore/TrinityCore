fd1619531b976db5a0bfd7d9ae587edc2a7b7e0c
洪义成 <xuehyc@163.com>
2023-01-10 16:44:19 +0800

    去掉不必要的换行-"\n"

--- src/common/Banner.cpp
+++ src/common/Banner.cpp
@@ -22,7 +22,7 @@
 void Trinity::Banner::Show(char const* applicationName, void(*log)(char const* text), void(*logExtraInfo)())
 {
     log(Trinity::StringFormat("{} ({})", GitRevision::GetFullVersion(), applicationName).c_str());
-    log(R"(<Ctrl-C> to stop.\n)");
+    log(R"(<Ctrl-C> to stop.)");
     log(R"( ______                       __)");
     log(R"(/\__  _\       __          __/\ \__)");
     log(R"(\/_/\ \/ _ __ /\_\    ___ /\_\ \, _\  __  __)");
