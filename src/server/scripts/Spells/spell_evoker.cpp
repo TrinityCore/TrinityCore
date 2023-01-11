bf283dca7f6c7e9a9c5b08755fdae208731df46d
aquadeus <efectokoleos13@gmail.com>
2023-01-11 19:36:00 +0100

    Core/Spells: Handle evoker talent "Living Flame" and "Energizing Flame"

--- src/server/scripts/Spells/spell_evoker.cpp
+++ src/server/scripts/Spells/spell_evoker.cpp
@@ -23,14 +23,19 @@
 #include "Player.h"
 #include "ScriptMgr.h"
 #include "Spell.h"
+#include "SpellAuraEffects.h"
 #include "SpellHistory.h"
 #include "SpellMgr.h"
 #include "SpellScript.h"
 
 enum EvokerSpells
 {
+    SPELL_EVOKER_ENERGIZING_FLAME          = 400006,
     SPELL_EVOKER_GLIDE_KNOCKBACK           = 358736,
     SPELL_EVOKER_HOVER                     = 358267,
+    SPELL_EVOKER_LIVING_FLAME              = 361469,
+    SPELL_EVOKER_LIVING_FLAME_DAMAGE       = 361500,
+    SPELL_EVOKER_LIVING_FLAME_HEAL         = 361509,
     SPELL_EVOKER_SOAR_RACIAL               = 369536
 };
 
@@ -73,7 +78,64 @@ class spell_evo_glide : public SpellScript
     }
 };
 
+// 361469 - Living Flame (Red)
+class spell_evo_living_flame : public SpellScript
+{
+    PrepareSpellScript(spell_evo_living_flame);
+
+    bool Validate(SpellInfo const* /*spellInfo*/) override
+    {
+        return ValidateSpellInfo ({ SPELL_EVOKER_LIVING_FLAME_DAMAGE, SPELL_EVOKER_LIVING_FLAME_HEAL });
+    }
+
+    void HandleTarget(SpellEffIndex /*effIndex*/)
+    {
+        if (GetCaster()->IsFriendlyTo(GetHitUnit()))
+            GetCaster()->CastSpell(GetHitUnit(), SPELL_EVOKER_LIVING_FLAME_HEAL, true);
+        else
+            GetCaster()->CastSpell(GetHitUnit(), SPELL_EVOKER_LIVING_FLAME_DAMAGE, true);
+    }
+
+    void Register() override
+    {
+        OnEffectHitTarget += SpellEffectFn(spell_evo_living_flame::HandleTarget, EFFECT_0, SPELL_EFFECT_DUMMY);
+    }
+};
+
+// 361500 - Living Flame (damage)
+class spell_evo_living_flame_damage : public SpellScript
+{
+    PrepareSpellScript(spell_evo_living_flame_damage);
+
+    bool Validate(SpellInfo const* /*spellInfo*/) override
+    {
+        return ValidateSpellInfo ({ SPELL_EVOKER_ENERGIZING_FLAME, SPELL_EVOKER_LIVING_FLAME });
+    }
+
+    void HandleManaRestored(SpellEffIndex /*effIndex*/)
+    {
+        if (AuraEffect* auraEffect = GetCaster()->GetAuraEffect(SPELL_EVOKER_ENERGIZING_FLAME, EFFECT_0))
+        {
+            SpellInfo const* spellInfo = sSpellMgr->AssertSpellInfo(SPELL_EVOKER_LIVING_FLAME, GetCastDifficulty());
+
+            Optional<SpellPowerCost> cost = spellInfo->CalcPowerCost(POWER_MANA, false, GetCaster(), GetSpellInfo()->GetSchoolMask(), nullptr);
+            if (!cost)
+                return;
+
+            int32 manaRestored = CalculatePct(cost->Amount, auraEffect->GetAmount());
+            GetCaster()->ModifyPower(POWER_MANA, manaRestored);
+        }
+    }
+
+    void Register() override
+    {
+        OnEffectHitTarget += SpellEffectFn(spell_evo_living_flame_damage::HandleManaRestored, EFFECT_0, SPELL_EFFECT_SCHOOL_DAMAGE);
+    }
+};
+
 void AddSC_evoker_spell_scripts()
 {
     RegisterSpellScript(spell_evo_glide);
+    RegisterSpellScript(spell_evo_living_flame);
+    RegisterSpellScript(spell_evo_living_flame_damage);
 }
