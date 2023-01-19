f1b2fa41d7975a48c82b5a017236a50bee13ac63
洪义成 <xuehyc@163.com>
2023-01-19 19:50:01 +0800

    修正

--- sql/updates/auth/master/2023_01_17_02_auth.sql
+++ sql/updates/auth/master/2023_01_17_02_auth.sql
@@ -1,6 +1,6 @@
 DELETE FROM `build_info` WHERE `build`=47657;
 INSERT INTO `build_info` (`build`,`majorVersion`,`minorVersion`,`bugfixVersion`,`hotfixVersion`,`winAuthSeed`,`win64AuthSeed`,`mac64AuthSeed`,`winChecksumSeed`,`macChecksumSeed`) VALUES
-(47657,10,0,2,NULL,NULL,'11857CA9FA336B5148E7B2A116858C92',NULL,NULL,NULL);
+(47657,10,0,2,NULL,NULL,'179D3DC3235629D07113A9B3867F97A7',NULL,NULL,NULL);
 
 UPDATE `realmlist` SET `gamebuild`=47657 WHERE `gamebuild`=47631;
 
