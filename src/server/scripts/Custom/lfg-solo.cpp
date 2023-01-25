fed7347050faa8fd22c8c4c76d51193e721dcf2e
洪义成 <xuehyc@163.com>
2023-01-25 23:58:53 +0800

    增加来源说明(kyrian)

--- src/server/scripts/Custom/lfg-solo.cpp
+++ src/server/scripts/Custom/lfg-solo.cpp
@@ -1,3 +1,11 @@
+/*
+** Made by Traesh https://github.com/Traesh
+** Conan513 https://github.com/conan513
+** Made into a module by Micrah https://github.com/milestorme/
+** Converted to TrinityCore by LEO33 http://leo33.info
+** Updated by qyh214 https://github.com/qyh214
+*/
+
 #include "ScriptMgr.h"
 #include "Player.h"
 #include "Configuration/Config.h"
