60380cd12dfafb2431847c0669685daf7e4bcbc8
Naddley <NaddleyTC@gmail.com>
2023-01-30 01:08:57 +0100

    DB/Monk: Added spellproc data for Jade Ignition

---
+++ sql/updates/world/master/2023_01_30_03_world.sql
@@ -0,0 +1,4 @@
+-- Jade Ignition
+DELETE FROM `spell_proc` WHERE `SpellId` IN (392979);
+INSERT INTO `spell_proc` (`SpellId`,`SchoolMask`,`SpellFamilyName`,`SpellFamilyMask0`,`SpellFamilyMask1`,`SpellFamilyMask2`,`SpellFamilyMask3`,`ProcFlags`,`ProcFlags2`,`SpellTypeMask`,`SpellPhaseMask`,`HitMask`,`AttributesMask`,`DisableEffectsMask`,`ProcsPerMinute`,`Chance`,`Cooldown`,`Charges`) VALUES
+(392979,0x00,53,0x00100000,0x00000000,0x00000000,0x00000000,0x0,0x0,0x1,0x2,0x0,0x0,0x0,0,0,0,0); -- Jade Ignition
