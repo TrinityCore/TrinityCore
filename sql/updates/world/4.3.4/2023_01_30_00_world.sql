843806a64054063917ad925f242a2df437281cbd
Ovahlord <dreadkiller@gmx.de>
2023-01-30 18:14:22 +0100

    DB: fixed multiple sql application

--- sql/updates/world/4.3.4/2023_01_30_00_world.sql
+++ sql/updates/world/4.3.4/2023_01_30_00_world.sql
@@ -3,7 +3,8 @@ DELETE FROM `spell_script_names` WHERE `ScriptName` IN
 'spell_mage_glyph_of_ice_block',
 'spell_mage_ice_block',
 'spell_mage_glyph_of_icy_veins',
-'spell_mage_glyph_of_polymorph');
+'spell_mage_glyph_of_polymorph',
+'spell_mage_icy_veins');
 
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (45438, 'spell_mage_ice_block'),
