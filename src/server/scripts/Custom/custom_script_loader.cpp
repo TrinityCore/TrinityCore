75526f7f6bead633d434cd259945c146ca03b702
小女孩 <xuehyc@163.com>
2023-02-12 15:58:36 +0800

    禁用新加的脚本,此脚本会导致服务端无法启动

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -29,7 +29,7 @@ void AddSC_custom_player_script();  //单人随机地下城查找器脚本
 void AddSC_GuardianAngel();         //守护神
 //void AddSC_debug_lfg();             //单人进地下城尝试, OpenLCore,已经自己通过配置实现,停用
 //void AddSC_double_xp();             //周六、周天双倍经验, OpenLCore,和现有XpWeekend重复,故而停用
-void AddSC_start_equipment();           //OpenLCore,出生送装备
+//void AddSC_start_equipment();           //OpenLCore,出生送装备
 void AddSC_quest_conversation();        //任务对话?
 
 
@@ -55,6 +55,6 @@ void AddCustomScripts()
    AddSC_GuardianAngel();          //守护神
    //AddSC_debug_lfg();              //单人进地下城尝试, OpenLCore,已经自己通过配置实现,停用
    //AddSC_double_xp();             //周六、周天双倍经验, OpenLCore,和现有XpWeekend重复,故而停用
-      AddSC_start_equipment();      //OpenLCore,出生送装备
+      //AddSC_start_equipment();      //OpenLCore,出生送装备
    AddSC_quest_conversation();      //任务对话?
 }
