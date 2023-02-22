81671602a2971201619658f829f9e738bf990e20
小女孩 <xuehyc@163.com>
2023-02-22 21:27:18 +0800

    更新名称错误(push改为pull)

--- 通用/git/特定次数git pull.bat
+++ 通用/git/特定次数git pull.bat
@@ -1,9 +1,9 @@
 @echo off
-set num=10
-echo 本程序共执行%num%次push...
+set num=999
+echo 本程序共执行%num%次pull...
 echo 如需修改执行次数,可编辑本文件,修改num的赋值.
 cd /d D:\Program Files (x86)\WOWDIY\TrinityCore
 for /l %%i in (1,1,%num%) do (
-echo 第%%i次push
+echo 第%%i次pull
  git pull
 )
