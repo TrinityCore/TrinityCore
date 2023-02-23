fb25863a736689ddd99f2d5b22fa2fdd53843d8b
小女孩 <xuehyc@163.com>
2023-02-24 07:48:01 +0800

    注释部分内容,以免无法正常生成

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -2066,13 +2066,13 @@ void Unit::HandleEmoteCommand(Emote emoteId, Player* target /*=nullptr*/, Trinit
     }
 }
 
-BrawlersGuild* Unit::GetBrawlerGuild()
-{
-    if (Map* map = GetMap())
-        return map->m_brawlerGuild;
-
-    return nullptr;
-}
+//BrawlersGuild* Unit::GetBrawlerGuild()
+//{
+//    if (Map* map = GetMap())
+//        return map->m_brawlerGuild;
+//
+//    return nullptr;
+//}
 
 void Unit::AttackerStateUpdate(Unit* victim, WeaponAttackType attType, bool extra)
 {
