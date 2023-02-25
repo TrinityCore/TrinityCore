15db8fb77f2bc9ed7c6a6339d0b01116ab117952
小女孩 <xuehyc@163.com>
2023-02-25 19:20:25 +0800

    改为拉取特定库

--- 通用/git/特定次数git fetch.bat
+++ 通用/git/特定次数git fetch.bat
@@ -5,5 +5,5 @@ echo 如需修改执行次数,可编辑本文件,修改num的赋值.
 cd /d D:\Program Files (x86)\WOWDIY\TrinityCore
 for /l %%i in (1,1,%num%) do (
 echo 第%%i次fetch
- git fetch
+ git fetch https://gitlab.com/xuehyc/origins-project-source-2
 )
