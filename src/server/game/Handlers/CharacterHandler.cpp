c5e2eaa41c780c84cbd8a1f7b202b6e18806cba4
小女孩 <xuehyc@163.com>
2023-02-26 13:33:32 +0800

    Revert "Update CharacterHandler.cpp"
    This reverts commit ae68f9a63bf6568f1def6b5f3bf852955c3f54b5.

--- src/server/game/Handlers/CharacterHandler.cpp
+++ src/server/game/Handlers/CharacterHandler.cpp
@@ -1259,8 +1259,6 @@ void WorldSession::HandlePlayerLogin(LoginQueryHolder const& holder)
     {
         if (Guild* guild = sGuildMgr->GetGuildById(pCurrChar->GetGuildId()))
             guild->SendLoginInfo(this);
-            break;       
-    
         else
         {
             // remove wrong guild data
@@ -1268,13 +1266,6 @@ void WorldSession::HandlePlayerLogin(LoginQueryHolder const& holder)
             pCurrChar->SetInGuild(UI64LIT(0));
         }
     }
-
-    if (pCurrChar->GetTeam() == ALLIANCE)
-            {
-                guidLow = ObjectGuid::Create<HighGuid::Transport>(35);
-                gobTransport = HashMapHolder<Transport>::Find(guidLow);
-                if (gobTransport)
-                {
 
     pCurrChar->RemoveAurasWithInterruptFlags(SpellAuraInterruptFlags::Login);
 
