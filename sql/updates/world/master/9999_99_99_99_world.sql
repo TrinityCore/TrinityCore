89540e064d1872dcc12333d522256c5cbc8e3686
ModoX <moardox@gmail.com>
2023-02-28 22:40:08 +0100

    dont OR 1 to 0 values

--- sql/updates/world/master/9999_99_99_99_world.sql
+++ sql/updates/world/master/9999_99_99_99_world.sql
@@ -1,14 +1,19 @@
 -- Shift existing rows by 3 bits to reserve space for 3 other tables
 UPDATE `waypoint_data` SET `id`=(`id` << 3) ORDER BY `id` DESC;
 UPDATE `creature_addon` SET `path_id`=(`path_id` << 3);
-UPDATE `smart_scripts` SET `event_param2`=((`event_param2` << 3))  WHERE `event_type`=34 AND `event_param2` <> 4294967295; -- skip -1
 
 -- use bit 0 for waypoints
 DELETE FROM `waypoint_data` WHERE `id` & 1;
 INSERT INTO `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`) SELECT ((w.`entry` << 3) | 1), w.`pointid`, w.`position_x`, w.`position_y`, w.`position_z`, w.`orientation`, w.`delay` FROM `waypoints` w;
 
-UPDATE `smart_scripts` SET `action_param2`=((`action_param2` << 3) | 1) WHERE `action_type`=53;
-UPDATE `smart_scripts` SET `action_param1`=((`action_param1` << 3) | 1), `action_param2`=((`action_param2` << 3) | 1), `action_param3`=((`action_param3` << 3) | 1), `action_param4`=((`action_param4` << 3) | 1), `action_param5`=((`action_param5` << 3) | 1), `action_param6`=((`action_param6` << 3) | 1), `action_param7`=((`action_param7` << 3) | 1) WHERE `action_type`=113;
-UPDATE `smart_scripts` SET `event_param2`=((`event_param2` << 3) | 1)  WHERE `event_type` IN(40, 55, 56, 57, 58);
+UPDATE `smart_scripts` SET `action_param2`=((`action_param2` << 3) | 1) WHERE `action_type`=53 AND `action_param2` > 0;
+UPDATE `smart_scripts` SET `action_param1`=((`action_param1` << 3) | 1) WHERE `action_type`=113 AND `action_param1` > 0
+UPDATE `smart_scripts` SET `action_param2`=((`action_param2` << 3) | 1) WHERE `action_type`=113 AND `action_param2` > 0;
+UPDATE `smart_scripts` SET `action_param3`=((`action_param3` << 3) | 1) WHERE `action_type`=113 AND `action_param3` > 0;
+UPDATE `smart_scripts` SET `action_param4`=((`action_param4` << 3) | 1) WHERE `action_type`=113 AND `action_param4` > 0;
+UPDATE `smart_scripts` SET `action_param5`=((`action_param5` << 3) | 1) WHERE `action_type`=113 AND `action_param5` > 0;
+UPDATE `smart_scripts` SET `action_param6`=((`action_param6` << 3) | 1) WHERE `action_type`=113 AND `action_param6` > 0;
+UPDATE `smart_scripts` SET `action_param7`=((`action_param7` << 3) | 1) WHERE `action_type`=113 AND `action_param7` > 0;
+UPDATE `smart_scripts` SET `event_param2`=((`event_param2` << 3) | 1)  WHERE `event_type` IN(40, 55, 56, 57, 58) AND `event_param2` > 0;
 
 DROP TABLE IF EXISTS `waypoints`;
