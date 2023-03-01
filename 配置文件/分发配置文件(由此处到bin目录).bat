df4ff6c100239886f99df2a046a9ae475799ddf0
小女孩 <xuehyc@163.com>
2023-03-01 23:12:21 +0800

    更改编码及备注(此批处理必须ANSI编码,不然在执行时会有乱码,导致无法执行)

--- 配置文件/分发配置文件(由此处到bin目录).bat
+++ 配置文件/分发配置文件(由此处到bin目录).bat
@@ -1,6 +1,7 @@
-copy /Y "D:\Program Files (x86)\WOWDIY\XCore\閰嶇疆鏂囦欢\bnetserver.conf" "D:\Program Files (x86)\WOWDIY\XCore\out\build\x64-Debug\bin"
-copy /Y "D:\Program Files (x86)\WOWDIY\XCore\閰嶇疆鏂囦欢\worldserver.conf" "D:\Program Files (x86)\WOWDIY\XCore\out\build\x64-Debug\bin"
+rem 注意此批处理,必须设置文本格式为ANSI,不然乱码,无法正常执行
+copy /Y "D:\Program Files (x86)\WOWDIY\XCore\配置文件\bnetserver.conf" "D:\Program Files (x86)\WOWDIY\XCore\out\build\x64-Debug\bin"
+copy /Y "D:\Program Files (x86)\WOWDIY\XCore\配置文件\worldserver.conf" "D:\Program Files (x86)\WOWDIY\XCore\out\build\x64-Debug\bin"
 
 
-copy /Y "D:\Program Files (x86)\WOWDIY\XCore\閰嶇疆鏂囦欢\bnetserver.conf" "D:\Program Files (x86)\WOWDIY\XCore\out\build\x64-Release\bin"
-copy /Y "D:\Program Files (x86)\WOWDIY\XCore\閰嶇疆鏂囦欢\worldserver.conf" "D:\Program Files (x86)\WOWDIY\XCore\out\build\x64-Release\bin"
+copy /Y "D:\Program Files (x86)\WOWDIY\XCore\配置文件\bnetserver.conf" "D:\Program Files (x86)\WOWDIY\XCore\out\build\x64-Release\bin"
+copy /Y "D:\Program Files (x86)\WOWDIY\XCore\配置文件\worldserver.conf" "D:\Program Files (x86)\WOWDIY\XCore\out\build\x64-Release\bin"
