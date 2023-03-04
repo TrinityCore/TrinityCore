0f88396ca1e9ddb2440e78f9d5c0a2521e4d1fc4
小女孩 <xuehyc@163.com>
2023-03-04 18:07:03 +0800

    添加小女孩的低语脚本

---
+++ src/server/scripts/Custom/XGirlWhispersOnLevelUp.cpp
@@ -0,0 +1,29 @@
+﻿#include "Config.h"
+#include "Chat.h"
+
+class XGirlWhispersOnLevelUp : public PlayerScript
+{
+    enum levels
+    {
+        start_level = 1,
+        max_level = 70
+    };
+
+public:
+	XGirlWhispersOnLevelUp() : PlayerScript("XGirlWhispersOnLevelUp") { }
+	void OnLevelChanged(Player * player, uint8 oldLevel)
+	{
+		if (sConfigMgr->GetBoolDefault("XGirlWhispersOnLevelUp", true))
+		{
+			if (oldLevel == start_level)//升2级
+                ChatHandler(player->GetSession()).SendSysMessage("Long long ago,the world is a mass...");
+            if (oldLevel == start_level+5)//升7级
+                ChatHandler(player->GetSession()).SendSysMessage("A little girl appeared...");
+		}
+	}
+};
+
+void AddSC_XGirlWhispersOnLevelUp()
+{
+	new XGirlWhispersOnLevelUp();
+}
