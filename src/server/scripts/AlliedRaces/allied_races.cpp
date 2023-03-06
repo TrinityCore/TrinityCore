0d0586556cfe18cbe2104c974e18312933382b28
小女孩 <xuehyc@163.com>
2023-03-06 18:39:19 +0800

    错别字修改

--- src/server/scripts/AlliedRaces/allied_races.cpp
+++ src/server/scripts/AlliedRaces/allied_races.cpp
@@ -56,7 +56,7 @@ public:
 		{
             /* Cinematic - Pandaren death knight */
             //Store all future Allied races cinematics on first login here, not in CharacterHandler, it will create camera bugs and some other issues
-            //看这意思是熊猫人DK要播放特地动画
+            //看这意思是熊猫人DK要播放特的动画
 			if (player->GetRace() == RACE_PANDAREN_HORDE || RACE_PANDAREN_ALLIANCE && player->GetMapId() == MAP_ALLIED_DK_ICECROWN && player->GetClass() == CLASS_DEATH_KNIGHT)
             {
                // player->GetSceneMgr().PlaySceneByPackageId(2780,  "待补充", nullptr);//此处"待补充"缺个参数,但是暂时不会写
