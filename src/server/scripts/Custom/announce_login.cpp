bb34278583cabb9ca18ff21653f4a181052fdf47
小女孩 <xuehyc@163.com>
2023-04-03 10:31:50 +0800

    格式化代码样式

--- src/server/scripts/Custom/announce_login.cpp
+++ src/server/scripts/Custom/announce_login.cpp
@@ -8,21 +8,21 @@ public:
 
     void OnLogin(Player* player, bool /*loginFirst*/)
     {
-		if (sConfigMgr->GetBoolDefault("Login.Announcer.Enable", true))//读取配置文件
-		{
-        if (player->GetTeam() == ALLIANCE)
-                {
-            std::ostringstream ss;
-                        ss << "|cff3DAEFF[ Login Announcer ]|cffFFD800 : Player|cff4CFF00 " << player->GetName() << " |cffFFD800Is Online. This Player is|cff0026FF Alliance";
-            sWorld->SendServerMessage(SERVER_MSG_STRING, ss.str().c_str());//**上线了,名字是:**,联盟
-                }
-                else
-                {
-            std::ostringstream ss;
-                        ss << "|cff3DAEFF[ Login Announcer ]|cffFFD800 : Player|cff4CFF00 " << player->GetName() << " |cffFFD800Is Online. This Player is|cffFF0000 Horde" ;
-            sWorld->SendServerMessage(SERVER_MSG_STRING, ss.str().c_str());//**上线了,名字是:**,部落
-                }
-		}
+        if (sConfigMgr->GetBoolDefault("Login.Announcer.Enable", true))//读取配置文件
+        {
+            if (player->GetTeam() == ALLIANCE)
+            {
+                std::ostringstream ss;
+                ss << "|cff3DAEFF[ Login Announcer ]|cffFFD800 : Player|cff4CFF00 " << player->GetName() << " |cffFFD800Is Online. This Player is|cff0026FF Alliance";
+                sWorld->SendServerMessage(SERVER_MSG_STRING, ss.str().c_str());//**上线了,名字是:**,联盟
+            }
+            else
+            {
+                std::ostringstream ss;
+                ss << "|cff3DAEFF[ Login Announcer ]|cffFFD800 : Player|cff4CFF00 " << player->GetName() << " |cffFFD800Is Online. This Player is|cffFF0000 Horde";
+                sWorld->SendServerMessage(SERVER_MSG_STRING, ss.str().c_str());//**上线了,名字是:**,部落
+            }
+        }
     }
 };
 
