ad9f88d1591274985f9ac5f8d4ce607f8ad3953c
Aokromes <Aokromes@users.noreply.github.com>
2023-04-05 09:39:03 +0200

    DB/Quest: Sons of Hodir reputation reward rate
    closes #28798 by Jildor

---
+++ sql/updates/world/3.3.5/2023_04_05_03_world.sql
@@ -0,0 +1,2 @@
+-- Sons of Hodir: quests reputation reward rate increased 30%
+UPDATE `reputation_reward_rate` SET `quest_rate`=1.3,`quest_daily_rate`=1.3,`quest_weekly_rate`=1.3,`quest_monthly_rate`=1.3,`quest_repeatable_rate`=1.3 WHERE `faction`=1119;
