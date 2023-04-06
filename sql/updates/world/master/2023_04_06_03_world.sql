c21a01442eb7f24dd9ba06eb2045f09ba27bff5b
Teleqraph <nyrdeveloper@gmail.com>
2023-04-06 17:26:19 +0200

    Scripts/SilverpineForest: Removed sparring hackfixes (#28896)

---
+++ sql/updates/world/master/2023_04_06_03_world.sql
@@ -0,0 +1,5 @@
+DELETE FROM `creature_template_sparring` WHERE `Entry` IN (44791, 44792, 44793);
+INSERT INTO `creature_template_sparring` (`Entry`, `NoNPCDamageBelowHealthPct`) VALUES
+(44791, 80),
+(44792, 80),
+(44793, 80);
