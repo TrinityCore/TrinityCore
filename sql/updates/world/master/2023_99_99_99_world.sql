cb5708ab1057be9c4fc805e479f95e24431127fa
Nyr <nyrdeveloper@gmail.com>
2023-04-06 03:18:03 +0200

    Scripts/SilverpineForest: Removed sparring hackfixes to use recently merged sparring system
    * also cleaned up a few things in code that were ugly.

---
+++ sql/updates/world/master/2023_99_99_99_world.sql
@@ -0,0 +1,5 @@
+DELETE FROM `creature_template_sparring` WHERE `Entry` IN (44791, 44792, 44793);
+INSERT INTO `creature_template_sparring` (`Entry`, `NoNPCDamageBelowHealthPct`) VALUES
+(44791, 85),
+(44792, 85),
+(44793, 85);
