8ad2d643114c168504a87654a9f33cd2b1ced6f6
小女孩 <xuehyc@163.com>
2023-04-11 07:44:45 +0800

    根据LgionPlayerBot补充枚举

--- src/common/Logging/Appender.h
+++ src/common/Logging/Appender.h
@@ -58,4 +58,63 @@ public:
     explicit InvalidAppenderArgsException(std::string const& message) : std::length_error(message) { }
 };
 
+enum LogFilterType
+{
+    LOG_FILTER_GENERAL = 0,  //bnet world          // This one should only be used inside Log.cpp
+    LOG_FILTER_UNITS = 1,  //world               // Anything related to units that doesn't fit in other categories. ie. creature formations
+    LOG_FILTER_PETS = 2,  //world
+    LOG_FILTER_VEHICLES = 3,  //world
+    LOG_FILTER_TSCR = 4,  //world               // C++ AI, instance scripts, etc.
+    LOG_FILTER_DATABASE_AI = 5,  //world               // SmartAI, Creature* * AI
+    LOG_FILTER_MAPSCRIPTS = 6,  //world
+    LOG_FILTER_NETWORKIO = 7,  //bnet world
+    LOG_FILTER_SPELLS_AURAS = 8,  //world
+    LOG_FILTER_ACHIEVEMENTSYS = 9,  //world
+    LOG_FILTER_CONDITIONSYS = 10, //world
+    LOG_FILTER_POOLSYS = 11, //world
+    LOG_FILTER_AUCTIONHOUSE = 12, //world
+    LOG_FILTER_BATTLEGROUND = 13, //world
+    LOG_FILTER_OUTDOORPVP = 14, //world
+    LOG_FILTER_CHATSYS = 15, //world
+    LOG_FILTER_LFG = 16, //world
+    LOG_FILTER_MAPS = 17, //world
+    LOG_FILTER_PLAYER = 18, //world               // Any player log that does not fit in other player filters
+    LOG_FILTER_PLAYER_LOADING = 19, //world               // Debug output from Player::_Load functions
+    LOG_FILTER_PLAYER_ITEMS = 20, //world
+    LOG_FILTER_PLAYER_SKILLS = 21, //world
+    LOG_FILTER_PLAYER_CHATLOG = 22, //world
+    LOG_FILTER_LOOT = 23, //world
+    LOG_FILTER_GUILD = 24, //world
+    LOG_FILTER_TRANSPORTS = 25, //world
+    LOG_FILTER_SQL = 26, //bnet world
+    LOG_FILTER_GMCOMMAND = 27, //world
+    LOG_FILTER_REMOTECOMMAND = 28, //world
+    LOG_FILTER_WARDEN = 29, //world
+    LOG_FILTER_WORLDSERVER = 31, //world
+    LOG_FILTER_GAMEEVENTS = 32, //world
+    LOG_FILTER_CALENDAR = 33, //world
+    LOG_FILTER_CHARACTER = 34, //world
+    LOG_FILTER_SQL_DRIVER = 36, //bnet world
+    LOG_FILTER_SQL_DEV = 37, //bnet world
+    LOG_FILTER_PLAYER_DUMP = 38, //world
+    LOG_FILTER_BATTLEFIELD = 39, //world
+    LOG_FILTER_SERVER_LOADING = 40, //world
+    LOG_FILTER_OPCODES = 41, //world
+    LOG_FILTER_REALMLIST = 44, //bnet
+    LOG_FILTER_BATTLENET = 45, //bnet
+    LOG_FILTER_BNET_SESSION = 47, //bnet
+    LOG_FILTER_PROC = 48, //world
+    LOG_FILTER_DONATE = 50, //world
+    LOG_FILTER_BATTLEPET = 51, //world
+    LOG_FILTER_PROTOBUF = 52, //world
+    LOG_FILTER_AREATRIGGER = 53, //world
+    LOG_FILTER_GOLD = 54, //world
+    LOG_FILTER_WORLD_QUEST = 55, //world
+    LOG_FILTER_CHALLENGE = 56, //world
+    LOG_FILTER_PATH_GENERATOR = 58, //world
+    LOG_FILTER_MMAPS = 59, //world
+    LOG_FILTER_VMAPS = 60, //world
+
+    LOG_FILTER_MAX
+};
 #endif
