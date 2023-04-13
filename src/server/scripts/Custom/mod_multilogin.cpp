14199d3673a72ce81ecaf0e3e4e7d1447f47091b
小女孩 <xuehyc@163.com>
2023-04-13 09:12:40 +0800

    剔除多连接用户(底层应该已实现,为安全与避免冗余考虑,全注释)

---
+++ src/server/scripts/Custom/mod_multilogin.cpp
@@ -0,0 +1,43 @@
+﻿///***
+// *  @project: Firestorm Freelance
+// *  @author: Meltie2013 (github) aka Lilcrazy
+// *  @copyright: 2017
+// */
+//
+//#include "ScriptMgr.h"
+//#include "Config.h"
+//#include "World.h"
+//#include "WorldSession.h"
+//
+// // Check to see if the player is attempting to multi-box
+//class multi_login_check : public PlayerScript
+//{
+//public:
+//    multi_login_check() : PlayerScript("multi_login_check") { }
+//
+//    void OnLogin(Player* player, bool /*firstLogin*/) override
+//    {
+//        if (sConfigMgr->GetBoolDefault("Disallow.Multiple.Client", true))
+//        {
+//            SessionMap sessions = sWorld->GetAllSessions();
+//            for (SessionMap::iterator itr = sessions.begin(); itr != sessions.end(); ++itr)
+//            {
+//                if (Player* login = itr->second->GetPlayer())
+//                {
+//                    if (player != login)
+//                    {
+//                        // If Remote Address matches, remove the player from the world
+//                        if (player->GetSession()->GetRemoteAddress() == login->GetSession()->GetRemoteAddress())
+//                            player->GetSession()->KickPlayer();
+//                    }
+//                }
+//            }
+//        }
+//    }
+//};
+//
+//
+//void AddSC_player_session_checks()
+//{
+//    new multi_login_check;
+//}
