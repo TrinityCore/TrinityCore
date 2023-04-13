9264cd4fd170f8667be6b94d871b578963877c94
小女孩 <xuehyc@163.com>
2023-04-13 11:51:37 +0800

    Azgath的坐骑礼物

---
+++ src/server/scripts/Custom/azgath_gift_mount.cpp
@@ -0,0 +1,32 @@
+﻿/* ------------------------------------ */
+/* AUTHOR   Azgath                      */
+/*    FOR   Az'gath Private Server      */
+/* ------------------------------------ */
+
+class azgath_gift_mount : public PlayerScript
+{
+private:
+    bool isEnabled = false;
+    uint32 flySpell = 90265; /* Maître cavalier */
+    uint32 mountSpell = 307932; /* Wyrm éternel ensorcelé */
+
+public:
+    azgath_gift_mount() : PlayerScript("azgath_gift_mount") {}
+
+    void OnLogin(Player* player, bool /*firstLogin*/) {
+        if (isEnabled) {
+            if (!player->HasSpell(flySpell)) {
+                player->LearnSpell(flySpell, false);
+            }
+
+            if (!player->HasSpell(mountSpell)) {
+                player->LearnSpell(mountSpell, false);
+            }
+        }
+    }
+};
+
+void AddSC_azgath_gift_mount()
+{
+    new azgath_gift_mount();
+}
