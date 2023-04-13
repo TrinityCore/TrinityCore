d308ab0f152a833330b2458da86e7b84cbfb897b
小女孩 <xuehyc@163.com>
2023-04-13 08:38:30 +0800

    单人进地下城脚本(因已有相同功能,全注释,留后续研究)

---
+++ src/server/scripts/Custom/spp_lfg_solo.cpp
@@ -0,0 +1,26 @@
+﻿//#include "Config.h"
+//#include "World.h"
+//#include "LFGMgr.h"
+//#include "Chat.h"
+//
+//class spp_lfg_solo : public PlayerScript
+//{
+//public:
+//    spp_lfg_solo() : PlayerScript("spp_lfg_solo") { }
+//
+//    void OnLogin(Player* player, bool /*loginFirst*/)
+//    {
+//        if (sConfigMgr->GetBoolDefault("LFG.SoloMode", true))
+//        {
+//            if (!sLFGMgr->IsTesting())
+//            {
+//                sLFGMgr->ToggleTesting();
+//            }
+//        }
+//    }
+//};
+//
+//void AddSC_LFG_SoloMode()
+//{
+//    new spp_lfg_solo;
+//}
