7cb4faba2067dd4695397aea5bd7ed2b661f1880
小女孩 <xuehyc@163.com>
2023-04-13 10:52:24 +0800

    春节事件

---
+++ src/server/scripts/Custom/event_april.cpp
@@ -0,0 +1,46 @@
+﻿//#include "PrecompiledHeaders/ScriptPCH.h"
+//#include <ScriptMgr.h>
+//
+//#include "GameEventMgr.h"
+//
+//#define EVENT_APRIL             100
+//
+//
+//class PlayerScript_Event_April : public PlayerScript
+//{
+//public:
+//    PlayerScript_Event_April() :PlayerScript("PlayerScript_Event_April") {}
+//
+//    void OnCreatureKill(Player* Player, Creature* /*Creature*/) override
+//    {
+//        int32 tabFish[14] = { 32263,40855,32646,42160,42161,42162,42163,44563,42808,44562,33312,42836,39936,45246 };
+//        if (!sGameEventMgr->IsActiveEvent(EVENT_APRIL))
+//            return;
+//
+//        if (!Player->GetMap())
+//            return;
+//
+//        if (Player->GetMap()->IsBattlegroundOrArena() || Player->GetMap()->IsRaid())
+//            return;
+//        uint8 idFish = urand(0, 14);
+//        Player->SetDisplayId(tabFish[idFish]);
+//    }
+//
+//    void OnPVPKill(Player* Killer, Player* Killed) override
+//    {
+//        int32 tabFish[14] = { 32263,40855,32646,42160,42161,42162,42163,44563,42808,44562,33312,42836,39936,45246 };
+//        if (Killer->GetMap()->IsBattlegroundOrArena() || Killer->GetMap()->IsRaid())
+//            return;
+//
+//        if (!sGameEventMgr->IsActiveEvent(EVENT_APRIL))
+//            return;
+//
+//        uint8 idFish = urand(0, 14);
+//        Killer->SetDisplayId(tabFish[idFish]);
+//    }
+//};
+//
+//void AddSC_event_april()
+//{
+//    new PlayerScript_Event_April();
+//};
