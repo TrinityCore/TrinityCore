73c620daacbe08ad3a8661187937675f054cc05a
小女孩 <xuehyc@163.com>
2023-04-13 08:25:50 +0800

    当有玩家升级时,自动保存所有玩家信息,并向全服发送提醒(默认启用,可分开配置)

---
+++ src/server/scripts/Custom/spp_save_on_levelup.cpp
@@ -0,0 +1,31 @@
+﻿/*
+Single Player Project
+*/
+
+#include "Chat.h"
+#include "Config.h"
+#include "ScriptMgr.h"
+
+class spp_save_on_levelup : public PlayerScript
+{
+public:
+    spp_save_on_levelup() : PlayerScript("spp_save_on_levelup") { };
+
+    void OnLevelChanged(Player* player, uint8 /*oldLevel*/)
+    {
+        if (sConfigMgr->GetBoolDefault("Save.On.LevelUp", true))
+        {
+            if (sConfigMgr->GetBoolDefault("Save.On.LevelUp.Announcer", true))
+            {
+                ChatHandler(player->GetSession()).SendSysMessage("Characters saved on the server.");
+            }
+
+            ObjectAccessor::SaveAllPlayers();
+        }
+    }
+};
+
+void AddSC_save_on_levelup()
+{
+    new spp_save_on_levelup();
+}
