33f9865a39c50fd07d77abafe4b28cd9bf7ca82c
Naddley <NaddleyTC@gmail.com>
2023-04-14 16:10:09 +0200

    Fix nopch

--- src/server/scripts/Zandalar/Underrot/boss_elder_leaxa.cpp
+++ src/server/scripts/Zandalar/Underrot/boss_elder_leaxa.cpp
@@ -21,6 +21,7 @@
 #include "CreatureAIImpl.h"
 #include "Containers.h"
 #include "InstanceScript.h"
+#include "MotionMaster.h"
 #include "ScriptMgr.h"
 #include "ScriptedCreature.h"
 #include "SpellScript.h"
