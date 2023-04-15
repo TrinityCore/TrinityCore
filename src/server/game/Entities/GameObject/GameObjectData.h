6c71a84e6cd980cfe9a35421e8473dceebafba5b
Meji <alvaro.megias@outlook.com>
2023-04-15 20:45:21 +0200

    Core/GameObjects: Allow certain types to be sent to the client without display (#28908)

--- src/server/game/Entities/GameObject/GameObjectData.h
+++ src/server/game/Entities/GameObject/GameObjectData.h
@@ -1232,6 +1232,19 @@ struct GameObjectTemplate
         }
     }
 
+    bool IsDisplayMandatory() const
+    {
+        switch (type)
+        {
+            case GAMEOBJECT_TYPE_SPELL_FOCUS:
+            case GAMEOBJECT_TYPE_MULTI:
+            case GAMEOBJECT_TYPE_SIEGEABLE_MULTI:
+                return false;
+            default:
+                return true;
+        }
+    }
+
     void InitializeQueryData();
     WorldPacket BuildQueryData(LocaleConstant loc) const;
 };
