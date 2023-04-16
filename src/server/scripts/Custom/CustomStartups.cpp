ac1e3ba6174a0085f9134f9252cb3311ee375b8a
小女孩 <xuehyc@163.com>
2023-04-17 07:51:49 +0800

    新自定义脚本(因功能重复,全部注释)

---
+++ src/server/scripts/Custom/CustomStartups.cpp
@@ -0,0 +1,42 @@
+﻿//#include "Config.h"
+//#include "Chat.h"
+//#include "Log.h"
+//#include "ScriptMgr.h"
+//
+//namespace {
+//
+//    class OnNewCharFirstLogin : public PlayerScript
+//    {
+//
+//    public:
+//        OnNewCharFirstLogin() : PlayerScript("OnNewCharFirstLogin")
+//        {
+//        }
+//
+//        // run at first login of a new character
+//        void OnLogin(Player* player, bool firstLogin) override
+//        {
+//            if (firstLogin)
+//            {
+//                ChatHandler(player->GetSession()).SendSysMessage("|cffFF0000LegionCore |r");
+//            }
+//        }
+//    };
+//}
+//
+//class OnWorldserverLoaded : public WorldScript
+//{
+//public:
+//    OnWorldserverLoaded() : WorldScript("OnWorldserverLoaded") {}
+//
+//    // run always when worldserver has loaded
+//    void OnStartup() override
+//    {
+//        //TC_LOG_INFO(LOG_FILTER_WORLDSERVER, "LegionCore loaded...");
+//    }
+//};
+//
+//void AddSC_CustomStartups() {
+//    new OnNewCharFirstLogin();
+//    new OnWorldserverLoaded();
+//}
