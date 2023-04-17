12cedd2d88237e34b531d5f3dc23d130539a9cc3
Nyr <nyrdeveloper@gmail.com>
2023-04-17 03:21:53 +0200

    sql addition

---
+++ sql/updates/world/master/2023_04_16_02_world.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (264667, 390386);
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(264667, 'spell_hun_primal_rage'),
+(390386, 'spell_evo_fury_of_the_aspects');
