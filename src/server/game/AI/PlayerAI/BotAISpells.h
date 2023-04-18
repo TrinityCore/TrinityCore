fd745f8464ed9519c46ed530c52f609dc2764091
小女孩 <xuehyc@163.com>
2023-04-18 14:35:38 +0800

    对一部分合并重复定义并格式化

--- src/server/game/AI/PlayerAI/BotAISpells.h
+++ src/server/game/AI/PlayerAI/BotAISpells.h
@@ -11,113 +11,60 @@ public:
 	void InitializeSpells(Player* player);
 
 protected:
-	uint32 WarriorIDLE_AOEAddLife;// = 47440;			// 命令怒吼
-	uint32 WarriorIDLE_AOEAddPower;// = 47436;			// 攻强怒吼
-    //uint32 WarriorIDLE_AOEAddLife = 45517;			 
-    //uint32 WarriorIDLE_AOEAddPower = 27578;			
+	uint32 WarriorIDLE_AOEAddLife;// = 47440,45517;			// 命令怒吼
+	uint32 WarriorIDLE_AOEAddPower;// = 47436,27578			// 攻强怒吼	 	
 
 	uint32 WarriorWeapon_Status;// = 2457;
 	uint32 WarriorDefance_Status;// = 71;
 	uint32 WarriorRage_Status;// = 2458;
 
-	uint32 WarriorCommon_PowerAtt;// = 47450;			// 英勇打击
+	uint32 WarriorCommon_PowerAtt;// = 47450;25712		// 英勇打击
 	uint32 WarriorCommon_PowerThrow;// = 57755;			// 英勇投掷
-	uint32 WarriorCommon_PowerRelife;// = 55694;		// 狂怒恢复（有狂暴时使用回血）
+	uint32 WarriorCommon_PowerRelife;// = 55694;184364	// 狂怒恢复（有狂暴时使用回血）
 	uint32 WarriorCommon_ClearCtrl;// = 18499;			// 狂暴之怒（移除控制）
-	uint32 WarriorCommon_AOEFear;// = 5246;			// 近战范围群恐惧
-	uint32 WarriorCommon_SweepAtt;// = 47520;			// 顺劈斩
-	uint32 WarriorCommon_AddPower;// = 2687;			// 加怒气 
-	uint32 WarriorCommon_AOEDecPower;// = 47437;		// 群体减攻强吼
-
-    //uint32 WarriorCommon_PowerAtt = 25712;			 
-    //uint32 WarriorCommon_PowerThrow = 57755;			 
-    //uint32 WarriorCommon_PowerRelife = 184364;		 
-    //uint32 WarriorCommon_ClearCtrl = 18499;			 
-    //uint32 WarriorCommon_AOEFear = 65930;			
-    //uint32 WarriorCommon_SweepAtt = 231833;			
-    //uint32 WarriorCommon_AddPower = 173401;			
-    //uint32 WarriorCommon_AOEDecPower = 27579;		
-
-	uint32 WarriorDefance_HPojia;// = 47498;			// 毁灭打击 
-	uint32 WarriorDefance_Fuchou;// = 57823;			// 复仇
-	uint32 WarriorDefance_ShieldBlock;// = 2565;		// 盾牌格挡
-	uint32 WarriorDefance_ShieldAtt;// = 47488;			// 盾牌猛击
-	uint32 WarriorDefance_Pojia;// = 7386;				// 破甲
+	uint32 WarriorCommon_AOEFear;// = 5246;65930		// 近战范围群恐惧
+	uint32 WarriorCommon_SweepAtt;// = 47520;231833		// 顺劈斩
+	uint32 WarriorCommon_AddPower;// = 2687;173401		// 加怒气 
+	uint32 WarriorCommon_AOEDecPower;// = 47437;27579	// 群体减攻强吼
+		 	 	 		
+	uint32 WarriorDefance_HPojia;// = 47498;186688		// 毁灭打击 
+	uint32 WarriorDefance_Fuchou;// = 57823;40392		// 复仇
+	uint32 WarriorDefance_ShieldBlock;// = 2565;38031	// 盾牌格挡
+	uint32 WarriorDefance_ShieldAtt;// = 47488;23922	// 盾牌猛击
+	uint32 WarriorDefance_Pojia;// = 7386;25051			// 破甲
 	uint32 WarriorDefance_MaxLife;// = 12975;			// 破釜沉舟
 	uint32 WarriorDefance_ShiledWall;// = 871;			// 盾墙
-	uint32 WarriorDefance_Disarm;// = 676;				// 缴械
-	uint32 WarriorDefance_Support;// = 3411;			// 援助
-	uint32 WarriorDefance_Conk;// = 12809;				// 单体击晕
+	uint32 WarriorDefance_Disarm;// = 676;11879			// 缴械
+	uint32 WarriorDefance_Support;// = 3411;41198		// 援助
+	uint32 WarriorDefance_Conk;// = 12809;22427			// 单体击晕
 	uint32 WarriorDefance_AOEConk;// = 46968;			// 前方范围击晕
 	uint32 WarriorDefance_Pull;// = 355;				// 嘲讽
 
-    /*
-    uint32 WarriorDefance_HPojia = 186688;			
-    uint32 WarriorDefance_Fuchou = 40392;			
-    uint32 WarriorDefance_ShieldBlock = 38031;		
-    uint32 WarriorDefance_ShieldAtt = 23922;			
-    uint32 WarriorDefance_Pojia = 25051;				
-    uint32 WarriorDefance_MaxLife = 12975;			
-    uint32 WarriorDefance_ShiledWall = 871;			
-    uint32 WarriorDefance_Disarm = 11879;				
-    uint32 WarriorDefance_Support = 41198;			
-    uint32 WarriorDefance_Conk = 22427;				
-    uint32 WarriorDefance_AOEConk = 46968;			
-    uint32 WarriorDefance_Pull = 355;				
-    */
+	uint32 WarriorWeapon_SwordStorm;// = 46924;9632		        // 剑刃风暴
+	uint32 WarriorWeaponDefance_AOEAtt;// = 47502;165749        // 雷霆一击
+	uint32 WarriorWeaponDefance_Bleed;// = 47465;43931		    // 撕裂
+	uint32 WarriorWeaponDefance_SpellReflect;// = 23920;57643   // 法术反射
+	uint32 WarriorWeaponDefance_ShieldHit;// = 72;149383		// 盾击（施法打断）
 
-	uint32 WarriorWeapon_SwordStorm;// = 46924;			// 剑刃风暴
-	uint32 WarriorWeaponDefance_AOEAtt;// = 47502;		// 雷霆一击
-	uint32 WarriorWeaponDefance_Bleed;// = 47465;		// 撕裂
-	uint32 WarriorWeaponDefance_SpellReflect;// = 23920;	// 法术反射
-	uint32 WarriorWeaponDefance_ShieldHit;// = 72;		// 盾击（施法打断）
-
-	uint32 WarriorWeapon_HighThrow;// = 64382;			// 破无敌投掷
-	uint32 WarriorWeapon_Charge;// = 11578;			// 冲锋
+	uint32 WarriorWeapon_HighThrow;// = 64382;65940		// 破无敌投掷
+	uint32 WarriorWeapon_Charge;// = 11578;100			// 冲锋
 	uint32 WarriorWeapon_Suppress;// = 7384;			// 压制
-	uint32 WarriorWeapon_Backstorm;// = 20230;			// 反击风暴
-	uint32 WarriorWeapon_DeadAtt;// = 47486;			// 致死打击
+	uint32 WarriorWeapon_Backstorm;// = 20230;22857		// 反击风暴
+	uint32 WarriorWeapon_DeadAtt;// = 47486;27580		// 致死打击
 
-	uint32 WarriorWeaponRage_FullKill;// = 47471;		// 斩杀
+	uint32 WarriorWeaponRage_FullKill;// = 47471;5308	// 斩杀
 	uint32 WarriorWeaponRage_WinAttack;// = 34428;		// 乘胜追击
 	uint32 WarriorWeaponRage_Backfillet;// = 1715;		// 断筋
 
 	uint32 WarriorRage_Harsh;// = 12323;				// 刺耳怒吼
 	uint32 WarriorRage_HeadAtt;// = 6552;				// 拳击（施法打断）
-	uint32 WarriorRage_Intercept;// = 20252;			// 拦截
-	uint32 WarriorRage_Whirlwind;// = 1680;			// 旋风斩
-	uint32 WarriorRage_Impertinency;// = 1719;			// 鲁莽
-	uint32 WarriorRage_Needdead;// = 12292;			// 死亡之愿
-	uint32 WarriorRage_Bloodthirsty;// = 23881;			// 嗜血
-	uint32 WarriorRage_ReIntercept;// = 60970;			// 解除移动限制和解除拦截CD
-
-    /*
-    uint32 WarriorWeapon_SwordStorm = 9632;			
-    uint32 WarriorWeaponDefance_AOEAtt = 165749;		
-    uint32 WarriorWeaponDefance_Bleed = 43931;		
-    uint32 WarriorWeaponDefance_SpellReflect = 57643;	
-    uint32 WarriorWeaponDefance_ShieldHit = 149383;		
-
-    uint32 WarriorWeapon_HighThrow = 65940;			
-    uint32 WarriorWeapon_Charge = 100;			
-    uint32 WarriorWeapon_Suppress = 7384;			
-    uint32 WarriorWeapon_Backstorm = 22857;			
-    uint32 WarriorWeapon_DeadAtt = 27580;			
-
-    uint32 WarriorWeaponRage_FullKill = 5308;		
-    uint32 WarriorWeaponRage_WinAttack = 34428;		
-    uint32 WarriorWeaponRage_Backfillet = 1715;		
-
-    uint32 WarriorRage_Harsh = 12323;				
-    uint32 WarriorRage_HeadAtt = 6552;				
-    uint32 WarriorRage_Intercept = 20615;			
-    uint32 WarriorRage_Whirlwind = 12950;			
-    uint32 WarriorRage_Impertinency = 13847;			
-    uint32 WarriorRage_Needdead = 199261;			
-    uint32 WarriorRage_Bloodthirsty = 39071;			
-    uint32 WarriorRage_ReIntercept = 184364;			
-    */
-};
+	uint32 WarriorRage_Intercept;// = 20252;20615		// 拦截
+	uint32 WarriorRage_Whirlwind;// = 1680;12950		// 旋风斩
+	uint32 WarriorRage_Impertinency;// = 1719;13847		// 鲁莽
+	uint32 WarriorRage_Needdead;// = 12292;199261		// 死亡之愿
+	uint32 WarriorRage_Bloodthirsty;// = 23881;39071	// 嗜血
+	uint32 WarriorRage_ReIntercept;// = 60970;184364	// 解除移动限制和解除拦截CD
+    };
 
 class BotPaladinSpells
 {
