5df71e9b14ba78850037602cd5475aa250682457
Naddley <64811442+Naddley@users.noreply.github.com>
2023-04-25 23:41:51 +0200

    Scripts/Underrot: Implement Cragmaw the Infested encounter (#28934)

--- src/server/scripts/Zandalar/zandalar_script_loader.cpp
+++ src/server/scripts/Zandalar/zandalar_script_loader.cpp
@@ -20,6 +20,7 @@
 // Underrot
 void AddSC_instance_underrot();
 void AddSC_boss_elder_leaxa();
+void AddSC_boss_cragmaw_the_infested();
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
@@ -28,4 +29,5 @@ void AddZandalarScripts()
     // Underrot
     AddSC_instance_underrot();
     AddSC_boss_elder_leaxa();
+    AddSC_boss_cragmaw_the_infested();
 }
