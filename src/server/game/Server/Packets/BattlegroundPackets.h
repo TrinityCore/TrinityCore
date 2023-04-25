1e11f5137541c011c384d0cc905ac926c3532863
François B <445742+Selenium@users.noreply.github.com>
2023-04-25 15:48:40 +0200

    Core/PacketIO: Fixed bracket num in SMSG_RATED_PVP_INFO

--- src/server/game/Server/Packets/BattlegroundPackets.h
+++ src/server/game/Server/Packets/BattlegroundPackets.h
@@ -436,7 +436,7 @@ namespace WorldPackets
         class RatedPvpInfo final : public ServerPacket
         {
         public:
-            RatedPvpInfo() : ServerPacket(SMSG_RATED_PVP_INFO, 6 * sizeof(BracketInfo)) { }
+            RatedPvpInfo() : ServerPacket(SMSG_RATED_PVP_INFO, 7 * sizeof(BracketInfo)) { }
 
             WorldPacket const* Write() override;
 
@@ -462,7 +462,7 @@ namespace WorldPackets
                 int32 Unused4 = 0;
                 int32 Rank = 0;
                 bool Disqualified = false;
-            } Bracket[6];
+            } Bracket[7];
         };
 
         struct RatedMatchDeserterPenalty
