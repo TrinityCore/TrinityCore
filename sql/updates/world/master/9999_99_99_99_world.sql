4dce43fcd6ed7a1e66699c6576a5db82e78ed16b
Shauren <shauren.trinity@gmail.com>
2023-05-07 16:24:15 +0200

    prepare merge

--- sql/updates/world/master/9999_99_99_99_world.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM `spell_script_names` WHERE `spell_id`=120644 AND `ScriptName`='spell_pri_halo_shadow';
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
-(120644, 'spell_pri_halo_shadow');
