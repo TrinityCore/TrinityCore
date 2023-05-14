5517a8159e1e8c672dcf9d680fcd7a8a7298bc48
Aokromes <aokromes@users.noreply.github.com>
2023-05-14 16:33:38 +0200

    DB/Creature: Update few template values for Stabled Kurenai Panther and Stabled Kurenai Lion in Telaar
    closes #28981 by CraftedRO
    
    (cherry picked from commit 659374154ce8fb7f69ae99efa173f390933ebdf1)
    
    (cherry picked from commit c6ab117cd68781ed9ec3a1ca576b9046380c6b25)

---
+++ sql/updates/world/cata_classic/2026_08_09_20_world_2026_05_25_14_world_2023_05_14_01_world.sql
@@ -0,0 +1,2 @@
+-- Stabled Kurenai Panther and Stabled Kurenai Lion sniffed values
+UPDATE `creature_template_addon` SET `StandState`=3, `SheathState`=0, `PvpFlags`=1 WHERE `entry` IN (19027, 19030);
