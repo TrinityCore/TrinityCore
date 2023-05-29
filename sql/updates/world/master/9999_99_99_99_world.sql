9ca2ef7c75a769d5661ad15e8ce7bf93664ae68f
Nyr <nyrdeveloper@gmail.com>
2023-05-29 19:58:40 +0200

    oopsies

--- sql/updates/world/master/9999_99_99_99_world.sql
+++ sql/updates/world/master/9999_99_99_99_world.sql
@@ -2,6 +2,6 @@ DELETE FROM `spell_proc` WHERE `SpellId` IN (390881);
 INSERT INTO `spell_proc` (`SpellId`,`SchoolMask`,`SpellFamilyName`,`SpellFamilyMask0`,`SpellFamilyMask1`,`SpellFamilyMask2`,`SpellFamilyMask3`,`ProcFlags`,`ProcFlags2`,`SpellTypeMask`,`SpellPhaseMask`,`HitMask`,`AttributesMask`,`DisableEffectsMask`,`ProcsPerMinute`,`Chance`,`Cooldown`,`Charges`) VALUES
 (390881,0x00,6,0x00000000,0x00000000,0x00400000,0x00000000,0x0,0x0,0x2,0x2,0x403,0x0,0x0,0,0,0,0); -- Healing Chorus
 
-DELETE FROM `spell_script_names` WHERE `spell_id`=391339;
+DELETE FROM `spell_script_names` WHERE `spell_id`=204883;
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
 (204883, 'spell_pri_circle_of_healing');
