ae61016b6443e87d5f30f0d2700179c66d3d0ae1
Teleqraph <nyrdeveloper@gmail.com>
2023-06-19 02:58:14 +0200

    Update 9999_99_99_99_world.sql

--- sql/updates/world/master/9999_99_99_99_world.sql
+++ sql/updates/world/master/9999_99_99_99_world.sql
@@ -1,3 +1,5 @@
-DELETE FROM `spell_script_names` WHERE `spell_id`=33110 AND `ScriptName`='spell_pri_prayer_of_mending_heal';
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (33076, 41635, 33110);
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(33076, 'spell_pri_prayer_of_mending_dummy'),
+(41635, 'spell_pri_prayer_of_mending'),
 (33110, 'spell_pri_prayer_of_mending_heal');
