005f60abfe4b5eb085bbb0bdf490f4a315b973fd
Meji <alvaro.megias@outlook.com>
2023-06-24 12:56:44 +0200

    Update sql

--- sql/updates/world/master/9999_99_99_99_world.sql
+++ sql/updates/world/master/9999_99_99_99_world.sql
@@ -1,2 +1,2 @@
 -- 
-UPDATE `smart_scripts` SET `event_param1`=0xFFFFFFFF WHERE `event_type`=58 AND `event_param1`=0;
+UPDATE `smart_scripts` SET `event_param1`=0xFFFFFFFF WHERE `event_type` IN (40,55,56,57,58) AND `event_param1`=0;
