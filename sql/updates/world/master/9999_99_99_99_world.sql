7dc5630e2758e539e1bb5877ac9d328137ca2e22
Nyr <nyrdeveloper@gmail.com>
2023-06-24 22:15:43 +0200

    remove Divine Hymn from spell proc
    * this one is the main spell, only the periodic trigger should trigger it (it triggers at the beginning due to SPELL_ATTR5_EXTRA_INITIAL_PERIOD.

--- sql/updates/world/master/9999_99_99_99_world.sql
+++ sql/updates/world/master/9999_99_99_99_world.sql
@@ -1,7 +1,7 @@
 DELETE FROM `spell_proc` WHERE `SpellId` IN (392988, 405216);
 INSERT INTO `spell_proc` (`SpellId`,`SchoolMask`,`SpellFamilyName`,`SpellFamilyMask0`,`SpellFamilyMask1`,`SpellFamilyMask2`,`SpellFamilyMask3`,`ProcFlags`,`ProcFlags2`,`SpellTypeMask`,`SpellPhaseMask`,`HitMask`,`AttributesMask`,`DisableEffectsMask`,`ProcsPerMinute`,`Chance`,`Cooldown`,`Charges`) VALUES
 (392988,0x00,6,0x10200400,0x00000000,0x00000020,0x00000000,0x0,0x0,0x0,0x1,0x0,0x0,0x0,0,0,0,0), -- Divine Image
-(405216,0x00,6,0x10309EC1,0x00420002,0x00508024,0x00620040,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0,0,0,0); -- Divine Image
+(405216,0x00,6,0x10309EC1,0x00020002,0x00508024,0x00620040,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0,0,0,0); -- Divine Image
 
 DELETE FROM `spell_script_names` WHERE `spell_id` IN (392988, 405216);
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
