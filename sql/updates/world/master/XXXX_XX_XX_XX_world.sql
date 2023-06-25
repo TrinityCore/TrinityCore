69d3af19e09642e2a95379a85d8a4a110d9f538c
Naddley <NaddleyTC@gmail.com>
2023-06-26 01:06:43 +0200

    Scripts/Hunter: Implement Rapid Fire

---
+++ sql/updates/world/master/XXXX_XX_XX_XX_world.sql
@@ -0,0 +1,4 @@
+-- 257044 - Rapid Fire
+DELETE FROM `spell_script_names` WHERE `spell_id` = 257044;
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(257044, 'spell_hun_rapid_fire');
