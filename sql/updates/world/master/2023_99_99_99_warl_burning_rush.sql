c809b12e23592563abf6c455fff1dcfe8c2a25da
aquadeus <efectokoleos13@gmail.com>
2023-07-02 20:34:52 +0200

    Fix typo

--- sql/updates/world/master/2023_99_99_99_warl_burning_rush.sql
+++ sql/updates/world/master/2023_99_99_99_warl_burning_rush.sql
@@ -1,3 +1,3 @@
 DELETE FROM `spell_script_names` WHERE `spell_id`=111400;
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
-(111400, 'spell_warl_legion_burning_rush');
+(111400, 'spell_warl_burning_rush');
