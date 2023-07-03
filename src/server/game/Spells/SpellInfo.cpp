3820fb0b2c635450f205ac139f14e99bb43a87f2
aquadeus <efectokoleos13@gmail.com>
2023-07-03 11:38:18 +0200

    Fix feedback

--- src/server/game/Spells/SpellInfo.cpp
+++ src/server/game/Spells/SpellInfo.cpp
@@ -4566,15 +4566,6 @@ bool _isPositiveEffectImpl(SpellInfo const* spellInfo, SpellEffectInfo const& ef
             if ((spellInfo->SpellFamilyFlags[0] & 0x20200000) != 0)
                 return false;
             break;
-        case SPELLFAMILY_WARLOCK:
-            switch (spellInfo->Id)
-            {
-                case 111400: // Burning Rush
-                    return true;
-                default:
-                    break;
-            }
-            break;
         default:
             break;
     }
