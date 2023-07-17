ea683e5f4e0fec29d34adadd2cc2e12d47e8307f
Teleqraph <nyrdeveloper@gmail.com>
2023-07-17 06:36:13 +0200

    fixed issue with Image moving

--- sql/updates/world/master/9999_99_99_99_world.sql
+++ sql/updates/world/master/9999_99_99_99_world.sql
@@ -9,3 +9,7 @@ INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (405216, 'spell_pri_divine_image_spell_triggered');
 
 UPDATE `creature_template` SET `ScriptName`='npc_pet_pri_divine_image' WHERE `entry`=198236;
+
+DELETE FROM `creature_template_movement` WHERE `CreatureId`=198236;
+INSERT INTO `creature_template_movement` (`CreatureId`, `Ground`, `Swim`, `Flight`, `Rooted`, `Chase`, `Random`, `InteractionPauseTimer`) VALUES 
+(198236, 0, 0, 0, 1, 0, 0, NULL);
