c69903f1d900f2922b682d1e317ca6380d6baa57
Nyr <nyrdeveloper@gmail.com>
2023-07-23 21:15:23 +0200

    missing header

--- src/server/scripts/Spells/spell_druid.cpp
+++ src/server/scripts/Spells/spell_druid.cpp
@@ -21,6 +21,7 @@
  * Scriptnames of files in this file should be prefixed with "spell_dru_".
  */
 
+#include "AreaTrigger.h"
 #include "ScriptMgr.h"
 #include "Containers.h"
 #include "DB2Stores.h"
