2745efeec384b8f605a30fab20677a361bdc4f48
Nyr <nyrdeveloper@gmail.com>
2023-07-24 01:21:04 +0200

    Scripts/Druid: Implement Photosynthesis and Verdancy

---
+++ sql/updates/world/master/9999_99_99_99_world.sql
@@ -0,0 +1,5 @@
+DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_dru_lifebloom', 'spell_dru_lifebloom_heal');
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(33763, 'spell_dru_lifebloom'),
+(188550, 'spell_dru_lifebloom'),
+(33778, 'spell_dru_lifebloom_heal');
