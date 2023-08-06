a550a08e3fabd4e0b04856cdf7dfa7b01d67178a
Ovahlord <dreadkiller@gmx.de>
2023-08-06 12:59:55 +0200

    2bcc645cd38697ccf9ba0700911a6ce6016956bc followup

--- sql/updates/world/4.3.4/2023_08_06_00_world.sql
+++ sql/updates/world/4.3.4/2023_08_06_00_world.sql
@@ -1,1 +1,1 @@
-DELETE FROM `spell_script_names` WHERE `ScriptName´= 'spell_anraphet_omega_stance_spider_effect';
+DELETE FROM `spell_script_names` WHERE `ScriptName`= 'spell_anraphet_omega_stance_spider_effect';
