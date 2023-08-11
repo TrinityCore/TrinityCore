959c1041c4cf9382c5648385c16a8d1171e7321d
Nyr <nyrdeveloper@gmail.com>
2023-08-12 01:32:36 +0200

    Scripts/Priest: Update Power Word: Shield

--- sql/updates/world/master/9999_99_99_99_world.sql
+++ sql/updates/world/master/9999_99_99_99_world.sql
@@ -1,3 +1,3 @@
-DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_pri_shadow_covenant');
+DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_pri_power_word_shield_aura', 'spell_pri_power_word_shield');
 INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES 
-(314867, 'spell_pri_shadow_covenant');
+(17, 'spell_pri_power_word_shield');
