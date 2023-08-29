882e20eb8a121e5cf3afdddee7024548e084acbe
aquadeus <efectokoleos13@gmail.com>
2023-08-29 22:12:52 +0200

    Core/Maps: Add assault on the dark portal map split by faction

--- src/server/game/DataStores/DB2Structure.h
+++ src/server/game/DataStores/DB2Structure.h
@@ -2738,7 +2738,7 @@ struct MapEntry
     bool IsDynamicDifficultyMap() const { return GetFlags().HasFlag(MapFlags::DynamicDifficulty); }
     bool IsFlexLocking() const { return GetFlags().HasFlag(MapFlags::FlexibleRaidLocking); }
     bool IsGarrison() const { return GetFlags().HasFlag(MapFlags::Garrison); }
-    bool IsSplitByFaction() const { return ID == 609 || ID == 2175 || ID == 2570; }
+    bool IsSplitByFaction() const { return ID == 609 || ID == 2175 || ID == 2570 || ID == 1265; }
 
     EnumFlag<MapFlags> GetFlags() const { return static_cast<MapFlags>(Flags[0]); }
     EnumFlag<MapFlags2> GetFlags2() const { return static_cast<MapFlags2>(Flags[1]); }
