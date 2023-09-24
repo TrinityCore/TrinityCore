2f7da0f3e422ba304be229434683d199271225d8
AlexIndex <alexkcodo@gmail.com>
2023-09-24 14:03:09 +0300

    Codestyling for instancescript

--- src/server/scripts/Shadowlands/SepulcherOfTheFirstOnes/instance_sepulcher_of_the_first_ones.cpp
+++ src/server/scripts/Shadowlands/SepulcherOfTheFirstOnes/instance_sepulcher_of_the_first_ones.cpp
@@ -66,11 +66,6 @@ public:
             LoadDungeonEncounterData(encounters);
             LoadObjectData(creatureData, nullptr);
             AnduinIntroductionData = NOT_STARTED;
-            
-        }
-
-        void OnPlayerEnter(Player* /*player*/) override
-        {
 
         }
 
@@ -80,37 +75,36 @@ public:
 
             switch (creature->GetEntry())
             {
-            case BOSS_ANDUIN_WRYNN:
-                AnduinWrynnGUID = creature->GetGUID();
-                break;
+                case BOSS_ANDUIN_WRYNN:
+                    AnduinWrynnGUID = creature->GetGUID();
+                    break;
 
-            case BOSS_REMNANT_OF_A_FALLEN_KING:
-                RemnantGUID = creature->GetGUID();
-                break;
+                case BOSS_REMNANT_OF_A_FALLEN_KING:
+                    RemnantGUID = creature->GetGUID();
+                    break;
 
-            case NPC_BEACON_OF_HOPE:
-                BeaconOfHopeGUID = creature->GetGUID();
-                break;
+                case NPC_BEACON_OF_HOPE:
+                    BeaconOfHopeGUID = creature->GetGUID();
+                    break;
 
-            case NPC_SYLVANAS_WINDRUNNER:
-                SylvanasGUID = creature->GetGUID();
-                break;
+                case NPC_SYLVANAS_WINDRUNNER:
+                    SylvanasGUID = creature->GetGUID();
+                    break;
 
-            case NPC_UTHER_THE_LIGHTBRINGER:
-                UtherGUID = creature->GetGUID();
-                break;
+                case NPC_UTHER_THE_LIGHTBRINGER:
+                    UtherGUID = creature->GetGUID();
+                    break;
 
-            case NPC_LADY_JAINA_PROUDMOORE:
-                JainaGUID = creature->GetGUID();
-                break;
+                case NPC_LADY_JAINA_PROUDMOORE:
+                    JainaGUID = creature->GetGUID();
+                    break;
 
-            case NPC_ANDUIN_SOUL:
-                AnduinGUID = creature->GetGUID();
-                break;
+                case NPC_ANDUIN_SOUL:
+                    AnduinGUID = creature->GetGUID();
+                    break;
 
-
-            default:
-                break;
+                default:
+                    break;
             }
         }
 
@@ -121,133 +115,106 @@ public:
 
             switch (id)
             {
-            case DATA_ANDUIN_WRYNN:
-            {
-                switch (state)
+                case DATA_ANDUIN_WRYNN:
                 {
-                case NOT_STARTED:
-                {
-                    DoUpdateWorldState(WORLD_STATE_ANDUIN_ENCOUNTER_STARTED, 0);
+                    switch (state)
+                    {
+                        case NOT_STARTED:
+                        {
+                            DoUpdateWorldState(WORLD_STATE_ANDUIN_ENCOUNTER_STARTED, 0);
+                            break;
+                        }
 
+                        case IN_PROGRESS:
+                        {
+                            Creature* anduin = GetCreature(DATA_ANDUIN_WRYNN);
+                            if (!anduin)
+                                return false;
+
+                            DoUpdateWorldState(WORLD_STATE_ANDUIN_ENCOUNTER_STARTED, 1);
+
+                            if (Creature* uther = GetCreature(DATA_UTHER_THE_LIGHTBRINGER))
+                            {
+                                uther->SetFaction(2);
+                                uther->AI()->JustEngagedWith(anduin);
+                                uther->GetThreatManager().AddThreat(anduin, 1000);
+                            }
+
+                            if (Creature* sylvanas = GetCreature(DATA_SYLVANAS_WINDRUNNER))
+                            {
+                                sylvanas->SetFaction(2);
+                                sylvanas->AI()->JustEngagedWith(anduin);
+                                sylvanas->GetThreatManager().AddThreat(anduin, 1000);
+                                sylvanas->GetAI()->AttackStartCaster(anduin, 25.0f);
+                            }
+
+                            if (Creature* jaina = GetCreature(DATA_JAINA_PROUDMOORE))
+                            {
+                                jaina->SetFaction(2);
+                                jaina->AI()->JustEngagedWith(anduin);
+                                jaina->GetThreatManager().AddThreat(anduin, 1000);
+                                jaina->GetAI()->AttackStartCaster(anduin, 25.0f);
+                            }
+                            break;
+                        }
+
+                        default:
+                            break;
+                    }
                     break;
                 }
-
-                case IN_PROGRESS:
-                {
-                    Creature* anduin = GetCreature(DATA_ANDUIN_WRYNN);
-                    if (!anduin)
-                        return false;
-
-                    DoUpdateWorldState(WORLD_STATE_ANDUIN_ENCOUNTER_STARTED, 1);
-
-                    if (Creature* uther = GetCreature(DATA_UTHER_THE_LIGHTBRINGER))
-                    {
-                        if (uther->IsAIEnabled())
-                            uther->SetFaction(2);
-                            uther->AI()->JustEngagedWith(anduin);
-                            uther->GetThreatManager().AddThreat(anduin, 1000);
-                            
-                    }
-
-                    if (Creature* sylvanas = GetCreature(DATA_SYLVANAS_WINDRUNNER))
-                    {
-
-                        if (sylvanas->IsAIEnabled())
-                            sylvanas->SetFaction(2);
-                            sylvanas->AI()->JustEngagedWith(anduin);
-                            sylvanas->GetThreatManager().AddThreat(anduin, 1000);
-                            sylvanas->GetAI()->AttackStartCaster(anduin, 25.0f);
-                    }
-
-                    if (Creature* jaina = GetCreature(DATA_JAINA_PROUDMOORE))
-                    {
-                        if (jaina->IsAIEnabled())
-                            jaina->SetFaction(2);
-                            jaina->AI()->JustEngagedWith(anduin);
-                            jaina->GetThreatManager().AddThreat(anduin, 1000);
-                            jaina->GetAI()->AttackStartCaster(anduin, 25.0f);
-
-                    }
-
-                    break;
-                }
-
-                case DONE:
-                {
-
-                    break;
-                }
-
-                case FAIL:
-                    break;
-
                 default:
                     break;
-                }
-
-                break;
             }
-
-            default:
-                break;
-            }
-
             return true;
         }
+
         void SetData(uint32 type, uint32 data) override
         {
             switch (type)
             {
-            case DATA_ANDUIN_WRYNN_INTRODUCTION:
-            {
-                switch (data)
+                case DATA_ANDUIN_WRYNN_INTRODUCTION:
                 {
-                case NOT_STARTED:
-                {
-                    AnduinIntroductionData = NOT_STARTED;
-                    if (Creature* anduin = GetCreature(DATA_ANDUIN_WRYNN))
+                    switch (data)
                     {
-                        anduin->CastSpell(anduin, SPELL_ANDUIN_PLUNGE_KINGSMOURNE);
-                        anduin->SetUnitFlag(UNIT_FLAG_NOT_ATTACKABLE_1);
-                        anduin->SetUnitFlag(UNIT_FLAG_IMMUNE_TO_PC);
-                        anduin->SetUnitFlag(UNIT_FLAG_IMMUNE_TO_NPC);
-                        anduin->SetImmuneToAll(true);
-                        anduin->SetSpeed(MOVE_WALK, 10.0f);
-                    }
-                    break;
-                }
-                case IN_PROGRESS:
-                {
-                    AnduinIntroductionData = IN_PROGRESS;
-                    break;
-                }
+                        case NOT_STARTED:
+                        {
+                            AnduinIntroductionData = NOT_STARTED;
+                            if (Creature* anduin = GetCreature(DATA_ANDUIN_WRYNN))
+                            {
+                                anduin->CastSpell(anduin, SPELL_ANDUIN_PLUNGE_KINGSMOURNE);
+                                anduin->SetUnitFlag(UNIT_FLAG_NOT_ATTACKABLE_1);
+                                anduin->SetUnitFlag(UNIT_FLAG_IMMUNE_TO_PC);
+                                anduin->SetUnitFlag(UNIT_FLAG_IMMUNE_TO_NPC);
+                                anduin->SetImmuneToAll(true);
+                                anduin->SetSpeed(MOVE_WALK, 10.0f);
+                            }
+                            break;
+                        }
+                        case IN_PROGRESS:
+                        {
+                            AnduinIntroductionData = IN_PROGRESS;
+                            break;
+                        }
 
-                case DONE:
-                {
-                    AnduinIntroductionData = DONE;
-                    if (Creature* anduin = GetCreature(DATA_ANDUIN_WRYNN))
-                    {
-                        anduin->RemoveUnitFlag(UNIT_FLAG_NOT_ATTACKABLE_1);
-                        anduin->RemoveUnitFlag(UNIT_FLAG_IMMUNE_TO_PC);
-                        anduin->RemoveUnitFlag(UNIT_FLAG_IMMUNE_TO_NPC);
-                        anduin->SetImmuneToAll(false);
-                        anduin->SetSpeed(MOVE_RUN, 11.0f);
-                        anduin->RemoveAurasDueToSpell(SPELL_ANDUIN_PLUNGE_KINGSMOURNE);
+                        case DONE:
+                        {
+                            AnduinIntroductionData = DONE;
+                            if (Creature* anduin = GetCreature(DATA_ANDUIN_WRYNN))
+                            {
+                                anduin->RemoveUnitFlag(UNIT_FLAG_NOT_ATTACKABLE_1);
+                                anduin->RemoveUnitFlag(UNIT_FLAG_IMMUNE_TO_PC);
+                                anduin->RemoveUnitFlag(UNIT_FLAG_IMMUNE_TO_NPC);
+                                anduin->SetImmuneToAll(false);
+                                anduin->SetSpeed(MOVE_RUN, 11.0f);
+                                anduin->RemoveAurasDueToSpell(SPELL_ANDUIN_PLUNGE_KINGSMOURNE);
+                            }
+                            break;
+                        }
+                        default:
+                            break;
                     }
-                    break;
                 }
-                default:
-                    break;
-                }
-                break;
-            }
-
-            case DATA_ANDUIN_WRYNN_FINAL_MOVIE:
-                AnduinFinalData = 1;
-                if (AnduinFinalData == 1)
-                    if (Creature* anduin = GetCreature(DATA_ANDUIN_WRYNN))
-                        if (anduin->IsAIEnabled())
-                            anduin->GetAI()->DoAction(ACTION_START_ANDUIN_OUTRODUCTION);
                 break;
             default:
                 break;
@@ -258,58 +225,41 @@ public:
         {
             switch (type)
             {
-            case DATA_ANDUIN_WRYNN_INTRODUCTION:
-                return AnduinIntroductionData;
-            case DATA_ANDUIN_WRYNN_FINAL_MOVIE:
-                return AnduinFinalData;
-            default:
-                break;
+                case DATA_ANDUIN_WRYNN_INTRODUCTION:
+                    return AnduinIntroductionData;
+                case DATA_ANDUIN_WRYNN_FINAL_MOVIE:
+                    return AnduinFinalData;
+                default:
+                    break;
             }
 
             return 0;
         }
 
-        void StartOutroduction()
-        {
-            if (Creature* anduin = GetCreature(DATA_ANDUIN_WRYNN))
-            {
-                anduin->GetAI()->DoAction(ACTION_START_ANDUIN_OUTRODUCTION);
-            }
-        }
         void OnGameObjectCreate(GameObject* go) override
         {
             InstanceScript::OnGameObjectCreate(go);
 
             switch (go->GetEntry())
             {
-            case GAMEOBJECT_BRIDGE_TO_ANDUIN:
-                BridgeToAnduinGUID = go->GetGUID();
-                break;
+                case GAMEOBJECT_BRIDGE_TO_ANDUIN:
+                    BridgeToAnduinGUID = go->GetGUID();
+                    break;
 
-            case GAMEOBJECT_BRIDGE_AFTER_ANDUIN:
-            {
-                BridgeAfterAnduinGUID = go->GetGUID();
-                break;
-            }
+                case GAMEOBJECT_BRIDGE_AFTER_ANDUIN:
+                    BridgeAfterAnduinGUID = go->GetGUID();
+                    break;
 
-                break;
-            default:
-                break;
+                default:
+                    break;
             }
         }
 
         void UpdateDoorState(GameObject* door) override
         {
-            // Leviathan doors are set to DOOR_TYPE_ROOM except the one it uses to enter the room
-            // which has to be set to DOOR_TYPE_PASSAGE
             if (door->GetEntry() == GAMEOBJECT_BRIDGE_AFTER_ANDUIN)
-            {
                 if (GetBossState(DATA_ANDUIN_WRYNN) == DONE)
-                {
                     door->UseDoorOrButton();
-                }
-            }
-                
             else
                 InstanceScript::UpdateDoorState(door);
         }
@@ -318,39 +268,37 @@ public:
         {
             switch (type)
             {
+                case DATA_ANDUIN_WRYNN:
+                    return AnduinWrynnGUID;
 
-            case DATA_ANDUIN_WRYNN:
-                return AnduinWrynnGUID;
+                case DATA_BEFOULED_BARRIER:
+                    return BefouledBarrierGUID;
 
-            case DATA_BEFOULED_BARRIER:
-                return BefouledBarrierGUID;
+                case DATA_EMPTY_VESSEL:
+                    return EmptyVesselGUID;
 
-            case DATA_EMPTY_VESSEL:
-                return EmptyVesselGUID;
+                case DATA_GRIM_REFLECTION:
+                    return GrimReflectionGUID;
 
-            case DATA_GRIM_REFLECTION:
-                return GrimReflectionGUID;
+                case DATA_REMNANT_OF_A_FALLEN_KING:
+                    return RemnantGUID;
 
-            case DATA_REMNANT_OF_A_FALLEN_KING:
-                return RemnantGUID;
+                case DATA_BEACON_OF_HOPE:
+                    return BeaconOfHopeGUID;
+                case DATA_UTHER_THE_LIGHTBRINGER:
+                    return UtherGUID;
 
-            case DATA_BEACON_OF_HOPE:
-                return BeaconOfHopeGUID;
-            case DATA_UTHER_THE_LIGHTBRINGER:
-                return UtherGUID;
+                case DATA_JAINA_PROUDMOORE:
+                    return JainaGUID;
 
-            case DATA_JAINA_PROUDMOORE:
-                return JainaGUID;
+                case DATA_SYLVANAS_WINDRUNNER:
+                    return SylvanasGUID;
 
-            case DATA_SYLVANAS_WINDRUNNER:
-                return SylvanasGUID;
+                case DATA_FIRIM:
+                    return FirimGUID;
 
-            case DATA_FIRIM:
-                return FirimGUID;
-
-
-            default:
-                break;
+                default:
+                    break;
             }
 
             return InstanceScript::GetGuidData(type);
@@ -376,7 +324,6 @@ public:
         uint8 AnduinGObjectData;
         uint8 AnduinIntroductionData;
         uint8 AnduinFinalData;
-
     };
 
     InstanceScript* GetInstanceScript(InstanceMap* map) const override
