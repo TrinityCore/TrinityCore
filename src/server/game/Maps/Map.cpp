a3702cc526cd35d6e0162607d509c241bdefdc63
Shauren <shauren.trinity@gmail.com>
2023-10-18 20:21:32 +0200

    Core/Misc: Remove incorrect (and unused) equality operator overloads from LinkedListHead::Iterator class

--- src/server/game/Maps/Map.cpp
+++ src/server/game/Maps/Map.cpp
@@ -3417,7 +3417,7 @@ AreaTrigger* Map::GetAreaTriggerBySpawnId(ObjectGuid::LowType spawnId) const
 
 void Map::UpdateIteratorBack(Player* player)
 {
-    if (m_mapRefIter == player->GetMapRef())
+    if (&*m_mapRefIter == &player->GetMapRef())
         m_mapRefIter = m_mapRefIter->nocheck_prev();
 }
 
