973b5f404f5b7115239a81030ad2f8dff8e165ea
Shauren <shauren.trinity@gmail.com>
2023-10-23 18:15:01 +0200

    Core/DataStores: Don't send hotfix ids in SMSG_AVAILABLE_HOTFIXES that don't have a hotfix blob for client locale
    Closes #29378

--- src/server/game/DataStores/DB2Stores.h
+++ src/server/game/DataStores/DB2Stores.h
@@ -370,6 +370,8 @@ public:
         HotfixId ID;
         Status HotfixStatus = Status::Invalid;
 
+        uint32 AvailableLocalesMask = 0;
+
         friend std::strong_ordering operator<=>(HotfixRecord const& left, HotfixRecord const& right)
         {
             if (std::strong_ordering cmp = left.ID <=> right.ID; advstd::is_neq(cmp))
@@ -388,7 +390,13 @@ public:
         std::vector<uint8> Data;
     };
 
-    using HotfixContainer = std::map<int32, std::vector<HotfixRecord>>;
+    struct HotfixPush
+    {
+        std::vector<HotfixRecord> Records;
+        uint32 AvailableLocalesMask = 0;
+    };
+
+    using HotfixContainer = std::map<int32, HotfixPush>;
 
     using FriendshipRepReactionSet = std::set<FriendshipRepReactionEntry const*, FriendshipRepReactionEntryComparator>;
     using MapDifficultyConditionsContainer = std::vector<std::pair<uint32, PlayerConditionEntry const*>>;
@@ -400,7 +408,7 @@ public:
     uint32 LoadStores(std::string const& dataPath, LocaleConstant defaultLocale);
     DB2StorageBase const* GetStorage(uint32 type) const;
 
-    void LoadHotfixData();
+    void LoadHotfixData(uint32 localeMask);
     void LoadHotfixBlob(uint32 localeMask);
     void LoadHotfixOptionalData(uint32 localeMask);
     uint32 GetHotfixCount() const;
