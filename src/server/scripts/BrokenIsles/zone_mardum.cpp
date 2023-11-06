d8805cb2e052099f123ee956bfadbdcafa2861b1
Naddley <NaddleyTC@gmail.com>
2023-11-06 18:51:06 +0100

    fix nopch

--- src/server/scripts/BrokenIsles/zone_mardum.cpp
+++ src/server/scripts/BrokenIsles/zone_mardum.cpp
@@ -30,6 +30,7 @@
 #include "Player.h"
 #include "ScriptMgr.h"
 #include "ScriptedCreature.h"
+#include "ScriptedGossip.h"
 #include "SpellAuras.h"
 #include "SpellScript.h"
 #include "TemporarySummon.h"
