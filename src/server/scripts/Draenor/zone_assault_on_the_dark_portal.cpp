5716fe9db1909d969a6a88ff1bdca6bd17059ec1
aquadeus <efectokoleos13@gmail.com>
2023-11-13 17:28:53 +0100

    Scripts/Draenor: Implement quest 34423 (altar altercation)

--- src/server/scripts/Draenor/zone_assault_on_the_dark_portal.cpp
+++ src/server/scripts/Draenor/zone_assault_on_the_dark_portal.cpp
@@ -20,17 +20,22 @@
 #include "QuestDef.h"
 #include "ScriptMgr.h"
 #include "ScriptedCreature.h"
+#include "ScriptedFollowerAI.h"
 #include "SpellScript.h"
+#include "TemporarySummon.h"
 
 enum AssaultOnTheDarkPortalSpells
 {
+    SPELL_ALTAR_ALTERCATION_CANCEL_AURAS = 167547, // Serverside Spell
     SPELL_BLEEDING_HOLLOW_HOLDOUT        = 164609,
     SPELL_BLEEDING_HOLLOW_TRAIL_OF_FLAME = 164611,
+    SPELL_CANCEL_PHASE_AURA              = 165053,
     SPELL_CANCEL_TRAIL_OF_FLAME_VISUAL   = 165993,
     SPELL_DARK_PORTAL_RUN_AWAY           = 158985,
     SPELL_HUT_CREDIT                     = 164613, // Serverside Spell
     SPELL_PUSH_ARMY                      = 165072,
-    SPELL_TRAIL_OF_FLAME_LARGE           = 165991
+    SPELL_TRAIL_OF_FLAME_LARGE           = 165991,
+    SPELL_UPDATE_PHASE_SHIFT_PLAYER      = 82238
 };
 
 enum AssaultOnTheDarkPortalQuests
@@ -131,6 +136,188 @@ class scene_bleeding_hollow_trail_of_flame : public SceneScript
     }
 };
 
+// 34423 - Altar Altercation
+class quest_altar_altercation : public QuestScript
+{
+public:
+    quest_altar_altercation() : QuestScript("quest_altar_altercation") { }
+
+    void OnQuestStatusChange(Player* player, Quest const* /*quest*/, QuestStatus /*oldStatus*/, QuestStatus newStatus) override
+    {
+        if (newStatus == QUEST_STATUS_NONE)
+            player->CastSpell(player, SPELL_ALTAR_ALTERCATION_CANCEL_AURAS, TRIGGERED_FULL_MASK);
+    }
+};
+
+// 161618 - Summon Ariok
+class spell_summon_ariok : public SpellScript
+{
+    bool Validate(SpellInfo const* /*spellInfo*/) override
+    {
+        return ValidateSpellInfo({ SPELL_UPDATE_PHASE_SHIFT_PLAYER });
+    }
+
+    void HandleOnCast()
+    {
+        GetCaster()->CastSpell(GetCaster(), SPELL_UPDATE_PHASE_SHIFT_PLAYER, TRIGGERED_FULL_MASK);
+    }
+
+    void Register() override
+    {
+        BeforeCast += SpellCastFn(spell_summon_ariok::HandleOnCast);
+    }
+};
+
+// 694 - Bleeding Hollow: Kilrogg Reveal
+class scene_bleeding_hollow_kilrogg_reveal : public SceneScript
+{
+public:
+    scene_bleeding_hollow_kilrogg_reveal() : SceneScript("scene_bleeding_hollow_kilrogg_reveal") { }
+
+    void OnSceneCancel(Player* player, uint32 /*sceneInstanceID*/, SceneTemplate const* /*sceneTemplate*/) override
+    {
+        player->CastSpell(player, SPELL_CANCEL_PHASE_AURA, false);
+    }
+
+    void OnSceneTriggerEvent(Player* player, uint32 /*sceneInstanceID*/, SceneTemplate const* /*sceneTemplate*/, std::string const& triggerName) override
+    {
+        if (triggerName == "FreeCamera")
+            player->CastSpell(player, SPELL_CANCEL_PHASE_AURA, false);
+    }
+};
+
+// 167955 - Destroying
+class spell_destroying : public SpellScript
+{
+    void HandleOnHit(SpellEffIndex /*effIndex*/)
+    {
+        GetHitUnit()->ToCreature()->DespawnOrUnsummon(); // @TODO: Spawntracking should cause despawn for casting player only and respawn on abandon quest
+    }
+
+    void Register() override
+    {
+        OnEffectHitTarget += SpellEffectFn(spell_destroying::HandleOnHit, EFFECT_1, SPELL_EFFECT_DUMMY);
+    }
+};
+
+// 80087 - Ariok
+enum Texts
+{
+    SAY_INTRO_1  = 0,
+    SAY_INTRO_2  = 1,
+    SAY_INTRO_3  = 2,
+    SAY_ALTAR_1  = 3,
+    SAY_ALTAR_2  = 4,
+    SAY_ALTAR_3  = 5,
+    SAY_ALTAR_4  = 6
+};
+
+enum Actions
+{
+    ACTION_INTRO = 1,
+    ACTION_ALTAR = 2
+};
+
+enum Events
+{
+    EVENT_TALK_1 = 1,
+    EVENT_TALK_2,
+    EVENT_TALK_3,
+    EVENT_ALTAR
+};
+
+struct npc_ariok : public FollowerAI
+{
+    npc_ariok(Creature* creature) : FollowerAI(creature) { }
+
+    void JustAppeared() override
+    {
+        DoAction(ACTION_INTRO);
+
+        if (TempSummon const* tempSummon = me->ToTempSummon())
+        {
+            if (Player* summoner = tempSummon->GetSummoner()->ToPlayer())
+            {
+                if (!summoner)
+                    return;
+
+                StartFollow(summoner);
+            }
+        }
+    }
+
+    void DoAction(int32 action) override
+    {
+        switch (action)
+        {
+            case ACTION_INTRO:
+            {
+                events.ScheduleEvent(EVENT_TALK_1, 1s);
+                break;
+            }
+            case ACTION_ALTAR:
+            {
+                Talk(SAY_ALTAR_1);
+                me->AddUnitState(UNIT_STATE_ROOT);
+                events.ScheduleEvent(EVENT_ALTAR, 5s + 400ms);
+                break;
+            }
+            default:
+                break;
+        }
+    }
+
+    void UpdateAI(uint32 diff) override
+    {
+        events.Update(diff);
+
+        while (uint32 eventId = events.ExecuteEvent())
+        {
+            switch (eventId)
+            {
+                case EVENT_TALK_1:
+                    Talk(SAY_INTRO_1);
+                    events.ScheduleEvent(EVENT_TALK_2, 5s + 700ms);
+                    break;
+                case EVENT_TALK_2:
+                    Talk(SAY_INTRO_2);
+                    events.ScheduleEvent(EVENT_TALK_3, 7s + 400ms);
+                    break;
+                case EVENT_TALK_3:
+                    Talk(SAY_INTRO_3);
+                    break;
+                case EVENT_ALTAR:
+                    me->ClearUnitState(UNIT_STATE_ROOT);
+                    Talk(SAY_ALTAR_2);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+private:
+    EventMap events;
+};
+
+// 164979 - (Serverside/Non-DB2) Trigger Ariok 
+class spell_trigger_ariok : public SpellScript
+{
+    void HandleOnHit(SpellEffIndex /*effIndex*/)
+    {
+        Creature* creature = GetHitUnit()->ToCreature();
+
+        if (!creature)
+            return;
+
+        creature->AI()->DoAction(ACTION_ALTAR);
+    }
+
+    void Register() override
+    {
+        OnEffectHitTarget += SpellEffectFn(spell_trigger_ariok::HandleOnHit, EFFECT_0, SPELL_EFFECT_DUMMY);
+    }
+};
+
 void AddSC_assault_on_the_dark_portal()
 {
     new scene_dark_portal_run_away();
@@ -138,4 +325,10 @@ void AddSC_assault_on_the_dark_portal()
     new quest_blaze_of_glory();
     new scene_bleeding_hollow_holdout();
     new scene_bleeding_hollow_trail_of_flame();
+    new quest_altar_altercation();
+    RegisterSpellScript(spell_summon_ariok);
+    new scene_bleeding_hollow_kilrogg_reveal();
+    RegisterSpellScript(spell_destroying);
+    RegisterCreatureAI(npc_ariok);
+    RegisterSpellScript(spell_trigger_ariok);
 };
