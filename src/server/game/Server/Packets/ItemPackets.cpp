da74cfeead1791194b140a61104932ef3ffba18c
Ovahlord <dreadkiller@gmx.de>
2023-11-16 20:50:54 +0100

    Core/Packets: fixed SMSG_ITEM_PUSH_RESULT packet structure

--- src/server/game/Server/Packets/ItemPackets.cpp
+++ src/server/game/Server/Packets/ItemPackets.cpp
@@ -260,27 +260,15 @@ WorldPacket const* WorldPackets::Item::ItemPushResult::Write()
     _worldPacket << uint32(BattlePetBreedQuality);
     _worldPacket << int32(BattlePetLevel);
     _worldPacket << ItemGUID;
-    _worldPacket << uint32(Toasts.size());
-    for (UiEventToast const& uiEventToast : Toasts)
-        _worldPacket << uiEventToast;
 
     _worldPacket.WriteBit(Pushed);
     _worldPacket.WriteBit(Created);
-    _worldPacket.WriteBit(Unused_1017);
     _worldPacket.WriteBits(DisplayText, 3);
     _worldPacket.WriteBit(IsBonusRoll);
     _worldPacket.WriteBit(IsEncounterLoot);
-    _worldPacket.WriteBit(CraftingData.has_value());
-    _worldPacket.WriteBit(FirstCraftOperationID.has_value());
     _worldPacket.FlushBits();
 
     _worldPacket << Item;
-
-    if (FirstCraftOperationID)
-        _worldPacket << uint32(*FirstCraftOperationID);
-
-    if (CraftingData)
-        _worldPacket << *CraftingData;
 
     return &_worldPacket;
 }
