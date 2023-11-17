9493a14b9f020a0ab409906c2d85994780694ab3
Ovahlord <dreadkiller@gmx.de>
2023-11-17 18:58:48 +0100

    Scripts/Misc: removed scripts of newer expansion's continents and dropped spell script files for unsupported classes

--- src/server/scripts/Spells/spell_script_loader.cpp
+++ src/server/scripts/Spells/spell_script_loader.cpp
@@ -17,13 +17,10 @@
 
 // This is where scripts' loading functions should be declared:
 void AddSC_deathknight_spell_scripts();
-void AddSC_demon_hunter_spell_scripts();
 void AddSC_druid_spell_scripts();
-void AddSC_evoker_spell_scripts();
 void AddSC_generic_spell_scripts();
 void AddSC_hunter_spell_scripts();
 void AddSC_mage_spell_scripts();
-void AddSC_monk_spell_scripts();
 void AddSC_paladin_spell_scripts();
 void AddSC_priest_spell_scripts();
 void AddSC_rogue_spell_scripts();
@@ -32,20 +29,16 @@ void AddSC_warlock_spell_scripts();
 void AddSC_warrior_spell_scripts();
 void AddSC_quest_spell_scripts();
 void AddSC_item_spell_scripts();
-void AddSC_azerite_item_spell_scripts();
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
 void AddSpellsScripts()
 {
     AddSC_deathknight_spell_scripts();
-    AddSC_demon_hunter_spell_scripts();
     AddSC_druid_spell_scripts();
-    AddSC_evoker_spell_scripts();
     AddSC_generic_spell_scripts();
     AddSC_hunter_spell_scripts();
     AddSC_mage_spell_scripts();
-    AddSC_monk_spell_scripts();
     AddSC_paladin_spell_scripts();
     AddSC_priest_spell_scripts();
     AddSC_rogue_spell_scripts();
@@ -54,5 +47,4 @@ void AddSpellsScripts()
     AddSC_warrior_spell_scripts();
     AddSC_quest_spell_scripts();
     AddSC_item_spell_scripts();
-    AddSC_azerite_item_spell_scripts();
 }
