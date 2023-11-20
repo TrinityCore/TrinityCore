352d8b5862f5a31ce65f08026eed414b5be12145
Ovahlord <dreadkiller@gmx.de>
2023-11-20 02:24:11 +0100

    DB: fixed a typo in 2023_11_19_00_characters.sql

--- sql/updates/characters/3.4.x/2023_11_19_00_characters.sql
+++ sql/updates/characters/3.4.x/2023_11_19_00_characters.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `characters_10x`.`characters`   
+ALTER TABLE `characters`   
 	DROP COLUMN `activeTalentGroup`, 
 	CHANGE `primarySpecialization` `activeTalentGroup` TINYINT UNSIGNED DEFAULT 0 NOT NULL,
 	ADD COLUMN `bonusTalentGroups` TINYINT UNSIGNED DEFAULT 0 NOT NULL AFTER `activeTalentGroup`;
