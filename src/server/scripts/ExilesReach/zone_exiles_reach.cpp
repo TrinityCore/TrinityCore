7fde03484580b5afed6827ca4a5e3909ea5d2734
Malcrom <malcromdev@gmail.com>
2023-12-11 21:31:45 -0400

    fuck nopch and its shit error messages

--- src/server/scripts/ExilesReach/zone_exiles_reach.cpp
+++ src/server/scripts/ExilesReach/zone_exiles_reach.cpp
@@ -15,6 +15,7 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "AreaTrigger.h"
 #include "AreaTriggerAI.h"
 #include "Conversation.h"
 #include "CreatureAIImpl.h"
