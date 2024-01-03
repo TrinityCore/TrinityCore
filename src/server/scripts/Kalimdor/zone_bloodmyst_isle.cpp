4fb7ceb05756c80998e14ec67b790e284524f091
Legoso <legoso_demo@outlook.com>
2024-01-03 20:52:59 +0100

    Scripts/Bloodmyst isle: add missing Legoso Talk to $n pointer
    **Changes proposed:**
    
    -  Legoso talk text now correctly references the player name,
        instead of showing "$n" in the text (4 talk lines updated).
    -  Reduce number of lines of `(Player* player = GetPlayerForEscort())`
        from 3 to 1 within `UpdateAI ()`.
    
    **Issues addressed:**
    
    Updates #21586
    
    **Tests performed:**
    
    - [x] Built multiple times without any observed issues.
    - [x] Tested in-game, both in this form and non-simplified.
    
    **Known issues and TODO list:** (add/remove lines as needed)
    
    Not an issue or TODO as such, but I am uncertain whether this solution
    will be acceptable in its current form, or if TC prefers to have
    one `if (Player* player = GetPlayerForEscort())` for each `Talk()`.
    I don't know whether the added `if (!player) return;` creates new issues.

--- src/server/scripts/Kalimdor/zone_bloodmyst_isle.cpp
+++ src/server/scripts/Kalimdor/zone_bloodmyst_isle.cpp
@@ -357,6 +357,10 @@ public:
         {
             _events.Update(diff);
 
+            Player* player = GetPlayerForEscort();
+            if (!player)
+                return;
+
             if (UpdateVictim())
             {
                 while (uint32 eventId = _events.ExecuteEvent())
@@ -383,9 +387,8 @@ public:
                             Unit* target = nullptr;
                             if (me->GetHealthPct() < 85)
                                 target = me;
-                            else if (Player* player = GetPlayerForEscort())
-                                if (player->GetHealthPct() < 85)
-                                    target = player;
+                            else if (player->GetHealthPct() < 85)
+                                target = player;
                             if (target)
                             {
                                 DoCast(target, SPELL_HEALING_SURGE);
@@ -441,7 +444,7 @@ public:
                             _phase = PHASE_PLANT_FIRST_WORK;
                             break;
                         case PHASE_PLANT_FIRST_WORK: // plant first explosives stage 2 work
-                            Talk(SAY_LEGOSO_4);
+                            Talk(SAY_LEGOSO_4, player);
                             _moveTimer = 17.5 * AsUnderlyingType(IN_MILLISECONDS);
                             _phase = PHASE_PLANT_FIRST_FINISH;
                             break;
@@ -506,7 +509,7 @@ public:
                             _phase = PHASE_FEEL_SIRONAS_2;
                             break;
                         case PHASE_FEEL_SIRONAS_2: // legoso exclamation before sironas 1.2
-                            Talk(SAY_LEGOSO_11);
+                            Talk(SAY_LEGOSO_11, player);
                             _moveTimer = 4 * IN_MILLISECONDS;
                             _phase = PHASE_CONTINUE;
                             break;
@@ -516,8 +519,7 @@ public:
                             _phase = PHASE_MEET_SIRONAS_TURN;
                             break;
                         case PHASE_MEET_SIRONAS_TURN: // legoso exclamation before sironas 2.2
-                            if (Player* player = GetPlayerForEscort())
-                                me->SetFacingToObject(player);
+                            me->SetFacingToObject(player);
                             _moveTimer = 1 * IN_MILLISECONDS;
                             _phase = PHASE_MEET_SIRONAS_SPEECH;
                             break;
@@ -548,7 +550,7 @@ public:
                                 if (GameObject* explosive = me->SummonGameObject(GO_DRAENEI_EXPLOSIVES_2, ExplosivesPos[1][i], QuaternionData(), 0s))
                                     _explosivesGuids.push_back(explosive->GetGUID());
                             }
-                            Talk(SAY_LEGOSO_15);
+                            Talk(SAY_LEGOSO_15, player);
                             _moveTimer = 1 * IN_MILLISECONDS;
                             _phase = PHASE_PLANT_SECOND_WAIT;
                             break;
@@ -630,8 +632,7 @@ public:
                             _phase = PHASE_SIRONAS_SLAIN_EMOTE_2;
                             break;
                         case PHASE_SIRONAS_SLAIN_EMOTE_2: // legoso exclamation after battle - stage 1.3
-                            if (Player* player = GetPlayerForEscort())
-                                player->GroupEventHappens(QUEST_ENDING_THEIR_WORLD, me);
+                            player->GroupEventHappens(QUEST_ENDING_THEIR_WORLD, me);
                             me->HandleEmoteCommand(EMOTE_ONESHOT_CHEER);
                             _moveTimer = 5 * IN_MILLISECONDS;
                             _phase = PHASE_SIRONAS_SLAIN_SPEECH_2;
@@ -661,7 +662,7 @@ public:
                 case WP_START:
                     SetEscortPaused(true);
                     me->SetFacingToObject(player);
-                    Talk(SAY_LEGOSO_1);
+                    Talk(SAY_LEGOSO_1, player);
                     _moveTimer = 2.5 * AsUnderlyingType(IN_MILLISECONDS);
                     _phase = PHASE_CONTINUE;
                     break;
