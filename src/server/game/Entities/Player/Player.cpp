9504640b340e2e64384a7bea1549f1a5c89fd957
Shauren <shauren.trinity@gmail.com>
2024-01-05 00:18:47 +0100

    Core/Players: Fixed objects not being visible after seamless teleports
    (cherry picked from commit 390f0be9fb22766638006f43e4d0887108ba49e8)

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -1518,6 +1518,9 @@ bool Player::TeleportTo(uint32 mapid, float x, float y, float z, float orientati
             }
 
             SendDirectMessage(transferPending.Write());
+
+            RemovePlayerLocalFlag(PLAYER_LOCAL_FLAG_OVERRIDE_TRANSPORT_SERVER_TIME);
+            SetTransportServerTime(0);
         }
 
         // remove from old map now
@@ -1654,9 +1657,6 @@ void Player::RemoveFromWorld()
             GetName(), GetGUID().ToString(), viewpoint->GetEntry(), viewpoint->GetTypeId());
         SetViewpoint(viewpoint, false);
     }
-
-    RemovePlayerLocalFlag(PLAYER_LOCAL_FLAG_OVERRIDE_TRANSPORT_SERVER_TIME);
-    SetTransportServerTime(0);
 }
 
 void Player::SetObjectScale(float scale)
