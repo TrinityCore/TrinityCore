02a2bfec17ff72859579995d49f0952f87d7d84f
r4dish <ovitnez@gmail.com>
2024-01-06 12:51:24 +0200

    Scripts/Commands: Implemented .bg start and .bg stop commands.

--- src/server/game/Accounts/RBAC.h
+++ src/server/game/Accounts/RBAC.h
@@ -746,6 +746,8 @@ enum RBACPermissions
     // 878-879 previously used, do not reuse
     RBAC_PERM_COMMAND_PDUMP_COPY                             = 880,
     RBAC_PERM_COMMAND_RELOAD_VEHICLE_TEMPLATE                = 881,
+    RBAC_PERM_COMMAND_BG_START                               = 884,
+    RBAC_PERM_COMMAND_BG_STOP                                = 885,
     //
     // IF YOU ADD NEW PERMISSIONS, ADD THEM IN MASTER BRANCH AS WELL!
     //
