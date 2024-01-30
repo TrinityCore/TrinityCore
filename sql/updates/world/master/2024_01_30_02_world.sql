71a852ce82b64d919079280bead574427a328604
Cristian Vintila <127750549+Mematoru23@users.noreply.github.com>
2024-01-30 16:01:32 +0200

    Update 2024_01_30_02_world.sql

--- sql/updates/world/master/2024_01_30_02_world.sql
+++ sql/updates/world/master/2024_01_30_02_world.sql
@@ -1,3 +1,4 @@
 DELETE FROM `spell_script_names` WHERE `spell_id` IN (8092);
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (8092, 'spell_pri_dark_indulgence');
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (8092, 'spell_pri_schism');
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(8092, 'spell_pri_dark_indulgence'),
+(8092, 'spell_pri_schism');
