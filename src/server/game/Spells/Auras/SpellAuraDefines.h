a629fe5cdaba1217976f0be060b254ec88ccad9b
Ovahlord <dreadkiller@gmx.de>
2024-01-30 14:59:20 +0100

    Core/Auras: updated spell aura flags and implemented AFLAG_CANCELABLE and AFLAG_PASSIVE (#29634)

--- src/server/game/Spells/Auras/SpellAuraDefines.h
+++ src/server/game/Spells/Auras/SpellAuraDefines.h
@@ -32,14 +32,15 @@ enum AURA_FLAGS
 {
     AFLAG_NONE                   = 0x0000,
     AFLAG_NOCASTER               = 0x0001,
-    AFLAG_POSITIVE               = 0x0002,
+    AFLAG_CANCELABLE             = 0x0002,
     AFLAG_DURATION               = 0x0004,
     AFLAG_SCALABLE               = 0x0008,
     AFLAG_NEGATIVE               = 0x0010,
     AFLAG_UNK20                  = 0x0020,
     AFLAG_UNK40                  = 0x0040,
     AFLAG_UNK80                  = 0x0080,
-    AFLAG_MAW_POWER              = 0x0100,
+    AFLAG_POSITIVE               = 0x0100,
+    AFLAG_PASSIVE                = 0x0200
 };
 
 // these are modes, in which aura effect handler may be called
