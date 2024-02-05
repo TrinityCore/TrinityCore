756f9dffca9a43d9912874df9137087b6db62ab7
Malcrom <malcromdev@gmail.com>
2024-02-05 17:32:25 -0400

    Update zone_exiles_reach.cpp

--- src/server/scripts/ExilesReach/zone_exiles_reach.cpp
+++ src/server/scripts/ExilesReach/zone_exiles_reach.cpp
@@ -6539,7 +6539,7 @@ struct npc_henry_q55879 : public ScriptedAI
 
 CreatureAI* HenryGarrickSelector(Creature* creature)
 {
-    if (Player* player = ObjectAccessor::GetPlayer(*creature, creature->GetOwnerGUID()))
+    if (ObjectAccessor::GetPlayer(*creature, creature->GetOwnerGUID()))
         return new npc_henry_q55879(creature);
 
     return new NullCreatureAI(creature);
