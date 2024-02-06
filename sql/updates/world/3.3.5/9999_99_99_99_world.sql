f6d9d2963ef64adf7b5809ed0c788a21eafeaf6b
bibi <125542784+14shagov@users.noreply.github.com>
2024-02-06 14:04:33 +0300

    Update 9999_99_99_99_world.sql

--- sql/updates/world/3.3.5/9999_99_99_99_world.sql
+++ sql/updates/world/3.3.5/9999_99_99_99_world.sql
@@ -1,4 +1,5 @@
-SET @GUID := NULL;
-DELETE FROM `gameobject` WHERE guid = @GUID;
-INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneid`, `areaid`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES
-(@GUID,149431,109,0,0,1,1,-518.154,-85.2353,-74.488,3.14159,0,0,0,0,43200,100,1);
+SET @GUID := 11011; -- (unused GUID from TDB 335.24011)
+
+DELETE FROM `gameobject` WHERE `guid` = @GUID;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `VerifiedBuild`) VALUES 
+(@GUID, 149431, 109, 0, 0, 1, 1, -518.154, -85.2353, -74.488, 3.14159, 0, 0, 0, 0, 43200, 100, 1, '', 0);
