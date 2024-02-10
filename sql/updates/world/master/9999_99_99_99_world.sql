1a66e8f1fea452448801c33472071f445268c3f8
Malcrom <malcromdev@gmail.com>
2024-02-10 15:33:11 -0400

    Update 9999_99_99_99_world.sql

--- sql/updates/world/master/9999_99_99_99_world.sql
+++ sql/updates/world/master/9999_99_99_99_world.sql
@@ -249,8 +249,8 @@ SET @NPC := 150245;
 UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@NPC;
 DELETE FROM `smart_scripts` WHERE `entryorguid` IN (@NPC) AND `source_type`=0;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`event_param5`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_param4`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
-(@NPC,0,0,1,1,0,100,0,8000,10000,8000,10000,0,66,0,0,0,0,0,0,19,156808,10,0,0,0,0,0,0,'Face Small Boar'),
-(@NPC,0,1,0,61,0,100,0,0,0,0,0,0,5,4,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'Emote OneShotCheer');
+(@NPC,0,0,1,1,0,100,0,8000,10000,8000,10000,0,66,0,0,0,0,0,0,19,156808,10,0,0,0,0,0,0,'OOC Face Small Boar'),
+(@NPC,0,1,0,61,0,100,0,0,0,0,0,0,5,4,0,0,0,0,0,1,0,0,0,0,0,0,0,0,'OOC Emote OneShotCheer');
 
 -- Captain Garrick SAI 'Ogre Ruins'
 SET @NPC := 156807;
