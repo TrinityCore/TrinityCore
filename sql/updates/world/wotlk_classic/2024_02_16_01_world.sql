26ba9ebf038e230c0725e7bff4c31e493b61c0b2
Ovahlord <dreadkiller@gmx.de>
2024-02-16 16:00:42 +0100

    DB/Transports: backported transports from 3.3.5 branch

---
+++ sql/updates/world/wotlk_classic/2024_02_16_01_world.sql
@@ -0,0 +1,22 @@
+TRUNCATE TABLE `transports`;
+INSERT INTO `transports` (`guid`,`entry`,`name`,`ScriptName`) VALUES 
+(1,176495,'Undercity, Tirisfal Glades and Grom\'gol Base Camp, Stranglethorn Vale (\"The Purple Princess\")',''),
+(2,176310,'Stormwind Harbor and Auberdine, Darkshore (\"Ship (The Bravery)\")',''),
+(3,176244,'Rut\'theran Village, Teldrassil and Auberdine, Darkshore (\"The Moonspray\")',''),
+(4,176231,'Menethil Harbor, Wetlands and Theramore Isle, Dustwallow Marsh (\"The Lady Mehley\")',''),
+(5,175080,'Orgrimmar, Durotar and Grom\'gol Base Camp, Stranglethorn Vale (\"The Iron Eagle\")',''),
+(6,164871,'Orgrimmar, Durotar and Undercity, Tirisfal Glades (\"The Thundercaller\")',''),
+(7,20808,'Steamwheedle Cartel ports, Ratchet and Booty Bay (\"The Maiden\'s Fancy\")',''),
+(8,177233,'The Forgotten Coast, Feralas and Feathermoon Stronghold, Sardor Isle, Feralas (\"Feathermoon Ferry\")',''),
+(9,181646,'Valaar\'s Berth, Azuremyst Isle and Auberdine, Darkshore (\"Elune\'s Blessing\")',''),
+(10,181688,'Menethil Harbor, Wetlands and Valgarde, Howling Fjord (\"Northspear\")',''),
+(11,181689,'Undercity, Tirisfal Glades and Vengeance Landing, Howling Fjord (\"Zeppelin, Horde (Cloudkisser)\")',''),
+(12,186238,'Orgrimmar, Durotar and Warsong Hold, Borean Tundra (\"Zeppelin, Horde (The Mighty Wind)\")',''),
+(13,186371,'Westguard Keep in Howling Fjord to bombard pirate (\"Zeppelin\")',''),
+(14,187038,'Not Boardable - Cyrcling in Howling Fjord (\"Sister Mercy\")',''),
+(15,187568,'Unu\'pe, Borean Tundra and Moa\'ki Harbor, Dragonblight (\"Turtle (Walker of Waves)\")',''),
+(16,188511,'Moa\'ki Harbor and Kamagua (\"Turtle (Green Island)\")',''),
+(17,190536,'Stormwing Harbor and Valiance Keep, Borean Tundra (\"The Kraken\")',''),
+(18,192241,'Horde gunship patrolling above Icecrown (\"Orgrim\'s Hammer\")',''),
+(19,192242,'Alliance gunship patrolling above Icecrown (\"The Skybreaker\")',''),
+(20,190549,'Orgrimmar and Thunder Bluff','');
