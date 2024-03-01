0e01b72df87d9bff4675df710b8bbd57254beb7e
Shauren <shauren.trinity@gmail.com>
2024-03-01 15:18:08 +0100

    Build: Silence cmake warning about newline in variable

--- cmake/macros/FindMySQL.cmake
+++ cmake/macros/FindMySQL.cmake
@@ -72,6 +72,7 @@ if(UNIX)
     execute_process(
       COMMAND "${MYSQL_CONFIG}" --include
       OUTPUT_VARIABLE MY_TMP
+      OUTPUT_STRIP_TRAILING_WHITESPACE
     )
 
     string(REGEX REPLACE "-I([^ ]*)( .*)?" "\\1" MY_TMP "${MY_TMP}")
@@ -81,6 +82,7 @@ if(UNIX)
     execute_process(
       COMMAND "${MYSQL_CONFIG}" --libs_r
       OUTPUT_VARIABLE MY_TMP
+      OUTPUT_STRIP_TRAILING_WHITESPACE
     )
     set(MYSQL_ADD_LIBRARIES "")
     string(REGEX MATCHALL "-l[^ ]*" MYSQL_LIB_LIST "${MY_TMP}")
