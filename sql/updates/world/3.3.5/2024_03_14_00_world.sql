fd0b00cff4e2dd2508b2f15a3329d17e8e6658c7
Aokromes <Aokromes@users.noreply.github.com>
2024-03-14 19:42:51 +0100

    DB/Quest: The Purification of Quel'Delar
    closes  #4866 by CraftedRO
    
    One issue exist, you need to enter creating raid but you cannot complete on raid, you must wait to end the blablabla then leave raid

---
+++ sql/updates/world/3.3.5/2024_03_14_00_world.sql
@@ -0,0 +1,18 @@
+-- Cast aura 70193 on player if one of the following conditions are meet
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 17 AND `SourceGroup` = 0 AND `SourceEntry` = 70193;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
+(17, 0, 70193, 0, 1, 8, 0, 24480, 0, 0, 0, 0, 0, '', "Cast aura 70193 on player only if quest The Halls Of Reflection (A) rewarded"),
+(17, 0, 70193, 0, 2, 47, 0, 24522, 74, 0, 0, 0, 0, '', "Cast aura 70193 on player only if quest Journey To The Sunwell (A) progress | completed | rewarded"),
+(17, 0, 70193, 0, 3, 47, 0, 24535, 74, 0, 0, 0, 0, '', "Cast aura 70193 on player only if quest Thalorien Dawnseeker (A) progress | completed | rewarded"),
+(17, 0, 70193, 0, 4, 8, 0, 24561, 0, 0, 0, 0, 0, '', "Cast aura 70193 on player only if quest The Halls Of Reflection (H) rewarded"),
+(17, 0, 70193, 0, 5, 47, 0, 24562, 74, 0, 0, 0, 0, '', "Cast aura 70193 on player only if quest Journey To The Sunwell (H) progress | completed | rewarded"),
+(17, 0, 70193, 0, 6, 47, 0, 24563, 74, 0, 0, 0, 0, '', "Cast aura 70193 on player only if quest Thalorien Dawnseeker (H) progress | completed | rewarded");
+
+--
+DELETE FROM `spell_area` WHERE `spell`=70193 AND `quest_start` IN (24535,24563);
+INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_end`, `aura_spell`, `racemask`, `gender`, `autocast`, `quest_start_status`, `quest_end_status`) VALUES
+(70193, 4092, 24535, 24535, 0, 0, 2, 1, 75, 75),
+(70193, 4092, 24563, 24563, 0, 0, 2, 1, 75, 75);
+
+--
+UPDATE `spell_area` SET `quest_start_status`=75,`quest_end_status`=75 WHERE `spell`=70193 AND `area`=4094;
