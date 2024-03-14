d3d0cadf414b94b4687363f475e432bae4fc335d
CraftedRO <24683355+CraftedRO@users.noreply.github.com>
2024-03-14 23:19:29 +0200

    Core/Player: fix incorrect Warrior Block Value calculations (#29767)
    (cherry picked from commit 37e220dc933264ed21a217fb2173b9b07ab835a3)

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -4903,7 +4903,7 @@ void Player::ApplyBaseModPctValue(BaseModGroup modGroup, float pct)
         return;
     }
 
-    AddPct(m_auraBasePctMod[modGroup], pct);
+    m_auraBasePctMod[modGroup] += CalculatePct(1.0f, pct);
     UpdateBaseModGroup(modGroup);
 }
 
