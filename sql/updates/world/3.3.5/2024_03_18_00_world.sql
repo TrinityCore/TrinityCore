640674d902de5f177feb1353f8c456b839b31512
Aokromes <Aokromes@users.noreply.github.com>
2024-03-18 00:27:34 +0100

    DB/Quest: Update What The Dragons Know quest_poi_points
    closes #29814 by CraftedRO

---
+++ sql/updates/world/3.3.5/2024_03_18_00_world.sql
@@ -0,0 +1,4 @@
+-- Alliance 
+UPDATE quest_poi_points SET `X`=5745, `Y`=721, `VerifiedBuild`=19831 WHERE `QuestID`=14444; 
+-- Horde 
+UPDATE quest_poi_points SET `X`=5893, `Y`=471, `VerifiedBuild`=19831 WHERE `QuestID`=24555;
