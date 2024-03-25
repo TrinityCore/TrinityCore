04e7c62341718e669f7552713022328c9badf566
Shauren <shauren.trinity@gmail.com>
2024-03-25 12:18:37 +0100

    Core/vmaps: Improved WMO detection for group models that don't have floor
    (cherry picked from commit 28c9474337d0d30bc1d131b12b635c31a98bc97a)

--- src/common/Collision/Models/WorldModel.h
+++ src/common/Collision/Models/WorldModel.h
@@ -90,12 +90,14 @@ namespace VMAP
             void setMeshData(std::vector<G3D::Vector3> &vert, std::vector<MeshTriangle> &tri);
             void setLiquidData(WmoLiquid*& liquid) { iLiquid = liquid; liquid = nullptr; }
             bool IntersectRay(const G3D::Ray &ray, float &distance, bool stopAtFirstHit) const;
-            bool IsInsideObject(const G3D::Vector3 &pos, const G3D::Vector3 &down, float &z_dist) const;
+            enum InsideResult { INSIDE = 0, MAYBE_INSIDE = 1, ABOVE = 2, OUT_OF_BOUNDS = -1 };
+            InsideResult IsInsideObject(G3D::Ray const& ray, float& z_dist) const;
             bool GetLiquidLevel(const G3D::Vector3 &pos, float &liqHeight) const;
             uint32 GetLiquidType() const;
             bool writeToFile(FILE* wf);
             bool readFromFile(FILE* rf);
-            const G3D::AABox& GetBound() const { return iBound; }
+            G3D::AABox const& GetBound() const { return iBound; }
+            G3D::AABox const& GetMeshTreeBound() const { return meshTree.bound(); }
             uint32 GetMogpFlags() const { return iMogpFlags; }
             uint32 GetWmoID() const { return iGroupWMOID; }
             std::vector<G3D::Vector3> const& GetVertices() const { return vertices; }
