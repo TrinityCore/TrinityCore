08e57239abb6daed21fc2c424fee4c4da2542274
Ovahlord <dreadkiller@gmx.de>
2024-04-01 22:06:40 +0200

    Core/Config: disable AE loot by default

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -1721,7 +1721,7 @@ void World::LoadConfigSettings(bool reload)
     m_bool_configs[CONFIG_ALLOW_LOGGING_IP_ADDRESSES_IN_DATABASE] = sConfigMgr->GetBoolDefault("AllowLoggingIPAddressesInDatabase", true, true);
 
     // Enable AE loot
-    m_bool_configs[CONFIG_ENABLE_AE_LOOT] = sConfigMgr->GetBoolDefault("Loot.EnableAELoot", true);
+    m_bool_configs[CONFIG_ENABLE_AE_LOOT] = sConfigMgr->GetBoolDefault("Loot.EnableAELoot", false);
 
     // call ScriptMgr if we're reloading the configuration
     if (reload)
