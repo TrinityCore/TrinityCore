2fe3cae4c21954fbabc8d6dfbd85ba3f18badb8b
ModoX <moardox@gmail.com>
2024-04-02 04:10:05 +0200

    Update and rename 9999_99_99_99_world.sql to 2024_04_02_00_world.sql

--- sql/updates/world/master/2024_04_02_00_world.sql
+++ sql/updates/world/master/2024_04_02_00_world.sql
@@ -1,5 +1,5 @@
 DELIMITER ;;
-CREATE PROCEDURE waypoint_path_velocity() BEGIN
+CREATE PROCEDURE waypoint_path_velocity_2024_04_02_00() BEGIN
     IF NOT EXISTS (SELECT * FROM `information_schema`.`columns` WHERE `table_schema`=SCHEMA() AND `table_name`='waypoint_path' AND `column_name`='Velocity') THEN
 
     ALTER TABLE `waypoint_path` 
@@ -9,9 +9,9 @@ CREATE PROCEDURE waypoint_path_velocity() BEGIN
 END;;
 
 DELIMITER ;
-CALL waypoint_path_velocity();
+CALL waypoint_path_velocity_2024_04_02_00();
 
-DROP PROCEDURE IF EXISTS waypoint_path_velocity;
+DROP PROCEDURE IF EXISTS waypoint_path_velocity_2024_04_02_00;
 
 UPDATE `waypoint_path` SET `Velocity`=4.0 WHERE `PathId`=9301100;
 UPDATE `waypoint_path` SET `Velocity`=35.0 WHERE `PathId` IN(11432300, 11432301, 11432302, 11432303, 11432304);
