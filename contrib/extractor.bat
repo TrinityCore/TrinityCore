2ae8c0d32da8a5cb167de82e93c993a4f7145418
RainbowBridgeSoftware <128447704+RainbowBridgeSoftware@users.noreply.github.com>
2024-04-08 12:20:51 +0100

    Misc/Extractor: Minor changes to windows extractor script (#29902)
    * Extract all with logs & some cosmetics
    
    Added an option to output extraction results to the console and log files.
    Also added a pause of 5 seconds between actions, and some cosmetic changes.

--- contrib/extractor.bat
+++ contrib/extractor.bat
@@ -3,57 +3,99 @@ CLS
 
 :MENU
 ECHO.
-ECHO ...............................................
-ECHO Trinitycore dbc/db2, maps, vmaps, mmaps extractor
-ECHO ...............................................
-ECHO PRESS 1, 2, 3 OR 4 to select your task, or 5 to EXIT.
-ECHO ...............................................
+ECHO ====================== Extractor ========================
+ECHO TrinityCore cameras, dbc, maps, vmaps and mmaps extractor
+ECHO =========================================================
 ECHO.
-ECHO WARNING! when extracting the vmaps extractor will
+ECHO ======================================================
+ECHO WARNING! When extracting the vmaps extractor will
 ECHO output the text below, it's intended and not an error:
-ECHO ..........................................
+ECHO ======================================================
 ECHO Extracting World\Wmo\Band\Final_Stage.wmo
 ECHO No such file.
 ECHO Couldn't open RootWmo!!!
 ECHO Done!
-ECHO ..........................................
+ECHO ======================================================
 ECHO.
-ECHO Press 1, 2, 3 or 4 to start extracting or 5 to exit.
-ECHO 1 - Extract base files (NEEDED) and cameras.
-ECHO 2 - Extract vmaps (needs maps to be extracted before you run this) (OPTIONAL, highly recommended)
-ECHO 3 - Extract mmaps (needs vmaps to be extracted before you run this, may take hours) (OPTIONAL, highly recommended)
-ECHO 4 - Extract all (may take hours)
-ECHO 5 - EXIT
+ECHO 1: Extract base files (NEEDED) and cameras.
+ECHO 2: Extract vmaps (needs maps to be extracted before you run this) (OPTIONAL, but highly recommended)
+ECHO 3: Extract mmaps (needs vmaps to be extracted before you run this, may take hours) (OPTIONAL, but highly recommended)
+ECHO 4: Extract all (may take hours)
+ECHO 5: Extract all with logs (may take hours)
+ECHO 6: EXIT
 ECHO.
-SET /P M=Type 1, 2, 3, 4 or 5 then press ENTER:
+SET /P M=Type 1, 2, 3, 4, 5 or 6 then press ENTER:
 IF %M%==1 GOTO MAPS
 IF %M%==2 GOTO VMAPS
 IF %M%==3 GOTO MMAPS
 IF %M%==4 GOTO ALL
-IF %M%==5 GOTO :EOF
+IF %M%==5 GOTO ALLWITHLOGS
+IF %M%==6 GOTO :EOF
 
 :MAPS
-start /b /w mapextractor.exe
+ECHO.
+START /b /w mapextractor.exe
+ECHO Cameras, dbc and maps extracted.
+TIMEOUT 5 > NUL
+CLS
 GOTO MENU
 
 :VMAPS
-start /b /w vmap4extractor.exe
-start /b /w vmap4assembler.exe Buildings vmaps
-rmdir Buildings /s /q
+ECHO.
+START /b /w vmap4extractor.exe
+START /b /w vmap4assembler.exe Buildings vmaps
+RMDIR Buildings /s /q
+ECHO Vmaps extracted and reassembled.
+TIMEOUT 5 > NUL
+CLS
 GOTO MENU
 
 :MMAPS
+ECHO.
 ECHO This may take a few hours to complete. Please be patient.
-PAUSE
-start /b /w mmaps_generator.exe
+TIMEOUT 5 > NUL
+START /b /w mmaps_generator.exe
+ECHO Mmaps generated.
+TIMEOUT 5 > NUL
+CLS
 GOTO MENU
 
 :ALL
+ECHO.
 ECHO This may take a few hours to complete. Please be patient.
-PAUSE
-start /b /w mapextractor.exe
-start /b /w vmap4extractor.exe
-start /b /w vmap4assembler.exe
-rmdir Buildings /s /q
-start /b /w mmaps_generator.exe
+TIMEOUT 5 > NUL
+START /b /w mapextractor.exe
+ECHO Cameras, dbc and maps extracted.
+TIMEOUT 5 > NUL
+START /b /w vmap4extractor.exe
+ECHO Vmaps extracted.
+TIMEOUT 5 > NUL
+START /b /w vmap4assembler.exe
+RMDIR Buildings /s /q
+ECHO Vmaps reassembled.
+TIMEOUT 5 > NUL
+START /b /w mmaps_generator.exe
+ECHO Mmaps generated.
+TIMEOUT 5 > NUL
+CLS
+GOTO MENU
+
+:ALLWITHLOGS
+ECHO.
+ECHO This may take a few hours to complete. Please be patient.
+TIMEOUT 5 > NUL
+START /b /w mapextractor.exe > mapextractor.log
+ECHO Cameras, dbc and maps extracted.
+TIMEOUT 5 > NUL
+START /b /w vmap4extractor.exe > vmap4extractor.log
+ECHO Vmaps extracted.
+TIMEOUT 5 > NUL
+START /b /w vmap4assembler.exe > vmap4assembler.log
+RMDIR Buildings /s /q
+ECHO Vmaps reassembled.
+TIMEOUT 5 > NUL
+START /b /w mmaps_generator.exe > mmaps_generator.log
+ECHO Mmaps generated.
+TIMEOUT 5 > NUL
+CLS
 GOTO MENU
