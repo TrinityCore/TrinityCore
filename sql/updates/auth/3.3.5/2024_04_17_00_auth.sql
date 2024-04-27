537e3593226b32edc53a2c02efe59e0ccdf69905
M'Dic <joshua.lee.betts@gmail.com>
2024-04-27 17:58:30 -0400

    Update: Better SQL Formatting

--- sql/updates/auth/3.3.5/2024_04_17_00_auth.sql
+++ sql/updates/auth/3.3.5/2024_04_17_00_auth.sql
@@ -1,11 +1,13 @@
--- Dumping structure for table auth.account_ip_history
+-- Drop table if exists
 DROP TABLE IF EXISTS `account_ip_history`;
+
+-- Create table
 CREATE TABLE IF NOT EXISTS `account_ip_history` (
   `Account` int unsigned NOT NULL DEFAULT 0,
   `RealmID` int unsigned DEFAULT NULL,
   `IP` varchar(15) NOT NULL DEFAULT '',
-  `date_recent` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `date_first` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `comment` text COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '',
-  PRIMARY KEY (`Account`,`IP`)
+  `date_recent` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_first` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `comment` text(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '',
+  PRIMARY KEY (`Account`, `IP`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
