a54a11f967220b0ddda0ed5d7f0606eecf432c88
aquadeus <efectokoleos13@gmail.com>
2024-05-02 10:48:18 +0200

    Spells/Evoker: Implement Blessing of the Bronze

---
+++ sql/updates/world/master/2024_99_99_99_spell_evo_blessing_of_the_bronze.sql
@@ -0,0 +1,15 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (381732, 381741, 381746, 381748, 381749, 381750, 381751, 381752, 381753, 381754, 381756, 381757, 381758);
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(381732, 'spell_evo_blessing_of_the_bronze'),
+(381741, 'spell_evo_blessing_of_the_bronze'),
+(381746, 'spell_evo_blessing_of_the_bronze'),
+(381748, 'spell_evo_blessing_of_the_bronze'),
+(381749, 'spell_evo_blessing_of_the_bronze'),
+(381750, 'spell_evo_blessing_of_the_bronze'),
+(381751, 'spell_evo_blessing_of_the_bronze'),
+(381752, 'spell_evo_blessing_of_the_bronze'),
+(381753, 'spell_evo_blessing_of_the_bronze'),
+(381754, 'spell_evo_blessing_of_the_bronze'),
+(381756, 'spell_evo_blessing_of_the_bronze'),
+(381757, 'spell_evo_blessing_of_the_bronze'),
+(381758, 'spell_evo_blessing_of_the_bronze');
