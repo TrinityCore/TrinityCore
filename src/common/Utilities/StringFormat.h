814c5c3a4bb596f9fca9370dde5e0cdcff6f3538
Shauren <shauren.trinity@gmail.com>
2024-05-03 19:38:57 +0200

    Dep/fmt: Upgrade to 10.2.1
    (cherry picked from commit 2aedd8ecdb16a8c72ff89c6c09ad99fff23609b1)

--- src/common/Utilities/StringFormat.h
+++ src/common/Utilities/StringFormat.h
@@ -122,4 +122,8 @@ struct fmt::formatter<Optional<T>, Char> : formatter<T, Char>
     }
 };
 
+// allow implicit enum to int conversions for formatting
+template <typename E, std::enable_if_t<std::is_enum_v<E>, std::nullptr_t> = nullptr>
+auto format_as(E e) { return std::underlying_type_t<E>(e); }
+
 #endif
