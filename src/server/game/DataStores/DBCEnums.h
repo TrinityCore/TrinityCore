9810fdca955cfb3986db7b3a49537673f6450338
Antonio Martín Berti <15972392+BertiRean@users.noreply.github.com>
2024-05-11 19:43:43 -0300

    Core/Misc: Removed NYI comment text on CriteriaType::PlayerTriggerGameEvent (#29974)
    (cherry picked from commit 10b8dcfb3c7077ce94507ab20945f72ab31405fe)

--- src/server/game/DataStores/DBCEnums.h
+++ src/server/game/DataStores/DBCEnums.h
@@ -574,7 +574,7 @@ enum class CriteriaType : int16
     KillPlayer                                     = 70,  // Kill a player (no honor check)
     CompleteChallengeMode                          = 71,  /*NYI*/ // Complete a challenge mode on map "{Map}"
     CatchFishInFishingHole                         = 72,  // Catch fish in the "{GameObjects}" fishing hole
-    PlayerTriggerGameEvent                         = 73,  /*NYI*/ // Player will Trigger game event "{GameEvents}"
+    PlayerTriggerGameEvent                         = 73,  // Player will Trigger game event "{GameEvents}"
     Login                                          = 74,  // Login (USE SPARINGLY!)
     LearnSpellFromSkillLine                        = 75,  // Learn spell from the "{SkillLine}" skill line
     WinDuel                                        = 76,  // Win a duel
