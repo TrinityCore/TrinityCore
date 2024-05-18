c15c8609f0b950804ce798369bd7c3f54b5ab221
Shauren <shauren.trinity@gmail.com>
2024-05-19 00:55:24 +0200

    Core/Spells: Implemented SummonPropertiesFlags::UseDemonTimeout
    Closes #29930

--- src/server/game/Entities/Creature/TemporarySummon.cpp
+++ src/server/game/Entities/Creature/TemporarySummon.cpp
@@ -193,7 +193,14 @@ void TempSummon::InitStats(WorldObject* summoner, Milliseconds duration)
     m_lifetime = duration;
 
     if (m_type == TEMPSUMMON_MANUAL_DESPAWN)
-        m_type = (duration <= 0ms) ? TEMPSUMMON_DEAD_DESPAWN : TEMPSUMMON_TIMED_DESPAWN;
+    {
+        if (duration <= 0s)
+            m_type = TEMPSUMMON_DEAD_DESPAWN;
+        else if (m_Properties && m_Properties->GetFlags().HasFlag(SummonPropertiesFlags::UseDemonTimeout))
+            m_type = TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT;
+        else
+            m_type = TEMPSUMMON_TIMED_DESPAWN;
+    }
 
     if (summoner && summoner->IsPlayer())
     {
