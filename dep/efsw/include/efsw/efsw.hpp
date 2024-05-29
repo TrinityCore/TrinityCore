85834564edd29a57a34a90b29e9e89725bb009a1
Shauren <shauren.trinity@gmail.com>
2024-05-29 12:23:06 +0200

    Dep/efsw: Update to SpartanJ/efsw@36c1c7004a34b6f40719f0830bcfb10325415451
    (cherry picked from commit 937e61853319ac0e5b29f0bcbe5390a71cb7625f)

--- dep/efsw/include/efsw/efsw.hpp
+++ dep/efsw/include/efsw/efsw.hpp
@@ -28,34 +28,37 @@
 #ifndef ESFW_HPP
 #define ESFW_HPP
 
+#include <vector>
 #include <string>
-#include <list>
+#include <vector>
 
-#if defined(_WIN32)
-	#ifdef EFSW_DYNAMIC
-		// Windows platforms
-		#ifdef EFSW_EXPORTS
-			// From DLL side, we must export
-			#define EFSW_API __declspec(dllexport)
-		#else
-			// From client application side, we must import
-			#define EFSW_API __declspec(dllimport)
-		#endif
-	#else
-		// No specific directive needed for static build
-		#ifndef EFSW_API
-		#define EFSW_API
-		#endif
-	#endif
+#if defined( _WIN32 )
+#ifdef EFSW_DYNAMIC
+// Windows platforms
+#ifdef EFSW_EXPORTS
+// From DLL side, we must export
+#define EFSW_API __declspec( dllexport )
 #else
-	#if ( __GNUC__ >= 4 ) && defined( EFSW_EXPORTS )
-		#define EFSW_API __attribute__ ((visibility("default")))
-	#endif
+// From client application side, we must import
+#define EFSW_API __declspec( dllimport )
+#endif
+#else
+// No specific directive needed for static build
+#ifndef EFSW_API
+#define EFSW_API
+#endif
+#endif
+#else
+#if ( __GNUC__ >= 4 ) && defined( EFSW_EXPORTS )
+#ifndef EFSW_API
+#define EFSW_API __attribute__( ( visibility( "default" ) ) )
+#endif
+#endif
 
-	// Other platforms don't need to define anything
-	#ifndef EFSW_API
-	#define EFSW_API
-	#endif
+// Other platforms don't need to define anything
+#ifndef EFSW_API
+#define EFSW_API
+#endif
 #endif
 
 namespace efsw {
@@ -66,128 +69,174 @@ typedef long WatchID;
 // forward declarations
 class FileWatcherImpl;
 class FileWatchListener;
+class WatcherOption;
 
 /// Actions to listen for. Rename will send two events, one for
 /// the deletion of the old file, and one for the creation of the
 /// new file.
 namespace Actions {
-	enum Action
-	{
-		/// Sent when a file is created or renamed
-		Add = 1,
-		/// Sent when a file is deleted or renamed
-		Delete = 2,
-		/// Sent when a file is modified
-		Modified = 3,
-		/// Sent when a file is moved
-		Moved = 4
-	};
+enum Action {
+	/// Sent when a file is created or renamed
+	Add = 1,
+	/// Sent when a file is deleted or renamed
+	Delete = 2,
+	/// Sent when a file is modified
+	Modified = 3,
+	/// Sent when a file is moved
+	Moved = 4
+};
 }
 typedef Actions::Action Action;
 
 /// Errors log namespace
 namespace Errors {
 
-enum Error
-{
-	FileNotFound	= -1,
-	FileRepeated	= -2,
-	FileOutOfScope	= -3,
-	FileNotReadable	= -4,
-	FileRemote		= -5, /** Directory in remote file system ( create a generic FileWatcher instance to watch this directory ). */
-	Unspecified		= -6
+enum Error {
+	NoError = 0,
+	FileNotFound = -1,
+	FileRepeated = -2,
+	FileOutOfScope = -3,
+	FileNotReadable = -4,
+	/// Directory in remote file system
+	/// ( create a generic FileWatcher instance to watch this directory ).
+	FileRemote = -5,
+	/// File system watcher failed to watch for changes.
+	WatcherFailed = -6,
+	Unspecified = -7
 };
 
-class EFSW_API Log
-{
-	public:
-		/// @return The last error logged
-		static std::string getLastErrorLog();
+class EFSW_API Log {
+  public:
+	/// @return The last error logged
+	static std::string getLastErrorLog();
 
-		/// Creates an error of the type specified
-		static Error createLastError( Error err, std::string log );
+	/// @return The code of the last error logged
+	static Error getLastErrorCode();
+
+	/// Reset last error
+	static void clearLastError();
+
+	/// Creates an error of the type specified
+	static Error createLastError( Error err, std::string log );
 };
 
-}
+} // namespace Errors
 typedef Errors::Error Error;
+
+/// Optional file watcher settings.
+namespace Options {
+enum Option {
+	/// For Windows, the default buffer size of 63*1024 bytes sometimes is not enough and
+	/// file system events may be dropped. For that, using a different (bigger) buffer size
+	/// can be defined here, but note that this does not work for network drives,
+	/// because a buffer larger than 64K will fail the folder being watched, see
+	/// http://msdn.microsoft.com/en-us/library/windows/desktop/aa365465(v=vs.85).aspx)
+	WinBufferSize = 1,
+	/// For Windows, per default all events are captured but we might only be interested
+	/// in a subset; the value of the option should be set to a bitwise or'ed set of
+	/// FILE_NOTIFY_CHANGE_* flags.
+	WinNotifyFilter = 2
+};
+}
+typedef Options::Option Option;
 
 /// Listens to files and directories and dispatches events
 /// to notify the listener of files and directories changes.
 /// @class FileWatcher
-class EFSW_API FileWatcher
-{
-	public:
-		/// Default constructor, will use the default platform file watcher
-		FileWatcher();
+class EFSW_API FileWatcher {
+  public:
+	/// Default constructor, will use the default platform file watcher
+	FileWatcher();
 
-		/// Constructor that lets you force the use of the Generic File Watcher
-		explicit FileWatcher( bool useGenericFileWatcher );
+	/// Constructor that lets you force the use of the Generic File Watcher
+	explicit FileWatcher( bool useGenericFileWatcher );
 
-		virtual ~FileWatcher();
+	virtual ~FileWatcher();
 
-		/// Add a directory watch. Same as the other addWatch, but doesn't have recursive option.
-		/// For backwards compatibility.
-		/// On error returns WatchID with Error type.
-		WatchID addWatch(const std::string& directory, FileWatchListener* watcher);
+	/// Add a directory watch. Same as the other addWatch, but doesn't have recursive option.
+	/// For backwards compatibility.
+	/// On error returns WatchID with Error type.
+	WatchID addWatch( const std::string& directory, FileWatchListener* watcher );
 
-		/// Add a directory watch
-		/// On error returns WatchID with Error type.
-		WatchID addWatch(const std::string& directory, FileWatchListener* watcher, bool recursive);
+	/// Add a directory watch
+	/// On error returns WatchID with Error type.
+	WatchID addWatch( const std::string& directory, FileWatchListener* watcher, bool recursive );
 
-		/// Remove a directory watch. This is a brute force search O(nlogn).
-		void removeWatch(const std::string& directory);
+	/// Add a directory watch, allowing customization with options
+	/// @param directory The folder to be watched
+	/// @param watcher The listener to receive events
+	/// @param recursive Set this to true to include subdirectories
+	/// @param options Allows customization of a watcher
+	/// @return Returns the watch id for the directory or, on error, a WatchID with Error type.
+	WatchID addWatch( const std::string& directory, FileWatchListener* watcher, bool recursive, 
+					  const std::vector<WatcherOption> &options );
 
-		/// Remove a directory watch. This is a map lookup O(logn).
-		void removeWatch(WatchID watchid);
+	/// Remove a directory watch. This is a brute force search O(nlogn).
+	void removeWatch( const std::string& directory );
 
-		/// Starts watching ( in other thread )
-		void watch();
+	/// Remove a directory watch. This is a map lookup O(logn).
+	void removeWatch( WatchID watchid );
 
-		/// @return Returns a list of the directories that are being watched
-		std::list<std::string> directories();
+	/// Starts watching ( in other thread )
+	void watch();
 
-		/** Allow recursive watchers to follow symbolic links to other directories
-		* followSymlinks is disabled by default
-		*/
-		void followSymlinks( bool follow );
+	/// @return Returns a list of the directories that are being watched
+	std::vector<std::string> directories();
 
-		/** @return If can follow symbolic links to directorioes */
-		const bool& followSymlinks() const;
+	/** Allow recursive watchers to follow symbolic links to other directories
+	 * followSymlinks is disabled by default
+	 */
+	void followSymlinks( bool follow );
 
-		/** When enable this it will allow symlinks to watch recursively out of the pointed directory.
-		* follorSymlinks must be enabled to this work.
-		* For example, added symlink to /home/folder, and the symlink points to /, this by default is not allowed,
-		* it's only allowed to symlink anything from /home/ and deeper. This is to avoid great levels of recursion.
-		* Enabling this could lead in infinite recursion, and crash the watcher ( it will try not to avoid this ).
-		* Buy enabling out of scope links, it will allow this behavior.
-		* allowOutOfScopeLinks are disabled by default.
-		*/
-		void allowOutOfScopeLinks( bool allow );
+	/** @return If can follow symbolic links to directorioes */
+	const bool& followSymlinks() const;
 
-		/// @return Returns if out of scope links are allowed
-		const bool& allowOutOfScopeLinks() const;
-	private:
-		/// The implementation
-		FileWatcherImpl	*	mImpl;
-		bool				mFollowSymlinks;
-		bool				mOutOfScopeLinks;
+	/** When enable this it will allow symlinks to watch recursively out of the pointed directory.
+	 * follorSymlinks must be enabled to this work.
+	 * For example, added symlink to /home/folder, and the symlink points to /, this by default is
+	 * not allowed, it's only allowed to symlink anything from /home/ and deeper. This is to avoid
+	 * great levels of recursion. Enabling this could lead in infinite recursion, and crash the
+	 * watcher ( it will try not to avoid this ). Buy enabling out of scope links, it will allow
+	 * this behavior. allowOutOfScopeLinks are disabled by default.
+	 */
+	void allowOutOfScopeLinks( bool allow );
+
+	/// @return Returns if out of scope links are allowed
+	const bool& allowOutOfScopeLinks() const;
+
+  private:
+	/// The implementation
+	FileWatcherImpl* mImpl;
+	bool mFollowSymlinks;
+	bool mOutOfScopeLinks;
 };
 
 /// Basic interface for listening for file events.
 /// @class FileWatchListener
-class FileWatchListener
-{
-	public:
-		/// Handles the action file action
-		/// @param watchid The watch id for the directory
-		/// @param dir The directory
-		/// @param filename The filename that was accessed (not full path)
-		/// @param action Action that was performed
-		/// @param oldFilename The name of the file or directory moved
-		virtual void handleFileAction(WatchID watchid, const std::string& dir, const std::string& filename, Action action, std::string oldFilename = "" ) = 0;
+class FileWatchListener {
+  public:
+	virtual ~FileWatchListener() {}
 
+	/// Handles the action file action
+	/// @param watchid The watch id for the directory
+	/// @param dir The directory
+	/// @param filename The filename that was accessed (not full path)
+	/// @param action Action that was performed
+	/// @param oldFilename The name of the file or directory moved
+	virtual void handleFileAction( WatchID watchid, const std::string& dir,
+								   const std::string& filename, Action action,
+								   std::string oldFilename = "" ) = 0;
 };
 
-}
+/// Optional, typically platform specific parameter for customization of a watcher.
+/// @class WatcherOption
+class WatcherOption {
+  public:
+	WatcherOption(Option option, int value) : mOption(option), mValue(value) {};
+	Option mOption;
+	int mValue;
+};
+
+} // namespace efsw
 
 #endif
--- dep/efsw/src/efsw/FileWatcherWin32.cpp
+++ dep/efsw/src/efsw/FileWatcherWin32.cpp
@@ -1,41 +1,43 @@
-#include <efsw/FileWatcherWin32.hpp>
 #include <efsw/FileSystem.hpp>
-#include <efsw/System.hpp>
-#include <efsw/String.hpp>
+#include <efsw/FileWatcherWin32.hpp>
 #include <efsw/Lock.hpp>
+#include <efsw/String.hpp>
+#include <efsw/System.hpp>
 
 #if EFSW_PLATFORM == EFSW_PLATFORM_WIN32
 
-namespace efsw
-{
+namespace efsw {
 
-FileWatcherWin32::FileWatcherWin32( FileWatcher * parent ) :
-	FileWatcherImpl( parent ),
-	mLastWatchID(0),
-	mThread( NULL )
-{
-	mInitOK = true;
+FileWatcherWin32::FileWatcherWin32( FileWatcher* parent ) :
+	FileWatcherImpl( parent ), mLastWatchID( 0 ), mThread( NULL ) {
+	mIOCP = CreateIoCompletionPort( INVALID_HANDLE_VALUE, NULL, 0, 1 );
+	if ( mIOCP && mIOCP != INVALID_HANDLE_VALUE )
+		mInitOK = true;
 }
 
-FileWatcherWin32::~FileWatcherWin32()
-{
+FileWatcherWin32::~FileWatcherWin32() {
 	mInitOK = false;
+
+	if ( mIOCP && mIOCP != INVALID_HANDLE_VALUE ) {
+		PostQueuedCompletionStatus( mIOCP, 0, reinterpret_cast<ULONG_PTR>( this ), NULL );
+	}
+
 	efSAFE_DELETE( mThread );
+
 	removeAllWatches();
+
+	CloseHandle( mIOCP );
 }
 
-WatchID FileWatcherWin32::addWatch(const std::string& directory, FileWatchListener* watcher, bool recursive)
-{
+WatchID FileWatcherWin32::addWatch( const std::string& directory, FileWatchListener* watcher,
+									bool recursive, const std::vector<WatcherOption> &options ) {
 	std::string dir( directory );
 
 	FileInfo fi( dir );
 
-	if ( !fi.isDirectory() )
-	{
+	if ( !fi.isDirectory() ) {
 		return Errors::Log::createLastError( Errors::FileNotFound, dir );
-	}
-	else if ( !fi.isReadable() )
-	{
+	} else if ( !fi.isReadable() ) {
 		return Errors::Log::createLastError( Errors::FileNotReadable, dir );
 	}
 
@@ -43,22 +45,22 @@ WatchID FileWatcherWin32::addWatch(const std::string& directory, FileWatchListen
 
 	Lock lock( mWatchesLock );
 
-	if ( pathInWatches( dir ) )
-	{
+	if ( pathInWatches( dir ) ) {
 		return Errors::Log::createLastError( Errors::FileRepeated, dir );
 	}
 
 	WatchID watchid = ++mLastWatchID;
 
-	WatcherStructWin32 * watch = CreateWatch( String::fromUtf8( dir ).toWideString().c_str(), recursive,		FILE_NOTIFY_CHANGE_CREATION |
-																			FILE_NOTIFY_CHANGE_LAST_WRITE |
-																			FILE_NOTIFY_CHANGE_FILE_NAME |
-																			FILE_NOTIFY_CHANGE_DIR_NAME |
-																			FILE_NOTIFY_CHANGE_SIZE
-	);
+	DWORD bufferSize = static_cast<DWORD>( getOptionValue(options, Option::WinBufferSize, 63 * 1024) );
+	DWORD notifyFilter = static_cast<DWORD>( getOptionValue(options, Option::WinNotifyFilter,
+		FILE_NOTIFY_CHANGE_CREATION | FILE_NOTIFY_CHANGE_LAST_WRITE |
+		FILE_NOTIFY_CHANGE_FILE_NAME | FILE_NOTIFY_CHANGE_DIR_NAME |
+		FILE_NOTIFY_CHANGE_SIZE) );
 
-	if( NULL == watch )
-	{
+	WatcherStructWin32* watch = CreateWatch( String::fromUtf8( dir ).toWideString().c_str(),
+											 recursive, bufferSize, notifyFilter, mIOCP );
+
+	if ( NULL == watch ) {
 		return Errors::Log::createLastError( Errors::FileNotFound, dir );
 	}
 
@@ -66,241 +68,191 @@ WatchID FileWatcherWin32::addWatch(const std::string& directory, FileWatchListen
 	watch->Watch->ID = watchid;
 	watch->Watch->Watch = this;
 	watch->Watch->Listener = watcher;
-	watch->Watch->DirName = new char[dir.length()+1];
-	strcpy(watch->Watch->DirName, dir.c_str());
+	watch->Watch->DirName = new char[dir.length() + 1];
+	strcpy( watch->Watch->DirName, dir.c_str() );
 
-	mWatchesNew.insert( watch );
 	mWatches.insert( watch );
 
 	return watchid;
 }
 
-void FileWatcherWin32::removeWatch(const std::string& directory)
-{
+void FileWatcherWin32::removeWatch( const std::string& directory ) {
 	Lock lock( mWatchesLock );
 
 	Watches::iterator iter = mWatches.begin();
 
-	for(; iter != mWatches.end(); ++iter)
-	{
-		if(directory == (*iter)->Watch->DirName)
-		{
-			removeWatch(*iter);
+	for ( ; iter != mWatches.end(); ++iter ) {
+		if ( directory == ( *iter )->Watch->DirName ) {
+			removeWatch( *iter );
 			break;
 		}
 	}
 }
 
-void FileWatcherWin32::removeWatch(WatchID watchid)
-{
+void FileWatcherWin32::removeWatch( WatchID watchid ) {
 	Lock lock( mWatchesLock );
 
 	Watches::iterator iter = mWatches.begin();
 
-	for(; iter != mWatches.end(); ++iter)
-	{
+	for ( ; iter != mWatches.end(); ++iter ) {
 		// Find the watch ID
-		if ( (*iter)->Watch->ID == watchid )
-		{
-			removeWatch(*iter);
+		if ( ( *iter )->Watch->ID == watchid ) {
+			removeWatch( *iter );
 			return;
 		}
 	}
 }
 
-void FileWatcherWin32::removeWatch(WatcherStructWin32* watch)
-{
-	mWatchesRemoved.insert(watch);
-
-	if( NULL == mThread )
-	{
-		removeWatches();
-	}
-}
-
-void FileWatcherWin32::removeWatches()
-{
+void FileWatcherWin32::removeWatch( WatcherStructWin32* watch ) {
 	Lock lock( mWatchesLock );
 
-	Watches::iterator remWatchIter = mWatchesRemoved.begin();
-
-	for( ; remWatchIter != mWatchesRemoved.end(); ++remWatchIter )
-	{
-		Watches::iterator iter = mWatches.find(*remWatchIter);
-
-		if( iter != mWatches.end() )
-		{
-			DestroyWatch(*iter);
-
-			mWatches.erase( iter );
-		}
-
-		iter = mWatchesNew.find(*remWatchIter);
-
-		if( iter != mWatchesNew.end() )
-		{
-			mWatchesNew.erase( iter );
-		}
-	}
-
-	mWatchesRemoved.clear();
+	DestroyWatch( watch );
+	mWatches.erase( watch );
 }
 
-void FileWatcherWin32::watch()
-{
-	if ( NULL == mThread )
-	{
+void FileWatcherWin32::watch() {
+	if ( NULL == mThread ) {
 		mThread = new Thread( &FileWatcherWin32::run, this );
 		mThread->launch();
 	}
 }
 
-void FileWatcherWin32::removeAllWatches()
-{
+void FileWatcherWin32::removeAllWatches() {
 	Lock lock( mWatchesLock );
 
 	Watches::iterator iter = mWatches.begin();
 
-	for( ; iter != mWatches.end(); ++iter )
-	{
-		DestroyWatch((*iter));
+	for ( ; iter != mWatches.end(); ++iter ) {
+		DestroyWatch( ( *iter ) );
 	}
 
 	mWatches.clear();
-	mWatchesRemoved.clear();
-	mWatchesNew.clear();
 }
 
-void FileWatcherWin32::run()
-{
-	do
-	{
-		if ( !mWatches.empty() )
-		{
-			{
-				Lock lock( mWatchesLock );
+void FileWatcherWin32::run() {
+	do {
+		if ( mInitOK && !mWatches.empty() ) {
+			DWORD numOfBytes = 0;
+			OVERLAPPED* ov = NULL;
+			ULONG_PTR compKey = 0;
+			BOOL res = FALSE;
 
-				for( Watches::iterator iter = mWatches.begin() ; iter != mWatches.end(); ++iter )
-				{
-					WatcherStructWin32 * watch = *iter;
-
-					if ( HasOverlappedIoCompleted( &watch->Overlapped ) )
-					{
-						DWORD bytes;
-
-						if ( GetOverlappedResult( watch->Watch->DirHandle, &watch->Overlapped, &bytes, FALSE ) )
-						{
-							WatchCallback( ERROR_SUCCESS, bytes, &watch->Overlapped );
-						}
-					}
+			while ( ( res = GetQueuedCompletionStatus( mIOCP, &numOfBytes, &compKey, &ov,
+													   INFINITE ) ) != FALSE ) {
+				if ( compKey != 0 && compKey == reinterpret_cast<ULONG_PTR>( this ) ) {
+					break;
+				} else {
+					Lock lock( mWatchesLock );
+					WatchCallback( numOfBytes, ov );
 				}
 			}
-
-			if ( mInitOK )
-			{
-				System::sleep( 10 );
-			}
-		}
-		else
-		{
-			// Wait for a new handle to be added
+		} else {
 			System::sleep( 10 );
 		}
-
-		removeWatches();
-
-		for ( Watches::iterator it = mWatchesNew.begin(); it != mWatchesNew.end(); ++it )
-		{
-			RefreshWatch(*it);
-		}
-
-		mWatchesNew.clear();
 	} while ( mInitOK );
 
 	removeAllWatches();
 }
 
-void FileWatcherWin32::handleAction(Watcher* watch, const std::string& filename, unsigned long action, std::string oldFilename)
-{
+void FileWatcherWin32::handleAction( Watcher* watch, const std::string& filename,
+									 unsigned long action, std::string /*oldFilename*/ ) {
 	Action fwAction;
 
-	switch(action)
-	{
-	case FILE_ACTION_RENAMED_OLD_NAME:
-		watch->OldFileName = filename;
-		return;
-	case FILE_ACTION_ADDED:
-		fwAction = Actions::Add;
-		break;
-	case FILE_ACTION_RENAMED_NEW_NAME:
-	{
-		fwAction = Actions::Moved;
+	switch ( action ) {
+		case FILE_ACTION_RENAMED_OLD_NAME:
+			watch->OldFileName = filename;
+			return;
+		case FILE_ACTION_ADDED:
+			fwAction = Actions::Add;
+			break;
+		case FILE_ACTION_RENAMED_NEW_NAME: {
+			fwAction = Actions::Moved;
 
-		std::string fpath( watch->Directory + filename );
+			std::string fpath( watch->Directory + filename );
 
-		// Update the directory path
-		if ( watch->Recursive && FileSystem::isDirectory( fpath ) )
-		{
-			// Update the new directory path
-			std::string opath( watch->Directory + watch->OldFileName );
-			FileSystem::dirAddSlashAtEnd( opath );
-			FileSystem::dirAddSlashAtEnd( fpath );
+			// Update the directory path
+			if ( watch->Recursive && FileSystem::isDirectory( fpath ) ) {
+				// Update the new directory path
+				std::string opath( watch->Directory + watch->OldFileName );
+				FileSystem::dirAddSlashAtEnd( opath );
+				FileSystem::dirAddSlashAtEnd( fpath );
 
-			for ( Watches::iterator it = mWatches.begin(); it != mWatches.end(); ++it )
-			{
-				if ( (*it)->Watch->Directory == opath )
-				{
-					(*it)->Watch->Directory = fpath;
+				for ( Watches::iterator it = mWatches.begin(); it != mWatches.end(); ++it ) {
+					if ( ( *it )->Watch->Directory == opath ) {
+						( *it )->Watch->Directory = fpath;
 
-					break;
+						break;
+					}
 				}
 			}
-		}
 
-		watch->Listener->handleFileAction(watch->ID, static_cast<WatcherWin32*>( watch )->DirName, filename, fwAction, watch->OldFileName);
-		return;
-	}
-	case FILE_ACTION_REMOVED:
-		fwAction = Actions::Delete;
-		break;
-	case FILE_ACTION_MODIFIED:
-		fwAction = Actions::Modified;
-		break;
+			std::string folderPath( static_cast<WatcherWin32*>( watch )->DirName );
+			std::string realFilename = filename;
+			std::size_t sepPos = filename.find_last_of( "/\\" );
+			std::string oldFolderPath =
+				static_cast<WatcherWin32*>( watch )->DirName +
+				watch->OldFileName.substr( 0, watch->OldFileName.find_last_of( "/\\" ) );
+
+			if ( sepPos != std::string::npos ) {
+				folderPath +=
+					filename.substr( 0, sepPos + 1 < filename.size() ? sepPos + 1 : sepPos );
+				realFilename = filename.substr( sepPos + 1 );
+			}
+
+			if ( folderPath == oldFolderPath ) {
+				watch->Listener->handleFileAction(
+					watch->ID, folderPath, realFilename, fwAction,
+					FileSystem::fileNameFromPath( watch->OldFileName ) );
+			} else {
+				watch->Listener->handleFileAction( watch->ID,
+												   static_cast<WatcherWin32*>( watch )->DirName,
+												   filename, fwAction, watch->OldFileName );
+			}
+			return;
+		}
+		case FILE_ACTION_REMOVED:
+			fwAction = Actions::Delete;
+			break;
+		case FILE_ACTION_MODIFIED:
+			fwAction = Actions::Modified;
+			break;
+		default:
+			return;
 	};
 
 	std::string folderPath( static_cast<WatcherWin32*>( watch )->DirName );
 	std::string realFilename = filename;
-	std::size_t sepPos = filename.find_last_of("/\\");
+	std::size_t sepPos = filename.find_last_of( "/\\" );
 
-	if ( sepPos != std::string::npos )
-	{
-		folderPath += filename.substr( 0, sepPos );
+	if ( sepPos != std::string::npos ) {
+		folderPath += filename.substr( 0, sepPos + 1 < filename.size() ? sepPos + 1 : sepPos );
 		realFilename = filename.substr( sepPos + 1 );
 	}
 
-	watch->Listener->handleFileAction(watch->ID, folderPath, realFilename, fwAction);
+	FileSystem::dirAddSlashAtEnd( folderPath );
+
+	watch->Listener->handleFileAction( watch->ID, folderPath, realFilename, fwAction );
 }
 
-std::list<std::string> FileWatcherWin32::directories()
-{
-	std::list<std::string> dirs;
+std::vector<std::string> FileWatcherWin32::directories() {
+	std::vector<std::string> dirs;
 
 	Lock lock( mWatchesLock );
 
-	for ( Watches::iterator it = mWatches.begin(); it != mWatches.end(); ++it )
-	{
-		dirs.push_back( std::string( (*it)->Watch->DirName ) );
+	dirs.reserve( mWatches.size() );
+
+	for ( Watches::iterator it = mWatches.begin(); it != mWatches.end(); ++it ) {
+		dirs.push_back( std::string( ( *it )->Watch->DirName ) );
 	}
 
 	return dirs;
 }
 
-bool FileWatcherWin32::pathInWatches( const std::string& path )
-{
-	for ( Watches::iterator it = mWatches.begin(); it != mWatches.end(); ++it )
-	{
-		if ( (*it)->Watch->DirName == path )
-		{
+bool FileWatcherWin32::pathInWatches( const std::string& path ) {
+	Lock lock( mWatchesLock );
+
+	for ( Watches::iterator it = mWatches.begin(); it != mWatches.end(); ++it ) {
+		if ( ( *it )->Watch->DirName == path ) {
 			return true;
 		}
 	}
@@ -308,6 +260,6 @@ bool FileWatcherWin32::pathInWatches( const std::string& path )
 	return false;
 }
 
-}
+} // namespace efsw
 
 #endif
