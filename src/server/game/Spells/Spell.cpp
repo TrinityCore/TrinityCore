4a30ca00c7d9adc3ec46267ca267d36c46a3c88e
Shauren <shauren.trinity@gmail.com>
2024-05-31 16:07:47 +0200

    Core/Spells: Removed early access_requirement checks for summon spells to prevent cross map inventory access (it is checked later by TeleportTo)
    Closes #26877
    Closes #29906

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -5874,8 +5874,6 @@ SpellCastResult Spell::CheckCast(bool strict, uint32* param1 /*= nullptr*/, uint
                     InstanceTemplate const* instance = sObjectMgr->GetInstanceTemplate(mapId);
                     if (!instance)
                         return SPELL_FAILED_TARGET_NOT_IN_INSTANCE;
-                    if (!target->Satisfy(sObjectMgr->GetAccessRequirement(mapId, difficulty), mapId))
-                        return SPELL_FAILED_BAD_TARGETS;
                 }
                 break;
             }
