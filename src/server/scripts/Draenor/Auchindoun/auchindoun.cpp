b4e43c23d48aa665e71ddcf7112dc9788be51d65
aquadeus <efectokoleos13@gmail.com>
2024-06-14 13:52:58 +0200

    Fix nopch

--- src/server/scripts/Draenor/Auchindoun/auchindoun.cpp
+++ src/server/scripts/Draenor/Auchindoun/auchindoun.cpp
@@ -15,6 +15,7 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "AreaTrigger.h"
 #include "AreaTriggerAI.h"
 #include "GameObject.h"
 #include "InstanceScript.h"
