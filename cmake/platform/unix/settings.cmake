9866df17c03a4fd88e1fd3b74b1d6fa85c95a58f
Shauren <shauren.trinity@gmail.com>
2024-06-15 17:07:25 +0200

    Build: Decouple compiler specific settings from os settings
    (cherry picked from commit 12d1f4448c30d313e8f0e70d89f29b709f5dd70d)

--- cmake/platform/unix/settings.cmake
+++ cmake/platform/unix/settings.cmake
@@ -49,11 +49,3 @@ message(STATUS "UNIX: Detected compiler: ${CMAKE_C_COMPILER}")
 
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin/$<CONFIG>/bin")
 set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin/$<CONFIG>/lib")
-
-if(CMAKE_C_COMPILER MATCHES "gcc" OR CMAKE_C_COMPILER_ID STREQUAL "GNU")
-  include(${CMAKE_SOURCE_DIR}/cmake/compiler/gcc/settings.cmake)
-elseif(CMAKE_C_COMPILER MATCHES "icc")
-  include(${CMAKE_SOURCE_DIR}/cmake/compiler/icc/settings.cmake)
-elseif(CMAKE_C_COMPILER MATCHES "clang" OR CMAKE_C_COMPILER_ID MATCHES "Clang")
-  include(${CMAKE_SOURCE_DIR}/cmake/compiler/clang/settings.cmake)
-endif()
