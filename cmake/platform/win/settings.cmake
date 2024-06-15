5269de59191f1adde28aaefbac2c8a81cdf27991
Shauren <shauren.trinity@gmail.com>
2024-06-15 17:07:25 +0200

    Build: Decouple compiler specific settings from os settings
    (cherry picked from commit 12d1f4448c30d313e8f0e70d89f29b709f5dd70d)

--- cmake/platform/win/settings.cmake
+++ cmake/platform/win/settings.cmake
@@ -6,11 +6,3 @@ add_definitions(-DTRINITY_REQUIRED_WINDOWS_BUILD=18362)
 
 # set up output paths for executable binaries (.exe-files, and .dll-files on DLL-capable platforms)
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin/$<CONFIG>")
-
-if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC" OR CMAKE_CXX_COMPILER_FRONTEND_VARIANT STREQUAL "MSVC")
-  include(${CMAKE_SOURCE_DIR}/cmake/compiler/msvc/settings.cmake)
-elseif(CMAKE_CXX_PLATFORM_ID MATCHES "MinGW")
-  include(${CMAKE_SOURCE_DIR}/cmake/compiler/mingw/settings.cmake)
-elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-  include(${CMAKE_SOURCE_DIR}/cmake/compiler/clang/settings.cmake)
-endif()
