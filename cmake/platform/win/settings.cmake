063551a65f73936c589e6dcdb3e8652e6f56c739
Shauren <shauren.trinity@gmail.com>
2024-06-15 16:01:22 +0200

    Build: Normalize executable locations in build directory for all platforms
    (cherry picked from commit a7c42f1ed55e586d0d817dd99865aa7c024b0140)
    
    # Conflicts:
    #	.github/workflows/win-x64-build.yml

--- cmake/platform/win/settings.cmake
+++ cmake/platform/win/settings.cmake
@@ -4,6 +4,9 @@ add_definitions(-DWIN32_LEAN_AND_MEAN)
 add_definitions(-DNOMINMAX)
 add_definitions(-DTRINITY_REQUIRED_WINDOWS_BUILD=18362)
 
+# set up output paths for executable binaries (.exe-files, and .dll-files on DLL-capable platforms)
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin/$<CONFIG>")
+
 if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC" OR CMAKE_CXX_COMPILER_FRONTEND_VARIANT STREQUAL "MSVC")
   include(${CMAKE_SOURCE_DIR}/cmake/compiler/msvc/settings.cmake)
 elseif(CMAKE_CXX_PLATFORM_ID MATCHES "MinGW")
