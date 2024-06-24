de6f87d2504e1c18100428b2cf8101f92616a161
Shauren <shauren.trinity@gmail.com>
2024-06-24 12:26:14 +0200

    Core/Spells: Implemented SPELL_ATTR9_AUTOCAST_OFF_BY_DEFAULT
    (cherry picked from commit 24096f6b661161f296bfef4a3303789f245873c4)

--- src/server/game/Spells/SpellInfo.cpp
+++ src/server/game/Spells/SpellInfo.cpp
@@ -1584,6 +1584,11 @@ bool SpellInfo::IsAutocastable() const
     return true;
 }
 
+bool SpellInfo::IsAutocastEnabledByDefault() const
+{
+    return !HasAttribute(SPELL_ATTR9_AUTOCAST_OFF_BY_DEFAULT);
+}
+
 bool SpellInfo::IsStackableWithRanks() const
 {
     if (IsPassive())
