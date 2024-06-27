46e49c3d89b3d2769f06a84232e196f2acb72567
Shauren <shauren.trinity@gmail.com>
2024-06-27 19:38:49 +0200

    prepare merge

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -3752,27 +3752,10 @@ void Player::RemoveArenaSpellCooldowns(bool removeActivePetCooldowns)
     GetSpellHistory()->ResetCooldowns([](SpellHistory::CooldownStorageType::iterator itr) -> bool
     {
         SpellInfo const* spellInfo = sSpellMgr->AssertSpellInfo(itr->first);
-        uint32 maxCooldown = std::max(spellInfo->RecoveryTime, spellInfo->CategoryRecoveryTime);
-        if (Milliseconds(maxCooldown) < Minutes(10))
-        {
-            // Also check item spell cooldowns
-            if (ItemTemplate const* itemTemplate = sObjectMgr->GetItemTemplate(itr->second.ItemId))
-            {
-                for (uint8 i = 0; i < MAX_ITEM_PROTO_SPELLS; ++i)
-                {
-                    if (int32(spellInfo->Id) != itemTemplate->Spells[i].SpellId)
-                        continue;
-
-                    int32 itemMaxCooldown = std::max(itemTemplate->Spells[i].SpellCooldown, itemTemplate->Spells[i].SpellCategoryCooldown);
-                    if (itemMaxCooldown > 0 && Milliseconds(itemMaxCooldown) >= Minutes(10))
-                        return false;
-                }
-            }
-
-            return true;
-        }
-
-        return false;
+        int32 cooldown = -1;
+        int32 categoryCooldown = -1;
+        SpellHistory::GetCooldownDurations(spellInfo, itr->second.ItemId, &cooldown, nullptr, &categoryCooldown);
+        return cooldown < 10 * MINUTE * IN_MILLISECONDS && categoryCooldown < 10 * MINUTE * IN_MILLISECONDS;
     }, true);
 
     // pet cooldowns
