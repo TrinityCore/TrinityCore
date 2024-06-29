4a087ca6f234351f8bad0dcf473493b378665a4b
ModoX <moardox@gmail.com>
2024-06-30 01:49:07 +0200

    use intersection boundary

--- src/server/scripts/KulTiras/WaycrestManor/instance_waycrest_manor.cpp
+++ src/server/scripts/KulTiras/WaycrestManor/instance_waycrest_manor.cpp
@@ -22,7 +22,7 @@
 
 static BossBoundaryData const boundaries =
 {
-    { DATA_HEARTSBANE_TRIAD, new BoundaryUnionBoundary(new ZRangeBoundary(235.0f, 243.0f), new RectangleBoundary(-618.580f, -524.305f, -188.971f, -137.363f)) },
+    { DATA_HEARTSBANE_TRIAD, new BoundaryIntersectionBoundary(new ZRangeBoundary(235.0f, 243.0f), new RectangleBoundary(-618.580f, -524.305f, -188.971f, -137.363f)) },
 };
 
 ObjectData const creatureData[] =
