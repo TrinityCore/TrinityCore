cce07dca0944f030ff530d484ad9df67fdbcce45
Ovahlord <dreadkiller@gmx.de>
2024-07-10 01:06:56 +0200

    remove whitespace

--- src/server/scripts/Spells/spell_warrior.cpp
+++ src/server/scripts/Spells/spell_warrior.cpp
@@ -28,7 +28,7 @@
 // 78 - Heroic Strike
 class spell_warr_heroic_strike : public SpellScript
 {
-    // Damage = {8+$ap*60/100} 
+    // Damage = {8+$ap*60/100}
     void CalculateDamage(Unit* /*victim*/, int32& damage, int32& /*flatMod*/, float& /*pctMod*/)
     {
         damage = static_cast<int32>(8 + GetCaster()->GetTotalAttackPowerValue(BASE_ATTACK) * 0.6f);
