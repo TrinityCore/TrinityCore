34cd99d3bfa0035f17649cb46cd6498b2dc4b80a
heyitsbench <benjymansy123@gmail.com>
2024-07-14 21:42:42 -0400

    I can't read.

--- src/server/game/Handlers/GroupHandler.cpp
+++ src/server/game/Handlers/GroupHandler.cpp
@@ -911,7 +911,7 @@ void WorldSession::HandleRequestPartyMemberStatsOpcode(WorldPacket &recvData)
     recvData >> Guid;
 
     Player* player = ObjectAccessor::FindConnectedPlayer(Guid);
-    if (!player || !GetPlayer()->GetGroup() || !(player->GetGroup()->GetGUID() != GetPlayer()->GetGroup()->GetGUID()))
+    if (!player || !GetPlayer()->GetGroup() || !(player->GetGroup()->GetGUID() == GetPlayer()->GetGroup()->GetGUID()))
     {
         WorldPacket data(SMSG_PARTY_MEMBER_STATS_FULL, 3+4+2);
         data << uint8(0);                                   // only for SMSG_PARTY_MEMBER_STATS_FULL, probably arena/bg related
