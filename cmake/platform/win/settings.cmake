c12f669fc5050017d98ec57c8a45e575154a5edb
Shauren <shauren.trinity@gmail.com>
2024-07-15 15:24:35 +0200

    Build: CMake cleanup
    * Migrate all add_definitions to target_compile_definitions
    * Remove -D from preprocessor definitions added by target_compile_definitions (unneccessary, cmake strips it anyway)
    * Fixed NO_BUFFERPOOL not being set on g3d if jemalloc is used
    * Moved library/compiler specific compile flag settings spread all over various CMakeLists to their related library/compiler file
    * Remove ancient manual link flag settings for worldserver
    
    (cherry picked from commit 77fe2745febab9e4362b09750093d831325e6091)

--- cmake/platform/win/settings.cmake
+++ cmake/platform/win/settings.cmake
@@ -1,8 +1,10 @@
-add_definitions(-D_WIN32_WINNT=0x0A00)      # Windows 10
-add_definitions(-DNTDDI_VERSION=0x0A000007) # 19H1 (1903)
-add_definitions(-DWIN32_LEAN_AND_MEAN)
-add_definitions(-DNOMINMAX)
-add_definitions(-DTRINITY_REQUIRED_WINDOWS_BUILD=18362)
+target_compile_definitions(trinity-compile-option-interface
+  INTERFACE
+    _WIN32_WINNT=0x0A00                     # Windows 10
+    NTDDI_VERSION=0x0A000007                # 19H1 (1903)
+    WIN32_LEAN_AND_MEAN
+    NOMINMAX
+    TRINITY_REQUIRED_WINDOWS_BUILD=18362)
 
 # set up output paths for executable binaries (.exe-files, and .dll-files on DLL-capable platforms)
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin/$<CONFIG>")
