allankrdec/TrinityCore-10-2024

Core/DataStores: Removed hardcoded cap for taxi nodes mask

src/server/game/DataStores/DBCEnums.h
@@ -20,7 +20,7 @@
 
 #include "Define.h"
 #include "EnumFlag.h"
-#include <array>
+#include <vector>
 
 #pragma pack(push, 1)
 
@@ -1579,8 +1579,25 @@ enum class SpellVisualEffectNameType : uint32
     UnitItemRangedIgnoreDisarmed    = 10
 };
 
-#define TaxiMaskSize 345
-typedef std::array<uint8, TaxiMaskSize> TaxiMask;
+class TaxiMask
+{
+public:
+    using value_type = uint8;
+
+    TaxiMask();
+
+    value_type& operator[](size_t i) { return _data[i]; }
+    value_type const& operator[](size_t i) const { return _data[i]; }
+
+    size_t size() const { return _data.size(); }
+    value_type const* data() const { return _data.data(); }
+
+    decltype(auto) begin() { return _data.begin(); }
+    decltype(auto) end() { return _data.end(); }
+
+private:
+    std::vector<value_type> _data;
+};
 
 enum TotemCategoryType
 {

TrinityCore/TrinityCore

(cherry picked from commit 376dc7402a41a03b4c5bc718863c1e0eb410ebec)

src/server/shared/DataStores/DBCEnums.h
@@ -19,7 +19,7 @@
 #define DBCENUMS_H
 
 #include "Define.h"
-#include <array>
+#include <vector>
 
 #pragma pack(push, 1)
 
@@ -433,8 +433,25 @@ enum SummonPropFlags
 #define MAX_PET_TALENT_RANK 3                               // use in calculations, expected <= MAX_TALENT_RANK
 #define MAX_TALENT_TABS 3
 
-static constexpr size_t TaxiMaskSize = 14;
-typedef std::array<uint32, TaxiMaskSize> TaxiMask;
+class TaxiMask
+{
+public:
+    using value_type = uint32;
+
+    TaxiMask();
+
+    value_type& operator[](size_t i) { return _data[i]; }
+    value_type const& operator[](size_t i) const { return _data[i]; }
+
+    size_t size() const { return _data.size(); }
+    value_type const* data() const { return _data.data(); }
+
+    decltype(auto) begin() { return _data.begin(); }
+    decltype(auto) end() { return _data.end(); }
+
+private:
+    std::vector<value_type> _data;
+};
 
 enum TotemCategoryType
 {