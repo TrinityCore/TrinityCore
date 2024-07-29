9242eda6c704ab5c10ab07763e662bb2c65b0396
Shauren <shauren.trinity@gmail.com>
2024-07-29 22:18:22 +0200

    Core/Misc: Reduce amount of virtual functions and copypasting in Reference<> implementations

--- src/server/game/Grids/GridRefManager.h
+++ src/server/game/Grids/GridRefManager.h
@@ -24,15 +24,7 @@ template<class OBJECT>
 class GridReference;
 
 template<class OBJECT>
-class GridRefManager : public RefManager<GridRefManager<OBJECT>, OBJECT>
+class GridRefManager : public RefManager<GridReference<OBJECT>>
 {
-    public:
-        typedef LinkedListHead::Iterator< GridReference<OBJECT> > iterator;
-
-        GridReference<OBJECT>* getFirst() { return (GridReference<OBJECT>*)RefManager<GridRefManager<OBJECT>, OBJECT>::getFirst(); }
-        GridReference<OBJECT>* getLast() { return (GridReference<OBJECT>*)RefManager<GridRefManager<OBJECT>, OBJECT>::getLast(); }
-
-        iterator begin() { return iterator(getFirst()); }
-        iterator end() { return iterator(nullptr); }
 };
 #endif
