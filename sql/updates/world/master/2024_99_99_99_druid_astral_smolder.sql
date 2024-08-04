d899e4d1d446b20a76240fa8bbd4fb0c20a6756d
aquadeus <efectokoleos13@gmail.com>
2024-08-04 18:49:19 +0200

    Fix proc

--- sql/updates/world/master/2024_99_99_99_druid_astral_smolder.sql
+++ sql/updates/world/master/2024_99_99_99_druid_astral_smolder.sql
@@ -1,6 +1,6 @@
 DELETE FROM `spell_proc` WHERE `SpellId` IN (394058);
 INSERT INTO `spell_proc` (`SpellId`,`SchoolMask`,`SpellFamilyName`,`SpellFamilyMask0`,`SpellFamilyMask1`,`SpellFamilyMask2`,`SpellFamilyMask3`,`ProcFlags`,`ProcFlags2`,`SpellTypeMask`,`SpellPhaseMask`,`HitMask`,`AttributesMask`,`DisableEffectsMask`,`ProcsPerMinute`,`Chance`,`Cooldown`,`Charges`) VALUES
-(394058,0x48,7,0x00040005,0x00000000,0x00000000,0x00000000,0x0,0x0,0x1,0x2,0x0,0x2,0x0,0,0,0,0); -- Astral Smolder
+(394058,0x48,7,0x00000005,0x00000000,0x00000000,0x00000000,0x0,0x0,0x1,0x2,0x0,0x2,0x0,0,0,0,0); -- Astral Smolder
 
 DELETE FROM `spell_script_names` WHERE `spell_id`=394058;
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
