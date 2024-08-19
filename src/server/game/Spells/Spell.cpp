2c06e7fcdab677df4e5908f69b9655577e08fe69
Shauren <shauren.trinity@gmail.com>
2024-08-19 12:30:37 +0200

    Core/Spells: Implemented SpellAuraInterruptFlags2::ReleaseEmpower

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -4347,7 +4347,7 @@ void Spell::update(uint32 difftime)
 
             if (IsEmpowerSpell())
             {
-                auto getCompletedEmpowerStages = [&]() -> int32
+                int32 completedStages = [&]() -> int32
                 {
                     Milliseconds passed(m_channeledDuration - m_timer);
                     for (std::size_t i = 0; i < m_empower->StageDurations.size(); ++i)
@@ -4358,9 +4358,8 @@ void Spell::update(uint32 difftime)
                     }
 
                     return m_empower->StageDurations.size();
-                };
+                }();
 
-                int32 completedStages = getCompletedEmpowerStages();
                 if (completedStages != m_empower->CompletedStages)
                 {
                     WorldPackets::Spells::SpellEmpowerSetStage empowerSetStage;
@@ -4380,6 +4379,7 @@ void Spell::update(uint32 difftime)
                     m_empower->IsReleased = true;
                     m_timer = 0;
                     CallScriptEmpowerCompletedHandlers(m_empower->CompletedStages);
+                    m_caster->ToUnit()->RemoveAurasWithInterruptFlags(SpellAuraInterruptFlags2::ReleaseEmpower, m_spellInfo);
                 }
             }
 
