f604661a40d05cb1f06e5e0b3112591ebb3aa1a6
Shauren <shauren.trinity@gmail.com>
2024-09-08 21:59:32 +0200

    Core/Spells: Remove setting OriginalCastId from CastSpellExtraArgs::SetTriggeringAura

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -9668,15 +9668,6 @@ CastSpellExtraArgs& CastSpellExtraArgs::SetTriggeringSpell(Spell const* triggeri
     return *this;
 }
 
-CastSpellExtraArgs& CastSpellExtraArgs::SetTriggeringAura(AuraEffect const* triggeringAura)
-{
-    TriggeringAura = triggeringAura;
-    if (triggeringAura)
-        OriginalCastId = triggeringAura->GetBase()->GetCastId();
-
-    return *this;
-}
-
 SpellCastVisual::operator UF::SpellCastVisual() const
 {
     UF::SpellCastVisual visual;
