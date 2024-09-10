jairoe05/tc_duplicate

Core/Units: Some cleanup in movement packet related functions - try to keep only one function handling a packet, not have it scattered all over the place

src/server/game/Handlers/CharacterHandler.cpp
@@ -1031,7 +1031,7 @@ void WorldSession::HandlePlayerLogin(LoginQueryHolder* holder)
             pCurrChar->CastSpell(pCurrChar, 20584, true, 0);// auras SPELL_AURA_INCREASE_SPEED(+speed in wisp form), SPELL_AURA_INCREASE_SWIM_SPEED(+swim speed in wisp form), SPELL_AURA_TRANSFORM (to wisp form)
         pCurrChar->CastSpell(pCurrChar, 8326, true, 0);     // auras SPELL_AURA_GHOST, SPELL_AURA_INCREASE_SPEED(why?), SPELL_AURA_INCREASE_SWIM_SPEED(why?)
 
-        pCurrChar->SetMovement(MOVE_WATER_WALK);
+        pCurrChar->SendMovementSetWaterWalking(true);
     }
 
     pCurrChar->ContinueTaxiFlight();

TrinityCore/TrinityCore

(cherry picked from commit 075e5d67515bec9650f4f3976880ea5862190b79)

src/server/game/Handlers/CharacterHandler.cpp
@@ -863,7 +863,7 @@ void WorldSession::HandlePlayerLogin(LoginQueryHolder const& holder)
 
     // setting Ghost+speed if dead
     if (pCurrChar->m_deathState == DEAD)
-        pCurrChar->SetMovement(MOVE_WATER_WALK);
+        pCurrChar->SetWaterWalking(true);
 
     pCurrChar->ContinueTaxiFlight();
 