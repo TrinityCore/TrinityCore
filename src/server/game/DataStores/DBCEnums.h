768e63ad1a50b87d2da0a769e509f4bc3660d400
Ovahlord <dreadkiller@gmx.de>
2024-09-14 17:05:51 +0200

    Core/DataStores: updated MAX_LEVEL to 100 as Cata Classic only provides game table data up to level 100 instead of 123

--- src/server/game/DataStores/DBCEnums.h
+++ src/server/game/DataStores/DBCEnums.h
@@ -48,7 +48,7 @@ enum LevelLimit
 
     // client supported max level for player/pets/etc. Avoid overflow or client stability affected.
     // also see GT_MAX_LEVEL define
-    MAX_LEVEL = 123,
+    MAX_LEVEL = 100,
 
     // Server side limitation. Base at used code requirements.
     // also see MAX_LEVEL and GT_MAX_LEVEL define
