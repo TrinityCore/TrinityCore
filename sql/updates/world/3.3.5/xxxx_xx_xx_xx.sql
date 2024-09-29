2d920be46cb6a1d0cac5835c09b73004fafc0e6c
Malcrom <malcromdev@gmail.com>
2024-09-28 21:19:55 -0300

    Update xxxx_xx_xx_xx.sql

--- sql/updates/world/3.3.5/xxxx_xx_xx_xx.sql
+++ sql/updates/world/3.3.5/xxxx_xx_xx_xx.sql
@@ -2,6 +2,6 @@ DELETE FROM `trinity_string` WHERE `entry` = 5089;
 INSERT INTO `trinity_string` (`entry`,`content_default`) VALUES
 (5089, 'Template StringID: %.*s\r\nSpawn StringID: %.*s\r\nScript StringID: %.*s');
 
-ALTER TABLE `creature` ADD `StringId` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL AFTER ScriptName;
+ALTER TABLE `creature` ADD `StringId` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL AFTER `ScriptName`;
 
 ALTER TABLE `creature_template` ADD `StringId` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL AFTER `ScriptName`;
