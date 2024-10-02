7bc13f3307c0b0da65ed13f2085adf6d3dd2fc70
nekolisa42 <141879263+nekolisa42@users.noreply.github.com>
2024-10-02 18:33:06 +0200

    Tools/vmap_assembler: Fix missing <mutex> header in TileAssembler.cpp (#30293)
    (cherry picked from commit 5959e6e374a75d2337c8c8bc1165576c33df3e3b)

--- src/tools/vmap4_assembler/TileAssembler.cpp
+++ src/tools/vmap4_assembler/TileAssembler.cpp
@@ -27,6 +27,7 @@
 #include "VMapDefinitions.h"
 #include <boost/filesystem/directory.hpp>
 #include <boost/filesystem/operations.hpp>
+#include <mutex>
 #include <set>
 
 template<> struct BoundsTrait<VMAP::ModelSpawn*>
