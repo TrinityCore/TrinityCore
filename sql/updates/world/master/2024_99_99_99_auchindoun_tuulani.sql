68b1b58eacec264ba1c3ee11e1d3c6e37b05abfc
ModoX <moardox@gmail.com>
2024-10-18 11:51:12 +0200

    Update 2024_99_99_99_auchindoun_tuulani.sql

--- sql/updates/world/master/2024_99_99_99_auchindoun_tuulani.sql
+++ sql/updates/world/master/2024_99_99_99_auchindoun_tuulani.sql
@@ -5,7 +5,7 @@ UPDATE `creature` SET `StringId`='npc_auchenai_defender_intro' WHERE `guid`= 500
 UPDATE `creature_template` SET `ScriptName` = 'npc_auchindoun_soulbinder_tuulani' WHERE `entry`=79248; -- Soulbinder Tuulani
 UPDATE `creature_template` SET `ScriptName` = 'npc_auchindoun_auchenai_defender' WHERE `entry`=77693; -- Auchenai Defender
 
-UPDATE `instance_template` SET `ScriptName` = 'instance_auchindoun' WHERE `map`=1182;
+UPDATE `instance_template` SET `script` = 'instance_auchindoun' WHERE `map`=1182;
 
 -- Texts
 DELETE FROM `creature_text` WHERE `CreatureID` IN (77693, 79248);
