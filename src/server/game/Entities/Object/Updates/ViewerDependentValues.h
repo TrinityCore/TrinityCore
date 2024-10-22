73ec2a541d1bd0dd2d700d7ecea00fdd996eebfc
Meji <alvaro.megias@outlook.com>
2024-10-22 03:16:42 +0200

    Core/GameObjects: Don't apply GO_DYNFLAG_LO_INTERACT_COND for GAMEOBJECT_TYPE_FLAGSTAND (#30368)

--- src/server/game/Entities/Object/Updates/ViewerDependentValues.h
+++ src/server/game/Entities/Object/Updates/ViewerDependentValues.h
@@ -138,7 +138,8 @@ public:
             {
                 // GO_DYNFLAG_LO_INTERACT_COND should be applied to GOs with conditional interaction (without GO_FLAG_INTERACT_COND) to disable interaction
                 // (Ignore GAMEOBJECT_TYPE_GATHERING_NODE as some profession-related GOs may include quest loot and can always be interacted with)
-                if (gameObject->GetGoType() != GAMEOBJECT_TYPE_GATHERING_NODE)
+                // (Ignore GAMEOBJECT_TYPE_FLAGSTAND as interaction is handled by GO_DYNFLAG_LO_NO_INTERACT)
+                if (gameObject->GetGoType() != GAMEOBJECT_TYPE_FLAGSTAND && gameObject->GetGoType() != GAMEOBJECT_TYPE_GATHERING_NODE)
                     if (gameObject->HasConditionalInteraction() && !gameObject->HasFlag(GO_FLAG_INTERACT_COND))
                         dynFlags |= GO_DYNFLAG_LO_INTERACT_COND;
 
