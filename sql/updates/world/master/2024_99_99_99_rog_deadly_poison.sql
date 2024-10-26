7b1a308e126467a8fe6eded6837e1b0ec8e452e7
aquadeus <efectokoleos13@gmail.com>
2024-10-26 20:21:39 +0200

    Not needed

--- sql/updates/world/master/2024_99_99_99_rog_deadly_poison.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM `spell_script_names` WHERE `spell_id`=2818 AND `ScriptName`='spell_rog_deadly_poison';
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
-(2818, 'spell_rog_deadly_poison');
