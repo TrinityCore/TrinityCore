98d1505e3301f0c7ee006f4643ce9b218ead9d10
Meji <alvaro.megias@outlook.com>
2024-11-05 14:42:35 +0100

    Core/Summons: Handle summons using SummonProperties.Control type 5 as vehicles (#30391)
    (cherry picked from commit 23bb1a56b74191e131e41ad25edbe40ee4e76a96)

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -1894,7 +1894,6 @@ void Spell::EffectSummonType()
     {
         case SUMMON_CATEGORY_WILD:
         case SUMMON_CATEGORY_ALLY:
-        case SUMMON_CATEGORY_UNK:
         {
             if (properties->GetFlags().HasFlag(SummonPropertiesFlags::JoinSummonerSpawnGroup))
             {
@@ -1998,6 +1997,7 @@ void Spell::EffectSummonType()
             summon = unitCaster->GetMap()->SummonCreature(entry, *destTarget, properties, duration, unitCaster, m_spellInfo->Id, 0, privateObjectOwner);
             break;
         }
+        case SUMMON_CATEGORY_POSSESSED_VEHICLE:
         case SUMMON_CATEGORY_VEHICLE:
         {
             if (!unitCaster)
