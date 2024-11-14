eb48fea43ad4e9df8da7a58e0d2abb428de1acb7
Aokromes <Aokromes@users.noreply.github.com>
2024-11-14 07:51:47 +0100

    DB/Quest: Update Cloth Scavenging Completion (RewardText)
    closes #30438 by CraftedRO
    
    (cherry picked from commit a72de0c32650e23d7e4e6190caa6136a92c47e63)

---
+++ sql/updates/world/master/2026_06_06_11_world_2024_11_14_00_world.sql
@@ -0,0 +1,6 @@
+-- Cloth Scavenging - Dalaran (13272)
+UPDATE `quest_offer_reward` SET `RewardText`="I think you will find this skill helps you a great deal on your path to tailoring greatness!" WHERE `ID`=13272;
+-- Cloth Scavenging - Valiance Keep (13265)
+UPDATE `quest_offer_reward` SET `RewardText`="Turn that extra cloth you find into something that looks marvelous!" WHERE `ID`=13265;
+-- Cloth Scavenging - Warsong Hold (13270)
+UPDATE `quest_offer_reward` SET `RewardText`="I be appreciatin' your business." WHERE `ID`=13270;
