3860471879c56a1b5cf5e8c34e9202eb9904068e
Ovahlord <dreadkiller@gmx.de>
2024-11-17 16:47:35 +0100

    DB/Creatures: relocate the spawns which have been moved by the retail trading post back to where they belong

---
+++ sql/updates/world/cata_classic/2024_11_17_06_world.sql
@@ -0,0 +1,4 @@
+UPDATE `creature` SET `position_x`= -8828.627, `position_y`= 844.3177, `position_z`= 99.63461, `orientation`= 5.829399585723876953 WHERE `guid`= 314020;
+UPDATE `creature` SET `position_x`= -8828.326, `position_y`= 846.7083, `position_z`= 99.71476, `orientation`= 0.575958669185638427 WHERE `guid`= 313882;
+UPDATE `creature` SET `position_x`= -8834.219, `position_y`= 825.97923, `position_z`= 99.17755, `orientation`= 0.453785598278045654 WHERE `guid`= 314026;
+UPDATE `gameobject` SET `position_x`= -8829.564, `position_y`= 844.1094, `position_z`= 100.90334, `orientation`= 2.4958167, `rotation0`= 0.0068912506, `rotation1`= -0.005352974, `rotation2`= 0.9482794, `rotation3`= 0.31731707 WHERE `guid`= 220051;
