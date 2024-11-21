fade8be3b133a58bfba4cf59e73f432aaa3444f4
Shauren <shauren.trinity@gmail.com>
2024-11-21 22:42:07 +0100

    Core/PacketIO: Rename more unknown packet fields

--- src/server/game/Server/Packets/SpellPackets.h
+++ src/server/game/Server/Packets/SpellPackets.h
@@ -399,8 +399,8 @@ namespace WorldPackets
         struct LearnedSpellInfo
         {
             int32 SpellID = 0;
-            bool IsFavorite = false;
-            Optional<int32> field_8;
+            bool Favorite = false;
+            Optional<int32> EquipableSpellInvSlot;
             Optional<int32> Superceded;
             Optional<int32> TraitDefinitionID;
         };
@@ -596,8 +596,8 @@ namespace WorldPackets
             int32 CategoryRecoveryTime = 0;
             float ModRate = 1.0f;
             bool OnHold = false;
-            Optional<uint32> unused622_1; ///< This field is not used for anything in the client in 6.2.2.20444
-            Optional<uint32> unused622_2; ///< This field is not used for anything in the client in 6.2.2.20444
+            Optional<int32> RecoveryTimeStartOffset;
+            Optional<int32> CategoryRecoveryTimeStartOffset;
         };
 
         class SendSpellHistory final : public ServerPacket
