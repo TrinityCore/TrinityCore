99b4f659a4708b31c46ad64f8b8e4b1f2aa9df1a
Meji <alvaro.megias@outlook.com>
2024-11-23 17:09:43 +0100

    Fix typo

--- sql/updates/world/master/2024_99_99_99_world.sql
+++ sql/updates/world/master/2024_99_99_99_world.sql
@@ -30,14 +30,14 @@ INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `Diffic
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=30 AND `SourceEntry` IN (7045, 7046);
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=-@CONDREFID;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `ConditionStringValue1`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
-(30,0,7045,0,0,6,0,469,0,0,'',0,0,0,'',' Areatrigger 7045 triggers if player is Alliance'),
+(30,0,7045,0,0,6,0,469,0,0,'',0,0,0,'','Areatrigger 7045 triggers if player is Alliance'),
 (30,0,7045,0,0,-@CONDREFID,0,0,0,0,'',0,0,0,'','Areatrigger 7045 triggers if condition reference 40004 is fulfilled'),
-(30,0,7046,0,0,6,0,469,0,0,'',0,0,0,'',' Areatrigger 7046 triggers if player is Alliance'),
+(30,0,7046,0,0,6,0,469,0,0,'',0,0,0,'','Areatrigger 7046 triggers if player is Alliance'),
 (30,0,7046,0,0,-@CONDREFID,0,0,0,0,'',0,0,0,'','Areatrigger 7046 triggers if condition reference 40004 is fulfilled'),
-(-@CONDREFID,0,0,0,1,47,0,29547,8,0,'',0,0,0,'','Apply condition reference if quest 29547 is taken'),
-(-@CONDREFID,0,0,0,2,47,0,49556,8,0,'',0,0,0,'','Apply condition reference if quest 49556 is taken'),
-(-@CONDREFID,0,0,0,3,47,0,49866,8,0,'',0,0,0,'','Apply condition reference if quest 49866 is taken'),
-(-@CONDREFID,0,0,0,4,47,0,60125,8,0,'',0,0,0,'','Apply condition reference if quest 60125 is taken');
+(-@CONDREFID,0,0,0,0,47,0,29547,8,0,'',0,0,0,'','Apply condition reference if quest 29547 is taken'),
+(-@CONDREFID,0,0,0,1,47,0,49556,8,0,'',0,0,0,'','Apply condition reference if quest 49556 is taken'),
+(-@CONDREFID,0,0,0,2,47,0,49866,8,0,'',0,0,0,'','Apply condition reference if quest 49866 is taken'),
+(-@CONDREFID,0,0,0,3,47,0,60125,8,0,'',0,0,0,'','Apply condition reference if quest 60125 is taken');
 
 UPDATE `conditions` SET `Comment`='Areatrigger 8463 triggers if player is Horde' WHERE `SourceTypeOrReferenceId`=30 AND `SourceEntry`=8463 AND `ConditionTypeOrReference`=6;
 UPDATE `conditions` SET `Comment`='Areatrigger 8463 triggers if condition reference 40001 is fulfilled' WHERE `SourceTypeOrReferenceId`=30 AND `SourceEntry`=8463 AND `ConditionTypeOrReference`=-40001;
