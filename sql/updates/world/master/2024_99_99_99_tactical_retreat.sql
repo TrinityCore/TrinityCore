6e93684d5b3febf6f6e703934bf4e964d35cc930
aquadeus <efectokoleos13@gmail.com>
2024-11-29 21:45:54 +0100

    Spells/DH: Implement talent "Tactical Retreat"

---
+++ sql/updates/world/master/2024_99_99_99_tactical_retreat.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_script_names` WHERE `spell_id`=198793 AND `ScriptName`='spell_dh_tactical_retreat';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(198793, 'spell_dh_tactical_retreat');
