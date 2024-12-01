719274d33601af5699e5264647a7487e55911d8b
Shauren <shauren.trinity@gmail.com>
2024-12-01 13:14:12 +0100

    prepare merge

--- sql/updates/world/master/2024_12_01_01_world.sql
+++ sql/updates/world/master/2024_12_01_01_world.sql
@@ -1,3 +1,3 @@
-DELETE FROM `spell_script_names` WHERE `spell_id`=198013 AND `ScriptName`='spell_dh_eye_beam';
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_dh_eye_beam';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (198013, 'spell_dh_eye_beam');
