1942647c0d0229aca96602f3617365bd477e94de
Shauren <shauren.trinity@gmail.com>
2024-12-09 21:13:18 +0100

    Dep: Replace basic_deadline_timer with std::chrono based basic_waitable_timer
    (cherry picked from commit c81183a6600722f3a9bb4996c0849b530fbdd1b0)

--- src/common/Metric/Metric.cpp
+++ src/common/Metric/Metric.cpp
@@ -220,7 +220,7 @@ void Metric::ScheduleSend()
 {
     if (_enabled)
     {
-        _batchTimer->expires_from_now(boost::posix_time::seconds(_updateInterval));
+        _batchTimer->expires_after(std::chrono::seconds(_updateInterval));
         _batchTimer->async_wait(std::bind(&Metric::SendBatch, this));
     }
     else
@@ -250,7 +250,7 @@ void Metric::ScheduleOverallStatusLog()
 {
     if (_enabled)
     {
-        _overallStatusTimer->expires_from_now(boost::posix_time::seconds(_overallStatusTimerInterval));
+        _overallStatusTimer->expires_after(std::chrono::seconds(_overallStatusTimerInterval));
         _overallStatusTimer->async_wait([this](const boost::system::error_code&)
         {
             _overallStatusTimerTriggered = true;
