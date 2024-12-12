c5a076e5b5567647cc4e9876b617c1ca03920b92
aquadeus <efectokoleos13@gmail.com>
2024-12-12 20:27:58 +0100

    Scripts/Spells: Fix demon hunter talent "Collective Anguish"

---
+++ sql/updates/world/master/2024_99_99_99_dh_collective_anguish.sql
@@ -0,0 +1,5 @@
+DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_dh_collective_anguish', 'spell_dh_collective_anguish_eye_beam');
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(212084, 'spell_dh_collective_anguish'),
+(198013, 'spell_dh_collective_anguish'),
+(391057, 'spell_dh_collective_anguish_eye_beam');
