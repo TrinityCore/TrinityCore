9ed5f8812b58d1d99d81fbb3df38001206d3f6ed
Shauren <shauren.trinity@gmail.com>
2024-12-19 14:55:40 +0100

    Core/PacketIO: Remove overriding Underlying type from ByteBuffer reads in favor of PacketUtilities::As

--- src/server/game/Server/Packets/MiscPackets.cpp
+++ src/server/game/Server/Packets/MiscPackets.cpp
@@ -771,7 +771,7 @@ WorldPacket const* WorldPackets::Misc::StartTimer::Write()
 
 void WorldPackets::Misc::QueryCountdownTimer::Read()
 {
-    TimerType = _worldPacket.read<CountdownTimerType, int32>();
+    _worldPacket >> As<int32>(TimerType);
 }
 
 void WorldPackets::Misc::ConversationLineStarted::Read()
