38a5956b8544ee43ec4faeba42a40dbee4dcc65f
Shauren <shauren.trinity@gmail.com>
2024-12-22 14:22:39 +0100

    Scripts/Spells: Added missing Throw Glaive part to Cycle of Hatred implementation

--- sql/updates/world/master/2024_12_22_11_world.sql
+++ sql/updates/world/master/2024_12_22_11_world.sql
@@ -2,4 +2,5 @@ DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_dh_cycle_of_hatred';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (188499, 'spell_dh_cycle_of_hatred'),
 (162794, 'spell_dh_cycle_of_hatred'),
+(185123, 'spell_dh_cycle_of_hatred'),
 (342817, 'spell_dh_cycle_of_hatred');
