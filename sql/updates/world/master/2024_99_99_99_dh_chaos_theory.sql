139c13bf799c7c5afe5b9e076197caea92afdba7
aquadeus <efectokoleos13@gmail.com>
2024-12-27 21:57:20 +0100

    Scripts/Spells: Implement demon hunter talent "Chaos Theory"

---
+++ sql/updates/world/master/2024_99_99_99_dh_chaos_theory.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_dh_chaos_theory';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(210152, 'spell_dh_chaos_theory'),
+(188499, 'spell_dh_chaos_theory');
