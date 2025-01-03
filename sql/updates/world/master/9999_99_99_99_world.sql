f92334f181dde9bf99bb9347d33903d5fd8718dc
Osleyder <osleyder.gonzalez@gmail.com>
2025-01-03 13:12:23 -0500

    Fixed: missing space between Valley and of.
    Signed-off-by: Osleyder <osleyder.gonzalez@gmail.com>

--- sql/updates/world/master/9999_99_99_99_world.sql
+++ sql/updates/world/master/9999_99_99_99_world.sql
@@ -1,7 +1,7 @@
 
 DELETE FROM `graveyard_zone` WHERE `ID`=709;
 INSERT INTO `graveyard_zone` (`ID`, `GhostZone`, `Comment`) VALUES
-(709, 363, 'Kalimdor, Valley of Trials GY - Valleyof Trials Start'),
+(709, 363, 'Kalimdor, Valley of Trials GY - Valley of Trials Start'),
 (709, 364, 'Kalimdor, Valley of Trials GY - The Den'),
 (709, 365, 'Kalimdor, Valley of Trials GY - Burning Blade Coven'),
 (709, 638, 'Kalimdor, Valley of Trials GY - Hidden Path'),
