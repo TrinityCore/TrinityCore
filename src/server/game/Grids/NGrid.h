04433910424540e42a2270156e7c9a8722c09913
Shauren <shauren.trinity@gmail.com>
2025-01-05 18:00:15 +0100

    Core/Grids: Add missing changes to commit e245e8ad124cee19d35e4f67f7aed4dcc871b3bf

--- src/server/game/Grids/NGrid.h
+++ src/server/game/Grids/NGrid.h
@@ -132,8 +132,16 @@ class NGrid
         */
 
         // Visit all Grids (cells) in NGrid (grid)
-        template<class T, class TT>
-        void VisitAllGrids(TypeContainerVisitor<T, TypeMapContainer<TT> > &visitor)
+        template<class VISITOR>
+        void VisitAllGrids(TypeContainerVisitor<VISITOR, WORLD_OBJECT_CONTAINER>& visitor)
+        {
+            for (uint32 x = 0; x < N; ++x)
+                for (uint32 y = 0; y < N; ++y)
+                    GetGridType(x, y).Visit(visitor);
+        }
+
+        template<class VISITOR>
+        void VisitAllGrids(TypeContainerVisitor<VISITOR, GRID_OBJECT_CONTAINER>& visitor)
         {
             for (uint32 x = 0; x < N; ++x)
                 for (uint32 y = 0; y < N; ++y)
@@ -141,8 +149,14 @@ class NGrid
         }
 
         // Visit a single Grid (cell) in NGrid (grid)
-        template<class T, class TT>
-        void VisitGrid(const uint32 x, const uint32 y, TypeContainerVisitor<T, TypeMapContainer<TT> > &visitor)
+        template<class VISITOR>
+        void VisitGrid(uint32 x, uint32 y, TypeContainerVisitor<VISITOR, WORLD_OBJECT_CONTAINER>& visitor)
+        {
+            GetGridType(x, y).Visit(visitor);
+        }
+
+        template<class VISITOR>
+        void VisitGrid(uint32 x, uint32 y, TypeContainerVisitor<VISITOR, GRID_OBJECT_CONTAINER>& visitor)
         {
             GetGridType(x, y).Visit(visitor);
         }
