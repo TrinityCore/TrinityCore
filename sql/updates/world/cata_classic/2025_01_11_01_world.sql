a940ae963def816e1530067f7e332cd6ef2bc9df
Ovahlord <dreadkiller@gmx.de>
2025-01-11 20:41:26 +0100

    DB/Spells: fixed Glyph of Howling Blast

---
+++ sql/updates/world/cata_classic/2025_01_11_01_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_proc` WHERE `SpellId`= 63335;
+INSERT INTO `spell_proc` (`SpellId`, `SpellFamilyName`, `SpellFamilyMask1`, `SpellTypeMask`, `SpellPhaseMask`) VALUES
+(63335, 15, 0x2, 0x1, 0x2);
