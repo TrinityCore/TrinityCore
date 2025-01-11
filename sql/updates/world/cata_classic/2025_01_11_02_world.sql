a8ceccebf9ce26ed501002bf0213d7f58f187ef7
Ovahlord <dreadkiller@gmx.de>
2025-01-11 21:05:04 +0100

    DB/Spells: fixed Chill of the Grave

---
+++ sql/updates/world/cata_classic/2025_01_11_02_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_proc` WHERE `SpellId`= -49149;
+INSERT INTO `spell_proc` (`SpellId`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellTypeMask`, `SpellPhaseMask`) VALUES
+(-49149, 15, 0x2 | 0x4, 0x2 | 0x00020000, 0x1, 0x1);
