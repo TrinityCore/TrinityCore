f6360849cecde354224d0ffc128ea39b2747c35e
Shauren <shauren.trinity@gmail.com>
2025-01-17 12:37:33 +0100

    prepare merge

--- sql/updates/world/master/2025_01_17_03_world.sql
+++ sql/updates/world/master/2025_01_17_03_world.sql
@@ -4,4 +4,4 @@ INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 
 DELETE FROM `spell_proc` WHERE `SpellId` IN (444931);
 INSERT INTO `spell_proc` (`SpellId`,`SchoolMask`,`SpellFamilyName`,`SpellFamilyMask0`,`SpellFamilyMask1`,`SpellFamilyMask2`,`SpellFamilyMask3`,`ProcFlags`,`ProcFlags2`,`SpellTypeMask`,`SpellPhaseMask`,`HitMask`,`AttributesMask`,`DisableEffectsMask`,`ProcsPerMinute`,`Chance`,`Cooldown`,`Charges`) VALUES
-(444931,0x00,107,0x00000008,0x00000000,0x00000000,0x00000000,0x0,0x0,0x4,0x1,0x0,0x0,0x0,0,0,0,0); -- Unhindered Assault
+(444931,0x00,107,0x00000008,0x00000000,0x00000000,0x00000000,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0,0,0,0); -- Unhindered Assault
