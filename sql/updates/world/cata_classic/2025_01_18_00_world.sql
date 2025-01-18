32bd6e58f7114a1b38fd889a530584c735b7c8f4
Ovahlord <dreadkiller@gmx.de>
2025-01-18 23:22:04 +0100

    DB/Creatures: corrected armor and attack power values for UNIT_CLASS_WARRIOR creatures between level 1 and 5

---
+++ sql/updates/world/cata_classic/2025_01_18_00_world.sql
@@ -0,0 +1,5 @@
+UPDATE `creature_classlevelstats` SET `basearmor`= 0, `attackpower`= 24, `rangedattackpower`= 0, `comment`= 'Sniff validated in 4.4.1' WHERE `level`= 1 AND `class`= 1;
+UPDATE `creature_classlevelstats` SET `basearmor`= 0, `attackpower`= 26, `rangedattackpower`= 0, `comment`= 'Sniff validated in 4.4.1' WHERE `level`= 2 AND `class`= 1;
+UPDATE `creature_classlevelstats` SET `basearmor`= 25, `attackpower`= 28, `rangedattackpower`= 0, `comment`= 'Sniff validated in 4.4.1' WHERE `level`= 3 AND `class`= 1;
+UPDATE `creature_classlevelstats` SET `basearmor`= 60, `attackpower`= 30, `rangedattackpower`= 0, `comment`= 'Sniff validated in 4.4.1' WHERE `level`= 4 AND `class`= 1;
+UPDATE `creature_classlevelstats` SET `basearmor`= 103, `attackpower`= 32, `rangedattackpower`= 0, `comment`= 'Sniff validated in 4.4.1' WHERE `level`= 5 AND `class`= 1;
