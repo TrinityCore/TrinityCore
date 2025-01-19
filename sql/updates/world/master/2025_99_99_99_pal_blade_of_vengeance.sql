6a58001876f99e88ea13bdd15365afbdf16cfb39
aquadeus <efectokoleos13@gmail.com>
2025-01-19 13:15:38 +0100

    Scripts/Spells: Implement paladin talent "Blade of Vengeance"

---
+++ sql/updates/world/master/2025_99_99_99_pal_blade_of_vengeance.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_pal_blade_of_vengeance';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(184575, 'spell_pal_blade_of_vengeance');
