1c04f09609f09bed912e68e9a6d50074ec53861b
Shauren <shauren.trinity@gmail.com>
2025-01-20 22:49:52 +0100

    Scripts/Spells: Use Spell::GetPowerTypeCostAmount where possible instead of iterating Spell::GetPowerCost
    (cherry picked from commit 26376d89e165aece42e58213632ef43ecb0d81b3)
    
    # Conflicts:
    #	src/server/scripts/Spells/spell_druid.cpp
    #	src/server/scripts/Spells/spell_rogue.cpp
    #	src/server/scripts/Spells/spell_shaman.cpp

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -534,8 +534,7 @@ bool SpellMgr::CanSpellTriggerProcOnEvent(SpellProcEntry const& procEntry, ProcE
             return false;
 
         std::vector<SpellPowerCost> const& costs = eventInfo.GetProcSpell()->GetPowerCost();
-        auto m = std::find_if(costs.begin(), costs.end(), [](SpellPowerCost const& cost) { return cost.Amount > 0; });
-        if (m == costs.end())
+        if (std::ranges::none_of(costs, [](SpellPowerCost const& cost) { return cost.Amount > 0; }))
             return false;
     }
 
