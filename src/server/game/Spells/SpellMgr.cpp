b78bdd0fa1de39953e4632c2df4e538d5833378b
aquadeus <efectokoleos13@gmail.com>
2025-01-22 00:57:53 +0100

    Core/Spells: Remove hacks related to TARGET_DEST_NEARBY_DB and handle by DB

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -4983,24 +4983,6 @@ void SpellMgr::LoadSpellInfoCorrections()
         });
     });
 
-    // Summon Amberleaf Troublemaker
-    ApplySpellFix({ 114698 }, [](SpellInfo* spellInfo)
-    {
-        ApplySpellEffectFix(spellInfo, EFFECT_0, [](SpellEffectInfo* spellEffectInfo)
-        {
-            spellEffectInfo->TargetA = SpellImplicitTargetInfo(TARGET_DEST_DEST);
-        });
-    });
-
-    // Summon Living Air
-    ApplySpellFix({ 102207 }, [](SpellInfo* spellInfo)
-    {
-        ApplySpellEffectFix(spellInfo, EFFECT_0, [](SpellEffectInfo* spellEffectInfo)
-        {
-            spellEffectInfo->TargetA = SpellImplicitTargetInfo(TARGET_DEST_TARGET_RANDOM);
-        });
-    });
-
     // ENDOF THE WANDERING ISLE SPELLS
     //
 
