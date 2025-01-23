1f434a953ec6c0eae137513db02a4e861478dd85
Ovahlord <dreadkiller@gmx.de>
2025-01-23 12:21:13 +0100

    Core/Database: Implement accessing query result fields by name

--- src/server/database/Database/QueryResult.cpp
+++ src/server/database/Database/QueryResult.cpp
@@ -400,10 +400,13 @@ _result(result),
 _fields(fields)
 {
     _fieldMetadata.resize(_fieldCount);
+    _fieldIndexByAlias.reserve(_fieldCount);
     _currentRow = new Field[_fieldCount];
     for (uint32 i = 0; i < _fieldCount; i++)
     {
         InitializeDatabaseFieldMetadata(&_fieldMetadata[i], &_fields[i], i, false);
+        bool success = _fieldIndexByAlias.try_emplace(_fieldMetadata[i].Alias, i).second;
+        ASSERT(success);
         _currentRow[i].SetMetadata(&_fieldMetadata[i]);
     }
 }
@@ -452,6 +455,7 @@ m_metadataResult(result)
     //- This is where we prepare the buffer based on metadata
     MySQLField* field = reinterpret_cast<MySQLField*>(mysql_fetch_fields(m_metadataResult));
     m_fieldMetadata.resize(m_fieldCount);
+    m_fieldIndexByAlias.reserve(m_fieldCount);
     std::size_t rowSize = 0;
     for (uint32 i = 0; i < m_fieldCount; ++i)
     {
@@ -459,6 +463,8 @@ m_metadataResult(result)
         rowSize += size;
 
         InitializeDatabaseFieldMetadata(&m_fieldMetadata[i], &field[i], i, true);
+        bool success = m_fieldIndexByAlias.try_emplace(m_fieldMetadata[i].Alias, i).second;
+        ASSERT(success);
 
         m_rBind[i].buffer_type = field[i].type;
         m_rBind[i].buffer_length = size;
@@ -631,6 +637,13 @@ Field const& ResultSet::operator[](std::size_t index) const
     return _currentRow[index];
 }
 
+Field const& ResultSet::operator[](std::string_view const& alias) const
+{
+    auto itr = _fieldIndexByAlias.find(alias);
+    ASSERT(itr != _fieldIndexByAlias.end());
+    return _currentRow[itr->second];
+}
+
 QueryResultFieldMetadata const& ResultSet::GetFieldMetadata(std::size_t index) const
 {
     ASSERT(index < std::size_t(_fieldCount));
@@ -648,6 +661,14 @@ Field const& PreparedResultSet::operator[](std::size_t index) const
     ASSERT(m_rowPosition < m_rowCount);
     ASSERT(index < std::size_t(m_fieldCount));
     return m_rows[std::size_t(m_rowPosition) * m_fieldCount + index];
+}
+
+Field const& PreparedResultSet::operator[](std::string_view const& alias) const
+{
+    ASSERT(m_rowPosition < m_rowCount);
+    auto itr = m_fieldIndexByAlias.find(alias);
+    ASSERT(itr != m_fieldIndexByAlias.end());
+    return m_rows[std::size_t(m_rowPosition) * m_fieldCount + itr->second];
 }
 
 QueryResultFieldMetadata const& PreparedResultSet::GetFieldMetadata(std::size_t index) const
