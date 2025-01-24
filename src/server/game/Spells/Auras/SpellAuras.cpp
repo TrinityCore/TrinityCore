bc8874f30597a996eb30b7b278bcc71b2f082f10
Shauren <shauren.trinity@gmail.com>
2025-01-24 18:40:25 +0100

    Core/Objects: Removed type casts before accessing WorldObject::m_Events that became unneccessary after moving it from Unit to WorldObject

--- src/server/game/Spells/Auras/SpellAuras.cpp
+++ src/server/game/Spells/Auras/SpellAuras.cpp
@@ -1025,13 +1025,9 @@ void Aura::DropChargeDelayed(uint32 delay, AuraRemoveMode removeMode)
     // aura is already during delayed charge drop
     if (m_dropEvent)
         return;
-    // only units have events
-    Unit* owner = m_owner->ToUnit();
-    if (!owner)
-        return;
 
     m_dropEvent = new ChargeDropEvent(this, removeMode);
-    owner->m_Events.AddEvent(m_dropEvent, owner->m_Events.CalculateTime(Milliseconds(delay)));
+    m_owner->m_Events.AddEventAtOffset(m_dropEvent, Milliseconds(delay));
 }
 
 void Aura::SetStackAmount(uint8 stackAmount)
