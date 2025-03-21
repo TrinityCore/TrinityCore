333f2ebd17ca86cb1a2eb17106c6fee64e9a8d0c
-- <lol31283@gmail.com>
2025-03-21 22:31:33 +0100

    Core/SAI: Allow SMART_ACTION_OFFER_QUEST to offer quest through player menu

--- src/server/game/AI/SmartScripts/SmartScript.cpp
+++ src/server/game/AI/SmartScripts/SmartScript.cpp
@@ -501,7 +501,14 @@ void SmartScript::ProcessAction(SmartScriptHolder& e, Unit* unit, uint32 var0, u
                                 if (WorldSession* session = player->GetSession())
                                 {
                                     PlayerMenu menu(session);
-                                    menu.SendQuestGiverQuestDetails(q, me->GetGUID(), true, false);
+
+                                    if (e.action.questOffer.playerMenu)
+                                    {
+                                        player->SetQuestSharingInfo(player->GetGUID(), e.action.questOffer.questID);
+                                        player->SetSelfOfferQuest(e.action.questOffer.questID);
+                                    }
+
+                                    menu.SendQuestGiverQuestDetails(q, e.action.questOffer.playerMenu ? player->GetGUID() : me->GetGUID(), true, false);
                                     TC_LOG_DEBUG("scripts.ai", "SmartScript::ProcessAction:: SMART_ACTION_OFFER_QUEST: Player {} - offering quest {}", player->GetGUID().ToString(), e.action.questOffer.questID);
                                 }
                             }
