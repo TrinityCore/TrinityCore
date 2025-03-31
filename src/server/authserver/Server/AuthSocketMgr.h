32a526fa28dd4fc95aa8abe1277bea4199f2ffeb
nexrail <nexrail@protonmail.com>
2025-03-31 15:39:03 +0200

    Proxy Protocol v2 support

--- src/server/authserver/Server/AuthSocketMgr.h
+++ src/server/authserver/Server/AuthSocketMgr.h
@@ -44,7 +44,11 @@ public:
 protected:
     NetworkThread<AuthSession>* CreateThreads() const override
     {
-        return new NetworkThread<AuthSession>[1];
+        NetworkThread<AuthSession>* threads = new NetworkThread<AuthSession>[1];
+        bool proxyProtocolEnabled = sConfigMgr->GetOption<bool>("EnableProxyProtocol", false, true);
+        if (proxyProtocolEnabled)
+            threads[0].EnableProxyProtocol();
+        return threads;
     }
 
     static void OnSocketAccept(tcp::socket&& sock, uint32 threadIndex)
