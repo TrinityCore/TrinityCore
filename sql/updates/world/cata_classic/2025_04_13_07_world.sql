1be6ce0da74160bd1032daa8646f7c9110576bb2
Ovahlord <dreadkiller@gmx.de>
2025-04-13 19:17:38 +0200

    DB/Creatures: updated level values of the Molten Front recruiters and the Twilight Protocol Wyrmrest Defender

---
+++ sql/updates/world/cata_classic/2025_04_13_07_world.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_template_difficulty` SET `MinLevel`=85, `MaxLevel`=85, `VerifiedBuild`=60192 WHERE (`Entry`=239945 AND `DifficultyID`=0); -- 239945 (Alania Hollowbark) - 
+UPDATE `creature_template_difficulty` SET `MinLevel`=85, `MaxLevel`=85, `VerifiedBuild`=60192 WHERE (`Entry`=239974 AND `DifficultyID`=0); -- 239974 (Denaa Hollowbark) - 
+UPDATE `creature_template_difficulty` SET `MinLevel`=87, `MaxLevel`=87 WHERE (`Entry`=239974 AND `DifficultyID`=0); -- 240309 (Wyrmrest Defender) -
