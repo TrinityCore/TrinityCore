e10077e7c450c90a314cb374e40848b7f41d87a5
Ovahlord <dreadkiller@gmx.de>
2025-04-16 20:11:04 +0200

    DB/GameObjects: added gameobject_template_addon entries for new gameobjects

---
+++ sql/updates/world/cata_classic/2025_04_16_07_world.sql
@@ -0,0 +1,32 @@
+DELETE FROM `gameobject_template_addon` WHERE `entry` IN (214516 /*Instance Portal (Party + Heroic)*/, 381415 /*Instance Portal LKC (Party + Heroic)*/, 214539 /*Instance Portal (Raid 4 Difficulties)*/, 412239 /*Death Knight Shield (4.5x)*/, 412092 /*Scourge Crate 02 (1.5x)*/, 411665 /*Scourge Chest*/, 411671 /*Scourge Weaponrack DK*/, 412232 /*Argent Shield (4x)*/, 412083 /*Scourge Crate 01 (1.5x)*/, 412228 /*Argent Crusade Banner 02 (12x)*/, 412226 /*Argent Crusade Banner 02 (7x)*/, 411924 /*Argent Crusade Tent (1.3x)*/, 411923 /*Argent Crusade Banner (1.75x)*/, 411570 /*Argent Crusade Battering Ram*/, 411574 /*Argent Crusade Battering Ram*/, 411733 /*Argent Crusade Banner(3x)*/, 411928 /*Ebon Blade Banner 01 (2X)*/, 411724 /*Ebon Blade Banner 01 (3X)*/, 411927 /*Scourge Tent (1.15x)*/, 381400 /*Instance Portal ToC Dungeon (Party + Heroic)*/, 214525 /*Instance Portal (Raid 4 Difficulties)*/, 214524 /*Instance Portal (Raid 10/25 No Heroic)*/, 214517 /*Instance Portal (Party + Heroic)*/, 214502 /*Instance Portal (Party + Heroic)*/, 381417 /*Instance Portal LKC (Party + Heroic)*/, 381416 /*Instance Portal LKC (Party + Heroic)*/, 381414 /*Instance Portal LKC (Party + Heroic)*/, 214523 /*Instance Portal (Raid 10/25 No Heroic)*/, 214536 /*Instance Portal (Raid 10/25 No Heroic)*/, 214441 /*Instance Portal (Raid 4 Difficulties)*/);
+INSERT INTO `gameobject_template_addon` (`entry`, `faction`, `flags`, `WorldEffectID`, `AIAnimKitID`) VALUES
+(214516, 35, 0x2000, 0, 0), -- Instance Portal (Party + Heroic)
+(381415, 35, 0x2000, 0, 0), -- Instance Portal LKC (Party + Heroic)
+(214539, 35, 0x2000, 0, 0), -- Instance Portal (Raid 4 Difficulties)
+(412239, 0, 0x2000, 0, 0), -- Death Knight Shield (4.5x)
+(412092, 0, 0x2000, 0, 0), -- Scourge Crate 02 (1.5x)
+(411665, 0, 0x2000, 0, 0), -- Scourge Chest
+(411671, 0, 0x2000, 0, 0), -- Scourge Weaponrack DK
+(412232, 0, 0x2000, 0, 0), -- Argent Shield (4x)
+(412083, 0, 0x2000, 0, 0), -- Scourge Crate 01 (1.5x)
+(412228, 0, 0x20, 0, 0), -- Argent Crusade Banner 02 (12x)
+(412226, 0, 0x20, 0, 0), -- Argent Crusade Banner 02 (7x)
+(411924, 0, 0x20, 0, 0), -- Argent Crusade Tent (1.3x)
+(411923, 0, 0x20, 0, 0), -- Argent Crusade Banner (1.75x)
+(411570, 0, 0x20, 0, 0), -- Argent Crusade Battering Ram
+(411574, 0, 0x20, 0, 0), -- Argent Crusade Battering Ram
+(411733, 0, 0x20, 0, 0), -- Argent Crusade Banner(3x)
+(411928, 0, 0x20, 0, 0), -- Ebon Blade Banner 01 (2X)
+(411724, 0, 0x20, 0, 0), -- Ebon Blade Banner 01 (3X)
+(411927, 0, 0x20, 0, 0), -- Scourge Tent (1.15x)
+(381400, 35, 0x2000, 0, 0), -- Instance Portal ToC Dungeon (Party + Heroic)
+(214525, 35, 0x2000, 0, 0), -- Instance Portal (Raid 4 Difficulties)
+(214524, 35, 0x2000, 0, 0), -- Instance Portal (Raid 10/25 No Heroic)
+(214517, 35, 0x2000, 0, 0), -- Instance Portal (Party + Heroic)
+(214502, 35, 0x2000, 0, 0), -- Instance Portal (Party + Heroic)
+(381417, 35, 0x2000, 0, 0), -- Instance Portal LKC (Party + Heroic)
+(381416, 35, 0x2000, 0, 0), -- Instance Portal LKC (Party + Heroic)
+(381414, 35, 0x2000, 0, 0), -- Instance Portal LKC (Party + Heroic)
+(214523, 35, 0x2000, 0, 0), -- Instance Portal (Raid 10/25 No Heroic)
+(214536, 35, 0x2000, 0, 0), -- Instance Portal (Raid 10/25 No Heroic)
+(214441, 35, 0x2000, 0, 0); -- Instance Portal (Raid 4 Difficulties)
