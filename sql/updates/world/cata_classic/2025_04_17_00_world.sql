cec861222dab0c1f946d3266c529dc4f58571a6a
Ovahlord <dreadkiller@gmx.de>
2025-04-17 22:19:24 +0200

    DB/Quests: added more quest data for Gilneas

---
+++ sql/updates/world/cata_classic/2025_04_17_00_world.sql
@@ -0,0 +1,40 @@
+UPDATE `quest_offer_reward` SET `RewardText`='I am glad to have you here, $N.  We\'re surrounded by Forsaken on all sides and can use all the help we can get.', `VerifiedBuild`=60192 WHERE `ID`=24677; -- Flank the Forsaken
+UPDATE `quest_offer_reward` SET `RewardText`='I wish it could\'ve been avoided, $N.  Let us ensure this is resolved without further bloodshed.', `VerifiedBuild`=60192 WHERE `ID`=24592; -- Betrayal at Tempest's Reach
+UPDATE `quest_offer_reward` SET `RewardText`='$N!$B$BGenn... they\'ve taken Genn... they\'ve taken... our king!', `VerifiedBuild`=60192 WHERE `ID`=24672; -- Onwards and Upwards
+UPDATE `quest_offer_reward` SET `RewardText`='It is done then, $N.  You are one of us now.', `VerifiedBuild`=60192 WHERE `ID`=24593; -- Neither Human Nor Beast
+UPDATE `quest_offer_reward` SET `RewardText`='I knew we could count on you.  You\'ve done well, $N.', `VerifiedBuild`=60192 WHERE `ID`=24646; -- Take Back What's Ours
+UPDATE `quest_offer_reward` SET `RewardText`='You are as good as I remember, $N.  It is good to have you back.', `VerifiedBuild`=60192 WHERE `ID`=24627; -- At Our Doorstep
+UPDATE `quest_offer_reward` SET `RewardText`='You\'re all right, $N!  I\'ve been waiting for this day for a long time, it truly is great to see you friend.$B$BI\'ve heard of what you\'ve done and I\'m thankful... especially for Lorna -- she\'s all I\'ve left.  I will send for her right away.', `VerifiedBuild`=60192 WHERE `ID`=24617; -- Tal'doren, the Wild Home
+UPDATE `quest_offer_reward` SET `RewardText`='Well done, $N.  The scout never had a chance.', `VerifiedBuild`=60192 WHERE `ID`=24616; -- Losing Your Tail
+UPDATE `quest_offer_reward` SET `RewardText`='I\'ve been expecting you, $N.  Do not be alarmed.$B$BMy name is Belysra.  I am a priestess of the moon... a night elf.$B$BYou might not know my people, but the destinies of our two races have been linked since the Curse befell you.', `VerifiedBuild`=60192 WHERE `ID`=24578; -- The Blackwald
+UPDATE `quest_offer_reward` SET `RewardText`='Great job, $N.  We\'ve heard rumors of survivors further in the mountains.  Now we\'ll be able to send scouts there.', `VerifiedBuild`=60192 WHERE `ID`=24501; -- Queen-Sized Troubles
+UPDATE `quest_offer_reward` SET `RewardText`='Thank you, $N.  It will take some time, but I\'ll try to make sense of what we have.', `VerifiedBuild`=60192 WHERE `ID`=24495; -- Pieces of the Past
+UPDATE `quest_offer_reward` SET `RewardText`='You\'ve done well, $N.  The spiders are everywhere, however, and I\'m afraid we\'ve barely put a dent in their numbers.', `VerifiedBuild`=60192 WHERE `ID`=24484; -- Pest Control
+UPDATE `quest_offer_reward` SET `RewardText`='It\'s good to see you made it, $N.  It looks like most everybody did.$B$BWe\'re not doing too bad so far for an emergency evacuation.', `VerifiedBuild`=60192 WHERE `ID`=24483; -- Stormglen
+UPDATE `quest_offer_reward` SET `RewardText`='You definitely got the ettin angry, $N.   I heard him myself.$B$BLet\'s hope this works.', `VerifiedBuild`=60192 WHERE `ID`=24472; -- Introductions Are in Order
+UPDATE `quest_offer_reward` SET `RewardText`='You\'ve done it again, $N.  You have my thanks.', `VerifiedBuild`=60192 WHERE `ID`=24468; -- Stranded at the Marsh
+UPDATE `quest_offer_reward` SET `RewardText`='Thanks for stopping, $N.  Our carriage got hit pretty bad.$B$BThe one in front of us got it worse.', `VerifiedBuild`=60192 WHERE `ID`=24438; -- Exodus
+UPDATE `quest_offer_reward` SET `RewardText`='Look, $N!  Look at what\'s become of Duskhaven!$B$BLook at what\'s become of the last safe place in Gilneas!', `VerifiedBuild`=60192 WHERE `ID`=14467; -- Alas, Gilneas!
+UPDATE `quest_offer_reward` SET `RewardText`='There you are, $N.  I\'ve been expecting you.$B$BI got word of your recovery and... wait -- do you feel that?', `VerifiedBuild`=60192 WHERE `ID`=14466; -- The King's Observatory
+UPDATE `quest_offer_reward` SET `RewardText`='$N.  I\'ve heard much about you.$B$BI understand you were crucial in my family\'s survival during the outbreak in Gilneas City.', `VerifiedBuild`=60192 WHERE `ID`=14465; -- To Greymane Manor
+UPDATE `quest_offer_reward` SET `RewardText`='It\'s just as well, $N.  Maybe the Haywards will fare better than us.', `VerifiedBuild`=60192 WHERE `ID`=14405; -- Escape By Sea
+UPDATE `quest_offer_reward` SET `RewardText`='This is great, $N.  I should be able to finish the repairs in no time.', `VerifiedBuild`=60192 WHERE `ID`=14404; -- Not Quite Shipshape
+UPDATE `quest_offer_reward` SET `RewardText`='You\'re a peach, $N.  Thanks!', `VerifiedBuild`=60192 WHERE `ID`=14400; -- I Can't Wear This
+UPDATE `quest_offer_reward` SET `RewardText`='Why thank you, $N.  I hope you didn\'t peek!$B$BWhere are your manners, dear?', `VerifiedBuild`=60192 WHERE `ID`=14399; -- Grandma's Lost It Alright
+UPDATE `quest_offer_reward` SET `RewardText`='Stay back! Don\'t make me...$B$BIs it you?  By the Light!  It\'s you, $N!', `VerifiedBuild`=60192 WHERE `ID`=14406; -- The Crowley Orchard
+UPDATE `quest_offer_reward` SET `RewardText`='You did what you could, $N.  With any luck a few others will find their way to shore.', `VerifiedBuild`=60192 WHERE `ID`=14395; -- Gasping for Breath
+UPDATE `quest_offer_reward` SET `RewardText`='The ocean, $N.  It swallowed everything... the land... the Forsaken... our men!', `VerifiedBuild`=60192 WHERE `ID`=14396; -- As the Land Shatters
+UPDATE `quest_offer_reward` SET `RewardText`='You did it, $N.  That should take the wind out of their sails.', `VerifiedBuild`=60192 WHERE `ID`=14386; -- Leader of the Pack
+UPDATE `quest_offer_reward` SET `RewardText`='Well done, $N.  You might be a bloody beast, but you\'re our beast.', `VerifiedBuild`=60192 WHERE `ID`=14382; -- Two By Sea
+UPDATE `quest_offer_reward` SET `RewardText`='Not bad, $N.  It\'s a good thing you\'re on our side.', `VerifiedBuild`=60192 WHERE `ID`=14369; -- Unleash the Beast
+UPDATE `quest_offer_reward` SET `RewardText`='My children are safe!  You\'ve done a wonderful thing, $N.  I don\'t know how to thank you!', `VerifiedBuild`=60192 WHERE `ID`=14368; -- Save the Children!
+UPDATE `quest_offer_reward` SET `RewardText`='The Forsaken are here in full strength, $N.  We barely have enough men to hold them back.', `VerifiedBuild`=60192 WHERE `ID`=14367; -- The Allens' Storm Cellar
+UPDATE `quest_offer_reward` SET `RewardText`='Great news, $N.  I\'ve sent the remaining militia to the shore to meet the Forsaken force head on.', `VerifiedBuild`=60192 WHERE `ID`=14366; -- Holding Steady
+UPDATE `quest_offer_reward` SET `RewardText`='Yes, $N!  Just like old times...$B$BI\'m going to have to work hard at not shooting you, but Krennan explained everything to us.', `VerifiedBuild`=60192 WHERE `ID`=14347; -- Hold the Line
+UPDATE `quest_offer_reward` SET `RewardText`='You and me, $N.  We make a great team...$B$BIt\'s good to have you back.', `VerifiedBuild`=60192 WHERE `ID`=14348; -- You Can't Take 'Em Alone
+UPDATE `quest_offer_reward` SET `RewardText`='$N!!!  You ARE alive!$B$BI thought I was having dreams about the old days when I heard your voice...', `VerifiedBuild`=60192 WHERE `ID`=14336; -- Kill or Be Killed
+UPDATE `quest_offer_reward` SET `RewardText`='Forsaken!  Quick, $N!  We must mount a defense.', `VerifiedBuild`=60192 WHERE `ID`=14321; -- Invasion
+
+DELETE FROM `quest_request_items` WHERE `ID`=24674;
+INSERT INTO `quest_request_items` (`ID`, `EmoteOnComplete`, `EmoteOnIncomplete`, `EmoteOnCompleteDelay`, `EmoteOnIncompleteDelay`, `CompletionText`, `VerifiedBuild`) VALUES
+(24674, 0, 0, 0, 0, 'Is it done?', 60192); -- Slaves to No One
