3a06772707a68f83fcc7813590c9bf9b95dd6af0
aquadeus <efectokoleos13@gmail.com>
2025-04-17 12:39:27 +0200

    fix feedback

--- sql/updates/world/master/2024_99_99_99_wandering_isle_quest_29420.sql
+++ sql/updates/world/master/2024_99_99_99_wandering_isle_quest_29420.sql
@@ -6,19 +6,19 @@ INSERT INTO `areatrigger_scripts` VALUES
 (7736, 'SmartTrigger');
 
 -- Conditions
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=22 AND `SourceGroup`=1 AND `SourceEntry`=7736;
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=30 AND `SourceGroup`=0 AND `SourceEntry`=7736;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (30, 0, 7736, 0, 0, 47, 0, 29420, 2, 0, 0, 0, 0, '', 'Areatrigger 7736 only triggers when quest 29420 is taken'),
 (30, 0, 7736, 0, 0, 1, 0, 92571, 0, 0, 1, 0, 0, '', 'Areatrigger 7736 only triggers when player has not aura 92571');
 
-DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=32 AND `SourceEntry`=59626;
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=32 AND `SourceGroup`=0 AND `SourceEntry`=59626;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `ConditionStringValue1`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
 (32, 5, 59626, 0, 0, 47, 0, 29420, 2, 0, '', 0, 0, 0, '', 'Flame Spout gets visible if Quest: 29420 is completed');
 
 -- SAI
 DELETE FROM `smart_scripts` WHERE `entryorguid`=7736 AND `source_type`=2;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param_string`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
-(7736, 2, 0, 0, 46, 0, 100, 0, 7736, 0, 0, 0, 0, '', 86, 92571, 2, 19, 60176, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'On Trigger - Cast CSA Area Trigger Dummy Timer Aura');
+(7736, 2, 0, 0, 46, 0, 100, 0, 0, 0, 0, 0, 0, '', 86, 92571, 2, 19, 60176, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'On Trigger - Cast CSA Area Trigger Dummy Timer Aura');
 
 DELETE FROM `smart_scripts` WHERE `entryorguid`=60176 AND `source_type`=0;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `event_param_string`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
