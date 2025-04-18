44992e5ab8bedb8a2f8586011426b8d05d25c558
Foldor1 <187382119+Foldor1@users.noreply.github.com>
2025-04-18 08:43:35 +0200

    DB/Quests: updated vendor data for Lurah Wrathvine
    closes #30883

---
+++ sql/updates/world/cata_classic/2025_04_18_00_world.sql
@@ -0,0 +1,23 @@
+DELETE FROM `npc_vendor` WHERE `entry`=54402;
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `ExtendedCost`, `type`, `PlayerConditionID`, `IgnoreFiltering`, `VerifiedBuild`) VALUES
+(54402, 21, 71593, 0, 3649, 1, 0, 0, 60192), -- Giantslicer
+(54402, 20, 71592, 0, 3648, 1, 0, 0, 60192), -- Deflecting Star
+(54402, 19, 71568, 0, 3647, 1, 0, 0, 60192), -- Morningstar Shard
+(54402, 18, 71590, 0, 3646, 1, 0, 0, 60192), -- Deathclutch Figurine
+(54402, 17, 71587, 0, 3645, 1, 0, 0, 60192), -- Relic of the Elemental Lords
+(54402, 16, 71580, 0, 3644, 1, 0, 0, 60192), -- Soulflame Vial
+(54402, 15, 71577, 0, 3643, 1, 0, 0, 60192), -- Singed Plume of Aviana
+(54402, 14, 71567, 0, 3642, 1, 0, 0, 60192), -- Covenant of the Flame
+(54402, 13, 71579, 0, 3641, 1, 0, 0, 60192), -- Scorchvine Wand
+(54402, 12, 71575, 0, 3640, 1, 0, 0, 60192), -- Trail of Embers
+(54402, 11, 69109, 0, 3639, 1, 0, 0, 60192), -- Scales of Life
+(54402, 10, 69113, 0, 3638, 1, 0, 0, 60192), -- Apparatus of Khaz'goroth
+(54402, 9, 71564, 0, 3637, 1, 0, 0, 60192), -- Theck's Emberseal
+(54402, 8, 71563, 0, 3636, 1, 0, 0, 60192), -- Firebound Gorget
+(54402, 7, 71562, 0, 3635, 1, 0, 0, 60192), -- Obsidium Cleaver
+(54402, 6, 71561, 0, 3634, 1, 0, 0, 60192), -- Hide-Bound Chains
+(54402, 5, 71641, 0, 3633, 1, 0, 0, 60192), -- Riplimb's Lost Collar
+(54402, 4, 71560, 0, 3632, 1, 0, 0, 60192), -- Chelley's Sterilized Scalpel
+(54402, 3, 71559, 0, 3631, 1, 0, 0, 60192), -- Spire of Scarlet Pain
+(54402, 2, 71558, 0, 3630, 1, 0, 0, 60192), -- Lava Bolt Crossbow
+(54402, 1, 71557, 0, 3629, 1, 0, 0, 60192); -- Ranseur of Hatred
