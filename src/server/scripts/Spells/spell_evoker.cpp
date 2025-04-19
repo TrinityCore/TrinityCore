8506b476444d970943d261c83a035280515d2d42
aquadeus <efectokoleos13@gmail.com>
2025-04-20 00:00:54 +0200

    Spells/Evoker: Implement talent Burnout

--- src/server/scripts/Spells/spell_evoker.cpp
+++ src/server/scripts/Spells/spell_evoker.cpp
@@ -51,6 +51,7 @@ enum EvokerSpells
     SPELL_EVOKER_BLESSING_OF_THE_BRONZE_SHAMAN  = 381756,
     SPELL_EVOKER_BLESSING_OF_THE_BRONZE_WARLOCK = 381757,
     SPELL_EVOKER_BLESSING_OF_THE_BRONZE_WARRIOR = 381758,
+    SPELL_EVOKER_BURNOUT                        = 375802,
     SPELL_EVOKER_CAUSALITY                      = 375777,
     SPELL_EVOKER_DISINTEGRATE                   = 356995,
     SPELL_EVOKER_EMERALD_BLOSSOM_HEAL           = 355916,
@@ -150,6 +151,50 @@ class spell_evo_blessing_of_the_bronze : public SpellScript
     void Register() override
     {
         OnObjectAreaTargetSelect += SpellObjectAreaTargetSelectFn(spell_evo_blessing_of_the_bronze::RemoveInvalidTargets, EFFECT_ALL, TARGET_UNIT_CASTER_AREA_RAID);
+    }
+};
+
+// 375801 - Burnout
+class spell_evo_burnout : public AuraScript
+{
+    bool Validate(SpellInfo const* /*spellInfo*/) override
+    {
+        return ValidateSpellInfo({ SPELL_EVOKER_BURNOUT });
+    }
+
+    static bool CheckProc(AuraEffect const* aurEff, ProcEventInfo const& /*eventInfo*/)
+    {
+        return roll_chance_i(aurEff->GetAmount());
+    }
+
+    static void HandleProc(AuraEffect const* /*aurEff*/, ProcEventInfo const& eventInfo)
+    {
+        eventInfo.GetActor()->CastSpell(eventInfo.GetActor(), SPELL_EVOKER_BURNOUT, TRIGGERED_IGNORE_CAST_IN_PROGRESS | TRIGGERED_DONT_REPORT_CAST_ERROR);
+    }
+
+    void Register() override
+    {
+        DoCheckEffectProc += AuraCheckEffectProcFn(spell_evo_burnout::CheckProc, EFFECT_0, SPELL_AURA_DUMMY);
+        OnEffectProc += AuraEffectProcFn(spell_evo_burnout::HandleProc, EFFECT_0, SPELL_AURA_DUMMY);
+    }
+};
+
+// 361469 - Living Flame (Red)
+class spell_evo_burnout_proc : public SpellScript
+{
+    bool Load() override
+    {
+        return GetCaster()->HasAura(SPELL_EVOKER_BURNOUT);
+    }
+
+    void HandleProc() const
+    {
+        GetCaster()->RemoveAurasDueToSpell(SPELL_EVOKER_BURNOUT);
+    }
+
+    void Register() override
+    {
+        AfterCast += SpellCastFn(spell_evo_burnout_proc::HandleProc);
     }
 };
 
@@ -685,6 +730,8 @@ void AddSC_evoker_spell_scripts()
 {
     RegisterSpellScript(spell_evo_azure_strike);
     RegisterSpellScript(spell_evo_blessing_of_the_bronze);
+    RegisterSpellScript(spell_evo_burnout);
+    RegisterSpellScript(spell_evo_burnout_proc);
     RegisterSpellScript(spell_evo_causality_disintegrate);
     RegisterSpellScript(spell_evo_causality_pyre);
     RegisterSpellScript(spell_evo_charged_blast);
