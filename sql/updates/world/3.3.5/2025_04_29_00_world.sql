3e753ba77690cd70ed9749bad22e704d5d5a0c54
Aokromes <Aokromes@users.noreply.github.com>
2025-04-29 09:28:07 +0200

    DB/Quest: Darkshore, Two quests are missing Reward next quest
    closes #30901
    by Jonne733

---
+++ sql/updates/world/3.3.5/2025_04_29_00_world.sql
@@ -0,0 +1,5 @@
+-- The quest "The Absent Minded Prospector" (729) is missing Reward Next Quest
+UPDATE `quest_template` SET `RewardNextQuest`=731 WHERE `Id`=729;
+
+-- The quest "The Cliffspring River" (4762) is missing Reward Next Quest
+UPDATE `quest_template` SET `RewardNextQuest`=4763 WHERE `Id`=4762;
