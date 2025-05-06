18436298f43372edef32587ed1bf6e6f625c200c
Shauren <shauren.trinity@gmail.com>
2025-05-06 19:22:21 +0200

    Core/Spells: Implemented new SpellMisc field, MinDuration that controls minimum spell missile travel time
    (cherry picked from commit a63d404e7bf2dce3b15ce5f66ac98fe71a41f51c)

--- src/server/game/Spells/SpellInfo.cpp
+++ src/server/game/Spells/SpellInfo.cpp
@@ -1226,6 +1226,7 @@ SpellInfo::SpellInfo(SpellNameEntry const* spellName, ::Difficulty difficulty, S
         RangeEntry = sSpellRangeStore.LookupEntry(_misc->RangeIndex);
         Speed = _misc->Speed;
         LaunchDelay = _misc->LaunchDelay;
+        MinDuration = _misc->MinDuration;
         SchoolMask = _misc->SchoolMask;
         IconFileDataId = _misc->SpellIconFileDataID;
         ActiveIconFileDataId = _misc->ActiveIconFileDataID;
