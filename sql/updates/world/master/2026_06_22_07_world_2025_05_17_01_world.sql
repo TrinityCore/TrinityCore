3b2ca37df2106b58401a141288849e4542ed9090
offl <11556157+offl@users.noreply.github.com>
2025-05-17 14:03:45 +0300

    Scripts/TheArcatraz: Modernize scripts (#30945)
    (cherry picked from commit 86f8a1e43f4b09ebd808d8b27ee8532839bf6f78)

---
+++ sql/updates/world/master/2026_06_22_07_world_2025_05_17_01_world.sql
@@ -0,0 +1,5 @@
+--
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_harbinger_skyriss_health';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(36928, 'spell_harbinger_skyriss_health'),
+(36930, 'spell_harbinger_skyriss_health');
