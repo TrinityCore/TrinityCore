c631c938e65e1a758425a67c3802fa1e5b23b08b
Shauren <shauren.trinity@gmail.com>
2025-06-08 22:18:28 +0200

    Core/Spells: Allow removing weapon damage effects by preventing target selection in scripts
    (cherry picked from commit ea692f5a248e5e67e3b983767044901cc743309b)

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -2784,8 +2784,12 @@ void Spell::EffectWeaponDmg()
     // multiple weapon dmg effect workaround
     // execute only the last weapon damage
     // and handle all effects at once
+    uint32 effectMask = std::ranges::find(m_UniqueTargetInfo, unitTarget->GetGUID(), &TargetInfo::TargetGUID)->EffectMask;
     for (size_t j = effectInfo->EffectIndex + 1; j < m_spellInfo->GetEffects().size(); ++j)
     {
+        if (!(effectMask & (1 << j)))
+            continue;
+
         switch (m_spellInfo->GetEffect(SpellEffIndex(j)).Effect)
         {
             case SPELL_EFFECT_WEAPON_DAMAGE:
@@ -2819,6 +2823,9 @@ void Spell::EffectWeaponDmg()
     float weaponDamagePercentMod = 1.0f;
     for (SpellEffectInfo const& spellEffectInfo : m_spellInfo->GetEffects())
     {
+        if (!(effectMask & (1 << spellEffectInfo.EffectIndex)))
+            continue;
+
         switch (spellEffectInfo.Effect)
         {
             case SPELL_EFFECT_WEAPON_DAMAGE:
@@ -2864,6 +2871,9 @@ void Spell::EffectWeaponDmg()
     // Sequence is important
     for (SpellEffectInfo const& spellEffectInfo : m_spellInfo->GetEffects())
     {
+        if (!(effectMask & (1 << spellEffectInfo.EffectIndex)))
+            continue;
+
         // We assume that a spell have at most one fixed_bonus
         // and at most one weaponDamagePercentMod
         switch (spellEffectInfo.Effect)
