dbdcb20d6856f2096b10c0d5d8e28e4d9f90fe3c
Meji <alvaro.megias@outlook.com>
2025-06-10 18:49:54 +0200

    DB/TirisfalGlades: Fix talk targets for quest "The Shadow Grave"
    Closes #31058
    
    (cherry picked from commit 7041bb49f77ba86dd64de1a9cc2b510aaded5047)

---
+++ sql/updates/world/cata_classic/2026_07_21_00_world_2025_06_10_00_world.sql
@@ -0,0 +1,3 @@
+-- The Shadow Grave
+UPDATE `smart_scripts` SET `target_type`=23 WHERE `action_type`=1 AND `entryorguid`=49141 AND `source_type`=0;
+UPDATE `smart_scripts` SET `target_type`=23 WHERE `action_type`=1 AND `entryorguid` IN (4914100,4914101,4914102,4914103,4914104) AND `source_type`=9;
