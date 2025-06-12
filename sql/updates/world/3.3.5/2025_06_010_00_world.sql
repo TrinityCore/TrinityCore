0b07719b46f677b8bc76f130924d9fa8bd6b383e
offl <suffle.o@gmail.com>
2025-06-12 21:39:31 +0300

    O'mrogg

--- sql/updates/world/3.3.5/2025_06_010_00_world.sql
+++ sql/updates/world/3.3.5/2025_06_010_00_world.sql
@@ -1,4 +1,4 @@
---
+-- Warchief Kargath Bladefist
 UPDATE `creature_template` SET `unit_flags` = 33554432, `ScriptName` = 'npc_warchiefs_portal' WHERE `entry` = 17611;
 DELETE FROM `creature` WHERE `id` = 17611;
 
@@ -45,3 +45,71 @@ UPDATE `creature_template_addon` SET `auras` = '19818' WHERE `entry` = 16808;
 
 DELETE FROM `creature` WHERE `guid` IN (86464,86465,86466);
 DELETE FROM `spawn_group` WHERE `spawnType` = 0 AND `spawnId` IN (86464,86465,86466);
+
+-- Warbringer O'mrogg
+DELETE FROM `creature` WHERE `guid` IN (130847,130848) AND `id` IN (19523,19524);
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `VerifiedBuild`) VALUES
+(130847, 19523, 540, 0, 0, 3, 1, 0, 0, 374.378570556640625, 51.28566360473632812, 30.643890380859375, 0.03490658476948738, 86400, 0, 0, 74, 0, 0, 0, 0, 0, 14007),
+(130848, 19524, 540, 0, 0, 3, 1, 0, 0, 374.356475830078125, 64.50481414794921875, 31.22058868408203125, 6.0737457275390625, 86400, 0, 0, 74, 0, 0, 0, 0, 0, 14007);
+
+UPDATE `creature_template` SET `flags_extra` = `flags_extra`|128 WHERE `entry` IN (19523,19524,20572,20573);
+
+DELETE FROM `creature_template_movement` WHERE `CreatureId` IN (19523,19524);
+INSERT INTO `creature_template_movement` (`CreatureId`, `Ground`, `Swim`, `Flight`, `Rooted`, `Chase`, `Random`, `InteractionPauseTimer`) VALUES
+(19523,1,1,1,1,0,0,NULL),
+(19524,1,1,1,1,0,0,NULL);
+
+DELETE FROM `creature_text` WHERE `CreatureID` IN (19523,19524,16809);
+INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES
+(16809,0,0,"%s roars!",16,0,100,0,0,0,14029,0,"omrogg EMOTE_ROAR"),
+
+-- Smash! 15:26:08.000 -- Why don't you let me do the talking? 15:26:11.000 -- Hey! 15:26:14.000
+-- https://youtu.be/9aHROEXiVSA?t=1548
+/* L */ (19523,0,0,"Smash!",                                                    14,0,100,0,0,10306,14046,0,"omrogg SAY_AGGRO_1"),
+/* R */ (19524,0,0,"Why don't you let me do the talking?",                      14,0,100,0,0,10317,14047,0,"omrogg SAY_AGGRO_1_1"),
+/* L */ (19523,1,0,"Hey!",                                                      14,0,100,0,0,10307,16917,0,"omrogg SAY_AGGRO_1_2"),
+
+-- https://youtu.be/cmZMZFc09oM?t=26
+/* L */ (19523,2,0,"If you nice me let you live.",                              14,0,100,0,0,10308,14048,0,"omrogg SAY_AGGRO_2"),
+/* R */ (19524,1,0,"No, we will NOT let you live.",                             14,0,100,0,0,10318,16916,0,"omrogg SAY_AGGRO_2_1"),
+
+-- https://youtu.be/c7anPGIo9pA?t=5
+/* L */ (19523,3,0,"Me hungry.",                                                14,0,100,0,0,10309,16918,0,"omrogg SAY_AGGRO_3"),
+/* R */ (19524,2,0,"You're always hungry.  That's why we so fat!",              14,0,100,0,0,10319,16919,0,"omrogg SAY_AGGRO_3_1"),
+
+-- Me not like this one... 15:26:36.000 -- Hey, you numbskull! 15:26:39.000 -- We kill his friend! 15:26:41.000
+-- https://youtu.be/FfjIPe1F7v4?t=1810
+/* L */ (19523,4,0,"Me not like this one...",                                   14,0,100,0,0,10300,14043,0,"omrogg SAY_ATTACK_1"),
+/* R */ (19524,3,0,"Hey, you numbskull!",                                       14,0,100,0,0,10312,14044,0,"omrogg SAY_ATTACK_1_1"),
+/* L */ (19523,5,0,"We kill his friend!",                                       14,0,100,0,0,10301,14045,0,"omrogg SAY_ATTACK_1_2"),
+
+-- https://youtu.be/H4hvmeYjIYI?t=3130
+/* L */ (19523,6,0,"We kill someone else!",                                     14,0,100,0,0,10302,16895,0,"omrogg SAY_ATTACK_2"),
+/* R */ (19524,4,0,"I'm not done yet, idiot!",                                  14,0,100,0,0,10313,16896,0,"omrogg SAY_ATTACK_2_1"),
+
+-- https://youtu.be/H4hvmeYjIYI?t=3197
+/* L */ (19523,7,0,"You stay here.  Me go kill someone else!",                  14,0,100,0,0,10303,16898,0,"omrogg SAY_ATTACK_3"),
+/* R */ (19524,5,0,"That's not funny!",                                         14,0,100,0,0,10314,16899,0,"omrogg SAY_ATTACK_3_1"),
+/* L */ (19523,8,0,"Ha ha ha.",                                                 14,0,100,0,0,10304,16900,0,"omrogg SAY_ATTACK_3_2"),
+
+-- https://youtu.be/0phkTyrfzpo?t=484
+/* R */ (19524,6,0,"What are you doing?",                                       14,0,100,0,0,10315,16901,0,"omrogg SAY_ATTACK_4"),
+/* L */ (19523,9,0,"Me get bored.",                                             14,0,100,0,0,10305,16902,0,"omrogg SAY_ATTACK_4_1"),
+/* R */ (19524,7,0,"Bored? He was almost dead!",                                14,0,100,0,0,10316,16903,0,"omrogg SAY_ATTACK_4_2"),
+
+-- https://wowpedia.fandom.com/wiki/Warbringer_O%27mrogg
+/* R */ (19524,8,0,"I'm tired.  You kill next one!",                            14,0,100,0,0,10320,16921,0,"omrogg SAY_SLAY_1"),
+
+-- https://wowpedia.fandom.com/wiki/Warbringer_O%27mrogg
+/* L */ (19523,10,0,"This one die easy!",                                       14,0,100,0,0,10310,16922,0,"omrogg SAY_SLAY_2"),
+/* R */ (19524,9,0,"That's because I do all the hard work!",                    14,0,100,0,0,10321,16923,0,"omrogg SAY_SLAY_2_1"),
+
+-- This all... your fault! 15:26:57.000 -- I... hate... you. 15:27:00.000
+-- https://youtu.be/FfjIPe1F7v4?t=1830
+/* L */ (19523,11,0,"This all... your fault!",                                  14,0,100,0,0,10311,16924,0,"omrogg SAY_DEATH_1"),
+/* R */ (19524,10,0,"I... hate... you.",                                        14,0,100,0,0,10322,16925,0,"omrogg SAY_DEATH_1_1");
+
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_omrogg_burning_maul';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(30598, 'spell_omrogg_burning_maul'),
+(36056, 'spell_omrogg_burning_maul');
