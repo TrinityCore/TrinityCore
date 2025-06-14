f00bcb438fae2aa0d458a2266affd15f32911adc
Golrag <golrag.jeremy@gmail.com>
2025-06-14 18:54:20 +0200

    Handle Fresh Meat in enrage_proc aurascript

--- sql/updates/world/master/2025_mm_dd_xx_world.sql
+++ sql/updates/world/master/2025_mm_dd_xx_world.sql
@@ -1,5 +1,3 @@
-DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_warr_fresh_meat', 'spell_warr_fresh_meat_aura_dummy');
+DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_warr_fresh_meat_aura_dummy');
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
-(23881, 'spell_warr_fresh_meat'),
-(335096, 'spell_warr_fresh_meat'),
 (316044, 'spell_warr_fresh_meat_aura_dummy');
