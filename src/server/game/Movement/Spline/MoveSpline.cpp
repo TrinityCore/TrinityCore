13be3e3e2d2ef755f9af4dcdf8ff0a0680c7767a
Shauren <shauren.trinity@gmail.com>
2025-06-17 23:09:08 +0200

    Core/Movement: Spline code cleanup
    * Remove unneccessary reserve in MoveSplineInitArgs constructor - MoveTo always resizes it
    * Remove unused function computeDuration
    * Split cyclic spline reinitialization to separate function
    * Don't expose TransportPathTransform outside of MoveSplineInit
    * Ensure function argument names are the same in declaration and definition
    
    (cherry picked from commit e02e8a474244c229cc34c4efb987e24b6dd417f5)

--- src/server/game/Movement/Spline/MoveSpline.cpp
+++ src/server/game/Movement/Spline/MoveSpline.cpp
@@ -114,11 +114,6 @@ void MoveSpline::computeFallElevation(int32 time_point, float& el) const
     el = std::max(z_now, final_z);
 }
 
-inline uint32 computeDuration(float length, float velocity)
-{
-    return SecToMS(length / velocity);
-}
-
 struct FallInitializer
 {
     FallInitializer(float _start_elevation) : start_elevation(_start_elevation) { }
@@ -147,16 +142,16 @@ struct CommonInitializer
 
 void MoveSpline::init_spline(MoveSplineInitArgs const& args)
 {
-    static SplineBase::EvaluationMode const modes[2] = { SplineBase::ModeLinear, SplineBase::ModeCatmullrom };
+    SplineBase::EvaluationMode mode = args.flags.isSmooth() ? SplineBase::ModeCatmullrom : SplineBase::ModeLinear;
     if (args.flags.Cyclic)
     {
         uint32 cyclic_point = 0;
         if (splineflags.Enter_Cycle)
             cyclic_point = 1;   // shouldn't be modified, came from client
-        spline.init_cyclic_spline(&args.path[0], args.path.size(), modes[args.flags.isSmooth()], cyclic_point, args.initialOrientation);
+        spline.init_cyclic_spline(args.path.data(), args.path.size(), mode, cyclic_point, args.initialOrientation);
     }
     else
-        spline.init_spline(&args.path[0], args.path.size(), modes[args.flags.isSmooth()], args.initialOrientation);
+        spline.init_spline(args.path.data(), args.path.size(), mode, args.initialOrientation);
 
     // init spline timestamps
     if (splineflags.Falling)
@@ -241,18 +236,15 @@ MoveSpline::MoveSpline() : m_Id(0), time_passed(0),
 bool MoveSplineInitArgs::Validate(Unit const* unit)
 {
 #define CHECK(exp, verbose) \
-    do if (!(exp))\
-    {\
-        if (unit)\
-            TC_LOG_ERROR("misc.movesplineinitargs", "MoveSplineInitArgs::Validate: expression '{}' failed for {}", #exp, verbose);\
-        else\
-            TC_LOG_ERROR("misc.movesplineinitargs", "MoveSplineInitArgs::Validate: expression '{}' failed for cyclic spline continuation", #exp); \
-        return false;\
-    } while (0)
+    if (!(exp)) return [&]{ \
+        TC_LOG_ERROR("misc.movesplineinitargs", "MoveSplineInitArgs::Validate: expression '{}' failed for {}", #exp, unit ? std::string_view(verbose) : "cyclic spline continuation"sv); \
+        return false; \
+    }()
+
     CHECK(path.size() > 1, unit->GetDebugInfo());
     CHECK(velocity >= 0.01f, unit->GetDebugInfo());
     CHECK(effect_start_time_percent >= 0.f && effect_start_time_percent <= 1.f, unit->GetDebugInfo());
-    CHECK(_checkPathLengths(), unit->GetGUID());
+    CHECK(_checkPathLengths(), unit->GetGUID().ToString());
     if (spellEffectExtra)
     {
         CHECK(!spellEffectExtra->ProgressCurveId || sCurveStore.LookupEntry(spellEffectExtra->ProgressCurveId), unit->GetDebugInfo());
@@ -294,12 +286,11 @@ bool MoveSplineInitArgs::_checkPathLengths()
     return true;
 }
 
-MoveSplineInitArgs::MoveSplineInitArgs(size_t path_capacity /*= 16*/) : path_Idx_offset(0), velocity(0.f),
+MoveSplineInitArgs::MoveSplineInitArgs() : path_Idx_offset(0), velocity(0.f),
 parabolic_amplitude(0.f), vertical_acceleration(0.0f), effect_start_time_percent(0.f), effect_start_time(0ms),
 splineId(0), initialOrientation(0.f),
 walk(false), HasVelocity(false), TransformForTransport(true)
 {
-    path.reserve(path_capacity);
 }
 
 MoveSplineInitArgs::MoveSplineInitArgs(MoveSplineInitArgs&& args) noexcept = default;
@@ -343,34 +334,7 @@ MoveSpline::UpdateResult MoveSpline::_updateState(int32& ms_time_diff)
                 if (splineflags.Enter_Cycle)
                 {
                     splineflags.Enter_Cycle = false;
-
-                    MoveSplineInitArgs args{ (size_t)spline.getPointCount() };
-                    args.path.assign(spline.getPoints().begin() + spline.first() + 1, spline.getPoints().begin() + spline.last());
-                    args.facing = facing;
-                    args.flags = splineflags;
-                    args.path_Idx_offset = point_Idx_offset;
-                    // MoveSplineFlag::Parabolic | MoveSplineFlag::Animation not supported currently
-                        //args.parabolic_amplitude = ?;
-                        //args.time_perc = ?;
-                    args.splineId = m_Id;
-                    args.initialOrientation = initialOrientation;
-                    args.velocity = 1.0f; // Calculated below
-                    args.HasVelocity = true;
-                    args.TransformForTransport = onTransport;
-                    if (args.Validate(nullptr))
-                    {
-                        // New cycle should preserve previous cycle's duration for some weird reason, even though
-                        // the path is really different now. Blizzard is weird. Or this was just a simple oversight.
-                        // Since our splines precalculate length with velocity in mind, if we want to find the desired
-                        // velocity, we have to make a fake spline, calculate its duration and then compare it to the
-                        // desired duration, thus finding out how much the velocity has to be increased for them to match.
-                        MoveSpline tempSpline;
-                        tempSpline.Initialize(args);
-                        args.velocity = (float)tempSpline.Duration() / Duration();
-
-                        if (args.Validate(nullptr))
-                            init_spline(args);
-                    }
+                    reinit_spline_for_next_cycle();
                 }
             }
             else
@@ -385,24 +349,52 @@ MoveSpline::UpdateResult MoveSpline::_updateState(int32& ms_time_diff)
     return result;
 }
 
+void MoveSpline::reinit_spline_for_next_cycle()
+{
+    MoveSplineInitArgs args;
+    args.path.assign(spline.getPoints().begin() + spline.first() + 1, spline.getPoints().begin() + spline.last());
+    args.facing = facing;
+    args.flags = splineflags;
+    args.path_Idx_offset = point_Idx_offset;
+    args.splineId = m_Id;
+    args.initialOrientation = initialOrientation;
+    args.velocity = 1.0f; // Calculated below
+    args.HasVelocity = true;
+    args.TransformForTransport = onTransport;
+    if (args.Validate(nullptr))
+    {
+        // New cycle should preserve previous cycle's duration for some weird reason, even though
+        // the path is really different now. Blizzard is weird. Or this was just a simple oversight.
+        // Since our splines precalculate length with velocity in mind, if we want to find the desired
+        // velocity, we have to make a fake spline, calculate its duration and then compare it to the
+        // desired duration, thus finding out how much the velocity has to be increased for them to match.
+        MoveSpline tempSpline;
+        tempSpline.Initialize(args);
+        args.velocity = (float)tempSpline.Duration() / Duration();
+
+        if (args.Validate(nullptr))
+            init_spline(args);
+    }
+}
+
 std::string MoveSpline::ToString() const
 {
     std::stringstream str;
-    str << "MoveSpline" << std::endl;
-    str << "spline Id: " << GetId() << std::endl;
-    str << "flags: " << splineflags.ToString() << std::endl;
+    str << "MoveSpline\n";
+    str << "spline Id: " << GetId() << '\n';
+    str << "flags: " << splineflags.ToString() << '\n';
     if (facing.type == MONSTER_MOVE_FACING_ANGLE)
-        str << "facing  angle: " << facing.angle << std::endl;
+        str << "facing  angle: " << facing.angle << '\n';
     else if (facing.type == MONSTER_MOVE_FACING_TARGET)
-        str << "facing target: " << facing.target.ToString() << std::endl;
+        str << "facing target: " << facing.target.ToString() << '\n';
     else if (facing.type == MONSTER_MOVE_FACING_SPOT)
-        str << "facing  point: " << facing.f.x << " " << facing.f.y << " " << facing.f.z << std::endl;
-    str << "time passed: " << time_passed << std::endl;
-    str << "total  time: " << Duration() << std::endl;
-    str << "spline point Id: " << point_Idx << std::endl;
-    str << "path  point  Id: " << currentPathIdx() << std::endl;
+        str << "facing  point: " << facing.f.x << " " << facing.f.y << " " << facing.f.z << '\n';
+    str << "time passed: " << time_passed << '\n';
+    str << "total  time: " << Duration() << '\n';
+    str << "spline point Id: " << point_Idx << '\n';
+    str << "path  point  Id: " << currentPathIdx() << '\n';
     str << spline.ToString();
-    return str.str();
+    return std::move(str).str();
 }
 
 void MoveSpline::_Finalize()
