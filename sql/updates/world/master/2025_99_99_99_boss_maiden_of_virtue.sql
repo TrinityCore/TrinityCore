42555117c3a2b21e9f15902e2813a1c97b480528
aquadeus <efectokoleos13@gmail.com>
2025-06-26 10:54:11 +0200

    Scripts/ReturnToKarazhan: Implement Maiden of Virtue encounter

---
+++ sql/updates/world/master/2025_99_99_99_boss_maiden_of_virtue.sql
@@ -0,0 +1,38 @@
+UPDATE `creature_template` SET `ScriptName`='boss_rtk_maiden_of_virtue' WHERE `entry`=113971;
+
+DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_maiden_of_virtue_sacred_ground', 'spell_maiden_of_virtue_holy_bulwark');
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(227793, 'spell_maiden_of_virtue_sacred_ground'),
+(227817, 'spell_maiden_of_virtue_holy_bulwark');
+
+DELETE FROM `areatrigger_create_properties` WHERE (`IsCustom`=0 AND `Id` = 8812);
+INSERT INTO `areatrigger_create_properties` (`Id`, `IsCustom`, `AreaTriggerId`, `IsAreatriggerCustom`, `Flags`, `MoveCurveId`, `ScaleCurveId`, `MorphCurveId`, `FacingCurveId`, `AnimId`, `AnimKitId`, `DecalPropertiesId`, `SpellForVisuals`, `TimeToTargetScale`, `Speed`, `Shape`, `ShapeData0`, `ShapeData1`, `ShapeData2`, `ShapeData3`, `ShapeData4`, `ShapeData5`, `ShapeData6`, `ShapeData7`, `ScriptName`, `VerifiedBuild`) VALUES
+(8812, 0, 12880, 0, 2, 0, 2537, 0, 0, -1, 0, 86, 0, 180000, 0, 0, 5, 5, 0, 0, 0, 0, 0, 0, 'at_maiden_of_virtue_sacred_ground', 61609); -- Spell: 227789 (Sacred Ground)
+
+DELETE FROM `areatrigger_template` WHERE (`IsCustom`=0 AND `Id` = 12880);
+INSERT INTO `areatrigger_template` (`Id`, `IsCustom`, `Flags`, `VerifiedBuild`) VALUES
+(12880, 0, 0, 61609);
+
+DELETE FROM `creature_text` WHERE `CreatureID` = 113971;
+INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES
+(113971, 0, 0, 'And so begins your purification!', 14, 0, 100, 0, 0, 77786, 123837, 0, 'Maiden of Virtue'),
+(113971, 0, 1, 'The stench of corruption is upon you!', 14, 0, 100, 0, 0, 77787, 123840, 0, 'Maiden of Virtue'),
+(113971, 1, 0, 'The lesson is complete.', 14, 0, 100, 0, 0, 77788, 123843, 0, 'Maiden of Virtue'),
+(113971, 1, 1, 'A pure mind is an orderly mind.', 14, 0, 100, 0, 0, 77789, 123844, 0, 'Maiden of Virtue'),
+(113971, 2, 0, 'Purified!', 14, 0, 100, 0, 0, 77784, 123827, 0, 'Maiden of Virtue'),
+(113971, 2, 1, 'A teachable moment.', 14, 0, 100, 0, 0, 77785, 123830, 0, 'Maiden of Virtue'),
+(113971, 3, 0, 'All shall be sanctified!', 14, 0, 100, 0, 0, 77775, 123772, 0, 'Maiden of Virtue'),
+(113971, 4, 0, '|TInterface\\Icons\\Spell_Holy_InnerFire:20|t%s is casting |cFFFF0000|Hspell:227789|h[Sacred Ground]|h|r at you!', 42, 0, 100, 0, 0, 0, 123100, 0, 'Maiden of Virtue'),
+(113971, 5, 0, 'The righteous have nothing to fear!', 14, 0, 100, 0, 0, 77778, 123775, 0, 'Maiden of Virtue'),
+(113971, 5, 1, 'Step into the light, mortals!', 14, 0, 100, 0, 0, 77779, 123807, 0, 'Maiden of Virtue'),
+(113971, 6, 0, 'By fire be cleansed!', 14, 0, 100, 0, 0, 77777, 123774, 0, 'Maiden of Virtue'),
+(113971, 7, 0, '|TInterface\\Icons\\Spell_Holy_PrayerOfHealing:20|t%s is casting |cFFFF0000|Hspell:227508|h[Mass Repentance]|h|r!', 41, 0, 100, 0, 0, 0, 123101, 0, 'Maiden of Virtue'),
+(113971, 8, 0, 'Hold, mortals! Open your hearts to virtue!', 14, 0, 100, 0, 0, 77780, 123808, 0, 'Maiden of Virtue'),
+(113971, 9, 0, 'Renounce your greed and perversion!', 14, 0, 100, 0, 0, 77781, 123811, 0, 'Maiden of Virtue'),
+(113971, 10, 0, 'Repent... or DIE!', 14, 0, 100, 0, 0, 77782, 123814, 0, 'Maiden of Virtue'),
+(113971, 11, 0, 'I will... never... relent...', 14, 0, 100, 0, 0, 77790, 123845, 0, 'Maiden of Virtue'),
+(113971, 11, 1, 'You are all... as corrupt... as the Guardian...', 14, 0, 100, 0, 0, 77791, 123846, 0, 'Maiden of Virtue');
+
+DELETE FROM `instance_template` WHERE `map`=1651;
+INSERT INTO `instance_template` (`map`, `parent`, `script`) VALUES
+(1651, 0, 'instance_return_to_karazhan');
---
+++ src/server/scripts/EasternKingdoms/ReturnToKarazhan/boss_rtk_maiden_of_virtue.cpp
@@ -0,0 +1,260 @@
+/*
+ * This file is part of the TrinityCore Project. See AUTHORS file for Copyright information
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "AreaTrigger.h"
+#include "AreaTriggerAI.h"
+#include "InstanceScript.h"
+#include "ScriptedCreature.h"
+#include "ScriptMgr.h"
+#include "SpellAuras.h"
+#include "SpellScript.h"
+#include "return_to_karazhan.h"
+
+enum MaidenOfVirtueSpells
+{
+    SPELL_HOLY_BOLT              = 227809,
+    SPELL_HOLY_SHOCK             = 227800,
+    SPELL_SACRED_GROUND          = 227789,
+    SPELL_SACRED_GROUND_DAMAGE   = 227848,
+    SPELL_SACRED_GROUND_PERIODIC = 227793,
+    SPELL_MASS_REPENTANCE        = 227508,
+    SPELL_HOLY_BULWARK           = 227817,
+    SPELL_HOLY_WRATH             = 227823
+};
+
+enum MaidenOfVirtueEvents
+{
+    EVENT_HOLY_BOLT = 1,
+    EVENT_HOLY_SHOCK,
+    EVENT_SACRED_GROUND,
+    EVENT_MASS_REPENTANCE,
+    EVENT_HOLY_BULWARK,
+    EVENT_HOLY_WRATH
+};
+
+enum MaidenOfVirtueActions
+{
+    ACTION_INTERRUPT_HOLY_WRATH = 1
+};
+
+enum MaidenOfVirtueTexts
+{
+    SAY_AGGRO                   = 0,
+    SAY_WIPE                    = 1,
+    SAY_SLAY                    = 2,
+    SAY_HOLY_BOLT               = 3,
+    SAY_SACRED_GROUND_WARNING   = 4,
+    SAY_SACRED_GROUND           = 5,
+    SAY_HOLY_SHOCK              = 6,
+    SAY_MASS_REPENTANCE_WARNING = 7,
+    SAY_MASS_REPENTANCE         = 8,
+    SAY_HOLY_BULWARK            = 9,
+    SAY_HOLY_WRATH              = 10,
+    SAY_DEAD                    = 11
+};
+
+// 113971 - Maiden of Virtue
+struct boss_rtk_maiden_of_virtue : public BossAI
+{
+    boss_rtk_maiden_of_virtue(Creature* creature) : BossAI(creature, DATA_RTK_MAIDEN_OF_VIRTUE) { }
+
+    void JustDied(Unit* /*killer*/) override
+    {
+        instance->SendEncounterUnit(ENCOUNTER_FRAME_DISENGAGE, me);
+        _JustDied();
+        Talk(SAY_DEAD);
+    }
+
+    void EnterEvadeMode(EvadeReason /*why*/) override
+    {
+        instance->SendEncounterUnit(ENCOUNTER_FRAME_DISENGAGE, me);
+
+        Talk(SAY_WIPE);
+        summons.DespawnAll();
+        _EnterEvadeMode();
+        _DespawnAtEvade();
+    }
+
+    void KilledUnit(Unit* victim) override
+    {
+        if (!victim->IsPlayer())
+            return;
+
+        Talk(SAY_SLAY);
+    }
+
+    void JustEngagedWith(Unit* who) override
+    {
+        BossAI::JustEngagedWith(who);
+        Talk(SAY_AGGRO);
+        instance->SendEncounterUnit(ENCOUNTER_FRAME_ENGAGE, me, 1);
+
+        events.ScheduleEvent(EVENT_HOLY_BOLT, 8500ms);
+        events.ScheduleEvent(EVENT_HOLY_SHOCK, 14900ms);
+        events.ScheduleEvent(EVENT_SACRED_GROUND, 10s);
+        events.ScheduleEvent(EVENT_MASS_REPENTANCE, 45200ms);
+    }
+
+    void DoAction(int32 action) override
+    {
+        if (action == ACTION_INTERRUPT_HOLY_WRATH)
+        {
+            me->InterruptNonMeleeSpells(false);
+            events.ScheduleEvent(EVENT_HOLY_BOLT, 8500ms);
+            events.ScheduleEvent(EVENT_HOLY_SHOCK, 14900ms);
+            events.ScheduleEvent(EVENT_SACRED_GROUND, 10s);
+            events.ScheduleEvent(EVENT_MASS_REPENTANCE, 45200ms);
+        }
+    }
+
+    void UpdateAI(uint32 diff) override
+    {
+        if (!UpdateVictim())
+            return;
+
+        events.Update(diff);
+
+        if (me->HasUnitState(UNIT_STATE_CASTING))
+            return;
+
+        while (uint32 eventId = events.ExecuteEvent())
+        {
+            switch (eventId)
+            {
+                case EVENT_HOLY_BOLT:
+                {
+                    Talk(SAY_HOLY_BOLT);
+                    if (Unit* target = SelectTarget(SelectTargetMethod::Random, 0, 55.0f, true))
+                        DoCast(target, SPELL_HOLY_BOLT);
+                    events.Repeat(9700ms);
+                    break;
+                }
+                case EVENT_HOLY_SHOCK:
+                {
+                    Talk(SAY_HOLY_SHOCK);
+                    if (Unit* target = SelectTarget(SelectTargetMethod::Random, 0, 55.0f, true))
+                        DoCast(target, SPELL_HOLY_SHOCK);
+                    events.Repeat(13300ms);
+                    break;
+                }
+                case EVENT_SACRED_GROUND:
+                {
+                    Talk(SAY_SACRED_GROUND);
+                    if (Unit* target = SelectTarget(SelectTargetMethod::Random, 0))
+                    {
+                        Talk(SAY_SACRED_GROUND_WARNING, target);
+                        DoCast(target, SPELL_SACRED_GROUND);
+                    }
+                    events.Repeat(19400ms);
+                    break;
+                }
+                case EVENT_MASS_REPENTANCE:
+                {
+                    Talk(SAY_MASS_REPENTANCE_WARNING);
+                    Talk(SAY_MASS_REPENTANCE);
+                    DoCastSelf(SPELL_MASS_REPENTANCE);
+                    events.CancelEvent(EVENT_HOLY_BOLT);
+                    events.CancelEvent(EVENT_HOLY_SHOCK);
+                    events.CancelEvent(EVENT_SACRED_GROUND);
+                    events.ScheduleEvent(EVENT_HOLY_BULWARK, 7300ms);
+                    events.Repeat(51s);
+                    break;
+                }
+                case EVENT_HOLY_BULWARK:
+                {
+                    Talk(SAY_HOLY_BULWARK);
+                    DoCastSelf(SPELL_HOLY_BULWARK);
+                    events.ScheduleEvent(EVENT_HOLY_WRATH, 1200ms);
+                    break;
+                }
+                case EVENT_HOLY_WRATH:
+                {
+                    Talk(SAY_HOLY_WRATH);
+                    DoCastSelf(SPELL_HOLY_WRATH);
+                    events.Repeat(2s);
+                    break;
+                }
+                default:
+                    break;
+            }
+
+            if (me->HasUnitState(UNIT_STATE_CASTING))
+                return;
+        }
+    }
+};
+
+// 227793 - Sacred Ground
+class spell_maiden_of_virtue_sacred_ground : public AuraScript
+{
+    void HandlePeriodic(AuraEffect const* aurEff) const
+    {
+        if (!GetCaster())
+            return;
+
+        if (aurEff->GetTickNumber() == 1)
+            GetCaster()->CastSpell(GetTarget(), GetEffectInfo(EFFECT_0).CalcValue(), TRIGGERED_IGNORE_CAST_IN_PROGRESS | TRIGGERED_DONT_REPORT_CAST_ERROR);
+    }
+
+    void Register() override
+    {
+        OnEffectPeriodic += AuraEffectPeriodicFn(spell_maiden_of_virtue_sacred_ground::HandlePeriodic, EFFECT_0, SPELL_AURA_PERIODIC_DUMMY);
+    }
+};
+
+// 227817 - Holy Bulwark
+class spell_maiden_of_virtue_holy_bulwark : public AuraScript
+{
+    void OnAbsorb(AuraEffect const* /*aurEff*/, DamageInfo& /*dmgInfo*/, uint32& /*absorbAmount*/)
+    {
+        InstanceScript* instance = GetCaster()->GetInstanceScript();
+        if (!instance)
+            return;
+
+        if (Creature* maiden = instance->GetCreature(DATA_RTK_MAIDEN_OF_VIRTUE))
+            maiden->AI()->DoAction(ACTION_INTERRUPT_HOLY_WRATH);
+    }
+
+    void Register() override
+    {
+        AfterEffectAbsorb += AuraEffectAbsorbFn(spell_maiden_of_virtue_holy_bulwark::OnAbsorb, EFFECT_0);
+    }
+};
+
+// 227789 - Sacred Ground
+struct at_maiden_of_virtue_sacred_ground : AreaTriggerAI
+{
+    using AreaTriggerAI::AreaTriggerAI;
+
+    void OnUnitEnter(Unit* unit) override
+    {
+        if (!unit->IsPlayer())
+            return;
+
+        unit->CastSpell(unit, SPELL_SACRED_GROUND_PERIODIC, TRIGGERED_IGNORE_CAST_IN_PROGRESS | TRIGGERED_DONT_REPORT_CAST_ERROR);
+    }
+};
+
+void AddSC_boss_rtk_maiden_of_virtue()
+{
+    RegisterReturnToKarazhanCreatureAI(boss_rtk_maiden_of_virtue);
+
+    RegisterSpellScript(spell_maiden_of_virtue_sacred_ground);
+    RegisterSpellScript(spell_maiden_of_virtue_holy_bulwark);
+
+    RegisterAreaTriggerAI(at_maiden_of_virtue_sacred_ground);
+}
