7ed6ef95591c226c962eb14d9ef3545062c598db
Shauren <shauren.trinity@gmail.com>
2025-06-29 00:23:21 +0200

    Scripts: Fixed possible crashes caused by passing nullptr to Trinity::WorldObjectListSearcher constructor
    (cherry picked from commit 5334467f493eb16141f4ad0bce956bc3e19244a0)

--- src/server/scripts/OutdoorPvP/OutdoorPvPNA.cpp
+++ src/server/scripts/OutdoorPvP/OutdoorPvPNA.cpp
@@ -22,6 +22,7 @@
 #include "GridNotifiersImpl.h"
 #include "Map.h"
 #include "ObjectMgr.h"
+#include "PhasingHandler.h"
 #include "Player.h"
 #include "ScriptMgr.h"
 #include "WorldStatePackets.h"
@@ -155,12 +156,12 @@ uint32 OPvPCapturePointNA::GetAliveGuardsCount() const
 {
     Position searchCenter = { -1572.57f, 7945.3f, -22.475f, 2.05949f };
 
-    std::vector<WorldObject*> guards;
+    std::vector<Creature*> guards;
     Trinity::ObjectEntryAndPrivateOwnerIfExistsCheck check(ObjectGuid::Empty, GetControllingFaction() == HORDE ? 18192 : 18256);
-    Trinity::WorldObjectListSearcher<Trinity::ObjectEntryAndPrivateOwnerIfExistsCheck> searcher(nullptr, guards, check, GRID_MAP_TYPE_MASK_CREATURE);
+    Trinity::CreatureListSearcher searcher(PhasingHandler::GetEmptyPhaseShift(), guards, check);
     Cell::VisitGridObjects(searchCenter.GetPositionX(), searchCenter.GetPositionY(), m_PvP->GetMap(), searcher, SIZE_OF_GRIDS);
 
-    return std::count_if(guards.begin(), guards.end(), [](WorldObject* guard) { return guard->IsUnit() && guard->ToUnit()->IsAlive(); });
+    return std::ranges::count_if(guards, [](Creature const* guard) { return guard->IsAlive(); });
 }
 
 Team OPvPCapturePointNA::GetControllingFaction() const
