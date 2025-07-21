1c1e570db04883d6e6a83086ff3a3255a5173c42
Ergar <ergar@gmx.net>
2025-07-22 00:08:57 +0200

    Core/Unit: Named npc flags UNIT_NPC_FLAG_AREA_SPIRIT_HEALER and UNIT_NPC_FLAG_2_AREA_SPIRIT_HEALER_INDIVIDUAL
    (cherry picked from commit 920b9a99bdd2d7eb9b7a787c4872a38b862f75c8)

--- src/server/game/Handlers/NPCHandler.cpp
+++ src/server/game/Handlers/NPCHandler.cpp
@@ -170,7 +170,7 @@ void WorldSession::HandleGossipHelloOpcode(WorldPacket& recvData)
     unit->SetHomePosition(unit->GetPosition());
 
     // If spiritguide, no need for gossip menu, just put player into resurrect queue
-    if (unit->IsSpiritGuide())
+    if (unit->IsAreaSpiritHealer())
     {
         Battleground* bg = _player->GetBattleground();
         if (bg)
@@ -197,7 +197,7 @@ void WorldSession::HandleSpiritHealerActivateOpcode(WorldPacket& recvData)
     ObjectGuid guid;
     recvData >> guid;
 
-    Creature* unit = GetPlayer()->GetNPCIfCanInteractWith(guid, UNIT_NPC_FLAG_SPIRITHEALER);
+    Creature* unit = GetPlayer()->GetNPCIfCanInteractWith(guid, UNIT_NPC_FLAG_SPIRIT_HEALER);
     if (!unit)
     {
         TC_LOG_DEBUG("network", "WORLD: HandleSpiritHealerActivateOpcode - {} not found or you can not interact with him.", guid.ToString());
