b7e58ef72298c935d226ef59f246019143b3af29
Wishmaster117 <140754794+Wishmaster117@users.noreply.github.com>
2025-08-25 15:26:34 +0100

    Update Language.h

--- src/server/game/Miscellaneous/Language.h
+++ src/server/game/Miscellaneous/Language.h
@@ -995,7 +995,7 @@ enum TrinityStrings
     LANG_ACCOUNT_BNET_UNLINKED            = 1216,
     LANG_ACCOUNT_BNET_NOT_LINKED          = 1217,
     LANG_DISALLOW_TICKETS_CONFIG          = 1218,
-	
+
     // Guild list (.guild list)
     LANG_GUILD_LIST_TITLE                 = 1219,
     LANG_GUILD_LIST_HEADER                = 1220,
