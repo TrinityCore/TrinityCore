e592bcf8e36ed7e7227dfaac11eff90012c741fb
Edan <79129327+EdnY1@users.noreply.github.com>
2025-09-04 17:21:33 -0400

    Core/Unit: Also remove snare mechanics caused by effects on RemoveMovementImpairingAuras (#31207)
    (cherry picked from commit 0728b899a76a07f7758b1e0d39e843463d4ea2b5)

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -4299,7 +4299,7 @@ void Unit::RemoveMovementImpairingAuras(bool withRoot)
     if (withRoot)
         RemoveAurasWithMechanic(1 << MECHANIC_ROOT, AURA_REMOVE_BY_DEFAULT, 0, true);
 
-    RemoveAurasWithMechanic(1 << MECHANIC_SNARE, AURA_REMOVE_BY_DEFAULT, 0, false);
+    RemoveAurasWithMechanic(1 << MECHANIC_SNARE, AURA_REMOVE_BY_DEFAULT, 0, true);
 }
 
 void Unit::RemoveAurasWithMechanic(uint64 mechanicMaskToRemove, AuraRemoveMode removeMode, uint32 exceptSpellId, bool withEffectMechanics)
