d02d03d84369f575cda498110f3156ec556568d3
aquadeus <efectokoleos13@gmail.com>
2025-09-04 23:12:38 +0200

    Spells/Mage: Implement talent "Pyrotechnics"

--- src/server/scripts/Spells/spell_mage.cpp
+++ src/server/scripts/Spells/spell_mage.cpp
@@ -86,6 +86,7 @@ enum MageSpells
     SPELL_MAGE_METEOR_MISSILE                    = 153564,
     SPELL_MAGE_MOLTEN_FURY                       = 458910,
     SPELL_MAGE_PHOENIX_FLAMES                    = 257541,
+    SPELL_MAGE_PYROTECHNICS                      = 157644,
     SPELL_MAGE_RADIANT_SPARK_PROC_BLOCKER        = 376105,
     SPELL_MAGE_RAY_OF_FROST_BONUS                = 208141,
     SPELL_MAGE_RAY_OF_FROST_FINGERS_OF_FROST     = 269748,
@@ -1466,6 +1467,33 @@ class spell_mage_prismatic_barrier : public AuraScript
     }
 };
 
+// 157642 - Pyrotechnics
+class spell_mage_pyrotechnics : public AuraScript
+{
+    bool Validate(SpellInfo const* /*spellInfo*/) override
+    {
+        return ValidateSpellInfo({ SPELL_MAGE_PYROTECHNICS });
+    }
+
+    void HandleProc(AuraEffect const* /*aurEff*/, ProcEventInfo& procInfo)
+    {
+        Unit* caster = GetCaster();
+        if (!caster)
+            return;
+
+        if (procInfo.GetHitMask() & PROC_HIT_CRITICAL)
+        {
+            PreventDefaultAction();
+            caster->RemoveAurasDueToSpell(SPELL_MAGE_PYROTECHNICS);
+        }
+    }
+
+    void Register() override
+    {
+        OnEffectProc += AuraEffectProcFn(spell_mage_pyrotechnics::HandleProc, EFFECT_0, SPELL_AURA_PROC_TRIGGER_SPELL);
+    }
+};
+
 // 376103 - Radiant Spark
 class spell_mage_radiant_spark : public AuraScript
 {
@@ -1891,6 +1919,7 @@ void AddSC_mage_spell_scripts()
     RegisterSpellScript(spell_mage_molten_fury);
     RegisterSpellScript(spell_mage_polymorph_visual);
     RegisterSpellScript(spell_mage_prismatic_barrier);
+    RegisterSpellScript(spell_mage_pyrotechnics);
     RegisterSpellScript(spell_mage_radiant_spark);
     RegisterSpellAndAuraScriptPair(spell_mage_ray_of_frost, spell_mage_ray_of_frost_aura);
     RegisterSpellScript(spell_mage_ring_of_frost);
