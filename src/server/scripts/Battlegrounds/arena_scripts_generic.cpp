987ff569247e5a852970b5c998f7c98fbe143c63
Golrag <golrag.jeremy@gmail.com>
2025-09-13 19:16:29 +0200

    include gametime

--- src/server/scripts/Battlegrounds/arena_scripts_generic.cpp
+++ src/server/scripts/Battlegrounds/arena_scripts_generic.cpp
@@ -18,6 +18,7 @@
 #include "ScriptMgr.h"
 #include "Battleground.h"
 #include "ConditionMgr.h"
+#include "GameTime.h"
 #include "Map.h"
 
 class condition_is_shadow_sight_enabled : public ConditionScript
