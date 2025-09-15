allankrdec/TrinityCore-10-2024

Scripts/Stratholme: Added missing dungeon encounter ids

src/server/scripts/EasternKingdoms/Stratholme/boss_postmaster_malown.cpp
@@ -59,7 +59,7 @@ class boss_postmaster_malown : public CreatureScript
 
         struct boss_postmaster_malownAI : public BossAI
         {
-            boss_postmaster_malownAI(Creature* creature) : BossAI(creature, TYPE_MALOWN) { }
+            boss_postmaster_malownAI(Creature* creature) : BossAI(creature, BOSS_POSTMASTER_MALOWN) { }
 
             void Reset() override { }
 

TrinityCore/TrinityCore

(cherry picked from commit 4370f2fe2933d54be4ed4dd815d62ff34a701ed2)

src/server/scripts/EasternKingdoms/Stratholme/boss_postmaster_malown.cpp
@@ -49,7 +49,7 @@ enum MalownEvents
 // 11143 - Postmaster Malown
 struct boss_postmaster_malown : public BossAI
 {
-    boss_postmaster_malown(Creature* creature) : BossAI(creature, TYPE_MALOWN) { }
+    boss_postmaster_malown(Creature* creature) : BossAI(creature, BOSS_POSTMASTER_MALOWN) { }
 
     void Reset() override
     {