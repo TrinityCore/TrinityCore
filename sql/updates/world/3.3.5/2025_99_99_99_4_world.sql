5a324aad91cfffa8928d116f7c59de0178418efd
Thor1423 <info@clangaming.eu>
2025-09-15 02:43:15 +0200

    Add files via upload
    (cherry picked from commit eeda2ccf7534112f05038beeb99c17f24bcf47a4)

---
+++ sql/updates/world/3.3.5/2025_99_99_99_4_world.sql
@@ -0,0 +1,24 @@
+--
+UPDATE `gameobject_template` SET `ScriptName` = 'go_188499_chest' WHERE `entry` = 188499;
+
+-- Texts expanded by the NPC speaking their text when they attack a player.
+
+DELETE FROM `creature_text` WHERE `CreatureID` = 26811;
+INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES
+(26811, 0, 0, 'I could sleep forever, mon....', 12, 0, 100, 0, 0, 0, 26149, 0, 'Ancient Drakkari Warmonger'),
+(26811, 0, 1, 'Finally, I can be restin\'...', 12, 0, 100, 0, 0, 0, 26151, 0, 'Ancient Drakkari Warmonger'),
+(26811, 0, 2, 'Free at last, mon.', 12, 0, 100, 0, 0, 0, 26148, 0, 'Ancient Drakkari Warmonger'),
+(26811, 0, 3, 'Where\'s mah mummy?', 12, 0, 100, 0, 0, 0, 26150, 0, 'Ancient Drakkari Warmonger'),
+(26811, 1, 0, 'Dat me liver you be squeezin, mon!', 12, 0, 100, 0, 0, 0, 26087, 0, 'Ancient Drakkari Warmonger'),
+(26811, 1, 1, 'You take my heart, now I take yours!', 12, 0, 100, 0, 0, 0, 26085, 0, 'Ancient Drakkari Warmonger'),
+(26811, 1, 2, 'Why ya wanna mess wit me innards, mon?', 12, 0, 100, 0, 0, 0, 26086, 0, 'Ancient Drakkari Warmonger');
+
+DELETE FROM `creature_text` WHERE `CreatureID` = 26812;
+INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES
+(26812, 0, 0, 'I could sleep forever, mon....', 12, 0, 100, 0, 0, 0, 26149, 0, 'Ancient Drakkari Soothsayer'),
+(26812, 0, 1, 'Finally, I can be restin\'...', 12, 0, 100, 0, 0, 0, 26151, 0, 'Ancient Drakkari Soothsayer'),
+(26812, 0, 2, 'Free at last, mon.', 12, 0, 100, 0, 0, 0, 26148, 0, 'Ancient Drakkari Soothsayer'),
+(26812, 0, 3, 'Where\'s mah mummy?', 12, 0, 100, 0, 0, 0, 26150, 0, 'Ancient Drakkari Soothsayer'),
+(26812, 1, 0, 'Dat me liver you be squeezin, mon!', 12, 0, 100, 0, 0, 0, 26087, 0, 'Ancient Drakkari Soothsayer // Ancient Drakkari Warmonger'),
+(26812, 1, 1, 'You take my heart, now I take yours!', 12, 0, 100, 0, 0, 0, 26085, 0, 'Ancient Drakkari Soothsayer // Ancient Drakkari Warmonger'),
+(26812, 1, 2, 'Why ya wanna mess wit me innards, mon?', 12, 0, 100, 0, 0, 0, 26086, 0, 'Ancient Drakkari Soothsayer // Ancient Drakkari Warmonger');
