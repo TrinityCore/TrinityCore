d7e6d338a8b0488dd92be935debdac151c7498f7
Shauren <shauren.trinity@gmail.com>
2025-09-20 19:01:52 +0200

    Core/Spells: Remove health override for Lightwell summon
    Closes #31269
    
    (cherry picked from commit b64567f98fc217c670416a3bfebefecef288a314)

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -1962,6 +1962,7 @@ void Spell::EffectSummonType()
                     // Summons a vehicle, but doesn't force anyone to enter it (see SUMMON_CATEGORY_VEHICLE)
                 case SummonTitle::Vehicle:
                 case SummonTitle::Mount:
+                case SummonTitle::Lightwell:
                 {
                     if (!unitCaster)
                         return;
@@ -1969,7 +1970,6 @@ void Spell::EffectSummonType()
                     summon = unitCaster->GetMap()->SummonCreature(entry, *destTarget, properties, duration, unitCaster, m_spellInfo->Id, 0, privateObjectOwner);
                     break;
                 }
-                case SummonTitle::Lightwell:
                 case SummonTitle::Totem:
                 {
                     if (!unitCaster)
