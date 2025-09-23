c77b07aa3f7db6f4a494fc9f4098407cf2414ef0
Meji <alvaro.megias@outlook.com>
2025-09-23 15:48:58 +0200

    Core/GameEvents: Added holiday/event world state field (#31284)
     Closes #30622
    
    (cherry picked from commit c575b397c591e7051c325046204da61086a0dbb9)

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -2032,6 +2032,9 @@ bool World::SetInitialWorldSettings()
     TC_LOG_INFO("server.loading", "Loading Quest Pooling Data...");
     sQuestPoolMgr->LoadFromDB();                                // must be after quest templates
 
+    TC_LOG_INFO("server.loading", "Loading World State templates...");
+    sWorldStateMgr->LoadFromDB();                               // must be loaded before battleground, outdoor PvP, game events and conditions
+
     TC_LOG_INFO("server.loading", "Loading Game Event Data...");               // must be after loading pools fully
     sGameEventMgr->LoadFromDB();
 
@@ -2244,9 +2247,6 @@ bool World::SetInitialWorldSettings()
 
     TC_LOG_INFO("server.loading", "Loading Creature Formations...");
     sFormationMgr->LoadCreatureFormations();
-
-    TC_LOG_INFO("server.loading", "Loading World State templates...");
-    sWorldStateMgr->LoadFromDB();                               // must be loaded before battleground, outdoor PvP and conditions
 
     TC_LOG_INFO("server.loading", "Loading Persistend World Variables...");
     LoadPersistentWorldVariables();
