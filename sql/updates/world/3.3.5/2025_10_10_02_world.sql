e7cb86d369b8e5e92daa3a8b9f123fea380fcc8c
ccrs <ccrs@users.noreply.github.com>
2025-10-10 18:36:33 +0200

    Scripts/ICC: override default jump behavior in Sindragosa's Icy Grip Jump - she pulls everyone up and into the model, not just to the unit's margin

---
+++ sql/updates/world/3.3.5/2025_10_10_02_world.sql
@@ -0,0 +1,4 @@
+-- Icy Grip Jump
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_sindragosa_icy_grip_jump';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(70122, 'spell_sindragosa_icy_grip_jump');
