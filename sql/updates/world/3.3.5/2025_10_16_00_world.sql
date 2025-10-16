da8f7ec0db856c353231c0dda143bae54bde12cb
ccrs <ccrs@users.noreply.github.com>
2025-10-16 18:26:18 +0200

    much better

--- sql/updates/world/3.3.5/2025_10_16_00_world.sql
+++ sql/updates/world/3.3.5/2025_10_16_00_world.sql
@@ -1,5 +1,5 @@
 --
-UPDATE `spell_dbc` SET `ProcChance` = 101, `Effect1` = 140, `EffectImplicitTargetA1` = 25, `RangeIndex`= 13,`EffectTriggerSpell1` = 47681 WHERE `Id` = 47680;
+UPDATE `spell_dbc` SET `ProcChance` = 101, `Effect1` = 140, `EffectImplicitTargetA1` = 25, `RangeIndex`= 12,`EffectTriggerSpell1` = 47681 WHERE `Id` = 47680;
 
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId` = 13 AND `SourceEntry` = 47681;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceGroup`,`SourceEntry`,`SourceId`,`ElseGroup`,`ConditionTypeOrReference`,`ConditionTarget`,`ConditionValue1`,`ConditionValue2`,`ConditionValue3`,`NegativeCondition`,`ErrorType`,`ErrorTextId`,`ScriptName`,`Comment`) VALUES
@@ -18,9 +18,10 @@ INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Lan
 (26812,1,2,"Dat me liver you be squeezin, mon!",12,0,100,0,0,0,26087,0,"Ancient Drakkari Soothsayer"),
 (26812,1,3,"Come back here with me guts, $r!",12,0,100,0,0,0,26088,0,"Ancient Drakkari Soothsayer");
 
-DELETE FROM `smart_scripts` WHERE `entryorguid` = 26811 AND `source_type` = 0 AND `id` IN (12,13);
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 26811 AND `source_type` = 0 AND `id` IN (9,12,13);
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 26812 AND `source_type` = 0 AND `id` IN (21,22);
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`event_param5`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_param4`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(26811,0,9,0,34,0,100,0,8,1,0,0,80,2681200,2,0,0,0,0,1,0,0,0,0,0,0,0,'On movement of type POINT_MOTION_TYPE inform, point 1 - Self: Start timed action list id #Ancient Drakkari Soothsayer #0 (2681200) (update always)'),
 (26811,0,12,0,8,0,100,0,47681,0,0,0,0,1,1,0,0,0,0,0,7,0,0,0,0,0,0,0,0,"Ancient Drakkari Warmonger - On Spellhit 'Aggro Ancient Drakkari' - Say Line 1"),
 (26811,0,13,0,8,0,100,0,47681,0,0,0,0,49,0,0,0,0,0,0,7,0,0,0,0,0,0,0,0,"Ancient Drakkari Warmonger - On Spellhit 'Aggro Ancient Drakkari' - Start Attacking"),
 
