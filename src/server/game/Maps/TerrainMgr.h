e2f80f8f6c39956b231bbb2fe57862c572eb14d2
Shauren <shauren.trinity@gmail.com>
2025-10-17 12:47:33 +0200

    Tools/mmaps_generator: Namespace/include cleanup

--- src/server/game/Maps/TerrainMgr.h
+++ src/server/game/Maps/TerrainMgr.h
@@ -112,7 +112,7 @@ private:
     std::mutex _loadMutex;
     std::unique_ptr<GridMap> _gridMap[MAX_NUMBER_OF_GRIDS][MAX_NUMBER_OF_GRIDS];
     std::atomic<uint16> _referenceCountFromMap[MAX_NUMBER_OF_GRIDS][MAX_NUMBER_OF_GRIDS];
-    std::array<uint64, (MAX_NUMBER_OF_GRIDS * MAX_NUMBER_OF_GRIDS + 63) / 64> _loadedGrids;
+    std::array<uint64, MAX_NUMBER_OF_GRIDS> _loadedGrids;
     std::bitset<MAX_NUMBER_OF_GRIDS * MAX_NUMBER_OF_GRIDS> _gridFileExists; // cache what grids are available for this map (not including parent/child maps)
 
     static constexpr Milliseconds CleanupInterval = 1min;
