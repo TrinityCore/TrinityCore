616e2ac8664f7b3023703fa4162f88375cf71950
ze10r <ze10r@users.noreply.github.com>
2025-10-17 12:38:24 +0200

    DB/Quest: Fix "The Hunt Begins" "A Task Unfinished" ruRU locales
    closes #31233
    
    (cherry picked from commit 1085643b44525df8215b464198751b6f2f174946)
    
    (cherry picked from commit 79f7da4099bf73535c1fa04949d94f70b7223559)

---
+++ sql/updates/world/cata_classic/2026_08_11_217_world_2026_06_28_00_world_2025_10_17_03_world.sql
@@ -0,0 +1,5 @@
+-- The Hunt Begins
+UPDATE `quest_request_items_locale` SET `CompletionText` = 'Добыча пищи и перьев для племени – первый шаг на пути к завоеванию расположения вождя.' WHERE `id` = 747 AND `locale` = 'ruRU';
+
+-- A Task Unfinished
+UPDATE `quest_request_items_locale` SET `CompletionText` = 'Приветствую! Чем Каут может вам помочь?$B$BПуть сюда от самого плато неблизок: кто-то всю дорогу думает об уставших мышцах и стертых ногах, а кто-то наслаждается великолепными пейзажами... А вы что скажете?' WHERE `id` = 1656 AND `locale` = 'ruRU';
