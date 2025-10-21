05afc3aa8950a5240b07472bb887bccbaf96101b
ccrs <ccrs@users.noreply.github.com>
2025-10-21 20:00:55 +0200

    Core/Movement: allow to manually set up SWIM flag on init splines

--- src/server/game/Movement/Spline/MoveSplineInit.h
+++ src/server/game/Movement/Spline/MoveSplineInit.h
@@ -144,6 +144,8 @@ namespace Movement
          */
         void SetVelocity(float velocity);
 
+        void SetSwim();
+
         PointsArray& Path() { return args.path; }
 
         /* Disables transport coordinate transformations for cases where raw offsets are available
@@ -164,6 +166,7 @@ namespace Movement
     inline void MoveSplineInit::SetTransportEnter() { args.flags.EnableTransportEnter(); }
     inline void MoveSplineInit::SetTransportExit() { args.flags.EnableTransportExit(); }
     inline void MoveSplineInit::SetOrientationFixed(bool enable) { args.flags.orientationFixed = enable; }
+    inline void MoveSplineInit::SetSwim() { args.flags.EnableSwim(); }
 
     inline void MoveSplineInit::SetParabolic(float amplitude, float time_shift)
     {
