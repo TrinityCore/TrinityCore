7c3d56a9b21f628b04718125b3d5bbca5445a8a5
Shauren <shauren.trinity@gmail.com>
2025-10-26 15:42:35 +0100

    Core/Units: Correct Z coord change when enabling hover above ground
    (cherry picked from commit 8d49be5e1cb84e26b93fa2cde415b3eba9353164)

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -13601,7 +13601,7 @@ bool Unit::SetHover(bool enable, bool updateAnimTier /*= true*/)
         //! No need to check height on ascent
         AddUnitMovementFlag(MOVEMENTFLAG_HOVER);
         if (hoverHeight && GetPositionZ() - GetFloorZ() < hoverHeight)
-            UpdateHeight(GetPositionZ() + hoverHeight);
+            UpdateHeight(std::max(GetFloorZ() + hoverHeight, GetPositionZ()));
     }
     else
     {
