004b33c34f13b639583cce21c94e461079c55b4c
Mematoru <mematoru@gmail.com>
2025-11-07 18:55:18 +0200

    Spells/Priest: Protector of the Frail talent cd reduction fixed

---
+++ sql/updates/world/master/9999_99_99_99_world_protector_of_the_frail.sql
@@ -0,0 +1,13 @@
+DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_pri_protector_of_the_frail');
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(373036, 'spell_pri_protector_of_the_frail');
+
+DELETE FROM `spell_proc` WHERE `SpellId` IN (373035);
+INSERT INTO `spell_proc` (`SpellId`,`SchoolMask`,`SpellFamilyName`,`SpellFamilyMask0`,`SpellFamilyMask1`,`SpellFamilyMask2`,`SpellFamilyMask3`,`ProcFlags`,`ProcFlags2`,`SpellTypeMask`,`SpellPhaseMask`,`HitMask`,`AttributesMask`,`DisableEffectsMask`,`ProcsPerMinute`,`Chance`,`Cooldown`,`Charges`) VALUES
+(373035,0x00,6,0x00000001,0x00000000,0x00000000,0x00000000,0x0,0x0,0x0,0x1,0x403,0x0,0x0,0,0,0,0); -- Protector of the Frail
+
+
+
+DELETE FROM `spell_proc` WHERE `SpellId` IN (368275);
+INSERT INTO `spell_proc` (`SpellId`,`SchoolMask`,`SpellFamilyName`,`SpellFamilyMask0`,`SpellFamilyMask1`,`SpellFamilyMask2`,`SpellFamilyMask3`,`ProcFlags`,`ProcFlags2`,`SpellTypeMask`,`SpellPhaseMask`,`HitMask`,`AttributesMask`,`DisableEffectsMask`,`ProcsPerMinute`,`Chance`,`Cooldown`,`Charges`) VALUES
+(368275,0x00,6,0x00001800,0x00000000,0x00000000,0x00000000,0x0,0x0,0x2,0x2,0x403,0x0,0x0,0,0,0,0); -- Binding Heals
