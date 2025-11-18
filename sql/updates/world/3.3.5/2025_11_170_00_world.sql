a768fbc74291e3ed54c3a44730c6f1e4c6aed6dc
offl <suffle.o@gmail.com>
2025-11-18 16:50:24 +0200

    pt 3

--- sql/updates/world/3.3.5/2025_11_170_00_world.sql
+++ sql/updates/world/3.3.5/2025_11_170_00_world.sql
@@ -63,3 +63,22 @@ DELETE FROM `spell_script_names` WHERE `ScriptName` IN (
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (49625, 'spell_fjord_braves_flare'),
 (49634, 'spell_fjord_sergeants_flare');
+
+--
+DELETE FROM `spell_scripts` WHERE `id` = 44563;
+
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 24823;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 24823 AND `source_type` = 0;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`event_param5`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_param4`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(24823,0,0,0,8,0,100,512,44563,0,0,0,0,11,44569,0,0,0,0,0,1,0,0,0,0,0,0,0,0,"Iron Rune Construct - On Spellhit 'Bluff Quest Credit' - Cast 'Bluff Quest Credit'");
+
+--
+DELETE FROM `spell_scripts` WHERE `id` = 51659;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 28379 AND `source_type` = 0 AND `id` = 2;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`event_param5`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_param4`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(28379,0,2,0,8,0,100,0,51659,0,0,0,0,11,51656,0,0,0,0,0,7,0,0,0,0,0,0,0,0,"Shattertusk Mammoth - On Spellhit 'Apply Harness' - Cast 'Mount Mammoth Cue'");
+
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_sholazar_mount_mammoth_cue';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(51656, 'spell_sholazar_mount_mammoth_cue');
