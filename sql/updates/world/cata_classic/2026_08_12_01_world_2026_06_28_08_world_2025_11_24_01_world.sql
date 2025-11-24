94b962e7a2685950fef921f09e38a6b48faa4c1c
Aokromes <aokromes@users.noreply.github.com>
2025-11-24 22:18:42 +0100

    DB/Creature: Update Old Ironbark spawn position
    closes #31414
    by CraftedRO
    
    (cherry picked from commit a97e1668bff634e8a9a233e674397dce8cb44f86)
    
    (cherry picked from commit c91d93ce8cc81f2e310ffd25be8a528c422476c5)

---
+++ sql/updates/world/cata_classic/2026_08_12_01_world_2026_06_28_08_world_2025_11_24_01_world.sql
@@ -0,0 +1,2 @@
+-- Old Ironbark (Area: 0 - Difficulty: 0) CreateObject1
+UPDATE `creature` SET `position_x`=-72.1718673706054687, `position_y`=-283.25238037109375, `position_z`=-57.749664306640625, `orientation`=4.084070205688476562, `VerifiedBuild`=11159 WHERE `guid`=56943 AND `id`=11491;
