623ac8a47ccaf9f10afb33954a3e12b2890ef52b
MoltenCrystal <moltencrystal_dev@outlook.com>
2025-12-12 15:56:51 +0100

    Spells/Warrior: Implement Finishing Blows

---
+++ sql/updates/world/master/9999_99_99_99_world_finishing_blows.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_script_names` WHERE `ScriptName` = 'spell_warr_finishing_blows';
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(400205,'spell_warr_finishing_blows');
