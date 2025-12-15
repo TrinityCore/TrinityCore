8b9dc7676eb9926026f9ff431f28c04a3804136b
FlyingArowana <TheSCREWEDSoftware@users.noreply.github.com>
2025-12-15 20:38:28 +0000

    Revert "Create 2025_12_15_08_world.sql"
    This reverts commit 96da7c5a4f0352cf861710a95c4c8f4fabf8a8ac.

--- sql/updates/world/3.3.5/2025_12_15_08_world.sql
+++
@@ -1,3 +0,0 @@
--- Fixes Zangarmarsh Quest "A Job Undone" invalid prerequisite quest
--- closes https://github.com/azerothcore/azerothcore-wotlk/issues/21708
-UPDATE `quest_template_addon` SET `PrevQuestID` = 9773 WHERE (`ID` = 9899);
