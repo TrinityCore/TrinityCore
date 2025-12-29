1ea4cc20f9033f51ae3a01d6cd57221116c77c9f
Shauren <shauren.trinity@gmail.com>
2025-12-30 00:18:50 +0100

    add missing script registration

--- sql/updates/world/master/9999_99_99_99_world_entropic_rift.sql
+++ sql/updates/world/master/9999_99_99_99_world_entropic_rift.sql
@@ -6,8 +6,9 @@ DELETE FROM `areatrigger_create_properties` WHERE `Id`=33200 AND `IsCustom`=0;
 INSERT INTO `areatrigger_create_properties` (`Id`,`IsCustom`,`AreaTriggerId`,`IsAreatriggerCustom`,`Flags`,`MoveCurveId`,`ScaleCurveId`,`MorphCurveId`,`FacingCurveId`,`AnimId`,`AnimKitId`,`DecalPropertiesId`,`SpellForVisuals`,`TimeToTargetScale`,`Speed`,`SpeedIsTime`,`Shape`,`ShapeData0`,`ShapeData1`,`ShapeData2`,`ShapeData3`,`ShapeData4`,`ShapeData5`,`ShapeData6`,`ShapeData7`,`ScriptName`,`VerifiedBuild`) VALUES
 (33200,0,35700,0,0,0,0,0,0,-1,0,0,447445,20000,12,0,0,4,4,0,0,0,0,0,0,'areatrigger_pri_entropic_rift',64978);
 
-DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_pri_entropic_rift','spell_pri_entropic_rift_aura');
+DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_pri_entropic_rift','spell_pri_entropic_rift_aura','spell_pri_entropic_rift_periodic');
 INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
 (8092,'spell_pri_entropic_rift'),
 (263165,'spell_pri_entropic_rift'),
-(450193,'spell_pri_entropic_rift_aura');
+(450193,'spell_pri_entropic_rift_aura'),
+(459314,'spell_pri_entropic_rift_periodic');
