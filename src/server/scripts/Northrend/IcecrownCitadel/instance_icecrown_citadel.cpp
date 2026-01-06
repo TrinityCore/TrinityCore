25303dbaba4078d563e5f6706c891ae608043147
Ergar <ergar@gmx.net>
2026-01-06 02:03:35 +0100

    Update instance_icecrown_citadel.cpp

--- src/server/scripts/Northrend/IcecrownCitadel/instance_icecrown_citadel.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/instance_icecrown_citadel.cpp
@@ -370,8 +370,8 @@ class instance_icecrown_citadel : public InstanceMapScript
                         break;
                     case NPC_ZAFOD_BOOMBOX:
                         if (GameObjectTemplate const* go = sObjectMgr->GetGameObjectTemplate(GO_THE_SKYBREAKER_A))
-                            if ((TeamInInstance == ALLIANCE && int32(data->mapId) == go->moTransport.mapID) ||
-                                (TeamInInstance == HORDE && int32(data->mapId) != go->moTransport.mapID))
+                            if ((GetData(DATA_TEAM_IN_INSTANCE) == ALLIANCE && int32(data->mapId) == go->moTransport.mapID) ||
+                                (GetData(DATA_TEAM_IN_INSTANCE) == HORDE && int32(data->mapId) != go->moTransport.mapID))
                                 return entry;
                         return 0;
                     case NPC_IGB_MURADIN_BRONZEBEARD:
