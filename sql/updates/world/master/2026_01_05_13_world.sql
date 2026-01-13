5f1eceebf48161d9a2d36c2609d6e5a1fe867935
Naddley <64811442+Naddley@users.noreply.github.com>
2026-01-13 11:38:34 +0100

    update

--- sql/updates/world/master/2026_01_05_13_world.sql
+++ sql/updates/world/master/2026_01_05_13_world.sql
@@ -11,7 +11,7 @@ UPDATE `quest_request_items` SET `VerifiedBuild`=65299 WHERE `ID`=66577;
 -- Template
 UPDATE `creature_template_gossip` SET `VerifiedBuild`=64978 WHERE (`CreatureID`=189569 AND `MenuID`=28666);
 
--- Gossiü
+-- Gossip
 UPDATE `gossip_menu` SET `VerifiedBuild`=64978 WHERE (`MenuID`=28666 AND `TextID`=590013);
 UPDATE `gossip_menu_option` SET `VerifiedBuild`=64978 WHERE (`MenuID`=28666 AND `OptionID`=1);
 
@@ -22,5 +22,5 @@ DELETE FROM `smart_scripts` WHERE `id` IN (0, 1, 2, 3) AND `entryOrGuid` = @ENTR
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `action_param7`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`, `Difficulties`) VALUES
 (@ENTRY, 0, 0, 1, 64, 0, 100, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'On player opened gossip - Self: Talk 0 to invoker', ''),
 (@ENTRY, 0, 1, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 153, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'On player opened gossip - Gossip player: Complete Meet Wrathion in Stormwind objective (189569)', ''),
-(@ENTRY, 0, 2, 2, 62, 0, 100, 0, 28666, 1, 0, 0, 0, 85, 373796, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'On gossip action 1 from menu 28666 selected - Gossip player: Cast spell 373796 on self', ''),
+(@ENTRY, 0, 2, 3, 62, 0, 100, 0, 28666, 1, 0, 0, 0, 85, 373796, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'On gossip action 1 from menu 28666 selected - Gossip player: Cast spell 373796 on self', ''),
 (@ENTRY, 0, 3, 0, 61, 0, 100, 0, 0, 0, 0, 0, 0, 72, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'On gossip action 1 from menu 28666 selected - Gossip player: Close gossip', '');
