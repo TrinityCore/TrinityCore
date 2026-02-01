e61ed8ee8f610c928fb91777add0988447023dda
ModoX <moardox@gmail.com>
2026-02-01 13:39:02 +0100

    reorder includes

--- src/server/scripts/Pandaria/zone_the_wandering_isle.cpp
+++ src/server/scripts/Pandaria/zone_the_wandering_isle.cpp
@@ -15,6 +15,8 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "AreaTrigger.h"
+#include "AreaTriggerDataStore.h"
 #include "CellImpl.h"
 #include "Containers.h"
 #include "CreatureAI.h"
@@ -31,9 +33,6 @@
 #include "SpellScript.h"
 #include "TaskScheduler.h"
 #include "TemporarySummon.h"
-#include "AreaTrigger.h"
-#include "AreaTriggerDataStore.h"
-#include "DB2Stores.h"
 
 namespace Scripts::Pandaria::TheWanderingIsle
 {
