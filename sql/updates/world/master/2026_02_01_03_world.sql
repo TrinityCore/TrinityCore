8c3c091e6a444c4938c9c696588dba9a25e021ce
ModoX <moardox@gmail.com>
2026-02-01 20:08:54 +0100

    Update spell_proc for Thorns of Iron

--- sql/updates/world/master/2026_02_01_03_world.sql
+++ sql/updates/world/master/2026_02_01_03_world.sql
@@ -5,5 +5,3 @@ INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 DELETE FROM `spell_proc` WHERE `SpellId` IN (400222);
 INSERT INTO `spell_proc` (`SpellId`,`SchoolMask`,`SpellFamilyName`,`SpellFamilyMask0`,`SpellFamilyMask1`,`SpellFamilyMask2`,`SpellFamilyMask3`,`ProcFlags`,`ProcFlags2`,`SpellTypeMask`,`SpellPhaseMask`,`HitMask`,`AttributesMask`,`DisableEffectsMask`,`ProcsPerMinute`,`Chance`,`Cooldown`,`Charges`) VALUES
 (400222,0x00,7,0x00000000,0x00000000,0x00004000,0x00000000,0x0,0x0,0x0,0x4,0x0,0x0,0x0,0,0,0,0); -- Thorns of Iron
-
-
