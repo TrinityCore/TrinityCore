489ebf8e631d2c69bdf57027467b66acc3733d0f
aquadeus <efectokoleos13@gmail.com>
2026-02-05 21:16:25 +0100

    Spells/Dk: Implement Blood Bond talent

---
+++ sql/updates/world/master/2026_99_99_99_dk_blood_bond.sql
@@ -0,0 +1,6 @@
+UPDATE `creature_template` SET `RegenHealth`=0, `ScriptName`='npc_pet_dk_risen_ghoul' WHERE `entry`=26125;
+
+DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_dk_birth', 'spell_dk_blood_bond_periodic');
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(1217740, 'spell_dk_birth'),
+(1267044, 'spell_dk_blood_bond_periodic');
