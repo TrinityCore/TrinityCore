ecf7c346eb3f57aa1075833e05ed7d10261bca14
Golrag <golrag.jeremy@gmail.com>
2026-02-14 14:26:17 +0100

    Scripts/Spells: Implement Rampaging Ruin (Fury Warrior Talent)

---
+++ sql/updates/world/master/2026_mm_dd_xx_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_warr_rampaging_ruin');
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(184367, 'spell_warr_rampaging_ruin');
