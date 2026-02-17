242960027541798a6c6cedb9b58ccb6e10fe77bc
MoltenCrystal <moltencrystal_dev@outlook.com>
2026-02-17 10:14:02 +0100

    Disable unused proc from legion artifact

--- sql/updates/world/master/9999_99_99_99_world_executioners_precision.sql
+++ sql/updates/world/master/9999_99_99_99_world_executioners_precision.sql
@@ -5,3 +5,7 @@ INSERT INTO `spell_proc` (`SpellId`,`SchoolMask`,`SpellFamilyName`,`SpellFamilyM
 DELETE FROM `spell_proc` WHERE `SpellId` IN (386633);
 INSERT INTO `spell_proc` (`SpellId`,`SchoolMask`,`SpellFamilyName`,`SpellFamilyMask0`,`SpellFamilyMask1`,`SpellFamilyMask2`,`SpellFamilyMask3`,`ProcFlags`,`ProcFlags2`,`SpellTypeMask`,`SpellPhaseMask`,`HitMask`,`AttributesMask`,`DisableEffectsMask`,`ProcsPerMinute`,`Chance`,`Cooldown`,`Charges`) VALUES
 (386633,0x00,4,0x02000000,0x00000000,0x00000000,0x00000000,0x0,0x0,0x0,0x4,0x0,0x0,0x0,0,0,0,1); -- Executioner's Precision
+
+DELETE FROM `disables` WHERE `sourceType`=0 AND `entry`=238147;
+INSERT INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) VALUES 
+(0, 238147, 1, '', '', 'Disable unused Executioners Precision proc');
