b73660e558c270029edb93f4d34207a310cfa5a7
ModoX <moardox@gmail.com>
2026-02-19 20:46:31 +0100

    drop unnecessary change

--- src/server/scripts/Pet/pet_dk.cpp
+++ src/server/scripts/Pet/pet_dk.cpp
@@ -32,7 +32,7 @@ enum DeathKnightSpells
     SPELL_DK_SUMMON_GARGOYLE_1      = 49206,
     SPELL_DK_SUMMON_GARGOYLE_2      = 50514,
     SPELL_DK_DISMISS_GARGOYLE       = 50515,
-    SPELL_DK_SANCTUARY              = 54661,
+    SPELL_DK_SANCTUARY              = 54661
 };
 
 // 28017 - Bloodworm
