46b8c32fdaa427fc2a98d7015b0fd2e1791c1b3c
Shauren <shauren.trinity@gmail.com>
2026-03-04 19:59:49 +0100

    prepare merge 3

--- sql/updates/world/master/2026_03_04_00_world.sql
+++ sql/updates/world/master/2026_03_04_00_world.sql
@@ -1,6 +1,6 @@
 DELETE FROM `spell_proc` WHERE `SpellId` IN (85739);
 INSERT INTO `spell_proc` (`SpellId`,`SchoolMask`,`SpellFamilyName`,`SpellFamilyMask0`,`SpellFamilyMask1`,`SpellFamilyMask2`,`SpellFamilyMask3`,`ProcFlags`,`ProcFlags2`,`SpellTypeMask`,`SpellPhaseMask`,`HitMask`,`AttributesMask`,`DisableEffectsMask`,`ProcsPerMinute`,`Chance`,`Cooldown`,`Charges`) VALUES
-(85739,0x00,4,0x00300022,0x00202700,0x00000120,0x00181000,0x0,0x0,0x0,0x1,0x0,0x10,0x0,0,0,0,0); -- Whirlwind
+(85739,0x00,4,0x00000000,0x00000000,0x00000000,0x00000000,0x0,0x0,0x0,0x1,0x0,0x18,0x0,0,0,0,0); -- Whirlwind
 
 DELETE FROM `spell_script_names` WHERE `ScriptName` IN ('spell_warr_improved_whirlwind_cleave');
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
