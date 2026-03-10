566477a858f71795bdaf541db66697b0c761638b
Meji <alvaro.megias@outlook.com>
2026-03-10 21:33:32 +0100

    DB/WorldStates: Added new worldstates related to dungeon encounters

---
+++ sql/updates/world/master/2026_03_10_02_world.sql
@@ -0,0 +1,15 @@
+DELETE FROM `world_state` WHERE `ID` IN (18066, 24904, 28305, 28307, 28309, 28311, 28313, 28315, 28255, 28303, 29079, 29044, 29957);
+INSERT INTO `world_state` (`ID`, `DefaultValue`, `MapIDs`, `AreaIDs`, `ScriptName`, `Comment`) VALUES
+(18066, 0, '2236', NULL, '', 'Darkmaul Citadel - Kalecgos - Encounter completed'),
+(24904, 0, '2648', NULL, '', 'The Rookery - Voidstone Monstrosity - Encounter completed'),
+(28305, 0, '2912', NULL, '', 'The Voidspire - Imperator Averzian - Encounter completed'),
+(28307, 0, '2912', NULL, '', 'The Voidspire - Vorasius - Encounter completed'),
+(28309, 0, '2912', NULL, '', 'The Voidspire - Vaelgor & Ezzorak - Encounter completed'),
+(28311, 0, '2912', NULL, '', 'The Voidspire - Fallen-King Salhadaar - Encounter completed'),
+(28313, 0, '2912', NULL, '', 'The Voidspire - Lightblinded Vanguard - Encounter completed'),
+(28315, 0, '2912', NULL, '', 'The Voidspire - Crown of the Cosmos - Encounter completed'),
+(28255, 0, '2913', NULL, '', 'March on Quel''Danas - Belo''ren, Child of Al''ar - Encounter completed'),
+(28303, 0, '2913', NULL, '', 'March on Quel''Danas - Midnight Falls - Encounter completed'),
+(29079, 0, '2939', NULL, '', 'The Dreamrift - Chimaerus the Undreamt God - Encounter completed'),
+(29044, 0, '2950', NULL, '', 'Vault of the Incarnates - Raszageth the Storm-Eater - Encounter completed'),
+(29957, 0, '2966', NULL, '', 'Torment''s Rise - Nullaeus - Encounter completed');
