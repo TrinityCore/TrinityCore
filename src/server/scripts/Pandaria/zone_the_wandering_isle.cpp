0793843d5642f54b8dec4588a88c7f11da8e3544
.:::ReZaRr:::. <sudazayavochki@gmail.com>
2026-03-31 20:30:38 +0300

    fix

--- src/server/scripts/Pandaria/zone_the_wandering_isle.cpp
+++ src/server/scripts/Pandaria/zone_the_wandering_isle.cpp
@@ -1590,7 +1590,8 @@ struct npc_cai : public ScriptedAI
             Unit* summoner = summon->GetSummonerUnit();
             if (!summoner)
                 return;
-
+            
+            me->AI()->Talk(Talks::CaiTalk2);
             me->GetMotionMaster()->MoveFleeing(summoner);
             me->DespawnOrUnsummon(3s);
         });
