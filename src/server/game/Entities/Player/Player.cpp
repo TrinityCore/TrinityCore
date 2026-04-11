7178c5fe25ca9dd37dad9af90f02e58f1500149e
stevebone <zavoianu.stefan@gmail.com>
2026-04-11 21:10:53 +0200

    Core/Player - Fix https://github.com/TrinityCore/TrinityCore/issues/31782

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -1375,7 +1375,6 @@ bool Player::TeleportTo(TeleportLocation const& teleportLocation, TeleportToOpti
         SetSemaphoreTeleportNear(false);
         //setup delayed teleport flag
         SetDelayedTeleportFlag(IsCanDelayTeleport());
-        SetSemaphoreTeleportFar(true);
         //if teleport spell is cast in Unit::Update() func
         //then we need to delay it until update process will be finished
         if (IsHasDelayedTeleport())
@@ -1386,6 +1385,7 @@ bool Player::TeleportTo(TeleportLocation const& teleportLocation, TeleportToOpti
             m_teleportSpellId = teleportSpellId;
             return true;
         }
+        SetSemaphoreTeleportFar(true);
 
         SetSelection(ObjectGuid::Empty);
 
