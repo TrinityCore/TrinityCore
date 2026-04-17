fb48e74a965140532fe4c35af8fb224bf53ee63e
Nik <102656911+notwonderful@users.noreply.github.com>
2026-04-18 00:34:31 +0300

    DB/Valdrakken: Add ambient flying dragons

---
+++ sql/updates/world/master/2026_04_18_00_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_area` WHERE `spell` = 388530 AND `area` = 13862;
+INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_end`, `aura_spell`, `racemask`, `gender`, `flags`, `quest_start_status`, `quest_end_status`) VALUES
+(388530, 13862, 0, 0, 0, 0, 2, 3, 64, 11);
