c16c6c24fe62913d54dd71238eea0d5f1008d628
Naddley <NaddleyTC@gmail.com>
2026-04-19 12:38:08 +0200

    Scripts/Silvermoon: Implement Intro skip for Midnight

---
+++ src/server/scripts/QuelThalas/zone_silvermoon_city_midnight.cpp
@@ -0,0 +1,48 @@
+/*
+ * This file is part of the TrinityCore Project. See AUTHORS file for Copyright information
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Player.h"
+#include "ScriptMgr.h"
+#include "SpellAuraEffects.h"
+#include "SpellScript.h"
+
+namespace Scripts::QuelThalas::SilvermoonCityMidnight
+{
+
+// 1278767 - [DNT] Skip Midnight Intro - Flag Campaign
+class spell_silvermoon_skip_midnight_intro : public SpellScript
+{
+    void HandleHitTarget(SpellEffIndex /*effIndex*/)
+    {
+        if (Player* player = GetCaster()->ToPlayer())
+            player->CompleteQuest(90806);
+    }
+
+    void Register() override
+    {
+        OnEffectHitTarget += SpellEffectFn(spell_silvermoon_skip_midnight_intro::HandleHitTarget, EFFECT_0, SPELL_EFFECT_COMPLETE_CAMPAIGN);
+    }
+};
+}
+
+void AddSC_silvermoon_city_midnight()
+{
+    using namespace Scripts::QuelThalas::SilvermoonCityMidnight;
+
+    // Spells
+    RegisterSpellScript(spell_silvermoon_skip_midnight_intro);
+}
