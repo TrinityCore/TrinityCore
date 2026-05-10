jairoe05/tc_duplicate

Core/Packets: Added CMSG_TIME_SYNC_REQ and SMSG_TIME_SYNC_RESP packets

src/server/game/Server/WorldSession.h
@@ -109,6 +109,7 @@ namespace WorldPackets
     namespace Misc
     {
         class ViolenceLevel;
+        class TimeSyncResponse;
     }
 
     namespace Query
@@ -900,7 +901,7 @@ class WorldSession
         void HandleMoveSetCanFlyAckOpcode(WorldPacket& recvData);
         void HandleSetTitleOpcode(WorldPacket& recvData);
         void HandleRealmSplitOpcode(WorldPacket& recvData);
-        void HandleTimeSyncResp(WorldPacket& recvData);
+        void HandleTimeSyncResp(WorldPackets::Misc::TimeSyncResponse& packet);
         void HandleWhoisOpcode(WorldPacket& recvData);
         void HandleResetInstancesOpcode(WorldPacket& recvData);
         void HandleHearthAndResurrect(WorldPacket& recvData);

TrinityCore/TrinityCore

(cherry picked from commit eb7f4e79d60205bd037dd459a62b5ff4e1aabb65)

src/server/game/Server/WorldSession.h
@@ -219,6 +219,7 @@ namespace WorldPackets
 
     namespace Misc
     {
+        class TimeSyncResponse;
         class CompleteCinematic;
         class CompleteMovie;
         class NextCinematicCamera;
@@ -1057,7 +1058,7 @@ class TC_GAME_API WorldSession
         void HandleSetRaidDifficultyOpcode(WorldPacket& recvData);
         void HandleSetTitleOpcode(WorldPacket& recvData);
         void HandleRealmSplitOpcode(WorldPacket& recvData);
-        void HandleTimeSyncResponse(WorldPacket& recvData);
+        void HandleTimeSyncResponse(WorldPackets::Misc::TimeSyncResponse& packet);
         void HandleWhoIsOpcode(WorldPacket& recvData);
         void HandleResetInstancesOpcode(WorldPacket& recvData);
         void HandleInstanceLockResponse(WorldPacket& recvPacket);