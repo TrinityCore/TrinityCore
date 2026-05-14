jairoe05/tc_duplicate

Core/Packets: updated EquipmentSet save packets

src/server/game/Server/WorldSession.h
@@ -126,6 +126,11 @@ namespace WorldPackets
         class AttackStop;
     }
 
+    namespace EquipmentSet
+    {
+        class SaveEquipmentSet;
+    }
+
     namespace Guild
     {
         class QueryGuildInfo;
@@ -1095,7 +1100,7 @@ class WorldSession
         void HandleRemoveGlyph(WorldPacket& recvData);
         void HandleQueryInspectAchievements(WorldPacket& recvData);
         void HandleGuildAchievementProgressQuery(WorldPacket& recvData);
-        void HandleEquipmentSetSave(WorldPacket& recvData);
+        void HandleEquipmentSetSave(WorldPackets::EquipmentSet::SaveEquipmentSet& packet);
         void HandleEquipmentSetDelete(WorldPacket& recvData);
         void HandleEquipmentSetUse(WorldPacket& recvData);
         void HandleWorldStateUITimerUpdate(WorldPacket& recvData);

TrinityCore/TrinityCore

(cherry picked from commit abff9de719ac189fc82da87f077a24dba69cfdbb)

src/server/game/Server/WorldSession.h
@@ -164,6 +164,11 @@ namespace WorldPackets
         class SetSheathed;
     }
 
+    namespace EquipmentSet
+    {
+        class SaveEquipmentSet;
+    }
+
     namespace Guild
     {
         class QueryGuildInfo;
@@ -1179,7 +1184,7 @@ class TC_GAME_API WorldSession
         void HandleMirrorImageDataRequest(WorldPacket& recvData);
         void HandleRemoveGlyph(WorldPacket& recvData);
         void HandleQueryInspectAchievements(WorldPacket& recvData);
-        void HandleEquipmentSetSave(WorldPacket& recvData);
+        void HandleEquipmentSetSave(WorldPackets::EquipmentSet::SaveEquipmentSet& saveEquipmentSet);
         void HandleEquipmentSetDelete(WorldPacket& recvData);
         void HandleEquipmentSetUse(WorldPacket& recvData);
         void HandleWorldStateUITimerUpdate(WorldPackets::Misc::UITimeRequest& recvData);