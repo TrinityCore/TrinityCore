15655afd54959e39dbda8cc72574eef586838df8
taylorfrey529-ai <taylorfrey529@gmail.com>
2026-07-14 13:00:58 -0400

    Rewire: implement declarative JSON config loader

---
+++ src/server/scripts/Custom/Rewire/RewireConfig.cpp
@@ -0,0 +1,24 @@
+/*
+ * This file is part of the TrinityCore Project. See AUTHORS file for Copyright information
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ */
+
+#include "RewireConfig.h"
+
+#include <rapidjson/document.h>
+#include <rapidjson/error/en.h>
+#include <rapidjson/istreamwrapper.h>
+
+#include <fstream>
+#include <limits>
+#include <sstream>
+
+namespace Trinity::Rewire
+{
+namespace
+{
+bool ReadUnsigned(rapid
