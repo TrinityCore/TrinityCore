e2da95dee32858c83dcffd0de781df8cbaecb3ca
Shauren <shauren.trinity@gmail.com>
2026-07-24 00:37:23 +0200

    Scripts/Zul'Aman: Add Jan'alai Flame Breath cataclysm effect
    (cherry picked from commit 6bab7e2976bfba751ff008be62f4a52383a055a9)

---
+++ sql/updates/world/cata_classic/2026_08_12_58_world_2026_07_24_02_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_janalai_flame_breath';
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(43140,'spell_janalai_flame_breath');
