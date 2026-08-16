42f6a5e0a619412420c46d9ed479a56b2ee65fa9
Ovahlord <18347559+ovahlord@users.noreply.github.com>
2026-08-16 19:30:38 +0200

    DB/Creature: added missing vendor data to Jahi

---
+++ sql/updates/world/cata_classic/2026_08_16_02_world.sql
@@ -0,0 +1,7 @@
+DELETE FROM `npc_vendor` WHERE `entry`= 48884;
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `ExtendedCost`, `type`, `PlayerConditionID`, `IgnoreFiltering`, `VerifiedBuild`) VALUES
+(48884, 5, 4470, 0, 0, 1, 0, 0, 54737), -- 4470
+(48884, 4, 60335, 0, 0, 1, 0, 0, 54737), -- 60335
+(48884, 3, 58275, 0, 0, 1, 0, 0, 54737), -- 58275
+(48884, 2, 58274, 0, 0, 1, 0, 0, 54737), -- 58274
+(48884, 1, 159, 0, 0, 1, 0, 0, 54737); -- 159
