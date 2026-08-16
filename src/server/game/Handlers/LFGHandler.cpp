93b193c4cd2de6d917fc418a2c9cf3453fb9e5bd
Ovahlord <18347559+ovahlord@users.noreply.github.com>
2026-08-16 16:38:33 +0200

    Core/LFG: fixed displaying the first lfg reward in lfg window

--- src/server/game/Handlers/LFGHandler.cpp
+++ src/server/game/Handlers/LFGHandler.cpp
@@ -176,7 +176,7 @@ void WorldSession::SendLfgPlayerLockInfo()
         {
             if (Quest const* quest = sObjectMgr->GetQuestTemplate(reward->firstQuest))
             {
-                playerDungeonInfo.FirstReward = !GetPlayer()->CanRewardQuest(quest, false);
+                playerDungeonInfo.FirstReward = GetPlayer()->CanRewardQuest(quest, false);
                 if (!playerDungeonInfo.FirstReward)
                     quest = sObjectMgr->GetQuestTemplate(reward->otherQuest);
 
