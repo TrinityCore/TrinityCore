24ade9e1967f2600d4e72bf29fdbeb7f3652a760
Ovahlord <18347559+ovahlord@users.noreply.github.com>
2026-08-16 11:14:38 +0200

    DB/Quests: added level requirements for Hero's Call and Warchief's Command breadcrumb quests

---
+++ sql/updates/world/cata_classic/2026_08_16_00_world.sql
@@ -0,0 +1,84 @@
+DELETE FROM `quest_template_addon` WHERE `ID` IN (28709, 28702, 28666, 28578, 28563, 28504, 28509, 28510, 28554, 28532, 28711);
+INSERT INTO `quest_template_addon` (`ID`) VALUES
+(28709),
+(28702),
+(28666),
+(28578),
+(28563),
+(28504),
+(28509),
+(28510),
+(28554),
+(28532),
+(28711);
+
+UPDATE `quest_template_addon` SET `MaxLevel`= 14 WHERE `ID`= 26378;
+UPDATE `quest_template_addon` SET `MaxLevel`= 19 WHERE `ID`= 26365;
+UPDATE `quest_template_addon` SET `MaxLevel`= 47 WHERE `ID`= 28579;
+UPDATE `quest_template_addon` SET `MaxLevel`= 59 WHERE `ID`= 28673;
+UPDATE `quest_template_addon` SET `MaxLevel`= 29 WHERE `ID`= 28699;
+UPDATE `quest_template_addon` SET `MaxLevel`= 24 WHERE `ID`= 28565;
+UPDATE `quest_template_addon` SET `MaxLevel`= 24 WHERE `ID`= 28564;
+UPDATE `quest_template_addon` SET `MaxLevel`= 34 WHERE `ID`= 28550;
+UPDATE `quest_template_addon` SET `MaxLevel`= 49 WHERE `ID`= 28543;
+UPDATE `quest_template_addon` SET `MaxLevel`= 39 WHERE `ID`= 28576;
+UPDATE `quest_template_addon` SET `MaxLevel`= 39 WHERE `ID`= 28576;
+UPDATE `quest_template_addon` SET `MaxLevel`= 19 WHERE `ID`= 28563;
+UPDATE `quest_template_addon` SET `MaxLevel`= 69 WHERE `ID`= 28708;
+UPDATE `quest_template_addon` SET `MaxLevel`= 34 WHERE `ID`= 26542;
+UPDATE `quest_template_addon` SET `MaxLevel`= 29 WHERE `ID`= 28539;
+UPDATE `quest_template_addon` SET `MaxLevel`= 19 WHERE `ID`= 28490;
+UPDATE `quest_template_addon` SET `MaxLevel`= 44 WHERE `ID`= 28578;
+UPDATE `quest_template_addon` SET `MaxLevel`= 44 WHERE `ID`= 28503;
+UPDATE `quest_template_addon` SET `MaxLevel`= 51 WHERE `ID`= 28666;
+UPDATE `quest_template_addon` SET `MaxLevel`= 29 WHERE `ID`= 28573;
+UPDATE `quest_template_addon` SET `MaxLevel`= 54 WHERE `ID`= 28525;
+UPDATE `quest_template_addon` SET `MaxLevel`= 14 WHERE `ID`= 28562;
+UPDATE `quest_template_addon` SET `MaxLevel`= 19 WHERE `ID`= 28559;
+UPDATE `quest_template_addon` SET `MaxLevel`= 53 WHERE `ID`= 28675;
+UPDATE `quest_template_addon` SET `MaxLevel`= 79 WHERE `ID`= 28709;
+UPDATE `quest_template_addon` SET `MaxLevel`= 49 WHERE `ID`= 28507;
+UPDATE `quest_template_addon` SET `MaxLevel`= 24 WHERE `ID`= 28492;
+UPDATE `quest_template_addon` SET `MaxLevel`= 24 WHERE `ID`= 26728;
+UPDATE `quest_template_addon` SET `MaxLevel`= 54 WHERE `ID`= 28544;
+UPDATE `quest_template_addon` SET `MaxLevel`= 19 WHERE `ID`= 28567;
+UPDATE `quest_template_addon` SET `MaxLevel`= 50 WHERE `ID`= 28582;
+UPDATE `quest_template_addon` SET `MaxLevel`= 39 WHERE `ID`= 28511;
+UPDATE `quest_template_addon` SET `MaxLevel`= 39 WHERE `ID`= 28552;
+UPDATE `quest_template_addon` SET `MaxLevel`= 34 WHERE `ID`= 28551;
+UPDATE `quest_template_addon` SET `MaxLevel`= 34 WHERE `ID`= 28702;
+UPDATE `quest_template_addon` SET `MaxLevel`= 59 WHERE `ID`= 28528;
+UPDATE `quest_template_addon` SET `MaxLevel`= 34 WHERE `ID`= 28531;
+UPDATE `quest_template_addon` SET `MaxLevel`= 39 WHERE `ID`= 28510;
+UPDATE `quest_template_addon` SET `MaxLevel`= 49 WHERE `ID`= 28509;
+UPDATE `quest_template_addon` SET `MaxLevel`= 69 WHERE `ID`= 28705;
+UPDATE `quest_template_addon` SET `MaxLevel`= 39 WHERE `ID`= 28575;
+UPDATE `quest_template_addon` SET `MaxLevel`= 19 WHERE `ID`= 28494;
+UPDATE `quest_template_addon` SET `MaxLevel`= 19 WHERE `ID`= 28560;
+UPDATE `quest_template_addon` SET `MaxLevel`= 47 WHERE `ID`= 28580;
+UPDATE `quest_template_addon` SET `MaxLevel`= 34 WHERE `ID`= 28548;
+UPDATE `quest_template_addon` SET `MaxLevel`= 79 WHERE `ID`= 28711;
+UPDATE `quest_template_addon` SET `MaxLevel`= 39 WHERE `ID`= 28554;
+UPDATE `quest_template_addon` SET `MaxLevel`= 59 WHERE `ID`= 28671;
+UPDATE `quest_template_addon` SET `MaxLevel`= 54 WHERE `ID`= 28526;
+UPDATE `quest_template_addon` SET `MaxLevel`= 51 WHERE `ID`= 28667;
+UPDATE `quest_template_addon` SET `MaxLevel`= 34 WHERE `ID`= 28549;
+UPDATE `quest_template_addon` SET `MaxLevel`= 19 WHERE `ID`= 28568;
+UPDATE `quest_template_addon` SET `MaxLevel`= 34 WHERE `ID`= 28574;
+UPDATE `quest_template_addon` SET `MaxLevel`= 19 WHERE `ID`= 28496;
+UPDATE `quest_template_addon` SET `MaxLevel`= 44 WHERE `ID`= 28504;
+UPDATE `quest_template_addon` SET `MaxLevel`= 29 WHERE `ID`= 28688;
+UPDATE `quest_template_addon` SET `MaxLevel`= 24 WHERE `ID`= 28571;
+UPDATE `quest_template_addon` SET `MaxLevel`= 24 WHERE `ID`= 28089;
+UPDATE `quest_template_addon` SET `MaxLevel`= 24 WHERE `ID`= 28493;
+UPDATE `quest_template_addon` SET `MaxLevel`= 34 WHERE `ID`= 28704;
+UPDATE `quest_template_addon` SET `MaxLevel`= 54 WHERE `ID`= 28545;
+UPDATE `quest_template_addon` SET `MaxLevel`= 29 WHERE `ID`= 28572;
+UPDATE `quest_template_addon` SET `MaxLevel`= 53 WHERE `ID`= 28677;
+UPDATE `quest_template_addon` SET `MaxLevel`= 50 WHERE `ID`= 28581;
+UPDATE `quest_template_addon` SET `MaxLevel`= 29 WHERE `ID`= 28619;
+UPDATE `quest_template_addon` SET `MaxLevel`= 59 WHERE `ID`= 28527;
+UPDATE `quest_template_addon` SET `MaxLevel`= 49 WHERE `ID`= 28542;
+UPDATE `quest_template_addon` SET `MaxLevel`= 44 WHERE `ID`= 28577;
+UPDATE `quest_template_addon` SET `MaxLevel`= 29 WHERE `ID`= 28532;
+UPDATE `quest_template_addon` SET `MaxLevel`= 19 WHERE `ID`= 26964;
