ea04f7952c5624c4177994573e4c7e74baa7131d
Ovahlord <18347559+ovahlord@users.noreply.github.com>
2026-08-31 22:06:34 +0200

    DB/Quests: added QUEST_SPECIAL_FLAGS_DF_QUEST to Cataclysm lfg reward quests

---
+++ sql/updates/world/cata_classic/2026_08_31_02_world.sql
@@ -0,0 +1,12 @@
+DELETE FROM `quest_template_addon` WHERE `ID` IN (28905, 28906, 28907, 28908, 30110, 30111, 29185, 29183, 86553, 86554);
+INSERT INTO `quest_template_addon` (`ID`, `SpecialFlags`) VALUES
+(28905, 0x8),
+(28906, 0x8),
+(28907, 0x8),
+(28908, 0x8),
+(30110, 0x8),
+(30111, 0x8),
+(29185, 0x8),
+(29183, 0x8),
+(86553, 0x8),
+(86554, 0x8);
