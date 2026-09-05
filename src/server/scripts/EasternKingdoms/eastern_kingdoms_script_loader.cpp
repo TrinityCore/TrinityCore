0596f2ab06e6419ea128793f085479a36f62fd65
Naddley <64811442+Naddley@users.noreply.github.com>
2026-09-05 15:36:56 +0200

    Scripts/Vashjir: Implement Worldboss Mobus (#31995)
    * Scripts/Vashjir: Implement Worldboss Mobus
    
    * Update
    
    * remove reset()
    
    Co-authored-by: ModoX <moardox@gmail.com>
    
    * Prep merge
    
    ---------
    
    Co-authored-by: ModoX <moardox@gmail.com>
    Co-authored-by: Ovahlord <18347559+Ovahlord@users.noreply.github.com>

--- src/server/scripts/EasternKingdoms/eastern_kingdoms_script_loader.cpp
+++ src/server/scripts/EasternKingdoms/eastern_kingdoms_script_loader.cpp
@@ -216,8 +216,9 @@ void AddSC_boss_shade_of_medivh();
 
 // Vashjir
 void AddSC_vashjir();
-void AddSC_vashjir_chapter_1_defense_of_the_briny_cutter();
 void AddSC_abyssal_depths();
+void AddSC_worldboss_mobus();
+void AddSC_vashjir_chapter_1_defense_of_the_briny_cutter();
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
@@ -423,6 +424,7 @@ void AddEasternKingdomsScripts()
 
     // Vashjir
     AddSC_vashjir();
-    AddSC_vashjir_chapter_1_defense_of_the_briny_cutter();
     AddSC_abyssal_depths();
+    AddSC_worldboss_mobus();
+    AddSC_vashjir_chapter_1_defense_of_the_briny_cutter();
 }
