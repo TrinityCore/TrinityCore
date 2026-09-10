752f3fed8f195b987a609b3c66ac685cf149822b
imstuckbro <frenlock@gmail.com>
2026-09-10 14:26:51 +0200

    Misc: Few spelling improvements and config updates (#31919)
    (cherry picked from commit 463441351d40e0d139d1db87847a3c6bbf5a343b)

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -557,7 +557,7 @@ void World::LoadConfigSettings(bool reload)
 
     ///- Read the player limit and the Message of the day from the config file
     SetPlayerAmountLimit(sConfigMgr->GetIntDefault("PlayerLimit", 100));
-    SetMotd(sConfigMgr->GetStringDefault("Motd", "Welcome to a Trinity Core Server."));
+    SetMotd(sConfigMgr->GetStringDefault("Motd", "Welcome to a TrinityCore Server."));
 
     ///- Read support system setting from the config file
     m_bool_configs[CONFIG_SUPPORT_ENABLED] = sConfigMgr->GetBoolDefault("Support.Enabled", true);
