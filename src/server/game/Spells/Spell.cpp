3a728b3e1ed91a5b2703a85ee383a49c8bdf1ea9
Shauren <shauren.trinity@gmail.com>
2026-09-14 15:50:00 +0200

    prepare merge

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -3322,14 +3322,6 @@ void Spell::cancel(SpellCastResult result /*= SPELL_FAILED_INTERRUPTED*/, Option
                         unit->RemoveOwnedAura(m_spellInfo->Id, m_originalCasterGUID, 0, AURA_REMOVE_BY_CANCEL);
 
             SendChannelUpdate(0);
-
-            if (m_originalCaster)
-            {
-                GameObject* ritual = m_originalCaster->GetGameObject(m_spellInfo->Id);
-                if (ritual && ritual->GetGoType() == GAMEOBJECT_TYPE_RITUAL)
-                    SendCastResult(result);
-            }
-
             SendInterrupted(result, resultOther);
 
             m_appliedMods.clear();
@@ -3347,7 +3339,14 @@ void Spell::cancel(SpellCastResult result /*= SPELL_FAILED_INTERRUPTED*/, Option
     {
         m_originalCaster->RemoveDynObject(m_spellInfo->Id);
         if (m_spellInfo->IsChanneled()) // if not channeled then the object for the current cast wasn't summoned yet
-            m_originalCaster->RemoveGameObject(m_spellInfo->Id, true);
+        {
+            if (GameObject* gameObject = m_originalCaster->GetGameObject(m_spellInfo->Id))
+            {
+                m_originalCaster->RemoveGameObject(gameObject, true);
+                if (gameObject->GetGoType() == GAMEOBJECT_TYPE_RITUAL)
+                    m_originalCaster->GetSpellHistory()->ResetCooldown(m_spellInfo->Id, true);
+            }
+        }
     }
 
     //set state back so finish will be processed
