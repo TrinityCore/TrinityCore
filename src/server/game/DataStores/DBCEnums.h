8f9e0f085c4df5201eb6bccdae5cedcbe803bb38
Shauren <shauren.trinity@gmail.com>
2026-09-14 12:48:46 +0200

    Core/AreaTriggers: Update areatrigger shape enum

--- src/server/game/DataStores/DBCEnums.h
+++ src/server/game/DataStores/DBCEnums.h
@@ -196,11 +196,13 @@ enum class AreaTriggerShapeType : int8
 {
     Sphere                         = 0,
     Box                            = 1,
-    Unk                            = 2,
+    Quad2D                         = 2,
     Polygon                        = 3,
     Cylinder                       = 4,
-    Disk                           = 5,
-    BoundedPlane                   = 6,
+    Script                         = 5,
+    FromUnit                       = 6,
+    Disk                           = 7,
+    BoundedPlane                   = 8,
     Max
 };
 
