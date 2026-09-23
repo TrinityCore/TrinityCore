6155a12ae8f4b03f459c093440cfff05f6d504e4
Aqua Deus <95978183+aquadeus@users.noreply.github.com>
2026-09-24 00:19:11 +0200

    Core/Auras: Implement gravity opcodes and SPELL_AURA_MOD_GRAVITY (#31997)

--- src/server/game/Entities/Unit/Unit.h
+++ src/server/game/Entities/Unit/Unit.h
@@ -1192,6 +1192,8 @@ class TC_GAME_API Unit : public WorldObject
         void ApplyInertia(int32 id, Milliseconds duration);
         void RemoveInertia(int32 id);
 
+        void SetGravityModifier(float gravityModifier);
+
         void SetInFront(WorldObject const* target);
         void SetFacingTo(float ori, bool force = true, uint32 movementId = EVENT_FACE);
         void SetFacingToObject(WorldObject const* object, bool force = true, uint32 movementId = EVENT_FACE);
